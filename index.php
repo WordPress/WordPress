c6224fdd21b1c83e3c5145e445112483dfc04bfc
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2003-04-01 14:20:21 +0000

    Some semantic updates
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- index.php
+++ index.php
@@ -7,13 +7,12 @@
 <title><?php bloginfo('name') ?><?php single_post_title(' :: ') ?><?php single_cat_title(' :: ') ?><?php single_month_title(' :: ') ?></title>
 
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<meta http-equiv="reply-to" content="<?php bloginfo('admin_email'); ?>" />
 <meta http-equiv="imagetoolbar" content="no" />
-<meta content="TRUE" name="MSSmartTagsPreventParsing" />
 
 <style type="text/css" media="screen">
-@import url( layout2b.css );
+	@import url( layout2b.css );
 </style>
+
 <link rel="stylesheet" type="text/css" media="print" href="print.css" />
 <link rel="alternate" type="text/xml" title="RDF" href="<?php bloginfo('rdf_url'); ?>" />
 <link rel="alternate" type="text/xml" title="RSS" href="<?php bloginfo('rss2_url'); ?>" />
@@ -22,21 +21,21 @@
 
 </head>
 <body>
-<div id="header"><a href="" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></div>
+<h1 id="header"><a href="" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></h1>
 
 <div id="content">
 
 
 <!-- // b2 loop start -->
-	<?php while($row = mysql_fetch_object($result)) { start_b2(); ?>
+<?php while($row = mysql_fetch_object($result)) { start_b2(); ?>
 
 
 <?php the_date("","<h2>","</h2>"); ?>
 <?php permalink_anchor(); ?>
-<div class="storyTitle"><?php the_title(); ?>
+<h3 class="storyTitle"><?php the_title(); ?>
    <a href="?cat=<?php the_category_ID() ?>" title="category: <?php the_category() ?>"><span class="storyCategory">[<?php the_category() ?>]</span></a>&nbsp;-&nbsp;
 <span class="storyAuthor"><?php the_author() ?> - <?php the_author_email() ?></span> @ <a href="<?php permalink_link() ?>"><?php the_time() ?></a>
-</div>
+</h3>
 
 <div class="storyContent">
 <?php the_content(); ?>
@@ -78,8 +77,8 @@
 
 <h4>quick links:</h4>
 
+<a href="http://wordpress.org" title="Wordpress Fork">Wordpress</a><br />
 <a href="http://www.cafelog.com" title="b2's homepage">cafelog.com</a><br />
-<a href="http://some other site" title="another link">another link</a><br />
 <a href="http://some other site" title="another link">another link</a><br />
 <a href="http://some other site" title="another link">another link</a><br />
 
@@ -91,10 +90,10 @@
 <h4>search:</h4>
 
 <form name="searchform" method="get" action="<?php echo $PHP_SELF; /*$siteurl."/".$blogfilename*/ ?>">
-<p>
+<div>
 <input type="text" name="s" size="15" /><br />
 <input type="submit" name="submit" value="search" />
-</p>
+</div>
 </form>
 
 <h4>archives:</h4>
