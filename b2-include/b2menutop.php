55e3b9aab1353ad5fa6d0b91d9432dcd538b83c6
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2003-05-21 12:47:27 +0000

    New admin interface start.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@30 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- b2-include/b2menutop.php
+++ b2-include/b2menutop.php
@@ -1,31 +1,9 @@
-<img src="b2-img/blank.gif" width="1" height="5" alt="" border="0" />
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr height="15">
-
-<td height="15" width="20"><img src="b2-img/blank.gif" width="1" height="1" alt="" /></td>
-
-<td rowspan="3" width="50" valign="top"><a href="http://cafelog.com/"><img src="b2-img/b2minilogo.png" width="50" height="50" border="0" alt="visit b2's website" style="border-width:1px; border-color: #999999; border-style: dashed" /></a></td>
-
-<td><img src="b2-img/blank.gif" width="1" height="1" alt="" /></td>
-<td width="150" style="text-align: right; padding-rightt: 6px;">
-<span style="color: #b0b0b0; font-family: verdana, arial, helvetica; font-size: 10px;">logged in as : <b><?php echo $user_login; ?></b></span>
-</td>
-
-</tr>
-<tr>
-
-<td class="menutop" width="20">&nbsp;
-</td>
-
-<td class="menutop"<?php if ($is_NS4) { echo " width=\"500\""; } ?>>
-<div class="menutop"<?php if ($is_NS4) { echo " width=\"500\""; } ?>>
-
-
-<?php if ($is_NS4) { echo $HTTP_USER_AGENT; } ?>
-
-
-<?php $sep = "&nbsp;&nbsp;|&nbsp;&nbsp;"; ?>
-&nbsp;<a href="b2edit.php" class="menutop" style="-font-weight: bold;">Post / Edit</a><?php echo $sep ?><a href="javascript:profile(<?php echo $user_ID ?>)" class="menutop">My Profile</a><?php echo $sep ?><a href="b2team.php" class="menutop">Team</a><?php
+<h1 id="wphead"><a href="http://wordpress.org" rel="external"><span>WordPress</span></a></h1> 
+<ul id="adminmenu">
+  <li><a href="b2edit.php"><strong>Post / Edit</strong></a></li>
+  <li><a href="javascript:profile(<?php echo $user_ID ?>)">My Profile</a></li>
+  <li><a href="b2team.php">Team</a></li>
+    <?php
 
 if ($pagenow != "b2profile.php") {
 
@@ -35,7 +13,7 @@ $j=$menu[0];
 while ($j != "") {
 	$k = explode("\t",$j);
 	if ($user_level >= $k[0]) {
-		echo "$sep<a href=\"".$k[1]."\" class=\"menutop\">".trim($k[2])."</a>";
+		echo "\n<li><a href='".$k[1]."'>".trim($k[2]).'</a></li>';
 	}
 	$i=$i+1;
 	$j=$menu[$i];
@@ -45,23 +23,8 @@ while ($j != "") {
 
 }
 ?>
+<li><a href="<?php echo $siteurl."/".$blogfilename; ?>">View site</a></li>
+<li><a href="b2login.php?action=logout">Logout</a></li>
+</ul>
 
-</div>
-</td>
-
-<td width="150" class="menutop" align="right" bgcolor="#FF9900">
-<a href="<?php echo $siteurl."/".$blogfilename; ?>" class="menutop">View site</a>
-<?php echo $sep; ?>
-<a href="b2login.php?action=logout" class="menutop">Logout</a>
-</td>
-
-</tr>
-<tr>
-
-<td>&nbsp;</td>
-<td style="padding-left: 6px;"><span class="menutoptitle">:: <?php echo $title; ?></span></td>
-<td>&nbsp;</td>
-
-</tr>
-
-</table>
+<h2><?php echo $title; ?></h2>
