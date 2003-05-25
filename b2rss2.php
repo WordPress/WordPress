ac5b52f2291db7166aa75a7c28f5bbc0e9b21bc1
mikelittle <mikelittle@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2003-05-25 21:10:25 +0000

    Removed rogue quote
    git-svn-id: http://svn.automattic.com/wordpress/trunk@97 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- b2rss2.php
+++ b2rss2.php
@@ -2,7 +2,7 @@
 		   In every template you do, you got to copy them before the CafeLog 'loop' */
 $blog=1; // enter your blog's ID
 header('Content-type: text/xml');
-include('blog.header.php"');
+include('blog.header.php');
 if (!isset($rss_language)) { $rss_language = 'en'; }
 if (!isset($rss_encoded_html)) { $rss_encoded_html = 0; }
 if (!isset($rss_excerpt_length) || ($rss_encoded_html == 1)) { $rss_excerpt_length = 0; }
