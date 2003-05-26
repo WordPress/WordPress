0c41ba83057126bfa2414083a1d596cb5e25e010
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2003-05-26 13:31:55 +0000

    Earlier path fix that was missed.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@107 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- blog.header.php
+++ blog.header.php
@@ -4,13 +4,9 @@ $use_cache = 1;
 $use_gzipcompression = 1;
 
 /* Including config and functions files */
-/* Including config and functions files */
 $curpath = dirname(__FILE__).'/';
-require_once ($curpath.'/b2config.php');
 
-if (($b2inc{0} == '/') || ($b2inc{1} == ':')) {
-	$curpath='./';
-}
+require_once ($curpath.'/b2config.php');
 require_once ($curpath.$b2inc.'/b2template.functions.php');
 require_once ($curpath.$b2inc.'/b2vars.php');
 require_once ($curpath.$b2inc.'/b2functions.php');
