af360561eadc49a371e1a91071bc3b7bd2f1aa30
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2003-05-29 14:05:37 +0000

    Major comments overhaul: decreased # of queries, combined *backs and comments, and made ezSQL mod.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@115 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- b2comments.php
+++ b2comments.php
@@ -1,46 +1,42 @@
-	<?php // Do not delete these lines
-	if (basename($HTTP_SERVER_VARS["SCRIPT_FILENAME"]) == "b2comments.php")
-		die ("please, do not load this page directly");
+<?php // Do not delete these lines
+	if ('b2comments.php' == basename($HTTP_SERVER_VARS['SCRIPT_FILENAME']))
+		die ('Please do not load this page directly. Thanks!');
 	if (($withcomments) or ($c)) {
 
 		$comment_author = (empty($HTTP_COOKIE_VARS["comment_author"])) ? "name" : $HTTP_COOKIE_VARS["comment_author"];
 		$comment_author_email = (empty($HTTP_COOKIE_VARS["comment_author"])) ? "email" : trim($HTTP_COOKIE_VARS["comment_author_email"]);
 		$comment_author_url = (empty($HTTP_COOKIE_VARS["comment_author"])) ? "url" : trim($HTTP_COOKIE_VARS["comment_author_url"]);
 
-	$queryc = "SELECT * FROM $tablecomments WHERE comment_post_ID = $id AND comment_content NOT LIKE '%<trackback />%' AND comment_content NOT LIKE '%<pingback />%' ORDER BY comment_date";
-	$resultc = mysql_query($queryc);
-	if ($resultc) {
-	?>
+	$comments = $wpdb->get_results("SELECT * FROM $tablecomments WHERE comment_post_ID = $id ORDER BY comment_date");
+?>
 
 <!-- you can start editing here -->
 
 <h2>Comments</h2>
+<p>The URL to TrackBack this entry is:</p>
+<p><em><?php trackback_url() ?></em></p>
+
 <ol id="comments">
-	<?php /* this line is b2's motor, do not delete it */ $wxcvbn_c=0; while($rowc = mysql_fetch_object($resultc)) { $wxcvbn_c++; $commentdata = get_commentdata($rowc->comment_ID); ?>
+<?php 
+// this line is WordPress' motor, do not delete it.
+if ($comments) {
+	foreach ($comments as $comment) {
+?>
 	
-<!-- comment -->
 <li id="comment-<?php comment_ID() ?>">
 <?php comment_text() ?>
-<p><cite>By <?php if ($commentdata["comment_author_url"] && $commentdata["comment_author_url"] != 'http://url') {
-	echo <<<QQQ
-<a href="{$commentdata["comment_author_url"]}">{$commentdata["comment_author"]}</a>
-QQQ;
-} else {
-	echo $commentdata["comment_author"];
-} ?> <?php comment_date() ?> @ <a href="#comment-<?php comment_ID() ?>"><?php comment_time() ?></a></cite></p>
+<p><cite><?php comment_type(); ?> by <?php comment_author_link() ?> <?php comment_date() ?> @ <a href="#comment-<?php comment_ID() ?>"><?php comment_time() ?></a></cite></p>
 </li>
-<!-- /comment -->
-	<?php /* end of the loop, don't delete */ } if (!$wxcvbn_c) { ?>
 
-<!-- this is displayed if there are no comments so far -->
+<?php } // end for each comment
+} else { // this is displayed if there are no comments so far 
+?>
 	<li>No comments yet.</li>
-
-	<?php /* if you delete this the sky will fall on your head */ } ?>
+<?php } ?>
 </ol>
 <h2>Leave a Comment</h2>
 <p>Line and paragraph breaks automatic, website trumps email, <acronym title="Hypertext Markup Language">HTML</acronym> allowed: <?php echo htmlentities($comment_allowed_tags); ?></p>
 
-<!-- form to add a comment -->
 
 <form action="<?php echo $siteurl; ?>/b2comments.post.php" method="post" id="commentform">
 	<p>
@@ -52,16 +48,16 @@ QQQ;
 
 	<p>
 	  <input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="28" tabindex="2" />
-	   <label for="email">email</label>
+	   <label for="email">Email</label>
 	</p>
 
 	<p>
 	  <input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="28" tabindex="3" />
-	   <label for="url"><acronym title="Uniform Resource Locator">url</acronym></label>
+	   <label for="url"><acronym title="Uniform Resource Locator">URL</acronym></label>
 	</p>
 
 	<p>
-	  <label for="comment">your comment</label>
+	  <label for="comment">Your Comment</label>
 	<br />
 	  <textarea name="comment" id="comment" cols="70" rows="4" tabindex="4"></textarea>
 	</p>
@@ -69,16 +65,11 @@ QQQ;
 	<p>
 	  <input name="submit" type="submit" tabindex="5" value="Say it!" />
 	</p>
-
 </form>
 
-<!-- /form -->
 
 <div><a href="javascript:history.go(-1)">Go back</a>.</div>
 
 <?php // if you delete this the sky will fall on your head
-}
-} else {
-	return false;
 }
 ?>
