52622808bdab82fc8bd1a36a774babd063612977
mikelittle <mikelittle@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2003-08-03 00:01:07 +0000

    Added blog name to page title. http://wordpress.org/support/4/391
    git-svn-id: http://svn.automattic.com/wordpress/trunk@286 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/b2header.php
+++ wp-admin/b2header.php
@@ -47,7 +47,7 @@ if ($standalone == 0) {
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>WordPress > <?php echo $title; ?></title>
+<title>WordPress > <?php bloginfo('name') ?> > <?php echo $title; ?></title>
 <link rel="stylesheet" href="b2.css" type="text/css" />
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 
