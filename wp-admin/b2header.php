81ab3dfe9b2bf15127ed3dc1388acd4afeb2b989
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2003-10-13 05:15:47 +0000

    Nice seperators.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@446 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/b2header.php
+++ wp-admin/b2header.php
@@ -55,7 +55,7 @@ if ($standalone == 0) {
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>WordPress > <?php bloginfo('name') ?> > <?php echo $title; ?></title>
+<title>WordPress &rsaquo; <?php bloginfo('name') ?> &rsaquo; <?php echo $title; ?></title>
 <link rel="stylesheet" href="wp-admin.css" type="text/css" />
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $admin_area_charset ?>" />
 <?php
