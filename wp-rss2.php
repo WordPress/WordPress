8e7909ad046a1d7c4366fb189adc9cd22baa625b
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2003-11-10 07:44:50 +0000

    Newer, cleaner RSS 2.0.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@533 1a063a9b-81f0-0310-95a4-ce76da25c4cd

---
+++ wp-rss2.php
@@ -0,0 +1,82 @@
+<?php 
+$blog=1;
+$doing_rss=1;
+header('Content-type: text/xml');
+
+include('blog.header.php');
+
+// Handle Conditional GET
+
+// Get the time of the most recent article
+$sql = "SELECT max(post_date) FROM $tableposts";
+
+$maxdate = $wpdb->get_var($sql);
+
+$unixtime = strtotime($maxdate);
+
+// format timestamp for Last-Modified header
+$clast = gmdate("D, d M Y H:i:s \G\M\T",$unixtime);
+$cetag = md5($last);
+
+$slast = $_SERVER['HTTP_IF_MODIFIED_SINCE'];
+$setag = $_SERVER['HTTP_IF_NONE_MATCH'];
+
+// send it in a Last-Modified header
+header("Last-Modified: " . $clast, true);
+header("Etag: " . $cetag, true);
+
+// compare it to aggregator's If-Modified-Since and If-None-Match headers
+// if they match, send a 304 and die
+
+// This logic says that if only one header is provided, just use that one,
+// but if both headers exist, they *both* must match up with the locally
+// generated values.
+//if (($slast?($slast == $clast):true) && ($setag?($setag == $cetag):true)){
+if (($slast && $setag)?(($slast == $clast) && ($setag == $cetag)):(($slast == $clast) || ($setag == $cetag))) { 
+	header("HTTP/1.1 304 Not Modified");
+	echo "\r\n\r\n";
+	exit;
+}
+
+if (!isset($rss_language)) { $rss_language = 'en'; }
+if (!isset($rss_encoded_html)) { $rss_encoded_html = 0; }
+if (!isset($rss_excerpt_length) || ($rss_encoded_html == 1)) { $rss_excerpt_length = 0; }
+?>
+<?php echo "<?xml version=\"1.0\"?".">"; ?>
+<!-- generator="wordpress/<?php echo $b2_version ?>" -->
+<rss version="2.0" 
+	xmlns:content="http://purl.org/rss/1.0/modules/content/">
+
+<channel>
+	<title><?php bloginfo_rss("name") ?></title>
+	<link><?php bloginfo_rss("url") ?></link>
+	<description><?php bloginfo_rss("description") ?></description>
+	<language><?php echo $rss_language ?></language>
+	<copyright>Copyright <?php echo mysql2date('Y', get_lastpostdate()); ?></copyright>
+	<pubDate><?php echo gmdate('r'); ?></pubDate>
+	<generator>http://wordpress.org/?v=<?php echo $b2_version ?>"</generator>
+
+	<?php $items_count = 0; if ($posts) { foreach ($posts as $post) { start_b2(); ?>
+	<item>
+		<title><?php the_title_rss() ?></title>
+		<link><?php permalink_single_rss() ?></link>
+		<comments><?php comments_link(); ?></comments>
+		<pubDate><?php the_time('r'); ?></pubDate>
+		<author><?php the_author() ?> (mailto:<?php the_author_email() ?>)</author>
+		<category><?php the_category_rss() ?></category>
+		<guid isPermaLink="false"><?php echo $id; ?>@<?php bloginfo_rss("url") ?></guid>
+<?php $more = 1; if ($rss_use_excerpt) {
+?>
+		<description><?php the_excerpt_rss($rss_excerpt_length, 2) ?></description>
+<?php
+} else { // use content
+?>
+		<description><?php the_content_rss('', 0, '', $rss_excerpt_length, 2) ?></description>
+<?php
+} // end else use content
+?>
+		<content:encoded><![CDATA[<?php the_content('', 0, '') ?>]]></content:encoded>
+	</item>
+	<?php $items_count++; if (($items_count == $posts_per_rss) && empty($m)) { break; } } } ?>
+</channel>
+</rss>
