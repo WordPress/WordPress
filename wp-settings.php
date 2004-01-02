9b09572d57fd397ef925dcbf390bfb70009fba3d
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-01-02 23:55:40 +0000

    Turn down error reporting.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@692 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-settings.php
+++ wp-settings.php
@@ -3,6 +3,9 @@ $HTTP_HOST = getenv('HTTP_HOST');  /* domain name */
 $REMOTE_ADDR = getenv('REMOTE_ADDR'); /* visitor's IP */
 $HTTP_USER_AGENT = getenv('HTTP_USER_AGENT'); /* visitor's browser */
 
+// Change to E_ALL for development/debugging
+error_reporting(E_ALL ^ E_NOTICE);
+
 // Table names
 $tableposts               = $table_prefix . 'posts';
 $tableusers               = $table_prefix . 'users';
