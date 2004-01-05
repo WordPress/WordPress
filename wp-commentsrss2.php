e3091a372cf693ce1ff6ed5a0f1419c9fddbbcad
alex_t_king <alex_t_king@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-01-05 09:01:41 +0000

    removed check to post_category > 0
    git-svn-id: http://svn.automattic.com/wordpress/trunk@722 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-commentsrss2.php
+++ wp-commentsrss2.php
@@ -55,7 +55,6 @@ foreach ($posts as $post) { start_wp();
 											WHERE comment_post_ID = '$id'
 											AND $tablecomments.comment_approved = '1'
 											AND $tableposts.post_status = 'publish'
-											AND post_category > '0'
 											AND post_date < '".date("Y-m-d H:i:s")."' 
 											ORDER BY comment_date 
 											LIMIT $posts_per_rss");
@@ -74,7 +73,6 @@ foreach ($posts as $post) { start_wp();
 											LEFT JOIN $tableposts ON comment_post_id = id
 											WHERE $tableposts.post_status = 'publish'
 											AND $tablecomments.comment_approved = '1'
-											AND post_category > '0'
 											AND post_date < '".date("Y-m-d H:i:s")."' 
 											ORDER BY comment_date DESC
 											LIMIT $posts_per_rss");
