3f58a8429a783639fc8ebd72c0b7157ef9e55a4c
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-01-12 13:13:06 +0000

    Removed ref to missing files. This upload thing needs to be looked at and redone.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@765 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/upload.php
+++ wp-admin/upload.php
@@ -1,5 +1,5 @@
 <?php
-/* b2 File Upload - original hack by shockingbird.com */
+/* WP File Upload - original hack by shockingbird.com */
 
 $standalone="1";
 require_once("./admin-header.php");
@@ -14,19 +14,11 @@ $allowed_types = explode(" ", trim($fileupload_allowedtypes));
 
 ?><html>
 <head>
-<title>WordPress :: upload images/files</title>
-<link rel="stylesheet" href="<?php echo WPINC; ?>/b2.css" type="text/css">
+<title>WordPress &raquo; Upload images/files</title>
 <style type="text/css">
 <!--
 body {
-	background-image: url('<?php
-if ($is_gecko || $is_macIE) {
-?>../wp-images/bgbookmarklet3.gif<?php
-} else {
-?>../wp-images/bgbookmarklet3.gif<?php
-}
-?>');
-	background-repeat: no-repeat;
+
 	margin: 30px;
 }
 <?php
@@ -34,13 +26,7 @@ if (!$is_NS4) {
 ?>
 textarea,input,select {
 	background-color: white;
-/*<?php if ($is_gecko || $is_macIE) { ?>
-	background-image: url('../wp-images/bgbookmarklet3.gif');
-<?php } elseif ($is_winIE) { ?>
-	background-color: #cccccc;
-	filter: alpha(opacity:80);
-<?php } ?>
-*/  border-width: 1px;
+  border-width: 1px;
 	border-color: #cccccc;
 	border-style: solid;
 	padding: 2px;
