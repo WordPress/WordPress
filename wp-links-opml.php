5ad9e0c4e67830edd3c5dffe1b25f6afc042e1aa
mikelittle <mikelittle@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-02-01 02:13:11 +0000

    First pass at OPML export of links. Task ID 73
    git-svn-id: http://svn.automattic.com/wordpress/trunk@816 1a063a9b-81f0-0310-95a4-ce76da25c4cd

---
+++ wp-links-opml.php
@@ -0,0 +1,63 @@
+<?php 
+$blog = 1; // Your blog's ID
+$doing_rss = 1;
+header('Content-type: text/xml', true);
+require('wp-blog-header.php');
+
+$link_cat = $HTTP_GET_VARS['link_cat'];
+if ((empty($link_cat)) || ($link_cat == 'all') || ($link_cat == '0')) {
+    $sql_cat = '';
+} else { // be safe
+    $link_cat = ''.urldecode($link_cat).'';
+    $link_cat = addslashes_gpc($link_cat);
+    $link_cat = intval($link_cat);
+    if ($link_cat != 0) {
+        $sql_cat = "AND $tablelinks.link_category = $link_cat";
+        $cat_name = $wpdb->get_var("SELECT $tablelinkcategories.cat_name FROM $tablelinkcategories WHERE $tablelinkcategories.cat_id = $link_cat");
+        if (!empty($cat_name)) {
+            $cat_name = ": category $cat_name";
+        }
+    }
+}
+?><?php echo "<?xml version=\"1.0\"?".">\n"; ?>
+<!-- generator="wordpress/<?php echo $wp_version ?>" -->
+<opml version="1.0">
+    <head>
+        <title>Links for <?php echo get_bloginfo('name').$cat_name ?></title>
+        <ownerName><?php echo antispambot(get_bloginfo('admin_email')) ?></ownerName>
+        <ownerEmail><?php echo antispambot(get_bloginfo('admin_email')) ?></ownerEmail>
+        <dateCreated><?php echo gmdate("D, d M Y H:i:s"); ?> GMT</dateCreated>
+        <dateModified><?php echo gmdate("D, d M Y H:i:s"); ?> GMT</dateModified>
+    </head>
+    <body>
+<?php $sql = "SELECT $tablelinks.link_url, $tablelinks.link_name, $tablelinks.link_category, $tablelinkcategories.cat_name \n"
+        . " FROM $tablelinks \n"
+        . " LEFT JOIN $tablelinkcategories on $tablelinks.link_category = $tablelinkcategories.cat_id \n"
+        . " WHERE $tablelinks.link_url IS NOT NULL AND $tablelinks.link_url <> '' \n"
+        . " $sql_cat \n"
+        . " ORDER BY $tablelinkcategories.cat_name, $tablelinks.link_name \n";
+ //echo("<!-- $sql -->");
+ $prev_cat_id = 0;
+ $results = $wpdb->get_results($sql);
+ if ($results) {
+     foreach ($results as $result) {
+         if ($result->link_category != $prev_cat_id) { // new category
+             if ($prev_cat_id != 0)  { // not first time
+?>
+        </outline>
+<?php
+             } // end if not first time
+?>
+        <outline type="category" text="<?php echo($result->cat_name) ?>">
+<?php
+             $prev_cat_id = $result->link_category;
+        } // end if new category
+?>
+            <outline type="link" text="<?php echo($result->link_name) ?>" url="<?php echo($result->link_url) ?>"/>
+<?php
+        } // end foreach
+    } // end if
+?>
+        </outline>
+    </body>
+</opml>
