4f3a01a6be9fc267bf0fb3156146b8d2534f6c26
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-02-02 02:35:08 +0000

    Redirect to profile.php if user level == 0.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@822 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/index.php
+++ wp-admin/index.php
@@ -1,6 +1,13 @@
 <?php
 
-/* This will possibly be more later but for now let's just redirect. */
+require('../wp-config.php');
 
-header ('Location: post.php');
+$user = get_userdatabylogin($log);
+	
+if (0 == $user->user_level) {
+	$redirect_to = $site . '/wp-admin/profile.php';
+} else {
+	$redirect_to = $site . '/wp-admin/post.php';
+}
+header ("Location: $redirect_to");
 ?>
