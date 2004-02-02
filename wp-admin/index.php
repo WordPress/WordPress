3f247c34d60156eb82214f125a7c45bb735b6b3f
jverber <jverber@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-02-02 21:54:32 +0000

    Fixed wp-admin/index.php redirection.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@824 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/index.php
+++ wp-admin/index.php
@@ -2,12 +2,12 @@
 
 require('../wp-config.php');
 
-$user = get_userdatabylogin($log);
-	
-if (0 == $user->user_level) {
-	$redirect_to = $site . '/wp-admin/profile.php';
+get_currentuserinfo();
+
+if (0 == $user_level) {
+	$redirect_to = $siteurl . '/wp-admin/profile.php';
 } else {
-	$redirect_to = $site . '/wp-admin/post.php';
+	$redirect_to = $siteurl . '/wp-admin/post.php';
 }
 header ("Location: $redirect_to");
 ?>
