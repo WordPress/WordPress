c208cf854216fa962b0be706db4135ed953acf17
rboren <rboren@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-02-05 21:17:32 +0000

    Clean feed and trackback URLs.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@837 1a063a9b-81f0-0310-95a4-ce76da25c4cd

---
+++ wp-feed.php
@@ -0,0 +1,31 @@
+<?php
+
+$blog = 1;
+$doing_rss = 1;
+require('wp-blog-header.php');
+
+if ($feed == '' || $feed = 'feed') {
+    // TODO:  Get default feed from options DB.
+    $feed = 'rss2';
+}
+
+if ( (($p != '') && ($p != 'all')) || ($name != '') || ($withcomments == 1) ) {
+    require('wp-commentsrss2.php');
+} else {
+    switch ($feed) {
+    case 'atom':
+        require('wp-atom.php');
+        break;
+    case 'rdf':
+        require('wp-rdf.php');
+        break;
+    case 'rss':
+        require('wp-rss.php');
+        break;
+    case 'rss2':
+        require('wp-rss2.php');
+        break;
+    }
+}
+
+?>
