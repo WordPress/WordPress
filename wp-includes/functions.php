c49fecf3f056e6ef52442996934116da3e37450b
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-02-09 06:57:02 +0000

    Breaking out some of the formatting functions into a different file.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@846 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -1,5 +1,9 @@
 <?php
 
+$curpath = dirname(__FILE__).'/';
+
+require($curpath . 'functions-formatting.php');
+
 if (!function_exists('_')) {
 	function _($string) {
 		return $string;
@@ -12,146 +16,12 @@ if (!function_exists('floatval')) {
 	}
 }
 
-/* functions... */
-
-/***** Formatting functions *****/
-function wptexturize($text) {
-	$output = "";
-	$textarr = preg_split("/(<.*>)/Us", $text, -1, PREG_SPLIT_DELIM_CAPTURE); // capture the tags as well as in between
-	$stop = count($textarr); $next = true; // loop stuff
-	for ($i = 0; $i < $stop; $i++) {
-		$curl = $textarr[$i];
-		if (!strstr($_SERVER['HTTP_USER_AGENT'], 'Gecko')) {
-			$curl = str_replace('<q>', '&#8220;', $curl);
-			$curl = str_replace('</q>', '&#8221;', $curl);
-		}
-		if (isset($curl{0}) && '<' != $curl{0} && $next) { // If it's not a tag
-			$curl = str_replace('---', '&#8212;', $curl);
-			$curl = str_replace('--', '&#8211;', $curl);
-			$curl = str_replace("...", '&#8230;', $curl);
-			$curl = str_replace('``', '&#8220;', $curl);
-
-			// This is a hack, look at this more later. It works pretty well though.
-			$cockney = array("'tain't","'twere","'twas","'tis","'twill","'til","'bout","'nuff","'round");
-			$cockneyreplace = array("&#8217;tain&#8217;t","&#8217;twere","&#8217;twas","&#8217;tis","&#8217;twill","&#8217;til","&#8217;bout","&#8217;nuff","&#8217;round");
-			$curl = str_replace($cockney, $cockneyreplace, $curl);
-
-			$curl = preg_replace("/'s/", "&#8217;s", $curl);
-			$curl = preg_replace("/'(\d\d(?:&#8217;|')?s)/", "&#8217;$1", $curl);
-			$curl = preg_replace('/(\s|\A|")\'/', '$1&#8216;', $curl);
-			$curl = preg_replace("/(\d+)\"/", "$1&Prime;", $curl);
-			$curl = preg_replace("/(\d+)'/", "$1&prime;", $curl);
-			$curl = preg_replace("/(\S)'([^'\s])/", "$1&#8217;$2", $curl);
-			$curl = preg_replace('/"([\s.,!?;:&\']|\Z)/', '&#8221;$1', $curl);
-            $curl = preg_replace('/(\s|\A)"/', '$1&#8220;', $curl);
-			$curl = preg_replace("/'([\s.]|\Z)/", '&#8217;$1', $curl);
-			$curl = preg_replace("/\(tm\)/i", '&#8482;', $curl);
-			$curl = preg_replace("/\(c\)/i", '&#169;', $curl);
-			$curl = preg_replace("/\(r\)/i", '&#174;', $curl);
-			$curl = preg_replace('/&([^#])(?![a-z]{1,8};)/', '&#038;$1', $curl);
-			$curl = str_replace("''", '&#8221;', $curl);
-			
-			$curl = preg_replace('/(d+)x(\d+)/', "$1&#215;$2", $curl);
-
-		} elseif (strstr($curl, '<code') || strstr($curl, '<pre') || strstr($curl, '<kbd' || strstr($curl, '<style') || strstr($curl, '<script'))) {
-			// strstr is fast
-			$next = false;
-		} else {
-			$next = true;
-		}
-		$output .= $curl;
-	}
-	return $output;
-}
-
-function wpautop($pee, $br = 1) {
-	$pee = $pee . "\n"; // just to make things a little easier, pad the end
-	$pee = preg_replace('|<br />\s*<br />|', "\n\n", $pee);
-	$pee = preg_replace('!(<(?:table|tr|td|th|div|ul|ol|li|pre|select|form|blockquote|p|h[1-6])[^>]*>)!', "\n$1", $pee); // Space things out a little
-	$pee = preg_replace('!(</(?:table|tr|td|th|div|ul|ol|li|pre|select|form|blockquote|p|h[1-6])>)!', "$1\n", $pee); // Space things out a little
-	$pee = preg_replace("/(\r\n|\r)/", "\n", $pee); // cross-platform newlines 
-	$pee = preg_replace("/\n\n+/", "\n\n", $pee); // take care of duplicates
-	$pee = preg_replace('/\n?(.+?)(?:\n\s*\n|\z)/s', "\t<p>$1</p>\n", $pee); // make paragraphs, including one at the end 
-	$pee = preg_replace('|<p>\s*?</p>|', '', $pee); // under certain strange conditions it could create a P of entirely whitespace 
-    $pee = preg_replace('!<p>\s*(</?(?:table|tr|td|th|div|ul|ol|li|pre|select|form|blockquote|p|h[1-6])[^>]*>)\s*</p>!', "$1", $pee); // don't pee all over a tag
-	$pee = preg_replace("|<p>(<li.+?)</p>|", "$1", $pee); // problem with nested lists
-	$pee = preg_replace('|<p><blockquote([^>]*)>|i', "<blockquote$1><p>", $pee);
-	$pee = str_replace('</blockquote></p>', '</p></blockquote>', $pee);
-	$pee = preg_replace('!<p>\s*(</?(?:table|tr|td|th|div|ul|ol|li|pre|select|form|blockquote|p|h[1-6])[^>]*>)!', "$1", $pee);
-	$pee = preg_replace('!(</?(?:table|tr|td|th|div|ul|ol|li|pre|select|form|blockquote|p|h[1-6])[^>]*>)\s*</p>!', "$1", $pee); 
-	if ($br) $pee = preg_replace('|(?<!<br />)\s*\n|', "<br />\n", $pee); // optionally make line breaks
-	$pee = preg_replace('!(</?(?:table|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|select|form|blockquote|p|h[1-6])[^>]*>)\s*<br />!', "$1", $pee);
-	$pee = preg_replace('!<br />(\s*</?(?:p|li|div|th|pre|td|ul|ol)>)!', '$1', $pee);
-	$pee = preg_replace('/&([^#])(?![a-z]{1,8};)/', '&#038;$1', $pee);
-	
-	return $pee; 
-}
-
-function sanitize_title($title) {
-    $title = strtolower($title);
-	$title = preg_replace('/&.+?;/', '', $title); // kill entities
-    $title = preg_replace('/[^a-z0-9 -]/', '', $title);
-    $title = preg_replace('/\s+/', ' ', $title);
-    $title = trim($title);
-    $title = str_replace(' ', '-', $title);
-	$title = preg_replace('|-+|', '-', $title);
-	return $title;
-}
-
 function popuplinks($text) {
 	// Comment text in popup windows should be filtered through this.
 	// Right now it's a moderately dumb function, ideally it would detect whether
 	// a target or rel attribute was already there and adjust its actions accordingly.
 	$text = preg_replace('/<a (.+?)>/i', "<a $1 target='_blank' rel='external'>", $text);
 	return $text;
-}
-
-function autobrize($content) {
-	$content = preg_replace("/<br>\n/", "\n", $content);
-	$content = preg_replace("/<br \/>\n/", "\n", $content);
-	$content = preg_replace("/(\015\012)|(\015)|(\012)/", "<br />\n", $content);
-	return $content;
-	}
-function unautobrize($content) {
-	$content = preg_replace("/<br>\n/", "\n", $content);   //for PHP versions before 4.0.5
-	$content = preg_replace("/<br \/>\n/", "\n", $content);
-	return $content;
-	}
-
-
-function format_to_edit($content) {
-	global $autobr;
-	$content = stripslashes($content);
-	if ($autobr) { $content = unautobrize($content); }
-	$content = htmlspecialchars($content);
-	return $content;
-	}
-function format_to_post($content) {
-	global $post_autobr,$comment_autobr;
-	$content = addslashes($content);
-	if ($post_autobr || $comment_autobr) { $content = autobrize($content); }
-	return $content;
-	}
-
-
-function zeroise($number,$threshold) { // function to add leading zeros when necessary
-	$l=strlen($number);
-	if ($l<$threshold)
-		for ($i=0; $i<($threshold-$l); $i=$i+1) { $number='0'.$number;	}
-	return $number;
-	}
-
-
-function backslashit($string) {
-	$string = preg_replace('/([a-z])/i', '\\\\\1', $string);
-	return $string;
-}
-
-function trailingslashit($string) {
-    if ( '/' != substr($string, -1)) {
-        $string .= '/';
-    }
-    return $string;
 }
 
 function mysql2date($dateformatstring, $mysqlstring, $use_b2configmonthsdays = 1) {
@@ -191,13 +61,6 @@ function current_time($type) {
 	}
 }
 
-function addslashes_gpc($gpc) {
-	if (!get_magic_quotes_gpc()) {
-		$gpc = addslashes($gpc);
-	}
-	return $gpc;
-}
-
 function date_i18n($dateformatstring, $unixtimestamp) {
 	global $month, $weekday;
 	$i = $unixtimestamp; 
@@ -215,8 +78,6 @@ function date_i18n($dateformatstring, $unixtimestamp) {
 	return $j;
 	}
 
-
-
 function get_weekstartend($mysqlstring, $start_of_week) {
 	$my = substr($mysqlstring,0,4);
 	$mm = substr($mysqlstring,8,2);
@@ -233,152 +94,6 @@ function get_weekstartend($mysqlstring, $start_of_week) {
 	$week['end']   = $day + 691199;
 	return $week;
 }
-
-function convert_chars($content,$flag='obsolete attribute left there for backwards compatibility') { // html/unicode entities output
-
-	global $use_htmltrans, $wp_htmltrans, $wp_htmltranswinuni;
-
-	// removes metadata tags
-	$content = preg_replace('/<title>(.+?)<\/title>/','',$content);
-	$content = preg_replace('/<category>(.+?)<\/category>/','',$content);
-	
-	if ($use_htmltrans) {
-
-		// converts lone & characters into &#38; (a.k.a. &amp;)
-		$content = preg_replace('/&[^#](?![a-z]*;)/ie', '"&#38;".substr("\0",1)', $content);
-
-		// converts HTML-entities to their display values in order to convert them again later
-		$content = preg_replace('/['.chr(127).'-'.chr(255).']/e', '"&#".ord(\'\0\').";"', $content );
-		$content = strtr($content, $wp_htmltrans);
-
-		// now converting: Windows CP1252 => Unicode (valid HTML)
-		// (if you've ever pasted text from MSWord, you'll understand)
-
-		$content = strtr($content, $wp_htmltranswinuni);
-
-	}
-
-	// you can delete these 2 lines if you don't like <br /> and <hr />
-	$content = str_replace("<br>","<br />",$content);
-	$content = str_replace("<hr>","<hr />",$content);
-
-	return $content;
-
-}
-
-function convert_bbcode($content) {
-	global $wp_bbcode, $use_bbcode;
-	if ($use_bbcode) {
-		$content = preg_replace($wp_bbcode["in"], $wp_bbcode["out"], $content);
-	}
-	$content = convert_bbcode_email($content);
-	return $content;
-}
-
-function convert_bbcode_email($content) {
-	global $use_bbcode;
-	$bbcode_email["in"] = array(
-		'#\[email](.+?)\[/email]#eis',
-		'#\[email=(.+?)](.+?)\[/email]#eis'
-	);
-	$bbcode_email["out"] = array(
-		"'<a href=\"mailto:'.antispambot('\\1').'\">'.antispambot('\\1').'</a>'",		// E-mail
-		"'<a href=\"mailto:'.antispambot('\\1').'\">\\2</a>'"
-	);
-
-	$content = preg_replace($bbcode_email["in"], $bbcode_email["out"], $content);
-	return $content;
-}
-
-function convert_gmcode($content) {
-	global $wp_gmcode, $use_gmcode;
-	if ($use_gmcode) {
-		$content = preg_replace($wp_gmcode["in"], $wp_gmcode["out"], $content);
-	}
-	return $content;
-}
-
-function convert_smilies($text) {
-	global $smilies_directory, $use_smilies;
-	global $wp_smiliessearch, $wp_smiliesreplace;
-    $output = '';
-	if ($use_smilies) {
-		// HTML loop taken from texturize function, could possible be consolidated
-		$textarr = preg_split("/(<.*>)/U", $text, -1, PREG_SPLIT_DELIM_CAPTURE); // capture the tags as well as in between
-		$stop = count($textarr);// loop stuff
-		for ($i = 0; $i < $stop; $i++) {
-			$content = $textarr[$i];
-			if ((strlen($content) > 0) && ('<' != $content{0})) { // If it's not a tag
-				$content = str_replace($wp_smiliessearch, $wp_smiliesreplace, $content);
-			}
-			$output .= $content;
-		}
-	} else {
-		// return default text.
-		$output = $text;
-	}
-	return $output;
-}
-
-function antispambot($emailaddy, $mailto=0) {
-	$emailNOSPAMaddy = '';
-	srand ((float) microtime() * 1000000);
-	for ($i = 0; $i < strlen($emailaddy); $i = $i + 1) {
-		$j = floor(rand(0, 1+$mailto));
-		if ($j==0) {
-			$emailNOSPAMaddy .= '&#'.ord(substr($emailaddy,$i,1)).';';
-		} elseif ($j==1) {
-			$emailNOSPAMaddy .= substr($emailaddy,$i,1);
-		} elseif ($j==2) {
-			$emailNOSPAMaddy .= '%'.zeroise(dechex(ord(substr($emailaddy, $i, 1))), 2);
-		}
-	}
-	$emailNOSPAMaddy = str_replace('@','&#64;',$emailNOSPAMaddy);
-	return $emailNOSPAMaddy;
-}
-
-function make_clickable($text) { // original function: phpBB, extended here for AIM & ICQ
-    $ret = " " . $text;
-    $ret = preg_replace("#([\n ])([a-z]+?)://([^, <>{}\n\r]+)#i", "\\1<a href=\"\\2://\\3\" target=\"_blank\">\\2://\\3</a>", $ret);
-    $ret = preg_replace("#([\n ])aim:([^,< \n\r]+)#i", "\\1<a href=\"aim:goim?screenname=\\2\\3&message=Hello\">\\2\\3</a>", $ret);
-    $ret = preg_replace("#([\n ])icq:([^,< \n\r]+)#i", "\\1<a href=\"http://wwp.icq.com/scripts/search.dll?to=\\2\\3\">\\2\\3</a>", $ret);
-    $ret = preg_replace("#([\n ])www\.([a-z0-9\-]+)\.([a-z0-9\-.\~]+)((?:/[^,< \n\r]*)?)#i", "\\1<a href=\"http://www.\\2.\\3\\4\" target=\"_blank\">www.\\2.\\3\\4</a>", $ret);
-    $ret = preg_replace("#([\n ])([a-z0-9\-_.]+?)@([^,< \n\r]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret);
-    $ret = substr($ret, 1);
-    return $ret;
-}
-
-
-function is_email($user_email) {
-	$chars = "/^([a-z0-9_]|\\-|\\.)+@(([a-z0-9_]|\\-)+\\.)+[a-z]{2,4}\$/i";
-	if(strstr($user_email, '@') && strstr($user_email, '.')) {
-		if (preg_match($chars, $user_email)) {
-			return true;
-		} else {
-			return false;
-		}
-	} else {
-		return false;
-	}
-}
-
-
-function strip_all_but_one_link($text, $mylink) {
-	$match_link = '#(<a.+?href.+?'.'>)(.+?)(</a>)#';
-	preg_match_all($match_link, $text, $matches);
-	$count = count($matches[0]);
-	for ($i=0; $i<$count; $i++) {
-		if (!strstr($matches[0][$i], $mylink)) {
-			$text = str_replace($matches[0][$i], $matches[2][$i], $text);
-		}
-	}
-	return $text;
-}
-
-
-/***** // Formatting functions *****/
-
-
 
 function get_lastpostdate() {
 	global $tableposts, $cache_lastpostdate, $use_cache, $time_difference, $pagenow, $wpdb;
@@ -1212,141 +927,6 @@ function pingback($content, $post_ID) {
 
 	debug_fwrite($log, "\nEND: ".time()."\n****************************\n\r");
 	debug_fclose($log);
-}
-
-/**
- ** sanitise HTML attributes, remove frame/applet/*script/mouseovers,etc. tags
- ** so that this kind of thing cannot be done:
- ** This is how we can do <b onmouseover="alert('badbadbad')">bad stuff</b>!
- **/
-function sanitise_html_attributes($text) {
-    $text = preg_replace('#(([\s"\'])on[a-z]{1,}|style|class|id)="(.*?)"#i', '$1', $text);
-    $text = preg_replace('#(([\s"\'])on[a-z]{1,}|style|class|id)=\'(.*?)\'#i', '$1', $text);
-    $text = preg_replace('#(([\s"\'])on[a-z]{1,}|style|class|id)[ \t]*=[ \t]*([^ \t\>]*?)#i', '$1', $text);
-    $text = preg_replace('#([a-z]{1,})="(( |\t)*?)(javascript|vbscript|about):(.*?)"#i', '$1=""', $text);
-    $text = preg_replace('#([a-z]{1,})=\'(( |\t)*?)(javascript|vbscript|about):(.*?)\'#i', '$1=""', $text);
-    $text = preg_replace('#\<(\/{0,1})([a-z]{0,2})(frame|applet)(.*?)\>#i', '', $text);
-    return $text;
-}
-
-/*
- balanceTags
- 
- Balances Tags of string using a modified stack.
- 
- @param text      Text to be balanced
- @return          Returns balanced text
- @author          Leonard Lin (leonard@acm.org)
- @version         v1.1
- @date            November 4, 2001
- @license         GPL v2.0
- @notes           
- @changelog       
-             1.2  ***TODO*** Make better - change loop condition to $text
-             1.1  Fixed handling of append/stack pop order of end text
-                  Added Cleaning Hooks
-             1.0  First Version
-*/
-function balanceTags($text, $is_comment = 0) {
-	global $use_balanceTags;
-
-	if ($is_comment) {
-        $text = sanitise_html_attributes($text);
-	}
-	
-	if ($use_balanceTags == 0) {
-		return $text;
-	}
-
-	$tagstack = array();
-	$stacksize = 0;
-	$tagqueue = '';
-	$newtext = '';
-
-	# b2 bug fix for comments - in case you REALLY meant to type '< !--'
-	$text = str_replace('< !--', '<    !--', $text);
-
-	# b2 bug fix for LOVE <3 (and other situations with '<' before a number)
-	$text = preg_replace('#<([0-9]{1})#', '&lt;$1', $text);
-
-
-	while (preg_match("/<(\/?\w*)\s*([^>]*)>/",$text,$regex)) {
-		$newtext = $newtext . $tagqueue;
-
-		$i = strpos($text,$regex[0]);
-		$l = strlen($tagqueue) + strlen($regex[0]);
-
-		// clear the shifter
-		$tagqueue = '';
-
-		// Pop or Push
-		if ($regex[1][0] == "/") { // End Tag
-			$tag = strtolower(substr($regex[1],1));
-
-			// if too many closing tags
-			if($stacksize <= 0) { 
-				$tag = '';
-				//or close to be safe $tag = '/' . $tag;
-			}
-			// if stacktop value = tag close value then pop
-			else if ($tagstack[$stacksize - 1] == $tag) { // found closing tag
-				$tag = '</' . $tag . '>'; // Close Tag
-				// Pop
-				array_pop ($tagstack);
-				$stacksize--;
-			} else { // closing tag not at top, search for it
-				for ($j=$stacksize-1;$j>=0;$j--) {
-					if ($tagstack[$j] == $tag) {
-					// add tag to tagqueue
-						for ($k=$stacksize-1;$k>=$j;$k--){
-							$tagqueue .= '</' . array_pop ($tagstack) . '>';
-							$stacksize--;
-						}
-						break;
-					}
-				}
-				$tag = '';
-			}
-		} else { // Begin Tag
-			$tag = strtolower($regex[1]);
-
-			// Tag Cleaning
-
-			// Push if not img or br or hr
-			if($tag != 'br' && $tag != 'img' && $tag != 'hr') {
-				$stacksize = array_push ($tagstack, $tag);
-			}
-
-			// Attributes
-			// $attributes = $regex[2];
-			$attributes = $regex[2];
-			if($attributes) {
-				$attributes = ' '.$attributes;
-			}
-
-			$tag = '<'.$tag.$attributes.'>';
-		}
-
-		$newtext .= substr($text,0,$i) . $tag;
-		$text = substr($text,$i+$l);
-	}  
-
-	// Clear Tag Queue
-	$newtext = $newtext . $tagqueue;
-
-	// Add Remaining text
-	$newtext .= $text;
-
-	// Empty Stack
-	while($x = array_pop($tagstack)) {
-		$newtext = $newtext . '</' . $x . '>'; // Add remaining tags to close      
-	}
-
-	# b2 fix for the bug with HTML comments
-	$newtext = str_replace("< !--","<!--",$newtext);
-	$newtext = str_replace("<    !--","< !--",$newtext);
-
-	return $newtext;
 }
 
 function doGeoUrlHeader($posts) {
