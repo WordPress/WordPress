24f5b1f9c752f5590aa2bc949cc44db4b174f0fb
michelvaldrighi <michelvaldrighi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-02-23 14:17:51 +0000

    now posts show up :P (put :00 back)
    git-svn-id: http://svn.automattic.com/wordpress/trunk@923 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -377,7 +377,7 @@ if ($p == 'all') {
     $where = '';
 }
 
-$now = gmdate('Y-m-d H:i:s');
+$now = gmdate('Y-m-d H:i:00');
 
 if ($pagenow != 'post.php' && $pagenow != 'edit.php') {
     if ((empty($poststart)) || (empty($postend)) || !($postend > $poststart)) {
