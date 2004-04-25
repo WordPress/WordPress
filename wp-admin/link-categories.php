12d0266b0b787141e5e53d7472167ab6a15b5453
rboren <rboren@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-04-25 23:49:45 +0000

    Mark strings for translation.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1175 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/link-categories.php
+++ wp-admin/link-categories.php
@@ -1,8 +1,8 @@
 <?php
 // Links
 // Copyright (C) 2002, 2003 Mike Little -- mike@zed1.com
-
-$title = 'Link Categories';
+require_once('../wp-includes/wp-l10n.php');
+$title = __('Link Categories');
 $this_file='link-categories.php';
 $parent_file = 'link-manager.php';
 
@@ -29,7 +29,7 @@ switch ($action) {
       include_once('admin-header.php');
 
       if ($user_level < get_settings('links_minadminlevel'))
-          die ("Cheatin' uh ?");
+          die (__("Cheatin' uh ?"));
 
       $cat_name = addslashes($_POST['cat_name']);
       $auto_toggle = $_POST['auto_toggle'];
@@ -90,10 +90,10 @@ switch ($action) {
     $cat_name=addslashes($cat_name);
 
     if ($cat_id=="1")
-        die("Can't delete the <strong>$cat_name</strong> link category: this is the default one");
+        die(sprintf(__("Can't delete the <strong>%s</strong> link category: this is the default one"), $cat_name));
 
     if ($user_level < get_settings('links_minadminlevel'))
-    die ("Cheatin' uh ?");
+      die (__("Cheatin' uh ?"));
 
     $wpdb->query("DELETE FROM $tablelinkcategories WHERE cat_id='$cat_id'");
     $wpdb->query("UPDATE $tablelinks SET link_category=1 WHERE link_category='$cat_id'");
@@ -115,10 +115,10 @@ switch ($action) {
 ?>
 
 <ul id="adminmenu2">
-	<li><a href="link-manager.php" >Manage Links</a></li>
-	<li><a href="link-add.php">Add Link</a></li>
-	<li><a href="link-categories.php" class="current">Link Categories</a></li>
-	<li class="last"><a href="link-import.php">Import Blogroll</a></li>
+	<li><a href="link-manager.php" ><?php _e('Manage Links') ?></a></li>
+	<li><a href="link-add.php"><?php _e('Add Link') ?></a></li>
+	<li><a href="link-categories.php" class="current"><?php _e('Link Categories') ?></a></li>
+	<li class="last"><a href="link-import.php"><?php _e('Import Blogroll') ?></a></li>
 </ul>
 
 <div class="wrap">
@@ -128,81 +128,81 @@ switch ($action) {
       <input type="hidden" name="action" value="editedcat" />
       <input type="hidden" name="cat_id" value="<?php echo $row->cat_id ?>" />
 <fieldset class="options">
-<legend>Category Options</legend>
+<legend><?php _e('Category Options') ?></legend>
 <table class="editform" width="100%" cellspacing="2" cellpadding="5">
 <tr>
-	<th width="33%" scope="row">Name:</th>
+	<th width="33%" scope="row"><?php _e('Name:') ?></th>
 	<td width="67%"><input name="cat_name" type="text" value="<?php echo stripslashes($row->cat_name)?>" size="30" /></td>
 </tr>
 <tr>
-	<th scope="row">Show:</th>
+	<th scope="row"><?php _e('Show:') ?></th>
         <td>
             <label>
             <input type="checkbox" name="show_images"  value="Y" <?php checked('Y', $row->show_images); ?> /> 
-            Image</label> <br />
+            <?php _e('Image') ?></label> <br />
             <label>
             <input type="checkbox" name="show_description" value="Y" <?php checked('Y', $row->show_description); ?> /> 
-            Description</label> 
-            (shown in <code>title</code> regardless)<br />
+            <?php _e('Description') ?></label> 
+            <?php _e('(shown in <code>title</code> regardless)') ?><br />
             <label>
             <input type="checkbox" name="show_rating"  value="Y" <?php checked('Y', $row->show_rating); ?> /> 
-            Rating</label> <br />
+            <?php _e('Rating') ?></label> <br />
             <label>
             <input type="checkbox" name="show_updated" value="Y" <?php checked('Y', $row->show_updated); ?> /> 
-            Updated</label>
-(shown in <code>title</code> regardless)</td>
+            <?php _e('Updated') ?></label>
+<?php _e('(shown in <code>title</code> regardless)') ?></td>
 </tr>
 <tr>
-	<th scope="row">Sort order:</th>
+	<th scope="row"><?php _e('Sort order:') ?></th>
 	<td>
 	<select name="sort_order" size="1">
-            <option value="name"    <?php echo ($row->sort_order == 'name') ? 'selected' : ''?>>Name</option>
-            <option value="id"      <?php echo ($row->sort_order == 'id') ? 'selected' : ''?>>Id</option>
-            <option value="url"     <?php echo ($row->sort_order == 'url') ? 'selected' : ''?>>URL</option>
-            <option value="rating"  <?php echo ($row->sort_order == 'rating') ? 'selected' : ''?>>Rating</option>
-            <option value="updated" <?php echo ($row->sort_order == 'updated') ? 'selected' : ''?>>Updated</option>
-            <option value="rand"  <?php echo ($row->sort_order == 'rand') ? 'selected' : ''?>>Random</option>
-            <option value="length"  <?php echo ($row->sort_order == 'length') ? 'selected' : ''?>>Name Length</option>
+            <option value="name"    <?php echo ($row->sort_order == 'name') ? 'selected' : ''?>><?php _e('Name') ?></option>
+            <option value="id"      <?php echo ($row->sort_order == 'id') ? 'selected' : ''?>><?php _e('Id') ?></option>
+            <option value="url"     <?php echo ($row->sort_order == 'url') ? 'selected' : ''?>><?php _e('URL') ?></option>
+            <option value="rating"  <?php echo ($row->sort_order == 'rating') ? 'selected' : ''?>><?php _e('Rating') ?></option>
+            <option value="updated" <?php echo ($row->sort_order == 'updated') ? 'selected' : ''?>><?php _e('Updated') ?></option>
+            <option value="rand"  <?php echo ($row->sort_order == 'rand') ? 'selected' : ''?>><?php _e('Random') ?></option>
+            <option value="length"  <?php echo ($row->sort_order == 'length') ? 'selected' : ''?>><?php _e('Name Length') ?></option>
 	</select>
 	<label>
 	<input type="checkbox" name="sort_desc" value="Y" <?php checked('Y', $row->sort_desc); ?> /> 
-	Descending</label>
+	<?php _e('Descending') ?></label>
 	</td>
 </tr>
 <tr>
-	<th scope="row">Limit:</th>
+	<th scope="row"><?php _e('Limit:') ?></th>
 	<td>
 	<input type="text" name="list_limit" size="5" value="<?php echo $row->list_limit ?>" /> 
-	(Leave empty for no limit to number of links shown)
+	<?php _e('(Leave empty for no limit to number of links shown)') ?>
 	</td>
 </tr>
 <tr>
-	<th scope="row">Toggle:</th>
+	<th scope="row"><?php _e('Toggle:') ?></th>
 	<td><label>
 		<input type="checkbox" name="auto_toggle"  value="Y" <?php checked('Y', $row->auto_toggle); ?> /> 
-		When new link is added toggle all others to be invisible</label></td>
+		<?php _e('When new link is added toggle all others to be invisible') ?></label></td>
 </tr>
 
 </table>
 </fieldset>
 <fieldset class="options">
-<legend>Formatting</legend>
+<legend><?php _e('Formatting') ?></legend>
 <table class="editform" width="100%" cellspacing="2" cellpadding="5">
 <tr>
-	<th width="33%" scope="row">Before Link:</th>
+	<th width="33%" scope="row"><?php _e('Before Link:') ?></th>
 	<td width="67%"><input type="text" name="text_before_link" size="45" value="<?php echo stripslashes($row->text_before_link)?>" /></td>
 </tr>
 <tr>
-<th scope="row">Between Link and Description:</th>
+<th scope="row"><?php _e('Between Link and Description:') ?></th>
 <td><input type="text" name="text_after_link" size="45" value="<?php echo stripslashes($row->text_after_link)?>" /></td>
 </tr>
 <tr>
-<th scope="row">After Link:</th>
+<th scope="row"><?php _e('After Link:') ?></th>
 <td><input type="text" name="text_after_all" size="45" value="<?php echo stripslashes($row->text_after_all)?>"/></td>
 </tr>
 </table>
 </fieldset>
-<p class="submit"><input type="submit" name="submit" value="Save Category Settings &raquo;" /></p>
+<p class="submit"><input type="submit" name="submit" value="<?php _e('Save Category Settings &raquo;') ?>" /></p>
 </form>
 
 </div>
@@ -216,7 +216,7 @@ switch ($action) {
     include_once("./admin-header.php");
 
     if ($user_level < get_settings('links_minadminlevel'))
-      die ("Cheatin' uh ?");
+      die (__("Cheatin' uh ?"));
 
     $submit=$_POST["submit"];
     if (isset($submit)) {
@@ -289,37 +289,37 @@ switch ($action) {
     $standalone=0;
     include_once ("./admin-header.php");
     if ($user_level < get_settings('links_minadminlevel')) {
-      die("You have do not have sufficient permissions to edit the link categories for this blog. :)");
+      die(__("You have do not have sufficient permissions to edit the link categories for this blog. :)"));
     }
 ?>
 <ul id="adminmenu2">
-	<li><a href="link-manager.php" >Manage Links</a></li>
-	<li><a href="link-add.php">Add Link</a></li>
-	<li><a href="link-categories.php" class="current">Link Categories</a></li>
-	<li class="last"><a href="link-import.php">Import Blogroll</a></li>
+	<li><a href="link-manager.php" ><?php _e('Manage Links') ?></a></li>
+	<li><a href="link-add.php"><?php _e('Add Link') ?></a></li>
+	<li><a href="link-categories.php" class="current"><?php _e('Link Categories') ?></a></li>
+	<li class="last"><a href="link-import.php"><?php _e('Import Blogroll') ?></a></li>
 </ul>
 <div class="wrap">
-            <h2>Link Categories:<?php echo gethelp_link($this_file,'edit_link_category');?></h2>
+            <h2><?php _e('Link Categories:') ?><?php echo gethelp_link($this_file,'edit_link_category');?></h2>
             <table width="100%" cellpadding="5" cellspacing="0" border="0">
               <tr>
-			  <th rowspan="2" valign="bottom">Name</th>
-                <th rowspan="2" valign="bottom">ID</th>
-                <th rowspan="2" valign="bottom">Toggle?</th>
-                <th colspan="4" valign="bottom">Show</th>
-                <th rowspan="2" valign="bottom">Sort Order</th>
-                <th rowspan="2" valign="bottom">Desc?</th>
-                <th colspan="3" valign="bottom">Formatting</th>
-                <th rowspan="2" valign="bottom">Limit</th>
+ 	        <th rowspan="2" valign="bottom"><?php _e('Name') ?></th>
+                <th rowspan="2" valign="bottom"><?php _e('ID') ?></th>
+                <th rowspan="2" valign="bottom"><?php _e('Toggle?') ?></th>
+                <th colspan="4" valign="bottom"><?php _e('Show') ?></th>
+                <th rowspan="2" valign="bottom"><?php _e('Sort Order') ?></th>
+                <th rowspan="2" valign="bottom"><?php _e('Desc?') ?></th>
+                <th colspan="3" valign="bottom"><?php _e('Formatting') ?></th>
+                <th rowspan="2" valign="bottom"><?php _e('Limit') ?></th>
                 <th rowspan="2" colspan="2">&nbsp;</th>
               </tr>
               <tr>
-                <th valign="top">Images</th>
-                <th valign="top">Desc.</th>
-                <th valign="top">Rating</th>
-                <th valign="top">Updated</th>
-                <th valign="top">Before</th>
-                <th valign="top">Between</th>
-                <th valign="top">After</th>
+                <th valign="top"><?php _e('Images') ?></th>
+                <th valign="top"><?php _e('Desc.') ?></th>
+                <th valign="top"><?php _e('Rating') ?></th>
+                <th valign="top"><?php _e('Updated') ?></th>
+                <th valign="top"><?php _e('Before') ?></th>
+                <th valign="top"><?php _e('Between') ?></th>
+                <th valign="top"><?php _e('After') ?></th>
               </tr>
 <?php
 $results = $wpdb->get_results("SELECT cat_id, cat_name, auto_toggle, show_images, show_description, "
@@ -346,103 +346,102 @@ foreach ($results as $row) {
                 <td nowrap="nowrap"><?php echo htmlentities($row->text_after_link)?>&nbsp;</td>
                 <td nowrap="nowrap"><?php echo htmlentities($row->text_after_all)?></td>
                 <td><?php echo $row->list_limit?></td>
-                <td><a href="link-categories.php?cat_id=<?php echo $row->cat_id?>&amp;action=Edit" class="edit">Edit</a></td>
-                <td><a href="link-categories.php?cat_id=<?php echo $row->cat_id?>&amp;action=Delete" onclick="return confirm('You are about to delete this category.\n  \'Cancel\' to stop, \'OK\' to delete.');" class="delete">Delete</a></td>
+                <td><a href="link-categories.php?cat_id=<?php echo $row->cat_id?>&amp;action=Edit" class="edit"><?php _e('Edit') ?></a></td>
+                <td><a href="link-categories.php?cat_id=<?php echo $row->cat_id?>&amp;action=Delete" onclick="return confirm('<?php _e("You are about to delete this category.\\n  \'Cancel\' to stop, \'OK\' to delete.") ?>');" class="delete"><?php _e('Delete') ?></a></td>
               </tr>
 <?php
         ++$i;
     }
 ?>
             </table>
-<p>These are the defaults for when you call a link category with no additional arguments. All of these settings may be overwritten.</p>
+<p><?php _e('These are the defaults for when you call a link category with no additional arguments. All of these settings may be overwritten.') ?></p>
 
 </div>
 
 <div class="wrap">
     <form name="addcat" method="post">
       <input type="hidden" name="action" value="addcat" />
-	  <h2>Add a Link Category:<?php echo gethelp_link($this_file,'add_link_category');?></h2>
+	  <h2><?php _e('Add a Link Category:') ?><?php echo gethelp_link($this_file,'add_link_category');?></h2>
 <fieldset class="options">
-<legend>Category Options</legend>
+<legend><?php _e('Category Options') ?></legend>
 <table class="editform" width="100%" cellspacing="2" cellpadding="5">
 <tr>
-	<th width="33%" scope="row">Name:</th>
+	<th width="33%" scope="row"><?php _e('Name:') ?></th>
 	<td width="67%"><input type="text" name="cat_name" size="30" /></td>
 </tr>
 <tr>
-	<th scope="row">Show:</th>
+	<th scope="row"><?php _e('Show:') ?></th>
         <td>
             <label>
             <input type="checkbox" name="show_images"  value="Y" /> 
-            Image</label> <br />
+            <?php _e('Image') ?></label> <br />
             <label>
             <input type="checkbox" name="show_description" value="Y" /> 
-            Description</label> 
-            (shown in <code>title</code> regardless)<br />
+            <?php _e('Description') ?></label> 
+            <?php _e('(shown in <code>title</code> regardless)') ?><br />
             <label>
             <input type="checkbox" name="show_rating"  value="Y" /> 
-            Rating</label> <br />
+            <?php _e('Rating') ?></label> <br />
             <label>
             <input type="checkbox" name="show_updated" value="Y" /> 
-            Updated</label>
-(shown in <code>title</code> regardless)</td>
+            <?php _e('Updated') ?></label>
+<?php _e('(shown in <code>title</code> regardless)') ?></td>
 </tr>
 <tr>
-	<th scope="row">Sort order:</th>
+	<th scope="row"><?php _e('Sort order:') ?></th>
 	<td>
 	<select name="sort_order" size="1">
-	<option value="name">Name</option>
-	<option value="id">Id</option>
-	<option value="url">URL</option>
-	<option value="rating">Rating</option>
-	<option value="updated">Updated</option>
-	<option value="rand">Random</option>
+	<option value="name"><?php _e('Name') ?></option>
+	<option value="id"><?php _e('Id') ?></option>
+	<option value="url"><?php _e('URL') ?></option>
+	<option value="rating"><?php _e('Rating') ?></option>
+	<option value="updated"><?php _e('Updated') ?></option>
+	<option value="rand"><?php _e('Random') ?></option>
 	</select>
 	<label>
 	<input type="checkbox" name="sort_desc" value="Y" /> 
-	Descending</label>
+	<?php _e('Descending') ?></label>
 	</td>
 </tr>
 <tr>
-	<th scope="row">Limit:</th>
+	<th scope="row"><?php _e('Limit:') ?></th>
 	<td>
-	<input type="text" name="list_limit" size="5" value="" /> (Leave empty for no limit to number of links shown)
+	<input type="text" name="list_limit" size="5" value="" /> <?php _e('(Leave empty for no limit to number of links shown)') ?>
 	</td>
 </tr>
 <tr>
-	<th scope="row">Toggle:</th>
+	<th scope="row"><?php _e('Toggle:') ?></th>
 	<td><label>
 		<input type="checkbox" name="auto_toggle"  value="Y" /> 
-		When new link is added toggle all others to be invisible</label></td>
+		<?php _e('When new link is added toggle all others to be invisible') ?></label></td>
 </tr>
 
 </table>
 </fieldset>
 <fieldset class="options">
-<legend>Formatting</legend>
+<legend><?php _e('Formatting') ?></legend>
 <table class="editform" width="100%" cellspacing="2" cellpadding="5">
 <tr>
-	<th width="33%" scope="row">Before Link:</th>
+	<th width="33%" scope="row"><?php _e('Before Link:') ?></th>
 	<td width="67%"><input type="text" name="text_before_link" size="45" value="&lt;li&gt;" /></td>
 </tr>
 <tr>
-<th scope="row">Between Link and Description:</th>
+<th scope="row"><?php _e('Between Link and Description:') ?></th>
 <td><input type="text" name="text_after_link" size="45" value="&lt;br /&gt;" /></td>
 </tr>
 <tr>
-<th scope="row">After Link:</th>
+<th scope="row"><?php _e('After Link:') ?></th>
 <td><input type="text" name="text_after_all" size="45" value="&lt;/li&gt;"/></td>
 </tr>
 </table>
 </fieldset>
-<p class="submit"><input type="submit" name="submit" value="Add Category &raquo;" /></p>
+<p class="submit"><input type="submit" name="submit" value="<?php _e('Add Category &raquo;') ?>" /></p>
   </form>
 </div>
 <div class="wrap">
-    <h3>Note:</h3>
-    <p>Deleting a link category does not delete links from that category.<br />
-    It will
-    just set them back to the default category <b><?php echo get_linkcatname(1) ?></b>.
+    <h3><?php _e('Note:') ?></h3>
+    <?php printf(__('<p>Deleting a link category does not delete links from that category.<br />
+    It will just set them back to the default category <b>%s</b>.'), get_linkcatname(1)) ?>
     </p>
 </div>
 <?php
