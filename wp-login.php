d56aae211c2de4326044399ecc11b9b8adf3e5dc
rboren <rboren@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-04-25 19:45:22 +0000

    Mark strings for translation.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1170 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-login.php
+++ wp-login.php
@@ -77,12 +77,12 @@ case 'login':
 		$pwd = md5($pwd);
 		$password = &$pwd;
 		if (!$user_login) {
-			$error = '<strong>Error</strong>: the login field is empty.';
+			$error = __('<strong>Error</strong>: the login field is empty.');
 			return false;
 		}
 
 		if (!$password) {
-			$error = '<strong>Error</strong>: the password field is empty.';
+			$error = __('<strong>Error</strong>: the password field is empty.');
 			return false;
 		}
 
@@ -91,7 +91,7 @@ case 'login':
 		$login = $wpdb->get_row($query);
 
 		if (!$login) {
-			$error = '<strong>Error</strong>: wrong login or password.';
+			$error = __('<strong>Error</strong>: wrong login or password.');
 			$pwd = '';
 			return false;
 		} else {
@@ -99,7 +99,7 @@ case 'login':
 			if (($pass_is_md5 == 0 && $login->user_login == $user_login && $login->user_pass == $password) || ($pass_is_md5 == 1 && $login->user_login == $user_login && $login->user_pass == md5($password))) {
 				return true;
 			} else {
-				$error = '<strong>Error</strong>: wrong login or password.';
+				$error = __('<strong>Error</strong>: wrong login or password.');
 				$pwd = '';
 			return false;
 			}
@@ -161,7 +161,7 @@ case 'lostpassword':
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>WordPress &rsaquo; Lost password ?</title>
+	<title><?php _e('WordPress &rsaquo; Lost password ?') ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo get_settings('blog_charset'); ?>" />
 	<link rel="stylesheet" href="<?php echo get_settings('siteurl'); ?>/wp-admin/wp-admin.css" type="text/css" />
 	<script type="text/javascript">
@@ -176,15 +176,15 @@ case 'lostpassword':
 
 
 <div id="login">
-<p>Please enter your information here. We will send you a new password. </p>
+<p><?php _e('Please enter your information here. We will send you a new password.') ?> </p>
 <?php
 if ($error) echo "<div align=\"right\" style=\"padding:4px;\"><font color=\"#FF0000\">$error</font><br />&nbsp;</div>";
 ?>
 
 <form name="" action="wp-login.php" method="post" id="lostpass">
 <input type="hidden" name="action" value="retrievepassword" />
-<label>Login: <input type="text" name="user_login" id="user_login" value="" size="12" /></label><br />
-<label>Email: <input type="text" name="email" id="email" value="" size="12" /></label><br />
+<label><?php _e('Login:') ?> <input type="text" name="user_login" id="user_login" value="" size="12" /></label><br />
+<label><?php _e('Email:') ?> <input type="text" name="email" id="email" value="" size="12" /></label><br />
 <input type="submit" name="Submit2" value="OK" class="search">
 
 </form>
@@ -204,7 +204,7 @@ case 'retrievepassword':
 	$user_data = get_userdatabylogin($user_login);
 	$user_email = $user_data->user_email;
 
-	if (!$user_email || $user_email != $_POST['email']) die('Sorry, that user does not seem to exist in our database. Perhaps you have the wrong username or email address? <a href="wp-login.php?action=lostpassword">Try again</a>.');
+	if (!$user_email || $user_email != $_POST['email']) die(sprintf(__('Sorry, that user does not seem to exist in our database. Perhaps you have the wrong username or email address? <a href="%s">Try again</a>.'), 'wp-login.php?action=lostpassword'));
  	// Generate something random for a password... md5'ing current time with a rand salt
     $user_pass = substr((MD5("time" . rand(1,16000))), 0, 6);
  	// now insert the new pass md5'd into the db
@@ -265,7 +265,7 @@ default:
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>WordPress > <?php bloginfo('name') ?> > Login form</title>
+	<title><?php printf(__('WordPress > %s > Login form'), get_settings('blogname')) ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo get_settings('blog_charset'); ?>" />
 	<link rel="stylesheet" href="<?php echo get_settings('siteurl'); ?>/wp-admin/wp-admin.css" type="text/css" />
 	<script type="text/javascript">
@@ -280,11 +280,11 @@ default:
 
 <div id="login">
 <p>
-	<a href="<?php echo get_settings('siteurl'); ?>" title="Are you lost?">Back to blog?</a><br />
+	<a href="<?php echo get_settings('siteurl'); ?>" title="<?php _e('Are you lost?') ?>"><?php _e('Back to blog?') ?></a><br />
 <?php if (get_settings('users_can_register')) { ?>
-	<a href="<?php echo get_settings('siteurl'); ?>/wp-register.php" title="Register to be an author">Register?</a><br />
+	<a href="<?php echo get_settings('siteurl'); ?>/wp-register.php" title="<?php _e('Register to be an author') ?>"><?php _e('Register?') ?></a><br />
 <?php } ?>
-	<a href="<?php echo get_settings('siteurl'); ?>/wp-login.php?action=lostpassword" title="Password Lost and Found">Lost your password?</a>
+	<a href="<?php echo get_settings('siteurl'); ?>/wp-login.php?action=lostpassword" title="<?php _e('Password Lost and Found') ?>"><?php _e('Lost your password?') ?></a>
 </p>
 
 <?php
@@ -304,8 +304,8 @@ if ($error) echo "<div align=\"right\" style=\"padding:4px;\"><font color=\"#FF0
 	<input type="hidden" name="redirect_to" value="wp-admin/" />
 <?php } ?>
 	<input type="hidden" name="action" value="login" />
-	<label>Login: <input type="text" name="log" id="log" value="" size="20" tabindex="1" /></label><br />
-	<label>Password: <input type="password" name="pwd" value="" size="20" tabindex="2" /></label><br />
+	<label><?php _e('Login:') ?> <input type="text" name="log" id="log" value="" size="20" tabindex="1" /></label><br />
+	<label><?php _e('Password:') ?> <input type="password" name="pwd" value="" size="20" tabindex="2" /></label><br />
 	<input type="submit" name="Submit2" value="OK" class="search" tabindex="3" />
 </form>
 
