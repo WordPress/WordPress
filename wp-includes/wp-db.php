0864fdf0f2478ad5d6881f65b67b8e29fdc29424
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-04-26 02:54:06 +0000

    Updated to use the latest and greatest.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1180 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -1,34 +1,30 @@
 <?php
-// ==================================================================
-//  Author: Justin Vincent (justin@visunet.ie)
-//	Web: 	http://php.justinvincent.com
-//	Name: 	ezSQL
-// 	Desc: 	Class to make it very easy to deal with mySQL database connections.
-//  WordPress is using this class to make the code cleaner and faster.
-//  We highly recommend it.
-//  We have modified the HTML it returns slightly.
+//  WordPress DB Class
 
-define('EZSQL_VERSION', '1.21');
+//  ORIGINAL CODE FROM:
+//  Justin Vincent (justin@visunet.ie)
+//	http://php.justinvincent.com
+
+define('EZSQL_VERSION', 'WP1.25');
 define('OBJECT', 'OBJECT', true);
-define('ARRAY_A', 'ARRAY_A', true);
-define('ARRAY_N', 'ARRAY_N', true);
-define('SAVEQUERIES', true);
-
-//	The Main Class, renamed to avoid conflicts.
+define('ARRAY_A', 'ARRAY_A', false);
+define('ARRAY_N', 'ARRAY_N', false);
+if (!defined('SAVEQUERIES'))
+	define('SAVEQUERIES', false);
 
 class wpdb {
 
-	var $debug_called;
-	var $vardump_called;
 	var $show_errors = true;
-	var $querycount;
+	var $num_queries = 0;	
+	var $last_query;
+	var $col_info;
 
 	// ==================================================================
 	//	DB Constructor - connects to the server and selects a database
 
 	function wpdb($dbuser, $dbpassword, $dbname, $dbhost) {
 		$this->dbh = @mysql_connect($dbhost,$dbuser,$dbpassword);
-		if ( ! $this->dbh )	{
+		if (!$this->dbh) {
 			die("<div>
 			<p><strong>Error establishing a database connection!</strong> This probably means that the connection information in your <code>wp-config.php</code> file is incorrect. Double check it and try again.</p>
 			<ul>
@@ -41,14 +37,13 @@ class wpdb {
 		}
 
 		$this->select($dbname);
-		$this->querycount = 0;
 	}
 
 	// ==================================================================
 	//	Select a DB (if another one needs to be selected)
 
 	function select($db) {
-		if ( !@mysql_select_db($db,$this->dbh)) {
+		if (!@mysql_select_db($db,$this->dbh)) {
 			die("
 			<p>We're having a little trouble selecting the proper database for WordPress.</p>
 			<ul>
@@ -71,31 +66,21 @@ class wpdb {
 	//	Print SQL/DB error.
 
 	function print_error($str = '') {
-		// All errors go to the global error array $EZSQL_ERROR..
 		global $EZSQL_ERROR;
-
-		// If no special error string then use mysql default..
-		if ( !$str ) $str = mysql_error();
-		
-		// Log this error to the global array..
-		$EZSQL_ERROR[] = array 
-						(
-							'query' => $this->last_query,
-							'error_str'  => $str
-						);
+		if (!$str) $str = mysql_error();
+		$EZSQL_ERROR[] = 
+		array ('query' => $this->last_query, 'error_str' => $str);
 
 		// Is error output turned on or not..
 		if ( $this->show_errors ) {
 			// If there is an error then take note of it
 			print "<div id='error'>
-			<p><strong>SQL/DB Error:</strong><br />
-			[<span style='color: #007;'>$str</span>]<br />
+			<p><strong>Database error:</strong> [$str]<br />
 			<code>$this->last_query</code></p>
 			</div>";
 		} else {
 			return false;	
 		}
-
 	}
 
 	// ==================================================================
@@ -113,19 +98,17 @@ class wpdb {
 	//	Kill cached query results
 
 	function flush() {
-		// Get rid of these
 		$this->last_result = null;
 		$this->col_info = null;
 		$this->last_query = null;
-
 	}
 
 	// ==================================================================
 	//	Basic Query	- see docs for more detail
 
 	function query($query) {
-
-		// Flush cached values..
+		// initialise return
+		$return_val = 0;
 		$this->flush();
 
 		// Log how the function was called
@@ -135,84 +118,60 @@ class wpdb {
 		$this->last_query = $query;
 
 		// Perform the query via std mysql_query function..
-		$this->result = mysql_query($query, $this->dbh);
-		++$this->querycount;
+		$this->result = @mysql_query($query,$this->dbh);
+		++$this->num_queries;
 		if (SAVEQUERIES) {
 			$this->savedqueries[] = $query;
 		}
 
-		// If there was an insert, delete or update see how many rows were affected
-		// (Also, If there there was an insert take note of the insert_id
-		$query_type = array('insert','delete','update','replace');
-
-		// loop through the above array
-		foreach ( $query_type as $word ) {
-			// This is true if the query starts with insert, delete or update
-			if ( preg_match("/^\\s*$word /i",$query) ) {
-				$this->rows_affected = mysql_affected_rows();
-				// This gets the insert ID
-				if ( $word == 'insert' || $word == 'replace' ) {
-					$this->insert_id = mysql_insert_id($this->dbh);
-				}
-				$this->result = false;
-			}
-		}
-
-		if ( mysql_error() ) { // If there is an error then take note of it..
+		// If there is an error then take note of it..
+		if ( mysql_error() ) {
 			$this->print_error();
-		} else {
-			// In other words if this was a select statement..
-			if ( $this->result ) {
-				// =======================================================
-				// Take note of column info
-
-				$i=0;
-				while ($i < @mysql_num_fields($this->result)) {
-					$this->col_info[$i] = @mysql_fetch_field($this->result);
-					$i++;
-				}
-
-				// =======================================================
-				// Store Query Results
-
-				$i=0;
-				while ( $row = @mysql_fetch_object($this->result) ) {
-					// Store relults as an objects within main array
-					$this->last_result[$i] = $row;
-					$i++;
-				}
-
-				// Log number of rows the query returned
-				$this->num_rows = $i;
-
-				@mysql_free_result($this->result);
-
-
-				// If there were results then return true for $db->query
-				if ( $i ) {
-					return true;
-				} else {
-					return false;
-				}
-
-			} else { // Update insert etc. was good..
-				return true;
-			}
+			return false;
 		}
+
+		if ( preg_match("/^\\s*(insert|delete|update|replace) /i",$query) ) {
+			$this->rows_affected = mysql_affected_rows();
+			// Take note of the insert_id
+			if ( preg_match("/^\\s*(insert|replace) /i",$query) ) {
+				$this->insert_id = mysql_insert_id($this->dbh);	
+			}
+			// Return number of rows affected
+			$return_val = $this->rows_affected;
+		} else {
+			$i = 0;
+			while ($i < @mysql_num_fields($this->result)) {
+				$this->col_info[$i] = @mysql_fetch_field($this->result);
+				$i++;
+			}
+			$num_rows = 0;
+			while ( $row = @mysql_fetch_object($this->result) ) {
+				$this->last_result[$num_rows] = $row;
+				$num_rows++;
+			}
+
+			@mysql_free_result($this->result);
+
+			// Log number of rows the query returned
+			$this->num_rows = $num_rows;
+			
+			// Return number of rows selected
+			$return_val = $this->num_rows;
+		}
+
+		// If debug ALL queries
+		$this->trace || $this->debug_all ? $this->debug() : null ;
+
+		return $return_val;
 	}
 
 	// ==================================================================
 	//	Get one variable from the DB - see docs for more detail
 
-	function get_var($query=null, $x=0, $y=0) {
-
-		// Log how the function was called
+	function get_var($query=null, $x = 0, $y = 0) {
 		$this->func_call = "\$db->get_var(\"$query\",$x,$y)";
-
-		// If there is a query then perform it if not then use cached results..
-		if ( $query ) {
+		if ( $query )
 			$this->query($query);
-		}
 
 		// Extract var out of cached results based x,y vals
 		if ( $this->last_result[$y] ) {
@@ -226,47 +185,33 @@ class wpdb {
 	// ==================================================================
 	//	Get one row from the DB - see docs for more detail
 
-	function get_row($query=null, $output=OBJECT, $y=0) {
-
-		// Log how the function was called
+	function get_row($query = null, $output = OBJECT, $y = 0) {
 		$this->func_call = "\$db->get_row(\"$query\",$output,$y)";
-
-		// If there is a query then perform it if not then use cached results..
-		if ( $query ) {
+		if ( $query )
 			$this->query($query);
-		}
 
-		// If the output is an object then return object using the row offset..
 		if ( $output == OBJECT ) {
-			return $this->last_result[$y]?$this->last_result[$y]:null;
-		}
-		// If the output is an associative array then return row as such..
-		elseif ( $output == ARRAY_A ) {
-			return $this->last_result[$y]?get_object_vars($this->last_result[$y]):null;
-		}
-		// If the output is an numerical array then return row as such..
-		elseif ( $output == ARRAY_N ) {
-			return $this->last_result[$y]?array_values(get_object_vars($this->last_result[$y])):null;
-		}
-		// If invalid output type was specified..
-		else {
+			return $this->last_result[$y] ? $this->last_result[$y] : null;
+		} elseif ( $output == ARRAY_A ) {
+			return $this->last_result[$y] ? get_object_vars($this->last_result[$y]) : null;
+		} elseif ( $output == ARRAY_N ) {
+			return $this->last_result[$y] ? array_values(get_object_vars($this->last_result[$y])) : null;
+		} else {
 			$this->print_error(" \$db->get_row(string query, output type, int offset) -- Output type must be one of: OBJECT, ARRAY_A, ARRAY_N");
 		}
-
 	}
 
 	// ==================================================================
 	//	Function to get 1 column from the cached result set based in X index
 	// se docs for usage and info
 
-	function get_col($query=null,$x=0) {
-		// If there is a query then perform it if not then use cached results..
-		if ( $query ) {
+	function get_col($query = null , $x = 0) {
+		if ( $query )
 			$this->query($query);
-		}
+
 		// Extract the column values
 		for ( $i=0; $i < count($this->last_result); $i++ ) {
-			$new_array[$i] = $this->get_var(null,$x,$i);
+			$new_array[$i] = $this->get_var(null, $x, $i);
 		}
 		return $new_array;
 	}
@@ -274,31 +219,25 @@ class wpdb {
 	// ==================================================================
 	// Return the the query as a result set - see docs for more details
 
-	function get_results($query=null, $output = OBJECT){
-
-		// Log how the function was called
+	function get_results($query = null, $output = OBJECT) {
 		$this->func_call = "\$db->get_results(\"$query\", $output)";
 
-		// If there is a query then perform it if not then use cached results..
-		if ( $query ) {
+		if ( $query )
 			$this->query($query);
-		}
 
 		// Send back array of objects. Each row is an object
 		if ( $output == OBJECT ) {
 			return $this->last_result;
 		} elseif ( $output == ARRAY_A || $output == ARRAY_N ) {
 			if ( $this->last_result ) {
-				$i=0;
+				$i = 0;
 				foreach( $this->last_result as $row ) {
 					$new_array[$i] = (array) $row;
 					if ( $output == ARRAY_N ) {
 						$new_array[$i] = array_values($new_array[$i]);
 					}
-
 					$i++;
 				}
-
 				return $new_array;
 			} else {
 				return null;
@@ -311,11 +250,10 @@ class wpdb {
 	// Function to get column meta data info pertaining to the last query
 	// see docs for more info and usage
 
-	function get_col_info($info_type='name', $col_offset=-1) {
-
+	function get_col_info($info_type = 'name', $col_offset = -1) {
 		if ( $this->col_info ) {
 			if ( $col_offset == -1 ) {
-				$i=0;
+				$i = 0;
 				foreach($this->col_info as $col ) {
 					$new_array[$i] = $col->{$info_type};
 					$i++;
@@ -324,11 +262,8 @@ class wpdb {
 			} else {
 				return $this->col_info[$col_offset]->{$info_type};
 			}
-
 		}
-
 	}
-
 }
 
 $wpdb = new wpdb(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
