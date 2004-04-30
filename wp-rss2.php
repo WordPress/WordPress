168f5ce5678a94e993640e77f1a1080e2b90ca7e
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-04-30 09:13:43 +0000

    Remove language elements from RSS files.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1216 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-rss2.php
+++ wp-rss2.php
@@ -20,7 +20,6 @@ header('Content-type: text/xml', true);
 	<title><?php bloginfo_rss('name') ?></title>
 	<link><?php bloginfo_rss('url') ?></link>
 	<description><?php bloginfo_rss("description") ?></description>
-	<language><?php echo get_settings('rss_language'); ?></language>
 	<copyright>Copyright <?php echo mysql2date('Y', get_lastpostdate()); ?></copyright>
 	<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_lastpostmodified('GMT')); ?></pubDate>
 	<generator>http://wordpress.org/?v=<?php echo $wp_version ?></generator>
