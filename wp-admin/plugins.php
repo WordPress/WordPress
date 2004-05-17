0dbe04e28e9d6b6c2171babc48998c41122da753
michelvaldrighi <michelvaldrighi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-05-17 20:16:54 +0000

    using check_admin_referer for plugin (de)activation
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1299 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/plugins.php
+++ wp-admin/plugins.php
@@ -3,6 +3,9 @@
 if (isset($_GET['action'])) {
 	$standalone = 1;
 	require_once('admin-header.php');
+
+	check_admin_referer();
+
 	if ('activate' == $_GET['action']) {
 		$current = "\n" . get_settings('active_plugins') . "\n";
 		$current = preg_replace("|(\n)+\s*|", "\n", $current);
