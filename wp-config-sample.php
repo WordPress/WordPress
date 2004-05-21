901d6403b89ddf5908236a067c5ccb8a8d9caaee
rboren <rboren@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-05-21 02:47:45 +0000

    Fix spelling error.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1330 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -15,7 +15,7 @@ $table_prefix  = 'wp_';   // example: 'wp_' or 'b2' or 'mylogin_'
 // Change this to localize WordPress.  A corresponding MO file for the
 // chosen language must be installed to wp-includes/languages.
 // For example, install de.mo to wp-includes/languages and set WPLANG to 'de'
-// to enable German lanuage support.
+// to enable German language support.
 define ('WPLANG', '');
 
 /* Stop editing */
