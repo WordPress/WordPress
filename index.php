713e887711e2a8dfeeef1fff15cac03b75438763
rboren <rboren@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-06-06 15:39:23 +0000

    Pass 'Edit This' string to edit_post_link so that it can be translated. http://wordpress.org/support/4/6976
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1390 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ require('./wp-blog-header.php');
 	
 <div class="post">
 	 <h3 class="storytitle" id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>"><?php the_title(); ?></a></h3>
-	<div class="meta"><?php _e("Filed under:"); ?> <?php the_category() ?> &#8212; <?php the_author() ?> @ <?php the_time() ?> <?php edit_post_link(); ?></div>
+	<div class="meta"><?php _e("Filed under:"); ?> <?php the_category() ?> &#8212; <?php the_author() ?> @ <?php the_time() ?> <?php edit_post_link(__('Edit This')); ?></div>
 	
 	<div class="storycontent">
 		<?php the_content(); ?>
