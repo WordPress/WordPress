a2f67bc9a4ec110de6481f3065136da4c12a5208
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-06-11 18:09:21 +0000

    Ugly but works. http://mosquito.wordpress.org/bug_view_page.php?bug_id=0000014
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1410 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/template-functions-general.php
+++ wp-includes/template-functions-general.php
@@ -419,9 +419,9 @@ function get_calendar($daylength = 1) {
                 $ak_titles_for_day['day_'.$ak_post_title->dom] = '';
             }
             if (empty($ak_titles_for_day["$ak_post_title->dom"])) { // first one
-                $ak_titles_for_day["$ak_post_title->dom"] = htmlspecialchars(stripslashes($ak_post_title->post_title));
+                $ak_titles_for_day["$ak_post_title->dom"] = str_replace('"', '&quot;', wptexturize(stripslashes($ak_post_title->post_title)));
             } else {
-                $ak_titles_for_day["$ak_post_title->dom"] .= $ak_title_separator . htmlspecialchars(stripslashes($ak_post_title->post_title));
+                $ak_titles_for_day["$ak_post_title->dom"] .= $ak_title_separator . str_replace('"', '&quot;', wptexturize(stripslashes($ak_post_title->post_title)));
             }
         }
     }
