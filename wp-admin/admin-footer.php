f0ab0be0cbfcad9cd6e56c3c6d63c4aee7983ae4
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-07-23 07:50:21 +0000

    New, pretty footer.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1476 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -1,9 +1,10 @@
 
-<p id="footer"><strong><a href="http://wordpress.org/">WordPress</a></strong> <?php bloginfo('version'); ?> &#8212; <a href="http://wordpress.org/support/"><?php _e('Support Forums'); ?></a><br />
-<?php
-     printf(__('%s seconds'), number_format(timer_stop(), 2));
-?>
+<div id="footer"><p><a href="http://wordpress.org/"><img src="../wp-images/wp-small.png" alt="WordPress" /></a><br />
+<?php bloginfo('version'); ?> &#8212; <a href="http://wordpress.org/support/"><?php _e('Support Forums'); ?></a><br />
+<?php printf(__('%s seconds'), number_format(timer_stop(), 2)); ?>
 </p>
+<p>	<a href="http://getfirefox.com/" title="<?php _e('WordPress recommends the open-source Firefox browser') ?>"><img src="http://www.mozilla.org/products/firefox/buttons/getfirefox_88x31.png" width="88" height="31" alt="Get Firefox"></a></p>
+</div>
 <?php do_action('admin_footer', ''); ?>
 </body>
 </html>
