3c6bc4ae08753c21f0b68ef2528a8c28348f7e88
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-08-20 17:14:27 +0000

    Header/footer template start
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1542 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- index.php
+++ index.php
@@ -1,36 +1,9 @@
 <?php 
 /* Don't remove this line. */
 require('./wp-blog-header.php');
+include(ABSPATH . '/wp-header.php');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
 
-<head profile="http://gmpg.org/xfn/1">
-	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
-	
-	<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
-	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
-
-	<style type="text/css" media="screen">
-		@import url( <?php echo get_settings('siteurl'); ?>/wp-layout.css );
-	</style>
-	
-	<link rel="stylesheet" type="text/css" media="print" href="<?php echo get_settings('siteurl'); ?>/print.css" />
-	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
-	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
-	
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-    <?php wp_get_archives('type=monthly&format=link'); ?>
-	<?php //comments_popup_script(); // off by default ?>
-	<?php wp_head(); ?>
-</head>
-
-<body>
-<div id="rap">
-<h1 id="header"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
-
-<div id="content">
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 <?php the_date('','<h2>','</h2>'); ?>
@@ -61,55 +34,5 @@ require('./wp-blog-header.php');
 <?php endif; ?>
 
 <?php posts_nav_link(' &#8212; ', __('&laquo; Previous Page'), __('Next Page &raquo;')); ?>
-</div>
 
-
-
-<div id="menu">
-
-<ul>
-	<?php get_links_list(); ?>
- <li id="categories"><?php _e('Categories:'); ?>
-	<ul>
-	<?php wp_list_cats(); ?>
-	</ul>
- </li>
- <li id="search">
-   <label for="s"><?php _e('Search:'); ?></label>	
-   <form id="searchform" method="get" action="<?php echo $PHP_SELF; ?>">
-	<div>
-		<input type="text" name="s" id="s" size="15" /><br />
-		<input type="submit" name="submit" value="<?php _e('Search'); ?>" />
-	</div>
-	</form>
- </li>
- <li id="archives"><?php _e('Archives:'); ?>
- 	<ul>
-	 <?php wp_get_archives('type=monthly'); ?>
- 	</ul>
- </li>
- <li id="calendar">
-	<?php get_calendar(); ?>
- </li>
- <li id="meta"><?php _e('Meta:'); ?>
- 	<ul>
-		<li><?php wp_register(); ?></li>
-		<li><?php wp_loginout(); ?></li>
-		<li><a href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('Syndicate this site using RSS'); ?>"><?php _e('<abbr title="Really Simple Syndication">RSS</abbr>'); ?></a></li>
-		<li><a href="<?php bloginfo('comments_rss2_url'); ?>" title="<?php _e('The latest comments to all posts in RSS'); ?>"><?php _e('Comments <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a></li>
-		<li><a href="http://validator.w3.org/check/referer" title="<?php _e('This page validates as XHTML 1.0 Transitional'); ?>"><?php _e('Valid <abbr title="eXtensible HyperText Markup Language">XHTML</abbr>'); ?></a></li>
-		<li><a href="http://gmpg.org/xfn/"><abbr title="XHTML Friends Network">XFN</abbr></a></li>
-		<li><a href="http://wordpress.org/" title="<?php _e('Powered by WordPress, state-of-the-art semantic personal publishing platform.'); ?>"><abbr title="WordPress">WP</abbr></a></li>
-		<?php wp_meta(); ?>
-	</ul>
- </li>
-
-</ul>
-
-</div>
-
-</div>
-
-<p class="credit"><!--<?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. --> <cite><?php echo sprintf(__("Powered by <a href='http://wordpress.org' title='%s'><strong>WordPress</strong></a>"), __("Powered by WordPress, state-of-the-art semantic personal publishing platform.")); ?></cite></p>
-</body>
-</html>
+<?php include(ABSPATH . '/wp-footer.php'); ?>
---
+++ wp-header.php
@@ -0,0 +1,37 @@
+<?php
+// If a header.php file exists in the WP root directory we
+// use that, otherwise use this default wp-header.php file.
+if ( file_exists(ABSPATH . '/header.php') ) :
+	include(ABSPATH . '/header.php');
+else :
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head profile="http://gmpg.org/xfn/1">
+	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
+	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
+
+	<style type="text/css" media="screen">
+		@import url( <?php echo get_settings('siteurl'); ?>/wp-layout.css );
+	</style>
+	
+	<link rel="stylesheet" type="text/css" media="print" href="<?php echo get_settings('siteurl'); ?>/print.css" />
+	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
+	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
+	
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <?php wp_get_archives('type=monthly&format=link'); ?>
+	<?php //comments_popup_script(); // off by default ?>
+	<?php wp_head(); ?>
+</head>
+
+<body>
+<div id="rap">
+<h1 id="header"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+
+<div id="content">
+<?php endif; ?>
