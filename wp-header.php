660dc0ce3a3ab1b49796a4c7ce470eafceb7f251
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-08-25 07:40:46 +0000

    XFN 1.1 profile
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1559 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-header.php
+++ wp-header.php
@@ -8,7 +8,7 @@ else :
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
-<head profile="http://gmpg.org/xfn/1">
+<head profile="http://gmpg.org/xfn/11">
 	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
 	
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
