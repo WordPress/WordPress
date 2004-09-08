167b0ac0f5db6326b59973962fdecbe7777ebace
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-09-08 09:15:18 +0000

    Extra handy plugin hook.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1625 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-footer.php
+++ wp-footer.php
@@ -55,6 +55,8 @@ else :
 </div>
 
 <p class="credit"><!--<?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. --> <cite><?php echo sprintf(__("Powered by <a href='http://wordpress.org' title='%s'><strong>WordPress</strong></a>"), __("Powered by WordPress, state-of-the-art semantic personal publishing platform.")); ?></cite></p>
+
+<?php do_action('wp_footer', '');
 </body>
 </html>
 <?php endif; ?>
