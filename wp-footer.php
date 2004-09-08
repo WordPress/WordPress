e227cb587fd398b8b8d91dc9b423a8e45f42ff2b
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-09-08 09:53:20 +0000

    Fixed type and moving out the sidebar.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1626 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-footer.php
+++ wp-footer.php
@@ -9,54 +9,16 @@ else :
 
 
 
-<div id="menu">
-
-<ul>
-	<?php get_links_list(); ?>
- <li id="categories"><?php _e('Categories:'); ?>
-	<ul>
-	<?php wp_list_cats(); ?>
-	</ul>
- </li>
- <li id="search">
-   <label for="s"><?php _e('Search:'); ?></label>	
-   <form id="searchform" method="get" action="<?php echo $PHP_SELF; ?>">
-	<div>
-		<input type="text" name="s" id="s" size="15" /><br />
-		<input type="submit" name="submit" value="<?php _e('Search'); ?>" />
-	</div>
-	</form>
- </li>
- <li id="archives"><?php _e('Archives:'); ?>
- 	<ul>
-	 <?php wp_get_archives('type=monthly'); ?>
- 	</ul>
- </li>
- <li id="calendar">
-	<?php get_calendar(); ?>
- </li>
- <li id="meta"><?php _e('Meta:'); ?>
- 	<ul>
-		<li><?php wp_register(); ?></li>
-		<li><?php wp_loginout(); ?></li>
-		<li><a href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('Syndicate this site using RSS'); ?>"><?php _e('<abbr title="Really Simple Syndication">RSS</abbr>'); ?></a></li>
-		<li><a href="<?php bloginfo('comments_rss2_url'); ?>" title="<?php _e('The latest comments to all posts in RSS'); ?>"><?php _e('Comments <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a></li>
-		<li><a href="http://validator.w3.org/check/referer" title="<?php _e('This page validates as XHTML 1.0 Transitional'); ?>"><?php _e('Valid <abbr title="eXtensible HyperText Markup Language">XHTML</abbr>'); ?></a></li>
-		<li><a href="http://gmpg.org/xfn/"><abbr title="XHTML Friends Network">XFN</abbr></a></li>
-		<li><a href="http://wordpress.org/" title="<?php _e('Powered by WordPress, state-of-the-art semantic personal publishing platform.'); ?>"><abbr title="WordPress">WP</abbr></a></li>
-		<?php wp_meta(); ?>
-	</ul>
- </li>
-
-</ul>
-
-</div>
+<?php
+// This code pulls in the sidebar:
+include(ABSPATH . '/wp-sidebar.php');
+?>
 
 </div>
 
 <p class="credit"><!--<?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. --> <cite><?php echo sprintf(__("Powered by <a href='http://wordpress.org' title='%s'><strong>WordPress</strong></a>"), __("Powered by WordPress, state-of-the-art semantic personal publishing platform.")); ?></cite></p>
 
-<?php do_action('wp_footer', '');
+<?php do_action('wp_footer', ''); ?>
 </body>
 </html>
 <?php endif; ?>
