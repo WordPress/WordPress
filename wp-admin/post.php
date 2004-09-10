d3927282550d1015e003b1f8f60821f8fe6fc4f4
rboren <rboren@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-09-10 06:31:19 +0000

    Honor default category in post forms. Bug 182.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1634 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/post.php
+++ wp-admin/post.php
@@ -783,6 +783,7 @@ default:
 		$comment_status = get_settings('default_comment_status');
 		$ping_status = get_settings('default_ping_status');
 		$post_pingback = get_settings('default_pingback_flag');
+		$default_post_cat = get_settings('default_category');
 
 		if (get_settings('advanced_edit')) {
 			include('edit-form-advanced.php');
