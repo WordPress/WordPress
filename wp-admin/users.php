d7364bbf7056efde2124b2ecc020cef4771545ba
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-09-21 23:41:27 +0000

    Usability enhancements.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1702 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/users.php
+++ wp-admin/users.php
@@ -243,7 +243,7 @@ default:
 	if ($users) {
 ?>
 <div class="wrap">
-	<h2><?php _e('Users') ?></h2>
+	<h2><?php _e('Registered Users') ?></h2>
 	<table cellpadding="3" cellspacing="3" width="100%">
 	<tr>
 		<th><?php _e('ID') ?></th>
@@ -251,7 +251,7 @@ default:
 		<th><?php _e('Name') ?></th>
 		<th><?php _e('E-mail') ?></th>
 		<th><?php _e('Website') ?></th>
-		<th><?php _e('Level') ?></th>
+		<th></th>
 		<th></th>
 		<th></th>
 	</tr>
@@ -276,16 +276,15 @@ echo "\n<tr $style>
 <td><a href='$url' title='website: $url'>$short_url</a></td>
 <td align='center'>";
 
-	echo $user_data->user_level;
 	if ($user_level >= 6)
-		echo " <a href=\"users.php?action=promote&amp;id=".$user_data->ID."&amp;prom=up\">+</a> ";	
+		echo "<a href='users.php?action=promote&amp;id=$user_data->ID&amp;prom=up' class='edit'>". __('Promote') . '</a>';	
 	echo "</td>\n";
 	echo '<td>';
 	if (($user_level >= 6) and ($user_level > $user_data->user_level))
 		echo "<a href='user-edit.php?user_id=$user_data->ID' class='edit'>Edit</a>";
 	echo '</td><td>';
 	if ($user_level >= 6)
-		echo "<a href='users.php?action=delete&amp;id=$user_data->ID' class='delete'>Delete</a>";
+		echo "<a href='users.php?action=delete&amp;id=$user_data->ID' class='delete'>" . __('Delete'). '</a>';
 	echo '</td></tr>';
 
 }
