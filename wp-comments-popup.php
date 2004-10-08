e2f52aedec9f90bdfb3dd64306b0aa5b2089037a
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-10-08 17:58:02 +0000

    Remove print.css reference, don't echo bloginfo.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1763 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-comments-popup.php
+++ wp-comments-popup.php
@@ -12,7 +12,7 @@ foreach ($posts as $post) { start_wp();
 
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo get_settings('blog_charset'); ?>" />
 	<style type="text/css" media="screen">
-		@import url( <?php echo bloginfo('stylesheet_url'); ?> );
+		@import url( <?php bloginfo('stylesheet_url'); ?> );
 		body { margin: 3px; }
 	</style>
 
--- wp-header.php
+++ wp-header.php
@@ -12,13 +12,12 @@ else :
 	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
 	
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
-	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
+	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats please -->
 
 	<style type="text/css" media="screen">
-		@import url( <?php echo bloginfo('stylesheet_url'); ?> );
+		@import url( <?php bloginfo('stylesheet_url'); ?> );
 	</style>
-	
-	<link rel="stylesheet" type="text/css" media="print" href="<?php echo get_settings('siteurl'); ?>/print.css" />
+
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
