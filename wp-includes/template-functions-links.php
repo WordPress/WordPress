a53659c2ab82ba4bf71db4b216f8f282af38c883
michelvaldrighi <michelvaldrighi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-10-13 14:18:17 +0000

    unb0rked the pagination links
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1791 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/template-functions-links.php
+++ wp-includes/template-functions-links.php
@@ -504,9 +504,9 @@ function next_posts($max_page = 0) { // original by cfactor at cooltux.org
 function next_posts_link($label='Next Page &raquo;', $max_page=0) {
     global $paged, $result, $request, $posts_per_page, $wpdb;
     if (!$max_page) {
-        preg_match('#WHERE (.*) GROUP BY#', $request, $matches);
-        $where = $matches[1];
-        $numposts = $wpdb->get_var("SELECT COUNT(ID) FROM $wpdb->posts WHERE $where");
+        preg_match('#FROM (.*) GROUP BY#', $request, $matches);
+        $fromwhere = $matches[1];
+        $numposts = $wpdb->get_var("SELECT COUNT(ID) FROM $fromwhere");
         $max_page = ceil($numposts / $posts_per_page);
     }
     if (!$paged)
@@ -545,9 +545,9 @@ function posts_nav_link($sep=' &#8212; ', $prelabel='&laquo; Previous Page', $nx
 	if (! is_single()) {
 
 		if (get_query_var('what_to_show') == 'posts') {
-			preg_match('#WHERE (.*) GROUP BY#', $request, $matches);
-			$where = $matches[1];
-			$numposts = $wpdb->get_var("SELECT COUNT(ID) FROM $wpdb->posts WHERE $where");
+			preg_match('#FROM (.*) GROUP BY#', $request, $matches);
+			$fromwhere = $matches[1];
+			$numposts = $wpdb->get_var("SELECT COUNT(ID) FROM $fromwhere");
 			$max_page = ceil($numposts / $posts_per_page);
 		} else {
 			$max_page = 999999;
