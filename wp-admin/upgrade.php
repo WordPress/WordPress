eb08740fc0f31ce6cee9400749141302989a1a31
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-10-15 04:50:40 +0000

    Fix for http://mosquito.wordpress.org/bug_view_page.php?bug_id=0000351 from MC_incubus
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1803 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/upgrade.php
+++ wp-admin/upgrade.php
@@ -1,9 +1,9 @@
-<?php
+l<?php
 $_wp_installing = 1;
 if (!file_exists('../wp-config.php')) die("There doesn't seem to be a wp-config.php file. Double check that you updated wp-config-sample.php with the proper database connection information and renamed it to wp-config.php.");
 require('../wp-config.php');
 timer_start();
-require('upgrade-functions.php');
+require_once(ABSPATH . '/wp-admin/upgrade-functions.php');
 
 $step = $_GET['step'];
 if (!$step) $step = 0;
