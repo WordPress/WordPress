4581b28d54d336174531d4cdf4630ac4c81bbde5
michelvaldrighi <michelvaldrighi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-10-17 12:42:18 +0000

    honey, your cookies are delicious, but not when they're empty
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1807 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/auth.php
+++ wp-admin/auth.php
@@ -1,7 +1,8 @@
 <?php
 require_once('../wp-config.php');
 
-if ( !empty($_COOKIE['wordpressuser_' . COOKIEHASH]) && !wp_login($_COOKIE['wordpressuser_' . COOKIEHASH], $_COOKIE['wordpresspass_' . COOKIEHASH], true) ) {
+if ( (!empty($_COOKIE['wordpressuser_' . COOKIEHASH]) && !wp_login($_COOKIE['wordpressuser_' . COOKIEHASH], $_COOKIE['wordpresspass_' . COOKIEHASH], true))
+	|| (empty($_COOKIE['wordpressuser_' . COOKIEHASH])) ) {
 	header('Expires: Wed, 5 Jun 1979 23:41:00 GMT'); // Michel's birthday
 	header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 	header('Cache-Control: no-cache, must-revalidate');
