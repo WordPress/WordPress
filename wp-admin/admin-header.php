cf2d3b5aab163d29b2e40235f8ca450281fadb64
rboren <rboren@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-10-28 04:03:56 +0000

    require_once admin.php from admin-header.php.  This will avoid having to change plugins that use the old style include.  Bug 393.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@1837 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -1,3 +1,4 @@
+<?php require_once('admin.php'); ?>
 <?php get_admin_page_title(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
