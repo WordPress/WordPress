52929c176b5d2f23bc299e38438ea46cfcdd5eef
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2004-12-30 11:27:33 +0000

    Move inside for better clearing and such.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2015 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/wp-footer.php
+++ wp-includes/wp-footer.php
@@ -3,9 +3,9 @@
 
 <?php get_sidebar(); ?>
 
-</div>
-
 <p class="credit"><!--<?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. --> <cite><?php echo sprintf(__("Powered by <a href='http://wordpress.org' title='%s'><strong>WordPress</strong></a>"), __("Powered by WordPress, state-of-the-art semantic personal publishing platform.")); ?></cite></p>
+
+</div>
 
 <?php do_action('wp_footer', ''); ?>
 </body>
