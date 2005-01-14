02beec9806378212c8eed03a7707635f941c7747
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-01-14 06:18:47 +0000

    Conditional support for feed:
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2089 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/footer.php
+++ wp-content/themes/default/footer.php
@@ -4,8 +4,8 @@
 	<p>
 		<?php bloginfo('name'); ?> is proudly powered by 
 		<a href="http://wordpress.org">WordPress</a>
-		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
-		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+		<br /><a href="feed:<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
+		and <a href="feed:<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
 		<!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. -->
 	</p>
 </div>
