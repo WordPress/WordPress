31de67acf1950e30b426ab7928f648054c24fcd4
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-01-24 07:39:23 +0000

    Don't show other drafts on one-user blogs - http://mosquito.wordpress.org/view.php?id=723
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2132 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit.php
+++ wp-admin/edit.php
@@ -12,10 +12,12 @@ get_currentuserinfo();
 $drafts = $wpdb->get_results("SELECT ID, post_title FROM $wpdb->posts WHERE post_status = 'draft' AND post_author = $user_ID");
 if (1 < $user_level) {
 	$editable = $wpdb->get_col("SELECT ID FROM $wpdb->users WHERE user_level <= '$user_level' AND ID != $user_ID");
-        if( is_array( $editable ) == false )
-            $editable = array( "1" );
-	$editable = join(',', $editable);
-	$other_drafts = $wpdb->get_results("SELECT ID, post_title FROM $wpdb->posts WHERE post_status = 'draft' AND post_author IN ($editable) ");
+	if( is_array( $editable ) == false )
+			$other_drafts = '';
+	else {
+		$editable = join(',', $editable);
+		$other_drafts = $wpdb->get_results("SELECT ID, post_title FROM $wpdb->posts WHERE post_status = 'draft' AND post_author IN ($editable) ");
+	}
 } else {
 	$other_drafts = false;
 }
