fa2d1eca23b9a6f0409df1fba2aa2ecbab231fd7
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-01-24 06:15:25 +0000

    Encode for JS
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2126 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit.php
+++ wp-admin/edit.php
@@ -149,7 +149,7 @@ $class = ('alternate' == $class) ? '' : 'alternate';
     <td><?php the_author() ?></td> 
 	<td><a href="<?php the_permalink(); ?>" rel="permalink" class="edit"><?php _e('View'); ?></a></td>
     <td><?php if (($user_level > $authordata->user_level) or ($user_login == $authordata->user_login)) { echo "<a href='post.php?action=edit&amp;post=$id' class='edit'>" . __('Edit') . "</a>"; } ?></td> 
-    <td><?php if (($user_level > $authordata->user_level) or ($user_login == $authordata->user_login)) { echo "<a href='post.php?action=delete&amp;post=$id' class='delete' onclick=\"return confirm('" . sprintf(__("You are about to delete this post \'%s\'\\n  \'OK\' to delete, \'Cancel\' to stop."), the_title('','',0)) . "')\">" . __('Delete') . "</a>"; } ?></td> 
+    <td><?php if (($user_level > $authordata->user_level) or ($user_login == $authordata->user_login)) { echo "<a href='post.php?action=delete&amp;post=$id' class='delete' onclick=\"return confirm('" . sprintf(__("You are about to delete this post \'%s\'\\n  \'OK\' to delete, \'Cancel\' to stop."), wp_specialchars(get_the_title('', ''), 1) ) . "')\">" . __('Delete') . "</a>"; } ?></td> 
   </tr> 
 <?php
 }
