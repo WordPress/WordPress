5dd45ddd29dd263dbd1d1988ab1683c0dc699fbc
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-01-26 07:21:53 +0000

    Use home instead of siteurl.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2149 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/header.php
+++ wp-content/themes/default/header.php
@@ -58,7 +58,7 @@
 
 <div id="header">
 	<div id="headerimg">
-		<h1><a href="<?php echo get_settings('siteurl'); ?>"><?php bloginfo('name'); ?></a></h1>
+		<h1><a href="<?php echo get_settings('home'); ?>"><?php bloginfo('name'); ?></a></h1>
 		<div class="description"><?php bloginfo('description'); ?></div>
 	</div>
 </div>
