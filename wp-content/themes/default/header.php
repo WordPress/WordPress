4c6cb0f12e453e27a8d9fc09dc957f7ba55aad2e
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-02-07 07:25:04 +0000

    There is no spoon, or print.css
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2237 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/header.php
+++ wp-content/themes/default/header.php
@@ -8,7 +8,6 @@
 	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
 
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-	<link rel="stylesheet" type="text/css" media="print" href="<?php echo get_settings('siteurl'); ?>/print.css" />
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
