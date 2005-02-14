0cd0269cc832a8efe5a7f66f9bf24b3073aa13ea
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-02-14 00:58:51 +0000

    Support for categories - http://mosquito.wordpress.org/view.php?id=52
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2311 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -974,14 +974,19 @@ function get_posts($args) {
 	parse_str($args, $r);
 	if (!isset($r['numberposts'])) $r['numberposts'] = 5;
 	if (!isset($r['offset'])) $r['offset'] = 0;
-	// The following not implemented yet
 	if (!isset($r['category'])) $r['category'] = '';
+	// The following not implemented yet
 	if (!isset($r['orderby'])) $r['orderby'] = '';
 	if (!isset($r['order'])) $r['order'] = '';
 
 	$now = current_time('mysql');
 
-	$posts = $wpdb->get_results("SELECT DISTINCT * FROM $wpdb->posts WHERE post_date <= '$now' AND (post_status = 'publish') GROUP BY $wpdb->posts.ID ORDER BY post_date DESC LIMIT " . $r['offset'] . ',' . $r['numberposts']);
+	$posts = $wpdb->get_results(
+		"SELECT DISTINCT * FROM $wpdb->posts " .
+		( empty( $r['category'] ) ? "" : ", $wpdb->post2cat " ) .
+		" WHERE post_date <= '$now' AND (post_status = 'publish') ".
+		( empty( $r['category'] ) ? "" : "AND $wpdb->posts.ID = $wpdb->post2cat.post_id AND $wpdb->post2cat.category_id = " . $r['category']. " " ) .
+		" GROUP BY $wpdb->posts.ID ORDER BY post_date DESC LIMIT " . $r['offset'] . ',' . $r['numberposts'] );
 
     update_post_caches($posts);
 	
