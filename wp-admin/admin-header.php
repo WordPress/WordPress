b566c4323c83e8922561d861aae26a3dec328fa6
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-02-14 20:08:29 +0000

    Send content-type header - http://mosquito.wordpress.org/view.php?id=874
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2335 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -1,4 +1,6 @@
-<?php if (!isset($_GET["page"])) require_once('admin.php'); ?>
+<?php 
+@header('Content-type: ' . get_option('html_type') . '; charset=' . get_option('blog_charset'));
+if (!isset($_GET["page"])) require_once('admin.php'); ?>
 <?php get_admin_page_title(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
