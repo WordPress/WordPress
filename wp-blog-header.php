2389a0aa7cadc33de0a943e5566303ae6990b61d
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-02-14 00:49:03 +0000

    Don't double-nest wp-admin. http://mosquito.wordpress.org/view.php?id=465
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2309 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -1,8 +1,11 @@
 <?php
 
 if (! isset($wp_did_header)):
-if ( !file_exists( dirname(__FILE__) . '/wp-config.php') )
-    die("There doesn't seem to be a <code>wp-config.php</code> file. I need this before we can get started. Need more help? <a href='http://wordpress.org/docs/faq/#wp-config'>We got it</a>. You can <a href='wp-admin/setup-config.php'>create a <code>wp-config.php</code> file through a web interface</a>, but this doesn't work for all server setups. The safest way is to manually create the file.");
+if ( !file_exists( dirname(__FILE__) . '/wp-config.php') ) {
+	if ( strstr( $_SERVER['PHP_SELF'], 'wp-admin') ) $path = '';
+	else $path = 'wp-admin/';
+    die("There doesn't seem to be a <code>wp-config.php</code> file. I need this before we can get started. Need more help? <a href='http://wordpress.org/docs/faq/#wp-config'>We got it</a>. You can <a href='{$path}setup-config.php'>create a <code>wp-config.php</code> file through a web interface</a>, but this doesn't work for all server setups. The safest way is to manually create the file.");
+}
 
 $wp_did_header = true;
 
