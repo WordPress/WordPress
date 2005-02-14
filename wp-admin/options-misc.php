eda8b55ac0256b369c08070c9d2ef34d3cbbe4b7
saxmatt <saxmatt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-02-14 03:11:23 +0000

    Removal of changes.xml bloat.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2313 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options-misc.php
+++ wp-admin/options-misc.php
@@ -58,37 +58,8 @@ include('admin-header.php');
 	</tr>
 	</table> 
 	</fieldset>
-	<fieldset class="options">
-	<legend>
-	<input name="use_linksupdate" type="checkbox" id="use_linksupdate" value="1" <?php checked('1', get_settings('use_linksupdate')); ?> />
-	<label for="use_linksupdate"><?php _e('Track Link&#8217;s Update Times') ?></label></legend>
-	<table width="100%" cellspacing="2" cellpadding="5" class="editform"> 
-	<tr> 
-	<th width="33%" valign="top" scope="row"><?php _e('Update file:') ?> </th> 
-	<td>
-	<input name="weblogs_xml_url" type="text" id="weblogs_xml_url" value="<?php form_option('weblogs_xml_url'); ?>" size="50" /><br />
-	<?php __('Recommended: <code>http://static.wordpress.org/changes.xml</code>') ?>
-	
-	</td> 
-	</tr> 
-	<tr>
-	<th valign="top" scope="row"><?php _e('Updated link time format:') ?> </th>
-	<td>          
-	<input name="links_updated_date_format" type="text" id="links_updated_date_format" value="<?php form_option('links_updated_date_format'); ?>" size="50" />
-	</td>
-	</tr>
-	<tr>
-	<th scope="row"><?php _e('Prepend updated with:') ?> </th>
-	<td><input name="links_recently_updated_prepend" type="text" id="links_recently_updated_prepend" value="<?php form_option('links_recently_updated_prepend'); ?>" size="50" /></td>
-	</tr>
-	<tr>
-	<th valign="top" scope="row"><?php _e('Append updated with:') ?></th>
-	<td><input name="links_recently_updated_append" type="text" id="links_recently_updated_append" value="<?php form_option('links_recently_updated_append'); ?>" size="50" /></td>
-	</tr>
-	</table>
-	<p><?php printf(__('A link is "recent" if it has been updated in the past %s minutes.'), '<input name="links_recently_updated_time" type="text" id="links_recently_updated_time" size="3" value="' . get_settings('links_recently_updated_time'). '" />' ) ?></p>
-	</fieldset>
-
+	<p><input name="use_linksupdate" type="checkbox" id="use_linksupdate" value="1" <?php checked('1', get_settings('use_linksupdate')); ?> />
+	<label for="use_linksupdate"><?php _e('Track Links&#8217; Update Times') ?></label></p>
 	<p>
 	<label><input type="checkbox" name="hack_file" value="1" <?php checked('1', get_settings('hack_file')); ?> /> <?php _e('Use legacy <code>my-hacks.php</code> file support') ?></label>
 	</p>
---
+++ wp-admin/update-links.php
@@ -0,0 +1,44 @@
+<?php
+require_once( dirname( dirname(__FILE__) ) . '/wp-config.php');
+require_once( ABSPATH . 'wp-includes/class-snoopy.php');
+
+if ( !get_option('use_linksupdate') )
+	die('Feature disabled.');
+
+$link_uris = $wpdb->get_col("SELECT link_url FROM $wpdb->links");
+
+if ( !$link_uris )
+	die('No links');
+
+$link_uris = urlencode( join( $link_uris, "\n" ) );
+
+$query_string = "uris=$link_uris";
+
+$http_request  = "POST /updated-batch/ HTTP/1.0\r\n";
+$http_request .= "Host: api.pingomatic.com\r\n";
+$http_request .= 'Content-Type: application/x-www-form-urlencoded; charset='.get_settings('blog_charset')."\r\n";
+$http_request .= 'Content-Length: ' . strlen($query_string) . "\r\n";
+$http_request .= 'User-Agent: WordPress/' . $wp_version . "\r\n";
+$http_request .= "\r\n";
+$http_request .= $query_string;
+
+$response = '';
+$fs = fsockopen('api.pingomatic.com', 80, $errno, $errstr, 5);
+fwrite($fs, $http_request);
+while ( !feof($fs) )
+	$response .= fgets($fs, 1160); // One TCP-IP packet
+fclose($fs);
+
+$response = explode("\r\n\r\n", $response, 2);
+$body = trim( $response[1] );
+$body = str_replace(array("\r\n", "\r"), "\n", $body);
+
+$returns = explode("\n", $body);
+
+foreach ($returns as $return) :
+	$time = addslashes( substr($return, 0, 19) );
+	$uri = addslashes( preg_replace('/(.*?) | (.*?)/', '$2', $return) );
+	$wpdb->query("UPDATE $wpdb->links SET link_updated = '$time' WHERE link_url = '$uri'");
+endforeach;
+
+?>
