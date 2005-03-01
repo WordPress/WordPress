26808caf61012c43b5b52ba4218c009b8f3ba2fa
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-03-01 10:10:59 +0000

    Fix for emergent whitelisting, hat tip MtDewVirus - http://mosquito.wordpress.org/view.php?id=995
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2397 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment-functions.php
+++ wp-includes/comment-functions.php
@@ -757,6 +757,8 @@ function check_comment($author, $email, $url, $comment, $user_ip, $user_agent, $
 			$domain = $uri['host'];
 			if ( $wpdb->get_var("SELECT link_id FROM $wpdb->links WHERE link_url LIKE ('%$domain%') LIMIT 1") )
 				return true;
+			else
+				return false;
 		} elseif( $author != '' && $email != '' ) {
 			$ok_to_comment = $wpdb->get_var("SELECT comment_approved FROM $wpdb->comments WHERE comment_author = '$author' AND comment_author_email = '$email' and comment_approved = '1' ");
 			if ( 1 == $ok_to_comment && false === strpos( $email, get_settings('moderation_keys')) )
