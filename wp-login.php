16b593ec3542ea6940d33d254227beaef3d50119
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-03-13 22:29:44 +0000

    Use wp_redirect() when logging out.  http://mosquito.wordpress.org/view.php?id=592 Props: toby
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2440 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-login.php
+++ wp-login.php
@@ -27,7 +27,7 @@ case 'logout':
 	header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 	header('Cache-Control: no-cache, must-revalidate, max-age=0');
 	header('Pragma: no-cache');
-	header('Location: wp-login.php');
+	wp_redirect('wp-login.php');
 	exit();
 
 break;
