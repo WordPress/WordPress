56933a4e6d44cbf574ee55c282ace9c44f91700e
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-03-18 06:07:36 +0000

    Fix from Kimmo/Craig
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2456 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -303,7 +303,7 @@ function url_to_postid($url = '') {
 
 function get_settings($setting) {
   global $wpdb, $cache_settings, $cache_nonexistantoptions;
-	if ( strstr($_SERVER['REQUEST_URI'], 'wp-admin/install.php') || constant('WP_INSTALLING') )
+	if ( strstr($_SERVER['REQUEST_URI'], 'wp-admin/install.php') || defined('WP_INSTALLING') )
 		return false;
 
 	if ( empty($cache_settings) )
