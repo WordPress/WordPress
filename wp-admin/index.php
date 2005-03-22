6851c1e0abce7e1443ad1532009ed4d94b70e762
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-03-22 17:33:05 +0000

    strtotime needs to now that the time string is in GMT.  http://mosquito.wordpress.org/view.php?id=1075
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2471 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/index.php
+++ wp-admin/index.php
@@ -39,7 +39,7 @@ if ( $scheduled = $wpdb->get_results("SELECT ID, post_title, post_date_gmt FROM 
 foreach ($scheduled as $post) {
 	if ($post->post_title == '')
 		$post->post_title = sprintf(__('Post #%s'), $post->ID);
-	echo "<li><a href='post.php?action=edit&amp;post=$post->ID' title='" . __('Edit this post') . "'>$post->post_title</a> in " . human_time_diff( current_time('timestamp', 1), strtotime($post->post_date_gmt) )  . "</li>";
+	echo "<li><a href='post.php?action=edit&amp;post=$post->ID' title='" . __('Edit this post') . "'>$post->post_title</a> in " . human_time_diff( current_time('timestamp', 1), strtotime($post->post_date_gmt. ' GMT') )  . "</li>";
 }
 ?> 
 </ul>
