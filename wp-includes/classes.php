4e1fa082fdfd33e55cec2e1e06788551697093fa
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-03-23 21:39:22 +0000

    Feeds should always show x posts, not x days. http://mosquito.wordpress.org/view.php?id=1159
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2475 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/classes.php
+++ wp-includes/classes.php
@@ -249,8 +249,10 @@ class WP_Query {
 				$q['nopaging'] = false;
 			}
 		}
-		if ( $this->is_feed )
+		if ( $this->is_feed ) {
 			$q['posts_per_page'] = get_settings('posts_per_rss');
+			$q['what_to_show'] = 'posts';
+		}
 	
 		$add_hours = intval(get_settings('gmt_offset'));
 		$add_minutes = intval(60 * (get_settings('gmt_offset') - $add_hours));
