9697579316fba81a0e5f39763c8b41f4a71912c7
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-03-23 01:14:05 +0000

    Gracefully handle the lack of themes in get_page_templates().  http://mosquito.wordpress.org/view.php?id=1153  Props: donncha
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2474 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-functions.php
+++ wp-admin/admin-functions.php
@@ -626,16 +626,18 @@ function get_page_templates() {
 	$templates = $themes[$theme]['Template Files'];
 	$page_templates = array();
 
-	foreach ($templates as $template) {
-		$template_data = implode('', file(ABSPATH . $template));
-		preg_match("|Template Name:(.*)|i", $template_data, $name);
-		preg_match("|Description:(.*)|i", $template_data, $description);
+	if( is_array( $templates ) ) {
+		foreach ($templates as $template) {
+			$template_data = implode('', file(ABSPATH . $template));
+			preg_match("|Template Name:(.*)|i", $template_data, $name);
+			preg_match("|Description:(.*)|i", $template_data, $description);
 
-		$name = $name[1];
-		$description = $description[1];
+			$name = $name[1];
+			$description = $description[1];
 
-		if (! empty($name)) {
-			$page_templates[trim($name)] = basename($template);
+			if (! empty($name)) {
+				$page_templates[trim($name)] = basename($template);
+			}
 		}
 	}
 
