1eac787dce81a790949173abb7f8d6597ec849ea
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-04-18 16:05:33 +0000

    The Codex is ready
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2539 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -1,6 +1,7 @@
 
 <div id="footer"><p><a href="http://wordpress.org/"><img src="../wp-images/wp-small.png" alt="WordPress" /></a><br />
-<?php bloginfo('version'); ?> &#8212; <a href="http://wordpress.org/support/"><?php _e('Support Forums'); ?></a><br />
+<?php bloginfo('version'); ?> <br /> 
+<a href="http://codex.wordpress.org/"><?php _e('Documentation'); ?></a> &#8212; <a href="http://wordpress.org/support/"><?php _e('Support Forums'); ?></a> <br />
 <?php printf(__('%s seconds'), number_format(timer_stop(), 2)); ?>
 </p>
 
