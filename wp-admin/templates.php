8f48c9ca992bff863c38c84fea120d6359ad368a
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-05-09 11:01:12 +0000

    Remove reference to old wiki - http://mosquito.wordpress.org/view.php?id=1317
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2590 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/templates.php
+++ wp-admin/templates.php
@@ -137,7 +137,7 @@ endif;
 <div class="wrap">
 <h2><?php _e('Other Files') ?></h2>
 
-  <p><?php _e('To edit a file, type its name here. You can edit any file <a href="http://wiki.wordpress.org/index.php/MakeWritable" title="Read more about making files writable">writable by the server</a>, e.g. CHMOD 666.') ?></p> 
+  <p><?php _e('To edit a file, type its name here. You can edit any file <a href="http://codex.wordpress.org/Changing_File_Permissions" title="Read more about making files writable">writable by the server</a>, e.g. CHMOD 666.') ?></p> 
   <form name="file" action="templates.php" method="get"> 
     <input type="text" name="file" /> 
     <input type="submit" name="submit"  value="<?php _e('Edit file &raquo;') ?>" /> 
