70ae8e667be9c38dd7bf1ee5396b925016f18569
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-05-09 11:06:45 +0000

    Better config include in admin - http://mosquito.wordpress.org/view.php?id=1307
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2592 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin.php
+++ wp-admin/admin.php
@@ -1,5 +1,9 @@
 <?php
-require_once('../wp-config.php');
+if ( defined('ABSPATH') )
+	require_once( ABSPATH . 'wp-config.php');
+else
+    require_once('../wp-config.php');
+    
 require_once(ABSPATH . 'wp-admin/admin-functions.php');
 auth_redirect();
 
