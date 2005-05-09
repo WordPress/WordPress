5242c3eec32c24fbc702ab2fcc30add211a7d26b
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-05-09 11:11:19 +0000

    Summary is always text/plain. - http://mosquito.wordpress.org/view.php?id=1314
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2593 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-atom.php
+++ wp-atom.php
@@ -35,7 +35,7 @@ $more = 1;
 		<modified><?php echo get_post_time('Y-m-d\TH:i:s\Z', true); ?></modified>
 		<issued><?php echo get_post_time('Y-m-d\TH:i:s\Z', true); ?></issued>
 		<?php the_category_rss('rdf') ?> 
-		<summary type="<?php bloginfo('html_type'); ?>" mode="escaped"><![CDATA[<?php the_excerpt_rss(); ?>]]></summary>
+		<summary type="text/plain" mode="escaped"><![CDATA[<?php the_excerpt_rss(); ?>]]></summary>
 <?php if ( !get_settings('rss_use_excerpt') ) : ?>
 		<content type="<?php bloginfo('html_type'); ?>" mode="escaped" xml:base="<?php permalink_single_rss() ?>"><![CDATA[<?php the_content('', 0, '') ?>]]></content>
 <?php endif; ?>
