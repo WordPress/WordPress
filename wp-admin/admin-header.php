1dbfb4ce580af2bbec5978985e47daae42d9f761
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-08-18 00:13:08 +0000

    Forgot a file
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2788 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -91,6 +91,7 @@ tinyMCE.init({
 	theme_advanced_toolbar_location : "top",
 	theme_advanced_toolbar_align : "left",
 	theme_advanced_path_location : "bottom",
+	theme_advanced_resizing : true,
 	entity_encoding : "raw",
 	extended_valid_elements : "a[id|href|title|onclick],img[class|src|alt|title|width|height|align]",
 	plugins : "emotions"
