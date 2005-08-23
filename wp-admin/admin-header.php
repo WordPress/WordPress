3cac59f10c39f634982a794fb42419375a472a6c
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-08-23 07:52:47 +0000

    No horizontal resizing
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2807 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -92,6 +92,7 @@ tinyMCE.init({
 	theme_advanced_toolbar_align : "left",
 	theme_advanced_path_location : "bottom",
 	theme_advanced_resizing : true,
+	theme_advanced_resize_horizontal : false,
 	entity_encoding : "raw",
 	extended_valid_elements : "a[id|href|title|onclick],img[class|src|alt|title|width|height|align]",
 	plugins : "emotions"
