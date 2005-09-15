032349cfe13ff886024516ba1281bf056ef0aaed
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-09-15 23:44:41 +0000

    Fix for systems with no PHP_INFO
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2884 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/searchform.php
+++ wp-content/themes/default/searchform.php
@@ -1,4 +1,4 @@
-<form method="get" id="searchform" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<form method="get" id="searchform" action="<?php bloginfo('home'); ?>">
 <div><input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" />
 <input type="submit" id="searchsubmit" value="Search" />
 </div>
