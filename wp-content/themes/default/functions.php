a32bad283f1dd3a92734d8ccd98a619494c3e5ee
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-09-26 17:21:51 +0000

    Validate!
    git-svn-id: http://svn.automattic.com/wordpress/trunk@2920 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/functions.php
+++ wp-content/themes/default/functions.php
@@ -87,7 +87,7 @@ function kubrick_add_theme_page() {
 					if ( preg_match('/[0-9A-F]{6}|[0-9A-F]{3}/i', $_REQUEST['njuppercolor'], $uc) && preg_match('/[0-9A-F]{6}|[0-9A-F]{3}/i', $_REQUEST['njlowercolor'], $lc) ) {
 						$uc = ( strlen($uc[0]) == 3 ) ? $uc[0]{0}.$uc[0]{0}.$uc[0]{1}.$uc[0]{1}.$uc[0]{2}.$uc[0]{2} : $uc[0];
 						$lc = ( strlen($lc[0]) == 3 ) ? $lc[0]{0}.$lc[0]{0}.$lc[0]{1}.$lc[0]{1}.$lc[0]{2}.$lc[0]{2} : $lc[0];
-						update_option('kubrick_header_image', "header-img.php?upper=$uc&lower=$lc");
+						update_option('kubrick_header_image', "header-img.php?upper=$uc&amp;lower=$lc");
 					}
 
 					if ( isset($_REQUEST['toggledisplay']) ) {
