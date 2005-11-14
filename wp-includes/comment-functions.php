1301fdf266fddbccd1b0c9baf9e0410ad164c5fd
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-11-14 03:06:52 +0000

    More accessible comment links
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3065 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment-functions.php
+++ wp-includes/comment-functions.php
@@ -281,7 +281,7 @@ function comments_popup_link($zero='No Comments', $one='1 Comment', $more='% Com
 		if (!empty($CSSclass)) {
 			echo ' class="'.$CSSclass.'"';
 		}
-		echo '>';
+		echo 'title="' . __( sprintf('Comment on %s', $post->post_title) ) .'">';
 		comments_number($zero, $one, $more, $number);
 		echo '</a>';
 	}
