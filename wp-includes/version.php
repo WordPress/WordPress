94c145394ca243d37ea3f12f401e2e5722d88006
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-11-19 00:19:43 +0000

    Dat da-da duuuummmmmmmm
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3160 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -2,7 +2,7 @@
 
 // This just holds the version number, in a separate file so we can bump it without cluttering the SVN
 
-$wp_version = '1.6-ALPHA-2-still-dont-use';
+$wp_version = '2.0-beta-1';
 $wp_db_version = 3104;
 
 ?>
