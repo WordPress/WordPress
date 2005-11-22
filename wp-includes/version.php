ac47eb038cd763b5e75b7839f41782694c28ce43
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-11-22 09:56:15 +0000

    Bumpa version
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3195 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -2,7 +2,7 @@
 
 // This just holds the version number, in a separate file so we can bump it without cluttering the SVN
 
-$wp_version = '2.0-beta-1';
+$wp_version = '2.0-beta-2';
 $wp_db_version = 3183;
 
 ?>
