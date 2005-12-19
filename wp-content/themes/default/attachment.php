dbbd5efeb2034dc6b9d294acf08c37b043db16a1
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-12-19 19:08:30 +0000

    Attachment template fix. fixes #2098
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3328 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/attachment.php
+++ wp-content/themes/default/attachment.php
@@ -8,10 +8,12 @@
 			<div class="alignleft">&nbsp;</div>
 			<div class="alignright">&nbsp;</div>
 		</div>
+<?php $attachment_link = get_the_attachment_link($post->ID, true, array(450, 800)); // This also populates the iconsize for the next line ?>
+<?php $_post = &get_post($post->ID); $classname = ($_post->iconsize[0] <= 128 ? 'small' : '') . 'attachment'; // This lets us style narrow icons specially ?>
 		<div class="post" id="post-<?php the_ID(); ?>">
 			<h2><a href="<?php echo get_permalink($post->post_parent); ?>" rev="attachment"><?php echo get_the_title($post->post_parent); ?></a> &raquo; <a href="<?php echo get_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>"><?php the_title(); ?></a></h2>
 			<div class="entrytext">
-				<p class="<?php $link = get_the_attachment_link($post->ID, true, array(450, 800)); /* Doing this now populates the imagesize stuff */ echo $post->iconsize[0] <= 128 ? 'small' : ''; ?>attachment"><?php echo get_the_attachment_link($post->ID, true, array(450, 800)); ?><br /><?php echo basename($post->guid); ?></p>
+				<p class="<?php echo $classname; ?>"><?php echo $attachment_link; ?><br /><?php echo basename($post->guid); ?></p>
 
 				<?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>
 	
