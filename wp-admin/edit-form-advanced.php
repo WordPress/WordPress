71d5605298831e607da4e0ada14f08eb4d50ec74
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2005-12-23 20:43:30 +0000

    Fix already pinged list.  Props pirateking. fixes #2136
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3347 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-form-advanced.php
+++ wp-admin/edit-form-advanced.php
@@ -33,9 +33,9 @@ $form_prevstatus = '<input type="hidden" name="prev_status" value="' . $post->po
 
 $form_trackback = '<input type="text" name="trackback_url" style="width: 415px" id="trackback" tabindex="7" value="'. str_replace("\n", ' ', $post->to_ping) .'" />';
 
-if ('' != $pinged) {
-	$pings .= '<p>'. __('Already pinged:') . '</p><ul>';
-	$already_pinged = explode("\n", trim($pinged));
+if ('' != $post->pinged) {
+	$pings = '<p>'. __('Already pinged:') . '</p><ul>';
+	$already_pinged = explode("\n", trim($post->pinged));
 	foreach ($already_pinged as $pinged_url) {
 		$pings .= "\n\t<li>$pinged_url</li>";
 	}
@@ -163,7 +163,7 @@ if ( tinyMCE.isMSIE )
 			if (e.keyCode == 9 && !e.shiftKey && !e.controlKey && !e.altKey) {
 				var i = tinyMCE.selectedInstance;
 				if(typeof i ==  'undefined')
-					return true;
+					return true;http://boren.nu/
                                 tinyMCE.execCommand("mceStartTyping");
 				this.blur();
 				i.contentWindow.focus();
@@ -234,7 +234,7 @@ if ( false != $uploading_iframe_src )
 <h3 class="dbx-handle"><?php _e('Trackbacks') ?></h3>
 <div class="dbx-content"><?php _e('Send trackbacks to'); ?>: <?php echo $form_trackback; ?> (<?php _e('Separate multiple URIs with spaces'); ?>)
 <?php 
-if ('' != $pinged)
+if ( ! empty($pings) )
 	echo $pings;
 ?>
 </div>
