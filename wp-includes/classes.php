9c9f5f8aaaad7cefb89bb0e70864e5c8efb87fb0
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-01-05 03:47:38 +0000

    Add [L] to rewrite rule. Props cavemonkey50. fixes #2222
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3401 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/classes.php
+++ wp-includes/classes.php
@@ -1398,7 +1398,7 @@ class WP_Rewrite {
 		} else {
 			$rules .= "RewriteCond %{REQUEST_FILENAME} !-f\n" .
 				"RewriteCond %{REQUEST_FILENAME} !-d\n" .
-				"RewriteRule . {$home_root}{$this->index}\n";
+				"RewriteRule . {$home_root}{$this->index} [L]\n";
 		}
 
 		$rules .= "</IfModule>\n";
