ba29615e7edfefdb7471d0bcff31ac032e9b0ed0
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-01-11 21:35:36 +0000

    Trigger pre_ping action outside of loop.  Props Mark Jaquith.  fixes #2269
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3427 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment-functions.php
+++ wp-includes/comment-functions.php
@@ -634,9 +634,10 @@ function pingback($content, $post_ID) {
 				$post_links[] = $link_test;
 			elseif(($test['path'] != '/') && ($test['path'] != ''))
 				$post_links[] = $link_test;
-			do_action('pre_ping',  array(&$post_links, &$pung));
 		endif;
 	endforeach;
+
+	do_action('pre_ping',  array(&$post_links, &$pung));
 
 	foreach ($post_links as $pagelinkedto){
 		debug_fwrite($log, "Processing -- $pagelinkedto\n");
