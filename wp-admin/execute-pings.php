fe8cbf801a72f22790e4fd09134a9c70c3101375
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-01-19 00:03:28 +0000

    Execute pings rework from skeltoac.  #2197
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3463 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/execute-pings.php
+++ wp-admin/execute-pings.php
@@ -1,34 +1,25 @@
 <?php
+
 require_once('../wp-config.php');
 
-register_shutdown_function('execute_all_pings');
-//execute_all_pings();
-
-function execute_all_pings() {
-	global $wpdb;
-	// Do pingbacks
-	while ($ping = $wpdb->get_row("SELECT * FROM {$wpdb->posts}, {$wpdb->postmeta} WHERE {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id AND {$wpdb->postmeta}.meta_key = '_pingme' LIMIT 1")) {
-		$wpdb->query("DELETE FROM {$wpdb->postmeta} WHERE post_id = {$ping->ID} AND meta_key = '_pingme';");
-		pingback($ping->post_content, $ping->ID);
-		echo "Pingback: $ping->post_title : $ping->ID<br/>";
-	}
-	// Do Enclosures
-	while ($enclosure = $wpdb->get_row("SELECT * FROM {$wpdb->posts}, {$wpdb->postmeta} WHERE {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id AND {$wpdb->postmeta}.meta_key = '_encloseme' LIMIT 1")) {
-		$wpdb->query("DELETE FROM {$wpdb->postmeta} WHERE post_id = {$enclosure->ID} AND meta_key = '_encloseme';");
-		do_enclose($enclosure->post_content, $enclosure->ID);
-		echo "Enclosure: $enclosure->post_title : $enclosure->ID<br/>";
-	}
-	// Do Trackbacks
-	$trackbacks = $wpdb->get_results("SELECT ID FROM $wpdb->posts WHERE TRIM(to_ping) != '' AND post_status != 'draft'");
-
-	if (is_array($trackbacks) && count($trackbacks)) {
-		foreach ($trackbacks as $trackback  ) {
-			echo "Trackback : $trackback->ID<br/>";
-			do_trackbacks($trackback->ID);
-		}
-	}
+// Do pingbacks
+while ($ping = $wpdb->get_row("SELECT * FROM {$wpdb->posts}, {$wpdb->postmeta} WHERE {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id AND {$wpdb->postmeta}.meta_key = '_pingme' LIMIT 1")) {
+	$wpdb->query("DELETE FROM {$wpdb->postmeta} WHERE post_id = {$ping->ID} AND meta_key = '_pingme';");
+	pingback($ping->post_content, $ping->ID);
 }
 
-_e('Done.');
+// Do Enclosures
+while ($enclosure = $wpdb->get_row("SELECT * FROM {$wpdb->posts}, {$wpdb->postmeta} WHERE {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id AND {$wpdb->postmeta}.meta_key = '_encloseme' LIMIT 1")) {
+	$wpdb->query("DELETE FROM {$wpdb->postmeta} WHERE post_id = {$enclosure->ID} AND meta_key = '_encloseme';");
+	do_enclose($enclosure->post_content, $enclosure->ID);
+}
+
+// Do Trackbacks
+$trackbacks = $wpdb->get_results("SELECT ID FROM $wpdb->posts WHERE CHAR_LENGTH(TRIM(to_ping)) > 7 AND post_status != 'draft'");
+if ( is_array($trackbacks) ) {
+	foreach ( $trackbacks as $trackback ) {
+		do_trackbacks($trackback->ID);
+	}
+}
 
 ?>
