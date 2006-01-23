51944d9f2c37ddb0dd72666e49e423fc9c3e75e4
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-01-23 23:10:08 +0000

    Link OPML changes, fixes #2325
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3470 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-links-opml.php
+++ wp-links-opml.php
@@ -29,6 +29,7 @@ if ((empty($link_cat)) || ($link_cat == 'all') || ($link_cat == '0')) {
 <?php $sql = "SELECT $wpdb->links.link_url, link_rss, $wpdb->links.link_name, $wpdb->links.link_category, $wpdb->linkcategories.cat_name, link_updated 
 FROM $wpdb->links 
  JOIN $wpdb->linkcategories on $wpdb->links.link_category = $wpdb->linkcategories.cat_id
+ AND $wpdb->links.link_visible = 'Y'
  $sql_cat
  ORDER BY $wpdb->linkcategories.cat_name, $wpdb->links.link_name \n";
  //echo("<!-- $sql -->");
@@ -51,8 +52,10 @@ FROM $wpdb->links
             <outline text="<?php echo wp_specialchars($result->link_name); ?>" type="link" xmlUrl="<?php echo wp_specialchars($result->link_rss); ?>" htmlUrl="<?php echo wp_specialchars($result->link_url); ?>" updated="<?php if ('0000-00-00 00:00:00' != $result->link_updated) echo $result->link_updated; ?>" />
 <?php
         } // end foreach
+?>
+	</outline>
+<?php
     } // end if
 ?>
-        </outline>
-    </body>
+</body>
 </opml>
