392ba189b35d9d74167dfd70e0d7ed541aa0b22f
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-01-24 07:53:26 +0000

    Bumpity, bump, bump.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3478 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -2,7 +2,7 @@
 
 // This just holds the version number, in a separate file so we can bump it without cluttering the SVN
 
-$wp_version = '2.0.1-alpha';
+$wp_version = '2.0.1-rc1';
 $wp_db_version = 3437;
 
 ?>
