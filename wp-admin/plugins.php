4c08bf3d7c8e4c2978bfb96da276fb19ec6b9750
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-01-24 07:48:21 +0000

    Sort plugins by plugin name, not file name.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3477 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/plugins.php
+++ wp-admin/plugins.php
@@ -87,6 +87,13 @@ if (empty($plugins)) {
 	</tr>
 <?php
 	$style = '';
+
+	function sort_plugins($plug1, $plug2) {
+		return strnatcasecmp($plug1['Name'], $plug2['Name']);
+	}
+	
+	usort($plugins, 'sort_plugins');
+
 	foreach($plugins as $plugin_file => $plugin_data) {
 		$style = ('class="alternate"' == $style|| 'class="alternate active"' == $style) ? '' : 'alternate';
 
