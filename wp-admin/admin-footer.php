1fe8b77d266f1bfccb3c349b78ab300e09b3a339
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-01-29 00:12:14 +0000

    TB fix.  fixes #2197
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3492 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -9,9 +9,11 @@
 <?php do_action('admin_footer', ''); ?>
 <script type="text/javascript">if(typeof wpOnload=='function')wpOnload();</script>
 
-<?php if ( (substr(php_sapi_name(), 0, 3) == 'cgi') && spawn_pinger() ) : ?>
-<iframe id="pingcheck" src="' . get_settings('siteurl') .'/wp-admin/execute-pings.php?time=' . time() . '" style="border:none;width:1px;height:1px;"></iframe>
-<?php endif; ?>
+<?php
+if ( (substr(php_sapi_name(), 0, 3) == 'cgi') && spawn_pinger() ) {
+	echo '<iframe id="pingcheck" src="' . get_settings('siteurl') .'/wp-admin/execute-pings.php?time=' . time() . '" style="border:none;width:1px;height:1px;"></iframe>';
+}
+?>
 
 </body>
 </html>
