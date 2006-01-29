e4e35319e1020b6234bf4c8652d3eeee13adab57
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-01-29 00:15:57 +0000

    Fix RSD link. Props Juergen.  fixes #2349
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3493 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/template-functions-general.php
+++ wp-includes/template-functions-general.php
@@ -700,7 +700,7 @@ function the_weekday_date($before='',$after='') {
 }
 
 function rsd_link() {
-	echo '<link rel="EditURI" type="application/rsd+xml" title="RSD" href="' . get_bloginfo('url') . "/xmlrpc.php?rsd\" />\n";
+	echo '<link rel="EditURI" type="application/rsd+xml" title="RSD" href="' . get_bloginfo('wpurl') . "/xmlrpc.php?rsd\" />\n";
 }
 
 ?>
