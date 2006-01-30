60ccea94fd03f57923232270bb15b9c0f72bcdbe
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-01-30 21:19:44 +0000

    Check if HTTP_RAW_POST_DATA is set to avoid undefined var warning.  Props Juergen.  fixes #2350
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3498 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- xmlrpc.php
+++ xmlrpc.php
@@ -6,7 +6,8 @@ define('XMLRPC_REQUEST', true);
 $_COOKIE = array();
 
 # fix for mozBlog and other cases where '<?xml' isn't on the very first line
-$HTTP_RAW_POST_DATA = trim($HTTP_RAW_POST_DATA);
+if ( isset($HTTP_RAW_POST_DATA) )
+	$HTTP_RAW_POST_DATA = trim($HTTP_RAW_POST_DATA);
 
 include('./wp-config.php');
 
