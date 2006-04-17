7b2be719222346e37bd117d083e053691697387f
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-04-17 23:27:18 +0000

    We like big inputs.  fixes #2662
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3709 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions-post.php
+++ wp-includes/functions-post.php
@@ -1033,8 +1033,9 @@ function do_all_pings() {
  * Places two script links in <head>: one to get tinyMCE (big), one to configure and start it (small)
  */
 function tinymce_include() {
-	$src1 = get_settings('siteurl') . '/wp-includes/js/tinymce/tiny_mce_gzip.php?ver=04122006';
-	$src2 = get_settings('siteurl') . '/wp-includes/js/tinymce/tiny_mce_config.php?ver=04122006';
+	$ver = '04162006';
+	$src1 = get_settings('siteurl') . "/wp-includes/js/tinymce/tiny_mce_gzip.php?ver=$ver";
+	$src2 = get_settings('siteurl') . "/wp-includes/js/tinymce/tiny_mce_config.php?ver=$ver";
 
 	echo "<script type='text/javascript' src='$src1'></script>\n";
 	echo "<script type='text/javascript' src='$src2'></script>\n";
