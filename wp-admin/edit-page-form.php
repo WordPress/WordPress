e92d1c8341a699c3674218c1267ec2b4a40a0387
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-05-17 21:32:16 +0000

    Fix cap. Props ryanscheuermann. fixes #2723
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3775 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-page-form.php
+++ wp-admin/edit-page-form.php
@@ -61,7 +61,7 @@ addLoadEvent(focusit);
 
 <fieldset class="dbx-box">
 <h3 class="dbx-handle"><?php _e('Page Status') ?></h3> 
-<div class="dbx-content"><?php if ( current_user_can('publish_posts') ) : ?>
+<div class="dbx-content"><?php if ( current_user_can('publish_pages') ) : ?>
 <label for="post_status_publish" class="selectit"><input id="post_status_publish" name="post_status" type="radio" value="publish" <?php checked($post->post_status, 'publish'); checked($post->post_status, 'future'); ?> /> <?php _e('Published') ?></label>
 <?php endif; ?>
 	  <label for="post_status_draft" class="selectit"><input id="post_status_draft" name="post_status" type="radio" value="draft" <?php checked($post->post_status, 'draft'); ?> /> <?php _e('Draft') ?></label>
