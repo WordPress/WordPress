1cdc6a5cd3d249201f4c98afe03c3896ce0d764a
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-06-15 08:18:04 +0000

    Update opml.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3875 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-links-opml.php
+++ wp-links-opml.php
@@ -1,61 +1,50 @@
-<?php 
-$doing_rss = 1;
+<?php
 
-require('wp-blog-header.php');
+if (empty($wp)) {
+	require_once('./wp-config.php');
+	wp();
+}
+
 header('Content-type: text/xml; charset=' . get_settings('blog_charset'), true);
 $link_cat = $_GET['link_cat'];
-if ((empty($link_cat)) || ($link_cat == 'all') || ($link_cat == '0')) {
-    $sql_cat = '';
+if ((empty ($link_cat)) || ($link_cat == 'all') || ($link_cat == '0')) {
+	$link_cat = '';
 } else { // be safe
-    $link_cat = ''.urldecode($link_cat).'';
-    $link_cat = addslashes_gpc($link_cat);
-    $link_cat = intval($link_cat);
-    if ($link_cat != 0) {
-        $sql_cat = "AND $wpdb->links.link_category = $link_cat";
-        $cat_name = $wpdb->get_var("SELECT $wpdb->linkcategories.cat_name FROM $wpdb->linkcategories WHERE $wpdb->linkcategories.cat_id = $link_cat");
-        if (!empty($cat_name)) {
-            $cat_name = ": category $cat_name";
-        }
-    }
+	$link_cat = '' . urldecode($link_cat) . '';
+	$link_cat = intval($link_cat);
 }
 ?><?php echo '<?xml version="1.0"?'.">\n"; ?>
-<!-- generator="wordpress/<?php echo $wp_version ?>" -->
+<!-- generator="wordpress/<?php bloginfo_rss('version') ?>" -->
 <opml version="1.0">
     <head>
         <title>Links for <?php echo get_bloginfo('name').$cat_name ?></title>
         <dateCreated><?php echo gmdate("D, d M Y H:i:s"); ?> GMT</dateCreated>
     </head>
     <body>
-<?php $sql = "SELECT $wpdb->links.link_url, link_rss, $wpdb->links.link_name, $wpdb->links.link_category, $wpdb->linkcategories.cat_name, link_updated 
-FROM $wpdb->links 
- JOIN $wpdb->linkcategories on $wpdb->links.link_category = $wpdb->linkcategories.cat_id
- AND $wpdb->links.link_visible = 'Y'
- $sql_cat
- ORDER BY $wpdb->linkcategories.cat_name, $wpdb->links.link_name \n";
- //echo("<!-- $sql -->");
- $prev_cat_id = 0;
- $results = $wpdb->get_results($sql);
- if ($results) {
-     foreach ($results as $result) {
-         if ($result->link_category != $prev_cat_id) { // new category
-             if ($prev_cat_id != 0)  { // not first time
-?>
-        </outline>
 <?php
-             } // end if not first time
+
+if (empty ($link_cat))
+	$cats = get_categories("type=link&hierarchical=0");
+else
+	$cats = array (get_category($link_cat));
+
+foreach ((array) $cats as $cat) {
 ?>
-        <outline type="category" title="<?php echo wp_specialchars($result->cat_name); ?>">
+<outline type="category" title="<?php echo wp_specialchars($cat->cat_name); ?>">
 <?php
-             $prev_cat_id = $result->link_category;
-        } // end if new category
+
+	$bookmarks = get_bookmarks("category={$cat->cat_ID}");
+	foreach ((array) $bookmarks as $bookmark) {
 ?>
-            <outline text="<?php echo wp_specialchars($result->link_name); ?>" type="link" xmlUrl="<?php echo wp_specialchars($result->link_rss); ?>" htmlUrl="<?php echo wp_specialchars($result->link_url); ?>" updated="<?php if ('0000-00-00 00:00:00' != $result->link_updated) echo $result->link_updated; ?>" />
+            <outline text="<?php echo wp_specialchars($bookmark->link_name); ?>" type="link" xmlUrl="<?php echo wp_specialchars($bookmark->link_rss); ?>" htmlUrl="<?php echo wp_specialchars($bookmark->link_url); ?>" updated="<?php if ('0000-00-00 00:00:00' != $bookmark->link_updated) echo $bookmark->link_updated; ?>" />
 <?php
-        } // end foreach
+
+	}
 ?>
-	</outline>
+</outline>
 <?php
-    } // end if
+
+}
 ?>
 </body>
 </opml>
