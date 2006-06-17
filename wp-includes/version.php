b14d632f4e6b0d805a374a8ef2c542e84a0e309e
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-06-17 02:02:29 +0000

    Tweak, fixes #2832
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3882 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -1,6 +1,6 @@
 <?php
 
-// This just holds the version number, in a separate file so we can bump it without cluttering the SVN
+// This just holds the version number in a separate file so we can bump it without cluttering the SVN
 
 $wp_version = '2.1-alpha2';
 $wp_db_version = 3845;
