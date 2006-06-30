435d479ec9e86f20c5780913e59c3f71df150c15
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-06-30 00:43:23 +0000

    Allow % so entities such as slashes don't break.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@3939 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -260,7 +260,10 @@ if ( !function_exists('wp_redirect') ) :
 function wp_redirect($location) {
 	global $is_IIS;
 
-	$location = preg_replace('|[^a-z0-9-~+_.?#=&;,/:]|i', '', $location);
+	$location = preg_replace('|[^a-z0-9-~+_.?#=&;,/:%]|i', '', $location);
+
+	$strip = array('%0d', '%0a');
+	$location = str_replace($strip, '', $location);
 
 	if ($is_IIS)
 		header("Refresh: 0;url=$location");
