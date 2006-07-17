ee691916c433d57f27a33b3962ef38dad1f8f245
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-07-17 17:08:00 +0000

    Remove unneeded php tags.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4019 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -311,7 +311,7 @@ class wpdb {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title><?php WordPress &rsaquo; Error ?></title>
+	<title>WordPress &rsaquo; Error</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<style media="screen" type="text/css">
 	<!--
