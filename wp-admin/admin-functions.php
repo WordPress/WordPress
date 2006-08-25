39e27240a8f6de8bd02b421c66ef53b92a6324cf
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-08-25 17:47:19 +0000

    Don't specialchar user description on save. fixes #3069
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4118 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-functions.php
+++ wp-admin/admin-functions.php
@@ -460,7 +460,7 @@ function edit_user($user_id = 0) {
 	if (isset ($_POST['display_name']))
 		$user->display_name = wp_specialchars(trim($_POST['display_name']));
 	if (isset ($_POST['description']))
-		$user->description = wp_specialchars(trim($_POST['description']));
+		$user->description = trim($_POST['description']);
 	if (isset ($_POST['jabber']))
 		$user->jabber = wp_specialchars(trim($_POST['jabber']));
 	if (isset ($_POST['aim']))
