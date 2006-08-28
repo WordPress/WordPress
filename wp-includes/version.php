eb78ba192c377c5cfae0765a2d755e7ed1307359
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-08-28 23:15:52 +0000

    Alpha bumpage.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4121 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -2,7 +2,7 @@
 
 // This holds the version number in a separate file so we can bump it without cluttering the SVN
 
-$wp_version = '2.1-alpha2';
+$wp_version = '2.1-alpha3';
 $wp_db_version = 3845;
 
 ?>
