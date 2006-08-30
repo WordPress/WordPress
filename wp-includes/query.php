2df111aadfe231d69d09d40596ac59b1b7d27f6c
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-08-30 09:41:40 +0000

    Cast page_id to int before deciding if is_page.  fixes #2769
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4132 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/query.php
+++ wp-includes/query.php
@@ -375,7 +375,7 @@ class WP_Query {
 			// If year, month, day, hour, minute, and second are set, a single 
 			// post is being queried.        
 			$this->is_single = true;
-		} elseif ('' != $qv['static'] || '' != $qv['pagename'] || '' != $qv['page_id']) {
+		} elseif ('' != $qv['static'] || '' != $qv['pagename'] || (int) $qv['page_id']) {
 			$this->is_page = true;
 			$this->is_single = false;
 		} elseif (!empty($qv['s'])) {
