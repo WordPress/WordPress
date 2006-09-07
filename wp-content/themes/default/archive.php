25b7a56d8bbb484b5f191da8761cbbaf9e3cf419
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-09-07 00:25:48 +0000

    Remove is_search() check that will never be reached. fixes #1076
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4166 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/archive.php
+++ wp-content/themes/default/archive.php
@@ -17,9 +17,6 @@
 		<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
 		<h2 class="pagetitle">Archive for <?php the_time('Y'); ?></h2>
 
-	  <?php /* If this is a search */ } elseif (is_search()) { ?>
-		<h2 class="pagetitle">Search Results</h2>
-
 	  <?php /* If this is an author archive */ } elseif (is_author()) { ?>
 		<h2 class="pagetitle">Author Archive</h2>
 
