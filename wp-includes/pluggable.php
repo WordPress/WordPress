6d3a4429f31a0215eeaa604697b07bb2e49aa945
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-09-26 00:55:21 +0000

    Send 302 on redirect.  Props leflo. fixes #3166
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4252 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -257,7 +257,7 @@ endif;
 // Cookie safe redirect.  Works around IIS Set-Cookie bug.
 // http://support.microsoft.com/kb/q176113/
 if ( !function_exists('wp_redirect') ) :
-function wp_redirect($location) {
+function wp_redirect($location, $status = 302) {
 	global $is_IIS;
 
 	$location = preg_replace('|[^a-z0-9-~+_.?#=&;,/:%]|i', '', $location);
@@ -265,6 +265,8 @@ function wp_redirect($location) {
 
 	$strip = array('%0d', '%0a');
 	$location = str_replace($strip, '', $location);
+
+	status_header($status);
 
 	if ($is_IIS)
 		header("Refresh: 0;url=$location");
