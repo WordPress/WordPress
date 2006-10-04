c95f68bcb959a30719b68b641f8f5a67f8aad2e0
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-10-04 10:35:49 +0000

    printf(), not print_f().
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4327 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options-writing.php
+++ wp-admin/options-writing.php
@@ -106,7 +106,7 @@ endforeach;
 
 <?php else : ?>
 
-	<p><?php print_f(__('WordPress is not notifying any <a href="http://codex.wordpress.org/Update_Services">Update Services</a> because of your blog\'s <a href="%s">privacy settings</a>'), 'options-privacy.php'); ?>
+	<p><?php printf(__('WordPress is not notifying any <a href="http://codex.wordpress.org/Update_Services">Update Services</a> because of your blog\'s <a href="%s">privacy settings</a>'), 'options-privacy.php'); ?>
 
 <?php endif; ?>
 
