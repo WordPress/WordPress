f771adf9ee3e25cf843c6fd39b0110da53b47468
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-10-12 10:14:14 +0000

    Somewhat stricter smilies replacements by Nazgul.  fixes #3222
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4380 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -618,7 +618,14 @@ function convert_smilies($text) {
 	if (get_option('use_smilies')) {
 		// HTML loop taken from texturize function, could possible be consolidated
 		$textarr = preg_split("/(<.*>)/U", $text, -1, PREG_SPLIT_DELIM_CAPTURE); // capture the tags as well as in between
-		$output = implode('', preg_replace($wp_smiliessearch, $wp_smiliesreplace, $textarr));
+		$stop = count($textarr);// loop stuff 
+		for ($i = 0; $i < $stop; $i++) { 
+			$content = $textarr[$i]; 
+			if ((strlen($content) > 0) && ('<' != $content{0})) { // If it's not a tag 
+				$content = preg_replace($wp_smiliessearch, $wp_smiliesreplace, $content); 
+			} 
+			$output .= $content; 
+		}
 	} else {
 		// return default text.
 		$output = $text;
