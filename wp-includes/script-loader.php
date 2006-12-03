936dfdd36f82549b00223320b84781304872b192
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2006-12-03 21:53:54 +0000

    Prototype compressed doesn't seem to be working out so well :/
    Fixes #3414
    
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4592 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -18,7 +18,7 @@ class WP_Scripts {
 		$this->add( 'tiny_mce', '/wp-includes/js/tinymce/tiny_mce_gzip.php', false, '20061113' );
 		$mce_config = apply_filters('tiny_mce_config_url', '/wp-includes/js/tinymce/tiny_mce_config.php');
 		$this->add( 'wp_tiny_mce', $mce_config, array('tiny_mce'), '20061113' );
-		$this->add( 'prototype', '/wp-includes/js/prototype.compressed.js', false, '1.5.0');
+		$this->add( 'prototype', '/wp-includes/js/prototype.js', false, '1.5.0');
 		$this->add( 'autosave', '/wp-includes/js/autosave.js.php', array('prototype', 'sack'), '4508');
 		$this->add( 'wp-ajax', '/wp-includes/js/wp-ajax-js.php', array('prototype'), '4459');
 		$this->add( 'listman', '/wp-includes/js/list-manipulation-js.php', array('wp-ajax', 'fat'), '4583');
