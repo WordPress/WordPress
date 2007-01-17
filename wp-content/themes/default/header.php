e79ff075c1ef23da8200d688df7aac97dcb55051
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-01-17 00:01:21 +0000

    Add language_attributes() to theme headers.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4757 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/header.php
+++ wp-content/themes/default/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
