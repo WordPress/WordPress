1e019126eb77c718c841c8c755c4b4832fa49c6c
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-01-20 06:48:56 +0000

    RC2
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4776 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -2,7 +2,7 @@
 
 // This holds the version number in a separate file so we can bump it without cluttering the SVN
 
-$wp_version = '2.1-RC1';
+$wp_version = '2.1-RC2';
 $wp_db_version = 4772;
 
 ?>
