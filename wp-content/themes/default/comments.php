683e079c00ba0c4cb73d25a49eb95dc572521ca9
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-02-21 01:55:16 +0000

    Wrapped allowed html in code tags.  Props rob1n.  fixes #998
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4901 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/comments.php
+++ wp-content/themes/default/comments.php
@@ -88,7 +88,7 @@
 
 <?php endif; ?>
 
-<!--<p><small><strong>XHTML:</strong> You can use these tags: <?php echo allowed_tags(); ?></small></p>-->
+<!--<p><small><strong>XHTML:</strong> You can use these tags: <code><?php echo allowed_tags(); ?></code></small></p>-->
 
 <p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
 
