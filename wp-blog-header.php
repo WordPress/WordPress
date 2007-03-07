8160b01da1b1e95dda77026983108dd2672c420a
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-03-07 00:17:58 +0000

    Fix wp_die() includes when bailing out in the blog header.  Props tacker. fixes #3883
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4973 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -5,8 +5,9 @@ if ( !file_exists( dirname(__FILE__) . '/wp-config.php') ) {
 	if ( strstr( $_SERVER['PHP_SELF'], 'wp-admin') ) $path = '';
 	else $path = 'wp-admin/';
 
-  require_once( dirname(__FILE__) . '/wp-includes/functions.php');
-  wp_die("There doesn't seem to be a <code>wp-config.php</code> file. I need this before we can get started. Need more help? <a href='http://codex.wordpress.org/Editing_wp-config.php'>We got it</a>. You can <a href='{$path}setup-config.php'>create a <code>wp-config.php</code> file through a web interface</a>, but this doesn't work for all server setups. The safest way is to manually create the file.", "WordPress &rsaquo; Error");
+	require_once( dirname(__FILE__) . '/wp-includes/classes.php');
+	require_once( dirname(__FILE__) . '/wp-includes/functions.php');
+	wp_die("There doesn't seem to be a <code>wp-config.php</code> file. I need this before we can get started. Need more help? <a href='http://codex.wordpress.org/Editing_wp-config.php'>We got it</a>. You can <a href='{$path}setup-config.php'>create a <code>wp-config.php</code> file through a web interface</a>, but this doesn't work for all server setups. The safest way is to manually create the file.", "WordPress &rsaquo; Error");
 }
 
 $wp_did_header = true;
