ab79f95e5f9f8469e47566c8ed4260d6b0df810d
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-03-07 02:26:12 +0000

    Use get_bloginfo to get the blog name.  Props jhodgdon.  fixes #3903
    git-svn-id: http://svn.automattic.com/wordpress/trunk@4982 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -44,7 +44,7 @@ do_action('admin_head');
 </head>
 <body>
 <div id="wphead">
-<h1><?php echo wptexturize(get_option(('blogname'))); ?> <span>(<a href="<?php echo get_option('home') . '/'; ?>"><?php _e('View site &raquo;') ?></a>)</span></h1>
+<h1><?php echo wptexturize(get_bloginfo(('name'))); ?> <span>(<a href="<?php echo get_option('home') . '/'; ?>"><?php _e('View site &raquo;') ?></a>)</span></h1>
 </div>
 <div id="user_info"><p><?php printf(__('Howdy, <strong>%s</strong>.'), $user_identity) ?> [<a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="<?php _e('Log out of this account') ?>"><?php _e('Sign Out'); ?></a>, <a href="profile.php"><?php _e('My Profile'); ?></a>] </p></div>
 
