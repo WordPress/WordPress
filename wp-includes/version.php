194f7b4a752d0f978159a341eb7827cafccedf84
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-03-07 05:38:23 +0000

    Working on 2.1.3
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.1@4992 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -2,7 +2,7 @@
 
 // This holds the version number in a separate file so we can bump it without cluttering the SVN
 
-$wp_version = '2.1.2';
+$wp_version = '2.1.3-alpha';
 $wp_db_version = 4773;
 
 ?>
