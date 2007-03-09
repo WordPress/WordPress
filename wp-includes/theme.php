0cd477441ea158ccdb119edb4ff25d50ba2a259b
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-03-09 04:15:17 +0000

    Return theme uri in theme data.  Props nazgul. fixes #3866
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5006 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/theme.php
+++ wp-includes/theme.php
@@ -78,6 +78,7 @@ function get_theme_data( $theme_file ) {
 	$name = $theme_name[1];
 	$name = trim( $name );
 	$theme = $name;
+	$theme_uri = trim( $theme_uri[1] );
 
 	if ( '' == $author_uri[1] ) {
 		$author = trim( $author_name[1] );
@@ -85,7 +86,7 @@ function get_theme_data( $theme_file ) {
 		$author = '<a href="' . trim( $author_uri[1] ) . '" title="' . __('Visit author homepage') . '">' . trim( $author_name[1] ) . '</a>';
 	}
 
-	return array( 'Name' => $name, 'Title' => $theme, 'Description' => $description, 'Author' => $author, 'Version' => $version, 'Template' => $template[1], 'Status' => $status );
+	return array( 'Name' => $name, 'Title' => $theme, 'URI' => $theme_uri, 'Description' => $description, 'Author' => $author, 'Version' => $version, 'Template' => $template[1], 'Status' => $status );
 }
 
 function get_themes() {
