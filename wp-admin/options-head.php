e43516e5f6208fcf3106bf2e81c24736e99e190b
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-03-16 08:10:02 +0000

    Removing superfluous <br /> from Options-General.  Props johnbillion.  fixes #3929 for trunk.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5048 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options-head.php
+++ wp-admin/options-head.php
@@ -1,7 +1,5 @@
 <?php wp_reset_vars(array('action', 'standalone', 'option_group_id')); ?>
 
-<br clear="all" />
-
 <?php if (isset($_GET['updated'])) : ?>
 <div id="message" class="updated fade"><p><strong><?php _e('Options saved.') ?></strong></p></div>
 <?php endif; ?>
