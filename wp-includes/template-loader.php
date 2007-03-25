37c1e764de6dd3cc81d1f55707b865e6eef357e1
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-03-25 23:39:14 +0000

    Return instead of exit in template-loader.  Props Otto42 and Nazgul. fixes #3790
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5104 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/template-loader.php
+++ wp-includes/template-loader.php
@@ -3,70 +3,70 @@ if ( defined('WP_USE_THEMES') && constant('WP_USE_THEMES') ) {
 	do_action('template_redirect');
 	if ( is_robots() ) {
 		do_action('do_robots');
-		exit;
+		return;
 	} else if ( is_feed() ) {
 		do_feed();
-		exit;
+		return;
 	} else if ( is_trackback() ) {
 		include(ABSPATH . '/wp-trackback.php');
-		exit;
+		return;
 	} else if ( is_404() && $template = get_404_template() ) {
 		include($template);
-		exit;
+		return;
 	} else if ( is_search() && $template = get_search_template() ) {
 		include($template);
-		exit;
+		return;
 	} else if ( is_home() && $template = get_home_template() ) {
 		include($template);
-		exit;
+		return;
 	} else if ( is_attachment() && $template = get_attachment_template() ) {
 		include($template);
-		exit;
+		return;
 	} else if ( is_single() && $template = get_single_template() ) {
 		if ( is_attachment() )
 			add_filter('the_content', 'prepend_attachment');
 		include($template);
-		exit;
+		return;
 	} else if ( is_page() && $template = get_page_template() ) {
 		if ( is_attachment() )
 			add_filter('the_content', 'prepend_attachment');
 		include($template);
-		exit;
+		return;
 	} else if ( is_category() && $template = get_category_template()) {
 		include($template);
-		exit;
+		return;
 	} else if ( is_author() && $template = get_author_template() ) {
 		include($template);
-		exit;
+		return;
 	} else if ( is_date() && $template = get_date_template() ) {
 		include($template);
-		exit;
+		return;
 	} else if ( is_archive() && $template = get_archive_template() ) {
 		include($template);
-		exit;
+		return;
 	} else if ( is_comments_popup() && $template = get_comments_popup_template() ) {
 		include($template);
-		exit;
+		return;
 	} else if ( is_paged() && $template = get_paged_template() ) {
 		include($template);
-		exit;
+		return;
 	} else if ( file_exists(TEMPLATEPATH . "/index.php") ) {
 		if ( is_attachment() )
 			add_filter('the_content', 'prepend_attachment');
 		include(TEMPLATEPATH . "/index.php");
-		exit;
+		return;
 	}
 } else {
 	// Process feeds and trackbacks even if not using themes.
 	if ( is_robots() ) {
 		do_action('do_robots');
-		exit;
+		return;
 	} else if ( is_feed() ) {
 		do_feed();
-		exit;
+		return;
 	} else if ( is_trackback() ) {
 		include(ABSPATH . '/wp-trackback.php');
-		exit;
+		return;
 	}
 }
 
