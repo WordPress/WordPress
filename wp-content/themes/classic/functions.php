04f7f566b0c3b47c9fe5adafc9058d0347ff9678
rob1n <rob1n@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-04-04 00:58:55 +0000

    Fix typo. Props johnbillion. fixes #4057
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5176 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/classic/functions.php
+++ wp-content/themes/classic/functions.php
@@ -1,5 +1,5 @@
 <?php
-if ( function_exists('register_sidebars') )
+if ( function_exists('register_sidebar') )
 	register_sidebar(array(
         'before_widget' => '<li id="%1$s" class="widget %2$s">',
         'after_widget' => '</li>',
