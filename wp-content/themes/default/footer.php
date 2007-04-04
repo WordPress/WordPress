ad243d0be338912dab7f2f1af47932bf69a33f90
rob1n <rob1n@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-04-04 23:35:37 +0000

    Fix typo. Props Nazgul. fixes #4083
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5180 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/footer.php
+++ wp-content/themes/default/footer.php
@@ -1,7 +1,7 @@
 
 <hr />
 <div id="footer">
-<!-- If you'd like to support WordPress, having the "powered by" link someone on your blog is the best way, it's our only promotion or advertising. -->
+<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way, it's our only promotion or advertising. -->
 	<p>
 		<?php bloginfo('name'); ?> is proudly powered by
 		<a href="http://wordpress.org/">WordPress</a>
