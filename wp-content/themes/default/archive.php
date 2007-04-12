aab2df9f6a5c1b123170dd6d684365a51a0df530
rob1n <rob1n@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-04-12 04:39:55 +0000

    Improve tag archive's title. Props johnbillion. fixes #4133
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5250 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/archive.php
+++ wp-content/themes/default/archive.php
@@ -4,27 +4,22 @@
 <?php is_tag(); ?>
 		<?php if (have_posts()) : ?>
 
-		 <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-<?php /* If this is a category archive */ if (is_category()) { ?>
+ 	  <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
+ 	  <?php /* If this is a category archive */ if (is_category()) { ?>
 		<h2 class="pagetitle">Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category</h2>
-<?php } elseif( is_tag() ) { ?>
-<h2 class="pagetitle">Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Tag</h2>
+ 	  <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
+		<h2 class="pagetitle">Posts Tagged &#8216;<?php single_cat_title(); ?>&#8217;</h2>
  	  <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
 		<h2 class="pagetitle">Archive for <?php the_time('F jS, Y'); ?></h2>
-
-	 <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
+ 	  <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
 		<h2 class="pagetitle">Archive for <?php the_time('F, Y'); ?></h2>
-
-		<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
+ 	  <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
 		<h2 class="pagetitle">Archive for <?php the_time('Y'); ?></h2>
-
 	  <?php /* If this is an author archive */ } elseif (is_author()) { ?>
 		<h2 class="pagetitle">Author Archive</h2>
-
-		<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
+ 	  <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
 		<h2 class="pagetitle">Blog Archives</h2>
-
-		<?php } ?>
+ 	  <?php } ?>
 
 
 		<div class="navigation">
