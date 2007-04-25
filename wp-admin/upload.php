129931fdcdf11b33af67c48ad3d1d6e9a5df17f6
rob1n <rob1n@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-04-25 00:56:22 +0000

    Add admin_xml_ns to upload.php's <html>, since it doesn't use admin-header.php.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5309 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/upload.php
+++ wp-admin/upload.php
@@ -59,7 +59,7 @@ foreach ( $wp_upload_tabs as $t => $tab_array ) {
 
 if ( 'inline' == $style ) : ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<html xmlns="http://www.w3.org/1999/xhtml" <?php do_action('admin_xml_ns'); ?> <?php language_attributes(); ?>>
 <head>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php echo get_option('blog_charset'); ?>" />
 <title><?php bloginfo('name') ?> &rsaquo; <?php _e('Uploads'); ?> &#8212; WordPress</title>
