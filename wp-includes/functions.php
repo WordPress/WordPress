9e54a128b8b8e4794868999f19bf76a259e0e421
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-05-02 00:32:22 +0000

    Call get_option('siteurl') outside the loop to save a few dozen get_option calls. Props andissimo.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5370 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -1477,10 +1477,11 @@ function smilies_init() {
 		);
 	}
 
+	$siteurl = get_option('siteurl');
 	foreach ( (array) $wpsmiliestrans as $smiley => $img ) {
 		$wp_smiliessearch[] = '/(\s|^)'.preg_quote($smiley, '/').'(\s|$)/';
 		$smiley_masked = htmlspecialchars(trim($smiley), ENT_QUOTES);
-		$wp_smiliesreplace[] = " <img src='" . get_option('siteurl') . "/wp-includes/images/smilies/$img' alt='$smiley_masked' class='wp-smiley' /> ";
+		$wp_smiliesreplace[] = " <img src='$siteurl/wp-includes/images/smilies/$img' alt='$smiley_masked' class='wp-smiley' /> ";
 	}
 }
 
