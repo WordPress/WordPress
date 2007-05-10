1f0a61fd73c1c408b8143f77c8ef130c37ce0ff6
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-05-10 00:05:03 +0000

    Don't preg_match_all when processing items.  fixes #4239 for 2.2
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.2@5422 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/import/wordpress.php
+++ wp-admin/import/wordpress.php
@@ -84,19 +84,40 @@ class WP_Import {
 
 	function get_entries() {
 		set_magic_quotes_runtime(0);
-		$importdata = file($this->file); // Read the file into an array
-		$importdata = implode('', $importdata); // squish it
-		$importdata = preg_replace("/(\r\n|\n|\r)/", "\n", $importdata);
-		preg_match_all('|<item>(.*?)</item>|is', $importdata, $this->posts);
-		$this->posts = $this->posts[1];
+		$importdata = array_map('rtrim', file($this->file)); // Read the file into an array
+
+		$this->posts = array();
+		$this->categories = array();
+		$num = 0;
+		$doing_entry = false;
+		foreach ($importdata as $importline) {
+			if ( false !== strpos($importline, '<wp:category>') ) {
+				preg_match('|<wp:category>(.*?)</wp:category>|is', $importline, $category);
+				$this->categories[] = $category[1];
+				continue;
+			}
+			if ( false !== strpos($importline, '<item>') ) {
+				$this->posts[$num] = '';
+				$doing_entry = true;
+				continue;	
+			}
+			if ( false !== strpos($importline, '</item>') ) {
+				$num++;
+				$doing_entry = false;
+				continue;	
+			}
+			if ( $doing_entry ) {
+				$this->posts[$num] .= $importline . "\n";
+			}
+		}
+
 		foreach ($this->posts as $post) {
 			$post_ID = (int) $this->get_tag( $post, 'wp:post_id' );
-			if ($post_ID)
+			if ($post_ID) {
 				$this->posts_processed[$post_ID][0] = &$post;
 				$this->posts_processed[$post_ID][1] = 0;
+			}
 		}
-		preg_match_all('|<wp:category>(.*?)</wp:category>|is', $importdata, $this->categories);
-		$this->categories = $this->categories[1];
 	}
 
 	function get_wp_authors() {
