ac72ff2a422a61bcf81d3849f9007d1d3d88ebdb
rob1n <rob1n@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-05-11 03:51:21 +0000

    Show authors' logins in alphabetical order. fixes #4232
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5448 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/import/wordpress.php
+++ wp-admin/import/wordpress.php
@@ -43,7 +43,7 @@ class WP_Import {
 
 	function users_form($n) {
 		global $wpdb, $testing;
-		$users = $wpdb->get_results("SELECT * FROM $wpdb->users ORDER BY ID");
+		$users = $wpdb->get_results("SELECT user_login FROM $wpdb->users ORDER BY user_login");
 ?><select name="userselect[<?php echo $n; ?>]">
 	<option value="#NONE#">- Select -</option>
 	<?php
