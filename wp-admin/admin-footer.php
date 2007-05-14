05c0fccdf91b26b3a00ae807552d7846a02e4547
rob1n <rob1n@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-05-14 03:28:10 +0000

    Get rid of stray space.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5470 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -2,7 +2,7 @@
 <div id="footer">
 <p class="logo"><a href="http://wordpress.org/" id="wordpress-logo"><img src="images/wordpress-logo.png" alt="WordPress" /></a></p>
 <p class="docs"><?php _e('<a href="http://codex.wordpress.org/">Documentation</a>'); ?> &#8212; <?php _e('<a href="http://wordpress.org/support/">Support Forums</a>'); ?><br />
-<?php printf( __('Version %s '), get_bloginfo('version') ); ?></p>
+<?php printf( __('Version %s'), get_bloginfo('version') ); ?></p>
 </div>
 <?php do_action('admin_footer', ''); ?>
 <script type="text/javascript">if(typeof wpOnload=='function')wpOnload();</script>
