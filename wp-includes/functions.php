7f4475bedbae4cfb4bd2d32b9ea435d66f2e579e
rob1n <rob1n@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-05-21 22:50:32 +0000

    Don't call is_wp_error() in wp_die() if it hasn't been defined yet. fixes #4308
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.2@5507 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -1317,7 +1317,7 @@ function wp_nonce_ays($action) {
 function wp_die( $message, $title = '' ) {
 	global $wp_locale;
 
-	if ( is_wp_error( $message ) ) {
+	if ( function_exists( 'is_wp_error' ) && is_wp_error( $message ) ) {
 		if ( empty($title) ) {
 			$error_data = $message->get_error_data();
 			if ( is_array($error_data) && isset($error_data['title']) )
