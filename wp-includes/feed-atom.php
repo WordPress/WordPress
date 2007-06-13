72c169c2f19130e803a2c9ddf50988338995020b
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-06-13 18:03:17 +0000

    Don't output author uri tag if uri is empty.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5699 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/feed-atom.php
+++ wp-includes/feed-atom.php
@@ -26,7 +26,9 @@ $more = 1;
 	<entry>
 		<author>
 			<name><?php the_author() ?></name>
+			<?php $author_url = get_the_author_url(); if ( !empty($author_url) ) : ?>
 			<uri><?php the_author_url()?></uri>
+			<?php endif; ?>
 		</author>
 		<title type="<?php html_type_rss(); ?>"><![CDATA[<?php the_title_rss() ?>]]></title>
 		<link rel="alternate" type="text/html" href="<?php permalink_single_rss() ?>" />
