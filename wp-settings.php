2fd0d47c6d49c9a8a391cb66e22891487560bd19
rob1n <rob1n@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-06-22 21:48:36 +0000

    Check for PHP 4.2 or greater in 2.2 branch. fixes #4510
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.2@5745 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-settings.php
+++ wp-settings.php
@@ -48,7 +48,7 @@ $PHP_SELF = $_SERVER['PHP_SELF'];
 if ( empty($PHP_SELF) )
 	$_SERVER['PHP_SELF'] = $PHP_SELF = preg_replace("/(\?.*)?$/",'',$_SERVER["REQUEST_URI"]);
 
-if ( !(phpversion() >= '4.1') )
+if ( !(phpversion() >= '4.2') )
 	die( 'Your server is running PHP version ' . phpversion() . ' but WordPress requires at least 4.1' );
 
 if ( !extension_loaded('mysql') && !file_exists(ABSPATH . 'wp-content/db.php') )
