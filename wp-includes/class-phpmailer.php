44df30f2d690a1af2855db921dd1834ef6c04ff8
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-08-05 18:13:24 +0000

    Fix class-smtp.php typo.  Props Nazgul, tension7.  fixes #4700
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5852 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-phpmailer.php
+++ wp-includes/class-phpmailer.php
@@ -457,7 +457,7 @@ class PHPMailer
      * @return bool
      */
     function SmtpSend($header, $body) {
-        include_once($this->PluginDir . "class.smtp.php");
+        include_once($this->PluginDir . "class-smtp.php");
         $error = "";
         $bad_rcpt = array();
 
