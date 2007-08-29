816f89fb03a6d5ae02a21c7f43444bda51249f39
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-08-29 21:06:51 +0000

    More cautious canonical-ness.  Exclude IIS and POSTs.  Better paging handling.  Still a work in progress!  see #4773
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5978 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/canonical.php
+++ wp-includes/canonical.php
@@ -2,9 +2,9 @@
 // Based on "Permalink Redirect" from Scott Yang and "Enforce www. Preference" by Mark Jaquith
 
 function redirect_canonical() {
-	global $wp_rewrite, $posts;
+	global $wp_rewrite, $posts, $is_IIS;
 
-	if ( is_feed() || is_trackback() || is_search() || is_comments_popup() || is_admin() )
+	if ( is_feed() || is_trackback() || is_search() || is_comments_popup() || is_admin() || $is_IIS || ( isset($_POST) && count($_POST) ) )
 		return;
 
 	// build the URL in the address bar
@@ -24,13 +24,13 @@ function redirect_canonical() {
 		$redirect_url = redirect_guess_404_permalink();
 	} elseif ( is_object($wp_rewrite) && $wp_rewrite->using_permalinks() ) {
 		// rewriting of old ?p=X, ?m=2004, ?m=200401, ?m=20040101
-		if ( is_single() && get_query_var('p') ) {
+		if ( is_single() && isset($_GET['p']) ) {
 			if ( $redirect_url = get_permalink(get_query_var('p')) )
 				$redirect['query'] = remove_query_arg('p', $redirect['query']);
-		} elseif ( is_page() && get_query_var('page_id') ) {
+		} elseif ( is_page() && isset($_GET['page_id']) ) {
 			if ( $redirect_url = get_permalink(get_query_var('page_id')) )
 				$redirect['query'] = remove_query_arg('page_id', $redirect['query']);
-		} elseif ( get_query_var('m') && ( is_year() || is_month() || is_day() ) ) {
+		} elseif ( isset($_GET['m']) && ( is_year() || is_month() || is_day() ) ) {
 			$m = get_query_var('m');
 			switch ( strlen($m) ) {
 				case 4: // Yearly
@@ -46,19 +46,19 @@ function redirect_canonical() {
 			if ( $redirect_url )
 				$redirect['query'] = remove_query_arg('m', $redirect['query']);
 		// now moving on to non ?m=X year/month/day links
-		} elseif ( is_day() && get_query_var('year') && get_query_var('monthnum') && get_query_var('day') ) { 
+		} elseif ( is_day() && get_query_var('year') && get_query_var('monthnum') && isset($_GET['day']) ) { 
 			if ( $redirect_url = get_day_link(get_query_var('year'), get_query_var('monthnum'), get_query_var('day')) )
 				$redirect['query'] = remove_query_arg(array('year', 'monthnum', 'day'), $redirect['query']);
-		} elseif ( is_month() && get_query_var('year') && get_query_var('monthnum') ) {
+		} elseif ( is_month() && get_query_var('year') && isset($_GET['monthnum']) ) {
 			if ( $redirect_url = get_month_link(get_query_var('year'), get_query_var('monthnum')) )
 				$redirect['query'] = remove_query_arg(array('year', 'monthnum'), $redirect['query']);
-		} elseif ( is_year() && get_query_var('year') ) {
+		} elseif ( is_year() && isset($_GET['year']) ) {
 			if ( $redirect_url = get_year_link(get_query_var('year')) )
 				$redirect['query'] = remove_query_arg('year', $redirect['query']);
-		} elseif ( is_category() && get_query_var('cat') ) {
+		} elseif ( is_category() && isset($_GET['cat']) ) {
 			if ( $redirect_url = get_category_link(get_query_var('cat')) )
 				$redirect['query'] = remove_query_arg('cat', $redirect['query']);
-		} elseif ( is_author() && get_query_var('author') ) {
+		} elseif ( is_author() && isset($_GET['author']) ) {
 			$author = get_userdata(get_query_var('author'));
 			if ( false !== $author && $redirect_url = get_author_link(false, $author->ID, $author->user_nicename) )
 				$redirect['query'] = remove_query_arg('author', $redirect['author']);
@@ -66,48 +66,70 @@ function redirect_canonical() {
 
 	// paging
 		if ( $paged = get_query_var('paged') ) {
-			if ( $paged > 1 ) {
+			if ( $paged > 0 ) {
 				if ( !$redirect_url )
 					$redirect_url = $requested_url;
 				$paged_redirect = @parse_url($redirect_url);
-				$paged_redirect['path'] = preg_replace('|/index.php/?$|', '/', $paged_redirect['path']);
-				$paged_redirect['path'] = trailingslashit($paged_redirect['path']);
-				if ( $wp_rewrite->using_index_permalinks() && strpos($paged_redirect['path'], '/index.php/') === false )
-					$paged_redirect['path'] .= 'index.php/';
-				$paged_redirect['path'] .= user_trailingslashit("page/$paged");
+				$paged_redirect['path'] = preg_replace('|/page/[0-9]+?/?$|', '/', $paged_redirect['path']); // strip off any existing paging
+				$paged_redirect['path'] = preg_replace('|/index.php/?$|', '/', $paged_redirect['path']); // strip off trailing /index.php/
+				if ( $paged > 1 && !is_singular() ) {
+					$paged_redirect['path'] = trailingslashit($paged_redirect['path']);
+					if ( $wp_rewrite->using_index_permalinks() && strpos($paged_redirect['path'], '/index.php/') === false )
+						$paged_redirect['path'] .= 'index.php/';
+					$paged_redirect['path'] .= user_trailingslashit("page/$paged", 'paged');
+				} elseif ( !is_home() && !is_singular() ){
+					$paged_redirect['path'] = user_trailingslashit($paged_redirect['path'], 'paged');
+				}
 				$redirect_url = $paged_redirect['scheme'] . '://' . $paged_redirect['host'] . $paged_redirect['path'];
+				$redirect['path'] = $paged_redirect['path'];
 			}
 			$redirect['query'] = remove_query_arg('paged', $redirect['query']);
 		}
 	}
 
-	// tack on any additional query vars
-	if ( $redirect_url && $redirect['query'] ) {
-		if ( strpos($redirect_url, '?') !== false )
-			$redirect_url .= '&';
-		else
-			$redirect_url .= '?';
-		$redirect_url .= $redirect['query'];
+// tack on any additional query vars
+if ( $redirect_url && $redirect['query'] ) {
+	if ( strpos($redirect_url, '?') !== false )
+		$redirect_url .= '&';
+	else
+		$redirect_url .= '?';
+	$redirect_url .= $redirect['query'];
+}
+
+if ( $redirect_url )
+	$redirect = @parse_url($redirect_url);
+
+	// www.example.com vs example.com
+	$user_home = @parse_url(get_option('home'));
+	$redirect['host'] = $user_home['host'];
+
+	// trailing /index.php or /index.php/
+	$redirect['path'] = preg_replace('|/index.php/?$|', '/', $redirect['path']);
+
+	// strip /index.php/ when we're not using PATHINFO permalinks
+	if ( !$wp_rewrite->using_index_permalinks() )
+		$redirect['path'] = str_replace('/index.php/', '/', $redirect['path']);
+
+	// trailing slashes
+	if ( is_object($wp_rewrite) && $wp_rewrite->using_permalinks() && !is_404() && (!is_home() || ( is_home() && (get_query_var('paged') > 1) ) ) ) {
+		$user_ts_type = '';
+		if ( get_query_var('paged') > 0 ) {
+			$user_ts_type = 'paged';
+		} else {
+			foreach ( array('single', 'category', 'page', 'day', 'month', 'year') as $type ) {
+				$func = 'is_' . $type;
+				if ( call_user_func($func) )
+					$user_ts_type = $type;
+					break;
+				}
+			}
+		$redirect['path'] = user_trailingslashit($redirect['path'], $user_ts_type);
 	}
 
-	if ( !$redirect_url ) { // we're only going down this road if we don't have a WP-generated link from above
-
-		// www.example.com vs example.com
-		$user_home = parse_url(get_option('home'));
-		$redirect['host'] = $user_home['host'];
-
-		// trailing /index.php or /index.php/
-		$redirect['path'] = preg_replace('|/index.php/?$|', '/', $redirect['path']);
-
-		// need to strip off the query string
-		if ( is_object($wp_rewrite) && $wp_rewrite->using_permalinks() && !is_404() && (!is_home() || ( is_home() && is_paged() ) ) )
-			$redirect['path'] = user_trailingslashit($redirect['path']);
-
-		if ( array($original['host'], $original['path'], $original['query']) !== array($redirect['host'], $redirect['path'], $redirect['query']) ) {
-			$redirect_url = $redirect['scheme'] . '://' . $redirect['host'] . $redirect['path'];
-			if ( $redirect['query'] )
-				$redirect_url .= '?' . $redirect['query'];
-		}
+	if ( array($original['host'], $original['path'], $original['query']) !== array($redirect['host'], $redirect['path'], $redirect['query']) ) {
+		$redirect_url = $redirect['scheme'] . '://' . $redirect['host'] . $redirect['path'];
+		if ( $redirect['query'] )
+			$redirect_url .= '?' . $redirect['query'];
 	}
 
 	if ( $redirect_url && $redirect_url != $requested_url ) {
