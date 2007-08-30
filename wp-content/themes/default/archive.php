c8d7b27ed2015555e7f327e63c7a46939031ba96
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-08-30 19:54:46 +0000

    Use single_tag_title() on tag pages in the Default theme.  Props Andy.  fixes #4876
    git-svn-id: http://svn.automattic.com/wordpress/trunk@5996 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/archive.php
+++ wp-content/themes/default/archive.php
@@ -8,7 +8,7 @@
  	  <?php /* If this is a category archive */ if (is_category()) { ?>
 		<h2 class="pagetitle">Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category</h2>
  	  <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-		<h2 class="pagetitle">Posts Tagged &#8216;<?php single_cat_title(); ?>&#8217;</h2>
+		<h2 class="pagetitle">Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h2>
  	  <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
 		<h2 class="pagetitle">Archive for <?php the_time('F jS, Y'); ?></h2>
  	  <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
