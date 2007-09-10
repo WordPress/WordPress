e703203b8103cd213777c8d322f73bfcdb4dbdb8
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-09-10 14:07:18 +0000

    Preparing for 2.0.12
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.0@6067 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -2,7 +2,7 @@
 
 // This just holds the version number, in a separate file so we can bump it without cluttering the SVN
 
-$wp_version = '2.0.11';
+$wp_version = '2.0.12-alpha';
 $wp_db_version = 3441;
 
 ?>
