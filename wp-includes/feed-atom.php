cf26b9d04960b0a7c39cd7cec355bd7edc6c4713
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-09-12 16:26:39 +0000

    Make the atom feed updated field actually tell you when the post was updated. Fixes #4816 props lapcat.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@6093 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/feed-atom.php
+++ wp-includes/feed-atom.php
@@ -33,7 +33,7 @@ $more = 1;
 		<title type="<?php html_type_rss(); ?>"><![CDATA[<?php the_title_rss() ?>]]></title>
 		<link rel="alternate" type="text/html" href="<?php the_permalink_rss() ?>" />
 		<id><?php the_guid(); ?></id>
-		<updated><?php echo get_post_time('Y-m-d\TH:i:s\Z', true); ?></updated>
+		<updated><?php echo get_post_modified_time('Y-m-d\TH:i:s\Z', true); ?></updated>
 		<published><?php echo get_post_time('Y-m-d\TH:i:s\Z', true); ?></published>
 		<?php the_category_rss('atom') ?>
 		<summary type="<?php html_type_rss(); ?>"><![CDATA[<?php the_excerpt_rss(); ?>]]></summary>
