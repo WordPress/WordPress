fbd6081f4626159cab409cb86a042481827b67c2
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-09-28 17:37:16 +0000

    Spelling fix from zamoose. fixes #5095 for trunk
    git-svn-id: http://svn.automattic.com/wordpress/trunk@6174 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/import/utw.php
+++ wp-admin/import/utw.php
@@ -157,7 +157,7 @@ class UTW_Import {
 		// run that funky magic!
 		$tags_added = $this->tag2post();
 
-		echo '<p>' . sprintf( __('Done! <strong>%s</strong> tags where added!'), $tags_added ) . '<br /></p>';
+		echo '<p>' . sprintf( __('Done! <strong>%s</strong> tags were added!'), $tags_added ) . '<br /></p>';
 
 		echo '<form action="admin.php?import=utw&amp;step=4" method="post">';
 		wp_nonce_field('import-utw');
