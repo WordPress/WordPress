99ae520d0df08557d79fe3c203bcef99781a34b1
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-10-17 19:37:21 +0000

    Fix atom version number in the classic theme. Relates #5213 props rubys
    git-svn-id: http://svn.automattic.com/wordpress/trunk@6261 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/classic/header.php
+++ wp-content/themes/classic/header.php
@@ -12,7 +12,7 @@
 
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
-	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
+	<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url'); ?>" />
 
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_get_archives('type=monthly&format=link'); ?>
