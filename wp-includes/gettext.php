d1cdea8eb26cbaa9778dd7b0267f3214a9a827dc
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2007-12-24 15:14:07 +0000

    Avoid the 64bit dragons. Fixes #3780 props nbachiyski.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@6488 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/gettext.php
+++ wp-includes/gettext.php
@@ -114,7 +114,7 @@ class gettext_reader {
 
 		$this->STREAM = $Reader;
 		$magic = $this->readint();
-		if ($magic == ($MAGIC1 & 0xFFFFFFFF) || $magic == ($MAGIC3 & 0xFFFFFFFF)) { // to make sure it works for 64-bit platforms
+		if ($magic == $MAGIC1 || $magic == $MAGIC3) { // to make sure it works for 64-bit platforms
 			$this->BYTEORDER = 0;
 		} elseif ($magic == ($MAGIC2 & 0xFFFFFFFF)) {
 			$this->BYTEORDER = 1;
