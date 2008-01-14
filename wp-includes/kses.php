076634d99bd1777b42fe8946be092fb343ecffc3
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-01-14 23:56:53 +0000

    Allow more tags in kses. Props lloydbudd. fixes #5617
    git-svn-id: http://svn.automattic.com/wordpress/trunk@6617 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/kses.php
+++ wp-includes/kses.php
@@ -21,195 +21,262 @@ if (!CUSTOM_TAGS) {
 	$allowedposttags = array(
 		'address' => array(),
 		'a' => array(
-			'href' => array(), 'title' => array(),
-			'rel' => array(), 'rev' => array(),
-			'name' => array()
-			),
+			'class' => array (),
+			'href' => array (),
+			'id' => array (),
+			'title' => array (),
+			'rel' => array (),
+			'rev' => array (),
+			'name' => array (),
+			'target' => array()),
 		'abbr' => array(
-			'title' => array(), 'class' => array()
-			),
+			'class' => array (),
+			'title' => array ()),
 		'acronym' => array(
-			'title' => array()
-			),
+			'title' => array ()),
 		'b' => array(),
 		'big' => array(),
 		'blockquote' => array(
-			'cite' => array(), 'xml:lang' => array(),
-			'lang' => array()
-			),
-		'br' => array(),
+			'id' => array (),
+			'cite' => array (),
+			'class' => array(),
+			'lang' => array(),
+			'xml:lang' => array()),
+		'br' => array (
+			'class' => array ()),
 		'button' => array(
-			'disabled' => array(), 'name' => array(),
-			'type' => array(), 'value' => array()
-			),
+			'disabled' => array (),
+			'name' => array (),
+			'type' => array (),
+			'value' => array ()),
 		'caption' => array(
-			'align' => array()
-			),
-		'code' => array(),
+			'align' => array (),
+			'class' => array ()),
+		'cite' => array (
+			'class' => array(),
+			'dir' => array(),
+			'lang' => array(),
+			'title' => array ()),
+		'code' => array (
+			'style' => array()),
 		'col' => array(
-			'align' => array(), 'char' => array(),
-			'charoff' => array(), 'span' => array(),
-			'valign' => array(), 'width' => array()
-			),
+			'align' => array (),
+			'char' => array (),
+			'charoff' => array (),
+			'span' => array (),
+			'dir' => array(),
+			'style' => array (),
+			'valign' => array (),
+			'width' => array ()),
 		'del' => array(
-			'datetime' => array()
-			),
+			'datetime' => array ()),
 		'dd' => array(),
 		'div' => array(
-			'align' => array(), 'xml:lang' => array(),
-			'lang' => array()
-			),
+			'align' => array (),
+			'class' => array (),
+			'dir' => array (),
+			'lang' => array(),
+			'style' => array (),
+			'xml:lang' => array()),
 		'dl' => array(),
 		'dt' => array(),
 		'em' => array(),
 		'fieldset' => array(),
 		'font' => array(
-			'color' => array(), 'face' => array(),
-			'size' => array()
-			),
+			'color' => array (),
+			'face' => array (),
+			'size' => array ()),
 		'form' => array(
-			'action' => array(), 'accept' => array(),
-			'accept-charset' => array(), 'enctype' => array(),
-			'method' => array(), 'name' => array(),
-			'target' => array()
-			),
+			'action' => array (),
+			'accept' => array (),
+			'accept-charset' => array (),
+			'enctype' => array (),
+			'method' => array (),
+			'name' => array (),
+			'target' => array ()),
 		'h1' => array(
-			'align' => array()
-			),
+			'align' => array (),
+			'class' => array ()),
 		'h2' => array(
-			'align' => array()
-			),
+			'align' => array (),
+			'class' => array ()),
 		'h3' => array(
-			'align' => array()
-			),
+			'align' => array (),
+			'class' => array ()),
 		'h4' => array(
-			'align' => array()
-			),
+			'align' => array (),
+			'class' => array ()),
 		'h5' => array(
-			'align' => array()
-			),
+			'align' => array (),
+			'class' => array ()),
 		'h6' => array(
-			'align' => array()
-			),
+			'align' => array (),
+			'class' => array ()),
 		'hr' => array(
-			'align' => array(), 'noshade' => array(),
-			'size' => array(), 'width' => array()
-			),
+			'align' => array (),
+			'class' => array (),
+			'noshade' => array (),
+			'size' => array (),
+			'width' => array ()),
 		'i' => array(),
 		'img' => array(
-			'alt' => array(), 'align' => array(),
-			'border' => array(), 'height' => array(),
-			'hspace' => array(), 'longdesc' => array(),
-			'vspace' => array(), 'src' => array(),
-			'width' => array()
-			),
+			'alt' => array (),
+			'align' => array (),
+			'border' => array (),
+			'class' => array (),
+			'height' => array (),
+			'hspace' => array (),
+			'longdesc' => array (),
+			'vspace' => array (),
+			'src' => array (),
+			'style' => array (),
+			'width' => array ()),
 		'ins' => array(
-			'datetime' => array(), 'cite' => array()
-			),
+			'datetime' => array (),
+			'cite' => array ()),
 		'kbd' => array(),
 		'label' => array(
-			'for' => array()
-			),
+			'for' => array ()),
 		'legend' => array(
-			'align' => array()
-			),
-		'li' => array(),
+			'align' => array ()),
+		'li' => array (
+			'align' => array (),
+			'class' => array ()),
 		'p' => array(
-			'align' => array(), 'xml:lang' => array(),
-			'lang' => array()
-			),
+			'class' => array (),
+			'align' => array (),
+			'dir' => array(),
+			'lang' => array(),
+			'style' => array (),
+			'xml:lang' => array()),
 		'pre' => array(
-			'width' => array()
-			),
+			'style' => array(),
+			'width' => array ()),
 		'q' => array(
-			'cite' => array()
-			),
+			'cite' => array ()),
 		's' => array(),
+		'span' => array (
+			'class' => array (),
+			'dir' => array (),
+			'align' => array (),
+			'style' => array (),
+			'title' => array ()),
 		'strike' => array(),
 		'strong' => array(),
 		'sub' => array(),
 		'sup' => array(),
 		'table' => array(
-			'align' => array(), 'bgcolor' => array(),
-			'border' => array(), 'cellpadding' => array(),
-			'cellspacing' => array(), 'rules' => array(),
-			'summary' => array(), 'width' => array()
-			),
+			'align' => array (),
+			'bgcolor' => array (),
+			'border' => array (),
+			'cellpadding' => array (),
+			'cellspacing' => array (),
+			'class' => array (),
+			'dir' => array(),
+			'id' => array(),
+			'rules' => array (),
+			'style' => array (),
+			'summary' => array (),
+			'width' => array ()),
 		'tbody' => array(
-			'align' => array(), 'char' => array(),
-			'charoff' => array(), 'valign' => array()
-			),
+			'align' => array (),
+			'char' => array (),
+			'charoff' => array (),
+			'valign' => array ()),
 		'td' => array(
-			'abbr' => array(), 'align' => array(),
-			'axis' => array(), 'bgcolor' => array(),
-			'char' => array(), 'charoff' => array(),
-			'colspan' => array(), 'headers' => array(),
-			'height' => array(), 'nowrap' => array(),
-			'rowspan' => array(), 'scope' => array(),
-			'valign' => array(), 'width' => array()
-			),
+			'abbr' => array (),
+			'align' => array (),
+			'axis' => array (),
+			'bgcolor' => array (),
+			'char' => array (),
+			'charoff' => array (),
+			'class' => array (),
+			'colspan' => array (),
+			'dir' => array(),
+			'headers' => array (),
+			'height' => array (),
+			'nowrap' => array (),
+			'rowspan' => array (),
+			'scope' => array (),
+			'style' => array (),
+			'valign' => array (),
+			'width' => array ()),
 		'textarea' => array(
-			'cols' => array(), 'rows' => array(),
-			'disabled' => array(), 'name' => array(),
-			'readonly' => array()
-			),
+			'cols' => array (),
+			'rows' => array (),
+			'disabled' => array (),
+			'name' => array (),
+			'readonly' => array ()),
 		'tfoot' => array(
-			'align' => array(), 'char' => array(),
-			'charoff' => array(), 'valign' => array()
-			),
+			'align' => array (),
+			'char' => array (),
+			'class' => array (),
+			'charoff' => array (),
+			'valign' => array ()),
 		'th' => array(
-			'abbr' => array(), 'align' => array(),
-			'axis' => array(), 'bgcolor' => array(),
-			'char' => array(), 'charoff' => array(),
-			'colspan' => array(), 'headers' => array(),
-			'height' => array(), 'nowrap' => array(),
-			'rowspan' => array(), 'scope' => array(),
-			'valign' => array(), 'width' => array()
-			),
+			'abbr' => array (),
+			'align' => array (),
+			'axis' => array (),
+			'bgcolor' => array (),
+			'char' => array (),
+			'charoff' => array (),
+			'class' => array (),
+			'colspan' => array (),
+			'headers' => array (),
+			'height' => array (),
+			'nowrap' => array (),
+			'rowspan' => array (),
+			'scope' => array (),
+			'valign' => array (),
+			'width' => array ()),
 		'thead' => array(
-			'align' => array(), 'char' => array(),
-			'charoff' => array(), 'valign' => array()
-			),
+			'align' => array (),
+			'char' => array (),
+			'charoff' => array (),
+			'class' => array (),
+			'valign' => array ()),
 		'title' => array(),
 		'tr' => array(
-			'align' => array(), 'bgcolor' => array(),
-			'char' => array(), 'charoff' => array(),
-			'valign' => array()
-			),
+			'align' => array (),
+			'bgcolor' => array (),
+			'char' => array (),
+			'charoff' => array (),
+			'class' => array (),
+			'style' => array (),
+			'valign' => array ()),
 		'tt' => array(),
 		'u' => array(),
-		'ul' => array(),
-		'ol' => array(),
-		'var' => array()
-	);
-
+		'ul' => array (
+			'class' => array ()),
+		'ol' => array (
+			'class' => array ()),
+		'var' => array ());
 	$allowedtags = array(
 		'a' => array(
-			'href' => array(), 'title' => array()
-			),
+			'href' => array (),
+			'title' => array ()),
 		'abbr' => array(
-			'title' => array()
-			),
+			'title' => array ()),
 		'acronym' => array(
-			'title' => array()
-			),
+			'title' => array ()),
 		'b' => array(),
 		'blockquote' => array(
-			'cite' => array()
-			),
+			'cite' => array ()),
 		//	'br' => array(),
+		'cite' => array (),
 		'code' => array(),
-		//	'del' => array('datetime' => array()),
+		'del' => array(
+			'datetime' => array ()),
 		//	'dd' => array(),
 		//	'dl' => array(),
 		//	'dt' => array(),
-		'em' => array(),
-		'i' => array(),
+		'em' => array (), 'i' => array (),
 		//	'ins' => array('datetime' => array(), 'cite' => array()),
 		//	'li' => array(),
 		//	'ol' => array(),
 		//	'p' => array(),
-		//	'q' => array(),
+		'q' => array(
+			'cite' => array ()),
 		'strike' => array(),
 		'strong' => array(),
 		//	'sub' => array(),
