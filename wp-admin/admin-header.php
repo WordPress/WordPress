60ae3bf7666a2871444f627168cabb48e5b576eb
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-02-15 06:46:54 +0000

    Fix postbox layout for IE. Props hansengel. fixes #5812
    git-svn-id: http://svn.automattic.com/wordpress/trunk@6857 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -18,6 +18,10 @@ get_admin_page_title();
 wp_admin_css( 'css/global' );
 wp_admin_css();
 ?>
+<!--[if gte IE 6]>
+<?php wp_admin_css( 'css/ie' );
+?>
+<!endif-->
 <script type="text/javascript">
 //<![CDATA[
 addLoadEvent=(function(){var e=[],t,s,n,i,o,d=document,w=window,r='readyState',c='onreadystatechange',x=function(){n=1;clearInterval(t);while(i=e.shift())i();if(s)s[c]=''};return function(f){if(n)return f();if(!e[0]){d.addEventListener&&d.addEventListener("DOMContentLoaded",x,false);/*@cc_on@*//*@if(@_win32)d.write("<script id=__ie_onload defer src=//0><\/scr"+"ipt>");s=d.getElementById("__ie_onload");s[c]=function(){s[r]=="complete"&&x()};/*@end@*/if(/WebKit/i.test(navigator.userAgent))t=setInterval(function(){/loaded|complete/.test(d[r])&&x()},10);o=w.onload;w.onload=function(){x();o&&o()}}e.push(f)}})();//]]>
