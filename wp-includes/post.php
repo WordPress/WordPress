2f7d3a377eb930330c216834064056516f07c65f
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-02-21 00:52:51 +0000

    Add a default for post_conten_filtered
    git-svn-id: http://svn.automattic.com/wordpress/trunk@6951 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -1121,7 +1121,7 @@ function wp_insert_post($postarr = array()) {
 	$defaults = array('post_status' => 'draft', 'post_type' => 'post', 'post_author' => $user_ID,
 		'ping_status' => get_option('default_ping_status'), 'post_parent' => 0,
 		'menu_order' => 0, 'to_ping' =>  '', 'pinged' => '', 'post_password' => '',
-		'guid' => '');
+		'guid' => '', 'post_content_filtered' => '');
 
 	$postarr = wp_parse_args($postarr, $defaults);
 	$postarr = sanitize_post($postarr, 'db');
