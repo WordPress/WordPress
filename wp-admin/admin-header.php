c0dd0a503b97a6a513e8ef4cd2bf5231f5b83a74
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-02-24 19:37:58 +0000

    Localize help and forum links. Props SteveAgl. fixes #5983
    git-svn-id: http://svn.automattic.com/wordpress/trunk@7004 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -61,7 +61,7 @@ do_action('admin_head');
 <div id="wphead">
 <h1><?php bloginfo('name'); ?><span id="viewsite"><a href="<?php echo get_option('home') . '/'; ?>"><?php _e('Visit Site') ?></a></span></h1>
 </div>
-<div id="user_info"><p><?php printf(__('Howdy, <a href="%1$s">%2$s</a>!'), 'profile.php', $user_identity) ?> | <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="<?php _e('Log Out') ?>"><?php _e('Log Out'); ?></a> | <?php printf(__('<a href="%s">Help</a>'), 'http://codex.wordpress.org/') ?> | <?php printf(__('<a href="%s">Forums</a>'), 'http://wordpress.org/support/') ?></p></div>
+<div id="user_info"><p><?php printf(__('Howdy, <a href="%1$s">%2$s</a>!'), 'profile.php', $user_identity) ?> | <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="<?php _e('Log Out') ?>"><?php _e('Log Out'); ?></a> | <?php _e('<a href="http://codex.wordpress.org/">Help</a>') ?> | <?php _e('<a href="http://wordpress.org/support/">Forums</a>') ?></p></div>
 
 <?php
 require(ABSPATH . 'wp-admin/menu-header.php');
