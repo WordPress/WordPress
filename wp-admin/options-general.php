37c69acd2d428b03ca1a5d821434a380d7300317
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-02-29 19:00:52 +0000

    Hardcode offset range since the step argument to range is PHP 5 only.  fixes #6047
    git-svn-id: http://svn.automattic.com/wordpress/trunk@7108 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options-general.php
+++ wp-admin/options-general.php
@@ -59,7 +59,9 @@ include('./admin-header.php');
 <select name="gmt_offset">
 <?php 
 $current_offset = get_option('gmt_offset');
-foreach ( range(-12, 12, 0.5) as $offset ) {
+$offset_range = array (-12, -11.5, -11, -10.5, -10, -9.5, -9, -8.5, -8, -7.5, -7, -6.5, -6, -5.5, -5, -4.5, -4, -3.5, -3, -2.5, -2, -1.5, -1, -0.5,
+	0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12);
+foreach ( $offset_range as $offset ) {
 	if ( 0 < $offset )
 		$offset_name = '+' . $offset;
 	elseif ( 0 == $offset )
