1567bfb1954bb5ae1f751cfb60a334219695b6e3
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-03-13 20:39:56 +0000

    Move the restrict_manage_post hook back to a useful place. Fixes #6196.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@7274 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit.php
+++ wp-admin/edit.php
@@ -120,8 +120,6 @@ endif;
 	<input type="submit" value="<?php _e( 'Search Posts' ); ?>" class="button" />
 </p>
 
-<?php do_action('restrict_manage_posts'); ?>
-
 <div class="tablenav">
 
 <?php
@@ -170,7 +168,9 @@ foreach ($arc_result as $arc_row) {
 <?php } ?>
 
 <?php wp_dropdown_categories('show_option_all='.__('View all categories').'&hide_empty=1&hierarchical=1&show_count=1&selected='.$cat);?>
+<?php do_action('restrict_manage_posts'); ?>
 <input type="submit" id="post-query-submit" value="<?php _e('Filter'); ?>" class="button-secondary" />
+
 <?php } ?>
 </div>
 
