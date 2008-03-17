c7d6a001d06d532f77e30432b8e576685c6416a7
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-03-17 23:16:10 +0000

    Avatar support for classic theme
    git-svn-id: http://svn.automattic.com/wordpress/trunk@7361 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/classic/comments.php
+++ wp-content/themes/classic/comments.php
@@ -13,6 +13,7 @@
 
 <?php foreach ($comments as $comment) : ?>
 	<li id="comment-<?php comment_ID() ?>">
+	<?php echo get_avatar( $comment, 32 ); ?>
 	<?php comment_text() ?>
 	<p><cite><?php comment_type(__('Comment'), __('Trackback'), __('Pingback')); ?> <?php _e('by'); ?> <?php comment_author_link() ?> &#8212; <?php comment_date() ?> @ <a href="#comment-<?php comment_ID() ?>"><?php comment_time() ?></a></cite> <?php edit_comment_link(__("Edit This"), ' |'); ?></p>
 	</li>
