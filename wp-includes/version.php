8427f15bca26d9ba7b3169f1dcbe4e3f2b3aa3f0
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-03-18 07:01:01 +0000

    Buhbump
    git-svn-id: http://svn.automattic.com/wordpress/trunk@7379 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '2.5-beta1';
+$wp_version = '2.5-RC1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB scheme
