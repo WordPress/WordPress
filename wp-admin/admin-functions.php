075b9e764e1717a55159292efa148b9ed0b97792
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-03-21 09:29:22 +0000

    Mark files as deprecated. See #4960.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@7448 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-functions.php
+++ wp-admin/admin-functions.php
@@ -1,4 +1,5 @@
 <?php
 // Deprecated.  Use includes/admin.php.
+_deprecated_file( basename(__FILE__), '2.5', 'wp-admin/includes/admin.php' );
 require_once(ABSPATH . 'wp-admin/includes/admin.php');
 ?>
--- wp-admin/upgrade-functions.php
+++ wp-admin/upgrade-functions.php
@@ -1,4 +1,5 @@
 <?php
 // Deprecated.  Use includes/upgrade.php.
+_deprecated_file( basename(__FILE__), '2.5', 'wp-admin/includes/upgrade.php' );
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 ?>
