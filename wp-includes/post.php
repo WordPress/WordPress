31d303ea1b25c99f1c9da2340b785ccedb00e7ed
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-03-23 00:58:47 +0000

    Add default for post_excerpt. see #6355
    git-svn-id: http://svn.automattic.com/wordpress/trunk@7482 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -1155,7 +1155,7 @@ function wp_insert_post($postarr = array()) {
 	$defaults = array('post_status' => 'draft', 'post_type' => 'post', 'post_author' => $user_ID,
 		'ping_status' => get_option('default_ping_status'), 'post_parent' => 0,
 		'menu_order' => 0, 'to_ping' =>  '', 'pinged' => '', 'post_password' => '',
-		'guid' => '', 'post_content_filtered' => '');
+		'guid' => '', 'post_content_filtered' => '', 'post_excerpt' => '');
 
 	$postarr = wp_parse_args($postarr, $defaults);
 	$postarr = sanitize_post($postarr, 'db');
