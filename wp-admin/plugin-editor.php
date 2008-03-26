6a700defeba9c3941f4ccebd0cdb3f5966df36fc
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-03-26 16:14:29 +0000

    Fix plugin error scraping in the plugin editor. Props DD32. fixes #6332
    git-svn-id: http://svn.automattic.com/wordpress/trunk@7525 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/plugin-editor.php
+++ wp-admin/plugin-editor.php
@@ -14,9 +14,8 @@ function theme_editor_css(){
 $plugins = get_plugins();
 $plugin_files = array_keys($plugins);
 
-if (empty($file)) {
+if (empty($file))
 	$file = $plugin_files[0];
-}
 
 $file = validate_file_to_edit($file, $plugin_files);
 $real_file = get_real_file_to_edit( PLUGINDIR . "/$file");
@@ -31,28 +30,23 @@ case 'update':
 		wp_die('<p>'.__('You do not have sufficient permissions to edit templates for this blog.').'</p>');
 
 	$newcontent = stripslashes($_POST['newcontent']);
-	if (is_writeable($real_file)) {
+	if ( is_writeable($real_file) ) {
 		$f = fopen($real_file, 'w+');
 		fwrite($f, $newcontent);
 		fclose($f);
 
 		// Deactivate so we can test it.
-		$current = get_option('active_plugins');
-		if ( in_array($file, $current) || isset($_POST['phperror']) ) {
-			if ( in_array($file, $current) ) {
-				if ( ( $key = array_search( $plugin, $current) ) !== false )
-					array_splice($current, $key, 1 ); // Fixed Array-fu!
-				update_option('active_plugins', $current);
-			}
+		if ( is_plugin_active($file) || isset($_POST['phperror']) ) {
+			if ( is_plugin_active($file) )
+				deactivate_plugins($file, true);
 			wp_redirect(add_query_arg('_wpnonce', wp_create_nonce('edit-plugin-test_' . $file), "plugin-editor.php?file=$file&liveupdate=1"));
-			exit();
+			exit;
 		}
 		wp_redirect("plugin-editor.php?file=$file&a=te");
 	} else {
 		wp_redirect("plugin-editor.php?file=$file");
 	}
-
-	exit();
+	exit;
 
 break;
 
@@ -61,42 +55,40 @@ default:
 	if ( !current_user_can('edit_plugins') )
 		wp_die('<p>'.__('You do not have sufficient permissions to edit plugins for this blog.').'</p>');
 
-	if ( $_GET['liveupdate'] ) {
+	if ( isset($_GET['liveupdate']) ) {
 		check_admin_referer('edit-plugin-test_' . $file);
-		$current = get_option('active_plugins');
-		$plugin = $file;
-		if ( validate_file($plugin) )
-			wp_die(__('Invalid plugin.'));
-		if ( ! file_exists(ABSPATH . PLUGINDIR . '/' . $plugin) )
-			wp_die(__('Plugin file does not exist.'));
-		if (!in_array($plugin, $current)) {
-			wp_redirect("plugin-editor.php?file=$file&phperror=1"); // we'll override this later if the plugin can be included without fatal error
-			@include(ABSPATH . PLUGINDIR . '/' . $plugin);
-			$current[] = $plugin;
-			sort($current);
-			update_option('active_plugins', $current);
-		}
+
+		$error = validate_plugin($file);
+		if( is_wp_error($error) )
+			wp_die( $error );
+
+		if ( ! is_plugin_active($file) )
+			activate_plugin($file, "plugin-editor.php?file=$file&phperror=1");// we'll override this later if the plugin can be included without fatal error
+
 		wp_redirect("plugin-editor.php?file=$file&a=te");
+		exit;
 	}
 
 	require_once('admin-header.php');
 
 	update_recently_edited(PLUGINDIR . "/$file");
 
-	if (!is_file($real_file))
+	if ( ! is_file($real_file) )
 		$error = 1;
 
-	if (!$error) {
-		$f = fopen($real_file, 'r');
-		$content = fread($f, filesize($real_file));
-		$content = htmlspecialchars($content);
-	}
+	if ( ! $error )
+		$content = htmlspecialchars(file_get_contents($real_file));
 
 	?>
 <?php if (isset($_GET['a'])) : ?>
  <div id="message" class="updated fade"><p><?php _e('File edited successfully.') ?></p></div>
 <?php elseif (isset($_GET['phperror'])) : ?>
- <div id="message" class="updated fade"><p><?php _e('This plugin has been deactivated because your changes resulted in a <strong>fatal error</strong>.') ?></p></div>
+ <div id="message" class="updated fade"><p><?php _e('This plugin has been deactivated because your changes resulted in a <strong>fatal error</strong>.') ?></p>
+	<?php
+		if ( wp_verify_nonce($_GET['_error_nonce'], 'plugin-activation-error_' . $file) ) { ?>
+	<iframe style="border:0" width="100%" height="70px" src="<?php bloginfo('wpurl'); ?>/wp-admin/plugins.php?action=error_scrape&amp;plugin=<?php echo attribute_escape($file); ?>&amp;_wpnonce=<?php echo attribute_escape($_GET['_error_nonce']); ?>"></iframe>
+	<?php } ?>
+</div>
 <?php endif; ?>
  <div class="wrap">
 <div class="bordertitle">
@@ -105,18 +97,16 @@ default:
 <div class="tablenav">
 <div class="alignleft">
 <big><strong><?php
-	if ( in_array($file, (array) get_option('active_plugins')) ) {
-		if (is_writeable($real_file)) {
+	if ( is_plugin_active($file) ) {
+		if ( is_writeable($real_file) )
 			echo sprintf(__('Editing <strong>%s</strong> (active)'), $file);
-		} else {
-		echo sprintf(__('Browsing <strong>%s</strong> (active)'), $file);
-		}
+		else
+			echo sprintf(__('Browsing <strong>%s</strong> (active)'), $file);
 	} else {
-		if (is_writeable($real_file)) {
+		if ( is_writeable($real_file) )
 			echo sprintf(__('Editing <strong>%s</strong> (inactive)'), $file);
-		} else {
-		echo sprintf(__('Browsing <strong>%s</strong> (inactive)'), $file);
-		}
+		else
+			echo sprintf(__('Browsing <strong>%s</strong> (inactive)'), $file);
 	}
 	?></strong></big>
 </div>
@@ -124,16 +114,16 @@ default:
 </div>
 <br class="clear" />
 	<div id="templateside">
-	<h3 id="bordertitle"><?php _e("Plugin Files"); ?></h3>
+	<h3 id="bordertitle"><?php _e('Plugin Files'); ?></h3>
 
 	<h4><?php _e('Plugins'); ?></h4>
 	<ul>
 <?php foreach($plugin_files as $plugin_file) : ?>
-		<li><a href="plugin-editor.php?file=<?php echo "$plugin_file"; ?>"><?php echo $plugins[$plugin_file]['Name']; ?></a></li>
+		<li><a href="plugin-editor.php?file=<?php echo $plugin_file; ?>"><?php echo $plugins[$plugin_file]['Name']; ?></a></li>
 <?php endforeach; ?>
 	</ul>
 	</div>
-<?php	if (!$error) { ?>
+<?php	if ( ! $error ) { ?>
 	<form name="template" id="template" action="plugin-editor.php" method="post">
 	<?php wp_nonce_field('edit-plugin_' . $file) ?>
 		<div><textarea cols="70" rows="25" name="newcontent" id="newcontent" tabindex="1"><?php echo $content ?></textarea>
@@ -164,6 +154,6 @@ default:
 <div class="clear"> &nbsp; </div>
 </div>
 <?php
-break;
+	break;
 }
 include("admin-footer.php") ?>
