fe65f84555b579aa2ea332d0afe599b726302ec1
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-03-30 21:13:52 +0000

    Mark the branch as what it is.
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.5@7580 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '2.5';
+$wp_version = '2.5.1-alpha';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB scheme
