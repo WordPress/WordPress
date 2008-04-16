957a61318bc04315e85b24affaf90a31f274d688
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-04-16 23:09:51 +0000

    array_shift() error fixes from DD32. fixes #6499 for trunk
    git-svn-id: http://svn.automattic.com/wordpress/trunk@7713 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/widgets.php
+++ wp-admin/widgets.php
@@ -18,7 +18,7 @@ if ( isset($_GET['sidebar']) && isset($wp_registered_sidebars[$_GET['sidebar']])
 	$sidebar = attribute_escape( $_GET['sidebar'] );
 } elseif ( is_array($wp_registered_sidebars) && !empty($wp_registered_sidebars) ) {
 	// By default we look at the first defined sidebar
-	$sidebar = array_shift( array_keys($wp_registered_sidebars) );
+	$sidebar = array_shift( $keys = array_keys($wp_registered_sidebars) );
 } else {
 	// If no sidebars, die.
 	require_once( 'admin-header.php' );
