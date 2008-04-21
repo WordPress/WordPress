d46ca509a464711f1268acae4f1f935c0b9f4b1a
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-04-21 17:35:57 +0000

    Check if PATH_INFO is set to avoid undefined index errors. see #5160
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.5@7758 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-settings.php
+++ wp-settings.php
@@ -69,10 +69,12 @@ if ( empty( $_SERVER['REQUEST_URI'] ) ) {
 	else
 	{
 		// Some IIS + PHP configurations puts the script-name in the path-info (No need to append it twice)
-		if ( $_SERVER['PATH_INFO'] == $_SERVER['SCRIPT_NAME'] )
-			$_SERVER['REQUEST_URI'] = $_SERVER['PATH_INFO'];
-		else
-			$_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'] . $_SERVER['PATH_INFO'];
+		if ( isset($_SERVER['PATH_INFO']) ) {
+			if ( $_SERVER['PATH_INFO'] == $_SERVER['SCRIPT_NAME'] )
+				$_SERVER['REQUEST_URI'] = $_SERVER['PATH_INFO'];
+			else
+				$_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'] . $_SERVER['PATH_INFO'];
+		}
 
 		// Append the query string if it exists and isn't null
 		if (isset($_SERVER['QUERY_STRING']) && !empty($_SERVER['QUERY_STRING'])) {
