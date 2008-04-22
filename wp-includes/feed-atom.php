6f22f9fa1e12771046a8f9c16bd96ac37214ad9c
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-04-22 21:16:25 +0000

    Typo fix from peaceablewhale.  fixes #6579 for 2.5
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.5@7771 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/feed-atom.php
+++ wp-includes/feed-atom.php
@@ -49,7 +49,7 @@ $more = 1;
 <?php atom_enclosure(); ?>
 <?php do_action('atom_entry'); ?>
 		<link rel="replies" type="text/html" href="<?php the_permalink_rss() ?>#comments" thr:count="<?php echo get_comments_number()?>"/>
-		<link rel="replies" type="appication/atom+xml" href="<?php echo get_post_comments_feed_link(0,'atom') ?>" thr:count="<?php echo get_comments_number()?>"/>
+		<link rel="replies" type="application/atom+xml" href="<?php echo get_post_comments_feed_link(0,'atom') ?>" thr:count="<?php echo get_comments_number()?>"/>
 		<thr:total><?php echo get_comments_number()?></thr:total>
 	</entry>
 	<?php endwhile ; ?>
