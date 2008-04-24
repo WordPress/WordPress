72e90ed56bd33cbd608437a0f43a563a9fb995ed
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-04-24 05:45:34 +0000

    Bumpity bump bump
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.5@7806 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '2.5.1-alpha';
+$wp_version = '2.5.1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB scheme
