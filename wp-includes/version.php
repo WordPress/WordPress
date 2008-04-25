abaf6b5f7cf8b6534a2d6b805d3d0997e7729385
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-04-25 06:29:37 +0000

    Still polishing 2.5.1, so let's unbump so our "for reals, this time" 2.5.1 bump matches the zip
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.5@7824 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '2.5.1';
+$wp_version = '2.5.1-beta';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB scheme
