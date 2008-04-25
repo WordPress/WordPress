b5f095aaf1a955596418d6dca991a52681a20d97
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-04-25 15:40:25 +0000

    Bump
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.5@7834 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '2.5.1-beta';
+$wp_version = '2.5.1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB scheme
