847413f7781113b574a3db50bef8d5f1fbba6f6f
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-04-30 20:12:54 +0000

    send_to_editor fix for FF. Props azaozz. fixes #6864 for 2.5
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.5@7861 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/media-upload.js
+++ wp-admin/js/media-upload.js
@@ -1,8 +1,7 @@
 // send html to the post editor
 function send_to_editor(h) {
-	var win = window.opener ? window.opener : window.dialogArguments;
-	if ( !win )
-		win = top;
+	var win = window.dialogArguments || opener || parent || top;
+
 	tinyMCE = win.tinyMCE;
 	if ( typeof tinyMCE != 'undefined' && ( ed = tinyMCE.getInstanceById('content') ) && !ed.isHidden() ) {
 		tinyMCE.selectedInstance.getWin().focus();
