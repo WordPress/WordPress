5322be300a27460ae28fb79772d55f946179c84c
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-05-25 20:33:13 +0000

    File file level phpdoc from jacobsantos. see #7037
    git-svn-id: http://svn.automattic.com/wordpress/trunk@7992 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 /**
- * Front to the WordPress application. Most of WordPress is loaded through this
- * file. This file doesn't do anything, but loads the file which does and tells
- * WordPress to load the theme.
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
  * @package WordPress
  */
--- wp-login.php
+++ wp-login.php
@@ -1,5 +1,9 @@
 <?php
 /**
+ * WordPress User Page
+ *
+ * Handles authentication, registering, resetting passwords, forgot password,
+ * and other user handling.
  *
  * @package WordPress
  */
