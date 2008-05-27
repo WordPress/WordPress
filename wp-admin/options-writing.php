67c2a2aced7c1979adb18cba2166bdd8f1d41232
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-05-27 11:48:07 +0000

    Better random email addresses. Props nbachiyski. fixes #7020
    git-svn-id: http://svn.automattic.com/wordpress/trunk@7997 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options-writing.php
+++ wp-admin/options-writing.php
@@ -58,7 +58,7 @@ endforeach;
 </table>
 
 <h3><?php _e('Post via e-mail') ?></h3>
-<p><?php printf(__('To post to WordPress by e-mail you must set up a secret e-mail account with POP3 access. Any mail received at this address will be posted, so it&#8217;s a good idea to keep this address very secret. Here are three random strings you could use: <code>%s</code>, <code>%s</code>, <code>%s</code>.'), wp_generate_password(), wp_generate_password(), wp_generate_password()) ?></p>
+<p><?php printf(__('To post to WordPress by e-mail you must set up a secret e-mail account with POP3 access. Any mail received at this address will be posted, so it&#8217;s a good idea to keep this address very secret. Here are three random strings you could use: <code>%s</code>, <code>%s</code>, <code>%s</code>.'), wp_generate_password(8, false), wp_generate_password(8, false), wp_generate_password(8, false)) ?></p>
 
 <table class="form-table">
 <tr valign="top">
