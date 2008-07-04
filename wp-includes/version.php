e38b2bcaa8d9ac58dbdc0b5f8dbf07f445ad4723
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-07-04 06:12:24 +0000

    Bumpage
    git-svn-id: http://svn.automattic.com/wordpress/trunk@8254 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '2.6-beta2';
+$wp_version = '2.6-beta3';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
