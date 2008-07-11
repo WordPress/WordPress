e7e42b56d5a1028d4d359e0ed7a89f52bad7fd02
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-07-11 22:04:03 +0000

    Run plugin checks both admin and blog side.  Props DD32 and mdawaffe. fixes #7265
    git-svn-id: http://svn.automattic.com/wordpress/trunk@8317 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/update.php
+++ wp-includes/update.php
@@ -10,7 +10,7 @@
  * wp_version_check() - Check WordPress version against the newest version.
  *
  * The WordPress version, PHP version, and Locale is sent. Checks against the WordPress server at
- * api.wordpress.org server. Will only check if PHP has fsockopen enabled and WordPress isn't installing.
+ * api.wordpress.org. Will only check if PHP has fsockopen enabled and WordPress isn't installing.
  *
  * @package WordPress
  * @since 2.3
@@ -19,7 +19,7 @@
  * @return mixed Returns null if update is unsupported. Returns false if check is too soon.
  */
 function wp_version_check() {
-	if ( !function_exists('fsockopen') || strpos($_SERVER['PHP_SELF'], 'install.php') !== false || defined('WP_INSTALLING') )
+	if ( !function_exists('fsockopen') || defined('WP_INSTALLING') )
 		return;
 
 	global $wp_version;
@@ -69,7 +69,94 @@ function wp_version_check() {
 	}
 	update_option( 'update_core', $new_option );
 }
-
 add_action( 'init', 'wp_version_check' );
+
+/**
+ * wp_update_plugins() - Check plugin versions against the latest versions hosted on WordPress.org.
+ *
+ * The WordPress version, PHP version, and Locale is sent along with a list of all plugins installed.
+ * Checks against the WordPress server at api.wordpress.org.
+ * Will only check if PHP has fsockopen enabled and WordPress isn't installing.
+ *
+ * @package WordPress
+ * @since 2.3
+ * @uses $wp_version Used to notidy the WordPress version.
+ *
+ * @return mixed Returns null if update is unsupported. Returns false if check is too soon.
+ */
+function wp_update_plugins() {
+	global $wp_version;
+
+	if ( !function_exists('fsockopen') || defined('WP_INSTALLING') )
+		return false;
+
+	$current = get_option( 'update_plugins' );
+
+	$time_not_changed = isset( $current->last_checked ) && 43200 > ( time() - $current->last_checked );
+
+	// If running blog-side, bail unless we've not checked in the last 12 hours
+	if ( !function_exists( 'get_plugins' ) ) {
+		if ( $time_not_changed )
+			return false;
+		require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	}
+
+	$plugins = get_plugins();
+	$active  = get_option( 'active_plugins' );
+
+	$new_option = '';
+	$new_option->last_checked = time();
+
+	$plugin_changed = false;
+	foreach ( $plugins as $file => $p ) {
+		$new_option->checked[ $file ] = $p['Version'];
+
+		if ( !isset( $current->checked[ $file ] ) ) {
+			$plugin_changed = true;
+			continue;
+		}
+
+		if ( strval($current->checked[ $file ]) !== strval($p['Version']) )
+			$plugin_changed = true;
+	}
+
+	// Bail if we've checked in the last 12 hours and if nothing has changed
+	if ( $time_not_changed && !$plugin_changed )
+		return false;
+
+	$to_send->plugins = $plugins;
+	$to_send->active = $active;
+	$send = serialize( $to_send );
+
+	$request = 'plugins=' . urlencode( $send );
+	$http_request  = "POST /plugins/update-check/1.0/ HTTP/1.0\r\n";
+	$http_request .= "Host: api.wordpress.org\r\n";
+	$http_request .= "Content-Type: application/x-www-form-urlencoded; charset=" . get_option('blog_charset') . "\r\n";
+	$http_request .= "Content-Length: " . strlen($request) . "\r\n";
+	$http_request .= 'User-Agent: WordPress/' . $wp_version . '; ' . get_bloginfo('url') . "\r\n";
+	$http_request .= "\r\n";
+	$http_request .= $request;
+
+	$response = '';
+	if( false != ( $fs = @fsockopen( 'api.wordpress.org', 80, $errno, $errstr, 3) ) && is_resource($fs) ) {
+		fwrite($fs, $http_request);
+
+		while ( !feof($fs) )
+			$response .= fgets($fs, 1160); // One TCP-IP packet
+		fclose($fs);
+		$response = explode("\r\n\r\n", $response, 2);
+	}
+
+	$response = unserialize( $response[1] );
+
+	if ( $response )
+		$new_option->response = $response;
+
+	update_option( 'update_plugins', $new_option );
+}
+if ( defined( 'WP_ADMIN' ) && WP_ADMIN )
+	add_action( 'admin_init', 'wp_update_plugins' );
+else
+	add_action( 'init', 'wp_update_plugins' );
 
 ?>
