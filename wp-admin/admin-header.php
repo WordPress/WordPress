01b3ad2f64165e13470540df73c20ae764b35286
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-07-22 04:09:37 +0000

    Gears integration update in anticipation of Safari support. Fixes #7371
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.6@8396 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -74,10 +74,7 @@ unset($hook_suffixes, $hook_suffix);
 </div>
 
 <?php
-$gears_compat = false;
-
-if ( ($is_gecko || $is_winIE) && strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'webkit') === false ) { 
-	$gears_compat = true;
+if ( ! $is_opera ) { 
 ?>
 	<div id="gears-info-box" class="info-box" style="display:none;">
 	<img src="images/gear.png" title="Gear" alt="" class="gears-img" />
@@ -110,7 +107,7 @@ if ( ($is_gecko || $is_winIE) && strpos(strtolower($_SERVER['HTTP_USER_AGENT']),
 
 <?php } ?>
 
-<div id="user_info"><p><?php printf(__('Howdy, <a href="%1$s">%2$s</a>!'), 'profile.php', $user_identity) ?> | <a href="<?php echo site_url('wp-login.php?action=logout', 'login') ?>" title="<?php _e('Log Out') ?>"><?php _e('Log Out'); ?></a> | <?php _e('<a href="http://codex.wordpress.org/">Help</a>') ?> | <?php _e('<a href="http://wordpress.org/support/">Forums</a>'); if ( $gears_compat ) { ?> | <span id="gears-menu"><a href="#" onclick="wpGears.message(1);return false;"><?php _e('Turbo') ?></a></span><?php } ?></p></div>
+<div id="user_info"><p><?php printf(__('Howdy, <a href="%1$s">%2$s</a>!'), 'profile.php', $user_identity) ?> | <a href="<?php echo site_url('wp-login.php?action=logout', 'login') ?>" title="<?php _e('Log Out') ?>"><?php _e('Log Out'); ?></a> | <?php _e('<a href="http://codex.wordpress.org/">Help</a>') ?> | <?php _e('<a href="http://wordpress.org/support/">Forums</a>'); if ( ! $is_opera ) { ?> | <span id="gears-menu"><a href="#" onclick="wpGears.message(1);return false;"><?php _e('Turbo') ?></a></span><?php } ?></p></div>
 
 <?php
 require(ABSPATH . 'wp-admin/menu-header.php');
