a3a1f83d7f0c1df1d4d8449e5ff04fad30a31a6a
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-07-28 16:26:59 +0000

    Use ORIG_PATH_INFO if PATH_INFO is not available. fixes #7058 for trunk
    git-svn-id: http://svn.automattic.com/wordpress/trunk@8470 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-settings.php
+++ wp-settings.php
@@ -68,6 +68,10 @@ if ( empty( $_SERVER['REQUEST_URI'] ) ) {
 	}
 	else
 	{
+		// Use ORIG_PATH_INFO if there is no PATH_INFO
+		if ( !isset($_SERVER['PATH_INFO']) && isset($_SERVER['ORIG_PATH_INFO']) )
+			$_SERVER['PATH_INFO'] = $_SERVER['ORIG_PATH_INFO'];
+
 		// Some IIS + PHP configurations puts the script-name in the path-info (No need to append it twice)
 		if ( isset($_SERVER['PATH_INFO']) ) {
 			if ( $_SERVER['PATH_INFO'] == $_SERVER['SCRIPT_NAME'] )
