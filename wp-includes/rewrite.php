f43cf39f19c24c7e34a9b5a92b084fc7b63bfd78
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-08-11 20:17:07 +0000

    Do not prepend /archives/ et al to category/tag URLs unless category/tag slug is missing. fixes #7490 for trunk
    git-svn-id: http://svn.automattic.com/wordpress/trunk@8616 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rewrite.php
+++ wp-includes/rewrite.php
@@ -447,8 +447,9 @@ class WP_Rewrite {
 		}
 
 		if (empty($this->category_base))
-			$this->category_base = 'category';
-		$this->category_structure = trailingslashit( $this->front . $this->category_base );
+			$this->category_structure = trailingslashit( $this->front . 'category' );
+		else
+			$this->category_structure = trailingslashit( '/' . $this->root . $this->category_base );
 
 		$this->category_structure .= '%category%';
 
@@ -466,8 +467,9 @@ class WP_Rewrite {
 		}
 
 		if (empty($this->tag_base))
-			$this->tag_base = 'tag';
-		$this->tag_structure = trailingslashit( $this->front . $this->tag_base );
+			$this->tag_structure = trailingslashit( $this->front . 'tag' );
+		else
+			$this->tag_structure = trailingslashit( '/' . $this->root . $this->tag_base );
 
 		$this->tag_structure .= '%tag%';
 
