8ef8a05cf0f6e41ff3383e059c04a7cba8ba326b
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-08-14 23:11:30 +0000

    Revert [8409] pending discussion. see #7341
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.6@8649 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/header.php
+++ wp-content/themes/default/header.php
@@ -8,7 +8,6 @@
 
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
-<link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 <style type="text/css" media="screen">
