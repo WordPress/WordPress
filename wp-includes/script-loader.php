4bff75f6376137ce81b40b17d5887661e51a3c38
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-09-25 23:53:16 +0000

    Bump css ver
    git-svn-id: http://svn.automattic.com/wordpress/trunk@8987 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -302,7 +302,7 @@ function wp_default_styles( &$styles ) {
 
 	$rtl_styles = array( 'global', 'colors', 'dashboard', 'ie', 'install', 'login', 'media', 'theme-editor', 'upload', 'widgets', 'press-this', 'press-this-ie' );
 
-	$styles->add( 'wp-admin', '/wp-admin/wp-admin.css', array(), '20080925' );
+	$styles->add( 'wp-admin', '/wp-admin/wp-admin.css', array(), '20080926' );
 	$styles->add_data( 'wp-admin', 'rtl', '/wp-admin/rtl.css' );
 
 	$styles->add( 'ie', '/wp-admin/css/ie.css' );
