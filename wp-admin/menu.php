ae1cfca1476d8934e12ca9b743f3bfb67cebf18a
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-10-20 19:46:07 +0000

    Bring back plugin updates count. Needs styling.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@9257 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/menu.php
+++ wp-admin/menu.php
@@ -59,7 +59,9 @@ $menu[30] = array( __('Appearance'), 'switch_themes', 'themes.php', '', '', 'men
 	$submenu['themes.php'][5]  = array(__('Themes'), 'switch_themes', 'themes.php');
 	$submenu['themes.php'][10] = array(__('Editor'), 'edit_themes', 'theme-editor.php');
 
-$menu[35] = array( __('Plugins'), 'activate_plugins', 'plugins.php', '', '', 'menu-plugins', 'images/menu/plugins.png' );
+$update_plugins = get_option( 'update_plugins' );
+$update_count = count( $update_plugins->response );	
+$menu[35] = array( sprintf( __('Plugins %s'), "<span id='update-plugins' class='count-$update_count'><span class='plugin-count'>" . number_format_i18n($update_count) . "</span></span>" ), 'activate_plugins', 'plugins.php', '', '', 'menu-plugins', 'images/menu/plugins.png' );
 	$submenu['plugins.php'][15] = array(_c('Add New|plugin'), 'install_plugins', 'plugin-install.php');	
 	$submenu['plugins.php'][5]  = array( __('Installed'), 'activate_plugins', 'plugins.php' );
 	$submenu['plugins.php'][10] = array( __('Editor'), 'edit_plugins', 'plugin-editor.php' );
