f8090206fa18b864f22ece031085e78d8a47711d
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-10-31 19:24:45 +0000

    Allow nested install. Props sivel. fixes #7997
    git-svn-id: http://svn.automattic.com/wordpress/trunk@9443 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-load.php
+++ wp-load.php
@@ -26,7 +26,7 @@ if ( file_exists( ABSPATH . 'wp-config.php') ) {
 	/** The config file resides in ABSPATH */
 	require_once( ABSPATH . 'wp-config.php' );
 
-} elseif ( file_exists( dirname(ABSPATH) . '/wp-config.php' ) ) {
+} elseif ( file_exists( dirname(ABSPATH) . '/wp-config.php' ) && ! file_exists( dirname(ABSPATH) . '/wp-load.php' ) ) {
 
 	/** The config file resides one level below ABSPATH */
 	require_once( dirname(ABSPATH) . '/wp-config.php' );
