1d29fbd3d81b291f0e041d41f51d6750a1f8d9af
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-11-01 07:43:07 +0000

    Beta 1
    git-svn-id: http://svn.automattic.com/wordpress/trunk@9462 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '2.7-almost-beta-9461';
+$wp_version = '2.7-beta1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
