883462a60b5375d812f4ce182df60ede88214859
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-11-05 19:51:07 +0000

    Move notices below h2
    git-svn-id: http://svn.automattic.com/wordpress/trunk@9531 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-tags.php
+++ wp-admin/edit-tags.php
@@ -131,13 +131,13 @@ $messages[6] = __('Tags deleted.'); ?>
 
 <?php screen_options('tag') ?>
 
+<div class="wrap nosubsub">
+<h2><?php echo wp_specialchars( $title ); ?></h2> 
+
 <?php if ( isset($_GET['message']) && ( $msg = (int) $_GET['message'] ) ) : ?>
 <div id="message" class="updated fade"><p><?php echo $messages[$msg]; ?></p></div>
 <?php $_SERVER['REQUEST_URI'] = remove_query_arg(array('message'), $_SERVER['REQUEST_URI']);
 endif; ?>
-
-<div class="wrap nosubsub">
-<h2><?php echo wp_specialchars( $title ); ?></h2> 
 
 <form class="search-form" action="" method="get">
 <p class="search-box">
