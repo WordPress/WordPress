544ea955d834185d46fd204da4fa8ab214cc0fc7
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-11-08 23:27:49 +0000

    Eliminate chaining for php 4 compat. Props DD32. fixes #5324
    git-svn-id: http://svn.automattic.com/wordpress/trunk@9577 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/archive.php
+++ wp-content/themes/default/archive.php
@@ -55,14 +55,16 @@ get_header();
 		</div>
 	<?php else :
 
-		if (is_category()) // If this is a category archive
+		if ( is_category() ) { // If this is a category archive
 			printf("<h2 class='center'>Sorry, but there aren't any posts in the %s category yet.</h2>", single_cat_title('',false));
-		else if (is_date()) // If this is a date archive
+		} else if ( is_date() ) { // If this is a date archive
 			echo("<h2>Sorry, but there aren't any posts with this date.</h2>");
-		else if (is_author()) // If this is a category archive
-			printf("<h2 class='center'>Sorry, but there aren't any posts by %s yet.</h2>", get_userdatabylogin(get_query_var('author_name'))->display_name);
-		else
+		} else if ( is_author() ) { // If this is a category archive
+			$userdata = get_userdatabylogin(get_query_var('author_name'));
+			printf("<h2 class='center'>Sorry, but there aren't any posts by %s yet.</h2>", $userdata->display_name);
+		} else {
 			echo("<h2 class='center'>No posts found.</h2>");
+		}
 		get_search_form();
 
 	endif;
