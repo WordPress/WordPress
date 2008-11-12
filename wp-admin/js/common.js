ef6f5c31d724b8e4a60db035ce416c856007f59e
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-11-12 00:27:01 +0000

    Merge all JS files that always load with common.js
    git-svn-id: http://svn.automattic.com/wordpress/trunk@9626 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/common.js
+++ wp-admin/js/common.js
@@ -1,37 +1,297 @@
-jQuery(document).ready( function() {
+
+wpCookies = {
+// The following functions are from Cookie.js class in TinyMCE, Moxiecode, used under LGPL.
+
+	each : function(o, cb, s) {
+		var n, l;
+
+		if (!o)
+			return 0;
+
+		s = s || o;
+
+		if (typeof(o.length) != 'undefined') {
+			for (n=0, l = o.length; n<l; n++) {
+				if (cb.call(s, o[n], n, o) === false)
+					return 0;
+			}
+		} else {
+			for (n in o) {
+				if (o.hasOwnProperty(n)) {
+					if (cb.call(s, o[n], n, o) === false)
+						return 0;
+				}
+			}
+		}
+		return 1;
+	},
+
+	getHash : function(n) {
+		var v = this.get(n), h;
+
+		if (v) {
+			this.each(v.split('&'), function(v) {
+				v = v.split('=');
+				h = h || {};
+				h[v[0]] = v[1];
+			});
+		}
+		return h;
+	},
+
+	setHash : function(n, v, e, p, d, s) {
+		var o = '';
+
+		this.each(v, function(v, k) {
+			o += (!o ? '' : '&') + k + '=' + v;
+		});
+
+		this.set(n, o, e, p, d, s);
+	},
+
+	get : function(n) {
+		var c = document.cookie, e, p = n + "=", b;
+
+		if (!c)
+			return;
+
+		b = c.indexOf("; " + p);
+
+		if (b == -1) {
+			b = c.indexOf(p);
+
+			if (b != 0)
+				return null;
+		} else
+			b += 2;
+
+		e = c.indexOf(";", b);
+
+		if (e == -1)
+			e = c.length;
+
+		return decodeURIComponent(c.substring(b + p.length, e));
+	},
+
+	set : function(n, v, e, p, d, s) {
+		document.cookie = n + "=" + encodeURIComponent(v) +
+			((e) ? "; expires=" + e.toGMTString() : "") +
+			((p) ? "; path=" + p : "") +
+			((d) ? "; domain=" + d : "") +
+			((s) ? "; secure" : "");
+	},
+
+	remove : function(n, p) {
+		var d = new Date();
+
+		d.setTime(d.getTime() - 1000);
+
+		this.set(n, '', d, p, d);
+	}
+};
+
+// Returns the value as string. Second arg or empty string is returned when value is not set.
+function getUserSetting( name, def ) {
+	var o = getAllUserSettings();
+
+	if ( o.hasOwnProperty(name) )
+		return o[name];
+
+	if ( typeof def != 'undefined' )
+		return def;
+
+	return '';
+}
+
+// Both name and value must be only ASCII letters, numbers or underscore
+// and the shorter, the better (cookies can store maximum 4KB). Not suitable to store text.
+function setUserSetting( name, value, del ) {
+	var c = 'wp-settings-'+userSettings.uid, o = wpCookies.getHash(c) || {}, d = new Date();
+	var n = name.toString().replace(/[^A-Za-z0-9_]/, ''), v = value.toString().replace(/[^A-Za-z0-9_]/, '');
+
+	if ( del ) delete o[n];
+	else o[n] = v;
+
+	d.setTime( d.getTime() + 31536000000 );
+	p = userSettings.url;
+
+	wpCookies.setHash(c, o, d, p );
+	wpCookies.set('wp-settings-time-'+userSettings.uid, userSettings.time, d, p );
+}
+
+function deleteUserSetting( name ) {
+	setUserSetting( name, '', 1 );
+}
+
+// Returns all settings as js object.
+function getAllUserSettings() {
+	return wpCookies.getHash('wp-settings-'+userSettings.uid) || {};
+}
+
+
+jQuery(document).ready( function($) {
 	// pulse
-	jQuery('.fade').animate( { backgroundColor: '#ffffe0' }, 300).animate( { backgroundColor: '#fffbcc' }, 300).animate( { backgroundColor: '#ffffe0' }, 300).animate( { backgroundColor: '#fffbcc' }, 300);
+	$('.fade').animate( { backgroundColor: '#ffffe0' }, 300).animate( { backgroundColor: '#fffbcc' }, 300).animate( { backgroundColor: '#ffffe0' }, 300).animate( { backgroundColor: '#fffbcc' }, 300);
 
 	// show things that should be visible, hide what should be hidden
-	jQuery('.hide-if-no-js').removeClass('hide-if-no-js');
-	jQuery('.hide-if-js').hide();
+	$('.hide-if-no-js').removeClass('hide-if-no-js');
+	$('.hide-if-js').hide();
 
 	// Basic form validation
-	if ( ( 'undefined' != typeof wpAjax ) && jQuery.isFunction( wpAjax.validateForm ) ) {
-		jQuery('form.validate').submit( function() { return wpAjax.validateForm( jQuery(this) ); } );
+	if ( ( 'undefined' != typeof wpAjax ) && $.isFunction( wpAjax.validateForm ) ) {
+		$('form.validate').submit( function() { return wpAjax.validateForm( $(this) ); } );
 	}
 
 	// Move .updated and .error alert boxes 
-	jQuery('div.wrap h2 ~ div.updated, div.wrap h2 ~ div.error').addClass('below-h2');
-	jQuery('div.updated, div.error').not('.below-h2').insertAfter('div.wrap h2:first');
+	$('div.wrap h2 ~ div.updated, div.wrap h2 ~ div.error').addClass('below-h2');
+	$('div.updated, div.error').not('.below-h2').insertAfter('div.wrap h2:first');
 
+	// screen settings tab
+	$('#show-settings-link').click(function () {
+		if ( ! $('#screen-options-wrap').hasClass('screen-options-open') ) {
+			$('#contextual-help-link-wrap').addClass('invisible');
+		}
+		$('#screen-options-wrap').slideToggle('fast', function(){
+			if ( $(this).hasClass('screen-options-open') ) {
+				$('#show-settings-link').css({'backgroundImage':'url("images/screen-options-right.gif")'});
+				$('#contextual-help-link-wrap').removeClass('invisible');
+				$(this).removeClass('screen-options-open');
+				
+			} else {
+				$('#show-settings-link').css({'backgroundImage':'url("images/screen-options-right-up.gif")'});
+				$(this).addClass('screen-options-open');
+			}
+		});
+		return false;
+	});
+	
+	// help tab
+	$('#contextual-help-link').click(function () {
+		if ( ! $('#contextual-help-wrap').hasClass('contextual-help-open') ) {
+			$('#screen-options-link-wrap').addClass('invisible');
+		}
+		$('#contextual-help-wrap').slideToggle('fast', function(){
+			if ( $(this).hasClass('contextual-help-open') ) {
+				$('#contextual-help-link').css({'backgroundImage':'url("images/screen-options-right.gif")'});
+				$('#screen-options-link-wrap').removeClass('invisible');
+				$(this).removeClass('contextual-help-open');
+			} else {
+				$('#contextual-help-link').css({'backgroundImage':'url("images/screen-options-right-up.gif")'});
+				$(this).addClass('contextual-help-open');
+			}
+		});
+		return false;
+	});
 });
 
-// stub for doing better warnings
 (function($){
-	showNotice = {
-		warn : function(text) {
-			if ( confirm(text) )
-				return true;
 
-			return false;
-		},
-		
-		note : function(text) {
-			alert(text);
-		}
+// stub for doing better warnings
+showNotice = {
+	warn : function(text) {
+		if ( confirm(text) )
+			return true;
+
+		return false;
+	},
+	
+	note : function(text) {
+		alert(text);
 	}
+};
+
+// sidebar admin menu
+adminMenu = {
+		
+	init : function() {
+		$('#adminmenu a').attr('tabindex', '10');
+		$('#adminmenu div.wp-menu-toggle').each( function() {
+			if ( $(this).siblings('.wp-submenu').length ) 
+				$(this).click(function(){ adminMenu.toggle( $(this).siblings('.wp-submenu') ); });
+			else
+				$(this).hide();
+		});
+		$('#adminmenu li.menu-top .wp-menu-image').click( function() { window.location = $(this).siblings('a.menu-top')[0].href; } );
+		this.favorites();
+
+		$('.wp-menu-separator').click(function(){
+			if ( $('#adminmenu').hasClass('folded') ) {
+				adminMenu.fold(1);
+				setUserSetting( 'mfold', 'o' );
+			} else {
+				adminMenu.fold();
+				setUserSetting( 'mfold', 'f' );
+			}
+		});
+
+		if ( 'f' != getUserSetting( 'mfold' ) ) {
+			this.restoreMenuState();
+		} else {
+			this.fold();
+		}
+	},
+
+	restoreMenuState : function() {
+		$('#adminmenu li.wp-has-submenu').each(function(i, e) {
+			var v = getUserSetting( 'm'+i );
+			if ( $(e).hasClass('wp-has-current-submenu') ) return true; // leave the current parent open
+
+			if ( 'o' == v ) $(e).addClass('wp-menu-open');
+			else if ( 'c' == v ) $(e).removeClass('wp-menu-open');	
+		});
+	},
+
+	toggle : function(el) {
+
+		el['slideToggle'](150, function(){el.css('display','');}).parent().toggleClass( 'wp-menu-open' );
+
+		$('#adminmenu li.wp-has-submenu').each(function(i, e) {
+			var v = $(e).hasClass('wp-menu-open') ? 'o' : 'c';
+			setUserSetting( 'm'+i, v );
+		});
+
+		return false;
+	},
+	
+	fold : function(off) {
+		if (off) {
+			$('#adminmenu').removeClass('folded');
+			$('#adminmenu li.wp-has-submenu').unbind();
+		} else {
+			$('#adminmenu').addClass('folded');
+			$('#adminmenu li.wp-has-submenu').hoverIntent({
+				over: function(e){
+					var m = $(this).find('.wp-submenu'), t = e.clientY, H = $(window).height(), h = m.height(), o;
+
+					if ( (t+h+10) > H ) {
+						o = (t+h+10) - H;
+						m.css({'marginTop':'-'+o+'px'});
+					} else if ( m.css('marginTop') ) {
+						m.css({'marginTop':''})
+					}
+					m.addClass('sub-open');
+				},
+				out: function(){ $(this).find('.wp-submenu').removeClass('sub-open'); },
+				timeout: 220,
+				sensitivity: 8,
+				interval: 100
+			});
+			
+		}
+	},
+	
+	favorites : function() {
+		$('#favorite-inside').width($('#favorite-actions').width()-4);
+		$('#favorite-toggle, #favorite-inside').bind( 'mouseenter', function(){$('#favorite-inside').removeClass('slideUp').addClass('slideDown'); setTimeout(function(){if ( $('#favorite-inside').hasClass('slideDown') ) { $('#favorite-inside').slideDown(100); $('#favorite-first').addClass('slide-down'); }}, 200) } );
+	
+		$('#favorite-toggle, #favorite-inside').bind( 'mouseleave', function(){$('#favorite-inside').removeClass('slideDown').addClass('slideUp'); setTimeout(function(){if ( $('#favorite-inside').hasClass('slideUp') ) { $('#favorite-inside').slideUp(100, function(){ $('#favorite-first').removeClass('slide-down'); } ); }}, 300) } );
+	}
+};
+
+$(document).ready(function(){adminMenu.init();});
+
 })(jQuery);
+
 
 /*
 (function($) {
--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -41,7 +41,7 @@ function wp_default_scripts( &$scripts ) {
 	$scripts->base_url = $guessurl;
 	$scripts->default_version = get_bloginfo( 'version' );
 
-	$scripts->add( 'common', '/wp-admin/js/common.js', array('jquery', 'user-settings', 'menu'), '20081107' );
+	$scripts->add( 'common', '/wp-admin/js/common.js', array('jquery', 'hoverIntent'), '20081111' );
 	$scripts->add( 'sack', '/wp-includes/js/tw-sack.js', false, '1.6.1' );
 
 	$scripts->add( 'quicktags', '/wp-includes/js/quicktags.js', false, '20081103' );
@@ -145,8 +145,8 @@ function wp_default_scripts( &$scripts ) {
 			'add' => attribute_escape(__('Add')),
 			'how' => __('Separate multiple categories with commas.')
 		) );
-		$scripts->add( 'admin-categories', '/wp-admin/js/categories.js', array('wp-lists', 'columns', 'settings-box'), '20080925' );
-		$scripts->add( 'admin-tags', '/wp-admin/js/tags.js', array('wp-lists', 'columns', 'settings-box'), '20080925' );
+		$scripts->add( 'admin-categories', '/wp-admin/js/categories.js', array('wp-lists', 'columns'), '20080925' );
+		$scripts->add( 'admin-tags', '/wp-admin/js/tags.js', array('wp-lists', 'columns'), '20080925' );
 		$scripts->add( 'admin-custom-fields', '/wp-admin/js/custom-fields.js', array('wp-lists'), '20070823' );
 		$scripts->add( 'password-strength-meter', '/wp-admin/js/password-strength-meter.js', array('jquery'), '20081021' );
 		$scripts->localize( 'password-strength-meter', 'pwsL10n', array(
@@ -156,16 +156,15 @@ function wp_default_scripts( &$scripts ) {
 			'good' => __('Medium'),
 			'strong' => __('Strong')
 		) );
-		$scripts->add( 'admin-comments', '/wp-admin/js/edit-comments.js', array('wp-lists', 'jquery-ui-resizable', 'quicktags', 'columns', 'settings-box'), '20081031' );
+		$scripts->add( 'admin-comments', '/wp-admin/js/edit-comments.js', array('wp-lists', 'jquery-ui-resizable', 'quicktags', 'columns'), '20081031' );
 		$scripts->localize( 'admin-comments', 'adminCommentsL10n', array(
 			'pending' => __('%i% pending'), // must look like: "# blah blah"
 			'hotkeys_highlight_first' => isset($_GET['hotkeys_highlight_first']),
 			'hotkeys_highlight_last' => isset($_GET['hotkeys_highlight_last']),
 		) );
-		$scripts->add( 'admin-users', '/wp-admin/js/users.js', array('wp-lists', 'columns', 'settings-box'), '20080925' );
+		$scripts->add( 'admin-users', '/wp-admin/js/users.js', array('wp-lists', 'columns'), '20080925' );
 		$scripts->add( 'admin-forms', '/wp-admin/js/forms.js', array('jquery'), '20080729');
 		$scripts->add( 'xfn', '/wp-admin/js/xfn.js', false, '3517' );
-		$scripts->add( 'upload', '/wp-admin/js/upload.js', array('jquery'), '20070518' );
 		$scripts->add( 'postbox', '/wp-admin/js/postbox.js', array('jquery-ui-sortable'), '20081109' );
 		$scripts->localize( 'postbox', 'postboxL10n', array(
 			'requestFile' => admin_url('admin-ajax.php'),
@@ -176,7 +175,7 @@ function wp_default_scripts( &$scripts ) {
 			'save' => __('Save'),
 			'cancel' => __('Cancel'),
 		) );
-		$scripts->add( 'post', '/wp-admin/js/post.js', array('suggest', 'jquery-ui-tabs', 'wp-lists', 'postbox', 'slug', 'settings-box'), '20081107' );
+		$scripts->add( 'post', '/wp-admin/js/post.js', array('suggest', 'jquery-ui-tabs', 'wp-lists', 'postbox', 'slug'), '20081107' );
 		$scripts->localize( 'post', 'postL10n', array(
 			'tagsUsed' =>  __('Tags used on this post:'),
 			'add' => attribute_escape(__('Add')),
@@ -195,7 +194,7 @@ function wp_default_scripts( &$scripts ) {
 			'savePending' => __('Save as Pending'),
 			'saveDraft' => __('Save Draft')
 		) );
-		$scripts->add( 'page', '/wp-admin/js/page.js', array('jquery', 'slug', 'wp-lists', 'postbox', 'settings-box'), '20081107' );
+		$scripts->add( 'page', '/wp-admin/js/page.js', array('jquery', 'slug', 'wp-lists', 'postbox'), '20081107' );
 		$scripts->localize( 'page', 'postL10n', array(
 			'cancel' => __('Cancel'),
 			'edit' => __('Edit'),
@@ -210,7 +209,7 @@ function wp_default_scripts( &$scripts ) {
 			'savePending' => __('Save as Pending'),
 			'saveDraft' => __('Save Draft')
 		) );
-		$scripts->add( 'link', '/wp-admin/js/link.js', array('jquery-ui-tabs', 'wp-lists', 'postbox', 'settings-box'), '20080925' );
+		$scripts->add( 'link', '/wp-admin/js/link.js', array('jquery-ui-tabs', 'wp-lists', 'postbox'), '20080925' );
 		$scripts->add( 'comment', '/wp-admin/js/comment.js', array('jquery'), '20081103' );
 		$scripts->localize( 'comment', 'commentL10n', array(
 			'cancel' => __('Cancel'),
@@ -219,28 +218,7 @@ function wp_default_scripts( &$scripts ) {
 		) );
 		$scripts->add( 'admin-gallery', '/wp-admin/js/gallery.js', array( 'jquery-ui-sortable' ), '20080709' );
 		$scripts->add( 'media-upload', '/wp-admin/js/media-upload.js', array( 'thickbox' ), '20081031' );
-		$scripts->localize( 'upload', 'uploadL10n', array(
-			'browseTitle' => attribute_escape(__('Browse your files')),
-			'back' => __('&laquo; Back'),
-			'directTitle' => attribute_escape(__('Direct link to file')),
-			'edit' => __('Edit'),
-			'thumb' => __('Thumbnail'),
-			'full' => __('Full size'),
-			'icon' => __('Icon'),
-			'title' => __('Title'),
-			'show' => __('Show:'),
-			'link' => __('Link to:'),
-			'file' => __('File'),
-			'page' => __('Page'),
-			'none' => __('None'),
-			'editorText' => attribute_escape(__('Send to editor &raquo;')),
-			'insert' => __('Insert'),
-			'urlText' => __('URL'),
-			'desc' => __('Description'),
-			'deleteText' => attribute_escape(__('Delete File')),
-			'saveText' => attribute_escape(__('Save &raquo;')),
-			'confirmText' => __("Are you sure you want to delete the file '%title%'?\nClick ok to delete or cancel to go back.")
-		) );
+		
 		$scripts->add( 'admin-widgets', '/wp-admin/js/widgets.js', array( 'interface' ), '20081010' );
 		$scripts->localize( 'admin-widgets', 'widgetsL10n', array(
 			'add' => __('Add'),
@@ -280,24 +258,19 @@ function wp_default_scripts( &$scripts ) {
 
 		$scripts->add( 'farbtastic', '/wp-admin/js/farbtastic.js', array('jquery'), '1.2' );
 
-		$scripts->add( 'user-settings', '/wp-admin/js/user-settings.js', array(), '20080829' );
-
-		$scripts->add( 'posts', '/wp-admin/js/posts.js', array('columns', 'settings-box'), '20080925' );
-		$scripts->add( 'pages', '/wp-admin/js/pages.js', array('columns', 'settings-box'), '20080925' );
-		$scripts->add( 'links', '/wp-admin/js/links.js', array('columns', 'settings-box'), '20080925' );
-		$scripts->add( 'media', '/wp-admin/js/media.js', array('columns', 'settings-box'), '20080925' );
+		$scripts->add( 'posts', '/wp-admin/js/posts.js', array('columns'), '20080925' );
+		$scripts->add( 'pages', '/wp-admin/js/pages.js', array('columns'), '20080925' );
+		$scripts->add( 'links', '/wp-admin/js/links.js', array('columns'), '20080925' );
+		$scripts->add( 'media', '/wp-admin/js/media.js', array('columns'), '20080925' );
 
 		$scripts->add( 'columns', '/wp-admin/js/columns.js', false, '20080910' );
 		$scripts->localize( 'columns', 'columnsL10n', array(
 			'requestFile' => admin_url('admin-ajax.php'),
 		) );
 
-		$scripts->add( 'settings-box', '/wp-admin/js/settings-box.js', array( 'jquery' ), '20081107' );
-
-		$scripts->add( 'dashboard', '/wp-admin/js/dashboard.js', array( 'jquery', 'admin-comments', 'postbox', 'settings-box' ), '20081016' );
+		$scripts->add( 'dashboard', '/wp-admin/js/dashboard.js', array( 'jquery', 'admin-comments', 'postbox' ), '20081016' );
 
 		$scripts->add( 'hoverIntent', '/wp-includes/js/hoverIntent.js', array('jquery'), '20081109' );
-		$scripts->add( 'menu', '/wp-admin/js/menu.js', array( 'jquery', 'hoverIntent' ), '20081109' );
 
 	}
 }
@@ -400,7 +373,7 @@ function wp_just_in_time_script_localization() {
 	) );
 
 	$userid = isset($current_user) ? $current_user->ID : 0;
-	wp_localize_script( 'user-settings', 'userSettings', array(
+	wp_localize_script( 'common', 'userSettings', array(
 		'url' => SITECOOKIEPATH,
 		'uid' => $userid,
 		'time' => time()
