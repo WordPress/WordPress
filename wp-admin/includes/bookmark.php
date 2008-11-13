b9098f869b68557e793e61a7a9bb0ebfca9a64b5
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-11-13 07:17:40 +0000

    Code cleanup for wp-admin/includes/bookmark.php
    git-svn-id: http://svn.automattic.com/wordpress/trunk@9659 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/bookmark.php
+++ wp-admin/includes/bookmark.php
@@ -76,18 +76,18 @@ function get_default_link_to_edit() {
  * @param unknown_type $link_id
  * @return unknown
  */
-function wp_delete_link($link_id) {
+function wp_delete_link( $link_id ) {
 	global $wpdb;
 
-	do_action('delete_link', $link_id);
+	do_action( 'delete_link', $link_id );
 
-	wp_delete_object_term_relationships($link_id, 'link_category');
+	wp_delete_object_term_relationships( $link_id, 'link_category' );
 
-	$wpdb->query( $wpdb->prepare("DELETE FROM $wpdb->links WHERE link_id = %d", $link_id) );
+	$wpdb->query( $wpdb->prepare( "DELETE FROM $wpdb->links WHERE link_id = %d", $link_id ) );
 
-	do_action('deleted_link', $link_id);
+	do_action( 'deleted_link', $link_id );
 
-	clean_bookmark_cache($link_id);
+	clean_bookmark_cache( $link_id );
 
 	return true;
 }
@@ -100,11 +100,11 @@ function wp_delete_link($link_id) {
  * @param unknown_type $link_id
  * @return unknown
  */
-function wp_get_link_cats($link_id = 0) {
+function wp_get_link_cats( $link_id = 0 ) {
 
-	$cats = wp_get_object_terms($link_id, 'link_category', 'fields=ids');
+	$cats = wp_get_object_terms( $link_id, 'link_category', 'fields=ids' );
 
-	return array_unique($cats);
+	return array_unique( $cats );
 }
 
 /**
@@ -127,19 +127,19 @@ function get_link_to_edit( $link_id ) {
  * @param unknown_type $linkdata
  * @return unknown
  */
-function wp_insert_link($linkdata, $wp_error = false) {
+function wp_insert_link( $linkdata, $wp_error = false ) {
 	global $wpdb, $current_user;
 
-	$defaults = array('link_id' => 0, 'link_name' => '', 'link_url' => '', 'link_rating' => 0 );
+	$defaults = array( 'link_id' => 0, 'link_name' => '', 'link_url' => '', 'link_rating' => 0 );
 
-	$linkdata = wp_parse_args($linkdata, $defaults);
-	$linkdata = sanitize_bookmark($linkdata, 'db');
+	$linkdata = wp_parse_args( $linkdata, $defaults );
+	$linkdata = sanitize_bookmark( $linkdata, 'db' );
 
-	extract(stripslashes_deep($linkdata), EXTR_SKIP);
+	extract( stripslashes_deep( $linkdata ), EXTR_SKIP );
 
 	$update = false;
 
-	if ( !empty($link_id) )
+	if ( !empty( $link_id ) )
 		$update = true;
 
 	if ( trim( $link_name ) == '' )
@@ -148,68 +148,68 @@ function wp_insert_link($linkdata, $wp_error = false) {
 	if ( trim( $link_url ) == '' )
 		return 0;
 
-	if ( empty($link_rating) )
+	if ( empty( $link_rating ) )
 		$link_rating = 0;
 
-	if ( empty($link_image) )
+	if ( empty( $link_image ) )
 		$link_image = '';
 
-	if ( empty($link_target) )
+	if ( empty( $link_target ) )
 		$link_target = '';
 
-	if ( empty($link_visible) )
+	if ( empty( $link_visible ) )
 		$link_visible = 'Y';
 
-	if ( empty($link_owner) )
+	if ( empty( $link_owner ) )
 		$link_owner = $current_user->id;
 
-	if ( empty($link_notes) )
+	if ( empty( $link_notes ) )
 		$link_notes = '';
 
-	if ( empty($link_description) )
+	if ( empty( $link_description ) )
 		$link_description = '';
 
-	if ( empty($link_rss) )
+	if ( empty( $link_rss ) )
 		$link_rss = '';
 
-	if ( empty($link_rel) )
+	if ( empty( $link_rel ) )
 		$link_rel = '';
 
 	// Make sure we set a valid category
-	if (0 == count($link_category) || !is_array($link_category)) {
-		$link_category = array(get_option('default_link_category'));
+	if ( 0 == count( $link_category ) || !is_array( $link_category ) ) {
+		$link_category = array( get_option( 'default_link_category' ) );
 	}
 
 	if ( $update ) {
-		if ( false === $wpdb->query( $wpdb->prepare("UPDATE $wpdb->links SET link_url = %s,
+		if ( false === $wpdb->query( $wpdb->prepare( "UPDATE $wpdb->links SET link_url = %s,
 			link_name = %s, link_image = %s, link_target = %s,
 			link_visible = %s, link_description = %s, link_rating = %s,
 			link_rel = %s, link_notes = %s, link_rss = %s
-			WHERE link_id = %s", $link_url, $link_name, $link_image, $link_target, $link_visible, $link_description, $link_rating, $link_rel, $link_notes, $link_rss, $link_id) ) ) {
+			WHERE link_id = %s", $link_url, $link_name, $link_image, $link_target, $link_visible, $link_description, $link_rating, $link_rel, $link_notes, $link_rss, $link_id ) ) ) {
 			if ( $wp_error )
-				return new WP_Error('db_update_error', __('Could not update link in the database'), $wpdb->last_error);
+				return new WP_Error( 'db_update_error', __( 'Could not update link in the database' ), $wpdb->last_error );
 			else
 				return 0;
 		}
 	} else {
-		if ( false === $wpdb->query( $wpdb->prepare("INSERT INTO $wpdb->links (link_url, link_name, link_image, link_target, link_description, link_visible, link_owner, link_rating, link_rel, link_notes, link_rss) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-		$link_url,$link_name, $link_image, $link_target, $link_description, $link_visible, $link_owner, $link_rating, $link_rel, $link_notes, $link_rss) ) ) {
+		if ( false === $wpdb->query( $wpdb->prepare( "INSERT INTO $wpdb->links (link_url, link_name, link_image, link_target, link_description, link_visible, link_owner, link_rating, link_rel, link_notes, link_rss) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+		$link_url,$link_name, $link_image, $link_target, $link_description, $link_visible, $link_owner, $link_rating, $link_rel, $link_notes, $link_rss ) ) ) {
 			if ( $wp_error )
-				return new WP_Error('db_insert_error', __('Could not insert link into the database'), $wpdb->last_error);
+				return new WP_Error( 'db_insert_error', __( 'Could not insert link into the database' ), $wpdb->last_error );
 			else
 				return 0;
 		}
 		$link_id = (int) $wpdb->insert_id;
 	}
 
-	wp_set_link_cats($link_id, $link_category);
+	wp_set_link_cats( $link_id, $link_category );
 
 	if ( $update )
-		do_action('edit_link', $link_id);
+		do_action( 'edit_link', $link_id );
 	else
-		do_action('add_link', $link_id);
+		do_action( 'add_link', $link_id );
 
-	clean_bookmark_cache($link_id);
+	clean_bookmark_cache( $link_id );
 
 	return $link_id;
 }
@@ -222,17 +222,17 @@ function wp_insert_link($linkdata, $wp_error = false) {
  * @param unknown_type $link_id
  * @param unknown_type $link_categories
  */
-function wp_set_link_cats($link_id = 0, $link_categories = array()) {
+function wp_set_link_cats( $link_id = 0, $link_categories = array() ) {
 	// If $link_categories isn't already an array, make it one:
-	if (!is_array($link_categories) || 0 == count($link_categories))
-		$link_categories = array(get_option('default_link_category'));
+	if ( !is_array( $link_categories ) || 0 == count( $link_categories ) )
+		$link_categories = array( get_option( 'default_link_category' ) );
 
-	$link_categories = array_map('intval', $link_categories);
-	$link_categories = array_unique($link_categories);
+	$link_categories = array_map( 'intval', $link_categories );
+	$link_categories = array_unique( $link_categories );
 
-	wp_set_object_terms($link_id, $link_categories, 'link_category');
+	wp_set_object_terms( $link_id, $link_categories, 'link_category' );
 
-	clean_bookmark_cache($link_id);
+	clean_bookmark_cache( $link_id );
 }	// wp_set_link_cats()
 
 /**
@@ -243,26 +243,26 @@ function wp_set_link_cats($link_id = 0, $link_categories = array()) {
  * @param unknown_type $linkdata
  * @return unknown
  */
-function wp_update_link($linkdata) {
+function wp_update_link( $linkdata ) {
 	$link_id = (int) $linkdata['link_id'];
 
-	$link = get_link($link_id, ARRAY_A);
+	$link = get_link( $link_id, ARRAY_A );
 
 	// Escape data pulled from DB.
-	$link = add_magic_quotes($link);
+	$link = add_magic_quotes( $link );
 
 	// Passed link category list overwrites existing category list if not empty.
-	if ( isset($linkdata['link_category']) && is_array($linkdata['link_category'])
-			 && 0 != count($linkdata['link_category']) )
+	if ( isset( $linkdata['link_category'] ) && is_array( $linkdata['link_category'] )
+			 && 0 != count( $linkdata['link_category'] ) )
 		$link_cats = $linkdata['link_category'];
 	else
 		$link_cats = $link['link_category'];
 
 	// Merge old and new fields with new fields overwriting old ones.
-	$linkdata = array_merge($link, $linkdata);
+	$linkdata = array_merge( $link, $linkdata );
 	$linkdata['link_category'] = $link_cats;
 
-	return wp_insert_link($linkdata);
+	return wp_insert_link( $linkdata );
 }
 
 ?>
