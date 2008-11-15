022c232b9840fe24d5e065ef9ec77d6d1be0fd44
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-11-15 19:08:25 +0000

    Bump CSS versions
    git-svn-id: http://svn.automattic.com/wordpress/trunk@9722 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -301,23 +301,23 @@ function wp_default_styles( &$styles ) {
 
 	$rtl_styles = array( 'global', 'colors', 'dashboard', 'ie', 'install', 'login', 'media', 'theme-editor', 'upload', 'widgets', 'press-this', 'press-this-ie', 'plugin-install', 'farbtastic' );
 
-	$styles->add( 'wp-admin', '/wp-admin/wp-admin.css', array(), '20081113' );
+	$styles->add( 'wp-admin', '/wp-admin/wp-admin.css', array(), '20081115' );
 	$styles->add_data( 'wp-admin', 'rtl', '/wp-admin/rtl.css' );
 
 	$styles->add( 'ie', '/wp-admin/css/ie.css' );
 	$styles->add_data( 'ie', 'conditional', 'gte IE 6' );
 
 	$styles->add( 'colors', true ); // Register "meta" stylesheet for admin colors
-	$styles->add( 'colors-fresh', '/wp-admin/css/colors-fresh.css', array(), '20081113'); // for login.php.  Is there a better way?
+	$styles->add( 'colors-fresh', '/wp-admin/css/colors-fresh.css', array(), '20081115'); // for login.php.  Is there a better way?
 	$styles->add_data( 'colors-fresh', 'rtl', true );
 
 	$styles->add( 'global', '/wp-admin/css/global.css', array(), '20081106' );
 	$styles->add( 'media', '/wp-admin/css/media.css', array(), '20080709' );
 	$styles->add( 'widgets', '/wp-admin/css/widgets.css' );
-	$styles->add( 'dashboard', '/wp-admin/css/dashboard.css', array(), '20081113' );
+	$styles->add( 'dashboard', '/wp-admin/css/dashboard.css', array(), '20081115' );
 	$styles->add( 'install', '/wp-admin/css/install.css', array(), '20080708' );
 	$styles->add( 'theme-editor', '/wp-admin/css/theme-editor.css' );
-	$styles->add( 'press-this', '/wp-admin/css/press-this.css', array(), '20081113' );
+	$styles->add( 'press-this', '/wp-admin/css/press-this.css', array(), '20081115' );
 	$styles->add( 'press-this-ie', '/wp-admin/css/press-this-ie.css', array(), '20080915' );
 	$styles->add_data( 'press-this-ie', 'conditional', 'gte IE 6' );
 	$styles->add( 'thickbox', '/wp-includes/js/thickbox/thickbox.css', array(), '20080613' );
