7513c5a56980d8a8a11a214dfa804b2f6705d5fe
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-11-18 22:25:54 +0000

    Remove background from QuickPress publish bar
    git-svn-id: http://svn.automattic.com/wordpress/trunk@9766 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -307,7 +307,7 @@ function wp_default_styles( &$styles ) {
 	$styles->add_data( 'ie', 'conditional', 'gte IE 6' );
 
 	$styles->add( 'colors', true ); // Register "meta" stylesheet for admin colors
-	$styles->add( 'colors-fresh', '/wp-admin/css/colors-fresh.css', array(), '20081117'); // for login.php.  Is there a better way?
+	$styles->add( 'colors-fresh', '/wp-admin/css/colors-fresh.css', array(), '20081118'); // for login.php.  Is there a better way?
 	$styles->add_data( 'colors-fresh', 'rtl', true );
 
 	$styles->add( 'global', '/wp-admin/css/global.css', array(), '20081106' );
