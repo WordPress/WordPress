2ca70c2ea3ffe379bfe0e591a7cf1631a73baac3
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-11-18 08:26:00 +0000

    Use clean_url() in self_link(), for 2.6.x
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.6@9754 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/feed.php
+++ wp-includes/feed.php
@@ -495,10 +495,12 @@ function prep_atom_text_construct($data) {
  * @since 2.5
  */
 function self_link() {
-	echo 'http'
-		. ( $_SERVER['https'] == 'on' ? 's' : '' ) . '://'
+	echo clean_url( 
+		'http'
+		. ( (isset($_SERVER['https']) && $_SERVER['https'] == 'on') ? 's' : '' ) . '://'
 		. $_SERVER['HTTP_HOST']
-		. wp_specialchars(stripslashes($_SERVER['REQUEST_URI']), 1);
+		. stripslashes($_SERVER['REQUEST_URI'])
+		);
 }
 
 ?>
