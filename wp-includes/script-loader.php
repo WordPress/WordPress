4bff9364a341a4ff5062066239390d85de1f2869
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-11-19 07:21:05 +0000

    Move color declarations out of wp-admin.css, props Simek and johnhennmacc, fixes #7961
    git-svn-id: http://svn.automattic.com/wordpress/trunk@9783 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -300,13 +300,13 @@ function wp_default_styles( &$styles ) {
 
 	$rtl_styles = array( 'global', 'colors', 'dashboard', 'ie', 'install', 'login', 'media', 'theme-editor', 'upload', 'widgets', 'press-this', 'press-this-ie', 'plugin-install', 'farbtastic' );
 
-	$styles->add( 'wp-admin', '/wp-admin/wp-admin.css', array(), '20081119' );
+	$styles->add( 'wp-admin', '/wp-admin/wp-admin.css', array(), '20081119b' );
 	$styles->add_data( 'wp-admin', 'rtl', '/wp-admin/rtl.css' );
 
 	$styles->add( 'ie', '/wp-admin/css/ie.css', array(), '20081117' );
 	$styles->add_data( 'ie', 'conditional', 'gte IE 6' );
 
-	$styles->add( 'colors', true ); // Register "meta" stylesheet for admin colors
+	$styles->add( 'colors', true, array(), '20081118' ); // Register "meta" stylesheet for admin colors
 	$styles->add( 'colors-fresh', '/wp-admin/css/colors-fresh.css', array(), '20081119'); // for login.php.  Is there a better way?
 	$styles->add_data( 'colors-fresh', 'rtl', true );
 
