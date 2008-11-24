0b17615051b4531e9f0edf882aab4c7a74632649
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-11-24 00:20:34 +0000

    Fix #favorite-actions position in IE7, footer position, horizontal scrollbar and expansion of postboxes in IE7, put back Browse Happy, fixes #8280
    git-svn-id: http://svn.automattic.com/wordpress/trunk@9852 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -13,12 +13,14 @@
 </div><!-- wpwrap -->
 
 <div id="footer">
-<p><?php
+<p id="footer-left" class="alignleft"><?php
 do_action( 'in_admin_footer' );
 $upgrade = apply_filters( 'update_footer', '' );
-echo '<span id="footer-thankyou">' . __('Thank you for creating with <a href="http://wordpress.org/">WordPress</a>.').'</span> | '.__('<a href="http://codex.wordpress.org/">Documentation</a>').' | '.__('<a href="http://wordpress.org/support/forum/4">Feedback</a>').' <span id="footer-upgrade">'.$upgrade . '</span>';
-?>
+echo '<span id="footer-thankyou">' . __('Thank you for creating with <a href="http://wordpress.org/">WordPress</a>.').'</span> | '.__('<a href="http://codex.wordpress.org/">Documentation</a>').' | '.__('<a href="http://wordpress.org/support/forum/4">Feedback</a>'); ?>
 </p>
+<?php if ( $is_IE ) browse_happy(); ?>
+<p id="footer-upgrade" class="alignright"><?php echo $upgrade; ?></p>
+<div class="clear"></div>
 </div>
 <?php do_action('admin_footer', ''); ?>
 <script type="text/javascript">if(typeof wpOnload=='function')wpOnload();</script>
