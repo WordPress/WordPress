ac1ccbba3a11c7708df8e9b5928afd7c11233c14
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-11-29 15:52:17 +0000

    CSS cleanup
    git-svn-id: http://svn.automattic.com/wordpress/trunk@9964 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -18,7 +18,7 @@ do_action( 'in_admin_footer' );
 $upgrade = apply_filters( 'update_footer', '' );
 echo '<span id="footer-thankyou">' . __('Thank you for creating with <a href="http://wordpress.org/">WordPress</a>.').'</span> | '.__('<a href="http://codex.wordpress.org/">Documentation</a>').' | '.__('<a href="http://wordpress.org/support/forum/4">Feedback</a>'); ?>
 </p>
-<?php if ( $is_IE ) browse_happy(); ?>
+<?php // if ( $is_IE ) browse_happy(); ?>
 <p id="footer-upgrade" class="alignright"><?php echo $upgrade; ?></p>
 <div class="clear"></div>
 </div>
