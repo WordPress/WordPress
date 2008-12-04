93b7e3bde74887c673cfe4b9935fb4e93df85bda
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-12-04 07:44:16 +0000

    Better display and wording for themes with no widget-enabled sidebars
    git-svn-id: http://svn.automattic.com/wordpress/trunk@10032 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/widgets.php
+++ wp-admin/widgets.php
@@ -34,12 +34,12 @@ if ( isset($_GET['sidebar']) && isset($wp_registered_sidebars[$_GET['sidebar']])
 	require_once( 'admin-header.php' );
 ?>
 
-	<div class="error">
-		<p><?php _e( 'No Sidebars Defined' ); ?></p>
-	</div>
-
 	<div class="wrap">
-		<p><?php _e( 'You are seeing this message because the theme you are currently using isn&#8217;t widget-aware, meaning that it has no sidebars that you are able to change. For information on making your theme widget-aware, please <a href="http://codex.wordpress.org/Widgetizing_Themes">follow these instructions</a>.' ); ?></p>
+	<h2><?php echo wp_specialchars( $title ); ?></h2>
+		<div class="error">
+			<p><?php _e( 'No Sidebars Defined' ); ?></p>
+		</div>
+		<p><?php _e( 'The theme you are currently using isn&#8217;t widget-aware, meaning that it has no sidebars that you are able to change. For information on making your theme widget-aware, please <a href="http://codex.wordpress.org/Widgetizing_Themes">follow these instructions</a>.' ); ?></p>
 	</div>
 
 <?php
