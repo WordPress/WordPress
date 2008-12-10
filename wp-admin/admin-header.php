04c819feca3ad7815027744026af60e8ec9b1ae9
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-12-10 09:26:23 +0000

    Bringing "Visit Site" link back because lack thereof caused massive confusion from people who didn't know to click on the blog title, so a helpful hint.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@10168 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -101,7 +101,7 @@ if ( function_exists('mb_strlen') ) {
 }
 ?>
 
-<img id="header-logo" src="../wp-includes/images/blank.gif" alt="" width="32" height="32" /> <h1 <?php echo $title_class ?>><a href="<?php echo trailingslashit( get_bloginfo('url') ); ?>" title="<?php _e('Visit site') ?>"><?php echo $blog_name ?></a></h1>
+<img id="header-logo" src="../wp-includes/images/blank.gif" alt="" width="32" height="32" /> <h1 <?php echo $title_class ?>><a href="<?php echo trailingslashit( get_bloginfo('url') ); ?>" title="<?php _e('Visit site') ?>"><?php echo $blog_name ?> <span>&larr; <?php _e('Visit site') ?></span></a></h1>
 
 <div id="wphead-info">
 <div id="user_info">
