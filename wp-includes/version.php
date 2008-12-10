05343e02b54ec23568ee0c8c4ca0163ba387a3d2
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-12-10 22:48:48 +0000

    2.7
    git-svn-id: http://svn.automattic.com/wordpress/tags/2.7@10186 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '2.7-RC2-10171';
+$wp_version = '2.7';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
