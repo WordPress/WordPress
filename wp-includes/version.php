fb5b14606082012a0f77318a8e46ecf50e9b17eb
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-12-11 18:02:53 +0000

    Prepare the branch for the inevitable.
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.7@10189 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '2.7';
+$wp_version = '2.7.1-alpha-10188';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
