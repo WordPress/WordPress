0575c94329656f905d1bc1342ecd73fb61773391
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2008-12-11 18:00:28 +0000

    Make trunk bleed again.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@10188 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '2.7';
+$wp_version = '2.8-bleeding-10187';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
