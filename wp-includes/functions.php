1da228d1389756af9b3d1e4335bab231dfa74698
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-01-13 14:48:30 +0000

    Faster wp_clone function fix, props sambauers, fixes #8844 for 2.7
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.7@10354 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -2910,7 +2910,7 @@ function wp_clone( $object ) {
 	if ( !isset( $can_clone ) ) {
 		$can_clone = version_compare( phpversion(), '5.0', '>=' );
 	}
-	return $can_clone ? clone $object : $object;
+	return $can_clone ? clone( $object ) : $object;
 }
 
 
