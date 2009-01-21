f19f65a5e5855dd4d01485742d74bad8c7c2d551
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-01-21 19:47:51 +0000

    Only redirect categories if one "cat" positive integer is specified. fixes #8784 for trunk
    git-svn-id: http://svn.automattic.com/wordpress/trunk@10396 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/canonical.php
+++ wp-includes/canonical.php
@@ -119,7 +119,7 @@ function redirect_canonical($requested_url=null, $do_redirect=true) {
 		} elseif ( is_year() && !empty($_GET['year']) ) {
 			if ( $redirect_url = get_year_link(get_query_var('year')) )
 				$redirect['query'] = remove_query_arg('year', $redirect['query']);
-		} elseif ( is_category() && !empty($_GET['cat']) ) {
+		} elseif ( is_category() && !empty($_GET['cat']) && preg_match( '|^[0-9]+$|', $_GET['cat'] ) ) {
 			if ( $redirect_url = get_category_link(get_query_var('cat')) )
 				$redirect['query'] = remove_query_arg('cat', $redirect['query']);
 		} elseif ( is_author() && !empty($_GET['author']) ) {
