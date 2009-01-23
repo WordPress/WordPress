a7d2a19c706c186719f8c795f660be157568d954
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-01-23 17:55:16 +0000

    Sort out the error_reporting for PHP 5.3. See #8701.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@10420 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-settings.php
+++ wp-settings.php
@@ -201,9 +201,9 @@ timer_start();
 if (defined('WP_DEBUG') and WP_DEBUG == true) {
 	error_reporting(E_ALL);
 } else {
-	if ( defined( 'E_DEPRECATED' ) )
-		error_reporting(E_ALL ^ E_STRICT ^ E_DEPRECATED ^ E_NOTICE ^ E_USER_NOTICE);
-	else if ( defined( 'E_STRICT' ) )
+	if ( defined( 'E_DEPRECATED' ) ) // Introduced in PHP 5.3
+		error_reporting( (E_ALL ^ E_DEPRECATED ^ E_NOTICE ^ E_USER_NOTICE) & E_STRICT);
+	else if ( defined( 'E_STRICT' ) ) // Introduced in PHP 5.0 not part of E_ALL
 		error_reporting( ( E_ALL ^ E_NOTICE ^ E_USER_NOTICE ) & E_STRICT);
 	else
 		error_reporting(E_ALL ^ E_NOTICE ^ E_USER_NOTICE);
