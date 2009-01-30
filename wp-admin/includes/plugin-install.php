cb35d830c4e337d3a5a562346057e21226e8c7ae
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-01-30 14:30:30 +0000

    Typo fix: occured/occurred, props demetris, fixes #8950
    git-svn-id: http://svn.automattic.com/wordpress/trunk@10468 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/plugin-install.php
+++ wp-admin/includes/plugin-install.php
@@ -40,11 +40,11 @@ function plugins_api($action, $args = null) {
 	if ( ! $res ) {
 		$request = wp_remote_post('http://api.wordpress.org/plugins/info/1.0/', array( 'body' => array('action' => $action, 'request' => serialize($args))) );
 		if ( is_wp_error($request) ) {
-			$res = new WP_Error('plugins_api_failed', __('An Unexpected HTTP Error occured during the API request.</p> <p><a href="?" onclick="document.location.reload(); return false;">Try again</a>'), $request->get_error_message() );
+			$res = new WP_Error('plugins_api_failed', __('An Unexpected HTTP Error occurred during the API request.</p> <p><a href="?" onclick="document.location.reload(); return false;">Try again</a>'), $request->get_error_message() );
 		} else {
 			$res = unserialize($request['body']);
 			if ( ! $res )
-				$res = new WP_Error('plugins_api_failed', __('An unknown error occured'), $request['body']);
+				$res = new WP_Error('plugins_api_failed', __('An unknown error occurred'), $request['body']);
 		}
 	}
 
