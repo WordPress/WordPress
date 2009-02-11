1592a20d3993c0ebbbd1757fa8c4ff8736821369
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-02-11 20:15:57 +0000

    Commit hook test. fixes #9099
    git-svn-id: http://svn.automattic.com/wordpress/trunk@10548 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '2.8-bleeding';
+$wp_version = '2.8-bleeding-edge';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
