8da10297ba6c718426fc791b8c28ecc74ab62722
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-02-27 17:14:22 +0000

    Explain consequences of changing the *_KEY defines. Fixes #9181.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@10659 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -38,6 +38,7 @@ define('DB_COLLATE', '');
  *
  * Change these to different unique phrases!
  * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies this does mean that all users will have to login again.
  *
  * @since 2.6.0
  */
