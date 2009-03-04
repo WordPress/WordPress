15d96da0f46c2859b03e7554aa0244b381e5c16e
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-03-04 08:20:32 +0000

    Display the search query when searching users. Fixes #9265 for 2.7.2 props johnbillion.
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.7@10695 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/users.php
+++ wp-admin/users.php
@@ -240,8 +240,8 @@ if ( ! empty($messages) ) {
 <div class="wrap">
 <?php screen_icon(); ?>
 <h2><?php echo wp_specialchars( $title );
-if ( isset($_GET['s']) && $_GET['s'] )
-	printf( '<span class="subtitle">' . __('Search results for &#8220;%s&#8221;') . '</span>', wp_specialchars( get_search_query() ) ); ?>
+if ( isset($_GET['usersearch']) && $_GET['usersearch'] )
+	printf( '<span class="subtitle">' . __('Search results for &#8220;%s&#8221;') . '</span>', wp_specialchars( $_GET['usersearch'] ) ); ?>
 </h2>
 
 <div class="filter">
