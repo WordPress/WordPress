32ed8e4b8a61f12203ac4bd300f110f84b1029a1
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-03-04 19:30:16 +0000

    Nested install fixes. Props sivel. fixes #7997 for trunk
    git-svn-id: http://svn.automattic.com/wordpress/trunk@10698 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-load.php
+++ wp-load.php
@@ -26,9 +26,9 @@ if ( file_exists( ABSPATH . 'wp-config.php') ) {
 	/** The config file resides in ABSPATH */
 	require_once( ABSPATH . 'wp-config.php' );
 
-} elseif ( file_exists( dirname(ABSPATH) . '/wp-config.php' ) && ! file_exists( dirname(ABSPATH) . '/wp-load.php' ) ) {
+} elseif ( file_exists( dirname(ABSPATH) . '/wp-config.php' ) && ! file_exists( dirname(ABSPATH) . '/wp-settings.php' ) ) {
 
-	/** The config file resides one level below ABSPATH */
+	/** The config file resides one level above ABSPATH but is not part of another install*/
 	require_once( dirname(ABSPATH) . '/wp-config.php' );
 
 } else {
