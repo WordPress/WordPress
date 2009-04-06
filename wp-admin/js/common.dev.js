25c13543f2be65250d5ef2d02d5430e9523d4271
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-04-06 22:21:45 +0000

    Fix confirmation for second bulk delete, fixes #9473
    git-svn-id: http://svn.automattic.com/wordpress/trunk@10881 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/common.dev.js
+++ wp-admin/js/common.dev.js
@@ -174,7 +174,7 @@ jQuery(document).ready( function($) {
 
 	// show warnings
 	$('#doaction, #doaction2').click(function(){
-		if ( $('select[name^="action"]').val() == 'delete' ) {
+		if ( $('select[name="action"]').val() == 'delete' || $('select[name="action2"]').val() == 'delete' ) {
 			return showNotice.warn();
 		}
 	});
