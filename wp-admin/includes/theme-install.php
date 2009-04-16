7bdbfe50d7fc6ec77e9f39cddb7a2c1da859aaad
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-04-16 03:46:45 +0000

    Fix theme install downloads. Props josephscott. fixes #9543
    git-svn-id: http://svn.automattic.com/wordpress/trunk@10942 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/theme-install.php
+++ wp-admin/includes/theme-install.php
@@ -618,7 +618,6 @@ function do_theme_install($download_url, $theme_information = null) {
 
 	$theme        = isset($_REQUEST['theme'])        ? stripslashes( $_REQUEST['theme'] )        : '';
 	$theme_name   = isset($_REQUEST['theme_name'])   ? stripslashes( $_REQUEST['theme_name'] )   : '';
-	$download_url = isset($_REQUEST['download_url']) ? stripslashes( $_REQUEST['download_url'] ) : '';
 
 	$url = 'theme-install.php?tab=install';
 	$url = add_query_arg(array('theme' => $theme, 'theme_name' => $theme_name, 'download_url' => $download_url ), $url);
