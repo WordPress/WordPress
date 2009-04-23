4f250c27cf627ccf663f759f77e536272fbdc3f9
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-04-23 21:45:38 +0000

    Remove slash from plugin_basename(), props alexrabe, fixes #9561
    git-svn-id: http://svn.automattic.com/wordpress/trunk@11072 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/plugin.php
+++ wp-includes/plugin.php
@@ -494,7 +494,7 @@ function plugin_basename($file) {
 	$plugin_dir = preg_replace('|/+|','/', $plugin_dir); // remove any duplicate slash
 	$mu_plugin_dir = str_replace('\\','/',WPMU_PLUGIN_DIR); // sanitize for Win32 installs
 	$mu_plugin_dir = preg_replace('|/+|','/', $mu_plugin_dir); // remove any duplicate slash
-	$file = preg_replace('#^' . preg_quote($plugin_dir, '#') . '|^' . preg_quote($mu_plugin_dir, '#') . '/#','',$file); // get relative path from plugins dir
+	$file = preg_replace('#^' . preg_quote($plugin_dir, '#') . '/|^' . preg_quote($mu_plugin_dir, '#') . '/#','',$file); // get relative path from plugins dir
 	return $file;
 }
 
