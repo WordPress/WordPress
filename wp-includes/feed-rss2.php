18e6d80bc18e1a8091f272cea49ec32eca53846e
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-05-05 20:38:47 +0000

    Use lastBuildDate. Props peaceablewhale. fixes #9566
    git-svn-id: http://svn.automattic.com/wordpress/trunk@11206 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/feed-rss2.php
+++ wp-includes/feed-rss2.php
@@ -24,7 +24,7 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 	<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
 	<link><?php bloginfo_rss('url') ?></link>
 	<description><?php bloginfo_rss("description") ?></description>
-	<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_lastpostmodified('GMT'), false); ?></pubDate>
+	<lastBuildDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_lastpostmodified('GMT'), false); ?></lastBuildDate>
 	<?php the_generator( 'rss2' ); ?>
 	<language><?php echo get_option('rss_language'); ?></language>
 	<sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'hourly' ); ?></sy:updatePeriod>
