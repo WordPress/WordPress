a5e73d3b9bcd3b7e9c40deafca42cd23f10532dc
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-05-16 05:15:21 +0000

    Fix feed autodiscovery. Props peaceablewhale. fixes #9810
    git-svn-id: http://svn.automattic.com/wordpress/trunk@11358 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/feed-rss.php
+++ wp-includes/feed-rss.php
@@ -5,7 +5,7 @@
  * @package WordPress
  */
 
-header('Content-Type: ' . feed_content_type('rss') . '; charset=' . get_option('blog_charset'), true);
+header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
 $more = 1;
 
 echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
--- wp-includes/feed-rss2-comments.php
+++ wp-includes/feed-rss2-comments.php
@@ -5,7 +5,7 @@
  * @package WordPress
  */
 
-header('Content-Type: ' . feed_content_type('rss2') . '; charset=' . get_option('blog_charset'), true);
+header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
 
 echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
 ?>
