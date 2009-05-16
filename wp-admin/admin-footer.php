e8b025b971308c06c5f8d3eb78fa7b445004fa1a
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-05-16 05:05:11 +0000

    Check if get_site_option() is defined. It isn't when auto upgrading from <= 2.7.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@11357 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -27,8 +27,12 @@ do_action('admin_footer', '');
 do_action('admin_print_footer_scripts');
 do_action("admin_footer-$hook_suffix");
 
-if ( false === get_site_option('can_compress_scripts') )
-	compression_test();
+// get_site_option() won't exist when auto upgrading from <= 2.7
+if ( function_exists('get_site_option') ) {
+	if ( false === get_site_option('can_compress_scripts') )
+		compression_test();
+}
+
 ?>
 
 <script type="text/javascript">if(typeof wpOnload=='function')wpOnload();</script>
