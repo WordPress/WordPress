f6a809c4afaa22c8cb1281e057014bb0c74af60b
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-05-28 16:50:39 +0000

    Note that paging applies to top-level comment counts. Props hailin. fixes #9900
    git-svn-id: http://svn.automattic.com/wordpress/trunk@11475 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options-discussion.php
+++ wp-admin/options-discussion.php
@@ -83,7 +83,7 @@ $default_comments_page .= '>' . __('last') . '</option><option value="oldest"';
 if ( 'oldest' == get_option('default_comments_page') ) $default_comments_page .= ' selected="selected"';
 $default_comments_page .= '>' . __('first') . '</option></select>';
 
-printf( __('Break comments into pages with %1$s comments per page and the %2$s page displayed by default'), '</label><label for="comments_per_page"><input name="comments_per_page" type="text" id="comments_per_page" value="' . esc_attr(get_option('comments_per_page')) . '" class="small-text" />', $default_comments_page );
+printf( __('Break comments into pages with %1$s top level comments per page and the %2$s page displayed by default'), '</label><label for="comments_per_page"><input name="comments_per_page" type="text" id="comments_per_page" value="' . esc_attr(get_option('comments_per_page')) . '" class="small-text" />', $default_comments_page );
 
 ?></label>
 <br />
