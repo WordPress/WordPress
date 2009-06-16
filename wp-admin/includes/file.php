9d293d9f0ecc5fd60131ca507fde44360b41c9b3
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-06-16 19:39:31 +0000

    Increase download_url() timeout from 30 to 60. see #10104
    git-svn-id: http://svn.automattic.com/wordpress/trunk@11587 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/file.php
+++ wp-admin/includes/file.php
@@ -445,7 +445,7 @@ function download_url( $url ) {
 	if ( ! $handle )
 		return new WP_Error('http_no_file', __('Could not create Temporary file'));
 
-	$response = wp_remote_get($url, array('timeout' => 30));
+	$response = wp_remote_get($url, array('timeout' => 60));
 
 	if ( is_wp_error($response) ) {
 		fclose($handle);
