fc507c466e5c0dc951190d8781bc6a3e6cce0f7d
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-08-27 21:23:52 +0000

    Comments for default theme Pages. Fixes #3753.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@11885 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/page.php
+++ wp-content/themes/default/page.php
@@ -20,6 +20,9 @@ get_header(); ?>
 		</div>
 		<?php endwhile; endif; ?>
 	<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+	
+	<?php comments_template(); ?>
+	
 	</div>
 
 <?php get_sidebar(); ?>
