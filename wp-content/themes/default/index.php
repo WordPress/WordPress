83364a9737f52efdaaadbfa7edf016ced116b977
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-10-15 14:20:35 +0000

    Revert errant local theme modifications in [12036]. Note to self: svn status is your friend.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@12038 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/default/index.php
+++ wp-content/themes/default/index.php
@@ -17,9 +17,6 @@ get_header(); ?>
 				<small><?php the_time('F jS, Y') ?> <!-- by <?php the_author() ?> --></small>
 
 				<div class="entry">
-					<?php if ( has_post_image() ) : ?>
-						<p class="post-thumbnail"><?php the_post_image( array( '150', '150') ) ?></p>
-					<?php endif; ?>
 					<?php the_content('Read the rest of this entry &raquo;'); ?>
 				</div>
 
