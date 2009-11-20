4cb414fc176888d5b9562100ebd29e8d432b8d13
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-11-20 21:59:34 +0000

    Restore 'Add New Themes' menu name. Props  nacin. fixes #10738
    git-svn-id: http://svn.automattic.com/wordpress/trunk@12248 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/menu.php
+++ wp-admin/menu.php
@@ -70,7 +70,7 @@ $menu[59] = array( '', 'read', 'separator2', '', 'wp-menu-separator' );
 $menu[60] = array( __('Appearance'), 'switch_themes', 'themes.php', '', 'menu-top', 'menu-appearance', 'div' );
 	$submenu['themes.php'][5]  = array(__('Themes'), 'switch_themes', 'themes.php');
 	$submenu['themes.php'][10] = array(__('Editor'), 'edit_themes', 'theme-editor.php');
-	$submenu['themes.php'][15] = array(_x('Add New', 'theme'), 'install_themes', 'theme-install.php');
+	$submenu['themes.php'][15] = array(__('Add New Themes'), 'install_themes', 'theme-install.php');
 
 $update_plugins = get_transient( 'update_plugins' );
 $update_count = 0;
