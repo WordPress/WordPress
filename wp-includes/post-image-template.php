9957a119b3baa93fe43f3568e985ae5017a61256
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-12-03 15:59:27 +0000

    phpdoc for post thumbnail template functions. Props ramiy. fixes #11310
    git-svn-id: http://svn.automattic.com/wordpress/trunk@12320 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post-image-template.php
+++ wp-includes/post-image-template.php
@@ -9,22 +9,55 @@
  * @subpackage Template
  */
 
+/**
+ * Check if post has an image attached.
+ * 
+ * @since 2.9.0
+ *
+ * @param int $post_id Optional. Post ID.
+ * @return bool Whether post has an image attached (true) or not (false).
+ */
 function has_post_image( $post_id = NULL ) {
 	global $id;
 	$post_id = ( NULL === $post_id ) ? $id : $post_id;
 	return !! get_post_image_id( $post_id );
 }
 
+/**
+ * Retrieve Post Image ID.
+ * 
+ * @since 2.9.0
+ *
+ * @param int $post_id Optional. Post ID.
+ * @return int
+ */
 function get_post_image_id( $post_id = NULL ) {
 	global $id;
 	$post_id = ( NULL === $post_id ) ? $id : $post_id;
 	return get_post_meta( $post_id, '_thumbnail_id', true );
 }
 
+/**
+ * Display Post Image.
+ * 
+ * @since 2.9.0
+ *
+ * @param int $size Optional. Image size.  Defaults to 'thumbnail'.
+ * @param string|array $attr Optional. Query string or array of attributes.
+ */
 function the_post_image( $size = 'thumbnail', $attr = '' ) {
 	echo get_the_post_image( NULL, $size, $attr );
 }
 
+/**
+ * Retrieve Post Image.
+ * 
+ * @since 2.9.0
+ *
+ * @param int $post_id Optional. Post ID.
+ * @param string $size Optional. Image size.  Defaults to 'thumbnail'.
+ * @param string|array $attr Optional. Query string or array of attributes.
+  */
 function get_the_post_image( $post_id = NULL, $size = 'thumbnail', $attr = '' ) {
 	global $id;
 	$post_id = ( NULL === $post_id ) ? $id : $post_id;
