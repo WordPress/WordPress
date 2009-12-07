706def0206d58d705a7c99f72ef94ec6b962e6c2
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-12-07 18:09:11 +0000

    Do not display comments on non-public posts in comments widget. props Denis-de-Bernardy. fixes #9211
    git-svn-id: http://svn.automattic.com/wordpress/trunk@12333 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-widgets.php
+++ wp-includes/default-widgets.php
@@ -634,7 +634,7 @@ class WP_Widget_Recent_Comments extends WP_Widget {
 			$number = 15;
 
 		if ( !$comments = wp_cache_get( 'recent_comments', 'widget' ) ) {
-			$comments = $wpdb->get_results("SELECT * FROM $wpdb->comments WHERE comment_approved = '1' ORDER BY comment_date_gmt DESC LIMIT 15");
+			$comments = $wpdb->get_results("SELECT $wpdb->comments.* FROM $wpdb->comments JOIN $wpdb->posts ON $wpdb->posts.ID = $wpdb->comments.comment_post_ID WHERE comment_approved = '1' AND post_status = 'publish' ORDER BY comment_date_gmt DESC LIMIT 15");
 			wp_cache_add( 'recent_comments', $comments, 'widget' );
 		}
 
