182ec07a449922b19043414e8ec7b6ee787019a3
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-12-10 09:13:11 +0000

    Strip HTML from comments_popup_link title attribute. props ewestp, xenlab. fixes #10997
    git-svn-id: http://svn.automattic.com/wordpress/trunk@12362 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment-template.php
+++ wp-includes/comment-template.php
@@ -970,7 +970,7 @@ function comments_popup_link( $zero = false, $one = false, $more = false, $css_c
 	if ( !empty( $css_class ) ) {
 		echo ' class="'.$css_class.'" ';
 	}
-	$title = esc_attr( get_the_title() );
+	$title = the_title_attribute( 'echo=0' );
 
 	echo apply_filters( 'comments_popup_link_attributes', '' );
 
