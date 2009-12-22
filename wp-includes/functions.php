41dd7d25b729b64f84fe59fd30ec0d658a99de1b
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2009-12-22 13:42:58 +0000

    Revert [12485] as it breaks the functionality of is_serialized() and make more strings appear serialized. See #9930.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@12486 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -245,7 +245,29 @@ function maybe_unserialize( $original ) {
  * @return bool False if not serialized and true if it was.
  */
 function is_serialized( $data ) {
-	return is_string($data) && preg_match('/^(N;)|([aOs]:[0-9]+:.*[;}])|([bid]:[0-9.E+-]+;)$/s', $data);
+	// if it isn't a string, it isn't serialized
+	if ( !is_string( $data ) )
+		return false;
+	$data = trim( $data );
+	if ( 'N;' == $data )
+		return true;
+	if ( !preg_match( '/^([adObis]):/', $data, $badions ) )
+		return false;
+	switch ( $badions[1] ) {
+		case 'a' :
+		case 'O' :
+		case 's' :
+			if ( preg_match( "/^{$badions[1]}:[0-9]+:.*[;}]\$/s", $data ) )
+				return true;
+			break;
+		case 'b' :
+		case 'i' :
+		case 'd' :
+			if ( preg_match( "/^{$badions[1]}:[0-9.E-]+;\$/", $data ) )
+				return true;
+			break;
+	}
+	return false;
 }
 
 /**
