c7dae7d3beb7dae38ea518d7253d2cb6ad406f3b
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-01-07 07:32:44 +0000

    phpDoc for ms-deprecated.php props nacin. Make generate_random_password use wp_generate_password() Fixes #11746.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@12635 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-deprecated.php
+++ wp-includes/ms-deprecated.php
@@ -1,10 +1,26 @@
 <?php
+/**
+ * Deprecated functions from WordPress MU and the multisite feature. You shouldn't
+ * use these functions and look for the alternatives instead. The functions will be
+ * removed in a later version.
+ *
+ * @package WordPress
+ * @subpackage Deprecated
+ */
 
+/*
+ * Deprecated functions come here to die.
+ */
+ 
+/**
+ * @since unknown
+ * @deprecated 3.0
+ * @deprecated Use wp_generate_password()
+ * @see wp_generate_password()
+ */
 function generate_random_password( $len = 8 ) {
-	_deprecated_function( __FUNCTION__, '3.0', 'wp_generarte_password()' );
-	$random_password = substr(md5(uniqid(microtime())), 0, intval( $len ) );
-	$random_password = apply_filters('random_password', $random_password);
-	return $random_password;
+	_deprecated_function( __FUNCTION__, '3.0', 'wp_generate_password()' );
+	return wp_generate_password($len);
 }
 
 ?>
