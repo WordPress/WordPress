48aec5f822694203667bbe83af6867cebcf64d9e
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-02-02 22:58:06 +0000

    phpdoc for global_terms(). see #11644
    git-svn-id: http://svn.automattic.com/wordpress/trunk@12932 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-functions.php
+++ wp-includes/ms-functions.php
@@ -1703,7 +1703,16 @@ function fix_import_form_size( $size ) {
 		return $size; // default
 }
 
-/* Global Categories */
+/**
+ * Maintains a canonical list of terms by syncing terms created for each blog with the global terms table.
+ *
+ * @since 3.0.0
+ *
+ * @see term_id_filter
+ *
+ * @param int $term_id An ID for a term on the current blog.
+ * @return int An ID from the global terms table mapped from $term_id.
+ */
 function global_terms( $term_id, $deprecated = '' ) {
 	global $wpdb;
 
