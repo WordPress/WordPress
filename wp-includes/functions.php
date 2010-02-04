4cf8bb890d6b75d320fe3c1557602596191933a0
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-02-04 22:00:48 +0000

    Fix bug in _search_terms_tidy. Fixes #12060 for 2.9 branch props miqrogroove.
    git-svn-id: http://svn.automattic.com/wordpress/branches/2.9@12959 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -3635,6 +3635,6 @@ function get_file_data( $file, $default_headers, $context = '' ) {
  * @since 2.9.0
  */
 function _search_terms_tidy($t) {
-	return trim($t, "\"\'\n\r ");
+	return trim($t, "\"'\n\r ");
 }
 ?>
