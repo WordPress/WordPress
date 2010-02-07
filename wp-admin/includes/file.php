ca925d3bfa9cbd0cd5576d173537df178a1fefb7
dd32 <dd32@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-02-07 05:07:36 +0000

    PHPdoc for _unzip_file_pclzip & _unzip_file_ziparchive. See #10403
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13007 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/file.php
+++ wp-admin/includes/file.php
@@ -534,6 +534,19 @@ function unzip_file($file, $to) {
 		return _unzip_file_pclzip($file, $to, $needed_dirs);
 }
 
+/**
+ * This function should not be called directly, use unzip_file instead. Attempts to unzip an archive using the ZipArchive class.
+ * Assumes that WP_Filesystem() has already been called and set up.
+ *
+ * @since 3.0
+ * @see unzip_file
+ * @access private
+ *
+ * @param string $file Full path and filename of zip archive
+ * @param string $to Full path on the filesystem to extract archive to
+ * @param array $needed_dirs A partial list of required folders needed to be created.
+ * @return mixed WP_Error on failure, True on success
+ */
 function _unzip_file_ziparchive($file, $to, $needed_dirs = array()) {
 	global $wp_filesystem;
 
@@ -579,6 +592,19 @@ function _unzip_file_ziparchive($file, $to, $needed_dirs = array()) {
 	return true;
 }
 
+/**
+ * This function should not be called directly, use unzip_file instead. Attempts to unzip an archive using the PclZip library.
+ * Assumes that WP_Filesystem() has already been called and set up.
+ *
+ * @since 3.0
+ * @see unzip_file
+ * @access private
+ *
+ * @param string $file Full path and filename of zip archive
+ * @param string $to Full path on the filesystem to extract archive to
+ * @param array $needed_dirs A partial list of required folders needed to be created.
+ * @return mixed WP_Error on failure, True on success
+ */
 function _unzip_file_pclzip($file, $to, $needed_dirs = array()) {
 	global $wp_filesystem;
 
