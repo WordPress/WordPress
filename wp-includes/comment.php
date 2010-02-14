775bebfade1e5d2662524e3076f40837eb92bdd0
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-02-14 10:59:37 +0000

    Adjust regex for counting links in a comment when checking if it needs to be held for moderation. Also fix notice in wp_new_comment(). see #11830
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13150 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment.php
+++ wp-includes/comment.php
@@ -45,7 +45,7 @@ function check_comment($author, $email, $url, $comment, $user_ip, $user_agent, $
 	if ( 1 == get_option('comment_moderation') )
 		return false; // If moderation is set to manual
 
-	if ( get_option('comment_max_links') && preg_match_all("/<[Aa][^>]*[Hh][Rr][Ee][Ff]=['\"]([^\"'>]+)[^>]*>/", apply_filters('comment_text',$comment), $out) >= get_option('comment_max_links') )
+	if ( get_option( 'comment_max_links' ) && preg_match_all( '/<a [^>]*href/i', apply_filters( 'comment_text', $comment ), $out) >= get_option( 'comment_max_links' ) )
 		return false; // Check # of external links
 
 	$mod_keys = trim(get_option('moderation_keys'));
@@ -1267,7 +1267,7 @@ function wp_new_comment( $commentdata ) {
 
 		$post = &get_post($commentdata['comment_post_ID']); // Don't notify if it's your own comment
 
-		if ( get_option('comments_notify') && $commentdata['comment_approved'] && $post->post_author != $commentdata['user_id'] )
+		if ( isset( $commentdata['user_id'] ) && get_option('comments_notify') && $commentdata['comment_approved'] && $post->post_author != $commentdata['user_id'] )
 			wp_notify_postauthor($comment_ID, $commentdata['comment_type']);
 	}
 
