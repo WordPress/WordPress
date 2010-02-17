252bd08b8cf0cdb0d2a60d0d5362c1eb37ac81cf
iammattthomas <iammattthomas@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-02-17 19:05:39 +0000

    Adding single-column, no sidebar page template to twentyten. See #9015
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13179 1a063a9b-81f0-0310-95a4-ce76da25c4cd

---
+++ wp-content/themes/twentyten/onecolumn-page.php
@@ -0,0 +1,28 @@
+
+<?php
+/*
+Template Name: One column, no sidebar
+Description: A template with no sidebar
+*/
+get_header(); ?>
+
+		<div id="container" class="onecolumn">
+			<div id="content">
+
+<?php the_post(); ?>
+
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php wp_link_pages( 'before=<div class="page-link">' . __( 'Pages:', 'twentyten' ) . '&after=</div>'); ?>
+						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+					</div><!-- .entry-content -->
+				</div><!-- #post-<?php the_ID(); ?> -->
+
+				<?php comments_template( '', true ); ?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_footer(); ?>
