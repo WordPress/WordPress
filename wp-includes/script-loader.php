16dd7cbc3264c373031f7786e11dd6fc4db49001
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-02-19 22:55:01 +0000

    Add ms.css to script-loader.php. See #11644
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13237 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -444,6 +444,7 @@ function wp_default_styles( &$styles ) {
 	$styles->add( 'colors-classic', "/wp-admin/css/colors-classic$suffix.css", array(), $colors_version);
 	$styles->add_data( 'colors-classic', 'rtl', true );
 
+	$styles->add( 'ms', "/wp-admin/css/ms$suffix.css", array(), '20100219' );
 	$styles->add( 'global', "/wp-admin/css/global$suffix.css", array(), '20100108' );
 	$styles->add( 'media', "/wp-admin/css/media$suffix.css", array(), '20091029' );
 	$styles->add( 'widgets', "/wp-admin/css/widgets$suffix.css", array(), '20091118' );
