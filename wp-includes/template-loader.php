8ad91e8b54e4f4d323ee84baef663a4b44a5d5b3
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-02-20 12:58:25 +0000

    Add template_include filter, to modify the template path before it is included. Fixes #11242
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13252 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/template-loader.php
+++ wp-includes/template-loader.php
@@ -3,76 +3,44 @@
  * Loads the correct template based on the visitor's url
  * @package WordPress
  */
-if ( defined('WP_USE_THEMES') && WP_USE_THEMES ) {
+if ( defined('WP_USE_THEMES') && WP_USE_THEMES )
 	do_action('template_redirect');
-	if ( is_robots() ) {
-		do_action('do_robots');
-		return;
-	} else if ( is_feed() ) {
-		do_feed();
-		return;
-	} else if ( is_trackback() ) {
-		include(ABSPATH . 'wp-trackback.php');
-		return;
-	} else if ( is_404() && $template = get_404_template() ) {
-		include($template);
-		return;
-	} else if ( is_search() && $template = get_search_template() ) {
-		include($template);
-		return;
-	} else if ( is_tax() && $template = get_taxonomy_template()) {
-		include($template);
-		return;
-	} else if ( is_home() && $template = get_home_template() ) {
-		include($template);
-		return;
-	} else if ( is_attachment() && $template = get_attachment_template() ) {
+
+// Process feeds and trackbacks even if not using themes.
+if ( is_robots() ) :
+	do_action('do_robots');
+	return;
+elseif ( is_feed() ) :
+	do_feed();
+	return;
+elseif ( is_trackback() ) :
+	include( ABSPATH . 'wp-trackback.php' );	
+	return;
+endif;
+
+if ( defined('WP_USE_THEMES') && WP_USE_THEMES ) :
+	$template = false;
+	if     ( is_404()            && $template = get_404_template()            ) :
+	elseif ( is_search()         && $template = get_search_template()         ) :
+	elseif ( is_tax()            && $template = get_taxonomy_template()       ) :
+	elseif ( is_home()           && $template = get_home_template()           ) :
+	elseif ( is_attachment()     && $template = get_attachment_template()     ) :
 		remove_filter('the_content', 'prepend_attachment');
-		include($template);
-		return;
-	} else if ( is_single() && $template = get_single_template() ) {
-		include($template);
-		return;
-	} else if ( is_page() && $template = get_page_template() ) {
-		include($template);
-		return;
-	} else if ( is_category() && $template = get_category_template()) {
-		include($template);
-		return;
-	} else if ( is_tag() && $template = get_tag_template()) {
-		include($template);
-		return;
-	} else if ( is_author() && $template = get_author_template() ) {
-		include($template);
-		return;
-	} else if ( is_date() && $template = get_date_template() ) {
-		include($template);
-		return;
-	} else if ( is_archive() && $template = get_archive_template() ) {
-		include($template);
-		return;
-	} else if ( is_comments_popup() && $template = get_comments_popup_template() ) {
-		include($template);
-		return;
-	} else if ( is_paged() && $template = get_paged_template() ) {
-		include($template);
-		return;
-	} else if ( file_exists(TEMPLATEPATH . "/index.php") ) {
-		include(TEMPLATEPATH . "/index.php");
-		return;
-	}
-} else {
-	// Process feeds and trackbacks even if not using themes.
-	if ( is_robots() ) {
-		do_action('do_robots');
-		return;
-	} else if ( is_feed() ) {
-		do_feed();
-		return;
-	} else if ( is_trackback() ) {
-		include(ABSPATH . 'wp-trackback.php');
-		return;
-	}
-}
+	elseif ( is_single()         && $template = get_single_template()         ) :
+	elseif ( is_page()           && $template = get_page_template()           ) :
+	elseif ( is_category()       && $template = get_category_template()       ) :
+	elseif ( is_tag()            && $template = get_tag_template()            ) :
+	elseif ( is_author()         && $template = get_author_template()         ) :
+	elseif ( is_date()           && $template = get_date_template()           ) :
+	elseif ( is_archive()        && $template = get_archive_template()        ) :
+	elseif ( is_comments_popup() && $template = get_comments_popup_template() ) :
+	elseif ( is_paged()          && $template = get_paged_template()          ) :
+	elseif ( file_exists( TEMPLATEPATH . '/index.php' ) ) :
+		$template = TEMPLATEPATH . '/index.php';
+	endif;
+	if ( $template = apply_filters( 'template_include', $template ) )
+		include( $template );
+	return;
+endif;
 
 ?>
