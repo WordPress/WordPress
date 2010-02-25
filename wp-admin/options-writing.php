97090ba1a21dc18e3c93dc1339dcd49e0e40cd28
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-02-25 21:48:11 +0000

    Switch to using a filter for {en|dis}abling update services configuration. Fixes #12229. See #12381.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13412 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options-writing.php
+++ wp-admin/options-writing.php
@@ -114,7 +114,7 @@ wp_dropdown_categories(array('hide_empty' => 0, 'name' => 'default_email_categor
 <?php do_settings_fields('writing', 'post_via_email'); ?>
 </table>
 <?php } ?>
-<?php if ( !is_multisite() || ( defined( 'UPDATE_SERVICES' ) && UPDATE_SERVICES ) ) { ?>
+<?php if ( apply_filters( 'enable_update_services_configuration', true ) ) { ?>
 <h3><?php _e('Update Services') ?></h3>
 
 <?php if ( get_option('blog_public') ) : ?>
