b90b27a6d6b1db1076d5c4328a4d7e1b43928e02
dd32 <dd32@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-02-28 12:19:09 +0000

    Display PHP Start-up Errors/Warnings. Props Denis-de-Bernardy. Fixes #12395.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13499 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/plugins.php
+++ wp-admin/plugins.php
@@ -149,9 +149,9 @@ if ( !empty($action) ) {
 
 			if ( ! WP_DEBUG ) {
 				if ( defined('E_RECOVERABLE_ERROR') )
-					error_reporting(E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR);
+					error_reporting(E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR);
 				else
-					error_reporting(E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING);
+					error_reporting(E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING);
 			}
 
 			@ini_set('display_errors', true); //Ensure that Fatal errors are displayed.
--- wp-includes/load.php
+++ wp-includes/load.php
@@ -262,9 +262,9 @@ function wp_debug_mode() {
 		}
 	} else {
 		if ( defined( 'E_RECOVERABLE_ERROR' ) )
-			error_reporting( E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
+			error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
 		else
-			error_reporting( E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING );
+			error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING );
 	}
 }
 
