73a11f4d4ec8b4d23ff9446a45ae38ff818ac0d6
matt <matt@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-03-02 21:59:15 +0000

    Better subdomains for faster Gravatar loading. An update to [13385].
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13560 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -1619,9 +1619,9 @@ function get_avatar( $id_or_email, $size = '96', $default = '', $alt = false ) {
 		$host = 'https://secure.gravatar.com';
 	} else {
 		if ( !empty($email) )
-			$host = sprintf( "http://w%d.gravatar.com", ( hexdec( $email_hash{0} ) % 2 ) );
+			$host = sprintf( "http://%d.gravatar.com", ( hexdec( $email_hash{0} ) % 2 ) );
 		else
-			$host = 'http://www.gravatar.com';
+			$host = 'http://0.gravatar.com';
 	}
 
 	if ( 'mystery' == $default )
