4ca0960178c096b5bf5fd94f71778c37a49435e1
dd32 <dd32@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-03-13 04:05:54 +0000

    Force Theme update check on GUU. See #12528
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13687 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/update.php
+++ wp-includes/update.php
@@ -323,6 +323,7 @@ add_action( 'wp_update_plugins', 'wp_update_plugins' );
 
 add_action( 'load-themes.php', 'wp_update_themes' );
 add_action( 'load-update.php', 'wp_update_themes' );
+add_action( 'load-update-core.php', 'wp_update_themes' );
 add_action( 'admin_init', '_maybe_update_themes' );
 add_action( 'wp_update_themes', 'wp_update_themes' );
 
