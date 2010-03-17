6fe61df60f7b126332988d9e8f5b32ee3dd2445d
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-03-17 17:42:49 +0000

    Introduce WP_DEFAULT_THEME
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13737 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-constants.php
+++ wp-includes/default-constants.php
@@ -284,12 +284,20 @@ function wp_templating_constants( ) {
 	define('STYLESHEETPATH', get_stylesheet_directory());
 
 	/**
+	 * Slug of the default theme for this install.
+	 * Used as the default theme when installing new sites.
+	 * @since 3.0.0
+	 */
+	if ( !defined('WP_DEFAULT_THEME') )
+		define( 'WP_DEFAULT_THEME', 'twentyten' );
+
+	/**
 	 * Slug of the fallback theme for this install.
 	 * Will be used as the fallback if the current theme doesn't exist.
 	 * @since 3.0.0
 	 */
 	if ( !defined('WP_FALLBACK_THEME') )
-		define( 'WP_FALLBACK_THEME', 'twentyten' );
+		define( 'WP_FALLBACK_THEME', WP_DEFAULT_THEME );
 }
 
 ?>
