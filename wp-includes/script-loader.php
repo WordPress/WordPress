cea2e6eaa3c88380bffd275ba190507e23b8bfe2
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-03-17 20:44:59 +0000

    Remove JS nav-menu functions that we no longer use. see #11817
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13742 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -397,7 +397,7 @@ function wp_default_scripts( &$scripts ) {
 		) );
 
 		// Custom Navigation
-		$scripts->add( 'nav-menu', "/wp-admin/js/nav-menu$suffix.js", false, '20100317' );
+		$scripts->add( 'nav-menu', "/wp-admin/js/nav-menu$suffix.js", false, '20100317b' );
 		$scripts->localize( 'nav-menu', 'navMenuL10n', array(
 			'custom' => _x('Custom', 'menu nav item type'),
 			'page' => _x('Page', 'menu nav item type'),
