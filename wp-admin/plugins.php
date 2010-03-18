cc03c9c11bf524758dfe9728be44d7f6c640fa39
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-03-18 20:50:00 +0000

    s/blog/site/. props ocean90.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13755 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/plugins.php
+++ wp-admin/plugins.php
@@ -10,7 +10,7 @@
 require_once('admin.php');
 
 if ( ! current_user_can( 'activate_plugins' ) )
-	wp_die( __( 'You do not have sufficient permissions to manage plugins for this blog.' ) );
+	wp_die( __( 'You do not have sufficient permissions to manage plugins for this site.' ) );
 
 if ( isset($_POST['clear-recent-list']) )
 	$action = 'clear-recent-list';
