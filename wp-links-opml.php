973c124105a295ed31b07edc9a9edaaa45af7baf
dd32 <dd32@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-03-18 08:39:01 +0000

    Clean up OPML link export script.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13747 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-links-opml.php
+++ wp-links-opml.php
@@ -12,54 +12,51 @@
  * @package WordPress
  */
 
-if (empty($wp)) {
+if ( empty($wp) ) {
 	require_once('./wp-load.php');
 	wp();
 }
 
 header('Content-Type: text/xml; charset=' . get_option('blog_charset'), true);
-$link_cat = $_GET['link_cat'];
-if ((empty ($link_cat)) || ($link_cat == 'all') || ($link_cat == '0')) {
-	$link_cat = '';
-} else { // be safe
-	$link_cat = '' . urldecode($link_cat) . '';
-	$link_cat = intval($link_cat);
+$link_cat = '';
+if ( !empty($_GET['link_cat']) ) {
+	$link_cat = $_GET['link_cat'];
+	if ( !in_array($link_cat, array('all', '0')) )
+		$link_cat = absint( (string)urldecode($link_cat) );
 }
-?><?php echo '<?xml version="1.0"?'.">\n"; ?>
+
+echo '<?xml version="1.0"?'.">\n";
+?>
 <opml version="1.0">
 	<head>
-		<title>Links for <?php echo esc_attr(get_bloginfo('name', 'display').$cat_name); ?></title>
+		<title><?php printf( __('Links for %s'), esc_attr(get_bloginfo('name', 'display')) ); ?></title>
 		<dateCreated><?php echo gmdate("D, d M Y H:i:s"); ?> GMT</dateCreated>
 		<?php do_action('opml_head'); ?>
 	</head>
 	<body>
 <?php
-
-if (empty ($link_cat))
+if ( empty($link_cat) )
 	$cats = get_categories(array('type' => 'link', 'hierarchical' => 0));
 else
 	$cats = get_categories(array('type' => 'link', 'hierarchical' => 0, 'include' => $link_cat));
 
-foreach ((array) $cats as $cat) {
+foreach ( (array)$cats as $cat ) :
 	$catname = apply_filters('link_category', $cat->name);
 
 ?>
 <outline type="category" title="<?php echo esc_attr($catname); ?>">
 <?php
-
 	$bookmarks = get_bookmarks(array("category" => $cat->term_id));
-	foreach ((array) $bookmarks as $bookmark) {
-		$title = esc_attr(apply_filters('link_title', $bookmark->link_name));
+	foreach ( (array)$bookmarks as $bookmark ) :
+		$title = apply_filters('link_title', $bookmark->link_name);
 ?>
-	<outline text="<?php echo $title; ?>" type="link" xmlUrl="<?php echo esc_attr($bookmark->link_rss); ?>" htmlUrl="<?php echo esc_attr($bookmark->link_url); ?>" updated="<?php if ('0000-00-00 00:00:00' != $bookmark->link_updated) echo $bookmark->link_updated; ?>" />
+	<outline text="<?php echo esc_attr($title); ?>" type="link" xmlUrl="<?php echo esc_attr($bookmark->link_rss); ?>" htmlUrl="<?php echo esc_attr($bookmark->link_url); ?>" updated="<?php if ('0000-00-00 00:00:00' != $bookmark->link_updated) echo $bookmark->link_updated; ?>" />
 <?php
-
-	}
+	endforeach; // $bookmarks
 ?>
 </outline>
 <?php
-
-}
+endforeach; // $cats
 ?>
 </body>
 </opml>
