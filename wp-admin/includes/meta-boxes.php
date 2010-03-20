e2736b7583d15c135206c22595feb519a69de6cd
dd32 <dd32@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-03-20 02:39:58 +0000

    Tabs are better than spaces. Whitespace cleanup.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13777 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/meta-boxes.php
+++ wp-admin/includes/meta-boxes.php
@@ -13,7 +13,7 @@ function post_submit_meta_box($post) {
 	global $action;
 
 	$post_type = $post->post_type;
-    $post_type_object = get_post_type_object($post_type);
+	$post_type_object = get_post_type_object($post_type);
 	$type_cap = $post_type_object->capability_type;
 	$can_publish = current_user_can("publish_${type_cap}s");
 ?>
@@ -283,19 +283,19 @@ function post_tags_meta_box($post, $box) {
  */
 function post_categories_meta_box( $post, $box ) {
 	$defaults = array('taxonomy' => 'category');
- 	if ( !isset($box['args']) || !is_array($box['args']) )
- 		$args = array();
+	if ( !isset($box['args']) || !is_array($box['args']) )
+		$args = array();
 	else
 		$args = $box['args'];
- 	extract( wp_parse_args($args, $defaults), EXTR_SKIP );
+	extract( wp_parse_args($args, $defaults), EXTR_SKIP );
 	$tax = get_taxonomy($taxonomy);
 
- 	?>
- 	<div id="taxonomy-<?php echo $taxonomy; ?>" class="categorydiv">
- 		<ul id="<?php echo $taxonomy; ?>-tabs" class="category-tabs">
- 			<li class="tabs"><a href="#<?php echo $taxonomy; ?>-all" tabindex="3"><?php printf( __( 'All %s' ), $tax->label ); ?></a></li>
- 			<li class="hide-if-no-js"><a href="#<?php echo $taxonomy; ?>-pop" tabindex="3"><?php _e( 'Most Used' ); ?></a></li>
- 		</ul>
+	?>
+	<div id="taxonomy-<?php echo $taxonomy; ?>" class="categorydiv">
+		<ul id="<?php echo $taxonomy; ?>-tabs" class="category-tabs">
+			<li class="tabs"><a href="#<?php echo $taxonomy; ?>-all" tabindex="3"><?php printf( __( 'All %s' ), $tax->label ); ?></a></li>
+			<li class="hide-if-no-js"><a href="#<?php echo $taxonomy; ?>-pop" tabindex="3"><?php _e( 'Most Used' ); ?></a></li>
+		</ul>
 
 		<div id="<?php echo $taxonomy; ?>-pop" class="tabs-panel" style="display: none;">
 			<ul id="<?php echo $taxonomy; ?>checklist-pop" class="categorychecklist form-no-clear" >
@@ -309,20 +309,20 @@ function post_categories_meta_box( $post, $box ) {
 			</ul>
 		</div>
 
-    <?php if ( current_user_can($tax->edit_cap) ) : ?>
- 			<div id="<?php echo $taxonomy; ?>-adder" class="wp-hidden-children">
- 				<h4><a id="<?php echo $taxonomy; ?>-add-toggle" href="#<?php echo $taxonomy; ?>-add" class="hide-if-no-js" tabindex="3"><?php printf( __( '+ Add New %s' ), $tax->singular_label ); ?></a></h4>
- 				<p id="<?php echo $taxonomy; ?>-add" class="category-add wp-hidden-child">
- 					<label class="screen-reader-text" for="new<?php echo $taxonomy; ?>"><?php printf( __( 'Add New %s' ), $tax->singular_label ); ?></label><input type="text" name="new<?php echo $taxonomy; ?>" id="new<?php echo $taxonomy; ?>" class="form-required form-input-tip" value="<?php echo esc_attr( sprintf( 'New %s Name', $tax->singular_label ) ); ?>" tabindex="3" aria-required="true"/>
- 					<label class="screen-reader-text" for="new<?php echo $taxonomy; ?>_parent"><?php printf( __('Parent %s'), $tax->singular_label ); ?>:</label><?php wp_dropdown_categories( array( 'taxonomy' => $taxonomy, 'hide_empty' => 0, 'name' => 'new'.$taxonomy.'_parent', 'orderby' => 'name', 'hierarchical' => 1, 'show_option_none' => sprintf( __('&mdash; Parent %s &mdash;'), $tax->singular_label ), 'tab_index' => 3 ) ); ?>
- 					<input type="button" id="<?php echo $taxonomy; ?>-add-submit" class="add:<?php echo $taxonomy ?>checklist:<?php echo $taxonomy ?>-add button category-add-sumbit" value="<?php esc_attr_e( 'Add' ); ?>" tabindex="3" />
- 					<?php wp_nonce_field( 'add-'.$taxonomy, '_ajax_nonce', false ); ?>
- 					<span id="<?php echo $taxonomy; ?>-ajax-response"></span>
- 				</p>
- 			</div>
- 		<?php endif; ?>
- 	</div>
- 	<?php
+	<?php if ( current_user_can($tax->edit_cap) ) : ?>
+			<div id="<?php echo $taxonomy; ?>-adder" class="wp-hidden-children">
+				<h4><a id="<?php echo $taxonomy; ?>-add-toggle" href="#<?php echo $taxonomy; ?>-add" class="hide-if-no-js" tabindex="3"><?php printf( __( '+ Add New %s' ), $tax->singular_label ); ?></a></h4>
+				<p id="<?php echo $taxonomy; ?>-add" class="category-add wp-hidden-child">
+					<label class="screen-reader-text" for="new<?php echo $taxonomy; ?>"><?php printf( __( 'Add New %s' ), $tax->singular_label ); ?></label><input type="text" name="new<?php echo $taxonomy; ?>" id="new<?php echo $taxonomy; ?>" class="form-required form-input-tip" value="<?php echo esc_attr( sprintf( 'New %s Name', $tax->singular_label ) ); ?>" tabindex="3" aria-required="true"/>
+					<label class="screen-reader-text" for="new<?php echo $taxonomy; ?>_parent"><?php printf( __('Parent %s'), $tax->singular_label ); ?>:</label><?php wp_dropdown_categories( array( 'taxonomy' => $taxonomy, 'hide_empty' => 0, 'name' => 'new'.$taxonomy.'_parent', 'orderby' => 'name', 'hierarchical' => 1, 'show_option_none' => sprintf( __('&mdash; Parent %s &mdash;'), $tax->singular_label ), 'tab_index' => 3 ) ); ?>
+					<input type="button" id="<?php echo $taxonomy; ?>-add-submit" class="add:<?php echo $taxonomy ?>checklist:<?php echo $taxonomy ?>-add button category-add-sumbit" value="<?php esc_attr_e( 'Add' ); ?>" tabindex="3" />
+					<?php wp_nonce_field( 'add-'.$taxonomy, '_ajax_nonce', false ); ?>
+					<span id="<?php echo $taxonomy; ?>-ajax-response"></span>
+				</p>
+			</div>
+		<?php endif; ?>
+	</div>
+	<?php
 }
 
 
