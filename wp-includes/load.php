82720ead317692b163bec39da8997f516333433c
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-03-26 14:06:12 +0000

    WP_I18N_* for .maintenance strings. fixes #12485
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13822 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -163,11 +163,11 @@ function wp_maintenance() {
 	<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>Maintenance</title>
+		<title><?php echo /*WP_I18N_MAINTENANCE*/'Maintenance'/*/WP_I18N_MAINTENANCE*/; ?></title>
 
 	</head>
 	<body>
-		<h1>Briefly unavailable for scheduled maintenance. Check back in a minute.</h1>
+		<h1><?php echo /*WP_I18N_MAINT_MSG*/'Briefly unavailable for scheduled maintenance. Check back in a minute.'/*/WP_I18N_MAINT_MSG*/; ?></h1>
 	</body>
 	</html>
 <?php
