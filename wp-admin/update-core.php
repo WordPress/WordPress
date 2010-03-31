f1793b308acbd7400f05e99ac0785daa1d0a8fb1
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-03-31 08:16:57 +0000

    Add link to Updating WordPress codex page. props mrmist, fixes #11280.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13898 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/update-core.php
+++ wp-admin/update-core.php
@@ -131,7 +131,7 @@ function core_upgrade_preamble() {
 		echo '</h3>';
 	} else {
 		echo '<div class="updated"><p>';
-		_e('<strong>Important:</strong> before updating, please <a href="http://codex.wordpress.org/WordPress_Backups">backup your database and files</a>.');
+		_e('<strong>Important:</strong> before updating, please <a href="http://codex.wordpress.org/WordPress_Backups">backup your database and files</a>. For help with updates, visit the <a href="http://codex.wordpress.org/Updating_WordPress">Updating WordPress Codex page</a>.');
 		echo '</p></div>';
 
 		echo '<h3 class="response">';
