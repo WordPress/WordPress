3344bf4a6e44a5e1ce5f0c53357405fdeaca0e7b
dd32 <dd32@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-04-01 10:30:58 +0000

    Small coding standards cleanup of wp_die();
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13910 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -2641,19 +2641,20 @@ function _default_wp_die_handler( $message, $title = '', $args = array() ) {
 		$admin_dir = 'wp-admin/';
 
 	if ( !function_exists( 'did_action' ) || !did_action( 'admin_head' ) ) :
-	if ( !headers_sent() ){
+	if ( !headers_sent() ) {
 		status_header( $r['response'] );
 		nocache_headers();
 		header( 'Content-Type: text/html; charset=utf-8' );
 	}
 
-	if ( empty($title) ) {
-		$title = $have_gettext? __('WordPress &rsaquo; Error') : 'WordPress &rsaquo; Error';
-	}
+	if ( empty($title) )
+		$title = $have_gettext ? __('WordPress &rsaquo; Error') : 'WordPress &rsaquo; Error';
 
 	$text_direction = 'ltr';
-	if ( isset($r['text_direction']) && $r['text_direction'] == 'rtl' ) $text_direction = 'rtl';
-	if ( ( $wp_locale ) && ( 'rtl' == $wp_locale->text_direction ) ) $text_direction = 'rtl';
+	if ( isset($r['text_direction']) && 'rtl' == $r['text_direction'] )
+		$text_direction = 'rtl';
+	elseif ( isset($wp_locale ) && 'rtl' == $wp_locale->text_direction )
+		$text_direction = 'rtl';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- Ticket #11289, IE bug fix: always pad the error page with enough characters such that it is greater than 512 bytes, even after gzip compression abcdefghijklmnopqrstuvwxyz1234567890aabbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzz11223344556677889900abacbcbdcdcededfefegfgfhghgihihjijikjkjlklkmlmlnmnmononpopoqpqprqrqsrsrtstsubcbcdcdedefefgfabcadefbghicjkldmnoepqrfstugvwxhyz1i234j567k890laabmbccnddeoeffpgghqhiirjjksklltmmnunoovppqwqrrxsstytuuzvvw0wxx1yyz2z113223434455666777889890091abc2def3ghi4jkl5mno6pqr7stu8vwx9yz11aab2bcc3dd4ee5ff6gg7hh8ii9j0jk1kl2lmm3nnoo4p5pq6qrr7ss8tt9uuvv0wwx1x2yyzz13aba4cbcb5dcdc6dedfef8egf9gfh0ghg1ihi2hji3jik4jkj5lkl6kml7mln8mnm9ono -->
