44ae35ec0997c9d774d982b4276729bc3d69d2db
dd32 <dd32@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-04-02 01:23:58 +0000

    Use the taxonomy show_ui flag to control if all WordPress generated UI is exposed (Menu previously, This affects metaboxes). See #10437
    git-svn-id: http://svn.automattic.com/wordpress/trunk@13924 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-form-advanced.php
+++ wp-admin/edit-form-advanced.php
@@ -92,15 +92,21 @@ require_once('includes/meta-boxes.php');
 
 add_meta_box('submitdiv', __('Publish'), 'post_submit_meta_box', $post_type, 'side', 'core');
 
-// all tag-style taxonomies
+// all taxonomies
 foreach ( get_object_taxonomies($post_type) as $tax_name ) {
 	$taxonomy = get_taxonomy($tax_name);
+	if ( ! $taxonomy->show_ui )
+		continue;
+
 	$label = isset($taxonomy->label) ? esc_attr($taxonomy->label) : $tax_name;
 
 	if ( !is_taxonomy_hierarchical($tax_name) )
 		add_meta_box('tagsdiv-' . $tax_name, $label, 'post_tags_meta_box', $post_type, 'side', 'core');
 	else
 		add_meta_box($tax_name . 'div', $label, 'post_categories_meta_box', $post_type, 'side', 'core', array( 'taxonomy' => $tax_name ));
+
+	// register callback handling for metabox
+ 	add_filter('wp_ajax_add-' . $tax_name, '_wp_ajax_add_hierarchical_term');
 }
 
 if ( post_type_supports($post_type, 'page-attributes') )
