659fa59ec5e7082804e6357b38db294c4696cee4
dd32 <dd32@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-04-10 13:05:04 +0000

    Password nag should be a global user option. Fixes #12926
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14060 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/upgrade.php
+++ wp-admin/includes/upgrade.php
@@ -67,7 +67,7 @@ function wp_install( $blog_title, $user_name, $user_email, $public, $deprecated 
 		$user_password = wp_generate_password();
 		$message = __('<strong><em>Note that password</em></strong> carefully! It is a <em>random</em> password that was generated just for you.');
 		$user_id = wp_create_user($user_name, $user_password, $user_email);
-		update_user_option($user_id, 'default_password_nag', true);
+		update_user_option($user_id, 'default_password_nag', true, true);
 		$email_password = true;
 	} else if ( !$user_id ) {
 		// Password has been provided
