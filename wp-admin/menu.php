c6569e6a53904d99f585b0a4d8ca8c35725f4652
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-04-15 22:16:22 +0000

    Bump tools.php from read cap to edit_posts. Old cap was for Gears. fixes #12903, see #11301.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14097 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/menu.php
+++ wp-admin/menu.php
@@ -182,8 +182,8 @@ if ( current_user_can('edit_users') ) {
 	$submenu['profile.php'][5] = array(__('Your Profile'), 'read', 'profile.php');
 }
 
-$menu[75] = array( __('Tools'), 'read', 'tools.php', '', 'menu-top menu-icon-tools', 'menu-tools', 'div' );
-	$submenu['tools.php'][5] = array( __('Tools'), 'read', 'tools.php' );
+$menu[75] = array( __('Tools'), 'edit_posts', 'tools.php', '', 'menu-top menu-icon-tools', 'menu-tools', 'div' );
+	$submenu['tools.php'][5] = array( __('Tools'), 'edit_posts', 'tools.php' );
 	$submenu['tools.php'][10] = array( __('Import'), 'import', 'import.php' );
 	$submenu['tools.php'][15] = array( __('Export'), 'import', 'export.php' );
 	if ( is_multisite() && !is_main_site() )
