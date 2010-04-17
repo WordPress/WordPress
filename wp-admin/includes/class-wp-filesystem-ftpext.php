629432887e2c964dcaaf1ed48b25b026e874f3c3
dd32 <dd32@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-04-17 04:00:59 +0000

    Return false on WP_Filesystem_FTPext::dirlist() for non-existant folders. See #12232. See #12057
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14120 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-filesystem-ftpext.php
+++ wp-admin/includes/class-wp-filesystem-ftpext.php
@@ -338,7 +338,8 @@ class WP_Filesystem_FTPext extends WP_Filesystem_Base {
 		}
 
 		$pwd = @ftp_pwd($this->link);
-		$chdir = @ftp_chdir($this->link, $path);
+		if ( ! @ftp_chdir($this->link, $path) ) // Cant change to folder = folder doesnt exist
+			return false;
 		$list = @ftp_rawlist($this->link, '-a', false);
 		@ftp_chdir($this->link, $pwd);
 
