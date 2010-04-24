7341a67e76fe20ab2fb6e556c5f4f158c9b6cb08
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-04-24 16:57:44 +0000

    Prevent HTML editor from being resized horizontally. props nomulous, fixes #13064
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14220 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -443,7 +443,7 @@ function wp_default_styles( &$styles ) {
 	// all colors stylesheets need to have the same query strings (cache manifest compat)
 	$colors_version = '20100418';
 
-	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20100411' );
+	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20100424' );
 
 	$styles->add( 'ie', "/wp-admin/css/ie$suffix.css", array(), '20100219' );
 	$styles->add_data( 'ie', 'conditional', 'lte IE 7' );
