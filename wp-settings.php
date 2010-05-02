7843826140eaf6937731da3ebfaf0a5c5653d4ca
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-02 22:18:36 +0000

    Don't silence inclusion of advanced-cache.php for WP_DEBUG. fixes #13185.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14345 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-settings.php
+++ wp-settings.php
@@ -57,7 +57,7 @@ wp_debug_mode();
 
 // For an advanced caching plugin to use. Uses a static drop-in because you would only want one.
 if ( WP_CACHE )
-	@include( WP_CONTENT_DIR . '/advanced-cache.php' );
+	WP_DEBUG ? include( WP_CONTENT_DIR . '/advanced-cache.php' ) : @include( WP_CONTENT_DIR . '/advanced-cache.php' );
 
 // Define WP_LANG_DIR if not set.
 wp_set_lang_dir();
