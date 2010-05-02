1eb297c9823ffd463238f81661b2a121b6281315
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-02 18:39:31 +0000

    Fix admin.php?import=x pages, which can re-include itself in certain environments. fixes #13184 for trunk.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14334 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -7,7 +7,8 @@
  */
 
 @header('Content-Type: ' . get_option('html_type') . '; charset=' . get_option('blog_charset'));
-if (!isset($_GET["page"])) require_once('./admin.php');
+if ( ! defined( 'WP_ADMIN' ) )
+	require_once( './admin.php' );
 
 get_admin_page_title();
 $title = esc_html( strip_tags( $title ) );
