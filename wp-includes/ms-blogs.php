e2f361cea4fc4a601a197591970ce6759beb5c66
wpmuguru <wpmuguru@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-02 21:40:31 +0000

    use correct var in r14342, see #13104
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14343 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-blogs.php
+++ wp-includes/ms-blogs.php
@@ -106,9 +106,9 @@ function get_blog_details( $fields, $get_all = true ) {
 			$blog = wp_cache_get($key, 'blog-lookup');
 			if ( false !== $blog )
 				return $blog;
-			if ( substr( $domain, 0, 4 ) == 'www.' ) {
-				$nowww = substr( $domain, 4 );
-				$blog = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->blogs WHERE domain IN (%s,%s) ORDER BY CHAR_LENGTH(domain) DESC", $nowww, $fields['domain'], $fields['path'] ) );
+			if ( substr( $fields['domain'], 0, 4 ) == 'www.' ) {
+				$nowww = substr( $fields['domain'], 4 );
+				$blog = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->blogs WHERE domain IN (%s,%s) ORDER BY CHAR_LENGTH(domain) DESC", $nowww, $fields['domain'] ) );
 			} else {
 				$blog = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->blogs WHERE domain = %s", $fields['domain'] ) );
 			}
