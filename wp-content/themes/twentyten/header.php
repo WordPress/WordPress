1f94349afb524d2150d8d92f05488a558ed96c55
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-02 20:51:30 +0000

    Check thread_comments before loading the comment-reply script. props ptahdunbar, fixes #12932
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14339 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyten/header.php
+++ wp-content/themes/twentyten/header.php
@@ -35,7 +35,7 @@
 	?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+	<?php if ( is_singular() && get_option('thread_comments') ) wp_enqueue_script( 'comment-reply' ); ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
 </head>
