d20d059440b227f584f1a2afe2d2a45b91081c51
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-03 19:28:44 +0000

    Typo fixes. Props zeo. see #13198
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14389 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyten/single.php
+++ wp-content/themes/twentyten/single.php
@@ -35,7 +35,7 @@
 						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
 					</div><!-- .entry-content -->
 
-<?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their decscription show a bio on their entries  ?>
+<?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
 					<div id="entry-author-info">
 						<div id="author-avatar">
 							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
