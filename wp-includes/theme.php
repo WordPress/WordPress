1afa45d117c1aeb88671a1682c1d487dd4162ef4
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-03 20:56:57 +0000

    Add is_child_theme(). Fixes #12998 props ptahdunbar.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14409 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/theme.php
+++ wp-includes/theme.php
@@ -7,6 +7,17 @@
  */
 
 /**
+ * Whether a child theme is in use.
+ *
+ * @since 3.0.0
+ *
+ * @return bool true if a child theme is in use, false otherwise.
+ **/
+function is_child_theme() {
+	return ( TEMPLATEPATH !== STYLESHEETPATH );
+}
+
+/**
  * Retrieve name of the current stylesheet.
  *
  * The theme name that the administrator has currently set the front end theme
