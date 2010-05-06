eda3a209e009dbee7ab4bc6f4ba656be02b28531
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-06 20:54:05 +0000

    Bump
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14488 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.0-beta1';
+$wp_version = '3.0-beta2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
