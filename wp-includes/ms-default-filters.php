4deb7b8b48bd387b16e0de94074e1eb0e8857347
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-08 19:31:42 +0000

    Don't use WP_SITEURL and WP_HOME in multisite. fixes #13191.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14517 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-default-filters.php
+++ wp-includes/ms-default-filters.php
@@ -56,4 +56,9 @@ if ( ! defined('POST_BY_EMAIL') || ! POST_BY_EMAIL ) // back compat constant.
 	add_filter( 'enable_post_by_email_configuration', '__return_false' );
 if ( ! defined('EDIT_ANY_USER') || ! EDIT_ANY_USER ) // back compat constant.
 	add_filter( 'enable_edit_any_user_configuration', '__return_false' );
+
+// WP_HOME and WP_SITEURL should not have any effect in MS
+remove_filter( 'option_siteurl', '_config_wp_siteurl' );
+remove_filter( 'option_home',    '_config_wp_home'    );
+
 ?>
