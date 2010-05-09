616bc49e047adef4efc6330c1cb54acce1af7d46
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-09 22:53:25 +0000

    Remove codepress dir. see #13307.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14527 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/codepress/languages/text.js
+++
@@ -1,9 +0,0 @@
-/*
- * CodePress regular expressions for Text syntax highlighting
- */
-
-// plain text
-Language.syntax = []
-Language.snippets = []
-Language.complete = []
-Language.shortcuts = []
