d4e3352f1ef6c0f4fa7c2c1e134a92ec34967cdb
nbachiyski <nbachiyski@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-10 06:04:14 +0000

    I18n fixes for header.php in Twentyten. Props zeo. See #13198
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14535 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyten/header.php
+++ wp-content/themes/twentyten/header.php
@@ -35,7 +35,7 @@
 	?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-	<?php if ( is_singular() && get_option('thread_comments') ) wp_enqueue_script( 'comment-reply' ); ?>
+	<?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
 </head>
@@ -45,19 +45,19 @@
 	<div id="header">
 		<div id="masthead">
 			<div id="branding">
-				<?php if ( is_home() || is_front_page() ) { ?>
-					<h1 id="site-title"><span><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
-				<?php } else { ?>
-					<div id="site-title"><span><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></div>
-				<?php } ?>
-				
+				<?php $heading_tag = ( is_home() || is_front_page() )? 'h1' : 'div'; ?>
+				<<?php echo $heading_tag; ?> id="site-title">
+					<span>
+						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					</span>
+				</<?php echo $heading_tag; ?>>				
 				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
 
 				<?php
 					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
 					if ( is_singular() &&
 							has_post_thumbnail( $post->ID ) &&
-							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail') ) &&
+							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' ) ) &&
 							$image[1] >= HEADER_IMAGE_WIDTH ) :
 						// Houston, we have a new header image!
 						echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
