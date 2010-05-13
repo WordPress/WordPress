24f2415b802b6ad10206297f32f5bae920238220
wpmuguru <wpmuguru@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-13 21:40:42 +0000

    block ms-files.php when not multisite, see #11742
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14609 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-files.php
+++ wp-includes/ms-files.php
@@ -11,6 +11,9 @@
 define( 'SHORTINIT', true );
 require_once( dirname( dirname( __FILE__ ) ) . '/wp-load.php' );
 
+if( !is_multisite() )
+	die( 'Multisite support not enabled' );
+
 ms_file_constants();
 
 error_reporting( 0 );
