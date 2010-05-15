64c5bb0a2618f71448ca4e0ce749877140400102
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-15 21:36:38 +0000

    Use upgrade_db instead of step=1 in ms-upgrade-network. Also, no need to check spam/deleted/archived as that is in the query. see #11869.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14678 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/ms-upgrade-network.php
+++ wp-admin/ms-upgrade-network.php
@@ -43,15 +43,13 @@ switch ( $action ) {
 		}
 		echo "<ul>";
 		foreach ( (array) $blogs as $details ) {
-			if ( $details['spam'] == 0 && $details['deleted'] == 0 && $details['archived'] == 0 ) {
-				$siteurl = get_blog_option( $details['blog_id'], 'siteurl' );
-				echo "<li>$siteurl</li>";
-				$response = wp_remote_get( trailingslashit( $siteurl ) . "wp-admin/upgrade.php?step=1", array( 'timeout' => 120, 'httpversion' => '1.1' ) );
-				if ( is_wp_error( $response ) )
-					wp_die( "<strong>Warning!</strong> Problem updating {$siteurl}. Your server may not be able to connect to sites running on it.<br /> Error message: <em>" . $response->get_error_message() ."</em>" );
-				do_action( 'after_mu_upgrade', $response );
-				do_action( 'wpmu_upgrade_site', $details[ 'blog_id' ] );
-			}
+			$siteurl = get_blog_option( $details['blog_id'], 'siteurl' );
+			echo "<li>$siteurl</li>";
+			$response = wp_remote_get( trailingslashit( $siteurl ) . "wp-admin/upgrade.php?step=upgrade_db", array( 'timeout' => 120, 'httpversion' => '1.1' ) );
+			if ( is_wp_error( $response ) )
+				wp_die( "<strong>Warning!</strong> Problem updating {$siteurl}. Your server may not be able to connect to sites running on it.<br /> Error message: <em>" . $response->get_error_message() ."</em>" );
+			do_action( 'after_mu_upgrade', $response );
+			do_action( 'wpmu_upgrade_site', $details[ 'blog_id' ] );
 		}
 		echo "</ul>";
 		?><p><?php _e( 'If your browser doesn&#8217;t start loading the next page automatically, click this link:' ); ?> <a class="button" href="ms-upgrade-network.php?action=upgrade&amp;n=<?php echo ($n + 5) ?>"><?php _e("Next Sites"); ?></a></p>
