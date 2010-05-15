eba281e2816b7400e3b3d3f0aba0cac144185b20
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-15 10:21:10 +0000

    Update media upload window tabs CSS to support wrapping of tabs when they exceed the available width. props edward mindreantre. fixes #10242
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14661 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -463,7 +463,7 @@ function wp_default_styles( &$styles ) {
 
 	$styles->add( 'ms', "/wp-admin/css/ms$suffix.css", array(), '20100512' );
 	$styles->add( 'global', "/wp-admin/css/global$suffix.css", array(), '20100108' );
-	$styles->add( 'media', "/wp-admin/css/media$suffix.css", array(), '20091029' );
+	$styles->add( 'media', "/wp-admin/css/media$suffix.css", array(), '20100515' );
 	$styles->add( 'widgets', "/wp-admin/css/widgets$suffix.css", array(), '20091118' );
 	$styles->add( 'dashboard', "/wp-admin/css/dashboard$suffix.css", array(), '20100424' );
 	$styles->add( 'install', "/wp-admin/css/install$suffix.css", array(), '20100313' ); // Readme as well
