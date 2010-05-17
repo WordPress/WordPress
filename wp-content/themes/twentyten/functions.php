fd31852e68590b1e94c5fa9a67d6fcba1c25413a
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-17 06:52:47 +0000

    Refer to the right codex article. props MichaelH, see #12695.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14709 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyten/functions.php
+++ wp-content/themes/twentyten/functions.php
@@ -10,7 +10,7 @@
  * for various features in WordPress, such as post thumbnails, navigation menus, and the like.
  *
  * When using a child theme (see http://codex.wordpress.org/Theme_Development and
- * http://codex.wordpress.org/Theme_Development), you can override certain functions
+ * http://codex.wordpress.org/Child_Themes), you can override certain functions
  * (those wrapped in a function_exists() call) by defining them first in your child theme's
  * functions.php file. The child theme's functions.php file is included before the parent
  * theme's file, so the child theme functions would be used.
