d83dcfc9208c2ef820e02de32501a29d345f54ba
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-21 19:01:37 +0000

    Fix typo. Props filosofo.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14787 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyten/category.php
+++ wp-content/themes/twentyten/category.php
@@ -19,7 +19,7 @@
 				<?php
 					$category_description = category_description();
 					if ( ! empty( $category_description ) )
-						echo '<div class="archive-meta">' . $categorydesc . '</div>';
+						echo '<div class="archive-meta">' . $category_description . '</div>';
 
 				/* Run the loop for the category page to output the posts.
 				 * If you want to overload this in a child theme then include a file
