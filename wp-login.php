8199a1be9b670cdbf5b010494992e383153ddecc
dd32 <dd32@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-23 11:06:44 +0000

    Reset position after shaking the login screen, Fixes IE6 hiding the Remember Me & Login buttons. Props ocean90. Fixes #13495
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14817 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-login.php
+++ wp-login.php
@@ -124,7 +124,7 @@ function wp_shake_js() {
 addLoadEvent = function(func){if(typeof jQuery!="undefined")jQuery(document).ready(func);else if(typeof wpOnload!='function'){wpOnload=func;}else{var oldonload=wpOnload;wpOnload=function(){oldonload();func();}}};
 function s(id,pos){g(id).left=pos+'px';}
 function g(id){return document.getElementById(id).style;}
-function shake(id,a,d){c=a.shift();s(id,c);if(a.length>0){setTimeout(function(){shake(id,a,d);},d);}else{try{wp_attempt_focus();}catch(e){}}}
+function shake(id,a,d){c=a.shift();s(id,c);if(a.length>0){setTimeout(function(){shake(id,a,d);},d);}else{try{g(id).position='static';wp_attempt_focus();}catch(e){}}}
 addLoadEvent(function(){ var p=new Array(15,30,15,0,-15,-30,-15,0);p=p.concat(p.concat(p));var i=document.forms[0].id;g(i).position='relative';shake(i,p,20);});
 </script>
 <?php
