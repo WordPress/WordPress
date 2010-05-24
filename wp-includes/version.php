6c817a180ca87909cf2a9f71f4987ffbe9d0c9fd
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-24 21:28:37 +0000

    New high score!
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14860 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.0-beta2-14822';
+$wp_version = '3.0-beta2-14860';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
