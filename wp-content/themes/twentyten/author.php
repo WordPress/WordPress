78ba3b94b1f85e2c4a4c57485b30cc992505bc56
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-24 08:40:02 +0000

    Check to see we have posts before calling the_post().
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14830 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyten/author.php
+++ wp-content/themes/twentyten/author.php
@@ -21,7 +21,8 @@
 	 * We reset this later so we can run the loop
 	 * properly with a call to rewind_posts().
 	 */
-	the_post();
+	if ( have_posts() )
+		the_post();
 ?>
 
 				<h1 class="page-title author"><?php printf( __( 'Author Archives: %s', 'twentyten' ), "<span class='vcard'><a class='url fn n' href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a></span>" ); ?></h1>
