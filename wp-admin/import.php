c08015c0155fdcf62a55da6e946482f45241ba17
nbachiyski <nbachiyski@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-25 11:37:25 +0000

    Fix MovableType importer plugin slug typo. The real one is movabletype-importer, not mt-importer.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14874 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/import.php
+++ wp-admin/import.php
@@ -48,7 +48,7 @@ if ( current_user_can('install_plugins') )
 		'blogger' => array( __('Blogger'), __('Install the Blogger importer to import posts, comments, and users from a Blogger blog.'), 'install' ),
 		'wpcat2tag' => array(__('Categories and Tags Converter'), __('Install the category/tag converter to convert existing categories to tags or tags to categories, selectively.'), 'install'),
 		'livejournal' => array( __( 'LiveJournal' ), __( 'Install the LiveJounral importer to import posts from LiveJournal using their API.' ), 'install' ),
-		'mt' => array( __('Movable Type and TypePad'), __('Install the Movable Type importer to import posts and comments from a Movable Type or TypePad blog.'), 'install' ),
+		'movabletype' => array( __('Movable Type and TypePad'), __('Install the Movable Type importer to import posts and comments from a Movable Type or TypePad blog.'), 'install' ),
 		'opml' => array( __('Blogroll'), __('Install the blogroll importer to import links in OPML format.'), 'install' ),
 		'rss' => array( __('RSS'), __('Install the RSS importer to import posts from an RSS feed.'), 'install' ),
 		'wordpress' => array( 'WordPress', __('Install the WordPress importer to import posts, pages, comments, custom fields, categories, and tags from a WordPress export file.'), 'install' )
