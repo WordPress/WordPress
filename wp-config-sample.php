78e8ac0bee777d41ae8a90bb2adbb89298e89bd3
wpmuguru <wpmuguru@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-25 23:47:18 +0000

    shorten header in wp-config-sample, props lloydbudd, fixes #13542
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14912 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -3,8 +3,8 @@
  * The base configurations of the WordPress.
  *
  * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information by
- * visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
  * wp-config.php} Codex page. You can get the MySQL settings from your web host.
  *
  * This file is used by the wp-config.php creation script during the
