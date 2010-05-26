e18c9ba08af28aa879c2214db0a6f533a7b02b60
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-26 20:06:54 +0000

    Do not display category blurb for post types that do not support that taxonomy.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@14955 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyten/functions.php
+++ wp-content/themes/twentyten/functions.php
@@ -462,8 +462,10 @@ function twentyten_posted_in() {
 	$tag_list = get_the_tag_list( '', ', ' );
 	if ( $tag_list ) {
 		$posted_in = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'twentyten' );
-	} else {
+	} elseif ( is_object_in_taxonomy( get_post_type(), 'category' ) ) {
 		$posted_in = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'twentyten' );
+	} else {
+		$posted_in = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'twentyten' );
 	}
 	// Prints the string, replacing the placeholders.
 	printf(
