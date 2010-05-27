51701eed1a416ac780c28b75486b6728d0085b1f
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-27 23:45:45 +0000

    Edit comment help text. props jane, dougwrites. see #13467.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@15014 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/comment.php
+++ wp-admin/comment.php
@@ -45,6 +45,13 @@ switch( $action ) {
 case 'editcomment' :
 	$title = __('Edit Comment');
 
+	add_contextual_help( $current_screen, '<p>' . __( 'You can edit the information left in a comment if needed. This is often useful when you notice that a commenter has made a typographical error.' ) . '</p>' .
+	'<p>' . __( 'You can also moderate the comment from this screen using the Status box, where you can also change the timestamp of the comment.' ) . '</p>' .
+	'<p><strong>' . __( 'For more information:' ) . '</strong></p>' .
+	'<p>' . __( '<a href="http://codex.wordpress.org/Administration_Panels#Comments">Comments Documentation</a>' ) . '</p>' .
+	'<p>' . __( '<a href="http://wordpress.org/support/">Support Forums</a>' ) . '</p>'
+	);
+
 	wp_enqueue_script('comment');
 	require_once('./admin-header.php');
 
