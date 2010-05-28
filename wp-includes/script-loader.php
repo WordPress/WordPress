fd7fe309c83e9d230b96a81a94a58751be59b230
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-28 22:40:56 +0000

    Allow the text labels in the multisite elements of the Right Now dashboard widget. Fixes display with narrow columns due to screen options or browser window width. props ocean90.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@15048 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -450,7 +450,7 @@ function wp_default_styles( &$styles ) {
 	$styles->add( 'colors-classic', "/wp-admin/css/colors-classic$suffix.css", array(), $colors_version );
 	$styles->add_data( 'colors-classic', 'rtl', true );
 
-	$styles->add( 'ms', "/wp-admin/css/ms$suffix.css", array(), '20100512' );
+	$styles->add( 'ms', "/wp-admin/css/ms$suffix.css", array(), '20100528' );
 	$styles->add( 'global', "/wp-admin/css/global$suffix.css", array(), '20100108' );
 	$styles->add( 'media', "/wp-admin/css/media$suffix.css", array(), '20100519' );
 	$styles->add( 'widgets', "/wp-admin/css/widgets$suffix.css", array(), '20091118' );
