9880197be6d8ae2ec980c9630d68b26dfbc04768
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-05-31 21:13:24 +0000

    Use default wp_nav_menu sorting in Twenty Ten. props iandstewart, fixes #13660.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@15082 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyten/header.php
+++ wp-content/themes/twentyten/header.php
@@ -83,7 +83,7 @@
 			  <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
 				<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'twentyten' ); ?></a></div>
 				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
-				<?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
+				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
 			</div><!-- #access -->
 		</div><!-- #masthead -->
 	</div><!-- #header -->
