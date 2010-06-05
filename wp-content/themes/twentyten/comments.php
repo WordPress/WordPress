7142f4ec36244b25ee375be708066ae3b3e1fd47
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-06-05 14:55:18 +0000

    Check if comment paging is on. Props iandstewart. fixes #13739
    git-svn-id: http://svn.automattic.com/wordpress/trunk@15145 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyten/comments.php
+++ wp-content/themes/twentyten/comments.php
@@ -36,7 +36,7 @@
 			number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' );
 			?></h3>
 
-<?php if ( get_comment_pages_count() > 1 ) : // Are there comments to navigate through? ?>
+<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 			<div class="navigation">
 				<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'twentyten' ) ); ?></div>
 				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
@@ -55,7 +55,7 @@
 				?>
 			</ol>
 
-<?php if ( get_comment_pages_count() > 1 ) : // Are there comments to navigate through? ?>
+<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 			<div class="navigation">
 				<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'twentyten' ) ); ?></div>
 				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
