b538f27b0a358b066105cd2371c7a9ad93d00874
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-06-07 17:41:39 +0000

    ++;
    git-svn-id: http://svn.automattic.com/wordpress/trunk@15161 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.0-RC2-15136';
+$wp_version = '3.0-RC2-15161';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
