64f3e4088007039a0aeac3030e73e807e0ae653c
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-06-09 20:21:43 +0000

    Improve phpDoc for add_settings_section. Fixes #13801 props frankieroberto.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@15180 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/template.php
+++ wp-admin/includes/template.php
@@ -2972,11 +2972,11 @@ function get_hidden_meta_boxes( $screen ) {
  * @since 2.7.0
  *
  * @global $wp_settings_sections Storage array of all settings sections added to admin pages
-
+ *
  * @param string $id Slug-name to identify the section. Used in the 'id' attribute of tags.
  * @param string $title Formatted title of the section. Shown as the heading for the section.
- * @param string $callback Function that echo's out content for the section heading.
- * @param string $page The slug-name of the settings page on which to show the section (general, reading, writing, ...).
+ * @param string $callback Function that echos out any content at the top of the section (between heading and fields).
+ * @param string $page The slug-name of the settings page on which to show the section. Built-in pages include 'general', 'reading', 'writing', 'discussion', 'media', etc. Create your own using add_options_page();
  */
 function add_settings_section($id, $title, $callback, $page) {
 	global $wp_settings_sections;
