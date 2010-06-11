faf9bd8483326944edc16882284eb84b8822a1c1
wpmuguru <wpmuguru@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-06-11 17:18:17 +0000

    add create site link to My Sites page, original patch props filosofo, fixes #13852
    git-svn-id: http://svn.automattic.com/wordpress/trunk@15228 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/ms.php
+++ wp-admin/includes/ms.php
@@ -796,6 +796,13 @@ function choose_primary_blog() {
 		?>
 		</td>
 	</tr>
+	<?php if ( in_array( get_site_option( 'registration' ), array( 'all', 'blog' ) ) ) : ?>
+		<tr>
+			<th scope="row" colspan="2" class="th-full">
+				<a href="<?php echo apply_filters( 'wp_signup_location', network_home_url( 'wp-signup.php' ) ); ?>"><?php _e( 'Create a New Site' ); ?></a>
+			</th>
+		</tr>
+	<?php endif; ?>
 	</table>
 	<?php
 }
