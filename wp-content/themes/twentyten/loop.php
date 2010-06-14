44f8ce89093afd2a9ce48cc5b51893f501bf5196
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-06-14 08:24:34 +0000

    Don't pass bogus argument to the_excerpt. props zeo, fixes #13883.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@15247 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyten/loop.php
+++ wp-content/themes/twentyten/loop.php
@@ -83,7 +83,7 @@
 						$total_images
 					); ?></em></p>
 
-				<?php the_excerpt( '' ); ?>
+				<?php the_excerpt(); ?>
 <?php endif; ?>
 			</div><!-- .entry-content -->
 
@@ -130,7 +130,7 @@
 
 	<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
 			<div class="entry-summary">
-				<?php the_excerpt( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?>
+				<?php the_excerpt(); ?>
 			</div><!-- .entry-summary -->
 	<?php else : ?>
 			<div class="entry-content">
