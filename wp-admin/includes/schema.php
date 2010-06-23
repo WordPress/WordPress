a1ded4c2707855be5408bfd49c3f5dcb0f3cff62
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-06-23 05:10:45 +0000

    Tiny code cleanup. intval is used earlier so it is stored properly in the db; we just need true/false.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@15305 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/schema.php
+++ wp-admin/includes/schema.php
@@ -714,7 +714,7 @@ Thanks!
 		'subdomain_install' => intval( $subdomain_install ),
 		'global_terms_enabled' => global_terms_enabled() ? '1' : '0'
 	);
-	if ( !intval( $subdomain_install ) )
+	if ( ! $subdomain_install )
 		$sitemeta['illegal_names'][] = 'blog';
 
 	$insert = '';
