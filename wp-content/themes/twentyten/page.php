ea8f1e0f32a743d904e53058faed59f817d1763a
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-07-13 03:00:58 +0000

    [15400] for 3.0 branch, keep Twenty Ten sync'd. It's called WordPress. props zeo, fixes #14293.
    git-svn-id: http://svn.automattic.com/wordpress/branches/3.0@15401 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyten/page.php
+++ wp-content/themes/twentyten/page.php
@@ -3,8 +3,8 @@
  * The template for displaying all pages.
  *
  * This is the template that displays all pages by default.
- * Please note that this is the wordpress construct of pages
- * and that other 'pages' on your wordpress site will use a
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
  * different template.
  *
  * @package WordPress
@@ -22,9 +22,9 @@ get_header(); ?>
 				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<?php if ( is_front_page() ) { ?>
 						<h2 class="entry-title"><?php the_title(); ?></h2>
-					<?php } else { ?>	
+					<?php } else { ?>
 						<h1 class="entry-title"><?php the_title(); ?></h1>
-					<?php } ?>				
+					<?php } ?>
 
 					<div class="entry-content">
 						<?php the_content(); ?>
