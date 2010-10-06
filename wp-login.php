36e537424dfd4efcbf9d6255fb9e9dc6967ddc02
scribu <scribu@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-10-06 19:02:43 +0000

    Fix password reset procedure. See #5919
    git-svn-id: http://svn.automattic.com/wordpress/trunk@15735 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-login.php
+++ wp-login.php
@@ -437,7 +437,7 @@ case 'rp' :
 	if ( isset($_POST['pass1']) && $_POST['pass1'] != $_POST['pass2'] ) {
 		$errors = new WP_Error('password_reset_mismatch', __('The passwords do not match.'));
 	} elseif ( isset($_POST['pass1']) && !empty($_POST['pass1']) ) {
-		reset_password($user, $_POST['pass']);
+		reset_password($user, $_POST['pass1']);
 		login_header(__('Password Reset'), '<p class="message reset-pass">' . __('Your password has been reset.') . ' <a href="' . site_url('wp-login.php', 'login') . '">' . __('Log in') . '</a></p>');
 		login_footer();
 		exit;
