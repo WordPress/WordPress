f67cae286dae4f9bb93d40f0564a852af976c167
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-10-20 22:44:15 +0000

    Push capital P filter back behind the wpautop one to catch Wordpress after opening HTML tag
    git-svn-id: http://svn.automattic.com/wordpress/trunk@15877 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-filters.php
+++ wp-includes/default-filters.php
@@ -95,8 +95,9 @@ foreach ( array( 'comment_author', 'term_name', 'link_name', 'link_description',
 }
 
 // Format WordPress
-foreach ( array( 'the_content', 'the_title', 'comment_text' ) as $filter )
+foreach ( array( 'the_content', 'the_title' ) as $filter )
 	add_filter( $filter, 'capital_P_dangit', 11 );
+add_filter( 'comment_text', 'capital_P_dangit', 31 );
 
 // Format titles
 foreach ( array( 'single_post_title', 'single_cat_title', 'single_tag_title', 'single_month_title', 'nav_menu_attr_title', 'nav_menu_description' ) as $filter ) {
