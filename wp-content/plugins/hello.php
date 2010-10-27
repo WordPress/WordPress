a613fa5d92c38dbafc9b345ea5efd33809f74fe8
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-10-27 02:23:57 +0000

    Wasn't me.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@15997 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/plugins/hello.php
+++ wp-content/plugins/hello.php
@@ -8,7 +8,7 @@ Plugin Name: Hello Dolly
 Plugin URI: http://wordpress.org/#
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Matt Mullenweg
-Version: 1.4
+Version: 1.5.1
 Author URI: http://ma.tt/
 */
 
