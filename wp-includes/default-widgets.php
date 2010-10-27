2f7035c68d2133aee25de12621f39848c687afb6
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-10-27 14:10:47 +0000

    Restore the (at most 15) on the Recent Posts Widgets UI - Accidentally removed as part of [14483].
    git-svn-id: http://svn.automattic.com/wordpress/trunk@16013 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-widgets.php
+++ wp-includes/default-widgets.php
@@ -590,6 +590,7 @@ class WP_Widget_Recent_Posts extends WP_Widget {
 
 		<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of posts to show:'); ?></label>
 		<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
+		<small><?php _e('(at most 15)'); ?></small>
 <?php
 	}
 }
