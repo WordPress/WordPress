e0c367642352de46fcd4d3c06377e24fd796c8bf
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-11-10 12:43:04 +0000

    Hello Dolly 1.6. fixes typo, props sbressler, fixes #15229. Removes absolute from positioning, props kapeels, fixes #15228. Add some whitespace.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@16270 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/plugins/hello.php
+++ wp-content/plugins/hello.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Hello_Dolly
- * @version 1.5.1
+ * @version 1.6
  */
 /*
 Plugin Name: Hello Dolly
-Plugin URI: http://wordpress.org/#
+Plugin URI: http://wordpress.org/extend/plugins/hello-dolly/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Matt Mullenweg
-Version: 1.5.1
+Version: 1.6
 Author URI: http://ma.tt/
 */
 
@@ -44,10 +44,10 @@ Dolly'll never go away
 Dolly'll never go away again";
 
 	// Here we split it into lines
-	$lyrics = explode("\n", $lyrics);
+	$lyrics = explode( "\n", $lyrics );
 
 	// And then randomly choose a line
-	return wptexturize( $lyrics[ mt_rand(0, count($lyrics) - 1) ] );
+	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
 }
 
 // This just echoes the chosen line, we'll position it later
@@ -56,28 +56,27 @@ function hello_dolly() {
 	echo "<p id='dolly'>$chosen</p>";
 }
 
-// Now we set that function up to execute when the admin_footer action is called
-add_action('admin_footer', 'hello_dolly');
+// Now we set that function up to execute when the admin_notices action is called
+add_action( 'admin_notices', 'hello_dolly' );
 
 // We need some CSS to position the paragraph
 function dolly_css() {
-	// This makes sure that the posinioning is also good for right-to-left languages
-	$x = ( is_rtl() ) ? 'left' : 'right';
+	// This makes sure that the positioning is also good for right-to-left languages
+	$x = is_rtl() ? 'left' : 'right';
 
 	echo "
 	<style type='text/css'>
 	#dolly {
-		position: absolute;
-		top: 4.5em;
+		float: $x;
+		padding-$x: 15px;
+		padding-top: 5px;		
 		margin: 0;
-		padding: 0;
-		$x: 215px;
 		font-size: 11px;
 	}
 	</style>
 	";
 }
 
-add_action('admin_head', 'dolly_css');
+add_action( 'admin_head', 'dolly_css' );
 
 ?>
