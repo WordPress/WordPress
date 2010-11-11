c10218f665a27f94fb44d95ede91ec939d85ca99
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-11-11 16:19:25 +0000

    Admin bar tweaks. Props filosofo. see #15308
    git-svn-id: http://svn.automattic.com/wordpress/trunk@16302 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/admin-bar.php
+++ wp-includes/admin-bar.php
@@ -206,7 +206,7 @@ function wp_admin_bar_new_content_menu() {
 	if ( empty( $actions ) )
 		return;
 
-	$wp_admin_bar->add_menu( array( 'id' => 'new-content', 'title' => __( 'New Content' ), 'href' => '', ) );
+	$wp_admin_bar->add_menu( array( 'id' => 'new-content', 'title' => __( 'Add New' ), 'href' => '', ) );
 
 	foreach ( $actions as $link => $action ) {
 		$wp_admin_bar->add_menu( array( 'parent' => 'new-content', 'id' => $action[2], 'title' => $action[0], 'href' => admin_url($link) ) );
