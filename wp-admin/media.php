12d42895eef82b1a79c79d91a66109e6d9981dbd
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-11-12 15:03:34 +0000

    Contextual help updates. Props dougwrites. see #15346
    git-svn-id: http://svn.automattic.com/wordpress/trunk@16326 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/media.php
+++ wp-admin/media.php
@@ -66,6 +66,16 @@ case 'edit' :
 	wp_enqueue_script( 'wp-ajax-response' );
 	wp_enqueue_script('image-edit');
 	wp_enqueue_style('imgareaselect');
+	
+	add_contextual_help( $current_screen,
+	'<p>' . __('This screen allows you to edit five fields for metadata in a file within the media library.') . '</p>' .
+	'<p>' . __('For images only, you can click on Edit Image under the thumbnail to expand out an inline image editor with icons for cropping, rotating, or flipping the image as well as for undoing and redoing. The boxes on the right give you more options for scaling the image, for cropping it, and for cropping the thumbnail in a different way than you crop the original image. You can click on Help in those boxes to get more information.') . '</p>' .
+	'<p>' . __('Note that you crop the image by clicking on it (the Crop icon is already selected) and dragging the cropping frame to select the desired part. Then click Save to retain the cropping.') . '</p>' .
+	'<p>' . __('Remember to click Update Media to save metadata entered or changed.') . '</p>' .
+	'<p><strong>' . __('For more information:') . '</strong></p>' .
+	'<p>' . __('<a href="http://codex.wordpress.org/Media_Add_New_SubPanel#Edit_Media" target="_blank">Documentation on Edit Media</a>') . '</p>' .
+	'<p>' . __('<a href="http://wordpress.org/support/" target="_blank">Support Forums</a>') . '</p>'
+);
 
 	require( './admin-header.php' );
 
