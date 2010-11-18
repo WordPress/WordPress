f9434dc3f6254347e36493d10254d57562476394
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-11-18 08:50:20 +0000

    Kill the background from the default jQuery UI dialog theme. We'll get this from .wp-dialog in the colors stylesheets. props koopersmith, see #11420.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@16461 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -510,7 +510,7 @@ function wp_default_styles( &$styles ) {
 	$styles->add( 'imgareaselect', '/wp-includes/js/imgareaselect/imgareaselect.css', array(), '0.9.1' );
 	$styles->add( 'nav-menu', "/wp-admin/css/nav-menu$suffix.css", array(), '20100907' );
 	$styles->add( 'admin-bar', "/wp-includes/css/admin-bar$suffix.css", array(), '20101117' );
-	$styles->add( 'wp-jquery-ui-dialog', "/wp-includes/css/jquery-ui-dialog$suffix.css", array(), '20101117' );
+	$styles->add( 'wp-jquery-ui-dialog', "/wp-includes/css/jquery-ui-dialog$suffix.css", array(), '20101117b' );
 	$styles->add( 'wplink', "/wp-includes/js/tinymce/plugins/wplink/css/wplink$suffix.css", array(), '20101117' );
 
 	foreach ( $rtl_styles as $rtl_style ) {
