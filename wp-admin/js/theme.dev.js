5fa3c84fdb1a144795c13660cdd21f61cbf3e5ce
scribu <scribu@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-11-23 00:06:15 +0000

    Remove vestige code from theme.js. See #14936
    git-svn-id: http://svn.automattic.com/wordpress/trunk@16540 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/theme.dev.js
+++ wp-admin/js/theme.dev.js
@@ -27,33 +27,6 @@ var ThemeViewer;
 			$( '#filters :checkbox' ).unbind( 'click' ).click( function() {
 				filter_count();
 			});
-
-			$( 'p.tags a' ).unbind( 'click' ).click(function() {
-				$( 'p.tags a' ).unbind( 'click' ).click(function() { slow_down(); return false });   // Stop further clicks until we've done
-				$( '.loading' ).fadeIn();
-				$( '.random-info' ).fadeOut();
-
-				var item = this.href.replace( /.*?s=(.*?)#.*/, '$1' );
-
-				// Is this in the features list?
-				if ( $( 'input[value="' + item + '"]' ).length > 0 ) {
-					$( 'input[value="' + item + '"]' ).attr( 'checked', $( 'input[value="' + item + '"]' ).attr( 'checked' ) ? false : true );
-
-					filter_count();
-				}
-				else
-					$( 'input[name=s]' ).val( item );   // Can't find it, just use a search
-
-				// Set the options
-				opts.search = $( 'input[name=s]' ).val();
-				opts.order  = document.location.href.match( /order=(\w*)/ ) ? document.location.href.match( /order=(\w*)/ )[1] : 'random';
-
-				$( '#availablethemes td' ).fadeTo( 500, 0.1, function() {
-					$( '#availablethemes td img' ).hide();
-				} );
-
-				return false;
-			});
 		}
 
 		// These are the functions we expose
