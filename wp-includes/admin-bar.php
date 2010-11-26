50cb4c26c6b2efe5bce54ac473c3be8d1b581d99
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-11-26 21:51:12 +0000

    Admin bar phpdoc update. Props filosofo. fixes #15418
    git-svn-id: http://svn.automattic.com/wordpress/trunk@16599 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/admin-bar.php
+++ wp-includes/admin-bar.php
@@ -39,7 +39,7 @@ add_action( 'init', 'wp_admin_bar_init' );
  * This is called very late on the footer actions so that it will render after anything else being
  * added to the footer.
  *
- * It includes the action "wp_before_admin_bar_render" which should be used to hook in and
+ * It includes the action "admin_bar_menu" which should be used to hook in and
  * add new menus to the admin bar. That way you can be sure that you are adding at most optimal point,
  * right before the admin bar is rendered. This also gives you access to the $post global, among others.
  *
