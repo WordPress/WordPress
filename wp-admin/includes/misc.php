ce7bc3977b6ff843fa23c3957ed37acea9e7c1fd
nbachiyski <nbachiyski@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-11-30 19:13:27 +0000

    Proper spacing in win_is_writable(), according to the coding standards
    git-svn-id: http://svn.automattic.com/wordpress/trunk@16622 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/misc.php
+++ wp-admin/includes/misc.php
@@ -570,25 +570,25 @@ function saveDomDocument($doc, $filename) {
  * @param object $path
  * @return bool
  */
-function win_is_writable($path) {
+function win_is_writable( $path ) {
 	/* will work in despite of Windows ACLs bug
 	 * NOTE: use a trailing slash for folders!!!
 	 * see http://bugs.php.net/bug.php?id=27609
 	 * see http://bugs.php.net/bug.php?id=30931
 	 */
 
-	if ( $path[strlen($path)-1] == '/' ) // recursively return a temporary file path
-		return win_is_writable($path . uniqid(mt_rand()) . '.tmp');
-	else if ( is_dir($path) )
-		return win_is_writable($path . '/' . uniqid(mt_rand()) . '.tmp');
+	if ( $path[strlen( $path ) - 1] == '/' ) // recursively return a temporary file path
+		return win_is_writable( $path . uniqid( mt_rand() ) . '.tmp');
+	else if ( is_dir( $path ) )
+		return win_is_writable( $path . '/' . uniqid( mt_rand() ) . '.tmp' );
 	// check tmp file for read/write capabilities
-	$rm = file_exists($path);
-	$f = @fopen($path, 'a');
-	if ($f===false)
+	$rm = file_exists( $path );
+	$f = @fopen( $path, 'a' );
+	if ( $f === false )
 		return false;
-	fclose($f);
+	fclose( $f );
 	if ( ! $rm )
-		unlink($path);
+		unlink( $path );
 	return true;
 }
 
