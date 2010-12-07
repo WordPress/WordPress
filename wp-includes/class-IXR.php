c697ff4926588a4b19cf5df50273f7b98d8f47a4
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-12-07 18:55:13 +0000

    Initialize a variable. props kapeels for initial patch. fixes #15703.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@16770 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-IXR.php
+++ wp-includes/class-IXR.php
@@ -216,6 +216,7 @@ class IXR_Message
         xml_set_element_handler($this->_parser, 'tag_open', 'tag_close');
         xml_set_character_data_handler($this->_parser, 'cdata');
         $chunk_size = 262144; // 256Kb, parse in chunks to avoid the RAM usage on very large messages
+        $final = false;
         do {
             if (strlen($this->message) <= $chunk_size) {
                 $final = true;
