dce14f6ed248964510fbeb44d0b017aeedf7c2d1
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-12-08 18:13:18 +0000

    Revert 16805 that was so not what I meant to do
    git-svn-id: http://svn.automattic.com/wordpress/tags/3.0.2@16806 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- 3.0/wp-includes/registration-functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Deprecated. Use registration.php.
- *
- * @package WordPress
- */
-_deprecated_file( basename(__FILE__), '2.1', WPINC .  '/registration.php' );
-require_once(ABSPATH . WPINC .  '/registration.php');
-?>
