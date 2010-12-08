6a83feb219a4e93508a177d6875fe74532be62d4
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-12-08 22:07:29 +0000

    Remove post_type_supports check from register_post_type caps all together. fixes #14122.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@16825 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -1094,14 +1094,6 @@ function get_post_type_capabilities( $args ) {
 		$default_capabilities = array_merge( $default_capabilities, $default_capabilities_for_mapping );
 	}
 
-	if ( ! post_type_supports( $args->name, 'author' ) ) {
-		// While these may be checked in core, users/roles shouldn't need to be
-		// granted these by default if the post type doesn't support authors.
-		$default_capabilities['edit_others_posts']   = $default_capabilities['edit_posts'];
-		if ( $args->map_meta_cap )
-			$default_capabilities['delete_others_posts'] = $default_capabilities['delete_posts'];
-	}
-
 	$capabilities = array_merge( $default_capabilities, $args->capabilities );
 
 	// Remember meta capabilities for future reference.
