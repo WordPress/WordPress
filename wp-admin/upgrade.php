af5cc65c1c743bb7592c2bcb01448e2ec41a7304
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-12-08 21:35:08 +0000

    Fix typo. props SergeyBiryukov, fixes #14107.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@16823 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/upgrade.php
+++ wp-admin/upgrade.php
@@ -65,7 +65,7 @@ $mysql_compat   = version_compare( $mysql_version, $required_mysql_version, '>='
 	if ( !$mysql_compat && !$php_compat )
 		printf( __('You cannot update because <a href="http://codex.wordpress.org/Version_%1$s">WordPress %1$s</a> requires PHP version %2$s or higher and MySQL version %3$s or higher. You are running PHP version %4$s and MySQL version %5$s.'), $wp_version, $required_php_version, $required_mysql_version, $php_version, $mysql_version );
 	elseif ( !$php_compat )
-		printf( __('You cannot udate because <a href="http://codex.wordpress.org/Version_%1$s">WordPress %1$s</a> requires PHP version %2$s or higher. You are running version %3$s.'), $wp_version, $required_php_version, $php_version );
+		printf( __('You cannot update because <a href="http://codex.wordpress.org/Version_%1$s">WordPress %1$s</a> requires PHP version %2$s or higher. You are running version %3$s.'), $wp_version, $required_php_version, $php_version );
 	elseif ( !$mysql_compat )
 		printf( __('You cannot update because <a href="http://codex.wordpress.org/Version_%1$s">WordPress %1$s</a> requires MySQL version %2$s or higher. You are running version %3$s.'), $wp_version, $required_mysql_version, $mysql_version );
 ?>
