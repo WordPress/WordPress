bb27a217a5f39b00f803e067cb64764b42b23814
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-12-15 09:16:15 +0000

    Bumpity
    git-svn-id: http://svn.automattic.com/wordpress/trunk@16936 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.1-beta2';
+$wp_version = '3.1-beta2-16936';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
