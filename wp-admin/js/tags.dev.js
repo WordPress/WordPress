af0d6246ae06511a56f950fe4813771188a8b216
scribu <scribu@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-12-16 19:13:47 +0000

    Remove placeholder row when adding first term. See #15849
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17003 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/tags.dev.js
+++ wp-admin/js/tags.dev.js
@@ -45,6 +45,8 @@ jQuery(document).ready(function($) {
 			else
 				$('.tags').prepend( res.responses[0].supplemental['parents'] ); // As the parent is not visible, Insert the version with Parent - Child - ThisTerm
 
+			$('.tags .no-items').remove();
+
 			if ( form.find('select#parent') ) {
 				// Parents field exists, Add new term to the list.
 				var term = res.responses[1].supplemental;
