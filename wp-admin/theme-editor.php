afd6a5e45394689a3a88ca58e59ebfa14432bd77
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-12-18 18:37:11 +0000

    Slight tweak. props andrea_r, #15314.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17047 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/theme-editor.php
+++ wp-admin/theme-editor.php
@@ -27,7 +27,7 @@ $help .= '<p>' . __('After typing in your edits, click Update File.') . '</p>';
 $help .= '<p>' . __('<strong>Advice:</strong> think very carefully about your site crashing if you are live-editing the theme currently in use.') . '</p>';
 $help .= '<p>' . __('Upgrading to a newer version of the same theme will override changes made here. To avoid this, consider creating a <a href="http://codex.wordpress.org/Child_Themes" target="_blank">child theme</a> instead.') . '</p>';
 if ( is_network_admin() )
-	$help .= '<p>' . __('Any edits to files from this screen will be available to all sites in the network.') . '</p>';
+	$help .= '<p>' . __('Any edits to files from this screen will be reflected on all sites in the network.') . '</p>';
 $help .= '<p><strong>' . __('For more information:') . '</strong></p>';
 $help .= '<p>' . __('<a href="http://codex.wordpress.org/Theme_Development" target="_blank">Documentation on Theme Development</a>') . '</p>';
 $help .= '<p>' . __('<a href="http://codex.wordpress.org/Using_Themes" target="_blank">Documentation on Using Themes</a>') . '</p>';
