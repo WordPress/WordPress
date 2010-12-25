fc003bf8b1666ee9a4c9efb0c1c178aeeb02245d
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-12-25 23:38:33 +0000

    Happy holidays :D
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17148 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -8,7 +8,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.1-beta2-17056';
+$wp_version = '3.1-RC1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
