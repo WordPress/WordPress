0630489d888343b0af0f13d7c5b969f6003208fc
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2010-12-28 19:34:31 +0000

    Issue manage_pages_custom_column for hierarchical post types. Props mrwok, duck_. fixes #16000
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17160 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-posts-list-table.php
+++ wp-admin/includes/class-wp-posts-list-table.php
@@ -666,7 +666,7 @@ class WP_Posts_List_Table extends WP_List_Table {
 			default:
 			?>
 			<td <?php echo $attributes ?>><?php
-				if ( 'page' == $post->post_type )
+				if ( is_post_type_hierarchical( $post->post_type ) )
 					do_action( 'manage_pages_custom_column', $column_name, $post->ID );
 				else
 					do_action( 'manage_posts_custom_column', $column_name, $post->ID );
