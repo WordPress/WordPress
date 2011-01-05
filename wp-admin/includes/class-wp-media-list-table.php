694e3e9bf3275b72dac7cb4b24dd61fe8ba91855
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-01-05 16:27:39 +0000

    Don't show button in placeholder row. Props SergeyBiryukov. fixes #16095
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17222 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-media-list-table.php
+++ wp-admin/includes/class-wp-media-list-table.php
@@ -122,15 +122,6 @@ class WP_Media_List_Table extends WP_List_Table {
 	}
 
 	function no_items() {
-		if ( $this->detached ) {
-?>
-		<div class="tablenav">
-			<?php $this->extra_tablenav( 'top' ); ?>
-		</div>
-		<br class="clear">
-<?php
-		}
-
 		_e( 'No media attachments found.' );
 	}
 
