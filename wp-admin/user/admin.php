405e7dfa4fb94e918e851d153d714a572eaec0cf
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-01-20 03:04:20 +0000

    Correct user admin redirection checks, and deny access to the user admin when not running multisite. fixes #16297.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17332 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/user/admin.php
+++ wp-admin/user/admin.php
@@ -11,6 +11,11 @@ define('WP_USER_ADMIN', TRUE);
 
 require_once( dirname(dirname(__FILE__)) . '/admin.php');
 
+if ( ! is_multisite() ) {
+	wp_redirect( admin_url() );
+	exit;
+}
+
 if ( ! is_main_site() ) {
 	wp_redirect( user_admin_url() );
 	exit;
