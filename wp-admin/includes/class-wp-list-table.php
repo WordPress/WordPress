34358d9a4283d4c5fb4ed753f9fbfb7babcae765
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-01-22 18:36:54 +0000

    Add orderby and order hidden fields to each list form, to allow for state. props SergeyBiryukov, fixes #16341.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17353 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-list-table.php
+++ wp-admin/includes/class-wp-list-table.php
@@ -196,6 +196,10 @@ class WP_List_Table {
 
 		$input_id = $input_id . '-search-input';
 
+		if ( ! empty( $_REQUEST['orderby'] ) )
+			echo '<input type="hidden" name="orderby" value="' . esc_attr( $_REQUEST['orderby'] ) . '" />';
+		if ( ! empty( $_REQUEST['order'] ) ) 
+			echo '<input type="hidden" name="order" value="' . esc_attr( $_REQUEST['order'] ) . '" />';
 ?>
 <p class="search-box">
 	<label class="screen-reader-text" for="<?php echo $input_id ?>"><?php echo $text; ?>:</label>
