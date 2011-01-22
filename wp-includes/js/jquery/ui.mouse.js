9d0ec2454feb4f09db843df807abb188685122e7
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-01-22 02:30:03 +0000

    jQuery UI 1.8.9. Props demetris. fixes #16331
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17346 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/jquery/ui.mouse.js
+++ wp-includes/js/jquery/ui.mouse.js
@@ -1,7 +1,7 @@
 /*!
- * jQuery UI Mouse 1.8.8
+ * jQuery UI Mouse 1.8.9
  *
- * Copyright 2010, AUTHORS.txt (http://jqueryui.com/about)
+ * Copyright 2011, AUTHORS.txt (http://jqueryui.com/about)
  * Dual licensed under the MIT or GPL Version 2 licenses.
  * http://jquery.org/license
  *
