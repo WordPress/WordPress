6393921f0bed58e8e47676d783d1839ea45593e5
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-03-28 21:39:39 +0000

    Reduce backtracking in make_clickable() regex.  Reduce pcre.recursion_limit to avoid segfaulting. Props hakre. For 3.1.
    git-svn-id: http://svn.automattic.com/wordpress/branches/3.1@17571 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -1386,7 +1386,11 @@ function _make_email_clickable_cb($matches) {
 function make_clickable($ret) {
 	$ret = ' ' . $ret;
 	// in testing, using arrays here was found to be faster
-	$ret = preg_replace_callback('#(?<!=[\'"])(?<=[*\')+.,;:!&$\s>])(\()?([\w]+?://(?:[\w\\x80-\\xff\#%~/?@\[\]-]|[\'*(+.,;:!=&$](?![\b\)]|(\))?([\s]|$))|(?(1)\)(?![\s<.,;:]|$)|\)))+)#is', '_make_url_clickable_cb', $ret);
+	$save = @ini_set('pcre.recursion_limit', 10000);
+	$retval = preg_replace_callback('#(?<!=[\'"])(?<=[*\')+.,;:!&$\s>])(\()?([\w]+?://(?:[\w\\x80-\\xff\#%~/?@\[\]-]{1,2000}|[\'*(+.,;:!=&$](?![\b\)]|(\))?([\s]|$))|(?(1)\)(?![\s<.,;:]|$)|\)))+)#is', '_make_url_clickable_cb', $ret);
+	if (null !== $retval )
+		$ret = $retval;
+	@ini_set('pcre.recursion_limit', $save);
 	$ret = preg_replace_callback('#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]+)#is', '_make_web_ftp_clickable_cb', $ret);
 	$ret = preg_replace_callback('#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', '_make_email_clickable_cb', $ret);
 	// this one is not in an array because we need it to run last, for cleanup of accidental links within links
