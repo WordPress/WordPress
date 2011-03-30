7baf775b85080135733bf9fb04e39d569e40c038
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-03-30 20:50:05 +0000

    Assigning the return value of new by reference is deprecated. Props hakre. see #16767
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17573 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-settings.php
+++ wp-settings.php
@@ -216,7 +216,7 @@ do_action( 'sanitize_comment_cookies' );
  * @global object $wp_the_query
  * @since 2.0.0
  */
-$wp_the_query =& new WP_Query();
+$wp_the_query = new WP_Query();
 
 /**
  * Holds the reference to @see $wp_the_query
@@ -231,21 +231,21 @@ $wp_query =& $wp_the_query;
  * @global object $wp_rewrite
  * @since 1.5.0
  */
-$wp_rewrite =& new WP_Rewrite();
+$wp_rewrite = new WP_Rewrite();
 
 /**
  * WordPress Object
  * @global object $wp
  * @since 2.0.0
  */
-$wp =& new WP();
+$wp = new WP();
 
 /**
  * WordPress Widget Factory Object
  * @global object $wp_widget_factory
  * @since 2.8.0
  */
-$wp_widget_factory =& new WP_Widget_Factory();
+$wp_widget_factory = new WP_Widget_Factory();
 
 do_action( 'setup_theme' );
 
@@ -270,7 +270,7 @@ require( ABSPATH . WPINC . '/locale.php' );
  * @global object $wp_locale
  * @since 2.1.0
  */
-$wp_locale =& new WP_Locale();
+$wp_locale = new WP_Locale();
 
 // Load the functions for the active theme, for both parent and child theme if applicable.
 if ( TEMPLATEPATH !== STYLESHEETPATH && file_exists( STYLESHEETPATH . '/functions.php' ) )
