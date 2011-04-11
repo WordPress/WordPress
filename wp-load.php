dfafbed043d258c538dbddb33304941f473aca02
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-04-11 18:51:52 +0000

    E_RECOVERABLE_ERROR is always available in PHP 5.2+.  Remove back compat code.  Props hakre. see #16920
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17629 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-load.php
+++ wp-load.php
@@ -19,10 +19,7 @@
 /** Define ABSPATH as this files directory */
 define( 'ABSPATH', dirname(__FILE__) . '/' );
 
-if ( defined('E_RECOVERABLE_ERROR') )
-	error_reporting(E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR);
-else
-	error_reporting(E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING);
+error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
 
 if ( file_exists( ABSPATH . 'wp-config.php') ) {
 
