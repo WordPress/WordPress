ca1923c5105c5de7b2cee281e527512ecb1d12f3
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-04-21 22:21:27 +0000

    Update json2.js, props niallkennedy, fixes #16669
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17679 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -243,7 +243,7 @@ function wp_default_scripts( &$scripts ) {
 
 	$scripts->add( 'comment-reply', "/wp-includes/js/comment-reply$suffix.js", false, '20090102');
 
-	$scripts->add( 'json2', "/wp-includes/js/json2$suffix.js", false, '20110113');
+	$scripts->add( 'json2', "/wp-includes/js/json2$suffix.js", false, '2011-02-23');
 
 	$scripts->add( 'imgareaselect', "/wp-includes/js/imgareaselect/jquery.imgareaselect$suffix.js", array('jquery'), '0.9.1-20110113' );
 	$scripts->add_data( 'imgareaselect', 'group', 1 );
