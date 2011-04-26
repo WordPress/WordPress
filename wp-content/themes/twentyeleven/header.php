34cdfcdd4194a4246e45b9c038316396e51e7c2c
lancewillett <lancewillett@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-04-26 18:54:03 +0000

    Twenty Eleven: add additional conditional IDs for the html element, props iandstewart - see #17198
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17718 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyeleven/header.php
+++ wp-content/themes/twentyeleven/header.php
@@ -4,10 +4,16 @@
  * @subpackage Twenty Eleven
  */
 ?><!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
 <!--[if IE 7]>
 <html id="ie7" <?php language_attributes(); ?>>
 <![endif]-->
-<!--[if !IE 7]><!-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
