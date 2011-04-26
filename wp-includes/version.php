61e239a53307b5af183b5b02506a1511cf0c38d4
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-04-26 18:30:09 +0000

    Bump
    git-svn-id: http://svn.automattic.com/wordpress/branches/3.1@17712 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  * @global string $wp_version
  */
-$wp_version = '3.1.2-alpha';
+$wp_version = '3.1.2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
