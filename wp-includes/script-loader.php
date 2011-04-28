9fc2cddeea03f5534ca0f60ee82fe7b1e5a753f5
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-04-28 16:51:12 +0000

    Define border-radius with vendor properties. props hebbet, see #17226.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17750 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -483,7 +483,7 @@ function wp_default_styles( &$styles ) {
 	// Any rtl stylesheets that don't have a .dev version for ltr
 	$no_suffix = array( 'farbtastic' );
 
-	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20110425' );
+	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20110428' );
 
 	$styles->add( 'ie', "/wp-admin/css/ie$suffix.css", array(), '20101102' );
 	$styles->add_data( 'ie', 'conditional', 'lte IE 7' );
