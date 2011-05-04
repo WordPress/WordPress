061283b3d53b1d58f6d44ed17bd1a0cb1d9572e6
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-05-04 23:53:36 +0000

    Make twentyeleven the default theme. see #17198
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17804 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-constants.php
+++ wp-includes/default-constants.php
@@ -294,7 +294,7 @@ function wp_templating_constants( ) {
 	 * @since 3.0.0
 	 */
 	if ( !defined('WP_DEFAULT_THEME') )
-		define( 'WP_DEFAULT_THEME', 'twentyten' );
+		define( 'WP_DEFAULT_THEME', 'twentyeleven' );
 
 }
 
