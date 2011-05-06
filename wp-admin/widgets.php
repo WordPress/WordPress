edfc0fce3d5fdeb1d3662620527f38deb8a3bc21
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-05-06 20:38:49 +0000

    Add context for widget Deactivate string. Props ocean90. fixes #17295
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17824 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/widgets.php
+++ wp-admin/widgets.php
@@ -357,7 +357,7 @@ require_once( './admin-header.php' ); ?>
 	<div id="available-widgets" class="widgets-holder-wrap">
 		<div class="sidebar-name">
 		<div class="sidebar-name-arrow"><br /></div>
-		<h3><?php _e('Available Widgets'); ?> <span id="removing-widget"><?php _e('Deactivate'); ?> <span></span></span></h3></div>
+		<h3><?php _e('Available Widgets'); ?> <span id="removing-widget"><?php _ex('Deactivate', 'removing-widget'); ?> <span></span></span></h3></div>
 		<div class="widget-holder">
 		<p class="description"><?php _e('Drag widgets from here to a sidebar on the right to activate them. Drag widgets back here to deactivate them and delete their settings.'); ?></p>
 		<div id="widget-list">
