427a92a0d4c231b90581a49ac7fea41bbb7c75b1
iandstewart <iandstewart@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-05-06 17:58:41 +0000

    Twenty Eleven: correct typo in inline documentation props markmcwilliams; See #17198
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17819 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyeleven/category.php
+++ wp-content/themes/twentyeleven/category.php
@@ -28,7 +28,7 @@ get_header(); ?>
 					<?php
 						/* Include the Post-Format-specific template for the content.
 						 * If you want to overload this in a child theme then include a file
-						 * called loop-___.php (where ___ is the Post Format name) and that will be used instead.
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 						 */
 						get_template_part( 'content', get_post_format() );
 					?>
