342aa25c3e095327335cac0625047614a2527d48
koopersmith <koopersmith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-05-11 22:37:48 +0000

    Widgets: nibble the corners and use new arrows. Props chexee. see #17324.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17869 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -493,7 +493,7 @@ function wp_default_styles( &$styles ) {
 	$styles->add_data( 'ie', 'conditional', 'lte IE 7' );
 
 	// all colors stylesheets need to have the same query strings (cache manifest compat)
-	$colors_version = '20110511f';
+	$colors_version = '20110511g';
 
 	// Register "meta" stylesheet for admin colors. All colors-* style sheets should have the same version string.
 	$styles->add( 'colors', true, array(), $colors_version );
@@ -505,7 +505,7 @@ function wp_default_styles( &$styles ) {
 	$styles->add( 'ms', "/wp-admin/css/ms$suffix.css", array(), '20101213' );
 	$styles->add( 'global', "/wp-admin/css/global$suffix.css", array(), '20110511d' );
 	$styles->add( 'media', "/wp-admin/css/media$suffix.css", array(), '20110121' );
-	$styles->add( 'widgets', "/wp-admin/css/widgets$suffix.css", array(), '20110504' );
+	$styles->add( 'widgets', "/wp-admin/css/widgets$suffix.css", array(), '20110511' );
 	$styles->add( 'dashboard', "/wp-admin/css/dashboard$suffix.css", array(), '20110511b' );
 	$styles->add( 'install', "/wp-admin/css/install$suffix.css", array(), '20110506' ); // Readme as well
 	$styles->add( 'theme-editor', "/wp-admin/css/theme-editor$suffix.css", array(), '20110506' );
