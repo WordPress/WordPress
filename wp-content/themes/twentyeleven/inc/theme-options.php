033b47c5d3e97a961eb12ff1b058e40f6fb77fd1
lancewillett <lancewillett@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-05-12 23:13:56 +0000

    Twenty Eleven footer style fixes, props matveb
     * Improve footer credits, add WordPress PNG
     * Add hover color to footer links
    
    git-svn-id: http://svn.automattic.com/wordpress/trunk@17908 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyeleven/inc/theme-options.php
+++ wp-content/themes/twentyeleven/inc/theme-options.php
@@ -296,7 +296,8 @@ function twentyeleven_print_link_color_style() {
 		.widget_twentyeleven_ephemera .comments-link a:hover,
 		section.recent-posts .other-recent-posts a[rel="bookmark"]:hover,
 		section.recent-posts .other-recent-posts .comments-link a:hover,
-		.format-image footer.entry-meta a:hover {
+		.format-image footer.entry-meta a:hover,
+		#site-generator a:hover {
 			color: <?php echo $link_color; ?>;
 		}
 		section.recent-posts .other-recent-posts .comments-link a:hover {
