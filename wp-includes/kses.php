95279728aee62200af7d4f13a31cdade3523485c
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-05-25 22:15:12 +0000

    Upddate FSF address in kses. props mcepl, fixes #14967.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18052 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/kses.php
+++ wp-includes/kses.php
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA  or visit
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  * http://www.gnu.org/licenses/gpl.html
  *
  * [kses strips evil scripts!]
