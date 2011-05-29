7536a1107e382c7f25e4b78c6ec75d804aa338ea
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-05-29 21:18:58 +0000

    Improve JS array/object detection when outputting the TinyMCE init array.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18071 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/post.php
+++ wp-admin/includes/post.php
@@ -1649,7 +1649,7 @@ function wp_tiny_mce( $teeny = false, $settings = false ) {
 			$val = $v ? 'true' : 'false';
 			$mce_options .= $k . ':' . $val . ', ';
 			continue;
-		} elseif ( !empty($v) && is_string($v) && ( '{' == $v{0} || '[' == $v{0} || preg_match('/^\(?function ?\(/', $v) ) ) {
+		} elseif ( is_string($v) && ( ('{' == $v{0} && '}' == $v{strlen($v) - 1}) || ('[' == $v{0} && ']' == $v{strlen($v) - 1}) || preg_match('/^\(?function ?\(/', $v) ) ) {
 			$mce_options .= $k . ':' . $v . ', ';
 			continue;
 		}
