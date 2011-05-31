fc52dfeec4407f98816a8a781bc768c1bc36b04f
dd32 <dd32@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-05-31 05:05:42 +0000

    Correct the logic for Taxonomy edit page actions. Props michaeltyson for initial patch. Fixes #17617
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18078 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-tag-form.php
+++ wp-admin/edit-tag-form.php
@@ -68,7 +68,7 @@ do_action($taxonomy . '_pre_edit_form', $tag, $taxonomy);  ?>
 		// Back compat hooks
 		if ( 'category' == $taxonomy )
 			do_action('edit_category_form_fields', $tag);
-		if ( 'link_category' == $taxonomy )
+		elseif ( 'link_category' == $taxonomy )
 			do_action('edit_link_category_form_fields', $tag);
 		else
 			do_action('edit_tag_form_fields', $tag);
@@ -80,7 +80,7 @@ do_action($taxonomy . '_pre_edit_form', $tag, $taxonomy);  ?>
 // Back compat hooks
 if ( 'category' == $taxonomy )
 	do_action('edit_category_form', $tag);
-if ( 'link_category' == $taxonomy )
+elseif ( 'link_category' == $taxonomy )
 	do_action('edit_link_category_form', $tag);
 else
 	do_action('edit_tag_form', $tag);
