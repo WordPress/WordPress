46d0e53c7bf86172da2fbfe56d3a80df801bbc30
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-06-02 16:04:02 +0000

    RTL fixes for dfw tagline, permalink editing, and hscroll. Props yoavf. fixes #17639
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18113 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -482,7 +482,7 @@ function wp_default_styles( &$styles ) {
 	// Any rtl stylesheets that don't have a .dev version for ltr
 	$no_suffix = array( 'farbtastic' );
 
-	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20110601' );
+	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20110602' );
 
 	$styles->add( 'ie', "/wp-admin/css/ie$suffix.css", array(), '20110601' );
 	$styles->add_data( 'ie', 'conditional', 'lte IE 7' );
@@ -498,7 +498,7 @@ function wp_default_styles( &$styles ) {
 	$styles->add( 'colors-classic', "/wp-admin/css/colors-classic$suffix.css", array(), $colors_version );
 
 	$styles->add( 'ms', "/wp-admin/css/ms$suffix.css", array(), '20110601' );
-	$styles->add( 'global', "/wp-admin/css/global$suffix.css", array(), '20110511g' );
+	$styles->add( 'global', "/wp-admin/css/global$suffix.css", array(), '20110602' );
 	$styles->add( 'media', "/wp-admin/css/media$suffix.css", array(), '20110519' );
 	$styles->add( 'widgets', "/wp-admin/css/widgets$suffix.css", array(), '20110601' );
 	$styles->add( 'dashboard', "/wp-admin/css/dashboard$suffix.css", array(), '20110511c' );
