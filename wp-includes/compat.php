38e198f8f125d8ae3adf4f2ee15da8789e51bb29
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-06-02 14:21:09 +0000

    Resurrect hash_hmac() compat for hosts that --disable-hash. Props aaroncampbell. fixes #17647
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18111 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/compat.php
+++ wp-includes/compat.php
@@ -31,3 +31,32 @@ function _mb_substr( $str, $start, $length=null, $encoding=null ) {
 	$chars = is_null( $length )? array_slice( $match[0], $start ) : array_slice( $match[0], $start, $length );
 	return implode( '', $chars );
 }
+
+if ( !function_exists('hash_hmac') ):
+function hash_hmac($algo, $data, $key, $raw_output = false) {
+	return _hash_hmac($algo, $data, $key, $raw_output);
+}
+endif;
+
+function _hash_hmac($algo, $data, $key, $raw_output = false) {
+	$packs = array('md5' => 'H32', 'sha1' => 'H40');
+
+	if ( !isset($packs[$algo]) )
+		return false;
+
+	$pack = $packs[$algo];
+
+	if (strlen($key) > 64)
+		$key = pack($pack, $algo($key));
+
+	$key = str_pad($key, 64, chr(0));
+
+	$ipad = (substr($key, 0, 64) ^ str_repeat(chr(0x36), 64));
+	$opad = (substr($key, 0, 64) ^ str_repeat(chr(0x5C), 64));
+
+	$hmac = $algo($opad . pack($pack, $algo($ipad . $data)));
+
+	if ( $raw_output )
+		return pack( $pack, $hmac );
+	return $hmac;
+}
