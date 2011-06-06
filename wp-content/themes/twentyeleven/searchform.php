0a85e3f74fd339e04f37ef2da641319dde60bacb
iandstewart <iandstewart@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-06-06 14:35:30 +0000

    Twenty Eleven: Add a hidden label for search forms; See #17524
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18166 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyeleven/searchform.php
+++ wp-content/themes/twentyeleven/searchform.php
@@ -8,6 +8,7 @@
  */
 ?>
 	<form method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+		<label for="s" class="assistive-text"><?php _e( 'Search', 'twentyeleven' ); ?></label> 
 		<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'twentyeleven' ); ?>" />
 		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'twentyeleven' ); ?>" />
 	</form>
