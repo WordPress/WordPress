ae81d75d39e968ec865c86ffd23747b703925671
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-06-06 02:22:50 +0000

    Fix typo in remove_accents(), props scribu, fixes #16232
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18159 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -562,7 +562,7 @@ function remove_accents($string) {
 		chr(195).chr(176) => 'd', chr(195).chr(177) => 'n',
 		chr(195).chr(178) => 'o', chr(195).chr(179) => 'o',
 		chr(195).chr(180) => 'o', chr(195).chr(181) => 'o',
-		chr(195).chr(182) => 'o', chr(195).chr(182) => 'o',
+		chr(195).chr(182) => 'o', chr(195).chr(184) => 'o',
 		chr(195).chr(185) => 'u', chr(195).chr(186) => 'u',
 		chr(195).chr(187) => 'u', chr(195).chr(188) => 'u',
 		chr(195).chr(189) => 'y', chr(195).chr(190) => 'th',
