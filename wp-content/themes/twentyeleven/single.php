667fe521ec04e460999126453ae8dfb0552ea121
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-06-07 00:08:01 +0000

    Use correct textdomain. props knutsp, fixes #17712.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18174 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyeleven/single.php
+++ wp-content/themes/twentyeleven/single.php
@@ -15,7 +15,7 @@ get_header(); ?>
 				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 					<nav id="nav-single">
-						<h1 class="assistive-text"><?php _e( 'Post navigation', 'toolbox' ); ?></h1>
+						<h1 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h1>
 						<span class="nav-previous"><?php previous_post_link( '%link', __( '&larr; Previous', 'twentyeleven' ) ); ?></span>
 						<span class="nav-next"><?php next_post_link( '%link', __( 'Next &rarr;', 'twentyeleven' ) ); ?></span>
 					</nav><!-- #nav-single -->
