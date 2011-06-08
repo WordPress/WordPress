c0cec1175eb98158ce3a53ce3b1246a54d987af2
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-06-08 22:39:08 +0000

    Enable autocomplete on the href and title fields in the link popup, props nacin, fixes #17101 Remove unneeded for=".." attributes from the <label> tags there.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18207 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/internal-linking.php
+++ wp-admin/includes/internal-linking.php
@@ -76,20 +76,20 @@ function wp_link_dialog() {
 	<div id="link-options">
 		<p class="howto"><?php _e( 'Enter the destination URL' ); ?></p>
 		<div>
-			<label for="url-field"><span><?php _e( 'URL' ); ?></span><input id="url-field" type="text" tabindex="10" autocomplete="off" /></label>
+			<label><span><?php _e( 'URL' ); ?></span><input id="url-field" type="text" tabindex="10" name="href" /></label>
 		</div>
 		<div>
-			<label for="link-title-field"><span><?php _e( 'Title' ); ?></span><input id="link-title-field" type="text" tabindex="20" autocomplete="off" /></label>
+			<label><span><?php _e( 'Title' ); ?></span><input id="link-title-field" type="text" tabindex="20" name="linktitle" /></label>
 		</div>
 		<div class="link-target">
-			<label for="link-target-checkbox"><input type="checkbox" id="link-target-checkbox" tabindex="30" /> <?php _e( 'Open link in a new window/tab' ); ?></label>
+			<label><input type="checkbox" id="link-target-checkbox" tabindex="30" /> <?php _e( 'Open link in a new window/tab' ); ?></label>
 		</div>
 	</div>
 	<?php $show_internal = '1' == get_user_setting( 'wplink', '0' ); ?>
 	<p class="howto toggle-arrow <?php if ( $show_internal ) echo 'toggle-arrow-active'; ?>" id="internal-toggle"><?php _e( 'Or link to existing content' ); ?></p>
 	<div id="search-panel"<?php if ( ! $show_internal ) echo ' style="display:none"'; ?>>
 		<div class="link-search-wrapper">
-			<label for="search-field">
+			<label>
 				<span><?php _e( 'Search' ); ?></span>
 				<input type="text" id="search-field" class="link-search-field" tabindex="60" autocomplete="off" />
 				<img class="waiting" src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" alt="" />
