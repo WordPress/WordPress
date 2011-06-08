07356cc1a7ca80be18b9659155089aab39b1b674
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-06-08 18:23:49 +0000

    Fix borders at the bottom of tabs in postboxes, props dcowgill, fixes #17699 Fix color of active tab in Categories postbox.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18202 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -482,13 +482,13 @@ function wp_default_styles( &$styles ) {
 	// Any rtl stylesheets that don't have a .dev version for ltr
 	$no_suffix = array( 'farbtastic' );
 
-	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20110606' );
+	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20110608' );
 
 	$styles->add( 'ie', "/wp-admin/css/ie$suffix.css", array(), '20110601' );
 	$styles->add_data( 'ie', 'conditional', 'lte IE 7' );
 
 	// all colors stylesheets need to have the same query strings (cache manifest compat)
-	$colors_version = '20110605b';
+	$colors_version = '20110608';
 
 	// Register "meta" stylesheet for admin colors. All colors-* style sheets should have the same version string.
 	$styles->add( 'colors', true, array(), $colors_version );
