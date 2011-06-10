e67a3b2e32a579a1c6c96505ca14d01fd5561bd1
markjaquith <markjaquith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-06-10 16:35:32 +0000

    Be less specific about theme and plugin repo licenses (as they can vary in specifics). They are all compatible with the license WordPress uses, which is what matters. fixes #16514
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18231 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/theme-install.php
+++ wp-admin/theme-install.php
@@ -39,7 +39,7 @@ $body_id = $tab;
 
 do_action('install_themes_pre_' . $tab); //Used to override the general interface, Eg, install or theme information.
 
-$help = '<p>' . sprintf(__('You can find additional themes for your site by using the Theme Browser/Installer on this screen, which will display themes from the <a href="%s" target="_blank">WordPress.org Theme Directory</a>. These themes are designed and developed by third parties, are available free of charge, and are licensed under the GNU General Public License, version 2, just like WordPress.'), 'http://wordpress.org/extend/themes/') . '</p>';
+$help = '<p>' . sprintf(__('You can find additional themes for your site by using the Theme Browser/Installer on this screen, which will display themes from the <a href="%s" target="_blank">WordPress.org Theme Directory</a>. These themes are designed and developed by third parties, are available free of charge, and are compatible with the license WordPress uses.'), 'http://wordpress.org/extend/themes/') . '</p>';
 $help .= '<p>' . __('You can Search for themes by keyword, author, or tag, or can get more specific and search by criteria listed in the feature filter. Alternately, you can browse the themes that are Featured, Newest, or Recently Updated. When you find a theme you like, you can preview it or install it.') . '</p>';
 $help .= '<p>' . __('You can Upload a theme manually if you have already downloaded its ZIP archive onto your computer (make sure it is from a trusted and original source). You can also do it the old-fashioned way and copy a downloaded theme&#8217;s folder via FTP into your <code>/wp-content/themes</code> directory.') . '</p>';
 $help .= '<p><strong>' . __('For more information:') . '</strong></p>';
