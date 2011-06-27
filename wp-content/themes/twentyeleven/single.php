e38b292638665f2201b04e2b0f3140839a57ca45
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-06-27 22:02:29 +0000

    Fix arrows in 2011, props iandstewart, fixes #17809
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18359 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyeleven/single.php
+++ wp-content/themes/twentyeleven/single.php
@@ -16,8 +16,8 @@ get_header(); ?>
 
 					<nav id="nav-single">
 						<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
-						<span class="nav-previous"><?php previous_post_link( '%link', __( '&larr; Previous', 'twentyeleven' ) ); ?></span>
-						<span class="nav-next"><?php next_post_link( '%link', __( 'Next &rarr;', 'twentyeleven' ) ); ?></span>
+						<span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span> Previous', 'twentyeleven' ) ); ?></span>
+						<span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></span>
 					</nav><!-- #nav-single -->
 
 					<?php get_template_part( 'content', 'single' ); ?>
