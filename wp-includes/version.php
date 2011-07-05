36d399aa36650d017361fb462e3b30cc2213a099
westi <westi@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-07-05 10:07:13 +0000

    Update the version in the 3.2 branch to alpha status.
    git-svn-id: http://svn.automattic.com/wordpress/branches/3.2@18401 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.2';
+$wp_version = '3.2.1-alpha';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
