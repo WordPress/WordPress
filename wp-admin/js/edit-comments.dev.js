3713abe855410066a8e979d4691a13f609579258
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-08-03 10:36:18 +0000

    Remove debug remnants, props scribu, see #16695
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18499 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/edit-comments.dev.js
+++ wp-admin/js/edit-comments.dev.js
@@ -548,12 +548,8 @@ $(document).ready(function(){
 	commentReply.init();
 	$(document).delegate('span.delete a.delete', 'click', function(){return false;});
 
-	if ( typeof QTags != 'undefined' ) {
+	if ( typeof QTags != 'undefined' )
 		quicktags({quicktags_id: 'replycontent', quicktags_buttons: 'strong,em,link,block,del,ins,img,ul,ol,li,code,spell,close'});
-	
-	
-	console.debug('typeof QTags')
-	}
 
 	if ( typeof $.table_hotkeys != 'undefined' ) {
 		make_hotkeys_redirect = function(which) {
