51e91e68c32740b6500305873f272477989e3fd4
nbachiyski <nbachiyski@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-08-11 04:33:22 +0000

    Exclude link tags from internationalized strings in Credits and Freedoms links in the admin footer
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18529 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -22,9 +22,9 @@ $upgrade = apply_filters( 'update_footer', '' );
 $footer_text = array(
 	'<span id="footer-thankyou">' . __( 'Thank you for creating with <a href="http://wordpress.org/">WordPress</a>.' ) . '</span>',
 	__( '<a href="http://codex.wordpress.org/">Documentation</a>' ),
-	sprintf( __( '<a href="%s">Freedoms</a>' ), admin_url( 'freedoms.php' ) ),
+	sprintf( '<a href="%s">' . __( 'Freedoms' ) . '</a>', admin_url( 'freedoms.php' ) ),
 	__('<a href="http://wordpress.org/support/forum/4">Feedback</a>'),
-	sprintf(__('<a href="%s">Credits</a>'), admin_url('credits.php') ),
+	sprintf( '<a href="%s">' . __('Credits') . '</a>', admin_url('credits.php') ),
 );
 echo apply_filters( 'admin_footer_text', implode( ' &bull; ', $footer_text ) );
 unset( $footer_text );
