39ba82bb3f319f746d88b9e77d712d8c43d330cc
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-08-26 19:00:07 +0000

    Handle locales that use a comma as a separator when generating version classes. Props olivM. fixes #18460
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18608 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -96,7 +96,7 @@ if ( is_admin_bar_showing() )
 if ( is_rtl() )
 	$admin_body_class .= ' rtl';
 
-$admin_body_class .= ' branch-' . str_replace( '.', '-', floatval( $wp_version ) );
+$admin_body_class .= ' branch-' . str_replace( array( '.', ',' ), '-', floatval( $wp_version ) );
 $admin_body_class .= ' version-' . str_replace( '.', '-', preg_replace( '/^([.0-9]+).*/', '$1', $wp_version ) );
 $admin_body_class .= ' admin-color-' . sanitize_html_class( get_user_option( 'admin_color' ), 'fresh' );
 
