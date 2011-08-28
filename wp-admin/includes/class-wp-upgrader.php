b16b6f8a3127be46893d16602cbfaa231d817994
dd32 <dd32@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-08-28 05:51:38 +0000

    Only clean up the uploaded files after a successful (or failed) install. Allows files to persist past the FTP credential screen. See #18182
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18616 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-upgrader.php
+++ wp-admin/includes/class-wp-upgrader.php
@@ -399,9 +399,13 @@ class Plugin_Upgrader extends WP_Upgrader {
 					'hook_extra' => array()
 					));
 
+		if ( ! $this->result || is_wp_error($this->result) )
+			return $this->result;
+
 		// Force refresh of plugin update information
 		delete_site_transient('update_plugins');
 
+		return true;
 	}
 
 	function upgrade($plugin) {
@@ -646,10 +650,7 @@ class Theme_Upgrader extends WP_Upgrader {
 		// Force refresh of theme update information
 		delete_site_transient('update_themes');
 
-		if ( empty($result['destination_name']) )
-			return false;
-		else
-			return $result['destination_name'];
+		return true;
 	}
 
 	function upgrade($theme) {
