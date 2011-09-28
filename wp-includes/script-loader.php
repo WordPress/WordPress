21d9701150a2f476c06afacb3a7c29edb49033a0
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-09-28 16:05:59 +0000

    Remove max-width from list tables. Better to limit the width on individual fields or areas where readability is impacted, see #18198
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18801 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -432,7 +432,7 @@ function wp_default_styles( &$styles ) {
 	// Any rtl stylesheets that don't have a .dev version for ltr
 	$no_suffix = array( 'farbtastic' );
 
-	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20110927' );
+	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20110928' );
 
 	$styles->add( 'ie', "/wp-admin/css/ie$suffix.css", array(), '20110919' );
 	$styles->add_data( 'ie', 'conditional', 'lte IE 7' );
