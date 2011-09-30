ed4c502e1662fe2acedd20e1304680f4afbf566d
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-09-30 17:26:14 +0000

    s/Settings/Network Settings/. Props wpmuguru. fixes #16027
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18842 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/network/settings.php
+++ wp-admin/network/settings.php
@@ -16,7 +16,7 @@ if ( ! is_multisite() )
 if ( ! current_user_can( 'manage_network_options' ) )
 	wp_die( __( 'You do not have permission to access this page.' ) );
 
-$title = __( 'Settings' );
+$title = __( 'Network Settings' );
 $parent_file = 'settings.php';
 
 add_contextual_help( $current_screen,
@@ -109,7 +109,7 @@ if ( isset( $_GET['updated'] ) ) {
 
 <div class="wrap">
 	<?php screen_icon('options-general'); ?>
-	<h2><?php _e( 'Settings' ) ?></h2>
+	<h2><?php echo esc_html( $title ); ?></h2>
 	<form method="post" action="settings.php">
 		<?php wp_nonce_field( 'siteoptions' ); ?>
 		<h3><?php _e( 'Operational Settings' ); ?></h3>
