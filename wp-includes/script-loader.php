4801064ecda8c83ecae0a8c2ee6527e2ae6dd165
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-10-02 17:04:09 +0000

    Fix border radius for editor split-buttons, props trepmal, fixes #18839
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18865 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -454,7 +454,7 @@ function wp_default_styles( &$styles ) {
 	$styles->add( 'imgareaselect', '/wp-includes/js/imgareaselect/imgareaselect.css', array(), '0.9.1' );
 	$styles->add( 'admin-bar', "/wp-includes/css/admin-bar$suffix.css", array(), '20110926' );
 	$styles->add( 'wp-jquery-ui-dialog', "/wp-includes/css/jquery-ui-dialog$suffix.css", array(), '20101224' );
-	$styles->add( 'editor-buttons', "/wp-includes/css/editor-buttons$suffix.css", array(), '20110928' );
+	$styles->add( 'editor-buttons', "/wp-includes/css/editor-buttons$suffix.css", array(), '20111002' );
 	$styles->add( 'wp-pointer', "/wp-includes/css/wp-pointer$suffix.css", array(), '20110918' );
 
 	foreach ( $rtl_styles as $rtl_style ) {
