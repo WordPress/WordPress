d60869e4901a585405c4ff3a151c5f4dcffacea2
koopersmith <koopersmith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-10-11 21:52:40 +0000

    Improve padding on admin menu submenus. see #18382.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@18947 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -431,7 +431,7 @@ function wp_default_styles( &$styles ) {
 	// Any rtl stylesheets that don't have a .dev version for ltr
 	$no_suffix = array( 'farbtastic' );
 
-	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20111011b' );
+	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20111011c' );
 
 	$styles->add( 'ie', "/wp-admin/css/ie$suffix.css", array(), '20111010' );
 	$styles->add_data( 'ie', 'conditional', 'lte IE 7' );
