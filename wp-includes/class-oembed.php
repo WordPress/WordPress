c3bcc9800eb4b41f2f254b0a75959f49b952db6a
duck_ <duck_@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-10-28 19:42:57 +0000

    Fix polldaddy oEmbed regular expression to work with new style URLs. Props r-a-y, azaozz. Fixes #17106.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19076 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-oembed.php
+++ wp-includes/class-oembed.php
@@ -45,7 +45,7 @@ class WP_oEmbed {
 			'http://gi*.photobucket.com/groups/*'           => array( 'http://photobucket.com/oembed',            false ),
 			'#http://(www\.)?scribd\.com/.*#i'              => array( 'http://www.scribd.com/services/oembed',    true  ),
 			'http://wordpress.tv/*'                         => array( 'http://wordpress.tv/oembed/',              false ),
-			'#http://(answers|surveys)\.polldaddy.com/.*#i' => array( 'http://polldaddy.com/oembed/',             true  ),
+			'#http://(.+\.)?polldaddy\.com/.*#i' 		=> array( 'http://polldaddy.com/oembed/',             true  ),
 			'#http://(www\.)?funnyordie\.com/videos/.*#i'   => array( 'http://www.funnyordie.com/oembed',         true  ),
 		) );
 
