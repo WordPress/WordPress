307f243ee4d2bb438aba443d9b188189cdcce141
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-11-07 19:49:50 +0000

    Fix tabbing from 'title' to 'content', see #19088
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19206 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-form-advanced.php
+++ wp-admin/edit-form-advanced.php
@@ -317,7 +317,7 @@ wp_nonce_field( 'samplepermalink', 'samplepermalinknonce', false );
 <?php if ( post_type_supports($post_type, 'editor') ) { ?>
 <div id="postdivrich" class="postarea">
 
-<?php wp_editor($post->post_content, 'content', array('dfw' => true) ); ?>
+<?php wp_editor($post->post_content, 'content', array('dfw' => true, 'tabindex' => 1) ); ?>
 
 <table id="post-status-info" cellspacing="0"><tbody><tr>
 	<td id="wp-word-count"><?php printf( __( 'Word count: %s' ), '<span class="word-count">0</span>' ); ?></td>
