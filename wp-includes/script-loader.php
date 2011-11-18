042635b68cf8def4d5a81ad1aea4cc6b31deff49
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-11-18 01:56:50 +0000

    Tweak install.php CSS to better fit translations, props SergeyBiryukov, fixes #19281
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19331 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -449,7 +449,7 @@ function wp_default_styles( &$styles ) {
 	$styles->add( 'colors-classic', "/wp-admin/css/colors-classic$suffix.css", array('wp-admin'), $colors_version );
 
 	$styles->add( 'media', "/wp-admin/css/media$suffix.css", array(), '20111110' );
-	$styles->add( 'install', "/wp-admin/css/install$suffix.css", array(), '20111115' ); // Readme as well
+	$styles->add( 'install', "/wp-admin/css/install$suffix.css", array(), '20111117' ); // Readme as well
 	$styles->add( 'thickbox', '/wp-includes/js/thickbox/thickbox.css', array(), '20111117' );
 	$styles->add( 'farbtastic', '/wp-admin/css/farbtastic.css', array(), '1.3u1' );
 	$styles->add( 'jcrop', '/wp-includes/js/jcrop/jquery.Jcrop.css', array(), '0.9.8' );
