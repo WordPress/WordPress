91746f31794a7f82788b64161292b84c0110605d
duck_ <duck_@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-11-22 21:47:28 +0000

    Further tweaks to What's New layout and content. Props chexee. See #18742.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19409 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -36,17 +36,17 @@ include( './admin-header.php' );
 <div class="changelog">
 	<h3><?php _e( 'Easier Uploading' ); ?></h3>
 	
-	<div class="feature-section images-stagger-left">
+	<div class="feature-section images-stagger-right">
 		<div class="feature-images">
-			<img src="images/screenshots/drag-and-drop.png" width="200" class="angled-left" />
 			<img src="images/screenshots/media-icon.png" width="200" class="angled-right" />
+			<img src="images/screenshots/drag-and-drop.png" width="200" class="angled-left" />
 		</div>
-		<div class="right-feature">
-			<h4><?php _e( 'Drag-and-Drop Media Uploader' ); ?></h4>
-			<p><?php _e( 'Adding photos or other files to posts and pages just got easier. Drag files from your desktop and drop them into the uploader. Add one file at a time, or many at once.' ); ?></p>
-
+		<div class="left-feature">
 			<h4><?php _e( 'File Type Detection' ); ?></h4>
 			<p><?php _e( 'We&#039;ve streamlined things! Instead of needing to click on a specific upload icon based on your file type, now there&#039;s just one. Once your file is uploaded, the appropriate fields will be displayed for entering information based on the file type.' ); ?></p>
+
+			<h4><?php _e( 'Drag-and-Drop Media Uploader' ); ?></h4>
+			<p><?php _e( 'Adding photos or other files to posts and pages just got easier. Drag files from your desktop and drop them into the uploader. Add one file at a time, or many at once.' ); ?></p>
 
 			<h4><?php _e( 'More File Formats' ); ?></h4>
 			<p><?php _e( 'We&#039;ve added the rar and 7z file formats to the list of allowed file types in the uploader.' ); ?></p>
@@ -60,28 +60,26 @@ include( './admin-header.php' );
 	<div class="feature-section angled-left">
 		<div class="left-feature">
 			<h4><?php _e( 'Flyout Menus' ); ?></h4>
-			<p><?php _e( 'Speed up navigating the dashboard and reduce repetitive clicking with our new flyout submenus. As you hover over each main menu item in your dashboard navigation, the submenus will magically appear, providing single-click access to any dashboard screen from any other.' ); ?></p>
+			<p><?php _e( 'Speed up navigating the dashboard and reduce repetitive clicking with our new flyout submenus. As you hover over each main menu item in your dashboard navigation, the submenus will magically appear, providing single-click access to any dashboard screen.' ); ?></p>
 		</div>
-		<span class="image-mask"><img src="images/screenshots/admin-flyouts.png" /></span>
+		<img src="images/screenshots/admin-flyouts.png" />
 		<div class="right-feature">
-			<h4><?php _e( 'Header + Admin Bar = Toolbar' ); ?></h4>
-			<p><?php _e( 'To save space and increase efficiency, we&#039;ve combined the admin bar and the old Dashboard header into one persistent toolbar. Hovering over the toolbar items will reveal submenus when available for quick access. ' ); ?></p>
-		</div>
-
-	</div>
-	<div class="feature-section angled-right">
-
-		<div class="left-feature">
 			<h4><?php _e( 'Responsive Design' ); ?></h4>
 			<p><?php _e( 'Certain dashboard screens have been updated to look better at various sizes, including improved iPad/tablet support.' ); ?></p>
 		</div>
-		<span class="image-mask"><img src="images/screenshots/help-screen.png" width="250" height="170" /></span>
+	</div>
+	
+	<div class="feature-section angled-right">
+		<div class="left-feature">
+			<h4><?php _e( 'Header + Admin Bar = Toolbar' ); ?></h4>
+			<p><?php _e( 'To save space and increase efficiency, we&#039;ve combined the admin bar and the old Dashboard header into one persistent toolbar. Hovering over the toolbar items will reveal submenus when available for quick access. ' ); ?></p>
+		</div>
+		<img src="images/screenshots/help-screen.png" />
 		<div class="right-feature">
 			<h4><?php _e( 'Help Tabs' ); ?></h4>
-			<p><?php _e( 'The Help tabs located in the upper corner of the dashboard screens below your name have gotten a facelift. Help content is broken in smaller sections for easier access, with links to relevant documentation and the support forums always visible.' ); ?></p>
+			<p><?php _e( 'The Help tabs located in the upper corner of the dashboard screens below your name have gotten a facelift. Help content is broken into smaller sections for easier access, with links to relevant documentation and the support forums always visible.' ); ?></p>
 		</div>
 	</div>
-
 </div>
 
 <div class="changelog">
@@ -89,8 +87,8 @@ include( './admin-header.php' );
 
 	<div class="feature-section images-stagger-right">
 		<div class="feature-images">
-			<img src="images/screenshots/welcome-screen.png" class="angled-right" />
-			<img src="images/screenshots/new-feature-pointer.png" class="angled-left" />
+			<img src="images/screenshots/new-feature-pointer.png" class="angled-right" />
+			<img src="images/screenshots/welcome-screen.png" class="angled-left" />
 		</div>
 		<div class="left-feature">
 			<h4><?php _e( 'New Feature Pointers' ); ?></h4>
@@ -111,16 +109,17 @@ include( './admin-header.php' );
 
 	<div class="feature-section three-col">
 		<div>
-			<h4><?php _e( 'Widget Improvements' ); ?></h4>
-			<p><?php _e( 'Changing themes often requires widget re-configuration based on the number and position of sidebars. Now if you change back to a previous theme within a week, the widgets will automatically go back to how you had them arranged in that theme. <em>Note: if you&#039;ve added new widgets since the switch, you&#039;ll need to rescue them from the Inactive Widgets area.</em>' ); ?></p>
+			<h4><?php _e( 'Better Co-Editing' ); ?></h4>
+			<img src="images/screenshots/coediting.png" />
+			<p><?php _e( 'Have you ever gone to edit a post after someone else has finished with it, only to get an alert that tells you the other person is still editing the post? From now on, you&#039;ll only get that alert if another person is still on the editing screen &mdash; no more time lag.' ); ?></p>
 		</div>
 		<div>
 			<h4><?php _e( 'Tumblr Importer' ); ?></h4>
-			<p><?php _e( 'Want to import content from Tumblr to WordPress? No problem! Go to Tools &rarr; Import to get the new Tumblr Importer.' ); ?></p>
+			<p><?php _e( 'Want to import content from Tumblr to WordPress? No problem! Go to <span class="no-break">Tools &rarr; Import</span> to get the new Tumblr Importer, which maps your Tumblog posts to the matching WordPress post formats. Tip: Choose a theme designed to display post formats to get the greatest benefit from the importer.' ); ?></p>
 		</div>
 		<div class="last-feature">
-			<h4><?php _e( 'Better Co-Editing' ); ?></h4>
-			<p><?php _e( 'Have you ever gone to edit a post after someone else has finished with it, only to get an alert that tells you the other person is still editing the post? From now on, you&#039;ll only get that alert if another person is still on the editing screen &mdash; no more time lag.' ); ?></p>
+			<h4><?php _e( 'Widget Improvements' ); ?></h4>
+			<p><?php _e( 'Changing themes often requires widget re-configuration based on the number and position of sidebars. Now if you change back to a previous theme within a week, the widgets will automatically go back to how you had them arranged in that theme. <em>Note: if you&#039;ve added new widgets since the switch, you&#039;ll need to rescue them from the Inactive Widgets area.</em>' ); ?></p>
 		</div>
 	</div>
 	
@@ -135,8 +134,8 @@ include( './admin-header.php' );
 			<p><?php _e( 'You have more freedom when choosing a post permalink structure. Skip the date information or add a category slug without a performance penalty.' ); ?></p>
 		</div>
 		<div>
-			<h4><?php _e( 'WP_Screen API' ); ?></h4>
-			<p><?php _e( 'WordPress admin screens have a nice new API. Create rich screens with help documentation, settings, and more.' ); ?></p>
+			<h4><?php _e( 'Post Slugs: Less Funky' ); ?></h4>
+			<p><?php _e( 'Funky characters in post titles (e.g. curly quotes from a word processor) will no longer result in garbled post slugs.' ); ?></p>
 		</div>
 		<div class="last-feature">
 			<h4><?php _e( 'jQuery and jQuery UI' ); ?></h4>
@@ -150,8 +149,8 @@ include( './admin-header.php' );
 			<p><?php _e( 'This handy method will tell you if a <code>WP_Query</code> object is the main WordPress query or a secondary query.' ); ?></p>
 		</div>
 		<div>
-			<h4><?php _e( 'Post Slugs: Less Funky' ); ?></h4>
-			<p><?php _e( 'Funky characters in post titles (e.g. curly quotes from a word processor) will no longer result in garbled post slugs.' ); ?></p>
+			<h4><?php _e( 'WP_Screen API' ); ?></h4>
+			<p><?php _e( 'WordPress admin screens have a nice new API. Create rich screens with help documentation, settings, and more.' ); ?></p>
 		</div>
 		<div class="last-feature">
 			<h4><?php _e( 'Editor API Overhaul' ); ?></h4>
@@ -162,7 +161,8 @@ include( './admin-header.php' );
 </div>
 
 <div class="return-to-dashboard">
-	<a href="<?php echo admin_url(); ?>"><?php _e( 'Go to the Dashboard' ); ?></a>
+	<a href="<?php echo admin_url( 'update-core.php' ); ?>"><?php _e( 'Return to Dashboard &rarr; Updates' ); ?></a> |
+	<a href="<?php echo admin_url(); ?>"><?php _e( 'Go to Dashboard &rarr; Home' ); ?></a>
 </div>
 
 </div>
