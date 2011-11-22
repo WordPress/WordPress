b4debb96486e3bbc82e5cd2f18b7a3bf21fd7dc7
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-11-22 21:27:27 +0000

    Because we can. see #18034.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19406 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -157,7 +157,7 @@ function get_search_form($echo = true) {
 		return;
 	}
 
-	$form = '<form role="search" method="get" id="searchform" action="' . home_url( '/' ) . '" >
+	$form = '<form role="search" method="get" id="searchform" action="' . esc_url( home_url( '/' ) ) . '" >
 	<div><label class="screen-reader-text" for="s">' . __('Search for:') . '</label>
 	<input type="text" value="' . get_search_query() . '" name="s" id="s" />
 	<input type="submit" id="searchsubmit" value="'. esc_attr__('Search') .'" />
