32267a39ddf74b21f2e5a2eff277353fb7cf8940
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-11-24 00:40:31 +0000

    Few more RTL and IE7 fixes, see #19042
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19446 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -433,9 +433,9 @@ function wp_default_styles( &$styles ) {
 	// Any rtl stylesheets that don't have a .dev version for ltr
 	$no_suffix = array( 'farbtastic' );
 
-	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20111123e' );
+	$styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css", array(), '20111123f' );
 
-	$styles->add( 'ie', "/wp-admin/css/ie$suffix.css", array(), '20111119' );
+	$styles->add( 'ie', "/wp-admin/css/ie$suffix.css", array(), '20111123' );
 	$styles->add_data( 'ie', 'conditional', 'lte IE 7' );
 
 	// all colors stylesheets need to have the same query strings (cache manifest compat)
