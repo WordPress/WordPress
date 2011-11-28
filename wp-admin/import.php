626751a2f6104e4882c07668fbd4c3b3e86aefd7
koopersmith <koopersmith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-11-28 16:38:13 +0000

    Update help content for import page. props Ipstenu, see #19020.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19461 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/import.php
+++ wp-admin/import.php
@@ -16,10 +16,13 @@ if ( !current_user_can('import') )
 
 $title = __('Import');
 
-add_contextual_help($current_screen,
-	'<p>' . __('This screen lists links to plugins to import data from blogging/content management platforms. Choose the platform you want to import from, and click Install Now when you are prompted in the popup window. If your platform is not listed, click the link to search the plugin directory for other importer plugins to see if there is one for your platform.') . '</p>' .
-	'<p>' . __('In previous versions of WordPress, all the importers were built-in, but they have been turned into plugins as of version 3.0 since most people only use them once or infrequently.') . '</p>'
-);
+get_current_screen()->add_help_tab( array(
+	'id'      => 'import',
+	'title'   => __('Import'),
+	'content' => '<p>' . __('This screen lists links to plugins to import data from blogging/content management platforms. Choose the platform you want to import from, and click Install Now when you are prompted in the popup window. If your platform is not listed, click the link to search the plugin directory for other importer plugins to see if there is one for your platform.') . '</p>' .
+		'<p>' . __('In previous versions of WordPress, all importers were built-in.  They have been turned into plugins since most people only use them once or infrequently.') . '</p>',
+) );
+
 
 get_current_screen()->set_help_sidebar(
 	'<p><strong>' . __('For more information:') . '</strong></p>' .
