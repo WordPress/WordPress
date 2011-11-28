580048525d6a13cb94e668abf45c8e5a4143aa2d
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-11-28 00:31:11 +0000

    Admin bar: change the tabbing to behave as if the admin bar menus were click-to-open (Tab to select, Enter to open/close), fixes #19088
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19458 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/admin-bar.dev.js
+++ wp-includes/js/admin-bar.dev.js
@@ -4,6 +4,12 @@ if ( typeof(jQuery) != 'undefined' ) {
 		(function(a){a.fn.hoverIntent=function(l,j){var m={sensitivity:7,interval:100,timeout:0};m=a.extend(m,j?{over:l,out:j}:l);var o,n,h,d;var e=function(f){o=f.pageX;n=f.pageY};var c=function(g,f){f.hoverIntent_t=clearTimeout(f.hoverIntent_t);if((Math.abs(h-o)+Math.abs(d-n))<m.sensitivity){a(f).unbind("mousemove",e);f.hoverIntent_s=1;return m.over.apply(f,[g])}else{h=o;d=n;f.hoverIntent_t=setTimeout(function(){c(g,f)},m.interval)}};var i=function(g,f){f.hoverIntent_t=clearTimeout(f.hoverIntent_t);f.hoverIntent_s=0;return m.out.apply(f,[g])};var b=function(q){var f=this;var g=(q.type=="mouseover"?q.fromElement:q.toElement)||q.relatedTarget;while(g&&g!=this){try{g=g.parentNode}catch(q){g=this}}if(g==this){if(a.browser.mozilla){if(q.type=="mouseout"){f.mtout=setTimeout(function(){k(q,f)},30)}else{if(f.mtout){f.mtout=clearTimeout(f.mtout)}}}return}else{if(f.mtout){f.mtout=clearTimeout(f.mtout)}k(q,f)}};var k=function(p,f){var g=jQuery.extend({},p);if(f.hoverIntent_t){f.hoverIntent_t=clearTimeout(f.hoverIntent_t)}if(p.type=="mouseover"){h=g.pageX;d=g.pageY;a(f).bind("mousemove",e);if(f.hoverIntent_s!=1){f.hoverIntent_t=setTimeout(function(){c(g,f)},m.interval)}}else{a(f).unbind("mousemove",e);if(f.hoverIntent_s==1){f.hoverIntent_t=setTimeout(function(){i(g,f)},m.timeout)}}};return this.mouseover(b).mouseout(b)}})(jQuery);
 
 	jQuery(document).ready(function($){
+		var refresh = function(i, el){ // force the browser to refresh the tabbing index
+			var node = $(el), tab = node.attr('tabindex');
+			if ( tab )
+				node.attr('tabindex', '0').attr('tabindex', tab);
+		};
+
 		$('#wpadminbar').removeClass('nojq').removeClass('nojs').find('li.menupop').hoverIntent({
 			over: function(e){
 				$(this).addClass('hover');
@@ -23,33 +29,18 @@ if ( typeof(jQuery) != 'undefined' ) {
 			}).focus().select();
 		});
 
-		$('#wpadminbar ul > li > .ab-item').bind('focus.adminbar', function(){
-			$(this).parent().addClass('hover').find('.ab-item').each(function(i, el){
-				var node = $(el), tab = node.attr('tabindex');
-				if ( tab )
-					node.attr('tabindex', '0').attr('tabindex', tab);
-			});
-		}).bind('blur.adminbar', function(){
-			var t = $(this);
+		$('#wpadminbar li.menupop > .ab-item').bind('keydown.adminbar', function(e){
+			if ( e.which != 13 )
+				return;
 
-			setTimeout(function(){
-				if ( !t.siblings('div').find('.ab-item:focus').length )
-					t.parent().removeClass('hover');
-			}, 200);
-		});
+			var target = $(e.target);
 
-		$('.ab-top-menu li ul li .ab-item').bind('blur.adminbar', function(){
-			var top = $(this).parents('li.menupop');
+			e.stopPropagation();
+			e.preventDefault();
+			target.parent().toggleClass('hover');
 
-			setTimeout(function(){
-				top.each( function() {
-					var t = $(this);
-
-					if ( ! t.find('.ab-item:focus').length )
-						t.removeClass('hover');
-				});
-			}, 100);
-		});
+			target.siblings('.ab-sub-wrapper').find('.ab-item').each(refresh);
+		}).each(refresh);
 
 	});
 } else {
