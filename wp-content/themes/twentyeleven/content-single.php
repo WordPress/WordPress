6e8e8298d06a503ecf7a2389cfa95d925a6cc52f
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-12-10 19:43:49 +0000

    Back compat for twentyeleven. Props duck_. fixes #19504
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19584 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyeleven/content-single.php
+++ wp-content/themes/twentyeleven/content-single.php
@@ -51,7 +51,7 @@
 		?>
 		<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
 
-		<?php if ( get_the_author_meta( 'description' ) && is_multi_author() ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries ?>
+		<?php if ( get_the_author_meta( 'description' ) && ( ! function_exists( 'is_multi_author' ) || is_multi_author() ) ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries ?>
 		<div id="author-info">
 			<div id="author-avatar">
 				<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyeleven_author_bio_avatar_size', 68 ) ); ?>
