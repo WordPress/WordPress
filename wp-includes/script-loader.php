8499eb25a7f93f16810318323f962921d86578b7
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-12-20 20:57:46 +0000

    Bump.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19606 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -279,7 +279,7 @@ function wp_default_scripts( &$scripts ) {
 
 	$scripts->add( 'media-upload', "/wp-admin/js/media-upload$suffix.js", array( 'thickbox' ), '20110930', 1 );
 
-	$scripts->add( 'hoverIntent', "/wp-includes/js/hoverIntent$suffix.js", array('jquery'), '20090102', 1 );
+	$scripts->add( 'hoverIntent', "/wp-includes/js/hoverIntent$suffix.js", array('jquery'), 'r6', 1 );
 
 	if ( is_admin() ) {
 		$scripts->add( 'ajaxcat', "/wp-admin/js/cat$suffix.js", array( 'wp-lists' ), '20090102' );
