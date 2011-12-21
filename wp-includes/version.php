9a62c5781cb6c7779d55045777067d80835139be
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2011-12-21 04:27:29 +0000

    Compress scripts/styles: 3.4-alpha-19620
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19620 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.4-alpha-19618';
+$wp_version = '3.4-alpha-19620';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
