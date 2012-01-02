85dff92772a70979839dddd4a63f647976b6e9e6
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-01-02 21:49:58 +0000

    Immediately close a flyout when opening another one. Prevents brief occurrence of two flyouts at once. Props SergeyBiryukov. fixes #19575 for trunk
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19647 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.4-alpha-19620';
+$wp_version = '3.4-alpha-19643';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
