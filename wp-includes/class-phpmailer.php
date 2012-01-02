98a96139df6bd5c476f41ffea26e976b15d99a6f
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-01-02 20:44:04 +0000

    Include class-smtp.php not class.smtp.php. fixes #19677
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19642 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-phpmailer.php
+++ wp-includes/class-phpmailer.php
@@ -757,7 +757,7 @@ class PHPMailer {
    * @return bool
    */
   protected function SmtpSend($header, $body) {
-    require_once $this->PluginDir . 'class.smtp.php';
+    require_once $this->PluginDir . 'class-smtp.php';
     $bad_rcpt = array();
 
     if(!$this->SmtpConnect()) {
