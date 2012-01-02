61aef00dc4deb72dcd8f5ec14b58dcc458286cf2
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-01-02 21:31:27 +0000

    An empty database prefix is not supported for multisite. <small>Add a sanity check anyway.</small> fixes #19566 for 3.3
    git-svn-id: http://svn.automattic.com/wordpress/branches/3.3@19643 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -696,7 +696,7 @@ function get_blogs_of_user( $user_id, $all = false ) {
 	foreach ( $keys as $key ) {
 		if ( 'capabilities' !== substr( $key, -12 ) )
 			continue;
-		if ( 0 !== strpos( $key, $wpdb->base_prefix ) )
+		if ( $wpdb->base_prefix && 0 !== strpos( $key, $wpdb->base_prefix ) )
 			continue;
 		$blog_id = str_replace( array( $wpdb->base_prefix, '_capabilities' ), '', $key );
 		if ( ! is_numeric( $blog_id ) )
