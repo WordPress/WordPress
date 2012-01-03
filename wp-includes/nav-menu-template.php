cf981b4f257d0476b08c5f9cf7e4b1c48b105f96
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-01-03 18:15:16 +0000

    Properly handle a blank nav menu custom URL when determining what page we're on. see #19699 for trunk.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19657 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/nav-menu-template.php
+++ wp-includes/nav-menu-template.php
@@ -362,10 +362,11 @@ function _wp_menu_item_classes_by_context( &$menu_items ) {
 		} elseif ( 'custom' == $menu_item->object ) {
 			$_root_relative_current = untrailingslashit( $_SERVER['REQUEST_URI'] );
 			$current_url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_root_relative_current;
-			$item_url = untrailingslashit( strpos( $menu_item->url, '#' ) ? substr( $menu_item->url, 0, strpos( $menu_item->url, '#' ) ) : $menu_item->url );
+			$raw_item_url = strpos( $menu_item->url, '#' ) ? substr( $menu_item->url, 0, strpos( $menu_item->url, '#' ) ) : $menu_item->url;
+			$item_url = untrailingslashit( $raw_item_url );
 			$_indexless_current = untrailingslashit( preg_replace( '/index.php$/', '', $current_url ) );
 
-			if ( in_array( $item_url, array( $current_url, $_indexless_current, $_root_relative_current ) ) ) {
+			if ( $raw_item_url && in_array( $item_url, array( $current_url, $_indexless_current, $_root_relative_current ) ) ) {
 				$classes[] = 'current-menu-item';
 				$menu_items[$key]->current = true;
 				$_anc_id = (int) $menu_item->db_id;
