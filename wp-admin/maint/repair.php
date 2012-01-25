0c141a4721cc2fbfe940d47eedea01a9470652e0
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-01-25 23:27:13 +0000

    Formatting and i18n cleanup of repair.php. props zeo for initial patch. fixes #18770.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19757 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/maint/repair.php
+++ wp-admin/maint/repair.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * Database Repair and Optimization Script.
+ *
+ * @package WordPress
+ * @subpackage Database
+ */
 define('WP_REPAIRING', true);
 
 require_once('../../wp-load.php');
@@ -10,7 +15,7 @@ header( 'Content-Type: text/html; charset=utf-8' );
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title><?php _e('WordPress &rsaquo; Database Repair'); ?></title>
+	<title><?php _e( 'WordPress &rsaquo; Database Repair' ); ?></title>
 	<?php wp_admin_css( 'install', true ); ?>
 </head>
 <body>
@@ -18,16 +23,12 @@ header( 'Content-Type: text/html; charset=utf-8' );
 
 <?php
 
-if ( !defined('WP_ALLOW_REPAIR') ) {
-	echo '<p>'.__('To allow use of this page to automatically repair database problems, please add the following line to your wp-config.php file. Once this line is added to your config, reload this page.')."</p><code>define('WP_ALLOW_REPAIR', true);</code>";
-} elseif ( isset($_GET['repair']) ) {
-	check_admin_referer('repair_db');
+if ( ! defined( 'WP_ALLOW_REPAIR' ) ) {
+	echo '<p>' . __( 'To allow use of this page to automatically repair database problems, please add the following line to your <code>wp-config.php</code> file. Once this line is added to your config, reload this page.' ) . "</p><code>define('WP_ALLOW_REPAIR', true);</code>";
+} elseif ( isset( $_GET['repair'] ) ) {
+	check_admin_referer( 'repair_db' );
 
-	if ( 2 == $_GET['repair'] )
-		$optimize = true;
-	else
-		$optimize = false;
-
+	$optimize = 2 == $_GET['repair'];
 	$okay = true;
 	$problems = array();
 
@@ -41,71 +42,71 @@ if ( !defined('WP_ALLOW_REPAIR') ) {
 
 	// Loop over the tables, checking and repairing as needed.
 	foreach ( $tables as $table ) {
-		$check = $wpdb->get_row("CHECK TABLE $table");
+		$check = $wpdb->get_row( "CHECK TABLE $table" );
 
 		echo '<p>';
 		if ( 'OK' == $check->Msg_text ) {
 			/* translators: %s: table name */
-			printf( __( 'The %s table is okay.' ), $table );
+			printf( __( 'The %s table is okay.' ), "<code>$table</code>" );
 		} else {
 			/* translators: 1: table name, 2: error message, */
-			printf( __( 'The %1$s table is not okay. It is reporting the following error: %2$s. WordPress will attempt to repair this table&hellip;' ) , $table, "<code>$check->Msg_text</code>" );
+			printf( __( 'The %1$s table is not okay. It is reporting the following error: %2$s. WordPress will attempt to repair this table&hellip;' ) , "<code>$table</code>", "<code>$check->Msg_text</code>" );
 
-			$repair = $wpdb->get_row("REPAIR TABLE $table");
+			$repair = $wpdb->get_row( "REPAIR TABLE $table" );
 
 			echo '<br />&nbsp;&nbsp;&nbsp;&nbsp;';
 			if ( 'OK' == $check->Msg_text ) {
 				/* translators: %s: table name */
-				printf( __( 'Successfully repaired the %s table.' ), $table );
+				printf( __( 'Successfully repaired the %s table.' ), "<code>$table</code>" );
 			} else {
 				/* translators: 1: table name, 2: error message, */
-				echo sprintf( __( 'Failed to repair the %1$s table. Error: %2$s' ), $table, "<code>$check->Msg_text</code>" ) . '<br />';
+				echo sprintf( __( 'Failed to repair the %1$s table. Error: %2$s' ), "<code>$table</code>", "<code>$check->Msg_text</code>" ) . '<br />';
 				$problems[$table] = $check->Msg_text;
 				$okay = false;
 			}
 		}
 
 		if ( $okay && $optimize ) {
-			$check = $wpdb->get_row("ANALYZE TABLE $table");
+			$check = $wpdb->get_row( "ANALYZE TABLE $table" );
 
-			echo '<br />&nbsp;&nbsp;&nbsp;&nbsp';
+			echo '<br />&nbsp;&nbsp;&nbsp;&nbsp;';
 			if ( 'Table is already up to date' == $check->Msg_text )  {
 				/* translators: %s: table name */
-				printf( __( 'The %s table is already optimized.' ), $table );
+				printf( __( 'The %s table is already optimized.' ), "<code>$table</code>" );
 			} else {
-				$check = $wpdb->get_row("OPTIMIZE TABLE $table");
+				$check = $wpdb->get_row( "OPTIMIZE TABLE $table" );
 
-				echo '<br />&nbsp;&nbsp;&nbsp;&nbsp';
+				echo '<br />&nbsp;&nbsp;&nbsp;&nbsp;';
 				if ( 'OK' == $check->Msg_text || 'Table is already up to date' == $check->Msg_text ) {
 					/* translators: %s: table name */
-					printf( __( 'Successfully optimized the %s table.' ), $table );
+					printf( __( 'Successfully optimized the %s table.' ), "<code>$table</code>" );
 				} else {
 					/* translators: 1: table name, 2: error message, */
-					printf( __( 'Failed to optimize the %1$s table. Error: %2$s' ), $table, "<code>$check->Msg_text</code>" );
+					printf( __( 'Failed to optimize the %1$s table. Error: %2$s' ), "<code>$table</code>", "<code>$check->Msg_text</code>" );
 				}
 			}
 		}
 		echo '</p>';
 	}
 
-	if ( !empty($problems) ) {
-		printf('<p>'.__('Some database problems could not be repaired. Please copy-and-paste the following list of errors to the <a href="%s">WordPress support forums</a> to get additional assistance.').'</p>', 'http://wordpress.org/support/forum/3');
-		$problem_output = array();
+	if ( $problems ) {
+		printf( '<p>' . __('Some database problems could not be repaired. Please copy-and-paste the following list of errors to the <a href="%s">WordPress support forums</a> to get additional assistance.') . '</p>', __( 'http://wordpress.org/support/forum/how-to-and-troubleshooting' ) );
+		$problem_output = '';
 		foreach ( $problems as $table => $problem )
-			$problem_output[] = "$table: $problem";
-		echo '<textarea name="errors" id="errors" rows="20" cols="60">' . esc_textarea( implode("\n", $problem_output) ) . '</textarea>';
+			$problem_output .= "$table: $problem\n";
+		echo '<p><textarea name="errors" id="errors" rows="20" cols="60">' . esc_textarea( $problem_output ) . '</textarea></p>';
 	} else {
-		echo '<p>'.__('Repairs complete. Please remove the following line from wp-config.php to prevent this page from being used by unauthorized users.')."</p><code>define('WP_ALLOW_REPAIR', true);</code>";
+		echo '<p>' . __( 'Repairs complete. Please remove the following line from wp-config.php to prevent this page from being used by unauthorized users.' ) . "</p><code>define('WP_ALLOW_REPAIR', true);</code>";
 	}
 } else {
-	if ( isset($_GET['referrer']) && 'is_blog_installed' == $_GET['referrer'] )
-		_e('One or more database tables are unavailable. To allow WordPress to attempt to repair these tables, press the &#8220;Repair Database&#8221; button. Repairing can take a while, so please be patient.');
+	if ( isset( $_GET['referrer'] ) && 'is_blog_installed' == $_GET['referrer'] )
+		echo '<p>' . __( 'One or more database tables are unavailable. To allow WordPress to attempt to repair these tables, press the &#8220;Repair Database&#8221; button. Repairing can take a while, so please be patient.' ) . '</p>';
 	else
-		_e('WordPress can automatically look for some common database problems and repair them. Repairing can take a while, so please be patient.')
+		echo '<p>' . __( 'WordPress can automatically look for some common database problems and repair them. Repairing can take a while, so please be patient.' ) . '</p>';
 ?>
-	<p class="step"><a class="button" href="<?php echo wp_nonce_url('repair.php?repair=1', 'repair_db') ?>"><?php _e( 'Repair Database' ); ?></a></p>
-	<?php _e('WordPress can also attempt to optimize the database. This improves performance in some situations. Repairing and optimizing the database can take a long time and the database will be locked while optimizing.'); ?>
-	<p class="step"><a class="button" href="<?php echo wp_nonce_url('repair.php?repair=2', 'repair_db') ?>"><?php _e( 'Repair and Optimize Database' ); ?></a></p>
+	<p class="step"><a class="button" href="<?php echo wp_nonce_url( 'repair.php?repair=1', 'repair_db' ); ?>"><?php _e( 'Repair Database' ); ?></a></p>
+	<p><?php _e( 'WordPress can also attempt to optimize the database. This improves performance in some situations. Repairing and optimizing the database can take a long time and the database will be locked while optimizing.' ); ?></p>
+	<p class="step"><a class="button" href="<?php echo wp_nonce_url( 'repair.php?repair=2', 'repair_db' ); ?>"><?php _e( 'Repair and Optimize Database' ); ?></a></p>
 <?php
 }
 ?>
