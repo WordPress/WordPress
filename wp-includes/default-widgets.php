56633093fd58e3fdc8e6e40f4ea6127fe3c67349
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-02-04 18:37:53 +0000

    Use translate, not translate with context. props azaozz. see #19603.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19829 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-widgets.php
+++ wp-includes/default-widgets.php
@@ -298,7 +298,7 @@ class WP_Widget_Meta extends WP_Widget {
 			<li><a href="<?php bloginfo('comments_rss2_url'); ?>" title="<?php echo esc_attr(__('The latest comments to all posts in RSS')); ?>"><?php _e('Comments <abbr title="Really Simple Syndication">RSS</abbr>'); ?></a></li>
 			<li><a href="<?php esc_attr_e( 'http://wordpress.org/' ); ?>" title="<?php echo esc_attr(__('Powered by WordPress, state-of-the-art semantic personal publishing platform.')); ?>"><?php
 			/* translators: meta widget link text */
-			_x( 'WordPress.org' );
+			__( 'WordPress.org' );
 			?></a></li>
 			<?php wp_meta(); ?>
 			</ul>
