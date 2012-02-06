006c6b6b9deda3f7d6622ff9fe11b88142d29126
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-02-06 19:43:26 +0000

    Add a translator's comment. see #19924.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19837 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/locale.php
+++ wp-includes/locale.php
@@ -180,6 +180,7 @@ class WP_Locale {
 		// Set text direction.
 		if ( isset( $GLOBALS['text_direction'] ) )
 			$this->text_direction = $GLOBALS['text_direction'];
+		/* translators: 'rtl' or 'ltr'. This sets the text direction for WordPress. */
 		elseif ( 'rtl' == _x( 'ltr', 'text direction' ) )
 			$this->text_direction = 'rtl';
 	}
