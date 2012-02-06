5267fef339bf5426490e0747376a1dff1a68ac0a
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-02-06 22:37:45 +0000

    Synchronize archive.php docs with Twenty Eleven. Go easy on the PHP/HTML context switching. see #19978.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19843 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/archive.php
+++ wp-content/themes/twentytwelve/archive.php
@@ -2,6 +2,11 @@
 /**
  * The template for displaying Archive pages.
  *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
  * @package WordPress
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
@@ -15,43 +20,47 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title">
-					<?php if ( is_day() ) : ?>
-						<?php printf( __( 'Daily Archives: %s', 'twentytwelve' ), '<span>' . get_the_date() . '</span>' ); ?>
-					<?php elseif ( is_month() ) : ?>
-						<?php printf( __( 'Monthly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentytwelve' ) ) . '</span>' ); ?>
-					<?php elseif ( is_year() ) : ?>
-						<?php printf( __( 'Yearly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentytwelve' ) ) . '</span>' ); ?>
-					<?php elseif ( is_tag() ) : ?>
-					<?php
+				<h1 class="page-title"><?php
+					if ( is_day() ) {
+						printf( __( 'Daily Archives: %s', 'twentytwelve' ), '<span>' . get_the_date() . '</span>' );
+					} elseif ( is_month() ) {
+						printf( __( 'Monthly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentytwelve' ) ) . '</span>' );
+					} elseif ( is_year() ) {
+						printf( __( 'Yearly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentytwelve' ) ) . '</span>' );
+					} elseif ( is_tag() ) {
 						printf( __( 'Tag Archives: %s', 'twentytwelve' ), '<span>' . single_tag_title( '', false ) . '</span>' );
 						// Show an optional tag description
 						$tag_description = tag_description();
-						if ( ! empty( $tag_description ) )
+						if ( $tag_description )
 							echo apply_filters( 'tag_archive_meta', '<div class="tag-archive-meta">' . $tag_description . '</div>' );
-					?>
-					<?php elseif ( is_category() ) : ?>
-					<?php
+					} elseif ( is_category() ) {
 						printf( __( 'Category Archives: %s', 'twentytwelve' ), '<span>' . single_cat_title( '', false ) . '</span>' );
 						// Show an optional category description
 						$category_description = category_description();
-						if ( ! empty( $category_description ) )
+						if ( $category_description )
 							echo apply_filters( 'category_archive_meta', '<div class="category-archive-meta">' . $category_description . '</div>' );
-					?>
-					<?php else : ?>
-						<?php _e( 'Blog Archives', 'twentytwelve' ); ?>
-					<?php endif; ?>
-				</h1>
+					} else {
+						_e( 'Blog Archives', 'twentytwelve' );
+					}
+				?></h1>
 			</header>
 
-			<?php twentytwelve_content_nav( 'nav-above' ); ?>
+			<?php
+			twentytwelve_content_nav( 'nav-above' );
+			
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
+				/* Include the post format-specific template for the content. If you want to
+				 * this in a child theme then include a file called called content-___.php
+				 * (where ___ is the post format) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
 
-			<?php twentytwelve_content_nav( 'nav-below' ); ?>
+			endwhile;
+
+			twentytwelve_content_nav( 'nav-below' );
+			?>
 
 		<?php else : ?>
 
