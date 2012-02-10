70a6d8fb9c6c356f4026a292e11a31d363a3605d
bumpbot <bumpbot@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-02-10 09:00:50 +0000

    Compress scripts/styles: 3.4-alpha-19898.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19898 1a063a9b-81f0-0310-95a4-ce76da25c4cd

---
+++ wp-admin/js/user-search.js
@@ -0,0 +1,1 @@
+jQuery(function(a){var b=typeof(current_site_id)!="undefined"?"&site_id="+current_site_id:"";a("#adduser-email, #newuser").autocomplete({source:ajaxurl+"?action=autocomplete-user"+b,delay:500,minLength:2,})});
