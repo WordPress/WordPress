2dff362a1f4b11f65f2c518ca1141b5fd98091e5
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-02-19 23:08:10 +0000

    Refresh wpCookie, fixes #20078
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19954 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/utils.dev.js
+++ wp-admin/js/utils.dev.js
@@ -3,23 +3,23 @@
 var wpCookies = {
 // The following functions are from Cookie.js class in TinyMCE, Moxiecode, used under LGPL.
 
-	each : function(o, cb, s) {
+	each : function(obj, cb, scope) {
 		var n, l;
 
-		if (!o)
+		if ( !obj )
 			return 0;
 
-		s = s || o;
+		scope = scope || obj;
 
-		if (typeof(o.length) != 'undefined') {
-			for (n=0, l = o.length; n<l; n++) {
-				if (cb.call(s, o[n], n, o) === false)
+		if ( typeof(obj.length) != 'undefined' ) {
+			for ( n = 0, l = obj.length; n < l; n++ ) {
+				if ( cb.call(scope, obj[n], n, obj) === false )
 					return 0;
 			}
 		} else {
-			for (n in o) {
-				if (o.hasOwnProperty(n)) {
-					if (cb.call(s, o[n], n, o) === false) {
+			for ( n in obj ) {
+				if ( obj.hasOwnProperty(n) ) {
+					if ( cb.call(scope, obj[n], n, obj) === false ) {
 						return 0;
 					}
 				}
@@ -28,78 +28,108 @@ var wpCookies = {
 		return 1;
 	},
 
-	getHash : function(n) {
-		var v = this.get(n), h;
+	/**
+	 * Get a multi-values cookie.
+	 * Returns a JS object with the name: 'value' pairs.
+	 */	 	 	
+	getHash : function(name) {
+		var all = this.get(name), ret;
 
-		if (v) {
-			this.each(v.split('&'), function(v) {
-				v = v.split('=');
-				h = h || {};
-				h[v[0]] = v[1];
+		if ( all ) {
+			this.each( all.split('&'), function(pair) {
+				pair = pair.split('=');
+				ret = ret || {};
+				ret[pair[0]] = pair[1];
 			});
 		}
-		return h;
+		return ret;
 	},
 
-	setHash : function(n, v, e, p, d, s) {
-		var o = '';
+	/**
+	 * Set a multi-values cookie.
+	 *
+	 * 'values_obj' is the JS object that is stored. It is encoded as URI in wpCookies.set().
+	 */	 	 	
+	setHash : function(name, values_obj, expires, path, domain, secure) {
+		var str = '';
 
-		this.each(v, function(v, k) {
-			o += (!o ? '' : '&') + k + '=' + v;
+		this.each(values_obj, function(val, key) {
+			str += (!str ? '' : '&') + key + '=' + val;
 		});
 
-		this.set(n, o, e, p, d, s);
+		this.set(name, str, expires, path, domain, secure);
 	},
 
-	get : function(n) {
-		var c = document.cookie, e, p = n + "=", b;
+	/**
+	 * Get a cookie.
+	 */	 	
+	get : function(name) {
+		var cookie = document.cookie, e, p = name + "=", b;
 
-		if (!c)
+		if ( !cookie )
 			return;
 
-		b = c.indexOf("; " + p);
+		b = cookie.indexOf("; " + p);
 
-		if (b == -1) {
-			b = c.indexOf(p);
+		if ( b == -1 ) {
+			b = cookie.indexOf(p);
 
-			if (b != 0)
+			if ( b != 0 )
 				return null;
 
 		} else {
 			b += 2;
 		}
 
-		e = c.indexOf(";", b);
+		e = cookie.indexOf(";", b);
 
-		if (e == -1)
-			e = c.length;
+		if ( e == -1 )
+			e = cookie.length;
 
-		return decodeURIComponent(c.substring(b + p.length, e));
+		return decodeURIComponent( cookie.substring(b + p.length, e) );
 	},
 
-	set : function(n, v, e, p, d, s) {
-		document.cookie = n + "=" + encodeURIComponent(v) +
-			((e) ? "; expires=" + e.toGMTString() : "") +
-			((p) ? "; path=" + p : "") +
-			((d) ? "; domain=" + d : "") +
-			((s) ? "; secure" : "");
-	},
-
-	remove : function(n, p) {
+	/**
+	 * Set a cookie.
+	 *
+	 * The 'expires' arg can be either a JS Date() object set to the expiration date (back-compat)
+	 * or the number of seconds until expiration
+	 */	 	
+	set : function(name, value, expires, path, domain, secure) {
 		var d = new Date();
 
-		d.setTime(d.getTime() - 1000);
+		if ( typeof(expires) == 'object' && expires.toGMTString ) {
+			expires = expires.toGMTString();
+		} else if ( parseInt(expires, 10) ) {
+			d.setTime( d.getTime() + ( parseInt(expires, 10) * 1000 ) ); // time must be in miliseconds
+			expires = d.toGMTString();
+		} else {
+			expires = '';
+		}
 
-		this.set(n, '', d, p, d);
+		document.cookie = name + "=" + encodeURIComponent(value) +
+			((expires) ? "; expires=" + expires : "") +
+			((path) ? "; path=" + path : "") +
+			((domain) ? "; domain=" + domain : "") +
+			((secure) ? "; secure" : "");
+	},
+
+	/**
+	 * Remove a cookie.
+	 *
+	 * This is done by setting it to an empty value and setting the expiration time in the past.
+	 */	 	
+	remove : function(name, path) {
+		this.set(name, '', -1000, path);
 	}
 };
 
 // Returns the value as string. Second arg or empty string is returned when value is not set.
 function getUserSetting( name, def ) {
-	var o = getAllUserSettings();
+	var obj = getAllUserSettings();
 
-	if ( o.hasOwnProperty(name) )
-		return o[name];
+	if ( obj.hasOwnProperty(name) )
+		return obj[name];
 
 	if ( typeof def != 'undefined' )
 		return def;
@@ -109,24 +139,21 @@ function getUserSetting( name, def ) {
 
 // Both name and value must be only ASCII letters, numbers or underscore
 // and the shorter, the better (cookies can store maximum 4KB). Not suitable to store text.
-function setUserSetting( name, value, del ) {
+function setUserSetting( name, value, _del ) {
 	if ( 'object' !== typeof userSettings )
 		return false;
 
-	var c = 'wp-settings-' + userSettings.uid, o = wpCookies.getHash(c) || {}, d = new Date(), p,
+	var cookie = 'wp-settings-' + userSettings.uid, all = wpCookies.getHash(cookie) || {}, path = userSettings.url,
 	n = name.toString().replace(/[^A-Za-z0-9_]/, ''), v = value.toString().replace(/[^A-Za-z0-9_]/, '');
 
-	if ( del ) {
-		delete o[n];
+	if ( _del ) {
+		delete all[n];
 	} else {
-		o[n] = v;
+		all[n] = v;
 	}
 
-	d.setTime( d.getTime() + 31536000000 );
-	p = userSettings.url;
-
-	wpCookies.setHash(c, o, d, p);
-	wpCookies.set('wp-settings-time-'+userSettings.uid, userSettings.time, d, p);
+	wpCookies.setHash(cookie, all, 31536000, path);
+	wpCookies.set('wp-settings-time-'+userSettings.uid, userSettings.time, 31536000, path);
 
 	return name;
 }
