82087e02f336d6288a8faebf8ccc955429909f49
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-02-19 01:46:48 +0000

    Update TinyMCE mark loaded src. see #19592.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19952 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/tinymce/mark_loaded_src.js
+++ wp-includes/js/tinymce/mark_loaded_src.js
@@ -1,20 +1,20 @@
-(function(){
-	if ( 'undefined' == tinyMCEPreInit )
+(function(undefined){
+	if ( undefined === tinyMCEPreInit )
 		return;
 
-	var baseurl = tinyMCEPreInit.base, sl = tinymce.ScriptLoader, lang = tinyMCEPreInit.ref.language,
-		theme = tinyMCEPreInit.ref.theme, plugins = tinyMCEPreInit.ref.plugins, suffix = tinyMCEPreInit.suffix;
+	var t = tinyMCEPreInit, baseurl = t.base, markDone = tinymce.ScriptLoader.markDone, lang = t.ref.language,
+		theme = t.ref.theme, plugins = t.ref.plugins, suffix = t.suffix;
 
-	sl.markDone( baseurl+'/langs/'+lang+'.js' );
-	sl.markDone( baseurl+'/themes/'+theme+'/editor_template'+suffix+'.js' );
-	sl.markDone( baseurl+'/themes/'+theme+'/langs/'+lang+'.js' );
-	sl.markDone( baseurl+'/themes/'+theme+'/langs/'+lang+'_dlg.js' );
+	markDone( baseurl+'/langs/'+lang+'.js' );
+	markDone( baseurl+'/themes/'+theme+'/editor_template'+suffix+'.js' );
+	markDone( baseurl+'/themes/'+theme+'/langs/'+lang+'.js' );
+	markDone( baseurl+'/themes/'+theme+'/langs/'+lang+'_dlg.js' );
 
 	tinymce.each( plugins.split(','), function(plugin){
 		if ( plugin && plugin.charAt(0) != '-' ) {
-			sl.markDone( baseurl+'/plugins/'+plugin+'/editor_plugin'+suffix+'.js' );
-			sl.markDone( baseurl+'/plugins/'+plugin+'/langs/'+lang+'.js' );
-			sl.markDone( baseurl+'/plugins/'+plugin+'/langs/'+lang+'_dlg.js' )
+			markDone( baseurl+'/plugins/'+plugin+'/editor_plugin'+suffix+'.js' );
+			markDone( baseurl+'/plugins/'+plugin+'/langs/'+lang+'.js' );
+			markDone( baseurl+'/plugins/'+plugin+'/langs/'+lang+'_dlg.js' )
 		}
 	});
 })();
