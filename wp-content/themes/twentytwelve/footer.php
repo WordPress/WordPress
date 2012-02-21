a78b34752a5ac0fbba46e1faee0053baf20d112f
lancewillett <lancewillett@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-02-21 06:24:49 +0000

    Twenty Twelve: no need to translate the footer generator URL. See #19978.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19959 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/footer.php
+++ wp-content/themes/twentytwelve/footer.php
@@ -15,7 +15,7 @@
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'twentytwelve_credits' ); ?>
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
+			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
