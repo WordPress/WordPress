285fd9bc55154638ea2a132a0d527baba8f60a55
lancewillett <lancewillett@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-02-21 06:14:51 +0000

    Twenty Twelve style updates, props drewstrojny. See #19978.
    And, use a simpler and more consistent `site-header` class value. Props iandstewart.
    
    
    git-svn-id: http://svn.automattic.com/wordpress/trunk@19958 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/header.php
+++ wp-content/themes/twentytwelve/header.php
@@ -34,7 +34,7 @@ wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri() );
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed">
-	<header id="masthead" role="banner">
+	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
