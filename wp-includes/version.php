b7b715a2624c5d71c6e01444b4f81eac371f7fae
bumpbot <bumpbot@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-02-28 14:18:24 +0000

    Compress scripts/styles: 3.4-alpha-20014.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@20014 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.4-alpha-19998';
+$wp_version = '3.4-alpha-20014';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 19863;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '348-19998';
+$tinymce_version = '348-20014';
 
 /**
  * Holds the cache manifest version
