9a4874ec7a1431f982ba28f66f8467c255ab2143
lancewillett <lancewillett@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-02-28 03:48:34 +0000

    Twenty Twelve: better "no posts found" message and action for index view. If a logged-in user can edit posts, show an appropriate message. Otherwise, show the normal "no posts found" message. See #19978.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@20006 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/index.php
+++ wp-content/themes/twentytwelve/index.php
@@ -28,17 +28,34 @@ get_header(); ?>
 		<?php else : ?>
 
 			<article id="post-0" class="post no-results not-found">
+
+			<?php if ( current_user_can( 'edit_posts' ) ) :
+				// Show a different message to a logged-in user who can add posts.
+			?>
 				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentytwelve' ); ?></h1>
+					<h1 class="entry-title"><?php _e( 'No posts to display', 'twentytwelve' ); ?></h1>
+				</header><!-- .entry-header -->
+
+				<div class="entry-content">
+					<p><?php printf( __( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'twentytwelve' ), admin_url( 'post-new.php' ) ); ?></p>
+				</div><!-- .entry-content -->
+
+			<?php else :
+				// Show the default message to everyone else.
+			?>
+				<header class="entry-header">
+					<h1 class="entry-title"><?php _e( 'Nothing found', 'twentytwelve' ); ?></h1>
 				</header><!-- .entry-header -->
 
 				<div class="entry-content">
 					<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentytwelve' ); ?></p>
 					<?php get_search_form(); ?>
 				</div><!-- .entry-content -->
+			<?php endif; // end current_user_can() check ?>
+
 			</article><!-- #post-0 -->
 
-		<?php endif; ?>
+		<?php endif; // end have_posts() check ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
