57f8d7715f2440e339c06950579fca7799887ff6
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-02-29 22:54:50 +0000

    Remove extra $. see #20103.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@20052 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-ms-themes-list-table.php
+++ wp-admin/includes/class-wp-ms-themes-list-table.php
@@ -163,7 +163,7 @@ class WP_MS_Themes_List_Table extends WP_List_Table {
 	}
 
 	function no_items() {
-		if ( ! $$this->has_items )
+		if ( ! $this->has_items )
 			_e( 'No themes found.' );
 		else
 			_e( 'You do not appear to have any themes available at this time.' );
