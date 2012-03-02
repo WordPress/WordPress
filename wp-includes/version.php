2e1c1131734abad182fa581752dc3c984a15c97c
bumpbot <bumpbot@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-03-02 00:00:20 +0000

    Compress scripts/styles: 3.4-alpha-20066.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@20066 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.4-alpha-20061';
+$wp_version = '3.4-alpha-20066';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
