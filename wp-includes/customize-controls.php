125a02a3c58a0fe3d3911df81ed5408cab3bb6d9
koopersmith <koopersmith@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-03-04 02:40:18 +0000

    Theme Customizer: Add mini screenshot and description to theme info section. see #19910.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@20108 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/customize-controls.php
+++ wp-includes/customize-controls.php
@@ -52,12 +52,19 @@ do_action( 'customize_controls_print_scripts' );
 
 		<div id="customize-info" class="customize-section">
 			<div class="customize-section-title">
+				<?php if ( $screenshot ) : ?>
+					<img class="theme-screenshot" src="<?php echo esc_url( $screenshot ); ?>" />
+				<?php endif; ?>
 				<strong class="theme-name"><?php echo $theme['Name']; ?></strong>
 				<span class="theme-by"><?php printf( __( 'By %s' ), $theme['Author'] ); ?></span>
 			</div>
 			<div class="customize-section-content">
 				<?php if ( $screenshot ) : ?>
 					<img class="theme-screenshot" src="<?php echo esc_url( $screenshot ); ?>" />
+				<?php endif; ?>
+
+				<?php if ( $theme->description ): ?>
+					<div class="theme-description"><?php echo $theme->description; ?></div>
 				<?php endif; ?>
 			</div>
 		</div>
