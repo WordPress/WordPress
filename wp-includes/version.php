411853beb6359fc1b3b80c9268be6bb89ebf9713
bumpbot <bumpbot@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-03-15 01:00:22 +0000

    Compress scripts/styles: 3.4-alpha-20178. TinyMCE updated.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@20178 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.4-alpha-20175';
+$wp_version = '3.4-alpha-20178';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 20148;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '349-20175';
+$tinymce_version = '349-20178';
 
 /**
  * Holds the cache manifest version
