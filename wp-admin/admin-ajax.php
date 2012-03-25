a1a764cbc92a0a03cb7f01ab760ebb9293ea3f1e
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-03-25 12:53:17 +0000

    Send X-Robots-Tag: noindex in admin-ajax. props ryan, joostdevalk. fixes #18465.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@20288 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-ajax.php
+++ wp-admin/admin-ajax.php
@@ -30,6 +30,8 @@ require_once( ABSPATH . 'wp-admin/includes/admin.php' );
 require_once( ABSPATH . 'wp-admin/includes/ajax-actions.php' );
 
 @header( 'Content-Type: text/html; charset=' . get_option( 'blog_charset' ) );
+@header( 'X-Robots-Tag: noindex' );
+
 send_nosniff_header();
 
 do_action( 'admin_init' );
