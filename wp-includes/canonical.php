1b65e28f8fa0c457ae971e6a4440e970fd7cb120
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-04-11 21:18:40 +0000

    Don't do a canonical redirect for singular paged pages when on the front-page. This was breaking pagination on sites with a static home page. Props batmoo. see #20385
    git-svn-id: http://svn.automattic.com/wordpress/trunk@20444 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/canonical.php
+++ wp-includes/canonical.php
@@ -209,7 +209,7 @@ function redirect_canonical( $requested_url = null, $do_redirect = true ) {
 		}
 
 		// Post Paging
-		if ( is_singular() && get_query_var('page') ) {
+		if ( is_singular() && ! is_front_page() && get_query_var('page') ) {
 			if ( !$redirect_url )
 				$redirect_url = get_permalink( get_queried_object_id() );
 			$redirect_url = trailingslashit( $redirect_url ) . user_trailingslashit( get_query_var( 'page' ), 'single_paged' );
