5d5db6bccf5917593abd3d2ce15a0deb16ece45a
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-04-16 22:46:36 +0000

    Update Plupload to 1.5.4
    git-svn-id: http://svn.automattic.com/wordpress/branches/3.3@20487 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/plupload/handlers.dev.js
+++ wp-includes/js/plupload/handlers.dev.js
@@ -44,11 +44,13 @@ function fileUploading(up, file) {
 
 	if ( max > hundredmb && file.size > hundredmb ) {
 		setTimeout(function(){
-			if ( file.status == 2 && file.loaded == 0 ) { // not uploading
-				wpFileError(file, pluploadL10n.big_upload_failed.replace('%1$s', '<a class="uploader-html" href="#">').replace('%2$s', '</a>'));
+			var done;
 
-				if ( up.current && up.current.file.id == file.id && up.current.xhr.abort )
-					up.current.xhr.abort();
+			if ( file.status < 3 && file.loaded == 0 ) { // not uploading
+				wpFileError(file, pluploadL10n.big_upload_failed.replace('%1$s', '<a class="uploader-html" href="#">').replace('%2$s', '</a>'));
+				up.stop(); // stops the whole queue
+				up.removeFile(file);
+				up.start(); // restart the queue
 			}
 		}, 10000); // wait for 10 sec. for the file to start uploading
 	}
