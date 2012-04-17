060e3f39ef293c4d8998f2fb4fdac7c1a7f69264
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-04-17 23:13:20 +0000

    Update swfobject
    git-svn-id: http://svn.automattic.com/wordpress/branches/3.3@20499 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -179,7 +179,7 @@ function wp_default_scripts( &$scripts ) {
 
 	$scripts->add( 'jcrop', "/wp-includes/js/jcrop/jquery.Jcrop$suffix.js", array('jquery'), '0.9.8-20110113');
 
-	$scripts->add( 'swfobject', "/wp-includes/js/swfobject.js", false, '2.2');
+	$scripts->add( 'swfobject', "/wp-includes/js/swfobject.js", false, '2.2-20120417');
 
 	// common bits for both uploaders
 	$max_upload_size = ( (int) ( $max_up = @ini_get('upload_max_filesize') ) < (int) ( $max_post = @ini_get('post_max_size') ) ) ? $max_up : $max_post;
