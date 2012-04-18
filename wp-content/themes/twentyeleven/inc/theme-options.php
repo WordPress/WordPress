00ab660e013cd4142649c50498b02d75894922fc
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-04-18 17:32:53 +0000

    Check for wp_get_theme() in Twenty Eleven before using it, for compatibility down to WP 3.2 when Twenty Eleven was first released. see #20103.
    git-svn-id: http://svn.automattic.com/wordpress/trunk@20508 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyeleven/inc/theme-options.php
+++ wp-content/themes/twentyeleven/inc/theme-options.php
@@ -314,7 +314,8 @@ function twentyeleven_theme_options_render_page() {
 	?>
 	<div class="wrap">
 		<?php screen_icon(); ?>
-		<h2><?php printf( __( '%s Theme Options', 'twentyeleven' ), wp_get_theme() ); ?></h2>
+		<?php $theme_name = function_exists( 'wp_get_theme' ) ? wp_get_theme() : get_current_theme(); ?>
+		<h2><?php printf( __( '%s Theme Options', 'twentyeleven' ), $theme_name ); ?></h2>
 		<?php settings_errors(); ?>
 
 		<form method="post" action="options.php">
