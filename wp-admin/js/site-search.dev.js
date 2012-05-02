de86b02f42a1e5f4b9525c1f8e48f1fa38989874
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-05-02 21:33:43 +0000

    Styling and RTL style fixes for user/site autocomplete. Props ocean90, helenyhou. fixes #20584
    git-svn-id: http://core.svn.wordpress.org/trunk@20705 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/site-search.dev.js
+++ wp-admin/js/site-search.dev.js
@@ -2,6 +2,8 @@ jQuery( function($) {
 	$( '#site-search-input' ).autocomplete({
 		source:   ajaxurl + '?action=autocomplete-site',
 		delay:    500,
-		minLength: 2
+		minLength: 2,
+		open: function(e, ui) { $(this).addClass('open'); },
+		close: function(e, ui) { $(this).removeClass('open'); }
 	});
 });
--- wp-admin/js/user-search.dev.js
+++ wp-admin/js/user-search.dev.js
@@ -4,18 +4,24 @@ jQuery( function($) {
 	$( '#adduser-email, #newuser' ).autocomplete({
 		source:   ajaxurl + '?action=autocomplete-user&autocomplete_type=add' + id,
 		delay:    500,
-		minLength: 2
+		minLength: 2,
+		open: function(e, ui) { $(this).addClass('open'); },
+		close: function(e, ui) { $(this).removeClass('open'); }
 	});
 
 	$( '#user-search-input' ).autocomplete({
 		source:   ajaxurl + '?action=autocomplete-user&autocomplete_type=search' + id,
 		delay:    500,
-		minLength: 2
+		minLength: 2,
+		open: function(e, ui) { $(this).addClass('open'); },
+		close: function(e, ui) { $(this).removeClass('open'); }
 	});
 
 	$( '#all-user-search-input' ).autocomplete({
 		source:   ajaxurl + '?action=autocomplete-user&autocomplete_type=search-all' + id,
 		delay:    500,
-		minLength: 2
+		minLength: 2,
+		open: function(e, ui) { $(this).addClass('open'); },
+		close: function(e, ui) { $(this).removeClass('open'); }
 	});
 });
