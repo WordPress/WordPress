811d4d517d943a7bd1332625c1504983663f7b00
bumpbot <bumpbot@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-06-04 14:58:23 +0000

    Compress scripts/styles: 3.4-RC1-20985.
    git-svn-id: http://core.svn.wordpress.org/trunk@20985 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/site-search.js
+++ wp-admin/js/site-search.js
@@ -1,1 +1,1 @@
-jQuery(function(a){a("#site-search-input").autocomplete({source:ajaxurl+"?action=autocomplete-site",delay:500,minLength:2,open:function(c,b){a(this).addClass("open")},close:function(c,b){a(this).removeClass("open")}})});
+jQuery(function(b){var a=!!("undefined"!=typeof isRtl&&isRtl);b("#site-search-input").autocomplete({source:ajaxurl+"?action=autocomplete-site",delay:500,minLength:2,position:a?{my:"right top",at:"right bottom",offset:"0, -1"}:{offset:"0, -1"},open:function(d,c){b(this).addClass("open")},close:function(d,c){b(this).removeClass("open")}})});
