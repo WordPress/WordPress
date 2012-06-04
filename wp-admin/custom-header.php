e6e9f5976125067289ae5cbbdfad2baa4f749ea8
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-06-04 14:13:20 +0000

    Update custom header help text to reflect recent UI changes. Props SergeyBiryukov, rasheed. fixes #20827
    git-svn-id: http://core.svn.wordpress.org/trunk@20981 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/custom-header.php
+++ wp-admin/custom-header.php
@@ -116,7 +116,7 @@ class Custom_Image_Header {
 			'id'      => 'set-header-image',
 			'title'   => __('Header Image'),
 			'content' =>
-				'<p>' . __( 'You can set a custom image header for your site. Simply upload the image and crop it, and the new header will go live immediately. Alternatively, you can use an image that has already been uploaded to your Media Library by clicking the &#8220;Choose from image library&#8221; link.' ) . '</p>' .
+				'<p>' . __( 'You can set a custom image header for your site. Simply upload the image and crop it, and the new header will go live immediately. Alternatively, you can use an image that has already been uploaded to your Media Library by clicking the &#8220;Choose Image&#8221; button.' ) . '</p>' .
 				'<p>' . __( 'Some themes come with additional header images bundled. If you see multiple images displayed, select the one you&#8217;d like and click the &#8220;Save Changes&#8221; button.' ) . '</p>' .
 				'<p>' . __( 'If your theme has more than one default header image, or you have uploaded more than one custom header image, you have the option of having WordPress display a randomly different image on each page of your site. Click the &#8220;Random&#8221; radio button next to the Uploaded Images or Default Images section to enable this feature.') . '</p>' .
 				'<p>' . __( 'If you don&#8217;t want a header image to be displayed on your site at all, click the &#8220;Remove Header Image&#8221; button at the bottom of the Header Image section of this page. If you want to re-enable the header image later, you just have to select one of the other image options and click &#8220;Save Changes&#8221;.') . '</p>'
