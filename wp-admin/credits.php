3a2ea5ed3719cd1acb10ad270f2d008a8c3cc63e
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-06-07 15:34:21 +0000

    Copy 3.4 about.php header text to credits and freedoms. props pavelevap. fixes #20794.
    git-svn-id: http://core.svn.wordpress.org/trunk@21021 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/credits.php
+++ wp-admin/credits.php
@@ -52,7 +52,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 <h1><?php printf( __( 'Welcome to WordPress %s' ), $display_version ); ?></h1>
 
-<div class="about-text"><?php printf( __( 'Thank you for updating to the latest version! Using WordPress %s will improve your looks, personality, and web publishing experience. Okay, just the last one, but still. :)' ), $display_version ); ?></div>
+<div class="about-text"><?php printf( __( 'Thank you for updating to the latest version! WordPress %s is already making your website better, faster, and more attractive, just like you!' ), $display_version ); ?></div>
 
 <div class="wp-badge"><?php printf( __( 'Version %s' ), $display_version ); ?></div>
 
