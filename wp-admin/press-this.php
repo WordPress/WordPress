7c214624b6e275fe22483d103dc3507dc7ba61f4
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-06-11 20:34:42 +0000

    Add RTL and locale classes to the press this body. Props SergeyBiryukov. fixes #19603
    git-svn-id: http://core.svn.wordpress.org/trunk@21052 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/press-this.php
+++ wp-admin/press-this.php
@@ -435,7 +435,11 @@ var photostorage = false;
 	});
 </script>
 </head>
-<body class="press-this wp-admin<?php if ( is_rtl() ) echo ' rtl'; ?>">
+<?php
+$admin_body_class = ( is_rtl() ) ? 'rtl' : '';
+$admin_body_class .= ' locale-' . sanitize_html_class( strtolower( str_replace( '_', '-', get_locale() ) ) );
+?>
+<body class="press-this wp-admin <?php echo $admin_body_class; ?>">
 <form action="press-this.php?action=post" method="post">
 <div id="poststuff" class="metabox-holder">
 	<div id="side-sortables" class="press-this-sidebar">
