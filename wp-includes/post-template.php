60191c0968d651783c3941544d2e68d9bd9e9e9f
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-06-26 03:21:37 +0000

    Fix validation issue in get_the_password_form() caused by wpautop(). fixes #21018 for 3.4.
    git-svn-id: http://core.svn.wordpress.org/branches/3.4@21125 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post-template.php
+++ wp-includes/post-template.php
@@ -1220,7 +1220,7 @@ function get_the_password_form() {
 	$output = '<form action="' . esc_url( site_url( 'wp-login.php?action=postpass', 'login_post' ) ) . '" method="post">
 	<p>' . __("This post is password protected. To view it please enter your password below:") . '</p>
 	<p><label for="' . $label . '">' . __("Password:") . ' <input name="post_password" id="' . $label . '" type="password" size="20" /></label> <input type="submit" name="Submit" value="' . esc_attr__("Submit") . '" /></p>
-	</form>
+</form>
 	';
 	return apply_filters('the_password_form', $output);
 }
