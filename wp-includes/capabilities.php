f5df7e19ad5ea422ccbf7e1973a89ce19c279c10
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-06-27 19:29:16 +0000

    Add a check that previously fell through to the next branch in map_meta_cap(). for 3.4.
    git-svn-id: http://core.svn.wordpress.org/branches/3.4@21153 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/capabilities.php
+++ wp-includes/capabilities.php
@@ -1130,6 +1130,8 @@ function map_meta_cap( $cap, $user_id ) {
 		// Disallow unfiltered_html for all users, even admins and super admins.
 		if ( defined( 'DISALLOW_UNFILTERED_HTML' ) && DISALLOW_UNFILTERED_HTML )
 			$caps[] = 'do_not_allow';
+		elseif ( is_multisite() && ! is_super_admin( $user_id ) )
+			$caps[] = $cap;
 		else
 			$caps[] = $cap;
 		break;
