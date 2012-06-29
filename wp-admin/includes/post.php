dfa7b94b8325a73f89a200cc3a33e1b28d9b6cbf
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-06-29 15:09:14 +0000

    Add inline documentation for wp_edit_attachments_query(). props simonwheatley, SergeyBiryukov. fixes #21105.
    git-svn-id: http://core.svn.wordpress.org/trunk@21179 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/post.php
+++ wp-admin/includes/post.php
@@ -925,12 +925,14 @@ function get_available_post_mime_types($type = 'attachment') {
 }
 
 /**
- * {@internal Missing Short Description}}
- *
+ * Executes a query for attachments. An array of WP_Query arguments
+ * can be passed in, which will override the arguments set by this function.
+ * 
  * @since 2.5.0
+ * @uses apply_filters() Calls 'upload_per_page' on posts_per_page argument
  *
- * @param unknown_type $q
- * @return unknown
+ * @param array|bool $q Array of query variables to use to build the query or false to use $_GET superglobal.
+ * @return array
  */
 function wp_edit_attachments_query( $q = false ) {
 	if ( false === $q )
