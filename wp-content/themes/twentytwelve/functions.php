85f7094ce6234a37b9b3651aecb73dd39a73b166
lancewillett <lancewillett@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-07-13 00:27:29 +0000

    Twenty Twelve: change content_width variable to the correct value, matching the width of the main content container. Fixes #21248.
    git-svn-id: http://core.svn.wordpress.org/trunk@21267 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/functions.php
+++ wp-content/themes/twentytwelve/functions.php
@@ -29,7 +29,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) )
-	$content_width = 584;
+	$content_width = 625;
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
