dcb200efc95672ed07a4629edfd880a4948076f1
nacin <nacin@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-07-20 15:09:40 +0000

    Fix order of @param's for deactivate_plugins(). props Viper007Bond, fixes #21269.
    git-svn-id: http://core.svn.wordpress.org/trunk@21291 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/plugin.php
+++ wp-admin/includes/plugin.php
@@ -573,9 +573,9 @@ function activate_plugin( $plugin, $redirect = '', $network_wide = false, $silen
  * @since 2.5.0
  *
  * @param string|array $plugins Single plugin or list of plugins to deactivate.
+ * @param bool $silent Prevent calling deactivation hooks. Default is false.
  * @param mixed $network_wide Whether to deactivate the plugin for all sites in the network.
  * 	A value of null (the default) will deactivate plugins for both the site and the network.
- * @param bool $silent Prevent calling deactivation hooks. Default is false.
  */
 function deactivate_plugins( $plugins, $silent = false, $network_wide = null ) {
 	if ( is_multisite() )
