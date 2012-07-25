d4e0c684d3a5b80013c5a87c0b5fa5d33e24682b
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-07-25 19:59:29 +0000

    Bail out themes and plugins that have copied code from core and don't maintain it by requiring class-wp-editor.php in wp-langs.php, fixes #21350 for trunk
    git-svn-id: http://core.svn.wordpress.org/trunk@21345 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/tinymce/langs/wp-langs.php
+++ wp-includes/js/tinymce/langs/wp-langs.php
@@ -3,6 +3,9 @@
 if ( ! defined( 'ABSPATH' ) )
 	exit;
 
+if ( ! class_exists( '_WP_Editors' ) )
+	require( ABSPATH . WPINC . '/class-wp-editor.php' );
+
 // deprecated, not used
 function mce_escape($text) {
 	return esc_js($text);
