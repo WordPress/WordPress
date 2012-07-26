2417e42fe56d4cedc7ea24d000f8703c6a547b21
ryan <ryan@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-07-26 15:08:30 +0000

    Improved phpdoc for file.php. Props tommcfarlin. fixes #21328
    git-svn-id: http://core.svn.wordpress.org/trunk@21350 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/file.php
+++ wp-admin/includes/file.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * File contains all the administration image manipulation functions.
+ * Functions for reading, writing, modifying, and deleting files on the file system.
+ * Includes functionality for theme-specific files as well as operations for uploading, 
+ * archiving, and rendering output when necessary.
  *
  * @package WordPress
  * @subpackage Administration
