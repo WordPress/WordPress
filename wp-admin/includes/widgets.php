93979aa0efec6dec360bf726ee3ad38bf3befae0
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-07-27 03:33:38 +0000

    Widgets: Add the name of the widget to the Add and Edit buttons on the Accessible Widget Management Screen, props cannona, fixes #21338
    git-svn-id: http://core.svn.wordpress.org/trunk@21355 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/widgets.php
+++ wp-admin/includes/widgets.php
@@ -179,7 +179,11 @@ function wp_widget_control( $sidebar_args ) {
 	<div class="widget-top">
 	<div class="widget-title-action">
 		<a class="widget-action hide-if-no-js" href="#available-widgets"></a>
-		<a class="widget-control-edit hide-if-js" href="<?php echo esc_url( add_query_arg( $query_arg ) ); ?>"><span class="edit"><?php _e('Edit'); ?></span><span class="add"><?php _e('Add'); ?></span></a>
+		<a class="widget-control-edit hide-if-js" href="<?php echo esc_url( add_query_arg( $query_arg ) ); ?>">
+			<span class="edit"><?php _e( 'Edit' ); ?></span>
+			<span class="add"><?php _e( 'Add' ); ?></span>
+			<span class="screen-reader-text"><?php echo $widget_title; ?></span>
+		</a>
 	</div>
 	<div class="widget-title"><h4><?php echo $widget_title ?><span class="in-widget-title"></span></h4></div>
 	</div>
