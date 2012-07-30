ba2fe5b89ab0c23e7b52743f7469bf0cc0e401c8
lancewillett <lancewillett@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-07-30 19:48:43 +0000

    Twenty Twelve: fix typo, s/#entry-meta/.entry-meta/ props zeo. Closes #21418.
    git-svn-id: http://core.svn.wordpress.org/trunk@21369 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/content-aside.php
+++ wp-content/themes/twentytwelve/content-aside.php
@@ -19,5 +19,5 @@
 	<footer class="entry-meta">
 		<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<div class="edit-link">', '</div>' ); ?>
 		<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_date(); ?></a>
-	</footer><!-- #entry-meta -->
+	</footer><!-- .entry-meta -->
 </article><!-- #post -->
