22fa9e4f7440701d723c47099737daa068ec7df0
lancewillett <lancewillett@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-08-05 22:27:22 +0000

    Twenty Twelve: only show excerpt on search view, not full content.
    Props bradthomas127 and obenland. See #21462.
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@21437 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/content.php
+++ wp-content/themes/twentytwelve/content.php
@@ -30,10 +30,16 @@
 			<?php endif; // comments_open() ?>
 		</header><!-- .entry-header -->
 
+		<?php if ( is_search() ) : // Only display Excerpts for Search ?>
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+		<?php else : ?>
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-content -->
+		<?php endif; ?>
 
 		<footer class="entry-meta">
 			<?php twentytwelve_entry_meta(); ?>
