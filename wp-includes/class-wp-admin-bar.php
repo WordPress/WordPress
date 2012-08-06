4ef4df2f3e365de61e632decd67bb93d642ca7e1
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-08-06 00:34:44 +0000

    Accessibility: add off-screen Log Out link in the toolbar, tweak the class names of the accessible links in #adminbar, see #21312
    git-svn-id: http://core.svn.wordpress.org/trunk@21452 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-admin-bar.php
+++ wp-includes/class-wp-admin-bar.php
@@ -348,12 +348,13 @@ class WP_Admin_Bar {
 
 		?>
 		<div id="wpadminbar" class="<?php echo $class; ?>" role="navigation">
-			<a id="wpadminbar-shortcut" href="#wp-toolbar" tabindex="1"><?php _e('Skip to toolbar'); ?></a>
+			<a class="screen-reader-text screen-reader-shortcut" href="#wp-toolbar" tabindex="1"><?php _e('Skip to toolbar'); ?></a>
 			<div class="quicklinks" id="wp-toolbar" role="navigation" aria-label="<?php esc_attr_e('Top navigation toolbar.'); ?>">
 				<?php foreach ( $root->children as $group ) {
 					$this->_render_group( $group );
 				} ?>
 			</div>
+			<a class="screen-reader-text screen-reader-shortcut" href="<?php echo esc_url( wp_logout_url() ); ?>"><?php _e('Log Out'); ?></a>
 		</div>
 
 		<?php
