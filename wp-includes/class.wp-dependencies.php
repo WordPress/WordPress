cbc9aabfa4f8b71022987872c32f774d566f53fe
azaozz <azaozz@1a063a9b-81f0-0310-95a4-ce76da25c4cd>
2012-08-08 17:21:24 +0000

    When WP_Dependencies accept a string for a single dependency, make sure the string is not empty, see #20683
    git-svn-id: http://core.svn.wordpress.org/trunk@21481 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class.wp-dependencies.php
+++ wp-includes/class.wp-dependencies.php
@@ -245,7 +245,7 @@ class _WP_Dependency {
 
 	function __construct() {
 		@list($this->handle, $this->src, $this->deps, $this->ver, $this->args) = func_get_args();
-		if ( is_string( $this->deps ) )
+		if ( is_string( $this->deps ) && ! empty( $this->deps ) )
 			$this->deps = array( $this->deps );
 		elseif ( !is_array( $this->deps ) )
 			$this->deps = array();
