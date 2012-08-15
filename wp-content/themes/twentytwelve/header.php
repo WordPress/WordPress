24c16e3b059ab1a8a9faa9f6c81d248354a377c4
Lance Willett <nanobar@gmail.com>
2012-08-15 16:05:13 +0000

    Twenty Twelve: remove `initial-scale` value from viewport meta element to avoid iOS zoom bug. Props bradthomas127, see #21577.
    git-svn-id: http://core.svn.wordpress.org/trunk@21525 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/header.php
+++ wp-content/themes/twentytwelve/header.php
@@ -17,7 +17,7 @@
 <!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="viewport" content="width=device-width" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
