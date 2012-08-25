b3711fba97a8bb8c5e38ab03497249228e0ad8ba
Lance Willett <nanobar@gmail.com>
2012-08-25 17:41:32 +0000

    Twenty Twelve: comments.php, cleanup and better indentation. Props obenland, see #21685.
    git-svn-id: http://core.svn.wordpress.org/trunk@21621 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/comments.php
+++ wp-content/themes/twentytwelve/comments.php
@@ -11,16 +11,16 @@
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() )
+	return;
 ?>
-<?php
-	/*
-	 * If the current post is protected by a password and
-	 * the visitor has not yet entered the password we will
-	 * return early without loading the comments.
-	 */
-	if ( post_password_required() )
-			return;
-?>
+
 <div id="comments" class="comments-area">
 
 	<?php // You can start editing here -- including this comment! ?>
