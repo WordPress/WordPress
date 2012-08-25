30ba6ef7785ce25b6ef2ba55d859310a03446329
Lance Willett <nanobar@gmail.com>
2012-08-25 17:57:42 +0000

    Twenty Twelve: pinking your trailing spaces, see #21675.
    git-svn-id: http://core.svn.wordpress.org/trunk@21624 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/footer.php
+++ wp-content/themes/twentytwelve/footer.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying the footer.
  *
- * Contains footer content and the closing of the 
+ * Contains footer content and the closing of the
  * #main and #page div elements.
  *
  * @package WordPress
