8932111b0747537693c23d19d12995d38c4fdee3
Lance Willett <nanobar@gmail.com>
2012-08-25 16:50:05 +0000

    Twenty Twelve: spaces to tabs in navigation.js, props obenland. See #21685.
    git-svn-id: http://core.svn.wordpress.org/trunk@21616 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/js/navigation.js
+++ wp-content/themes/twentytwelve/js/navigation.js
@@ -4,26 +4,26 @@
  * Handles toggling the navigation menu for small screens.
  */
 ( function() {
-    var button = document.getElementById( 'site-navigation' ).getElementsByTagName( 'h3' )[0],
-        menu   = document.getElementById( 'site-navigation' ).getElementsByTagName( 'ul' )[0];
+	var button = document.getElementById( 'site-navigation' ).getElementsByTagName( 'h3' )[0],
+	    menu   = document.getElementById( 'site-navigation' ).getElementsByTagName( 'ul' )[0];
 
-    if ( undefined == button || undefined == menu )
-    	return false;
+	if ( undefined == button || undefined == menu )
+		return false;
 
-    button.onclick = function() {
-        if ( -1 == menu.className.indexOf( 'nav-menu' ) )
-            menu.className = 'nav-menu';
+	button.onclick = function() {
+		if ( -1 == menu.className.indexOf( 'nav-menu' ) )
+			menu.className = 'nav-menu';
 
-        if ( -1 != button.className.indexOf( 'toggled-on' ) ) {
-            button.className = button.className.replace( ' toggled-on', '' );
-            menu.className = menu.className.replace( ' toggled-on', '' );
-        } else {
-            button.className += ' toggled-on';
-            menu.className += ' toggled-on';
-        }
-    };
+		if ( -1 != button.className.indexOf( 'toggled-on' ) ) {
+			button.className = button.className.replace( ' toggled-on', '' );
+			menu.className = menu.className.replace( ' toggled-on', '' );
+		} else {
+			button.className += ' toggled-on';
+			menu.className += ' toggled-on';
+		}
+	};
 
-    // Hide menu toggle button if menu is empty.
-    if ( ! menu.childNodes.length )
-        button.style.display = 'none';
+	// Hide menu toggle button if menu is empty.
+	if ( ! menu.childNodes.length )
+		button.style.display = 'none';
 } )();
