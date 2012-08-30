708c9746a9ed5affb88fa582229356bfdf3becf2
Lance Willett <nanobar@gmail.com>
2012-08-30 19:32:05 +0000

    Twenty Twelve: go with `echo get_the_date()` here, added in r21670 but reverted accidentally in r21674. Props obenland.
    git-svn-id: http://core.svn.wordpress.org/trunk@21675 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/content-image.php
+++ wp-content/themes/twentytwelve/content-image.php
@@ -16,7 +16,7 @@
 		<footer class="entry-meta">
 			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
 				<h1><?php the_title(); ?></h1>
-				<h2><time class="entry-date" datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>" pubdate><?php the_date(); ?></time></h2>
+				<h2><time class="entry-date" datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>" pubdate><?php echo get_the_date(); ?></time></h2>
 			</a>
 		</footer><!-- .entry-meta -->
 	</article><!-- #post -->
