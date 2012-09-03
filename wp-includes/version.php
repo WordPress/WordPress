95de46bf19b7903ef7a3e501348a1c97c0eaa605
Bot (Assets) <nobody@localhost>
2012-09-03 15:59:27 +0000

    Compress scripts/styles: 3.5-alpha-21698.
    git-svn-id: http://core.svn.wordpress.org/trunk@21698 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.5-alpha-21694';
+$wp_version = '3.5-alpha-21698';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
