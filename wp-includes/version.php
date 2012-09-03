d067f633e5f63a78a36aff7f3eb06e9fb9c1c8d5
Andrew Nacin <wp@andrewnacin.com>
2012-09-03 23:23:49 +0000

    Version bumps for the 3.4 branch, including a DB version bump for [21709] (see [21707]).
    git-svn-id: http://core.svn.wordpress.org/branches/3.4@21710 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,14 +4,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.4.1';
+$wp_version = '3.4.2-alpha';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 21115;
+$wp_db_version = 21707;
 
 /**
  * Holds the TinyMCE version
