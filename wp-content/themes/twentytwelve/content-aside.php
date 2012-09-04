a10b65eb971ffa4c247aff67589255e43fba4aab
Lance Willett <nanobar@gmail.com>
2012-09-04 04:17:49 +0000

    Twenty Twelve: fix edit_post_link() inconsistencies, props obenland. Fixes #21722.
    git-svn-id: http://core.svn.wordpress.org/trunk@21723 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/content-aside.php
+++ wp-content/themes/twentytwelve/content-aside.php
@@ -17,7 +17,7 @@
 	</div><!-- .aside -->
 
 	<footer class="entry-meta">
-		<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<div class="edit-link">', '</div>' ); ?>
 		<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php echo get_the_date(); ?></a>
+		<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post -->
--- wp-content/themes/twentytwelve/content-image.php
+++ wp-content/themes/twentytwelve/content-image.php
@@ -17,6 +17,7 @@
 			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
 				<h1><?php the_title(); ?></h1>
 				<h2><time class="entry-date" datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>" pubdate><?php echo get_the_date(); ?></time></h2>
+				<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
 			</a>
 		</footer><!-- .entry-meta -->
 	</article><!-- #post -->
