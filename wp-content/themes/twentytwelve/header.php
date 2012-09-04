1aef2113079fd7c6fc6e67d3ab17857535bfd074
Lance Willett <nanobar@gmail.com>
2012-09-04 05:18:17 +0000

    Twenty Twelve: s/Show Menu/Menu/ for clarity and simplicity, and maybe more semantic and accessible. Fixes #21783, props iandstewart.
    git-svn-id: http://core.svn.wordpress.org/trunk@21730 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/header.php
+++ wp-content/themes/twentytwelve/header.php
@@ -36,7 +36,7 @@
 		</hgroup>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h3 class="menu-toggle"><?php _e( 'Show menu', 'twentytwelve' ); ?></h3>
+			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
 			<div class="skip-link assistive-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a></div>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
