2f5090bfb8f1f231694fd93286d76688bfeb68c0
Daryl Koopersmith <dkoopersmith@gmail.com>
2012-09-07 22:09:40 +0000

    Prevent img elements in the media modal from being draggable. see #21390.
    Browsers natively support dragging img elements, which would then set off the drop zone in the modal. We disable this by setting the draggable attribute to false.
    
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@21785 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/media.php
+++ wp-includes/media.php
@@ -1649,7 +1649,7 @@ function wp_print_media_templates( $attachment ) {
 	<script type="text/html" id="tmpl-attachment">
 		<div class="attachment-thumbnail type-<%- type %> subtype-<%- subtype %> <%- orientation %>">
 			<% if ( thumbnail ) { %>
-				<img src="<%- thumbnail %>" />
+				<img src="<%- thumbnail %>" draggable="false" />
 			<% } %>
 
 			<% if ( uploading ) { %>
@@ -1663,7 +1663,7 @@ function wp_print_media_templates( $attachment ) {
 	<script type="text/html" id="tmpl-media-selection-preview">
 		<div class="selected-img selected-count-<%- count %>">
 			<% if ( thumbnail ) { %>
-				<img src="<%- thumbnail %>" />
+				<img src="<%- thumbnail %>" draggable="false" />
 			<% } %>
 
 			<span class="count"><%- count %></span>
