bd25a2e9109bc20793274010c377061ed844614e
Andrew Nacin <wp@andrewnacin.com>
2012-09-15 19:57:46 +0000

    Correct a translation context in the network admin. props SergeyBiryukov. fixes #20910.
    git-svn-id: http://core.svn.wordpress.org/trunk@21858 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/network/menu.php
+++ wp-admin/network/menu.php
@@ -38,7 +38,7 @@ if ( current_user_can( 'update_plugins' ) ) {
 	$menu[20] = array( __('Plugins'), 'manage_network_plugins', 'plugins.php', '', 'menu-top menu-icon-plugins', 'menu-plugins', 'div' );
 }
 $submenu['plugins.php'][5]  = array( __('Installed Plugins'), 'manage_network_plugins', 'plugins.php' );
-$submenu['plugins.php'][10] = array( _x('Add New', 'plugin editor'), 'install_plugins', 'plugin-install.php' );
+$submenu['plugins.php'][10] = array( _x('Add New', 'plugin'), 'install_plugins', 'plugin-install.php' );
 $submenu['plugins.php'][15] = array( _x('Editor', 'plugin editor'), 'edit_plugins', 'plugin-editor.php' );
 
 $menu[25] = array(__('Settings'), 'manage_network_options', 'settings.php', '', 'menu-top menu-icon-settings', 'menu-settings', 'div');
