6d132acb804a68f4bd0e7298d946bc122e1d38b2
Lance Willett <nanobar@gmail.com>
2012-09-20 21:51:48 +0000

    Twenty Twelve: rework markup and styles for status post format after discussion with drewstrojny and testing for float clearing. Closes #21743.
    git-svn-id: http://core.svn.wordpress.org/trunk@21933 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/content-status.php
+++ wp-content/themes/twentytwelve/content-status.php
@@ -9,14 +9,15 @@
 ?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<div class="entry-header">
+			<header>
+				<h1><?php the_author(); ?></h1>
+				<h2><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php echo get_the_date(); ?></a></h2>
+			</header>
+			<?php echo get_avatar( get_the_author_meta( 'ID' ), apply_filters( 'twentytwelve_status_avatar', '48' ) ); ?>
+		</div><!-- .entry-header -->
+
 		<div class="entry-content">
-			<div class="avatar">
-				<header>
-					<h1><?php the_author(); ?></h1>
-					<h2><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php echo get_the_date(); ?></a></h2>
-				</header>
-				<?php echo get_avatar( get_the_author_meta( 'ID' ), apply_filters( 'twentytwelve_status_avatar', '48' ) ); ?>
-			</div>
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
 		</div><!-- .entry-content -->
 
