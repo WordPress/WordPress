3ffb74a6efc265f819b2d7f3664b30379f420520
Lance Willett <nanobar@gmail.com>
2012-09-27 03:47:21 +0000

    Twenty Twelve: better i18n for next and previous post arrow entities in single.php, props SergeyBiryukov. Fixes #22008.
    git-svn-id: http://core.svn.wordpress.org/trunk@22035 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/single.php
+++ wp-content/themes/twentytwelve/single.php
@@ -18,8 +18,8 @@ get_header(); ?>
 
 				<nav class="nav-single">
 					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-					<span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span>', 'twentytwelve' ) . ' %title' ); ?></span>
-					<span class="nav-next"><?php next_post_link( '%link', '%title ' . __( '<span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?></span>
+					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
+					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
 				</nav><!-- .nav-single -->
 
 				<?php
