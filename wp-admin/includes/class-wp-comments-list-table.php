a820d7481b358c57d31024e027a946c560a11c26
Ryan Boren <ryan@boren.nu>
2012-09-29 10:05:15 +0000

    Handle screen arg in WP_Comments_List_Table constructor. Avoids warning when calling WP_Screen::get() later. fixes #22039
    git-svn-id: http://core.svn.wordpress.org/trunk@22085 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-comments-list-table.php
+++ wp-admin/includes/class-wp-comments-list-table.php
@@ -21,7 +21,7 @@ class WP_Comments_List_Table extends WP_List_Table {
 
 	var $pending_count = array();
 
-	function __construct() {
+	function __construct( $args = array() ) {
 		global $post_id;
 
 		$post_id = isset( $_REQUEST['p'] ) ? absint( $_REQUEST['p'] ) : 0;
@@ -33,6 +33,7 @@ class WP_Comments_List_Table extends WP_List_Table {
 			'plural' => 'comments',
 			'singular' => 'comment',
 			'ajax' => true,
+			'screen' => isset( $args['screen'] ) ? $args['screen'] : null,
 		) );
 	}
 
