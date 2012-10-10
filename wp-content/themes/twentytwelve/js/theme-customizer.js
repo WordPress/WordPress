ce1fc874613b7d1b60b9f0b2dd1c523a3aa9a8bf
Lance Willett <nanobar@gmail.com>
2012-10-10 20:47:07 +0000

    Twenty Twelve: remove a duplicate/misplaced condition in theme-customizer.js, props DrewAPicture. Closes #21685.
    git-svn-id: http://core.svn.wordpress.org/trunk@22168 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/js/theme-customizer.js
+++ wp-content/themes/twentytwelve/js/theme-customizer.js
@@ -21,7 +21,7 @@
 	// Hook into background color change and adjust body class value as needed.
 	wp.customize( 'background_color', function( value ) {
 		value.bind( function( to ) {
-			if ( '#ffffff' == to || '#fff' == to || '' == to )
+			if ( '#ffffff' == to || '#fff' == to )
 				$( 'body' ).addClass( 'custom-background-white' );
 			else if ( '' == to )
 				$( 'body' ).addClass( 'custom-background-empty' );
