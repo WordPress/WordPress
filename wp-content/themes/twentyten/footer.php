c01a25a113a8bc4d585c04333607b9a708d40975
Lance Willett <nanobar@gmail.com>
2012-10-12 16:13:37 +0000

    Twenty Ten and Eleven: remove invalid rel attribute generator from footer markup, see #22095.
    git-svn-id: http://core.svn.wordpress.org/trunk@22214 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyten/footer.php
+++ wp-content/themes/twentyten/footer.php
@@ -30,7 +30,7 @@
 
 			<div id="site-generator">
 				<?php do_action( 'twentyten_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyten' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyten' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s.', 'twentyten' ), 'WordPress' ); ?></a>
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyten' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyten' ); ?>"><?php printf( __( 'Proudly powered by %s.', 'twentyten' ), 'WordPress' ); ?></a>
 			</div><!-- #site-generator -->
 
 		</div><!-- #colophon -->
