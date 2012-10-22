7b62c8753daa484dc30ca28ccc614b635c4e9bee
Ryan Boren <ryan@boren.nu>
2012-10-22 12:44:28 +0000

    Fix typo in phpdoc. Props SergeyBiryukov. fixes #20429
    git-svn-id: http://core.svn.wordpress.org/trunk@22266 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-activate.php
+++ wp-activate.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Confirms that the activation key that is sent in an email after a user signs
- * up for a new blog matchs the key for that user and then displays confirmation.
+ * up for a new blog matches the key for that user and then displays confirmation.
  * 
  * @package WordPress
  */
