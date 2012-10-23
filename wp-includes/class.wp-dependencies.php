02d584b4c7928bb4b0c79cff214debe60b552b5d
Ryan Boren <ryan@boren.nu>
2012-10-23 20:32:12 +0000

    Revert [21420] and [21481]. Accepting a string caused back compat problems including the possibility of revealing previously hidden circular dependencies resulting in infinite loops.
    fixes #20683 #22111
    see #21520
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@22286 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class.wp-dependencies.php
+++ wp-includes/class.wp-dependencies.php
@@ -247,10 +247,8 @@ class _WP_Dependency {
 	var $extra = array();
 
 	function __construct() {
-		@list($this->handle, $this->src, $this->deps, $this->ver, $this->args) = func_get_args();
-		if ( is_string( $this->deps ) && ! empty( $this->deps ) )
-			$this->deps = array( $this->deps );
-		elseif ( !is_array( $this->deps ) )
+		@list( $this->handle, $this->src, $this->deps, $this->ver, $this->args ) = func_get_args();
+		if ( ! is_array($this->deps) )
 			$this->deps = array();
 	}
 
