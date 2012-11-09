8f08f77601d8bb4e22bc3da3d7c3ca81c9e4a084
Andrew Nacin <wp@andrewnacin.com>
2012-11-09 22:36:35 +0000

    WordPress dialog jQuery UI widget:  * Link to the upstream bug report.  * No need to specify the default value for an option.  * Use the new _super() UI method to open the dialog, avoiding a direct reference to the parent.  * Fix typo in comment.  * Use public isOpen() method rather than a private property.
    props scott.gonzalez.
    fixes #22399.
    
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@22514 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/tinymce/plugins/wpdialogs/js/wpdialog.js
+++ wp-includes/js/tinymce/plugins/wpdialogs/js/wpdialog.js
@@ -1,26 +1,25 @@
 (function($){
 	$.ui.dialog.prototype.options.closeOnEscape = false;
-	$.widget("wp.wpdialog", $.ui.dialog, {
-		options: {
-			closeOnEscape: false
-		},
+	$.widget('wp.wpdialog', $.ui.dialog, {
+		// Work around a bug in jQuery UI 1.9.1.
+		// http://bugs.jqueryui.com/ticket/8805
 		widgetEventPrefix: 'wpdialog',
 
 		open: function() {
 			var ed;
 
-			// Initialize tinyMCEPopup if it exists and is the editor is active.
+			// Initialize tinyMCEPopup if it exists and the editor is active.
 			if ( tinyMCEPopup && typeof tinyMCE != 'undefined' && ( ed = tinyMCE.activeEditor ) && !ed.isHidden() ) {
 				tinyMCEPopup.init();
 			}
 
 			// Add beforeOpen event.
-			if ( this._isOpen || false === this._trigger('beforeOpen') ) {
+			if ( this.isOpen() || false === this._trigger('beforeOpen') ) {
 				return;
 			}
 
 			// Open the dialog.
-			$.ui.dialog.prototype.open.apply( this, arguments );
+			this._super();
 			// WebKit leaves focus in the TinyMCE editor unless we shift focus.
 			this.element.focus();
 			this._trigger('refresh');
