29d5523b23813a4155ed08b2e55980f69d2f4149
Lance Willett <nanobar@gmail.com>
2012-11-15 00:43:43 +0000

    Twenty Twelve: better closed comments notes. Check only for "comment" type comments when deciding whether to show the message.
    git-svn-id: http://core.svn.wordpress.org/trunk@22587 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/comments.php
+++ wp-content/themes/twentytwelve/comments.php
@@ -45,11 +45,16 @@ if ( post_password_required() )
 		</nav>
 		<?php endif; // check for comment navigation ?>
 
-	<?php // If comments are closed and there are comments, let's leave a little note.
-		elseif ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="nocomments"><?php _e( 'Comments are closed.', 'twentytwelve' ); ?></p>
-	<?php endif; ?>
+		<?php
+		/* If there are no comments and comments are closed, let's leave a note.
+		 * But we only want the note on posts and pages that had comments in the first place.
+		 */
+		if ( ! comments_open() && ! empty( $comments_by_type['comment'] ) ) : ?>
+		<!-- If comments are closed. -->
+		<p class="nocomments"><?php _e( 'Comments are closed.' , 'twentytwelve' ); ?></p>
+		<?php endif; ?>
+
+	<?php endif; // have_comments() ?>
 
 	<?php comment_form(); ?>
 
