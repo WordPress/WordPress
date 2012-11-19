f5e7edf2f2dc164424b8d94c5468d43cc8f98058
Andrew Nacin <wp@andrewnacin.com>
2012-11-19 06:11:42 +0000

    Add manual new lines to a string, as double quotes with \n is not optimal due to sprintf-parsing reasons. props SergeyBiryukov, fixes #22486. see #22260.
    git-svn-id: http://core.svn.wordpress.org/trunk@22664 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/network/site-new.php
+++ wp-admin/network/site-new.php
@@ -85,7 +85,10 @@ if ( isset($_REQUEST['action']) && 'add-site' == $_REQUEST['action'] ) {
 	if ( !is_wp_error( $id ) ) {
 		if ( !is_super_admin( $user_id ) && !get_user_option( 'primary_blog', $user_id ) )
 			update_user_option( $user_id, 'primary_blog', $id, true );
-		$content_mail = sprintf( __( "New site created by %1$s\n\nAddress: %2$s\nName: %3$s"), $current_user->user_login , get_site_url( $id ), stripslashes( $title ) );
+		$content_mail = sprintf( __( 'New site created by %1$s
+
+Address: %2$s
+Name: %3$s' ), $current_user->user_login , get_site_url( $id ), stripslashes( $title ) );
 		wp_mail( get_site_option('admin_email'), sprintf( __( '[%s] New Site Created' ), $current_site->site_name ), $content_mail, 'From: "Site Admin" <' . get_site_option( 'admin_email' ) . '>' );
 		wpmu_welcome_notification( $id, $user_id, $password, $title, array( 'public' => 1 ) );
 		wp_redirect( add_query_arg( array( 'update' => 'added', 'id' => $id ), 'site-new.php' ) );
