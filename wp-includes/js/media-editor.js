c95bd0198a348395d60f0820f10da13372f40720
Daryl Koopersmith <dkoopersmith@gmail.com>
2012-11-21 16:50:13 +0000

    Media: Remove dead thickbox code from media-editor.js. see #21390.
    git-svn-id: http://core.svn.wordpress.org/trunk@22772 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/media-editor.js
+++ wp-includes/js/media-editor.js
@@ -45,48 +45,6 @@ function send_to_editor(h) {
 	try{tb_remove();}catch(e){};
 }
 
-// thickbox settings
-var tb_position;
-(function($) {
-	tb_position = function() {
-		var tbWindow = $('#TB_window'), width = $(window).width(), H = $(window).height(), W = ( 720 < width ) ? 720 : width, adminbar_height = 0;
-
-		if ( $('body.admin-bar').length )
-			adminbar_height = 28;
-
-		if ( tbWindow.size() ) {
-			tbWindow.width( W - 50 ).height( H - 45 - adminbar_height );
-			$('#TB_iframeContent').width( W - 50 ).height( H - 75 - adminbar_height );
-			tbWindow.css({'margin-left': '-' + parseInt((( W - 50 ) / 2),10) + 'px'});
-			if ( typeof document.body.style.maxWidth != 'undefined' )
-				tbWindow.css({'top': 20 + adminbar_height + 'px','margin-top':'0'});
-		};
-
-		return $('a.thickbox').each( function() {
-			var href = $(this).attr('href');
-			if ( ! href ) return;
-			href = href.replace(/&width=[0-9]+/g, '');
-			href = href.replace(/&height=[0-9]+/g, '');
-			$(this).attr( 'href', href + '&width=' + ( W - 80 ) + '&height=' + ( H - 85 - adminbar_height ) );
-		});
-	};
-
-	$(window).resize(function(){ tb_position(); });
-
-	// store caret position in IE
-	$(document).ready(function($){
-		$('a.thickbox').click(function(){
-			var ed;
-
-			if ( typeof(tinymce) != 'undefined' && tinymce.isIE && ( ed = tinymce.get(wpActiveEditor) ) && !ed.isHidden() ) {
-				ed.focus();
-				ed.windowManager.insertimagebookmark = ed.selection.getBookmark();
-			}
-		});
-	});
-
-})(jQuery);
-
 // WordPress, TinyMCE, and Media
 // -----------------------------
 (function($){
