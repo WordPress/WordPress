219716e1017d1e2cb2eaf0688bee2eb9e01fedb8
Lance Willett <nanobar@gmail.com>
2012-12-03 20:12:22 +0000

    Twenty Twelve: escape navigation ID output, props nacin. See #22690.
    git-svn-id: http://core.svn.wordpress.org/trunk@22999 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/functions.php
+++ wp-content/themes/twentytwelve/functions.php
@@ -240,15 +240,17 @@ if ( ! function_exists( 'twentytwelve_content_nav' ) ) :
  *
  * @since Twenty Twelve 1.0
  */
-function twentytwelve_content_nav( $nav_id ) {
+function twentytwelve_content_nav( $html_id ) {
 	global $wp_query;
 
+	$html_id = esc_attr( $html_id );
+
 	if ( $wp_query->max_num_pages > 1 ) : ?>
-		<nav id="<?php echo $nav_id; ?>" class="navigation" role="navigation">
+		<nav id="<?php echo $html_id; ?>" class="navigation" role="navigation">
 			<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
 			<div class="nav-previous alignleft"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentytwelve' ) ); ?></div>
 			<div class="nav-next alignright"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?></div>
-		</nav><!-- #<?php echo $nav_id; ?> .navigation -->
+		</nav><!-- #<?php echo $html_id; ?> .navigation -->
 	<?php endif;
 }
 endif;
