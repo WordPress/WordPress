30cf06406841eb65d6257113abc7e8560f5db764
Bot (Assets) <nobody@localhost>
2012-12-04 01:29:58 +0000

    Compress scripts/styles: 3.5-RC2-23007. props koopersmith. :-)
    git-svn-id: http://core.svn.wordpress.org/trunk@23007 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.5-RC2-23003';
+$wp_version = '3.5-RC2-23007';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
