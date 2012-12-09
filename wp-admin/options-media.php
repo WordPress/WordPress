60be66457ace616df57f19480eeb89a6510f058b
Andrew Nacin <wp@andrewnacin.com>
2012-12-09 17:04:08 +0000

    Only show help for upload_path and upload_url_path if those fields are visible. fixes #21720.
    git-svn-id: http://core.svn.wordpress.org/trunk@23134 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options-media.php
+++ wp-admin/options-media.php
@@ -17,7 +17,7 @@ $parent_file = 'options-general.php';
 
 $media_options_help = '<p>' . __('You can set maximum sizes for images inserted into your written content; you can also insert an image as Full Size.') . '</p>';
 
-if ( ! is_multisite() ) {
+if ( ! is_multisite() && ( get_option('upload_url_path') || ( get_option('upload_path') != 'wp-content/uploads' && get_option('upload_path') ) ) ) {
 	$media_options_help .= '<p>' . __('Uploading Files allows you to choose the folder and path for storing your uploaded files.') . '</p>';
 }
 
