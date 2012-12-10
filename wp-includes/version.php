df1b69a6d993b0e1839bf849fe58523e625d9360
Andrew Nacin <wp@andrewnacin.com>
2012-12-10 23:32:04 +0000

    3.5-RC6-23166
    git-svn-id: http://core.svn.wordpress.org/branches/3.5@23166 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.5-RC6';
+$wp_version = '3.5-RC6-23166';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
