69c189e9ee7afe9cc31d65f31cf96121744a2b26
Andrew Nacin <wp@andrewnacin.com>
2012-12-28 04:40:46 +0000

    Switch to a string already available in the wordpress.pot, via the XML-RPC server class. The other string is only available in the wordpress-admin.pot.
    Prevents any string movements in the 3.5 branch. see #22858.
    
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.5@23225 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -1453,7 +1453,7 @@ function wp_update_user($userdata) {
 	// First, get all of the original fields
 	$user_obj = get_userdata( $ID );
 	if ( ! $user_obj )
-		return new WP_Error( 'invalid_user_id', __( 'Invalid user ID.' ) );
+		return new WP_Error( 'invalid_user_id', __( 'Invalid user ID' ) );
 
 	$user = $user_obj->to_array();
 
