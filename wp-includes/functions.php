57f65d2906da3887c7840bdd7afc3e1ef314a3cf
Ryan Boren <ryan@boren.nu>
2013-01-04 19:51:31 +0000

    Try not to send Last-Modified, even with an empty value. Some servers interpret an empty value as the epoch.
    Props nacin, slene, SergeyBiryukov, andy
    see #23021 for trunk
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@23267 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -902,7 +902,6 @@ function status_header( $header ) {
 function wp_get_nocache_headers() {
 	$headers = array(
 		'Expires' => 'Wed, 11 Jan 1984 05:00:00 GMT',
-		'Last-Modified' => '',
 		'Cache-Control' => 'no-cache, must-revalidate, max-age=0',
 		'Pragma' => 'no-cache',
 	);
@@ -910,6 +909,7 @@ function wp_get_nocache_headers() {
 	if ( function_exists('apply_filters') ) {
 		$headers = (array) apply_filters('nocache_headers', $headers);
 	}
+	$headers['Last-Modified'] = false;
 	return $headers;
 }
 
@@ -924,10 +924,25 @@ function wp_get_nocache_headers() {
  */
 function nocache_headers() {
 	$headers = wp_get_nocache_headers();
+
+	unset( $headers['Last-Modified'] );
+
+	// In PHP 5.3+, make sure we are not sending a Last-Modified header.
+	if ( function_exists( 'header_remove' ) ) {
+		@header_remove( 'Last-Modified' );
+	} else {
+		// In PHP 5.2, send an empty Last-Modified header, but only as a
+		// last resort to override a header already sent. #WP23021
+		foreach ( headers_list() as $header ) {
+			if ( 0 === stripos( $header, 'Last-Modified' ) ) {
+				$headers['Last-Modified'] = '';
+				break;
+			}
+		}
+	}
+
 	foreach( $headers as $name => $field_value )
 		@header("{$name}: {$field_value}");
-	if ( empty( $headers['Last-Modified'] ) && function_exists( 'header_remove' ) )
-		@header_remove( 'Last-Modified' );
 }
 
 /**
