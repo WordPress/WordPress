ea8265b2a97de48f3a33740753fa3561d39c656a
Ryan Boren <ryan@boren.nu>
2013-01-21 14:39:39 +0000

    Whitespace cleanup.
    Props dimadin
    fixes #23126
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@23308 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-constants.php
+++ wp-includes/default-constants.php
@@ -75,11 +75,11 @@ function wp_initial_constants() {
 
 	// Constants for expressing human-readable intervals
 	// in their respective number of seconds.
- 	define( 'MINUTE_IN_SECONDS', 60 );
- 	define( 'HOUR_IN_SECONDS',   60 * MINUTE_IN_SECONDS );
- 	define( 'DAY_IN_SECONDS',    24 * HOUR_IN_SECONDS   );
+	define( 'MINUTE_IN_SECONDS', 60 );
+	define( 'HOUR_IN_SECONDS',   60 * MINUTE_IN_SECONDS );
+	define( 'DAY_IN_SECONDS',    24 * HOUR_IN_SECONDS   );
 	define( 'WEEK_IN_SECONDS',    7 * DAY_IN_SECONDS    );
- 	define( 'YEAR_IN_SECONDS',  365 * DAY_IN_SECONDS    );
+	define( 'YEAR_IN_SECONDS',  365 * DAY_IN_SECONDS    );
 }
 
 /**
