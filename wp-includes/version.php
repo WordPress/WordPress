31b8240f6b4bfa45b8ee5556cb6b9524d937decf
Bot (Assets) <nobody@localhost>
2013-01-22 22:57:24 +0000

    Compress scripts/styles: 3.6-alpha-23334. TinyMCE updated.
    git-svn-id: http://core.svn.wordpress.org/trunk@23334 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.6-alpha-23288';
+$wp_version = '3.6-alpha-23334';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 22441;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '358-23223';
+$tinymce_version = '358-23334';
 
 /**
  * Holds the required PHP version
