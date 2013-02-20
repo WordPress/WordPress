6263d728ecb8fb6e0c3f7b5fdffc072f4d1002e9
Lance Willett <nanobar@gmail.com>
2013-02-20 04:32:31 +0000

    Twenty Thirteen: proper alignment for featured images on pages with a sidebar. Props kwight, fixes #23527.
    git-svn-id: http://core.svn.wordpress.org/trunk@23460 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentythirteen/page.php
+++ wp-content/themes/twentythirteen/page.php
@@ -20,15 +20,15 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
-					<div class="entry-thumbnail">
-						<?php the_post_thumbnail(); ?>
-					</div>
-					<?php endif; ?>
-
 					<header class="entry-header">
+						<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+						<div class="entry-thumbnail">
+							<?php the_post_thumbnail(); ?>
+						</div>
+						<?php endif; ?>
+
 						<h1 class="entry-title"><?php the_title(); ?></h1>
-					</header>
+					</header><!-- .entry-header -->
 
 					<div class="entry-content">
 						<?php the_content(); ?>
