ba90b8bb63051cf66878959ef21356fe1dd51fdf
Lance Willett <nanobar@gmail.com>
2013-02-21 05:54:19 +0000

    Twenty Thirteen: show featured image on single posts, fixes #23528. Props kwight.
    git-svn-id: http://core.svn.wordpress.org/trunk@23463 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentythirteen/content.php
+++ wp-content/themes/twentythirteen/content.php
@@ -10,7 +10,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php if ( has_post_thumbnail() && ! is_single() && ! post_password_required() ) : ?>
+		<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
 		<div class="entry-thumbnail">
 			<?php the_post_thumbnail(); ?>
 		</div>
