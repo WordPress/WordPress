e0fac43f8d4c16b6e981ce5e4ab4d64dd9480d1f
Lance Willett <nanobar@gmail.com>
2013-02-22 16:07:28 +0000

    Twenty Twelve: show featured image in single page views. Fixes #22943, props MikeHansenMe.
    git-svn-id: http://core.svn.wordpress.org/trunk@23471 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/content-page.php
+++ wp-content/themes/twentytwelve/content-page.php
@@ -10,6 +10,7 @@
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
+			<?php the_post_thumbnail(); ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 		</header>
 
