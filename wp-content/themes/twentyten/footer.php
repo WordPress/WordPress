27a5ff73bdf19860971c2d70c87059e859b8b654
Jrmlstf <jmaelstaf@gmail.com>
2013-02-25 23:24:32 +0100

    Update wp-content/themes/twentyten/footer.php
    Fixed a tipo

--- wp-content/themes/twentyten/footer.php
+++ wp-content/themes/twentyten/footer.php
@@ -16,7 +16,7 @@
 		<div id="colophon">
 
 <?php
-	/* A sidebar in the footer? Yep. You can can customize
+	/* A sidebar in the footer? Yep. You can customize
 	 * your footer with four columns of widgets.
 	 */
 	get_sidebar( 'footer' );
