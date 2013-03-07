af53edb6967965e3d3f1ab1630708794663514c6
Andrew Nacin <wp@andrewnacin.com>
2013-03-07 06:57:56 +0000

    Revert [23411] until encoding differences are worked out. see #20771.
    git-svn-id: http://core.svn.wordpress.org/trunk@23637 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -1175,7 +1175,7 @@ function is_blog_installed() {
  */
 function wp_nonce_url( $actionurl, $action = -1, $name = '_wpnonce' ) {
 	$actionurl = str_replace( '&amp;', '&', $actionurl );
-	return esc_url( add_query_arg( $name, wp_create_nonce( $action ), $actionurl ) );
+	return esc_html( add_query_arg( $name, wp_create_nonce( $action ), $actionurl ) );
 }
 
 /**
