b1d55a57968f6d2bdeccdbde272e8dc318f46a05
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2013-03-09 03:18:26 +0000

    Update Network Dashboard help text. props DrewAPicture, raggedrobins. see #23163.
    git-svn-id: http://core.svn.wordpress.org/trunk@23657 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/network/index.php
+++ wp-admin/network/index.php
@@ -22,14 +22,30 @@ if ( ! current_user_can( 'manage_network' ) )
 $title = __( 'Dashboard' );
 $parent_file = 'index.php';
 
-	get_current_screen()->add_help_tab( array(
-		'id'      => 'overview',
-		'title'   => __('Overview'),
-		'content' =>
-			'<p>' . __('Until WordPress 3.0, running multiple sites required using WordPress MU instead of regular WordPress. In version 3.0, these applications have merged. If you are a former MU user, you should be aware of the following changes:') . '</p>' .
-			'<ul><li>' . __('Site Admin is now Super Admin (we highly encourage you to get yourself a cape!).') . '</li>' .
-			'<li>' . __('Blogs are now called Sites; Site is now called Network.') . '</li></ul>' .
-			'<p>' . __('The Right Now box provides the network administrator with links to the screens to either create a new site or user, or to search existing users and sites. Screens for Sites and Users are also accessible through the left-hand navigation in the Network Admin section.') . '</p>'
+$overview = '<p>' . __( 'Welcome to your Network Admin. This area of the Administration Screens is used for managing all aspects of your Multisite Network.' ) . '</p>';
+$overview .= '<p>' . __( 'From here you can:' ) . '</p>';
+$overview .= '<ul><li>' . __( 'Add and manage sites or users' ) . '</li>';
+$overview .= '<li>' . __( 'Install and activate themes or plugins' ) . '</li>';
+$overview .= '<li>' . __( 'Update your network' ) . '</li>';
+$overview .= '<li>' . __( 'Modify global network settings' ) . '</li></ul>';
+
+get_current_screen()->add_help_tab( array(
+	'id'      => 'overview',
+	'title'   => __( 'Overview' ),
+	'content' => $overview
+) );
+
+$quick_tasks = '<p>' . __( 'The Right Now widget on this screen provides current user and site counts on your network.' ) . '</p>';
+$quick_tasks .= '<ul><li>' . __( 'To add a new user <strong>click Create a New User</strong>' ) . '</li>';
+$quick_tasks .= '<li>' . __( 'To add a new site <strong>click Create a New Site</strong>' ) . '</li></ul>';
+$quick_tasks .= '<p>' . __( 'To search for a user or site, use the search boxes.' ) . '</p>';
+$quick_tasks .= '<ul><li>' . __( '<strong>To search for a user</strong> enter all or part of a username or email address' ) . '</li>';
+$quick_tasks .= '<li>' . __( '<strong>To search for a site</strong> enter the path or domain' ) . '</li></ul>';
+
+get_current_screen()->add_help_tab( array(
+	'id'      => 'quick-tasks',
+	'title'   => __( 'Quick Tasks' ),
+	'content' => $quick_tasks
 ) );
 
 get_current_screen()->set_help_sidebar(
