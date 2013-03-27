bb2b6fd418201b8f76ad1d8e91980b22dfad8f96
Lance Willett <nanobar@gmail.com>
2013-03-27 22:08:16 +0000

    Twenty Thirteen: add support and styling for Audio structured post format. Moves audio content out from the main post content, using `the_audio()` and `the_extra_content()`. Props obenland, fixes #23871.
    git-svn-id: http://core.svn.wordpress.org/trunk@23828 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentythirteen/content-audio.php
+++ wp-content/themes/twentythirteen/content-audio.php
@@ -19,11 +19,15 @@
 		<?php endif; // is_single() ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-media">
 		<div class="audio-content">
-			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+			<?php the_audio(); ?>
 		</div><!-- .audio-content -->
+	</div><!-- .entry-media -->
+
+	<div class="entry-content">
+		<?php the_extra_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-meta">
