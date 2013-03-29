a1ef45a3ec5bedd1a114dcd2d66822572cd00fe8
Mark Jaquith <mark@wordpress.org>
2013-03-29 07:50:40 +0000

    Remove duplicated code that was accidentally committed in [23417].
    fixes #23889. props DrewAPicture.
    
    git-svn-id: http://core.svn.wordpress.org/trunk@23855 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/accordion.js
+++ wp-admin/js/accordion.js
@@ -15,37 +15,3 @@ jQuery(document).ready( function($) {
 		event.preventDefault();
 	});
 });
-jQuery(document).ready( function($) {
-	// Expand/Collapse
-	$('.accordion-section-title').on('click keydown', function( event ) {
-
-		if ( event.type === 'keydown' &&  13 !== event.which ) // enter
-				return;
-
-		var clicked = $( this ).closest( '.accordion-section' );
-
-		if ( clicked.hasClass('cannot-expand') )
-			return;
-
-		clicked.closest( '.accordion-container' ).find( '.accordion-section' ).not( clicked ).removeClass( 'open' );
-		clicked.toggleClass( 'open' );
-		event.preventDefault();
-	});
-});
-jQuery(document).ready( function($) {
-	// Expand/Collapse
-	$('.accordion-section-title').on('click keydown', function( event ) {
-
-		if ( event.type === 'keydown' &&  13 !== event.which ) // enter
-				return;
-
-		var clicked = $( this ).closest( '.accordion-section' );
-
-		if ( clicked.hasClass('cannot-expand') )
-			return;
-
-		clicked.closest( '.accordion-container' ).find( '.accordion-section' ).not( clicked ).removeClass( 'open' );
-		clicked.toggleClass( 'open' );
-		event.preventDefault();
-	});
-});
