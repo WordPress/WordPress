bbed923f5a2592f93044e71799d670968b8d979c
Bot (Assets) <nobody@localhost>
2013-04-05 00:58:45 +0000

    Compress scripts/styles: 3.6-beta1-23917.
    git-svn-id: http://core.svn.wordpress.org/trunk@23917 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.6-beta1-23913';
+$wp_version = '3.6-beta1-23917';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
