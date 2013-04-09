63e67b414b89bdbab80652573062c92beb7a4c6a
Ryan Boren <ryan@boren.nu>
2013-04-09 22:43:28 +0000

    Allow turning off the post formats UI via a show_post_format_ui filter.
    Props johnbillion
    see #23929
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@23946 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-form-advanced.php
+++ wp-admin/edit-form-advanced.php
@@ -128,7 +128,7 @@ foreach ( get_object_taxonomies( $post ) as $tax_name ) {
 
 // post format
 $format_class = '';
-if ( post_type_supports( $post_type, 'post-formats' ) ) {
+if ( post_type_supports( $post_type, 'post-formats' ) && apply_filters( 'show_post_format_ui', true, $post ) ) {
 	wp_enqueue_script( 'post-formats' );
 	wp_enqueue_script( 'wp-mediaelement' );
 	wp_enqueue_style( 'wp-mediaelement' );
@@ -450,7 +450,7 @@ if ( has_action( 'edit_form_after_title' ) ) {
 }
 
 // post format fields
-if ( post_type_supports( $post_type, 'post-formats' ) )
+if ( post_type_supports( $post_type, 'post-formats' ) && apply_filters( 'show_post_format_ui', true, $post ) )
 	require_once( './includes/post-formats.php' );
 
 if ( post_type_supports($post_type, 'editor') ) {
