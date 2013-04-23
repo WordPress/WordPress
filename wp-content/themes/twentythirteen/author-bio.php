4a130a9e9c0ab5dd7f4ce26779ed6722efb4c75f
Lance Willett <nanobar@gmail.com>
2013-04-23 21:29:00 +0000

    Twenty Thirteen: fix case where title in author bio was indented when avatars were disabled in Discussion settings. Props obenland, fixes #24167.
    git-svn-id: http://core.svn.wordpress.org/trunk@24073 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentythirteen/author-bio.php
+++ wp-content/themes/twentythirteen/author-bio.php
@@ -13,8 +13,8 @@
 		<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentythirteen_author_bio_avatar_size', 74 ) ); ?>
 	</div><!-- .author-avatar -->
 	<div class="author-description">
-		<h2><?php printf( __( 'About %s', 'twentythirteen' ), get_the_author() ); ?></h2>
-		<p>
+		<h2 class="author-title"><?php printf( __( 'About %s', 'twentythirteen' ), get_the_author() ); ?></h2>
+		<p class="author-bio">
 			<?php the_author_meta( 'description' ); ?>
 			<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
 				<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentythirteen' ), get_the_author() ); ?>
