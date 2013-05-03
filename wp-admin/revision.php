baab49b174cf337fefefcbf55de85fedee12fbc5
Dominik Schilling <dominikschilling+git@gmail.com>
2013-05-03 23:00:14 +0000

    Revisions UI: Add a help tab. props siobhan, DrewAPicture. see #23899.
    git-svn-id: http://core.svn.wordpress.org/trunk@24173 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/revision.php
+++ wp-admin/revision.php
@@ -87,6 +87,27 @@ $settings = array(
 
 wp_localize_script( 'revisions', 'wpRevisionsSettings', $settings );
 
+/* Revisions Help Tab */
+
+$revisions_overview  = '<p>' . __( 'This screen is used for managing your content revisions.' ) . '</p>';
+$revisions_overview .= '<p>' . __( 'Revisions are saved copies of your post or page, which are periodically created as you update your content. The red text on the left shows the content that was removed. The green text on the right shows the content that was added.' ) . '</p>';
+$revisions_overview .= '<p>' . __( 'From this screen you can review, compare, and restore revisions:' ) . '</p>';
+$revisions_overview .= '<ul><li>' . __( 'To navigate between revisions, <strong>drag the slider arrow left or right</strong> or <strong>use the Previous or Next buttons</strong>.' ) . '</li>';
+$revisions_overview .= '<li>' . __( 'Compare two different revisions by <strong>selecting the &#8220;Compare two revisions&#8221; box</strong> to the side.' ) . '</li>';
+$revisions_overview .= '<li>' . __( 'To restore a revision, <strong>click Restore This Revision</strong>.' ) . '</li></ul>';
+
+get_current_screen()->add_help_tab( array(
+	'id'      => 'revisions-overview',
+	'title'   => __( 'Overview' ),
+	'content' => $revisions_overview
+) );
+
+$revisions_sidebar  = '<p><strong>' . __( 'For more information:' ) . '</strong></p>';
+$revisions_sidebar .= '<p>' . __( '<a href="http://codex.wordpress.org/Revision_Management" target="_blank">Revisions Management</a>' ) . '</p>';
+$revisions_sidebar .= '<p>' . __( '<a href="http://wordpress.org/support/" target="_blank">Support Forums</a>' ) . '</p>';
+
+get_current_screen()->set_help_sidebar( $revisions_sidebar );
+
 require_once( './admin-header.php' );
 
 ?>
