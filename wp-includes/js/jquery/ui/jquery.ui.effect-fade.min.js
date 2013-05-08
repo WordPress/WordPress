434114d40e471861b466bb3182bbe123b69d2fbd
Dominik Schilling <dominikschilling+git@gmail.com>
2013-05-08 20:54:30 +0000

    jQuery UI 1.10.3. fixes #23370.
    git-svn-id: http://core.svn.wordpress.org/trunk@24204 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/jquery/ui/jquery.ui.effect-fade.min.js
+++ wp-includes/js/jquery/ui/jquery.ui.effect-fade.min.js
@@ -1,4 +1,4 @@
-/*! jQuery UI - v1.10.2 - 2013-03-14
+/*! jQuery UI - v1.10.3 - 2013-05-03
 * http://jqueryui.com
 * Copyright 2013 jQuery Foundation and other contributors; Licensed MIT */
 (function(t){t.effects.effect.fade=function(e,i){var s=t(this),n=t.effects.setMode(s,e.mode||"toggle");s.animate({opacity:n},{queue:!1,duration:e.duration,easing:e.easing,complete:i})}})(jQuery);
