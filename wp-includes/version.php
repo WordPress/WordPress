d47b53b8722f4d17ad93e8d62cb814bf412c893d
Mark Jaquith <mark@wordpress.org>
2013-05-10 23:57:27 +0000

    3.6-beta3
    git-svn-id: http://core.svn.wordpress.org/trunk@24244 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.6-beta2-24237';
+$wp_version = '3.6-beta3';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
