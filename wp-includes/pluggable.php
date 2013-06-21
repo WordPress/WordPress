c1acd7920e5cff08663c70ecab033d3eb873d8ac
Andrew Nacin <wp@andrewnacin.com>
2013-06-21 02:15:55 +0000

    Add strict check to wp_verify_nonce() to avoid issues when it is improperly called.
    Merges [24461] to the 3.5 branch.
    
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.5@24462 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -1257,10 +1257,10 @@ function wp_verify_nonce($nonce, $action = -1) {
 	$i = wp_nonce_tick();
 
 	// Nonce generated 0-12 hours ago
-	if ( substr(wp_hash($i . $action . $uid, 'nonce'), -12, 10) == $nonce )
+	if ( substr(wp_hash($i . $action . $uid, 'nonce'), -12, 10) === $nonce )
 		return 1;
 	// Nonce generated 12-24 hours ago
-	if ( substr(wp_hash(($i - 1) . $action . $uid, 'nonce'), -12, 10) == $nonce )
+	if ( substr(wp_hash(($i - 1) . $action . $uid, 'nonce'), -12, 10) === $nonce )
 		return 2;
 	// Invalid nonce
 	return false;
