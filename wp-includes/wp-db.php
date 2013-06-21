01b017e3b99759b6b3da2c01cb4df1e00641cfe1
Andrew Nacin <wp@andrewnacin.com>
2013-06-21 18:41:52 +0000

    3.5.2 - include [24495].
    git-svn-id: http://core.svn.wordpress.org/tags/3.5.2@24496 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -1120,7 +1120,7 @@ class wpdb {
 		$this->last_result = array();
 		$this->col_info    = null;
 		$this->last_query  = null;
-		$this->rows_affected = $this->num_rows = $this->insert_id = 0;
+		$this->rows_affected = $this->num_rows = 0;
 		$this->last_error  = '';
 
 		if ( is_resource( $this->result ) )
@@ -1302,6 +1302,7 @@ class wpdb {
 	function _insert_replace_helper( $table, $data, $format = null, $type = 'INSERT' ) {
 		if ( ! in_array( strtoupper( $type ), array( 'REPLACE', 'INSERT' ) ) )
 			return false;
+		$this->insert_id = 0;
 		$formats = $format = (array) $format;
 		$fields = array_keys( $data );
 		$formatted_fields = array();
