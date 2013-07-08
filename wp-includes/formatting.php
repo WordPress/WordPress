720f37c37477addb6ff286737a388eeb8c76e487
Andrew Nacin <wp@andrewnacin.com>
2013-07-08 13:00:34 +0000

    Expand human_time_diff() from minutes/hours/days to also include weeks/months/years. Fix off-by-one issue.
    props SergeyBiryukov, westi.
    fixes #9272.
    
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@24582 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -2133,27 +2133,42 @@ function sanitize_email( $email ) {
 function human_time_diff( $from, $to = '' ) {
 	if ( empty( $to ) )
 		$to = time();
+
 	$diff = (int) abs( $to - $from );
-	if ( $diff <= HOUR_IN_SECONDS ) {
+
+	if ( $diff < HOUR_IN_SECONDS ) {
 		$mins = round( $diff / MINUTE_IN_SECONDS );
-		if ( $mins <= 1 ) {
+		if ( $mins <= 1 )
 			$mins = 1;
-		}
 		/* translators: min=minute */
 		$since = sprintf( _n( '%s min', '%s mins', $mins ), $mins );
-	} elseif ( ( $diff <= DAY_IN_SECONDS ) && ( $diff > HOUR_IN_SECONDS ) ) {
+	} elseif ( $diff < DAY_IN_SECONDS && $diff >= HOUR_IN_SECONDS ) {
 		$hours = round( $diff / HOUR_IN_SECONDS );
-		if ( $hours <= 1 ) {
+		if ( $hours <= 1 )
 			$hours = 1;
-		}
 		$since = sprintf( _n( '%s hour', '%s hours', $hours ), $hours );
-	} elseif ( $diff >= DAY_IN_SECONDS ) {
+	} elseif ( $diff < WEEK_IN_SECONDS && $diff >= DAY_IN_SECONDS ) {
 		$days = round( $diff / DAY_IN_SECONDS );
-		if ( $days <= 1 ) {
+		if ( $days <= 1 )
 			$days = 1;
-		}
 		$since = sprintf( _n( '%s day', '%s days', $days ), $days );
+	} elseif ( $diff < 30 * DAY_IN_SECONDS && $diff >= WEEK_IN_SECONDS ) {
+		$weeks = round( $diff / WEEK_IN_SECONDS );
+		if ( $weeks <= 1 )
+			$weeks = 1;
+		$since = sprintf( _n( '%s week', '%s weeks', $weeks ), $weeks );
+	} elseif ( $diff < YEAR_IN_SECONDS && $diff >= 30 * DAY_IN_SECONDS ) {
+		$months = round( $diff / ( 30 * DAY_IN_SECONDS ) );
+		if ( $months <= 1 )
+			$months = 1;
+		$since = sprintf( _n( '%s month', '%s months', $months ), $months );
+	} elseif ( $diff >= YEAR_IN_SECONDS ) {
+		$years = round( $diff / YEAR_IN_SECONDS );
+		if ( $years <= 1 )
+			$years = 1;
+		$since = sprintf( _n( '%s year', '%s years', $years ), $years );
 	}
+
 	return $since;
 }
 
