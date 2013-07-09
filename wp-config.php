7c87262f9ca0e290261c617623d6c02d16e8979e
timani <timani27@gmail.com>
2013-07-09 15:36:33 -0700

    Remove 'G' from output and uploads directory plus the local config conditional

--- wp-config.php
+++ wp-config.php
@@ -1,103 +1,62 @@
-G<?php
-// Don't show deprecations
-error_reporting(E_ALL ^ E_DEPRECATED);
+<?php
 
-/**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
+if (isset($_SERVER['PANTHEON_ENVIRONMENT'])) {
+  // Tweak #1
+  // Load database settings from PRESSFLOW_SETTINGS environment variable...
+  $pressflow_settings = json_decode($_SERVER['PRESSFLOW_SETTINGS'], TRUE);
+  $database_settings = $pressflow_settings['databases']['default']['default'];
+  $wp_upload_url = "sites/default/files";
 
-// Load database settings from PRESSFLOW_SETTINGS environment variable...
-$pressflow_settings = json_decode($_SERVER['PRESSFLOW_SETTINGS'], TRUE);
+  /** MySQL configs */
+  define('DB_NAME', $database_settings['database']);
+  define('DB_USER', $database_settings['username']);
+  define('DB_PASSWORD', $database_settings['password']);
+  define('DB_HOST', $database_settings['host'] . ":" . $database_settings['port']);
+  define('DB_CHARSET', 'utf8');
+  define('DB_COLLATE', '');
 
-define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST']);
-define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST']);
+  // Tweak #2
+  // Sneak in wordpress cookies as Drupal cookies (change prefix from wordpress_ to SESS)
+  $salt = md5($_SERVER['PRESSFLOW_SETTINGS']);
+  define('USER_COOKIE', 'SESSuser' . $salt);
+  define('PASS_COOKIE', 'SESSpass' . $salt);
+  define('AUTH_COOKIE', 'SESSauth' . $salt);
+  define('SECURE_AUTH_COOKIE', 'SESSsecure' . $salt);
+  define('LOGGED_IN_COOKIE', 'SESSloggedin' . $salt);
+  define('TEST_COOKIE', 'SESStest' . $salt);
+  
+  // Tweak #3
+  // Other constants that need to be dynamic for pantheon */
+  define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST']);
+  define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST']);
+  define('UPLOADS', $wp_upload_url);  // @TODO Set this to be the binding path
+} else {
+  //These setting will be used on environments other than Pantheon (ie, your localhost)
+  define('DB_NAME', 'local_db_database');
+  /** MySQL database username */
+  define('DB_USER', 'local_db_username');
+  /** MySQL database password */
+  define('DB_PASSWORD', 'local_db_password');
+  /** MySQL hostname */
+  define('DB_HOST', 'localhost');
+  /** Database Charset to use in creating database tables. */
+  define('DB_CHARSET', 'utf8');
+  /** The Database Collate type. Don't change this if in doubt. */
+  define('DB_COLLATE', '');
+}
 
-$database_settings = $pressflow_settings['databases']['default']['default'];
-
-/** MySQL configs */
-define('DB_NAME', $database_settings['database']);
-define('DB_USER', $database_settings['username']);
-define('DB_PASSWORD', $database_settings['password']);
-define('DB_HOST', $database_settings['host'].":".$database_settings['port']);
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
+// Standard wp-config.php from here on down.
+define('AUTH_KEY', 'put your unique phrase here');
+define('SECURE_AUTH_KEY', 'put your unique phrase here');
+define('LOGGED_IN_KEY', 'put your unique phrase here');
+define('NONCE_KEY', 'put your unique phrase here');
+define('AUTH_SALT', 'put your unique phrase here');
 define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
-
-// Sneak in wordpress cookies as Drupal cookies (change prefix from wordpress_ to SESS)
-$salt = md5($_SERVER['PRESSFLOW_SETTINGS']);
-define('USER_COOKIE', 'SESSuser' . $salt);
-define('PASS_COOKIE', 'SESSpass' . $salt);
-define('AUTH_COOKIE', 'SESSauth' . $salt);
-define('SECURE_AUTH_COOKIE', 'SESSsecure' . $salt);
-define('LOGGED_IN_COOKIE', 'SESSloggedin' . $salt);
-define('TEST_COOKIE', 'SESStest' . $salt);
-
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix  = 'wp_';
-
-/**
- * WordPress Localized Language, defaults to English.
- *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
- */
+define('LOGGED_IN_SALT', 'put your unique phrase here');
+define('NONCE_SALT', 'put your unique phrase here');
+$table_prefix = 'wp_';
 define('WPLANG', '');
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
 define('WP_DEBUG', false);
-
-/* That's all, stop editing! Happy blogging. */
-
-/** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-    define('ABSPATH', dirname(__FILE__) . '/');
-
-/** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+if (!defined('ABSPATH'))
+	define('ABSPATH', dirname(__FILE__) . '/');
+require_once (ABSPATH . 'wp-settings.php');
