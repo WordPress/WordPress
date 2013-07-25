f17fdf58162a15118bdce4c9aa0a8cb127e97a3c
timani <timani27@gmail.com>
2013-07-24 22:19:05 -0700

    Adjusting the spacing

--- wp-content/db-error.php
+++ wp-content/db-error.php
@@ -1,5 +1,6 @@
-<?php // custom WordPress database error page to handle reaping on Pantheon
-	header('HTTP/1.1 550 Database connection error');
-	header('Status: 550 Database connection error');
-	header('Retry-After: 3600'); // 1 hour = 3600 seconds
-	//mail("email@domain.tld", "Database Error", "Database connection error", "From: Website");
+<?php
+// custom WordPress database error page to handle reaping on Pantheon
+header('HTTP/1.1 550 Database connection error');
+header('Status: 550 Database connection error');
+header('Retry-After: 3600'); // 1 hour = 3600 seconds
+//mail("email@domain.tld", "Database Error", "Database connection error", "From: Website");
