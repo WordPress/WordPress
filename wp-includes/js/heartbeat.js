19fb0f1153586d49821f2ab940973a680381eefe
Andrew Ozz <admin@laptoptips.ca>
2013-07-28 20:54:48 +0000

    Add "experimental" to heartbeat phpdoc, fixes #24855 for trunk.
    git-svn-id: http://core.svn.wordpress.org/trunk@24818 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/heartbeat.js
+++ wp-includes/js/heartbeat.js
@@ -1,6 +1,10 @@
 /**
  * Heartbeat API
  *
+ * Note: this API is "experimental" meaning it will likely change a lot
+ * in the next few releases based on feedback from 3.6.0. If you intend
+ * to use it, please follow the development closely.
+ *
  * Heartbeat is a simple server polling API that sends XHR requests to
  * the server every 15 seconds and triggers events (or callbacks) upon
  * receiving data. Currently these 'ticks' handle transports for post locking,
@@ -47,7 +51,6 @@ window.wp = window.wp || {};
 		 * Returns a boolean that's indicative of whether or not there is a connection error
 		 *
 		 * @returns boolean
-		 * @private
 		 */
 		this.hasConnectionError = function() {
 			return hasConnectionError;
