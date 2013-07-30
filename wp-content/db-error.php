5003a35834199b1a82eecd086edde78682f7a8f2
timani <timani27@gmail.com>
2013-07-29 17:14:12 -0700

    Removing the retry header for reaping

--- wp-content/db-error.php
+++ wp-content/db-error.php
@@ -2,5 +2,3 @@
 // custom WordPress database error page to handle reaping on Pantheon
 header('HTTP/1.1 550 Database connection error');
 header('Status: 550 Database connection error');
-header('Retry-After: 3600'); // 1 hour = 3600 seconds
-//mail("email@domain.tld", "Database Error", "Database connection error", "From: Website");
