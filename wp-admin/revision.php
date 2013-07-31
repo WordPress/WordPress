bd60a7c1aca0b0c9100afe3ea4aa71abe0f4d01c
Andrew Nacin <wp@andrewnacin.com>
2013-07-31 14:31:54 +0000

    Update help text for [24860]. props ocean90. see #24804. for 3.6.
    git-svn-id: http://core.svn.wordpress.org/branches/3.6@24921 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/revision.php
+++ wp-admin/revision.php
@@ -95,7 +95,7 @@ $revisions_overview  = '<p>' . __( 'This screen is used for managing your conten
 $revisions_overview .= '<p>' . __( 'Revisions are saved copies of your post or page, which are periodically created as you update your content. The red text on the left shows the content that was removed. The green text on the right shows the content that was added.' ) . '</p>';
 $revisions_overview .= '<p>' . __( 'From this screen you can review, compare, and restore revisions:' ) . '</p>';
 $revisions_overview .= '<ul><li>' . __( 'To navigate between revisions, <strong>drag the slider handle left or right</strong> or <strong>use the Previous or Next buttons</strong>.' ) . '</li>';
-$revisions_overview .= '<li>' . __( 'Compare two different revisions by <strong>selecting the &#8220;Compare two revisions&#8221; box</strong> to the side.' ) . '</li>';
+$revisions_overview .= '<li>' . __( 'Compare two different revisions by <strong>selecting the &#8220;Compare any two revisions&#8221; box</strong> to the side.' ) . '</li>';
 $revisions_overview .= '<li>' . __( 'To restore a revision, <strong>click Restore This Revision</strong>.' ) . '</li></ul>';
 
 get_current_screen()->add_help_tab( array(
