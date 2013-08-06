16a5c2794139aa362b809165af26d94a8d1ead7d
Ryan Boren <ryan@boren.nu>
2013-08-06 19:04:32 +0000

    Remove 'admin' as default username in install.
    Props chrisrudzki
    fixes #24078
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@24998 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/install.php
+++ wp-admin/install.php
@@ -85,7 +85,7 @@ function display_setup_form( $error = null ) {
 		$blog_public = isset( $_POST['blog_public'] );
 
 	$weblog_title = isset( $_POST['weblog_title'] ) ? trim( wp_unslash( $_POST['weblog_title'] ) ) : '';
-	$user_name = isset($_POST['user_name']) ? trim( wp_unslash( $_POST['user_name'] ) ) : 'admin';
+	$user_name = isset($_POST['user_name']) ? trim( wp_unslash( $_POST['user_name'] ) ) : '';
 	$admin_password = isset($_POST['admin_password']) ? trim( wp_unslash( $_POST['admin_password'] ) ) : '';
 	$admin_email  = isset( $_POST['admin_email']  ) ? trim( wp_unslash( $_POST['admin_email'] ) ) : '';
 
