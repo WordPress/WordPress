bf5b75c033988cb7e0b617dbf87fa4777bcf6c13
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2013-08-24 13:57:09 +0000

    Remove 'admin' as a fallback for username in install. props tivnet, DrewAPicture. fixes #24078. Built from https://develop.svn.wordpress.org/trunk@25115
    git-svn-id: http://core.svn.wordpress.org/trunk@25096 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/install.php
+++ wp-admin/install.php
@@ -190,7 +190,7 @@ switch($step) {
 		display_header();
 		// Fill in the data we gathered
 		$weblog_title = isset( $_POST['weblog_title'] ) ? trim( wp_unslash( $_POST['weblog_title'] ) ) : '';
-		$user_name = isset($_POST['user_name']) ? trim( wp_unslash( $_POST['user_name'] ) ) : 'admin';
+		$user_name = isset($_POST['user_name']) ? trim( wp_unslash( $_POST['user_name'] ) ) : '';
 		$admin_password = isset($_POST['admin_password']) ? wp_unslash( $_POST['admin_password'] ) : '';
 		$admin_password_check = isset($_POST['admin_password2']) ? wp_unslash( $_POST['admin_password2'] ) : '';
 		$admin_email  = isset( $_POST['admin_email']  ) ?trim( wp_unslash( $_POST['admin_email'] ) ) : '';
