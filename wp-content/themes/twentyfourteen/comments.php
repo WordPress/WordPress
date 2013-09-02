b2e6922bc524a8739f7215bb8c93f9e8ad5e9524
Lance Willett <nanobar@gmail.com>
2013-09-02 22:03:08 +0000

    Twenty Fourteen: style adjustments for the new HTML5 comment markup, and minor bug fixes for comment responsive pattern. Props iamtakashi, fixes #25045. Built from https://develop.svn.wordpress.org/trunk@25216
    git-svn-id: http://core.svn.wordpress.org/trunk@25187 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfourteen/comments.php
+++ wp-content/themes/twentyfourteen/comments.php
@@ -40,6 +40,7 @@ if ( post_password_required() )
 			wp_list_comments( array(
 				'style'      => 'ol',
 				'short_ping' => true,
+				'avatar_size'=> 42,
 			) );
 		?>
 	</ol><!-- .comment-list -->
