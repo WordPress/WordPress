d7704991b368381fd23235b8b40711ee00339fdd
Scott Taylor <scott.c.taylor@mac.com>
2013-09-06 23:39:09 +0000

    Check the value passed to `get_post_type_object()`. If it's an array, use the first item. `get_query_var( 'post_type' )` can be an array if the query has been altered via filters/actions. There are several places in core that pass the query var. Adds unit tests.
    In `template-loader.php`, move `is_post_type_archive()` and `is_tax()` directly below `is_home()`.
    
    See #18614, [25291].
    
    
    Built from https://develop.svn.wordpress.org/trunk@25292
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@25256 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/template-loader.php
+++ wp-includes/template-loader.php
@@ -26,10 +26,10 @@ if ( defined('WP_USE_THEMES') && WP_USE_THEMES ) :
 	$template = false;
 	if     ( is_404()            && $template = get_404_template()            ) :
 	elseif ( is_search()         && $template = get_search_template()         ) :
-	elseif ( is_post_type_archive() && $template = get_post_type_archive_template() ) : 
-	elseif ( is_tax()            && $template = get_taxonomy_template()       ) :
 	elseif ( is_front_page()     && $template = get_front_page_template()     ) :
 	elseif ( is_home()           && $template = get_home_template()           ) :
+	elseif ( is_post_type_archive() && $template = get_post_type_archive_template() ) :
+	elseif ( is_tax()            && $template = get_taxonomy_template()       ) :
 	elseif ( is_attachment()     && $template = get_attachment_template()     ) :
 		remove_filter('the_content', 'prepend_attachment');
 	elseif ( is_single()         && $template = get_single_template()         ) :
