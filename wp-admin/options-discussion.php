3b08ae25e4714b9d5efeee284f0c7c0caae9580b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2013-09-07 15:36:09 +0000

    Clarify a string in discussion settings. props Ipstenu, DrewAPicture. fixes #16705. Built from https://develop.svn.wordpress.org/trunk@25295
    git-svn-id: http://core.svn.wordpress.org/trunk@25258 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options-discussion.php
+++ wp-admin/options-discussion.php
@@ -136,7 +136,7 @@ printf( __('Comments should be displayed with the %s comments at the top of each
 <td><fieldset><legend class="screen-reader-text"><span><?php _e('Before a comment appears'); ?></span></legend>
 <label for="comment_moderation">
 <input name="comment_moderation" type="checkbox" id="comment_moderation" value="1" <?php checked('1', get_option('comment_moderation')); ?> />
-<?php _e('An administrator must always approve the comment'); ?> </label>
+<?php _e('Comment must be manually approved'); ?> </label>
 <br />
 <label for="comment_whitelist"><input type="checkbox" name="comment_whitelist" id="comment_whitelist" value="1" <?php checked('1', get_option('comment_whitelist')); ?> /> <?php _e('Comment author must have a previously approved comment'); ?></label>
 </fieldset></td>
