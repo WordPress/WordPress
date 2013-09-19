f1dfbc6e8055c53d9ca8f4b2346d3e5f6d1e04bb
Lance Willett <nanobar@gmail.com>
2013-09-19 21:33:09 +0000

    Twenty Fourteen: first pass at adding a Contributors page template to highlight authors. Props MikeHansenMe, iamtakashi, obenland, and Kuzmanov. See #24863. Built from https://develop.svn.wordpress.org/trunk@25510
    git-svn-id: http://core.svn.wordpress.org/trunk@25430 1a063a9b-81f0-0310-95a4-ce76da25c4cd

---
+++ wp-content/themes/twentyfourteen/contributor-page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Contributor Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content full-width" role="main">
+			<?php
+				while ( have_posts() ) :
+					the_post();
+			?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<?php
+					the_title( '<header class="entry-header"><h1 class="entry-title">', '</h1></header><!-- .entry-header -->' );
+
+					twentyfourteen_list_authors();
+
+					edit_post_link( __( 'Edit', 'twentyfourteen' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' );
+				?>
+			</article><!-- #post-## -->
+
+			<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() )
+						comments_template();
+				endwhile;
+			?>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
--- wp-content/themes/twentyfourteen/functions.php
+++ wp-content/themes/twentyfourteen/functions.php
@@ -340,6 +340,45 @@ function twentyfourteen_the_attached_image() {
 }
 endif;
 
+if ( ! function_exists( 'twentyfourteen_list_authors' ) ) :
+/**
+ * Prints a list of all site contributors who published at least one post.
+ *
+ * @return void
+ */
+function twentyfourteen_list_authors() {
+	$contributor_ids = get_users( array(
+		'fields'  => 'ID',
+		'orderby' => 'post_count',
+		'who'     => 'authors',
+	) );
+
+	foreach ( $contributor_ids as $contributor_id ) :
+		$post_count = count_user_posts( $contributor_id );
+
+		// Move on if user has not published a post (yet).
+		if ( ! $post_count )
+			continue;
+	?>
+
+	<div class="contributor clear">
+		<div class="contributor-avatar"><?php echo get_avatar( $contributor_id, 132 ); ?></div>
+		<div class="contributor-summary">
+			<h2 class="contributor-name"><?php echo get_the_author_meta( 'display_name', $contributor_id ); ?></h2>
+			<p class="contributor-bio">
+				<?php echo get_the_author_meta( 'description', $contributor_id ); ?>
+			</p>
+			<a class="contributor-posts-link" href="<?php echo esc_url( get_author_posts_url( $contributor_id ) ); ?>">
+				<?php printf( _n( '%d Article', '%d Articles', $post_count, 'twentyfourteen' ), $post_count ); ?>
+			</a>
+		</div>
+	</div><!-- .contributor -->
+
+	<?php
+	endforeach;
+}
+endif;
+
 /**
  * Gets recent formatted posts that are not featured in FC plugin.
  *
