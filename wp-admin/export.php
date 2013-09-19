7b2584c71f857260f0d1b1b2c663e127ea57aa50
Drew Jaynes <info@drewapicture.com>
2013-09-19 02:09:08 +0000

    Add missing phpdoc block for `export_date_options()`.
    Props siobhan for the initial patch. See #20425
    
    Built from https://develop.svn.wordpress.org/trunk@25490
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@25411 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/export.php
+++ wp-admin/export.php
@@ -109,6 +109,16 @@ if ( isset( $_GET['download'] ) ) {
 
 require_once ('admin-header.php');
 
+/**
+ * Create the date options fields for exporting a given post type.
+ *
+ * @global wpdb      $wpdb      WordPress database object.
+ * @global WP_Locale $wp_locale Date and Time Locale object.
+ *
+ * @since 3.1.0
+ *
+ * @param string $post_type The post type. Default 'post'.
+ */
 function export_date_options( $post_type = 'post' ) {
 	global $wpdb, $wp_locale;
 
