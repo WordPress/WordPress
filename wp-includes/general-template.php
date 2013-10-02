32917eb0bd96c16d3c5498ea1eea8987d373397b
Andrew Nacin <wp@andrewnacin.com>
2013-10-02 21:15:09 +0000

    Ensure that get_bloginfo( 'pingback_url' ) uses site_url(), for SSL awareness.
    props technosailor.
    fixes #25418.
    
    Built from https://develop.svn.wordpress.org/trunk@25671
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@25587 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -478,7 +478,7 @@ function get_bloginfo( $show = '', $filter = 'raw' ) {
 			$output = get_feed_link('comments_rss2');
 			break;
 		case 'pingback_url':
-			$output = get_option('siteurl') .'/xmlrpc.php';
+			$output = site_url( 'xmlrpc.php' );
 			break;
 		case 'stylesheet_url':
 			$output = get_stylesheet_uri();
