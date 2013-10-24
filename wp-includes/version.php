66dc1e25a1ed18d49e11cc1a797e3cc8008ee05a
Andrew Nacin <wp@andrewnacin.com>
2013-10-24 18:54:08 +0000

    Always escape URLs at the last possible moment. for the 3.7 branch.
    Built from https://develop.svn.wordpress.org/branches/3.7@25896
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.7@25808 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.7-RC2-25892';
+$wp_version = '3.7-RC2-25896';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
