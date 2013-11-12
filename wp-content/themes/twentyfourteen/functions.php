d6fdcfc43294c7afbef9f72807bab0c21e70b794
Lance Willett <nanobar@gmail.com>
2013-11-12 18:38:10 +0000

    Twenty Fourteen: rename `fonts` to `genericons`. See #25929. Built from https://develop.svn.wordpress.org/trunk@26103
    git-svn-id: http://core.svn.wordpress.org/trunk@26018 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfourteen/functions.php
+++ wp-content/themes/twentyfourteen/functions.php
@@ -221,7 +221,7 @@ function twentyfourteen_scripts() {
 	wp_enqueue_style( 'twentyfourteen-lato', twentyfourteen_font_url(), array(), null );
 
 	// Add Genericons font, used in the main stylesheet.
-	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/fonts/genericons.css', array(), '3.0.2' );
+	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/genericons/genericons.css', array(), '3.0.2' );
 
 	// Load our main stylesheet.
 	wp_enqueue_style( 'twentyfourteen-style', get_stylesheet_uri() );
