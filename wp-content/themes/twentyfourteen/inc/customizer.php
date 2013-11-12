cdc03fe491a5716b9e5ee07c16595e1d0db32a65
Lance Willett <nanobar@gmail.com>
2013-11-12 17:13:09 +0000

    Twenty Fourteen: add in more contextual help for setting up Featured Content, links to "featured" tag posts and "sticky" posts from inside the Customizer. See #25837. Built from https://develop.svn.wordpress.org/trunk@26101
    git-svn-id: http://core.svn.wordpress.org/trunk@26016 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfourteen/inc/customizer.php
+++ wp-content/themes/twentyfourteen/inc/customizer.php
@@ -40,7 +40,7 @@ function twentyfourteen_customize_register( $wp_customize ) {
 	// Add the featured content section in case it's not already there.
 	$wp_customize->add_section( 'featured_content', array(
 		'title'       => __( 'Featured Content', 'twentyfourteen' ),
-		'description' => __( 'Easily feature all posts with the "featured" tag or a tag of your choice; if no posts match the tag, "sticky" posts will be displayed instead.', 'twentyfourteen' ),
+		'description' => sprintf( __( 'Easily feature all posts with the <a href="%s">"featured" tag</a> or a tag of your choice; if no posts match the tag, <a href="%s">"sticky" posts</a> will be displayed instead.', 'twentyfourteen' ), admin_url( '/edit.php?tag=featured' ), admin_url( '/edit.php?show_sticky=1' ) ),
 		'priority'    => 130,
 	) );
 
