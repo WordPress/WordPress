5b8bd6e5ae7ad59bbda51a5c1b92bcbd78d39868
Andrew Nacin <wp@andrewnacin.com>
2013-11-13 04:44:09 +0000

    3.8-alpha-26127 to bust all the caches.
    Built from https://develop.svn.wordpress.org/trunk@26127
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@26039 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.8-alpha';
+$wp_version = '3.8-alpha-26127';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
