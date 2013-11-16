7d1fda5b52bedac3bd36d4cfff367c2528932ede
Andrew Nacin <wp@andrewnacin.com>
2013-11-16 20:41:12 +0000

    JSHint fixes for user-suggest.js.
    props dougwollison.
    fixes #26017.
    
    Built from https://develop.svn.wordpress.org/trunk@26238
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@26145 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/user-suggest.js
+++ wp-admin/js/user-suggest.js
@@ -1,13 +1,24 @@
-(function($) {
-	var id = 'undefined' !== typeof current_site_id ? '&site_id=' + current_site_id : '';
+/* global ajaxurl, current_site_id, isRtl */
+
+(function( $ ) {
+	var id = ( typeof current_site_id !== 'undefined' ) ? '&site_id=' + current_site_id : '';
 	$(document).ready( function() {
+		var position = { offset: '0, -1' };
+		if ( typeof isRtl !== 'undefined' && isRtl ) {
+			position.my = 'right top';
+			position.at = 'right bottom';
+		}
 		$( '.wp-suggest-user' ).autocomplete({
 			source:    ajaxurl + '?action=autocomplete-user&autocomplete_type=add' + id,
 			delay:     500,
 			minLength: 2,
-			position:  ( 'undefined' !== typeof isRtl && isRtl ) ? { my: 'right top', at: 'right bottom', offset: '0, -1' } : { offset: '0, -1' },
-			open:      function() { $(this).addClass('open'); },
-			close:     function() { $(this).removeClass('open'); }
+			position:  position,
+			open: function() {
+				$( this ).addClass( 'open' );
+			},
+			close: function() {
+				$( this ).removeClass( 'open' );
+			}
 		});
 	});
-})(jQuery);
+})( jQuery );
