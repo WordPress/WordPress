42293a5c7c9e42290d56df74ffd9ca5850a7674d
Andrew Nacin <wp@andrewnacin.com>
2013-11-19 04:40:10 +0000

    Fix JShint errors in user-profile.js.
    props dougwollison.
    fixes #26016.
    
    Built from https://develop.svn.wordpress.org/trunk@26266
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@26172 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/user-profile.js
+++ wp-admin/js/user-profile.js
@@ -1,7 +1,8 @@
+/* global ajaxurl, pwsL10n, svgPainter */
 (function($){
 
 	function check_pass_strength() {
-		var pass1 = $('#pass1').val(), user = $('#user_login').val(), pass2 = $('#pass2').val(), strength;
+		var pass1 = $('#pass1').val(), pass2 = $('#pass2').val(), strength;
 
 		$('#pass-strength-result').removeClass('short bad good strong');
 		if ( ! pass1 ) {
@@ -13,16 +14,16 @@
 
 		switch ( strength ) {
 			case 2:
-				$('#pass-strength-result').addClass('bad').html( pwsL10n['bad'] );
+				$('#pass-strength-result').addClass('bad').html( pwsL10n.bad );
 				break;
 			case 3:
-				$('#pass-strength-result').addClass('good').html( pwsL10n['good'] );
+				$('#pass-strength-result').addClass('good').html( pwsL10n.good );
 				break;
 			case 4:
-				$('#pass-strength-result').addClass('strong').html( pwsL10n['strong'] );
+				$('#pass-strength-result').addClass('strong').html( pwsL10n.strong );
 				break;
 			case 5:
-				$('#pass-strength-result').addClass('short').html( pwsL10n['mismatch'] );
+				$('#pass-strength-result').addClass('short').html( pwsL10n.mismatch );
 				break;
 			default:
 				$('#pass-strength-result').addClass('short').html( pwsL10n['short'] );
@@ -30,7 +31,8 @@
 	}
 
 	$(document).ready( function() {
-		var select = $('#display_name');
+		var $colorpicker, $stylesheet, user_id, current_user_id,
+			select = $( '#display_name' );
 
 		$('#pass1').val('').keyup( check_pass_strength );
 		$('#pass2').val('').keyup( check_pass_strength );
@@ -50,8 +52,8 @@
 					};
 
 				if ( inputs.display_firstname && inputs.display_lastname ) {
-					inputs['display_firstlast'] = inputs.display_firstname + ' ' + inputs.display_lastname;
-					inputs['display_lastfirst'] = inputs.display_lastname + ' ' + inputs.display_firstname;
+					inputs.display_firstlast = inputs.display_firstname + ' ' + inputs.display_lastname;
+					inputs.display_lastfirst = inputs.display_lastname + ' ' + inputs.display_firstname;
 				}
 
 				$.each( $('option', select), function( i, el ){
@@ -59,12 +61,13 @@
 				});
 
 				$.each(inputs, function( id, value ) {
-					if ( ! value )
+					if ( ! value ) {
 						return;
+					}
 
 					var val = value.replace(/<\/?[a-z][^>]*>/gi, '');
 
-					if ( inputs[id].length && $.inArray( val, dub ) == -1 ) {
+					if ( inputs[id].length && $.inArray( val, dub ) === -1 ) {
 						dub.push(val);
 						$('<option />', {
 							'text': val
@@ -74,10 +77,10 @@
 			});
 		}
 
-		var $colorpicker = $( '#color-picker' ),
-			$stylesheet = $( '#colors-css' ),
-			user_id = $( 'input#user_id' ).val(),
-			current_user_id = $( 'input[name="checkuser_id"]' ).val();
+		$colorpicker = $( '#color-picker' );
+		$stylesheet = $( '#colors-css' );
+		user_id = $( 'input#user_id' ).val();
+		current_user_id = $( 'input[name="checkuser_id"]' ).val();
 
 		// dropdown toggle
 		$colorpicker.on( 'click', '.dropdown-current', function() {
@@ -89,7 +92,7 @@
 			var color_scheme = $( this ).children( 'input[name="admin_color"]' ).val();
 
 			// update selected
-			$( this ).siblings( '.selected' ).removeClass( 'selected' )
+			$( this ).siblings( '.selected' ).removeClass( 'selected' );
 			$( this ).addClass( 'selected' );
 			$( this ).find( 'input' ).prop( 'checked', true );
 
@@ -99,7 +102,7 @@
 			$colorpicker.toggleClass( 'picker-expanded' );
 
 			// preview/save color scheme
-			if ( user_id == current_user_id ) {
+			if ( user_id === current_user_id ) {
 
 				// repaint icons
 				$stylesheet.attr( 'href', $( this ).children( '.css_url' ).val() );
