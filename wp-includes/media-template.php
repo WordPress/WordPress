04509a1b2395238794336b86926b1ec327085d64
Drew Jaynes <info@drewapicture.com>
2013-11-26 07:52:08 +0000

    Inline documentation for hooks in wp-includes/media-template.php.
    Props odysseygate for the initial patch. Props kpdesign.
    Fixes #25847.
    
    Built from https://develop.svn.wordpress.org/trunk@26400
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@26300 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/media-template.php
+++ wp-includes/media-template.php
@@ -51,7 +51,9 @@ function wp_print_media_templates() {
 			<h3 class="upload-instructions"><?php printf( __('The web browser on your device cannot be used to upload files. You may be able to use the <a href="%s">native app for your device</a> instead.'), 'http://wordpress.org/mobile/' ); ?></h3>
 		<?php elseif ( is_multisite() && ! is_upload_space_available() ) : ?>
 			<h3 class="upload-instructions"><?php _e( 'Upload Limit Exceeded' ); ?></h3>
-			<?php do_action( 'upload_ui_over_quota' ); ?>
+			<?php
+			/** This action is documented in wp-admin/includes/media.php */
+			do_action( 'upload_ui_over_quota' ); ?>
 
 		<?php else : ?>
 			<div class="upload-ui">
@@ -63,13 +65,17 @@ function wp_print_media_templates() {
 
 			<div class="post-upload-ui">
 				<?php
+				/** This action is documented in wp-admin/includes/media.php */
 				do_action( 'pre-upload-ui' );
+				/** This action is documented in wp-admin/includes/media.php */
 				do_action( 'pre-plupload-upload-ui' );
 
 				if ( 10 === remove_action( 'post-plupload-upload-ui', 'media_upload_flash_bypass' ) ) {
+					/** This action is documented in wp-admin/includes/media.php */
 					do_action( 'post-plupload-upload-ui' );
 					add_action( 'post-plupload-upload-ui', 'media_upload_flash_bypass' );
 				} else {
+					/** This action is documented in wp-admin/includes/media.php */
 					do_action( 'post-plupload-upload-ui' );
 				}
 
@@ -100,7 +106,9 @@ function wp_print_media_templates() {
 						$browser_uploader, '_blank' ); ?></p>
 				<?php endif; ?>
 
-				<?php do_action( 'post-upload-ui' ); ?>
+				<?php
+				/** This action is documented in wp-admin/includes/media.php */
+				do_action( 'post-upload-ui' ); ?>
 			</div>
 		<?php endif; ?>
 		</div>
@@ -340,7 +348,6 @@ function wp_print_media_templates() {
 						data-user-setting="imgsize"
 					<# } #>>
 					<?php
-
 					/** This filter is documented in wp-admin/includes/media.php */
 					$sizes = apply_filters( 'image_size_names_choose', array(
 						'thumbnail' => __('Thumbnail'),
@@ -416,7 +423,9 @@ function wp_print_media_templates() {
 			<img src="{{ data.model.url }}" draggable="false" />
 		</div>
 
-		<?php if ( ! apply_filters( 'disable_captions', '' ) ) : ?>
+		<?php
+		/** This filter is documented in wp-admin/includes/media.php */
+		if ( ! apply_filters( 'disable_captions', '' ) ) : ?>
 			<label class="setting caption">
 				<span><?php _e('Caption'); ?></span>
 				<textarea data-setting="caption" />
@@ -493,5 +502,10 @@ function wp_print_media_templates() {
 	</script>
 	<?php
 
+	/**
+	 * Prints the media manager custom media templates.
+	 *
+	 * @since 3.5.0
+	 */
 	do_action( 'print_media_templates' );
 }
