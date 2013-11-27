8c7b6ac0494ac0f568befda447de5fb523163389
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2013-11-27 23:10:10 +0000

    Twenty Fourteen: use display context for site description in sidebar.php.
    props michelwppi.
    fixes #25808.
    Built from https://develop.svn.wordpress.org/trunk@26444
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@26342 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfourteen/sidebar.php
+++ wp-content/themes/twentyfourteen/sidebar.php
@@ -9,7 +9,7 @@
 ?>
 <div id="secondary">
 	<?php
-		$description = get_bloginfo( 'description' );
+		$description = get_bloginfo( 'description', 'display' );
 		if ( ! empty ( $description ) ) :
 	?>
 	<h2 class="site-description"><?php echo esc_html( $description ); ?></h2>
