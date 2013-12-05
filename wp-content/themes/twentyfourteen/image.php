6d4eca6cdf08b09fb7702dcf8d48a9878755f3c8
Lance Willett <nanobar@gmail.com>
2013-12-05 22:51:11 +0000

    Twenty Fourteen: remove unneeded code from image template, see #25946. Built from https://develop.svn.wordpress.org/trunk@26699
    git-svn-id: http://core.svn.wordpress.org/trunk@26589 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfourteen/image.php
+++ wp-content/themes/twentyfourteen/image.php
@@ -58,20 +58,6 @@ get_header();
 						) );
 					?>
 				</div><!-- .entry-content -->
-
-				<footer class="entry-meta">
-					<?php
-						if ( comments_open() && pings_open() ) : // Comments and trackbacks open
-							printf( __( '<a class="comment-link" href="#respond">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" rel="trackback">Trackback URL</a>.', 'twentyfourteen' ), get_trackback_url() );
-						elseif ( ! comments_open() && pings_open() ) : // Only trackbacks open
-							printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" rel="trackback">Trackback URL</a>.', 'twentyfourteen' ), get_trackback_url() );
-						elseif ( comments_open() && ! pings_open() ) : // Only comments open
-							_e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond">post a comment</a>.', 'twentyfourteen' );
-						elseif ( ! comments_open() && ! pings_open() ) : // Comments and trackbacks closed
-							_e( 'Both comments and trackbacks are currently closed.', 'twentyfourteen' );
-						endif;
-					?>
-				</footer><!-- .entry-meta -->
 			</article><!-- #post-## -->
 
 			<nav id="image-navigation" class="navigation image-navigation">
