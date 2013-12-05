72f9712a1f4de216d081a2b60e8a3fc6cbbace8a
Andrew Nacin <wp@andrewnacin.com>
2013-12-05 18:42:16 +0000

    Themes: Refine how we display the current theme.
    props shaunandrews, kwight, matveb.
    see #26003.
    
    Built from https://develop.svn.wordpress.org/trunk@26681
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@26571 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/themes.php
+++ wp-admin/themes.php
@@ -221,12 +221,16 @@ if ( ! is_multisite() && current_user_can('edit_themes') && $broken_themes = wp_
 		<div class="theme-screenshot blank"></div>
 	<# } #>
 	<div class="theme-author"><?php printf( __( 'By %s' ), '{{{ data.author }}}' ); ?></div>
-	<h3 class="theme-name">{{{ data.name }}}</h3>
+	
+	<# if ( data.active ) { #>
+		<h3 class="theme-name"><span><?php _ex( 'Active:', 'theme' ); ?></span> {{{ data.name }}}</h3>
+	<# } else { #>
+		<h3 class="theme-name">{{{ data.name }}}</h3>
+	<# } #>
 
 	<div class="theme-actions">
 
 	<# if ( data.active ) { #>
-		<span class="current-label"><?php _e( 'Current Theme' ); ?></span>
 		<# if ( data.actions.customize ) { #>
 			<a class="button button-primary hide-if-no-customize" href="{{ data.actions.customize }}"><?php _e( 'Customize' ); ?></a>
 		<# } #>
