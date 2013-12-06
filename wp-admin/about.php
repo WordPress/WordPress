135adf3975ea46ec72e1162499996c5c13f0ed3e
Andrew Nacin <wp@andrewnacin.com>
2013-12-06 14:20:12 +0000

    Un-translate all of the about page strings so translators don't start working on incomplete copy. see #26387.
    Built from https://develop.svn.wordpress.org/trunk@26723
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@26612 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -36,24 +36,24 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 </h2>
 
 <div class="changelog">
-	<h2 class="about-headline-callout"><?php _e( 'Introducing a new, modern admin design' ); ?></h2>
+	<h2 class="about-headline-callout"><?php echo ( 'Introducing a new, modern admin design' ); ?></h2>
 	<img class="about-overview-img" src="<?php echo admin_url( 'images/about-overview.png' ); ?>" />
 
 	<div class="feature-section col three-col about-updates">
 		<div class="col-1">
 			<p style="margin-top: 20px; background-color: grey; padding: 1em; color: white; min-height: 150px;">Image</p>
-			<h3><?php _e( 'Modern aesthetics' ); ?></h3>
-			<p><?php _e( 'Goodbye decoration, hello simplicity. We removed extraneous details, focusing on a cleaner, more streamlined admin design.' ); ?></p>
+			<h3><?php echo ( 'Modern aesthetics' ); ?></h3>
+			<p><?php echo ( 'Goodbye decoration, hello simplicity. We removed extraneous details, focusing on a cleaner, more streamlined admin design.' ); ?></p>
 		</div>
 		<div class="col-2">
 			<p style="margin-top: 20px; background-color: grey; padding: 1em; color: white; min-height: 150px;">Image</p>
-			<h3><?php _e( 'Improved typography' ); ?></h3>
-			<p><?php _e( 'You might notice the type is a little bit bigger. We improved the typography, crafting a better reading experience.' ); ?></p>
+			<h3><?php echo ( 'Improved typography' ); ?></h3>
+			<p><?php echo ( 'You might notice the type is a little bit bigger. We improved the typography, crafting a better reading experience.' ); ?></p>
 		</div>
 		<div class="col-3 last-feature">
 			<p style="margin-top: 20px; background-color: grey; padding: 1em; color: white; min-height: 150px;">Image</p>
-			<h3><?php _e( 'Higher contrast' ); ?></h3>
-			<p><?php _e( 'With bigger typography and both high and low contrast color schemes, our new admin design is great for users of all ages.' ); ?></p>
+			<h3><?php echo ( 'Higher contrast' ); ?></h3>
+			<p><?php echo ( 'With bigger typography and both high and low contrast color schemes, our new admin design is great for users of all ages.' ); ?></p>
 		</div>
 	</div>
 </div>
@@ -66,10 +66,10 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 <div class="changelog">
 	<div class="feature-section col two-col">
 		<div>
-			<h3><?php _e( 'Take WordPress with you anywhere with our responsive design' ); ?></h3>
-			<p><?php _e( 'The WordPress admin is now completely responsive: you can work on your website easily from your smartphone or tablet. The full power of WordPress is at your fingertips, even when you’re on the go.' ); ?></p>
-			<h4><?php _e( 'Naturally HiDPI' ); ?></h4>
-			<p><?php _e( 'No more blurry edges &#8212; with the inclusion of vector icons and graphics, the admin is now entirely HiDPI, so you get the best viewing experience no matter what kind of computer or mobile device you use.' ); ?></p>
+			<h3><?php echo ( 'Take WordPress with you anywhere with our responsive design' ); ?></h3>
+			<p><?php echo ( 'The WordPress admin is now completely responsive: you can work on your website easily from your smartphone or tablet. The full power of WordPress is at your fingertips, even when you’re on the go.' ); ?></p>
+			<h4><?php echo ( 'Naturally HiDPI' ); ?></h4>
+			<p><?php echo ( 'No more blurry edges &#8212; with the inclusion of vector icons and graphics, the admin is now entirely HiDPI, so you get the best viewing experience no matter what kind of computer or mobile device you use.' ); ?></p>
 		</div>
 		<div class="last-feature about-colors-img">
 			<img src="<?php echo admin_url( 'images/about-colors.png' ); ?>" />
@@ -82,11 +82,11 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 <div class="changelog about-colors">
 	<div class="feature-section col one-col">
 		<div>
-			<h3><?php _e( 'Now with more color' ); ?></h3>
-			<p><?php _e( 'Your admin is not longer monochromatic &#8212; we&#8217;ve brought some more color to keep it looking fresh. You now have the option of four different default color schemes.' ); ?></p>
-			<p><?php _e( 'Try them out below:' ); ?></p>
+			<h3><?php echo ( 'Now with more color' ); ?></h3>
+			<p><?php echo ( 'Your admin is not longer monochromatic &#8212; we&#8217;ve brought some more color to keep it looking fresh. You now have the option of four different default color schemes.' ); ?></p>
+			<p><?php echo ( 'Try them out below:' ); ?></p>
 			<img src="https://i.cloudup.com/NBlGusRk0H.png" style="border: 2px solid red; max-width: 99%; margin: 0;" />
-			<p><?php _e( 'You can change your color scheme at any time from your profile page.' ); ?></p>
+			<p><?php echo ( 'You can change your color scheme at any time from your profile page.' ); ?></p>
 		</div>
 	</div>
 </div>
@@ -96,14 +96,14 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 <div class="changelog">
 	<div class="feature-section col two-col">
 		<div>
-			<h3><?php _e( 'A new theme experience' ); ?></h3>
-			<p><?php _e( 'A sleeker, faster, and more visual organization of your themes that is responsive.' ); ?></p>
-			<h4><?php _e( 'Browse better' ); ?></h4>
-			<p><?php _e( 'Enjoy a focused experience with theme screenshots at the center. Quickly search through your themes or add new ones.' ); ?></p>
-			<h4><?php _e( 'Dive into the details' ); ?></h4>
-			<p><?php _e( 'Expand a theme to see more information and preview it. Use the arrow navigation to quickly swift through your themes.' ); ?></p>
-			<h4><?php _e( 'Easier updates' ); ?></h4>
-			<p><?php _e( 'Identify immediately when a theme update is available.' ); ?></p>				
+			<h3><?php echo ( 'A new theme experience' ); ?></h3>
+			<p><?php echo ( 'A sleeker, faster, and more visual organization of your themes that is responsive.' ); ?></p>
+			<h4><?php echo ( 'Browse better' ); ?></h4>
+			<p><?php echo ( 'Enjoy a focused experience with theme screenshots at the center. Quickly search through your themes or add new ones.' ); ?></p>
+			<h4><?php echo ( 'Dive into the details' ); ?></h4>
+			<p><?php echo ( 'Expand a theme to see more information and preview it. Use the arrow navigation to quickly swift through your themes.' ); ?></p>
+			<h4><?php echo ( 'Easier updates' ); ?></h4>
+			<p><?php echo ( 'Identify immediately when a theme update is available.' ); ?></p>
 		</div>
 		<div class="last-feature about-themes-img">
 			<img src="<?php echo admin_url( 'images/about-themes.png' ); ?>" />
@@ -114,14 +114,14 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 <hr class="flushtopdivider">
 
 <div class="changelog">
-	<h2 class="about-headline-callout"><?php _e( 'Twenty Fourteen, a sleek new magazine theme' ); ?></h2>
+	<h2 class="about-headline-callout"><?php echo ( 'Twenty Fourteen, a sleek new magazine theme' ); ?></h2>
 	<img src="<?php echo admin_url( 'images/about-twentyfourteen.png' ); ?>" />
 
 	<div class="feature-section col one-col">
 		<div>
-			<h3><?php _e( 'Our new default theme lets you create a responsive magazine website with an elegant, modern design.' ); ?></h3>
-			<p><?php _e( 'Feature your favorite homepage content in either a grid or a slider. Use the three widget areas to customize your website, and change your content&#8217;s layout with a full width page template and a contributor page to show of your authors.' ); ?></p>
-			<p><?php _e( 'Creating a magazine website with WordPress has never been easier.' ); ?></p>
+			<h3><?php echo ( 'Our new default theme lets you create a responsive magazine website with an elegant, modern design.' ); ?></h3>
+			<p><?php echo ( 'Feature your favorite homepage content in either a grid or a slider. Use the three widget areas to customize your website, and change your content&#8217;s layout with a full width page template and a contributor page to show of your authors.' ); ?></p>
+			<p><?php echo ( 'Creating a magazine website with WordPress has never been easier.' ); ?></p>
 		</div>
 	</div>
 </div>
@@ -129,20 +129,20 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 <hr>
 
 <div class="changelog">
-	<h3><?php _e( 'Under the Hood' ); ?></h3>
+	<h3><?php echo ( 'Under the Hood' ); ?></h3>
 
 	<div class="feature-section col three-col">
 		<div>
-			<h4><?php _e( 'Meta query fixes' ); ?></h4>
-			<p><?php _e( 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis.' ); ?></p>
+			<h4><?php echo ( 'Meta query fixes' ); ?></h4>
+			<p><?php echo ( 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis.' ); ?></p>
 		</div>
 		<div>
-			<h4><?php _e( 'Automated RTL styles' ); ?></h4>
-			<p><?php _e( 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis.' ); ?></p>
+			<h4><?php echo ( 'Automated RTL styles' ); ?></h4>
+			<p><?php echo ( 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis.' ); ?></p>
 		</div>
 		<div class="last-feature">
-			<h4><?php _e( 'Improved customizer' ); ?></h4>
-			<p><?php _e( 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis.' ); ?></p>
+			<h4><?php echo ( 'Improved customizer' ); ?></h4>
+			<p><?php echo ( 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis.' ); ?></p>
 		</div>
 </div>
 
