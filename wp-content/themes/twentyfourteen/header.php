2f2b86270e3e754e5cb1718c49cbb9cf2f5f51f5
Andrew Nacin <wp@andrewnacin.com>
2013-12-06 17:16:11 +0000

    Twenty Fourteen: Remove unused and untoggled no-js class.
    props johnbillion.
    fixes #26453.
    
    Built from https://develop.svn.wordpress.org/trunk@26729
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@26618 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfourteen/header.php
+++ wp-content/themes/twentyfourteen/header.php
@@ -10,13 +10,13 @@
  */
 ?><!DOCTYPE html>
 <!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?> class="no-js">
+<html class="ie ie7" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?> class="no-js">
+<html class="ie ie8" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
