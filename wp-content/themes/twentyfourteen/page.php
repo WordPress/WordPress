ba4c047973602c815ae192b8e3829d4cb83ea475
Andrew Nacin <wp@andrewnacin.com>
2013-12-06 23:49:11 +0000

    Twenty Fourteen: Remove stray slash.
    props Ipstenu.
    fixes #26467.
    
    Built from https://develop.svn.wordpress.org/trunk@26757
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@26645 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfourteen/page.php
+++ wp-content/themes/twentyfourteen/page.php
@@ -16,7 +16,7 @@ get_header(); ?>
 <div id="main-content" class="main-content">
 
 <?php
-	if ( is_front_page() && twentyfourteen_has_featured_posts() ) :\
+	if ( is_front_page() && twentyfourteen_has_featured_posts() ) :
 		// Include the featured content template.
 		get_template_part( 'featured-content' );
 	endif;
