c06ed485b8afe71469b9582de8492a453920251a
Andrew Nacin <wp@andrewnacin.com>
2013-12-11 22:49:09 +0000

    Don't use a JS reserved word in a Backbone template, this breaks IE8.
    props ocean90.
    fixes #26539.
    
    Built from https://develop.svn.wordpress.org/trunk@26853
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@26740 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/themes.php
+++ wp-admin/themes.php
@@ -360,8 +360,8 @@ if ( ! is_multisite() && current_user_can('edit_themes') && $broken_themes = wp_
 				<a href="{{{ data.actions.preview }}}" class="button button-secondary hide-if-customize"><?php _e( 'Preview' ); ?></a>
 			</div>
 
-			<# if ( ! data.active && data.actions.delete ) { #>
-				<a href="{{{ data.actions.delete }}}" class="button button-secondary delete-theme"><?php _e( 'Delete' ); ?></a>
+			<# if ( ! data.active && data.actions['delete'] ) { #>
+				<a href="{{{ data.actions['delete'] }}}" class="button button-secondary delete-theme"><?php _e( 'Delete' ); ?></a>
 			<# } #>
 		</div>
 	</div>
