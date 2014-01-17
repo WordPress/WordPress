7e18ca2dec1cf8f46593c2adb2f162f4cb5ee0ae
Andrew Nacin <wp@andrewnacin.com>
2014-01-17 03:26:10 +0000

    Switch Twitter oEmbed to SSL due to a Twitter API change. Ah, the open web.
    props yurivictor, cojennin.
    see #26844.
    
    Built from https://develop.svn.wordpress.org/trunk@26967
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@26846 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-oembed.php
+++ wp-includes/class-oembed.php
@@ -46,7 +46,7 @@ class WP_oEmbed {
 			'http://wordpress.tv/*'                              => array( 'http://wordpress.tv/oembed/',                       false ),
 			'#https?://(.+\.)?polldaddy\.com/.*#i'               => array( 'http://polldaddy.com/oembed/',                      true  ),
 			'#https?://(www\.)?funnyordie\.com/videos/.*#i'      => array( 'http://www.funnyordie.com/oembed',                  true  ),
-			'#https?://(www\.)?twitter\.com/.+?/status(es)?/.*#i'=> array( 'http://api.twitter.com/1/statuses/oembed.{format}', true  ),
+			'#https?://(www\.)?twitter\.com/.+?/status(es)?/.*#i'=> array( 'https://api.twitter.com/1/statuses/oembed.{format}', true ),
  			'#https?://(www\.)?soundcloud\.com/.*#i'             => array( 'http://soundcloud.com/oembed',                      true  ),
 			'#https?://(www\.)?slideshare\.net/*#'               => array( 'http://www.slideshare.net/api/oembed/2',            true  ),
 			'#http://instagr(\.am|am\.com)/p/.*#i'               => array( 'http://api.instagram.com/oembed',                   true  ),
