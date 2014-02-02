9c4a97c676cf85bb7af6d0742e6e986ff3a6e2c6
Andrew Nacin <wp@andrewnacin.com>
2014-02-02 21:47:11 +0000

    Throw an incorrect usage notice when the query argument of wpdb::prepare() does not include a placeholder.
    props ounziw.
    fixes #25604.
    
    Built from https://develop.svn.wordpress.org/trunk@27073
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@26946 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -1053,6 +1053,10 @@ class wpdb {
 		if ( is_null( $query ) )
 			return;
 
+		if ( strpos( $query, '%' ) === false ) {
+			_doing_it_wrong( 'wpdb::prepare', sprintf( __( 'The query argument of %s must have a placeholder.' ), 'wpdb::prepare()' ), '3.9' );
+		}
+
 		$args = func_get_args();
 		array_shift( $args );
 		// If args were passed as an array (as in vsprintf), move them up
