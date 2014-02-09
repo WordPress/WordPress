60cc98b86d6b88f81432df600e32ae91dd8a65ac
Andrew Nacin <wp@andrewnacin.com>
2014-02-09 22:28:12 +0000

    Run wp_auth_check on every heartbeat tick.
    wp_heartbeat_received only runs when data is sent. We want to always pass this data back, though.
    
    fixes #27081.
    
    Built from https://develop.svn.wordpress.org/trunk@27153
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@27020 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-filters.php
+++ wp-includes/default-filters.php
@@ -294,8 +294,8 @@ add_filter( 'heartbeat_settings', 'wp_heartbeat_settings' );
 
 // Check if the user is logged out
 add_action( 'admin_enqueue_scripts',     'wp_auth_check_load'   );
-add_filter( 'heartbeat_received',        'wp_auth_check', 10, 2 );
-add_filter( 'heartbeat_nopriv_received', 'wp_auth_check', 10, 2 );
+add_filter( 'heartbeat_send',            'wp_auth_check', 10, 2 );
+add_filter( 'heartbeat_nopriv_send',     'wp_auth_check', 10, 2 );
 
 // Default authentication filters
 add_filter( 'authenticate', 'wp_authenticate_username_password',  20, 3 );
