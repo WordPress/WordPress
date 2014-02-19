6919d9f19761b3c862e05f93bd0efd5b7f08e4fc
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2014-02-19 15:29:14 +0000

    Use correct variable. props avryl. fixes #27153. Built from https://develop.svn.wordpress.org/trunk@27194
    git-svn-id: http://core.svn.wordpress.org/trunk@27053 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/template.php
+++ wp-admin/includes/template.php
@@ -656,7 +656,7 @@ function touch_time( $edit = 1, $for_post = 1, $tab_index = 0, $multi = 0 ) {
 	$month = "<select " . ( $multi ? '' : 'id="mm" ' ) . "name=\"mm\"$tab_index_attribute>\n";
 	for ( $i = 1; $i < 13; $i = $i +1 ) {
 		$monthnum = zeroise($i, 2);
-		$month .= "\t\t\t" . '<option value="' . $monthnum . '" ' . selected( $i, $mm, false ) . '>';
+		$month .= "\t\t\t" . '<option value="' . $monthnum . '" ' . selected( $monthnum, $mm, false ) . '>';
 		/* translators: 1: month number (01, 02, etc.), 2: month abbreviation */
 		$month .= sprintf( __( '%1$s-%2$s' ), $monthnum, $wp_locale->get_month_abbrev( $wp_locale->get_month( $i ) ) ) . "</option>\n";
 	}
