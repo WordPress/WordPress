20398546a0e0c6e1b16f61bfdbc0abbe4734a099
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2014-02-20 16:42:12 +0000

    Update $parent_file value in wp-admin/update-core.php.
    props bcworkz.
    fixes #27109.
    Built from https://develop.svn.wordpress.org/trunk@27205
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@27062 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/update-core.php
+++ wp-admin/update-core.php
@@ -457,7 +457,7 @@ if ( ( 'do-theme-upgrade' == $action || ( 'do-plugin-upgrade' == $action && ! is
 }
 
 $title = __('WordPress Updates');
-$parent_file = 'tools.php';
+$parent_file = 'index.php';
 
 $updates_overview  = '<p>' . __( 'On this screen, you can update to the latest version of WordPress, as well as update your themes and plugins from the WordPress.org repositories.' ) . '</p>';
 $updates_overview .= '<p>' . __( 'If an update is available, you&#8127;ll see a notification appear in the Toolbar and navigation menu.' ) . ' ' . __( 'Keeping your site updated is important for security. It also makes the internet a safer place for you and your readers.' ) . '</p>';
