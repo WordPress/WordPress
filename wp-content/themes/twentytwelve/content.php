8dd126190459ad1bc9a4147511ea60b86777c3ab
Scott Taylor <scott.c.taylor@mac.com>
2014-02-28 22:09:18 +0000

    In TwentyTwelve and TwentyThirteen, don't print the featurd image when viewing an attachment.
    See #27243.
    
    
    Built from https://develop.svn.wordpress.org/trunk@27331
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@27183 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/content.php
+++ wp-content/themes/twentytwelve/content.php
@@ -17,7 +17,10 @@
 		</div>
 		<?php endif; ?>
 		<header class="entry-header">
-			<?php the_post_thumbnail(); ?>
+			<?php if ( ! post_password_required() && ! is_attachment() ) :
+				the_post_thumbnail();
+			endif; ?>
+
 			<?php if ( is_single() ) : ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 			<?php else : ?>
