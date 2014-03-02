4da1691fd50a1d1a2481f37474344251973717cc
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2014-03-02 19:51:14 +0000

    Additional clarification for wp_authenticate_cookie() parameters.
    fixes #26148.
    Built from https://develop.svn.wordpress.org/trunk@27354
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@27205 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -162,8 +162,8 @@ function wp_authenticate_username_password($user, $username, $password) {
  * @since 2.8.0
  *
  * @param WP_User|WP_Error|null $user     WP_User or WP_Error object from a previous callback. Default null.
- * @param string                $username Username. If passed, cancels the cookie authentication.
- * @param string                $password Password. If passed, cancels the cookie authentication.
+ * @param string                $username Username. If not empty, cancels the cookie authentication.
+ * @param string                $password Password. If not empty, cancels the cookie authentication.
  * @return WP_User|WP_Error WP_User on success, WP_Error on failure.
  */
 function wp_authenticate_cookie($user, $username, $password) {
