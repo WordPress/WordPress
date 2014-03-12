40bd03b242f50ff672bd1558edb8848c2f54280e
_CraftMovies <lpcraftmovieslp@gmail.com>
2014-03-12 20:51:22 +0100

    Changed names of folders, and added content to index.php files
    Renamed directories (for example: wp-content > ep-content), and added
    custom index.php files in folders like wp-content.

---
+++ ep-content/plugins/index.php
@@ -0,0 +1,13 @@
+<?php
+// EasyPress Error File
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>EasyPress - Error</title>
+</head>
+<body>
+<h1>I can´t show it for you :(</h1>
+	<br>
+	<p>You dont have permission to view plugins.</p>
+</body>
--- wp-content/themes/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Silence is golden.
-?>
