a450512b7ba2d46c42cb41e19279f6cac12443c3
Lance Willett <nanobar@gmail.com>
2014-03-27 00:06:28 +0000

    Twenty Thirteen and Fourteen: implement HTML5 markup for galleries (see #26697) and captions (see #26642). Props obenland, closes #27502. Built from https://develop.svn.wordpress.org/trunk@27763
    git-svn-id: http://core.svn.wordpress.org/trunk@27600 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfourteen/functions.php
+++ wp-content/themes/twentyfourteen/functions.php
@@ -89,7 +89,7 @@ function twentyfourteen_setup() {
 	 * to output valid HTML5.
 	 */
 	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list',
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
 	) );
 
 	/*
