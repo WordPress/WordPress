3d8c17a9c8cf27b5ef82b79fbf9f3869c5d75e47
Andrew Nacin <wp@andrewnacin.com>
2014-04-01 04:02:10 +0000

    Fix a regression in wp_mkdir_p() where the $mode of the parent folder is not correctly applied to all created paths.
    Merges [26449] and [26927] from 3.8.x to the 3.7 branch.
    
    props dd32.
    fixes #25822.
    
    Built from https://develop.svn.wordpress.org/branches/3.7@27887
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.7@27718 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -1372,14 +1372,23 @@ function wp_mkdir_p( $target ) {
 	}
 
 	// Get the permission bits.
-	if ( $target_parent && '.' != $target_parent ) {
-		$stat = @stat( $target_parent );
+	$dir_perms = false;
+	if ( $stat = @stat( $target_parent ) ) {
 		$dir_perms = $stat['mode'] & 0007777;
 	} else {
 		$dir_perms = 0777;
 	}
 
 	if ( @mkdir( $target, $dir_perms, true ) ) {
+
+		// If a umask is set that modifies $dir_perms, we'll have to re-set the $dir_perms correctly with chmod()
+		if ( $dir_perms != ( $dir_perms & ~umask() ) ) {
+			$folder_parts = explode( '/', substr( $target, strlen( $target_parent ) + 1 ) );
+			for ( $i = 1; $i <= count( $folder_parts ); $i++ ) {
+				@chmod( $target_parent . '/' . implode( '/', array_slice( $folder_parts, 0, $i ) ), $dir_perms );
+			}
+		}
+
 		return true;
 	}
 
