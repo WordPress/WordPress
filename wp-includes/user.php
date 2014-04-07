5c3c97ead62e026083b5d6aca54f9d09ae5701e7
Drew Jaynes <info@drewapicture.com>
2014-04-07 21:43:15 +0000

    PHPDoc fixes for `wp_validate_logged_in_cookie()`, introduced in 3.9.
    See #27700.
    
    Built from https://develop.svn.wordpress.org/trunk@28015
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@27845 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -220,18 +220,19 @@ function wp_authenticate_spam_check( $user ) {
 }
 
 /**
- * Validates logged in cookie.
+ * Validate the logged-in cookie.
  *
- * Checks the logged_in cookie if the previous auth cookie could not be
+ * Checks the logged-in cookie if the previous auth cookie could not be
  * validated and parsed.
  *
  * This is a callback for the determine_current_user filter, rather than API.
  *
  * @since 3.9.0
  *
- * @param int|boolean $user The user ID (or false) as received from the determine_current_user filter.
- * @return int|boolean User ID if validated, or false otherwise. If it receives a user ID from
- *                     an earlier filter callback, that value is returned.
+ * @param int|bool $user The user ID (or false) as received from the
+ *                       determine_current_user filter.
+ * @return int|bool User ID if validated, false otherwise. If a user ID from
+ *                  an earlier filter callback is received, that value is returned.
  */
 function wp_validate_logged_in_cookie( $user_id ) {
 	if ( $user_id ) {
