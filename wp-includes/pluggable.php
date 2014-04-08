78a915e0e5927cf413aa6c2cef2fca3dc587f8be
Andrew Nacin <wp@andrewnacin.com>
2014-04-08 18:07:11 +0000

    Harden HMAC verification. props duck_. [28053] for 3.8.
    Built from https://develop.svn.wordpress.org/branches/3.8@28054
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.8@27884 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -543,7 +543,7 @@ function wp_validate_auth_cookie($cookie = '', $scheme = '') {
 	$key = wp_hash($username . $pass_frag . '|' . $expiration, $scheme);
 	$hash = hash_hmac('md5', $username . '|' . $expiration, $key);
 
-	if ( $hmac != $hash ) {
+	if ( hash_hmac( 'md5', $hmac, $key ) !== hash_hmac( 'md5', $hash, $key ) ) {
 		do_action('auth_cookie_bad_hash', $cookie_elements);
 		return false;
 	}
