ad850a34a4bf4db95e8f70c4dc8d37105f8cb58e
Andrew Nacin <wp@andrewnacin.com>
2014-04-13 16:35:14 +0000

    Add context to string added in [28097].
    props azaozz.
    fixes #27785.
    
    Built from https://develop.svn.wordpress.org/trunk@28099
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@27930 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-editor.php
+++ wp-includes/class-wp-editor.php
@@ -776,7 +776,7 @@ final class _WP_Editors {
 			'Blockquote' => __( 'Blockquote' ),
 			'Div' => _x( 'Div', 'HTML tag' ),
 			'Pre' => _x( 'Pre', 'HTML tag' ),
-			'Address' => __( 'Address' ),
+			'Address' => _x( 'Address', 'HTML tag' ),
 
 			'Inline' => _x( 'Inline', 'HTML elements' ),
 			'Underline' => __( 'Underline' ),
