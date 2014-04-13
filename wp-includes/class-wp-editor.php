23d78b1777b18e0fd02a8867c26727da23ba941d
Andrew Nacin <wp@andrewnacin.com>
2014-04-13 16:21:15 +0000

    Add missing 'Address' string for TinyMCE.
    props pavelevap, ocean90.
    fixes #27785.
    
    Built from https://develop.svn.wordpress.org/trunk@28097
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@27928 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-editor.php
+++ wp-includes/class-wp-editor.php
@@ -776,6 +776,7 @@ final class _WP_Editors {
 			'Blockquote' => __( 'Blockquote' ),
 			'Div' => _x( 'Div', 'HTML tag' ),
 			'Pre' => _x( 'Pre', 'HTML tag' ),
+			'Address' => __( 'Address' ),
 
 			'Inline' => _x( 'Inline', 'HTML elements' ),
 			'Underline' => __( 'Underline' ),
