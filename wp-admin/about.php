4a0ae35eb17fe10c000773d4abb06f874ee700c8
Andrew Nacin <wp@andrewnacin.com>
2014-04-15 07:53:14 +0000

    Final about page text.
    props siobhan, kerikae, DH-Shredder, nacin.
    see #27713.
    
    Built from https://develop.svn.wordpress.org/trunk@28132
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@27963 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -19,7 +19,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 <h1><?php printf( __( 'Welcome to WordPress&nbsp;%s' ), $display_version ); ?></h1>
 
-<div class="about-text"><?php printf( ( 'Thank you for updating! WordPress %s refines the way you write and edit.<br />We hope you like&nbsp;it.' ), $display_version ); ?></div>
+<div class="about-text"><?php printf( __( 'Thank you for updating! WordPress %s has lots of refinements we think you&#8127;ll love.' ), $display_version ); ?></div>
 
 <div class="wp-badge"><?php printf( __( 'Version %s' ), $display_version ); ?></div>
 
@@ -34,26 +34,26 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 </h2>
 
 <div class="changelog">
-	<h2 class="about-headline-callout"><?php echo ( 'A smoother media editing&nbsp;experience' ); ?></h2>
 	<div class="about-overview">
 		<img class="about-overview-img" src="//wordpress.org/images/core/3.9/overview.png?0" />
 	</div>
+	<h2 class="about-headline-callout"><?php _e( 'A smoother media editing&nbsp;experience' ); ?></h2>
 	<div class="feature-section col three-col">
 		<div class="col-1">
 			<img src="//wordpress.org/images/core/3.9/editor.jpg?0" />
-			<h4><?php echo ( 'Improved visual editor' ); ?></h4>
-			<p><?php echo ( 'We&#8217;ve updated the visual editor with better mobile support, improved speed and accessibility, and a modern API for developers.' ); ?></p>
-			<p><?php echo ( 'The visual editor will now automatically clean up the messy styling that certain word processing applications insert when copying and pasting. Yeah, we&#8217;re talking about you, Microsoft Word.' ); ?></p>
+			<h4><?php _e( 'Improved visual editing' ); ?></h4>
+			<p><?php _e( 'The updated visual editor has improved speed, accessibility, and mobile support.' );
+				echo ' ' . __( 'You can paste into the visual editor from your word processor without wasting time to clean up messy styling. (Yeah, we&#8217;re talking about you, Microsoft Word.)' ); ?></p>
 		</div>
 		<div class="col-2">
 			<img src="//wordpress.org/images/core/3.9/image.jpg?0" />
-			<h4><?php echo ( 'Improved image editing' ); ?></h4>
-			<p><?php echo ( 'We&#8217;ve made it much easier to edit your images, with quicker access to cropping and rotation tools. You can also scale images directly in the editor to find just the right fit.' ); ?></p>
+			<h4><?php _e( 'Edit images easily' ); ?></h4>
+			<p><?php _e( 'With quicker access to crop and rotation tools, it&#8127;s now much easier to edit your images while editing posts. You can also scale images directly in the editor to find just the right fit.' ); ?></p>
 		</div>
 		<div class="col-3 last-feature">
 			<img src="//wordpress.org/images/core/3.9/drop.jpg?0" />
-			<h4><?php echo ( 'Drag and drop your images' ); ?></h4>
-			<p><?php echo ( 'Grab images from your desktop and drop them directly onto the editor, saving yourself that extra step.' ); ?></p>
+			<h4><?php _e( 'Drag and drop your images' ); ?></h4>
+			<p><?php _e( 'Uploading your images is easier than ever. Just grab them from your desktop and drop them in the editor.' ); ?></p>
 		</div>
 	</div>
 
@@ -62,12 +62,13 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 	<div class="feature-section col two-col">
 		<div class="col-1">
 			<img src="//wordpress.org/images/core/3.9/gallery.jpg?0" />
-			<h4><?php echo ( 'Gallery previews' ); ?></h4>
-			<p><?php echo ( 'Galleries display a beautiful grid of images right in the editor, just like they do in your published post.' ); ?></p>
+			<h4><?php _e( 'Gallery previews' ); ?></h4>
+			<p><?php _e( 'Galleries display a beautiful grid of images right in the editor, just like they do in your published post.' ); ?></p>
 		</div>
 		<div class="col-2 last-feature">
-			<h4><?php echo ( 'Do more with audio and video' ); ?></h4>
-			<p><?php echo ( 'Images have galleries; now we&#8217;ve added simple audio and video playlists, so you can showcase your music and clips.' ); ?></p>
+			<img src="//wordpress.org/images/core/3.9/gallery.jpg?0" />
+			<h4><?php _e( 'Do more with audio and video' ); ?></h4>
+			<p><?php _e( 'Images have galleries; now we&#8217;ve added simple audio and video playlists, so you can showcase your music and clips.' ); ?></p>
 		</div>
 	</div>
 </div>
@@ -75,19 +76,20 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 <hr>
 
 <div class="changelog customize">
-	<h3><?php echo ( 'Customize your heart out' ); ?></h3>
-
 	<div class="feature-section col two-col">
 		<div>
-			<h4><?php echo ( 'Live widget previews' ); ?></h4>
-			<p><?php echo ( 'Add, edit, and rearrange your site&#8217;s widgets right in the theme customizer. No &#8220;save and surprise&#8221; &mdash; preview your changes live and only save them when you&#8217;re ready.' ); ?></p>
-			<p><?php echo ( 'The improved header image tool also lets you upload, crop, and manage headers while customizing your theme.' ); ?></p>
+			<img src="//wordpress.org/images/core/3.9/theme.jpg?0" />
+			<h4><?php _e( 'Live widget and header previews' ); ?></h4>
+			<p><?php _e( 'Add, edit, and rearrange your site&#8217;s widgets right in the theme customizer. No &#8220;save and surprise&#8221; &mdash; preview your changes live and only save them when you&#8217;re ready.' ); ?></p>
+			<p><?php _e( 'The improved header image tool also lets you upload, crop, and manage headers while customizing your theme.' ); ?></p>
 		</div>
 		<div class="last-feature">
 			<img src="//wordpress.org/images/core/3.9/theme.jpg?0" />
 			<h4><?php _e( 'Stunning new theme browser' ); ?></h4>
 			<p><?php _e( 'Looking for a new theme should be easy and fun. Lose yourself in the boundless supply of free WordPress.org themes with the beautiful new theme browser.' ); ?></p>
-			<p><a href="<?php echo network_admin_url( 'theme-install.php' ); ?>" class="button button-large button-primary">Browse Themes</a></p>
+		<?php if ( current_user_can( 'install_themes' ) ) { ?>
+			<p><a href="<?php echo network_admin_url( 'theme-install.php' ); ?>" class="button button-large button-primary"><?php _e( 'Browse Themes' ); ?></a></p>
+		<?php } ?>
 		</div>
 	</div>
 </div>
