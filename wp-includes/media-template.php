6deb34ebb39fb5df1156619529d81086b28b502b
Scott Taylor <scott.c.taylor@mac.com>
2014-04-22 20:29:14 +0000

    Alter the layout of the checkboxes in the modal view for Audio/Video Details to allow translations more room to breathe.
    See #27893.
    
    
    Built from https://develop.svn.wordpress.org/trunk@28184
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@28015 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/media-template.php
+++ wp-includes/media-template.php
@@ -854,15 +854,14 @@ function wp_print_media_templates() {
 				</div>
 
 				<label class="setting checkbox-setting">
-					<span><?php _e( 'Autoplay' ); ?></span>
 					<input type="checkbox" data-setting="autoplay" />
+					<span><?php _e( 'Autoplay' ); ?></span>
 				</label>
 
 				<label class="setting checkbox-setting">
-					<span><?php _e( 'Loop' ); ?></span>
 					<input type="checkbox" data-setting="loop" />
+					<span><?php _e( 'Loop' ); ?></span>
 				</label>
-				<div class="clear"></div>
 			</div>
 		</div>
 	</script>
@@ -945,15 +944,14 @@ function wp_print_media_templates() {
 				</div>
 
 				<label class="setting checkbox-setting">
-					<span><?php _e( 'Autoplay' ); ?></span>
 					<input type="checkbox" data-setting="autoplay" />
+					<span><?php _e( 'Autoplay' ); ?></span>
 				</label>
 
 				<label class="setting checkbox-setting">
-					<span><?php _e( 'Loop' ); ?></span>
 					<input type="checkbox" data-setting="loop" />
+					<span><?php _e( 'Loop' ); ?></span>
 				</label>
-				<div class="clear"></div>
 
 				<label class="setting" data-setting="content">
 					<span><?php _e( 'Tracks (subtitles, captions, descriptions, chapters, or metadata)' ); ?></span>
