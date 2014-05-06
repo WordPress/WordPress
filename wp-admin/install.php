213a7de0b72282cd51a50635f31b60a252ecbe10
Scott Taylor <scott.c.taylor@mac.com>
2014-05-06 18:04:15 +0000

    In `display_setup_form()`, `$admin_password` is set and (fortunately) not used or displayed.
    See #27882.
    
    Built from https://develop.svn.wordpress.org/trunk@28306
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@28134 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/install.php
+++ wp-admin/install.php
@@ -83,7 +83,6 @@ function display_setup_form( $error = null ) {
 
 	$weblog_title = isset( $_POST['weblog_title'] ) ? trim( wp_unslash( $_POST['weblog_title'] ) ) : '';
 	$user_name = isset($_POST['user_name']) ? trim( wp_unslash( $_POST['user_name'] ) ) : '';
-	$admin_password = isset($_POST['admin_password']) ? trim( wp_unslash( $_POST['admin_password'] ) ) : '';
 	$admin_email  = isset( $_POST['admin_email']  ) ? trim( wp_unslash( $_POST['admin_email'] ) ) : '';
 
 	if ( ! is_null( $error ) ) {
