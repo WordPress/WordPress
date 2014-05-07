42d2f4fc214413c3e2ca40962047625c03b91575
Lance Willett <nanobar@gmail.com>
2014-05-07 04:27:15 +0000

    Bundled themes: use correct logic in IE conditional comments in header template. Props mikemanger, fixes #28151. Built from https://develop.svn.wordpress.org/trunk@28341
    git-svn-id: http://core.svn.wordpress.org/trunk@28169 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyeleven/header.php
+++ wp-content/themes/twentyeleven/header.php
@@ -18,7 +18,7 @@
 <!--[if IE 8]>
 <html id="ie8" <?php language_attributes(); ?>>
 <![endif]-->
-<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<!--[if !(IE 6) & !(IE 7) & !(IE 8)]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
--- wp-content/themes/twentyfourteen/header.php
+++ wp-content/themes/twentyfourteen/header.php
@@ -15,7 +15,7 @@
 <!--[if IE 8]>
 <html class="ie ie8" <?php language_attributes(); ?>>
 <![endif]-->
-<!--[if !(IE 7) | !(IE 8) ]><!-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
