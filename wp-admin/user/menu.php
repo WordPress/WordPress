f87cbbf9c93cc1baba7ccc8135fce42adf98ca17
Andrew Nacin <wp@andrewnacin.com>
2014-05-07 20:11:15 +0000

    Add missing Dashicons classes to User Admin menu.
    Merges [28284] to the 3.9 branch.
    
    props imath.
    fixes #28144, #26630.
    
    Built from https://develop.svn.wordpress.org/branches/3.9@28344
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.9@28172 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/user/menu.php
+++ wp-admin/user/menu.php
@@ -7,11 +7,11 @@
  * @since 3.1.0
  */
 
-$menu[2] = array(__('Dashboard'), 'exist', 'index.php', '', 'menu-top menu-top-first menu-icon-dashboard', 'menu-dashboard', 'div');
+$menu[2] = array(__('Dashboard'), 'exist', 'index.php', '', 'menu-top menu-top-first menu-icon-dashboard', 'menu-dashboard', 'dashicons-dashboard');
 
 $menu[4] = array( '', 'exist', 'separator1', '', 'wp-menu-separator' );
 
-$menu[70] = array( __('Profile'), 'exist', 'profile.php', '', 'menu-top menu-icon-users', 'menu-users', 'div' );
+$menu[70] = array( __('Profile'), 'exist', 'profile.php', '', 'menu-top menu-icon-users', 'menu-users', 'dashicons-admin-users' );
 
 $menu[99] = array( '', 'exist', 'separator-last', '', 'wp-menu-separator' );
 
