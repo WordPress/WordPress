87730b879fbe723a58cd0c0b3490ee96f69a6365
Scott Taylor <scott.c.taylor@mac.com>
2014-05-17 17:21:14 +0000

    Eliminate the use of `extract()` in `MO::import_from_reader()`.
    See #22400.
    
    Built from https://develop.svn.wordpress.org/trunk@28472
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@28299 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pomo/mo.php
+++ wp-includes/pomo/mo.php
@@ -155,46 +155,49 @@ class MO extends Gettext_Translations {
 		if (!is_array($header))
 			return false;
 
-		extract( $header );
-
 		// support revision 0 of MO format specs, only
-		if ($revision != 0)
+		if ( $header['revision'] != 0 ) {
 			return false;
+		}
 
 		// seek to data blocks
-		$reader->seekto($originals_lenghts_addr);
+		$reader->seekto( $header['originals_lenghts_addr'] );
 
 		// read originals' indices
-		$originals_lengths_length = $translations_lenghts_addr - $originals_lenghts_addr;
-		if ( $originals_lengths_length != $total * 8 )
+		$originals_lengths_length = $header['translations_lenghts_addr'] - $header['originals_lenghts_addr'];
+		if ( $originals_lengths_length != $header['total'] * 8 ) {
 			return false;
+		}
 
 		$originals = $reader->read($originals_lengths_length);
-		if ( $reader->strlen( $originals ) != $originals_lengths_length )
+		if ( $reader->strlen( $originals ) != $originals_lengths_length ) {
 			return false;
+		}
 
 		// read translations' indices
-		$translations_lenghts_length = $hash_addr - $translations_lenghts_addr;
-		if ( $translations_lenghts_length != $total * 8 )
+		$translations_lenghts_length = $header['hash_addr'] - $header['translations_lenghts_addr'];
+		if ( $translations_lenghts_length != $header['total'] * 8 ) {
 			return false;
+		}
 
 		$translations = $reader->read($translations_lenghts_length);
-		if ( $reader->strlen( $translations ) != $translations_lenghts_length )
+		if ( $reader->strlen( $translations ) != $translations_lenghts_length ) {
 			return false;
+		}
 
 		// transform raw data into set of indices
 		$originals    = $reader->str_split( $originals, 8 );
 		$translations = $reader->str_split( $translations, 8 );
 
 		// skip hash table
-		$strings_addr = $hash_addr + $hash_length * 4;
+		$strings_addr = $header['hash_addr'] + $header['hash_length'] * 4;
 
 		$reader->seekto($strings_addr);
 
 		$strings = $reader->read_all();
 		$reader->close();
 
-		for ( $i = 0; $i < $total; $i++ ) {
+		for ( $i = 0; $i < $header['total']; $i++ ) {
 			$o = unpack( "{$endian}length/{$endian}pos", $originals[$i] );
 			$t = unpack( "{$endian}length/{$endian}pos", $translations[$i] );
 			if ( !$o || !$t ) return false;
