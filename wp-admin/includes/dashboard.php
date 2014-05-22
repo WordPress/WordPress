0e03abda62b85ad85478f6492890669edbfb93f2
Scott Taylor <scott.c.taylor@mac.com>
2014-05-22 18:54:13 +0000

    `$theme_name` is set but no longer used in `wp_dashboard_right_now()`. The theme name is displayed with the call to `update_right_now_message()`.
    See #27882.
    
    Built from https://develop.svn.wordpress.org/trunk@28544
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@28370 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/dashboard.php
+++ wp-admin/includes/dashboard.php
@@ -218,11 +218,6 @@ function wp_dashboard() {
  * @since 2.7.0
  */
 function wp_dashboard_right_now() {
-	$theme = wp_get_theme();
-	if ( current_user_can( 'switch_themes' ) )
-		$theme_name = sprintf( '<a href="themes.php">%1$s</a>', $theme->display('Name') );
-	else
-		$theme_name = $theme->display('Name');
 ?>
 	<div class="main">
 	<ul>
