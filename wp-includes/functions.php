aab2e48fa0bfe991ba6feee0e6c2207c2f3b4a9b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2014-05-27 11:41:14 +0000

    Update documentation for is_new_day().
    props obenland.
    fixes #28333.
    Built from https://develop.svn.wordpress.org/trunk@28588
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@28413 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -605,11 +605,12 @@ function wp_get_http_headers( $url, $deprecated = false ) {
 }
 
 /**
- * Whether today is a new day.
+ * Whether the publish date of the current post in the loop is different from the
+ * publish date of the previous post in the loop.
  *
  * @since 0.71
- * @uses $day Today
- * @uses $previousday Previous day
+ * @global string $currentday The day of the current post in the loop.
+ * @global string $previousday The day of the previous post in the loop.
  *
  * @return int 1 when new day, 0 if not a new day.
  */
