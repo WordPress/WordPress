5897fb2ed12a4a9f8127afd0a899a7f97ca5f5bf
Lance Willett <nanobar@gmail.com>
2014-06-04 17:15:15 +0000

    Twenty Fourteen: add documentation for global variables used in `twentyfourteen_wp_title()`. Closes #28421. Built from https://develop.svn.wordpress.org/trunk@28661
    git-svn-id: http://core.svn.wordpress.org/trunk@28479 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfourteen/functions.php
+++ wp-content/themes/twentyfourteen/functions.php
@@ -463,6 +463,9 @@ add_filter( 'post_class', 'twentyfourteen_post_classes' );
  *
  * @since Twenty Fourteen 1.0
  *
+ * @global int $paged WordPress archive pagination page count.
+ * @global int $page  WordPress paginated post page count.
+ *
  * @param string $title Default title text for current view.
  * @param string $sep Optional separator.
  * @return string The filtered title.
