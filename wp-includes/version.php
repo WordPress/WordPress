620e6ebe37d15ebe4399ed46ad78646393a29770
Andrew Ozz <admin@laptoptips.ca>
2014-06-11 00:23:14 +0000

    Fix the layout of the Image Details modal, props gcorne, see #24716 [28682] Built from https://develop.svn.wordpress.org/trunk@28732
    git-svn-id: http://core.svn.wordpress.org/trunk@28546 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.0-alpha-20140610';
+$wp_version = '4.0-alpha-20140611';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
