c171f2ef772aeb21671f51b9954db7717a72bf7f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2014-06-17 14:37:16 +0000

    Fix labels on installation screen.
    props avryl.
    fixes #28548.
    Built from https://develop.svn.wordpress.org/trunk@28759
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@28572 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/install.php
+++ wp-admin/install.php
@@ -114,7 +114,7 @@ function display_setup_form( $error = null ) {
 		<?php if ( ! $user_table ) : ?>
 		<tr>
 			<th scope="row">
-				<label for="admin_password"><?php _e('Password, twice'); ?></label>
+				<label for="pass1"><?php _e('Password, twice'); ?></label>
 				<p><?php _e('A password will be automatically generated for you if you leave this blank.'); ?></p>
 			</th>
 			<td>
@@ -132,7 +132,7 @@ function display_setup_form( $error = null ) {
 		</tr>
 		<tr>
 			<th scope="row"><label for="blog_public"><?php _e( 'Privacy' ); ?></label></th>
-			<td colspan="2"><label><input type="checkbox" name="blog_public" value="1" <?php checked( $blog_public ); ?> /> <?php _e( 'Allow search engines to index this site.' ); ?></label></td>
+			<td colspan="2"><label><input type="checkbox" name="blog_public" id="blog_public" value="1" <?php checked( $blog_public ); ?> /> <?php _e( 'Allow search engines to index this site.' ); ?></label></td>
 		</tr>
 	</table>
 	<p class="step"><input type="submit" name="Submit" value="<?php esc_attr_e( 'Install WordPress' ); ?>" class="button button-large" /></p>
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.0-alpha-20140616';
+$wp_version = '4.0-alpha-20140617';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
