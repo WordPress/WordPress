0abf8699f3c2af29d033f3cdc28a00de60ea8c5c
Scott Taylor <scott.c.taylor@mac.com>
2014-06-20 16:30:16 +0000

    Further adjustments after [8364], listen to an editor instance event instead of delegating a body click when attempting to pause all players belonging to a particular MCE view.
    Props avryl.
    Fixes #27971.
    
    Built from https://develop.svn.wordpress.org/trunk@28784
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@28597 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/mce-view.js
+++ wp-includes/js/mce-view.js
@@ -417,7 +417,9 @@ window.wp = window.wp || {};
 				this.shortcode = options.shortcode;
 				_.bindAll( this, 'setPlayer', 'pausePlayers' );
 				$( this ).on( 'ready', this.setPlayer );
-				$( 'body' ).on( 'click', '.wp-switch-editor', this.pausePlayers );
+				$( this ).on( 'ready', function( event, editor ) {
+					editor.on( 'hide', this.pausePlayers );
+				} );
 				$( document ).on( 'media:edit', this.pausePlayers );
 			},
 
@@ -558,7 +560,9 @@ window.wp = window.wp || {};
 				this.attachments = [];
 				this.shortcode = options.shortcode;
 
-				$( 'body' ).on( 'click', '.wp-switch-editor', this.pausePlayers );
+				$( this ).on( 'ready', function( event, editor ) {
+					editor.on( 'hide', this.pausePlayers );
+				} );
 				$( document ).on( 'media:edit', this.pausePlayers );
 
 				this.fetch();
