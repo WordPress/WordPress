c936468da1dee834a93f3cbca93214c79ee1ba61
Andrew Ozz <admin@laptoptips.ca>
2014-06-21 01:15:15 +0000

    TinyMCE: revert https://github.com/tinymce/tinymce/commit/158b7424103a02f579bd2bd0cd18f91d1b5e2183 and https://github.com/tinymce/tinymce/commit/576fa77aa20c7e029adb4bce5cab4bf3654cd58a as they may cause selection restore problems in old IE. See #28391. Built from https://develop.svn.wordpress.org/trunk@28795
    git-svn-id: http://core.svn.wordpress.org/trunk@28608 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.0-alpha-20140620';
+$wp_version = '4.0-alpha-20140621';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 27916;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '4028-20140617';
+$tinymce_version = '4028-20140620';
 
 /**
  * Holds the required PHP version
