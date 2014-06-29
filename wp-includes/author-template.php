a10aff4d927615838e0461cc7220c042d5b7aef2
Drew Jaynes <info@drewapicture.com>
2014-06-29 22:34:14 +0000

    Convert documentation of default arguments in `wp_list_authors()` to the hash-notation style.
    Props Viper007Bond.
    Fixes #28684.
    
    Built from https://develop.svn.wordpress.org/trunk@28909
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@28708 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/author-template.php
+++ wp-includes/author-template.php
@@ -304,30 +304,35 @@ function get_author_posts_url($author_id, $author_nicename = '') {
 /**
  * List all the authors of the blog, with several options available.
  *
- * <ul>
- * <li>optioncount (boolean) (false): Show the count in parenthesis next to the
- * author's name.</li>
- * <li>exclude_admin (boolean) (true): Exclude the 'admin' user that is
- * installed by default.</li>
- * <li>show_fullname (boolean) (false): Show their full names.</li>
- * <li>hide_empty (boolean) (true): Don't show authors without any posts.</li>
- * <li>feed (string) (''): If isn't empty, show links to author's feeds.</li>
- * <li>feed_image (string) (''): If isn't empty, use this image to link to
- * feeds.</li>
- * <li>echo (boolean) (true): Set to false to return the output, instead of
- * echoing.</li>
- * <li>style (string) ('list'): Whether to display list of authors in list form
- * or as a string.</li>
- * <li>html (bool) (true): Whether to list the items in html form or plaintext.
- * </li>
- * <li>exclude (array): Array of user IDs to explicitly exclude.</li>
- * <li>include (array): Array of user IDs to explicitly include.</li>
- * </ul>
- *
  * @link http://codex.wordpress.org/Template_Tags/wp_list_authors
+ *
  * @since 1.2.0
- * @param array $args The argument array.
- * @return null|string The output, if echo is set to false.
+ *
+ * @param string|array $args {
+ *     Optional. Array or string of default arguments.
+ *
+ *     @type string $orderby       How to sort the authors. Accepts 'nicename', 'email', 'url', 'registered',
+ *                                 'user_nicename', 'user_email', 'user_url', 'user_registered', 'name',
+ *                                 'display_name', 'post_count', 'ID', 'meta_value', 'user_login'. Default 'name'.
+ *     @type string $order         Sorting direction for $orderby. Accepts 'ASC', 'DESC'. Default 'ASC'.
+ *     @type int    $number        Maximum authors to return or display. Default empty (all authors).
+ *     @type bool   $optioncount   Show the count in parenthesis next to the author's name. Default false.
+ *     @type bool   $exclude_admin Whether to exclude the 'admin' account, if it exists. Default false.
+ *     @type bool   $show_fullname Whether to show the author's full name. Default false.
+ *     @type bool   $hide_empty    Whether to hide any authors with no posts. Default true.
+ *     @type string $feed          If not empty, show a link to the author's feed and use this text as the alt
+ *                                 parameter of the link. Default empty.
+ *     @type string $feed_image    If not empty, show a link to the author's feed and use this image URL as
+ *                                 clickable anchor. Default empty.
+ *     @type string $feed_type     The feed type to link to, such as 'rss2'. Defaults to default feed type.
+ *     @type bool   $echo          Whether to output the result or instead return it. Default true.
+ *     @type string $style         If 'list', each author is wrapped in an `<li>` element, otherwise the authors
+ *                                 will be separated by commas.
+ *     @type bool   $html          Whether to list the items in HTML form or plaintext. Default true.
+ *     @type string $exclude       An array, comma-, or space-separated list of author IDs to exclude. Default empty.
+ *     @type string $exclude       An array, comma-, or space-separated list of author IDs to include. Default empty.
+ * }
+ * @return null|string The output, if echo is set to false. Otherwise null.
  */
 function wp_list_authors( $args = '' ) {
 	global $wpdb;
