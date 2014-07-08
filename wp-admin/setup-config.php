d543096d93ec6fc203acbf8e208ce622093afdca
John Blackbourn <johnbillion@git.wordpress.org>
2014-07-08 18:32:14 +0000

    Remove autocomplete from the database password screen on setup-config.php. Props hebbet. Fixes #28585. Built from https://develop.svn.wordpress.org/trunk@29032
    git-svn-id: http://core.svn.wordpress.org/trunk@28820 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/setup-config.php
+++ wp-admin/setup-config.php
@@ -152,7 +152,7 @@ switch($step) {
 		</tr>
 		<tr>
 			<th scope="row"><label for="pwd"><?php _e( 'Password' ); ?></label></th>
-			<td><input name="pwd" id="pwd" type="text" size="25" value="<?php echo htmlspecialchars( _x( 'password', 'example password' ), ENT_QUOTES ); ?>" /></td>
+			<td><input name="pwd" id="pwd" type="text" size="25" value="<?php echo htmlspecialchars( _x( 'password', 'example password' ), ENT_QUOTES ); ?>" autocomplete="off" /></td>
 			<td><?php _e( '&hellip;and your MySQL password.' ); ?></td>
 		</tr>
 		<tr>
