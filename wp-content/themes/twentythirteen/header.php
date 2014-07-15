f2adf0402508ce6d1eca7f85226cbfef57e49108
Lance Willett <nanobar@gmail.com>
2014-07-15 23:10:15 +0000

    Twenty Thirteen: make small-screen menu accessible to keyboard commands and voice-driven software by using a focusable `button` element rather than `h3` for the toggle element. Fixes #28293, props rclations and obenland. Built from https://develop.svn.wordpress.org/trunk@29181
    git-svn-id: http://core.svn.wordpress.org/trunk@28965 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentythirteen/header.php
+++ wp-content/themes/twentythirteen/header.php
@@ -40,7 +40,7 @@
 
 			<div id="navbar" class="navbar">
 				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
+					<button class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></button>
 					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 					<?php get_search_form(); ?>
