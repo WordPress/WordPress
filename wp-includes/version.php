78d0ddfa3696a89723e88e8cb0ba9672f9cf217b
Andrew Nacin <wp@andrewnacin.com>
2014-08-06 18:27:27 +0000

    3.8.4
    Built from https://develop.svn.wordpress.org/branches/3.8@29412
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.8@29190 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.8.4-alpha';
+$wp_version = '3.8.4';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
