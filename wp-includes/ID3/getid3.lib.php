1f14360fe85845b43a78e4cb22f72b703f4c4ac4
Andrew Nacin <wp@andrewnacin.com>
2014-08-06 05:58:10 +0000

    Disable external entities in ID3.
    Merges [29378] to the 3.8 branch.
    
    Built from https://develop.svn.wordpress.org/branches/3.8@29391
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.8@29169 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ID3/getid3.lib.php
+++ wp-includes/ID3/getid3.lib.php
@@ -519,11 +519,12 @@ class getid3_lib
 	}
 
 	public static function XML2array($XMLstring) {
-		if (function_exists('simplexml_load_string')) {
-			if (function_exists('get_object_vars')) {
-				$XMLobject = simplexml_load_string($XMLstring);
-				return self::SimpleXMLelement2array($XMLobject);
-			}
+		if ( function_exists( 'simplexml_load_string' ) && function_exists( 'libxml_disable_entity_loader' ) ) {
+			$loader = libxml_disable_entity_loader( true );
+			$XMLobject = simplexml_load_string( $XMLstring, 'SimpleXMLElement', LIBXML_NOENT );
+			$return = self::SimpleXMLelement2array( $XMLobject );
+			libxml_disable_entity_loader( $loader );
+			return $return;
 		}
 		return false;
 	}
