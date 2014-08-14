6b3a4e6ddd68598fd04799c920589d42e9e33305
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2014-08-14 21:41:16 +0000

    Media Grid: Use an existing string for posts without a title.
    see #24716.
    Built from https://develop.svn.wordpress.org/trunk@29493
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29271 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/media.php
+++ wp-includes/media.php
@@ -2647,7 +2647,7 @@ function wp_prepare_attachment_for_js( $attachment ) {
 		if ( $parent_type && $parent_type->show_ui && current_user_can( 'edit_post', $attachment->post_parent ) ) {
 			$response['uploadedToLink'] = get_edit_post_link( $attachment->post_parent, 'raw' );
 		}
-		$response['uploadedToTitle'] = $post_parent->post_title ? $post_parent->post_title : __( '(No title)' );
+		$response['uploadedToTitle'] = $post_parent->post_title ? $post_parent->post_title : __( '(no title)' );
 	}
 
 	$attached_file = get_attached_file( $attachment->ID );
