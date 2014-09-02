433210a9070a49d4cfe1ee5ea3e89d5e03854e01
Helen Hou-Sandí <helen.y.hou@gmail.com>
2014-09-02 19:47:16 +0000

    Plugin install: Better styling to accommodate plugin icons.
    fixes #29466.
    
    Built from https://develop.svn.wordpress.org/trunk@29679
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29454 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-plugin-install-list-table.php
+++ wp-admin/includes/class-wp-plugin-install-list-table.php
@@ -435,16 +435,16 @@ class WP_Plugin_Install_List_Table extends WP_List_Table {
 		?>
 		<div class="plugin-card">
 			<div class="plugin-card-top">
-				<a href="<?php echo esc_url( $details_link ); ?>" class="thickbox"><img src="<?php echo esc_attr( $plugin_icon_url ) ?>" class="plugin-icon" /></a>
+				<a href="<?php echo esc_url( $details_link ); ?>" class="thickbox plugin-icon"><img src="<?php echo esc_attr( $plugin_icon_url ) ?>" /></a>
+				<div class="name column-name">
+					<h4><a href="<?php echo esc_url( $details_link ); ?>" class="thickbox"><?php echo $title; ?></a></h4>
+				</div>
 				<div class="action-links">
 					<?php
 						if ( $action_links ) {
 							echo '<ul class="plugin-action-buttons"><li>' . implode( '</li><li>', $action_links ) . '</li></ul>';
 						}
 					?>
-				</div>
-				<div class="name column-name">
-					<h4><a href="<?php echo esc_url( $details_link ); ?>" class="thickbox"><?php echo $title; ?></a></h4>
 				</div>
 				<div class="desc column-description">
 					<p><?php echo $description; ?></p>
