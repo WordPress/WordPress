2c171053ed85b8eae4c8202b0d99ba423da75e54
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2014-09-09 01:23:18 +0000

    Remove 'WordPress Language' reference from wp-config-sample.php.
    props voldemortensen.
    see [29630]. fixes #29562.
    Built from https://develop.svn.wordpress.org/trunk@29720
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29494 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -3,9 +3,9 @@
  * The base configurations of the WordPress.
  *
  * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ * Secret Keys, and ABSPATH. You can find more information by visiting
+ * {@link http://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
+ * Codex page. You can get the MySQL settings from your web host.
  *
  * This file is used by the wp-config.php creation script during the
  * installation. You don't have to use the web site, you can just copy this file
