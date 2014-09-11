a1245a4a675ba70d091afac74b2dcc3ce0f5eb91
Scott Taylor <scott.c.taylor@mac.com>
2014-09-11 19:25:16 +0000

    Allow arbitrary subdomains for the oEmbed endpoint for Slideshare.
    Props Latz.
    Fixes #29427.
    
    Built from https://develop.svn.wordpress.org/trunk@29735
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29509 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-oembed.php
+++ wp-includes/class-oembed.php
@@ -52,7 +52,7 @@ class WP_oEmbed {
 			'#https?://(www\.)?funnyordie\.com/videos/.*#i'       => array( 'http://www.funnyordie.com/oembed',                   true  ),
 			'#https?://(www\.)?twitter\.com/.+?/status(es)?/.*#i' => array( 'https://api.twitter.com/1/statuses/oembed.{format}', true  ),
  			'#https?://(www\.)?soundcloud\.com/.*#i'              => array( 'http://soundcloud.com/oembed',                       true  ),
-			'#https?://(www\.)?slideshare\.net/.*#i'              => array( 'https://www.slideshare.net/api/oembed/2',            true  ),
+			'#https?://(.+?\.)?slideshare\.net/.*#i'              => array( 'https://www.slideshare.net/api/oembed/2',            true  ),
 			'#http://instagr(\.am|am\.com)/p/.*#i'                => array( 'http://api.instagram.com/oembed',                    true  ),
 			'#https?://(www\.)?rdio\.com/.*#i'                    => array( 'http://www.rdio.com/api/oembed/',                    true  ),
 			'#https?://rd\.io/x/.*#i'                             => array( 'http://www.rdio.com/api/oembed/',                    true  ),
