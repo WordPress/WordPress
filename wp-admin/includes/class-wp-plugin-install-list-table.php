9bf3007b55e23876bb6c154e9ba103e14c2aca6d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2014-09-28 20:26:17 +0000

    Remove unnecessary CSS overriding on Add Plugins screen.
    props pavelevap.
    fixes #29753.
    Built from https://develop.svn.wordpress.org/trunk@29775
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29547 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-plugin-install-list-table.php
+++ wp-admin/includes/class-wp-plugin-install-list-table.php
@@ -467,7 +467,7 @@ class WP_Plugin_Install_List_Table extends WP_List_Table {
 				<div class="column-compatibility">
 					<?php
 					if ( ! empty( $plugin['tested'] ) && version_compare( substr( $GLOBALS['wp_version'], 0, strlen( $plugin['tested'] ) ), $plugin['tested'], '>' ) ) {
-						echo '<span class="compatibility-untested">' . __( '<strong>Untested</strong> with your version of WordPress' ) . '</span>';
+						echo '<span class="compatibility-untested">' . __( 'Untested with your version of WordPress' ) . '</span>';
 					} elseif ( ! empty( $plugin['requires'] ) && version_compare( substr( $GLOBALS['wp_version'], 0, strlen( $plugin['requires'] ) ), $plugin['requires'], '<' ) ) {
 						echo '<span class="compatibility-incompatible">' . __( '<strong>Incompatible</strong> with your version of WordPress' ) . '</span>';
 					} else {
