5516c4897591b6e04d2c0e790838557d742e67a2
Andrew Nacin <wp@andrewnacin.com>
2014-09-29 10:28:17 +0000

    PHPMailer: Merge upstream commit (PR274).
    props zodiac1978.
    fixes #27946.
    
    Built from https://develop.svn.wordpress.org/trunk@29783
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29555 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-smtp.php
+++ wp-includes/class-smtp.php
@@ -409,7 +409,7 @@ class SMTP
         // RFC 2104 HMAC implementation for php.
         // Creates an md5 HMAC.
         // Eliminates the need to install mhash to compute a HMAC
-        // Hacked by Lance Rushing
+        // by Lance Rushing
 
         $b = 64; // byte length for md5
         if (strlen($key) > $b) {
