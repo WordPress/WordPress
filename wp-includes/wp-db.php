740df917fab8e4cf1e5248a13ec3bde0af1ff500
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2014-09-29 10:30:16 +0000

    Remove unnecessary semicolon.
    props ixkaito.
    fixes #29784.
    Built from https://develop.svn.wordpress.org/trunk@29784
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29556 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -2149,7 +2149,7 @@ class wpdb {
 				return version_compare( $version, '4.1', '>=' );
 			case 'set_charset' :
 				return version_compare( $version, '5.0.7', '>=' );
-		};
+		}
 
 		return false;
 	}
