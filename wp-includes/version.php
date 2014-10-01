3f1fcc7d71dc7f31a193d45bd569da4a9b28a3aa
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2014-10-01 14:59:19 +0000

    Version bump. Built from https://develop.svn.wordpress.org/trunk@29800
    git-svn-id: http://core.svn.wordpress.org/trunk@29567 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-alpha';
+$wp_version = '4.1-alpha-20141001';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
