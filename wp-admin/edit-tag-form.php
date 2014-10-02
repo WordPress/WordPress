6ee7e6061aa519453401361dad07bff8aac35cef
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2014-10-02 00:29:16 +0000

    Add classes to form containers on taxonomy screens.
    props jarednova.
    fixes #28196.
    Built from https://develop.svn.wordpress.org/trunk@29803
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29570 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-tag-form.php
+++ wp-admin/edit-tag-form.php
@@ -80,13 +80,13 @@ do_action( "{$taxonomy}_pre_edit_form", $tag, $taxonomy ); ?>
 <input type="hidden" name="taxonomy" value="<?php echo esc_attr($taxonomy) ?>" />
 <?php wp_original_referer_field(true, 'previous'); wp_nonce_field('update-tag_' . $tag_ID); ?>
 	<table class="form-table">
-		<tr class="form-field form-required">
+		<tr class="form-field form-required term-name-wrap">
 			<th scope="row"><label for="name"><?php _ex('Name', 'Taxonomy Name'); ?></label></th>
 			<td><input name="name" id="name" type="text" value="<?php if ( isset( $tag->name ) ) echo esc_attr($tag->name); ?>" size="40" aria-required="true" />
 			<p class="description"><?php _e('The name is how it appears on your site.'); ?></p></td>
 		</tr>
 <?php if ( !global_terms_enabled() ) { ?>
-		<tr class="form-field">
+		<tr class="form-field term-slug-wrap">
 			<th scope="row"><label for="slug"><?php _ex('Slug', 'Taxonomy Slug'); ?></label></th>
 			<?php
 			/**
@@ -102,7 +102,7 @@ do_action( "{$taxonomy}_pre_edit_form", $tag, $taxonomy ); ?>
 		</tr>
 <?php } ?>
 <?php if ( is_taxonomy_hierarchical($taxonomy) ) : ?>
-		<tr class="form-field">
+		<tr class="form-field term-parent-wrap">
 			<th scope="row"><label for="parent"><?php _ex('Parent', 'Taxonomy Parent'); ?></label></th>
 			<td>
 				<?php wp_dropdown_categories(array('hide_empty' => 0, 'hide_if_empty' => false, 'name' => 'parent', 'orderby' => 'name', 'taxonomy' => $taxonomy, 'selected' => $tag->parent, 'exclude_tree' => $tag->term_id, 'hierarchical' => true, 'show_option_none' => __('None'))); ?>
@@ -112,7 +112,7 @@ do_action( "{$taxonomy}_pre_edit_form", $tag, $taxonomy ); ?>
 			</td>
 		</tr>
 <?php endif; // is_taxonomy_hierarchical() ?>
-		<tr class="form-field">
+		<tr class="form-field term-description-wrap">
 			<th scope="row"><label for="description"><?php _ex('Description', 'Taxonomy Description'); ?></label></th>
 			<td><textarea name="description" id="description" rows="5" cols="50" class="large-text"><?php echo $tag->description; // textarea_escaped ?></textarea><br />
 			<span class="description"><?php _e('The description is not prominent by default; however, some themes may show it.'); ?></span></td>
