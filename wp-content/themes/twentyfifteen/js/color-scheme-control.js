e2041b8fbb8e265cb5dc9589a4e98c46c0104651
Andrew Nacin <wp@andrewnacin.com>
2014-10-14 19:48:19 +0000

    Twenty Fifteen: JSHint fixes.  * Define global variables set by wp_localize_script().  * Remove currently unused $ variable.  * Use dot notation when appropriate.  * Update Gruntfile.js to ignore the html5 shiv. see #29799.
    Built from https://develop.svn.wordpress.org/trunk@29893
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29649 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/js/color-scheme-control.js
+++ wp-content/themes/twentyfifteen/js/color-scheme-control.js
@@ -1,10 +1,11 @@
+/* global colorScheme */
 /**
  * Theme Customizer enhancements for a better user experience.
  *
  * Adds listener to Color Scheme control to update other color controls with new values/defaults
  */
 
-( function( wp, $ ) {
+( function( wp ) {
 	wp.customize.controlConstructor.colorScheme = wp.customize.Control.extend( {
 		ready: function() {
 			var parentSection    = this.container.closest( '.control-section' ),
@@ -16,34 +17,34 @@
 			this.setting.bind( 'change', function( value ) {
 				// if Header Text is not hidden, update value
 				if ( 'blank' !== wp.customize( 'header_textcolor' ).get() ) {
-					wp.customize( 'header_textcolor' ).set( colorScheme[value]['colors'][4] );
-					headerTextColor.val( colorScheme[value]['colors'][4] )
-						.data( 'data-default-color', colorScheme[value]['colors'][4] )
-						.wpColorPicker( 'color', colorScheme[value]['colors'][4] )
-						.wpColorPicker( 'defaultColor', colorScheme[value]['colors'][4] );
+					wp.customize( 'header_textcolor' ).set( colorScheme[value].colors[4] );
+					headerTextColor.val( colorScheme[value].colors[4] )
+						.data( 'data-default-color', colorScheme[value].colors[4] )
+						.wpColorPicker( 'color', colorScheme[value].colors[4] )
+						.wpColorPicker( 'defaultColor', colorScheme[value].colors[4] );
 				}
 
 				// update Background Color
-				wp.customize( 'background_color' ).set( colorScheme[value]['colors'][0] );
-				backgroundColor.val( colorScheme[value]['colors'][0] )
-					.data( 'data-default-color', colorScheme[value]['colors'][0] )
-					.wpColorPicker( 'color', colorScheme[value]['colors'][0] )
-					.wpColorPicker( 'defaultColor', colorScheme[value]['colors'][0] );
+				wp.customize( 'background_color' ).set( colorScheme[value].colors[0] );
+				backgroundColor.val( colorScheme[value].colors[0] )
+					.data( 'data-default-color', colorScheme[value].colors[0] )
+					.wpColorPicker( 'color', colorScheme[value].colors[0] )
+					.wpColorPicker( 'defaultColor', colorScheme[value].colors[0] );
 
 				// update Header/Sidebar Background Color
-				wp.customize( 'header_background_color' ).set( colorScheme[value]['colors'][1] );
-				sidebarColor.val( colorScheme[value]['colors'][1] )
-					.data( 'data-default-color', colorScheme[value]['colors'][1] )
-					.wpColorPicker( 'color', colorScheme[value]['colors'][1] )
-					.wpColorPicker( 'defaultColor', colorScheme[value]['colors'][1] );
+				wp.customize( 'header_background_color' ).set( colorScheme[value].colors[1] );
+				sidebarColor.val( colorScheme[value].colors[1] )
+					.data( 'data-default-color', colorScheme[value].colors[1] )
+					.wpColorPicker( 'color', colorScheme[value].colors[1] )
+					.wpColorPicker( 'defaultColor', colorScheme[value].colors[1] );
 
 				// update Sidebar Text Color
-				wp.customize( 'sidebar_textcolor' ).set( colorScheme[value]['colors'][4] );
-				sidebarTextColor.val( colorScheme[value]['colors'][4] )
-					.data( 'data-default-color', colorScheme[value]['colors'][4] )
-					.wpColorPicker( 'color', colorScheme[value]['colors'][4] )
-					.wpColorPicker( 'defaultColor', colorScheme[value]['colors'][4] );
+				wp.customize( 'sidebar_textcolor' ).set( colorScheme[value].colors[4] );
+				sidebarTextColor.val( colorScheme[value].colors[4] )
+					.data( 'data-default-color', colorScheme[value].colors[4] )
+					.wpColorPicker( 'color', colorScheme[value].colors[4] )
+					.wpColorPicker( 'defaultColor', colorScheme[value].colors[4] );
 			} );
 		}
 	} );
-} )( this.wp, jQuery );
+} )( this.wp );
--- wp-content/themes/twentyfifteen/js/functions.js
+++ wp-content/themes/twentyfifteen/js/functions.js
@@ -1,3 +1,4 @@
+/* global screenReaderText */
 /**
  * Theme functions file
  *
