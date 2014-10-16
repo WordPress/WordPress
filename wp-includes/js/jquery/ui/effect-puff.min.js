d79cc6382838dbe7311171c77aa1711e605cd11f
Dominik Schilling <dominikschilling+git@gmail.com>
2014-10-16 19:02:38 +0000

    Update jQuery UI to 1.11.2.
    Changelog: http://jqueryui.com/changelog/1.11.2/
    
    see #29833.
    Built from https://develop.svn.wordpress.org/trunk@29920
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29672 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/jquery/ui/effect-puff.min.js
+++ wp-includes/js/jquery/ui/effect-puff.min.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery UI Effects Puff 1.11.1
+ * jQuery UI Effects Puff 1.11.2
  * http://jqueryui.com
  *
  * Copyright 2014 jQuery Foundation and other contributors
--- wp-includes/js/jquery/ui/position.min.js
+++ wp-includes/js/jquery/ui/position.min.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery UI Position 1.11.1
+ * jQuery UI Position 1.11.2
  * http://jqueryui.com
  *
  * Copyright 2014 jQuery Foundation and other contributors
