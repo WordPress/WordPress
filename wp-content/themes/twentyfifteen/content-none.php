cbc554946bdd2ebe7f4d442233f839feb36247f9
Ian Stewart <ian@themeshaper.com>
2014-10-17 21:16:20 +0000

    Twenty Fifteen: correct escaping to render HTML in no-content admin message.
    Props dmchale, fixes #30011.
    
    Built from https://develop.svn.wordpress.org/trunk@29947
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29696 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/content-none.php
+++ wp-content/themes/twentyfifteen/content-none.php
@@ -18,7 +18,7 @@
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( esc_html__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentyfifteen' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentyfifteen' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 		<?php elseif ( is_search() ) : ?>
 
