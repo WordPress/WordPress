ccf74c19ded73c35ea70588583f409fd987de017
Ian Stewart <ian@themeshaper.com>
2014-10-20 19:21:18 +0000

    Twenty Fifteen: Adding in the twentyfifteen credits action.
    Props iamtakashi, fixes #30051.
    
    
    Built from https://develop.svn.wordpress.org/trunk@29975
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29721 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/footer.php
+++ wp-content/themes/twentyfifteen/footer.php
@@ -14,6 +14,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
+			<?php do_action( 'twentyfifteen_credits' ); ?>
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
