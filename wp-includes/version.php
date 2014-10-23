a58a4b1f75c1ebb6a342a85937ac00e2abaf499b
Andrew Nacin <wp@andrewnacin.com>
2014-10-23 21:00:55 +0000

    4.1-alpha-29953. (Bumping to permanently sync the revision number with https://develop.svn.wordpress.org.)
    git-svn-id: http://core.svn.wordpress.org/trunk@29953 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-alpha-29952';
+$wp_version = '4.1-alpha-29953';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
