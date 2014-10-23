10a752a61ddb6f0dbd9141ae04c8138233eaa960
Ian Stewart <ian@themeshaper.com>
2014-10-23 18:37:19 +0000

    Twenty Fifteen: documentation for the twentyfifteen_credits action
    Props iamtakashi, fixes #30051
    
    
    Built from https://develop.svn.wordpress.org/trunk@29998
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@29738 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/footer.php
+++ wp-content/themes/twentyfifteen/footer.php
@@ -14,7 +14,14 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<?php do_action( 'twentyfifteen_credits' ); ?>
+			<?php
+				/**
+				 * Fires before the Twenty Fifteen footer text for footer customization.
+				 *
+				 * @since Twenty Fifteen 1.0
+				 */
+				do_action( 'twentyfifteen_credits' );
+			?>
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
