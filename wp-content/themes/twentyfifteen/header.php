fdbd6b439bc12d75031e7acdc1e89774084e490b
Ian Stewart <ian@themeshaper.com>
2014-10-24 14:51:19 +0000

    Twenty Fifteen: Use a p for the site description for a better experience when using a screenreader.
    Props rianrietveld, fixes #30057, see #30065.
    
    Built from https://develop.svn.wordpress.org/trunk@30009
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30009 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/header.php
+++ wp-content/themes/twentyfifteen/header.php
@@ -30,7 +30,7 @@
 		<header id="masthead" class="site-header" role="banner">
 			<div class="site-branding">
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+				<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				<button class="secondary-toggle"><?php esc_html_e( 'Menu and widgets', 'twentyfifteen' ); ?></button>
 			</div><!-- .site-branding -->
 		</header><!-- .site-header -->
