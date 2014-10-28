4177b48b1e30856083b49c4488d08fb4dbc7cf1f
Boone Gorges <boonebgorges@gmail.com>
2014-10-28 14:57:22 +0000

    Allow 'slug' param of `get_terms()` to accept an array.
    Props jfarthing84, dlh.
    Fixes #23636.
    Built from https://develop.svn.wordpress.org/trunk@30042
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30042 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/taxonomy.php
+++ wp-includes/taxonomy.php
@@ -1594,7 +1594,7 @@ function get_term_to_edit( $id, $taxonomy ) {
  *     @type string       $fields            Term fields to query for. Accepts 'all' (returns an array of
  *                                           term objects), 'ids' or 'names' (returns an array of integers
  *                                           or strings, respectively. Default 'all'.
- *     @type string       $slug              Slug to return term(s) for. Default empty.
+ *     @type string|array $slug              Optional. Slug(s) to return term(s) for. Default empty.
  *     @type bool         $hierarchical      Whether to include terms that have non-empty descendants (even
  *                                           if $hide_empty is set to true). Default true.
  *     @type string       $search            Search criteria to match terms. Will be SQL-formatted with
@@ -1804,8 +1804,13 @@ function get_terms( $taxonomies, $args = '' ) {
 	}
 
 	if ( ! empty( $args['slug'] ) ) {
-		$slug = sanitize_title( $args['slug'] );
-		$where .= " AND t.slug = '$slug'";
+		if ( is_array( $args['slug'] ) ) {
+			$slug = array_map( 'sanitize_title', $args['slug'] );
+			$where .= " AND t.slug IN ('" . implode( "', '", $slug ) . "')";
+		} else {
+			$slug = sanitize_title( $args['slug'] );
+			$where .= " AND t.slug = '$slug'";
+		}
 	}
 
 	if ( ! empty( $args['name__like'] ) ) {
