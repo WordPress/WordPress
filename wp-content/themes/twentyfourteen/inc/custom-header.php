c604fd15b0955d28a15b5c30fa13b4e0dd242a7f
Lance Willett <nanobar@gmail.com>
2014-11-01 03:44:23 +0000

    Twenty Fourteen: fix keyboard focus on Appearance > Header view.
    Props florianziegler, fixes #29787.
    
    Built from https://develop.svn.wordpress.org/trunk@30146
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30146 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfourteen/inc/custom-header.php
+++ wp-content/themes/twentyfourteen/inc/custom-header.php
@@ -140,7 +140,7 @@ function twentyfourteen_admin_header_image() {
 		<?php if ( get_header_image() ) : ?>
 		<img src="<?php header_image(); ?>" alt="">
 		<?php endif; ?>
-		<h1 class="displaying-header-text"><a id="name"<?php echo sprintf( ' style="color:#%s;"', get_header_textcolor() ); ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+		<h1 class="displaying-header-text"><a id="name"<?php echo sprintf( ' style="color:#%s;"', get_header_textcolor() ); ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>" tabindex="-1"><?php bloginfo( 'name' ); ?></a></h1>
 	</div>
 <?php
 }
