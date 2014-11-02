00c86aea6363cdffbef7db1f2dbc23f860b29f97
Jeremy Felt <jeremy.felt@gmail.com>
2014-11-02 02:07:23 +0000

    Delete admin_created_user_subject() rather than deprecate
    As it was never used as anything more than a callback to a filter before the MU merge, and is only available in user-new.php in multisite, it is safe to remove this function entirely.
    
    Fixes #29915
    
    Built from https://develop.svn.wordpress.org/trunk@30176
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30176 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-alpha-30175';
+$wp_version = '4.1-alpha-30176';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
