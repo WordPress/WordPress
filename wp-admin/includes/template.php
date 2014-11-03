06f49a43a1fb4a8ca6cc4df3513b5e5f38af2f4e
Scott Taylor <scott.c.taylor@mac.com>
2014-11-03 05:45:24 +0000

    Fill in the `@param` types for the args for functions missing them in `wp-admin/includes/template.php`.
    See #30224.
    
    Built from https://develop.svn.wordpress.org/trunk@30196
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30196 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/template.php
+++ wp-admin/includes/template.php
@@ -289,7 +289,7 @@ function wp_popular_terms_checklist( $taxonomy, $default = 0, $number = 10, $ech
  *
  * @since 2.5.1
  *
- * @param unknown_type $link_id
+ * @param int $link_id
  */
 function wp_link_category_checklist( $link_id = 0 ) {
 	$default = 1;
@@ -324,7 +324,7 @@ function wp_link_category_checklist( $link_id = 0 ) {
  *
  * @since 2.7.0
  *
- * @param unknown_type $post
+ * @param WP_Post $post
  */
 function get_inline_data($post) {
 	$post_type_object = get_post_type_object($post->post_type);
@@ -519,7 +519,7 @@ function wp_comment_trashnotice() {
  *
  * @since 1.2.0
  *
- * @param unknown_type $meta
+ * @param array $meta
  */
 function list_meta( $meta ) {
 	// Exit if no meta
@@ -562,9 +562,9 @@ function list_meta( $meta ) {
  *
  * @since 2.5.0
  *
- * @param unknown_type $entry
- * @param unknown_type $count
- * @return unknown
+ * @param array $entry
+ * @param int   $count
+ * @return string
  */
 function _list_meta_row( $entry, &$count ) {
 	static $update_nonce = false;
@@ -1429,7 +1429,7 @@ function settings_errors( $setting = '', $sanitize = false, $hide_on_update = fa
  *
  * @since 2.7.0
  *
- * @param unknown_type $found_action
+ * @param string $found_action
  */
 function find_posts_div($found_action = '') {
 ?>
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-alpha-30195';
+$wp_version = '4.1-alpha-30196';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
