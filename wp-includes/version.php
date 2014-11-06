5ce7ae59df5382e0c9008b9d7cc0170f9f83c81c
Boone Gorges <boonebgorges@gmail.com>
2014-11-06 20:17:22 +0000

    Add tests for `get_adjacent_post_link()` wrappers.
    Props MikeHansenMe.
    See #29663.
    Built from https://develop.svn.wordpress.org/trunk@30264
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30264 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-alpha-30263';
+$wp_version = '4.1-alpha-30264';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
