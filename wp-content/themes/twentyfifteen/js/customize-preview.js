68a6a69a914afedf421cefa13b8ebe3d8f16cad4
Ian Stewart <ian@themeshaper.com>
2014-11-07 21:11:25 +0000

    Twenty Fifteen: updates to customize-preview.js missed in r30274.
    Props celloexpressions, See #29988.
    
    
    Built from https://develop.svn.wordpress.org/trunk@30275
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30275 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/js/customize-preview.js
+++ wp-content/themes/twentyfifteen/js/customize-preview.js
@@ -3,15 +3,30 @@
  */
 
 ( function( $ ) {
+	var $style = $( '#twentyfifteen-color-scheme-css' );
+
+	if ( ! $style.length ) {
+		$style = $( 'head' ).append( '<style type="text/css" id="twentyfifteen-color-scheme-css" />' )
+		                    .find( '#twentyfifteen-color-scheme-css' );
+	}
+
 	// Site title and description.
 	wp.customize( 'blogname', function( value ) {
 		value.bind( function( to ) {
 			$( '.site-title a' ).text( to );
 		} );
 	} );
+
 	wp.customize( 'blogdescription', function( value ) {
 		value.bind( function( to ) {
 			$( '.site-description' ).text( to );
 		} );
 	} );
+	
+	wp.customize( 'color_scheme_css', function( value ) {
+		value.bind( function( to ) {
+			$style.html( to );
+		} );
+	} );
+
 } )( jQuery );
