b6d3190be2b09a82b8c48844f830d058b9d38682
Boone Gorges <boonebgorges@gmail.com>
2014-11-08 15:30:25 +0000

    Share fixtures across a number of query-related test classes.
    This shaves 10-20 seconds off the running time for the suite.
    
    See #30017.
    Built from https://develop.svn.wordpress.org/trunk@30276
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30276 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-alpha-30275';
+$wp_version = '4.1-alpha-30276';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
