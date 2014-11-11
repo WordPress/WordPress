87bd82a0abab551027fb182c09b9f01b7cab5946
Ian Stewart <ian@themeshaper.com>
2014-11-11 19:24:22 +0000

    Twenty Fifteen: simplify the code used to generate screen reader text for the is_home title.
    Props davidakennedy, fixes #30305
    
    
    Built from https://develop.svn.wordpress.org/trunk@30304
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30303 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/index.php
+++ wp-content/themes/twentyfifteen/index.php
@@ -24,7 +24,7 @@ get_header(); ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
 				<header>
-					<h1 class="page-title screen-reader-text"><?php echo esc_html( get_the_title( get_option( 'page_for_posts' ) ) ); ?></h1>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
 			<?php endif; ?>
 
