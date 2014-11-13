92e3890daaeaddc7e6ba19210ce6370b923b9b87
Boone Gorges <boonebgorges@gmail.com>
2014-11-13 12:15:23 +0000

    Correct `@since` tag in `count_user_posts()` docs.
    Props pavelevap, DrewAPicture.
    See #21364.
    Built from https://develop.svn.wordpress.org/trunk@30328
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30327 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-alpha-30327';
+$wp_version = '4.1-alpha-30328';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
