3acf55cf96807706e4d2068c0d19acbe3c1d44c2
Ian Stewart <ian@themeshaper.com>
2014-11-13 03:18:23 +0000

    Twenty Fifteen: cleaning up JS introduced in r30305 that doesn't pass jshint.
    Props boonebgorges, fixes #29988.
    
    
    Built from https://develop.svn.wordpress.org/trunk@30325
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30324 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/js/color-scheme-control.js
+++ wp-content/themes/twentyfifteen/js/color-scheme-control.js
@@ -1,4 +1,4 @@
-/* global colorScheme */
+/* global colorScheme, Color */
 /**
  * Add a listener to the Color Scheme control to update other color controls to new values/defaults.
  * Also trigger an update of the Color Scheme CSS when a color is changed.
@@ -57,12 +57,12 @@
 		});
 
 		// Add additional colors.
-		colors['secondary_textcolor'] = Color( colors.textcolor ).toCSS( 'rgba', 0.7 );
-		colors['border_color'] = Color( colors.textcolor ).toCSS( 'rgba', 0.1 );
-		colors['border_focus_color'] = Color( colors.textcolor ).toCSS( 'rgba', 0.3 );
-		colors['secondary_sidebar_textcolor'] = Color( colors.sidebar_textcolor ).toCSS( 'rgba', 0.7 );
-		colors['sidebar_border_color'] = Color( colors.sidebar_textcolor ).toCSS( 'rgba', 0.1 );
-		colors['sidebar_border_focus_color'] = Color( colors.sidebar_textcolor ).toCSS( 'rgba', 0.3 );
+		colors.secondary_textcolor = Color( colors.textcolor ).toCSS( 'rgba', 0.7 );
+		colors.border_color = Color( colors.textcolor ).toCSS( 'rgba', 0.1 );
+		colors.border_focus_color = Color( colors.textcolor ).toCSS( 'rgba', 0.3 );
+		colors.secondary_sidebar_textcolor = Color( colors.sidebar_textcolor ).toCSS( 'rgba', 0.7 );
+		colors.sidebar_border_color = Color( colors.sidebar_textcolor ).toCSS( 'rgba', 0.1 );
+		colors.sidebar_border_focus_color = Color( colors.sidebar_textcolor ).toCSS( 'rgba', 0.3 );
 
 		return cssTemplate( colors );
 	}
@@ -70,7 +70,7 @@
 	// Update the CSS whenever a color setting is changed.
 	_.each( colorSettings, function( setting ) {
 		api( setting, function( setting ) {
-			setting.bind( _.throttle( function( value ) {
+			setting.bind( _.throttle( function() {
 				api( 'color_scheme_css' ).set( getCSS() );
 			}, 250 ) );
 		} );
