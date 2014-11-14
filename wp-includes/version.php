e61bebdbfc6b4f2af877b4da3e331b8e7bab5ad7
Gary Pendergast <gary@pento.net>
2014-11-14 21:34:22 +0000

    If a saving a post fails, remove any invalid characters (such as emoji) from the primary text fields, then try to save it again.
    See #21212.
    
    
    Built from https://develop.svn.wordpress.org/trunk@30346
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30345 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-alpha-30345';
+$wp_version = '4.1-alpha-30346';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
