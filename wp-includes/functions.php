53a9e3b420a6fac1d9ccbd9a690f3055863de5e2
John Blackbourn <johnbillion@git.wordpress.org>
2014-11-21 16:56:23 +0000

    Add support for `WP_Error` objects passed to `wp_send_json_error()`. The error object gets output as an array of error codes and messages, rather than as an empty object.
    Fixes #28978
    Props paulschreiber
    
    Built from https://develop.svn.wordpress.org/trunk@30506
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30495 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -2801,15 +2801,32 @@ function wp_send_json_success( $data = null ) {
 /**
  * Send a JSON response back to an Ajax request, indicating failure.
  *
+ * If the `$data` parameter is a `WP_Error` object, the errors within the object are processed
+ * and output as an array of error codes and corresponding messages. All other types are
+ * output without further processing.
+ *
  * @since 3.5.0
+ * @since 4.1.0 The `$data` parameter is now processed if a `WP_Error` object is passed in.
  *
  * @param mixed $data Data to encode as JSON, then print and die.
  */
 function wp_send_json_error( $data = null ) {
 	$response = array( 'success' => false );
 
-	if ( isset( $data ) )
-		$response['data'] = $data;
+	if ( isset( $data ) ) {
+		if ( is_wp_error( $data ) ) {
+			$result = array();
+			foreach ( $data->errors as $code => $messages ) {
+				foreach ( $messages as $message ) {
+					$result[] = array( 'code' => $code, 'message' => $message );
+				}
+			}
+
+			$response['data'] = $result;
+		} else {
+			$response['data'] = $data;
+		}
+	}
 
 	wp_send_json( $response );
 }
