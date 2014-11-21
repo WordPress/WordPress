14e6c301ea6fe22679030e621654bf0af5d16f7b
Boone Gorges <boonebgorges@gmail.com>
2014-11-21 03:14:23 +0000

    Pass the taxonomy of the split tt_id to the 'split_shared_term' filter.
    Props mboynes.
    See #30335.
    Built from https://develop.svn.wordpress.org/trunk@30493
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30482 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/taxonomy.php
+++ wp-includes/taxonomy.php
@@ -4125,11 +4125,12 @@ function _split_shared_term( $term_id, $term_taxonomy_id ) {
 	 *
 	 * @since 4.1.0
 	 *
-	 * @param int $term_id          ID of the formerly shared term.
-	 * @param int $new_term_id      ID of the new term created for the $term_taxonomy_id.
-	 * @param int $term_taxonomy_id ID for the term_taxonomy row affected by the split.
+	 * @param int    $term_id          ID of the formerly shared term.
+	 * @param int    $new_term_id      ID of the new term created for the $term_taxonomy_id.
+	 * @param int    $term_taxonomy_id ID for the term_taxonomy row affected by the split.
+	 * @param string $taxonomy         Taxonomy for the split term.
 	 */
-	do_action( 'split_shared_term', $term_id, $new_term_id, $term_taxonomy_id );
+	do_action( 'split_shared_term', $term_id, $new_term_id, $term_taxonomy_id, $term_taxonomy->taxonomy );
 
 	return $new_term_id;
 }
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-beta2-30492';
+$wp_version = '4.1-beta2-30493';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
