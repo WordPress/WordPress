d3604f485d63987ef71caa3c91e312bc7ddfacd0
Andrew Ozz <admin@laptoptips.ca>
2014-11-22 02:43:24 +0000

    TinyMCE: set the image toolbar's z-index to be the same as the other TinyMCE panels, or it is under the editor when in fullscreen mode. See #30147. Built from https://develop.svn.wordpress.org/trunk@30509
    git-svn-id: http://core.svn.wordpress.org/trunk@30498 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-beta2-30508';
+$wp_version = '4.1-beta2-30509';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
