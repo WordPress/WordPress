4ba21e7e0b24355c150f531b021ee04b345d4811
Boone Gorges <boonebgorges@gmail.com>
2014-11-24 20:57:24 +0000

    Fix the unit tests for Imagick alpha.
    These tests were written to use the `assertImageAtAlpha()` helper method, whose
    internals were specific to GD. As a result, the tests could not pass. This
    changeset introduces GD- and Imagick-specific versions of this method.
    
    Props voldemortensen.
    Fixes #24871.
    Built from https://develop.svn.wordpress.org/trunk@30549
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30538 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-beta2-30548';
+$wp_version = '4.1-beta2-30549';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
