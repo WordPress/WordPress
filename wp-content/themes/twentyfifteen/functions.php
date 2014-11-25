7a643924c2d9abbd15592a1f7d3b496670a5bfaa
Ian Stewart <ian@themeshaper.com>
2014-11-25 20:38:25 +0000

    Twenty Fifteen: correct number of menus used in comments
    Props jeherve, see #30149
    
    
    Built from https://develop.svn.wordpress.org/trunk@30568
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30558 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/functions.php
+++ wp-content/themes/twentyfifteen/functions.php
@@ -80,7 +80,7 @@ function twentyfifteen_setup() {
 	add_theme_support( 'post-thumbnails' );
 	set_post_thumbnail_size( 825, 510, true );
 
-	// This theme uses wp_nav_menu() in one location.
+	// This theme uses wp_nav_menu() in two locations.
 	register_nav_menus( array(
 		'primary' => __( 'Primary Menu',      'twentyfifteen' ),
 		'social'  => __( 'Social Links Menu', 'twentyfifteen' ),
