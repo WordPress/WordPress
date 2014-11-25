654047e35b4c6cce79525877d52f90dee7549668
Ian Stewart <ian@themeshaper.com>
2014-11-25 14:58:21 +0000

    Twenty Fifteen: pinching to zoom in on landscape on iPads is breaking the layout with the sticky sidebar. Providing a one-column view on those screens looks good and will likely prevent other bugs we don't even know about.
    Props iamtakashi, fixes #30366.
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@30565
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30554 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/header.php
+++ wp-content/themes/twentyfifteen/header.php
@@ -12,7 +12,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!--[if lt IE 9]>
