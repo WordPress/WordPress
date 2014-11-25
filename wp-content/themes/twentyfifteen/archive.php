7e4486803bc05fe757b16026ed169aa980e51dad
Ian Stewart <ian@themeshaper.com>
2014-11-25 21:11:22 +0000

    Twenty Fifteen: docs correction.
    Props rachelbaker, DrewAPicture, fixes #30456
    
    
    Built from https://develop.svn.wordpress.org/trunk@30572
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30562 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/archive.php
+++ wp-content/themes/twentyfifteen/archive.php
@@ -6,9 +6,8 @@
  * For example, puts together date-based pages if no date.php file exists.
  *
  * If you'd like to further customize these archive views, you may create a
- * new template file for each specific one. For example, Twenty Fifteen
- * already has tag.php for Tag archives, category.php for Category archives,
- * and author.php for Author archives.
+ * new template file for each one. For example, tag.php (Tag archives),
+ * category.php (Category archives), author.php (Author archives), etc.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
