b6ddf3ab5dc2284c25f527ee17bdd0abd8e70b36
Ian Stewart <ian@themeshaper.com>
2014-12-03 18:08:23 +0000

    Twenty Fifteen: It's about time we call it one point oh.
    Closes #30502.
    
    
    Built from https://develop.svn.wordpress.org/trunk@30729
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30719 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-beta2-30728';
+$wp_version = '4.1-beta2-30729';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
