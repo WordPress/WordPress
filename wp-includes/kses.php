ad1ecf88c5d5f6ee0530ca37bd7f455eb43f0d50
Drew Jaynes <info@drewapicture.com>
2014-12-03 11:47:23 +0000

    Only backtick-escape individual HTML entities in the DocBlock for `wp_kses_normalize_entities()`.
    Props TobiasBg.
    Fixes #30473.
    
    Built from https://develop.svn.wordpress.org/trunk@30726
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30716 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/kses.php
+++ wp-includes/kses.php
@@ -1173,7 +1173,7 @@ function wp_kses_bad_protocol_once2( $string, $allowed_protocols ) {
  * Converts and fixes HTML entities.
  *
  * This function normalizes HTML entities. It will convert `AT&T` to the correct
- * `AT&amp;T, &#00058; to &#58;, &#XYZZY; to &amp;#XYZZY;` and so on.
+ * `AT&amp;T`, `&#00058;` to `&#58;`, `&#XYZZY;` to `&amp;#XYZZY;` and so on.
  *
  * @since 1.0.0
  *
