1e9f5755aa30ea4e7b1a0f3dc80aae432200754f
Andrew Ozz <admin@laptoptips.ca>
2014-12-07 03:01:24 +0000

    Admin toolbar: remove unneeded overrides of `z-index` and `left` on screens narrower than 782px. Props avryl, babbardel. Fixes #29867. Built from https://develop.svn.wordpress.org/trunk@30764
    git-svn-id: http://core.svn.wordpress.org/trunk@30754 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-beta2-30763';
+$wp_version = '4.1-beta2-30764';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
