3ec55886cdd9a28e792bc4c654a033b90d419d1c
Ian Stewart <ian@themeshaper.com>
2014-12-08 17:46:23 +0000

    Twenty Fifteen: layout correction for IE8 and IE7 after r30388 introduced a defect with the header there.
    Props iamtakashi, fixes #30630.
    
    
    Built from https://develop.svn.wordpress.org/trunk@30789
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30779 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-beta2-30788';
+$wp_version = '4.1-beta2-30789';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
