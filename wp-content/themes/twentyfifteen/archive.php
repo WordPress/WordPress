2827fd2a64482065cdd5454cc5f9493b83c2bb39
John Blackbourn <johnbillion@git.wordpress.org>
2014-12-11 10:37:23 +0000

    Switch to `(get_)the_posts_pagination()` in Twenty Fifteen.
    Merges [30824] to the 4.1 branch.
    
    See #29808
    Props obenland
    
    Built from https://develop.svn.wordpress.org/branches/4.1@30826
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.1@30816 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/archive.php
+++ wp-content/themes/twentyfifteen/archive.php
@@ -45,7 +45,7 @@ get_header(); ?>
 			endwhile;
 
 			// Previous/next page navigation.
-			the_pagination( array(
+			the_posts_pagination( array(
 				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
 				'next_text'          => __( 'Next page', 'twentyfifteen' ),
 				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
