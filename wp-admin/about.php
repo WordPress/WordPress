a814ef7bcc08b73956da698ebf6555f7d9daea28
Andrew Nacin <wp@andrewnacin.com>
2014-12-18 17:55:22 +0000

    About page: CDN images.
    Built from https://develop.svn.wordpress.org/trunk@30971
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30958 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -44,7 +44,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 <div class="changelog headline-feature">
 	<h2><?php _e( 'Introducing Twenty&nbsp;Fifteen' ); ?></h2>
 	<div class="featured-image">
-		<img src="https://cldup.com/vIHybAE4UL.png" />
+		<img src="//s.w.org/images/core/4.1/theme.png" />
 	</div>
 
 	<div class="feature-section">
@@ -55,7 +55,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 			 <p><?php _e( 'Your content always takes center stage, whether viewed on a phone, tablet, laptop, or desktop computer.' ); ?></p>
 		</div>
 		<div class="col">
-			<img class="" src="https://cldup.com/a2KjmCSvRZ.png" />
+			<img class="" src="//s.w.org/images/core/4.1/mobile.png" />
 		</div>
 	</div>
 
@@ -68,10 +68,10 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 	<h2><?php _e( 'Distraction-free writing' ); ?></h2>
 	<div class="feature-section">
 		<div class="dfw-container">
-			<img src="https://cldup.com/SSDED-NdGa.png" class="base-image" />
-			<img src="https://cldup.com/cKGORq3sEV.png" class="overlay-image fade-in" />
-			<img src="https://cldup.com/sHKt9sCD31.png" class="overlay-image fade-in" />
-			<img src="https://cldup.com/XYpC35yRAU.png" class="overlay-image from-left" />
+			<img src="//s.w.org/images/core/4.1/focus.png" class="base-image" />
+			<img src="//s.w.org/images/core/4.1/focus1.png" class="overlay-image fade-in" />
+			<img src="//s.w.org/images/core/4.1/focus2.png" class="overlay-image fade-in" />
+			<img src="//s.w.org/images/core/4.1/focus3.png" class="overlay-image from-left" />
 		</div>
 		<h3><em><?php _e( 'Just write.' ); ?></em></h3>
 		<p><?php _e( 'Sometimes, you just need to concentrate on putting your thoughts into words. Try turning on <strong>distraction-free writing mode</strong>. When you start typing, all the distractions will fade away, letting you focus solely on your writing. All your editing tools instantly return when you need them.' ); ?></p>
