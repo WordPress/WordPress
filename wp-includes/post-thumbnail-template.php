7f9433308400f00c78bf2866b0b966ea80022ccb
Scott Taylor <scott.c.taylor@mac.com>
2014-12-31 18:09:25 +0000

    Correct a typo in `wp-includes/post-thumbnail-template.php`.
    Props ixkaito.
    Fixes #30867.
    
    Built from https://develop.svn.wordpress.org/trunk@31011
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@30992 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post-thumbnail-template.php
+++ wp-includes/post-thumbnail-template.php
@@ -3,7 +3,7 @@
  * WordPress Post Thumbnail Template Functions.
  *
  * Support for post thumbnails
- * Themes function.php must call add_theme_support( 'post-thumbnails' ) to use these.
+ * Themes functions.php must call add_theme_support( 'post-thumbnails' ) to use these.
  *
  * @package WordPress
  * @subpackage Template
