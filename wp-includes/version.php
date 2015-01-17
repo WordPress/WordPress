8b50aef14c1487acfedc70e0dfba4c780fdc5b1c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-01-17 13:15:23 +0000

    Customizer: Make "Search widgets" input sticky.
    props celloexpressions.
    fixes #30751.
    Built from https://develop.svn.wordpress.org/trunk@31227
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31208 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-alpha-31226';
+$wp_version = '4.2-alpha-31227';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
