81c0f7533219fe62a791b5cad40292db8d501038
Boone Gorges <boonebgorges@gmail.com>
2015-01-20 22:00:23 +0000

    Remove `WP_Post_IDs_Iterator` tests.
    The tests have been moved to a patch on the ticket where the class has been
    proposed, #22435.
    
    See #30284.
    Built from https://develop.svn.wordpress.org/trunk@31254
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31235 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-alpha-31253';
+$wp_version = '4.2-alpha-31254';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
