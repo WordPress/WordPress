a121f09d1cdcc4405c1305c1bc98fa957c655aab
Boone Gorges <boonebgorges@gmail.com>
2015-01-21 18:44:23 +0000

    Use https URLs for wordpress.com assets in `Tests_HTTP_Functions`.
    The http->https redirects were breaking some of the tests.
    
    Fixes #31091.
    Built from https://develop.svn.wordpress.org/trunk@31258
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31239 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-alpha-31257';
+$wp_version = '4.2-alpha-31258';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
