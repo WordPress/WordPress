3982471a5c5d458269aebcba9222cf4a27d3c65e
Lance Willett <nanobar@gmail.com>
2015-01-22 00:56:22 +0000

    Twenty Eleven and Twelve: fill in support for `screen-reader-text` class.
    Props obenland, fixes #31095.
    Built from https://develop.svn.wordpress.org/trunk@31268
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31249 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-alpha-31267';
+$wp_version = '4.2-alpha-31268';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
