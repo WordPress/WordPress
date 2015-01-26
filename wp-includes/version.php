cfba9170806529796a0de2e76c4337c20eaca292
Lance Willett <nanobar@gmail.com>
2015-01-26 15:58:23 +0000

    Twenty Fifteen: move RSS icon style rule lower to prevent it from being overridden by other social icon rules.
    Props iamtakashi, fixes #31129.
    
    Built from https://develop.svn.wordpress.org/trunk@31283
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31264 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-alpha-31282';
+$wp_version = '4.2-alpha-31283';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
