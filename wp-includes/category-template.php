e5a5992e2c6fb97d6b51e32d0ca52da71ece1c42
Drew Jaynes <info@drewapicture.com>
2015-02-06 09:04:22 +0000

    Update the DocBlock for `wp_dropdown_categories()` to reflect that the entire `$args` parameter array is optional instead of individual arugments.
    See #30306.
    
    Built from https://develop.svn.wordpress.org/trunk@31357
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31338 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/category-template.php
+++ wp-includes/category-template.php
@@ -296,47 +296,41 @@ function category_description( $category = 0 ) {
  * @since 4.2.0 Introduced the 'value_field' parameter.
  *
  * @param string|array $args {
- *     Array of arguments.
- *     @type string       $show_option_all   Optional. Text to display for showing all categories.
- *                                           Default is an empty string.
- *     @type string       $show_option_none  Optional. Text to display for showing no categories.
- *                                           Default is an empty string.
- *     @type string       $option_none_value Optional. Value to use when no category is selected.
- *                                           Default is an empty string.
- *     @type string       $orderby           Optional. Which column to use for ordering categories.
- *                                           See {@see get_terms()} for list of accepted values. Default: 'id' (term_id).
- *     @type string       $order             Optional. Whether to order terms in ascending or descending order.
- *                                           Accepts 'ASC' or 'DESC'. Default 'ASC'.
- *     @type bool         $pad_counts        Optional. See {@see get_terms()} for description. Default: false.
- *     @type bool|int     $show_count        Optional. Whether to include post counts. Accepts 0, 1, or their bool
- *                                           equivalents. Default 0.
- *     @type bool|int     $hide_empty        Optional. Whether to hide categories that don't have any posts.
- *                                           Accepts 0, 1, or their bool equivalents. Default 1.
- *     @type int          $child_of          Optional. Term ID to retrieve child terms of. See {@see get_terms()}.
+ *     Optional. Array or string of arguments to generate a categories drop-down element.
+ *
+ *     @type string       $show_option_all   Text to display for showing all categories. Default empty.
+ *     @type string       $show_option_none  Text to display for showing no categories. Default empty.
+ *     @type string       $option_none_value Value to use when no category is selected. Default empty.
+ *     @type string       $orderby           Which column to use for ordering categories. See get_terms() for a list
+ *                                           of accepted values. Default 'id' (term_id).
+ *     @type string       $order             Whether to order terms in ascending or descending order. Accepts 'ASC'
+ *                                           or 'DESC'. Default 'ASC'.
+ *     @type bool         $pad_counts        See get_terms() for an argument description. Default false.
+ *     @type bool|int     $show_count        Whether to include post counts. Accepts 0, 1, or their bool equivalents.
  *                                           Default 0.
- *     @type array|string $exclude           Optional. Array or comma/space-separated string of term ids to exclude.
- *                                           If $include is non-empty, $exclude is ignored.
- *                                           Default empty array.
- *     @type bool|int     $echo              Optional. Whether to echo or return the generated markup. Accepts 0, 1,
- *                                           or their bool equivalents. Default 1.
- *     @type bool|int     $hierarchical      Optional. Whether to traverse the taxonomy hierarchy. Accepts 0, 1, or
- *                                           their bool equivalents. Default: 0.
- *     @type int          $depth             Optional. Maximum depth. Default 0.
- *     @type int          $tab_index         Optional. Tab index for the select element. Default 0 (no tabindex).
- *     @type string       $name              Optional. Value for the 'name' attribute of the select element.
- *                                           Default: 'cat'.
- *     @type string       $id                Optional. Value for the 'id' attribute of the select element.
- *                                           Defaults to the value of $name.
- *     @type string       $class             Optional. Value for the 'class' attribute of the select element.
- *     @type int|string   $selected          Optional. Value of the option that should be selected.
- *     @type string       $value_field       Optional. Term field that should be used to populate the 'value' attribute
+ *     @type bool|int     $hide_empty        Whether to hide categories that don't have any posts. Accepts 0, 1, or
+ *                                           their bool equivalents. Default 1.
+ *     @type int          $child_of          Term ID to retrieve child terms of. See get_terms(). Default 0.
+ *     @type array|string $exclude           Array or comma/space-separated string of term ids to exclude.
+ *                                           If `$include` is non-empty, `$exclude` is ignored. Default empty array.
+ *     @type bool|int     $echo              Whether to echo or return the generated markup. Accepts 0, 1, or their
+ *                                           bool equivalents. Default 1.
+ *     @type bool|int     $hierarchical      Whether to traverse the taxonomy hierarchy. Accepts 0, 1, or their bool
+ *                                           equivalents. Default 0.
+ *     @type int          $depth             Maximum depth. Default 0.
+ *     @type int          $tab_index         Tab index for the select element. Default 0 (no tabindex).
+ *     @type string       $name              Value for the 'name' attribute of the select element. Default 'cat'.
+ *     @type string       $id                Value for the 'id' attribute of the select element. Defaults to the value
+ *                                           of `$name`.
+ *     @type string       $class             Value for the 'class' attribute of the select element. Default 'postform'.
+ *     @type int|string   $selected          Value of the option that should be selected. Default 0.
+ *     @type string       $value_field       Term field that should be used to populate the 'value' attribute
  *                                           of the option elements. Accepts any valid term field: 'term_id', 'name',
  *                                           'slug', 'term_group', 'term_taxonomy_id', 'taxonomy', 'description',
  *                                           'parent', 'count'. Default 'term_id'.
- *     @type string       $taxonomy          Optional. Name of the category to retrieve. Default 'category'.
- *     @type bool         $hide_if_empty     Optional. True to skip generating markup if no categories are found.
+ *     @type string       $taxonomy          Name of the category to retrieve. Default 'category'.
+ *     @type bool         $hide_if_empty     True to skip generating markup if no categories are found.
  *                                           Default false (create select element even if no categories are found).
- *
  * }
  * @return string HTML content only if 'echo' argument is 0.
  */
