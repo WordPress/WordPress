46f98473d12c1edfa8ad0355374ffd0f1391ad25
Dominik Schilling <dominikschilling+git@gmail.com>
2015-02-11 20:57:26 +0000

    Widgets: Add return param for widgets admin page to the "Manage in Customizer" link.
    props tywayne.
    fixes #30888.
    Built from https://develop.svn.wordpress.org/trunk@31420
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31401 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/widgets.php
+++ wp-admin/widgets.php
@@ -316,7 +316,13 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' ); ?>
 	if ( current_user_can( 'customize' ) ) {
 		printf(
 			' <a class="add-new-h2 hide-if-no-customize" href="%1$s">%2$s</a>',
-			admin_url( 'customize.php?autofocus[panel]=widgets' ),
+			esc_url( add_query_arg(
+				array(
+					array( 'autofocus' => array( 'panel' => 'widgets' ) ),
+					'return' => urlencode( wp_unslash( $_SERVER['REQUEST_URI'] ) )
+				),
+				admin_url( 'customize.php' )
+			) ),
 			__( 'Manage in Customizer' )
 		);
 	}
