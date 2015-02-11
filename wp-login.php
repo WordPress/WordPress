35f4e719b2892a206898168f619a9ead01532035
John Blackbourn <johnbillion@git.wordpress.org>
2015-02-11 19:19:26 +0000

    Introduce a `logout_redirect` filter so the redirect destination can be changed when a user logs out. Parameters:
     * string  $redirect_to           The redirect destination URL.
     * string  $requested_redirect_to The requested redirect destination URL passed as a parameter.
     * WP_User $user                  The WP_User object for the user that's logging out. 
    
    Fixes #27617
    Props SergeyBiryukov, johnbillion
    
    Built from https://develop.svn.wordpress.org/trunk@31417
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31398 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-login.php
+++ wp-login.php
@@ -484,9 +484,28 @@ case 'postpass' :
 
 case 'logout' :
 	check_admin_referer('log-out');
+
+	$user = wp_get_current_user();
+
 	wp_logout();
 
-	$redirect_to = !empty( $_REQUEST['redirect_to'] ) ? $_REQUEST['redirect_to'] : 'wp-login.php?loggedout=true';
+	if ( ! empty( $_REQUEST['redirect_to'] ) ) {
+		$redirect_to = $requested_redirect_to = $_REQUEST['redirect_to'];
+	} else {
+		$redirect_to = 'wp-login.php?loggedout=true';
+		$requested_redirect_to = '';
+	}
+
+	/**
+	 * Filter the log out redirect URL.
+	 *
+	 * @since 4.2.0
+	 *
+	 * @param string  $redirect_to           The redirect destination URL.
+	 * @param string  $requested_redirect_to The requested redirect destination URL passed as a parameter.
+	 * @param WP_User $user                  The WP_User object for the user that's logging out.
+	 */
+	$redirect_to = apply_filters( 'logout_redirect', $redirect_to, $requested_redirect_to, $user );
 	wp_safe_redirect( $redirect_to );
 	exit();
 
