1a45201b98079360f3a74110403f3a4111ae2595
Dominik Schilling <dominikschilling+git@gmail.com>
2015-02-11 22:28:29 +0000

    Update grunt-cssjanus to 0.2.4, we no longer need the fork.
    Upstream changes:
    * https://github.com/cssjanus/cssjanus/compare/v1.0.2...v1.1.2
    * https://github.com/cssjanus/grunt-cssjanus/compare/v0.2.2...v0.2.4
    
    fixes #30997.
    Built from https://develop.svn.wordpress.org/trunk@31425
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31406 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-alpha-31424';
+$wp_version = '4.2-alpha-31425';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
