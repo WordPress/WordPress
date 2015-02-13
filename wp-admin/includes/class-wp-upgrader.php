6fd67ad80e82a404213546d29739144652b80c10
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-02-13 04:19:27 +0000

    Correct parameter types for `Plugin_Upgrader::bulk_upgrade()` and `Theme_Upgrader::bulk_upgrade()`.
    props ipm-frommen.
    fixes #31312.
    Built from https://develop.svn.wordpress.org/trunk@31441
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31422 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-upgrader.php
+++ wp-admin/includes/class-wp-upgrader.php
@@ -824,8 +824,8 @@ class Plugin_Upgrader extends WP_Upgrader {
 	 * @since 2.8.0
 	 * @since 3.7.0 The `$args` parameter was added, making clearing the plugin update cache optional.
 	 *
-	 * @param string $plugins Array of the basename paths of the plugins' main files.
-	 * @param array  $args {
+	 * @param array $plugins Array of the basename paths of the plugins' main files.
+	 * @param array $args {
 	 *     Optional. Other arguments for upgrading several plugins at once. Default empty array.
 	 *
 	 *     @type bool $clear_update_cache Whether to clear the plugin updates cache if successful.
@@ -1351,8 +1351,8 @@ class Theme_Upgrader extends WP_Upgrader {
 	 * @since 3.0.0
 	 * @since 3.7.0 The `$args` parameter was added, making clearing the update cache optional.
 	 *
-	 * @param string $themes The theme slugs.
-	 * @param array  $args {
+	 * @param array $themes The theme slugs.
+	 * @param array $args {
 	 *     Optional. Other arguments for upgrading several themes at once. Default empty array.
 	 *
 	 *     @type bool $clear_update_cache Whether to clear the update cache if successful.
