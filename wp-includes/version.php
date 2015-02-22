3ef981335dccfb0e4a26ba0a8940842db7b083b6
Boone Gorges <boonebgorges@gmail.com>
2015-02-22 21:07:25 +0000

    Better image-type support checks in image unit tests.
    PHP can be compiled without support for certain image types. Our unit tests
    should be sensitive to these configurations.
    
    Fixes #31124.
    Built from https://develop.svn.wordpress.org/trunk@31510
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31491 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-alpha-31509';
+$wp_version = '4.2-alpha-31510';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
