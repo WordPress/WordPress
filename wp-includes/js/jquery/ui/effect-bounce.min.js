8f44036ea7e3b749290ac494f00c18912e5242c8
Dominik Schilling <dominikschilling+git@gmail.com>
2015-02-27 10:21:26 +0000

    Update jQuery UI to 1.11.3.
    Changelog:
    * http://jqueryui.com/changelog/1.11.3/
    * https://github.com/jquery/jquery-ui/compare/1.11.2...1.11.3
    
    fixes #31473.
    Built from https://develop.svn.wordpress.org/trunk@31569
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31550 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/jquery/ui/effect-bounce.min.js
+++ wp-includes/js/jquery/ui/effect-bounce.min.js
@@ -1,8 +1,8 @@
 /*!
- * jQuery UI Effects Bounce 1.11.2
+ * jQuery UI Effects Bounce 1.11.3
  * http://jqueryui.com
  *
- * Copyright 2014 jQuery Foundation and other contributors
+ * Copyright jQuery Foundation and other contributors
  * Released under the MIT license.
  * http://jquery.org/license
  *
