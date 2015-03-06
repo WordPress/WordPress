6dd747f27cb66895afdcbc71318de98c18b14dab
Scott Taylor <scott.c.taylor@mac.com>
2015-03-06 21:05:46 +0000

    Update `grunt-sass` to `0.18.0`.
    Props netweb.
    Fixes #31343.
    
    Built from https://develop.svn.wordpress.org/trunk@31648
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31629 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-alpha-31647';
+$wp_version = '4.2-alpha-31648';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
