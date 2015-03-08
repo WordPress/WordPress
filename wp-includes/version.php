ff5b22b872c86bc214a8f6506bc17e28d584160a
Andrew Ozz <admin@laptoptips.ca>
2015-03-08 23:23:26 +0000

    PressThis: fix styling of wpLink. Props Michael-Arestad, see #31373. Built from https://develop.svn.wordpress.org/trunk@31680
    git-svn-id: http://core.svn.wordpress.org/trunk@31661 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-alpha-31679';
+$wp_version = '4.2-alpha-31680';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
