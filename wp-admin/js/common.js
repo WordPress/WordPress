0f34b98bcff197f5922b683011dff060d14458a8
Jeremy Felt <jeremy.felt@gmail.com>
2015-03-08 02:27:27 +0000

    Reveal row actions on focus in the network users list table.
    In the future, we'll re-factor this selector pile in a way that makes things cleaner. See #25408.
    
    Props afercia.
    
    Fixes #31003.
    
    Built from https://develop.svn.wordpress.org/trunk@31674
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31655 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/common.js
+++ wp-admin/js/common.js
@@ -457,7 +457,7 @@ $(document).ready( function() {
 				focusedRowActions.removeClass( 'visible' );
 			}, 30 );
 		}
-	}, 'td.post-title, td.title, td.comment, .tags td.column-name, .bookmarks td.column-name, td.blogname, td.username, .dashboard-comment-wrap' );
+	}, 'td.post-title, td.title, td.comment, .tags td.column-name, .bookmarks td.column-name, td.blogname, .users-network td.column-blogs, td.username, .dashboard-comment-wrap' );
 
 	$('#default-password-nag-no').click( function() {
 		setUserSetting('default_password_nag', 'hide');
