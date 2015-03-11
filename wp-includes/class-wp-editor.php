7289cfcc7c1e675a4b16da0c2899618b4f6e73bc
Andrew Ozz <admin@laptoptips.ca>
2015-03-11 22:57:28 +0000

    TinyMCE: remove block_formats setting, this is now the default in TinyMCE 4.1.9. Props iseulde. Fixes #31585. Built from https://develop.svn.wordpress.org/trunk@31735
    git-svn-id: http://core.svn.wordpress.org/trunk@31716 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-editor.php
+++ wp-includes/class-wp-editor.php
@@ -491,15 +491,6 @@ final class _WP_Editors {
 						],
 						strikethrough: {inline: 'del'}
 					}",
-					'block_formats' =>
-						'Paragraph=p;' .
-						'Pre=pre;' .
-						'Heading 1=h1;' .
-						'Heading 2=h2;' .
-						'Heading 3=h3;' .
-						'Heading 4=h4;' .
-						'Heading 5=h5;' .
-						'Heading 6=h6',
 					'relative_urls' => false,
 					'remove_script_host' => false,
 					'convert_urls' => false,
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-alpha-31734';
+$wp_version = '4.2-alpha-31735';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
