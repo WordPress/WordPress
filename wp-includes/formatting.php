ac61ac62cd52a13971534c7012825a01c49080d8
Gary Pendergast <gary@pento.net>
2015-03-15 11:41:27 +0000

    Smilies: The new smilies added in [31733] and [31745] are larger than the old smilies. While this is taken care of by the CSS on normal pages, it means they're disproportionally large when seen in RSS and email.
    By adding a little bit of inline style to them, we get pleasingly sized smilies everywhere. :-)
    
    See #31242
    
    
    Built from https://develop.svn.wordpress.org/trunk@31781
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31761 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -2064,7 +2064,7 @@ function translate_smiley( $matches ) {
 	 */
 	$src_url = apply_filters( 'smilies_src', includes_url( "images/smilies/$img" ), $img, site_url() );
 
-	return sprintf( '<img src="%s" alt="%s" class="wp-smiley" />', esc_url( $src_url ), esc_attr( $smiley ) );
+	return sprintf( '<img src="%s" alt="%s" class="wp-smiley" style="height: 1em;" />', esc_url( $src_url ), esc_attr( $smiley ) );
 }
 
 /**
