48ce41f15a4fa1f57d71029abdcefdd345722645
Andrew Ozz <admin@laptoptips.ca>
2015-03-16 20:37:28 +0000

    Press This: exclude both bookmarklet.js and bookmarklet.min.js from the main uglify task. Props stephdau. Fixes #31657. Built from https://develop.svn.wordpress.org/trunk@31796
    git-svn-id: http://core.svn.wordpress.org/trunk@31778 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-beta1-31795';
+$wp_version = '4.2-beta1-31796';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
