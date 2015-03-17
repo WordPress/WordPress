d4e919776437b5e30a5524ed3ff63df00b30197a
Drew Jaynes <info@drewapicture.com>
2015-03-17 23:38:27 +0000

    Add a missing DocBlock for the core utility function `_get_cron_lock()`.
    Props mordauk, valendesigns.
    Fixes #31646.
    
    Built from https://develop.svn.wordpress.org/trunk@31804
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31786 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-cron.php
+++ wp-cron.php
@@ -26,7 +26,16 @@ if ( !defined('ABSPATH') ) {
 	require_once( dirname( __FILE__ ) . '/wp-load.php' );
 }
 
-// Uncached doing_cron transient fetch
+/**
+ * Retrieves the cron lock.
+ *
+ * Returns the uncached `doing_cron` transient.
+ *
+ * @ignore
+ * @since 3.3.0
+ *
+ * @return string|false Value of the `doing_cron` transient, 0|false otherwise.
+ */
 function _get_cron_lock() {
 	global $wpdb;
 
