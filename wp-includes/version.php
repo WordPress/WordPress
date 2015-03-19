42d7a802dbe42d0732a7e49b8335f7b85dd696e5
Drew Jaynes <info@drewapicture.com>
2015-03-19 19:19:27 +0000

    WordPress 4.2-beta2
    Built from https://develop.svn.wordpress.org/trunk@31835
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31817 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-beta1-31834';
+$wp_version = '4.2-beta2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
