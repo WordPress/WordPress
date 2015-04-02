3167253b7042d8e4227dbdecca62d4ff7781b571
Gary Pendergast <gary@pento.net>
2015-04-02 00:13:25 +0000

    Unit Tests added in [31970] need to be restricted to run in Multisite only.
    See #17904, #26784.
    
    
    Built from https://develop.svn.wordpress.org/trunk@31978
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@31957 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-beta3-31977';
+$wp_version = '4.2-beta3-31978';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
