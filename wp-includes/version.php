a7341b7c3f54e067f2cd24974a86f82684dd1035
Boone Gorges <boonebgorges@gmail.com>
2015-04-04 22:57:26 +0000

    Use backticks when describing the `value_field` parameter, er, argument.
    See [32020].
    Built from https://develop.svn.wordpress.org/trunk@32021
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32000 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-beta4-32020';
+$wp_version = '4.2-beta4-32021';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
