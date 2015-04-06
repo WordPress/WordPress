0b80f802091fc78994ec6aa9611ddc844057735d
Andrew Ozz <admin@laptoptips.ca>
2015-04-06 20:38:26 +0000

    TinyMCE: update to 4.1.9+. Changes: - Fixed bug where extra empty paragraphs would get deleted in WebKit/Blink due to recent Quriks fix. - Fixed bug where the editor wouldn't work properly on IE 12 due to some required browser sniffing. - Fixed bug where formatting shortcut keys where interfering with Mac OS X screenshot keys. Fixes #31895. Built from https://develop.svn.wordpress.org/trunk@32058
    git-svn-id: http://core.svn.wordpress.org/trunk@32037 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-beta4-32057';
+$wp_version = '4.2-beta4-32058';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 31532;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '4109-20150404';
+$tinymce_version = '4109-20150406';
 
 /**
  * Holds the required PHP version
