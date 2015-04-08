abfddb8f3ec292c6128898f59a2220d7674bf094
Aaron Jorbin <aaron@jorb.in>
2015-04-08 16:50:27 +0000

    Update placeholder for FTP credentials
    Two changes:
     * Remove superfluous __ function call
     * Update domain used
    
    Fixes #31922
    Props Tmeister
    
    
    Built from https://develop.svn.wordpress.org/trunk@32077
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32056 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/file.php
+++ wp-admin/includes/file.php
@@ -1148,7 +1148,7 @@ jQuery(function($){
 ?></p>
 <label for="hostname">
 	<span class="field-title"><?php _e( 'Hostname' ) ?></span>
-	<input name="hostname" type="text" id="hostname" aria-describedby="request-filesystem-credentials-desc" class="code" placeholder="<?php esc_attr_e( __( 'example: www.mysite.com' ) ) ?>" value="<?php echo esc_attr($hostname); if ( !empty($port) ) echo ":$port"; ?>"<?php disabled( defined('FTP_HOST') ); ?> />
+	<input name="hostname" type="text" id="hostname" aria-describedby="request-filesystem-credentials-desc" class="code" placeholder="<?php esc_attr_e( 'example: www.wordpress.org' ) ?>" value="<?php echo esc_attr($hostname); if ( !empty($port) ) echo ":$port"; ?>"<?php disabled( defined('FTP_HOST') ); ?> />
 </label>
 <div class="ftp-username">
 	<label for="username">
