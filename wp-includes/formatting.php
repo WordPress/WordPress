dcf2710bc9d85593f3eed1beedd3168112fe097d
Gary Pendergast <gary@pento.net>
2015-04-09 04:37:30 +0000

    Emoji: Tidy up the `wp_encode_emoji()` regex, and clarify the function comment on Unicode 8 support.
    Built from https://develop.svn.wordpress.org/trunk@32096
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32075 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -4178,8 +4178,8 @@ function print_emoji_detection_script() {
 /**
  * Convert any 4 byte emoji in a string to their equivalent HTML entity.
  *
- * Currently, only Unicode 7 emoji are supported. Unicode 8 emoji will be added
- * when the spec in finalised, along with the new skin-tone modifiers.
+ * Currently, only Unicode 7 emoji are supported. Skin tone modifiers are allowed,
+ * all other Unicode 8 emoji will be added when the spec is finalised.
  *
  * This allows us to store emoji in a DB using the utf8 character set.
  *
@@ -4198,7 +4198,6 @@ function wp_encode_emoji( $content ) {
 		   | \xF0\x9F\x98[\x80-\xBF]        # Smilies
 		   | \xF0\x9F\x99[\x80-\x8F]
 		   | \xF0\x9F\x9A[\x80-\xBF]        # Transport and map symbols
-		   | \xF0\x9F\x99[\x80-\x85]
 		)/x';
 
 		$matches = array();
