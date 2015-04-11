9df7b6ecf61057d4a376f5fb8b990b2daac5fd11
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-04-11 21:46:27 +0000

    Translator comment should just reference placeholder numbers, not the actual placeholders.
    see #31675.
    Built from https://develop.svn.wordpress.org/trunk@32112
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32091 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/widgets.php
+++ wp-includes/widgets.php
@@ -819,7 +819,7 @@ function register_sidebar($args = array()) {
 	$sidebar = wp_parse_args( $args, $defaults );
 
 	if ( $id_is_empty ) {
-		/* translators: %1$s: the id argument, %2$s: sidebar name, %3$s: recommended id value  */
+		/* translators: 1: the id argument, 2: sidebar name, 3: recommended id value */
 		_doing_it_wrong( __FUNCTION__, sprintf( __( 'No %1$s was set in the arguments array for the "%2$s" sidebar. Defaulting to "%3$s". Manually set the %1$s to "%3$s" to silence this notice and keep existing sidebar content.' ), '<code>id</code>', $sidebar['name'], $sidebar['id'] ), '4.2.0' );
 	}
 
