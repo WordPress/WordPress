2a42fc2e734e08721340bfa8fde36abd3fb971f5
Gary Pendergast <gary@pento.net>
2015-04-20 13:39:14 +0000

    Ensure post titles are correctly escaped on the Dashboard. Merge of [32175] to the 3.9 branch.
    Props helen, ocean90, dd32, pento.
    
    
    Built from https://develop.svn.wordpress.org/branches/3.9@32204
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.9@32177 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/dashboard.php
+++ wp-admin/includes/dashboard.php
@@ -531,7 +531,7 @@ function wp_dashboard_recent_drafts( $drafts = false ) {
 function _wp_dashboard_recent_comments_row( &$comment, $show_date = true ) {
 	$GLOBALS['comment'] =& $comment;
 
-	$comment_post_title = strip_tags(get_the_title( $comment->comment_post_ID ));
+	$comment_post_title = _draft_or_post_title( $comment->comment_post_ID );
 
 	if ( current_user_can( 'edit_post', $comment->comment_post_ID ) ) {
 		$comment_post_url = get_edit_post_link( $comment->comment_post_ID );
