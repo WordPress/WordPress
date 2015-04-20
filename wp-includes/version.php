dea554503267eebca11bc19a7043bf889b3475e1
Gary Pendergast <gary@pento.net>
2015-04-20 06:13:25 +0000

    Update the Plupload Flash file to the latest version.
    Props azaozz.
    
    
    Built from https://develop.svn.wordpress.org/trunk@32168
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32143 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-RC1-32166';
+$wp_version = '4.2-RC1-32168';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
