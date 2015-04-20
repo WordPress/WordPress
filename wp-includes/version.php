285af6e86cb11c8e29bc1d9244d4ca1bebaca432
Gary Pendergast <gary@pento.net>
2015-04-20 03:34:41 +0000

    The 4.0 branch is now 4.0.2-alpha.
    Built from https://develop.svn.wordpress.org/branches/4.0@32153
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.0@32128 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.0.1';
+$wp_version = '4.0.2-alpha';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
