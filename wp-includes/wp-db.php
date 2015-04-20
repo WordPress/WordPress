530a462ba155fadf6655cb39aad03084b0585902
Gary Pendergast <gary@pento.net>
2015-04-20 19:06:42 +0000

    Fix some `wpdb::check_safe_collation()` calls missed in [32181].
    Built from https://develop.svn.wordpress.org/branches/4.0@32225
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.0@32199 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -2029,6 +2029,11 @@ class wpdb {
 	 */
 	public function get_var( $query = null, $x = 0, $y = 0 ) {
 		$this->func_call = "\$db->get_var(\"$query\", $x, $y)";
+
+		if ( $this->check_safe_collation( $query ) ) {
+			$this->check_current_query = false;
+		}
+
 		if ( $query )
 			$this->query( $query );
 
@@ -2056,6 +2061,11 @@ class wpdb {
 	 */
 	public function get_row( $query = null, $output = OBJECT, $y = 0 ) {
 		$this->func_call = "\$db->get_row(\"$query\",$output,$y)";
+
+		if ( $this->check_safe_collation( $query ) ) {
+			$this->check_current_query = false;
+		}
+
 		if ( $query )
 			$this->query( $query );
 		else
@@ -2092,6 +2102,10 @@ class wpdb {
 	 * @return array Database query result. Array indexed from 0 by SQL result row number.
 	 */
 	public function get_col( $query = null , $x = 0 ) {
+		if ( $this->check_safe_collation( $query ) ) {
+			$this->check_current_query = false;
+		}
+
 		if ( $query )
 			$this->query( $query );
 
@@ -2118,6 +2132,10 @@ class wpdb {
 	 */
 	public function get_results( $query = null, $output = OBJECT ) {
 		$this->func_call = "\$db->get_results(\"$query\", $output)";
+
+		if ( $this->check_safe_collation( $query ) ) {
+			$this->check_current_query = false;
+		}
 
 		if ( $query )
 			$this->query( $query );
