3e99df28588a0693baba37bb088aae1dd9ac035a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-04-22 06:04:29 +0000

    About page design for 4.2.
    props melchoyce, ryelle.
    see #31929.
    Built from https://develop.svn.wordpress.org/trunk@32260
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32231 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -41,90 +41,69 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 	</a>
 </h2>
 
-<div class="changelog headline-feature dfw">
-	<h2>[video]</h2>
+<div class="headline-feature feature-video">
+	<?php
+		// echo wp_video_shortcode( array(
+		// 	// 'mp4'      => '//s.w.org/images/core/3.9/widgets.mp4',
+		// 	'ogv'      => '//videos.files.wordpress.com/bUdzKMro/wordpress-4-0_fmt1.ogv',
+		// 	// 'webm'     => '//s.w.org/images/core/3.9/widgets.webm',
+		// 	'loop'     => false,
+		// ) );
+	?>
+	<embed type="application/x-shockwave-flash" src="https://v0.wordpress.com/player.swf?v=1.04" width="1000" height="560" wmode="direct" seamlesstabbing="true" allowfullscreen="true" allowscriptaccess="always" overstretch="true" flashvars="guid=bUdzKMro&amp;isDynamicSeeking=true"></embed>
+</div>
 
-	<div class="feature-section">
-		<div class="dfw-container">
-			<img src="//s.w.org/images/core/4.1/focus.png" class="base-image" />
-		</div>
+<hr />
+
+<div class="feature-section two-col">
+	<div class="col">
 		<h3><?php _e( 'An easier way to share content' ); ?></h3>
 		<p><?php printf( __( 'Clip it, edit it, publish it. Get familiar with the new and improved Press This. From the <a href="%s">Tools</a> menu, add Press This to your browser bookmark bar or your mobile device home screen. Once installed you can share your content with lightning speed. Sharing your favorite videos, images, and content has never been this fast or this easy.' ), admin_url( 'tools.php' ) ); ?></p>
-		<p><?php _e( 'Drag the bookmarklet below to your bookmarks bar. Then, when you&#8217;re on a page you want to share, simply &#8220;press&#8221; it.' ); ?> [bookmarklet]</p>
+		<p><?php _e( 'Drag the bookmarklet below to your bookmarks bar. Then, when you&#8217;re on a page you want to share, simply &#8220;press&#8221; it.' ); ?></p>
+
+		<p class="pressthis-bookmarklet-demo">
+			<a class="pressthis-bookmarklet" onclick="return false;" href="<?php echo htmlspecialchars( get_shortcut_link() ); ?>"><span><?php _e( 'Press This' ); ?></span></a>
+		</p>
 	</div>
 
-</div>
-
-<hr />
-
-<div class="changelog headline-feature">
-
-	<div class="feature-section">
-		<div class="col">
-			<h3><?php _e( 'Extended character support' ); ?></h3>
-			<p><?php _e( 'Writing in WordPress, whatever your language, just got better. WordPress 4.2 supports a host of new characters out-of-the-box, including native Chinese, Japanese, and Korean characters, musical and mathematical symbols, and hieroglyphs.' ); ?></p>
-			<p><?php
-				/* translators: 1: heart emoji, 2: frog face emoji, 3, monkey emoji, 4: pizza emoji, 5: Emoji Codex link */
-				printf( __( 'Don&#8217;t use any of those characters? You can still have fun &mdash; emoji are now available in WordPress! Get creative and decorate your content with %1$s, %2$s, %3$s, %4$s, and all the many other <a href="%5$s">emoji</a>.' ), '&#x1F499', '&#x1F438', '&#x1F412', '&#x1F355', __( 'https://codex.wordpress.org/Emoji' ) );
-			?></p>
-		</div>
-		<div class="col">
-			<img class="" src="//s.w.org/images/core/4.1/mobile.png" />
-		</div>
-	</div>
-
-	<div class="clear"></div>
-</div>
-
-<hr />
-
-<div class="changelog customize">
-	<div class="feature-section col three-col">
-		<div>
-			<?php
-			echo wp_video_shortcode( array(
-				'mp4'      => '//s.w.org/images/core/3.9/widgets.mp4',
-				'ogv'      => '//s.w.org/images/core/3.9/widgets.ogv',
-				'webm'     => '//s.w.org/images/core/3.9/widgets.webm',
-				'loop'     => true,
-				'height'   => 218
-			) );
-			?>
-			<h4><?php _e( 'Switch themes in the Customizer' ); ?></h4>
-			<p><?php _e( 'Browse and preview your installed themes from the Customizer. Make sure the theme looks great with <em>your</em> content, before it debuts on your site. ' ); ?></p>
-		</div>
-		<div>
-			<?php
-			echo wp_video_shortcode( array(
-				'mp4'      => '//s.w.org/images/core/3.9/widgets.mp4',
-				'ogv'      => '//s.w.org/images/core/3.9/widgets.ogv',
-				'webm'     => '//s.w.org/images/core/3.9/widgets.webm',
-				'loop'     => true,
-				'height'   => 218
-			) );
-			?>
-			<h4><?php _e( 'Even more embeds' ); ?></h4>
-			<p><?php _e( 'Paste links from Tumblr.com and Kickstarter and watch them magically appear right in the editor. With every release, your publishing and editing experience get closer together.' ); ?></p>
-		</div>
-		<div class="last-feature">
-			<?php
-			echo wp_video_shortcode( array(
-				'mp4'      => '//s.w.org/images/core/3.9/widgets.mp4',
-				'ogv'      => '//s.w.org/images/core/3.9/widgets.ogv',
-				'webm'     => '//s.w.org/images/core/3.9/widgets.webm',
-				'loop'     => true,
-				'height'   => 218
-			) );
-			?>
-			<h4><?php _e( 'Streamlined plugin updates' ); ?></h4>
-			<p><?php _e( 'Goodbye boring loading screen, hello smooth and simple plugin updates. Click <em>Update&nbsp;Now</em> and watch the magic happen.' ); ?></p>
-		</div>
+	<div class="col">
+		<img src="https://cldup.com/xMJ2QCxvcz.jpg" />
 	</div>
 </div>
 
-<hr />
+<div class="feature-section two-col">
+	<div class="col">
+		<img src="https://cldup.com/q-2svtgKe1.jpg" />
+	</div>
+	<div class="col">
+		<h3><?php _e( 'Extended character support' ); ?></h3>
+		<p><?php _e( 'Writing in WordPress, whatever your language, just got better. WordPress 4.2 supports a host of new characters out-of-the-box, including native Chinese, Japanese, and Korean characters, musical and mathematical symbols, and hieroglyphs.' ); ?></p>
+		<p><?php
+			/* translators: 1: heart emoji, 2: frog face emoji, 3, monkey emoji, 4: pizza emoji, 5: Emoji Codex link */
+			printf( __( 'Don&#8217;t use any of those characters? You can still have fun &mdash; emoji are now available in WordPress! Get creative and decorate your content with %1$s, %2$s, %3$s, %4$s, and all the many other <a href="%5$s">emoji</a>.' ), '&#x1F499', '&#x1F438', '&#x1F412', '&#x1F355', __( 'https://codex.wordpress.org/Emoji' ) );
+		?></p>
+	</div>
+</div>
 
-<div class="changelog under-the-hood">
+<div class="changelog feature-section three-col">
+	<div>
+		<img src="https://cldup.com/LKcPHn8rdr.png" />
+		<h3><?php _e( 'Switch themes in the Customizer' ); ?></h3>
+		<p><?php _e( 'Browse and preview your installed themes from the Customizer. Make sure the theme looks great with <em>your</em> content, before it debuts on your site. ' ); ?></p>
+	</div>
+	<div>
+		<img src="https://cldup.com/h0VOskQA1C.png" />
+		<h3><?php _e( 'Even more embeds' ); ?></h3>
+		<p><?php _e( 'Paste links from Tumblr.com and Kickstarter and watch them magically appear right in the editor. With every release, your publishing and editing experience get closer together.' ); ?></p>
+	</div>
+	<div class="last-feature">
+		<img src="https://cldup.com/1_8tBZpvva.png" />
+		<h3><?php _e( 'Streamlined plugin updates' ); ?></h3>
+		<p><?php _e( 'Goodbye boring loading screen, hello smooth and simple plugin updates. Click <em>Update&nbsp;Now</em> and watch the magic happen.' ); ?></p>
+	</div>
+</div>
+
+<div class="changelog under-the-hood feature-list">
 	<h3><?php _e( 'Under the Hood' ); ?></h3>
 
 	<div class="feature-section col two-col">
