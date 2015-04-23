fa12b4a44c55e1c30a95639d8ba62e0305954b16
Gary Pendergast <gary@pento.net>
2015-04-23 11:48:16 +0000

    WPDB: When sanity checking a string by sending it to MySQL for conversion checks, the incorrect data structure was being returned from wpdb::strip_invalid_text(), causing all write queries to fail for some character sets when the query contained non-ASCII characters.
    Merge of [32261] to the 3.9 branch.
    
    See #32051.
    
    
    Built from https://develop.svn.wordpress.org/branches/3.9@32273
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.9@32244 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -2480,6 +2480,7 @@ class wpdb {
 
 					// Split the CONVERT() calls by charset, so we can make sure the connection is right
 					$queries[ $value['charset'] ][ $col ] = $this->prepare( "CONVERT( %s USING {$value['charset']} )", $value['value'] );
+					unset( $data[ $col ]['db'] );
 				}
 			}
 
