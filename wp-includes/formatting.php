89bf1266269516ac121699a725378380096789ca
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-04-26 10:36:28 +0000

    Check if `SCRIPT_DEBUG` is defined before using it in `print_emoji_detection_script()`.
    props Craig Ralston.
    fixes #32118 for trunk.
    Built from https://develop.svn.wordpress.org/trunk@32296
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32267 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -4141,7 +4141,7 @@ function print_emoji_detection_script() {
 
 	$version = 'ver=' . $wp_version;
 
-	if ( SCRIPT_DEBUG ) {
+	if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
 		$settings['source'] = array(
 			/** This filter is documented in wp-includes/class.wp-scripts.php */
 			'wpemoji' => apply_filters( 'script_loader_src', includes_url( "js/wp-emoji.js?$version" ), 'wpemoji' ),
