51402dc52013b831b8ac9447fad69ba937d3b5fb
Gary Pendergast <gary@pento.net>
2015-04-27 16:04:25 +0000

    When upgrading to 4.2.1, delete suspicious comments.
    Built from https://develop.svn.wordpress.org/trunk@32308
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32279 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,14 +4,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-alpha-32306';
+$wp_version = '4.3-alpha-32308';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 31532;
+$wp_db_version = 32308;
 
 /**
  * Holds the TinyMCE version
