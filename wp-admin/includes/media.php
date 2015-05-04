08e3572b7c4cbe78f465e121da34515bcb6f1a13
Drew Jaynes <info@drewapicture.com>
2015-05-04 01:13:25 +0000

    Add a missing DocBlock for `wp_caption_input_textarea()`.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@32341
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32312 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/media.php
+++ wp-admin/includes/media.php
@@ -1050,6 +1050,14 @@ function image_link_input_fields($post, $url_type = '') {
 ";
 }
 
+/**
+ * Output a textarea element for inputting an attachment caption.
+ *
+ * @since 3.4.0
+ *
+ * @param WP_Post $edit_post Attachment WP_Post object.
+ * @return string HTML markup for the textarea element.
+ */
 function wp_caption_input_textarea($edit_post) {
 	// Post data is already escaped.
 	$name = "attachments[{$edit_post->ID}][post_excerpt]";
