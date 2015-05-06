464ce8a75f6004acd70bd113e73f943a68095921
Michael Adams <mda@blogwaffe.com>
2015-05-06 21:53:15 +0000

    Upgrade: Ensure unintelligible DB schemas don't result in content loss.
    Merge of [32417] to the 3.9 branch.
    
    See #32165.
    
    Props ocean90.
    
    Built from https://develop.svn.wordpress.org/branches/3.9@32421
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.9@32391 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/upgrade.php
+++ wp-admin/includes/upgrade.php
@@ -1305,7 +1305,7 @@ function upgrade_397() {
 			);
 		}
 
-		if ( 'byte' !== $content_length['type'] ) {
+		if ( 'byte' !== $content_length['type'] || 0 === $content_length['length'] ) {
 			// Sites with malformed DB schemas are on their own.
 			return;
 		}
