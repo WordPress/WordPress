6e7fff514b9c4930090aeeb19fe74662584f1634
Gary Pendergast <gary@pento.net>
2015-05-06 05:01:26 +0000

    WPDB: Allow queries to reference tables in the `dbname.tablename` format, and allow table names to contain any valid character, rather than just ASCII.
    Props pento, willstedt for the initial patch.
    
    See #32090.
    
    
    Built from https://develop.svn.wordpress.org/trunk@32368
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32338 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-alpha-32366';
+$wp_version = '4.3-alpha-32368';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
