ba2de1e73676a2768a9c82ba584db14e4d093074
Michael Adams <mda@blogwaffe.com>
2015-05-07 01:12:29 +0000

    Upgrade: Since MySQL may auto-expand column widths when changing a column's character set, we must do our length-based comment checks prior to the character set changes.
    Props jorbin, et alii.
    
    Merges [32440] for the 4.2 branch.
    
    See #32165.
    
    Built from https://develop.svn.wordpress.org/branches/4.2@32441
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.2@32411 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/upgrade.php
+++ wp-admin/includes/upgrade.php
@@ -524,11 +524,14 @@ function upgrade_all() {
 	if ( $wp_current_db_version < 29630 )
 		upgrade_400();
 
-	if ( $wp_current_db_version < 31351 )
-		upgrade_420();
-
+	// Don't harsh my mellow. upgrade_422() must be called before
+	// upgrade_420() to catch bad comments prior to any auto-expansion of
+	// MySQL column widths.
 	if ( $wp_current_db_version < 31534 )
 		upgrade_422();
+
+	if ( $wp_current_db_version < 31351 )
+		upgrade_420();
 
 	maybe_disable_link_manager();
 
