837fd1ca1069b43dcbfd6e93bd62ffb219610d5c
Gary Pendergast <gary@pento.net>
2015-05-08 11:59:26 +0000

    When saving Writing Options, check that the UI is enabled for `use_smilies` and `use_balanceTags` before trying to update them.
    Fixes #5161.
    Fixes #32298.
    
    
    Built from https://develop.svn.wordpress.org/trunk@32455
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32425 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-alpha-32454';
+$wp_version = '4.3-alpha-32455';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
