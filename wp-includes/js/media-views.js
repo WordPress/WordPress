2c42a665a2ce5448c53ad94dcfb41104678fa238
Scott Taylor <scott.c.taylor@mac.com>
2015-05-08 20:39:27 +0000

    `wp.media.view.Button` should create `<button>`s, not `<a href="#">`s.
    Props afercia.
    See #32236.
    
    Built from https://develop.svn.wordpress.org/trunk@32464
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32434 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/media-views.js
+++ wp-includes/js/media-views.js
@@ -4211,9 +4211,9 @@ module.exports = ButtonGroup;
  * @augments Backbone.View
  */
 var Button = wp.media.View.extend({
-	tagName:    'a',
+	tagName:    'button',
 	className:  'media-button',
-	attributes: { href: '#' },
+	attributes: { type: 'button' },
 
 	events: {
 		'click': 'click'
