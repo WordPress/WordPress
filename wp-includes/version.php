7053929095b6be6adfa7ac3ab1055585ca007a1e
Lance Willett <nanobar@gmail.com>
2015-05-11 21:42:27 +0000

    Twenty Fifteen: update readme file.
    Props mercime, lancewillett. See #31814.
    Built from https://develop.svn.wordpress.org/trunk@32487
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32457 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-alpha-32486';
+$wp_version = '4.3-alpha-32487';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
