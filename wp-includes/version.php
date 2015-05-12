da99450d30b411b3ffc62dca71f37253d6a2e20b
John Blackbourn <johnbillion@git.wordpress.org>
2015-05-12 10:47:29 +0000

    Add a sanity check to `theme-compat/header.php` to avoid outputting CSS which could reference a non-existant file.
    Props Mte90
    
    Fixes 32240
    
    Built from https://develop.svn.wordpress.org/trunk@32496
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32466 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-alpha-32495';
+$wp_version = '4.3-alpha-32496';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
