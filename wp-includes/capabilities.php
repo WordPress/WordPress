e90a32769485ad8c2196889664c74ac7b8e90f7e
Drew Jaynes <info@drewapicture.com>
2015-05-22 17:38:25 +0000

    Add a missing `@since` version to a function introduced in [32541].
    See #32444.
    
    Built from https://develop.svn.wordpress.org/trunk@32548
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32518 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/capabilities.php
+++ wp-includes/capabilities.php
@@ -1474,10 +1474,13 @@ function user_can( $user, $capability ) {
 }
 
 /**
- * Retrieve the global WP_Roles instance, instantiate if necessary
+ * Retrieve the global WP_Roles instance, instantiate if necessary.
  *
- * @global WP_Roles $wp_roles
- * @return WP_Roles global instance
+ * @since 4.3.0
+ *
+ * @global WP_Roles $wp_roles WP_Roles global instance.
+ *
+ * @return WP_Roles WP_Roles global instance if not already instantiated.
  */
 function wp_roles() {
 	global $wp_roles;
