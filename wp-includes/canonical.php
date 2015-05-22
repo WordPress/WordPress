3d130ba909e24885924b10ef6a882d05b70a4f8b
Scott Taylor <scott.c.taylor@mac.com>
2015-05-22 04:26:26 +0000

    Add missing doc blocks for `canonical.php`.
    See #32444.
    
    Built from https://develop.svn.wordpress.org/trunk@32540
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32510 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/canonical.php
+++ wp-includes/canonical.php
@@ -27,8 +27,11 @@
  * or query in an attempt to figure the correct page to go to.
  *
  * @since 2.3.0
- * @uses $wp_rewrite
- * @uses $is_IIS
+ *
+ * @global WP_Rewrite $wp_rewrite
+ * @global bool $is_IIS
+ * @global WP_Query $wp_query
+ * @global wpdb $wpdb
  *
  * @param string $requested_url Optional. The URL that was requested, used to
  *		figure if redirect is needed.
@@ -524,6 +527,7 @@ function _remove_qs_args_if_not_in_url( $query_string, Array $args_to_check, $ur
  * @since 2.3.0
  *
  * @global wpdb $wpdb WordPress database abstraction object.
+ * @global WP_Rewrite $wp_rewrite
  *
  * @return false|string The correct URL if one is found. False on failure.
  */
@@ -560,6 +564,10 @@ function redirect_guess_404_permalink() {
 	return false;
 }
 
+/**
+ *
+ * @global WP_Rewrite $wp_rewrite
+ */
 function wp_redirect_admin_locations() {
 	global $wp_rewrite;
 	if ( ! ( is_404() && $wp_rewrite->using_permalinks() ) )
