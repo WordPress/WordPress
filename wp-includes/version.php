053790537faf25b18c1ed1cd141933d15d9373ed
Scott Taylor <scott.c.taylor@mac.com>
2015-05-25 17:32:27 +0000

    Cleanup doc blocks in `http.php`. In the few functions that used `$objFetchSite` instead of `$http`: use the `$http` naming, which is more civilized.
    See #32444.
    
    Built from https://develop.svn.wordpress.org/trunk@32599
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32569 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-alpha-32598';
+$wp_version = '4.3-alpha-32599';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
