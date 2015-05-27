4c6d8467e213b636624df10bd297c75e0def5122
Scott Taylor <scott.c.taylor@mac.com>
2015-05-27 16:06:25 +0000

    Add missing doc blocks to `post-formats.php`. Correct some types for `@param` and `@return`.
    See #32444.
    
    Built from https://develop.svn.wordpress.org/trunk@32616
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32586 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-alpha-32615';
+$wp_version = '4.3-alpha-32616';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
