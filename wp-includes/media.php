ff3f2ac24371ac5bfb0e10b67d1e3921a822e2ca
John Blackbourn <johnbillion@git.wordpress.org>
2015-05-31 16:56:26 +0000

    Remove an embed handler accidentally introduced in r32673.
    Built from https://develop.svn.wordpress.org/trunk@32674
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32644 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/media.php
+++ wp-includes/media.php
@@ -2313,7 +2313,6 @@ function wp_maybe_load_embeds() {
 	}
 
 	wp_embed_register_handler( 'youtube_embed_url', '#https?://(www.)?youtube\.com/embed/([^/]+)#i', 'wp_embed_handler_youtube' );
-	wp_embed_register_handler( 'youtube_v_url', '#https?://(www.)?youtube\.com/v/([^/]+)#i', 'wp_embed_handler_youtube' );
 
 	wp_embed_register_handler( 'googlevideo', '#http://video\.google\.([A-Za-z.]{2,5})/videoplay\?docid=([\d-]+)(.*?)#i', 'wp_embed_handler_googlevideo' );
 
