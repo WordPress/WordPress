4f9682f39e2cce6aa1a443f7f74150ba81849ac4
Andrew Ozz <admin@laptoptips.ca>
2015-06-01 18:09:26 +0000

    TinyMCE wpView: preserve HTML wrapped in shortcodes. Fixes #32078. Built from https://develop.svn.wordpress.org/trunk@32678
    git-svn-id: http://core.svn.wordpress.org/trunk@32648 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-alpha-32677';
+$wp_version = '4.3-alpha-32678';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
