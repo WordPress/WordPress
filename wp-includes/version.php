38d59c0828478528490ea70f37542eee9c61d788
Aaron Jorbin <aaron@jorb.in>
2015-06-14 19:23:26 +0000

    Improve dbDelta unit tests
    Add new tests for more parts of dbDelta.  This was cleaned up and prepared for commit at #wcphilly contributor day.
    
    Props jdgrimes, tryon, jtsternberg, ebinnion, JPry, avnarun, kevkoeh, jorbin, salcode.
    Fixes #29020.
    
    
    Built from https://develop.svn.wordpress.org/trunk@32770
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32741 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-alpha-32769';
+$wp_version = '4.3-alpha-32770';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
