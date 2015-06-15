629545aaf5c047c349d3df7ccc9d10558c89cf27
Dion Hulse <wordpress@dd32.id.au>
2015-06-15 04:28:25 +0000

    Updates: When an update fails (and it's not during the credential form) allow further updates to continue, with the assumption that the error only affects a single plugin. Props magicroundabout. Fixes #32110 for trunk.
    Built from https://develop.svn.wordpress.org/trunk@32780
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32751 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/updates.js
+++ wp-admin/js/updates.js
@@ -266,7 +266,15 @@ window.wp = window.wp || {};
 		$message.html( wp.updates.l10n.updateFailed.replace( '%s', response.error ) );
 		wp.a11y.speak( wp.updates.l10n.updateFailed );
 
+		/*
+		 * The lock can be released since this failure was
+		 * after the credentials form.
+		 */
+		wp.updates.updateLock = false;
+
 		$(document).trigger( 'wp-plugin-update-error', response );
+
+		wp.updates.queueChecker();
 	};
 
 	/**
