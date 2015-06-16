25829e05fa4956c231506687f55ca1148a453598
Drew Jaynes <info@drewapicture.com>
2015-06-16 23:18:25 +0000

    Add line-wrapping to a long return description because readability in documentation is important.
    See [32797]. See #32421.
    
    Built from https://develop.svn.wordpress.org/trunk@32810
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32781 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -4505,8 +4505,9 @@ function send_frame_options_header() {
  *
  * @staticvar array $protocols
  *
- * @return array Array of allowed protocols.
- *               Defaults to 'http', 'https', 'ftp', 'ftps', 'mailto', 'news', 'irc', 'gopher', 'nntp', 'feed', 'telnet', 'mms', 'rtsp', 'svn', 'tel', 'fax', 'xmpp', 'webcal'
+ * @return array Array of allowed protocols. Defaults to an array containing 'http', 'https',
+ *               'ftp', 'ftps', 'mailto', 'news', 'irc', 'gopher', 'nntp', 'feed', 'telnet',
+ *               'mms', 'rtsp', 'svn', 'tel', 'fax', 'xmpp', and 'webcal'.
  */
 function wp_allowed_protocols() {
 	static $protocols = array();
