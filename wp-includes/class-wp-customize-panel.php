50e32a19692725c3f583f3a3f0d3d4550cb21547
Konstantin Obenland <obenland@automattic.com>
2015-06-19 08:37:27 +0000

    Keep `WP_Customize_Nav_Menus_Panel` with other panels rather than controls.
    Accidentally merged into the wrong file in [32806].
    H/t celloexpressions for noticing.
    
    See https://wordpress.slack.com/archives/core-customize/p1434696254000258.
    See #32576.
    
    
    Built from https://develop.svn.wordpress.org/trunk@32855
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32826 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-customize-panel.php
+++ wp-includes/class-wp-customize-panel.php
@@ -380,3 +380,98 @@ class WP_Customize_Panel {
 		<?php
 	}
 }
+
+/**
+ * Customize Nav Menus Panel Class
+ *
+ * Needed to add screen options.
+ *
+ * @since 4.3.0
+ */
+class WP_Customize_Nav_Menus_Panel extends WP_Customize_Panel {
+
+	/**
+	 * Control type.
+	 *
+	 * @since 4.3.0
+	 *
+	 * @access public
+	 * @var string
+	 */
+	public $type = 'nav_menus';
+
+	/**
+	 * Render screen options for Menus.
+	 *
+	 * @since 4.3.0
+	 */
+	public function render_screen_options() {
+		// Essentially adds the screen options.
+		add_filter( 'manage_nav-menus_columns', array( $this, 'wp_nav_menu_manage_columns' ) );
+
+		// Display screen options.
+		$screen = WP_Screen::get( 'nav-menus.php' );
+		$screen->render_screen_options();
+	}
+
+	/**
+	 * Returns the advanced options for the nav menus page.
+	 *
+	 * Link title attribute added as it's a relatively advanced concept for new users.
+	 *
+	 * @since 4.3.0
+	 *
+	 * @return array The advanced menu properties.
+	 */
+	function wp_nav_menu_manage_columns() {
+		return array(
+			'_title'      => __( 'Show advanced menu properties' ),
+			'cb'          => '<input type="checkbox" />',
+			'link-target' => __( 'Link Target' ),
+			'attr-title'  => __( 'Title Attribute' ),
+			'css-classes' => __( 'CSS Classes' ),
+			'xfn'         => __( 'Link Relationship (XFN)' ),
+			'description' => __( 'Description' ),
+		);
+	}
+
+	/**
+	 * An Underscore (JS) template for this panel's content (but not its container).
+	 *
+	 * Class variables for this panel class are available in the `data` JS object;
+	 * export custom variables by overriding {@see WP_Customize_Panel::json()}.
+	 *
+	 * @since 4.3.0
+	 *
+	 * @see WP_Customize_Panel::print_template()
+	 *
+	 * @since 4.3.0
+	 */
+	protected function content_template() {
+		?>
+		<li class="panel-meta customize-info accordion-section <# if ( ! data.description ) { #> cannot-expand<# } #>">
+			<button type="button" class="customize-panel-back" tabindex="-1">
+				<span class="screen-reader-text"><?php _e( 'Back' ); ?></span>
+			</button>
+			<div class="accordion-section-title">
+				<span class="preview-notice">
+					<?php
+					/* Translators: %s is the site/panel title in the Customizer. */
+					printf( __( 'You are customizing %s' ), '<strong class="panel-title">{{ data.title }}</strong>' );
+					?>
+				</span>
+				<button type="button" class="customize-screen-options-toggle" aria-expanded="false">
+					<span class="screen-reader-text"><?php _e( 'Menu Options' ); ?></span>
+				</button>
+				<button type="button" class="customize-help-toggle dashicons dashicons-editor-help" aria-expanded="false">
+					<span class="screen-reader-text"><?php _e( 'Help' ); ?></span>
+				</button>
+			</div>
+			<# if ( data.description ) { #>
+			<div class="description customize-panel-description">{{{ data.description }}}</div>
+			<# } #>
+			<?php $this->render_screen_options(); ?>
+		</li>
+	<?php
+	}
+}
