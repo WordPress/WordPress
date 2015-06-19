cfe78e0e95970e8fda7d7c7abdcb4d0e34d66e8a
Scott Taylor <scott.c.taylor@mac.com>
2015-06-19 21:18:25 +0000

    Cleanup `(ms-)?admin-filters.php`
    Props johnjamesjacoby.
    Fixes #32529.
    
    Built from https://develop.svn.wordpress.org/trunk@32865
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32836 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/admin-filters.php
+++ wp-admin/includes/admin-filters.php
@@ -15,44 +15,52 @@ add_action( 'admin_page_access_denied', 'wp_link_manager_disabled_message' );
 add_action( 'activity_box_end', 'wp_dashboard_quota' );
 
 // Media hooks.
-add_filter( 'media_upload_tabs', 'update_gallery_tab' );
-add_filter( 'image_send_to_editor', 'image_add_caption', 20, 8 );
+add_action( 'attachment_submitbox_misc_actions', 'attachment_submitbox_metadata' );
+
 add_action( 'media_buttons', 'media_buttons' );
-
-add_filter( 'attachment_fields_to_save', 'image_attachment_fields_to_save', 10, 2 );
-add_filter( 'media_send_to_editor', 'image_media_send_to_editor', 10, 3 );
-
-add_action( 'post-plupload-upload-ui', 'media_upload_flash_bypass' );
-add_action( 'post-html-upload-ui', 'media_upload_html_bypass' );
-add_filter( 'async_upload_image', 'get_media_item', 10, 2 );
-add_filter( 'async_upload_audio', 'get_media_item', 10, 2 );
-add_filter( 'async_upload_video', 'get_media_item', 10, 2 );
-add_filter( 'async_upload_file',  'get_media_item', 10, 2 );
 
 add_action( 'media_upload_image', 'wp_media_upload_handler' );
 add_action( 'media_upload_audio', 'wp_media_upload_handler' );
 add_action( 'media_upload_video', 'wp_media_upload_handler' );
 add_action( 'media_upload_file',  'wp_media_upload_handler' );
 
+add_action( 'post-plupload-upload-ui', 'media_upload_flash_bypass' );
+
+add_action( 'post-html-upload-ui', 'media_upload_html_bypass'  );
+
+add_filter( 'async_upload_image', 'get_media_item', 10, 2 );
+add_filter( 'async_upload_audio', 'get_media_item', 10, 2 );
+add_filter( 'async_upload_video', 'get_media_item', 10, 2 );
+add_filter( 'async_upload_file',  'get_media_item', 10, 2 );
+
+add_filter( 'attachment_fields_to_save', 'image_attachment_fields_to_save', 10, 2 );
+
+add_filter( 'image_send_to_editor', 'image_add_caption', 20, 8 );
+
+add_filter( 'media_send_to_editor',      'image_media_send_to_editor',      10, 3 );
+
 add_filter( 'media_upload_gallery', 'media_upload_gallery' );
 add_filter( 'media_upload_library', 'media_upload_library' );
 
-add_action( 'attachment_submitbox_misc_actions', 'attachment_submitbox_metadata' );
+add_filter( 'media_upload_tabs', 'update_gallery_tab' );
 
 // Misc hooks.
-add_action( 'update_option_home', 'update_home_siteurl', 10, 2 );
-add_action( 'update_option_siteurl', 'update_home_siteurl', 10, 2 );
-add_action( 'update_option_page_on_front', 'update_home_siteurl', 10, 2 );
+add_action( 'admin_head', 'wp_admin_canonical_url'   );
 add_action( 'admin_head', 'wp_color_scheme_settings' );
-add_action( 'admin_head', '_ipad_meta' );
-add_filter( 'heartbeat_received', 'wp_check_locked_posts', 10, 3 );
-add_filter( 'heartbeat_received', 'wp_refresh_post_lock', 10, 3 );
-add_filter( 'heartbeat_received', 'wp_refresh_post_nonces', 10, 3 );
-add_filter( 'heartbeat_settings', 'wp_heartbeat_set_suspension' );
-// Run later as we have to set DOING_AUTOSAVE for back-compat
-add_filter( 'heartbeat_received', 'heartbeat_autosave', 500, 2 );
+add_action( 'admin_head', '_ipad_meta'               );
+
 add_action( 'post_edit_form_tag', 'post_form_autocomplete_off' );
-add_action( 'admin_head', 'wp_admin_canonical_url' );
+
+add_action( 'update_option_home',          'update_home_siteurl', 10, 2 );
+add_action( 'update_option_siteurl',       'update_home_siteurl', 10, 2 );
+add_action( 'update_option_page_on_front', 'update_home_siteurl', 10, 2 );
+
+add_filter( 'heartbeat_received', 'wp_check_locked_posts',  10,  3 );
+add_filter( 'heartbeat_received', 'wp_refresh_post_lock',   10,  3 );
+add_filter( 'heartbeat_received', 'wp_refresh_post_nonces', 10,  3 );
+add_filter( 'heartbeat_received', 'heartbeat_autosave',     500, 2 );
+
+add_filter( 'heartbeat_settings', 'wp_heartbeat_set_suspension' );
 
 // Nav Menu hooks.
 add_action( 'admin_head-nav-menus.php', '_wp_delete_orphaned_draft_menu_items' );
@@ -89,17 +97,19 @@ add_action( 'install_themes_pre_theme-information', 'install_theme_information' 
 
 // User hooks.
 add_action( 'admin_init', 'default_password_nag_handler' );
-add_action( 'profile_update', 'default_password_nag_edit_user', 10, 2 );
+
 add_action( 'admin_notices', 'default_password_nag' );
 
+add_action( 'profile_update', 'default_password_nag_edit_user', 10, 2 );
+
 // Update hooks.
-add_filter( 'update_footer', 'core_update_footer' );
-add_action( 'admin_notices', 'update_nag', 3 );
-add_action( 'network_admin_notices', 'update_nag', 3 );
 add_action( 'admin_init', 'wp_plugin_update_rows' );
-add_action( 'admin_init', 'wp_theme_update_rows' );
-add_action( 'admin_notices', 'maintenance_nag' );
-add_action( 'network_admin_notices', 'maintenance_nag' );
+add_action( 'admin_init', 'wp_theme_update_rows'  );
+
+add_action( 'admin_notices', 'update_nag',      3  );
+add_action( 'admin_notices', 'maintenance_nag', 10 );
+
+add_filter( 'update_footer', 'core_update_footer' );
 
 // Update Core hooks.
 add_action( '_core_updated_successfully', '_redirect_to_about_wordpress' );
