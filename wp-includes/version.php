a28586a6d76479595f66dd8ad2fea1af9068ca90
Dominik Schilling <dominikschilling+git@gmail.com>
2015-06-19 17:24:24 +0000

    Customizer: Improve accessibility of the toggle button in the available menu items panel.
    Prevents focus lost in Safari because the button was "invisible" until it got focused.
    
    fixes #32709.
    Built from https://develop.svn.wordpress.org/trunk@32859
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32830 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-alpha-32858';
+$wp_version = '4.3-alpha-32859';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
