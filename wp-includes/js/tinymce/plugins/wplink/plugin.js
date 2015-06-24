d710b768eb5bf2bd62e60369499a5976bb9a19f6
Ella Iseulde Van Dorpe <*@iseulde.com>
2015-06-24 23:05:26 +0000

    TinyMCE: wplink: add inline toolbar
    Now the URL can be viewed as soon as you put the cursor in an anchor tag. Clicking on it will open the page in a new tab. You also have the edit and remove button close at hand.
    
    First run.
    See #32604.
    
    Built from https://develop.svn.wordpress.org/trunk@32931
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32902 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/tinymce/plugins/wplink/plugin.js
+++ wp-includes/js/tinymce/plugins/wplink/plugin.js
@@ -1,5 +1,7 @@
 /* global tinymce */
 tinymce.PluginManager.add( 'wplink', function( editor ) {
+	var toolbar;
+
 	editor.addCommand( 'WP_Link', function() {
 		window.wpLink && window.wpLink.open( editor.id );
 	});
@@ -47,5 +49,82 @@ tinymce.PluginManager.add( 'wplink', function( editor ) {
 				event.preventDefault();
 			}
 		}
+	} );
+
+	tinymce.ui.WPLinkPreview = tinymce.ui.Control.extend( {
+		url: '#',
+		renderHtml: function() {
+			return (
+				'<div id="' + this._id + '" class="wp-link-preview">' +
+					'<a href="' + this.url + '" target="_blank" tabindex="-1">' + this.url + '</a>' +
+				'</div>'
+			);
+		},
+		setURL: function( url ) {
+			var index, lastIndex;
+
+			if ( this.url !== url ) {
+				this.url = url;
+
+				url = window.decodeURIComponent( url );
+
+				url = url.replace( /^(?:https?:)?\/\/(?:www\.)?/, '' );
+
+				if ( ( index = url.indexOf( '?' ) ) !== -1 ) {
+					url = url.slice( 0, index );
+				}
+
+				if ( ( index = url.indexOf( '#' ) ) !== -1 ) {
+					url = url.slice( 0, index );
+				}
+
+				url = url.replace( /(?:index)?\.html$/, '' );
+
+				if ( ( lastIndex = url.lastIndexOf( '/' ) ) === url.length - 1 ) {
+					url = url.slice( 0, lastIndex );
+				}
+
+				if ( ( index = url.indexOf( '/' ) ) !== -1 && ( lastIndex = url.lastIndexOf( '/' ) ) !== -1 && lastIndex !== index ) {
+					url = url.slice( 0, index + 1 ) + '\u2026' + url.slice( lastIndex, url.length );
+				}
+
+				tinymce.$( this.getEl().firstChild ).attr( 'href', this.url ).text( url );
+			}
+		},
+		postRender: function() {
+			var self = this;
+
+			editor.on( 'wptoolbar', function( event ) {
+				var anchor = editor.dom.getParent( event.element, 'a' ),
+					href;
+
+				if ( anchor && ( href = editor.$( anchor ).attr( 'href' ) ) ) {
+					self.setURL( href );
+
+					event.element = anchor;
+					event.toolbar = toolbar;
+				}
+			} );
+		}
+	} );
+
+	editor.addButton( 'wp_link_edit', {
+		tooltip: 'Edit ', // trailing space is needed, used for context
+		icon: 'dashicon dashicons-edit',
+		cmd: 'WP_Link'
+	} );
+
+	editor.addButton( 'wp_link_remove', {
+		tooltip: 'Remove',
+		icon: 'dashicon dashicons-no',
+		cmd: 'unlink'
+	} );
+
+	editor.on( 'preinit', function() {
+		toolbar = editor.wp._createToolbar( [
+			'WPLinkPreview',
+			'wp_link_edit',
+			'wp_link_remove'
+		], true );
 	} );
 });
