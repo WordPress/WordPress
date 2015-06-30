8217b0f2f7cc2e7829632d40ea452115cb861e7d
Konstantin Obenland <obenland@automattic.com>
2015-06-30 19:23:25 +0000

    Twenty Fifteen: Add missing textdomain.
    Props pavelevap.
    See #32001.
    
    
    Built from https://develop.svn.wordpress.org/trunk@33005
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@32976 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/inc/template-tags.php
+++ wp-content/themes/twentyfifteen/inc/template-tags.php
@@ -120,7 +120,7 @@ function twentyfifteen_entry_meta() {
 	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 		echo '<span class="comments-link">';
 		/* translators: %s: post title */
-		comments_popup_link( sprintf( __( 'Leave a comment<span class="screen-reader-text"> on %s</span>' ), get_the_title() ) );
+		comments_popup_link( sprintf( __( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'twentyfifteen' ), get_the_title() ) );
 		echo '</span>';
 	}
 }
