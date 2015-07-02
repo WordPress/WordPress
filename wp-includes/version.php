c919a1c1e91fa77f61435db2cf6006e6eb645efd
Konstantin Obenland <obenland@automattic.com>
2015-07-02 01:36:26 +0000

    4.3-beta1-33046
    Built from https://develop.svn.wordpress.org/trunk@33046
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33017 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-beta1';
+$wp_version = '4.3-beta1-33046';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
