c65b89be19570840281b4afca7efafbcb73fa1d2
Scott Taylor <scott.c.taylor@mac.com>
2015-07-06 15:34:26 +0000

    In `wp.media.model.Attachments.filters.type()`, return `true` earlier if `type` isn't set.
    Props vivekbhusal.
    Fixes #32746.
    
    Built from https://develop.svn.wordpress.org/trunk@33091
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33062 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/media-models.js
+++ wp-includes/js/media-models.js
@@ -897,14 +897,18 @@ var Attachments = Backbone.Collection.extend({
 		type: function( attachment ) {
 			var type = this.props.get('type'), atts = attachment.toJSON(), mime, found;
 
+			if ( ! type || ( _.isArray( type ) && ! type.length ) ) {
+				return true;
+			}
+
 			mime = atts.mime || ( atts.file && atts.file.type ) || '';
 
 			if ( _.isArray( type ) ) {
-				found = ! type.length || _.find( type, function (t) {
+				found = _.find( type, function (t) {
 					return -1 !== mime.indexOf( t );
 				} );
 			} else {
-				found = ! type || -1 !== mime.indexOf( type );
+				found = -1 !== mime.indexOf( type );
 			}
 
 			return found;
