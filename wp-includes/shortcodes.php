bc745f81a90dc48eb8266fd486d104b7604ed16a
John Blackbourn <johnbillion@git.wordpress.org>
2015-07-07 23:59:24 +0000

    Allow shortcode parameter names (attributes) to contain dashes.
    Props aaroncampbell, tyxla, izem
    Fixes #9405
    
    Built from https://develop.svn.wordpress.org/trunk@33118
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33089 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/shortcodes.php
+++ wp-includes/shortcodes.php
@@ -309,7 +309,7 @@ function do_shortcode_tag( $m ) {
  */
 function shortcode_parse_atts($text) {
 	$atts = array();
-	$pattern = '/(\w+)\s*=\s*"([^"]*)"(?:\s|$)|(\w+)\s*=\s*\'([^\']*)\'(?:\s|$)|(\w+)\s*=\s*([^\s\'"]+)(?:\s|$)|"([^"]*)"(?:\s|$)|(\S+)(?:\s|$)/';
+	$pattern = '/([\w-]+)\s*=\s*"([^"]*)"(?:\s|$)|([\w-]+)\s*=\s*\'([^\']*)\'(?:\s|$)|([\w-]+)\s*=\s*([^\s\'"]+)(?:\s|$)|"([^"]*)"(?:\s|$)|(\S+)(?:\s|$)/';
 	$text = preg_replace("/[\x{00a0}\x{200b}]+/u", " ", $text);
 	if ( preg_match_all($pattern, $text, $match, PREG_SET_ORDER) ) {
 		foreach ($match as $m) {
