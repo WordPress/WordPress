34eb53b4e626d7bf293a38194132c73d6713a6d4
Dominik Schilling <dominikschilling+git@gmail.com>
2015-07-08 19:38:25 +0000

    Dashicons: Update to the latest files.
    Revert the updates to .dashicons-plus (f132) in [33108] and instead introduce a new icon, .dashicons-plus-alt2 (f543).
    
    props liljimmi, melchoyce, empireoflight.
    see #30902.
    Built from https://develop.svn.wordpress.org/trunk@33126
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33097 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-beta1-33125';
+$wp_version = '4.3-beta1-33126';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
