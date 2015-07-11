72e83d6aec13c76d2934d7e12556064003276c8b
Jeremy Felt <jeremy.felt@gmail.com>
2015-07-11 23:17:24 +0000

    Restrict search inputs in network dashboard's Right Now to max width of 100%.
    This prevents the fields from bleeding outside of the widget area on smaller mobile devices.
    
    See #32846.
    
    Built from https://develop.svn.wordpress.org/trunk@33172
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33144 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-beta2-33171';
+$wp_version = '4.3-beta2-33172';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
