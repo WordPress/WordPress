1cd06052ead4b7e4789a3099767a8fe8dbb3b385
Jeremy Felt <jeremy.felt@gmail.com>
2015-07-11 21:38:24 +0000

    Improve formatting of network dashboard's Right Now actions on 320px screens.
    Prevents default `.subsubsub` change to center alignment and a large font that is applied at 782px.
    
    Fixes #32962.
    
    Built from https://develop.svn.wordpress.org/trunk@33168
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33140 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-beta2-33167';
+$wp_version = '4.3-beta2-33168';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
