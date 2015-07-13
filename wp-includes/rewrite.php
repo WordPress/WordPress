e6dd566b79cf6b4aba428e170245a3a0d1d68280
Drew Jaynes <info@drewapicture.com>
2015-07-13 21:54:24 +0000

    Add better line wrapping and syntax improvements to the description for `wp_resolve_numeric_slug_conflicts()`, added in 4.3.
    Also properly mark `$query_vars` as optional in the parameter description.
    
    See [32648]. See #32891.
    
    Built from https://develop.svn.wordpress.org/trunk@33236
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33208 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rewrite.php
+++ wp-includes/rewrite.php
@@ -287,17 +287,22 @@ function _wp_filter_taxonomy_base( $base ) {
 /**
  * Resolve numeric slugs that collide with date permalinks.
  *
- * Permalinks of posts with numeric slugs can sometimes look to `WP_Query::parse_query()` like a date archive,
- * as when your permalink structure is `/%year%/%postname%/` and a post with post_name '05' has the URL
- * `/2015/05/`. This function detects conflicts of this type and resolves them in favor of the post permalink.
+ * Permalinks of posts with numeric slugs can sometimes look to WP_Query::parse_query()
+ * like a date archive, as when your permalink structure is `/%year%/%postname%/` and
+ * a post with post_name '05' has the URL `/2015/05/`.
  *
- * Note that, since 4.3.0, `wp_unique_post_slug()` prevents the creation of post slugs that would result in a date
- * archive conflict. The resolution performed in this function is primarily for legacy content, as well as cases when
- * the admin has changed the site's permalink structure in a way that introduces URL conflicts.
+ * This function detects conflicts of this type and resolves them in favor of the
+ * post permalink.
+ *
+ * Note that, since 4.3.0, wp_unique_post_slug() prevents the creation of post slugs
+ * that would result in a date archive conflict. The resolution performed in this
+ * function is primarily for legacy content, as well as cases when the admin has changed
+ * the site's permalink structure in a way that introduces URL conflicts.
  *
  * @since 4.3.0
  *
- * @param array $query_vars Query variables for setting up the loop, as determined in `WP::parse_request()`.
+ * @param array $query_vars Optional. Query variables for setting up the loop, as determined in
+ *                          WP::parse_request(). Default empty array.
  * @return array Returns the original array of query vars, with date/post conflicts resolved.
  */
 function wp_resolve_numeric_slug_conflicts( $query_vars = array() ) {
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-beta2-33235';
+$wp_version = '4.3-beta2-33236';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
