536cdddbcd4815a77436ff46f013c113fc8aafaa
Drew Jaynes <info@drewapicture.com>
2015-07-13 21:23:25 +0000

    Fix inline documentation syntax for `get_default_comment_status()`, introduced in 4.3.
    See [33122]. See #32891.
    
    Built from https://develop.svn.wordpress.org/trunk@33223
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33195 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment.php
+++ wp-includes/comment.php
@@ -1034,12 +1034,12 @@ function get_comment_statuses() {
 }
 
 /**
- * Get the default comment status for a post type.
+ * Gets the default comment status for a post type.
  *
  * @since 4.3.0
  *
- * @param  string $post_type    Optional. Post type. Default 'post'.
- * @param  string $comment_type Optional. Comment type. Default 'comment'.
+ * @param string $post_type    Optional. Post type. Default 'post'.
+ * @param string $comment_type Optional. Comment type. Default 'comment'.
  * @return string Expected return value is 'open' or 'closed'.
  */
 function get_default_comment_status( $post_type = 'post', $comment_type = 'comment' ) {
