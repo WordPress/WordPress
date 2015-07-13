668bdf60fb2bc729931c00cd80a80166c79234e3
Drew Jaynes <info@drewapicture.com>
2015-07-13 22:03:24 +0000

    Wrap a long changelog entry description in the DocBlock for `count_user_posts()`.
    Also clarify the description for the new `$public_only` parameter, added in [32523].
    
    See #32891.
    
    Built from https://develop.svn.wordpress.org/trunk@33241
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33213 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -255,13 +255,14 @@ function wp_validate_logged_in_cookie( $user_id ) {
  *
  * @since 3.0.0
  * @since 4.1.0 Added `$post_type` argument.
- * @since 4.3.0 Added `$public_only` argument. Added the ability to pass an array of post types to `$post_type`.
+ * @since 4.3.0 Added `$public_only` argument. Added the ability to pass an array
+ *              of post types to `$post_type`.
  *
  * @global wpdb $wpdb WordPress database object for queries.
  *
  * @param int          $userid      User ID.
  * @param array|string $post_type   Optional. Post type(s) to count the number of posts for. Default 'post'.
- * @param bool         $public_only Optional. Only return counts for public posts. Defaults to false.
+ * @param bool         $public_only Optional. Whether to only return counts for public posts. Default false.
  * @return int Number of posts the user has written in this post type.
  */
 function count_user_posts( $userid, $post_type = 'post', $public_only = false ) {
