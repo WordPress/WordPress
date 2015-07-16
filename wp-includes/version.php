f9980c793cd3c32d6c5613bd3cc62c765a7eff98
Andrew Ozz <admin@laptoptips.ca>
2015-07-16 23:58:25 +0000

    wpLink: fix vertical alignment of the fields labels. Part props daniluk4000. Fixes #32829. Built from https://develop.svn.wordpress.org/trunk@33300
    git-svn-id: http://core.svn.wordpress.org/trunk@33272 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-beta3-33299';
+$wp_version = '4.3-beta3-33300';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
