8a090123ce36683a8d92dec105f411ee8513a7e8
Andrew Ozz <admin@laptoptips.ca>
2015-07-21 22:25:24 +0000

    TinyMCE, inline link toolbar: tweak styling on small screens to fix wrapping in iOS. Fixes #32604. Built from https://develop.svn.wordpress.org/trunk@33350
    git-svn-id: http://core.svn.wordpress.org/trunk@33322 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-beta3-33349';
+$wp_version = '4.3-beta3-33350';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
