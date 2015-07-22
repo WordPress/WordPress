333957fcbd449e1e66780945740250e3bd2297c1
Andrew Ozz <admin@laptoptips.ca>
2015-07-22 23:05:25 +0000

    TinyMCE, inline link toolbar: add a bit more specificity to the selectors in editor.css to fix overriding in IE < 10. See #32604. Built from https://develop.svn.wordpress.org/trunk@33372
    git-svn-id: http://core.svn.wordpress.org/trunk@33344 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-beta4-33371';
+$wp_version = '4.3-beta4-33372';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
