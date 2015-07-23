9a0402d6343f0465e9ae0991884f5982ca472c9e
Gary Pendergast <gary@pento.net>
2015-07-23 04:19:10 +0000

    Capabilities: When creating an auto-draft, ensure that the current user still has permission to do so.
    Merge of [33357] to the 3.8 branch.
    
    
    Built from https://develop.svn.wordpress.org/branches/3.8@33378
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.8@33349 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/post.php
+++ wp-admin/post.php
@@ -109,8 +109,9 @@ case 'post-quickdraft-save':
 	if ( ! wp_verify_nonce( $nonce, 'add-post' ) )
 		$error_msg = __( 'Unable to submit this form, please refresh and try again.' );
 
-	if ( ! current_user_can( 'edit_posts' ) )
-		$error_msg = __( 'Oops, you don&#8217;t have access to add new drafts.' );
+	if ( ! current_user_can( 'edit_posts' ) ) {
+		exit;
+	}
 
 	if ( $error_msg )
 		return wp_dashboard_quick_press( $error_msg );
