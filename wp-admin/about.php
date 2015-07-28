4cac76486f1a6949a26ba57687222d925aeac392
Konstantin Obenland <obenland@automattic.com>
2015-07-28 21:23:25 +0000

    About: First pass at WordPress 4.3 About page.
    Still missing images and some string revisions.
    
    See #32929.
    
    
    Built from https://develop.svn.wordpress.org/trunk@33466
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33433 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -21,119 +21,126 @@ $title = __( 'About' );
 list( $display_version ) = explode( '-', $wp_version );
 
 include( ABSPATH . 'wp-admin/admin-header.php' );
+
+$major_features = array(
+	array(
+		'src'         => '',
+		'heading'     => __( 'Formatting Shortcuts' ),
+		'description' => __( 'Your writing flow just got faster with new formatting shortcuts in WordPress 4.3. Use asterisks to create lists and number signs to make a heading. No more breaking your flow; your text looks great with a <code>*</code> and a <code>#</code>.' ),
+	),
+	array(
+		'src'         => '',
+		'heading'     => __( 'Menus in the Customizer' ),
+		'description' => __( 'Create your menu, update it, and preview it in the customizer, before sharing it with the world. With every release, it becomes easier and faster to build your site from the front-end. And a streamlined customizer design mean a mobile-first, accessibility ready interface.' ),
+	),
+	array(
+		'src'         => '',
+		'heading'     => __( 'Better Passwords' ),
+		'description' => __( 'Keep your site more secure with WordPress’ improved approach to passwords. Instead of receiving passwords via email, you’ll get a password reset link. When you add new users to your site, WordPress will automatically generate a secure password.' ),
+	),
+	array(
+		'src'         => '',
+		'heading'     => __( 'Site Icons' ),
+		'description' => __( 'Site icons represent your site in browser tabs, bookmark menus, and on the home screen of mobile devices. Add your unique site icon in the customizer; it will even stay in place when you switch themes. Make your whole site reflect your brand.' ),
+	),
+);
+shuffle( $major_features );
+
+$minor_features = array(
+	array(
+		'src'         => '',
+		'heading'     => __( 'A smoother admin experience' ),
+		'description' => __( 'Refinements to the list view across the admin make your WordPress more accessible and easier to work with on any device.' ),
+	),
+	array(
+		'src'         => '',
+		'heading'     => __( 'Comments turned off on pages' ),
+		'description' => __( 'All new pages that you create will have comments turned off. Keep discussions to your blog, right where they’re supposed to happen.' ),
+	),
+	array(
+		'src'         => '',
+		'heading'     => __( 'Customize your site quickly' ),
+		'description' => __( 'Wherever you are on the front-end, you can click the customize button in the toolbar to swiftly make changes to your site.' ),
+	),
+);
+
+$tech_features = array(
+	array(
+		'heading'     => __( 'Taxonomy Roadmap' ),
+		'description' => __( 'Terms shared across multiple taxonomies are now split into separate terms.' ),
+	),
+	array(
+		'heading'     => __( 'Template Hierarchy' ),
+		'description' => __( 'Added <code>singular.php</code> as a fallback for <code>single.php</code> and <code>page.php</code>.' ),
+	),
+	array(
+		'heading'     => __( 'List table' ),
+		'description' => __( 'List tables now can (and often should) have a primary column defined.' ),
+	),
+);
+
 ?>
-<div class="wrap about-wrap">
+	<div class="wrap about-wrap">
+		<h1><?php printf( __( 'Welcome to WordPress&nbsp;%s' ), $display_version ); ?></h1>
 
-<h1><?php printf( __( 'Welcome to WordPress&nbsp;%s' ), $display_version ); ?></h1>
+		<div class="about-text"><?php /* @TODO Fun tag line. */ printf( __( 'Thank you for updating to WordPress %s!' ), $display_version ); ?></div>
+		<div class="wp-badge"><?php printf( __( 'Version %s' ), $display_version ); ?></div>
 
-<div class="about-text"><?php printf( __( 'Thank you for updating! WordPress %s helps you communicate and share, globally.' ), $display_version ); ?></div>
+		<h2 class="nav-tab-wrapper">
+			<a href="about.php" class="nav-tab nav-tab-active"><?php _e( 'What&#8217;s New' ); ?></a>
+			<a href="credits.php" class="nav-tab"><?php _e( 'Credits' ); ?></a>
+			<a href="freedoms.php" class="nav-tab"><?php _e( 'Freedoms' ); ?></a>
+		</h2>
 
-<div class="wp-badge"><?php printf( __( 'Version %s' ), $display_version ); ?></div>
-
-<h2 class="nav-tab-wrapper">
-	<a href="about.php" class="nav-tab nav-tab-active">
-		<?php _e( 'What&#8217;s New' ); ?>
-	</a><a href="credits.php" class="nav-tab">
-		<?php _e( 'Credits' ); ?>
-	</a><a href="freedoms.php" class="nav-tab">
-		<?php _e( 'Freedoms' ); ?>
-	</a>
-</h2>
-
-<div class="headline-feature feature-video">
-<embed type="application/x-shockwave-flash" src="https://v0.wordpress.com/player.swf?v=1.04" width="1000" height="560" wmode="direct" seamlesstabbing="true" allowfullscreen="true" allowscriptaccess="always" overstretch="true" flashvars="guid=e9kH4FzP&amp;isDynamicSeeking=true"></embed>
-</div>
-
-<hr />
-
-<div class="feature-section two-col">
-	<div class="col">
-		<h3><?php _e( 'An easier way to share content' ); ?></h3>
-		<p><?php printf( __( 'Clip it, edit it, publish it. Get familiar with the new and improved Press This. From the <a href="%s">Tools</a> menu, add Press This to your browser bookmark bar or your mobile device home screen. Once installed you can share your content with lightning speed. Sharing your favorite videos, images, and content has never been this fast or this easy.' ), admin_url( 'tools.php' ) ); ?></p>
-		<p><?php _e( 'Drag the bookmarklet below to your bookmarks bar. Then, when you&#8217;re on a page you want to share, simply &#8220;press&#8221; it.' ); ?></p>
-
-		<p class="pressthis-bookmarklet-demo">
-			<a class="pressthis-bookmarklet" onclick="return false;" href="<?php echo htmlspecialchars( get_shortcut_link() ); ?>"><span><?php _e( 'Press This' ); ?></span></a>
-		</p>
-	</div>
-
-	<div class="col">
-		<img src="//s.w.org/images/core/4.2/press-this.jpg" />
-	</div>
-</div>
-
-<div class="feature-section two-col">
-	<div class="col">
-		<img src="//s.w.org/images/core/4.2/unicode.png" />
-	</div>
-	<div class="col">
-		<h3><?php _e( 'Extended character support' ); ?></h3>
-		<p><?php _e( 'Writing in WordPress, whatever your language, just got better. WordPress 4.2 supports a host of new characters out-of-the-box, including native Chinese, Japanese, and Korean characters, musical and mathematical symbols, and hieroglyphs.' ); ?></p>
-		<p><?php
-			/* translators: 1: heart emoji, 2: frog face emoji, 3, monkey emoji, 4: pizza emoji, 5: Emoji Codex link */
-			printf( __( 'Don&#8217;t use any of those characters? You can still have fun &mdash; emoji are now available in WordPress! Get creative and decorate your content with %1$s, %2$s, %3$s, %4$s, and all the many other <a href="%5$s">emoji</a>.' ), '&#x1F499', '&#x1F438', '&#x1F412', '&#x1F355', __( 'https://codex.wordpress.org/Emoji' ) );
-		?></p>
-	</div>
-</div>
-
-<div class="changelog feature-section three-col">
-	<div>
-		<img src="//s.w.org/images/core/4.2/theme-switcher.png" />
-		<h3><?php _e( 'Switch themes in the Customizer' ); ?></h3>
-		<p><?php _e( 'Browse and preview your installed themes from the Customizer. Make sure the theme looks great with <em>your</em> content, before it debuts on your site. ' ); ?></p>
-	</div>
-	<div>
-		<img src="//s.w.org/images/core/4.2/embeds.png" />
-		<h3><?php _e( 'Even more embeds' ); ?></h3>
-		<p><?php _e( 'Paste links from Tumblr.com and Kickstarter and watch them magically appear right in the editor. With every release, your publishing and editing experience get closer together.' ); ?></p>
-	</div>
-	<div class="last-feature">
-		<img src="//s.w.org/images/core/4.2/plugins.png" />
-		<h3><?php _e( 'Streamlined plugin updates' ); ?></h3>
-		<p><?php _e( 'Goodbye boring loading screen, hello smooth and simple plugin updates. Click <em>Update&nbsp;Now</em> and watch the magic happen.' ); ?></p>
-	</div>
-</div>
-
-<div class="changelog under-the-hood feature-list">
-	<h3><?php _e( 'Under the Hood' ); ?></h3>
-
-	<div class="feature-section col two-col">
-		<div>
-			<h4><?php _e( 'utf8mb4 support' ); ?></h4>
-			<p><?php _e( 'Database character encoding has changed from utf8 to utf8mb4, which adds support for a whole range of new 4-byte characters.' ); ?></p>
-
-			<h4><?php _e( 'JavaScript accessibility' ); ?></h4>
-			<p><?php
-				/* translators: %s wp.a11y.speak() */
-				printf( __( 'You can now send audible notifications to screen readers in JavaScript with %s. Pass it a string, and an update will be sent to a dedicated ARIA live notifications area.' ), '<code>wp.a11y.speak()</code>' );
-			?></p>
+		<div class="headline-feature feature-video">
+			<!-- [4.3 video] -->
 		</div>
-		<div class="last-feature">
-			<h4><?php _e( 'Shared term splitting' ); ?></h4>
-			<p><?php
-				/* translators: 1: Term splitting guide link */
-				printf( __( 'Terms shared across multiple taxonomies will be split when one of them is updated. Find out more in the <a href="%1$s">Plugin Developer Handbook</a>.' ), 'https://developer.wordpress.org/plugins/taxonomy/working-with-split-terms-in-wp-4-2/' );
-			?></p>
 
-			<h4><?php _e( 'Complex query ordering' ); ?></h4>
-			<p><?php
-				/* translators: 1: WP_Query, 2: WP_Comment_Query, 3: WP_User_Query */
-				printf( __( '%1$s, %2$s, and %3$s now support complex ordering with named meta query clauses.' ), '<code>WP_Query</code>', '<code>WP_Comment_Query</code>', '<code>WP_User_Query</code>' );
-			?></p>
+		<hr/>
+
+		<div class="feature-section two-col">
+			<?php foreach ( $major_features as $feature ) : ?>
+			<div class="col">
+				<img src="<?php echo esc_url( $feature['src'] ); ?>" />
+				<h3><?php echo $feature['heading']; ?></h3>
+				<p><?php echo $feature['description']; ?></p>
+			</div>
+			<?php endforeach; ?>
+		</div>
+
+		<div class="feature-section three-col">
+			<?php foreach ( $minor_features as $feature ) : ?>
+			<div class="col">
+				<img src="<?php echo esc_url( $feature['src'] ); ?>" />
+				<h3><?php echo $feature['heading']; ?></h3>
+				<p><?php echo $feature['description']; ?></p>
+			</div>
+			<?php endforeach; ?>
+		</div>
+
+		<div class="changelog">
+			<h3><?php _e( 'Under the Hood' ); ?></h3>
+
+			<div class="feature-section under-the-hood three-col">
+				<?php foreach ( $tech_features as $feature ) : ?>
+				<div class="col">
+					<h3><?php echo $feature['heading']; ?></h3>
+					<p><?php echo $feature['description']; ?></p>
+				</div>
+				<?php endforeach; ?>
+			</div>
+
+			<div class="return-to-dashboard">
+				<?php if ( current_user_can( 'update_core' ) && isset( $_GET['updated'] ) ) : ?>
+					<a href="<?php echo esc_url( self_admin_url( 'update-core.php' ) ); ?>">
+						<?php is_multisite() ? _e( 'Return to Updates' ) : _e( 'Return to Dashboard &rarr; Updates' ); ?>
+					</a> |
+				<?php endif; ?>
+				<a href="<?php echo esc_url( self_admin_url() ); ?>"><?php is_blog_admin() ? _e( 'Go to Dashboard &rarr; Home' ) : _e( 'Go to Dashboard' ); ?></a>
+			</div>
+
 		</div>
 	</div>
-
-	<div class="return-to-dashboard">
-		<?php if ( current_user_can( 'update_core' ) && isset( $_GET['updated'] ) ) : ?>
-		<a href="<?php echo esc_url( self_admin_url( 'update-core.php' ) ); ?>"><?php
-			is_multisite() ? _e( 'Return to Updates' ) : _e( 'Return to Dashboard &rarr; Updates' );
-		?></a> |
-		<?php endif; ?>
-		<a href="<?php echo esc_url( self_admin_url() ); ?>"><?php
-			is_blog_admin() ? _e( 'Go to Dashboard &rarr; Home' ) : _e( 'Go to Dashboard' ); ?></a>
-	</div>
-
-</div>
 <?php
 
 include( ABSPATH . 'wp-admin/admin-footer.php' );
