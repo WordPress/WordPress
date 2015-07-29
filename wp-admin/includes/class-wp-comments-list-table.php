7b7ae2ee25de7a373d4c157bc9af48e043dbe366
Helen Hou-Sandí <helen.y.hou@gmail.com>
2015-07-29 18:08:25 +0000

    Comments list table: Display that cute lil comment bubble.
    The comments list table suffers from "wall of text" problems, which the two plain text links compounded.
    
    props obenland. fixes #33149.
    
    Built from https://develop.svn.wordpress.org/trunk@33490
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33457 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-comments-list-table.php
+++ wp-admin/includes/class-wp-comments-list-table.php
@@ -687,16 +687,16 @@ class WP_Comments_List_Table extends WP_List_Table {
 		}
 
 		echo '<div class="response-links">';
-		echo $post_link;
-		$post_type_object = get_post_type_object( $post->post_type );
-		echo "<a href='" . get_permalink( $post->ID ) . "' class='comments-view-item-link'>" . $post_type_object->labels->view_item . '</a>';
 		if ( 'attachment' == $post->post_type && ( $thumb = wp_get_attachment_image( $post->ID, array( 80, 60 ), true ) ) ) {
 			echo $thumb;
 		}
-		echo '</div>';
+		echo $post_link;
+		$post_type_object = get_post_type_object( $post->post_type );
+		echo "<a href='" . get_permalink( $post->ID ) . "' class='comments-view-item-link'>" . $post_type_object->labels->view_item . '</a>';
 		echo '<span class="post-com-count-wrapper">';
 		$this->comments_bubble( $post->ID, $pending_comments );
 		echo '</span> ';
+		echo '</div>';
 	}
 
 	/**
