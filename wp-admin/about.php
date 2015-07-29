9152362ddab62d6e4016f5a83f9c88c07ee06fee
Konstantin Obenland <obenland@automattic.com>
2015-07-29 19:57:25 +0000

    About: Second pass at 4.3 about page.
    Adds images and videos for major and minor features.
    Restores a mangled placeholder in Formatting Shortcuts description.
    
    Assets still need to be moved to the .org CDN once deemed final.
    
    Props ryelle, melchoyce, helen.
    See #32929.
    
    
    Built from https://develop.svn.wordpress.org/trunk@33498
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33465 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -24,24 +24,28 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 $major_features = array(
 	array(
-		'src'         => '',
+		'src'         => array(
+			'mp4'  => 'https://cldup.com/2k26HVNP6P.mp4',
+			'ogv'  => '',
+			'webm' => '',
+		),
 		'heading'     => 'Formatting Shortcuts',
 
 		/* Translators: 1: asterisks; 2: number sign; */
-		'description' => sprintf( 'Your writing flow just got faster with new formatting shortcuts in WordPress 4.3. Use asterisks to create lists and number signs to make a heading. No more breaking your flow; your text looks great with a %1$s and a %2$.', '<code>*</code>', '<code>#</code>' ),
+		'description' => sprintf( 'Your writing flow just got faster with new formatting shortcuts in WordPress 4.3. Use asterisks to create lists and number signs to make a heading. No more breaking your flow; your text looks great with a %1$s and a %2$s.', '<code>*</code>', '<code>#</code>' ),
 	),
 	array(
-		'src'         => '',
+		'src'         => 'https://cldup.com/k23oK-g_v1.jpg',
 		'heading'     => 'Menus in the Customizer',
 		'description' => 'Create your menu, update it, and preview it in the customizer, before sharing it with the world. With every release, it becomes easier and faster to build your site from the front-end. And a streamlined customizer design mean a mobile-first, accessibility ready interface.',
 	),
 	array(
-		'src'         => '',
+		'src'         => 'https://cldup.com/t1HCztI0PR.jpg',
 		'heading'     => 'Better Passwords',
 		'description' => 'Keep your site more secure with WordPress’ improved approach to passwords. Instead of receiving passwords via email, you’ll get a password reset link. When you add new users to your site, WordPress will automatically generate a secure password.',
 	),
 	array(
-		'src'         => '',
+		'src'         => 'https://cldup.com/8LxuMwmsvE.jpg',
 		'heading'     => 'Site Icons',
 		'description' => 'Site icons represent your site in browser tabs, bookmark menus, and on the home screen of mobile devices. Add your unique site icon in the customizer; it will even stay in place when you switch themes. Make your whole site reflect your brand.',
 	),
@@ -50,17 +54,17 @@ shuffle( $major_features );
 
 $minor_features = array(
 	array(
-		'src'         => '',
+		'src'         => 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0MDAgNDAwIj48cGF0aCBmaWxsPSIjMDBhMGQyIiBkPSJNNTAgMjE1aDI0MHYzMEg1MHpNNTAgMjc1aDI0MHYzMEg1MHpNNTAgMTU1aDI0MHYzMEg1MHpNNTAgOTVoMjQwdjMwSDUwek0zMTAuMSA5NWwxOS45IDMwIDIwLjEtMzAiLz48L3N2Zz4=',
 		'heading'     => 'A smoother admin experience',
 		'description' => 'Refinements to the list view across the admin make your WordPress more accessible and easier to work with on any device.',
 	),
 	array(
-		'src'         => '',
+		'src'         => 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyMCAyMCI+PHBhdGggZmlsbD0iIzAwYTBkMiIgZD0iTTUgMmgxMHEuODIgMCAxLjQxLjU5VDE3IDR2OHEwIC44Mi0uNTkgMS40MVQxNSAxNGgtMmwtNSA1di01SDVxLS44MiAwLTEuNDEtLjU5VDMgMTJWNHEwLS44Mi41OS0xLjQxVDUgMnptOC41IDguNUwxMSA4bDIuNS0yLjUtMS0xTDEwIDcgNy41IDQuNWwtMSAxTDkgOGwtMi41IDIuNSAxIDFMMTAgOWwyLjUgMi41eiIvPjwvc3ZnPg==',
 		'heading'     => 'Comments turned off on pages',
 		'description' => 'All new pages that you create will have comments turned off. Keep discussions to your blog, right where they’re supposed to happen.',
 	),
 	array(
-		'src'         => '',
+		'src'         => 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzMiAzMiI+PHBhdGggZmlsbD0iIzAwYTBkMiIgZD0iTTI5LjMyOCA1LjcxMnEuMDQ4LS4xNDQuMDk2LS4zODR0LS4wNjQtLjgxNi0uNTI4LS45NzZxLS4zODQtLjM2OC0uODcyLS40NjR0LS43OTIgMGwtLjI4OC4wOHEtMS40NTYuNzItNS44OCAzLjczNnQtNi4zOTIgNS4xNzZxLS43MzYuODMyLTEuNDA4IDIuMzJ0LS44OCAzIC41NDQgMi4zOTJxLjgzMi43MzYgMi4zNDQuNTc2dDMuMDcyLS44MjQgMi4yNDgtMS4zNTJxMi4xNDQtMi4xNDQgNS4xNjgtNi42NTZ0My42MzItNS44MDh6TTIuMjQgMjguMjRxMS4wNTYtLjY4OCAxLjcxMi0xLjUyOHQuOTUyLTEuNjE2LjU0NC0xLjUyLjcyLTEuNDggMS4yNC0xLjI4cTEuMDg4LS44IDIuNTA0LS43MDR0Mi40MjQgMS4xNjhxLjgxNi44OC44MjQgMi42NHQtMS4wOCAyLjg5NnEtMS4yMTYgMS4xMi0yLjkwNCAxLjYyNHQtMy40MjQuNDI0LTMuNTEyLS42MjR6Ii8+PC9zdmc+',
 		'heading'     => 'Customize your site quickly' ,
 		'description' => 'Wherever you are on the front-end, you can click the customize button in the toolbar to swiftly make changes to your site.',
 	),
@@ -87,7 +91,7 @@ $tech_features = array(
 	<div class="wrap about-wrap">
 		<h1><?php printf( __( 'Welcome to WordPress&nbsp;%s' ), $display_version ); ?></h1>
 
-		<div class="about-text"><?php /* @TODO Fun tag line. */ printf( 'Thank you for updating to WordPress %s!', $display_version ); ?></div>
+		<div class="about-text"><?php printf( 'Thank you for updating! WordPress %s: faster workflow, easier customization, strong by default.', $display_version ); ?></div>
 		<div class="wp-badge"><?php printf( __( 'Version %s' ), $display_version ); ?></div>
 
 		<h2 class="nav-tab-wrapper">
@@ -105,7 +109,26 @@ $tech_features = array(
 		<div class="feature-section two-col">
 			<?php foreach ( $major_features as $feature ) : ?>
 			<div class="col">
-				<img src="<?php echo esc_url( $feature['src'] ); ?>" />
+				<div class="media-container">
+					<?php
+					// Video.
+					if ( is_array( $feature['src'] ) ) :
+						echo wp_video_shortcode( array(
+							'mp4'      => $feature['src']['mp4'],
+							'ogv'      => $feature['src']['ogv'],
+							'webm'     => $feature['src']['webm'],
+							'loop'     => true,
+							'autoplay' => true,
+							'width'    => 500,
+							'height'   => 284
+						) );
+
+					// Image.
+					else:
+					?>
+					<img src="<?php echo esc_url( $feature['src'] ); ?>" />
+					<?php endif; ?>
+				</div>
 				<h3><?php echo $feature['heading']; ?></h3>
 				<p><?php echo $feature['description']; ?></p>
 			</div>
@@ -115,7 +138,9 @@ $tech_features = array(
 		<div class="feature-section three-col">
 			<?php foreach ( $minor_features as $feature ) : ?>
 			<div class="col">
-				<img src="<?php echo esc_url( $feature['src'] ); ?>" />
+				<div class="svg-container">
+					<img src="<?php echo esc_attr( $feature['src'] ); ?>" />
+				</div>
 				<h3><?php echo $feature['heading']; ?></h3>
 				<p><?php echo $feature['description']; ?></p>
 			</div>
@@ -128,7 +153,7 @@ $tech_features = array(
 			<div class="feature-section under-the-hood three-col">
 				<?php foreach ( $tech_features as $feature ) : ?>
 				<div class="col">
-					<h3><?php echo $feature['heading']; ?></h3>
+					<h4><?php echo $feature['heading']; ?></h4>
 					<p><?php echo $feature['description']; ?></p>
 				</div>
 				<?php endforeach; ?>
