c5719ba15a0ade74d1a65aac28e23d99855c06d0
Mark Jaquith <mark@wordpress.org>
2015-07-29 14:06:25 +0000

    Make the "using default password" nag more accurate.
    "Easy to remember" isn't necessarily what we want to encourage.
    
    fixes #33168
    props arjunskumar
    Built from https://develop.svn.wordpress.org/trunk@33485
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33452 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/user.php
+++ wp-admin/includes/user.php
@@ -442,7 +442,7 @@ function default_password_nag() {
 	echo '<div class="error default-password-nag">';
 	echo '<p>';
 	echo '<strong>' . __('Notice:') . '</strong> ';
-	_e('You&rsquo;re using the auto-generated password for your account. Would you like to change it to something easier to remember?');
+	_e('You&rsquo;re using the auto-generated password for your account. Would you like to change it?');
 	echo '</p><p>';
 	printf( '<a href="%s">' . __('Yes, take me to my profile page') . '</a> | ', get_edit_profile_url() . '#password' );
 	printf( '<a href="%s" id="default-password-nag-no">' . __('No thanks, do not remind me again') . '</a>', '?default_password_nag=0' );
