8b91700d5b09de28a68ae546fca32ebeca29b8f3
Dominik Schilling <dominikschilling+git@gmail.com>
2015-07-30 19:59:25 +0000

    Fix PHP notice after [33492].
    props jesin.
    see #33178.
    fixes #33208.
    Built from https://develop.svn.wordpress.org/trunk@33519
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33486 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-upgrader-skins.php
+++ wp-admin/includes/class-wp-upgrader-skins.php
@@ -660,7 +660,7 @@ class Theme_Installer_Skin extends WP_Upgrader_Skin {
 		$install_actions = array();
 
 		if ( current_user_can( 'edit_theme_options' ) && current_user_can( 'customize' ) ) {
-			$install_actions['preview'] .= '<a href="' . wp_customize_url( $stylesheet ) . '" class="hide-if-no-customize load-customize"><span aria-hidden="true">' . __( 'Live Preview' ) . '</span><span class="screen-reader-text">' . sprintf( __( 'Live Preview &#8220;%s&#8221;' ), $name ) . '</span></a>';
+			$install_actions['preview'] = '<a href="' . wp_customize_url( $stylesheet ) . '" class="hide-if-no-customize load-customize"><span aria-hidden="true">' . __( 'Live Preview' ) . '</span><span class="screen-reader-text">' . sprintf( __( 'Live Preview &#8220;%s&#8221;' ), $name ) . '</span></a>';
 		}
 		$install_actions['activate'] = '<a href="' . esc_url( $activate_link ) . '" class="activatelink"><span aria-hidden="true">' . __( 'Activate' ) . '</span><span class="screen-reader-text">' . sprintf( __( 'Activate &#8220;%s&#8221;' ), $name ) . '</span></a>';
 
