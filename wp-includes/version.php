b7f9555444a1259d7557aeeb9dabbe3b8a0982c4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-07-31 13:02:24 +0000

    Customizer: Fix failing tests on PHP 5.2 after [33488].
    `assertNotFalse()` is not included in PHPUnit 3.6.12, use `assertInternalType( 'int' )` instead.
    
    fixes #32814.
    Built from https://develop.svn.wordpress.org/trunk@33526
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33493 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-RC1-33519';
+$wp_version = '4.3-RC1-33526';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
