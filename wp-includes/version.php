a3e45db4ac0507c85bdc889667fa865d64d7fb6e
Weston Ruter <weston@xwp.co>
2015-08-03 19:28:26 +0000

    Customizer: Add QUnit tests for menus.
    Props adamsilverstein, westonruter, jorbin.
    Fixes #32688.
    
    Built from https://develop.svn.wordpress.org/trunk@33528
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33495 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-RC1-33527';
+$wp_version = '4.3-RC1-33528';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
