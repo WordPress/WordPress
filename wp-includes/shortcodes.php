f8136f2ab22f5ac1ce7d6d65bb4d699e1c7a0b21
Dion Hulse <wordpress@dd32.id.au>
2015-08-07 02:50:25 +0000

    Fix `do_shortcode('<[shortcode]')` edge case. Props miqrogroove. Merges [33499] trunk. See #33116.
    Built from https://develop.svn.wordpress.org/trunk@33594
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33561 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/shortcodes.php
+++ wp-includes/shortcodes.php
@@ -359,6 +359,11 @@ function do_shortcodes_in_html_tags( $content, $ignore_html ) {
 
 		$attributes = wp_kses_attr_parse( $element );
 		if ( false === $attributes ) {
+			// Some plugins are doing things like [name] <[email]>.
+			if ( 1 === preg_match( '%^<\s*\[\[?[^\[\]]+\]%', $element ) ) {
+				$element = preg_replace_callback( "/$pattern/s", 'do_shortcode_tag', $element );
+			}
+
 			// Looks like we found some crazy unfiltered HTML.  Skipping it for sanity.
 			$element = strtr( $element, $trans );
 			continue;
