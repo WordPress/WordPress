8af2dbc67118d0b3c8fd0903b83008958be412d9
Scott Taylor <scott.c.taylor@mac.com>
2015-08-18 23:35:25 +0000

    Correct punctuation/case for inline comment in `async-upload.php`
    Props chriscct7.
    Fixes #33408.
    
    Built from https://develop.svn.wordpress.org/trunk@33640
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33607 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/async-upload.php
+++ wp-admin/async-upload.php
@@ -61,7 +61,7 @@ if ( isset($_REQUEST['attachment_id']) && ($id = intval($_REQUEST['attachment_id
 			if ( $thumb_url = wp_get_attachment_image_src( $id, 'thumbnail', true ) )
 				echo '<img class="pinkynail" src="' . esc_url( $thumb_url[0] ) . '" alt="" />';
 			echo '<a class="edit-attachment" href="' . esc_url( get_edit_post_link( $id ) ) . '" target="_blank">' . _x( 'Edit', 'media item' ) . '</a>';
-			$title = $post->post_title ? $post->post_title : wp_basename( $post->guid ); // title shouldn't ever be empty, but use filename just in cas.e
+			$title = $post->post_title ? $post->post_title : wp_basename( $post->guid ); // Title shouldn't ever be empty, but use filename just in case.
 			echo '<div class="filename new"><span class="title">' . esc_html( wp_html_excerpt( $title, 60, '&hellip;' ) ) . '</span></div>';
 			break;
 		case 2 :
