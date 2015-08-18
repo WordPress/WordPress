2e73c24e300b01e70fb6e1601e32e3105d60f575
Konstantin Obenland <obenland@automattic.com>
2015-08-18 18:02:25 +0000

    WordPress 4.3
    Built from https://develop.svn.wordpress.org/trunk@33633
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33600 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-RC3-33632';
+$wp_version = '4.3';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
