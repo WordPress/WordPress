b407bb0936f03c2812fde0948d3afbca244616f9
Drew Jaynes <info@drewapicture.com>
2015-08-20 22:44:25 +0000

    Docs: Standardize `@deprecated` tag formatting in the DocBlock for the `logIO()` function.
    Props Alphawolf.
    Fixes #28806.
    
    Built from https://develop.svn.wordpress.org/trunk@33680
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33647 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- xmlrpc.php
+++ xmlrpc.php
@@ -88,8 +88,8 @@ exit;
 /**
  * logIO() - Writes logging info to a file.
  *
- * @deprecated 3.4.0
- * @deprecated Use error_log()
+ * @deprecated 3.4.0 Use error_log()
+ * @see error_log()
  *
  * @param string $io Whether input or output
  * @param string $msg Information describing logging reason.
