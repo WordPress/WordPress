6d405d34dae096fb8df71173e3b46cb86b441fd1
Drew Jaynes <info@drewapicture.com>
2015-08-21 05:33:25 +0000

    Docs: Clarify the DocBlock summary and add a description and return description for `_get_additional_user_keys()`.
    As noted in the description, returned keys being set is dependent on the existence of those keys in user meta at the point where `_get_additional_user_keys()` is called in `wp_update_user()`.
    
    Fixes #29120.
    
    Built from https://develop.svn.wordpress.org/trunk@33687
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33654 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -2351,13 +2351,16 @@ function wp_create_user($username, $password, $email = '') {
 }
 
 /**
- * Return a list of meta keys that wp_insert_user() is supposed to set.
+ * Returns a list of meta keys to be (maybe) populated in wp_update_user().
+ *
+ * Values actually keys returned via this function are dependent on the presence
+ * of those keys in the user meta data.
  *
  * @since 3.3.0
  * @access private
  *
  * @param WP_User $user WP_User instance.
- * @return array
+ * @return array List of user keys to be populated in wp_update_user().
  */
 function _get_additional_user_keys( $user ) {
 	$keys = array( 'first_name', 'last_name', 'nickname', 'description', 'rich_editing', 'comment_shortcuts', 'admin_color', 'use_ssl', 'show_admin_bar_front' );
