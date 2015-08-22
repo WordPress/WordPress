f2a9cd365bdbddc443f12e16c67c96d0976939a9
Scott Taylor <scott.c.taylor@mac.com>
2015-08-22 16:25:28 +0000

    `update-core.php` help docs, update string to include translations: `themes, plugins, and translations`
    Props netweb, DrewAPicture.
    Fixes #28201.
    
    Built from https://develop.svn.wordpress.org/trunk@33704
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33671 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/update-core.php
+++ wp-admin/update-core.php
@@ -506,7 +506,7 @@ if ( ( 'do-theme-upgrade' == $action || ( 'do-plugin-upgrade' == $action && ! is
 $title = __('WordPress Updates');
 $parent_file = 'index.php';
 
-$updates_overview  = '<p>' . __( 'On this screen, you can update to the latest version of WordPress, as well as update your themes and plugins from the WordPress.org repositories.' ) . '</p>';
+$updates_overview  = '<p>' . __( 'On this screen, you can update to the latest version of WordPress, as well as update your themes, plugins, and translations from the WordPress.org repositories.' ) . '</p>';
 $updates_overview .= '<p>' . __( 'If an update is available, you&#8127;ll see a notification appear in the Toolbar and navigation menu.' ) . ' ' . __( 'Keeping your site updated is important for security. It also makes the internet a safer place for you and your readers.' ) . '</p>';
 
 get_current_screen()->add_help_tab( array(
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-33703';
+$wp_version = '4.4-alpha-33704';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
