b4a5dd59f3e03fcf081e4a07f4c4438410880d8e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-08-25 21:53:20 +0000

    Customizer: Use existing decoupled strings in Menu Locations section. See [31941] and [31951].
    props egill.
    fixes #33416.
    Built from https://develop.svn.wordpress.org/trunk@33741
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33709 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-customize-nav-menus.php
+++ wp-includes/class-wp-customize-nav-menus.php
@@ -464,10 +464,14 @@ final class WP_Customize_Nav_Menus {
 		) ) );
 		$menus = wp_get_nav_menus();
 
-		// Menu loactions.
+		// Menu locations.
 		$locations     = get_registered_nav_menus();
 		$num_locations = count( array_keys( $locations ) );
-		$description   = '<p>' . sprintf( _n( 'Your theme contains %s menu location. Select which menu you would like to use.', 'Your theme contains %s menu locations. Select which menu appears in each location.', $num_locations ), number_format_i18n( $num_locations ) );
+		if ( 1 == $num_locations ) {
+			$description = '<p>' . __( 'Your theme supports one menu. Select which menu you would like to use.' );
+		} else {
+			$description = '<p>' . sprintf( _n( 'Your theme supports %s menu. Select which menu appears in each location.', 'Your theme supports %s menus. Select which menu appears in each location.', $num_locations ), number_format_i18n( $num_locations ) );
+		}
 		$description  .= '</p><p>' . __( 'You can also place menus in widget areas with the Custom Menu widget.' ) . '</p>';
 
 		$this->manager->add_section( 'menu_locations', array(
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-33740';
+$wp_version = '4.4-alpha-33741';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
