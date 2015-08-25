d75453cc7c08e586b81fc48464da5465427b33f2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-08-25 23:14:21 +0000

    Remove rounded corners from "Choose from the most used tags" result in Tags meta box.
    props tyxla.
    fixes #31560.
    Built from https://develop.svn.wordpress.org/trunk@33742
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33710 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-33741';
+$wp_version = '4.4-alpha-33742';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
