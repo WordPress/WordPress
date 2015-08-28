f62b7a70ba50acd18a77e34f8aa87c5160a13f1a
Drew Jaynes <info@drewapicture.com>
2015-08-28 15:34:21 +0000

    Docs: Add a changelog entry to the `pre_add_site_option_$option` hook doc for the `$option` parameter, added in [33738].
    See #28402.
    
    Built from https://develop.svn.wordpress.org/trunk@33788
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33756 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/option.php
+++ wp-includes/option.php
@@ -1102,6 +1102,7 @@ function add_site_option( $option, $value ) {
 	 *
 	 * @since 2.9.0 As 'pre_add_site_option_' . $key
 	 * @since 3.0.0
+	 * @since 4.4.0 The `$option` parameter was added
 	 *
 	 * @param mixed  $value  Value of site option.
 	 * @param string $option Option name.
