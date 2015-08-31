cf7694717b89ccac25159bd937f6ce7d82ecacb0
Drew Jaynes <info@drewapicture.com>
2015-08-31 20:50:20 +0000

    Docs: Clarify the DocBlock summary for `wp_create_nonce()` to mention that nonce-creation is also now dependent on user tokens, as of [29221].
    Also adds a changelog entry for when user tokens were introduced into the nonce-creation process.
    
    Props ericlewis
    Fixes #33636.
    
    Built from https://develop.svn.wordpress.org/trunk@33827
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33795 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -1831,9 +1831,11 @@ endif;
 
 if ( !function_exists('wp_create_nonce') ) :
 /**
- * Creates a cryptographic token tied to a specific action, user, and window of time.
+ * Creates a cryptographic token tied to a specific action, user, user session,
+ * and window of time.
  *
  * @since 2.0.3
+ * @since 4.0.0 Session tokens were integrated with nonce creation
  *
  * @param string|int $action Scalar value to add context to the nonce.
  * @return string The token.
