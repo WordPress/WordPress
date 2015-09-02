a880c5c5761635084a6bb15831b766a34fdfb4c2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-09-02 18:31:21 +0000

    Provide more helpful feedback than just "Cheatin' uh?" for permission errors in `wp-admin/options.php`.
    props ericlewis, kraftbj, lukecarbis, mrmist.
    fixes #33674. see #14530.
    Built from https://develop.svn.wordpress.org/trunk@33863
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33831 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options.php
+++ wp-admin/options.php
@@ -44,8 +44,13 @@ if ( empty($option_page) ) {
 	$capability = apply_filters( "option_page_capability_{$option_page}", $capability );
 }
 
-if ( !current_user_can( $capability ) )
-	wp_die( __( 'Cheatin&#8217; uh?' ), 403 );
+if ( ! current_user_can( $capability ) ) {
+	wp_die(
+		'<h1>' . __( 'Cheatin&#8217; uh?' ) . '</h1>' .
+		'<p>' . __( 'You are not allowed to manage these items.' ) . '</p>',
+		403
+	);
+}
 
 // Handle admin email change requests
 if ( is_multisite() ) {
@@ -68,8 +73,13 @@ if ( is_multisite() ) {
 	}
 }
 
-if ( is_multisite() && !is_super_admin() && 'update' != $action )
-	wp_die( __( 'Cheatin&#8217; uh?' ), 403 );
+if ( is_multisite() && ! is_super_admin() && 'update' != $action ) {
+	wp_die(
+		'<h1>' . __( 'Cheatin&#8217; uh?' ) . '</h1>' .
+		'<p>' . __( 'You are not allowed to delete these items.' ) . '</p>',
+		403
+	);
+}
 
 $whitelist_options = array(
 	'general' => array( 'blogname', 'blogdescription', 'gmt_offset', 'date_format', 'time_format', 'start_of_week', 'timezone_string', 'WPLANG' ),
