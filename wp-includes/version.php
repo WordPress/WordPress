f35c1764265dde78f5ce5466ba8c742f8d89db2a
Andrea Fercia <a.fercia@gmail.com>
2015-09-02 16:56:20 +0000

    Customize: use correct CSS selectors for the Menu Customizer reorder-toggle button after [33604].
    Fixes #33633.
    Built from https://develop.svn.wordpress.org/trunk@33856
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33824 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-33855';
+$wp_version = '4.4-alpha-33856';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
