3e0929740e1fef4be63c7984125739e869ccc600
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-09-02 18:38:21 +0000

    Provide more helpful feedback than just "Cheatin' uh?" for permission errors in `wp-admin/press-this.php`.
    props ericlewis, kraftbj, lukecarbis, mrmist.
    fixes #33676. see #14530.
    Built from https://develop.svn.wordpress.org/trunk@33865
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33833 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-33864';
+$wp_version = '4.4-alpha-33865';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
