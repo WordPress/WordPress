e2038f9150594ae3f81522de824eb5b9dbe68c5c
Drew Jaynes <info@drewapicture.com>
2015-09-03 04:36:30 +0000

    Docs: Add inline DocBlocks for the `require_once()` calls that now bring in top-level HTTP API functionality and HTTP API classes.
    Classes brought in from separate files now include:
    * `WP_Http`
    * `WP_Http_Streams`
    * `WP_Http_Curl`
    * `WP_HTTP_Proxy`
    * `WP_Http_Cookie`
    * `WP_Http_Encoding`
    
    See #33413. See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@33882
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33851 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/http.php
+++ wp-includes/http.php
@@ -10,10 +10,23 @@
  * @since 2.7.0
  */
 
+/** Core HTTP API functionality */
 require_once( ABSPATH . WPINC . '/http-functions.php' );
+
+/** WP_Http class */
 require_once( ABSPATH . WPINC . '/class-http.php' );
+
+/** WP_Http_Streams class */
 require_once( ABSPATH . WPINC . '/class-wp-http-streams.php' );
+
+/** WP_Http_Curl transport class */
 require_once( ABSPATH . WPINC . '/class-wp-http-curl.php' );
+
+/** WP_HTTP_Proxy transport class */
 require_once( ABSPATH . WPINC . '/class-wp-http-proxy.php' );
+
+/** WP_Http_Cookie class */
 require_once( ABSPATH . WPINC . '/class-wp-http-cookie.php' );
+
+/** WP_Http_Encoding class */
 require_once( ABSPATH . WPINC . '/class-wp-http-encoding.php' );
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-33881';
+$wp_version = '4.4-alpha-33882';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
