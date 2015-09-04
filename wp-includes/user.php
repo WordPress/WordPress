5cf6c7449931829fe9a88a3b97fa07a3837e8bb0
Drew Jaynes <info@drewapicture.com>
2015-09-04 01:33:24 +0000

    Docs: Clarify the file header summary for wp-includes/user.php, the top-level file for the core Users API.
    Also adds inline DocBlocks for the `require_once()` calls that now bring in core users functionality and the `WP_User_Query` class, as of [33749].
    
    See #33413. See #33701.
    
    Built from https://develop.svn.wordpress.org/trunk@33897
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33866 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -1,10 +1,14 @@
 <?php
 /**
- * WordPress User API
+ * Core Users API
  *
  * @package WordPress
  * @subpackage Users
+ * @since 2.1.0
  */
 
+/** Core users functionality */
 require_once( ABSPATH . WPINC . '/user-functions.php' );
+
+/** WP_User_Query class */
 require_once( ABSPATH . WPINC . '/class-wp-user-query.php' );
