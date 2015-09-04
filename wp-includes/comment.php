bfe8b01ef290d519346da05489e84ebb7b916340
Drew Jaynes <info@drewapicture.com>
2015-09-04 01:47:25 +0000

    Docs: Clarify the file header summary for wp-includes/comment.php, the top-level file for the core Comments API.
    Also adds inline DocBlock for the `require_once()` calls that now bring in the `WP_Comment` and `WP_Comment_Query` classes, as well as core comments functionality.
    
    See #33413. See #33701.
    
    Built from https://develop.svn.wordpress.org/trunk@33900
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33869 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment.php
+++ wp-includes/comment.php
@@ -1,11 +1,17 @@
 <?php
 /**
- * Manages WordPress comments
+ * Core Comments API
  *
  * @package WordPress
  * @subpackage Comment
+ * @since 1.5.0
  */
 
+/** WP_Comment class */
 require_once( ABSPATH . WPINC . '/class-wp-comment.php' );
+
+/** WP_Comment_Query class */
 require_once( ABSPATH . WPINC . '/class-wp-comment-query.php' );
+
+/** Core comments functionality */
 require_once( ABSPATH . WPINC . '/comment-functions.php' );
