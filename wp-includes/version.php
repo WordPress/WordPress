f482b909e68fb3c42761d5bdc81e2996a4b75800
John Blackbourn <johnbillion@git.wordpress.org>
2015-09-05 22:33:23 +0000

    Introduce a new `$is_edge` global for the Microsoft Edge browser.
    The `$is_IE` and `$is_chrome` globals no longer return `true` for Edge, which is expected as Edge is its own browser and should not be treated like IE. While it might be beneficial for Edge to be treated like Chrome on the client side, it benefits nobody for the `$is_chrome` global to return true.
    
    See #33193
    Props gregrickaby, johnbillion
    
    Built from https://develop.svn.wordpress.org/trunk@33927
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33896 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-33926';
+$wp_version = '4.4-alpha-33927';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
