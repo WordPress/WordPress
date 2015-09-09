6d4442ba127d65897d5e4c4821e4d55d40248534
Scott Taylor <scott.c.taylor@mac.com>
2015-09-09 03:23:23 +0000

    Revisions screen: change language from "Return to post editor" to "Return to editor" - more generic for usage by custom post types.
    Props lancewillett, adamsilverstein.
    Fixes #31910.
    
    Built from https://develop.svn.wordpress.org/trunk@33966
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33935 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-33965';
+$wp_version = '4.4-alpha-33966';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
