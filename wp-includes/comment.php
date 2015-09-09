67f90df6a41650c3b774adff80f0797ed9b3c82f
Scott Taylor <scott.c.taylor@mac.com>
2015-09-09 02:41:24 +0000

    `Walker_Comment` should be in its own file. Loaded now via `wp-includes/comment.php`, which makes it 100% BC.
    See #33413.
    
    Built from https://develop.svn.wordpress.org/trunk@33962
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33931 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment.php
+++ wp-includes/comment.php
@@ -13,5 +13,8 @@ require_once( ABSPATH . WPINC . '/class-wp-comment.php' );
 /** WP_Comment_Query class */
 require_once( ABSPATH . WPINC . '/class-wp-comment-query.php' );
 
+/** Walker_Comment class */
+require_once( ABSPATH . WPINC . '/class-walker-comment.php' );
+
 /** Core comments functionality */
 require_once( ABSPATH . WPINC . '/comment-functions.php' );
