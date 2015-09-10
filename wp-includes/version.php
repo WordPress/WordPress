bd0a6bbf8c5d6c057db199c8e30a429fec4d7640
Scott Taylor <scott.c.taylor@mac.com>
2015-09-10 22:01:24 +0000

    Create `wp-admin/includes/network.php` via `svn cp` and only load it in `wp-admin/network.php`. Move the functions there.
    See #33813.
    
    Built from https://develop.svn.wordpress.org/trunk@34023
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33992 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34022';
+$wp_version = '4.4-alpha-34023';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
