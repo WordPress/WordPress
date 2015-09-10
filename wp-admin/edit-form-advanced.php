21dd98695c1266f4d4a10e8697977c3a966fb9c2
Scott Taylor <scott.c.taylor@mac.com>
2015-09-10 21:00:24 +0000

    After [34004], don't break apart the paragraph into multiple translatable strings.
    Fixes #18418.
    
    Built from https://develop.svn.wordpress.org/trunk@34016
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@33985 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-form-advanced.php
+++ wp-admin/edit-form-advanced.php
@@ -395,13 +395,8 @@ if ( 'post' == $post_type || 'page' == $post_type ) {
 
 if ( 'post' == $post_type ) {
 	$publish_box = '<p>' . __('Several boxes on this screen contain settings for how your content will be published, including:') . '</p>';
-	$publish_box .= '<ul><li>' . __( '<strong>Publish</strong> - ' ) .
-		__( 'You can set the terms of publishing your post in the Publish box. ') .
-		__( 'For Status, Visibility, and Publish (immediately), click on the Edit link to reveal more options. ') .
-		__( 'Visibility includes options for password-protecting a post or making it stay at the top of your blog indefinitely (sticky). ' ) .
-		__( 'Publish (immediately) allows you to set a future or past date and time, so you can schedule a post to be published in the future or backdate a post. ' ) .
-		__( 'The Password protected option allows you to set an arbitrary password for each post. ' ) .
-		__( 'The Private option hides the post from everyone except editors and administrators. ' ) .
+	$publish_box .= '<ul><li>' .
+		__( '<strong>Publish</strong> &mdash; You can set the terms of publishing your post in the Publish box. For Status, Visibility, and Publish (immediately), click on the Edit link to reveal more options. Visibility includes options for password-protecting a post or making it stay at the top of your blog indefinitely (sticky). Publish (immediately) allows you to set a future or past date and time, so you can schedule a post to be published in the future or backdate a post. The Password protected option allows you to set an arbitrary password for each post. The Private option hides the post from everyone except editors and administrators.' ) .
 	'</li>';
 
 	if ( current_theme_supports( 'post-formats' ) && post_type_supports( 'post', 'post-formats' ) ) {
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34015';
+$wp_version = '4.4-alpha-34016';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
