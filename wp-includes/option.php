99c85c5b45cddaa8b9ff7631ab9df48f6d4090c2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-09-11 15:10:27 +0000

    Update the length limit in `set_transient()` docs after [34030].
    Props OriginalEXE.
    Fixes #13310.
    Built from https://develop.svn.wordpress.org/trunk@34045
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34013 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/option.php
+++ wp-includes/option.php
@@ -670,7 +670,7 @@ function get_transient( $transient ) {
  * @since 2.8.0
  *
  * @param string $transient  Transient name. Expected to not be SQL-escaped. Must be
- *                           45 characters or fewer in length.
+ *                           172 characters or fewer in length.
  * @param mixed  $value      Transient value. Must be serializable if non-scalar.
  *                           Expected to not be SQL-escaped.
  * @param int    $expiration Optional. Time until expiration in seconds. Default 0.
