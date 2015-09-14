f72b21af23da6b6d54208e5c1d65ececdaa109c8
Nikolay Bachiyski <nb@nikolay.bg>
2015-09-14 22:47:23 +0000

    Shortcodes: don't allow unclosed HTML elements in attributes
    Merges [34134] for 4.3 branch
    
    Built from https://develop.svn.wordpress.org/branches/4.3@34144
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.3@34112 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/media.php
+++ wp-includes/media.php
@@ -863,6 +863,8 @@ function img_caption_shortcode( $attr, $content = null ) {
 			$content = $matches[1];
 			$attr['caption'] = trim( $matches[2] );
 		}
+	} elseif ( strpos( $attr['caption'], '<' ) !== false ) {
+		$attr['caption'] = wp_kses( $attr['caption'], 'post' );
 	}
 
 	/**
