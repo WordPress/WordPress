fde014e85e66004d924ecf8a484d55f0f922712d
Scott Taylor <scott.c.taylor@mac.com>
2015-09-15 04:02:25 +0000

    Move the admin Nav Menu Walker subclasses into their own files. Load in `nav-menu.php` to remain BC.
    See #33413.
    
    Built from https://develop.svn.wordpress.org/trunk@34168
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34136 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34167';
+$wp_version = '4.4-alpha-34168';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
