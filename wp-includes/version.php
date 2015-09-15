d5e02c6129a04a73ef35569abf577ef854994d00
Scott Taylor <scott.c.taylor@mac.com>
2015-09-15 18:44:48 +0000

    After [34188], fix unit test for image header.
    See #33319.
    
    Built from https://develop.svn.wordpress.org/trunk@34214
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34178 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34213';
+$wp_version = '4.4-alpha-34214';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
