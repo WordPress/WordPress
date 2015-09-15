5ef1e3048be9ff54137da14b19ab8f04ca4768f5
Dominik Schilling <dominikschilling+git@gmail.com>
2015-09-15 14:53:17 +0000

    Finish bumping the 3.9 branch to 3.9.11. Built from https://develop.svn.wordpress.org/branches/3.9@34196
    git-svn-id: http://core.svn.wordpress.org/branches/3.9@34163 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.9.8';
+$wp_version = '3.9.9';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
