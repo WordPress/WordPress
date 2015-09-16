faa54e4dc2d655ba0cca7bbdc98cc3a160d1f2e3
Drew Jaynes <info@drewapicture.com>
2015-09-16 11:02:24 +0000

    Docs: Make a minor adjustment to the description syntax for the optional `$comment` argument in the DocBlock for `comment_link()`, introduced in [34071].
    See #33638. See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@34230
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34194 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/feed.php
+++ wp-includes/feed.php
@@ -293,7 +293,7 @@ function get_comment_guid($comment_id = null) {
  * @since 1.5.0
  * @since 4.4.0 Introduced the `$comment` argument.
  *
- * @param int|WP_Comment $comment Optional comment object or id. Defaults to global comment object.
+ * @param int|WP_Comment $comment Optional. Comment object or id. Defaults to global comment object.
  */
 function comment_link( $comment = null ) {
 	/**
