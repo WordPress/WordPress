19bcadf2a6974680ba032ae4e8224fc43b216167
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-09-16 11:46:25 +0000

    Reset Password: Move `<div>` out of `<p>` in `wp-login.php`.
    Props ldinclaux.
    Fixes #33892.
    Built from https://develop.svn.wordpress.org/trunk@34232
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34196 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-login.php
+++ wp-login.php
@@ -653,16 +653,18 @@ case 'rp' :
 	<input type="hidden" id="user_login" value="<?php echo esc_attr( $rp_login ); ?>" autocomplete="off" />
 
 	<p class="user-pass1-wrap">
-		<label for="pass1"><?php _e('New password') ?></label><br />
-		<div class="wp-pwd">
-			<span class="password-input-wrapper">
-				<input type="password" data-reveal="1" data-pw="<?php echo esc_attr( wp_generate_password( 16 ) ); ?>" name="pass1" id="pass1" class="input" size="20" value="" autocomplete="off" aria-describedby="pass-strength-result" />
-			</span>
-			<div id="pass-strength-result" class="hide-if-no-js" aria-live="polite"><?php _e( 'Strength indicator' ); ?></div>
-		</div>
+		<label for="pass1"><?php _e( 'New password' ) ?></label>
 	</p>
+
+	<div class="wp-pwd">
+		<span class="password-input-wrapper">
+			<input type="password" data-reveal="1" data-pw="<?php echo esc_attr( wp_generate_password( 16 ) ); ?>" name="pass1" id="pass1" class="input" size="20" value="" autocomplete="off" aria-describedby="pass-strength-result" />
+		</span>
+		<div id="pass-strength-result" class="hide-if-no-js" aria-live="polite"><?php _e( 'Strength indicator' ); ?></div>
+	</div>
+
 	<p class="user-pass2-wrap">
-		<label for="pass2"><?php _e('Confirm new password') ?></label><br />
+		<label for="pass2"><?php _e( 'Confirm new password' ) ?></label><br />
 		<input type="password" name="pass2" id="pass2" class="input" size="20" value="" autocomplete="off" />
 	</p>
 
