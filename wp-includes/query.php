18adeb92fa28de83681dfcc45728590c27673f18
Drew Jaynes <info@drewapicture.com>
2015-09-18 10:27:24 +0000

    Docs: Fix some syntactical issues in the documentation for `get_queried_object_id()`.
    Adds descriptions for the global `WP_Query` instance and the return.
    
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@34286
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34250 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/query.php
+++ wp-includes/query.php
@@ -46,14 +46,16 @@ function get_queried_object() {
 }
 
 /**
- * Retrieve ID of the current queried object. Wrapper for $wp_query->get_queried_object_id()
+ * Retrieve ID of the current queried object.
+ *
+ * Wrapper for WP_Query::get_queried_object_id().
  *
  * @since 3.1.0
  * @access public
  *
- * @global WP_Query $wp_query
+ * @global WP_Query $wp_query Global WP_Query instance.
  *
- * @return int
+ * @return int ID of the queried object.
  */
 function get_queried_object_id() {
 	global $wp_query;
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34285';
+$wp_version = '4.4-alpha-34286';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
