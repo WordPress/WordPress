f3a62b7714eb5d03a214b2f75b1aed4ca291084b
Dominik Schilling <dominikschilling+git@gmail.com>
2015-09-18 16:03:24 +0000

    Theme Editor: Change file descriptions of `header.php` and `footer.php`.
    `header.php` is now "Theme Header" and `footer.php` is "Theme Footer". This prevents a clash with other "Header" strings in the context of "Custom Header".
    
    Props ramiy.
    Fixes #32975.
    Built from https://develop.svn.wordpress.org/trunk@34290
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34254 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34289';
+$wp_version = '4.4-alpha-34290';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
