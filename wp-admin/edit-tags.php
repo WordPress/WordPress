a87885262f218ab788afebe6cb77ea71d0d1605b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-09-18 19:03:25 +0000

    Include placeholder in translator comment.
    See #31863.
    Built from https://develop.svn.wordpress.org/trunk@34306
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34270 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-tags.php
+++ wp-admin/edit-tags.php
@@ -334,9 +334,9 @@ endif; ?>
 <div class="form-wrap">
 <p>
 	<?php
-	echo '<strong>' . __( 'Note:' ) . '</strong><br>';
+	echo '<strong>' . __( 'Note:' ) . '</strong><br />';
 	printf(
-		/* translators: default category */
+		/* translators: %s: default category */
 		__( 'Deleting a category does not delete the posts in that category. Instead, posts that were only assigned to the deleted category are set to the category %s.' ),
 		/** This filter is documented in wp-includes/category-template.php */
 		'<strong>' . apply_filters( 'the_category', get_cat_name( get_option( 'default_category') ) ) . '</strong>'
