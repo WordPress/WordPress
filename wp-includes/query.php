7a73dfbfb6772c522cbb98f9dbf456e7f592defc
Drew Jaynes <info@drewapicture.com>
2015-09-18 10:21:26 +0000

    Docs: Fix some syntactical isses in the DocBlock for `get_queried_object()`.
    Also add descriptions for the return and the `WP_Query` global.
    
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@34285
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34249 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/query.php
+++ wp-includes/query.php
@@ -29,14 +29,16 @@ function get_query_var( $var, $default = '' ) {
 }
 
 /**
- * Retrieve the currently-queried object. Wrapper for $wp_query->get_queried_object()
+ * Retrieve the currently-queried object.
+ *
+ * Wrapper for WP_Query::get_queried_object().
  *
  * @since 3.1.0
  * @access public
  *
- * @global WP_Query $wp_query
+ * @global WP_Query $wp_query Global WP_Query instance.
  *
- * @return object
+ * @return object Queried object.
  */
 function get_queried_object() {
 	global $wp_query;
