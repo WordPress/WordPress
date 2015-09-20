d2578f84d1272e2bca8bc79b15357ddc9a98a5c0
Scott Taylor <scott.c.taylor@mac.com>
2015-09-20 02:31:24 +0000

    MediaElement: apply monkey patch to include a PR which fixes a breaking change made upstream on a public interface: https://github.com/johndyer/mediaelement/commit/ccba8d0b6ad1661026e43879876126223c88c5e2
    Props bradyvercher.
    Fixes #33798.
    
    Built from https://develop.svn.wordpress.org/trunk@34339
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34303 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34338';
+$wp_version = '4.4-alpha-34339';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
