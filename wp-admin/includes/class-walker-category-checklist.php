82418138256250077ebf5eb89cd55dd799ae30fd
Drew Jaynes <info@drewapicture.com>
2015-09-21 23:13:26 +0000

    Docs: Move a duplicate hook comment accidentally left behind when its corresponding filter was relocated in [31765].
    See #31443. See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@34368
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34332 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-walker-category-checklist.php
+++ wp-admin/includes/class-walker-category-checklist.php
@@ -83,7 +83,6 @@ class Walker_Category_Checklist extends Walker {
 
 		$args['selected_cats'] = empty( $args['selected_cats'] ) ? array() : $args['selected_cats'];
 
-		/** This filter is documented in wp-includes/category-template.php */
 		if ( ! empty( $args['list_only'] ) ) {
 			$aria_cheched = 'false';
 			$inner_class = 'category';
@@ -93,11 +92,13 @@ class Walker_Category_Checklist extends Walker {
 				$aria_cheched = 'true';
 			}
 
+			/** This filter is documented in wp-includes/category-template.php */
 			$output .= "\n" . '<li' . $class . '>' .
 				'<div class="' . $inner_class . '" data-term-id=' . $category->term_id .
 				' tabindex="0" role="checkbox" aria-checked="' . $aria_cheched . '">' .
 				esc_html( apply_filters( 'the_category', $category->name ) ) . '</div>';
 		} else {
+			/** This filter is documented in wp-includes/category-template.php */
 			$output .= "\n<li id='{$taxonomy}-{$category->term_id}'$class>" .
 				'<label class="selectit"><input value="' . $category->term_id . '" type="checkbox" name="'.$name.'[]" id="in-'.$taxonomy.'-' . $category->term_id . '"' .
 				checked( in_array( $category->term_id, $args['selected_cats'] ), true, false ) .
