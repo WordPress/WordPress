b0749a1a0652af697476b84475926801b6e0fcd7
Drew Jaynes <info@drewapicture.com>
2015-09-22 13:06:25 +0000

    Docs: Clarify the file header summary for wp-includes/capabilities.php, which was broken up into multiple files in [33752].
    Also adds inline DocBlocks for files now brought in via `require_once()` from this file.
    
    See #33413. See #33701.
    
    Built from https://develop.svn.wordpress.org/trunk@34398
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34362 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/capabilities.php
+++ wp-includes/capabilities.php
@@ -1,12 +1,20 @@
 <?php
 /**
- * WordPress Roles and Capabilities.
+ * Core User Role & Capabilities API
  *
  * @package WordPress
  * @subpackage User
+ * @since 2.0.0
  */
 
+/** WP_Roles class */
 require_once( ABSPATH . WPINC . '/class-wp-roles.php' );
+
+/** WP_Role class */
 require_once( ABSPATH . WPINC . '/class-wp-role.php' );
+
+/** WP_User class */
 require_once( ABSPATH . WPINC . '/class-wp-user.php' );
+
+/** Core capabilities functionality */
 require_once( ABSPATH . WPINC . '/capabilities-functions.php' );
