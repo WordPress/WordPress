24180ac754fe2d2af6f7f42bbd3917c961f64e3e
Drew Jaynes <info@drewapicture.com>
2015-09-22 13:23:25 +0000

    Docs: Clarify the file header summary for wp-includes/taxonomy.php, the top-level file for the core Taxonomy API.
    See #33413. See #33701.
    
    Built from https://develop.svn.wordpress.org/trunk@34404
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34368 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/taxonomy.php
+++ wp-includes/taxonomy.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * Taxonomy API
- *
- * @since 2.3.0
+ * Core Taxonomy API
  *
  * @package WordPress
  * @subpackage Taxonomy
+ * @since 2.3.0
  */
 
+/** Core taxonomy functionality */
 require_once( ABSPATH . WPINC . '/taxonomy-functions.php' );
+
+/** WP_Tax_Query class */
 require_once( ABSPATH . WPINC . '/class-wp-tax-query.php' );
