f980e7b9bbe3c748ab6307a885622cc90d0806bb
Drew Jaynes <info@drewapicture.com>
2015-09-22 13:36:25 +0000

    Docs: Clarify the file header summary for wp-includes/default-widgets.php, the top-level file for bringing in the core widget classes.
    Also adds inline DocBlocks for the widget classes now brought in via `require_once()` as of [33843].
    
    See #33413. See #33701.
    
    Built from https://develop.svn.wordpress.org/trunk@34408
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34372 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-widgets.php
+++ wp-includes/default-widgets.php
@@ -1,21 +1,47 @@
 <?php
 /**
- * Default Widgets
+ * Widget API: Default core widgets
  *
  * @package WordPress
  * @subpackage Widgets
+ * @since 2.8.0
  */
 
+/** WP_Widget_Pages class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-pages.php' );
+
+/** WP_Widget_Links class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-links.php' );
+
+/** WP_Widget_Search class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-search.php' );
+
+/** WP_Widget_Archives class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-archives.php' );
+
+/** WP_Widget_Meta class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-meta.php' );
+
+/** WP_Widget_Calendar class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-calendar.php' );
+
+/** WP_Widget_Text class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-text.php' );
+
+/** WP_Widget_Categories class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-categories.php' );
+
+/** WP_Widget_Recent_Posts class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-recent-posts.php' );
+
+/** WP_Widget_Recent_Comments class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-recent-comments.php' );
+
+/** WP_Widget_RSS class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-rss.php' );
+
+/** WP_Widget_Tag_Cloud class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-tag-cloud.php' );
+
+/** WP_Nav_Menu_Widget class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-nav-menu-widget.php' );
