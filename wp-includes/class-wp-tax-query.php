bbe704403012e6585d9c85a79556d83e6d2ca9bd
Drew Jaynes <info@drewapicture.com>
2015-09-22 13:16:30 +0000

    Docs: Add a file header to wp-includes/class-wp-tax-query.php, introduced in [33760].
    Also clarifies the class DocBlock summary and description for `WP_Tax_Query`.
    
    See #33413. See #33701.
    
    Built from https://develop.svn.wordpress.org/trunk@34402
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34366 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-tax-query.php
+++ wp-includes/class-wp-tax-query.php
@@ -1,14 +1,23 @@
 <?php
 /**
- * Class for generating SQL clauses that filter a primary query according to object taxonomy terms.
+ * Taxonomy API: WP_Tax_Query class
  *
- * `WP_Tax_Query` is a helper that allows primary query classes, such as WP_Query, to filter
- * their results by object metadata, by generating `JOIN` and `WHERE` subclauses to be attached
- * to the primary SQL query string.
- *
- * @since 3.1.0
  * @package WordPress
  * @subpackage Taxonomy
+ * @since 4.4.0
+ */
+
+/**
+ * Core class used to implement taxonomy queries for the Taxonomy API.
+ *
+ * Used for generating SQL clauses that filter a primary query according to object
+ * taxonomy terms.
+ *
+ * WP_Tax_Query is a helper that allows primary query classes, such as WP_Query, to filter
+ * their results by object metadata, by generating `JOIN` and `WHERE` subclauses to be
+ * attached to the primary SQL query string.
+ *
+ * @since 3.1.0
  */
 class WP_Tax_Query {
 
