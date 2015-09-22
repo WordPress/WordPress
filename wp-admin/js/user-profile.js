39552b63cab042f11f1131559ffdf40869da2f4f
Scott Taylor <scott.c.taylor@mac.com>
2015-09-22 03:57:24 +0000

    Passwords: fix the markup on the Reset Password Form for `user-pass1` so the JavaScript operates properly.
    Props ldinclaux.
    See #33892.
    Fixes #33908.
    
    Built from https://develop.svn.wordpress.org/trunk@34371
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34335 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/user-profile.js
+++ wp-admin/js/user-profile.js
@@ -190,11 +190,16 @@
 			}
 		} );
 
-		$passwordWrapper = $pass1Row.find('.wp-pwd').hide();
+		$passwordWrapper = $pass1Row.find( '.wp-pwd' );
+		$generateButton  = $pass1Row.find( 'button.wp-generate-pw' );
 
 		bindToggleButton();
 
-		$generateButton = $pass1Row.find( 'button.wp-generate-pw' ).show();
+		if ( $generateButton.length ) {
+			$passwordWrapper.hide();
+		}
+
+		$generateButton.show();
 		$generateButton.on( 'click', function () {
 			updateLock = true;
 
