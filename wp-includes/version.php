38b561e8df7e01884d5f7a936dd2bc7a148c157a
Dominik Schilling <dominikschilling+git@gmail.com>
2015-09-24 16:03:24 +0000

    Twenty Fifteen: Add a missing comma in rtl.css.
    Props Toro_Unit.
    Fixes #33985.
    Built from https://develop.svn.wordpress.org/trunk@34501
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34465 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34500';
+$wp_version = '4.4-alpha-34501';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
