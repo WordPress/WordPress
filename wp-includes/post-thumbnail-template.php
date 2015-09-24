4e9c1692b0d1ff8ed3e19632b1f6707319a3e39a
Scott Taylor <scott.c.taylor@mac.com>
2015-09-24 04:38:25 +0000

    Post Thumbnails: In `get_the_post_thumbnail_url()`, return `false` instead of empty string when no URL is available.
    Fixes #33070.
    
    Built from https://develop.svn.wordpress.org/trunk@34480
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34444 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post-thumbnail-template.php
+++ wp-includes/post-thumbnail-template.php
@@ -179,11 +179,11 @@ function get_the_post_thumbnail( $post = null, $size = 'post-thumbnail', $attr =
  * @param int|WP_Post  $post Optional. Post ID or WP_Post object.  Default is global `$post`.
  * @param string|array $size Optional. Registered image size to retrieve the source for or a flat
  *                           array of height and width dimensions. Default 'post-thumbnail'.
- * @return string Post thumbnail URL or empty string.
+ * @return string|false Post thumbnail URL or false if no URL is available.
  */
 function get_the_post_thumbnail_url( $post = null, $size = 'post-thumbnail' ) {
 	$image = wp_get_attachment_image_url( get_post_thumbnail_id( $post ), $size );
-	return isset( $image ) ? $image : '';
+	return isset( $image ) ? $image : false;
 }
 
 /**
