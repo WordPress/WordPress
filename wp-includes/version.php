0f27ceae563bac96f8b108a06454a2edfc0b424b
Gary Pendergast <gary@pento.net>
2015-09-24 03:08:26 +0000

    Tests: Remove a `var_dump()` added in [34373].
    See #33070.
    
    
    Built from https://develop.svn.wordpress.org/trunk@34468
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34432 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34467';
+$wp_version = '4.4-alpha-34468';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
