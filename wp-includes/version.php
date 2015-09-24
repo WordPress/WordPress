ee04cc0638666608048e83ba5ae157c42b5f3ef8
John Blackbourn <johnbillion@git.wordpress.org>
2015-09-24 20:33:26 +0000

    Another missed file from [34172].
    See #33877
    
    Built from https://develop.svn.wordpress.org/trunk@34518
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34482 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34517';
+$wp_version = '4.4-alpha-34518';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
