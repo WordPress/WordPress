c69ec5a19a2b6a96f8a03d432f5c0a9164d58271
John Blackbourn <johnbillion@git.wordpress.org>
2015-09-25 14:29:23 +0000

    Introduce a `send_update_notification_email` which controls whether an update notification email is sent for background updates. This filter allows control over each of the update types (plugin, theme, translation) and compliments the `automatic_updates_send_debug_email` and `send_core_update_notification_email` filters.
    Fixes #33932
    Props ronalfy
    
    Built from https://develop.svn.wordpress.org/trunk@34543
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34507 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34542';
+$wp_version = '4.4-alpha-34543';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
