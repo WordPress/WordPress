a6fecf0aa0334010e9f59cb30bfcd73be63ac5f0
Aaron Jorbin <aaron@jorb.in>
2015-09-25 19:43:25 +0000

    Add Travis Notifications to the #core channel on Slack
    This is hopefully the final attempt at adding notifications to the #core channel on Slack. By default, Travis spams the channel. In order to cut that down a bit, notifications are set as follows:
     - On Failure: Always (If the tests fail, the world needs to know)
     - On Success: Change (this means that the first green run after a red run aka, the world is happy again)
     - On Start: Never (Travis runs every time there is a commit. No need for a notification)
    
    Props Netweb, samuelsidler
    Fixes #30755
    
    
    Built from https://develop.svn.wordpress.org/trunk@34555
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34519 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34554';
+$wp_version = '4.4-alpha-34555';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
