efad2735139f1a4c1f84dd27fd68caa0446b144b
Drew Jaynes <info@drewapicture.com>
2015-09-26 13:05:26 +0000

    Docs: Relocate the hook doc for the `show_password_fields` filter to its duplicate, since the original was removed in [34582].
    Adds a changelog entry noting the adjustment in behavior, and simplifies the conditional logic.
    
    Fixes #15115.
    
    Built from https://develop.svn.wordpress.org/trunk@34590
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34554 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/user-edit.php
+++ wp-admin/user-edit.php
@@ -438,9 +438,15 @@ if ( is_multisite() && is_network_admin() && ! IS_PROFILE_PAGE && current_user_c
 </tr>
 
 <?php
-/** This filter is documented in wp-admin/user-new.php */
-$show_password_fields = apply_filters( 'show_password_fields', true, $profileuser );
-if ( $show_password_fields ) :
+/**
+ * Filter the display of the password fields.
+ *
+ * @since 1.5.1
+ * @since 4.4.0 Now evaluated only in user-edit.php.
+ *
+ * @param bool $show Whether to show the password fields. Default true.
+ */
+if ( $show_password_fields = apply_filters( 'show_password_fields', true, $profileuser ) ) :
 ?>
 </table>
 
