d09f7b6c1d71dfde0616e8a8ce4a66421be54a39
Aaron Jorbin <aaron@jorb.in>
2015-09-26 20:59:24 +0000

    Revert change from true to false in multisite upload test
    This change was introduced in [34601]. 0 is a special snowflake. The code treats it as if it should fall back to the default, rather than respecting 0 as 0.  Essentially, 0 means 100.  WAT? This has been discussed previusly in https://core.trac.wordpress.org/ticket/19538#comment:10.  This test should return True right now, but the behavior should be discussed and in the future 0 may mean 0.
    
    See #34037
    Props jorbin, boonebgorges
    
    
    Built from https://develop.svn.wordpress.org/trunk@34606
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34570 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34605';
+$wp_version = '4.4-alpha-34606';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
