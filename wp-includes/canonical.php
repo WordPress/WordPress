8cd1db9c9cd25aa2d4bd4abdd66be98bc0f9d5cc
Scott Taylor <scott.c.taylor@mac.com>
2015-09-27 22:11:25 +0000

    Canonical: after [34272], don't redirect rewrite endpoints on attachment URLs when pretty permalinks are enabled.
    Fixes #19918.
    
    Built from https://develop.svn.wordpress.org/trunk@34643
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34607 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/canonical.php
+++ wp-includes/canonical.php
@@ -39,7 +39,7 @@
  * @return string|void The string of the URL, if redirect needed.
  */
 function redirect_canonical( $requested_url = null, $do_redirect = true ) {
-	global $wp_rewrite, $is_IIS, $wp_query, $wpdb;
+	global $wp_rewrite, $is_IIS, $wp_query, $wpdb, $wp;
 
 	if ( isset( $_SERVER['REQUEST_METHOD'] ) && ! in_array( strtoupper( $_SERVER['REQUEST_METHOD'] ), array( 'GET', 'HEAD' ) ) ) {
 		return;
@@ -157,7 +157,9 @@ function redirect_canonical( $requested_url = null, $do_redirect = true ) {
 
 	} elseif ( is_object($wp_rewrite) && $wp_rewrite->using_permalinks() ) {
 		// rewriting of old ?p=X, ?m=2004, ?m=200401, ?m=20040101
-		if ( is_attachment() && ! $redirect_url ) {
+		if ( is_attachment() &&
+			! array_diff( array_keys( $wp->query_vars ), array( 'attachment', 'attachment_id' ) ) &&
+			! $redirect_url ) {
 			if ( ! empty( $_GET['attachment_id'] ) ) {
 				$redirect_url = get_attachment_link( get_query_var( 'attachment_id' ) );
 				if ( $redirect_url ) {
