ea03ecbe668025cc497374a5db044b9ac69c4a2e
Jeremy Felt <jeremy.felt@gmail.com>
2015-09-28 23:30:24 +0000

    Fixes #33816.
    MS: Adjust formatting of code in `site-new.php` to improve readability.
    
    Fixes #34066.
    
    Built from https://develop.svn.wordpress.org/trunk@34673
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34637 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/network/site-new.php
+++ wp-admin/network/site-new.php
@@ -19,12 +19,12 @@ if ( ! is_multisite() )
 if ( ! current_user_can( 'manage_sites' ) )
 	wp_die( __( 'You do not have sufficient permissions to add sites to this network.' ) );
 
-	get_current_screen()->add_help_tab( array(
-		'id'      => 'overview',
-		'title'   => __('Overview'),
-		'content' =>
-			'<p>' . __('This screen is for Super Admins to add new sites to the network. This is not affected by the registration settings.') . '</p>' .
-			'<p>' . __('If the admin email for the new site does not exist in the database, a new user will also be created.') . '</p>'
+get_current_screen()->add_help_tab( array(
+	'id'      => 'overview',
+	'title'   => __('Overview'),
+	'content' =>
+		'<p>' . __('This screen is for Super Admins to add new sites to the network. This is not affected by the registration settings.') . '</p>' .
+		'<p>' . __('If the admin email for the new site does not exist in the database, a new user will also be created.') . '</p>'
 ) );
 
 get_current_screen()->set_help_sidebar(
