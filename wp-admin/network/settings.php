9bb09f0e0851e4b79ed2047dea9c46093e9d4de8
Andrea Fercia <a.fercia@gmail.com>
2015-09-29 18:52:26 +0000

    Bump H3 headings to H2 on the Network Settings screen for better accessibility.
    Fixes #34078.
    Built from https://develop.svn.wordpress.org/trunk@34695
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34659 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/network/settings.php
+++ wp-admin/network/settings.php
@@ -101,7 +101,7 @@ if ( isset( $_GET['updated'] ) ) {
 	<h1><?php echo esc_html( $title ); ?></h1>
 	<form method="post" action="settings.php" novalidate="novalidate">
 		<?php wp_nonce_field( 'siteoptions' ); ?>
-		<h3><?php _e( 'Operational Settings' ); ?></h3>
+		<h2><?php _e( 'Operational Settings' ); ?></h2>
 		<table class="form-table">
 			<tr>
 				<th scope="row"><label for="site_name"><?php _e( 'Network Title' ) ?></label></th>
@@ -120,7 +120,7 @@ if ( isset( $_GET['updated'] ) ) {
 				</td>
 			</tr>
 		</table>
-		<h3><?php _e( 'Registration Settings' ); ?></h3>
+		<h2><?php _e( 'Registration Settings' ); ?></h2>
 		<table class="form-table">
 			<tr>
 				<th scope="row"><?php _e( 'Allow new registrations' ) ?></th>
@@ -196,7 +196,7 @@ if ( isset( $_GET['updated'] ) ) {
 			</tr>
 
 		</table>
-		<h3><?php _e('New Site Settings'); ?></h3>
+		<h2><?php _e( 'New Site Settings' ); ?></h2>
 		<table class="form-table">
 
 			<tr>
@@ -268,7 +268,7 @@ if ( isset( $_GET['updated'] ) ) {
 				</td>
 			</tr>
 		</table>
-		<h3><?php _e( 'Upload Settings' ); ?></h3>
+		<h2><?php _e( 'Upload Settings' ); ?></h2>
 		<table class="form-table">
 			<tr>
 				<th scope="row"><?php _e( 'Site upload space' ) ?></th>
@@ -306,7 +306,7 @@ if ( isset( $_GET['updated'] ) ) {
 		$translations = wp_get_available_translations();
 		if ( ! empty( $languages ) || ! empty( $translations ) ) {
 			?>
-			<h3><?php _e( 'Language Settings' ); ?></h3>
+			<h2><?php _e( 'Language Settings' ); ?></h2>
 			<table class="form-table">
 				<tr>
 					<th><label for="WPLANG"><?php _e( 'Default Language' ); ?></label></th>
@@ -333,7 +333,7 @@ if ( isset( $_GET['updated'] ) ) {
 		}
 		?>
 
-		<h3><?php _e( 'Menu Settings' ); ?></h3>
+		<h2><?php _e( 'Menu Settings' ); ?></h2>
 		<table id="menu" class="form-table">
 			<tr>
 				<th scope="row"><?php _e( 'Enable administration menus' ); ?></th>
