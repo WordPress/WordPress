0da31506094856bed5a02c338337f1ce471a390d
Aaron Jorbin <aaron@jorb.in>
2015-10-02 16:51:28 +0000

    Add PHP Nightlies to the test suite
    PHP branched master for 7, which means that nightly builds are now technically 7.1. The difference is minimal at the moment, but it's a good idea for us to test against the nightly build in part so that regressions can be reported upstream. Nightlies are allowed failure so that the build doesn't go red due to unstable versions. Additionally, nightlies are not officially supported by WordPress Core.
    
    See #33410
    
    Built from https://develop.svn.wordpress.org/trunk@34776
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34741 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34775';
+$wp_version = '4.4-alpha-34776';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
