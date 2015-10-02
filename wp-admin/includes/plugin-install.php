8478a1341b5240f26f1ffaa51a030f3a49700a7e
Drew Jaynes <info@drewapicture.com>
2015-10-02 04:48:25 +0000

    Docs: Clarify the expected replacement element type if replacing the object or array in `plugins_api()` via the `plugins_api` hook.
    If the `$action` type is 'query_plugins' or 'plugin_information', the API will return an object, thus an object should be passed if replacing it. For 'hot_tags' and 'hot_categories', the same logic applies, but with arrays instead.
    
    See #34035.
    
    Built from https://develop.svn.wordpress.org/trunk@34763
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34728 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/plugin-install.php
+++ wp-admin/includes/plugin-install.php
@@ -19,8 +19,8 @@
  * an object is returned.
  *
  * The second filter, {@see 'plugins_api'}, allows a plugin to override the WordPress.org
- * Plugin Install API entirely. If `$action` is 'query_plugins', 'plugin_information', or
- * 'hot_categories', an object MUST be passed. If `$action` is 'hot_tags`, an array should
+ * Plugin Install API entirely. If `$action` is 'query_plugins' or 'plugin_information',
+ * an object MUST be passed. If `$action` is 'hot_tags` or 'hot_categories', an array MUST
  * be passed.
  *
  * Finally, the third filter, {@see 'plugins_api_result'}, makes it possible to filter the
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34762';
+$wp_version = '4.4-alpha-34763';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
