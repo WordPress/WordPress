8286a4f2143805e012eab84d2f7d873f50100e37
Jeremy Felt <jeremy.felt@gmail.com>
2015-10-06 21:56:24 +0000

    Registration: Remove the last remaining `Lucida Grande` from WordPress.
    Defer to default styling provided by the theme (and browser) in `wp-activate.php` and remove the `font-family` and `color` from the provided internal stylesheet.
    
    Previously, [17327] and [18406].
    
    Fixes #34145.
    
    Built from https://develop.svn.wordpress.org/trunk@34882
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34847 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-activate.php
+++ wp-activate.php
@@ -58,7 +58,7 @@ function wpmu_activate_stylesheet() {
 		#submit, #key { width: 90%; font-size: 24px; }
 		#language { margin-top: .5em; }
 		.error { background: #f66; }
-		span.h3 { padding: 0 8px; font-size: 1.3em; font-family: "Lucida Grande", Verdana, Arial, "Bitstream Vera Sans", sans-serif; font-weight: bold; color: #333; }
+		span.h3 { padding: 0 8px; font-size: 1.3em; font-weight: bold; }
 	</style>
 	<?php
 }
