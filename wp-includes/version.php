a3ec4d45f330a4630a70762427f123ae0e9cea17
Scott Taylor <scott.c.taylor@mac.com>
2015-10-07 01:40:24 +0000

    List Tables: after [34276], decrease horizontal padding for sorted column headers (causing weirdness in the Count column of tax list tables) - the vertical padding change suffices in fixing the original issue.
    Fixes #27945.
    
    Built from https://develop.svn.wordpress.org/trunk@34892
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34857 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34891';
+$wp_version = '4.4-alpha-34892';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
