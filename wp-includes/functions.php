226bb29ff0619f2502aa988e23e009ef8bd335d9
Boone Gorges <boonebgorges@gmail.com>
2015-10-07 03:02:23 +0000

    Move `wp_installing()` to load.php.
    Various functions in load.php need to check whether WP is in installation mode.
    Let's let them.
    
    Props adamsilverstein.
    See #31130.
    Built from https://develop.svn.wordpress.org/trunk@34896
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34861 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -1258,37 +1258,6 @@ function do_robots() {
 }
 
 /**
- * Check or set whether WordPress is in "installation" mode.
- *
- * If the `WP_INSTALLING` constant is defined during the bootstrap, `wp_installing()` will default to `true`.
- *
- * @since 4.4.0
- *
- * @staticvar bool $installing
- *
- * @param bool $is_installing Optional. True to set WP into Installing mode, false to turn Installing mode off.
- *                            Omit this parameter if you only want to fetch the current status.
- * @return bool True if WP is installing, otherwise false. When a `$is_installing` is passed, the function will
- *              report whether WP was in installing mode prior to the change to `$is_installing`.
- */
-function wp_installing( $is_installing = null ) {
-	static $installing = null;
-
-	// Support for the `WP_INSTALLING` constant, defined before WP is loaded.
-	if ( is_null( $installing ) ) {
-		$installing = defined( 'WP_INSTALLING' ) && WP_INSTALLING;
-	}
-
-	if ( ! is_null( $is_installing ) ) {
-		$old_installing = $installing;
-		$installing = $is_installing;
-		return (bool) $old_installing;
-	}
-
-	return (bool) $installing;
-}
-
-/**
  * Test whether blog is already installed.
  *
  * The cache will be checked first. If you have a cache plugin, which saves
