c50acdcb4c0b09d6f060166cc4a3a5ffc326dbf1
Dominik Schilling <dominikschilling+git@gmail.com>
2015-10-08 11:12:28 +0000

    Embeds: Update `$wp_db_version` to flush rewrite rules for [34903].
    Fixes #34209.
    Built from https://develop.svn.wordpress.org/trunk@34937
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34902 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,14 +4,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34936';
+$wp_version = '4.4-alpha-34937';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 34528;
+$wp_db_version = 34903;
 
 /**
  * Holds the TinyMCE version
