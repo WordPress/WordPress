a998ea45e29657a73d335d18aa977081fdbac2a2
Ryan McCue <me@ryanmccue.info>
2015-10-08 02:40:24 +0000

    REST API: Add missing reference to WP_HTTP_Response
    See #33982
    
    Built from https://develop.svn.wordpress.org/trunk@34930
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34895 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-34929';
+$wp_version = '4.4-alpha-34930';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
