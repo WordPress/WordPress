4aea111b29ebf1d1d48ffbe4b6e196505b4365eb
Drew Jaynes <info@drewapicture.com>
2015-10-12 16:11:24 +0000

    Docs: Fix syntax and properly mark optional parameters as such in the DocBlock for `get_image_send_to_editor()`.
    Also fixes the return description, and adds the `$size` default value, missed in [35055].
    
    See #34257. See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@35056
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35021 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/media.php
+++ wp-admin/includes/media.php
@@ -106,17 +106,18 @@ function the_media_upload_tabs() {
  *
  * @since 2.5.0
  *
- * @param int     $id      image attachment id
- * @param string  $caption image caption
- * @param string  $title   image title attribute
- * @param string  $align   image css alignment property
- * @param string  $url     image src url
- * @param string  $rel     image rel attribute
+ * @param int     $id        Image attachment id.
+ * @param string  $caption   Image caption.
+ * @param string  $title     Image title attribute.
+ * @param string  $align     Image CSS alignment property.
+ * @param string  $url       Optional. Image src URL. Default empty.
+ * @param string  $rel       Optional. Image rel attribute. Default empty.
  * @param string|array $size Optional. Image size. Accepts any valid image size, or an array of width
- *                           and height values in pixels (in that order).
- * @return string the html to insert into editor
+ *                           and height values in pixels (in that order). Default 'medium'.
+ * @param string  $alt       Optional. Image alt attribute. Default empty.
+ * @return string The HTML output to insert into the editor.
  */
-function get_image_send_to_editor( $id, $caption, $title, $align, $url='', $rel = '', $size='medium', $alt = '' ) {
+function get_image_send_to_editor( $id, $caption, $title, $align, $url = '', $rel = '', $size = 'medium', $alt = '' ) {
 
 	$html = get_image_tag($id, $alt, '', $align, $size);
 
