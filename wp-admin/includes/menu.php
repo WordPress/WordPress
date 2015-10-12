f60bd8d827af738f2a83972bff6e35525ecb8d69
Drew Jaynes <info@drewapicture.com>
2015-10-12 03:39:26 +0000

    Docs: Wrap a couple of hook name references in inline `@see` tags in the hook docs for `custom_menu_order` and `menu_order`.
    Inline `@see` tags in the context of hooks are parsed as links to hook pages in the Code Reference.
    
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@35027
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@34992 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/menu.php
+++ wp-admin/includes/menu.php
@@ -258,7 +258,7 @@ uksort($menu, "strnatcasecmp"); // make it all pretty
 /**
  * Filter whether to enable custom ordering of the administration menu.
  *
- * See the 'menu_order' filter for reordering menu items.
+ * See the {@see 'menu_order'} filter for reordering menu items.
  *
  * @since 2.8.0
  *
@@ -275,7 +275,7 @@ if ( apply_filters( 'custom_menu_order', false ) ) {
 	/**
 	 * Filter the order of administration menu items.
 	 *
-	 * A truthy value must first be passed to the 'custom_menu_order' filter
+	 * A truthy value must first be passed to the {@see 'custom_menu_order'} filter
 	 * for this filter to work. Use the following to enable custom menu ordering:
 	 *
 	 *     add_filter( 'custom_menu_order', '__return_true' );
