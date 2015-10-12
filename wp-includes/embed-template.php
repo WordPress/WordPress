993d5c1c4ba292cfd6a5ece6ff2a53709196e118
John Blackbourn <johnbillion@git.wordpress.org>
2015-10-12 22:54:24 +0000

    Remove an unnecessary `function_exists()` call which was included in the oEmbed feature plugin for compatibility with WordPress < 4.3.
    See #32522
    
    Built from https://develop.svn.wordpress.org/trunk@35087
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35052 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/embed-template.php
+++ wp-includes/embed-template.php
@@ -123,11 +123,7 @@ if ( have_posts() ) :
 			<div class="wp-embed-footer">
 				<div class="wp-embed-site-title">
 					<?php
-					$site_icon_url = admin_url( 'images/w-logo-blue.png' );
-
-					if ( function_exists( 'get_site_icon_url' ) ) {
-						$site_icon_url = get_site_icon_url( 32, $site_icon_url );
-					}
+					$site_icon_url = get_site_icon_url( 32, admin_url( 'images/w-logo-blue.png' ) );
 
 					/**
 					 * Filters the site icon URL for use in the embed template.
