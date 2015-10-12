75426585fb43898234c9c58eb72967ae65070733
Scott Taylor <scott.c.taylor@mac.com>
2015-10-12 21:17:25 +0000

    oEmbed: add fallback icons (not used yet) for older browsers.
    Props melchoyce.
    See #34204.
    
    Built from https://develop.svn.wordpress.org/trunk@35083
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35048 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-35082';
+$wp_version = '4.4-alpha-35083';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
