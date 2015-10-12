4534f13149740e4d3d309990a2655eb110243771
Drew Jaynes <info@drewapicture.com>
2015-10-12 16:15:24 +0000

    Docs: Fix the syntax for marking parameters optional and add defaults in the DocBlock for `wp_get_attachment_link()`.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@35057
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35022 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post-template.php
+++ wp-includes/post-template.php
@@ -1405,10 +1405,11 @@ function the_attachment_link( $id = 0, $fullsize = false, $deprecated = false, $
  * @param string|array $size      Optional. Image size. Accepts any valid image size, or an array
  *                                of width and height values in pixels (in that order).
  *                                Default 'thumbnail'.
- * @param bool         $permalink Optional, default is false. Whether to add permalink to image.
- * @param bool         $icon      Optional, default is false. Whether to include icon.
- * @param string|bool  $text      Optional, default is false. If string, then will be link text.
- * @param array|string $attr      Optional. Array or string of attributes.
+ * @param bool         $permalink Optional, Whether to add permalink to image. Default false.
+ * @param bool         $icon      Optional. Whether the attachment is an icon. Default false.
+ * @param string|false $text      Optional. Link text to use. Activated by passing a string, false otherwise.
+ *                                Default false.
+ * @param array|string $attr      Optional. Array or string of attributes. Default empty.
  * @return string HTML content.
  */
 function wp_get_attachment_link( $id = 0, $size = 'thumbnail', $permalink = false, $icon = false, $text = false, $attr = '' ) {
