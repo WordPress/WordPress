8544d69d355d36b055e8ea2cbbcfc4deab10d41c
Drew Jaynes <info@drewapicture.com>
2015-10-12 16:44:24 +0000

    Docs: Normalize spacing in the `wp_get_attachment_link` hook doc following [35065].
    See #34257. See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@35066
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35031 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post-template.php
+++ wp-includes/post-template.php
@@ -1437,13 +1437,13 @@ function wp_get_attachment_link( $id = 0, $size = 'thumbnail', $permalink = fals
 	 *
 	 * @since 2.7.0
 	 *
-	 * @param string      $link_html The page link HTML output.
-	 * @param int         $id        Post ID.
-	 * @param string|array $size     Size of the image. Image size or array of width and height values (in that order).
-	 *                               Default 'thumbnail'.
-	 * @param bool        $permalink Whether to add permalink to image. Default false.
-	 * @param bool        $icon      Whether to include an icon. Default false.
-	 * @param string|bool $text      If string, will be link text. Default false.
+	 * @param string       $link_html The page link HTML output.
+	 * @param int          $id        Post ID.
+	 * @param string|array $size      Size of the image. Image size or array of width and height values (in that order).
+	 *                                Default 'thumbnail'.
+	 * @param bool         $permalink Whether to add permalink to image. Default false.
+	 * @param bool         $icon      Whether to include an icon. Default false.
+	 * @param string|bool  $text      If string, will be link text. Default false.
 	 */
 	return apply_filters( 'wp_get_attachment_link', "<a href='$url'>$link_text</a>", $id, $size, $permalink, $icon, $text );
 }
