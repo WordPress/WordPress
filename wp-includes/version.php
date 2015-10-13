e19cda5c2d0921bfa6c39f78ab5ad8ea2ad083f2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-10-13 07:40:25 +0000

    Toolbar: Improve padding of toolbar submenu items on small screens.
    Props tillkruess.
    Fixes #28608.
    Built from https://develop.svn.wordpress.org/trunk@35132
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35097 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-35131';
+$wp_version = '4.4-alpha-35132';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
