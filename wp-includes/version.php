55e16aa98a82375d9c43776b480f3ebc4a8d7a80
Scott Taylor <scott.c.taylor@mac.com>
2015-10-13 17:18:25 +0000

    KSES: have you ever heard of the `<bdo>` HTML tag? Same. http://www.w3schools.com/tags/tag_bdo.asp
    Adds unit test.
    
    Props iandunn.
    Fixes #34063.
    
    Built from https://develop.svn.wordpress.org/trunk@35141
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35106 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-35140';
+$wp_version = '4.4-alpha-35141';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
