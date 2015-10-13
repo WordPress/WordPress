31b40bdbb7f3de4a9fc7d32d010b4c617dd3c883
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-10-13 21:36:24 +0000

    Remove `fast_finish: true` from Travis CI configuration for now.
    Upstream: https://github.com/travis-ci/travis-ci/issues/4928
    Built from https://develop.svn.wordpress.org/trunk@35150
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35116 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-35149';
+$wp_version = '4.4-alpha-35150';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
