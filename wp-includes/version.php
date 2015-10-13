3cd956f9bb4a8e5e948e871872fc91edf75aa1ab
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-10-13 06:51:24 +0000

    Add the CSS changes missed in [35126].
    Props afercia.
    See #34263.
    Built from https://develop.svn.wordpress.org/trunk@35127
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35092 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-35126';
+$wp_version = '4.4-alpha-35127';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
