317d62bfaddf2d2ccff67a98e07fd6969e8a4de7
Scott Taylor <scott.c.taylor@mac.com>
2015-10-15 01:43:24 +0000

    Unit Tests: make a fixture in `Tests_Media` to represent the large image, instead of creating it 10 times.
    See #30017, #33968.
    
    Built from https://develop.svn.wordpress.org/trunk@35179
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35145 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-35178';
+$wp_version = '4.4-alpha-35179';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
