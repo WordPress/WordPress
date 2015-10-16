0df9abe092e968aab8dabfabc9248d5d12ffb844
Dominik Schilling <dominikschilling+git@gmail.com>
2015-10-16 14:16:28 +0000

    Travis CI: Use a pinned version of the object cache drop-in.
    Props tollmanz.
    See #31491.
    Built from https://develop.svn.wordpress.org/trunk@35218
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35184 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-35217';
+$wp_version = '4.4-alpha-35218';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
