1cb39b180f8c1f20447e25c832e2286584e97d9b
Drew Jaynes <info@drewapicture.com>
2015-10-16 16:56:26 +0000

    Docs: Standardize the file header summary for wp-admin/includes/file.php.
    Also adds an `@since` version and corrects the `@subpackage` value.
    
    See #33701.
    
    Built from https://develop.svn.wordpress.org/trunk@35221
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35187 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/file.php
+++ wp-admin/includes/file.php
@@ -1,11 +1,14 @@
 <?php
 /**
+ * Filesystem API: Top-level functionality
+ *
  * Functions for reading, writing, modifying, and deleting files on the file system.
  * Includes functionality for theme-specific files as well as operations for uploading,
  * archiving, and rendering output when necessary.
  *
  * @package WordPress
- * @subpackage Administration
+ * @subpackage Filesystem
+ * @since 2.3.0
  */
 
 /** The descriptions for theme files. */
