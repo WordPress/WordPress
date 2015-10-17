10d94ea673d418815f743cb1010525236671c14d
Drew Jaynes <info@drewapicture.com>
2015-10-17 06:07:24 +0000

    Docs: Add a missing file header for wp-admin/includes/class-wp-list-table.php.
    See #33701.
    
    Built from https://develop.svn.wordpress.org/trunk@35237
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35203 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-list-table.php
+++ wp-admin/includes/class-wp-list-table.php
@@ -1,12 +1,17 @@
 <?php
 /**
+ * Administration API: WP_List_Table class
+ *
+ * @package WordPress
+ * @subpackage List_Table
+ * @since 3.1.0
+ */
+
+/**
  * Base class for displaying a list of items in an ajaxified HTML table.
  *
  * @since 3.1.0
  * @access private
- *
- * @package WordPress
- * @subpackage List_Table
  */
 class WP_List_Table {
 
