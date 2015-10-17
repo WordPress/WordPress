a9134691d4c86985a824c845c84ac83ff7650896
Scott Taylor <scott.c.taylor@mac.com>
2015-10-17 20:49:24 +0000

    Unit Tests: better fixtures for `Tests_AdminBar`. Don't force-delete some posts: some filter callbacks are no-ops on Multisite if the post is vanquished.
    See #30017, #33968.
    
    
    Built from https://develop.svn.wordpress.org/trunk@35246
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35212 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-35245';
+$wp_version = '4.4-alpha-35246';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
