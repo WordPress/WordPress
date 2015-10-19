d5a15475d2d96c74b34ce0e5bfe65ad6321f6c87
Drew Jaynes <info@drewapicture.com>
2015-10-19 03:29:24 +0000

    Tests: Add some more test coverage for `get_term_field()`.
    See #33968.
    
    Built from https://develop.svn.wordpress.org/trunk@35270
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35236 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-alpha-35269';
+$wp_version = '4.4-alpha-35270';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
