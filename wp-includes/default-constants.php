02c9aeaef37a6cd213b829cc72eba668ec34f00b
Drew Jaynes <info@drewapicture.com>
2015-10-20 06:55:25 +0000

    Filesystem: Introduce constants to allow for easier expression of file sizes and other data amounts.
    Adds:
    * `KB_IN_BYTES` (kilobytes)
    * `MB_IN_BYTES` (megabytes)
    * `GB_IN_BYTES` (gigabytes)
    * `TB_IN_BYTES` (terabytes)
    
    Props nbachiyski, sudar.
    See #22405.
    
    Built from https://develop.svn.wordpress.org/trunk@35286
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35252 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-constants.php
+++ wp-includes/default-constants.php
@@ -107,6 +107,17 @@ function wp_initial_constants() {
 	define( 'MONTH_IN_SECONDS',  30 * DAY_IN_SECONDS    );
 	define( 'YEAR_IN_SECONDS',  365 * DAY_IN_SECONDS    );
 	/**#@-*/
+
+	/**#@+
+	 * Constants for expressing human-readable data sizes in their respective number of bytes.
+	 *
+	 * @since 4.4.0
+	 */
+	define( 'KB_IN_BYTES', 1024 );
+	define( 'MB_IN_BYTES', 1024 * KB_IN_BYTES );
+	define( 'GB_IN_BYTES', 1024 * MB_IN_BYTES );
+	define( 'TB_IN_BYTES', 1024 * GB_IN_BYTES );
+	/**#@-*/
 }
 
 /**
