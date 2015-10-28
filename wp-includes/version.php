5f6bdd9d602ed2732fdc6c29a3240b85aff02bd2
Scott Taylor <scott.c.taylor@mac.com>
2015-10-28 20:32:24 +0000

    4.4-beta2
    Built from https://develop.svn.wordpress.org/trunk@35424
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35388 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-beta1-35423';
+$wp_version = '4.4-beta2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
