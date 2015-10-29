85c273818c1bc2b913367faa0dd6a1071b875b70
Gary Pendergast <gary@pento.net>
2015-10-29 11:42:25 +0000

    Embeds: Tweak a string added in [35429], and add a translator comment.
    See #32522.
    
    
    Built from https://develop.svn.wordpress.org/trunk@35430
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35394 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/embed-template.php
+++ wp-includes/embed-template.php
@@ -205,12 +205,13 @@ if ( have_posts() ) :
 else :
 	?>
 	<div class="wp-embed">
-		<p class="wp-embed-heading"><?php _e( 'Oops! That embed can’t be found.' ); ?></p>
+		<p class="wp-embed-heading"><?php _e( 'Oops! That embed can&#8217;t be found.' ); ?></p>
 
 		<div class="wp-embed-excerpt">
 			<p>
 				<?php
 				printf(
+					/* translators: %s: a link to the embedded site */
 					__( 'It looks like nothing was found at this location. Maybe try visiting %s directly?' ),
 					'<strong><a href="' . esc_url( home_url() ) . '">' . esc_html( get_bloginfo( 'name' ) ) . '</a></strong>'
 				);
