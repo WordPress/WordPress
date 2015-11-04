fd10901db52f4beffd5d5d54d42de7c054f05129
Drew Jaynes <info@drewapicture.com>
2015-11-04 15:47:27 +0000

    Docs: Add a missing return description for `_get_custom_object_labels()`, a private core helper function.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@35503
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35467 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post-functions.php
+++ wp-includes/post-functions.php
@@ -1411,6 +1411,7 @@ function get_post_type_labels( $post_type_object ) {
  *
  * @param object $object                  A custom-something object.
  * @param array  $nohier_vs_hier_defaults Hierarchical vs non-hierarchical default labels.
+ * @return object Object containing labels for the given custom-something object.
  */
 function _get_custom_object_labels( $object, $nohier_vs_hier_defaults ) {
 	$object->labels = (array) $object->labels;
