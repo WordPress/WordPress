9e5726c2330d567794cb94530705f2ae5f5cb1df
Aaron Jorbin <aaron@jorb.in>
2015-11-04 21:21:36 +0000

    Bump matchdep from 0.3.0 to 1.0.0
    This drops support for node 0.8.0 so if you still using that, it's time to upgrade your node.
    
    See #34177
    
    
    Built from https://develop.svn.wordpress.org/trunk@35513
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35477 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-beta2-35512';
+$wp_version = '4.4-beta2-35513';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
