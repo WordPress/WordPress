d60ca2f7583510b8a955b9a3f684613bf8ccd844
Aaron Jorbin <aaron@jorb.in>
2015-11-05 17:47:30 +0000

    Bump grunt-contrib-uglify to 0.10.0
    This includes an update to the underlying version of uglify which causes all of the JS to be modified.
    
    See #34177
    
    
    Built from https://develop.svn.wordpress.org/trunk@35538
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35502 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-beta3-35537';
+$wp_version = '4.4-beta3-35538';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
