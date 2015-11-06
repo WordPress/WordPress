8c2ed5330a8d2b4cefc565d83eb76f1d0d5eb251
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-11-06 00:28:25 +0000

    Remove `<code>` tag from translatable string in `wp-admin/options-media.php`.
    Add translator comment.
    
    Props ramiy.
    Fixes #34581.
    Built from https://develop.svn.wordpress.org/trunk@35550
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35514 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-beta3-35549';
+$wp_version = '4.4-beta3-35550';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
