b17272831ca922dbb3cd119a5e98193f52ec5b77
Scott Taylor <scott.c.taylor@mac.com>
2015-11-07 16:05:25 +0000

    List Tables: fix row links CSS for RTL.
    Props afercia.
    Fixes #34150.
    
    Built from https://develop.svn.wordpress.org/trunk@35566
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35530 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-beta3-35565';
+$wp_version = '4.4-beta3-35566';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
