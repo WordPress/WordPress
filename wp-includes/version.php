1f24f526734ae83f7c3332618d4f093158eee4c6
Gary Pendergast <gary@pento.net>
2015-11-10 03:35:25 +0000

    Emoji: Add some new flags, and a handful from Unicode 8.0.
    Including the following that should be used as often as possible:
    
    U+1F1E6 U+1F1FA Flag for Australia
    U+1F3CF Cricket Bat and Ball
    U+1F32F Burrito
    
    See #33592.
    
    
    Built from https://develop.svn.wordpress.org/trunk@35596
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35560 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-beta3-35595';
+$wp_version = '4.4-beta3-35596';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
