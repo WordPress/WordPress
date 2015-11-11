e6611c6b01b74f38d90f57d94265e50e2642f251
Scott Taylor <scott.c.taylor@mac.com>
2015-11-11 22:05:26 +0000

    Mail: after [33124], add unit tests.
    Props mdawaffe.
    Fixes #28039.
    
    Built from https://develop.svn.wordpress.org/trunk@35617
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35581 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-beta3-35616';
+$wp_version = '4.4-beta3-35617';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
