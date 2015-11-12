6209004bf792c066a2aaa3e240d2e53d26ae685a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-11-12 18:21:24 +0000

    Remove `<code>` tags from translatable string in `wp-includes/load.php`.
    Add translator comment.
    
    Props ramiy.
    Fixes #34621.
    Built from https://develop.svn.wordpress.org/trunk@35632
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35596 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -397,7 +397,13 @@ function wp_set_wpdb_vars() {
 
 	if ( is_wp_error( $prefix ) ) {
 		wp_load_translations_early();
-		wp_die( __( '<strong>ERROR</strong>: <code>$table_prefix</code> in <code>wp-config.php</code> can only contain numbers, letters, and underscores.' ) );
+		wp_die(
+			/* translators: 1: $table_prefix 2: wp-config.php */
+			sprintf( __( '<strong>ERROR</strong>: %1$s in %2$s can only contain numbers, letters, and underscores.' ),
+				'<code>$table_prefix</code>',
+				'<code>wp-config.php</code>'
+			)
+		);
 	}
 }
 
