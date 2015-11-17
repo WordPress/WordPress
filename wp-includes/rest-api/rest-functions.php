b0ee5efef2a85182f12d2aa5ad4a5b154d3bd860
Ryan McCue <me@ryanmccue.info>
2015-11-17 02:24:27 +0000

    REST API: Change link relations to api.w.org
    Fixes #34303.
    
    Built from https://develop.svn.wordpress.org/trunk@35650
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35614 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/rest-functions.php
+++ wp-includes/rest-api/rest-functions.php
@@ -531,7 +531,7 @@ function rest_output_link_wp_head() {
 		return;
 	}
 
-	echo "<link rel='https://github.com/WP-API/WP-API' href='" . esc_url( $api_root ) . "' />\n";
+	echo "<link rel='https://api.w.org/' href='" . esc_url( $api_root ) . "' />\n";
 }
 
 /**
@@ -550,7 +550,7 @@ function rest_output_link_header() {
 		return;
 	}
 
-	header( 'Link: <' . esc_url_raw( $api_root ) . '>; rel="https://github.com/WP-API/WP-API"', false );
+	header( 'Link: <' . esc_url_raw( $api_root ) . '>; rel="https://api.w.org/"', false );
 }
 
 /**
