16502d03f2b94e22d12fb66322b3a24406269474
John Blackbourn <johnbillion@git.wordpress.org>
2015-11-18 20:39:26 +0000

    Remove `register_api_field()` from core. This is the only core function that utilises the `$wp_rest_additional_fields` global, and doesn't belong as part of the infrastructure.
    See https://github.com/WP-API/WP-API/pull/1748
    
    Fixes #34730
    
    Built from https://develop.svn.wordpress.org/trunk@35687
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35651 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/rest-functions.php
+++ wp-includes/rest-api/rest-functions.php
@@ -64,48 +64,6 @@ function register_rest_route( $namespace, $route, $args = array(), $override = f
 }
 
 /**
- * Registers a new field on an existing WordPress object type.
- *
- * @since 4.4.0
- *
- * @global array $wp_rest_additional_fields Holds registered fields, organized
- *                                          by object type.
- *
- * @param string|array $object_type Object(s) the field is being registered
- *                                  to, "post"|"term"|"comment" etc.
- * @param string $attribute         The attribute name.
- * @param array  $args {
- *     Optional. An array of arguments used to handle the registered field.
- *
- *     @type string|array|null $get_callback    Optional. The callback function used to retrieve the field
- *                                              value. Default is 'null', the field will not be returned in
- *                                              the response.
- *     @type string|array|null $update_callback Optional. The callback function used to set and update the
- *                                              field value. Default is 'null', the value cannot be set or
- *                                              updated.
- *     @type string|array|null $schema          Optional. The callback function used to create the schema for
- *                                              this field. Default is 'null', no schema entry will be returned.
- * }
- */
-function register_api_field( $object_type, $attribute, $args = array() ) {
-	$defaults = array(
-		'get_callback'    => null,
-		'update_callback' => null,
-		'schema'          => null,
-	);
-
-	$args = wp_parse_args( $args, $defaults );
-
-	global $wp_rest_additional_fields;
-
-	$object_types = (array) $object_type;
-
-	foreach ( $object_types as $object_type ) {
-		$wp_rest_additional_fields[ $object_type ][ $attribute ] = $args;
-	}
-}
-
-/**
  * Registers rewrite rules for the API.
  *
  * @since 4.4.0
