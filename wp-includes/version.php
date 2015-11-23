64fdd0c6a09de7421b5ac9666a3fa451acff88e4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-11-23 18:40:25 +0000

    Users: Move the tests added in [35116] and [35618] to a more appropriate place and give them a better name.
    See #28435, #29880.
    Built from https://develop.svn.wordpress.org/trunk@35732
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35696 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-beta4-35731';
+$wp_version = '4.4-beta4-35732';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
