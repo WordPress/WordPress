2eb60b82784c1d3248c50c8657b3e1f1a941b9e7
Helen Hou-Sandí <helen.y.hou@gmail.com>
2015-11-25 21:52:26 +0000

    Set Twenty Sixteen as the default theme.
    With thanks to all those who contributed.
    
    props iamtakashi, karmatosed, iandstewart, dd32, mor10, grapplerulrich, davidakennedy, frank-klein, tywayne, wenthemes, monika, metodiew, nhuja, headonfire, Chrisdc1, philiparthurmoore, karpstrucking, cais, mt8.biz, fjarrett, sdavis2702, SergeyBiryukov, eduardozulian, webdevmattcrom, ehtis, peterwilsoncc, tfrommen, fsylum, wonderboymusic, ocean90, obenland, cainm, mrahmadawais, drewapicture, trenzterra, tevko, kraftbj, walbo, nacin.
    fixes #34306.
    
    Built from https://develop.svn.wordpress.org/trunk@35739
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35703 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-constants.php
+++ wp-includes/default-constants.php
@@ -354,6 +354,6 @@ function wp_templating_constants() {
 	 * @see WP_Theme::get_core_default_theme()
 	 */
 	if ( !defined('WP_DEFAULT_THEME') )
-		define( 'WP_DEFAULT_THEME', 'twentyfifteen' );
+		define( 'WP_DEFAULT_THEME', 'twentysixteen' );
 
 }
