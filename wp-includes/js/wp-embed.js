9834e9993af35e9212ff59ab27898616e1b039d0
Andrew Nacin <wp@andrewnacin.com>
2015-12-04 05:46:25 +0000

    Embeds: Enforce, via unit tests, the no-ampersand rule for wp-embed.js.
    fixes #34698.
    
    Built from https://develop.svn.wordpress.org/trunk@35762
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35726 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/wp-embed.js
+++ wp-includes/js/wp-embed.js
@@ -1,3 +1,12 @@
+/**
+ * WordPress inline HTML embed
+ *
+ * @since 4.4.0
+ *
+ * This file cannot have ampersands in it. This is to ensure
+ * it can be embedded in older versions of WordPress.
+ * See https://core.trac.wordpress.org/changeset/35708.
+ */
 (function ( window, document ) {
 	'use strict';
 
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-RC1-35761';
+$wp_version = '4.4-RC1-35762';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
