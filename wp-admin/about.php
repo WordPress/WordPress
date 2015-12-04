ef1280f77083b9bcec518fa5e0e48b889246918a
Drew Jaynes <info@drewapicture.com>
2015-12-04 18:09:25 +0000

    About page: Make strings translatable.
    See #34663.
    
    Built from https://develop.svn.wordpress.org/trunk@35769
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35733 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -43,15 +43,15 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 		</h2>
 
 		<div class="headline-feature feature-section one-col">
-			<h2><?php echo ( 'Twenty Sixteen' ); ?></h2>
+			<h2><?php _e( 'Twenty Sixteen' ); ?></h2>
 			<div class="media-container">
 				<img src="https://cldup.com/K6k4JfS2nW.png" alt="" />
 			</div>
 			<div class="two-col">
 				<div class="col">
-					<h3><?php echo ( 'Introducing Twenty Sixteen' ); ?></h3>
-					<p><?php echo ( 'Our newest default theme, Twenty Sixteen, is a modern take on a classic blog design.' ); ?></p>
-					<p><?php echo ( 'Twenty Sixteen was built to look great on any device. A fluid grid design, fun color schemes, a flexible header, and more, will make your content shine.' ); ?></p>
+					<h3><?php _e( 'Introducing Twenty Sixteen' ); ?></h3>
+					<p><?php _e( 'Our newest default theme, Twenty Sixteen, is a modern take on a classic blog design.' ); ?></p>
+					<p><?php _e( 'Twenty Sixteen was built to look great on any device. A fluid grid design, flexible header, fun color schemes, and more, will make your content shine.' ); ?></p>
 					<div class="horizontal-image">
 						<div class="content">
 							<img class="feature-image horizontal-screen" src="https://cldup.com/J-zxmMqkXs.png" alt="" />
@@ -73,8 +73,8 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 				</div>
 			</div>
 			<div class="col">
-				<h3><?php echo ( 'Responsive images' ); ?></h3>
-				<p><?php echo ( 'WordPress now takes a smarter approach to displaying appropriate image sizes on any device, ensuring a perfect fit every time. You don&#8217;t need to do anything to your theme, it just works.' ); ?></p>
+				<h3><?php _e( 'Responsive images' ); ?></h3>
+				<p><?php _e( 'WordPress now takes a smarter approach to displaying appropriate image sizes on any device, ensuring a perfect fit every time. You don&#8217;t need to do anything to your theme, it just works.' ); ?></p>
 			</div>
 		</div>
 
@@ -85,15 +85,15 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 				<div class="embed-container">
 					<?php echo wp_oembed_get( 'https://make.wordpress.org/core/2015/10/28/new-embeds-feature-in-wordpress-4-4/' ); ?>
 				</div>
-				<h3><?php echo ( 'Embed your WordPress content' ); ?></h3>
-				<p><?php echo ( 'Now you can embed your posts on other sites, even other WordPress sites. Simply drop a post URL into the editor and see an instant embed preview, complete with the title, excerpt, and featured image if you&#8217;ve set one. We&#8217;ll even include your site icon and links for comments and sharing.' ); ?></p>
+				<h3><?php _e( 'Embed your WordPress content' ); ?></h3>
+				<p><?php _e( 'Now you can embed your posts on other sites, even other WordPress sites. Simply drop a post URL into the editor and see an instant embed preview, complete with the title, excerpt, and featured image if you&#8217;ve set one. We&#8217;ll even include your site icon and links for comments and sharing.' ); ?></p>
 			</div>
 			<div class="col">
 				<div class="embed-container">
 					<?php echo wp_oembed_get( '' ); ?>
 				</div>
-				<h3><?php echo ( 'Even more embed providers' ); ?></h3>
-				<p><?php echo ( 'In addition to post embeds, WordPress 4.4 also adds support for five new oEmbed providers: Cloudup, Reddit&nbsp;Comments, ReverbNation, Speaker&nbsp;Deck, and VideoPress.' ); ?></p>
+				<h3><?php _e( 'Even more embed providers' ); ?></h3>
+				<p><?php _e( 'In addition to post embeds, WordPress 4.4 also adds support for five new oEmbed providers: Cloudup, Reddit&nbsp;Comments, ReverbNation, Speaker&nbsp;Deck, and VideoPress.' ); ?></p>
 			</div>
 		</div>
 
@@ -104,9 +104,9 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 			<div class="feature-section under-the-hood one-col">
 				<div class="col">
-					<h4><?php echo ( 'REST API infrastructure' ); ?></h4>
+					<h4><?php _e( 'REST API infrastructure' ); ?></h4>
 					<div class="two-col-text">
-						<p><?php echo ( 'Infrastructure for the REST API has been integrated into core, marking a new era in developing with WordPress. The REST API serves to provide developers with a path forward for building and extending RESTful APIs on top of WordPress.' ); ?></p>
+						<p><?php _e( 'Infrastructure for the REST API has been integrated into core, marking a new era in developing with WordPress. The REST API serves to provide developers with a path forward for building and extending RESTful APIs on top of WordPress.' ); ?></p>
 						<p><?php
 							if ( current_user_can( 'install_plugins' ) ) {
 								$url_args = array(
@@ -123,7 +123,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 							}
 
 							/* translators: WordPress REST API plugin link */
-							printf( ( 'Infrastructure is the first part of a multi-stage rollout for the REST API. Inclusion of core endpoints is targeted for an upcoming release. To get a sneak peek of the core endpoints, and for more information on extending the REST API, check out the official %s plugin.' ), $plugin_link );
+							printf( __( 'Infrastructure is the first part of a multi-stage rollout for the REST API. Inclusion of core endpoints is targeted for an upcoming release. To get a sneak peek of the core endpoints, and for more information on extending the REST API, check out the official %s plugin.' ), $plugin_link );
 						?></p>
 					</div>
 				</div>
@@ -131,10 +131,10 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 			<div class="feature-section under-the-hood three-col">
 				<div class="col">
-					<h4><?php echo ( 'Term meta' ); ?></h4>
+					<h4><?php _e( 'Term meta' ); ?></h4>
 					<p><?php
 						/* translators: 1: add_term_meta() docs link, 2: get_term_meta() docs link, 3: update_term_meta() docs link */
-						printf( ( 'Terms now support metadata, just like posts. See %1$s, %2$s, and %3$s for more information.' ),
+						printf( __( 'Terms now support metadata, just like posts. See %1$s, %2$s, and %3$s for more information.' ),
 							'<a href="https://developer.wordpress.org/reference/functions/add_term_meta"><code>add_term_meta()</code></a>',
 							'<a href="https://developer.wordpress.org/reference/functions/get_term_meta"><code>get_term_meta()</code></a>',
 							'<a href="https://developer.wordpress.org/reference/functions/update_term_meta"><code>update_term_meta()</code></a>'
@@ -142,17 +142,17 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 					?></p>
 				</div>
 				<div class="col">
-					<h4><?php echo ( 'Comment query improvements' ); ?></h4>
+					<h4><?php _e( 'Comment query improvements' ); ?></h4>
 					<p><?php
 						/* translators: WP_Comment_Query class name */
-						printf( ( 'Comment queries now have cache handling to improve performance. New arguments in %s make crafting robust comment queries simpler.' ), '<code>WP_Comment_Query</code>' );
+						printf( __( 'Comment queries now have cache handling to improve performance. New arguments in %s make crafting robust comment queries simpler.' ), '<code>WP_Comment_Query</code>' );
 					?></p>
 				</div>
 				<div class="col">
-					<h4><?php echo ( 'Term, comment, and network objects' ); ?></h4>
+					<h4><?php _e( 'Term, comment, and network objects' ); ?></h4>
 					<p><?php
 						/* translators: 1: WP_Term class name, WP_Comment class name, WP_Network class name */
-						printf( ( 'New %1$s, %2$s, and %3$s objects make interacting with terms, comments, and networks more predictable and intuitive in code.' ),
+						printf( __( 'New %1$s, %2$s, and %3$s objects make interacting with terms, comments, and networks more predictable and intuitive in code.' ),
 							'<code>WP_Term</code>',
 							'<code>WP_Comment</code>',
 							'<code>WP_Network</code>'
