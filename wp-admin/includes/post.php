3c0c5e6ad55ee76c12f34e2fc1e86020e8355179
Dominik Schilling <dominikschilling+git@gmail.com>
2015-12-07 19:16:26 +0000

    List Tables: Revert [34728] and [35482].
    Part of [34728] was already reverted in [35682], but the default values still made it impossible to set a default ordering for custom post types.
    
    See #25493.
    See #34825.
    Built from https://develop.svn.wordpress.org/trunk@35818
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35782 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/post.php
+++ wp-admin/includes/post.php
@@ -1008,16 +1008,12 @@ function wp_edit_posts_query( $q = false ) {
 		$orderby = $q['orderby'];
 	} elseif ( isset( $q['post_status'] ) && in_array( $q['post_status'], array( 'pending', 'draft' ) ) ) {
 		$orderby = 'modified';
-	} elseif ( ! is_post_type_hierarchical( $post_type ) ) {
-		$orderby = 'date';
 	}
 
 	if ( isset( $q['order'] ) ) {
 		$order = $q['order'];
 	} elseif ( isset( $q['post_status'] ) && 'pending' == $q['post_status'] ) {
 		$order = 'ASC';
-	} else {
-		$order = 'desc';
 	}
 
 	$per_page = "edit_{$post_type}_per_page";
