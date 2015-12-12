746c697099c3411cd46acd9950e1b40e3948e097
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-12-12 01:35:35 +0000

    I18N: In `wp_maybe_decline_date()`, bail early if translation functions are not available, e.g. in `SHORTINIT` mode.
    Fixes #34967 for trunk.
    Built from https://develop.svn.wordpress.org/trunk@35878
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35842 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-alpha-35877';
+$wp_version = '4.5-alpha-35878';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
