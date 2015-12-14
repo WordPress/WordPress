4836d6c7006f473c2949d2620da86229a1b03c45
John Blackbourn <johnbillion@git.wordpress.org>
2015-12-14 03:01:25 +0000

    Correct the position of the `$alt` parameter's `@param` doc in `image_add_caption()`.
    See #32246
    
    Built from https://develop.svn.wordpress.org/trunk@35918
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35882 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/media.php
+++ wp-admin/includes/media.php
@@ -158,11 +158,11 @@ function get_image_send_to_editor( $id, $caption, $title, $align, $url = '', $re
  * @param string $html
  * @param integer $id
  * @param string $caption image caption
- * @param string $alt image alt attribute
  * @param string $title image title attribute
  * @param string $align image css alignment property
  * @param string $url image src url
  * @param string $size image size (thumbnail, medium, large, full or added with add_image_size() )
+ * @param string $alt image alt attribute
  * @return string
  */
 function image_add_caption( $html, $id, $caption, $title, $align, $url, $size, $alt = '' ) {
