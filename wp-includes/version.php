bc6b11d0de0d956965c9fff5995738b45411438c
Dominik Schilling <dominikschilling+git@gmail.com>
2015-12-15 19:50:29 +0000

    Revert [35949].
    Passing an object to `wp_list_pluck()` throws also a fatal error, see https://3v4l.org/9YsaD.
    
    See #35087.
    Built from https://develop.svn.wordpress.org/trunk@35950
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35914 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-alpha-35949';
+$wp_version = '4.5-alpha-35950';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
