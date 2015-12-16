bc4a65a03693549c52e6dced370bafc59d6fa981
Drew Jaynes <info@drewapicture.com>
2015-12-16 23:11:25 +0000

    Docs: Clarify the summary and add missing parameter notations to the DocBlock for `_wp_delete_tax_menu_item()`.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@35979
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35944 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/nav-menu.php
+++ wp-includes/nav-menu.php
@@ -928,13 +928,14 @@ function _wp_delete_post_menu_item( $object_id = 0 ) {
 }
 
 /**
- * Callback for handling a menu item when its original object is deleted.
+ * Serves as a callback for handling a menu item when its original object is deleted.
  *
  * @since 3.0.0
  * @access private
  *
- * @param int $object_id The ID of the original object being trashed.
- *
+ * @param int    $object_id Optional. The ID of the original object being trashed. Default 0.
+ * @param int    $tt_id     Term taxonomy ID. Unused.
+ * @param string $taxonomy  Taxonomy slug.
  */
 function _wp_delete_tax_menu_item( $object_id = 0, $tt_id, $taxonomy ) {
 	$object_id = (int) $object_id;
