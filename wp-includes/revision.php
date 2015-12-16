81abb11839c2d27b32cbfa435d595bd9d26b40e7
Drew Jaynes <info@drewapicture.com>
2015-12-16 23:18:26 +0000

    Docs: Add a missing notation for the `$args` parameter in the DocBlock for `wp_get_post_revisions()`.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@35981
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35946 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/revision.php
+++ wp-includes/revision.php
@@ -411,7 +411,10 @@ function wp_delete_post_revision( $revision_id ) {
  *
  * @since 2.6.0
  *
- * @param int|WP_Post $post_id Optional. Post ID or WP_Post object. Default is global $post.
+ * @see get_children()
+ *
+ * @param int|WP_Post $post_id Optional. Post ID or WP_Post object. Default is global `$post`.
+ * @param array|null  $args    Optional. Arguments for retrieving post revisions. Default null.
  * @return array An array of revisions, or an empty array if none.
  */
 function wp_get_post_revisions( $post_id = 0, $args = null ) {
