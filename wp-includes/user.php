28412d2ef2581795f2307c030ecc6a592bdb422d
Drew Jaynes <info@drewapicture.com>
2015-12-17 16:41:27 +0000

    Docs: Add missing parameter and return descriptions to the DocBlock for `_wp_get_user_contactmethods()`.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@35986
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35951 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -1897,8 +1897,13 @@ function wp_get_user_contact_methods( $user = null ) {
 /**
  * The old private function for setting up user contact methods.
  *
+ * Use wp_get_user_contact_methods() instead.
+ *
  * @since 2.9.0
  * @access private
+ *
+ * @param WP_User $user Optional. WP_User object. Default null.
+ * @return array Array of contact methods and their labels.
  */
 function _wp_get_user_contactmethods( $user = null ) {
 	return wp_get_user_contact_methods( $user );
