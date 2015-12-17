9de8a1466ad22b96cc2d862bbb3fe1a94e7a94cd
Drew Jaynes <info@drewapicture.com>
2015-12-17 17:16:26 +0000

    Docs: Add a missing summary, `@since` version, and parameter descriptions to the DocBlock for `_register_widget_update_callback()`.
    Introduced in [10798].
    
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@35989
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35954 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/widgets.php
+++ wp-includes/widgets.php
@@ -513,13 +513,18 @@ function wp_register_widget_control( $id, $name, $control_callback, $options = a
 }
 
 /**
+ * Registers the update callback for a widget.
+ *
+ * @since 2.8.0
+ *
  * @global array $wp_registered_widget_updates
  *
- * @param string   $id_base
- * @param callable $update_callback
- * @param array    $options
+ * @param string   $id_base         The base ID of a widget created by extending WP_Widget.
+ * @param callable $update_callback Update callback method for the widget.
+ * @param array    $options         Optional. Widget control options. See {@see wp_register_widget_control()}.
+ *                                  Default empty array.
  */
-function _register_widget_update_callback($id_base, $update_callback, $options = array()) {
+function _register_widget_update_callback( $id_base, $update_callback, $options = array() ) {
 	global $wp_registered_widget_updates;
 
 	if ( isset($wp_registered_widget_updates[$id_base]) ) {
