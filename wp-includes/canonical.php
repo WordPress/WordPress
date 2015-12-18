f4af7643d841e9f463b9798a2baf1d524d3a319f
Drew Jaynes <info@drewapicture.com>
2015-12-18 23:08:26 +0000

    Docs: Add a missing summary, description, and `@since` version to the DocBlock for `wp_redirect_admin_locations()`.
    Introduced in [19880].
    
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@36010
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35975 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/canonical.php
+++ wp-includes/canonical.php
@@ -602,6 +602,12 @@ function redirect_guess_404_permalink() {
 }
 
 /**
+ * Redirects a variety of shorthand URLs to the admin.
+ *
+ * If a user visits example.com/admin, they'll be redirected to /wp-admin.
+ * Visiting /login redirects to /wp-login.php, and so on.
+ *
+ * @since 3.4.0
  *
  * @global WP_Rewrite $wp_rewrite
  */
