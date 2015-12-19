eb158d65c7b3f982c5b58d00da1af4e5f3bd4206
Drew Jaynes <info@drewapicture.com>
2015-12-19 08:58:25 +0000

    Docs: Add missing parameter and return notations to the DocBlock for the deprecated `wp_specialchars()` function.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@36026
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35991 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/deprecated.php
+++ wp-includes/deprecated.php
@@ -2048,10 +2048,16 @@ function js_escape( $text ) {
 }
 
 /**
- * Escaping for HTML blocks.
+ * Legacy escaping for HTML blocks.
  *
  * @deprecated 2.8.0 Use esc_html()
  * @see esc_html()
+ *
+ * @param string       $string        String to escape.
+ * @param string       $quote_style   Unused.
+ * @param false|string $charset       Unused.
+ * @param false        $double_encode Whether to double encode. Unused.
+ * @return string Escaped `$string`.
  */
 function wp_specialchars( $string, $quote_style = ENT_NOQUOTES, $charset = false, $double_encode = false ) {
 	_deprecated_function( __FUNCTION__, '2.8', 'esc_html()' );
