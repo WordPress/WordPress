013d1178e5b8563e4bd09dfda72381986817179e
Drew Jaynes <info@drewapicture.com>
2015-12-19 09:04:26 +0000

    Docs: Add missing parameter and return notations in the DocBlock for `get_profile()`.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@36028
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35993 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/deprecated.php
+++ wp-includes/deprecated.php
@@ -2397,6 +2397,10 @@ function automatic_feed_links( $add = true ) {
  * @since 1.5.0
  * @deprecated 3.0.0 Use get_the_author_meta()
  * @see get_the_author_meta()
+ *
+ * @param string    $field User meta field.
+ * @param false|int $user Optional. User ID to retrieve the field for. Default false (current user).
+ * @return string The author's field from the current author's DB object.
  */
 function get_profile( $field, $user = false ) {
 	_deprecated_function( __FUNCTION__, '3.0', 'get_the_author_meta()' );
