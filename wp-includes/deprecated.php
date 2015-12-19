5399b2137fe6d4061a89012ad8b5859d73311346
Drew Jaynes <info@drewapicture.com>
2015-12-19 19:41:30 +0000

    Docs: Add missing parameter and return notations to the DocBlock for the deprecated `get_usernumposts()` function.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@36030
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35995 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/deprecated.php
+++ wp-includes/deprecated.php
@@ -2412,11 +2412,14 @@ function get_profile( $field, $user = false ) {
 }
 
 /**
- * Number of posts user has written.
+ * Retrieves the number of posts a user has written.
  *
  * @since 0.71
  * @deprecated 3.0.0 Use count_user_posts()
  * @see count_user_posts()
+ *
+ * @param int $userid User to count posts for.
+ * @return int Number of posts the given user has written.
  */
 function get_usernumposts( $userid ) {
 	_deprecated_function( __FUNCTION__, '3.0', 'count_user_posts()' );
