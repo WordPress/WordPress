932c91b76695d29169c402b479d5ceb4f440396e
Drew Jaynes <info@drewapicture.com>
2015-12-19 08:48:27 +0000

    Docs: Add missing parameter and return notations in the DocBlock for the deprecated `_nc()` function.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@36024
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35989 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/deprecated.php
+++ wp-includes/deprecated.php
@@ -1757,12 +1757,20 @@ function translate_with_context( $text, $domain = 'default' ) {
 }
 
 /**
- * A version of _n(), which supports contexts.
+ * Legacy version of _n(), which supports contexts.
+ *
  * Strips everything from the translation after the last bar.
  *
  * @since 2.7.0
  * @deprecated 3.0.0 Use _nx()
  * @see _nx()
+ *
+ * @param string $single The text to be used if the number is singular.
+ * @param string $plural The text to be used if the number is plural.
+ * @param int    $number The number to compare against to use either the singular or plural form.
+ * @param string $domain Optional. Text domain. Unique identifier for retrieving translated strings.
+ *                       Default 'default'.
+ * @return string The translated singular or plural form.
  */
 function _nc( $single, $plural, $number, $domain = 'default' ) {
 	_deprecated_function( __FUNCTION__, '2.9', '_nx()' );
