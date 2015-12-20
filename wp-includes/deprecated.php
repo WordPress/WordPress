a28bfe8ba69c00829d520075bb5f8dc672345bb8
Drew Jaynes <info@drewapicture.com>
2015-12-20 21:21:27 +0000

    Docs: Add missing notations for the optional `$tab_index` and `$extended` parameters in the DocBlock for the deprecated `the_editor()` function.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@36033
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@35998 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/deprecated.php
+++ wp-includes/deprecated.php
@@ -2555,16 +2555,18 @@ function wp_timezone_supported() {
 }
 
 /**
- * Display editor: TinyMCE, HTML, or both.
+ * Displays an editor: TinyMCE, HTML, or both.
  *
  * @since 2.1.0
  * @deprecated 3.3.0 Use wp_editor()
  * @see wp_editor()
  *
- * @param string $content Textarea content.
- * @param string $id Optional, default is 'content'. HTML ID attribute value.
- * @param string $prev_id Optional, not used
- * @param bool $media_buttons Optional, default is true. Whether to display media buttons.
+ * @param string $content       Textarea content.
+ * @param string $id            Optional. HTML ID attribute value. Default 'content'.
+ * @param string $prev_id       Optional. Unused.
+ * @param bool   $media_buttons Optional. Whether to display media buttons. Default true.
+ * @param int    $tab_index     Optional. Unused.
+ * @param bool   $extended      Optional. Unused.
  * @param int $tab_index Optional, not used
  */
 function the_editor($content, $id = 'content', $prev_id = 'title', $media_buttons = true, $tab_index = 2, $extended = true) {
