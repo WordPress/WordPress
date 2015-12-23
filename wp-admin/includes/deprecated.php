ad64c0f2d7829895c8ac9c2149cc1f90622260fc
Drew Jaynes <info@drewapicture.com>
2015-12-23 06:53:26 +0000

    Docs: Properly mark the `$exclude_zeros` parameter in the DocBlock for `get_editable_user_ids()` as optional.
    Also fixes a couple of typos.
    
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@36070
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36035 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/deprecated.php
+++ wp-admin/includes/deprecated.php
@@ -266,9 +266,9 @@ function get_editable_authors( $user_id ) {
  *
  * @deprecated 3.1.0 Use get_users()
  *
- * @param int $user_id User ID.
- * @param bool $exclude_zeros Optional, default is true. Whether to exclude zeros.
- * @return mixed
+ * @param int  $user_id       User ID.
+ * @param bool $exclude_zeros Optional. Whether to exclude zeroes. Default true.
+ * @return array Array of editable user IDs, empty array otherwise.
  */
 function get_editable_user_ids( $user_id, $exclude_zeros = true, $post_type = 'post' ) {
 	_deprecated_function( __FUNCTION__, '3.1', 'get_users()' );
