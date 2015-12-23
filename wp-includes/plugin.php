7e43e204368885106a24e9376599bc71145f008f
John Blackbourn <johnbillion@git.wordpress.org>
2015-12-23 07:13:26 +0000

    Docs: `@see != @since`.
    See #32246
    
    Built from https://develop.svn.wordpress.org/trunk@36073
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36038 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/plugin.php
+++ wp-includes/plugin.php
@@ -245,7 +245,7 @@ function apply_filters( $tag, $value ) {
 /**
  * Execute functions hooked on a specific filter hook, specifying arguments in an array.
  *
- * @see 3.0.0
+ * @since 3.0.0
  *
  * @see apply_filters() This function is identical, but the arguments passed to the
  * functions hooked to `$tag` are supplied using an array.
