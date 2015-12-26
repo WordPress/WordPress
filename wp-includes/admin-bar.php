63cc66469be5f97d31a90ad40cc599445cd240cc
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-12-26 01:40:27 +0000

    Toolbar: In Comments link, replace title attribute containing the number of pending comments with a screen reader text.
    Props slushman, rachelbaker.
    Fixes #34895.
    Built from https://develop.svn.wordpress.org/trunk@36093
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36058 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/admin-bar.php
+++ wp-includes/admin-bar.php
@@ -692,16 +692,16 @@ function wp_admin_bar_comments_menu( $wp_admin_bar ) {
 
 	$awaiting_mod = wp_count_comments();
 	$awaiting_mod = $awaiting_mod->moderated;
-	$awaiting_title = esc_attr( sprintf( _n( '%s comment awaiting moderation', '%s comments awaiting moderation', $awaiting_mod ), number_format_i18n( $awaiting_mod ) ) );
+	$awaiting_text = sprintf( _n( '%s comment awaiting moderation', '%s comments awaiting moderation', $awaiting_mod ), number_format_i18n( $awaiting_mod ) );
 
 	$icon  = '<span class="ab-icon"></span>';
-	$title = '<span id="ab-awaiting-mod" class="ab-label awaiting-mod pending-count count-' . $awaiting_mod . '">' . number_format_i18n( $awaiting_mod ) . '</span>';
+	$title = '<span id="ab-awaiting-mod" class="ab-label awaiting-mod pending-count count-' . $awaiting_mod . '" aria-hidden="true">' . number_format_i18n( $awaiting_mod ) . '</span>';
+	$title .= '<span class="screen-reader-text">' . $awaiting_text . '</span>';
 
 	$wp_admin_bar->add_menu( array(
 		'id'    => 'comments',
 		'title' => $icon . $title,
 		'href'  => admin_url('edit-comments.php'),
-		'meta'  => array( 'title' => $awaiting_title ),
 	) );
 }
 
