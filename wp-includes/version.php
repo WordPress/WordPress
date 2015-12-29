f106974374666d8ec01ebf52b57dbe04e286cbdc
Dion Hulse <wordpress@dd32.id.au>
2015-12-29 05:44:25 +0000

    Tests: After [36100] use an object style which is compatible with PHP5 `get_object_vars()`.
    See #35058.
    
    Built from https://develop.svn.wordpress.org/trunk@36117
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36083 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-alpha-36116';
+$wp_version = '4.5-alpha-36117';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
