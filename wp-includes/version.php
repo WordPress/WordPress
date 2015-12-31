7b3b8f6fd4e29f3d4609195e29dd033d47a7e19b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2015-12-31 03:13:26 +0000

    Widgets: Revert [34465], as it introduced a regression, making the `$index` argument of `dynamic_sidebar()` case-sensitive.
    Fixes #34995 for trunk. See #23423.
    Built from https://develop.svn.wordpress.org/trunk@36130
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36096 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-alpha-36128';
+$wp_version = '4.5-alpha-36130';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
