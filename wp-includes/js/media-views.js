6abb7e62849c3c00ccd9135c6d0b33702e2547b8
Andrew Ozz <admin@laptoptips.ca>
2016-01-04 00:33:28 +0000

    Media: when inserting an attachment in the editor and it is not an image, ensure the link is set to something else than `none`.
    Props eherman24, azaozz.
    Fizes #35153 for trunk.
    Built from https://develop.svn.wordpress.org/trunk@36166
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36132 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/media-views.js
+++ wp-includes/js/media-views.js
@@ -1214,11 +1214,32 @@ Library = wp.media.controller.State.extend({
 	 * @returns {Object}
 	 */
 	defaultDisplaySettings: function( attachment ) {
-		var settings = this._defaultDisplaySettings;
+		var settings = _.clone( this._defaultDisplaySettings );
+
 		if ( settings.canEmbed = this.canEmbed( attachment ) ) {
 			settings.link = 'embed';
+		} else if ( ! this.isImageAttachment( attachment ) && settings.link === 'none' ) {
+			settings.link = 'file';
 		}
+
 		return settings;
+	},
+
+	/**
+	 * Whether an attachment is image.
+	 *
+	 * @since 4.4.1
+	 *
+	 * @param {wp.media.model.Attachment} attachment
+	 * @returns {Boolean}
+	 */
+	isImageAttachment: function( attachment ) {
+		// If uploading, we know the filename but not the mime type.
+		if ( attachment.get('uploading') ) {
+			return /\.(jpe?g|png|gif)$/i.test( attachment.get('filename') );
+		}
+
+		return attachment.get('type') === 'image';
 	},
 
 	/**
