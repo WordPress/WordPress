1f9c29d98fa9c2ea6072b3c9506bde30c0b8bbdb
Andrea Fercia <a.fercia@gmail.com>
2016-01-08 19:01:26 +0000

    Accessibility: remove the title attribute from the `get_search_form()` HTML5 search field.
    It was added in [23801] as a workaround for issues with very old browser/screen reader combos (Jaws7/IE6).
    No more necessary today. Reduces redundancy and noise for screen reader users.
    
    See #16539.
    Fixes #33952.
    Built from https://develop.svn.wordpress.org/trunk@36222
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36189 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -226,7 +226,7 @@ function get_search_form( $echo = true ) {
 			$form = '<form role="search" method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">
 				<label>
 					<span class="screen-reader-text">' . _x( 'Search for:', 'label' ) . '</span>
-					<input type="search" class="search-field" placeholder="' . esc_attr_x( 'Search &hellip;', 'placeholder' ) . '" value="' . get_search_query() . '" name="s" title="' . esc_attr_x( 'Search for:', 'label' ) . '" />
+					<input type="search" class="search-field" placeholder="' . esc_attr_x( 'Search &hellip;', 'placeholder' ) . '" value="' . get_search_query() . '" name="s" />
 				</label>
 				<input type="submit" class="search-submit" value="'. esc_attr_x( 'Search', 'submit button' ) .'" />
 			</form>';
