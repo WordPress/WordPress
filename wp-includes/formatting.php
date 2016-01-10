877d14883e8df7c28588bf2496b384314d37cd62
John Blackbourn <johnbillion@git.wordpress.org>
2016-01-10 01:24:26 +0000

    Emoji: Explictly use `https` as the scheme for emoji fallback images, as they're only served over HTTPS by the CDN anyway.
    Fixes #35376
    Props ocean90
    
    Built from https://develop.svn.wordpress.org/trunk@36249
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36216 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -4532,7 +4532,7 @@ function print_emoji_detection_script() {
 		 *
 		 * @param string The emoji base URL.
 		 */
-		'baseUrl' => apply_filters( 'emoji_url', set_url_scheme( '//s.w.org/images/core/emoji/72x72/' ) ),
+		'baseUrl' => apply_filters( 'emoji_url', 'https://s.w.org/images/core/emoji/72x72/' ),
 
 		/**
 		 * Filter the extension of the emoji files.
@@ -4644,7 +4644,7 @@ function wp_staticize_emoji( $text ) {
 	$text = wp_encode_emoji( $text );
 
 	/** This filter is documented in wp-includes/formatting.php */
-	$cdn_url = apply_filters( 'emoji_url', set_url_scheme( '//s.w.org/images/core/emoji/72x72/' ) );
+	$cdn_url = apply_filters( 'emoji_url', 'https://s.w.org/images/core/emoji/72x72/' );
 
 	/** This filter is documented in wp-includes/formatting.php */
 	$ext = apply_filters( 'emoji_ext', '.png' );
