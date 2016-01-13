7ab88221fad2f7703b033c4b7f24d4646a805316
Andrea Fercia <a.fercia@gmail.com>
2016-01-13 22:36:26 +0000

    Customizer: Aggregate similar CSS rules.
    Fixes #34333.
    Built from https://develop.svn.wordpress.org/trunk@36291
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36258 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-alpha-36290';
+$wp_version = '4.5-alpha-36291';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
