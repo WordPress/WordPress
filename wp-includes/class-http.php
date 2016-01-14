0f9b770b6cfca282d7e93a5787addf68105d7b3d
Joe Hoyle <joehoyle@gmail.com>
2016-01-14 03:16:27 +0000

    Add response status code aliases on WP_Http for convenience.
    These provide a more descriptive way to set response codes elsewhere,
    so it's readable and less chance for the wrong response code to be
    used such as 401 vs 403.
    
    Props rmccue for the idea.
    Fixes #35426.
    
    Built from https://develop.svn.wordpress.org/trunk@36294
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36261 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-http.php
+++ wp-includes/class-http.php
@@ -20,6 +20,67 @@
  */
 class WP_Http {
 
+	// Aliases for HTTP response codes.
+	const OK                              = 200;
+	const CREATED                         = 201;
+	const ACCEPTED                        = 202;
+	const NON_AUTHORITATIVE_INFORMATION   = 203;
+	const NO_CONTENT                      = 204;
+	const RESET_CONTENT                   = 205;
+	const PARTIAL_CONTENT                 = 206;
+	const MULTI_STATUS                    = 207;
+	const IM_USED                         = 226;
+
+	const MULTIPLE_CHOICES                = 300;
+	const MOVED_PERMANENTLY               = 301;
+	const FOUND                           = 302;
+	const SEE_OTHER                       = 303;
+	const NOT_MODIFIED                    = 304;
+	const USE_PROXY                       = 305;
+	const RESERVED                        = 306;
+	const TEMPORARY_REDIRECT              = 307;
+	const PERMANENT_REDIRECT              = 308;
+
+	const BAD_REQUEST                     = 400;
+	const UNAUTHORIZED                    = 401;
+	const PAYMENT_REQUIRED                = 402;
+	const FORBIDDEN                       = 403;
+	const NOT_FOUND                       = 404;
+	const METHOD_NOT_ALLOWED              = 405;
+	const NOT_ACCEPTABLE                  = 406;
+	const PROXY_AUTHENTICATION_REQUIRED   = 407;
+	const REQUEST_TIMEOUT                 = 408;
+	const CONFLICT                        = 409;
+	const GONE                            = 410;
+	const LENGTH_REQUIRED                 = 411;
+	const PRECONDITION_FAILED             = 412;
+	const REQUEST_ENTITY_TOO_LARGE        = 413;
+	const REQUEST_URI_TOO_LONG            = 414;
+	const UNSUPPORTED_MEDIA_TYPE          = 415;
+	const REQUESTED_RANGE_NOT_SATISFIABLE = 416;
+	const EXPECTATION_FAILED              = 417;
+	const IM_A_TEAPOT                     = 418;
+	const MISDIRECTED_REQUEST             = 421;
+	const UNPROCESSABLE_ENTITY            = 422;
+	const LOCKED                          = 423;
+	const FAILED_DEPENDENCY               = 424;
+	const UPGRADE_REQUIRED                = 426;
+	const PRECONDITION_REQUIRED           = 428;
+	const TOO_MANY_REQUESTS               = 429;
+	const REQUEST_HEADER_FIELDS_TOO_LARGE = 431;
+	const UNAVAILABLE_FOR_LEGAL_REASONS   = 451;
+
+	const INTERNAL_SERVER_ERROR           = 500;
+	const NOT_IMPLEMENTED                 = 501;
+	const BAD_GATEWAY                     = 502;
+	const SERVICE_UNAVAILABLE             = 503;
+	const GATEWAY_TIMEOUT                 = 504;
+	const HTTP_VERSION_NOT_SUPPORTED      = 505;
+	const VARIANT_ALSO_NEGOTIATES         = 506;
+	const INSUFFICIENT_STORAGE            = 507;
+	const NOT_EXTENDED                    = 510;
+	const NETWORK_AUTHENTICATION_REQUIRED = 511;
+
 	/**
 	 * Send an HTTP request to a URI.
 	 *
