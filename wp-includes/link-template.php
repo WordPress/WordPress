ca94aafb5057c9c0a151a56ef51168f44d9a65d6
Drew Jaynes <info@drewapicture.com>
2016-01-14 17:56:27 +0000

    Docs: Add a changelog entry to the DocBlock for `get_post_type_archive_link()` denoting that posts support was added in 4.5.
    See [36225]. See #19902.
    
    Built from https://develop.svn.wordpress.org/trunk@36300
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36267 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/link-template.php
+++ wp-includes/link-template.php
@@ -1110,13 +1110,14 @@ function get_search_comments_feed_link($search_query = '', $feed = '') {
 }
 
 /**
- * Retrieve the permalink for a post type archive.
+ * Retrieves the permalink for a post type archive.
  *
  * @since 3.1.0
+ * @since 4.5.0 Support for posts was added.
  *
  * @global WP_Rewrite $wp_rewrite
  *
- * @param string $post_type Post type
+ * @param string $post_type Post type.
  * @return string|false The post type archive permalink.
  */
 function get_post_type_archive_link( $post_type ) {
