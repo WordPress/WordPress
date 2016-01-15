1bad16273e06d8506bb7b9ecfadc9f35c9d030e4
Pascal Birchler <pascal.birchler@gmail.com>
2016-01-15 14:50:30 +0000

    Add tests missed and announced in [36319].
    See #27246.
    Built from https://develop.svn.wordpress.org/trunk@36320
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36287 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-alpha-1452869232832';
+$wp_version = '4.5-alpha-1452869412612';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
