ededb78efc523cd48d8f94dfce0a8425ace55027
Eric Lewis <eric.andrew.lewis@gmail.com>
2016-01-15 13:18:27 +0000

    Docs: Use "site" rather than "blog" in `get_current_blog_id()` docs.
    Fixes #35415.
    
    Built from https://develop.svn.wordpress.org/trunk@36317
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36284 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -761,13 +761,13 @@ function is_multisite() {
 }
 
 /**
- * Retrieve the current blog ID.
+ * Retrieve the current site ID.
  *
  * @since 3.1.0
  *
  * @global int $blog_id
  *
- * @return int Blog id
+ * @return int Site ID.
  */
 function get_current_blog_id() {
 	global $blog_id;
