96f0c643e3e9ea9ae35b0df7b4cab54b2e50f7d9
Eric Lewis <eric.andrew.lewis@gmail.com>
2016-01-19 03:56:27 +0000

    Build/Test Tools: Move PHP factory classes into their own files.
    This makes the code easier to browse.
    
    `factory.php` loads the new files, so this is backwards compatible in case `factory.php` is loaded directly for access to one of the classes.
    
    See #35492.
    
    Built from https://develop.svn.wordpress.org/trunk@36347
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36314 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-alpha-1453172592970';
+$wp_version = '4.5-alpha-1453175773216';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
