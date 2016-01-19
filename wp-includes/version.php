f1e6cde0a214c1006dbf3b1c0e71a88a818a84f3
Boone Gorges <boonebgorges@gmail.com>
2016-01-19 03:03:26 +0000

    Share post fixture in `WP_Comment_Query` tests.
    See #30017.
    Built from https://develop.svn.wordpress.org/trunk@36346
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36313 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-alpha-1453172113095';
+$wp_version = '4.5-alpha-1453172592970';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
