196271e5b42112bcbb513efcefa5fd11506add10
Drew Jaynes <info@drewapicture.com>
2016-01-22 20:22:25 +0000

    Docs: List out possible values for the `$show` parameter in the description for `get_bloginfo()`.
    Fixes #35581.
    
    Built from https://develop.svn.wordpress.org/trunk@36382
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36349 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -600,30 +600,50 @@ function bloginfo( $show = '' ) {
 }
 
 /**
- * Retrieve information about the blog.
+ * Retrieves information about the current site.
  *
- * Some show parameter values are deprecated and will be removed in future
- * versions. These options will trigger the {@see _deprecated_argument()}
- * function. The deprecated blog info options are listed in the function
- * contents.
+ * Possible values for `$show` include:
  *
- * The possible values for the 'show' parameter are listed below.
+ * - 'name' - Site title (set in Settings > General)
+ * - 'description' - Site tagline (set in Settings > General)
+ * - 'wpurl' - The WordPress address (URL) (set in Settings > General)
+ * - 'url' - The Site address (URL) (set in Settings > General)
+ * - 'admin_email' - Admin email (set in Settings > General)
+ * - 'charset' - The "Encoding for pages and feeds"  (set in Settings > Reading)
+ * - 'version' - The current WordPress version
+ * - 'html_type' - The content-type (default: "text/html"). Themes and plugins
+ *   can override the default value using the {@see 'pre_option_html_type'} filter
+ * - 'text_direction' - The text direction determined by the site's language. is_rtl()
+ *   should be used instead
+ * - 'language' - Language code for the current site
+ * - 'stylesheet_url' - URL to the stylesheet for the active theme. An active child theme
+ *   will take precedence over this value
+ * - 'stylesheet_directory' - Directory path for the active theme.  An active child theme
+ *   will take precedence over this value
+ * - 'template_url' / 'template_directory' - URL of the active theme's directory. An active
+ *   child theme will NOT take precedence over this value
+ * - 'pingback_url' - The pingback XML-RPC file URL (xmlrpc.php)
+ * - 'atom_url' - The Atom feed URL (/feed/atom)
+ * - 'rdf_url' - The RDF/RSS 1.0 feed URL (/feed/rfd)
+ * - 'rss_url' - The RSS 0.92 feed URL (/feed/rss)
+ * - 'rss2_url' - The RSS 2.0 feed URL (/feed)
+ * - 'comments_atom_url' - The comments Atom feed URL (/comments/feed)
+ * - 'comments_rss2_url' - The comments RSS 2.0 feed URL (/comments/feed)
  *
- * 1. url - Blog URI to homepage.
- * 2. wpurl - Blog URI path to WordPress.
- * 3. description - Secondary title
+ * Some `$show` values are deprecated and will be removed in future versions.
+ * These options will trigger the _deprecated_argument() function.
  *
- * The feed URL options can be retrieved from 'rdf_url' (RSS 0.91),
- * 'rss_url' (RSS 1.0), 'rss2_url' (RSS 2.0), or 'atom_url' (Atom feed). The
- * comment feeds can be retrieved from the 'comments_atom_url' (Atom comment
- * feed) or 'comments_rss2_url' (RSS 2.0 comment feed).
+ * Deprecated arguments include:
+ *
+ * - 'siteurl' - Use 'url' instead
+ * - 'home' - Use 'url' instead
  *
  * @since 0.71
  *
  * @global string $wp_version
  *
- * @param string $show   Blog info to retrieve.
- * @param string $filter How to filter what is retrieved.
+ * @param string $show   Optional. Site info to retrieve. Default empty (site name).
+ * @param string $filter Optional. How to filter what is retrieved. Default 'raw'.
  * @return string Mostly string values, might be empty.
  */
 function get_bloginfo( $show = '', $filter = 'raw' ) {
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-alpha-36381';
+$wp_version = '4.5-alpha-36382';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
