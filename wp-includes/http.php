f6cde8e3c502f313d8d4454654c891fd980cf690
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2016-01-26 00:39:26 +0000

    Docs: Correct return value for `is_allowed_http_origin()`.
    Props kraftbj.
    Fixes #35607.
    Built from https://develop.svn.wordpress.org/trunk@36398
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36365 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/http.php
+++ wp-includes/http.php
@@ -443,7 +443,7 @@ function get_allowed_http_origins() {
  * @since 3.4.0
  *
  * @param null|string $origin Origin URL. If not provided, the value of get_http_origin() is used.
- * @return string True if the origin is allowed. False otherwise.
+ * @return string Origin URL if allowed, empty string if not.
  */
 function is_allowed_http_origin( $origin = null ) {
 	$origin_arg = $origin;
@@ -459,7 +459,7 @@ function is_allowed_http_origin( $origin = null ) {
 	 *
 	 * @since 3.4.0
 	 *
-	 * @param string $origin     Result of check for allowed origin.
+	 * @param string $origin     Origin URL if allowed, empty string if not.
 	 * @param string $origin_arg Original origin string passed into is_allowed_http_origin function.
 	 */
 	return apply_filters( 'allowed_http_origin', $origin, $origin_arg );
