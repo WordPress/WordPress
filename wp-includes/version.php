487580f7c47ddce49898342a19acd49fb2fded93
Dion Hulse <wordpress@dd32.id.au>
2016-01-27 10:11:28 +0000

    Emoji: Work around a `mod_security` rule which prevents pages with 4 or more instances of `String.fromCharCode(` from being served.
    Merges [36359] to the 4.4 branch.
    Fixes #35412.
    
    Built from https://develop.svn.wordpress.org/branches/4.4@36410
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.4@36377 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4.2-alpha-36378';
+$wp_version = '4.4.2-alpha-36410';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
