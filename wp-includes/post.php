8db393ada6bd2c33e65a71e06a21cac235741c7f
Boone Gorges <boonebgorges@gmail.com>
2016-02-05 18:58:25 +0000

    Make the `$post` param optional in `get_post_field()`.
    When `$post` is `null`, the current post object will be returned.
    
    Props sebastian.pisula.
    Fixes #35683.
    Built from https://develop.svn.wordpress.org/trunk@36481
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36448 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -494,16 +494,17 @@ function get_post_ancestors( $post ) {
  * supported values are found within those functions.
  *
  * @since 2.3.0
+ * @since 4.5.0 The `$post` parameter was made optional.
  *
  * @see sanitize_post_field()
  *
  * @param string      $field   Post field name.
- * @param int|WP_Post $post    Post ID or post object.
+ * @param int|WP_Post $post    Optional. Post ID or post object. Defaults to current post.
  * @param string      $context Optional. How to filter the field. Accepts 'raw', 'edit', 'db',
  *                             or 'display'. Default 'display'.
  * @return string The value of the post field on success, empty string on failure.
  */
-function get_post_field( $field, $post, $context = 'display' ) {
+function get_post_field( $field, $post = null, $context = 'display' ) {
 	$post = get_post( $post );
 
 	if ( !$post )
