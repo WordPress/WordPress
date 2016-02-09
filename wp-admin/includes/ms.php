cbe5fc6fb8be67a0429e06eb8f3c79505c02538c
Dominik Schilling <dominikschilling+git@gmail.com>
2016-02-09 14:30:28 +0000

    Multisite: Don't show the database upgrade admin notice on the upgrade page itself.
    Fixes #35782.
    Built from https://develop.svn.wordpress.org/trunk@36505
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36472 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/ms.php
+++ wp-admin/includes/ms.php
@@ -748,16 +748,25 @@ function mu_dropdown_languages( $lang_files = array(), $current = '' ) {
  *
  * @since 3.0.0
  *
- * @global int $wp_db_version The version number of the database.
+ * @global int    $wp_db_version The version number of the database.
+ * @global string $pagenow
  *
  * @return false False if the current user is not a super admin.
  */
 function site_admin_notice() {
-	global $wp_db_version;
-	if ( !is_super_admin() )
+	global $wp_db_version, $pagenow;
+
+	if ( ! is_super_admin() ) {
 		return false;
-	if ( get_site_option( 'wpmu_upgrade_site' ) != $wp_db_version )
+	}
+
+	if ( 'upgrade.php' == $pagenow ) {
+		return;
+	}
+
+	if ( get_site_option( 'wpmu_upgrade_site' ) != $wp_db_version ) {
 		echo "<div class='update-nag'>" . sprintf( __( 'Thank you for Updating! Please visit the <a href="%s">Upgrade Network</a> page to update all your sites.' ), esc_url( network_admin_url( 'upgrade.php' ) ) ) . "</div>";
+	}
 }
 
 /**
