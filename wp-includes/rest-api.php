796f0c844c957327cd523203fce04642aff2ee38
Ryan McCue <me@ryanmccue.info>
2016-02-16 01:12:26 +0000

    REST API: Add helper function to get server instance.
    This allows using rest_do_request() outside of the API itself easily.
    
    Props danielbachhuber, swissspidy.
    
    Built from https://develop.svn.wordpress.org/trunk@36529
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36496 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api.php
+++ wp-includes/rest-api.php
@@ -140,36 +140,11 @@ function rest_api_loaded() {
 	 */
 	define( 'REST_REQUEST', true );
 
-	/** @var WP_REST_Server $wp_rest_server */
-	global $wp_rest_server;
-
-	/**
-	 * Filter the REST Server Class.
-	 *
-	 * This filter allows you to adjust the server class used by the API, using a
-	 * different class to handle requests.
-	 *
-	 * @since 4.4.0
-	 *
-	 * @param string $class_name The name of the server class. Default 'WP_REST_Server'.
-	 */
-	$wp_rest_server_class = apply_filters( 'wp_rest_server_class', 'WP_REST_Server' );
-	$wp_rest_server = new $wp_rest_server_class;
-
-	/**
-	 * Fires when preparing to serve an API request.
-	 *
-	 * Endpoint objects should be created and register their hooks on this action rather
-	 * than another action to ensure they're only loaded when needed.
-	 *
-	 * @since 4.4.0
-	 *
-	 * @param WP_REST_Server $wp_rest_server Server object.
-	 */
-	do_action( 'rest_api_init', $wp_rest_server );
+	// Initialize the server.
+	$server = rest_get_server();
 
 	// Fire off the request.
-	$wp_rest_server->serve_request( $GLOBALS['wp']->query_vars['rest_route'] );
+	$server->serve_request( $GLOBALS['wp']->query_vars['rest_route'] );
 
 	// We're done.
 	die();
@@ -273,9 +248,53 @@ function rest_url( $path = '', $scheme = 'json' ) {
  * @return WP_REST_Response REST response.
  */
 function rest_do_request( $request ) {
-	global $wp_rest_server;
 	$request = rest_ensure_request( $request );
-	return $wp_rest_server->dispatch( $request );
+	return rest_get_server()->dispatch( $request );
+}
+
+/**
+ * Get the current REST server instance.
+ *
+ * Instantiates a new instance if none exists already.
+ *
+ * @since 4.5.0
+ *
+ * @global WP_REST_Server $wp_rest_server REST server instance.
+ *
+ * @return  WP_REST_Server REST server instance.
+ */
+function rest_get_server() {
+	/* @var WP_REST_Server $wp_rest_server */
+	global $wp_rest_server;
+
+	if ( empty( $wp_rest_server ) ) {
+		/**
+		 * Filter the REST Server Class.
+		 *
+		 * This filter allows you to adjust the server class used by the API, using a
+		 * different class to handle requests.
+		 *
+		 * @since 4.4.0
+		 *
+		 * @param string $class_name The name of the server class. Default 'WP_REST_Server'.
+		 */
+		$wp_rest_server_class = apply_filters( 'wp_rest_server_class', 'WP_REST_Server' );
+		$wp_rest_server = new $wp_rest_server_class;
+
+		/**
+		 * Fires when preparing to serve an API request.
+		 *
+		 * Endpoint objects should be created and register their hooks on this action rather
+		 * than another action to ensure they're only loaded when needed.
+		 *
+		 * @since 4.4.0
+		 *
+		 * @param WP_REST_Server $wp_rest_server Server object.
+		 */
+		do_action( 'rest_api_init', $wp_rest_server );
+	}
+
+	return $wp_rest_server;
 }
 
 /**
