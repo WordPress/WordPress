284f88e35a0c0f2ce0d611302049338dbed21e2b
Dominik Schilling <dominikschilling+git@gmail.com>
2016-02-18 21:11:27 +0000

    Themes: Fix flickering of the theme screenshot on hover in WebKit browsers.
    Props JoeFusco.
    Fixes #35787.
    Built from https://develop.svn.wordpress.org/trunk@36579
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36546 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-alpha-36578';
+$wp_version = '4.5-alpha-36579';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
