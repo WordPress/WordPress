30866ceb5d2a5f87b6bde426cdfc4f4fe50f275d
Andrea Fercia <a.fercia@gmail.com>
2016-02-19 18:44:27 +0000

    Accessibility: Improve color contrast updating any `#999` gray used for text or icons to a darker gray.
    Fixes #35660.
    Built from https://develop.svn.wordpress.org/trunk@36587
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36554 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/misc.php
+++ wp-admin/includes/misc.php
@@ -710,7 +710,7 @@ function wp_color_scheme_settings() {
 		$icon_colors = $_wp_admin_css_colors['fresh']->icon_colors;
 	} else {
 		// Fall back to the default set of icon colors if the default scheme is missing.
-		$icon_colors = array( 'base' => '#999', 'focus' => '#00a0d2', 'current' => '#fff' );
+		$icon_colors = array( 'base' => '#82878c', 'focus' => '#00a0d2', 'current' => '#fff' );
 	}
 
 	echo '<script type="text/javascript">var _wpColorScheme = ' . wp_json_encode( array( 'icons' => $icon_colors ) ) . ";</script>\n";
--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -3144,7 +3144,7 @@ function register_admin_color_schemes() {
 	wp_admin_css_color( 'fresh', _x( 'Default', 'admin color scheme' ),
 		false,
 		array( '#222', '#333', '#0073aa', '#00a0d2' ),
-		array( 'base' => '#999', 'focus' => '#00a0d2', 'current' => '#fff' )
+		array( 'base' => '#82878c', 'focus' => '#00a0d2', 'current' => '#fff' )
 	);
 
 	// Other color schemes are not available when running out of src
