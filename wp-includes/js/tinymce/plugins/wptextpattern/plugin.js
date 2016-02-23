3d68488ff6010db0004d4fc7bd54ca508a94f6d4
Ella Iseulde Van Dorpe <*@iseulde.com>
2016-02-23 08:33:27 +0000

    TinyMCE: inline text patterns
    First run.
    
    See #33300.
    
    Built from https://develop.svn.wordpress.org/trunk@36627
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36594 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/tinymce/plugins/wptextpattern/plugin.js
+++ wp-includes/js/tinymce/plugins/wptextpattern/plugin.js
@@ -12,23 +12,56 @@
  */
 ( function( tinymce, setTimeout ) {
 	tinymce.PluginManager.add( 'wptextpattern', function( editor ) {
-		var VK = tinymce.util.VK,
-			spacePatterns = [
-				{ regExp: /^[*-]\s/, cmd: 'InsertUnorderedList' },
-				{ regExp: /^1[.)]\s/, cmd: 'InsertOrderedList' }
-			],
-			enterPatterns = [
-				{ start: '##', format: 'h2' },
-				{ start: '###', format: 'h3' },
-				{ start: '####', format: 'h4' },
-				{ start: '#####', format: 'h5' },
-				{ start: '######', format: 'h6' },
-				{ start: '>', format: 'blockquote' }
-			],
-			canUndo, refNode, refPattern;
+		var VK = tinymce.util.VK;
+
+		var spacePatterns = [
+			{ regExp: /^[*-]\s/, cmd: 'InsertUnorderedList' },
+			{ regExp: /^1[.)]\s/, cmd: 'InsertOrderedList' }
+		];
+
+		var enterPatterns = [
+			{ start: '##', format: 'h2' },
+			{ start: '###', format: 'h3' },
+			{ start: '####', format: 'h4' },
+			{ start: '#####', format: 'h5' },
+			{ start: '######', format: 'h6' },
+			{ start: '>', format: 'blockquote' }
+		];
+
+		var inlinePatterns = [
+			{ start: '*', end: '*', format: 'italic' },
+			{ start: '**', end: '**', format: 'bold' },
+			{ start: '_', end: '_', format: 'italic' },
+			{ start: '__', end: '__', format: 'bold' },
+			{ start: '`', end: '`', format: 'code' }
+		];
+
+		var canUndo;
+		var refNode;
+		var refPattern;
+		var chars = [];
+		var zeroWidthSpaceNode;
+
+		tinymce.each( inlinePatterns, function( pattern ) {
+			tinymce.each( ( pattern.start + pattern.end ).split( '' ), function( c ) {
+				if ( tinymce.inArray( chars, c ) === -1 ) {
+					chars.push( c );
+				}
+			} );
+		} );
 
 		editor.on( 'selectionchange', function() {
+			var offset;
+
 			canUndo = null;
+
+			if ( zeroWidthSpaceNode ) {
+				offset = zeroWidthSpaceNode.data.indexOf( '\u200b' );
+
+				if ( offset !== -1 ) {
+					zeroWidthSpaceNode.deleteData( offset, offset + 1 );
+				}
+			}
 		} );
 
 		editor.on( 'keydown', function( event ) {
@@ -48,8 +81,84 @@
 				space();
 			} else if ( event.keyCode === VK.ENTER && ! VK.modifierPressed( event ) ) {
 				enter();
+			} else if ( event.keyCode > 47 && ! ( event.keyCode >= 91 && event.keyCode <= 93 ) ) {
+				inline();
 			}
 		} );
+
+		function inline() {
+			var rng = editor.selection.getRng();
+			var node = rng.startContainer;
+			var offset = rng.startOffset;
+			var startOffset;
+			var endOffset;
+			var pattern;
+			var format;
+			var zero;
+
+			if ( node.nodeType !== 3 || ! node.data.length || ! offset ) {
+				return;
+			}
+
+			if ( tinymce.inArray( chars, node.data.charAt( offset - 1 ) ) === -1 ) {
+				return;
+			}
+
+			function findStart( node ) {
+				var i = inlinePatterns.length;
+				var offset;
+
+				while ( i-- ) {
+					pattern = inlinePatterns[ i ];
+					offset = node.data.indexOf( pattern.end );
+
+					if ( offset !== -1 ) {
+						return offset;
+					}
+				}
+			}
+
+			startOffset = findStart( node );
+			endOffset = node.data.lastIndexOf( pattern.end );
+
+			if ( startOffset === endOffset || endOffset === -1 ) {
+				return;
+			}
+
+			if ( endOffset - startOffset <= pattern.start.length ) {
+				return;
+			}
+
+			if ( node.data.slice( startOffset + pattern.start.length, endOffset ).indexOf( pattern.start.slice( 0, 1 ) ) !== -1 ) {
+				return;
+			}
+
+			format = editor.formatter.get( pattern.format );
+
+			if ( format && format[0].inline ) {
+				editor.undoManager.add();
+
+				editor.undoManager.transact( function() {
+					node.insertData( offset, '\u200b' );
+
+					node = node.splitText( startOffset );
+					zero = node.splitText( offset - startOffset );
+
+					node.deleteData( 0, pattern.start.length );
+					node.deleteData( node.data.length - pattern.end.length, pattern.end.length );
+
+					editor.formatter.apply( pattern.format, {}, node );
+
+					editor.selection.setCursorLocation( zero, 1 );
+				} );
+
+				// We need to wait for native events to be triggered.
+				setTimeout( function() {
+					canUndo = 'space';
+					zeroWidthSpaceNode = zero;
+				} );
+			}
+		}
 
 		function firstTextNode( node ) {
 			var parent = editor.dom.getParent( node, 'p' ),
@@ -157,11 +266,7 @@
 		}
 
 		function ltrim( text ) {
-			if ( text ) {
-				return text.replace( /^\s+/, '' );
-			}
-
-			return '';
+			return text ? text.replace( /^\s+/, '' ) : '';
 		}
 
 		function enter() {
