fa3032b07667b4ca67cd80fe0df477ede16cb08b
Weston Ruter <weston@xwp.co>
2016-02-23 21:46:27 +0000

    Customize: Fix and extend broken ajax unit tests to account for partials being skipped from rendering.
    Fixes regression introduced in [36643].
    
    See #35914.
    
    Built from https://develop.svn.wordpress.org/trunk@36650
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36617 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-alpha-36649';
+$wp_version = '4.5-alpha-36650';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
