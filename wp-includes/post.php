4c3c2b315f7d9f5f2d58a30b9f42e8b42e09b4ef
Drew Jaynes <info@drewapicture.com>
2016-02-25 04:55:25 +0000

    Posts: Rename the `$args` parameter in `get_post_types_by_support()` to `$feature` for better self-documentation.
    See #34010. See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@36704
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36671 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -1639,23 +1639,23 @@ function post_type_supports( $post_type, $feature ) {
 }
 
 /**
- * Get a list of post type names that support a specific feature.
+ * Retrieves a list of post type names that support a specific feature.
  *
  * @since 4.5.0
  *
- * @global array $_wp_post_type_features
+ * @global array $_wp_post_type_features Post type features
  *
- * @param array|string $args     Single feature or an array of features the post types should support.
+ * @param array|string $feature  Single feature or an array of features the post types should support.
  * @param string       $operator Optional. The logical operation to perform. 'or' means
  *                               only one element from the array needs to match; 'and'
  *                               means all elements must match; 'not' means no elements may
  *                               match. Default 'and'.
  * @return array A list of post type names.
  */
-function get_post_types_by_support( $args, $operator = 'and' ) {
+function get_post_types_by_support( $feature, $operator = 'and' ) {
 	global $_wp_post_type_features;
 
-	$features = array_fill_keys( (array) $args, true );
+	$features = array_fill_keys( (array) $feature, true );
 
 	return array_keys( wp_filter_object_list( $_wp_post_type_features, $features, $operator ) );
 }
