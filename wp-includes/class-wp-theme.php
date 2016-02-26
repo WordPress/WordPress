22ad47832504410c25d26d43fcefa32bb6c2e16b
Drew Jaynes <info@drewapicture.com>
2016-02-26 17:12:26 +0000

    Docs: Add a missing description for the `&$themes` parameter in the DocBlock for `WP_Theme::sort_by_name()`.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@36736
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36703 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-theme.php
+++ wp-includes/class-wp-theme.php
@@ -1316,12 +1316,14 @@ final class WP_Theme implements ArrayAccess {
 	}
 
 	/**
-	 * Sort themes by name.
+	 * Sorts themes by name.
 	 *
 	 * @since 3.4.0
 	 *
 	 * @static
 	 * @access public
+	 *
+	 * @param array $themes Array of themes to sort, passed by reference.
 	 */
 	public static function sort_by_name( &$themes ) {
 		if ( 0 === strpos( get_locale(), 'en_' ) ) {
