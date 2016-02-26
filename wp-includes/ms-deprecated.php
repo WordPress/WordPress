0f279543630075b7d7ac42d853586631775cf79b
Drew Jaynes <info@drewapicture.com>
2016-02-26 08:54:26 +0000

    Docs: Add a missing `@param` entry for the `$len` parameter in the DocBlock for the deprecated `generate_random_password()` function.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@36723
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36690 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-deprecated.php
+++ wp-includes/ms-deprecated.php
@@ -37,6 +37,8 @@ function get_dashboard_blog() {
  * @since MU
  * @deprecated 3.0.0 Use wp_generate_password()
  * @see wp_generate_password()
+ *
+ * @param int $len Optional. The length of password to generate. Default 8.
  */
 function generate_random_password( $len = 8 ) {
 	_deprecated_function( __FUNCTION__, '3.0', 'wp_generate_password()' );
