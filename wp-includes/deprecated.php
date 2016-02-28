d48cd1cb280f7172a902f0282b79dc8d29aeeec3
Drew Jaynes <info@drewapicture.com>
2016-02-28 18:18:28 +0000

    Docs: Add missing documentation for the `$wp_admin_bar` parameter in the DocBlock for the deprecated `wp_admin_bar_dashboard_view_site_menu()`.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@36754
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36721 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/deprecated.php
+++ wp-includes/deprecated.php
@@ -2777,6 +2777,8 @@ function parent_post_rel_link( $title = '%title' ) {
  *
  * @since 3.2.0
  * @deprecated 3.3.0
+ *
+ * @param WP_Admin_Bar $wp_admin_bar WP_Admin_Bar instance.
  */
 function wp_admin_bar_dashboard_view_site_menu( $wp_admin_bar ) {
 	_deprecated_function( __FUNCTION__, '3.3' );
