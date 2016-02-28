f976c0a6c8f62577ecee59ecc9a6ced369599827
Drew Jaynes <info@drewapicture.com>
2016-02-28 18:36:27 +0000

    Docs: Add missing parameter and return descriptions to the DocBlock for the deprecated `default_topic_count_text()`.
    See #32246.
    
    Built from https://develop.svn.wordpress.org/trunk@36760
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36727 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/deprecated.php
+++ wp-includes/deprecated.php
@@ -3382,6 +3382,9 @@ function rich_edit_exists() {
  * @since 2.7.0
  * @access private
  * @deprecated 3.9.0
+ *
+ * @param int $count Number of topics.
+ * @return int Number of topics.
  */
 function default_topic_count_text( $count ) {
 	return $count;
