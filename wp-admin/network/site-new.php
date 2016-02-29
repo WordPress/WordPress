d986a84152a2587cabdf3848f21c2de6dcc1fe2a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2016-02-29 02:44:26 +0000

    I18N: Remove `<code>` tag from translatable string in `wp-admin/network/site-new.php`.
    Props ramiy.
    Fixes #35989.
    Built from https://develop.svn.wordpress.org/trunk@36771
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36738 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/network/site-new.php
+++ wp-admin/network/site-new.php
@@ -49,7 +49,12 @@ if ( isset($_REQUEST['action']) && 'add-site' == $_REQUEST['action'] ) {
 		$subdirectory_reserved_names = get_subdirectory_reserved_names();
 
 		if ( in_array( $domain, $subdirectory_reserved_names ) ) {
-			wp_die( sprintf( __( 'The following words are reserved for use by WordPress functions and cannot be used as blog names: <code>%s</code>' ), implode( '</code>, <code>', $subdirectory_reserved_names ) ) );
+			wp_die(
+				/* translators: %s: reserved names list */
+				sprintf( __( 'The following words are reserved for use by WordPress functions and cannot be used as blog names: %s' ),
+					'<code>' . implode( '</code>, <code>', $subdirectory_reserved_names ) . '</code>'
+				)
+			);
 		}
 	}
 
