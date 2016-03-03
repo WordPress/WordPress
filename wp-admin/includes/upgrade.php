56fb3cc08d7e680dc4fdf70229e2286a290aba9a
Drew Jaynes <info@drewapicture.com>
2016-03-03 09:01:26 +0000

    Docs: Improve documentation for `upgrade_450()`, introduced in [36180].
    See #27772, #35323. See #35986.
    
    Built from https://develop.svn.wordpress.org/trunk@36825
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36792 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/upgrade.php
+++ wp-admin/includes/upgrade.php
@@ -1663,13 +1663,13 @@ function upgrade_440() {
 }
 
 /**
- * Execute changes made in WordPress 4.5.0
+ * Executes changes made in WordPress 4.5.0.
  *
  * @ignore
  * @since 4.5.0
  *
- * @global int  $wp_current_db_version
- * @global wpdb $wpdb
+ * @global int  $wp_current_db_version Current database version.
+ * @global wpdb $wpdb                  WordPress database abstraction object.
  */
 function upgrade_450() {
 	global $wp_current_db_version, $wpdb;
