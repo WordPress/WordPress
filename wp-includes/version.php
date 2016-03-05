c6b12480a124ae3d55f371353deb8ced2e26c543
Aaron Jorbin <aaron@jorb.in>
2016-03-05 23:25:29 +0000

    Update grunt-contrib-compress ~0.14.0 → ~1.1.0
    See #35104
    
    Built from https://develop.svn.wordpress.org/trunk@36862
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36829 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-beta2-36861';
+$wp_version = '4.5-beta2-36862';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
