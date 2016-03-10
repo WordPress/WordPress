643df239b1a5ad0dbe3f1c1d5fb6e260d0ec73bc
Drew Jaynes <info@drewapicture.com>
2016-03-10 17:46:27 +0000

    Docs: Improve the `$blog_id` parameter description in the DocBlock for `the_custom_logo()`, introduced in [36698].
    See #33755. See #35986.
    
    Built from https://develop.svn.wordpress.org/trunk@36938
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36906 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -918,7 +918,7 @@ function get_custom_logo( $blog_id = 0 ) {
  *
  * @since 4.5.0
  *
- * @param int $blog_id Optional. ID of the blog in question. Default current blog.
+ * @param int $blog_id Optional. ID of the blog in question. Default is the ID of the current blog.
  */
 function the_custom_logo( $blog_id = 0 ) {
 	echo get_custom_logo( $blog_id );
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-beta3-36937';
+$wp_version = '4.5-beta3-36938';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
