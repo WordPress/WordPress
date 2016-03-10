b04826bfe0f9f31579e953c233d7f65a41b17958
Drew Jaynes <info@drewapicture.com>
2016-03-10 17:44:25 +0000

    Docs: Improve inline documentation for `has_custom_logo()`, introduced in [36698].
    See #33755. See #35986.
    
    Built from https://develop.svn.wordpress.org/trunk@36936
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@36904 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -839,11 +839,11 @@ function has_site_icon( $blog_id = 0 ) {
 }
 
 /**
- * Whether the site has a custom logo.
+ * Determines whether the site has a custom logo.
  *
  * @since 4.5.0
  *
- * @param int $blog_id Optional. ID of the blog in question. Default current blog.
+ * @param int $blog_id Optional. ID of the blog in question. Default is the ID of the current blog.
  * @return bool Whether the site has a custom logo or not.
  */
 function has_custom_logo( $blog_id = 0 ) {
