a97d39785af15092699f0a8a0b3aea29dd332124
Andrew Ozz <admin@laptoptips.ca>
2016-03-23 21:45:28 +0000

    TinyMCE: include the upstream fix for inserting `<hr>` while the caret is in an inline element: https://github.com/tinymce/tinymce/commit/b1618d1adb57df992ee69907b1cfff9aa4d53c80.
    Fixes #36165.
    Built from https://develop.svn.wordpress.org/trunk@37074
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37041 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-beta4-37073';
+$wp_version = '4.5-beta4-37074';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 36686;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '4308-20160315';
+$tinymce_version = '4308-20160323';
 
 /**
  * Holds the required PHP version
