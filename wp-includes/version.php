1a9fda07a47f150dc142001479dfa3edd97b9441
Weston Ruter <weston@xwp.co>
2016-03-30 21:41:26 +0000

    Customize: Put focus on change button instead of remove button in media control.
    Allow controls to specify the primary element to receive focus by adding a `control-focus` class.
    
    Props adamsilverstein, westonruter.
    Fixes #36337.
    
    Built from https://develop.svn.wordpress.org/trunk@37152
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37118 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-RC1-37150';
+$wp_version = '4.5-RC1-37152';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
