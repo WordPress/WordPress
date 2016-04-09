4e6b257fa32b34785426a96cead2101e7197dc6a
Andrew Ozz <admin@laptoptips.ca>
2016-04-09 06:23:26 +0000

    4.5 About screen.
    Props mikeschroder, melchoyce, ocean90, ryelle, jeremyfelt, Ipstenu, celloexpressions, DrewAPicture.
    See #36173.
    Built from https://develop.svn.wordpress.org/trunk@37169
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37136 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -21,6 +21,10 @@ if ( current_user_can( 'install_plugins' ) ) {
 	wp_enqueue_script( 'plugin-install' );
 }
 
+if ( current_user_can( 'customize' ) ) {
+	wp_enqueue_script( 'customize-loader' );
+}
+
 $video_url = 'https://videopress.com/embed/J44FHXvg?hd=true';
 $locale    = str_replace( '_', '-', get_locale() );
 list( $locale ) = explode( '-', $locale );
@@ -39,7 +43,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 	<div class="wrap about-wrap">
 		<h1><?php printf( __( 'Welcome to WordPress&nbsp;%s' ), $display_version ); ?></h1>
 
-		<div class="about-text"><?php printf( __( 'Thank you for updating! WordPress %s makes your site more connected and responsive.' ), $display_version ); ?></div>
+		<div class="about-text"><?php printf( __( 'Thank you for updating! WordPress %s streamlines your workflow, whether you&#8217;re writing or building your site.' ), $display_version ); ?></div>
 		<div class="wp-badge"><?php printf( __( 'Version %s' ), $display_version ); ?></div>
 
 		<h2 class="nav-tab-wrapper wp-clearfix">
@@ -48,74 +52,54 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 			<a href="freedoms.php" class="nav-tab"><?php _e( 'Freedoms' ); ?></a>
 		</h2>
 
-		<div class="headline-feature feature-video">
-			<iframe width="1050" height="591" src="<?php echo esc_url( $video_url ); ?>" frameborder="0" allowfullscreen></iframe>
-			<script src="https://videopress.com/videopress-iframe.js"></script>
+		<div class="headline-feature feature-video" style="background-color:#191E23;">
+			<p>Video Here</p>
 		</div>
 
 		<hr>
 
-		<div class="headline-feature feature-section one-col">
-			<h2><?php _e( 'Twenty Sixteen' ); ?></h2>
-			<div class="media-container">
-				<img src="https://s.w.org/images/core/4.4/twenty-sixteen-white-fullsize-2x.png" alt="" srcset="https://s.w.org/images/core/4.4/twenty-sixteen-white-smartphone-1x.png 268w, https://s.w.org/images/core/4.4/twenty-sixteen-white-smartphone-2x.png 536w, https://s.w.org/images/core/4.4/twenty-sixteen-white-tablet-1x.png 558w, https://s.w.org/images/core/4.4/twenty-sixteen-white-desktop-1x.png 840w, https://s.w.org/images/core/4.4/twenty-sixteen-white-fullsize-1x.png 1086w, https://s.w.org/images/core/4.4/twenty-sixteen-white-tablet-2x.png 1116w, https://s.w.org/images/core/4.4/twenty-sixteen-white-desktop-2x.png 1680w, https://s.w.org/images/core/4.4/twenty-sixteen-white-fullsize-2x.png 2172w" sizes="(max-width: 500px) calc((100vw - 40px) * .8), (max-width: 782px) calc((100vw - 70px) * .8), (max-width: 960px) calc((100vw - 116px) * .8), (max-width: 1290px) calc((100vw - 240px) * .8), 840px" />
+		<div class="feature-section two-col">
+			<h2><?php _e( 'Editing Improvements' ); ?></h2>
+			<div class="col">
+				<img src="https://cldup.com/klO9vWGiT3.png" />
+				<h3><?php _e( 'Inline Linking' ); ?></h3>
+				<p><?php _e( 'Stay focused on your writing with a less distracting interface that keeps you in place and allows you to easily link to your content.' ); ?></p>
 			</div>
-			<div class="two-col">
-				<div class="col">
-					<h3><?php _e( 'Introducing Twenty Sixteen' ); ?></h3>
-					<p><?php _e( 'Our newest default theme, Twenty Sixteen, is a modern take on a classic blog design.' ); ?></p>
-					<p><?php _e( 'Twenty Sixteen was built to look great on any device. A fluid grid design, flexible header, fun color schemes, and more, will make your content shine.' ); ?></p>
-					<div class="horizontal-image">
-						<div class="content">
-							<img class="feature-image horizontal-screen" src="https://s.w.org/images/core/4.4/twenty-sixteen-dark-fullsize-2x.png" alt=""  srcset="https://s.w.org/images/core/4.4/twenty-sixteen-dark-smartphone-1x.png 268w, https://s.w.org/images/core/4.4/twenty-sixteen-dark-smartphone-2x.png 535w, https://s.w.org/images/core/4.4/twenty-sixteen-dark-desktop-1x.png 558w, https://s.w.org/images/core/4.4/twenty-sixteen-dark-fullsize-1x.png 783w, https://s.w.org/images/core/4.4/twenty-sixteen-dark-desktop-2x.png 1116w, https://s.w.org/images/core/4.4/twenty-sixteen-dark-fullsize-2x.png 1566w" sizes="(max-width: 500px) calc((100vw - 40px) * .8), (max-width: 782px) calc((100vw - 70px) * .8), (max-width: 960px) calc((100vw - 116px) * .5216), (max-width: 1290px) calc((100vw - 240px) * .5216), 548px" />
-						</div>
-					</div>
-				</div>
-				<div class="col feature-image">
-					<img class="vertical-screen" src="https://s.w.org/images/core/4.4/twenty-sixteen-red-fullsize-2x.png" alt="" srcset="https://s.w.org/images/core/4.4/twenty-sixteen-red-smartphone-1x.png 107w, https://s.w.org/images/core/4.4/twenty-sixteen-red-smartphone-2x.png 214w, https://s.w.org/images/core/4.4/twenty-sixteen-red-desktop-1x.png 252w, https://s.w.org/images/core/4.4/twenty-sixteen-red-fullsize-1x.png 410w, https://s.w.org/images/core/4.4/twenty-sixteen-red-desktop-2x.png 504w, https://s.w.org/images/core/4.4/twenty-sixteen-red-fullsize-2x.png 820w" sizes="(max-width: 500px) calc((100vw - 40px) * .32), (max-width: 782px) calc((100vw - 70px) * .32), (max-width: 960px) calc((100vw - 116px) * .24), (max-width: 1290px) calc((100vw - 240px) * .24), 252px" />
-				</div>
+			<div class="col">
+				<img src="https://cldup.com/TE-OBMWHkX.png" />
+				<h3><?php _e( 'Formatting Shortcuts' ); ?></h3>
+				<p><?php _e( 'Do you enjoy using formatting shortcuts for lists and headings? Now they&#8217;re even more useful, with horizontal lines and <code>&lt;code&gt;</code>.' ); ?></p>
 			</div>
 		</div>
 
 		<hr />
 
 		<div class="feature-section two-col">
+			<h2><?php _e( 'Customization Improvements' ); ?></h2>
 			<div class="col">
-				<div class="media-container">
-					<img src="https://s.w.org/images/core/4.4/responsive-devices-fullsize-2x.png" alt="" srcset="https://s.w.org/images/core/4.4/responsive-devices-smartphone-1x.png 335w, https://s.w.org/images/core/4.4/responsive-devices-desktop-1x.png 500w, https://s.w.org/images/core/4.4/responsive-devices-smartphone-2x.png 670w, https://s.w.org/images/core/4.4/responsive-devices-tablet-1x.png 698w, https://s.w.org/images/core/4.4/responsive-devices-desktop-2x.png 1000w, https://s.w.org/images/core/4.4/responsive-devices-fullsize-1x.png 1200w, https://s.w.org/images/core/4.4/responsive-devices-tablet-2x.png 1396w, https://s.w.org/images/core/4.4/responsive-devices-fullsize-2x.png 2400w" sizes="(max-width: 500px) calc(100vw - 40px), (max-width: 782px) calc(100vw - 70px), (max-width: 960px) calc((100vw - 116px) * .476), (max-width: 1290px) calc((100vw - 240px) * .476), 500px" />
-				</div>
+				<img src="https://cldup.com/0iRJNVbt4G.png" />
+				<h3><?php _e( 'Live Responsive Previews' ); ?></h3>
+				<?php
+				if ( current_user_can( 'customize' ) ) {
+					$customize_url = esc_url( admin_url( 'customize.php' ) );
+					$customizer_link = sprintf( __( '<a href="%1$s" class="load-customize">%2$s</a>' ), $customize_url, __( 'customizer' ) );
+				} else {
+					$customizer_link = __( 'customizer' );
+				}
+				?>
+				<p><?php printf( __( 'Make sure your site looks great on all screens! Preview mobile, tablet, and desktop views directly in the %s.' ), $customizer_link ); ?></p>
 			</div>
 			<div class="col">
-				<h3><?php _e( 'Responsive images' ); ?></h3>
-				<p><?php _e( 'WordPress now takes a smarter approach to displaying appropriate image sizes on any device, ensuring a perfect fit every time. You don&#8217;t need to do anything to your theme, it just works.' ); ?></p>
-			</div>
-		</div>
-
-		<hr />
-
-		<div class="feature-section two-col">
-			<div class="col">
-				<div class="embed-container">
-					<blockquote data-secret="OcUe7B6Edh" class="wp-embedded-content"><a href="https://wordpress.org/news/2015/12/clifford/">WordPress 4.4 &ldquo;Clifford&rdquo;</a></blockquote><iframe class="wp-embedded-content" sandbox="allow-scripts" security="restricted" style="display:none;" src="https://wordpress.org/news/2015/12/clifford/embed/#?secret=OcUe7B6Edh" data-secret="OcUe7B6Edh" width="600" height="338" title="<?php esc_attr_e( 'Embedded WordPress Post' ); ?>" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
-				</div>
-			</div>
-			<div class="col">
-				<h3><?php _e( 'Embed your WordPress content' ); ?></h3>
-				<p><?php _e( 'Now you can embed your posts on other sites, even other WordPress sites. Simply drop a post URL into the editor and see an instant embed preview, complete with the title, excerpt, and featured image if you&#8217;ve set one. We&#8217;ll even include your site icon and links for comments and sharing.' ); ?></p>
-			</div>
-		</div>
-
-		<hr />
-
-		<div class="feature-section two-col">
-			<div class="col">
-				<div class="embed-container embed-reverbnation">
-					<iframe width="640" height="150" scrolling="no" frameborder="no" src="https://www.reverbnation.com/widget_code/html_widget/artist_607?widget_id=55&amp;pwc[song_ids]=3731874&amp;pwc[size]=small"></iframe>
-				</div>
-			</div>
-			<div class="col">
-				<h3><?php _e( 'Even more embed providers' ); ?></h3>
-				<p><?php _e( 'In addition to post embeds, WordPress 4.4 also adds support for five new oEmbed providers: Cloudup, Reddit&nbsp;Comments, ReverbNation, Speaker&nbsp;Deck, and VideoPress.' ); ?></p>
+				<img src="https://cldup.com/HWDA8xR_8G.png" />
+				<h3><?php _e( 'Custom Logos' ); ?></h3>
+				<p><?php _e( 'Themes can now support logos for your business or brand.' ); ?>
+					<?php
+					if ( current_theme_supports( 'custom-logo' ) && current_user_can( 'customize' ) ) {
+						printf( __( 'Your theme supports custom logos! Try it out right now in the <a href="%s" class="load-customize">Site Identity</a> section of the customizer.' ), add_query_arg( 'autofocus[section]', 'title_tagline', $customize_url ) );
+					} else {
+						_e( 'The Twenty Fifteen and Twenty Sixteen themes have been updated to support custom logos, which can be found in the Site Identity section of the Customizer.' );
+					}
+					?></p>
 			</div>
 		</div>
 
@@ -124,62 +108,38 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 		<div class="changelog">
 			<h3><?php _e( 'Under the Hood' ); ?></h3>
 
-			<div class="feature-section under-the-hood one-col">
+			<div class="under-the-hood three-col">
 				<div class="col">
-					<h4><?php _e( 'REST API infrastructure' ); ?></h4>
-					<div class="two-col-text">
-						<p><?php _e( 'Infrastructure for the REST API has been integrated into core, marking a new era in developing with WordPress. The REST API serves to provide developers with a path forward for building and extending RESTful APIs on top of WordPress.' ); ?></p>
-						<p><?php
-							if ( current_user_can( 'install_plugins' ) ) {
-								$url_args = array(
-									'tab'       => 'plugin-information',
-									'plugin'    => 'rest-api',
-									'TB_iframe' => true,
-									'width'     => 600,
-									'height'    => 550
-								);
-
-								$plugin_link = '<a href="' . esc_url( add_query_arg( $url_args, network_admin_url( 'plugin-install.php' ) ) ) . '" class="thickbox open-plugin-details-modal">WordPress REST API</a>';
-							} else {
-								$plugin_link = '<a href="https://wordpress.org/plugins/rest-api">WordPress REST API</a>';
+					<h4><?php _e( 'Smart Image Resizing' ); ?></h4>
+					<p><?php printf( __( 'Generated images now load up to 50&#37; faster with no noticeable quality loss. <a href="%s">It&#8217;s really cool</a>.' ), 'https://make.wordpress.org/core/2016/03/12/performance-improvements-for-images-in-wordpress-4-5/' ); ?></p>
+				</div>
+				<div class="col">
+					<h4><?php _e( 'Selective Refresh' ); ?></h4>
+					<p><?php 
+						printf( __( 'The customizer now supports a <a href="%s">comprehensive framework</a> for rendering parts of the preview without rewriting your PHP code in JavaScript.' ), 'https://make.wordpress.org/core/2016/02/16/selective-refresh-in-the-customizer/' );
+						if ( current_user_can( 'customize' ) && current_user_can( 'edit_theme_options' ) ) {
+							if ( current_theme_supports( 'menus' ) && ! current_theme_supports( 'customize-selective-refresh-widgets' ) ) {
+								printf( ' ' . __( 'See it in action with <a href="%1$s" class="load-customize">Menus</a>.' ), add_query_arg( 'autofocus[panel]', 'nav_menus', $customize_url ) );
+							} elseif ( current_theme_supports( 'customize-selective-refresh-widgets' ) ) { // If widgets are supported, menus are also because of the menus widget.
+								printf( ' ' . __( 'See it in action with <a href="%1$s" class="load-customize">Menus</a> or <a href="%2$s" class="load-customize">Widgets</a>.' ), add_query_arg( 'autofocus[panel]', 'nav_menus', $customize_url ), add_query_arg( 'autofocus[panel]', 'widgets', $customize_url ) );
 							}
-
-							/* translators: WordPress REST API plugin link */
-							printf( __( 'Infrastructure is the first part of a multi-stage rollout for the REST API. Inclusion of core endpoints is targeted for an upcoming release. To get a sneak peek of the core endpoints, and for more information on extending the REST API, check out the official %s plugin.' ), $plugin_link );
-						?></p>
-					</div>
+						}
+					?></p>
+				</div>
+				<div class="col">
+					<h4><?php _e( 'JavaScript Library Updates' ); ?></h4>
+					<p><?php _e( 'jQuery 1.12.3, jQuery Migrate 1.4.0, Backbone 1.2.3, and Underscore 1.8.3 are bundled.' ); ?></p>
 				</div>
 			</div>
 
-			<div class="feature-section under-the-hood three-col">
+			<div class="under-the-hood two-col">
 				<div class="col">
-					<h4><?php _e( 'Term meta' ); ?></h4>
-					<p><?php
-						/* translators: 1: add_term_meta() docs link, 2: get_term_meta() docs link, 3: update_term_meta() docs link */
-						printf( __( 'Terms now support metadata, just like posts. See %1$s, %2$s, and %3$s for more information.' ),
-							'<a href="https://developer.wordpress.org/reference/functions/add_term_meta"><code>add_term_meta()</code></a>',
-							'<a href="https://developer.wordpress.org/reference/functions/get_term_meta"><code>get_term_meta()</code></a>',
-							'<a href="https://developer.wordpress.org/reference/functions/update_term_meta"><code>update_term_meta()</code></a>'
-				         );
-					?></p>
+					<h4><?php _e( 'Script Loader Improvements' ); ?></h4>
+					<p><?php printf( __( 'Better support has been added for script header/footer dependencies. New <code><a href="%s">wp_add_inline_script()</a></code> enables adding extra code to registered scripts.' ), 'https://make.wordpress.org/core/2016/03/08/enhanced-script-loader-in-wordpress-4-5/' ); ?></p>
 				</div>
 				<div class="col">
-					<h4><?php _e( 'Comment query improvements' ); ?></h4>
-					<p><?php
-						/* translators: WP_Comment_Query class name */
-						printf( __( 'Comment queries now have cache handling to improve performance. New arguments in %s make crafting robust comment queries simpler.' ), '<code>WP_Comment_Query</code>' );
-					?></p>
-				</div>
-				<div class="col">
-					<h4><?php _e( 'Term, comment, and network objects' ); ?></h4>
-					<p><?php
-						/* translators: 1: WP_Term class name, WP_Comment class name, WP_Network class name */
-						printf( __( 'New %1$s, %2$s, and %3$s objects make interacting with terms, comments, and networks more predictable and intuitive in code.' ),
-							'<code>WP_Term</code>',
-							'<code>WP_Comment</code>',
-							'<code>WP_Network</code>'
-						);
-					?></p>
+					<h4><?php _e( 'Better Embed Templates' ); ?></h4>
+					<p><?php printf( __( 'Embed templates have been split into parts and can be <a href="%s">directly overridden by themes</a> via the template hierarchy.' ), 'https://make.wordpress.org/core/2016/03/11/embeds-changes-in-wordpress-4-5/' ); ?></p>
 				</div>
 			</div>
 
