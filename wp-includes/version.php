59d0cd77dd996833e2c75e769b1eb7a79d907be0
Mike Schroder <mschrode@umich.edu>
2016-04-12 17:29:26 +0000

    WordPress 4.5 Built from https://develop.svn.wordpress.org/trunk@37179
    git-svn-id: http://core.svn.wordpress.org/trunk@37146 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-RC3-37178';
+$wp_version = '4.5';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
