5cbdf22c0fd021c5f6253e314843e03bf5334c87
Pascal Birchler <pascal.birchler@gmail.com>
2016-04-17 11:09:28 +0000

    TinyMCE, inline link: Fix VoiceOver in Safari for search suggestions.
    Props afercia.
    Fixes #36458.
    Built from https://develop.svn.wordpress.org/trunk@37228
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37194 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/tinymce/plugins/wplink/plugin.js
+++ wp-includes/js/tinymce/plugins/wplink/plugin.js
@@ -440,10 +440,26 @@
 							$input.autocomplete( 'search' );
 						}
 					} )
+					// Returns a jQuery object containing the menu element.
 					.autocomplete( 'widget' )
 						.addClass( 'wplink-autocomplete' )
 						.attr( 'role', 'listbox' )
-						.removeAttr( 'tabindex' ); // Remove the `tabindex=0` attribute added by jQuery UI.
+						.removeAttr( 'tabindex' ) // Remove the `tabindex=0` attribute added by jQuery UI.
+						/*
+						 * Looks like Safari and VoiceOver need an `aria-selected` attribute. See ticket #33301.
+						 * The `menufocus` and `menublur` events are the same events used to add and remove
+						 * the `ui-state-focus` CSS class on the menu items. See jQuery UI Menu Widget.
+						 */
+						.on( 'menufocus', function( event, ui ) {
+							ui.item.attr( 'aria-selected', 'true' );
+						})
+						.on( 'menublur', function() {
+							/*
+							 * The `menublur` event returns an object where the item is `null`
+							 * so we need to find the active item with other means.
+							 */
+							$( this ).find( '[aria-selected="true"]' ).removeAttr( 'aria-selected' );
+						});
 				}
 
 				tinymce.$( input ).on( 'keydown', function( event ) {
