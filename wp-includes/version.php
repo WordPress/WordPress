0e2978a5cda25783c55f6b03a80f02ba577522ce
Boone Gorges <boonebgorges@gmail.com>
2016-04-17 03:13:27 +0000

    Tests: Improvements to /post/query test class.
    * Add `@group` annotation.
    * Remove unnecessary `setUp()` method.
    Built from https://develop.svn.wordpress.org/trunk@37224
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37190 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37223';
+$wp_version = '4.6-alpha-37224';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
