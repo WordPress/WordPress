6040109de607dfbdeac59f0c4a45a99edf8212dd
Jeremy Felt <jeremy.felt@gmail.com>
2016-04-18 01:29:26 +0000

    Tests: Improve `get_blog_count()` tests
    * Use `wp_update_network_counts()` to update the database with the most current data before testing.
    * Use `wp_update_network_counts()` to update the database with the most current data after deleting the sites created during the test.
    * Create only 1 extra site in each test rather than 4. This shaves several seconds off the test time.
    * Stop testing for an extra count now that we update the network counts properly. Previously we looked at `$site_count_start + 9` rather than 8. Now this is `+ 1`, which aligns with the actual number of sites created.
    * Test 3 explicit conditions - default, filter applied as `true`, and filter applied as `false`.
    * Reset data before testing assertion to avoid a suspended state.
    
    See #36566.
    
    Built from https://develop.svn.wordpress.org/trunk@37233
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37199 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37232';
+$wp_version = '4.6-alpha-37233';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
