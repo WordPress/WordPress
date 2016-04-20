ddd3daf5adb87b1233cdf9f3c1cea337dc1421dd
Drew Jaynes <info@drewapicture.com>
2016-04-20 15:47:26 +0000

    Docs: Clarify return descriptions in the DocBlocks for `set_user_setting()` and `delete_user_setting()`.
    See [32613]. See #30989.
    
    Built from https://develop.svn.wordpress.org/trunk@37264
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37230 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/option.php
+++ wp-includes/option.php
@@ -844,13 +844,14 @@ function get_user_setting( $name, $default = false ) {
  * Add or update user interface setting.
  *
  * Both $name and $value can contain only ASCII letters, numbers and underscores.
+ *
  * This function has to be used before any output has started as it calls setcookie().
  *
  * @since 2.8.0
  *
  * @param string $name  The name of the setting.
  * @param string $value The value for the setting.
- * @return bool|void true if set successfully/false if not.
+ * @return bool|null True if set successfully, false if not. Null if the current user can't be established.
  */
 function set_user_setting( $name, $value ) {
 	if ( headers_sent() ) {
@@ -867,12 +868,13 @@ function set_user_setting( $name, $value ) {
  * Delete user interface settings.
  *
  * Deleting settings would reset them to the defaults.
+ *
  * This function has to be used before any output has started as it calls setcookie().
  *
  * @since 2.7.0
  *
  * @param string $names The name or array of names of the setting to be deleted.
- * @return bool|void true if deleted successfully/false if not.
+ * @return bool|null True if deleted successfully, false if not. Null if the current user can't be established.
  */
 function delete_user_setting( $names ) {
 	if ( headers_sent() ) {
