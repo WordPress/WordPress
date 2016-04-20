14a399b69ad9d3cab26ffee8e356725956e014b1
Drew Jaynes <info@drewapicture.com>
2016-04-20 15:45:29 +0000

    Docs: Clarify parameter and return descriptions in the DocBlock for `wp_set_all_user_settings()`.
    See [32613]. See #30989.
    
    Built from https://develop.svn.wordpress.org/trunk@37263
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37229 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/option.php
+++ wp-includes/option.php
@@ -941,11 +941,13 @@ function get_all_user_settings() {
  * Private. Set all user interface settings.
  *
  * @since 2.8.0
+ * @access private
  *
  * @global array $_updated_user_settings
  *
- * @param array $user_settings
- * @return bool|void
+ * @param array $user_settings User settings.
+ * @return bool|null False if the current user can't be found, null if the current
+ *                   user is not a super admin or a member of the site, otherwise true.
  */
 function wp_set_all_user_settings( $user_settings ) {
 	global $_updated_user_settings;
