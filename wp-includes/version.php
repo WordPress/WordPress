7c5f77ece3329d5c48a5b0d19eec79ec87144e0c
Drew Jaynes <info@drewapicture.com>
2016-04-21 02:56:26 +0000

    Tests: Pre-declare the `$year_url` property before initialization in `Tests_Get_Archives::setUp()`.
    Props pbearne.
    Fixes #36611.
    
    Built from https://develop.svn.wordpress.org/trunk@37271
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37237 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37270';
+$wp_version = '4.6-alpha-37271';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
