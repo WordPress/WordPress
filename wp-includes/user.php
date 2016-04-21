31b896d1aaf6cf942d1e8208ee1676e95c4d1dc0
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2016-04-21 12:01:27 +0000

    Docs: Reflect the new `'user'` option for `wp_new_user_notification()`'s `$notify` parameter added in [37276] in `wp_send_new_user_notifications()` docs as well.
    Props ocean90.
    Fixes #36009.
    Built from https://develop.svn.wordpress.org/trunk@37278
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37244 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -2310,10 +2310,11 @@ function register_new_user( $user_login, $user_email ) {
  * Notifications are sent both to the site admin and to the newly created user.
  *
  * @since 4.4.0
+ * @since 4.6.0 The `$notify` parameter accepts 'user' for sending notification only to the user created.
  *
  * @param int    $user_id ID of the newly created user.
  * @param string $notify  Optional. Type of notification that should happen. Accepts 'admin' or an empty string
- *                        (admin only), or 'both' (admin and user). Default 'both'.
+ *                        (admin only), 'user', or 'both' (admin and user). Default 'both'.
  */
 function wp_send_new_user_notifications( $user_id, $notify = 'both' ) {
 	wp_new_user_notification( $user_id, null, $notify );
