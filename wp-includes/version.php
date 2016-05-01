9354965638d65d6a4472ac9bf5f129faf37147d7
Dominik Schilling <dominikschilling+git@gmail.com>
2016-05-01 20:29:27 +0000

    Customize: Increase the target size of the expand/collapse button in the customizer.
    To improve accessibility on touch devices increase the size of the button in the collapsed view from 24×28 to 44×45 pixel.
    
    Props celloexpressions.
    Fixes #36093.
    Built from https://develop.svn.wordpress.org/trunk@37341
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37307 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37340';
+$wp_version = '4.6-alpha-37341';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
