5f62db2f21b64b4ee671261306c5e93ff05dfced
Drew Jaynes <info@drewapicture.com>
2016-05-02 20:04:28 +0000

    Comments: Realign parameter documentation in the DocBlocks for `comment_author_email_link()` and `get_comment_author_email_link()` following [37348].
    Also adds a missing return description for `get_comment_author_email_link()`.
    
    Props flixos90 for the initial patch.
    Fixes #36571.
    
    Built from https://develop.svn.wordpress.org/trunk@37349
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37315 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment-template.php
+++ wp-includes/comment-template.php
@@ -140,11 +140,11 @@ function comment_author_email( $comment_ID = 0 ) {
  * @since 0.71
  * @since 4.6.0 The `$comment` parameter was added.
  *
- * @param string $linktext Optional. Text to display instead of the comment author's email address.
- *                         Default empty.
- * @param string $before   Optional. Text or HTML to display before the email link. Default empty.
- * @param string $after    Optional. Text or HTML to display after the email link. Default empty.
- * @param int|WP_Comment $comment Optional. Comment ID or WP_Comment object. Default is the current comment.
+ * @param string         $linktext Optional. Text to display instead of the comment author's email address.
+ *                                 Default empty.
+ * @param string         $before   Optional. Text or HTML to display before the email link. Default empty.
+ * @param string         $after    Optional. Text or HTML to display after the email link. Default empty.
+ * @param int|WP_Comment $comment  Optional. Comment ID or WP_Comment object. Default is the current comment.
  */
 function comment_author_email_link( $linktext = '', $before = '', $after = '', $comment = null ) {
 	if ( $link = get_comment_author_email_link( $linktext, $before, $after, $comment ) ) {
@@ -164,12 +164,13 @@ function comment_author_email_link( $linktext = '', $before = '', $after = '', $
  * @since 2.7.0
  * @since 4.6.0 The `$comment` parameter was added.
  *
- * @param string $linktext Optional. Text to display instead of the comment author's email address.
- *                         Default empty.
- * @param string $before   Optional. Text or HTML to display before the email link. Default empty.
- * @param string $after    Optional. Text or HTML to display after the email link. Default empty.
- * @param int|WP_Comment $comment Optional. Comment ID or WP_Comment object. Default is the current comment.
- * @return string
+ * @param string         $linktext Optional. Text to display instead of the comment author's email address.
+ *                                 Default empty.
+ * @param string         $before   Optional. Text or HTML to display before the email link. Default empty.
+ * @param string         $after    Optional. Text or HTML to display after the email link. Default empty.
+ * @param int|WP_Comment $comment  Optional. Comment ID or WP_Comment object. Default is the current comment.
+ * @return string HTML markup for the comment author email link. By default, the email address is obfuscated
+ *                via the {@see 'comment_email'} filter with antispambot().
  */
 function get_comment_author_email_link( $linktext = '', $before = '', $after = '', $comment = null ) {
 	$comment = get_comment( $comment );
