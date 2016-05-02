f3a90cd1a6caea9f757ece7732b5323b7182b6e7
Drew Jaynes <info@drewapicture.com>
2016-05-02 16:24:29 +0000

    Tests: Remove debug cruft left over from [34816].
    See #17078.
    
    Built from https://develop.svn.wordpress.org/trunk@37344
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37310 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37343';
+$wp_version = '4.6-alpha-37344';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
