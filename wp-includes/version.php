096d0ccd639b7f0baefb1926d9f66b04d27550e4
Dominik Schilling <dominikschilling+git@gmail.com>
2016-05-09 19:17:29 +0000

    Tests: Don't create unused users in `Tests_Ajax_DeleteComment`.
    Fixes #36616.
    Built from https://develop.svn.wordpress.org/trunk@37404
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37370 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37403';
+$wp_version = '4.6-alpha-37404';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
