6af8a5d48cafa13eb0a151a98c4c4ccd409b8270
Aaron Jorbin <aaron@jorb.in>
2016-05-17 20:23:28 +0000

    Set a whitelist for PHPUnit code coverage.
    PHPUnit requires a whitelist as of version 5 in order to generate code coverage. See https://github.com/sebastianbergmann/phpunit-documentation/issues/335 .
    
    Fixes #36867.
    
    
    Built from https://develop.svn.wordpress.org/trunk@37449
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37417 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37448';
+$wp_version = '4.6-alpha-37449';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
