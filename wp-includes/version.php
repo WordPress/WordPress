4a57c6bace8f029ca0560a0d3d8577a3bfbc052f
Dominik Schilling <dominikschilling+git@gmail.com>
2016-05-17 20:40:29 +0000

    Media: Remove medium_large size from `$whitelist_options['media']` in `options.php`.
    The `medium_large_size_w` and `medium_large_size_h` options were added to the
    `$whitelist_options['media']` in options.php in [35479], which causes both options
    to be set to `0` when the media settings are changed. When this occurs, the
    medium_large image size will no longer be created when an image is uploaded.
    
    Since these options aren't meant to be updated via the settings UI, they can be
    safely removed from the whitelist.
    
    Merge of [37435] to the 4.5 branch.
    
    Props joemcgill.
    Fixes #36531.
    Built from https://develop.svn.wordpress.org/branches/4.5@37457
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.5@37425 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5.3-alpha-37455';
+$wp_version = '4.5.3-alpha-37457';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
