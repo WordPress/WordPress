53acd1636da0434cfc7eabf2b97cd1cd62be8987
Dominik Schilling <dominikschilling+git@gmail.com>
2016-05-17 20:34:30 +0000

    Customize: Handle filtering `sidebars_widgets` when the underlying option is non-existent.
    Fixes warning related to a non-array argument for `array_merge()` in `WP_Customize_Widgets::customize_register()`. 
    
    Merge of [37352] to the 4.5 branch.
    
    See [37166].
    See #36389.
    Fixes #36660.
    Built from https://develop.svn.wordpress.org/branches/4.5@37453
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.5@37421 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5.3-alpha-37451';
+$wp_version = '4.5.3-alpha-37453';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
