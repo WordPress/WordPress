6e72bc4ec568f1a1d9c402ca790a272085591fe4
Ella Iseulde Van Dorpe <*@iseulde.com>
2016-05-17 18:49:30 +0000

    TinyMCE: use new non-editable implementation
    Use TinyMCE's implementation of `contenteditable="false"` for views.
    
    First pass.
    
    See #36434.
    
    
    Built from https://develop.svn.wordpress.org/trunk@37446
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37414 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37444';
+$wp_version = '4.6-alpha-37446';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
