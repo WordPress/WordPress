0016e4bb122a8da9b70d2703f805ba5a9821eadd
Drew Jaynes <info@drewapicture.com>
2016-05-18 22:07:28 +0000

    Docs: Fix an incorrect Codex link in the file header for wp-includes/cache.php.
    Introduced in [7990].
    
    Props michaelbeil.
    See #36880.
    
    Built from https://develop.svn.wordpress.org/trunk@37460
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37428 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/cache.php
+++ wp-includes/cache.php
@@ -2,7 +2,7 @@
 /**
  * Object Cache API
  *
- * @link https://codex.wordpress.org/Function_Reference/WP_Cache
+ * @link https://codex.wordpress.org/Class_Reference/WP_Object_Cache
  *
  * @package WordPress
  * @subpackage Cache
