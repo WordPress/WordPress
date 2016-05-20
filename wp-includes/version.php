b777ce5b89287cf802ec798f244a0df88251ec84
Gary Pendergast <gary@pento.net>
2016-05-20 05:30:27 +0000

    Tests: Fix an incorrect `@ticket` header included in [37469].
    See #36122.
    
    
    Built from https://develop.svn.wordpress.org/trunk@37470
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37438 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37469';
+$wp_version = '4.6-alpha-37470';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
