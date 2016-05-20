720e2af2903fa758600857f48bcf4dc59dc97842
Boone Gorges <boonebgorges@gmail.com>
2016-05-20 19:21:30 +0000

    During password reset, user-submitted login/email should be stripslashed.
    This prevents errors when an email address contains an apostrophe. See [29966]
    for similar treatment of a related problem.
    
    Props dcavins.
    Fixes #36322.
    Built from https://develop.svn.wordpress.org/trunk@37474
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37442 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37473';
+$wp_version = '4.6-alpha-37474';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
--- wp-login.php
+++ wp-login.php
@@ -294,7 +294,7 @@ function retrieve_password() {
 	if ( empty( $_POST['user_login'] ) ) {
 		$errors->add('empty_username', __('<strong>ERROR</strong>: Enter a username or email address.'));
 	} elseif ( strpos( $_POST['user_login'], '@' ) ) {
-		$user_data = get_user_by( 'email', trim( $_POST['user_login'] ) );
+		$user_data = get_user_by( 'email', trim( wp_unslash( $_POST['user_login'] ) ) );
 		if ( empty( $user_data ) )
 			$errors->add('invalid_email', __('<strong>ERROR</strong>: There is no user registered with that email address.'));
 	} else {
