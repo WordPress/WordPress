b7fc55237d42d788b665025508e57b6c3dab4690
Drew Jaynes <info@drewapicture.com>
2016-05-22 18:24:28 +0000

    Docs: Standardize filter docs in wp-includes/bookmark-template.php to use third-person singular verbs per the inline documentation standards for PHP.
    See #36913.
    
    Built from https://develop.svn.wordpress.org/trunk@37494
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37462 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37493';
+$wp_version = '4.6-alpha-37494';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
