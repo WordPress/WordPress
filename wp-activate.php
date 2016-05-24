d23b1534b80b3b6f62439e0e9844533d0f53b10c
Dominik Schilling <dominikschilling+git@gmail.com>
2016-05-24 21:02:28 +0000

    I18N: Add translators comments to wp-activate.php.
    Props ramiy.
    Fixes #35734.
    Built from https://develop.svn.wordpress.org/trunk@37552
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37520 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-activate.php
+++ wp-activate.php
@@ -92,22 +92,37 @@ get_header( 'wp-activate' );
 		$result = wpmu_activate_signup( $key );
 		if ( is_wp_error($result) ) {
 			if ( 'already_active' == $result->get_error_code() || 'blog_taken' == $result->get_error_code() ) {
-			    $signup = $result->get_error_data();
+				$signup = $result->get_error_data();
 				?>
 				<h2><?php _e('Your account is now active!'); ?></h2>
 				<?php
 				echo '<p class="lead-in">';
 				if ( $signup->domain . $signup->path == '' ) {
-					printf( __('Your account has been activated. You may now <a href="%1$s">log in</a> to the site using your chosen username of &#8220;%2$s&#8221;. Please check your email inbox at %3$s for your password and login instructions. If you do not receive an email, please check your junk or spam folder. If you still do not receive an email within an hour, you can <a href="%4$s">reset your password</a>.'), network_site_url( 'wp-login.php', 'login' ), $signup->user_login, $signup->user_email, wp_lostpassword_url() );
+					printf(
+						/* translators: 1: login URL, 2: username, 3: user email, 4: lost password URL */
+						__( 'Your account has been activated. You may now <a href="%1$s">log in</a> to the site using your chosen username of &#8220;%2$s&#8221;. Please check your email inbox at %3$s for your password and login instructions. If you do not receive an email, please check your junk or spam folder. If you still do not receive an email within an hour, you can <a href="%4$s">reset your password</a>.' ),
+						network_site_url( 'wp-login.php', 'login' ),
+						$signup->user_login,
+						$signup->user_email,
+						wp_lostpassword_url()
+					);
 				} else {
-					printf( __('Your site at <a href="%1$s">%2$s</a> is active. You may now log in to your site using your chosen username of &#8220;%3$s&#8221;. Please check your email inbox at %4$s for your password and login instructions. If you do not receive an email, please check your junk or spam folder. If you still do not receive an email within an hour, you can <a href="%5$s">reset your password</a>.'), 'http://' . $signup->domain, $signup->domain, $signup->user_login, $signup->user_email, wp_lostpassword_url() );
+					printf(
+						/* translators: 1: site URL, 2: site domain, 3: username, 4: user email, 5: lost password URL */
+						__( 'Your site at <a href="%1$s">%2$s</a> is active. You may now log in to your site using your chosen username of &#8220;%3$s&#8221;. Please check your email inbox at %4$s for your password and login instructions. If you do not receive an email, please check your junk or spam folder. If you still do not receive an email within an hour, you can <a href="%5$s">reset your password</a>.' ),
+						'http://' . $signup->domain,
+						$signup->domain,
+						$signup->user_login,
+						$signup->user_email,
+						wp_lostpassword_url()
+					);
 				}
 				echo '</p>';
 			} else {
 				?>
-				<h2><?php _e('An error occurred during the activation'); ?></h2>
+				<h2><?php _e( 'An error occurred during the activation' ); ?></h2>
+				<p><?php echo $result->get_error_message(); ?></p>
 				<?php
-			    echo '<p>'.$result->get_error_message().'</p>';
 			}
 		} else {
 			$url = isset( $result['blog_id'] ) ? get_blogaddress_by_id( (int) $result['blog_id'] ) : '';
@@ -125,9 +140,15 @@ get_header( 'wp-activate' );
 				$login_url = wp_login_url();
 				restore_current_blog();
 				?>
-				<p class="view"><?php printf( __( 'Your account is now activated. <a href="%1$s">View your site</a> or <a href="%2$s">Log in</a>' ), $url, esc_url( $login_url ) ); ?></p>
+				<p class="view"><?php
+					/* translators: 1: site URL, 2: login URL */
+					printf( __( 'Your account is now activated. <a href="%1$s">View your site</a> or <a href="%2$s">Log in</a>' ), $url, esc_url( $login_url ) );
+				?></p>
 			<?php else: ?>
-				<p class="view"><?php printf( __('Your account is now activated. <a href="%1$s">Log in</a> or go back to the <a href="%2$s">homepage</a>.' ), network_site_url('wp-login.php', 'login'), network_home_url() ); ?></p>
+				<p class="view"><?php
+					/* translators: 1: login URL, 2: network home URL */
+					printf( __( 'Your account is now activated. <a href="%1$s">Log in</a> or go back to the <a href="%2$s">homepage</a>.' ), network_site_url( 'wp-login.php', 'login' ), network_home_url() );
+				?></p>
 			<?php endif;
 		}
 	}
