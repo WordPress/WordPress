a7c601b74f0674d52f124c7f5109dad7b7a4e195
Drew Jaynes <info@drewapicture.com>
2016-05-30 02:10:30 +0000

    Docs: Correct a duplicate filter comment for the `image_memory_limit` hook to point to the correct file.
    Props bobbingwide.
    See #36968.
    
    Built from https://develop.svn.wordpress.org/trunk@37591
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37559 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-image-editor-imagick.php
+++ wp-includes/class-wp-image-editor-imagick.php
@@ -137,7 +137,7 @@ class WP_Image_Editor_Imagick extends WP_Image_Editor {
 		if ( ! is_file( $this->file ) && ! preg_match( '|^https?://|', $this->file ) )
 			return new WP_Error( 'error_loading_image', __('File doesn&#8217;t exist?'), $this->file );
 
-		/** This filter is documented in wp-includes/class-wp-image-editor-imagick.php */
+		/** This filter is documented in wp-includes/class-wp-image-editor-gd.php */
 		// Even though Imagick uses less PHP memory than GD, set higher limit for users that have low PHP.ini limits
 		@ini_set( 'memory_limit', apply_filters( 'image_memory_limit', WP_MAX_MEMORY_LIMIT ) );
 
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37590';
+$wp_version = '4.6-alpha-37591';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
