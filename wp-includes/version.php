5415629ad276d11c6fb354c5af8a2fbfc5826f70
Drew Jaynes <info@drewapicture.com>
2016-06-06 15:18:31 +0000

    Nav Menus: Move the `Walker_Nav_Menu` class to its own file.
    The new class-walker-nav-menu.php file is loaded in nav-menu-template.php for backward compatibility purposes.
    
    Fixes #37035. See #33413.
    
    Built from https://develop.svn.wordpress.org/trunk@37640
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37608 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37639';
+$wp_version = '4.6-alpha-37640';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
