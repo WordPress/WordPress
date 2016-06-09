37a2c565e2667f9459f2d938aaf1396d83c9fb37
Jeremy Felt <jeremy.felt@gmail.com>
2016-06-09 00:28:29 +0000

    Tests: User a data provider for `wp_get_sites()` tests.
    * Convert existing tests into a data provider and clarify expectations.
    * Add shared test fixtures in preparation for future tests.
    
    This passes with the `wp_get_sites()` from 4.5 and the deprecated version in trunk.
    
    See #36566.
    
    Built from https://develop.svn.wordpress.org/trunk@37662
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37628 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37661';
+$wp_version = '4.6-alpha-37662';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
