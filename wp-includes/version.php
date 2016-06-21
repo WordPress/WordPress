01fbbebff4ceefdae1ba1fae77030563348a3b5a
Rachel Baker <rachel@rachelbaker.me>
2016-06-21 14:27:33 +0000

    Revisions: Change the capability needed to view revision diffs to `edit_post`.
    Merge of [37779] to the 4.5 branch.
    Built from https://develop.svn.wordpress.org/branches/4.5@37791
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.5@37756 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5.3-alpha-37783';
+$wp_version = '4.5.3-alpha-37791';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
