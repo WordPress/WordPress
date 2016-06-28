aeb8d2fbad60dc8a387d240ab3cca9c265e40940
Dominik Schilling <dominikschilling+git@gmail.com>
2016-06-28 11:53:28 +0000

    I18N: Move the `WP_Locale` class to its own file.
    The new `class-wp-locale.php` file is loaded in `locale.php` for backward compatibility purposes.
    
    See #26511.
    Fixes #37209.
    Built from https://develop.svn.wordpress.org/trunk@37889
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37830 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37888';
+$wp_version = '4.6-alpha-37889';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
