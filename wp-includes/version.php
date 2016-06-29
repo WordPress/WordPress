d83e08cdb10a53d068e57e70beb8100378e98461
Dominik Schilling <dominikschilling+git@gmail.com>
2016-06-29 13:14:27 +0000

    Tests: After [37861] move tests for deprecated filters into `filters.php`.
    See #10441.
    Built from https://develop.svn.wordpress.org/trunk@37909
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37850 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37908';
+$wp_version = '4.6-alpha-37909';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
