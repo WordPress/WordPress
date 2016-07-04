6edb48dd579af751d1df50fd9350d1003f331c45
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2016-07-04 14:34:27 +0000

    Docs: In `wp_list_authors()`, clarify that `include` and `exclude` arguments can also be an array.
    Fix duplicated `exclude` argument description.
    
    Props birgire.
    Fixes #37239.
    Built from https://develop.svn.wordpress.org/trunk@37949
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37890 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/author-template.php
+++ wp-includes/author-template.php
@@ -337,26 +337,26 @@ function get_author_posts_url( $author_id, $author_nicename = '' ) {
  * @param string|array $args {
  *     Optional. Array or string of default arguments.
  *
- *     @type string $orderby       How to sort the authors. Accepts 'nicename', 'email', 'url', 'registered',
- *                                 'user_nicename', 'user_email', 'user_url', 'user_registered', 'name',
- *                                 'display_name', 'post_count', 'ID', 'meta_value', 'user_login'. Default 'name'.
- *     @type string $order         Sorting direction for $orderby. Accepts 'ASC', 'DESC'. Default 'ASC'.
- *     @type int    $number        Maximum authors to return or display. Default empty (all authors).
- *     @type bool   $optioncount   Show the count in parenthesis next to the author's name. Default false.
- *     @type bool   $exclude_admin Whether to exclude the 'admin' account, if it exists. Default false.
- *     @type bool   $show_fullname Whether to show the author's full name. Default false.
- *     @type bool   $hide_empty    Whether to hide any authors with no posts. Default true.
- *     @type string $feed          If not empty, show a link to the author's feed and use this text as the alt
- *                                 parameter of the link. Default empty.
- *     @type string $feed_image    If not empty, show a link to the author's feed and use this image URL as
- *                                 clickable anchor. Default empty.
- *     @type string $feed_type     The feed type to link to, such as 'rss2'. Defaults to default feed type.
- *     @type bool   $echo          Whether to output the result or instead return it. Default true.
- *     @type string $style         If 'list', each author is wrapped in an `<li>` element, otherwise the authors
- *                                 will be separated by commas.
- *     @type bool   $html          Whether to list the items in HTML form or plaintext. Default true.
- *     @type string $exclude       An array, comma-, or space-separated list of author IDs to exclude. Default empty.
- *     @type string $exclude       An array, comma-, or space-separated list of author IDs to include. Default empty.
+ *     @type string       $orderby       How to sort the authors. Accepts 'nicename', 'email', 'url', 'registered',
+ *                                       'user_nicename', 'user_email', 'user_url', 'user_registered', 'name',
+ *                                       'display_name', 'post_count', 'ID', 'meta_value', 'user_login'. Default 'name'.
+ *     @type string       $order         Sorting direction for $orderby. Accepts 'ASC', 'DESC'. Default 'ASC'.
+ *     @type int          $number        Maximum authors to return or display. Default empty (all authors).
+ *     @type bool         $optioncount   Show the count in parenthesis next to the author's name. Default false.
+ *     @type bool         $exclude_admin Whether to exclude the 'admin' account, if it exists. Default false.
+ *     @type bool         $show_fullname Whether to show the author's full name. Default false.
+ *     @type bool         $hide_empty    Whether to hide any authors with no posts. Default true.
+ *     @type string       $feed          If not empty, show a link to the author's feed and use this text as the alt
+ *                                       parameter of the link. Default empty.
+ *     @type string       $feed_image    If not empty, show a link to the author's feed and use this image URL as
+ *                                       clickable anchor. Default empty.
+ *     @type string       $feed_type     The feed type to link to, such as 'rss2'. Defaults to default feed type.
+ *     @type bool         $echo          Whether to output the result or instead return it. Default true.
+ *     @type string       $style         If 'list', each author is wrapped in an `<li>` element, otherwise the authors
+ *                                       will be separated by commas.
+ *     @type bool         $html          Whether to list the items in HTML form or plaintext. Default true.
+ *     @type array|string $exclude       Array or comma/space-separated list of author IDs to exclude. Default empty.
+ *     @type array|string $include       Array or comma/space-separated list of author IDs to include. Default empty.
  * }
  * @return string|void The output, if echo is set to false.
  */
