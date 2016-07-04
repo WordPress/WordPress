a7bbdb23cc6594617fdcf2f816724b58334036f6
Dominik Schilling <dominikschilling+git@gmail.com>
2016-07-04 19:29:27 +0000

    Unit Tests: Remove `@group foo` annotation from `Tests_WP_Resource_Hints::test_dns_prefetch_styles()`. Built from https://develop.svn.wordpress.org/trunk@37951
    git-svn-id: http://core.svn.wordpress.org/trunk@37892 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-beta1-37950';
+$wp_version = '4.6-beta1-37951';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
