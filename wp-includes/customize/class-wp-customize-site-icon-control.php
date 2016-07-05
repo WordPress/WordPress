56e463a38eb9739e878aed4e9b86bda1db2b4224
Dominik Schilling <dominikschilling+git@gmail.com>
2016-07-05 12:42:27 +0000

    Customize: Fix site icon preview in RTL.
    * Set the direction of previews to LTR.
    * Remove the ID attribute from the favicon preview in the customizer panel. The preview in the media modal uses the same ID which prevented updating the correct favicon preview.
    
    Fixes #37286.
    Built from https://develop.svn.wordpress.org/trunk@37964
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37905 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/customize/class-wp-customize-site-icon-control.php
+++ wp-includes/customize/class-wp-customize-site-icon-control.php
@@ -67,7 +67,7 @@ class WP_Customize_Site_Icon_Control extends WP_Customize_Cropped_Image_Control 
 							<img src="<?php echo esc_url( admin_url( 'images/' . ( is_rtl() ? 'browser-rtl.png' : 'browser.png' ) ) ); ?>" class="browser-preview" width="182" alt="" />
 
 							<div class="favicon">
-								<img id="preview-favicon" src="{{ data.attachment.sizes.full.url }}" alt="<?php esc_attr_e( 'Preview as a browser icon' ); ?>"/>
+								<img src="{{ data.attachment.sizes.full.url }}" alt="<?php esc_attr_e( 'Preview as a browser icon' ); ?>"/>
 							</div>
 							<span class="browser-title" aria-hidden="true"><?php bloginfo( 'name' ); ?></span>
 						</div>
