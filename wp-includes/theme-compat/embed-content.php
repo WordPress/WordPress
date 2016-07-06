b9d99d3288633315d6bbf37bcd4a9bedbf98a176
Dominik Schilling <dominikschilling+git@gmail.com>
2016-07-06 17:08:31 +0000

    Embeds: Don't print the HTML for a featured image if a post has no featured image.
    See [36693].
    Fixes #37288.
    Built from https://develop.svn.wordpress.org/trunk@37988
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37929 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/theme-compat/embed-content.php
+++ wp-includes/theme-compat/embed-content.php
@@ -22,49 +22,51 @@
 			$thumbnail_id = get_the_ID();
 		}
 
-		$aspect_ratio = 1;
-		$measurements = array( 1, 1 );
-		$image_size   = 'full'; // Fallback.
+		if ( $thumbnail_id ) {
+			$aspect_ratio = 1;
+			$measurements = array( 1, 1 );
+			$image_size   = 'full'; // Fallback.
 
-		$meta = wp_get_attachment_metadata( $thumbnail_id );
-		if ( ! empty( $meta['sizes'] ) ) {
-			foreach ( $meta['sizes'] as $size => $data ) {
-				if ( $data['width'] / $data['height'] > $aspect_ratio ) {
-					$aspect_ratio = $data['width'] / $data['height'];
-					$measurements = array( $data['width'], $data['height'] );
-					$image_size   = $size;
+			$meta = wp_get_attachment_metadata( $thumbnail_id );
+			if ( ! empty( $meta['sizes'] ) ) {
+				foreach ( $meta['sizes'] as $size => $data ) {
+					if ( $data['width'] / $data['height'] > $aspect_ratio ) {
+						$aspect_ratio = $data['width'] / $data['height'];
+						$measurements = array( $data['width'], $data['height'] );
+						$image_size   = $size;
+					}
 				}
 			}
+
+			/**
+			 * Filters the thumbnail image size for use in the embed template.
+			 *
+			 * @since 4.4.0
+			 * @since 4.5.0 Added `$thumbnail_id` parameter.
+			 *
+			 * @param string $image_size   Thumbnail image size.
+			 * @param int    $thumbnail_id Attachment ID.
+			 */
+			$image_size = apply_filters( 'embed_thumbnail_image_size', $image_size, $thumbnail_id );
+
+			$shape = $measurements[0] / $measurements[1] >= 1.75 ? 'rectangular' : 'square';
+
+			/**
+			 * Filters the thumbnail shape for use in the embed template.
+			 *
+			 * Rectangular images are shown above the title while square images
+			 * are shown next to the content.
+			 *
+			 * @since 4.4.0
+			 * @since 4.5.0 Added `$thumbnail_id` parameter.
+			 *
+			 * @param string $shape        Thumbnail image shape. Either 'rectangular' or 'square'.
+			 * @param int    $thumbnail_id Attachment ID.
+			 */
+			$shape = apply_filters( 'embed_thumbnail_image_shape', $shape, $thumbnail_id );
 		}
 
-		/**
-		 * Filters the thumbnail image size for use in the embed template.
-		 *
-		 * @since 4.4.0
-		 * @since 4.5.0 Added `$thumbnail_id` parameter.
-		 *
-		 * @param string $image_size   Thumbnail image size.
-		 * @param int    $thumbnail_id Attachment ID.
-		 */
-		$image_size = apply_filters( 'embed_thumbnail_image_size', $image_size, $thumbnail_id );
-
-		$shape = $measurements[0] / $measurements[1] >= 1.75 ? 'rectangular' : 'square';
-
-		/**
-		 * Filters the thumbnail shape for use in the embed template.
-		 *
-		 * Rectangular images are shown above the title while square images
-		 * are shown next to the content.
-		 *
-		 * @since 4.4.0
-		 * @since 4.5.0 Added `$thumbnail_id` parameter.
-		 *
-		 * @param string $shape        Thumbnail image shape. Either 'rectangular' or 'square'.
-		 * @param int    $thumbnail_id Attachment ID.
-		 */
-		$shape = apply_filters( 'embed_thumbnail_image_shape', $shape, $thumbnail_id );
-
-		if ( 'rectangular' === $shape ) : ?>
+		if ( $thumbnail_id && 'rectangular' === $shape ) : ?>
 			<div class="wp-embed-featured-image rectangular">
 				<a href="<?php the_permalink(); ?>" target="_top">
 					<?php echo wp_get_attachment_image( $thumbnail_id, $image_size ); ?>
@@ -78,7 +80,7 @@
 			</a>
 		</p>
 
-		<?php if ( 'square' === $shape ) : ?>
+		<?php if ( $thumbnail_id && 'square' === $shape ) : ?>
 			<div class="wp-embed-featured-image square">
 				<a href="<?php the_permalink(); ?>" target="_top">
 					<?php echo wp_get_attachment_image( $thumbnail_id, $image_size ); ?>
