e3e4762b8f8252677e1292854f0213f70515f825
Drew Jaynes <info@drewapicture.com>
2016-07-13 16:04:29 +0000

    Docs: Update the default value for the optional `$args` parameter in `get_networks()` following [38042].
    See #32504.
    
    Built from https://develop.svn.wordpress.org/trunk@38055
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@37996 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-blogs.php
+++ wp-includes/ms-blogs.php
@@ -1069,8 +1069,8 @@ function get_last_updated( $deprecated = '', $start = 0, $quantity = 40 ) {
  *
  * @since 4.6.0
  *
- * @param string|array $args Optional. Array or string of arguments. See {@see WP_Network_Query::parse_query()}
- *                           for information on accepted arguments. Default empty.
+ * @param string|array $args Optional. Array or string of arguments. See WP_Network_Query::parse_query()
+ *                           for information on accepted arguments. Default empty array.
  * @return int|array List of networks or number of found networks if `$count` argument is true.
  */
 function get_networks( $args = array() ) {
