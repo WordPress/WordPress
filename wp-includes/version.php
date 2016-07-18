478f8c16dcc6e480b7765152f147a36b9a404a30
Dominik Schilling <dominikschilling+git@gmail.com>
2016-07-18 11:30:28 +0000

    Media: Update list of translatable strings for MediaElement.js.
    Fixes #37394.
    Built from https://develop.svn.wordpress.org/trunk@38089
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38030 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-beta3-38088';
+$wp_version = '4.6-beta3-38089';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
