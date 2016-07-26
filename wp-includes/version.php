8f6517c275ab27d1fc367c95c5a5b9de234b9d74
Helen Hou-Sandí <helen.y.hou@gmail.com>
2016-07-26 16:46:29 +0000

    System fonts: Adjust the smaller tabs so they appear as tabs.
    The tab effect comes from overlapping the border of the box below. Only applies to OSX; testing did not reveal adverse effects in other OSes.
    
    props ocean90.
    see #36753.
    
    Built from https://develop.svn.wordpress.org/trunk@38153
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38094 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-beta4-38152';
+$wp_version = '4.6-beta4-38153';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
