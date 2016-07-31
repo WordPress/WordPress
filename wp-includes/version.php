1abd5f0be71b4ab1b5c2120a2fe89617410555c7
Dominik Schilling <dominikschilling+git@gmail.com>
2016-07-31 18:11:29 +0000

    Plugins: Use `install_plugins_upload` action to print the upload form.
    Since [37221] the upload form is added to every plugin install screen via `install_plugins_upload()`. Previously the form was added through the `install_plugins_upload` (alias of `install_plugins_$tab`) action which allowed plugin authors to replace the form. This restores the previous behaviour.
    
    * Add the form only to non-upload plugin install screens.
    * Replace `install_plugins_upload()` with the `install_plugins_upload` and `install_plugins_pre_upload` actions.
    * Remove `$upload_tab_class` and add a CSS class for the current tab to `.wrap`
    * Adjust CSS selectors and toggle the whole container to support upload without an `upload-plugin` class.
    
    Props DavidAnderson, ocean90.
    Fixes #37495.
    Built from https://develop.svn.wordpress.org/trunk@38172
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38113 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-RC1-38171';
+$wp_version = '4.6-RC1-38172';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
