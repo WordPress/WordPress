672cf2766e041e70bfb511fd4d31a9c5c9d892a1
Dominik Schilling <dominikschilling+git@gmail.com>
2016-07-31 18:25:28 +0000

    Script Loader: Ignore deregistered dependencies in `wp_dependencies_unique_hosts()`.
    Prevents a PHP warning when a handle of a deregistered dependency is still in the queue.
    
    Fixes #37502.
    Built from https://develop.svn.wordpress.org/trunk@38174
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38115 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-RC1-38173';
+$wp_version = '4.6-RC1-38174';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
