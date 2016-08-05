4ff8257cb4f7e491388247b7fb177ec4e189b64a
Andrew Nacin <wp@andrewnacin.com>
2016-08-05 04:44:35 +0000

    MediaElement: Remove translated string from a plugin WP doesn't bundle.
    fixes #37394.
    
    Built from https://develop.svn.wordpress.org/trunk@38203
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38144 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38201';
+$wp_version = '4.7-alpha-38203';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
