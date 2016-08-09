210e23a63dca51b146892085cb270f9908a48de7
Gary Pendergast <gary@pento.net>
2016-08-09 11:39:36 +0000

    Multisite: Improve performance of the upgrade page on large networks.
    The query to select the next 5 blogs to upgrade was ordered by `registered`, which isn't indexed. This causes the query to table scan, which will be slow on networks with many blogs.
    
    The query only needs to be ordered by something that won't change, so ordering by `blog_id` is a good replacement. `blog_id` is indexed, and it's the only column being returned, so MySQL is able to optimize for a fast index read.
    
    Props fliespl.
    Fixes #37612.
    
    
    Built from https://develop.svn.wordpress.org/trunk@38229
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38170 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38227';
+$wp_version = '4.7-alpha-38229';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
