58e1032e13c746e51da0c0eb746677f3dfca295d
Aaron Jorbin <aaron@jorb.in>
2016-08-15 18:22:30 +0000

    External Libraries: Update Masonry shim to prevent error using isAnimated option
    Merges [38261] to the 4.6 branch.
    
    The isAnimated option throws an error and causes Masonry to stop functioning. Masonry.prototype.options is no longer defined in 3.3.2, this.options is and does the same. Masonry was updated to 3.3.2 in [37891].
    
    Reported upstream: https://github.com/desandro/masonry-v2-3-shim/pull/1
    
    Props stephenharris, ocean90, azaozz for testing and second sign off.
    Fixes #37666.
    See #32802.
    
    Built from https://develop.svn.wordpress.org/branches/4.6@38262
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.6@38203 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-RC3-38260';
+$wp_version = '4.6-RC3-38262';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
