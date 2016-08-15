75d4429926bfcd5667965f704169d01747e0a89c
Dominik Schilling <dominikschilling+git@gmail.com>
2016-08-15 19:16:31 +0000

    Post Thumbnails: Restore thumbnail support for media files.
    * Allow to add/remove a featured image to `attachment:audio` and `attachment:video` post types, see [27657].
    * Change conditionals to check for theme OR post type support.
    * Add tests for #12922.
    
    Broken in [37658].
    
    Props flixos90, joemcgill, DrewAPicture, wonderboymusic.
    See #12922.
    Fixes #37658.
    Built from https://develop.svn.wordpress.org/trunk@38263
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38204 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38261';
+$wp_version = '4.7-alpha-38263';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
