2b7d7f483349cbcabe92721d7e80c01240e7eeab
Boone Gorges <boonebgorges@gmail.com>
2016-08-18 19:15:32 +0000

    In `is_object_in_term()`, return error object rather than caching it.
    This change prevents an error object from being stored in the cache,
    and prevents notices from being thrown when plucking term IDs to put
    into the relationship cache.
    
    See #32044, #36814.
    
    Props rpayne7264.
    Fixes #37721.
    Built from https://develop.svn.wordpress.org/trunk@38277
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38218 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38276';
+$wp_version = '4.7-alpha-38277';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
