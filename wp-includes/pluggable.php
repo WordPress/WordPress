873999207dd9cf97c177a7e047df3d68dc51d0c7
Boone Gorges <boonebgorges@gmail.com>
2016-08-20 02:21:32 +0000

    Mail: Don't set Sender field when setting From.
    [38058] changed `wp_mail()` so that it used PHPMailer's `setFrom()`
    method rather than setting the From and FromName headers directly. See
    behavior of setting the `Sender` field. This causes `mail` to be
    called with the `-f` flag, which causes outgoing email to fail on some
    server environments.
    
    Merges [38286] to the 4.6 branch.
    
    Props Clorith, iandunn, DrewAPicture.
    Fixes #37736. 
    
    Built from https://develop.svn.wordpress.org/branches/4.6@38287
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.6@38228 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -349,7 +349,7 @@ function wp_mail( $to, $subject, $message, $headers = '', $attachments = array()
 	 */
 	$from_name = apply_filters( 'wp_mail_from_name', $from_name );
 
-	$phpmailer->setFrom( $from_email, $from_name );
+	$phpmailer->setFrom( $from_email, $from_name, false );
 
 	// Set destination addresses
 	if ( !is_array( $to ) )
