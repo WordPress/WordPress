188c568204d566f9d453b6cc156853e13f5a6d08
Boone Gorges <boonebgorges@gmail.com>
2016-08-20 17:25:30 +0000

    Introduce tests for `get_attachment_taxonomies()`.
    See #37368.
    Built from https://develop.svn.wordpress.org/trunk@38291
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38232 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38290';
+$wp_version = '4.7-alpha-38291';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
