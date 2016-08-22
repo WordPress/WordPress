d3312bc278e1c0bfee0177f0e995f8bffe3f0974
Scott Taylor <scott.c.taylor@mac.com>
2016-08-22 21:33:28 +0000

    Locale: declare the `$month_genitive` field on `WP_Locale`.
    See #37771.
    
    Built from https://develop.svn.wordpress.org/trunk@38316
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38257 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38315';
+$wp_version = '4.7-alpha-38316';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
