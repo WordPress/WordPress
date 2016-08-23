eb9cc0c13e86f6f94b625cdc3dff9c24c5db06d6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2016-08-23 23:33:28 +0000

    I18N: Add translator comments for strings in `wp-includes/class-walker-comment.php`.
    Merge two similar strings.
    
    Props ramiy.
    Fixes #37796.
    Built from https://develop.svn.wordpress.org/trunk@38342
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38283 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-walker-comment.php
+++ wp-includes/class-walker-comment.php
@@ -273,7 +273,12 @@ class Walker_Comment extends Walker {
 		<?php endif; ?>
 		<div class="comment-author vcard">
 			<?php if ( 0 != $args['avatar_size'] ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
-			<?php printf( __( '<cite class="fn">%s</cite> <span class="says">says:</span>' ), get_comment_author_link( $comment ) ); ?>
+			<?php
+				/* translators: %s: comment author link */
+				printf( __( '%s <span class="says">says:</span>' ),
+					sprintf( '<cite class="fn">%s</cite>', get_comment_author_link( $comment ) )
+				);
+			?>
 		</div>
 		<?php if ( '0' == $comment->comment_approved ) : ?>
 		<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ) ?></em>
@@ -325,7 +330,12 @@ class Walker_Comment extends Walker {
 				<footer class="comment-meta">
 					<div class="comment-author vcard">
 						<?php if ( 0 != $args['avatar_size'] ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
-						<?php printf( __( '%s <span class="says">says:</span>' ), sprintf( '<b class="fn">%s</b>', get_comment_author_link( $comment ) ) ); ?>
+						<?php
+							/* translators: %s: comment author link */
+							printf( __( '%s <span class="says">says:</span>' ),
+								sprintf( '<b class="fn">%s</b>', get_comment_author_link( $comment ) )
+							);
+						?>
 					</div><!-- .comment-author -->
 
 					<div class="comment-metadata">
