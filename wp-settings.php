d5f28fdad8c217ecfc1d6c040c1f97e702e99fa3
Scott Taylor <scott.c.taylor@mac.com>
2016-08-25 17:20:38 +0000

    Query: move `WP_Query` into its own file via `svn cp`.
    See #37827.
    
    Built from https://develop.svn.wordpress.org/trunk@38351
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38292 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-settings.php
+++ wp-settings.php
@@ -140,6 +140,7 @@ require( ABSPATH . WPINC . '/capabilities.php' );
 require( ABSPATH . WPINC . '/class-wp-roles.php' );
 require( ABSPATH . WPINC . '/class-wp-role.php' );
 require( ABSPATH . WPINC . '/class-wp-user.php' );
+require( ABSPATH . WPINC . '/class-wp-query.php' );
 require( ABSPATH . WPINC . '/query.php' );
 require( ABSPATH . WPINC . '/date.php' );
 require( ABSPATH . WPINC . '/theme.php' );
