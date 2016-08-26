4d17b67eed359f6f35a45526597a4f70a91ba3e0
Scott Taylor <scott.c.taylor@mac.com>
2016-08-26 09:58:28 +0000

    Load: move `is_wp_error()` to `load.php` so that `WP_Error` is in a file by itself.
    See #37827.
    
    Built from https://develop.svn.wordpress.org/trunk@38363
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38304 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-error.php
+++ wp-includes/class-wp-error.php
@@ -202,17 +202,3 @@ class WP_Error {
 		unset( $this->error_data[ $code ] );
 	}
 }
-
-/**
- * Check whether variable is a WordPress Error.
- *
- * Returns true if $thing is an object of the WP_Error class.
- *
- * @since 2.1.0
- *
- * @param mixed $thing Check if unknown variable is a WP_Error object.
- * @return bool True, if WP_Error. False, if not WP_Error.
- */
-function is_wp_error( $thing ) {
-	return ( $thing instanceof WP_Error );
-}
