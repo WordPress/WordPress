525bb2151332df04f117660d12163c078a858aec
Scott Taylor <scott.c.taylor@mac.com>
2016-08-26 20:51:43 +0000

    Unit Tests: after r38303, replace usage of `global $_wp_additional_image_sizes` with `wp_get_additional_image_sizes()`.
    See #37699.
    
    Built from https://develop.svn.wordpress.org/trunk@38383
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38324 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38382';
+$wp_version = '4.7-alpha-38383';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
