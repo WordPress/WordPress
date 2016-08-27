62717837b5b9e14744a27e5e8e3c74596871596e
Peter Wilson <wilson@peterwilson.cc>
2016-08-27 11:28:30 +0000

    Unit tests: Enforce $args object in `wp_nav_menu()` & `Walker_Nav_Menu`.
    WordPress always* passes $args to filters as an array of arugments. A exception is made in `wp_nav_menu()` and the associated walker where these are passed to filters as an object, this has been the case for seven years (since [13368]).
    
    These new tests enforce the use of an object in these filters to ensure backward compatibility is maintained.
    
    See #24587.
    
    Built from https://develop.svn.wordpress.org/trunk@38400
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38341 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38399';
+$wp_version = '4.7-alpha-38400';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
