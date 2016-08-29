0569c6219e431f084a4e82f4ea0493e1aa6da0f8
Joe McGill <joemcgill@gmail.com>
2016-08-29 12:25:29 +0000

    Post Thumbnails: Prevent post thumbnail previews from spilling into other images.
    After [38118], when previewing a page with a secondary loop, all post
    thumbnails would be filtered to display the post thumbnail for the
    page being previewed. This ensures `_wp_preview_post_thumbnail_filter()`
    is only applied if the `$post_id` of the post meta being filtered is
    equal to the post or page being previewed.
    
    Props swisspidy, joemcgill.
    Fixes #37697.
    Built from https://develop.svn.wordpress.org/trunk@38433
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38374 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38432';
+$wp_version = '4.7-alpha-38433';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
