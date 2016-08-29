e45ebb28f2e7fb2fbb14dfd279a33f266b8dcf85
Dion Hulse <wordpress@dd32.id.au>
2016-08-29 02:33:34 +0000

    HTTP: Handle an edgecase within the URI parsing library included in Requests, where if a double slash exists at the start of the path the URL is passed to cURL malformed.
    Props flixos90 for initial patch.
    Fixes #37733 for trunk.
    
    Built from https://develop.svn.wordpress.org/trunk@38429
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38370 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38428';
+$wp_version = '4.7-alpha-38429';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
