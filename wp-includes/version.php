4753f6e127d4fd633c02cbebdb532c1970fea4ef
Pascal Birchler <pascal.birchler@gmail.com>
2016-08-30 14:51:28 +0000

    Script Loader: Properly adjust tests missed in [38443].
    See #37800.
    Built from https://develop.svn.wordpress.org/trunk@38447
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38388 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38446';
+$wp_version = '4.7-alpha-38447';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
