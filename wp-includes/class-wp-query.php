227a80eba35d36b19a7e985450367c85db53c419
Dion Hulse <wordpress@dd32.id.au>
2016-09-01 05:11:31 +0000

    Query: Use `AND` in a SQL query rather than `&&`. This appears to have been the only instance of `&&` being used in SQL, so for consistency lets remove it.
    Props scrappy@hub.org.
    Fixes #37903.
    
    Built from https://develop.svn.wordpress.org/trunk@38491
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38432 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-query.php
+++ wp-includes/class-wp-query.php
@@ -2411,7 +2411,7 @@ class WP_Query {
 				$cgroupby = "{$this->db->comments}.comment_id";
 			} else { // Other non singular e.g. front
 				$cjoin = "JOIN {$this->db->posts} ON ( {$this->db->comments}.comment_post_ID = {$this->db->posts}.ID )";
-				$cwhere = "WHERE ( post_status = 'publish' OR ( post_status = 'inherit' && post_type = 'attachment' ) ) AND comment_approved = '1'";
+				$cwhere = "WHERE ( post_status = 'publish' OR ( post_status = 'inherit' AND post_type = 'attachment' ) ) AND comment_approved = '1'";
 				$cgroupby = '';
 			}
 
