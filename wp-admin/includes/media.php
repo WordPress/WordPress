c9e60dab176635d4bfaaf431c0ea891e4726d6e0
Jeremy Felt <jeremy.felt@gmail.com>
2016-09-07 13:55:36 +0000

    Media: Sanitize upload filename.
    Built from https://develop.svn.wordpress.org/trunk@38538
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38481 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/media.php
+++ wp-admin/includes/media.php
@@ -289,7 +289,7 @@ function media_handle_upload($file_id, $post_id, $post_data = array(), $override
 	$url = $file['url'];
 	$type = $file['type'];
 	$file = $file['file'];
-	$title = $basename;
+	$title = sanitize_title( $basename );
 	$content = '';
 	$excerpt = '';
 
