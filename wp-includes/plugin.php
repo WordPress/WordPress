4dcb7a0fa941c8a74d3a12207d015b904da97d07
Gary Pendergast <gary@pento.net>
2016-09-12 01:50:30 +0000

    Bootstrap: Use `dirname()` when loading `class-wp-hook.php` from `plugin.php`.
    To allow `plugin.php` to be loaded before the rest of WordPress is loaded, it cannot rely on WordPress constants, such as `ABSPATH` and `WPINC`.
    
    Instead, we can assume that `class-wp-hook.php` will be in the same directory as `plugin.php`, so `dirname( __FILE__ )` will give us the correct path to load from.
    
    Props pento, dd32.
    Fixes #37707.
    
    
    Built from https://develop.svn.wordpress.org/trunk@38589
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38532 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/plugin.php
+++ wp-includes/plugin.php
@@ -22,7 +22,7 @@
  */
 
 // Initialize the filter globals.
-require( ABSPATH . WPINC . '/class-wp-hook.php' );
+require( dirname( __FILE__ ) . '/class-wp-hook.php' );
 
 /** @var WP_Hook[] $wp_filter */
 global $wp_filter, $wp_actions, $wp_current_filter;
