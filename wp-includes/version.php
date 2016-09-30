8ee12405a5560bb9646910de1c56b743ea9b13c8
John Blackbourn <johnbillion@git.wordpress.org>
2016-09-30 22:11:28 +0000

    Role/Capability: Correct some meta capabilities that were incorrectly listed as primitive capabilities in the role and capability tests.
    See #38191
    
    Built from https://develop.svn.wordpress.org/trunk@38696
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38639 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38695';
+$wp_version = '4.7-alpha-38696';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
