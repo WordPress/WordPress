01e0a645ccc4489b4da5a07a424a921e4817c5c0
Jeremy Felt <jeremy.felt@gmail.com>
2016-09-30 05:02:27 +0000

    Admin Bar: Remove unused ID `ab-awaiting-mod` from comment count.
    This selector has been unused since [19516]. The (now) more appropriate `.awaiting-mod` is used instead.
    
    Props Presskopp.
    Fixes #37901.
    
    Built from https://develop.svn.wordpress.org/trunk@38683
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38626 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/admin-bar.php
+++ wp-includes/admin-bar.php
@@ -727,7 +727,7 @@ function wp_admin_bar_comments_menu( $wp_admin_bar ) {
 	$awaiting_text = sprintf( _n( '%s comment awaiting moderation', '%s comments awaiting moderation', $awaiting_mod ), number_format_i18n( $awaiting_mod ) );
 
 	$icon  = '<span class="ab-icon"></span>';
-	$title = '<span id="ab-awaiting-mod" class="ab-label awaiting-mod pending-count count-' . $awaiting_mod . '" aria-hidden="true">' . number_format_i18n( $awaiting_mod ) . '</span>';
+	$title = '<span class="ab-label awaiting-mod pending-count count-' . $awaiting_mod . '" aria-hidden="true">' . number_format_i18n( $awaiting_mod ) . '</span>';
 	$title .= '<span class="screen-reader-text">' . $awaiting_text . '</span>';
 
 	$wp_admin_bar->add_menu( array(
