62a43623a4456e22bec32f2b3aa33beed89206a6
Gary Pendergast <gary@pento.net>
2016-10-04 10:38:30 +0000

    Emoji: Update some failing unit tests.
    The changes in [38717] weren't reflected in the associated unit tests.
    
    See #38113.
    
    
    Built from https://develop.svn.wordpress.org/trunk@38724
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38667 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38723';
+$wp_version = '4.7-alpha-38724';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
