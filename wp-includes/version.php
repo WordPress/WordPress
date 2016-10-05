7869eaa0c61044393191ba1d4c3a74f369c744ad
John Blackbourn <johnbillion@git.wordpress.org>
2016-10-05 16:57:30 +0000

    Role/Capability: Reuse some user account fixtures in the user capability tests.
    See #38235
    
    Built from https://develop.svn.wordpress.org/trunk@38731
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38674 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38730';
+$wp_version = '4.7-alpha-38731';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
