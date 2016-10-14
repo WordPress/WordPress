8cbb98819648f6c592d5ef83deb53e0f0dbd38cb
Rachel Baker <rachel@rachelbaker.me>
2016-10-14 19:39:28 +0000

    Pings/Trackbacks: Add new `pre_trackback_post` action before a trackback is added to a post.
    Props dshanske, rachelbaker.
    Fixes #37007.
    Built from https://develop.svn.wordpress.org/trunk@38791
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38734 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-trackback.php
+++ wp-trackback.php
@@ -91,6 +91,20 @@ if (empty($title) && empty($tb_url) && empty($blog_name)) {
 }
 
 if ( !empty($tb_url) && !empty($title) ) {
+	/**
+	* Fires before the trackback is added to a post.
+	*
+	* @since 4.7.0
+	*
+	* @param int    $tb_id     Post ID related to the trackback.
+	* @param string $tb_url    Trackback URL.
+	* @param string $charset   Character Set.
+	* @param string $title     Trackback Title.
+	* @param string $excerpt   Trackback Excerpt.
+	* @param string $blog_name Blog Name.
+	*/
+	do_action( 'pre_trackback_post', $tb_id, $tb_url, $charset, $title, $excerpt, $blog_name );
+
 	header('Content-Type: text/xml; charset=' . get_option('blog_charset') );
 
 	if ( !pings_open($tb_id) )
