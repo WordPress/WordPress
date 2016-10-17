969e4255a9abd3221dce6b4fe309fa0876bc37cc
Aaron Jorbin <aaron@jorb.in>
2016-10-17 16:11:30 +0000

    REST API: Include `Vary: Origin` in cors headers
    `vary: origin` is a W3 CORS implementation recommendation( https://www.w3.org/TR/cors/#resource-implementation ). It's used by default in frameworks such as hapi and Laravel-cors. Overall, it helps sites siting behind a cache such as varnish.
    
    Fixes #38060.
    Props procodewp, pdufour for research.
    
    
    Built from https://develop.svn.wordpress.org/trunk@38806
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38749 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api.php
+++ wp-includes/rest-api.php
@@ -408,6 +408,7 @@ function rest_send_cors_headers( $value ) {
 		header( 'Access-Control-Allow-Origin: ' . esc_url_raw( $origin ) );
 		header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
 		header( 'Access-Control-Allow-Credentials: true' );
+		header( 'Vary: Origin' );
 	}
 
 	return $value;
