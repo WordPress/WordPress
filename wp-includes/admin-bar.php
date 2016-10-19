78cadd3ab2967f6cb03f713395f2aeec45af8d21
Josh Pollock <jpollock412@gmail.com>
2016-10-19 12:50:15 -0400

    Add revisions browser

--- wp-includes/admin-bar.php
+++ wp-includes/admin-bar.php
@@ -1003,3 +1003,88 @@ function _get_admin_bar_pref( $context = 'front', $user = 0 ) {
 
 	return 'true' === $pref;
 }
+
+//Load revisions browser
+add_action( 'wp_enqueue_scripts', 'admin_bar_revisions_browser' );
+
+/**
+ * Main function for loading the front-end revision browser.
+ *
+ * @uses "wp_enqueue_scripts" action
+ * @since  4.7.0
+ */
+function admin_bar_revisions_browser() {
+	if ( ! current_user_can( 'edit_posts' ) ) {
+		return;
+	}
+
+	if ( ! is_single() ) {
+		return;
+	}
+
+	add_action( 'admin_bar_menu', 'admin_bar_revisions_browser_toolbar', 999 );
+
+	//wp_enqueue_script( 'revision-browser', '/wp-includes/js/revision-browser.js', array( 'jquery', 'wp-api' ) );
+	wp_enqueue_script( 'revision-browser', plugin_dir_url(__FILE__) . 'revision-browser.js', array( 'jquery', 'wp-api' ) );
+
+	//wp_enqueue_style( 'revision-browser', plugin_dir_url(__FILE__) . 'revision-browser.css' );
+	wp_enqueue_style( 'revision-browser', '/wp-includes/css/revision-browser.css' );
+
+	$selectors = wp_parse_args( get_theme_support( 'revision-browser-selectors' ), array(
+		'content' => 'entry-content',
+		'title'   => 'entry-title',
+	) );
+
+	global  $post;
+	if ( ! is_object( $post ) ) {
+		return;
+	}
+
+	// REVBROWSER object.
+	wp_localize_script( 'revision-browser', 'REVBROWSER', [
+		'post'    => absint( $post->ID ),
+		'content' => $selectors[ 'content' ],
+		'title'   => $selectors[ 'title' ],
+		'none' => esc_html__( 'No Revisions')
+
+	]);
+};
+
+/**
+ * Add revision browser to toolbar
+ *
+ * @uses "admin_bar_menu" action
+ * @since 4.7.0
+ * @param WP_Admin_Bar $wp_admin_bar
+ */
+function admin_bar_revisions_browser_toolbar(  $wp_admin_bar ) {
+	$parent = 'revisions-browser';
+	$meta = array( 'class' => 'revisions-browser' );
+	$args = array(
+		'id'    => $parent,
+		'title' => __( 'Browse Revisions' ),
+		'href'  => '#revisions',
+		'meta'  => $meta
+	);
+
+	$wp_admin_bar->add_node( $args );
+
+	$wp_admin_bar->add_node( array(
+		'parent' => $parent,
+		'id' => $parent . '-previous',
+		'title' => __( 'Previous' ),
+		'href' => '#previous-revision',
+		'meta' => $meta,
+	) );
+
+	$wp_admin_bar->add_node( array(
+		'parent' => $parent,
+		'id' => $parent . '-next',
+		'title' => __( 'Next' ),
+		'href' => '#next-revision',
+		'meta' => $meta,
+	) );
+
+}
+
+
