6b46336251b20d6b601c3ace43c699066c700210
Rachel Baker <rachel@rachelbaker.me>
2016-10-20 05:25:29 +0000

    REST API: Fix comment option leak causing another test failure.
    Set the 'comment_whitelist' option back to the default when tearing down wpAllowComment tests.
    
    See #38373.
    Built from https://develop.svn.wordpress.org/trunk@38835
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38778 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38834';
+$wp_version = '4.7-alpha-38835';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
