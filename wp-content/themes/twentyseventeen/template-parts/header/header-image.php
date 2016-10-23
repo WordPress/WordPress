dc35c19f60f7ee35c1ab1f24b6b152691eb7340a
Tammie Lister <tammie@automattic.com>
2016-10-23 12:29:30 +0000

    Twenty Seventeen: Renaming of directory structure
    This changes components directory to be called template-parts. Changes reflected in all files that call those sections.
    
    Props bronsonquick, dd32
    
    Fixes #38375
    
    Built from https://develop.svn.wordpress.org/trunk@38875
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38818 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyseventeen/template-parts/header/header-image.php
+++ wp-content/themes/twentyseventeen/template-parts/header/header-image.php
@@ -17,12 +17,12 @@
 	if ( ! empty( $header_image ) ) : ?>
 
 		<div class="custom-header-image" style="background-image: url(<?php echo esc_url( $header_image ); ?>)"></div>
-		<?php get_template_part( 'components/header/site', 'branding' ); ?>
+		<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
 
 	<?php else : ?>
 		<?php // Otherwise, show a blank header. ?>
 		<div class="custom-header-simple">
-			<?php get_template_part( 'components/header/site', 'branding' ); ?>
+			<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
 		</div><!-- .custom-header-simple -->
 
 	<?php endif; ?>
