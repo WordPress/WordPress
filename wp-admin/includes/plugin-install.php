4bd54798ecd05de5c23d24664d78db6d7b1ae7e7
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2016-10-26 12:18:06 +0000

    I18N: Allow for WordPress Plugin Directory URL in plugin details modal to be localized.
    Props Soean.
    Fixes #38495. See #37501.
    Built from https://develop.svn.wordpress.org/trunk@38953
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38896 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/plugin-install.php
+++ wp-admin/includes/plugin-install.php
@@ -574,7 +574,7 @@ function install_plugin_information() {
 					}
 					?></li>
 			<?php } if ( ! empty( $api->slug ) && empty( $api->external ) ) { ?>
-				<li><a target="_blank" href="https://wordpress.org/plugins/<?php echo $api->slug; ?>/"><?php _e( 'WordPress.org Plugin Page &#187;' ); ?></a></li>
+				<li><a target="_blank" href="<?php echo __( 'https://wordpress.org/plugins/' ) . $api->slug; ?>/"><?php _e( 'WordPress.org Plugin Page &#187;' ); ?></a></li>
 			<?php } if ( ! empty( $api->homepage ) ) { ?>
 				<li><a target="_blank" href="<?php echo esc_url( $api->homepage ); ?>"><?php _e( 'Plugin Homepage &#187;' ); ?></a></li>
 			<?php } if ( ! empty( $api->donate_link ) && empty( $api->contributors ) ) { ?>
