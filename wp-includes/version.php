7343b3bf407c0c19be6bbbf73a1af8f9650a50ee
Helen Hou-Sandí <helen.y.hou@gmail.com>
2016-10-28 03:01:43 +0000

    WordPress 4.7 Beta 1.
    Built from https://develop.svn.wordpress.org/trunk@38992
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38935 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-alpha-38991';
+$wp_version = '4.7-beta1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
