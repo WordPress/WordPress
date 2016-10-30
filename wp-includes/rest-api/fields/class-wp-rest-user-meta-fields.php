b8f14e00ef5bd3b25e6120b057d23aa4314da259
Drew Jaynes <info@drewapicture.com>
2016-10-30 18:28:30 +0000

    Docs: Add much more complete and syntactically correct documentation throughout the `WP_REST_User_Meta_Fields` class.
    Props Soean, mrahmadawais, flixos90.
    See #38398.
    
    Built from https://develop.svn.wordpress.org/trunk@39034
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38976 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/fields/class-wp-rest-user-meta-fields.php
+++ wp-includes/rest-api/fields/class-wp-rest-user-meta-fields.php
@@ -1,19 +1,40 @@
 <?php
+/**
+ * REST API: WP_REST_User_Meta_Fields class
+ *
+ * @package WordPress
+ * @subpackage REST_API
+ * @since 4.7.0
+ */
 
+/**
+ * Core class used to manage meta values for users via the REST API.
+ *
+ * @since 4.7.0
+ *
+ * @see WP_REST_Meta_Fields
+ */
 class WP_REST_User_Meta_Fields extends WP_REST_Meta_Fields {
+
 	/**
-	 * Get the object type for meta.
+	 * Retrieves the object meta type.
 	 *
-	 * @return string
+	 * @since 4.7.0
+	 * @access protected
+	 *
+	 * @return string The user meta type.
 	 */
 	protected function get_meta_type() {
 		return 'user';
 	}
 
 	/**
-	 * Get the type for `register_rest_field`.
+	 * Retrieves the type for register_rest_field().
 	 *
-	 * @return string
+	 * @since 4.7.0
+	 * @access public
+	 *
+	 * @return string The user REST field type.
 	 */
 	public function get_rest_field_type() {
 		return 'user';
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-beta1-39033';
+$wp_version = '4.7-beta1-39034';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
