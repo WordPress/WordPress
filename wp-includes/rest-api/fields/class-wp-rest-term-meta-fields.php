b71e62b9eb783ef93b378113f49179f48ac4629b
Drew Jaynes <info@drewapicture.com>
2016-10-30 18:09:32 +0000

    Docs: Add much more complete and syntactically correct documentation throughout the `WP_REST_Term_Meta_Fields` class.
    Props Soean, mrahmadawais, flixos90.
    See #38398.
    
    Built from https://develop.svn.wordpress.org/trunk@39032
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38974 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/fields/class-wp-rest-term-meta-fields.php
+++ wp-includes/rest-api/fields/class-wp-rest-term-meta-fields.php
@@ -1,17 +1,35 @@
 <?php
+/**
+ * REST API: WP_REST_Term_Meta_Fields class
+ *
+ * @package WordPress
+ * @subpackage REST_API
+ * @since 4.7.0
+ */
 
 /**
- * Manage meta values for terms.
+ * Core class used to manage meta values for terms via the REST API.
+ *
+ * @since 4.7.0
+ *
+ * @see WP_REST_Meta_Fields
  */
 class WP_REST_Term_Meta_Fields extends WP_REST_Meta_Fields {
+
 	/**
 	 * Taxonomy to register fields for.
 	 *
+	 * @since 4.7.0
+	 * @access protected
 	 * @var string
 	 */
 	protected $taxonomy;
+
 	/**
 	 * Constructor.
+	 *
+	 * @since 4.7.0
+	 * @access public
 	 *
 	 * @param string $taxonomy Taxonomy to register fields for.
 	 */
@@ -20,18 +38,24 @@ class WP_REST_Term_Meta_Fields extends WP_REST_Meta_Fields {
 	}
 
 	/**
-	 * Get the object type for meta.
+	 * Retrieves the object meta type.
 	 *
-	 * @return string
+	 * @since 4.7.0
+	 * @access protected
+	 *
+	 * @return string The meta type.
 	 */
 	protected function get_meta_type() {
 		return 'term';
 	}
 
 	/**
-	 * Get the type for `register_rest_field`.
+	 * Retrieves the type for register_rest_field().
 	 *
-	 * @return string
+	 * @since 4.7.0
+	 * @access public
+	 *
+	 * @return string The REST field type.
 	 */
 	public function get_rest_field_type() {
 		return 'post_tag' === $this->taxonomy ? 'tag' : $this->taxonomy;
