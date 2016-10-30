7d25657c01a1edf4b992db90ced870bbe60e58a8
Drew Jaynes <info@drewapicture.com>
2016-10-30 06:34:31 +0000

    Docs: Add much more complete and syntactically correct documentation throughout the `WP_REST_Comment_Meta_Fields` class.
    Props Soean, mrahmadawais, flixos90.
    See #38398.
    
    Built from https://develop.svn.wordpress.org/trunk@39014
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38956 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/fields/class-wp-rest-comment-meta-fields.php
+++ wp-includes/rest-api/fields/class-wp-rest-comment-meta-fields.php
@@ -1,19 +1,40 @@
 <?php
+/**
+ * REST API: WP_REST_Comment_Meta_Fields class
+ *
+ * @package WordPress
+ * @subpackage REST_API
+ * @since 4.7.0
+ */
 
+/**
+ * Core class to manage comment meta via the REST API.
+ *
+ * @since 4.7.0
+ *
+ * @see WP_REST_Meta_Fields
+ */
 class WP_REST_Comment_Meta_Fields extends WP_REST_Meta_Fields {
+
 	/**
-	 * Get the object type for meta.
+	 * Retrieves the object type for comment meta.
 	 *
-	 * @return string
+	 * @since 4.7.0
+	 * @access protected
+	 *
+	 * @return string The meta type.
 	 */
 	protected function get_meta_type() {
 		return 'comment';
 	}
 
 	/**
-	 * Get the type for `register_rest_field`.
+	 * Retrieves the type for register_rest_field() in the context of comments.
 	 *
-	 * @return string
+	 * @since 4.7.0
+	 * @access public
+	 *
+	 * @return string The REST field type.
 	 */
 	public function get_rest_field_type() {
 		return 'comment';
