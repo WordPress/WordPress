c4df9d63ba69c51709ccc806298193c7ae20fabf
Drew Jaynes <info@drewapicture.com>
2016-10-30 16:36:32 +0000

    Docs: Add much more complete and syntactically correct documentation throughout the `WP_REST_Post_Meta_Fields` class.
    Props Soean, mrahmadawais, flixos90.
    See #38398.
    
    Built from https://develop.svn.wordpress.org/trunk@39023
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38965 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/fields/class-wp-rest-post-meta-fields.php
+++ wp-includes/rest-api/fields/class-wp-rest-post-meta-fields.php
@@ -1,15 +1,35 @@
 <?php
+/**
+ * REST API: WP_REST_Post_Meta_Fields class
+ *
+ * @package WordPress
+ * @subpackage REST_API
+ * @since 4.7.0
+ */
 
+/**
+ * Core class used to manage meta values for posts via the REST API.
+ *
+ * @since 4.7.0
+ *
+ * @see WP_REST_Meta_Fields
+ */
 class WP_REST_Post_Meta_Fields extends WP_REST_Meta_Fields {
+
 	/**
 	 * Post type to register fields for.
 	 *
+	 * @since 4.7.0
+	 * @access protected
 	 * @var string
 	 */
 	protected $post_type;
 
 	/**
 	 * Constructor.
+	 *
+	 * @since 4.7.0
+	 * @access public
 	 *
 	 * @param string $post_type Post type to register fields for.
 	 */
@@ -18,18 +38,26 @@ class WP_REST_Post_Meta_Fields extends WP_REST_Meta_Fields {
 	}
 
 	/**
-	 * Get the object type for meta.
+	 * Retrieves the object meta type.
 	 *
-	 * @return string
+	 * @since 4.7.0
+	 * @access protected
+	 *
+	 * @return string The meta type.
 	 */
 	protected function get_meta_type() {
 		return 'post';
 	}
 
 	/**
-	 * Get the type for `register_rest_field`.
+	 * Retrieves the type for register_rest_field().
 	 *
-	 * @return string Custom post type slug.
+	 * @since 4.7.0
+	 * @access public
+	 *
+	 * @see register_rest_field()
+	 *
+	 * @return string The REST field type.
 	 */
 	public function get_rest_field_type() {
 		return $this->post_type;
