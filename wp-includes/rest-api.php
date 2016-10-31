69539eb2a7d1f393ccb50a1a3461c7da34026b26
Gary Pendergast <gary@pento.net>
2016-10-31 00:19:30 +0000

    REST API: Add `PATCH` to CORS allowed methods.
    Editable resources in the REST API accept the `PATCH` method, but the CORS headers don't mention it.
    
    Props jnylen0.
    Fixes #38546.
    
    
    Built from https://develop.svn.wordpress.org/trunk@39042
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@38984 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api.php
+++ wp-includes/rest-api.php
@@ -514,7 +514,7 @@ function rest_send_cors_headers( $value ) {
 
 	if ( $origin ) {
 		header( 'Access-Control-Allow-Origin: ' . esc_url_raw( $origin ) );
-		header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
+		header( 'Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, PATCH, DELETE' );
 		header( 'Access-Control-Allow-Credentials: true' );
 		header( 'Vary: Origin' );
 	}
