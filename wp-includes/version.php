d8cbc610208e6b90fbe76e8a1217e4c4d436e4ab
Pascal Birchler <pascal.birchler@gmail.com>
2016-11-08 22:32:30 +0000

    Date/Time: Improve `date_i18n()` timezone tests.
    This uses a timezone without DST for the tests covering `date_i18n()`.
    
    Fixes #37910.
    Built from https://develop.svn.wordpress.org/trunk@39167
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39107 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-beta2-39166';
+$wp_version = '4.7-beta2-39167';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
