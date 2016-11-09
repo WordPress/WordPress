3b008dc2a02bc805204b3d16f62118687d756fe9
Weston Ruter <weston@xwp.co>
2016-11-09 20:55:33 +0000

    Customize: Eliminate use of flexbox for edit shortcuts.
    Props sirbrillig.
    See #27403.
    Fixes #38660.
    
    Built from https://develop.svn.wordpress.org/trunk@39186
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39126 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-beta2-39185';
+$wp_version = '4.7-beta2-39186';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
