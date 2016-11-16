7db38b916a5f607037ddc48313de11842a29d5e0
Weston Ruter <weston@xwp.co>
2016-11-16 18:42:32 +0000

    Customize: Allow URL for Codex link in Additional CSS section to be translated.
    Props odysseygate, ocean90, westonruter.
    See #35395.
    Fixes #38823.
    
    Built from https://develop.svn.wordpress.org/trunk@39268
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39208 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-beta4-39267';
+$wp_version = '4.7-beta4-39268';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
