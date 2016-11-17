9bc6aa15700fca20b85e38cad1a7c9603071e57a
Peter Wilson <wilson@peterwilson.cc>
2016-11-17 23:35:32 +0000

    Taxonomy: Update `register_taxonomy` hook to use `WP_Taxonomy` object.
    Casts `WP_Taxonomy` to an array for passing to the `register_taxonomy` hook. This maintains backward compatibility with the processed arguments used prior to WordPress 4.7.
    
    Fixes #38765.
    
    Built from https://develop.svn.wordpress.org/trunk@39283
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39223 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/taxonomy.php
+++ wp-includes/taxonomy.php
@@ -393,7 +393,7 @@ function register_taxonomy( $taxonomy, $object_type, $args = array() ) {
 	 * @param array|string $object_type Object type or array of object types.
 	 * @param array        $args        Array of taxonomy registration arguments.
 	 */
-	do_action( 'registered_taxonomy', $taxonomy, $object_type, $args );
+	do_action( 'registered_taxonomy', $taxonomy, $object_type, (array) $taxonomy_object );
 }
 
 /**
