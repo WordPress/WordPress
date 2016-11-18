560e93ec80730ce76d9e3c497c8e1d3ff3efa063
Weston Ruter <weston@xwp.co>
2016-11-18 05:22:31 +0000

    Customize: Remove obsolete edit shortcut style rules from Twenty Seventeen.
    Props celloexpressions.
    Fixes #38776.
    
    Built from https://develop.svn.wordpress.org/trunk@39285
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39225 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-beta4-39284';
+$wp_version = '4.7-beta4-39285';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
