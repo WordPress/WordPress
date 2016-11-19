dca7d8d0ea4ca90a715c1cbc46d5fb3cd1bcbdb2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2016-11-19 02:16:30 +0000

    Text Changes: Merge strings referring to `list_users` capability.
    See #38857.
    Built from https://develop.svn.wordpress.org/trunk@39312
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39252 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-beta4-39311';
+$wp_version = '4.7-beta4-39312';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
