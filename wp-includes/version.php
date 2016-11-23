68f5457770f5e9828f03c93cca7c46a29e028adf
David A. Kennedy <me@davidakennedy.com>
2016-11-23 00:44:30 +0000

    Twenty Seventeen: Ensure edit button label displays properly in other languages
    This bug occurred in any language where text wraps character-by-character, since those languages don't delimit words with spaces. In Japanese, Chinese and Korean, etc., it was possible for the label to be displayed vertically.
    
    Props tg29359, afercia, odysseygate.
    
    Fixes #38876.
    
    Built from https://develop.svn.wordpress.org/trunk@39341
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39281 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-beta4-39340';
+$wp_version = '4.7-beta4-39341';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
