a7ddf24f3de2ecff688c729ce57cd4eafff3f8ae
Drew Jaynes <info@drewapicture.com>
2016-11-23 17:35:32 +0000

    Docs: Add a missing changelog entry for the point where the `$tagnames` parameter was added to `get_shortcode_regex()`.
    Props keesiemeijer.
    Fixes #38914.
    
    Built from https://develop.svn.wordpress.org/trunk@39351
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39291 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/shortcodes.php
+++ wp-includes/shortcodes.php
@@ -244,10 +244,11 @@ function do_shortcode( $content, $ignore_html = false ) {
  * 6 - An extra ] to allow for escaping shortcodes with double [[]]
  *
  * @since 2.5.0
+ * @since 4.4.0 Added the `$tagnames` parameter.
  *
  * @global array $shortcode_tags
  *
- * @param array $tagnames List of shortcodes to find. Optional. Defaults to all registered shortcodes.
+ * @param array $tagnames Optional. List of shortcodes to find. Defaults to all registered shortcodes.
  * @return string The shortcode search regular expression
  */
 function get_shortcode_regex( $tagnames = null ) {
