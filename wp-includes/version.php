9011477638b169015ca6dfa01699f46618a96dd8
John Blackbourn <johnbillion@git.wordpress.org>
2016-11-29 04:58:10 +0000

    Build/Test Tools: Add an extra `WP_Error` assertion when testing a valid user activation key. This provides a better failure message if the assertion does fail.
    See #38716
    
    Built from https://develop.svn.wordpress.org/trunk@39364
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39304 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-39363';
+$wp_version = '4.8-alpha-39364';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
