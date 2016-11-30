849d8db4fc2ca80061bf159e0c98936828a43b7c
Dominik Schilling <dominikschilling+git@gmail.com>
2016-11-30 23:07:36 +0000

    Twenty Seventeen: Provide a background color fallback for non-webkit browsers on input styles
    This allows the styles to work correctly when modified by high contrast computer setups.
    
    Merge of [39388] to the 4.7 branch.
    
    Props presskopp.
    See #38939.
    Built from https://develop.svn.wordpress.org/branches/4.7@39389
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@39329 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-RC1-39387';
+$wp_version = '4.7-RC1-39389';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
