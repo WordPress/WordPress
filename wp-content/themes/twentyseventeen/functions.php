b6c2f11c5444743cdd6b2053e1622a604175d535
Helen Hou-Sandí <helen.y.hou@gmail.com>
2016-11-30 05:48:44 +0000

    Twenty Seventeen: Add textdomain for starter content attachment titles.
    fixes #38981.
    
    Built from https://develop.svn.wordpress.org/trunk@39373
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39313 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyseventeen/functions.php
+++ wp-content/themes/twentyseventeen/functions.php
@@ -139,15 +139,15 @@ function twentyseventeen_setup() {
 
 		'attachments' => array(
 			'image-espresso' => array(
-				'post_title' => _x( 'Espresso', 'Theme starter content' ),
+				'post_title' => _x( 'Espresso', 'Theme starter content', 'twentyseventeen' ),
 				'file' => 'assets/images/espresso.jpg',
 			),
 			'image-sandwich' => array(
-				'post_title' => _x( 'Sandwich', 'Theme starter content' ),
+				'post_title' => _x( 'Sandwich', 'Theme starter content', 'twentyseventeen' ),
 				'file' => 'assets/images/sandwich.jpg',
 			),
 			'image-coffee' => array(
-				'post_title' => _x( 'Coffee', 'Theme starter content' ),
+				'post_title' => _x( 'Coffee', 'Theme starter content', 'twentyseventeen' ),
 				'file' => 'assets/images/coffee.jpg',
 			),
 		),
