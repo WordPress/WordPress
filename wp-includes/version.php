96507046898ade58e3dc1a08f0eff9fd37bdf33e
Joe McGill <joemcgill@gmail.com>
2016-11-30 20:58:34 +0000

    Twenty Seventeen: Keep header videos from extending past footer.
    This adds 1px of bottom padding to header videos to keep them from
    extending past the footer at the bottom of the page.
    
    Fixes #38950 for 4.7 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.7@39381
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@39321 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-RC1-39379';
+$wp_version = '4.7-RC1-39381';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
