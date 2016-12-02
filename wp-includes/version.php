43eb2619e98e472f1608d0a72afd23eb699b20f8
Gary Pendergast <gary@pento.net>
2016-12-02 06:59:34 +0000

    REST API: Require the reassign parameter when deleting users.
    When deleting a user through the WordPress admin, a specific decision is presented - whether to assign all of the user's posts to another user, or to delete all of the posts.
    
    This change requires `reassign` as a parameter in the corresponding REST API endpoint, so that content isn't accidentally lost.
    
    Merges [39426] to the 4.7 branch.
    
    Props jeremyfelt.
    Fixes #39000.
    
    
    Built from https://develop.svn.wordpress.org/branches/4.7@39427
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@39367 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-RC1-39425';
+$wp_version = '4.7-RC1-39427';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
