afc9ef5123bd59f1b4ab05c9a92aa9af9cb7dc8e
Dominik Schilling <dominikschilling+git@gmail.com>
2016-12-02 12:03:45 +0000

    About page: Restore the double `%` in a `printf()` call which has been removed in [39420].
    We need this to escape the percent symbol for "100%".
    
    See #38616.
    Built from https://develop.svn.wordpress.org/trunk@39432
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39372 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/freedoms.php
+++ wp-admin/freedoms.php
@@ -45,7 +45,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 $plugins_url = current_user_can( 'activate_plugins' ) ? admin_url( 'plugins.php' ) : __( 'https://wordpress.org/plugins/' );
 $themes_url = current_user_can( 'switch_themes' ) ? admin_url( 'themes.php' ) : __( 'https://wordpress.org/themes/' );
 
-printf( __( 'Every plugin and theme in WordPress.org&#8217;s directory is 100% GPL or a similarly free and compatible license, so you can feel safe finding <a href="%1$s">plugins</a> and <a href="%2$s">themes</a> there. If you get a plugin or theme from another source, make sure to <a href="%3$s">ask them if it&#8217;s GPL</a> first. If they don&#8217;t respect the WordPress license, we don&#8217;t recommend them.' ), $plugins_url, $themes_url, 'https://wordpress.org/about/license/' ); ?></p>
+printf( __( 'Every plugin and theme in WordPress.org&#8217;s directory is 100%% GPL or a similarly free and compatible license, so you can feel safe finding <a href="%1$s">plugins</a> and <a href="%2$s">themes</a> there. If you get a plugin or theme from another source, make sure to <a href="%3$s">ask them if it&#8217;s GPL</a> first. If they don&#8217;t respect the WordPress license, we don&#8217;t recommend them.' ), $plugins_url, $themes_url, 'https://wordpress.org/about/license/' ); ?></p>
 
 <p><?php _e( 'Don&#8217;t you wish all software came with these freedoms? So do we! For more information, check out the <a href="https://www.fsf.org/">Free Software Foundation</a>.' ); ?></p>
 
