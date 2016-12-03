605e2f404a2552803426ddceb59d7c142367a55e
Gary Pendergast <gary@pento.net>
2016-12-03 04:16:38 +0000

    Load: No-op `locale.php`
    After re-adding `locale.php` in [39455], remove the content of the file so that it can't cause errors in the event of something loading it early in the WordPress load process.
    
    See #39027.
    
    
    Built from https://develop.svn.wordpress.org/trunk@39456
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39396 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/locale.php
+++ wp-includes/locale.php
@@ -8,20 +8,3 @@
  */
 
 _deprecated_file( basename( __FILE__ ), '4.7.0' );
-
-/** WP_Locale class */
-require_once ABSPATH . WPINC . '/class-wp-locale.php';
-
-/**
- * Checks if current locale is RTL.
- *
- * @since 3.0.0
- *
- * @global WP_Locale $wp_locale
- *
- * @return bool Whether locale is RTL.
- */
-function is_rtl() {
-	global $wp_locale;
-	return $wp_locale->is_rtl();
-}
