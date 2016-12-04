0a31089bf6f5cb8182d497ba8eaff6e3a79c7459
Weston Ruter <weston@xwp.co>
2016-12-04 17:31:33 +0000

    Customize: Ensure a `custom_css` post insertion gets an initial post revision.
    Props georgestephanis, westonruter.
    See #30854, #38672, #35395.
    Fixes #39032.
    
    Built from https://develop.svn.wordpress.org/trunk@39477
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39417 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-39475';
+$wp_version = '4.8-alpha-39477';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
