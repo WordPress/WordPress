ac2345ca3244dbfcc813a780fea3e3f3c15175a0
Weston Ruter <weston@xwp.co>
2016-12-05 14:00:35 +0000

    Customize: Ensure `textarea` for Custom CSS displays as code (in LTR) when an RTL language is active.
    Merges [39499] onto 4.7 branch.
    Fixes #39085 for 4.7.
    
    Built from https://develop.svn.wordpress.org/branches/4.7@39500
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@39440 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-RC2-39498';
+$wp_version = '4.7-RC2-39500';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
