537f4728bdaa2496d051b6256f940e2c12d61677
Helen Hou-Sandí <helen.y.hou@gmail.com>
2016-12-06 17:18:34 +0000

    Remove 4.7 cruft from `$_old_files`.
    fixes #39113.
    
    Built from https://develop.svn.wordpress.org/branches/4.7@39521
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@39461 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7-RC3-39519';
+$wp_version = '4.7-RC3-39521';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
