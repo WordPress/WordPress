9c023fc883ff28d9d657778b2f2ad81d665ba550
Helen Hou-Sandí <helen.y.hou@gmail.com>
2016-12-06 17:17:41 +0000

    Remove 4.7 cruft from `$_old_files`.
    see #39113.
    
    Built from https://develop.svn.wordpress.org/trunk@39520
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39460 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/update-core.php
+++ wp-admin/includes/update-core.php
@@ -705,12 +705,6 @@ $_old_files = array(
 'wp-includes/theme-compat/comments-popup.php',
 // 4.6
 'wp-admin/includes/class-wp-automatic-upgrader.php', // Wrong file name, see #37628.
-// 4.7
-// Don't delete, yet: 'wp-admin/includes/class-wp-upgrader-skins.php',
-// Don't delete, yet: 'wp-includes/class-feed.php',
-// Don't delete, yet: 'wp-includes/locale.php',
-// Don't delete, yet: 'wp-includes/session.php',
-'wp-includes/customize/class-wp-customize-themes-panel.php', // Removed in beta; when the feature comes back remember to remove it from this array. See #37661.
 );
 
 /**
