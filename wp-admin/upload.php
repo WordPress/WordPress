26ef4bd86d3e3902d2cc5004125f041d76b2489f
Andrea Fercia <a.fercia@gmail.com>
2016-12-06 22:18:42 +0000

    Accessibility: Remove inappropriate content from the Media Library screens headings.
    Props joedolson.
    
    See #26601.
    
    Built from https://develop.svn.wordpress.org/trunk@39526
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39466 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/upload.php
+++ wp-admin/upload.php
@@ -73,14 +73,16 @@ if ( 'grid' === $mode ) {
 	require_once( ABSPATH . 'wp-admin/admin-header.php' );
 	?>
 	<div class="wrap" id="wp-media-grid" data-search="<?php _admin_search_query() ?>">
-		<h1>
+		<h1 class="wp-heading-inline"><?php echo esc_html( $title ); ?></h1>
+
 		<?php
-		echo esc_html( $title );
 		if ( current_user_can( 'upload_files' ) ) { ?>
 			<a href="<?php echo admin_url( 'media-new.php' ); ?>" class="page-title-action"><?php echo esc_html_x( 'Add New', 'file' ); ?></a><?php
 		}
 		?>
-		</h1>
+
+		<hr class="wp-header-end">
+
 		<div class="error hide-if-js">
 			<p><?php printf(
 				/* translators: %s: list view URL */
@@ -221,18 +223,20 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
 ?>
 
 <div class="wrap">
-<h1>
+<h1 class="wp-heading-inline"><?php echo esc_html( $title ); ?></h1>
+
 <?php
-echo esc_html( $title );
 if ( current_user_can( 'upload_files' ) ) { ?>
-	<a href="<?php echo admin_url( 'media-new.php' ); ?>" class="page-title-action"><?php echo esc_html_x('Add New', 'file'); ?></a><?php
+	<a href="<?php echo admin_url( 'media-new.php' ); ?>" class="page-title-action"><?php echo esc_html_x( 'Add New', 'file' ); ?></a><?php
 }
+
 if ( isset( $_REQUEST['s'] ) && strlen( $_REQUEST['s'] ) ) {
 	/* translators: %s: search keywords */
 	printf( '<span class="subtitle">' . __( 'Search results for &#8220;%s&#8221;' ) . '</span>', get_search_query() );
 }
 ?>
-</h1>
+
+<hr class="wp-header-end">
 
 <?php
 $message = '';
