2d76e0dfb28fabbd94da262cfe6de3f92a51d3cc
Andrea Fercia <a.fercia@gmail.com>
2016-12-07 22:18:40 +0000

    Accessibility: Remove inappropriate content from the Widgets screen heading.
    See #26601.
    
    Built from https://develop.svn.wordpress.org/trunk@39541
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39481 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/widgets.php
+++ wp-admin/widgets.php
@@ -344,24 +344,27 @@ $errors = array(
 require_once( ABSPATH . 'wp-admin/admin-header.php' ); ?>
 
 <div class="wrap">
-<h1>
+<h1 class="wp-heading-inline"><?php
+echo esc_html( $title );
+?></h1>
+
 <?php
-	echo esc_html( $title );
-	if ( current_user_can( 'customize' ) ) {
-		printf(
-			' <a class="page-title-action hide-if-no-customize" href="%1$s">%2$s</a>',
-			esc_url( add_query_arg(
-				array(
-					array( 'autofocus' => array( 'panel' => 'widgets' ) ),
-					'return' => urlencode( wp_unslash( $_SERVER['REQUEST_URI'] ) )
-				),
-				admin_url( 'customize.php' )
-			) ),
-			__( 'Manage with Live Preview' )
-		);
-	}
+if ( current_user_can( 'customize' ) ) {
+	printf(
+		' <a class="page-title-action hide-if-no-customize" href="%1$s">%2$s</a>',
+		esc_url( add_query_arg(
+			array(
+				array( 'autofocus' => array( 'panel' => 'widgets' ) ),
+				'return' => urlencode( wp_unslash( $_SERVER['REQUEST_URI'] ) )
+			),
+			admin_url( 'customize.php' )
+		) ),
+		__( 'Manage with Live Preview' )
+	);
+}
 ?>
-</h1>
+
+<hr class="wp-header-end">
 
 <?php if ( isset($_GET['message']) && isset($messages[$_GET['message']]) ) { ?>
 <div id="message" class="updated notice is-dismissible"><p><?php echo $messages[$_GET['message']]; ?></p></div>
