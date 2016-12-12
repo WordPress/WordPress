826a0323a28a9df926819ef14fe9177680ada97b
Weston Ruter <weston@xwp.co>
2016-12-12 05:12:44 +0000

    Build/Test Tools: Facilitate SVN and Git being co-located in the same directory.
    Ignore .svn in .gitignore, and ignore .git in svn:ignore.
    
    Fixes #39245.
    
    Built from https://develop.svn.wordpress.org/trunk@39577
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39517 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-39565';
+$wp_version = '4.8-alpha-39577';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
