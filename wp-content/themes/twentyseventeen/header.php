405ff1e9fef012d871c97810569f0d73335cb968
Prajwal Koirala <LiveChief@users.noreply.github.com>
2016-12-11 19:11:25 -0500

    Updated header
    No need for "" on viewpoint

--- wp-content/themes/twentyseventeen/header.php
+++ wp-content/themes/twentyseventeen/header.php
@@ -16,7 +16,7 @@
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name=viewport content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
