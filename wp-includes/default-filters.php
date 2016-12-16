15ddaa269e35baf0b721abb2d886f701550d59c5
Weston Ruter <weston@xwp.co>
2016-12-16 07:29:42 +0000

    Customize: Bump `wp_custom_css_cb` from running at `wp_head` priority 11 to 101 to ensure Custom CSS overrides other CSS.
    Aligns `wp_head` action priority with Jetpack's Custom CSS.
    
    Amends [38829].
    See #35395, #38672.
    Fixes #39270.
    
    Built from https://develop.svn.wordpress.org/trunk@39616
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39556 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-filters.php
+++ wp-includes/default-filters.php
@@ -252,7 +252,7 @@ add_action( 'wp_head',             'wp_print_head_scripts',            9    );
 add_action( 'wp_head',             'wp_generator'                           );
 add_action( 'wp_head',             'rel_canonical'                          );
 add_action( 'wp_head',             'wp_shortlink_wp_head',            10, 0 );
-add_action( 'wp_head',             'wp_custom_css_cb',                11    );
+add_action( 'wp_head',             'wp_custom_css_cb',                101   );
 add_action( 'wp_head',             'wp_site_icon',                    99    );
 add_action( 'wp_footer',           'wp_print_footer_scripts',         20    );
 add_action( 'template_redirect',   'wp_shortlink_header',             11, 0 );
