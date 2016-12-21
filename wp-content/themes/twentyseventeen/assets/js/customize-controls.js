7d1db9607d2c54d256bb854010176e8c393df4c0
Gary Pendergast <gary@pento.net>
2016-12-21 05:48:34 +0000

    Twenty Seventeen: Ensure functions in `customize-controls.js` don't count on Customizer sections always being present
    Before, the code assumed the `theme_options` section is always present, but it can be removed by plugins. This way, if it is, no JavaScript errors occur.
    
    Merge of [39623] to the 4.7 branch.
    
    Props westonruter.
    Fixes #39355.
    
    
    Built from https://develop.svn.wordpress.org/branches/4.7@39632
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@39572 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyseventeen/assets/js/customize-controls.js
+++ wp-content/themes/twentyseventeen/assets/js/customize-controls.js
@@ -25,10 +25,12 @@
 		});
 
 		// Detect when the front page sections section is expanded (or closed) so we can adjust the preview accordingly.
-		wp.customize.section( 'theme_options' ).expanded.bind( function( isExpanding ) {
+		wp.customize.section( 'theme_options', function( section ) {
+			section.expanded.bind( function( isExpanding ) {
 
-			// Value of isExpanding will = true if you're entering the section, false if you're leaving it.
-			wp.customize.previewer.send( 'section-highlight', { expanded: isExpanding });
-		});
+				// Value of isExpanding will = true if you're entering the section, false if you're leaving it.
+				wp.customize.previewer.send( 'section-highlight', { expanded: isExpanding });
+			} );
+		} );
 	});
 })( jQuery );
