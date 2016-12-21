443bcb03bda9ddd6a9d718f0e1258aaf629add7d
Gary Pendergast <gary@pento.net>
2016-12-21 04:59:43 +0000

    Tests: Restore the database connection earlier when switching test groups.
    When plugins don't disable the `backupGlobals` PHPUnit option in their own tests, `$wpdb` is backed up and restored between classes of tests. The serialisation process used for this broke the database connection. This previously wasn't a problem, as it was reconnecting before each test.
    
    [38398] introduced some changes that required the connection to be available in `setUpBeforeClass()`, earlier than in was previously reconnecting. This didn't cause warnings in Core, but it did cause warnings for plugins that don't disable the `backupGlobals` option.
    
    The database connection now reconnects in `setUpBeforeClass()`. This change also fixes a few Core tests that weren't calling `parent::setUpBeforeClass()` or `parent::tearDown()` correctly.
    
    Fixes #39327.
    
    
    Built from https://develop.svn.wordpress.org/trunk@39626
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39566 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-39625';
+$wp_version = '4.8-alpha-39626';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
