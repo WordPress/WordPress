d16bdab87d182f983e23f67e99f3925cbef2b5a8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-01-04 13:21:41 +0000

    Docs: Correct `comment_email` duplicate hook reference in `wp-admin/includes/class-wp-comments-list-table.php`.
    Props keesiemeijer.
    See #39371.
    Built from https://develop.svn.wordpress.org/trunk@39669
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39609 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-comments-list-table.php
+++ wp-admin/includes/class-wp-comments-list-table.php
@@ -690,7 +690,7 @@ class WP_Comments_List_Table extends WP_List_Table {
 
 		if ( $this->user_can ) {
 			if ( ! empty( $comment->comment_author_email ) ) {
-				/* This filter is documented in wp-includes/comment-template.php */
+				/** This filter is documented in wp-includes/comment-template.php */
 				$email = apply_filters( 'comment_email', $comment->comment_author_email, $comment );
 
 				if ( ! empty( $email ) && '@' !== $email ) {
