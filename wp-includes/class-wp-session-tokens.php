b737cce4978ef84714909e2ea5e3889f43d9f22e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-01-04 13:22:42 +0000

    Docs: Add missing `session_token_manager` duplicate hook reference in `wp-includes/class-wp-session-tokens.php`.
    Props keesiemeijer.
    See #39371.
    Built from https://develop.svn.wordpress.org/trunk@39670
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39610 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-session-tokens.php
+++ wp-includes/class-wp-session-tokens.php
@@ -237,6 +237,7 @@ abstract class WP_Session_Tokens {
 	 * @static
 	 */
 	final public static function destroy_all_for_all_users() {
+		/** This filter is documented in wp-includes/class-wp-session-tokens.php */
 		$manager = apply_filters( 'session_token_manager', 'WP_User_Meta_Session_Tokens' );
 		call_user_func( array( $manager, 'drop_sessions' ) );
 	}
