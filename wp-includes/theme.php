cc4a68049392e0630d42b2072a843ab8d79e81dc
Dion Hulse <wordpress@dd32.id.au>
2017-01-05 08:15:35 +0000

    Customize: Ensure `theme_mod`-cache of `custom_css` lookup of `-1` short-circuits a `WP_Query` from being made.
    Props dlh, westonruter.
    See #35395.
    Merges [39688] to the 4.7 branch.
    Fixes #39259.
    
    Built from https://develop.svn.wordpress.org/branches/4.7@39694
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@39634 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/theme.php
+++ wp-includes/theme.php
@@ -1653,18 +1653,17 @@ function wp_get_custom_css_post( $stylesheet = '' ) {
 
 		if ( $post_id > 0 && get_post( $post_id ) ) {
 			$post = get_post( $post_id );
-		} else {
+		}
+
+		// `-1` indicates no post exists; no query necessary.
+		if ( ! $post && -1 !== $post_id ) {
 			$query = new WP_Query( $custom_css_query_vars );
 			$post = $query->post;
 			/*
-			 * Cache the lookup. See WP_Customize_Custom_CSS_Setting::update().
+			 * Cache the lookup. See wp_update_custom_css_post().
 			 * @todo This should get cleared if a custom_css post is added/removed.
 			 */
-			if ( $post ) {
-				set_theme_mod( 'custom_css_post_id', $post->ID );
-			} elseif ( -1 !== $post_id ) {
-				set_theme_mod( 'custom_css_post_id', -1 );
-			}
+			set_theme_mod( 'custom_css_post_id', $post ? $post->ID : -1 );
 		}
 	} else {
 		$query = new WP_Query( $custom_css_query_vars );
@@ -1788,9 +1787,15 @@ function wp_update_custom_css_post( $css, $args = array() ) {
 	} else {
 		$r = wp_insert_post( wp_slash( $post_data ), true );
 
-		// Trigger creation of a revision. This should be removed once #30854 is resolved.
-		if ( ! is_wp_error( $r ) && 0 === count( wp_get_post_revisions( $r ) ) ) {
-			wp_save_post_revision( $r );
+		if ( ! is_wp_error( $r ) ) {
+			if ( get_stylesheet() === $args['stylesheet'] ) {
+				set_theme_mod( 'custom_css_post_id', $r );
+			}
+
+			// Trigger creation of a revision. This should be removed once #30854 is resolved.
+			if ( 0 === count( wp_get_post_revisions( $r ) ) ) {
+				wp_save_post_revision( $r );
+			}
 		}
 	}
 
