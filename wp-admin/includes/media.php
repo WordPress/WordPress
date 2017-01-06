2e2570f669beab0bacc3cbe8b06e62e20b09d491
Joe McGill <joemcgill@gmail.com>
2017-01-06 22:00:06 +0000

    Media: Improved media titles when created from filename.
    Preserves spaces and generally creates more accurate, cleaner titles from filenames of uploaded media.
    
    Merge of [38615] to the 4.4 branch.
    
    Fixes #37989.
    
    Built from https://develop.svn.wordpress.org/branches/4.4@39712
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.4@39652 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/media.php
+++ wp-admin/includes/media.php
@@ -286,7 +286,7 @@ function media_handle_upload($file_id, $post_id, $post_data = array(), $override
 	$url = $file['url'];
 	$type = $file['type'];
 	$file = $file['file'];
-	$title = sanitize_title( $name );
+	$title = sanitize_text_field( $name );
 	$content = '';
 	$excerpt = '';
 
