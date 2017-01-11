edd582c69ee7b412a23145d7dfcc961b2c206981
Aaron Campbell <aaron@xavisys.com>
2017-01-11 16:58:53 +0000

    Bump 4.1 branch to version 4.1.14. Built from https://develop.svn.wordpress.org/branches/4.1@39866
    git-svn-id: http://core.svn.wordpress.org/branches/4.1@39803 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1.13';
+$wp_version = '4.1.14';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
