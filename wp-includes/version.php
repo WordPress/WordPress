871bb10c9ab02a7b47ca79695ca46352bc019a97
Jeremy Felt <jeremy.felt@gmail.com>
2017-01-11 05:31:35 +0000

    Multisite: Use `wp_rand()` in signup key creation.
    Merges [39795] to the 4.7 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.7@39796
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@39734 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7.1-RC1-39783';
+$wp_version = '4.7.1-RC1-39796';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
