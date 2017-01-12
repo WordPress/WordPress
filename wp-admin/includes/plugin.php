a989f0b1683ef4436cd27cbbe075ef28c7b6628f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-01-12 06:15:42 +0000

    Docs: Improve the DocBlock for `validate_plugin()`.
    Props theMikeD.
    Fixes #36333.
    Built from https://develop.svn.wordpress.org/trunk@39889
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39826 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/plugin.php
+++ wp-admin/includes/plugin.php
@@ -940,11 +940,11 @@ function validate_active_plugins() {
 /**
  * Validate the plugin path.
  *
- * Checks that the file exists and is a valid file. See validate_file().
+ * Checks that the main plugin file exists and is a valid plugin. See validate_file().
  *
  * @since 2.5.0
  *
- * @param string $plugin Plugin Path.
+ * @param string $plugin Path to the main plugin file from plugins directory.
  * @return WP_Error|int 0 on success, WP_Error on failure.
  */
 function validate_plugin($plugin) {
