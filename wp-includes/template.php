e836e47a8a352d6fe1a681f4dc4b99f69dfd6d35
Boone Gorges <boonebgorges@gmail.com>
2017-01-30 19:32:42 +0000

    Avoid PHP notices in `get_post_type_archive_template()`.
    The function should fail more gracefully when called in the context
    where `get_query_var( 'post_type' )` doesn't represent an actual post
    type.
    
    Props technopolitica, dlh.
    Fixes #38374.
    Built from https://develop.svn.wordpress.org/trunk@40031
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@39968 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/template.php
+++ wp-includes/template.php
@@ -135,8 +135,9 @@ function get_post_type_archive_template() {
 		$post_type = reset( $post_type );
 
 	$obj = get_post_type_object( $post_type );
-	if ( ! $obj->has_archive )
+	if ( ! ( $obj instanceof WP_Post_Type ) || ! $obj->has_archive ) {
 		return '';
+	}
 
 	return get_archive_template();
 }
