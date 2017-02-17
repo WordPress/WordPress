30cb64a0119d62eb7a1c659bcfa682272263a443
Dion Hulse <wordpress@dd32.id.au>
2017-02-17 06:46:42 +0000

    Media: Avoid PHP Warnings in `get_post_galleries()` when processing empty `[gallery]` shortcodes and avoid returning the incorrect results when the global `$post` does not match the provided post ID.
    Props dd32, joemcgill, seanchayes.
    Fixes #39277, #39304.
    
    Built from https://develop.svn.wordpress.org/trunk@40070
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40007 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40068';
+$wp_version = '4.8-alpha-40070';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
