ecf9c19e041b8180a446d5265d7a1b5ecf3ea3e9
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-02-24 23:13:34 +0000

    Plugins: After [38703], adjust the selector for checkbox selection to account for nested tables.
    Props afercia, swissspidy, reldev.
    Merges [40118] to the 4.7 branch.
    Fixes #39739.
    Built from https://develop.svn.wordpress.org/branches/4.7@40119
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@40056 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7.3-alpha-40117';
+$wp_version = '4.7.3-alpha-40119';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
