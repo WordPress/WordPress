a9f9235d88665698b272f9d3ab9fb24acbee34eb
Aaron Campbell <aaron@xavisys.com>
2017-03-06 13:42:04 +0000

    Strip control characters before validating redirect.
    Merges [40183] to 4.5 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.5@40186
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.5@40125 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -1271,7 +1271,7 @@ if ( !function_exists('wp_validate_redirect') ) :
  * @return string redirect-sanitized URL
  **/
 function wp_validate_redirect($location, $default = '') {
-	$location = trim( $location );
+	$location = trim( $location, " \t\n\r\0\x08\x0B" );
 	// browsers will assume 'http' is your protocol, and will obey a redirect to a URL starting with '//'
 	if ( substr($location, 0, 2) == '//' )
 		$location = 'http:' . $location;
