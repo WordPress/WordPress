a80351f7eda6a398df16d7e292d8dea784ed9d06
Jeremy Felt <jeremy.felt@gmail.com>
2017-03-06 08:03:35 +0000

    Validate video and audio metadata.
    merge of [40148] to the 4.7 branch
    
    Built from https://develop.svn.wordpress.org/branches/4.7@40149
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@40088 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/media.php
+++ wp-admin/includes/media.php
@@ -3072,6 +3072,8 @@ function wp_read_video_metadata( $file ) {
 
 	wp_add_id3_tag_data( $metadata, $data );
 
+	$metadata = wp_kses_post_deep( $metadata );
+
 	return $metadata;
 }
 
@@ -3116,6 +3118,8 @@ function wp_read_audio_metadata( $file ) {
 		$metadata['length_formatted'] = $data['playtime_string'];
 
 	wp_add_id3_tag_data( $metadata, $data );
+
+	$metadata = wp_kses_post_deep( $metadata );
 
 	return $metadata;
 }
