dfb508f59dd1a778226262b3575a6809527d082e
Aaron Campbell <aaron@xavisys.com>
2017-03-06 13:40:35 +0000

    Strip control characters before validating redirect.
    Merges [40183] to 4.7 branch.
    
    
    Built from https://develop.svn.wordpress.org/branches/4.7@40184
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@40123 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7.3-RC1-40182';
+$wp_version = '4.7.3-RC1-40184';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
