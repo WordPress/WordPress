f2ef35f4a9e4695f9fd25b4be8ab28c7d2f9cf8f
James Nylen <jnylen@gmail.com>
2017-03-06 16:42:15 +0000

    Bump 3.9 branch to version 3.9.17.
    Built from https://develop.svn.wordpress.org/branches/3.9@40210
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.9@40149 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.9.16';
+$wp_version = '3.9.17';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
