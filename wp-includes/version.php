d2c4cb36dbf237ec8d4d517faf659c8b9675f723
John Blackbourn <johnbillion@git.wordpress.org>
2017-03-07 01:35:33 +0000

    Build/Test Tools: Call `wp_head()` and `wp_footer()` in the theme used during tests.
    See #31550
    Fixes #39988
    
    Merges [40235] to the 4.7 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.7@40236
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@40166 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7.4-alpha-40225';
+$wp_version = '4.7.4-alpha-40236';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
