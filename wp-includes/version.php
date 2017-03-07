7b45ddd449a46f7eae2a2f60b55c29f7e2e849c9
John Blackbourn <johnbillion@git.wordpress.org>
2017-03-07 00:21:32 +0000

    Build/Test Tools: Disable Xdebug when testing on Travis to increase performance.
    See #39978
    
    Built from https://develop.svn.wordpress.org/branches/4.7@40225
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@40164 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7.4-alpha-40224';
+$wp_version = '4.7.4-alpha-40225';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
