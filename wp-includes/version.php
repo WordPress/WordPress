92357b6a80f909a52cf1e47b88384c73b6ed8ac3
John Blackbourn <johnbillion@git.wordpress.org>
2017-03-08 00:52:36 +0000

    Build/Test tools: In Travis, skip some tests when not on trunk.
    This skips time sensitive tests (copyright year and PHP/MySQL version requirements) when tests are run on branches on Travis.
    
    Props netweb, jorbin
    
    Fixes #39486
    
    Merges [40241] to the 4.7 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.7@40250
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@40172 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7.4-alpha-40236';
+$wp_version = '4.7.4-alpha-40250';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
