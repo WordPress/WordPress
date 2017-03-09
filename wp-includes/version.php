f79e867a96a971957189b393c9c287fe9844a72d
John Blackbourn <johnbillion@git.wordpress.org>
2017-03-09 19:14:44 +0000

    Build/Test tools: Explicitly use PHPUnit 5.7 for the PHP 7 builds on Travis.
    This avoids fatal errors while the tests remain incompatible with PHPUnit 6, which was recently introduced on Travis.
    
    See #39822, #40086
    
    Built from https://develop.svn.wordpress.org/trunk@40255
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40177 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40254';
+$wp_version = '4.8-alpha-40255';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
