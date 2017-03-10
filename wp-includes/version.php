d675e30e51cd90aebb3c4d6c8be871f64b279fe0
John Blackbourn <johnbillion@git.wordpress.org>
2017-03-10 15:31:41 +0000

    Build/Test tools: Don't attempt to report PHP's extensions when running HHVM jobs on Travis. It doesn't work.
    Built from https://develop.svn.wordpress.org/trunk@40267
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40186 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40264';
+$wp_version = '4.8-alpha-40267';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
