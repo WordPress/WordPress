d4350bc06554e354c68e14fb90846548bb009a0d
Boone Gorges <boonebgorges@gmail.com>
2017-03-14 18:42:43 +0000

    Fix the formatting of `$taxonomies` parameter of `'wp_get_object_terms'` filter.
    [38667] changed the way that the filter parameters are built. That
    changeset didn't fully account for the pre-4.7 format of `$taxonomies`.
    
    Props ig_communitysites.
    Fixes #40154.
    Built from https://develop.svn.wordpress.org/trunk@40290
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40197 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40284';
+$wp_version = '4.8-alpha-40290';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
