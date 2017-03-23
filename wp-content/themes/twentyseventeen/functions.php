090901009bae3944af3b79d1d5f2df1ea85d66a6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-03-23 16:22:44 +0000

    Twenty Seventeen: Declare jQuery as a dependency for `navigation.js`.
    Props chesio.
    Fixes #40224.
    Built from https://develop.svn.wordpress.org/trunk@40315
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40222 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyseventeen/functions.php
+++ wp-content/themes/twentyseventeen/functions.php
@@ -441,7 +441,7 @@ function twentyseventeen_scripts() {
 	);
 
 	if ( has_nav_menu( 'top' ) ) {
-		wp_enqueue_script( 'twentyseventeen-navigation', get_theme_file_uri( '/assets/js/navigation.js' ), array(), '1.0', true );
+		wp_enqueue_script( 'twentyseventeen-navigation', get_theme_file_uri( '/assets/js/navigation.js' ), array( 'jquery' ), '1.0', true );
 		$twentyseventeen_l10n['expand']         = __( 'Expand child menu', 'twentyseventeen' );
 		$twentyseventeen_l10n['collapse']       = __( 'Collapse child menu', 'twentyseventeen' );
 		$twentyseventeen_l10n['icon']           = twentyseventeen_get_svg( array( 'icon' => 'angle-down', 'fallback' => true ) );
