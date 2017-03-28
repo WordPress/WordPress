fa5f2d1fa29532ef27d106e5baf342e0b41945b0
Jeremy Felt <jeremy.felt@gmail.com>
2017-03-28 01:46:45 +0000

    Tests: Add missing global cache groups.
    Add `useremail`, `userslugs`, `networks`, `sites`, and `site-details` as global cache groups when added with unit tests. This aligns the list with the core default.
    
    Fixes #40283.
    
    Built from https://develop.svn.wordpress.org/trunk@40343
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40250 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40342';
+$wp_version = '4.8-alpha-40343';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
