4430527126d8fe3387ac7829f24cb8f28e2e4327
Pascal Birchler <pascal.birchler@gmail.com>
2017-04-06 18:20:33 +0000

    Build/Test Tools: Add assertNotFalse() method to WP_UnitTestCase and use it where appropriate.
    Props peterwilsoncc.
    Fixes #39219.
    
    Merges [39919] to the 4.7 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.7@40388
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@40295 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7.4-alpha-40386';
+$wp_version = '4.7.4-alpha-40388';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
