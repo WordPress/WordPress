4e1f22bf78e832f2d71d073126a2a2b103e45d1f
Joe McGill <joemcgill@gmail.com>
2017-04-10 01:28:47 +0000

    Fix broken audio/video functions when sanitizing ID3 data
    This fixes a bug where running `wp_kses_post_deep()` on all the ID3
    tag data corrupted blob data.
    
    Fixes #40075, #40085.
    
    Built from https://develop.svn.wordpress.org/trunk@40400
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40307 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40399';
+$wp_version = '4.8-alpha-40400';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
