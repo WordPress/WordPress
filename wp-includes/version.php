927bd9aeb8d061940498d75977e1d15a69c982bc
John Blackbourn <johnbillion@git.wordpress.org>
2017-04-11 10:48:43 +0000

    Build/Test tools: Improve the failure message reported by `assertQueryTrue()` so it's a little less cryptic.
    Fixes #40411
    
    Built from https://develop.svn.wordpress.org/trunk@40416
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40314 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40406';
+$wp_version = '4.8-alpha-40416';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
