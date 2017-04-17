e5c05e18a1c54e4189b86b9a8316304d03fbacd1
Pascal Birchler <pascal.birchler@gmail.com>
2017-04-17 10:09:43 +0000

    Twenty Seventeen: Correct heading hierarchy for posts on the front page.
    When the posts page is on the front page or within a front page section, the heading hierarchy for the individual post titles needs to be adjusted accordingly.
    
    Props joedolson, celloexpressions, davidakennedy.
    Fixes #40264.
    
    Built from https://develop.svn.wordpress.org/trunk@40458
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40334 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyseventeen/template-parts/post/content-gallery.php
+++ wp-content/themes/twentyseventeen/template-parts/post/content-gallery.php
@@ -33,6 +33,8 @@
 
 		if ( is_single() ) {
 			the_title( '<h1 class="entry-title">', '</h1>' );
+		} elseif ( is_front_page() && is_home() ) {
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 		} else {
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		}
