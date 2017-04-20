1897b61ccbd542f6c8b11169a190793d40539a15
Pascal Birchler <pascal.birchler@gmail.com>
2017-04-20 16:25:27 +0000

    Bump 4.3 branch to version 4.3.10.
    Built from https://develop.svn.wordpress.org/branches/4.3@40491
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.3@40367 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -108,7 +108,11 @@ $tech_features = array(
 		</h2>
 
 		<div class="changelog point-releases">
-			<h3><?php echo _n( 'Maintenance and Security Release', 'Maintenance and Security Releases', 9 ); ?></h3>
+			<h3><?php echo _n( 'Maintenance and Security Release', 'Maintenance and Security Releases', 10 ); ?></h3>
+			<p><?php printf( _n( '<strong>Version %1$s</strong> addressed %2$s bug.',
+					'<strong>Version %1$s</strong> addressed %2$s bugs.', 1 ), '4.3.10', number_format_i18n( 1 ) ); ?>
+				<?php printf( __( 'For more information, see <a href="%s">the release notes</a>.' ), 'https://codex.wordpress.org/Version_4.3.10' ); ?>
+			</p>
 			<p><?php printf( _n( '<strong>Version %1$s</strong> addressed a security issue.',
 					'<strong>Version %1$s</strong> addressed some security issues.', 6 ), '4.3.9' ); ?>
 				<?php printf( __( 'For more information, see <a href="%s">the release notes</a>.' ), 'https://codex.wordpress.org/Version_4.3.9' ); ?>
