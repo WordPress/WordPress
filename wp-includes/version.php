aa8f871f21eb8b789fb78dd9fb5484228b4963e9
John Blackbourn <johnbillion@git.wordpress.org>
2017-04-22 18:26:42 +0000

    Build/Test Tools: Avoid skipping canonical tests that are connected to open Trac tickets.
    This is a remnant of the test suite behaviour before #30284 fixed most of it and makes it far too easy to hide failing tests.
    
    If any tests begin to fail as a result of this change then they should be fixed.
    
    Fixes #40534
    
    Built from https://develop.svn.wordpress.org/trunk@40518
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40394 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40517';
+$wp_version = '4.8-alpha-40518';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
