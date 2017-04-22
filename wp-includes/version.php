dd0346b3309e84f9b0a1f9c1ab41e1e30f8e4d6f
John Blackbourn <johnbillion@git.wordpress.org>
2017-04-22 17:30:44 +0000

    Canonical: Don't skip tests if the test data is invalid.
    See #40533
    
    Built from https://develop.svn.wordpress.org/trunk@40517
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40393 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40516';
+$wp_version = '4.8-alpha-40517';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
