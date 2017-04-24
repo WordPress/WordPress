d3c74e8ce9fa4651457775074d9c7d25092fc02e
John Blackbourn <johnbillion@git.wordpress.org>
2017-04-24 01:23:43 +0000

    Feeds: Remove an incorrect usage of `sizeof()` in a helper class used during unit testing of XML element handling.
    This also helps tidy up the test output when running PHP 7.2.
    
    See #40109
    
    Built from https://develop.svn.wordpress.org/trunk@40555
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40424 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40546';
+$wp_version = '4.8-alpha-40555';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
