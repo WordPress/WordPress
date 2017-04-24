799bdcec005b9074dfbb56e5263a2c89cd0b84b0
John Blackbourn <johnbillion@git.wordpress.org>
2017-04-24 00:38:35 +0000

    Build/Test Tools: Backport various recent changes to the 4.7 branch.
    * Add support for PHPUnit 6+.
    * Add Composer files to the cache on Travis.
    * Remove HHVM from the test infrastructure on Travis.
    
    Merges [40536], [40538], [40539], and [40546] to the 4.7 branch.
    
    See #40539
    Fixes #39822, #40548
    
    Built from https://develop.svn.wordpress.org/branches/4.7@40547
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@40423 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7.5-alpha-40514';
+$wp_version = '4.7.5-alpha-40547';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
