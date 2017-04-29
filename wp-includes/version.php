53acb5b6d46b31cea71bc5f00537322c538052d9
John Blackbourn <johnbillion@git.wordpress.org>
2017-04-29 21:16:45 +0000

    Posts, Post Types: Correct the attachment post type template unit test introduced in [38951].
    Also adds additional assertions for the templates.
    
    See #18375
    
    Built from https://develop.svn.wordpress.org/trunk@40558
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40427 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40557';
+$wp_version = '4.8-alpha-40558';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
