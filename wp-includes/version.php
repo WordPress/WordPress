224b88ca42e50316aa81a9e4f34cc35216c716af
John Blackbourn <johnbillion@git.wordpress.org>
2017-04-30 11:19:45 +0000

    Posts, Post Types: Revert [40558] pending investigation.
    See #18375
    
    Built from https://develop.svn.wordpress.org/trunk@40559
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40428 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40558';
+$wp_version = '4.8-alpha-40559';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
