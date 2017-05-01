282408b6c09ea4909e64a9ee80ba1f59c7ab99dd
John Blackbourn <johnbillion@git.wordpress.org>
2017-05-01 16:19:44 +0000

    Users: Add two missing special cases to the capability tests for non-logged-in users.
    Also updates a `expectedIncorrectUsage` notation.
    
    See #37405
    
    Built from https://develop.svn.wordpress.org/trunk@40563
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40432 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40562';
+$wp_version = '4.8-alpha-40563';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
