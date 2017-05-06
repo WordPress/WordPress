7fd55738bec25712f103e32de6b073ef4b3bed95
Pascal Birchler <pascal.birchler@gmail.com>
2017-05-06 14:37:44 +0000

    Administration: Remove unit and prefixed version from a CSS declaration.
    The prefixed version gets added by Autoprefixer already.
    
    Props MatheusFD.
    Fixes #35358.
    
    Built from https://develop.svn.wordpress.org/trunk@40576
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40446 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40575';
+$wp_version = '4.8-alpha-40576';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
