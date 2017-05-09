314f2aececf1134e66eb8e1038a7e27ebfe3459d
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-05-09 16:02:46 +0000

    Multisite: Add missing documentation for new `$network_id` parameter of `wp_maybe_update_network_site_counts()`.
    This was overlooked in [40591].
    
    See #40384.
    
    Built from https://develop.svn.wordpress.org/trunk@40592
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40462 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-functions.php
+++ wp-includes/ms-functions.php
@@ -2301,6 +2301,8 @@ function wp_update_network_counts( $network_id = null ) {
  *
  * @since 3.7.0
  * @since 4.8.0 The $network_id parameter has been added.
+ *
+ * @param int|null $network_id ID of the network. Default is the current network.
  */
 function wp_maybe_update_network_site_counts( $network_id = null ) {
 	$is_small_network = ! wp_is_large_network( 'sites', $network_id );
