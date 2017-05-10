051f29186194f0d7ca5c438fcd003b2404063420
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-05-10 23:03:43 +0000

    Multisite: Fix unstable unit test for `get_blog_count()`.
    The unit test introduced in [40370] used the strict `assertSame()` check while comparison a count with the value of an option, which under most conditions is not an integer. While the test passed on some setups, it failed on others. This changeset ensures that `assertEquals()` is used to make the test stable.
    
    Fixes #40724.
    
    Built from https://develop.svn.wordpress.org/trunk@40610
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40480 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40609';
+$wp_version = '4.8-alpha-40610';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
