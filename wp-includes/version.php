30bdd11fd5cc9508d640caf108a3a3460dcd5c12
Gary Pendergast <gary@pento.net>
2017-05-10 10:29:44 +0000

    Build/Test: Post Travis results to Slack from `WordPress/wordpress-develop`
    Now that the `WordPress/wordpress-develop` GitHub repo is syncing correctly, we can use it for Travis integration.
    
    Props jorbin for getting the ball rolling so long ago, unprops jorbin because his Travis build can finally be retired.
    
    Fixes #40712.
    
    
    Built from https://develop.svn.wordpress.org/trunk@40604
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40474 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40603';
+$wp_version = '4.8-alpha-40604';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
