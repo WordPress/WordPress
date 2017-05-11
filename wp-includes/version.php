0cd74a6d6e8e1fd26753081eb8186963af927a5a
Dominik Schilling <dominikschilling+git@gmail.com>
2017-05-11 19:16:42 +0000

    Plugins: Don't strip line breaks and blockquote elements in the plugin information modal.
    Fixes #40679.
    Built from https://develop.svn.wordpress.org/trunk@40633
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40494 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8-alpha-40632';
+$wp_version = '4.8-alpha-40633';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
