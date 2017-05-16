454a0edba6be851c450fdcfebf1f74cd42377dbb
Aaron Campbell <aaron@xavisys.com>
2017-05-16 21:54:06 +0000

    Bump 3.8 branch to version 3.8.21. Built from https://develop.svn.wordpress.org/branches/3.8@40757
    git-svn-id: http://core.svn.wordpress.org/branches/3.8@40615 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.8.20';
+$wp_version = '3.8.21';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
