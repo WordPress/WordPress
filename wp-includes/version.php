58075bfc88a459066761ddb775b661c38ca315b0
Weston Ruter <weston@xwp.co>
2017-05-16 14:37:35 +0000

    Customize: Fix phpunit tests after [40704] due to logic inversion error.
    Merge of [40716] to the 4.7 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.7@40717
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@40580 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7.5-alpha-40705';
+$wp_version = '4.7.5-alpha-40717';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
