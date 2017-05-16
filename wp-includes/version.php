2d7fa9d0dc348ec810fbee25351c1c2bd24f8d85
Dominik Schilling <dominikschilling+git@gmail.com>
2017-05-16 12:14:35 +0000

    Customize: Ignore invalid customization sessions.
    Merge of [40704] to the 4.7 branch.
    Built from https://develop.svn.wordpress.org/branches/4.7@40705
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.7@40568 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7.5-alpha-40693';
+$wp_version = '4.7.5-alpha-40705';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
