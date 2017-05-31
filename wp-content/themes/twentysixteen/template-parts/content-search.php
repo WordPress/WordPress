e095e7b9d3b06b7b893117da83e8e9f88cde9217
Konstantin Obenland <obenland@automattic.com>
2017-05-31 01:36:31 +0000

    Import Twenty Sixteen, default theme for 2016.
    See #36497.
    
    Merges [40851] and [40852] to the 4.4 branch.
    
    
    Built from https://develop.svn.wordpress.org/branches/4.4@40860
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.4@40710 1a063a9b-81f0-0310-95a4-ce76da25c4cd

---
+++ wp-content/themes/twentysixteen/template-parts/content-search.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template part for displaying results in search pages
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php twentysixteen_post_thumbnail(); ?>
+
+	<?php twentysixteen_excerpt(); ?>
+
+	<?php if ( 'post' === get_post_type() ) : ?>
+
+		<footer class="entry-footer">
+			<?php twentysixteen_entry_meta(); ?>
+			<?php
+				edit_post_link(
+					sprintf(
+						/* translators: %s: Name of current post */
+						__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+						get_the_title()
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+			?>
+		</footer><!-- .entry-footer -->
+
+	<?php else : ?>
+
+		<?php
+			edit_post_link(
+				sprintf(
+					/* translators: %s: Name of current post */
+					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+					get_the_title()
+				),
+				'<footer class="entry-footer"><span class="edit-link">',
+				'</span></footer><!-- .entry-footer -->'
+			);
+		?>
+
+	<?php endif; ?>
+</article><!-- #post-## -->
+
