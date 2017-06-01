ff8af70bf109cdc3b4844eff886e1e18f0c003c3
Ella Iseulde Van Dorpe <*@iseulde.com>
2017-06-01 22:08:40 +0000

    Trunk is really 4.9-alpha now Built from https://develop.svn.wordpress.org/trunk@40871
    git-svn-id: http://core.svn.wordpress.org/trunk@40721 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-40870';
+$wp_version = '4.9-alpha-40871';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
