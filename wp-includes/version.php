39d2d9f19e705aa58fb140b354bc58e7d7ff454f
Drew Jaynes <info@drewapicture.com>
2017-06-13 03:11:40 +0000

    Docs: Include 'parent' as an acceptable entry for the `$orderby` argument in `WP_Term_Query`.
    h/t kalor.
    See #41017.
    
    Built from https://develop.svn.wordpress.org/trunk@40900
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40750 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-40899';
+$wp_version = '4.9-alpha-40900';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
