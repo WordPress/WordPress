6a3be0ea29ac6e942e100e8656d70852a4f8cd95
Dominik Schilling <dominikschilling+git@gmail.com>
2017-06-15 10:20:42 +0000

    Twenty Seventeen: Remove/merge unused variables in content-front-page.php.
    #wceu2017
    
    Props dingo_bastard, mariusvetrici.
    Fixes #39771.
    Built from https://develop.svn.wordpress.org/trunk@40904
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40754 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyseventeen/template-parts/page/content-front-page.php
+++ wp-content/themes/twentyseventeen/template-parts/page/content-front-page.php
@@ -14,12 +14,8 @@
 	<?php if ( has_post_thumbnail() ) :
 		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'twentyseventeen-featured-image' );
 
-		$post_thumbnail_id = get_post_thumbnail_id( $post->ID );
-
-		$thumbnail_attributes = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'twentyseventeen-featured-image' );
-
 		// Calculate aspect ratio: h / w * 100%.
-		$ratio = $thumbnail_attributes[2] / $thumbnail_attributes[1] * 100;
+		$ratio = $thumbnail[2] / $thumbnail[1] * 100;
 		?>
 
 		<div class="panel-image" style="background-image: url(<?php echo esc_url( $thumbnail[0] ); ?>);">
