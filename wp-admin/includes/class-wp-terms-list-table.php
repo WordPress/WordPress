01a6f4d38edbc3559266a8f9be2159e4613ed9d4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-06-27 00:34:41 +0000

    Administration: In taxonomy list tables, display a dash instead of a blank space and add "No description" as a screen reader text when the term description is blank.
    Props pratikgandhi, yahil.
    Fixes #40659.
    Built from https://develop.svn.wordpress.org/trunk@40957
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40807 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-terms-list-table.php
+++ wp-admin/includes/class-wp-terms-list-table.php
@@ -501,7 +501,11 @@ class WP_Terms_List_Table extends WP_List_Table {
 	 * @return string
 	 */
 	public function column_description( $tag ) {
-		return $tag->description;
+		if ( $tag->description ) {
+			return $tag->description;
+		} else {
+			return '<span aria-hidden="true">&#8212;</span><span class="screen-reader-text">' . __( 'No description' ) . '</span>';
+		}
 	}
 
 	/**
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-40956';
+$wp_version = '4.9-alpha-40957';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
