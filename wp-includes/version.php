37cbe7add105b87e9b338c2adbb6d9c26089bcc1
Weston Ruter <weston@xwp.co>
2017-07-11 22:47:43 +0000

    Widgets: Enqueue assets needed by media and text widgets in their `_register_one()` methods.
    The `WP_Widget::_register_one()` method is more guaranteed to be called as opposed to its wrapper `WP_Widget::_register()` which plugins may bypass for performance reasons.
    
    Amends [40631], [40640].
    See #35243, #32417.
    Fixes #41021.
    
    Built from https://develop.svn.wordpress.org/trunk@41028
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40878 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-41027';
+$wp_version = '4.9-alpha-41028';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
