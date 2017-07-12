581707de6cd0d94b3edc274dd3ca5a3193869562
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-07-12 21:40:40 +0000

    Docs: Add a `@global` entry for `$allowed_files` in `get_file_description()`.
    Props div33, ajayghaghretiya1.
    Fixes #41294.
    Built from https://develop.svn.wordpress.org/trunk@41032
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40882 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/file.php
+++ wp-admin/includes/file.php
@@ -69,7 +69,8 @@ $wp_file_descriptions = array(
  *
  * @since 1.5.0
  *
- * @global array $wp_file_descriptions
+ * @global array $wp_file_descriptions Theme file descriptions.
+ * @global array $allowed_files        List of allowed files. 
  * @param string $file Filesystem path or filename
  * @return string Description of file from $wp_file_descriptions or basename of $file if description doesn't exist.
  *                Appends 'Page Template' to basename of $file if the file is a page template
