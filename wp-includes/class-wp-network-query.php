d4607c17b7440bea4c32c64d5f9d862a3b0a553f
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-07-17 20:16:47 +0000

    Multisite: Improve caching in `WP_Network_Query` by ignoring the `$fields` argument.
    Prior to this change there were two different cache keys used for the same query. That is because regardless of the `$fields` argument, the query response will be the same. This was already fixed for `WP_Site_Query` in [41059].
    
    Props spacedmonkey.
    Fixes #41347.
    
    Built from https://develop.svn.wordpress.org/trunk@41063
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40915 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-network-query.php
+++ wp-includes/class-wp-network-query.php
@@ -208,7 +208,12 @@ class WP_Network_Query {
 		do_action_ref_array( 'pre_get_networks', array( &$this ) );
 
 		// $args can include anything. Only use the args defined in the query_var_defaults to compute the key.
-		$key = md5( serialize( wp_array_slice_assoc( $this->query_vars, array_keys( $this->query_var_defaults ) ) ) );
+		$_args = wp_array_slice_assoc( $this->query_vars, array_keys( $this->query_var_defaults ) );
+
+		// Ignore the $fields argument as the queried result will be the same regardless.
+		unset( $_args['fields'] );
+
+		$key = md5( serialize( $_args ) );
 		$last_changed = wp_cache_get_last_changed( 'networks' );
 
 		$cache_key = "get_network_ids:$key:$last_changed";
