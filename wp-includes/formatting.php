3946198df881eb17245f5ea1321dc3331e39e6e5
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-07-20 13:38:44 +0000

    Docs: Add `@staticvar` entries for `get_html_split_regex()` and `_get_wptexturize_split_regex()`.
    Props ajayghaghretiya1.
    Fixes #41385.
    Built from https://develop.svn.wordpress.org/trunk@41109
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40949 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -609,6 +609,8 @@ function wp_html_split( $input ) {
  *
  * @since 4.4.0
  *
+ * @staticvar string $regex
+ *
  * @return string The regular expression
  */
 function get_html_split_regex() {
@@ -665,6 +667,8 @@ function get_html_split_regex() {
  * @ignore
  * @internal This function will be removed in 4.5.0 per Shortcode API Roadmap.
  * @since 4.4.0
+ *
+ * @staticvar string $html_regex
  *
  * @param string $shortcode_regex The result from _get_wptexturize_shortcode_regex().  Optional.
  * @return string The regular expression
