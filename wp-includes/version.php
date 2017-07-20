fc475c2d71b02409a98d15ac6738434c3bd7e9b9
Weston Ruter <weston@xwp.co>
2017-07-20 00:09:35 +0000

    WordPress 4.8.1 Beta 1
    Built from https://develop.svn.wordpress.org/branches/4.8@41097
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.8@40937 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8';
+$wp_version = '4.8.1-beta1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
