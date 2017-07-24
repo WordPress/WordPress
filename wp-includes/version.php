6be40b7a13fdc44f373feea6b26f56a63e2a6efb
Weston Ruter <weston@xwp.co>
2017-07-24 20:46:38 +0000

    WP-API JS Client: Interpret Settings resource as a model.
    The REST API does not provide a mechanism to distinguish between endpoints representing models and those representing collections, so the Backbone client must make that distinction internally. Previously wp-api.js accounted for `/users/me`, but not for `/settings`. This patch updates the logic so that `/settings` is properly registered as a Backbone model.
    
    When calling `wp.api.init`, additional endpoints can be specified to be models using the `modelEndpoints` argument.
    
    Merges [41112] onto 4.8 branch.
    Props adamsilverstein, kadamwhite.
    Fixes #41056 for 4.8.1.
    
    Built from https://develop.svn.wordpress.org/branches/4.8@41126
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.8@40966 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8.1-beta1-41117';
+$wp_version = '4.8.1-beta1-41126';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
