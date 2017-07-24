030d6166f9ab8b70556f8403f098ab02fa5e6f26
John Blackbourn <johnbillion@git.wordpress.org>
2017-07-24 22:25:32 +0000

    General: Backport PHP 7.1 fixes to the 4.5 branch to avoid fatal errors and warnings.
    Props simonvik, ayeshrajans
    
    See #41135
    
    Built from https://develop.svn.wordpress.org/branches/4.5@41128
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.5@40968 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-oembed.php
+++ wp-includes/class-oembed.php
@@ -221,6 +221,7 @@ class WP_oEmbed {
 	 * @return false|string False on failure, otherwise the oEmbed provider URL.
 	 */
 	public function get_provider( $url, $args = '' ) {
+		$args = wp_parse_args( $args );
 
 		$provider = false;
 
@@ -315,6 +316,8 @@ class WP_oEmbed {
 	 * @return false|string False on failure, otherwise the UNSANITIZED (and potentially unsafe) HTML that should be used to embed.
 	 */
 	public function get_html( $url, $args = '' ) {
+		$args = wp_parse_args( $args );
+
 		/**
 		 * Filters the oEmbed result before any HTTP requests are made.
 		 *
