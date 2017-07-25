de31d6f81c302c86ff0544130c04c15c2974ffde
Andrea Fercia <a.fercia@gmail.com>
2017-07-25 16:13:45 +0000

    Widgets: make the Recent Posts widget output '(no title)' for posts without a title.
    Since WordPress 2.3, the Recent Posts widget used to output the post ID for posts
    with an empty title. Using '(no title)' seems a best option and is consistent with
    what's already used in other similar cases.
    
    Props antonrinas, Ankit K Gupta.
    Fixes #35709.
    
    Built from https://develop.svn.wordpress.org/trunk@41149
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@40989 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/widgets/class-wp-widget-recent-posts.php
+++ wp-includes/widgets/class-wp-widget-recent-posts.php
@@ -82,7 +82,7 @@ class WP_Widget_Recent_Posts extends WP_Widget {
 		<ul>
 		<?php while ( $r->have_posts() ) : $r->the_post(); ?>
 			<li>
-				<a href="<?php the_permalink(); ?>"><?php get_the_title() ? the_title() : the_ID(); ?></a>
+				<a href="<?php the_permalink(); ?>"><?php get_the_title() ? the_title() : _e( '(no title)' ); ?></a>
 			<?php if ( $show_date ) : ?>
 				<span class="post-date"><?php echo get_the_date(); ?></span>
 			<?php endif; ?>
