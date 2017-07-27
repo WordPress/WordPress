96183d09bdfc53e6ae71bed60d6e4668bde59874
John Blackbourn <johnbillion@git.wordpress.org>
2017-07-27 16:06:55 +0000

    Formatting: Update the author description tests since the formatting change made in [41172].
    See #40040
    
    Built from https://develop.svn.wordpress.org/trunk@41173
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41013 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-41172';
+$wp_version = '4.9-alpha-41173';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
