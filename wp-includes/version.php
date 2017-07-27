01e64d9ed0cdbd415ad8699352b8bd5714c2d338
John Blackbourn <johnbillion@git.wordpress.org>
2017-07-27 15:10:42 +0000

    Formatting: Add formatting and texturization to author descriptions so they match the formatting of term descriptions.
    Props henry.wright
    
    Fixes #40040
    
    Built from https://develop.svn.wordpress.org/trunk@41172
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41012 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-41171';
+$wp_version = '4.9-alpha-41172';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
