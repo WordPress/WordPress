546dcf7fed9f2b96d585dac991ba7fbee9c1f396
Weston Ruter <weston@xwp.co>
2017-08-01 22:21:32 +0000

    WordPress 4.8.1 RC2.
    Built from https://develop.svn.wordpress.org/branches/4.8@41202
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.8@41042 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8.1-RC1-41201';
+$wp_version = '4.8.1-RC2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
