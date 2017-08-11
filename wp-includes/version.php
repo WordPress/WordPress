f42a3a2440805a4d98806c32796de5b2ad3d8fa0
Adam Silverstein <adamsilverstein@earthboundhosting.com>
2017-08-11 20:37:45 +0000

    Docs: Improve JavaScript documentation in audiovideo.manifest.js.
    Add and improve JSDOC blocks.
    
    Props nicollle, manuelaugustin.
    Fixes #41602.
    
    Built from https://develop.svn.wordpress.org/trunk@41239
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41079 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-41238';
+$wp_version = '4.9-alpha-41239';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
