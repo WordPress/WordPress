01be3075b68d93cd0d0cf270134d6e5556c30648
Gary Pendergast <gary@pento.net>
2017-08-18 09:58:46 +0000

    Tests: Rename tests with underscore in the name
    There were a handful of files with an underscore in the name, which violated our naming scheme, and caused problems for anyone how enforced the scheme on their own systems.
    
    This commit renames all of the files to the correct camelCase scheme.
    
    Fixes #41658.
    
    
    Built from https://develop.svn.wordpress.org/trunk@41261
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41101 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-41260';
+$wp_version = '4.9-alpha-41261';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
