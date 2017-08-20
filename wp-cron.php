ffa7e7d4abb6f112dcfe7d8c20c556c77099110a
Drew Jaynes <info@drewapicture.com>
2017-08-20 04:37:45 +0000

    Docs: Improve the usefulness and accuracy of the file header for wp-cron.php.
    Props stevenlinx for the initial patch.
    Fixes #31401.
    
    Built from https://develop.svn.wordpress.org/trunk@41276
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41116 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-cron.php
+++ wp-cron.php
@@ -1,7 +1,14 @@
 <?php
 /**
- * WordPress Cron Implementation for hosts, which do not offer CRON or for which
- * the user has not set up a CRON job pointing to this file.
+ * A pseudo-CRON daemon for scheduling WordPress tasks
+ *
+ * WP Cron is triggered when the site receives a visit. In the scenario
+ * where a site may not receive enough visits to execute scheduled tasks
+ * in a timely manner, this file can be called directly or via a server
+ * CRON daemon for X number of times.
+ *
+ * Defining DISABLE_WP_CRON as true and calling this file directly are
+ * mutually exclusive and the latter does not rely on the former to work.
  *
  * The HTTP request to this file will not slow down the visitor who happens to
  * visit when the cron job is needed to run.
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-41275';
+$wp_version = '4.9-alpha-41276';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
