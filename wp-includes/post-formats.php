f94c77661d10536c1dd77913ad0155bed9542250
Drew Jaynes <info@drewapicture.com>
2017-08-20 20:03:54 +0000

    Docs: Improve the return description for `get_post_format_slugs()` to note that the array contains slugs as both keys and values.
    Props danielbachhuber.
    Fixes #38611.
    
    Built from https://develop.svn.wordpress.org/trunk@41284
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41124 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post-formats.php
+++ wp-includes/post-formats.php
@@ -102,11 +102,11 @@ function get_post_format_strings() {
 }
 
 /**
- * Retrieves an array of post format slugs.
+ * Retrieves the array of post format slugs.
  *
  * @since 3.1.0
  *
- * @return array The array of post format slugs.
+ * @return array The array of post format slugs as both keys and values.
  */
 function get_post_format_slugs() {
 	$slugs = array_keys( get_post_format_strings() );
