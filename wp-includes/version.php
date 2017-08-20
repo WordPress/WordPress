eccf27c514b8ad33fd0f913128badfbc14cfee16
Pascal Birchler <pascal.birchler@gmail.com>
2017-08-20 09:13:44 +0000

    Build/Test Tools: Remove unneeded commas to fix JSHint error introduced in [41271].
    See #30666.
    
    Built from https://develop.svn.wordpress.org/trunk@41277
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41117 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-41276';
+$wp_version = '4.9-alpha-41277';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
