78da113bd043ebbfaa905d8f460464b4e2b4aa8c
Drew Jaynes <info@drewapicture.com>
2017-08-20 04:04:44 +0000

    Docs: Add a missing return description for the deprecated `get_screen_icon()` function.
    See #41017.
    
    Built from https://develop.svn.wordpress.org/trunk@41275
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41115 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/deprecated.php
+++ wp-admin/includes/deprecated.php
@@ -1233,7 +1233,7 @@ function screen_icon() {
  * @since 3.2.0
  * @deprecated 3.8.0
  *
- * @return string
+ * @return string An HTML comment explaining that icons are no longer used.
  */
 function get_screen_icon() {
 	_deprecated_function( __FUNCTION__, '3.8.0' );
