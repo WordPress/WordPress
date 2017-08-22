ed21c69ca9562ce94fd21a3370bf586b1989b492
John Blackbourn <johnbillion@git.wordpress.org>
2017-08-22 15:08:43 +0000

    Build/Test tools: Remove usage of `DOING_AJAX` from the test suite, so all tests that expect either an Ajax request or a non-Ajax request can operate without being skipped.
    Props Mte90
    
    Fixes #41561
    
    Built from https://develop.svn.wordpress.org/trunk@41293
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41133 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-41292';
+$wp_version = '4.9-alpha-41293';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
