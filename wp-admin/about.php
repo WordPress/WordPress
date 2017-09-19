6b3afa9678e969150cf0cac60acbb53ca2d312f8
Aaron Campbell <aaron@xavisys.com>
2017-09-19 20:07:25 +0000

    Bump 4.1 branch to version 4.1.19. Built from https://develop.svn.wordpress.org/branches/4.1@41517
    git-svn-id: http://core.svn.wordpress.org/branches/4.1@41350 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -42,7 +42,11 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 </h2>
 
 <div class="changelog point-releases">
-	<h3><?php echo _n( 'Maintenance and Security Release', 'Maintenance and Security Releases', 18 ); ?></h3>
+	<h3><?php echo _n( 'Maintenance and Security Release', 'Maintenance and Security Releases', 19 ); ?></h3>
+	<p><?php printf( _n( '<strong>Version %1$s</strong> addressed a security issue.',
+			'<strong>Version %1$s</strong> addressed some security issues.', 8 ), '4.1.19' ); ?>
+		<?php printf( __( 'For more information, see <a href="%s">the release notes</a>.' ), 'https://codex.wordpress.org/Version_4.1.19' ); ?>
+	</p>
 	<p><?php printf( _n( '<strong>Version %1$s</strong> addressed a security issue.',
 			'<strong>Version %1$s</strong> addressed some security issues.', 5 ), '4.1.18' ); ?>
 		<?php printf( __( 'For more information, see <a href="%s">the release notes</a>.' ), 'https://codex.wordpress.org/Version_4.1.18' ); ?>
