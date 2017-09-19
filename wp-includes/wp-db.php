0ca1d61d9716e8fb63861a3fd00b89a5a810ca2d
Aaron Campbell <aaron@xavisys.com>
2017-09-19 16:25:25 +0000

    Database: Don’t trigger `_doing_it_wrong()` for null values in `wpdb::prepare()`.
    While `wpdb::prepare()` does not support null values (see #12819) they still appear in the wild like in the WordPress Importer and other plugins.
    
    Merges [41483] to 4.3 branch.
    
    
    Built from https://develop.svn.wordpress.org/branches/4.3@41489
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.3@41322 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -1225,7 +1225,7 @@ class wpdb {
 		}
 
 		foreach ( $args as $arg ) {
-			if ( ! is_scalar( $arg ) ) {
+			if ( ! is_scalar( $arg ) && ! is_null( $arg ) ) {
 				_doing_it_wrong( 'wpdb::prepare', sprintf( 'Unsupported value type (%s).', gettype( $arg ) ), '4.3.12' );
 			}
 		}
