435ca077479b459a4d84ef4398a6ed96bd8146f6
Dominik Schilling <dominikschilling+git@gmail.com>
2017-09-19 10:20:24 +0000

    Editor: Prevent adding `javascript:` and `data:` URLs through the inline link dialog.
    Merge of [41393] to the 3.9 branch.
    
    Built from https://develop.svn.wordpress.org/branches/3.9@41409
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.9@41242 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/wplink.js
+++ wp-includes/js/wplink.js
@@ -179,6 +179,13 @@ var wpLink;
 
 			attrs = wpLink.getAttrs();
 
+			var parser = document.createElement( 'a' );
+			parser.href = attrs.href;
+
+			if ( 'javascript:' === parser.protocol || 'data:' === parser.protocol ) { // jshint ignore:line
+				attrs.href = '';
+			}
+
 			// If there's no href, return.
 			if ( ! attrs.href || attrs.href == 'http://' )
 				return;
@@ -192,7 +199,7 @@ var wpLink;
 			}
 
 			if ( attrs.target ) {
-				html += ' target="' + attrs.target + '"';
+				html += ' rel="noopener" target="' + attrs.target + '"';
 			}
 
 			html += '>';
@@ -243,6 +250,13 @@ var wpLink;
 			}
 
 			link = editor.dom.getParent( editor.selection.getNode(), 'a[href]' );
+
+			var parser = document.createElement( 'a' );
+			parser.href = attrs.href;
+
+			if ( 'javascript:' === parser.protocol || 'data:' === parser.protocol ) { // jshint ignore:line
+				attrs.href = '';
+			}
 
 			// If the values are empty, unlink and return
 			if ( ! attrs.href || attrs.href == 'http://' ) {
