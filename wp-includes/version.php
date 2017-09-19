27433a5d25f91bb2d22345f5633515ddb542c294
Dominik Schilling <dominikschilling+git@gmail.com>
2017-09-19 19:42:33 +0000

    Bump 4.8 branch to version 4.8.2. Built from https://develop.svn.wordpress.org/branches/4.8@41509
    git-svn-id: http://core.svn.wordpress.org/branches/4.8@41342 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8.2-alpha-41497';
+$wp_version = '4.8.2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
