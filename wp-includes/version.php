79224df81a867b5c7631d3022e990a830a1cde70
Aaron Campbell <aaron@xavisys.com>
2017-09-19 20:13:15 +0000

    Bump 3.9 branch to version 3.9.20. Built from https://develop.svn.wordpress.org/branches/3.9@41519
    git-svn-id: http://core.svn.wordpress.org/branches/3.9@41352 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.9.19';
+$wp_version = '3.9.20';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
