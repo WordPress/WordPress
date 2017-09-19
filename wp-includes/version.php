40074d31a0285cdbb7fd30b0fc3f5207af57ac42
Dominik Schilling <dominikschilling+git@gmail.com>
2017-09-19 12:41:32 +0000

    TinyMCE: Improve the previews for shortcodes.
    Merge of [41395] to the 4.8 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.8@41435
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.8@41268 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8.2-alpha-41429';
+$wp_version = '4.8.2-alpha-41435';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
