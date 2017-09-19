83853f9fd2782389f0ab1c4f2dcf1556c4250854
Dominik Schilling <dominikschilling+git@gmail.com>
2017-09-19 08:19:45 +0000

    TinyMCE: Improve the previews for shortcodes. Built from https://develop.svn.wordpress.org/trunk@41395
    git-svn-id: http://core.svn.wordpress.org/trunk@41228 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -1058,6 +1058,10 @@ function wp_just_in_time_script_localization() {
 		'autosaveInterval' => AUTOSAVE_INTERVAL,
 		'blog_id' => get_current_blog_id(),
 	) );
+
+	wp_localize_script( 'mce-view', 'mceViewL10n', array(
+		'shortcodes' => ! empty( $GLOBALS['shortcode_tags'] ) ? array_keys( $GLOBALS['shortcode_tags'] ) : array()
+	) );
 }
 
 /**
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-41393';
+$wp_version = '4.9-alpha-41395';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
