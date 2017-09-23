fa5abb247434037dbd48ea4bc7b081507476580a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-09-23 12:21:47 +0000

    I18N: Remove HTML tags from translatable strings in `wp-activate.php`.
    Props ramiy.
    Fixes #41645.
    Built from https://develop.svn.wordpress.org/trunk@41579
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41412 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-activate.php
+++ wp-activate.php
@@ -108,10 +108,9 @@ get_header( 'wp-activate' );
 					);
 				} else {
 					printf(
-						/* translators: 1: site URL, 2: site domain, 3: username, 4: user email, 5: lost password URL */
-						__( 'Your site at <a href="%1$s">%2$s</a> is active. You may now log in to your site using your chosen username of &#8220;%3$s&#8221;. Please check your email inbox at %4$s for your password and login instructions. If you do not receive an email, please check your junk or spam folder. If you still do not receive an email within an hour, you can <a href="%5$s">reset your password</a>.' ),
-						'http://' . $signup->domain,
-						$signup->domain,
+						/* translators: 1: site URL, 2: username, 3: user email, 4: lost password URL */
+						__( 'Your site at %1$s is active. You may now log in to your site using your chosen username of &#8220;%2$s&#8221;. Please check your email inbox at %3$s for your password and login instructions. If you do not receive an email, please check your junk or spam folder. If you still do not receive an email within an hour, you can <a href="%4$s">reset your password</a>.' ),
+						sprintf( '<a href="http://%s">%s</a>', $signup->domain ),
 						$signup->user_login,
 						$signup->user_email,
 						wp_lostpassword_url()
