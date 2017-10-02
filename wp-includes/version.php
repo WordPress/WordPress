2e3581c3ad93da92e99ef8975c74054328922f59
John Blackbourn <johnbillion@git.wordpress.org>
2017-10-02 18:38:46 +0000

    Embeds: Alignment following [41615].
    See #38181
    
    Built from https://develop.svn.wordpress.org/trunk@41676
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41510 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-41675';
+$wp_version = '4.9-alpha-41676';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
