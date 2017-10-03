22fd6a190ba0482b05bb09ec6db3050bf1dd0590
John Blackbourn <johnbillion@git.wordpress.org>
2017-10-03 16:02:46 +0000

    Login and Registration: Add unit tests for `wp_auth_check()`.
    Props pbearne, birgire
    
    Fixes #41860
    
    Built from https://develop.svn.wordpress.org/trunk@41710
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41544 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-41709';
+$wp_version = '4.9-alpha-41710';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
