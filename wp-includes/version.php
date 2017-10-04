20f2edbb4e1f49a1253967158885ac80efe488f6
John Blackbourn <johnbillion@git.wordpress.org>
2017-10-04 13:27:47 +0000

    I18N: Improvements to the tests for plural forms.
    * Move the `create_function()` code into a file that's only loaded, and into a test that's only run, on PHP <= 7.2 to avoid deprecated warnings in 7.2+.
    * Convert the test skipping into a failure if the GlotPress locale file cannot be downloaded.
    * Ensure `test_exceptions` fails if an exception is not thrown.
    * Docs improvements
    
    See #41562, #40109
    
    Built from https://develop.svn.wordpress.org/trunk@41730
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41564 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-alpha-41729';
+$wp_version = '4.9-alpha-41730';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
