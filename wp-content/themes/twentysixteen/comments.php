44e23c2dabdbc74e92e9e7b5648f0c6ab8535508
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-10-04 13:22:48 +0000

    Twenty Sixteen: Make sure comment number comparison in `comments.php` works as expected.
    `get_comments_number()` returns a numeric string, not an integer.
    
    See #39660.
    Built from https://develop.svn.wordpress.org/trunk@41729
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41563 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentysixteen/comments.php
+++ wp-content/themes/twentysixteen/comments.php
@@ -26,7 +26,7 @@ if ( post_password_required() ) {
 		<h2 class="comments-title">
 			<?php
 				$comments_number = get_comments_number();
-				if ( 1 === $comments_number ) {
+				if ( '1' === $comments_number ) {
 					/* translators: %s: post title */
 					printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'twentysixteen' ), get_the_title() );
 				} else {
