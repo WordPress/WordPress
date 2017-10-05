0a2c1d77491edacfdec54f28c2bfde8a4da32976
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-10-05 00:41:46 +0000

    Twenty Fifteen: Add missing singular placeholder in `comments.php`.
    Uses the recommended pattern for better compatibility with languages that use the singular form for other numbers. Also adds translator comments.
    
    Props bplv, xkon.
    See #39660.
    Built from https://develop.svn.wordpress.org/trunk@41761
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41595 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/comments.php
+++ wp-content/themes/twentyfifteen/comments.php
@@ -25,8 +25,24 @@ if ( post_password_required() ) {
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'twentyfifteen' ),
-					number_format_i18n( get_comments_number() ), get_the_title() );
+				$comments_number = get_comments_number();
+				if ( '1' === $comments_number ) {
+					/* translators: %s: post title */
+					printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'twentyfifteen' ), get_the_title() );
+				} else {
+					printf(
+						/* translators: 1: number of comments, 2: post title */
+						_nx(
+							'%1$s thought on &ldquo;%2$s&rdquo;',
+							'%1$s thoughts on &ldquo;%2$s&rdquo;',
+							$comments_number,
+							'comments title',
+							'twentyfifteen'
+						),
+						number_format_i18n( $comments_number ),
+						get_the_title()
+					);
+				}
 			?>
 		</h2>
 
