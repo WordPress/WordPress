a19f05ad63bcde90e41e7f4e2dec385b77da9518
Mel Choyce <melchoyce@gmail.com>
2017-10-12 03:53:47 +0000

    Customizer: Improve specificity of small-screen styles in Widgets panel.
    Props mrasharirfan, celloexpressions.
    Fixes #41614.
    
    Built from https://develop.svn.wordpress.org/trunk@41837
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41671 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-beta1-41836';
+$wp_version = '4.9-beta1-41837';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
