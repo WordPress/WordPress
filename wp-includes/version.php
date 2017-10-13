8af4e6e3f9d7253fde3e1e9ebc3964eee94b9022
Weston Ruter <weston@xwp.co>
2017-10-13 08:23:50 +0000

    Customize: Fix positioning of Preview button in mobile.
    Props Mahvash-Fatima.
    Fixes #42172.
    
    Built from https://develop.svn.wordpress.org/trunk@41853
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41687 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-beta2-41852';
+$wp_version = '4.9-beta2-41853';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
