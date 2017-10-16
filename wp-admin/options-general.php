a88704d0262e929d7d738beb45709a22b403746c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-10-16 14:02:50 +0000

    Administration: Remove punctuation from the link text in "Site Address" option description.
    Props Presskopp.
    Fixes #42190.
    Built from https://develop.svn.wordpress.org/trunk@41874
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41708 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options-general.php
+++ wp-admin/options-general.php
@@ -79,7 +79,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 <th scope="row"><label for="home"><?php _e('Site Address (URL)') ?></label></th>
 <td><input name="home" type="url" id="home" aria-describedby="home-description" value="<?php form_option( 'home' ); ?>"<?php disabled( defined( 'WP_HOME' ) ); ?> class="regular-text code<?php if ( defined( 'WP_HOME' ) ) echo ' disabled' ?>" />
 <?php if ( ! defined( 'WP_HOME' ) ) : ?>
-<p class="description" id="home-description"><?php _e( 'Enter the address here if you <a href="https://codex.wordpress.org/Giving_WordPress_Its_Own_Directory">want your site home page to be different from your WordPress installation directory.</a>' ); ?></p></td>
+<p class="description" id="home-description"><?php _e( 'Enter the address here if you <a href="https://codex.wordpress.org/Giving_WordPress_Its_Own_Directory">want your site home page to be different from your WordPress installation directory</a>.' ); ?></p></td>
 <?php endif; ?>
 </tr>
 
