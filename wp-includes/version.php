021afced82344b48fbbc36e029df4cbb7b2c02ae
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-10-16 22:54:57 +0000

    Role/Capability: Prevent a fatal error when checking a language capability early or in the frontend.
    The meta capabilities `install_languages` and `update_languages` rely on the admin function `wp_can_install_language_pack()`. That function in turn relies on `request_filesystem_credentials()`, so it needs to be ensured that this function is available as well.
    
    Fixes #42230.
    
    Built from https://develop.svn.wordpress.org/trunk@41885
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41719 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-beta2-41884';
+$wp_version = '4.9-beta2-41885';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
