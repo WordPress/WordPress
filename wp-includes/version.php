955d5b0ab112a84efcf8fbba50b5a823c145dccb
Weston Ruter <weston@xwp.co>
2017-10-16 23:02:47 +0000

    Widgets: Prevent MediaElement.js player from overflowing sidebar in Audio widget.
    Props bradyvercher.
    See #39686.
    Fixes #42244.
    
    Built from https://develop.svn.wordpress.org/trunk@41886
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41720 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-beta2-41885';
+$wp_version = '4.9-beta2-41886';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
