71dca590dc5b6871f160f308f55e68986e1912d8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-10-18 21:48:48 +0000

    Build/Test Tools: Clean up `$_REQUEST` and `$GLOBALS` after modifying them in `_test_list_hierarchical_page()`.
    Props birgire.
    Fixes #42044.
    Built from https://develop.svn.wordpress.org/trunk@41929
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41763 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-beta2-41928';
+$wp_version = '4.9-beta2-41929';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
