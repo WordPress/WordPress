f9ff030127dc54b9a32cf237ebdfe3fb50562e62
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2017-10-18 15:46:52 +0000

    I18N: Replace hardcoded file name in a translatable string in `convert_to_screen()` with a placeholder.
    Props ramiy.
    Fixes #41668.
    Built from https://develop.svn.wordpress.org/trunk@41906
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41740 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/template.php
+++ wp-admin/includes/template.php
@@ -2067,7 +2067,17 @@ function _wp_admin_html_begin() {
  */
 function convert_to_screen( $hook_name ) {
 	if ( ! class_exists( 'WP_Screen' ) ) {
-		_doing_it_wrong( 'convert_to_screen(), add_meta_box()', __( "Likely direct inclusion of wp-admin/includes/template.php in order to use add_meta_box(). This is very wrong. Hook the add_meta_box() call into the add_meta_boxes action instead." ), '3.3.0' );
+		_doing_it_wrong(
+			'convert_to_screen(), add_meta_box()',
+			sprintf(
+				/* translators: 1: wp-admin/includes/template.php 2: add_meta_box() 3: add_meta_boxes */
+				__( 'Likely direct inclusion of %1$s in order to use %2$s. This is very wrong. Hook the %2$s call into the %3$s action instead.' ),
+				'<code>wp-admin/includes/template.php</code>',
+				'<code>add_meta_box()</code>',
+				'<code>add_meta_boxes</code>'
+			),
+			'3.3.0'
+		);
 		return (object) array( 'id' => '_invalid', 'base' => '_are_belong_to_us' );
 	}
 
