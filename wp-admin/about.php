02ff01d33181040dc32373b30fe0516228588bf1
Mel Choyce <melchoyce@gmail.com>
2017-10-19 13:57:48 +0000

    About Page: Commit initial strings for 4.9.
    Props kristastevens, dd32, hlashbrooke.
    See #42087.
    
    Built from https://develop.svn.wordpress.org/trunk@41943
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41777 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -37,7 +37,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 	<div class="wrap about-wrap">
 		<h1><?php printf( __( 'Welcome to WordPress&nbsp;%s' ), $display_version ); ?></h1>
 
-		<p class="about-text"><?php printf( __( 'Thank you for updating to the latest version! WordPress %s adds more ways for you to express yourself and represent your brand.' ), $display_version ); ?></p>
+		<p class="about-text"><?php printf( __( 'Thank you for updating to the latest version! WordPress %s will smooth your design workflow and keep you safe from coding errors.' ), $display_version ); ?></p>
 		<div class="wp-badge"><?php printf( __( 'Version %s' ), $display_version ); ?></div>
 
 		<h2 class="nav-tab-wrapper wp-clearfix">
@@ -49,129 +49,93 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 		<div class="feature-section one-col">
 			<div class="col">
-				<h2><?php _e( 'An Update with You in Mind' ); ?></h2>
-				<p class="lead-description"><?php _e( 'WordPress 4.8 adds some great new features. Gear up for a more intuitive WordPress!' ); ?></p>
-				<p><?php _e( 'Though some updates seem minor, they&#8217;ve been built by hundreds of contributors with <em>you</em> in mind. Get ready for new features you&#8217;ll welcome like an old friend: link improvements, <em>three</em> new media widgets covering images, audio, and video, an updated text widget that supports visual editing, and an upgraded news section in your dashboard which brings in nearby and upcoming WordPress events.' ); ?></p>
+				<h2><?php _e( 'Major Customizer Improvements, Code Error Checking, and More!' ); ?></h2>
+				<p><?php _e( 'Welcome to an improved Customizer workflow with design drafts,  locking, scheduling, and preview links.  What&#8217;s more, code syntax highlighting and error checking will make for a clean and smooth site building experience. Finally, if all that wasn&#8217;t pretty great, we&#8217;ve got a great new Gallery widget and improvements to theme browsing and switching.' ); ?></p>
 			</div>
 		</div>
 
 		<hr />
 
-		<h2><?php _e( 'Exciting Widget Updates' ); ?></h2>
+		<h2><?php _e( 'Customizer Workflow Improved' ); ?></h2>
 
 		<div class="headline-feature one-col">
 			<div class="col">
-				<picture>
-					<!-- Large image -->
-					<source media="( min-width: 1050px )"
-						srcset="
-							https://s.w.org/images/core/4.8/widgets-with-all-four-widescreen_w_810.png 810w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-widescreen_w_1054.png 1054w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-widescreen_w_1266.png 1266w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-widescreen_w_1458.png 1458w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-widescreen_w_1633.png 1633w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-widescreen_w_1797.png 1797w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-widescreen_w_1955.png 1955w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-widescreen_w_2100.png 2100w"
-						sizes="( max-width: 1290px ) calc( 100vw - 240px ), 1050px" />
-					<!-- Medium image -->
-					<source media="( min-width: 601px )"
-						srcset="
-							https://s.w.org/images/core/4.8/widgets-with-all-four_w_531.png 531w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four_w_745.png 745w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four_w_927.png 927w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four_w_1089.png 1089w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four_w_1236.png 1236w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four_w_1370.png 1370w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four_w_1498.png 1498w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four_w_1620.png 1620w"
-						sizes="( max-width: 782px ) calc( 100vw - 70px ), ( max-width: 960px ) calc( 100vw - 116px ), calc( 100vw - 240px )" />
-					<!-- Small image -->
-					<img src="https://s.w.org/images/core/4.8/widgets-with-all-four-mobile_w_685.png"
-						srcset="
-							https://s.w.org/images/core/4.8/widgets-with-all-four-mobile_w_300.png 300w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-mobile_w_451.png 451w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-mobile_w_575.png 575w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-mobile_w_685.png 685w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-mobile_w_784.png 784w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-mobile_w_873.png 873w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-mobile_w_959.png 959w,
-							https://s.w.org/images/core/4.8/widgets-with-all-four-mobile_w_1040.png 1040w"
-						sizes="( max-width: 500px ) calc( 100vw - 40px ), calc( 100vw - 70px )"
-						alt="" />
-				</picture>
+				
 			</div>
 		</div>
 
 		<div class="feature-section two-col">
 			<div class="col">
-				<h3><?php _e( 'Image Widget' ); ?></h3>
-				<p><?php _e( 'Adding an image to a widget is now a simple task that is achievable for any WordPress user without needing to know code. Simply insert your image right within the widget settings. Try adding something like a headshot or a photo of your latest weekend adventure &mdash; and see it appear automatically.' ); ?></p>
+				<h3><?php _e( 'Draft and Schedule Site Design Customization' ); ?></h3>
+				<p><?php _e( 'Yes, you read that right. Just like you can draft and revise posts and schedule them to go live on the date and time you choose, you can now tinker with your site&#8217;s design and schedule those design changes to go live as you please.' ); ?></p>
 			</div>
 			<div class="col">
-				<h3><?php _e( 'Video Widget' ); ?></h3>
-				<p><?php _e( 'A welcome video is a great way to humanize the branding of your website. You can now add any video from the Media Library to a sidebar on your site with the new Video widget. Use this to showcase a welcome video to introduce visitors to your site or promote your latest and greatest content.' ); ?></p>
+				<h3><?php _e( 'Collaborate with Design Preview Links' ); ?></h3>
+				<p><?php _e( 'Need to get some feedback on proposed site design changes? WordPress 4.9 gives you a preview link you can send to your team and customers so that you can collect and integrate feedback before you schedule the changes to go live. Can we say collaboration&#43;&#43;?' ); ?></p>
 			</div>
 			<div class="col">
-				<h3><?php _e( 'Audio Widget' ); ?></h3>
-				<p><?php _e( 'Are you a podcaster, musician, or avid blogger? Adding a widget with your audio file has never been easier. Upload your audio file to the Media Library, go to the widget settings, select your file, and you&#8217;re ready for listeners. This would be a easy way to add a more personal welcome message, too!' );?></p>
+				<h3><?php _e( 'One Design Lock To Protect Them All' ); ?></h3>
+				<p><?php _e( 'Ever encounter a scenario where two designers walk into a project and designer A overrides designer B&#8217;s beautiful changes? WordPress 4.9&#8217;s design lock feature (similar to post locking) secures your draft design so that no one can make changes to it or erase all your hard work.' );?></p>
 			</div>
 			<div class="col">
-				<h3><?php _e( 'Rich Text Widget' ); ?></h3>
-				<p><?php _e( 'This feature deserves a parade down the center of town! Rich-text editing capabilities are now native for Text widgets. Add a widget anywhere and format away. Create lists, add emphasis, and quickly and easily insert links. Have fun with your newfound formatting powers, and watch what you can accomplish in a short amount of time.' ); ?></p>
+				<h3><?php _e( 'Protection for Unsaved Design Changes' ); ?></h3>
+				<p><?php 
+					/* Translators: Donuts are a deliciously irresistible fried treat. They can be somewhat like cake, where crumbs can get into places like your keyboard. Insert your favorite local pastry here. */ 
+					_e( 'Those fresh donuts in the break room lured you away from your desk before you saved your new draft design. Fear not, when you return, WordPress 4.9 will politely ask whether or not you&#8217;d like to save your unsaved changes. We haven&#8217;t however, got a solution for donut crumbs in your keyboard. (Yet.)' ); 
+					?></p>
+			</div>
+		</div>
+
+		<hr />
+		
+		<h2><?php _e( 'Coding Enhancements for CSS Editing and the Custom HTML Widget' ); ?></h2>
+
+		<div class="feature-section three-col">
+			<div class="col">
+				<h3><?php _e( 'Syntax Highlighting and Error Checking? Yes, Please!' ); ?></h3>
+				<p><?php _e( 'You&#8217;ve got a display problem but can&#8217;t quite figure out exactly what went wrong in the CSS you lovingly wrote. With syntax highlighting and error checking for CSS editing and the Custom HTML widget introduced in WordPress 4.8.1, you&#8217;ll  pinpoint coding  errors quickly. Practically guaranteed to help you scan code more easily and suss out and fix code errors quickly.' ); ?></p>
+			</div>
+			<div class="col">
+				<h3><?php _e( 'Sandbox for Safety' ); ?></h3>
+				<p><?php _e( 'The dreaded white screen. You&#8217;ll avoid it when working on themes and plugin code because WordPress 4.9 will warn you about saving an error. You&#8217;ll sleep better at night.' ); ?></p>
+			</div>
+			<div class="col">
+				<h3><?php _e( 'Warning: Potential Danger Ahead! ' ); ?></h3>
+				<p><?php 
+					/* Translators: Walking across the Grand Canyon on a tightrope is a very dangerous activity: https://www.usatoday.com/story/news/nation/2013/06/23/wallenda-tightrope-colorado-river-gorge/2450505/ 
+						* If this concept doesn't translate well in your language, please insert a very dangerous, yet slightly humorous alternative. */
+					_e( 'When you edit themes and plugins directly, WordPress 4.9 will politely warn you of that you&#8217;re walking the coding equivalent of a tightrope over the Grand Canyon, and let you know that you have a safety net available if you draft and test changes  before update your file. Take the safe route: You&#8217;ll thank you. Your team and customers will thank you.' )
+					;?></p>
 			</div>
 		</div>
 
 		<hr />
 
+		<h2><?php _e( 'Oh Hey There, Gallery Widget!' ); ?></h2>
+
 		<div class="feature-section two-col">
 			<div class="col">
-				<h3><?php _e( 'Link Boundaries' ); ?></h3>
-				<p><?php _e( 'Have you ever tried updating a link, or the text around a link, and found you can&#8217;t seem to edit it correctly? When you edit the text after the link, your new text also ends up linked. Or you edit the text in the link, but your text ends up outside of it. This can be frustrating! With link boundaries, a great new feature, the process is streamlined and your links will work well. You’ll be happier. We promise.' ); ?></p>
+				<h3><?php _e( 'Gallery? Gallerah, Rah!' ); ?></h3>
+				<p><?php _e( 'An incremental improvement to the media changes hatched in WordPress 4.8, you can now add a gallery via widget. Yes!' ); ?></p>
 			</div>
 			<div class="col">
-				<?php
-				echo wp_video_shortcode( array(
-					'mp4'      => 'https://s.w.org/images/core/4.8/link-boundaries.mp4',
-					'poster'   => 'https://s.w.org/images/core/4.8/link-boundaries.png',
-					'width'    => 1140,
-					'height'   => 624,
-					// 'class'    => 'wp-video-shortcode feature-video',
-				) );
-				?>
+				<h3><?php _e( 'Press a Button, Add Media' ); ?></h3>
+				<p><?php _e( 'Want to add media to your text widget? Embed images, video, and audio directly into the widget along with your text, with our decidedly unfancy, yet wholly utilitarian Add Media button. Woo!' ); ?></p>
 			</div>
 		</div>
 
 		<hr />
 
+		<h2><?php _e( 'Theme Improvements: Previews and Persistent Widgets and Menus' ); ?></h2>
+
 		<div class="feature-section two-col">
 			<div class="col">
-				<h3><?php _e( 'Nearby WordPress Events' ); ?></h3>
-
-				<p><?php _e( 'Did you know that WordPress has a thriving offline community with groups meeting regularly in more than 400 cities around the world? WordPress now draws your attention to the events that help you continue improving your WordPress skills, meet friends, and, of course, publish!' ); ?></p>
-
-				<p><?php _e( 'This is quickly becoming one of our favorite features. While you are in the dashboard (because you&#8217;re running updates and writing posts, right?) all upcoming WordCamps and WordPress Meetups &mdash; local to you &mdash; will be displayed.' ); ?>
-
-				<p><?php _e( 'Being part of the community can help you improve your WordPress skills and network with people you wouldn&#8217;t otherwise meet. Now you can easily find your local events just by logging in to your dashboard and looking at the new Events and News dashboard widget.' ); ?>
+				<h3><?php _e( 'Oh Say, Can You Theme Switch?' ); ?></h3>
+				<p><?php _e( 'When you switch themes, widgets sometimes think they can just up and move location. Improvements in WordPress 4.9 offer more persistent menu and widget placement when you decide it&#8217;s time for a new theme.  Additionally, you can preview installed themes or download, install, and preview new themes right. Nothing says handy like being able to preview before you deploy. ' ); ?></p>
 			</div>
 			<div class="col">
-				<img
-					src="https://s.w.org/images/core/4.8/events-widget_w_732.png"
-					srcset="
-						https://s.w.org/images/core/4.8/events-widget_w_280.png 280w,
-						https://s.w.org/images/core/4.8/events-widget_w_420.png 420w,
-						https://s.w.org/images/core/4.8/events-widget_w_529.png 529w,
-						https://s.w.org/images/core/4.8/events-widget_w_638.png 638w,
-						https://s.w.org/images/core/4.8/events-widget_w_732.png 732w,
-						https://s.w.org/images/core/4.8/events-widget_w_827.png 827w,
-						https://s.w.org/images/core/4.8/events-widget_w_992.png 992w"
-					sizes="
-						( max-width: 500px ) calc( 100vw - 40px ),
-						( max-width: 782px ) calc( 48vw - 33px ),
-						( max-width: 960px ) calc( 47vw - 54px ),
-						( max-width: 1290px ) calc( 47vw - 112px ),
-						496px"
-					alt="" />
+				<h3><?php _e( 'Better Menu Instructions Equal Less Confusion' ); ?></h3>
+				<p><?php _e( 'Were you confused by the steps to create a new menu? Perhaps no longer! We&#8217;ve ironed out the UX for a smoother menu creation process.  Freshly pressed copy will guide you with grace and aplomb.' ); ?></p>
 			</div>
 		</div>
 
@@ -188,28 +152,28 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 			<div class="under-the-hood three-col">
 				<div class="col">
-					<h3><a href="https://make.wordpress.org/core/2017/05/17/cleaner-headings-in-the-admin-screens/"><?php _e( 'More Accessible Admin Panel Headings' ); ?></a></h3>
-					<p><?php _e( 'New CSS rules mean extraneous content (like &ldquo;Add New&rdquo; links) no longer need to be included in admin-area headings. These panel headings improve the experience for people using assistive technologies.' ); ?></p>
+					<h3><a href="#"><?php _e( 'Heading' ); ?></a></h3>
+					<p><?php _e( 'Paragraph' ); ?></p>
 				</div>
 				<div class="col">
-					<h3><a href="https://make.wordpress.org/core/2017/05/22/removal-of-core-embedding-support-for-wmv-and-wma-file-formats/"><?php _e( 'Removal of Core Support for WMV and WMA Files' ); ?></a></h3>
-					<p><?php _e( 'As fewer and fewer browsers support Silverlight, file formats which require the presence of the Silverlight plugin are being removed from core support. Files will still display as a download link, but will no longer be embedded automatically.' ); ?></p>
+					<h3><a href="#"><?php _e( 'Heading' ); ?></a></h3>
+					<p><?php _e( 'Paragraph' ); ?></p>
 				</div>
 				<div class="col">
-					<h3><a href="https://make.wordpress.org/core/2017/05/22/multisite-focused-changes-in-4-8/"><?php _e( 'Multisite Updates' ); ?></a></h3>
-					<p><?php _e( 'New capabilities have been introduced to 4.8 with an eye towards removing calls to <code>is_super_admin()</code>. Additionally, new hooks and tweaks to more granularly control site and user counts per network have been added.' ); ?></p>
+					<h3><a href="#"><?php _e( 'Heading' ); ?></a></h3>
+					<p><?php _e( 'Paragraph' ); ?></p>
 				</div>
 				<div class="col">
-					<h3><a href="https://make.wordpress.org/core/2017/05/23/addition-of-tinymce-to-the-text-widget/"><?php _e( 'Text-Editor JavaScript API' ); ?></a></h3>
-					<p><?php _e( 'With the addition of TinyMCE to the text widget in 4.8 comes a new JavaScript API for instantiating the editor after page load. This can be used to add an editor instance to any text area, and customize it with buttons and functions. Great for plugin authors!' ); ?></p>
+					<h3><a href="#"><?php _e( 'Heading' ); ?></a></h3>
+					<p><?php _e( 'Paragraph' ); ?></p>
 				</div>
 				<div class="col">
-					<h3><a href="https://make.wordpress.org/core/2017/05/26/media-widgets-for-images-video-and-audio/"><?php _e( 'Media Widgets API' ); ?></a></h3>
-					<p><?php _e( 'The introduction of a new base media widget REST API schema to 4.8 opens up possibilities for even more media widgets (like galleries or playlists) in the future. The three new media widgets are powered by a shared base class that covers most of the interactions with the media modal. That class also makes it easier to create new media widgets and paves the way for more to come.' ); ?></p>
+					<h3><a href="#"><?php _e( 'Heading' ); ?></a></h3>
+					<p><?php _e( 'Paragraph' ); ?></p>
 				</div>
 				<div class="col">
-					<h3><a href="https://make.wordpress.org/core/2017/05/16/customizer-sidebar-width-is-now-variable/"><?php _e( 'Customizer Width Variable' ); ?></a></h3>
-					<p><?php _e( 'Rejoice! New responsive breakpoints have been added to the customizer sidebar to make it wider on high-resolution screens. Customizer controls should use percentage-based widths instead of pixels.' ); ?></p>
+					<h3><a href="#"><?php _e( 'Heading' ); ?></a></h3>
+					<p><?php _e( 'Paragraph' ); ?></p>
 				</div>
 			</div>
 		</div>
