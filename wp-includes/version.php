cb1b49bca5d3bf8bb095fc6ff53ff03d55412302
Dion Hulse <wordpress@dd32.id.au>
2017-10-21 14:14:48 +0000

    Tests: Return an exit code of 1 in the event that the database server for tests is unavailable. This allows for automated scripts & test suites to handle the failure of the test suite easier.
    Props danielbachhuber
    Fixes #41916
    
    Built from https://develop.svn.wordpress.org/trunk@41966
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41800 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-beta3-41965';
+$wp_version = '4.9-beta3-41966';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
