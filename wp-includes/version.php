5e7e44aba72880677249bcdf3d8891b9c89e713d
Weston Ruter <weston@xwp.co>
2017-10-22 20:03:51 +0000

    Customize: Update 'Save & Activate' reference to 'Publish & Activate' in themes help tab text.
    See #39896.
    Fixes #42305.
    
    Built from https://develop.svn.wordpress.org/trunk@41968
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41802 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-beta3-41967';
+$wp_version = '4.9-beta3-41968';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
