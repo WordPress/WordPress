a7f2613fc7c4841b944e9378338c17fa460d048e
Helen Hou-Sandí <helen.y.hou@gmail.com>
2017-10-23 23:34:49 +0000

    Buttons: Ensure active styling applies to both `:active` and `.active`.
    props sayedwp, helen.
    fixes #42236.
    
    Built from https://develop.svn.wordpress.org/trunk@41984
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41818 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-beta3-41983';
+$wp_version = '4.9-beta3-41984';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
