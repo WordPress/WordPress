94e74066e97d8dd8a7b89c566fe2de0b0a16c689
Mel Choyce <melchoyce@gmail.com>
2017-10-23 22:24:47 +0000

    About Page: Revised strings to be easier to translate.
    Props kristastevens, Presskopp, swissspidy.
    See #42087.
    
    Built from https://develop.svn.wordpress.org/trunk@41981
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41815 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -49,14 +49,30 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 		<div class="feature-section one-col">
 			<div class="col">
-				<h2><?php _e( 'Major Customizer Improvements, Code Error Checking, and More!' ); ?></h2>
-				<p><?php _e( 'Welcome to an improved Customizer workflow with design drafts,  locking, scheduling, and preview links.  What&#8217;s more, code syntax highlighting and error checking will make for a clean and smooth site building experience. Finally, if all that wasn&#8217;t pretty great, we&#8217;ve got a great new Gallery widget and improvements to theme browsing and switching.' ); ?></p>
+				<h2>
+					<?php
+						printf(
+							/* translators: %s: party popper emoji */
+							__( 'Major Customizer Improvements, Code Error Checking, and More! %s' ),
+							'&#x1F389'
+						);
+					?>
+				</h2>
+				<p><?php _e( 'Welcome to an improved Customizer workflow with design drafts, locking, scheduling, and preview links. What&#8217;s more, code syntax highlighting and error checking will make for a clean and smooth site building experience. Finally, if all that wasn&#8217;t pretty great, we&#8217;ve got a great new Gallery widget and improvements to theme browsing and switching.' ); ?></p>
 			</div>
 		</div>
 
 		<hr />
 
-		<h2><?php _e( 'Customizer Workflow Improved' ); ?></h2>
+		<h2>
+			<?php
+				printf(
+					/* translators: %s: wrench emoji */
+					__( 'Customizer Workflow Improved %s' ),
+					'&#x1F527'
+				);
+			?>
+		</h2>
 
 		<div class="headline-feature one-col">
 			<div class="col">
@@ -66,7 +82,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 		<div class="feature-section two-col">
 			<div class="col">
-				<h3><?php _e( 'Draft and Schedule Site Design Customization' ); ?></h3>
+				<h3><?php _e( 'Draft and Schedule Site Design Customizations' ); ?></h3>
 				<p><?php _e( 'Yes, you read that right. Just like you can draft and revise posts and schedule them to go live on the date and time you choose, you can now tinker with your site&#8217;s design and schedule those design changes to go live as you please.' ); ?></p>
 			</div>
 			<div class="col">
@@ -74,78 +90,115 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 				<p><?php _e( 'Need to get some feedback on proposed site design changes? WordPress 4.9 gives you a preview link you can send to your team and customers so that you can collect and integrate feedback before you schedule the changes to go live. Can we say collaboration&#43;&#43;?' ); ?></p>
 			</div>
 			<div class="col">
-				<h3><?php _e( 'One Design Lock To Protect Them All' ); ?></h3>
+				<h3><?php _e( 'Design Locking To Guard Your Changes' ); ?></h3>
 				<p><?php _e( 'Ever encounter a scenario where two designers walk into a project and designer A overrides designer B&#8217;s beautiful changes? WordPress 4.9&#8217;s design lock feature (similar to post locking) secures your draft design so that no one can make changes to it or erase all your hard work.' );?></p>
 			</div>
 			<div class="col">
-				<h3><?php _e( 'Protection for Unsaved Design Changes' ); ?></h3>
-				<p><?php 
-					/* Translators: Donuts are a deliciously irresistible fried treat. They can be somewhat like cake, where crumbs can get into places like your keyboard. Insert your favorite local pastry here. */ 
-					_e( 'Those fresh donuts in the break room lured you away from your desk before you saved your new draft design. Fear not, when you return, WordPress 4.9 will politely ask whether or not you&#8217;d like to save your unsaved changes. We haven&#8217;t however, got a solution for donut crumbs in your keyboard. (Yet.)' ); 
-					?></p>
+				<h3><?php _e( 'A Prompt to Protect Your Work' ); ?></h3>
+				<p><?php _e( 'Were you lured away from your desk before you saved your new draft design? Fear not, when you return, WordPress 4.9 will politely ask whether or not you&#8217;d like to save your unsaved changes.' ); ?></p>
 			</div>
 		</div>
 
 		<hr />
 		
-		<h2><?php _e( 'Coding Enhancements for CSS Editing and the Custom HTML Widget' ); ?></h2>
+		<h2>
+			<?php
+				printf(
+					/* translators: %s: technologist (woman) emoji */
+					__( 'Coding Enhancements %s' ),
+					'&#x1F469;&#x1F3FE;&#x200D;&#x1F4BB'
+				);
+			?>
+		</h2>
 
 		<div class="feature-section three-col">
 			<div class="col">
 				<h3><?php _e( 'Syntax Highlighting and Error Checking? Yes, Please!' ); ?></h3>
-				<p><?php _e( 'You&#8217;ve got a display problem but can&#8217;t quite figure out exactly what went wrong in the CSS you lovingly wrote. With syntax highlighting and error checking for CSS editing and the Custom HTML widget introduced in WordPress 4.8.1, you&#8217;ll  pinpoint coding  errors quickly. Practically guaranteed to help you scan code more easily and suss out and fix code errors quickly.' ); ?></p>
+				<p><?php _e( 'You&#8217;ve got a display problem but can&#8217;t quite figure out exactly what went wrong in the CSS you lovingly wrote. With syntax highlighting and error checking for CSS editing and the Custom HTML widget introduced in WordPress 4.8.1, you&#8217;ll pinpoint coding errors quickly. Practically guaranteed to help you scan code more easily and suss out and fix code errors quickly.' ); ?></p>
 			</div>
 			<div class="col">
 				<h3><?php _e( 'Sandbox for Safety' ); ?></h3>
 				<p><?php _e( 'The dreaded white screen. You&#8217;ll avoid it when working on themes and plugin code because WordPress 4.9 will warn you about saving an error. You&#8217;ll sleep better at night.' ); ?></p>
 			</div>
 			<div class="col">
-				<h3><?php _e( 'Warning: Potential Danger Ahead! ' ); ?></h3>
-				<p><?php 
-					/* Translators: Walking across the Grand Canyon on a tightrope is a very dangerous activity: https://www.usatoday.com/story/news/nation/2013/06/23/wallenda-tightrope-colorado-river-gorge/2450505/ 
-						* If this concept doesn't translate well in your language, please insert a very dangerous, yet slightly humorous alternative. */
-					_e( 'When you edit themes and plugins directly, WordPress 4.9 will politely warn you of that you&#8217;re walking the coding equivalent of a tightrope over the Grand Canyon, and let you know that you have a safety net available if you draft and test changes  before update your file. Take the safe route: You&#8217;ll thank you. Your team and customers will thank you.' )
-					;?></p>
+				<h3><?php _e( 'Warning: Potential Danger Ahead!' ); ?></h3>
+				<p><?php _e( 'When you edit themes and plugins directly, WordPress 4.9 will politely warn you that this is a dangerous practice and will recommend that you draft and test changes before update your file. Take the safe route: You&#8217;ll thank you. Your team and customers will thank you.' );?></p>
 			</div>
 		</div>
 
 		<hr />
 
-		<h2><?php _e( 'Oh Hey There, Gallery Widget!' ); ?></h2>
+		<h2>
+			<?php
+				printf(
+					/* translators: %s: frame with picture emoji */
+					__( 'Even More Widget Updates %s' ),
+					'&#x1F5BC'
+				);
+			?>
+		</h2>
 
 		<div class="feature-section two-col">
 			<div class="col">
-				<h3><?php _e( 'Gallery? Gallerah, Rah!' ); ?></h3>
+				<h3><?php _e( 'The New Gallery Widget' ); ?></h3>
 				<p><?php _e( 'An incremental improvement to the media changes hatched in WordPress 4.8, you can now add a gallery via widget. Yes!' ); ?></p>
 			</div>
 			<div class="col">
 				<h3><?php _e( 'Press a Button, Add Media' ); ?></h3>
-				<p><?php _e( 'Want to add media to your text widget? Embed images, video, and audio directly into the widget along with your text, with our decidedly unfancy, yet wholly utilitarian Add Media button. Woo!' ); ?></p>
+				<p><?php _e( 'Want to add media to your text widget? Embed images, video, and audio directly into the widget along with your text, with our simple but useful Add Media button. Woo!' ); ?></p>
 			</div>
 		</div>
 
 		<hr />
 
-		<h2><?php _e( 'Theme Improvements: Previews and Persistent Widgets and Menus' ); ?></h2>
+		<h2>
+			<?php
+				printf(
+					/* translators: %s: hammer emoji */
+					__( 'Site Building Improvements %s' ),
+					'&#x1F528'
+				);
+			?>	
+		</h2>
 
 		<div class="feature-section two-col">
 			<div class="col">
-				<h3><?php _e( 'Oh Say, Can You Theme Switch?' ); ?></h3>
-				<p><?php _e( 'When you switch themes, widgets sometimes think they can just up and move location. Improvements in WordPress 4.9 offer more persistent menu and widget placement when you decide it&#8217;s time for a new theme.  Additionally, you can preview installed themes or download, install, and preview new themes right. Nothing says handy like being able to preview before you deploy. ' ); ?></p>
+				<h3><?php _e( 'More Reliable Theme Switching' ); ?></h3>
+				<p><?php _e( 'When you switch themes, widgets sometimes think they can just up and move location. Improvements in WordPress 4.9 offer more persistent menu and widget placement when you decide it&#8217;s time for a new theme. Additionally, you can preview installed themes or download, install, and preview new themes right. Nothing says handy like being able to preview before you deploy. ' ); ?></p>
 			</div>
 			<div class="col">
-				<h3><?php _e( 'Better Menu Instructions Equal Less Confusion' ); ?></h3>
-				<p><?php _e( 'Were you confused by the steps to create a new menu? Perhaps no longer! We&#8217;ve ironed out the UX for a smoother menu creation process.  Freshly pressed copy will guide you with grace and aplomb.' ); ?></p>
+				<h3><?php _e( 'Better Menu Instructions = Less Confusion' ); ?></h3>
+				<p><?php _e( 'Were you confused by the steps to create a new menu? Perhaps no longer! We&#8217;ve ironed out the UX for a smoother menu creation process. Newly updated copy will guide you.' ); ?></p>
 			</div>
 		</div>
 
+		<hr />
+		
+		<div class="feature-section one-col">
+			<div class="col">
+				<h2>
+					<?php
+						printf(
+							/* translators: %s: handshake emoji */
+							__( 'Lend a Hand with Gutenberg %s' ),
+							'&#x1F91D'
+						);
+					?>
+				</h2>
+				<p><?php printf(
+					__( 'WordPress is working on a new way to create and control your content and we&#8217;d love to have your help. Interested in being an <a href="%s">early tester</a> or getting involved with the Gutenberg project? <a href="%s">Contribute on GitHub</a>.' ),
+					__( 'https://wordpress.org/plugins/gutenberg/' ),
+					'https://github.com/WordPress/gutenberg' ); ?></p>
+			</div>
+		</div>
+		
 		<hr />
 
 		<div class="changelog">
 			<h2><?php
 				printf(
 					/* translators: %s: smiling face with smiling eyes emoji */
-					__( 'Even More Developer Happiness %s' ),
+					__( 'Developer Happiness %s' ),
 					'&#x1F60A'
 				);
 			?></h2>
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-beta3-41980';
+$wp_version = '4.9-beta3-41981';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
