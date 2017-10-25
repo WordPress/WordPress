3cd984f0249d0027418e4e2894ea7bb9bd525de7
Gary Pendergast <gary@pento.net>
2017-10-25 01:07:47 +0000

    About page: The link URLs in the privacy policy shouldn't be translatable.
    Both of these URLs live on the main wordpress.org site, not Rosetta sites.
    
    See #40794.
    
    
    Built from https://develop.svn.wordpress.org/trunk@42017
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41851 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-beta3-42016';
+$wp_version = '4.9-beta3-42017';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
