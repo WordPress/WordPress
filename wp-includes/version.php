cb7483b59543cd973cb2a7c2d4f9a5852e5e9d5c
Weston Ruter <weston@xwp.co>
2017-10-28 06:54:47 +0000

    Customize: Fix mobile placement of panes for available widgets and nav menu items.
    Props Mahvash-Fatima.
    Amends [41893].
    See #42215.
    Fixes #42359.
    
    Built from https://develop.svn.wordpress.org/trunk@42036
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41870 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-beta4-42035';
+$wp_version = '4.9-beta4-42036';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
