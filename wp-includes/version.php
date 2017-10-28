615a9afab86e6485e96d44aa700b53dfbcec539f
Gary Pendergast <gary@pento.net>
2017-10-28 01:56:46 +0000

    Tests: Update `wp-api-generated.js`.
    [41979] caused a change in `wp-api-generated.js`, so it needs to be updated.
    
    See #39996.
    
    
    Built from https://develop.svn.wordpress.org/trunk@42032
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41866 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-beta4-42031';
+$wp_version = '4.9-beta4-42032';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
