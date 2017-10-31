b4ba20d05a3500abc7177a656f0f547ae32c44fa
Gary Pendergast <gary@pento.net>
2017-10-31 13:33:26 +0000

    Bump 4.3 branch to version 4.3.13.
    Built from https://develop.svn.wordpress.org/branches/4.3@42074
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.3@41903 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -108,7 +108,10 @@ $tech_features = array(
 		</h2>
 
 		<div class="changelog point-releases">
-			<h3><?php echo _n( 'Maintenance and Security Release', 'Maintenance and Security Releases', 12 ); ?></h3>
+			<h3><?php echo _n( 'Maintenance and Security Release', 'Maintenance and Security Releases', 13 ); ?></h3>
+			<p><?php printf( __( '<strong>Version %s</strong> addressed one security issue.' ), '4.3.13' ); ?>
+				<?php printf( __( 'For more information, see <a href="%s">the release notes</a>.' ), 'https://codex.wordpress.org/Version_4.3.13' ); ?>
+			</p>
 			<p><?php printf( _n( '<strong>Version %1$s</strong> addressed a security issue.',
 					'<strong>Version %1$s</strong> addressed some security issues.', 8 ), '4.3.12' ); ?>
 				<?php printf( __( 'For more information, see <a href="%s">the release notes</a>.' ), 'https://codex.wordpress.org/Version_4.3.12' ); ?>
