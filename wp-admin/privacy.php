898141408220db799056e1e7dc4167f0d73bdeee
Gary Pendergast <gary@pento.net>
2017-10-31 00:21:49 +0000

    About page: Tweak the privacy policy language, for clarity.
    See #40794.
    
    
    Built from https://develop.svn.wordpress.org/trunk@42045
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41879 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/privacy.php
+++ wp-admin/privacy.php
@@ -32,7 +32,7 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 <p class="about-description"><?php _e( 'From time to time, your WordPress site may send data to WordPress.org &#8212; including, but not limited to &#8212; the version of WordPress you are using, and a list of installed plugins and themes.' ); ?></p>
 
-<p><?php printf( __( 'This data helps WordPress to protect your site by finding and automatically installing new updates, as well as other general enhancements. The data is also used to calculate statistics, such as those shown on the <a href="%s">WordPress.org stats page</a>.' ), 'https://wordpress.org/about/stats/' ); ?></p>
+<p><?php printf( __( 'This data is used to provide general enhancements to WordPress, which includes helping to protect your site by finding and automatically installing new updates. It is also used to calculate statistics, such as those shown on the <a href="%s">WordPress.org stats page</a>.' ), 'https://wordpress.org/about/stats/' ); ?></p>
 
 <p><?php printf( __( 'We take privacy and transparency very seriously. To learn more about what data we collect, and how we use it, please visit <a href="%s">WordPress.org/about/privacy</a>.' ), 'https://wordpress.org/about/privacy/' ); ?></p>
 
