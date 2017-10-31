a9c82ac560ef6fdfffabd9ff1381da1631e972ba
Gary Pendergast <gary@pento.net>
2017-10-31 13:44:19 +0000

    Bump 4.0 branch to version 4.0.20.
    Built from https://develop.svn.wordpress.org/branches/4.0@42077
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.0@41906 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.0.19';
+$wp_version = '4.0.20';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
