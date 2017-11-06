b8de3548d377026b2b303c8cbb86b33bdb641e78
John Blackbourn <johnbillion@git.wordpress.org>
2017-11-06 18:17:54 +0000

    Administration: Ensure the new Privacy Policy screen appears when within the Network Admin and User Admin.
    Props hnle, dd32
    Fixes #42443
    
    Built from https://develop.svn.wordpress.org/trunk@42116
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41945 1a063a9b-81f0-0310-95a4-ce76da25c4cd

---
+++ wp-admin/user/privacy.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User Dashboard Privacy administration panel.
+ *
+ * @package WordPress
+ * @subpackage Administration
+ * @since 4.9.0
+ */
+
+/** Load WordPress Administration Bootstrap */
+require_once( dirname( __FILE__ ) . '/admin.php' );
+
+require( ABSPATH . 'wp-admin/privacy.php' );
