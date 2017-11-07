4eda9c7388acb626565db3fc95f971f24f490ed0
Konstantin Obenland <obenland@automattic.com>
2017-11-07 04:26:48 +0000

    About: Hardcoded image sources.
    We're committed to these images now.
    
    See #42087.
    
    Built from https://develop.svn.wordpress.org/trunk@42123
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@41954 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-RC1-42122';
+$wp_version = '4.9-RC1-42123';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
