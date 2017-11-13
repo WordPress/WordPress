0d7f3264771b541791cced43054469a0f4fb08d4
Dominik Schilling <dominikschilling+git@gmail.com>
2017-11-13 22:44:24 +0000

    Build: Add `npm-shrinkwrap.json`.
    For compatibility with the build server `fsevent` has been manually removed.
    
    Props netweb.
    See #42483.
    Built from https://develop.svn.wordpress.org/branches/4.9@42168
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@41998 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-RC2-42167';
+$wp_version = '4.9-RC2-42168';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
