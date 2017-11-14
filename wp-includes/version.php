ce6b628972ad46d5130ecc9e43785f2d1394dc53
Weston Ruter <weston@xwp.co>
2017-11-14 06:08:40 +0000

    WordPress 4.9 RC3.
    Built from https://develop.svn.wordpress.org/branches/4.9@42179
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42009 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-RC2-42178';
+$wp_version = '4.9-RC3';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
