5cab65edabee892ec5f0b7d5dda5681f8bcb3558
Weston Ruter <weston@xwp.co>
2017-11-15 18:59:47 +0000

    Editor: Disable `wp_keep_scroll_position` in IE11 since buggy; fix `matches` polyfill conflict with ME.js by doing runtime feature detection in context window.
    Props westonruter, SergeyBiryukov, Clorith for testing.
    See #41962, #42029.
    Fixes #42553 for trunk.
    
    Built from https://develop.svn.wordpress.org/trunk@42191
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42021 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42189';
+$wp_version = '5.0-alpha-42191';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 38590;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '4607-20170918';
+$tinymce_version = '4607-20171115';
 
 /**
  * Holds the required PHP version
