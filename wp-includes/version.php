9986ebdc0b2771a57a577bab788b4b9072a53328
Weston Ruter <weston@xwp.co>
2017-11-15 00:21:48 +0000

    Editor: Improve scrolling behavior and prevent autosave logic from causing dirty state when just switching between Visual and Text tabs.
    Props pento.
    See #41962, #42029.
    Fixes #42530 for trunk.
    
    Built from https://develop.svn.wordpress.org/trunk@42183
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42013 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42181';
+$wp_version = '5.0-alpha-42183';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
