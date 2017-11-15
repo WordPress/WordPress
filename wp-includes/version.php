35b40d1001c067897782743fa9295f9031ccd091
Weston Ruter <weston@xwp.co>
2017-11-15 23:36:39 +0000

    Editor: Re-build `tinymce.min.js` after [42191].
    See #42553.
    
    Built from https://develop.svn.wordpress.org/branches/4.9@42194
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42024 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9-RC3-42192';
+$wp_version = '4.9-RC3-42194';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 38590;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '4607-20171115';
+$tinymce_version = '4607-20171116';
 
 /**
  * Holds the required PHP version
