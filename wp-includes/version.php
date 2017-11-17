24b1297fabe16a41f6847f7df036273f46a590e7
Dion Hulse <wordpress@dd32.id.au>
2017-11-17 03:01:47 +0000

    Remove the `svn:keywords` property from files that don't need it.
    See #42594
    
    Built from https://develop.svn.wordpress.org/trunk@42201
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42031 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42200';
+$wp_version = '5.0-alpha-42201';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
