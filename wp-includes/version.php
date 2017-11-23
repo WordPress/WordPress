4cb8d10856a8a1266d9b0e81a139d1f35ce756e0
John Blackbourn <johnbillion@git.wordpress.org>
2017-11-23 19:06:42 +0000

    Users: Correct the value of the `lang` attribute in the admin area.
    This corrects the value when the user's language is set to `English (United States)` but the site language is not.
    
    Props ocean90, afercia
    
    Fixes #42242
    
    Merges [42220] to the 4.9 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.9@42221
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42050 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.1-alpha-42219';
+$wp_version = '4.9.1-alpha-42221';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
