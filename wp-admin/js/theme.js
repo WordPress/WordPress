6dc1dd28f553383f2f260fb9a9eb2021050fc6ea
Dion Hulse <wordpress@dd32.id.au>
2017-11-24 02:56:42 +0000

    Themes: Prevent JS error on Themes admin screen when only one theme is installed.
    Amends [42029].
    Props chetan200891, afercia.
    See #42348, westonruter.
    Merges [42222] to the 4.9 branch.
    Fixes #42673.
    
    Built from https://develop.svn.wordpress.org/branches/4.9@42223
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42052 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/theme.js
+++ wp-admin/js/theme.js
@@ -102,7 +102,7 @@ themes.view.Appearance = wp.Backbone.View.extend({
 			collection: self.collection,
 			parent: this
 		});
-		self.searchView = view;
+		self.SearchView = view;
 
 		// Render and append after screen title
 		view.render();
@@ -1448,7 +1448,7 @@ themes.Run = {
 		this.render();
 
 		// Start debouncing user searches after Backbone.history.start().
-		this.view.searchView.doSearch = _.debounce( this.view.searchView.doSearch, 500 );
+		this.view.SearchView.doSearch = _.debounce( this.view.SearchView.doSearch, 500 );
 	},
 
 	render: function() {
@@ -1924,7 +1924,7 @@ themes.RunInstaller = {
 		this.render();
 
 		// Start debouncing user searches after Backbone.history.start().
-		this.view.searchView.doSearch = _.debounce( this.view.searchView.doSearch, 500 );
+		this.view.SearchView.doSearch = _.debounce( this.view.SearchView.doSearch, 500 );
 	},
 
 	render: function() {
