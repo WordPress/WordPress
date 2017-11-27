0236fd226ea4dce1c9cd09a8100a1d374e482a60
Dion Hulse <wordpress@dd32.id.au>
2017-11-27 03:53:40 +0000

    Theme Editor: Translate the URL to the Child Themes Codex page.
    Props odysseygate, johnpgreen.
    Merges [42247] to the 4.9 branch.
    Fixes #42454.
    
    Built from https://develop.svn.wordpress.org/branches/4.9@42248
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42077 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/theme-editor.php
+++ wp-admin/theme-editor.php
@@ -322,7 +322,7 @@ if ( ! in_array( 'theme_editor_notice', $dismissed_pointers, true ) ) :
 					echo sprintf(
 						/* translators: %s: Codex URL */
 						__( 'You appear to be making direct edits to your theme in the WordPress dashboard. We recommend that you don&#8217;t! Editing your theme directly could break your site and your changes may be lost in future updates. If you need to tweak more than your theme&#8217;s CSS, you might want to try <a href="%s">making a child theme</a>.' ),
-						esc_url( 'https://codex.wordpress.org/Child_Themes' )
+						esc_url( __( 'https://codex.wordpress.org/Child_Themes' ) )
 					);
 					?>
 				</p>
