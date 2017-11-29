e75247a179a91fbf4e621b300f04d1b19e223aeb
John Blackbourn <johnbillion@git.wordpress.org>
2017-11-29 16:11:38 +0000

    Hardening: Ensure the attributes of enclosures are correctly escaped in RSS and Atom feeds.
    Merges [42260] to the 4.9 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.9@42266
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42095 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.1-beta1-42265';
+$wp_version = '4.9.1-beta1-42266';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
