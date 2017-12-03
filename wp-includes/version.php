01eeeb80fdbd1393c2f33903d4df4d95f2b97fdf
Rachel Baker <rachel@rachelbaker.me>
2017-12-03 18:11:53 +0000

    REST API: Add existing term_id to the error data object when attempting to create a duplicate term.
    Props shooper, coleh.
    Fixes #42597. See #41370.
    Built from https://develop.svn.wordpress.org/trunk@42350
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42179 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42349';
+$wp_version = '5.0-alpha-42350';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
