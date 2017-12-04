438f7a0701914391fa99386faea70b46f3c82c4f
Boone Gorges <boonebgorges@gmail.com>
2017-12-04 20:47:49 +0000

    Fix incorrect test names from [42364].
    See #42771.
    Built from https://develop.svn.wordpress.org/trunk@42365
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42194 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42364';
+$wp_version = '5.0-alpha-42365';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
