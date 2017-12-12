04efa7e7ea146b7bc9baedc2f6257a6c2645813c
Dion Hulse <wordpress@dd32.id.au>
2017-12-12 05:43:48 +0000

    WPDB: Always use `mysqli` when available.
    This change makes WordPress use `mysqli` on PHP <5.5 when available, instead of only in PHP 5.5+ and development installs.
    
    WPDB includes a fallback to `mysql` in the event that the database connection fails with `mysqli` so incompatibilities should be few and far between.
    
    Fixes #42812 for trunk.
    
    Built from https://develop.svn.wordpress.org/trunk@42388
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42217 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42387';
+$wp_version = '5.0-alpha-42388';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
