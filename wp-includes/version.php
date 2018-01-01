2eca52eaa40817e7f170d3c2be619709f2d0d90b
Rachel Baker <rachel@rachelbaker.me>
2018-01-01 02:31:46 +0000

    REST API: Adjust unit testes to expect a 401 status code in error responses from permission callbacks when user is not authenticated.
    Missed in [42421].
    
    Fixes #42828.
    Built from https://develop.svn.wordpress.org/trunk@42423
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42254 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42421';
+$wp_version = '5.0-alpha-42423';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
