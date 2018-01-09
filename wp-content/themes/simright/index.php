7b6f212b07f717b75cde071c353498807b469178
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-01-09 15:06:18 +0800

    fix(test):simright 主题

---
+++ wp-content/themes/simright/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Aurelius | Blog</title>
+<!-- Stylesheets -->
+<link rel="stylesheet" href="./style.css" type="text/css" media="screen" />
+</head>
+<body>
+<div id="wrapper" class="container_12 clearfix">
+	<!-- Text Logo -->
+	<h1 id="logo" class="grid_4">Aurelius</h1>
+	<!-- Navigation Menu -->
+	<ul id="navigation" class="grid_8">
+		<li><a href="contact.html"><span class="meta">Get in touch</span><br />
+			Contact Us</a></li>
+		<li><a href="blog.html" class="current"><span class="meta">Latest news</span><br />
+			Blog</a></li>
+		<li><a href="index.html"><span class="meta">Homepage</span><br />
+			Home</a></li>
+	</ul>
+	<div class="hr grid_12 clearfix">&nbsp;</div>
+	<!-- Caption Line -->
+	<h2 class="grid_12 caption clearfix">Our <span>blog</span>, keeping you up-to-date on our latest news.</h2>
+	<div class="hr grid_12 clearfix">&nbsp;</div>
+	<!-- Column 1 /Content -->
+	<div class="grid_8">
+		<!-- Blog Post -->
+		<div class="post">
+			<!-- Post Title -->
+			<h3 class="title"><a href="single.html">Loreum ipsium massa cras phasellus</a></h3>
+			<!-- Post Data -->
+			<p class="sub"><a href="#">News</a>, <a href="#">Products</a> &bull; 31st Sep, 09 &bull; <a href="#">1 Comment</a></p>
+			<div class="hr dotted clearfix">&nbsp;</div>
+			<!-- Post Image -->
+			<img class="thumb" alt="" src="images/610x150.gif" />
+			<!-- Post Content -->
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <b>Mauris vel porta erat.</b> Quisque sit amet risus at odio pellentesque sollicitudin. Proin suscipit molestie facilisis. Aenean vel massa magna. Proin nec lacinia augue. Mauris venenatis libero nec odio viverra consequat. In hac habitasse platea dictumst.</p>
+			<p>Cras vestibulum lorem et dui mollis sed posuere leo semper. Integer ac ultrices neque. Cras lacinia orci a augue tempor egestas. Sed cursus, sem ut vehicula vehicula, ipsum est mattis justo, at volutpat nibh arcu sit amet risus. Vestibulum tincidunt, eros ut commodo laoreet, arcu eros ultrices nibh, ac auctor est dui vel nibh.</p>
+			<!-- Read More Button -->
+			<p class="clearfix"><a href="single.html" class="button right"> Read More...</a></p>
+		</div>
+		<div class="hr clearfix">&nbsp;</div>
+		<!-- Blog Post -->
+		<div class="post">
+			<!-- Post Title -->
+			<h3 class="title"><a href="single.html">Loreum ipsium massa cras phasellus</a></h3>
+			<!-- Post Data -->
+			<p class="sub"><a href="#">News</a>, <a href="#">Products</a> &bull; 31st Sep, 09 &bull; <a href="#">1 Comment</a></p>
+			<div class="hr dotted clearfix">&nbsp;</div>
+			<!-- Post Image -->
+			<img class="thumb" alt="" src="images/610x150.gif" />
+			<!-- Post Content -->
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <b>Mauris vel porta erat.</b> Quisque sit amet risus at odio pellentesque sollicitudin. Proin suscipit molestie facilisis. Aenean vel massa magna. Proin nec lacinia augue. Mauris venenatis libero nec odio viverra consequat. In hac habitasse platea dictumst.</p>
+			<p>Cras vestibulum lorem et dui mollis sed posuere leo semper. Integer ac ultrices neque. Cras lacinia orci a augue tempor egestas. Sed cursus, sem ut vehicula vehicula, ipsum est mattis justo, at volutpat nibh arcu sit amet risus. Vestibulum tincidunt, eros ut commodo laoreet, arcu eros ultrices nibh, ac auctor est dui vel nibh.</p>
+			<!-- Read More Button -->
+			<p class="clearfix"><a href="single.html" class="button right"> Read More...</a></p>
+		</div>
+		<div class="hr clearfix">&nbsp;</div>
+		<!-- Blog Post -->
+		<div class="post">
+			<!-- Post Title -->
+			<h3 class="title"><a href="single.html">Loreum ipsium massa cras phasellus</a></h3>
+			<!-- Post Data -->
+			<p class="sub"><a href="#">News</a>, <a href="#">Products</a> &bull; 31st Sep, 09 &bull; <a href="#">1 Comment</a></p>
+			<div class="hr dotted clearfix">&nbsp;</div>
+			<!-- Post Image -->
+			<img class="thumb" alt="" src="images/610x150.gif" />
+			<!-- Post Content -->
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <b>Mauris vel porta erat.</b> Quisque sit amet risus at odio pellentesque sollicitudin. Proin suscipit molestie facilisis. Aenean vel massa magna. Proin nec lacinia augue. Mauris venenatis libero nec odio viverra consequat. In hac habitasse platea dictumst.</p>
+			<p>Cras vestibulum lorem et dui mollis sed posuere leo semper. Integer ac ultrices neque. Cras lacinia orci a augue tempor egestas. Sed cursus, sem ut vehicula vehicula, ipsum est mattis justo, at volutpat nibh arcu sit amet risus. Vestibulum tincidunt, eros ut commodo laoreet, arcu eros ultrices nibh, ac auctor est dui vel nibh.</p>
+			<!-- Read More Button -->
+			<p class="clearfix"><a href="single.html" class="button right"> Read More...</a></p>
+		</div>
+		<div class="hr clearfix">&nbsp;</div>
+		<!-- Blog Navigation -->
+		<p class="clearfix"> <a href="#" class="button float">&lt;&lt; Previous Posts</a> <a href="#" class="button float right">Newer Posts >></a> </p>
+	</div>
+	<!-- Column 2 / Sidebar -->
+	<div class="grid_4">
+		<h4>Catagories</h4>
+		<ul class="sidebar">
+			<li><a href="">So who are we?</a></li>
+			<li><a href="">Philosophy</a></li>
+			<li><a href="">History</a></li>
+			<li><a href="">Jobs</a></li>
+			<li><a href="">Staff</a></li>
+			<li><a href="">Clients</a></li>
+		</ul>
+		<h4>Archives</h4>
+		<ul class="sidebar">
+			<li><a href="">January 2010</a></li>
+			<li><a href="">December 2009</a></li>
+			<li><a href="">Novemeber 2009</a></li>
+			<li><a href="">October 2009</a></li>
+			<li><a href="">September 2009</a></li>
+			<li><a href="">August 2009</a></li>
+		</ul>
+	</div>
+	<div class="hr grid_12 clearfix">&nbsp;</div>
+	<!-- Footer -->
+	<p class="grid_12 footer clearfix"> <span class="float"><strong>Design By</strong> QwibbleDesigns&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Code By</strong> <a href="http://www.ludou.org/">Ludou</a></span> <a class="float right" href="#">top</a> </p>
+</div>
+<!--end wrapper-->
+</body>
+</html>
