7bfc60864f6222bdcdcf739e4ea5f9f3814a8f73
Mike Schroder <mschrode@umich.edu>
2018-01-10 01:17:51 +0000

    Media: Skip `test_remove_orientation_data_on_rotate` if missing deps.
    The `test_remove_orientation_data_on_rotate()` test was failing on systems that did not have the `exif` extension available.
    
    Skips test when the `exif_read_data()` function does not exist.
    
    Props danielbachhuber, desrosj.
    Fixes #42447.
    Built from https://develop.svn.wordpress.org/trunk@42431
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42261 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42430';
+$wp_version = '5.0-alpha-42431';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
