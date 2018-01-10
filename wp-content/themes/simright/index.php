44d2e5d595172b55da34354d48bcac5207a72b68
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-01-10 11:24:04 +0800

    feat:添加video slide bar 及body 部分

--- wp-content/themes/simright/index.php
+++ wp-content/themes/simright/index.php
@@ -1,42 +1,57 @@
 <?php get_header(); ?>
-	<section class="main-box video-list">
-        <div class="banner">
-            <h2 class="text-center">ABOUT US</h2>
-            <P>Simulation in your browser</P>
-            <P>Accessible,Cost-efficient,For everyone</P>
+<section class="main-box video-list">
+    <div class="banner">
+        <h2 class="text-center">ABOUT US</h2>
+        <P>Simulation in your browser</P>
+        <P>Accessible,Cost-efficient,For everyone</P>
+    </div>
+    <section class="contain-box">
+        <div class="slide-bar">
+            <form class="neck-bar-search">
+                <div class="form-group input-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                    <button type="submit" class="input-group-addon"><i class="glyphicon glyphicon-search"></i></button>
+                </div>
+            </form>
+            <ul>
+                <li><a href="">Webinars</a></li>
+                <li><a href="">Webinars</a></li>
+                <li><a href="">Webinars</a></li>
+                <li><a href="">Webinars</a></li>
+                <li><a href="">Webinars</a></li>
+                <li><a href="">Webinars</a></li>
+                <li><a href="">Webinars</a></li>
+                <li><a href="">Webinars</a></li>
+            </ul>
         </div>
-        <section>
-            <div class="title">
-                <h2>OUR COMPANY</h2>
-            </div>
-            <div class="about-company">
-                <div>
-                    <div>
-                        <p><span>S</span><b>imright.com was founded in 2016, and our vision is to build a platform for product design.</b></p>
-                        <br>
-                        <p>We provide cloud-based tools for modeling, simulation and optimization, which users could easily access through web browsers. All the software and hardware are deployed at the remote servers, and users could use them on-demand - anywhere, anytime and using any device with web browser and internet connection.</p>
-                        <p></p>
+        <div class="contain-body">
+            <section class="list-item">
+                <h2>LATEST</h2>
+                <hr/>
+                <div class="list-contain">
+                    <div class="video-item">
+                        <a href="">
+                            <img src="https://oss.simright.com/images/ss.png" alt="">
+                            <span>使用教程</span>
+                            <p>2017-12-12</p>
+                        </a>
                     </div>
                 </div>
-            </div>
-        </section>
-        <section>
-            <div class="title">
-                <h2>OUR BUSINESS RANGE</h2>
-            </div>
-            <div class="about-businese">
-                <div class="businese-new-energy"><span>New Energy</span></div>
-                <div class="businese-civil"><span>Civil</span></div>
-                <div class="businese-ai"><span>AI</span></div>
-                <div class="businese-aerospace"><span>Aerospace</span></div>
-                <div class="businese-automotive"><span>Automotive</span></div>
-                <div class="businese-printing"><span>3D Printing</span></div>
-            </div>
-        </section>
-        <section class="about-introduce">
-            <div>
-                <p><span>W</span><b>e believe this is the way how design tools shall be accessed. As it is not only cost-efficient, but also makes a lot of things much easier, e.g. collaborative design, sharing and reusing of digital assets, etc.</b></p>
-            </div>
-        </section>
+            </section>
+            <section class="list-item">
+                <h2>LATEST</h2>
+                <hr/>
+                <div class="list-contain">
+                    <div class="video-item">
+                        <a href="">
+                            <img src="https://oss.simright.com/images/ss.png" alt="">
+                            <span>使用教程</span>
+                            <p>2017-12-12</p>
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </div>
     </section>
+</section>
 <?php get_footer(); ?>
