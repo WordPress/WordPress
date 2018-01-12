3155bb4ace61107277d1c5c8275309a3c6c10ffc
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-01-12 10:52:13 +0800

    test:register string

--- wp-content/themes/simright/functions.php
+++ wp-content/themes/simright/functions.php
@@ -10,12 +10,12 @@ if( function_exists('register_sidebar') ) {
 		'after_title' => '</h4>'
 	));
 }
-?>
 
-<?php
 /** 注册字符串 */
-pll_register_string('nav_home','Home');
-pll_register_string('nav_products','Products');
-pll_register_string('nav_products_public','Public Cloud Apps');
-pll_register_string('nav_products_private','Private Cloud Solutions');
+if( function_exists('pll_register_string') ) {
+	pll_register_string('nav_home','Home');
+	pll_register_string('nav_products','Products');
+	pll_register_string('nav_products_public','Public Cloud Apps');
+	pll_register_string('nav_products_private','Private Cloud Solutions');
+}
 ?>
