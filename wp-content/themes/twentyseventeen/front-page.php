351d71d7ea4fda49f2bebd0371b72846af00bbf8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-01-12 20:01:51 +0000

    Twenty Seventeen: Remove unnecessary comment in `front-page.php`.
    Props hardik-amipara, poena.
    Fixes #42820.
    Built from https://develop.svn.wordpress.org/trunk@42437
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42267 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyseventeen/front-page.php
+++ wp-content/themes/twentyseventeen/front-page.php
@@ -24,7 +24,7 @@ get_header(); ?>
 				the_post();
 				get_template_part( 'template-parts/page/content', 'front-page' );
 			endwhile;
-		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
+		else :
 			get_template_part( 'template-parts/post/content', 'none' );
 		endif;
 		?>
