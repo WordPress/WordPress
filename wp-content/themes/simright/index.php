d894655f7c486fe45a051f318d99ac4597307e57
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-01-12 13:36:04 +0800

    test:缩略图

--- wp-content/themes/simright/index.php
+++ wp-content/themes/simright/index.php
@@ -5,6 +5,23 @@
         <P>Simulation in your browser</P>
         <P>Accessible,Cost-efficient,For everyone</P>
     </div>
-   
+    <section class="contain-box">
+        <div class="slide-bar">
+            <form class="neck-bar-search">
+                <div class="form-group input-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                    <button type="submit" class="input-group-addon"><i class="glyphicon glyphicon-search"></i></button>
+                </div>
+            </form>
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Viedeo_list_classification') ) : ?>
+                <ul>
+                    <?php wp_list_categories('depth=1&title_li=&orderby=id&show_count=1&hide_empty=1&child_of=0'); ?>
+                </ul>
+            <?php endif; ?>
+        </div>
+        <div class="contain-body">
+            
+        </div>
+    </section>
 </section>
 <?php get_footer(); ?>
