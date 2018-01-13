a492541c3570c2853ed8546bcbd2929b257afe84
Peter Wilson <wilson@peterwilson.cc>
2018-01-13 01:11:49 +0000

    Formatting: Do not run formatting and texturization on author bios.
    Removes the formatting and texturization added to author bios in #40040 due to back-compatibility concerns.
    
    Reverts [41172], [41173].
    
    Props 0x6f0, rabmalin for review.
    Fixes #42578.
    
    
    Built from https://develop.svn.wordpress.org/trunk@42441
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42271 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42440';
+$wp_version = '5.0-alpha-42441';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
