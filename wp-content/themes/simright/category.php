db767052000477bd87a6a99ec6cde441dcdcd322
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-01-16 18:31:14 +0800

    fix:category class

--- wp-content/themes/simright/category.php
+++ wp-content/themes/simright/category.php
@@ -1,15 +1,15 @@
 <?php
     if ( in_category('video') || post_is_in_descendant_category('video') )
     {
-    include(TEMPLATEPATH .'/single-video.php');
+    include(TEMPLATEPATH .'/category-video.php');
     }
     elseif ( in_category('news') || post_is_in_descendant_category('news') )
     {
-    include(TEMPLATEPATH . '/single-news.php');
+    include(TEMPLATEPATH . '/category-news.php');
     }
     elseif ( in_category('joinus') || post_is_in_descendant_category('joinus') )
     {
-    include(TEMPLATEPATH . '/single-joinus.php');
+    include(TEMPLATEPATH . '/category-joinus.php');
     }
     else{
     return;
