912cef3697c260cffdacf733ca1e7229f8ee5a7d
Dion Hulse <wordpress@dd32.id.au>
2018-01-16 06:57:27 +0000

    Upgrade: When deleting old files, if deletion fails attempt to empty the file instead.
    Props joemcgill, dd32.
    Merges [42434] to the 4.3 branch.
    Fixes #42963 for 4.3.
    
    Built from https://develop.svn.wordpress.org/branches/4.3@42471
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.3@42300 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/update-core.php
+++ wp-admin/includes/update-core.php
@@ -1067,7 +1067,11 @@ function update_core($from, $to) {
 		$old_file = $to . $old_file;
 		if ( !$wp_filesystem->exists($old_file) )
 			continue;
-		$wp_filesystem->delete($old_file, true);
+
+		// If the file isn't deleted, try writing an empty string to the file instead.
+		if ( ! $wp_filesystem->delete( $old_file, true ) && $wp_filesystem->is_file( $old_file ) ) {
+			$wp_filesystem->put_contents( $old_file, '' );
+		}
 	}
 
 	// Remove any Genericons example.html's from the filesystem
