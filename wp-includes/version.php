3fe9cb61ee71fcfadb5e002399296fcc1198d850
Dion Hulse <wordpress@dd32.id.au>
2018-01-16 06:28:43 +0000

    External Libraries: Remove unnecessary / obsoleted MediaElement.js files.
    Props joemcgill.
    Fixes #42720 for trunk.
    
    Built from https://develop.svn.wordpress.org/trunk@42462
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42291 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42461';
+$wp_version = '5.0-alpha-42462';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
