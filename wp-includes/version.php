ceaaed6604ac38d4e03a5d3da9a9f349f5be5c5f
Dion Hulse <wordpress@dd32.id.au>
2018-01-16 03:58:49 +0000

    Build: Use Node 8.x for trunk.
    Props netweb.
    See #35105.
    
    Built from https://develop.svn.wordpress.org/trunk@42461
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42290 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42460';
+$wp_version = '5.0-alpha-42461';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
