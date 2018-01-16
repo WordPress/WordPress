82054452d2b535637cd581375ef59a4b492a473f
Dion Hulse <wordpress@dd32.id.au>
2018-01-16 06:36:30 +0000

    Build: Bump the version of node used on Travis to match WordPress.org.
    Props netweb, dd32.
    See #35105
    
    Built from https://develop.svn.wordpress.org/trunk@42464
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42293 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42463';
+$wp_version = '5.0-alpha-42464';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
