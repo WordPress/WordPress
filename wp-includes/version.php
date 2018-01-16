d75574cd84f75a2eca2a2db19bd57dd2e8f01601
Dion Hulse <wordpress@dd32.id.au>
2018-01-16 21:39:32 +0000

    Bump the 4.8 branch to 4.8.5. Built from https://develop.svn.wordpress.org/branches/4.8@42495
    git-svn-id: http://core.svn.wordpress.org/branches/4.8@42324 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8.4';
+$wp_version = '4.8.5';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
