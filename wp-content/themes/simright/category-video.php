9c4fe67492021eee3bf5b612407e6d9511843729
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-01-16 16:34:04 +0800

    test video category slide bar class

--- wp-content/themes/simright/category-video.php
+++ wp-content/themes/simright/category-video.php
@@ -1,7 +1,69 @@
 <?php get_header(); ?>
-<section class="main-box video-details">
+<section class="main-box video-list">
+    <div class="banner">
+        <h2 class="text-center">ABOUT US</h2>
+        <P>Simulation in your browser</P>
+        <P>Accessible,Cost-efficient,For everyone</P>
+    </div>
     <section class="contain-box">
-        category video
+        <div class="slide-bar">
+            <?php get_search_form(); ?>
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Viedeo_list_classification') ) : ?>
+                <ul>
+                    <?php $args = array(
+                        'show_option_all'    => '',
+                        'orderby'            => 'name',
+                        'order'              => 'ASC',
+                        'style'              => 'list',
+                        'show_count'         => 1,
+                        'hide_empty'         => 1,
+                        'use_desc_for_title' => 1,
+                        'child_of'           => 0,
+                        'feed'               => '',
+                        'feed_type'          => '',
+                        'feed_image'         => '',
+                        'exclude'            => '',
+                        'exclude_tree'       => '',
+                        'include'            => '',
+                        'hierarchical'       => 1,
+                        'title_li'           => __( 'Categories' ),
+                        'show_option_none'   => __('No categories'),
+                        'number'             => null,
+                        'echo'               => 1,
+                        'depth'              => 0,
+                        'current_category'   => 0,
+                        'pad_counts'         => 0,
+                        'taxonomy'           => 'category',
+                        'walker'             => null
+                    ); 
+                    wp_list_categories($args)
+                    ?>
+                </ul>
+            <?php endif; ?>
+        </div>
+        <div class="contain-body">
+            <section class="list-item">
+                <h2>LATEST</h2>
+                <hr/>
+                <div class="list-contain">
+                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <div class="video-item">
+                        <a href="<?php the_permalink(); ?>">
+                            <img src="<?php post_thumbnail_src('thumbnail'); ?>" alt="">
+                            <span><?php the_title(); ?></span>
+                            <p><?php the_time('Y-m-d') ?></p>
+                        </a>
+                    </div>
+                    <?php endwhile; ?>
+                    <?php else : ?>
+                        <h3 class="title"><a href="#" rel="bookmark">NOT FOUND</a></h3>
+                    <?php endif; ?>
+                </div>
+            </section>
+        </div>
+        <div>
+            <?php link_pages('<p><code>Pages:</strong> ', '</p>', 'number'); ?>
+        </div>
     </section>
 </section>
 <?php get_footer(); ?>
