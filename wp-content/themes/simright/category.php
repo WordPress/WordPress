46b586b9c51c40978e2feaa9921aea61b40c95d4
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-01-16 18:20:08 +0800

    test:category 分类

--- wp-content/themes/simright/category.php
+++ wp-content/themes/simright/category.php
@@ -1,7 +1,17 @@
-<?php get_header(); ?>
-<section class="main-box video-details">
-    <section class="contain-box">
-        category
-    </section>
-</section>
-<?php get_footer(); ?>
+<?php
+    if ( in_category('video') || post_is_in_descendant_category('video') )
+    {
+    include(TEMPLATEPATH .'/single-video.php');
+    }
+    elseif ( in_category('news') || post_is_in_descendant_category('news') )
+    {
+    include(TEMPLATEPATH . '/single-news.php');
+    }
+    elseif ( in_category('joinus') || post_is_in_descendant_category('joinus') )
+    {
+    include(TEMPLATEPATH . '/single-joinus.php');
+    }
+    else{
+    return;
+    }
+?>
