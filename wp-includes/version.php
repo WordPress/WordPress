443594dd0075e075fd563b10b0de1c25e573100a
Weston Ruter <weston@xwp.co>
2018-01-17 01:24:30 +0000

    Widgets: Prevent `checkValidity` from running on a form when widget is first adding to sidebar.
    Amends [41352].
    See #23120.
    Fixes #43003 for trunk.
    
    Built from https://develop.svn.wordpress.org/trunk@42521
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42350 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42492';
+$wp_version = '5.0-alpha-42521';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
