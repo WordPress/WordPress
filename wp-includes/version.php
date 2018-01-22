db02fcb8f82d1c416dbdf3f428fede55af6dbb3d
Weston Ruter <weston@xwp.co>
2018-01-22 19:12:43 +0000

    Customize: Let default status for Customizer be `draft` if user does not have capability to `publish`.
    Amends [41626].
    Props sayedwp, westonruter.
    See #30937.
    Fixes #42686 for 4.9 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.9@42544
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42373 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.3-alpha-42542';
+$wp_version = '4.9.3-alpha-42544';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
