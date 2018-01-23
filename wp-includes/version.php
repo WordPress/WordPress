bbb8a324c778996519c1ede6914bad6a5a9f0257
Andrew Ozz <admin@laptoptips.ca>
2018-01-23 19:41:42 +0000

    Merged revision(s) 42576 from trunk: TinyMCE: backport bug fixes to `NotificationManager` and `WindowManager` from https://github.com/tinymce/tinymce/commit/d1877933fa1e98430e5e9e1f78884be60766d2f0#diff-2e5c98d81ce2481aa1042b2320728b65 and to `MultiRange` from https://github.com/tinymce/tinymce/commit/250675bb7bae2e6ef5cef715ef57cdf5ab6bff8a.
    Props peterwilsoncc, azaozz.
    Fixes #42722 for 4.9.3, see #43012.
    
    Built from https://develop.svn.wordpress.org/branches/4.9@42577
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42406 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.3-alpha-42575';
+$wp_version = '4.9.3-alpha-42577';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 38590;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '4607-20171116';
+$tinymce_version = '4607-20180123';
 
 /**
  * Holds the required PHP version
