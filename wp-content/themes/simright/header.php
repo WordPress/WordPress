15556c7607647090dabefb8ed2fbe5e2d98bff95
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-01-23 11:16:35 +0800

    fix:添加google统计代码

--- wp-content/themes/simright/header.php
+++ wp-content/themes/simright/header.php
@@ -23,6 +23,16 @@
     <link rel="shortcut icon" href="https://oss.simright.com/img/favicon.ico">
     <link rel="stylesheet" href="https://oss.simright.com/static/bootstrap.min.css">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-86805722-1', 'auto');
+    ga('send', 'pageview');
+    </script>
+    <!--GA Code-->
 </head>
 <?php flush(); ?>
 <body>
