ee58ffcbe7dfa0c536efce99c54a44c0de044779
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-01-24 16:51:30 +0000

    Emoji: Update emoji base URL in `_print_emoji_detection_script()` and `wp_staticize_emoji()`.
    Props pento.
    Fixes #42862.
    Built from https://develop.svn.wordpress.org/trunk@42590
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42419 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -5325,7 +5325,7 @@ function _print_emoji_detection_script() {
 		 *
 		 * @param string The emoji base URL for png images.
 		 */
-		'baseUrl' => apply_filters( 'emoji_url', 'https://s.w.org/images/core/emoji/2.3/72x72/' ),
+		'baseUrl' => apply_filters( 'emoji_url', 'https://s.w.org/images/core/emoji/2.4/72x72/' ),
 
 		/**
 		 * Filters the extension of the emoji png files.
@@ -5343,7 +5343,7 @@ function _print_emoji_detection_script() {
 		 *
 		 * @param string The emoji base URL for svg images.
 		 */
-		'svgUrl'  => apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2.3/svg/' ),
+		'svgUrl'  => apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2.4/svg/' ),
 
 		/**
 		 * Filters the extension of the emoji SVG files.
@@ -5465,7 +5465,7 @@ function wp_staticize_emoji( $text ) {
 	}
 
 	/** This filter is documented in wp-includes/formatting.php */
-	$cdn_url = apply_filters( 'emoji_url', 'https://s.w.org/images/core/emoji/2.3/72x72/' );
+	$cdn_url = apply_filters( 'emoji_url', 'https://s.w.org/images/core/emoji/2.4/72x72/' );
 
 	/** This filter is documented in wp-includes/formatting.php */
 	$ext = apply_filters( 'emoji_ext', '.png' );
