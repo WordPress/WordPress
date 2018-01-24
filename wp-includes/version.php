723d0f266986412fe712e09bad5fd83f9be47327
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-01-24 01:53:38 +0000

    Media: Introduce a custom error handler for MediaElement.js to display a download link if a FLV file cannot be rendered for any reason.
    Props Clorith.
    Merges [42582] to the 4.9 branch.
    Fixes #42643.
    Built from https://develop.svn.wordpress.org/branches/4.9@42583
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42412 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.3-alpha-42580';
+$wp_version = '4.9.3-alpha-42583';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
