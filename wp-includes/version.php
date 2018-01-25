7d9c47d5368eb63cdec7f18f2cd5d14b24ed644d
Gary Pendergast <gary@pento.net>
2018-01-25 01:06:31 +0000

    Emoji: Tweak the generated emoji arrays to not cause coding standards errors.
    Props netweb, jrf.
    Fixes #43150.
    
    
    Built from https://develop.svn.wordpress.org/trunk@42610
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42439 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42606';
+$wp_version = '5.0-alpha-42610';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
