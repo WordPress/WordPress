a996d3df685f4c1e7e9d83c4368e7319f64347c6
Qmengxi <mengxiaoxi_qian@sina.cn>
2018-01-29 10:40:58 +0800

    fix(news):style

--- wp-content/themes/simright/category-news.php
+++ wp-content/themes/simright/category-news.php
@@ -5,16 +5,23 @@
     </div>
     <section class="contain-box">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <div class="news-item">
-            <p><?php the_time('Y-m-d') ?></p>
-            <hr>
-            <a href="<?php the_permalink(); ?>">
-                <h3 class="title"><?php the_title(); ?></h3>
-            </a>
-            <div class="content">
-                <?php the_content(); ?>
-            </div>
-        </div>
+		<div class="news-item-wrap">
+			<div class="point">
+			</div>
+			<div class="news-item">
+				<div class="arrow">
+					
+				</div>
+            	<p><?php the_time('Y-m-d') ?></p>
+				<hr>
+				<a href="<?php the_permalink(); ?>">
+					<h3 class="title"><?php the_title(); ?></h3>
+				</a>
+				<div class="content">
+					<?php the_excerpt(); ?>
+				</div>
+			</div>
+		</div>
         <?php endwhile; ?>
         <?php else : ?>
             <h3 class="title"><a href="#" rel="bookmark">NOT FOUND</a></h3>
--- wp-content/themes/simright/single-news.php
+++ wp-content/themes/simright/single-news.php
@@ -12,7 +12,7 @@
                             <p><?php the_time('Y-m-d') ?></p>
                         </div>
                         <div>
-                            <?php the_content(); ?>
+                            <?php the_excerpt(); ?>
                         </div>
                     </div>
                 <?php else : ?>
