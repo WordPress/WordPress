8edf395b50978417d20c2585c75c363fd8a71469
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-02-01 00:56:41 +0000

    Post WordPress 4.9.3 RC 1 version bump. Built from https://develop.svn.wordpress.org/branches/4.9@42629
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42458 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.3-RC1';
+$wp_version = '4.9.3-RC1-42629';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
