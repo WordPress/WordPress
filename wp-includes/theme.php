abdea23275dc0f7e17468014708cf833f0f683e6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-02-02 09:38:30 +0000

    Docs: Convert `$args` array description for `wp_get_themes()` to a hash notation.
    Props milana_cap.
    Fixes #43213.
    Built from https://develop.svn.wordpress.org/trunk@42640
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42469 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/theme.php
+++ wp-includes/theme.php
@@ -17,14 +17,17 @@
  * @global array $wp_theme_directories
  * @staticvar array $_themes
  *
- * @param array $args The search arguments. Optional.
- * - errors      mixed  True to return themes with errors, false to return themes without errors, null
- *                      to return all themes. Defaults to false.
- * - allowed     mixed  (Multisite) True to return only allowed themes for a site. False to return only
- *                      disallowed themes for a site. 'site' to return only site-allowed themes. 'network'
- *                      to return only network-allowed themes. Null to return all themes. Defaults to null.
- * - blog_id     int    (Multisite) The blog ID used to calculate which themes are allowed. Defaults to 0,
- *                      synonymous for the current blog.
+ * @param array $args {
+ *     Optional. The search arguments.
+ *
+ *     @type mixed $errors  True to return themes with errors, false to return themes without errors, null to return all themes.
+ *                          Defaults to false.
+ *     @type mixed $allowed (Multisite) True to return only allowed themes for a site. False to return only disallowed themes for a site.
+ *                          'site' to return only site-allowed themes. 'network' to return only network-allowed themes.
+ *                          Null to return all themes. Defaults to null.
+ *     @type int   $blog_id (Multisite) The blog ID used to calculate which themes are allowed.
+ *                          Defaults to 0, synonymous for the current blog.
+ * }
  * @return array Array of WP_Theme objects.
  */
 function wp_get_themes( $args = array() ) {
