f45e696e219ba8599233893bad4df649eec071ee
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-02-02 07:48:31 +0000

    Media: Avoid a PHP warning in `wp_calculate_image_srcset()` if a plugin returns a non-array value via `wp_calculate_image_srcset` filter.
    Props mrmadhat, codegrau.
    Fixes #43201.
    Built from https://develop.svn.wordpress.org/trunk@42639
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42468 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42638';
+$wp_version = '5.0-alpha-42639';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
