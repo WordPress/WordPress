4951b2897ea286249bf9fa66c98b5ec55d1479d9
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-02-03 13:54:31 +0000

    Media: Remove checkered background for icons in Attachment Details.
    Props BandonRandon.
    Fixes #42535. See #41948.
    Built from https://develop.svn.wordpress.org/trunk@42642
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42471 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42641';
+$wp_version = '5.0-alpha-42642';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
