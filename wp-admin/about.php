dd84717899d89d1cfd7cb4f77f66c9bcd371a734
Dion Hulse <wordpress@dd32.id.au>
2018-02-06 15:42:43 +0000

    Bump the 4.9 branch to version 4.9.4.
    Built from https://develop.svn.wordpress.org/branches/4.9@42656
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42485 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -39,6 +39,24 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 					_n(
 						'<strong>Version %1$s</strong> addressed %2$s bug.',
 						'<strong>Version %1$s</strong> addressed %2$s bugs.',
+						1
+					),
+					'4.9.4',
+					number_format_i18n( 1 )
+				);
+				?>
+				<?php
+				/* translators: %s: Codex URL */
+				printf( __( 'For more information, see <a href="%s">the release notes</a>.' ), 'https://codex.wordpress.org/Version_4.9.4' );
+				?>
+			</p>
+			<p>
+				<?php
+				printf(
+					/* translators: 1: WordPress version number, 2: plural number of bugs. */
+					_n(
+						'<strong>Version %1$s</strong> addressed %2$s bug.',
+						'<strong>Version %1$s</strong> addressed %2$s bugs.',
 						34
 					),
 					'4.9.3',
