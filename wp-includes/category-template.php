9dd9913ecdd47a821d68088ef2118e6432483c55
Drew Jaynes <info@drewapicture.com>
2018-02-07 15:22:34 +0000

    Docs: Document default arguments for `wp_tag_cloud()` in a hash notation, noting that full lists of additionally-supported arguments are already documented in `get_terms()` and `wp_generate_tag_cloud()`.
    Props birgire, johnbillion.
    Fixes #42019.
    
    Built from https://develop.svn.wordpress.org/trunk@42658
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42487 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/category-template.php
+++ wp-includes/category-template.php
@@ -652,37 +652,24 @@ function wp_list_categories( $args = '' ) {
 }
 
 /**
- * Display tag cloud.
- *
- * The text size is set by the 'smallest' and 'largest' arguments, which will
- * use the 'unit' argument value for the CSS text size unit. The 'format'
- * argument can be 'flat' (default), 'list', or 'array'. The flat value for the
- * 'format' argument will separate tags with spaces. The list value for the
- * 'format' argument will format the tags in a UL HTML list. The array value for
- * the 'format' argument will return in PHP array type format.
- *
- * The 'orderby' argument will accept 'name' or 'count' and defaults to 'name'.
- * The 'order' is the direction to sort, defaults to 'ASC' and can be 'DESC'.
- *
- * The 'number' argument is how many tags to return. By default, the limit will
- * be to return the top 45 tags in the tag cloud list.
- *
- * The 'topic_count_text' argument is a nooped plural from _n_noop() to generate the
- * text for the tag link count.
- *
- * The 'topic_count_text_callback' argument is a function, which given the count
- * of the posts with that tag returns a text for the tag link count.
- *
- * The 'post_type' argument is used only when 'link' is set to 'edit'. It determines the post_type
- * passed to edit.php for the popular tags edit links.
- *
- * The 'exclude' and 'include' arguments are used for the get_tags() function. Only one
- * should be used, because only one will be used and the other ignored, if they are both set.
+ * Displays a tag cloud.
  *
  * @since 2.3.0
  * @since 4.8.0 Added the `show_count` argument.
  *
- * @param array|string|null $args Optional. Override default arguments.
+ * @param array|string $args {
+ *     Optional. Array or string of arguments for displaying a tag cloud. See wp_generate_tag_cloud()
+ *     and get_terms() for the full lists of arguments that can be passed in `$args`.
+ *
+ *     @type int    $number    The number of tags to display. Accepts any positive integer
+ *                             or zero to return all. Default 0 (all tags).
+ *     @type string $link      Whether to display term editing links or term permalinks.
+ *                             Accepts 'edit' and 'view'. Default 'view'.
+ *     @type string $post_type The post type. Used to highlight the proper post type menu
+ *                             on the linked edit page. Defaults to the first post type
+ *                             associated with the taxonomy.
+ *     @type bool   $echo      Whether or not to echo the return value. Default true.
+ * }
  * @return void|array Generated tag cloud, only if no failures and 'array' is set for the 'format' argument.
  *                    Otherwise, this function outputs the tag cloud.
  */
@@ -771,9 +758,9 @@ function default_topic_count_scale( $count ) {
  * @since 2.3.0
  * @since 4.8.0 Added the `show_count` argument.
  *
- * @param array $tags List of tags.
+ * @param array $tags Array of WP_Term objects to generate the tag cloud for.
  * @param string|array $args {
- *     Optional. Array of string of arguments for generating a tag cloud.
+ *     Optional. Array or string of arguments for generating a tag cloud.
  *
  *     @type int      $smallest                   Smallest font size used to display tags. Paired
  *                                                with the value of `$unit`, to determine CSS text
