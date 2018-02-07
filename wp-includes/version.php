1c2ef0f4750f530dbcc6ca423e693e9701d9d8c7
John Blackbourn <johnbillion@git.wordpress.org>
2018-02-07 22:21:30 +0000

    Build/Test Tools: Allow code coverage reports to be generated.
    This corrects the attribute needed to allow PHPUnit to generate coverage reports, and also adds a blacklist of files and directories that are excluded from coverage reports.
    
    Props schlessera, jipmoors
    
    Fixes #43067, #43240
    
    Built from https://develop.svn.wordpress.org/trunk@42665
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42494 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42664';
+$wp_version = '5.0-alpha-42665';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
