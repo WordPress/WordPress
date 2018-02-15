7d2cd516d1e718754b8ad403374855b073264f89
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-02-15 03:34:30 +0000

    Twenty Seventeen: Underline links in comments.
    Props jainnidhi, larrach, audrasjb.
    Fixes #43317. 
    Built from https://develop.svn.wordpress.org/trunk@42713
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42541 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42712';
+$wp_version = '5.0-alpha-42713';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
