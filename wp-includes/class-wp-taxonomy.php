c3738e9617a26e5bab47fb124cd26a3e88391069
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-02-25 20:38:30 +0000

    Docs: Remove `@access` notation from `WP_Taxonomy::$meta_box_sanitize_cb`.
    See #42505, #41452.
    Built from https://develop.svn.wordpress.org/trunk@42748
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42578 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-taxonomy.php
+++ wp-includes/class-wp-taxonomy.php
@@ -131,7 +131,6 @@ final class WP_Taxonomy {
 	 * The callback function for sanitizing taxonomy data saved from a meta box.
 	 *
 	 * @since 5.0.0
-	 * @access public
 	 * @var callable
 	 */
 	public $meta_box_sanitize_cb = null;
