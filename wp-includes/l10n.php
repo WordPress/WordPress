5cde2018ea0ed9d2aa4c4fef72efcb6e3c57261b
Pascal Birchler <pascal.birchler@gmail.com>
2018-02-26 15:57:32 +0000

    Docs: Remove erroneous period in the description for `esc_html__()`.
    Props danieltj.
    Fixes #43419.
    
    Built from https://develop.svn.wordpress.org/trunk@42753
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42583 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/l10n.php
+++ wp-includes/l10n.php
@@ -222,7 +222,7 @@ function esc_attr__( $text, $domain = 'default' ) {
  * Retrieve the translation of $text and escapes it for safe use in HTML output.
  *
  * If there is no translation, or the text domain isn't loaded, the original text
- * is escaped and returned..
+ * is escaped and returned.
  *
  * @since 2.8.0
  *
