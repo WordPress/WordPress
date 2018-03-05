9842bd6d248ae57005dcc299a61f741a2ef7389a
Andrea Fercia <a.fercia@gmail.com>
2018-03-05 15:50:30 +0000

    Accessibility: Change the "Show / Hide dismissed updates" link to a button.
    For better accessibility and semantics, user interface controls that perform an
    action should be buttons. Links should exclusively be used for navigation.
    Also, adds an `aria-expanded` attribute to communicate the expandable panel state
    and improves the buttons spacing.
    
    Props Cheffheid, audrasjb, afercia.
    Fixes #38674.
    
    Built from https://develop.svn.wordpress.org/trunk@42785
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42615 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/update-core.php
+++ wp-admin/update-core.php
@@ -138,15 +138,14 @@ function dismissed_updates() {
 		$hide_text = esc_js( __( 'Hide hidden updates' ) );
 	?>
 	<script type="text/javascript">
-
-		jQuery(function($) {
-			$('dismissed-updates').show();
-			$('#show-dismissed').toggle(function(){$(this).text('<?php echo $hide_text; ?>');}, function() {$(this).text('<?php echo $show_text; ?>')});
-			$('#show-dismissed').click(function() { $('#dismissed-updates').toggle('slow');});
+		jQuery(function( $ ) {
+			$( 'dismissed-updates' ).show();
+			$( '#show-dismissed' ).toggle( function() { $( this ).text( '<?php echo $hide_text; ?>' ).attr( 'aria-expanded', 'true' ); }, function() { $( this ).text( '<?php echo $show_text; ?>' ).attr( 'aria-expanded', 'false' ); } );
+			$( '#show-dismissed' ).click( function() { $( '#dismissed-updates' ).toggle( 'fast' ); } );
 		});
 	</script>
 	<?php
-		echo '<p class="hide-if-no-js"><a id="show-dismissed" href="#">' . __( 'Show hidden updates' ) . '</a></p>';
+		echo '<p class="hide-if-no-js"><button type="button" class="button" id="show-dismissed" aria-expanded="false">' . __( 'Show hidden updates' ) . '</button></p>';
 		echo '<ul id="dismissed-updates" class="core-updates dismissed">';
 	foreach ( (array) $dismissed as $update ) {
 		echo '<li>';
