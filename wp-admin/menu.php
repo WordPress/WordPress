b93dfd3571fff236db62c729f4d688ded55db1d6
Andrew Ozz <admin@laptoptips.ca>
2018-03-09 15:04:33 +0000

    Add Privacy Tools admin page under the Tools menu.
    Props allendav, xkon, azaozz.
    See #43435.
    Built from https://develop.svn.wordpress.org/trunk@42814
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42644 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/menu.php
+++ wp-admin/menu.php
@@ -256,6 +256,7 @@ $menu[75]                     = array( __( 'Tools' ), 'edit_posts', 'tools.php',
 	$submenu['tools.php'][5]  = array( __( 'Available Tools' ), 'edit_posts', 'tools.php' );
 	$submenu['tools.php'][10] = array( __( 'Import' ), 'import', 'import.php' );
 	$submenu['tools.php'][15] = array( __( 'Export' ), 'export', 'export.php' );
+	$submenu['tools.php'][20] = array( __( 'Privacy' ), 'manage_options', 'privacy.php' );
 if ( is_multisite() && ! is_main_site() ) {
 	$submenu['tools.php'][25] = array( __( 'Delete Site' ), 'delete_site', 'ms-delete-site.php' );
 }
