076fb0ff4d9facdc8255a58a4932378a562559a2
Dominik Schilling <dominikschilling+git@gmail.com>
2018-04-03 15:22:42 +0000

    Login: Use `wp_safe_redirect()` when redirecting the login page if forced to use HTTPS.
    Merge of [42892] to the 4.9 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.9@42895
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42725 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.5-RC1-42885';
+$wp_version = '4.9.5-RC1-42895';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
