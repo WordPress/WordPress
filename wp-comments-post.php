cf02373b393bf5bcd52b0e55cbcd83723051b824
John Blackbourn <johnbillion@git.wordpress.org>
2018-04-30 13:10:20 +0000

    Comments: Update the inline docs following [42772].
    See #43436
    
    Built from https://develop.svn.wordpress.org/trunk@43042
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42871 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-comments-post.php
+++ wp-comments-post.php
@@ -44,10 +44,11 @@ $cookies_consent = ( isset( $_POST['wp-comment-cookies-consent'] ) );
  * Perform other actions when comment cookies are set.
  *
  * @since 3.4.0
+ * @since 4.9.6 The `$cookies_consent` parameter was added.
  *
  * @param WP_Comment $comment         Comment object.
- * @param WP_User    $user            User object. The user may not exist.
- * @param boolean    $cookies_consent Whether the user has opted-in commenter cookies.
+ * @param WP_User    $user            Comment author's user object. The user may not exist.
+ * @param boolean    $cookies_consent Comment author's consent to store cookies.
  */
 do_action( 'set_comment_cookies', $comment, $user, $cookies_consent );
 
--- wp-includes/comment.php
+++ wp-includes/comment.php
@@ -540,11 +540,12 @@ function wp_queue_comments_for_comment_meta_lazyload( $comments ) {
  * Sets the cookies used to store an unauthenticated commentator's identity. Typically used
  * to recall previous comments by this commentator that are still held in moderation.
  *
- * @param WP_Comment $comment Comment object.
- * @param object     $user    Comment author's object.
- * @param boolean    $cookies_consent Optional. Comment author's consent to store cookies. Default true.
- *
  * @since 3.4.0
+ * @since 4.9.6 The `$cookies_consent` parameter was added.
+ *
+ * @param WP_Comment $comment         Comment object.
+ * @param WP_User    $user            Comment author's user object. The user may not exist.
+ * @param boolean    $cookies_consent Optional. Comment author's consent to store cookies. Default true.
  */
 function wp_set_comment_cookies( $comment, $user, $cookies_consent = true ) {
 	// If the user already exists, or the user opted out of cookies, don't set cookies.
