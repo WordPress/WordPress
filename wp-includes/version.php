a6284311e838a422a5170b1c20f6da553d02eed5
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-05-02 03:44:24 +0000

    Privacy: improve `wp_privacy_erase_personal_data()`, return boolean values.
    Props ericdaams.
    Merges [43061] to the 4.9 branch.
    See #43602.
    Built from https://develop.svn.wordpress.org/branches/4.9@43110
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@42939 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.6-alpha-43109';
+$wp_version = '4.9.6-alpha-43110';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
