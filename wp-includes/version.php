64192e967e7b7f158fbbcbf343a0044816be45f8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-05-02 04:11:21 +0000

    Privacy: Correct unit test for `wp_user_personal_data_exporter()` added in [43055].
    `user_status` is not considered personal data, so the total number of exported user properties is 11.
    
    See #43547.
    Built from https://develop.svn.wordpress.org/trunk@43116
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42945 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43104';
+$wp_version = '5.0-alpha-43116';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
