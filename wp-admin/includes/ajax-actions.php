6145ef6deae0a861130ed355b0a18162b4f31c53
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-05-02 03:14:21 +0000

    Docs: Correct DocBlock formatting for `wp_privacy_personal_data_erasers` filter.
    See #43637.
    Built from https://develop.svn.wordpress.org/trunk@43104
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42933 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/ajax-actions.php
+++ wp-admin/includes/ajax-actions.php
@@ -4561,6 +4561,7 @@ function wp_ajax_wp_privacy_erase_personal_data() {
 	 *
 	 * @param array $args {
 	 *     An array of callable erasers of personal data. Default empty array.
+	 *
 	 *     @type array {
 	 *         Array of personal data exporters.
 	 *
