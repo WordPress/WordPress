fbc98b8e7573caed4d191db33c9d45e09f1831e2
Andrew Ozz <admin@laptoptips.ca>
2018-05-03 19:31:21 +0000

    Privacy: rename `manage_privacy_policy` to `manage_privacy_options`.
    Props desrosj.
    FIxes #43935.
    Built from https://develop.svn.wordpress.org/trunk@43155
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@42984 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/capabilities.php
+++ wp-includes/capabilities.php
@@ -557,7 +557,7 @@ function map_meta_cap( $cap, $user_id ) {
 			break;
 		case 'export_others_personal_data':
 		case 'erase_others_personal_data':
-		case 'manage_privacy_policy':
+		case 'manage_privacy_options':
 			$caps[] = is_multisite() ? 'manage_network' : 'manage_options';
 			break;
 		default:
