094c20d0041127ef6e062bcdf1154e770382f715
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-05-09 14:33:26 +0000

    Privacy: Mark erasure requests as completed after processing.
    r42986 introduced the beginnings of an Ajax handler for processing requests to erase personal data. At the time, a method for marking requests as completed was planned, but had not yet been created. This commit introduces that mechanism, bringing the erasure process closer to completion.
    
    Props coreymckrill, allendav.
    Merges [43185] to the 4.9 branch.
    Fixes #43922.
    Built from https://develop.svn.wordpress.org/branches/4.9@43188
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43017 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.6-beta1-43187';
+$wp_version = '4.9.6-beta1-43188';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
