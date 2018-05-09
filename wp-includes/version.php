1d477066e4cbddb1b0916dffdcb6127764becab3
Andrew Ozz <admin@laptoptips.ca>
2018-05-09 17:54:24 +0000

    Privacy: add request type and filter to the subject of request confirmation emails for GDPR.
    Props desrosj, azaozz.
    Merges [43197] to the 4.9 branch.
    Fixes #43968.
    Built from https://develop.svn.wordpress.org/branches/4.9@43198
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43027 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.6-beta1-43196';
+$wp_version = '4.9.6-beta1-43198';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
