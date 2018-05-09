83c07f7a50c05a57effca2b2faffe5e8a021c0a1
Andrew Ozz <admin@laptoptips.ca>
2018-05-09 16:13:21 +0000

    Privacy: fix spacing on small screens for the Use This Page button in Privacy Tools.
    Props PressTigers, desrosj.
    Fixes #43852.
    Built from https://develop.svn.wordpress.org/trunk@43193
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43022 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43191';
+$wp_version = '5.0-alpha-43193';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
