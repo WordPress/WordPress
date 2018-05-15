9b3e2b76b41bf5b5db5e85395174633a9188d5f0
Andrew Ozz <admin@laptoptips.ca>
2018-05-15 14:08:29 +0000

    Privacy: Escape comment URLs in personal export file to prevent XSS.
    There doesn't appear to be any way for an attacker to introduce malicious input into the URL, unless a plugin is filtering the URL to add it, but it's better to be safe than sorry.
    
    Props birgire.
    Merges [43245] to the 4.9 branch.
    Fixes #44054.
    Built from https://develop.svn.wordpress.org/branches/4.9@43270
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43099 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.6-RC1-43268';
+$wp_version = '4.9.6-RC1-43270';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
