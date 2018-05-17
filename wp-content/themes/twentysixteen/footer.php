d097213a7b575489dbfaf359e68e04d2a61e49c3
iandunn <iandunn@git.wordpress.org>
2018-05-17 18:21:26 +0000

    Bundled Themes: Add link to privacy policy page in footer.
    If a privacy policy has been set, then a link to it will automatically be shown in the footer.
    
    The element containing the "Proudly powered by WordPress" link was chosen for the new policy link, in order to minimize visual conflicts with custom CSS that was written before the new link existed. Unfortunately, some minor conflicts are expected and unavoidable. Adding this link is required as part of GDPR compliance, and the benefits outweigh the downsides. 
    
    To further mitigate the conflicts, a new imprint class was added to the "Proudly powered..." link, in order to facilitate targeting each link invididually with custom styles.
    
    This was accidentally not backported to the `4.9` branch before the beta/RC phase, but there was a consensus that it is safe to do that this late in the release cycle. 
    See https://wordpress.slack.com/archives/C02RQBWTW/p1526577643000132.
    See https://wordpress.slack.com/archives/C02RQBWTW/p1526580781000240.
    
    Props xkon, laurelfulford, birgire, azaozz, iandunn.
    Merges [43051] to the 4.9 branch.
    See #43715.
    
    Built from https://develop.svn.wordpress.org/branches/4.9@43294
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43123 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentysixteen/footer.php
+++ wp-content/themes/twentysixteen/footer.php
@@ -48,7 +48,14 @@
 					do_action( 'twentysixteen_credits' );
 				?>
 				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a>
+				<?php
+				if ( function_exists( 'the_privacy_policy_link' ) ) {
+					the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+				}
+				?>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>" class="imprint">
+					<?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?>
+				</a>
 			</div><!-- .site-info -->
 		</footer><!-- .site-footer -->
 	</div><!-- .site-inner -->
