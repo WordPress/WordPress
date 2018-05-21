f1773beb0cbe39559237a8084196350df81210dd
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-05-21 12:43:27 +0000

    Widgets: Allow basic inline tags in `wp_sidebar_description()`.
    The customizer has allowed HTML in sidebar descriptions since adding support for sidebars. This change ensures that basic HTML is also allowed for them in the widgets admin screen.
    
    Props flixos90.
    Merges [43275] to the 4.9 branch.
    Fixes #42608.
    Built from https://develop.svn.wordpress.org/branches/4.9@43302
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43131 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/widgets.php
+++ wp-includes/widgets.php
@@ -420,8 +420,9 @@ function wp_sidebar_description( $id ) {
 
 	global $wp_registered_sidebars;
 
-	if ( isset($wp_registered_sidebars[$id]['description']) )
-		return esc_html( $wp_registered_sidebars[$id]['description'] );
+	if ( isset( $wp_registered_sidebars[ $id ]['description'] ) ) {
+		return wp_kses( $wp_registered_sidebars[ $id ]['description'], 'sidebar_description' );
+	}
 }
 
 /**
