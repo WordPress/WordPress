54f004e18440f1cbbca860d514c5e21443be517d
Gary Pendergast <gary@pento.net>
2018-05-23 11:03:24 +0000

    Following [43309], I need to change one little line, And delete some left over cruft, only then shall we prevail. There are some things easily missed, when using Git, which does not persist— Empty directories, though that didn't derail— Our Travis-based tests, which now must prevail.             Quoth Travis CI, “Build did fail.”
    See #43055.
    
    
    Built from https://develop.svn.wordpress.org/trunk@43310
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43139 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43309';
+$wp_version = '5.0-alpha-43310';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
