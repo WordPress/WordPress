d668b72f5b96cfeb477e150106345b21dfd9add7
John Blackbourn <johnbillion@git.wordpress.org>
2018-05-25 22:48:28 +0000

    Build/Test Tools: Allow the unit test framework to be used without the data directory in place.
    Fixes #43982
    
    Merges [43315] to the 4.9 branch.
    
    Built from https://develop.svn.wordpress.org/branches/4.9@43316
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43145 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.7-alpha-43314';
+$wp_version = '4.9.7-alpha-43316';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
