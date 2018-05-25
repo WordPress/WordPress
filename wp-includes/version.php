e4b0a8d51162c73abfd1ba3db1e7f8f72b19f8d4
John Blackbourn <johnbillion@git.wordpress.org>
2018-05-25 22:43:26 +0000

    Build/Test Tools: Allow the unit test framework to be used without the data directory in place.
    Fixes #43982
    Built from https://develop.svn.wordpress.org/trunk@43315
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43144 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43313';
+$wp_version = '5.0-alpha-43315';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
