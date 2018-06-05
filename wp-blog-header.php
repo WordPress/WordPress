1b48d5aab44926706918cee6c585e50ee6ff56dd
jeuslhg <jeuslhg@gmail.com>
2018-06-05 19:10:28 +0900

    Update wp-blog-header.php
    해당 페이지 주석 번역 완료

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -4,17 +4,24 @@
  *
  * @package WordPress
  */
+/**[Korean]
+ * WordPress 환경와 템플릿을 로드한다.
+ *
+ * @package WordPress
+ */
 
 if ( ! isset( $wp_did_header ) ) {
 
 	$wp_did_header = true;
 
 	// Load the WordPress library.
+	// 워드프레스 라이브러리를 로드한다.[Korean] 
 	require_once( dirname( __FILE__ ) . '/wp-load.php' );
 
 	// Set up the WordPress query.
+	// 워드프레스 쿼리를 설정한다.
 	wp();
-
+	
 	// Load the theme template.
 	require_once( ABSPATH . WPINC . '/template-loader.php' );
 
