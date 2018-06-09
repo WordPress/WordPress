4f6f78c280eaed4149f4308ab71b7b697ddfe86c
jeuslhg <jeuslhg@gmail.com>
2018-06-09 13:16:57 +0900

    Update wp-blog-header.php

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -10,15 +10,12 @@ if ( ! isset( $wp_did_header ) ) {
 
 	$wp_did_header = true;
 
-	// Load the WordPress library.
 	// 워드프레스 라이브러리를 로드한다.[Korean] 
 	require_once( dirname( __FILE__ ) . '/wp-load.php' );
 
-	// Set up the WordPress query.
 	// 워드프레스 쿼리를 설정한다.[Korean] 
 	wp();
 	
-	// Load the theme template.
 	// 테마 템플릿을 로드한다.[]
 	require_once( ABSPATH . WPINC . '/template-loader.php' );
 
