5850531fb5b8f8f204de67281b4db9b7127cc0ba
M.Morawski <moras.technika@gmail.com>
2018-06-17 20:07:59 +0200

    zmiana stopki

--- wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -15,7 +15,5 @@
 		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
 	}
 	?>
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>" class="imprint">
-		<?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?>
-	</a>
+	
 </div><!-- .site-info -->
