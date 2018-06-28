67716d1367191e9e2d9a0156a9f6514b8200e24f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-06-28 02:45:29 +0000

    Docs: Correct description for `mysql_to_rfc3339()`.
    Despite historical function name, the output does not conform to RFC3339 format, which must contain timezone.
    
    Props Rarst.
    See #42542.
    Built from https://develop.svn.wordpress.org/trunk@43383
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43211 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -5849,15 +5849,18 @@ function wp_post_preview_js() {
 }
 
 /**
- * Parses and formats a MySQL datetime (Y-m-d H:i:s) for ISO8601/RFC3339.
+ * Parses and formats a MySQL datetime (Y-m-d H:i:s) for ISO8601 (Y-m-d\TH:i:s).
  *
  * Explicitly strips timezones, as datetimes are not saved with any timezone
  * information. Including any information on the offset could be misleading.
  *
+ * Despite historical function name, the output does not conform to RFC3339 format,
+ * which must contain timezone.
+ *
  * @since 4.4.0
  *
  * @param string $date_string Date string to parse and format.
- * @return string Date formatted for ISO8601/RFC3339.
+ * @return string Date formatted for ISO8601 without time zone.
  */
 function mysql_to_rfc3339( $date_string ) {
 	$formatted = mysql2date( 'c', $date_string, false );
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43382';
+$wp_version = '5.0-alpha-43383';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
