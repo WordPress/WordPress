cefaaa0b5111c071ce8e90d00c2662cc56bf4d3c
Andrew Ozz <admin@laptoptips.ca>
2018-06-28 02:40:21 +0000

    Privacy: add `esc_html` to assertion in test_wp_comments_personal_data_exporter.
    Props mermel, 1naveengiri.
    Fixes #44113.
    Built from https://develop.svn.wordpress.org/trunk@43371
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43199 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43370';
+$wp_version = '5.0-alpha-43371';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
