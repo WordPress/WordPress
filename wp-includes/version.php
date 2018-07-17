c173331dc445e5dbcd15bcb3ff570443c3455f50
Gary Pendergast <gary@pento.net>
2018-07-17 07:27:25 +0000

    REST API: Fix some incorrect `@since` tags.
    [43437] included some new methods, which were incorrectly tagged as being `@since 4.9.7`. This updates them to `4.9.8`.
    
    Props danielbachhuber.
    Fixes 44287.
    
    
    Built from https://develop.svn.wordpress.org/trunk@43463
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43290 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43462';
+$wp_version = '5.0-alpha-43463';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
