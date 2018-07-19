76138b9b66db3ed3cdc7857a11bcd473ab3fb72c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-07-19 12:17:25 +0000

    Privacy: Use a consistent case for "Privacy Policy page" on Privacy Settings screen.
    Props XpertOne, idea15, garrett-eclipse.
    Merges [43508] to the 4.9 branch.
    Fixes #44130.
    Built from https://develop.svn.wordpress.org/branches/4.9@43509
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43338 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.8-beta1-43507';
+$wp_version = '4.9.8-beta1-43509';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
