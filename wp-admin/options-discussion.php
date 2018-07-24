e7c1553bdef336d12b88d1a885f2feefbdf13582
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-07-24 17:08:26 +0000

    Privacy: Revert [43525].
    The commenter cookies checkbox is not legally required, so should be disabled by default.
    
    Merges [43531] to the 4.9 branch.
    Fixes #44373.
    Built from https://develop.svn.wordpress.org/branches/4.9@43532
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43361 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options-discussion.php
+++ wp-admin/options-discussion.php
@@ -77,6 +77,13 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 ); ?>
 </label>
 <br />
+
+<label for="show_comments_cookies_opt_in">
+<input name="show_comments_cookies_opt_in" type="checkbox" id="show_comments_cookies_opt_in" value="1" <?php checked( '1', get_option( 'show_comments_cookies_opt_in' ) ); ?> />
+<?php _e( 'Show comments cookies opt-in checkbox.' ); ?>
+</label>
+<br />
+
 <label for="thread_comments">
 <input name="thread_comments" type="checkbox" id="thread_comments" value="1" <?php checked('1', get_option('thread_comments')); ?> />
 <?php
