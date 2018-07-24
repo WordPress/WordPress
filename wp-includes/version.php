e3bce2d81a21ea4cb4f935394adc3dd8b211a6c4
Aaron Campbell <aaron@xavisys.com>
2018-07-24 19:47:26 +0000

    Post WordPress 4.9.8 RC 1 version bump. Built from https://develop.svn.wordpress.org/branches/4.9@43534
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43363 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.8-RC1';
+$wp_version = '4.9.8-RC1-43534';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
