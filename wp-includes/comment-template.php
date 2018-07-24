c0a7aaf659e2f2c6b41cba74d1a02d1b98045a96
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-07-24 12:53:26 +0000

    Privacy: Remove the setting to enable comment cookies consent added in [43469].
    This needs some more work to ensure expected behaviour.
    
    Merges [43525] to the 4.9 branch.
    See #44373.
    Built from https://develop.svn.wordpress.org/branches/4.9@43526
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43355 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment-template.php
+++ wp-includes/comment-template.php
@@ -2205,7 +2205,7 @@ function comment_form( $args = array(), $post_id = null ) {
 					 '<input id="url" name="url" ' . ( $html5 ? 'type="url"' : 'type="text"' ) . ' value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" maxlength="200" /></p>',
 	);
 
-	if ( has_action( 'set_comment_cookies', 'wp_set_comment_cookies' ) && get_option( 'show_comments_cookies_opt_in' ) ) {
+	if ( has_action( 'set_comment_cookies', 'wp_set_comment_cookies' ) ) {
 		$consent           = empty( $commenter['comment_author_email'] ) ? '' : ' checked="checked"';
 		$fields['cookies'] = '<p class="comment-form-cookies-consent"><input id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" type="checkbox" value="yes"' . $consent . ' />' .
 							 '<label for="wp-comment-cookies-consent">' . __( 'Save my name, email, and website in this browser for the next time I comment.' ) . '</label></p>';
