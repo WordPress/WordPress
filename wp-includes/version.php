58509ba12fddca02d401b33482300064fb7cc18e
John Blackbourn <johnbillion@git.wordpress.org>
2018-07-28 13:02:25 +0000

    Login and Registration: Pass the `$errors` parameter to the `lost_password` action.
    Props sebakurzyn
    
    Fixes #44512
    
    Built from https://develop.svn.wordpress.org/trunk@43542
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43371 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43541';
+$wp_version = '5.0-alpha-43542';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
