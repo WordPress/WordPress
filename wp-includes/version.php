b27951eb85037a062b99f7110ec77f62a4839c74
Aaron Jorbin <aaron@jorb.in>
2018-07-31 14:42:29 +0000

    Gutenberg: Restrict "Try Gutenberg" based on capability and gutenstatus
    When Gutenberg is either not installed, or not activated, only show the callout to users with the install_plugins capability.
    When Gutenberg is activated, expand that to include all users with the edit_posts capability.
    
    Props pento.
    Fixes #44680.
    
    Built from https://develop.svn.wordpress.org/trunk@43543
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43372 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43542';
+$wp_version = '5.0-alpha-43543';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
