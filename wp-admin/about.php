4ae8f8d4e6600776071f8bf736744e795665ae6c
Dominik Schilling <dominikschilling+git@gmail.com>
2018-08-02 20:13:24 +0000

    Bump 4.9 branch to version 4.9.8. Built from https://develop.svn.wordpress.org/branches/4.9@43552
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43381 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -36,7 +36,27 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 			<p>
 				<?php
 				printf(
-				/* translators: 1: WordPress version number, 2: plural number of bugs. */
+					/* translators: 1: WordPress version number, 2: plural number of bugs. */
+					_n(
+						'<strong>Version %1$s</strong> addressed %2$s bug.',
+						'<strong>Version %1$s</strong> addressed %2$s bugs.',
+						46
+					),
+					'4.9.8',
+					number_format_i18n( 46 )
+				);
+
+				printf(
+					/* translators: %s: Codex URL */
+					__( 'For more information, see <a href="%s">the release notes</a>.' ),
+					'https://codex.wordpress.org/Version_4.9.8'
+				);
+				?>
+			</p>
+			<p>
+				<?php
+				printf(
+					/* translators: 1: WordPress version number, 2: plural number of bugs. */
 					_n(
 						'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bug.',
 						'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bugs.',
