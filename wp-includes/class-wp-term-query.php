38c33611e063416b4d20137e169d4d451e8f22b0
Boone Gorges <boonebgorges@gmail.com>
2018-08-08 19:23:25 +0000

    Correct param documentation for `WP_Term_Query`.
    The description of `$meta_type` introduced in [40053] was incorrect.
    
    Props dlh.
    Fixes #44608.
    Built from https://develop.svn.wordpress.org/trunk@43559
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43388 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-term-query.php
+++ wp-includes/class-wp-term-query.php
@@ -172,8 +172,8 @@ class WP_Term_Query {
 	 *                                                Can be used in conjunction with `$meta_value`. Default empty.
 	 *     @type string       $meta_value             Limit terms to those matching a specific metadata value.
 	 *                                                Usually used in conjunction with `$meta_key`. Default empty.
-	 *     @type string       $meta_type              Type of object metadata is for (e.g., comment, post, or user).
-	 *                                                Default empty.
+	 *     @type string       $meta_type              MySQL data type that the `$meta_value` will be CAST to for
+	 *                                                comparisons. Default empty.
 	 *     @type string       $meta_compare           Comparison operator to test the 'meta_value'. Default empty.
 	 * }
 	 */
