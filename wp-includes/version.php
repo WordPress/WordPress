1521b6433725e7f895641b7d9bcacf95b8b12628
Andrew Ozz <admin@laptoptips.ca>
2018-08-20 13:51:24 +0000

    Script loader: remove (PHP based) compression from `load-styles.php` and `load-scripts.php`. WIth the amount of scripts and stylesheets grown a lot over the years, it has become pretty slow and consumes a lot of server resources. Also, most servers are set to compress PHP output anyway.
    Props LucasRolff, azaozz.
    
    Fixes #44815.
    See #43308.
    Built from https://develop.svn.wordpress.org/trunk@43580
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43409 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43579';
+$wp_version = '5.0-alpha-43580';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
