e92c6a2817d4aaf0e0a707b217fed19c2dc7cfed
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-08-31 06:29:28 +0000

    Embeds: Avoid a JS error in `wp.receiveEmbedMessage` if `data` parameter is not set.
    Props dsifford, kadamwhite.
    Merges [43593] and [43597] to the 4.9 branch.
    Fixes #44832.
    Built from https://develop.svn.wordpress.org/branches/4.9@43600
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43429 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.9-alpha-43575';
+$wp_version = '4.9.9-alpha-43600';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
