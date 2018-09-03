a3ff44115ba201d57a0f50915e377ce74f62505e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-09-03 21:17:25 +0000

    Script loader: remove (PHP based) compression from `load-styles.php` and `load-scripts.php`. WIth the amount of scripts and stylesheets grown a lot over the years, it has become pretty slow and consumes a lot of server resources. Also, most servers are set to compress PHP output anyway.
    Props LucasRolff, azaozz.
    Merges [43580] to the 4.9 branch.
    Fixes #44815. See #43308.
    Built from https://develop.svn.wordpress.org/branches/4.9@43618
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43447 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/load-styles.php
+++ wp-admin/load-styles.php
@@ -28,8 +28,6 @@ $load = array_unique( explode( ',', $load ) );
 if ( empty($load) )
 	exit;
 
-$compress = ( isset($_GET['c']) && $_GET['c'] );
-$force_gzip = ( $compress && 'gzip' == $_GET['c'] );
 $rtl = ( isset($_GET['dir']) && 'rtl' == $_GET['dir'] );
 $expires_offset = 31536000; // 1 year
 $out = '';
@@ -79,17 +77,6 @@ header("Etag: $wp_version");
 header('Content-Type: text/css; charset=UTF-8');
 header('Expires: ' . gmdate( "D, d M Y H:i:s", time() + $expires_offset ) . ' GMT');
 header("Cache-Control: public, max-age=$expires_offset");
-
-if ( $compress && ! ini_get('zlib.output_compression') && 'ob_gzhandler' != ini_get('output_handler') && isset($_SERVER['HTTP_ACCEPT_ENCODING']) ) {
-	header('Vary: Accept-Encoding'); // Handle proxies
-	if ( false !== stripos($_SERVER['HTTP_ACCEPT_ENCODING'], 'deflate') && function_exists('gzdeflate') && ! $force_gzip ) {
-		header('Content-Encoding: deflate');
-		$out = gzdeflate( $out, 3 );
-	} elseif ( false !== stripos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') && function_exists('gzencode') ) {
-		header('Content-Encoding: gzip');
-		$out = gzencode( $out, 3 );
-	}
-}
 
 echo $out;
 exit;
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.9-alpha-43617';
+$wp_version = '4.9.9-alpha-43618';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
