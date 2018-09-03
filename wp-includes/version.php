197fe91d39aaf8c0273f31b78cc3069886871212
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-09-03 21:26:26 +0000

    Docs: Correct param documentation for `WP_Term_Query`.
    The description of `$meta_type` introduced in [40053] was incorrect.
    
    Props dlh.
    Merges [43559] to the 4.9 branch.
    Fixes #44608.
    Built from https://develop.svn.wordpress.org/branches/4.9@43621
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43450 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.9-alpha-43620';
+$wp_version = '4.9.9-alpha-43621';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
