1963afa5ff0c6e395db656c5cf31854af464578c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-09-24 20:05:29 +0000

    General: PHP 7.3 throws an `E_WARNING` when using continue to target a switch.
    Applying continue to a switch is equivalent to using break and quite possibly, a continue targeting a higher level control structure is actually intended.
    
    To target the higher level control structure, a numeric argument has to be passed to continue. This fixes two cases in WordPress Core where this is currently happening.
    
    See: https://github.com/php/php-src/pull/3364
    See: https://wiki.php.net/rfc/continue_on_switch_deprecation
    
    Props jrf.
    Merges [43653] to the 4.9 branch.
    Fixes #44543.
    Built from https://develop.svn.wordpress.org/branches/4.9@43656
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@43485 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pomo/plural-forms.php
+++ wp-includes/pomo/plural-forms.php
@@ -207,7 +207,7 @@ class Plural_Forms {
 						$span = strspn( $str, self::NUM_CHARS, $pos );
 						$output[] = array( 'value', intval( substr( $str, $pos, $span ) ) );
 						$pos += $span;
-						continue;
+						break;
 					}
 
 					throw new Exception( sprintf( 'Unknown symbol "%s"', $next ) );
