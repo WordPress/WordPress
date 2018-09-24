eb6be7eee12d4581f1de30dd48551e47c6937b28
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-09-24 20:46:26 +0000

    Docs: Add a line break to `wp_is_uuid()` DocBlock for better readability.
    See #42505.
    Built from https://develop.svn.wordpress.org/trunk@43657
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43486 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -6144,7 +6144,8 @@ function wp_generate_uuid4() {
  * @since 4.9.0
  *
  * @param mixed $uuid    UUID to check.
- * @param int   $version Specify which version of UUID to check against. Default is none, to accept any UUID version. Otherwise, only version allowed is `4`.
+ * @param int   $version Specify which version of UUID to check against. Default is none,
+ *                       to accept any UUID version. Otherwise, only version allowed is `4`.
  * @return bool The string is a valid UUID or false on failure.
  */
 function wp_is_uuid( $uuid, $version = null ) {
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43655';
+$wp_version = '5.0-alpha-43657';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
