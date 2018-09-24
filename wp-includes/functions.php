357a4957ec17d41789c210d172b2548349d8b485
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-09-24 20:57:23 +0000

    General: Introduce `wp_unique_id()`, a PHP implementation of Underscore's `uniqueId` method.
    A static variable contains an integer that is incremented with each call. This number is returned with the optional prefix.
    As such the returned value is not universally unique, but it is unique across the life of the PHP process.
    
    Props westonruter, dlh.
    See #44883.
    Built from https://develop.svn.wordpress.org/trunk@43658
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43487 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -6168,6 +6168,26 @@ function wp_is_uuid( $uuid, $version = null ) {
 }
 
 /**
+ * Get unique ID.
+ *
+ * This is a PHP implementation of Underscore's uniqueId method. A static variable
+ * contains an integer that is incremented with each call. This number is returned
+ * with the optional prefix. As such the returned value is not universally unique,
+ * but it is unique across the life of the PHP process.
+ *
+ * @since 4.9.9
+ *
+ * @staticvar int $id_counter
+ *
+ * @param string $prefix Prefix for the returned ID.
+ * @return string Unique ID.
+ */
+function wp_unique_id( $prefix = '' ) {
+	static $id_counter = 0;
+	return $prefix . (string) ++$id_counter;
+}
+
+/**
  * Get last changed date for the specified cache group.
  *
  * @since 4.7.0
