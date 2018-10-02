3740085e9fb61e43d461b92d0ef132c222333361
Gary Pendergast <gary@pento.net>
2018-10-02 15:53:23 +0000

    Posts: Remove the slug from Quick Edit for CPTs with `publicly_queryable` disabled.
    In the full edit screen, CPTs that have disabled the `publicly_queryable` option will hide the slug field, as it doesn't need to be edited. This change brings the Quick Edit view into line with that behaviour.
    
    Props bhargavmehta, krutidugade.
    Fixes #43278.
    
    
    Built from https://develop.svn.wordpress.org/trunk@43664
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43493 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-posts-list-table.php
+++ wp-admin/includes/class-wp-posts-list-table.php
@@ -1458,12 +1458,15 @@ class WP_Posts_List_Table extends WP_List_Table {
 				<span class="input-text-wrap"><input type="text" name="post_title" class="ptitle" value="" /></span>
 			</label>
 
+		<?php if ( $post_type_object->publicly_queryable ) : // publicly_queryable check ?>
+
 			<label>
 				<span class="title"><?php _e( 'Slug' ); ?></span>
 				<span class="input-text-wrap"><input type="text" name="post_name" value="" /></span>
 			</label>
 
-	<?php
+			<?php
+	endif; // publicly_queryable check
 	endif; // $bulk
 	endif; // post_type_supports title
 			?>
