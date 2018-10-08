62a35c8545cba6c6b3db6103cb2b1fd3226e8015
danielbachhuber <danielbachhuber@git.wordpress.org>
2018-10-08 13:50:27 +0000

    REST API: Define taxonomy visibility settings in schema
    For Gutenberg and other admin-type interfaces, it's
    useful to be able to see the visibility settings for
    taxonomies.
    
    The original changeset was partially included in [43445].
    
    Merges [42729], [42730], [42973] to the 5.0 branch.
    Props joehoyle, TimothyBlynJacobs, pento.
    Fixes #42707
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43680
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43509 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/endpoints/class-wp-rest-taxonomies-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-taxonomies-controller.php
@@ -322,6 +322,12 @@ class WP_REST_Taxonomies_Controller extends WP_REST_Controller {
 					'context'      => array( 'view', 'edit' ),
 					'readonly'     => true,
 				),
+				'visibility'       => array(
+					'description'  => __( 'The visibility settings for the taxonomy.' ),
+					'type'         => 'object',
+					'context'      => array( 'edit' ),
+					'readonly'     => true,
+				),
 				'rest_base'            => array(
 					'description'  => __( 'REST base route for the taxonomy.' ),
 					'type'         => 'string',
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43679';
+$wp_version = '5.0-alpha-43680';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
