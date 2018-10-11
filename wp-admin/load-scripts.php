15d1c6398ccf7f51d4f953f013720936e9208dcd
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-10-11 03:23:22 +0000

    Script loader: remove (PHP based) compression from `load-styles.php` and `load-scripts.php`. WIth the amount of scripts and stylesheets grown a lot over the years, it has become pretty slow and consumes a lot of server resources. Also, most servers are set to compress PHP output anyway.
    Props LucasRolff, azaozz.
    Merges [43580] to the 5.0 branch.
    Fixes #44815. See #43308.
    Built from https://develop.svn.wordpress.org/branches/5.0@43697
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43526 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/load-scripts.php
+++ wp-admin/load-scripts.php
@@ -28,8 +28,6 @@ require( ABSPATH . 'wp-admin/includes/noop.php' );
 require( ABSPATH . WPINC . '/script-loader.php' );
 require( ABSPATH . WPINC . '/version.php' );
 
-$compress = ( isset($_GET['c']) && $_GET['c'] );
-$force_gzip = ( $compress && 'gzip' == $_GET['c'] );
 $expires_offset = 31536000; // 1 year
 $out = '';
 
@@ -57,17 +55,6 @@ header("Etag: $wp_version");
 header('Content-Type: application/javascript; charset=UTF-8');
 header('Expires: ' . gmdate( "D, d M Y H:i:s", time() + $expires_offset ) . ' GMT');
 header("Cache-Control: public, max-age=$expires_offset");
-
-if ( $compress && ! ini_get('zlib.output_compression') && 'ob_gzhandler' != ini_get('output_handler') && isset($_SERVER['HTTP_ACCEPT_ENCODING']) ) {
-	header('Vary: Accept-Encoding'); // Handle proxies
-	if ( false !== stripos($_SERVER['HTTP_ACCEPT_ENCODING'], 'deflate') && function_exists('gzdeflate') && ! $force_gzip ) {
-		header('Content-Encoding: deflate');
-		$out = gzdeflate( $out, 3 );
-	} elseif ( false !== stripos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') && function_exists('gzencode') ) {
-		header('Content-Encoding: gzip');
-		$out = gzencode( $out, 3 );
-	}
-}
 
 echo $out;
 exit;
