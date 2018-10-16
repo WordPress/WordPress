bf672769c2ec76d94a4509f7235ace85d8d83e39
Peter Wilson <wilson@peterwilson.cc>
2018-10-16 04:13:30 +0000

    Formatting: Add pre-save content filter to make target=_blank always secure.
    Props notnownikki, iseulde, azaozz.
    Merges [42770] to the 5.0 branch.
    Fixes #43187.
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43732
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43561 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43731';
+$wp_version = '5.0-alpha-43732';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
