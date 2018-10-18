699bf044efc53d368fbf9f365ea7240a1e84c9e4
danielbachhuber <danielbachhuber@git.wordpress.org>
2018-10-18 20:32:27 +0000

    REST API: Correct HTTP status code in error for requests to create a duplicate term.
    The 409 error code is intended for situations where it is expected that the user will resolve the conflict and resubmit the same request. We use 400 error codes for other routes when a duplicate request is made. The 400 status code tells the user they need to modify their request for it to be successful.
    
    Props shooper.
    Merges [42354] to the 5.0 branch.
    Fixes #42781. See #41370.
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43756
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43585 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43755';
+$wp_version = '5.0-alpha-43756';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
