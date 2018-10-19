99d061b53cdee104ba8251a7eac3731a0b613759
Gary Pendergast <gary@pento.net>
2018-10-19 06:45:39 +0000

    General: Extract the code editor settings from `wp_enqueue_code_editor()`.
    They're now returned by a new function, `wp_get_code_editor_settings()`, so they can be reused by the block editor.
    
    See #45127.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43761
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43590 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43760';
+$wp_version = '5.0-alpha-43761';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
