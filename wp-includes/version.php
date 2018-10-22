a618939ddb63092103dc64474c7fd47fed8aa729
Gary Pendergast <gary@pento.net>
2018-10-22 06:13:38 +0000

    Script/Style Dependencies: Make sure that inline scripts for handles without a source are printed.
    See [36550] for `WP_Styles`.
    
    Merges [43565] to the 5.0 branch.
    
    Props bpayton.
    Fixes #44551.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43788
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43617 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43787';
+$wp_version = '5.0-alpha-43788';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
