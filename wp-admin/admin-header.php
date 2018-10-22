433dfae50993d6512f5cd89646e98612208c3656
Gary Pendergast <gary@pento.net>
2018-10-22 03:14:40 +0000

    Block Editor: Add extra body classes when the block editor is loaded.
    See #45037.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43780
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43609 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -175,6 +175,15 @@ if ( is_network_admin() )
 
 $admin_body_class .= ' no-customize-support no-svg';
 
+if ( $current_screen->is_block_editor() ) {
+	// Default to is-fullscreen-mode to avoid jumps in the UI.
+	$admin_body_class .= ' block-editor-page is-fullscreen-mode wp-embed-responsive';
+
+	if ( current_theme_supports( 'editor-styles' ) && current_theme_supports( 'dark-editor-style' ) ) {
+		$admin_body_class .= ' is-dark-theme';
+	}
+}
+
 ?>
 </head>
 <?php
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43779';
+$wp_version = '5.0-alpha-43780';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
