878a6359dcca0e723e9444d202b91c27dfb08435
David A. Kennedy <me@davidakennedy.com>
2018-10-23 06:14:17 +0000

    Twenty Seventeen: Add styles and support for the new block-based editor.
    This update adds styles and theme support related to the new block-based editor to enhance the experience of using it with Twenty Seventeen. 
    
    These are the specific changes made to this theme:
    
    * Add blocks.css, to style blocks on the front end, to make sure they match the theme’s existing HTML element styles.
    * Add editor-blocks.css to style blocks in the editor, to make sure they match the theme’s existing HTML element styles.
    * Add theme support for `editor-styles`, to pull the existing editor stylesheet into the new editor.
    * Add theme support for `wp-block-styles`, to load the default block styles on the front end.
    
    Props laurelfulford, ianbelanger, crunnells.
    Fixes #45045.
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43800
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43629 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-43799';
+$wp_version = '5.0-alpha-43800';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
