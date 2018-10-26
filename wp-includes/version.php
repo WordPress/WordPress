0c541538861086926698f05eec733b815cbae76f
Gary Pendergast <gary@pento.net>
2018-10-26 08:12:39 +0000

    Meta Boxes: Fix error when a meta box is rendered with a static method callback.
    When `WP_DEBUG` is set, ensure that a static method callback is passed to `ReflectionMethod`, instead of `ReflectionFunction`.
    
    Props DrewAPicture.
    Fixes #45192.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43830
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43659 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-beta1-43829';
+$wp_version = '5.0-beta1-43830';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
