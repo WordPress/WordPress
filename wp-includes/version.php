d5e3670372485f881d30a36c0dd7dc43414bec4b
danielbachhuber <danielbachhuber@git.wordpress.org>
2018-10-31 18:17:35 +0000

    REST API: Restore access to audio/video metadata functions.
    Ensures `wp_read_video_metadata()`/`wp_read_audio_metadata()` functions are available when uploading video and audio. Fixes error introduced in [43589].
    
    Props ocean90.
    See #43757.
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43850
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43679 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-beta2-43849';
+$wp_version = '5.0-beta2-43850';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
