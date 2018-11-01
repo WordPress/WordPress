eb4031a209a1adc1e70fbe1523f84a7e44401733
Gary Pendergast <gary@pento.net>
2018-11-01 02:25:47 +0000

    Blocks: Add "Export as JSON" action to the `wp_block` post list screen.
    Remove the "Quick Edit" for `wp_block`s, too, as they don't need any of that functionality.
    
    Props noisysocks.
    Fixes #45247.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43854
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43683 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-posts-list-table.php
+++ wp-admin/includes/class-wp-posts-list-table.php
@@ -1237,12 +1237,15 @@ class WP_Posts_List_Table extends WP_List_Table {
 				esc_attr( sprintf( __( 'Edit &#8220;%s&#8221;' ), $title ) ),
 				__( 'Edit' )
 			);
-			$actions['inline hide-if-no-js'] = sprintf(
-				'<a href="#" class="editinline" aria-label="%s">%s</a>',
-				/* translators: %s: post title */
-				esc_attr( sprintf( __( 'Quick edit &#8220;%s&#8221; inline' ), $title ) ),
-				__( 'Quick&nbsp;Edit' )
-			);
+
+			if ( 'wp_block' !== $post->post_type ) {
+				$actions['inline hide-if-no-js'] = sprintf(
+					'<a href="#" class="editinline" aria-label="%s">%s</a>',
+					/* translators: %s: post title */
+					esc_attr( sprintf( __( 'Quick edit &#8220;%s&#8221; inline' ), $title ) ),
+					__( 'Quick&nbsp;Edit' )
+				);
+			}
 		}
 
 		if ( current_user_can( 'delete_post', $post->ID ) ) {
@@ -1295,6 +1298,16 @@ class WP_Posts_List_Table extends WP_List_Table {
 					__( 'View' )
 				);
 			}
+		}
+
+		if ( 'wp_block' === $post->post_type ) {
+			$actions['export'] = sprintf(
+				'<button type="button" class="wp-list-reusable-blocks__export button-link" data-id="%s" aria-label="%s">%s</button>',
+				$post->ID,
+				/* translators: %s: post title */
+				esc_attr( sprintf( __( 'Export &#8220;%s&#8221; as JSON' ), $title ) ),
+				__( 'Export as JSON' )
+			);
 		}
 
 		if ( is_post_type_hierarchical( $post->post_type ) ) {
