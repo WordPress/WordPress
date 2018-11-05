3e1ae5488452ff497b90f47ac908d904bcc38a0d
Gary Pendergast <gary@pento.net>
2018-11-05 02:47:49 +0000

    Tests: Skip symlinked theme file tests if the links could not be created, or if `symlink()` is not available
    Merges [42812,42819] to the 5.0 branch.
    
    Props SergeyBiryukov.
    Fixes #43508.
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43866
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43695 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-beta3-43865';
+$wp_version = '5.0-beta3-43866';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
