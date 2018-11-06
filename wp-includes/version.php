c42f50371eb82243df6a9f2b6151e1c8c1e1b5fa
laurelfulford <laurelfulford@git.wordpress.org>
2018-11-06 06:06:48 +0000

    Twenty Sixteen: Fix issues with theme's Gutenberg support.
    A handful of issues were missed in Twenty Sixteen's Gutenberg support, added in #45044. This commit includes the following fixes:
    
    * Center text and bullets in list-based widget blocks, when block itself is centered.
    * Correct styles for the editor placeholder text, so it's not inheriting a darker gray from the theme's CSS. 
    * Include non "image" variations of the Cover block classes `.wp-block-cover` and `.wp-block-cover-text` in the styles.
    * Reset Cover blocks as `display: flex` when they are centered, to maintain their original appearance.
    * Add spacing underneath the Media & Text block, but make sure the final paragraph in the block doesn't add too much space.
    * Simplify selectors used to set editor width to just `.wp-block`.
    * Prevent margin from theme's editor-style.css from interfering with editor alignment. 
     
    Props pento, davidakennedy.
    Fixes #45245.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43875
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43704 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-beta3-43874';
+$wp_version = '5.0-beta3-43875';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
