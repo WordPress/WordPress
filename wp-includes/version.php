5cf84bd3f73e52e5dfd4f914df177e31f6806b8f
danielbachhuber <danielbachhuber@git.wordpress.org>
2018-11-15 12:57:51 +0000

    REST API: Avoid using 'parent' as path argument name for autosaves.
    When 'parent' is set as the path argument name, it gets passed down through to the `create_item()` method and can erroneously reset the 'parent' value on the post itself. Instead, we rename the argument to 'id' and replicate the revision controller's `get_items_permissions_check()` to instead reference 'id'.
    
    Also ensures revision query params (of which there are many) aren't exposed as the query params for autosaves (of which there are two).
    
    Props TimothyBlynJacobs.
    See #43316.
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43897
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43729 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-beta4-43896';
+$wp_version = '5.0-beta4-43897';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
