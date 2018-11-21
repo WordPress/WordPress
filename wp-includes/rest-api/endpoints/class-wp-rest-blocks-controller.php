ded3bf63b72ad2f1f07a3c2744c8027af9245f39
danielbachhuber <danielbachhuber@git.wordpress.org>
2018-11-21 14:35:48 +0000

    REST API: Always include `title.raw`/`content.raw` for Blocks in `context=view`.
    Demarcations for reusable blocks are always expected to be accessible by clients.
    
    Props noisysocks, youknowriad.
    See #45145 for the patch, #45098 for the original ticket.
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43917
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43749 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/endpoints/class-wp-rest-blocks-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-blocks-controller.php
@@ -36,4 +36,57 @@ class WP_REST_Blocks_Controller extends WP_REST_Posts_Controller {
 
 		return parent::check_read_permission( $post );
 	}
+
+	/**
+	 * Filters a response based on the context defined in the schema.
+	 *
+	 * @since 5.0.0
+	 *
+	 * @param array  $data    Response data to fiter.
+	 * @param string $context Context defined in the schema.
+	 * @return array Filtered response.
+	 */
+	public function filter_response_by_context( $data, $context ) {
+		$data = parent::filter_response_by_context( $data, $context );
+
+		/*
+		 * Remove `title.rendered` and `content.rendered` from the response. It
+		 * doesn't make sense for a reusable block to have rendered content on its
+		 * own, since rendering a block requires it to be inside a post or a page.
+		 */
+		unset( $data['title']['rendered'] );
+		unset( $data['content']['rendered'] );
+
+		return $data;
+	}
+
+	/**
+	 * Retrieves the block's schema, conforming to JSON Schema.
+	 *
+	 * @since 5.0.0
+	 *
+	 * @return array Item schema data.
+	 */
+	public function get_item_schema() {
+		$schema = parent::get_item_schema();
+
+		/*
+		 * Allow all contexts to access `title.raw` and `content.raw`. Clients always
+		 * need the raw markup of a reusable block to do anything useful, e.g. parse
+		 * it or display it in an editor.
+		 */
+		$schema['properties']['title']['properties']['raw']['context']   = array( 'view', 'edit' );
+		$schema['properties']['content']['properties']['raw']['context'] = array( 'view', 'edit' );
+
+		/*
+		 * Remove `title.rendered` and `content.rendered` from the schema. It doesn’t
+		 * make sense for a reusable block to have rendered content on its own, since
+		 * rendering a block requires it to be inside a post or a page.
+		 */
+		unset( $schema['properties']['title']['properties']['rendered'] );
+		unset( $schema['properties']['content']['properties']['rendered'] );
+
+		return $schema;
+	}
+
 }
