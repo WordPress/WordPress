a69e8cf788ce5bdb32051d77a3f5c0a11d52d0dc
gziolo <gziolo@git.wordpress.org>
2018-11-22 18:03:45 +0000

    About: Update Freedoms page to include illustrations.
    @cathibosco has made some great illustrations of the Four Freedoms on https://wordpress.org/about/. Let's bring that over to the admin Freedoms page.
    
    Props @melchoyce, @cathibosco, @boemedia, @karmatosed, @tobifjellner, @helen, @matveb, @mcsf.
    Fixes #45386.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43938
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43770 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/freedoms.php
+++ wp-admin/freedoms.php
@@ -45,25 +45,50 @@ $is_privacy_notice = isset( $_GET['privacy-notice'] );
 
 <?php else : ?>
 <div class="about-wrap-content">
-	<p class="about-description"><?php printf( __( 'WordPress is Free and open source software, built by a distributed community of mostly volunteer developers from around the world. WordPress comes with some awesome, worldview-changing rights courtesy of its <a href="%s">license</a>, the GPL.' ), 'https://wordpress.org/about/license/' ); ?></p>
+	<div class="feature-section one-col">
+		<div class="col">
+			<h2><?php _e( 'Freedoms' ); ?></h2>
+			<p class="about-description"><?php printf( __( 'WordPress is Free and open source software, built by a distributed community of mostly volunteer developers from around the world. WordPress comes with some awesome, worldview-changing rights courtesy of its <a href="%s">license</a>, the GPL.' ), 'https://wordpress.org/about/license/' ); ?></p>
+		</div>
+	</div>
 
-	<ol start="0">
-		<li><p><?php _e( 'You have the freedom to run the program, for any purpose.' ); ?></p></li>
-		<li><p><?php _e( 'You have access to the source code, the freedom to study how the program works, and the freedom to change it to make it do what you wish.' ); ?></p></li>
-		<li><p><?php _e( 'You have the freedom to redistribute copies of the original program so you can help your neighbor.' ); ?></p></li>
-		<li><p><?php _e( 'You have the freedom to distribute copies of your modified versions to others. By doing this you can give the whole community a chance to benefit from your changes.' ); ?></p></li>
-	</ol>
+	<div class="feature-section four-col">
+		<div class="col">
+			<div class="freedoms-image"></div>
+			<h3><?php _e( 'The 1st Freedom' ); ?></h3>
+			<p><?php _e( 'To run the program for any purpose.' ); ?></p>
+		</div>
+		<div class="col">
+			<div class="freedoms-image"></div>
+			<h3><?php _e( 'The 2nd Freedom' ); ?></h3>
+			<p><?php _e( 'To study how the program works and change it to make it do what you wish.' ); ?></p>
+		</div>
+		<div class="col">
+			<div class="freedoms-image"></div>
+			<h3><?php _e( 'The 3rd Freedom' ); ?></h3>
+			<p><?php _e( 'To redistribute.' ); ?></p>
+		</div>
+		<div class="col">
+			<div class="freedoms-image"></div>
+			<h3><?php _e( 'The 4th Freedom' ); ?></h3>
+			<p><?php _e( 'To distribute copies of your modified versions to others.' ); ?></p>
+		</div>
+	</div>
 
-	<p><?php printf( __( 'WordPress grows when people like you tell their friends about it, and the thousands of businesses and services that are built on and around WordPress share that fact with their users. We&#8217;re flattered every time someone spreads the good word, just make sure to <a href="%s">check out our trademark guidelines</a> first.' ), 'https://wordpressfoundation.org/trademark-policy/' ); ?></p>
+	<div class="feature-section one-col">
+		<div class="col">
+			<p><?php printf( __( 'WordPress grows when people like you tell their friends about it, and the thousands of businesses and services that are built on and around WordPress share that fact with their users. We&#8217;re flattered every time someone spreads the good word, just make sure to <a href="%s">check out our trademark guidelines</a> first.' ), 'https://wordpressfoundation.org/trademark-policy/' ); ?></p>
 
-	<p><?php
+			<p><?php
 
-	$plugins_url = current_user_can( 'activate_plugins' ) ? admin_url( 'plugins.php' ) : __( 'https://wordpress.org/plugins/' );
-	$themes_url = current_user_can( 'switch_themes' ) ? admin_url( 'themes.php' ) : __( 'https://wordpress.org/themes/' );
+			$plugins_url = current_user_can( 'activate_plugins' ) ? admin_url( 'plugins.php' ) : __( 'https://wordpress.org/plugins/' );
+			$themes_url = current_user_can( 'switch_themes' ) ? admin_url( 'themes.php' ) : __( 'https://wordpress.org/themes/' );
 
-	printf( __( 'Every plugin and theme in WordPress.org&#8217;s directory is 100%% GPL or a similarly free and compatible license, so you can feel safe finding <a href="%1$s">plugins</a> and <a href="%2$s">themes</a> there. If you get a plugin or theme from another source, make sure to <a href="%3$s">ask them if it&#8217;s GPL</a> first. If they don&#8217;t respect the WordPress license, we don&#8217;t recommend them.' ), $plugins_url, $themes_url, 'https://wordpress.org/about/license/' ); ?></p>
+			printf( __( 'Every plugin and theme in WordPress.org&#8217;s directory is 100%% GPL or a similarly free and compatible license, so you can feel safe finding <a href="%1$s">plugins</a> and <a href="%2$s">themes</a> there. If you get a plugin or theme from another source, make sure to <a href="%3$s">ask them if it&#8217;s GPL</a> first. If they don&#8217;t respect the WordPress license, we don&#8217;t recommend them.' ), $plugins_url, $themes_url, 'https://wordpress.org/about/license/' ); ?></p>
 
-	<p><?php _e( 'Don&#8217;t you wish all software came with these freedoms? So do we! For more information, check out the <a href="https://www.fsf.org/">Free Software Foundation</a>.' ); ?></p>
+			<p><?php _e( 'Don&#8217;t you wish all software came with these freedoms? So do we! For more information, check out the <a href="https://www.fsf.org/">Free Software Foundation</a>.' ); ?></p>
+		</div>
+	</div>
 </div>
 
 <?php endif; ?>
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-beta5-43937';
+$wp_version = '5.0-beta5-43938';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
