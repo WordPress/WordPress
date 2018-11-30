50b094e3b0a409da45cf068887157c5693ab46b3
youknowriad <youknowriad@git.wordpress.org>
2018-11-30 13:24:45 +0000

    Block Editor: Rename the gutenberg_ functions in blocks/latest-comments.php.
    This was ported manually from https://github.com/WordPress/gutenberg/pull/12326
    
    Props pento, swissspidy.
    See #45145. 
    
    Built from https://develop.svn.wordpress.org/branches/5.0@43950
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43782 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks/latest-posts.php
+++ wp-includes/blocks/latest-posts.php
@@ -33,7 +33,7 @@ function render_block_core_latest_posts( $attributes ) {
 
 		$title = get_the_title( $post_id );
 		if ( ! $title ) {
-			$title = __( '(Untitled)', 'default' );
+			$title = __( '(Untitled)' );
 		}
 		$list_items_markup .= sprintf(
 			'<li><a href="%1$s">%2$s</a>',
