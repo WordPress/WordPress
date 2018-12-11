0a372b57d0130571d6943865617fb5674fcb6c26
atimmer <atimmer@git.wordpress.org>
2018-12-11 16:13:26 +0000

    Build/Test: Update dependencies for 5.0
    Updates package dependencies to more modern versions, also adds .nvmrc and package-lock.json as followups to [43683] and [43571].
    
    Merge notes: trunk already had an identical .nvmrc. package-lock.json exists in trunk, but has some changes based on the dependency updates.
    
    Props jorbin.
    Merges [43684], [43685] and [43686] to trunk.
    See #44600.
    Fixes #45064.
    
    Built from https://develop.svn.wordpress.org/trunk@43977
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43809 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-43976';
+$wp_version = '5.1-alpha-43977';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
