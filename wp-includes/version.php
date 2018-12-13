d4d16ec0830927009250c34c065b918d82136924
desrosj <desrosj@git.wordpress.org>
2018-12-13 22:42:38 +0000

    REST API: Introduce Autosaves controller and endpoint.
    - Adds `WP_REST_Autosaves_Controller` which extends `WP_REST_Revisions_Controller`.
    - Autosaves endpoint is registered for all post types except attachment because even post types without revisions enabled are expected to autosave.
    - Because setting the `DOING_AUTOSAVE` constant pollutes the test suite, autosaves tests are run last. We may want to improve upon this later. 
    
    Also, use a truly impossibly high number in User Controller tests. The number `100`, (or `7777` in `trunk`), could be valid in certain test run configurations. The `REST_TESTS_IMPOSSIBLY_HIGH_NUMBER` constant is impossibly high for this very reason.
    
    Finally, Skip Autosaves controller test for multisite. There's a PHP 5.2 edge case where paths calculated differently, possibly caused by differing version of PHPUnit.
    
    Props adamsilverstein, aduth, azaozz, danielbachhuber, rmccue, danielbachhuber.
    
    Merges [43767], [43768], [43769] to trunk.
    
    See #45132, #45131.
    Fixes #45128, #43316.
    Built from https://develop.svn.wordpress.org/trunk@44126
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43956 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44125';
+$wp_version = '5.1-alpha-44126';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
--- wp-settings.php
+++ wp-settings.php
@@ -230,6 +230,7 @@ require( ABSPATH . WPINC . '/rest-api/endpoints/class-wp-rest-attachments-contro
 require( ABSPATH . WPINC . '/rest-api/endpoints/class-wp-rest-post-types-controller.php' );
 require( ABSPATH . WPINC . '/rest-api/endpoints/class-wp-rest-post-statuses-controller.php' );
 require( ABSPATH . WPINC . '/rest-api/endpoints/class-wp-rest-revisions-controller.php' );
+require( ABSPATH . WPINC . '/rest-api/endpoints/class-wp-rest-autosaves-controller.php' );
 require( ABSPATH . WPINC . '/rest-api/endpoints/class-wp-rest-taxonomies-controller.php' );
 require( ABSPATH . WPINC . '/rest-api/endpoints/class-wp-rest-terms-controller.php' );
 require( ABSPATH . WPINC . '/rest-api/endpoints/class-wp-rest-users-controller.php' );
