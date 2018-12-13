c046ee1abd456a61d34b86f9f54291d829344039
Jeremy Felt <jeremy.felt@gmail.com>
2018-12-13 02:14:04 +0000

    Bump 4.6 branch to version 4.6.13.
    Built from https://develop.svn.wordpress.org/branches/4.6@44081
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.6@43911 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6.12';
+$wp_version = '4.6.13';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
