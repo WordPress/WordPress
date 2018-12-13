85eb0737486688549c69a3e735dbc8d0c04be04b
desrosj <desrosj@git.wordpress.org>
2018-12-13 22:26:27 +0000

    i18n: Remove some translation domains accidentally included in [43762], [44122].
    Moves [43764] to trunk.
    
    Fixes #45110.
    Built from https://develop.svn.wordpress.org/trunk@44124
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43954 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44123';
+$wp_version = '5.1-alpha-44124';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
