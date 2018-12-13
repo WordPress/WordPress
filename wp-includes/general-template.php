1894965f3f383f4aa1377635ced78a6884295b8f
Peter Wilson <wilson@peterwilson.cc>
2018-12-13 00:28:46 +0000

    Multisite: Improve messaging for previously activated users.
    Ensure activation of a site is not attempted multiple times and users are shown the correct message if they follow the link a second time.
    
    Merges [44021] to the 5.0 branch.
    
    Built from https://develop.svn.wordpress.org/branches/5.0@44022
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@43852 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -2813,6 +2813,24 @@ function wp_no_robots() {
 }
 
 /**
+ * Display a noindex,noarchive meta tag and referrer origin-when-cross-origin meta tag.
+ *
+ * Outputs a noindex,noarchive meta tag that tells web robots not to index or cache the page content.
+ * Outputs a referrer origin-when-cross-origin meta tag that tells the browser not to send the full
+ * url as a referrer to other sites when cross-origin assets are loaded.
+ *
+ * Typical usage is as a wp_head callback. add_action( 'wp_head', 'wp_sensitive_page_meta' );
+ *
+ * @since 5.0.0
+ */
+function wp_sensitive_page_meta() {
+	?>
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+	<?php
+}
+
+/**
  * Display site icon meta tags.
  *
  * @since 4.3.0
