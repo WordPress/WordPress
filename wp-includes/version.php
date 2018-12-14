0962e186e876c2c998a8401b26a905c5f0ee1d27
atimmer <atimmer@git.wordpress.org>
2018-12-14 09:47:52 +0000

    Build Tools: Add an npm script shortcut to run grunt.
    Rather than needing to install grunt globally, this allows running grunt from node_modules, instead.
    
    npm run grunt ... is effectively the equivalent of running grunt ....
    
    Props andrew.taylor, pento.
    Merges [43836] to trunk.
    See #45214.
    
    Built from https://develop.svn.wordpress.org/trunk@44176
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44006 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44175';
+$wp_version = '5.1-alpha-44176';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
