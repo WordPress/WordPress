3af9743b815e83d2c8bb1630367d26b1855bd25a
Jeremy Felt <jeremy.felt@gmail.com>
2018-12-14 02:15:37 +0000

    Blocks: Add the reusable block post type, `wp_block`.
    Merges [43804] from the 5.0 branch to trunk.
    
    See #45098.
    
    Built from https://develop.svn.wordpress.org/trunk@44146
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@43976 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit.php
+++ wp-admin/edit.php
@@ -200,6 +200,11 @@ $wp_list_table->prepare_items();
 wp_enqueue_script( 'inline-edit-post' );
 wp_enqueue_script( 'heartbeat' );
 
+if ( 'wp_block' === $post_type ) {
+	wp_enqueue_script( 'wp-list-reusable-blocks' );
+	wp_enqueue_style( 'wp-list-reusable-blocks' );
+}
+
 $title = $post_type_object->labels->name;
 
 if ( 'post' == $post_type ) {
