d8863244cbef0098c9e7862da27697ba2224d21b
Gary Pendergast <gary@pento.net>
2018-12-14 06:02:52 +0000

    Block Editor: Preload `wp/v2/media` with OPTIONS for caps check.
    Also introduces a `block_editor_preload_paths` filter for plugins and themes to preload additional data.
    
    Merges [43833] from the 5.0 branch to trunk.
    
    Props imath, mattheu, danielbachhuber.
    Fixes #45194.
    
    
    Built from https://develop.svn.wordpress.org/trunk@44172
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44002 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44171';
+$wp_version = '5.1-alpha-44172';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
