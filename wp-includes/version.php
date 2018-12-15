2d8697b0537959254bb8d1af12c64e5d5fa9675c
laurelfulford <laurelfulford@git.wordpress.org>
2018-12-15 20:01:46 +0000

    Bundled Themes: Update default block appender selector.
    The markup for the default block appender changed from a text input to a text area in the editor; this update makes the same change in the default theme styles.
    
    Fixes #45450.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.0@44212
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@44042 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0.2-alpha-44211';
+$wp_version = '5.0.2-alpha-44212';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
