362b7a42cf17bebb6d853592e1d061d175711a5c
desrosj <desrosj@git.wordpress.org>
2018-12-15 11:14:29 +0000

    Twenty Nineteen: Fixes and improvements.
    This commit fixes the final differences between the version of Twenty Nineteen in core and the one in GitHub. The two are now identical.
    
    See #45424.
    Built from https://develop.svn.wordpress.org/branches/5.0@44202
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@44032 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentynineteen/functions.php
+++ wp-content/themes/twentynineteen/functions.php
@@ -246,7 +246,7 @@ function twentynineteen_skip_link_focus_fix() {
 	// The following is minified via `terser --compress --mangle -- js/skip-link-focus-fix.js`.
 	?>
 	<script>
-		/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
 	</script>
 	<?php
 }
