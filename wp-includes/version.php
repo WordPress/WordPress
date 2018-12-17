bc9dfcfeb1d384e3060ddc25c5755647dbe50985
desrosj <desrosj@git.wordpress.org>
2018-12-17 18:00:52 +0000

    REST API: Preserve unknown, respect `null` in server-side block rendering.
    - Skips validation where there is no attribute definition, but keeps the attribute value. Previously, the attribute would be omitted from the attributes passed to `render_callback`. Notably, this resolves an issue where `render_callback` cannot receive a block's `align` and `customClassName` attribute values, since these are defined as a client-side filter.
    - Validates `null` as a proper value in its own right. Previously, a client implementation of a block could track `{“attribute":null}` as an explicitly empty value, and the server would wrongly initiate defaulting behavior. The new behavior will now only populate a default value if the attribute is not defined at all, including when unset in its being invalid per the attribute schema. 
    
    Props aduth, noisysocks, youknowriad, danielbachhuber.
    
    Merges [43918] to trunk.
    
    See #45145 for the patch, #45098 for the original ticket.
    Built from https://develop.svn.wordpress.org/trunk@44269
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44099 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44268';
+$wp_version = '5.1-alpha-44269';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
