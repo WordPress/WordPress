f2b97f3c7f237bcb55746943832e975d01436ce2
desrosj <desrosj@git.wordpress.org>
2018-12-17 17:22:52 +0000

    REST API: Always include `title.raw`/`content.raw` for Blocks in `context=view`.
    Demarcations for reusable blocks are always expected to be accessible by clients.
    
    Props noisysocks, youknowriad.
    
    Merges [43917] to trunk.
    
    See #45145 for the patch, #45098 for the original ticket.
    Built from https://develop.svn.wordpress.org/trunk@44268
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44098 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44267';
+$wp_version = '5.1-alpha-44268';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
