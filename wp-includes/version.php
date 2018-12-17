589af44fc17dffb83526b743d9b94dbfac8faf93
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-12-17 03:30:39 +0000

    Tests: Replace use of `$this->server` with `rest_get_server()` in `test_registered_query_params()`.
    In [42724], `$this->server` was replaced with `rest_get_server()` for better memory recycling.
    
    [43897], from the 5.0 branch, was merged into trunk in [44250] and used the now unavailable `$this->server`.
    
    This updates the new test from the 5.0 branch to use the expected `rest_get_server()`.
    
    See #43316, #41641.
    Built from https://develop.svn.wordpress.org/trunk@44255
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44085 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44254';
+$wp_version = '5.1-alpha-44255';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
