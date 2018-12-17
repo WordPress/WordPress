49418dbd7ea407026bdaacc439ed202db8414847
Jeremy Felt <jeremy.felt@gmail.com>
2018-12-17 03:01:56 +0000

    Posts: Remove the slug from Quick Edit for posts that are not `is_post_type_viewable()`.
    This is a followup to [43728], which added the same check, but using the `publicly_queryable` option. This behaviour is incorrect for `_builtin` post types, which go by the logic in `is_post_type_viewable()`, instead.
    
    Merges [43855] from the 5.0 branch to trunk.
    
    Props pento.
    See #43278.
    
    Built from https://develop.svn.wordpress.org/trunk@44221
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44051 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44220';
+$wp_version = '5.1-alpha-44221';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
