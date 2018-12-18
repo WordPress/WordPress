7b4a2c9827f8bcfbb1ddda5c73cb8ab6757ff9a1
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-12-18 03:42:49 +0000

    Build/Test Tools: Remove `vendor` file name entry from `.gitignore`.
    Props netweb.
    Merges [43747] to trunk.
    Fixes #43411.
    Built from https://develop.svn.wordpress.org/trunk@44285
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44115 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44283';
+$wp_version = '5.1-alpha-44285';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
