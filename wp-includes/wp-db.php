1ae038d3dba6bfb5212a536282c0337bdb8b32f8
Drew Jaynes <info@drewapicture.com>
2018-12-19 07:30:48 +0000

    Docs: Update an inline `@see` reference in the DocBlock for wpdb::prepare() to reference `wpdb::esc_like()` the method instead of `esc_like()` the nonexistent function.
    Props antaltettinger.
    Fixes #45204. 
    
    Built from https://develop.svn.wordpress.org/trunk@44330
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44160 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -1273,7 +1273,7 @@ class wpdb {
 	 *
 	 * Literal percentage signs (%) in the query string must be written as %%. Percentage wildcards (for example,
 	 * to use in LIKE syntax) must be passed via a substitution argument containing the complete LIKE string, these
-	 * cannot be inserted directly in the query string. Also see {@see esc_like()}.
+	 * cannot be inserted directly in the query string. Also see {@see wpdb::esc_like()}.
 	 *
 	 * Arguments may be passed as individual arguments to the method, or as a single array containing all arguments. A combination
 	 * of the two is not supported.
