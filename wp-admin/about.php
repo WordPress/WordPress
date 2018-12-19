ce30819c90345a411eeff848343c5babb3b99844
Gary Pendergast <gary@pento.net>
2018-12-19 22:19:48 +0000

    About: Add 5.0.2 details to the about page.
    Props desrosj.
    Fixes #45703.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.0@44337
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@44167 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -64,6 +64,24 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 			<h3><?php _e( 'Maintenance and Security Releases' ); ?></h3>
 			<p>
 				<?php
+				printf(
+				/* translators: 1: WordPress version number, 2: plural number of bugs. */
+					_n(
+						'<strong>Version %1$s</strong> addressed %2$s bug.',
+						'<strong>Version %1$s</strong> addressed %2$s bugs.',
+						73
+					),
+					'5.0.2',
+					number_format_i18n( 73 )
+				);
+				?>
+				<?php
+				/* translators: %s: Codex URL */
+				printf( __( 'For more information, see <a href="%s">the release notes</a>.' ), 'https://codex.wordpress.org/Version_5.0.2' );
+				?>
+			</p>
+			<p>
+				<?php
 				/* translators: %s: WordPress version number */
 				printf( __( '<strong>Version %s</strong> addressed some security issues.' ), '5.0.1' );
 				?>
