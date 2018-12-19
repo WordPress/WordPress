095e1c7f897ada2a4056243bc7106c3613078e70
Drew Jaynes <info@drewapicture.com>
2018-12-19 07:13:48 +0000

    Docs: Remove references to the now-defunct `$merged_filters` global from two method DocBlocks in `WP_UnitTestCase`.
    The `$merged_filters` global was removed in [38571].
    
    Props frank-klein.
    Fixes #44496.
    
    Built from https://develop.svn.wordpress.org/trunk@44327
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44157 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44326';
+$wp_version = '5.1-alpha-44327';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
