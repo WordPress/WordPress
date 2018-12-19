5b19e65c3b9c7b5e72e4048d4d99dc719d731e0e
Drew Jaynes <info@drewapicture.com>
2018-12-19 17:00:50 +0000

    Docs: Add a missing DocBlock summary and `$action` parameter description for `wp_ajax_delete_page()`.
    Props abhijitrakas.
    Fixes #44206.
    
    Built from https://develop.svn.wordpress.org/trunk@44335
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44165 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/ajax-actions.php
+++ wp-admin/includes/ajax-actions.php
@@ -800,9 +800,11 @@ function wp_ajax_untrash_post( $action ) {
 }
 
 /**
+ * Ajax handler to delete a page.
+ *
  * @since 3.1.0
  *
- * @param string $action
+ * @param string $action Action to perform.
  */
 function wp_ajax_delete_page( $action ) {
 	if ( empty( $action ) ) {
