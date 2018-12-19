110aae80486110e55b1c04460a5d9ae0d3cfe322
desrosj <desrosj@git.wordpress.org>
2018-12-19 03:18:26 +0000

    Twenty Nineteen: Code Quality Improvements.
    This change adds general code quality and documentation improvements.
    
    More info here: https://github.com/WordPress/twentynineteen/pull/546 
    
    Props grapplerulrich, iCaleb, allancole.
    
    Merges [44187] to trunk.
    
    See #45424.
    Built from https://develop.svn.wordpress.org/trunk@44298
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44128 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentynineteen/archive.php
+++ wp-content/themes/twentynineteen/archive.php
@@ -20,8 +20,6 @@ get_header();
 			<header class="page-header">
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					// Remove for now @TODO
-					// the_archive_description( '<div class="page-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
