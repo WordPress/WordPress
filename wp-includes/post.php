f05077e0b1e341b8deccb885ccd9b07c0500ad72
Drew Jaynes <info@drewapicture.com>
2018-12-19 16:39:48 +0000

    Docs: Add backtick escaping around a variable name in the DocBlock for `update_post_meta()`.
    Also adds a note to the description explaining that it can be used in place of `add_post_meta()`.
    
    See #42505.
    
    Built from https://develop.svn.wordpress.org/trunk@44334
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44164 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -2027,10 +2027,12 @@ function get_post_meta( $post_id, $key = '', $single = false ) {
 /**
  * Updates a post meta field based on the given post ID.
  *
- * Use the $prev_value parameter to differentiate between meta fields with the
+ * Use the `$prev_value` parameter to differentiate between meta fields with the
  * same key and post ID.
  *
  * If the meta field for the post does not exist, it will be added and its ID returned.
+ *
+ * Can be used in place of add_post_meta().
  *
  * @since 1.5.0
  *
