bf308f6e92b090653f069f1a58987634efb64d50
laurelfulford <laurelfulford@git.wordpress.org>
2018-12-28 18:12:51 +0000

    Twenty Nineteen: Remove transparency from submenus.
    On pages and posts with featured images, the top level menu items have a slight transparency on hover, which was being inherited by their submenus. This update removes that inheritance, improving readability and consistency in the menu's appearance. 
    
    Props kjellr.
    Fixes #45689.
    
    
    Built from https://develop.svn.wordpress.org/trunk@44368
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44198 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44367';
+$wp_version = '5.1-alpha-44368';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
