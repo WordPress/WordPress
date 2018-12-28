fcab8d02e08b63ce99598899117459c533720ab3
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-12-28 13:54:41 +0000

    Build Tools: Remove unnecessary `sprintf()` in translatable strings.
    Add a missing `sprintf()` for `npm install` substring.
    
    See #44492.
    Built from https://develop.svn.wordpress.org/trunk@44366
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44196 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44365';
+$wp_version = '5.1-alpha-44366';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
