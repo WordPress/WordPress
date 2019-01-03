65ecdb010913ef21a14ff000a7c5ec3d1486e231
desrosj <desrosj@git.wordpress.org>
2019-01-03 16:24:46 +0000

    Happy New Year!
    Fixes Tests_Basic::test_license
    
    Merges [44379] to the 5.0 branch.
    
    Props jorbin.
    Built from https://develop.svn.wordpress.org/branches/5.0@44384
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@44214 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0.3-alpha-44357';
+$wp_version = '5.0.3-alpha-44384';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
