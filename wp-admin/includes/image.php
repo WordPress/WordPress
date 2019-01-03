f29dfe7b6d597806d423a6daa664f5e0122f0868
desrosj <desrosj@git.wordpress.org>
2019-01-03 17:47:49 +0000

    Docs: Update `since` tag for new `intermediate_image_sizes_advanced` filter parameter.
    Originally added in [42407].
    
    Fixes #42745.
    Built from https://develop.svn.wordpress.org/trunk@44385
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44215 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/image.php
+++ wp-admin/includes/image.php
@@ -130,7 +130,7 @@ function wp_generate_attachment_metadata( $attachment_id, $file ) {
 		 *
 		 * @since 2.9.0
 		 * @since 4.4.0 Added the `$metadata` argument.
-		 * @since 5.0.0 Added the `$attachment_id` argument.
+		 * @since 5.1.0 Added the `$attachment_id` argument.
 		 *
 		 * @param array $sizes         An associative array of image sizes.
 		 * @param array $metadata      An associative array of image metadata: width, height, file.
