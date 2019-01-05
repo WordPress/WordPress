194e7eb034535c0a24cdfd08b716bc5908cd93c3
desrosj <desrosj@git.wordpress.org>
2019-01-05 03:51:46 +0000

    Editor: Allow the “Add Media” button tooltip to be translated in the Classic block.
    In the Classic Editor, the “Add Media” button was outside of the TinyMCE editor. In the Block Editor, the “Add Media” button was moved into the TinyMCE toolbar in the Classic block, but the tooltip was not added to the list of translatable strings. This adds “Add Media” to that list.
    
    The corresponding keyboard shortcut is also specified for the “Add Media” button.
    
    Props afercia.
    
    Merges [44398-44399] to the 5.0 branch.
    Fixes #45788.
    Built from https://develop.svn.wordpress.org/branches/5.0@44400
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@44230 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0.3-alpha-44393';
+$wp_version = '5.0.3-alpha-44400';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
