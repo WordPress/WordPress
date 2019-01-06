8b0f279239eae7eafca75dc277277cc94651b7e7
desrosj <desrosj@git.wordpress.org>
2019-01-06 20:19:45 +0000

    Twenty Seventeen: Use a simple counter incremented with each call instead of `uniqid()` for generating unique IDs for HTML elements.
    Props westonruter, laurelfulford.
    
    Merges [43659] and [44408] to the 5.0 branch.
    Fixes #44883.
    Built from https://develop.svn.wordpress.org/branches/5.0@44409
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@44239 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyseventeen/inc/icon-functions.php
+++ wp-content/themes/twentyseventeen/inc/icon-functions.php
@@ -74,7 +74,7 @@ function twentyseventeen_get_svg( $args = array() ) {
 	 */
 	if ( $args['title'] ) {
 		$aria_hidden     = '';
-		$unique_id       = uniqid();
+		$unique_id       = twentyseventeen_unique_id();
 		$aria_labelledby = ' aria-labelledby="title-' . $unique_id . '"';
 
 		if ( $args['desc'] ) {
