49351e0578797a70c592802674ff2b120c367f21
Dominik Schilling <dominikschilling+git@gmail.com>
2019-01-06 16:23:49 +0000

    I18N/Script Loader: Support text domains other than "messages".
    The inline JavaScript added by `WP_Scripts::print_translations()` should check whether `locale_data.$text_domain` exists and fall back to `locale_data.messages` otherwise.
    
    Props swissspidy.
    See #45441.
    Built from https://develop.svn.wordpress.org/trunk@44403
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44233 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44402';
+$wp_version = '5.1-alpha-44403';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
