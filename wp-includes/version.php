ef2e5af1f674a4f51167117ad64c3380f08aef74
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-01-07 16:06:45 +0000

    Twenty Nineteen: Push right-aligned image block outside of text column.
    Update right-aligned image block styles to push them outside of the text column on the front end, to match the original design and how the block looks in the editor.
    
    Props kjellr, joen, allancole.
    Merges [44371] to the 5.0 branch.
    Fixes #45716.
    Built from https://develop.svn.wordpress.org/branches/5.0@44428
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@44258 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0.3-alpha-44427';
+$wp_version = '5.0.3-alpha-44428';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
