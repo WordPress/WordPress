51155f3989c29bbae5322b6cba58952a7c58b0ba
desrosj <desrosj@git.wordpress.org>
2019-01-08 18:33:49 +0000

    Docs: Specify missing `global` variables used in `wp_print_theme_file_tree()`.
    The `$relative_file` and `$stylesheet` variables are used in the `wp_print_theme_file_tree()` function but were not noted within the inline documentation.
    
    Props mukesh27, desrosj,
    Fixes #43682.
    Built from https://develop.svn.wordpress.org/trunk@44477
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44308 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/misc.php
+++ wp-admin/includes/misc.php
@@ -313,6 +313,10 @@ function wp_make_theme_file_tree( $allowed_files ) {
  * @since 4.9.0
  * @access private
  *
+ * @global string $relative_file Name of the file being edited relative to the
+ *                               theme directory.
+ * @global string $stylesheet    The stylesheet name of the theme being edited.
+ *
  * @param array|string $tree  List of file/folder paths, or filename.
  * @param int          $level The aria-level for the current iteration.
  * @param int          $size  The aria-setsize for the current iteration.
