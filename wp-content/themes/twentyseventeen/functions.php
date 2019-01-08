58d5410ceb14af9229613c38c3b34af8dbdc3a4a
desrosj <desrosj@git.wordpress.org>
2019-01-08 19:22:49 +0000

    Docs: Fix typos in Twenty Seventeen function descriptions.
    Props faisal03, mukesh27.
    Fixes #45466.
    Built from https://develop.svn.wordpress.org/trunk@44479
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44310 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyseventeen/functions.php
+++ wp-content/themes/twentyseventeen/functions.php
@@ -439,7 +439,7 @@ function twentyseventeen_colors_css_wrap() {
 add_action( 'wp_head', 'twentyseventeen_colors_css_wrap' );
 
 /**
- * Enqueue scripts and styles.
+ * Enqueues scripts and styles.
  */
 function twentyseventeen_scripts() {
 	// Add custom fonts, used in the main stylesheet.
@@ -501,7 +501,7 @@ function twentyseventeen_scripts() {
 add_action( 'wp_enqueue_scripts', 'twentyseventeen_scripts' );
 
 /**
- * Enqueue styles for the block-based editor.
+ * Enqueues styles for the block-based editor.
  *
  * @since Twenty Seventeen 1.8
  */
