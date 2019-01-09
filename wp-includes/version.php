f88769fc3b9e470e47ddd18f2e373204a6e89c53
Gary Pendergast <gary@pento.net>
2019-01-09 06:54:49 +0000

    Block Editor: Obey the `AUTOSAVE_INTERVAL` constant.
    Props youknowriad.
    Fixes #45262.
    
    
    Built from https://develop.svn.wordpress.org/trunk@44504
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44335 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44503';
+$wp_version = '5.1-alpha-44504';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
