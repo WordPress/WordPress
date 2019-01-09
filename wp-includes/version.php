f57b25f4472adc48e0f9a8b6a5ea60ce884bf710
Gary Pendergast <gary@pento.net>
2019-01-09 11:14:51 +0000

    Tests: Revert [44509].
    This commit causes Travis to fail.
    
    See #45863.
    
    Built from https://develop.svn.wordpress.org/trunk@44512
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44343 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44511';
+$wp_version = '5.1-alpha-44512';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
