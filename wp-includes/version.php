f9208ac14e3553d9dbb4181413cbc1b40d0264d8
Gary Pendergast <gary@pento.net>
2019-01-09 10:31:51 +0000

    Tests: Improve REST API tests for categories and tags.
    Props birgire, SergeyBiryukov.
    See #39122.
    Fixes #45077.
    
    
    Built from https://develop.svn.wordpress.org/trunk@44510
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44341 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44509';
+$wp_version = '5.1-alpha-44510';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
