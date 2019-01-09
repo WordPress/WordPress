e0caaf7628bfc39818661b021d70b0a80ea3b426
Gary Pendergast <gary@pento.net>
2019-01-09 10:10:51 +0000

    Build/Tests: Default to running unit tests from `src`.
    This commit also defaults WP-CLI commands to running against `src`, too.
    
    
    Props atimmer, pento.
    See #44492.
    Fixes #45863.
    
    
    Built from https://develop.svn.wordpress.org/trunk@44508
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44339 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44507';
+$wp_version = '5.1-alpha-44508';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
