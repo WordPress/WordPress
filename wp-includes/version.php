64bbbda39e047426d7b8bd94aef915fc62f3ce7a
Andrea Fercia <a.fercia@gmail.com>
2019-01-10 22:08:49 +0000

    Accessibility: Improve color contrast ratio in the Events and News dashboard widget.
    Props boemedia, lucagrandicelli.
    Fixes #45280.
    
    Built from https://develop.svn.wordpress.org/trunk@44549
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44380 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44548';
+$wp_version = '5.1-alpha-44549';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
