be54938eef1e3a74b1f2698b7f8288945c984dfb
Felix Arntz <felix-arntz@leaves-and-love.net>
2019-01-10 21:17:51 +0000

    General: Fix existing coding standard violations in bookmark tests, as outlined in [44546].
    See #43977.
    
    Built from https://develop.svn.wordpress.org/trunk@44547
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44378 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44546';
+$wp_version = '5.1-alpha-44547';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
