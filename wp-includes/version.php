c00229969a9668ead153b05fe95a1a240fdfb718
Gary Pendergast <gary@pento.net>
2019-01-11 06:17:50 +0000

    Coding Standards: Add exceptions for `WordPress.Files.FileName.NotHyphenatedLowercase`.
    See #45934.
    
    
    Built from https://develop.svn.wordpress.org/trunk@44564
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44395 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-beta1-44563';
+$wp_version = '5.1-beta1-44564';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
