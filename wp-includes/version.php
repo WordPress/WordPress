57fcb7d8dda882ae548693d9ec33b3c1f5df838d
Gary Pendergast <gary@pento.net>
2019-01-12 03:49:49 +0000

    Coding Standards: Fix an incorrect `phpcs:ignore` comment.
    It's `phpcs:ignore`, not `wpcs:ignore`.
    
    Props GaryJ.
    
    See #45934.
    
    
    Built from https://develop.svn.wordpress.org/trunk@44570
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44401 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-beta1-44569';
+$wp_version = '5.1-beta1-44570';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
