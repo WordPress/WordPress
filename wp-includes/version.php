a7d029344662b3fe57788330fd4a4945749247f9
desrosj <desrosj@git.wordpress.org>
2019-01-14 21:34:55 +0000

    Tests: Add missing `parent::tearDown()`.
    Also, remove a `tearDown()` method that was not serving any purpose.
    
    Fixes #45931.
    Built from https://develop.svn.wordpress.org/trunk@44584
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44415 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-beta1-44583';
+$wp_version = '5.1-beta1-44584';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
