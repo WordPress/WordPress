56e77d8928a006eacff07cecf8c1a732b8475c2c
Gary Pendergast <gary@pento.net>
2019-01-14 23:12:50 +0000

    Docs: Improve the docblock of `wp_admin_bar_edit_menu()`.
    What age were you when you learned that there's a `$user_id` global, as well as `$user_ID` global? I was today years old.
    
    Props pento, mukesh27.
    Fixes #43448.
    
    
    Built from https://develop.svn.wordpress.org/trunk@44586
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44417 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/admin-bar.php
+++ wp-includes/admin-bar.php
@@ -677,6 +677,8 @@ function wp_admin_bar_shortlink_menu( $wp_admin_bar ) {
  *
  * @global WP_Term  $tag
  * @global WP_Query $wp_the_query
+ * @global int      $user_id      The ID of the user being edited. Not to be confused with the
+ *                                global $user_ID, which contains the ID of the current user.
  *
  * @param WP_Admin_Bar $wp_admin_bar
  */
