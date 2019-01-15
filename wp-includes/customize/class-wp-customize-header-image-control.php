656d8edcaf20e7768b6e58f5752b61467260c608
Gary Pendergast <gary@pento.net>
2019-01-15 05:54:48 +0000

    Docs: Add docblocks for `WP_Customize_Header_Image_Control` member variables.
    Props subrataemfluence.
    Fixes #44410.
    
    
    Built from https://develop.svn.wordpress.org/trunk@44600
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44431 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/customize/class-wp-customize-header-image-control.php
+++ wp-includes/customize/class-wp-customize-header-image-control.php
@@ -15,8 +15,28 @@
  * @see WP_Customize_Image_Control
  */
 class WP_Customize_Header_Image_Control extends WP_Customize_Image_Control {
+	/**
+	 * Customize control type.
+	 *
+	 * @since 4.2.0
+	 * @var string
+	 */
 	public $type = 'header';
+
+	/**
+	 * Uploaded header images.
+	 *
+	 * @since 3.9.0
+	 * @var string
+	 */
 	public $uploaded_headers;
+
+	/**
+	 * Default header images.
+	 *
+	 * @since 3.9.0
+	 * @var string
+	 */
 	public $default_headers;
 
 	/**
