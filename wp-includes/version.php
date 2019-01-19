5e6e3573bf373dd7ce0b8bbc2b0fd8037dbb9535
Andrea Fercia <a.fercia@gmail.com>
2019-01-19 15:28:50 +0000

    Build/Test Tools: Update QUnit index file after [43174].
    Updates the last occurrence of "(opens in a new window)" to "(opens in a new tab)".
    
    See #43803.
    
    Built from https://develop.svn.wordpress.org/trunk@44655
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44486 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-beta1-44654';
+$wp_version = '5.1-beta1-44655';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
