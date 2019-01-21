a57ead3d94d07d7447fff9a075bfcbffc2e4ac04
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-01-21 13:54:54 +0000

    I18N: Update translator comment for the notice displayed when viewing the block editor with JavaScript disabled.
    A URL in the comment creates an impression that the link is not translatable.
    
    See #45453, #45856.
    Built from https://develop.svn.wordpress.org/trunk@44665
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44496 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-form-blocks.php
+++ wp-admin/edit-form-blocks.php
@@ -408,7 +408,7 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
 			<p>
 				<?php
 					$message = sprintf(
-						/* translators: %s: https://wordpress.org/plugins/classic-editor/ */
+						/* translators: %s: Classic Editor plugin URL */
 						__( 'The block editor requires JavaScript. Please enable JavaScript in your browser settings, or try the <a href="%s">Classic Editor plugin</a>.' ),
 						__( 'https://wordpress.org/plugins/classic-editor/' )
 					);
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-beta1-44664';
+$wp_version = '5.1-beta1-44665';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
