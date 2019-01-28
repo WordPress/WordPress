74468758dd4dc50cb7867adda3dcf8de86713a2e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-01-28 14:11:51 +0000

    Build/Test Tools: Add support for PHPUnit 7.x.
    * Create an abstract `WP_UnitTestCase_Base` class to share between PHPUnit 7.x and older versions.
    * Add a speed-trap loader to determine which `SpeedTrapListener` class needs to be loaded for the current PHPUnit version.
    * Remove unnecessary `PHPUnit\Util\Test` and `PHPUnit_Util_Getopt` inheritances.
    * Update Travis CI config to use PHPUnit 7.x for PHP 7.1, 7.2, and nightly PHP versions.
    
    Props jipmoors, netweb, desrosj, ayeshrajans, soulseekah, SergeyBiryukov.
    See #43218.
    Built from https://develop.svn.wordpress.org/trunk@44701
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44532 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-beta2-44700';
+$wp_version = '5.1-beta2-44701';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
