2317cd839813813f7ceb158de130b103e9d9c1ea
Peter Wilson <wilson@peterwilson.cc>
2019-02-14 00:13:50 +0000

    Trunk is now 5.2-alpha.
    Built from https://develop.svn.wordpress.org/trunk@44742
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44574 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-RC1-44740';
+$wp_version = '5.2-alpha-44742';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
