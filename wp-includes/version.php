19eb54f15949fb574fcc8b77c5ed7e3ddcc447b6
Gary Pendergast <gary@pento.net>
2019-02-19 01:59:51 +0000

    Accessibility: Fix a regression in the old media modal pagination links.
    [43019] improved the table pagination link styling, but introduced a regression in the old media dialog.
    
    Merges [44747] to the 5.1 branch.
    
    Props afercia, joneiseman.
    Fixes #41858.
    
    Built from https://develop.svn.wordpress.org/branches/5.1@44750
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.1@44582 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-RC1-44745';
+$wp_version = '5.1-RC1-44750';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
