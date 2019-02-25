67e811c15e703d2415e7435bf9878282265e4028
desrosj <desrosj@git.wordpress.org>
2019-02-25 15:21:52 +0000

    Build: Minify images
    After `grunt-contrib-imagemin` was upgraded in WordPress 5.0, the `grunt precommit:image` task results in further minifications to `loading.gif`.
    
    Fixes #46295.
    Built from https://develop.svn.wordpress.org/trunk@44768
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44600 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-alpha-44767';
+$wp_version = '5.2-alpha-44768';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
