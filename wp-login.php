c78d28d9f10bf9de2c2c08d0c9b529e237b6836b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-02-28 11:44:52 +0000

    Login and Registration: Update URLs for browser cookie documentation.
    Props joostdevalk.
    Fixes #46254.
    Built from https://develop.svn.wordpress.org/trunk@44776
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44608 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-login.php
+++ wp-login.php
@@ -935,7 +935,7 @@ switch ( $action ) {
 					sprintf(
 						/* translators: 1: Browser cookie documentation URL, 2: Support forums URL */
 						__( '<strong>ERROR</strong>: Cookies are blocked due to unexpected output. For help, please see <a href="%1$s">this documentation</a> or try the <a href="%2$s">support forums</a>.' ),
-						__( 'https://codex.wordpress.org/Cookies' ),
+						__( 'https://wordpress.org/support/article/cookies/' ),
 						__( 'https://wordpress.org/support/' )
 					)
 				);
@@ -946,7 +946,7 @@ switch ( $action ) {
 					sprintf(
 						/* translators: %s: Browser cookie documentation URL */
 						__( '<strong>ERROR</strong>: Cookies are blocked or not supported by your browser. You must <a href="%s">enable cookies</a> to use WordPress.' ),
-						__( 'https://codex.wordpress.org/Cookies' )
+						__( 'https://wordpress.org/support/article/cookies/#enable-cookies-in-your-browser' )
 					)
 				);
 			}
