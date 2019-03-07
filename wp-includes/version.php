9dc2500b34ec9339de1911fd6debd549f7684ada
desrosj <desrosj@git.wordpress.org>
2019-03-07 22:35:51 +0000

    Widgets: Remove `change` event when editing a Custom HTML widget.
    The `change` event was previously required to ensure that the Customizer picked detected changes to the widget's content and synced them to the preview. In the current state, though, the `trigger( 'change' )` is no longer required and is causing issues with the widget's “Done” and “Save” buttons.
    
    Merges [44816] to the 5.1 branch.
    
    Fixes #46335.
    Props audrasjb, afercia, westonruter.
    Built from https://develop.svn.wordpress.org/branches/5.1@44817
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.1@44649 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1.1-alpha-44815';
+$wp_version = '5.1.1-alpha-44817';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
