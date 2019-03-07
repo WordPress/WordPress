ec417a34a7ce0d10a998b7eb6d50d7ba6291d94d
desrosj <desrosj@git.wordpress.org>
2019-03-07 21:32:50 +0000

    General: Add a way to specify a direct link for a user to update PHP.
    A direct URL to where a user can update PHP for their website can now be specified in one of two ways:
    
    - Defining the `WP_DIRECT_UPDATE_PHP_URL` environment variable.
    - Returning a URL to the `wp_direct_php_update_url` filter.
    
    When a URL is specified, an additional “Update PHP” button will be displayed at the bottom of the Core dashboard widget informing administrators that their site is running an outdated version of PHP (see [42832]).
    
    Fixes #46074.
    Props afragen, desrosj, lukecarbis.
    Built from https://develop.svn.wordpress.org/trunk@44814
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44646 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/dashboard.php
+++ wp-admin/includes/dashboard.php
@@ -1647,6 +1647,7 @@ function wp_dashboard_php_nag() {
 	<?php
 
 	wp_update_php_annotation();
+	wp_direct_php_update_button();
 }
 
 /**
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-alpha-44813';
+$wp_version = '5.2-alpha-44814';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
