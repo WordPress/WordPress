ed88ef20727ce994be28420ff3f2ab4637cb1755
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-03-12 22:33:45 +0000

    Comments: Improve comment content filtering.
    Merges [44842] to the 5.0 branch.
    Built from https://develop.svn.wordpress.org/branches/5.0@44844
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.0@44676 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0.4-alpha-44835';
+$wp_version = '5.0.4-alpha-44844';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
