c4f8bc468db56baa2a3bf917c99cdfd17c3391ce
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-03-12 22:56:49 +0000

    PHPCS: Fix errors introduced in [44833]. Built from https://develop.svn.wordpress.org/trunk@44858
    git-svn-id: http://core.svn.wordpress.org/trunk@44690 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-alpha-44842';
+$wp_version = '5.2-alpha-44858';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
