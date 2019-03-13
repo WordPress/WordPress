613e8c36f949e6c089b31cdde88491918adede20
Gary Pendergast <gary@pento.net>
2019-03-13 00:17:50 +0000

    WordPress 5.1.1 Built from https://develop.svn.wordpress.org/branches/5.1@44860
    git-svn-id: http://core.svn.wordpress.org/branches/5.1@44692 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -34,6 +34,35 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 			<a href="freedoms.php?privacy-notice" class="nav-tab"><?php _e( 'Privacy' ); ?></a>
 		</h2>
 
+		<div class="changelog point-releases">
+			<h3><?php _e( 'Maintenance and Security Releases' ); ?></h3>
+			<p>
+				<?php
+				printf(
+					/* translators: 1: WordPress version number, 2: plural number of bugs. */
+					_n(
+						'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bug.',
+						'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bugs.',
+						14
+					),
+					'5.1.1',
+					number_format_i18n( 14 )
+				);
+				?>
+				<?php
+				printf(
+					/* translators: %s: HelpHub URL */
+					__( 'For more information, see <a href="%s">the release notes</a>.' ),
+					sprintf(
+						/* translators: %s: WordPress version */
+						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+						sanitize_title( '5.1.1' )
+					)
+				);
+				?>
+			</p>
+		</div>
+
 		<h2 class="feature-section-header"><?php _e( 'A Little Better Every Day' ); ?></h2>
 
 		<div class="feature-section headline-feature one-col">
