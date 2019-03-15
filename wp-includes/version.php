f03ccf68138e19f59efb28eba4b4c1d2e82e85aa
Andrew Ozz <admin@laptoptips.ca>
2019-03-15 19:32:50 +0000

    TinyMCE: remove the reference to `skin.min.css.map` from `skin.min.css`. Fixes an unneeded 404 request.
    Fixes #46533.
    Built from https://develop.svn.wordpress.org/trunk@44911
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44742 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-alpha-44910';
+$wp_version = '5.2-alpha-44911';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
