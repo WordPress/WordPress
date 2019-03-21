fac9f5f9fb6c81be48060dedfca2abf7142cb3a0
K. Adam White <kadamwhite@gmail.com>
2019-03-21 18:59:52 +0000

    REST API: Avoid undefined-property notice when setting parent term to 0.
    Only try to access `term_id` once `$parent_term` is known to be a `WP_Term`.
    
    Props dlh, earnjam.
    Fixes #44983.
    
    Built from https://develop.svn.wordpress.org/trunk@44965
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44796 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/endpoints/class-wp-rest-terms-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-terms-controller.php
@@ -685,11 +685,15 @@ class WP_REST_Terms_Controller extends WP_REST_Controller {
 		}
 
 		if ( isset( $request['parent'] ) && ! empty( $schema['properties']['parent'] ) ) {
-			$parent_term_id = 0;
-			$parent_term    = get_term( (int) $request['parent'], $this->taxonomy );
+			$parent_term_id   = 0;
+			$requested_parent = (int) $request['parent'];
 
-			if ( $parent_term ) {
-				$parent_term_id = $parent_term->term_id;
+			if ( $requested_parent ) {
+				$parent_term = get_term( $requested_parent, $this->taxonomy );
+
+				if ( $parent_term instanceof WP_Term ) {
+					$parent_term_id = $parent_term->term_id;
+				}
 			}
 
 			$prepared_term->parent = $parent_term_id;
