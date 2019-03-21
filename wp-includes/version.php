217849aab35a3ddbf0a55242b361a699989263af
desrosj <desrosj@git.wordpress.org>
2019-03-21 02:56:51 +0000

    Menus: Ensure consistent padding in the menu structure form.
    Props shital-patel, welcher, hardik-amipara, audrasjb.
    Fixes #42596.
    Built from https://develop.svn.wordpress.org/trunk@44952
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44783 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-alpha-44951';
+$wp_version = '5.2-alpha-44952';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
