d36c01909ad324c42b134bf721f62dd110d58b7d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-04-06 15:42:52 +0000

    Site Health: Use a consistent heading and description when referring to Info and Status pages.
    Props chetan200891, afercia.
    Fixes #46728.
    Built from https://develop.svn.wordpress.org/trunk@45120
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44929 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-beta1-45119';
+$wp_version = '5.2-beta1-45120';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
