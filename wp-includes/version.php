7eafd6d81a7d09f5bc5462cc48fe6d402dfa2af4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-04-07 05:49:51 +0000

    CSS Coding Standards: Use unitless values for `line-height` in `wp-admin/css/customize-nav-menus.css`.
    Props ianbelanger, mukesh27, pbiron, afercia.
    Fixes #46495. See #44643.
    Built from https://develop.svn.wordpress.org/trunk@45129
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44938 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-beta1-45128';
+$wp_version = '5.2-beta1-45129';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
