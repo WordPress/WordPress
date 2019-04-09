ed8723fd8f0abdcaebd674fac1df2ddc18cb7c13
Gary Pendergast <gary@pento.net>
2019-04-09 00:16:53 +0000

    UI: Remove vertical alignment from active primary buttons.
    This caused the post-plugin installation actions to jump around when the "Activate Plugin" primary button became active.
    
    Props seedsca, pratikthink, ianbelanger, mukesh27.
    Fixes #44836.
    
    
    Built from https://develop.svn.wordpress.org/trunk@45150
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44959 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-beta1-45149';
+$wp_version = '5.2-beta1-45150';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
