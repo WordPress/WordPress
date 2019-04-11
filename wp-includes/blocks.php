17bdafca772c04ed8140830d6eed362de16394ce
John Blackbourn <johnbillion@git.wordpress.org>
2019-04-11 17:27:52 +0000

    General: Fix a typo in the docs for `_restore_wpautop_hook()`.
    Props manzoorwani.jk
    
    Fixes #46864
    
    Built from https://develop.svn.wordpress.org/trunk@45159
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@44968 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks.php
+++ wp-includes/blocks.php
@@ -280,7 +280,7 @@ function do_blocks( $content ) {
 }
 
 /**
- * If do_blocks() needs to remove wp_autop() from the `the_content` filter, this re-adds it afterwards,
+ * If do_blocks() needs to remove wpautop() from the `the_content` filter, this re-adds it afterwards,
  * for subsequent `the_content` usage.
  *
  * @access private
