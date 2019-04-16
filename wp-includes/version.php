fc7a8f71941eafb063be002eca872abf07debf86
Andrea Fercia <a.fercia@gmail.com>
2019-04-16 16:23:52 +0000

    Administration: Improve the Site Health "Passed tests" button style for small screens.
    Props chetan200891.
    Fixes #46939.
    
    Built from https://develop.svn.wordpress.org/trunk@45217
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45026 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-beta3-45216';
+$wp_version = '5.2-beta3-45217';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
