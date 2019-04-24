54c7c7683674bf7b38b0442d939b24f7b212e0f3
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-04-24 18:14:53 +0000

    Bootstrap/Load: Modify the fatal error template to indicate to the user that they can check the admin email address for further instructions.
    Props spacedmonkey, TimothyBlynJacobs.
    Fixes #46950.
    Built from https://develop.svn.wordpress.org/trunk@45263
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45072 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-fatal-error-handler.php
+++ wp-includes/class-wp-fatal-error-handler.php
@@ -161,7 +161,11 @@ class WP_Fatal_Error_Handler {
 			require_once ABSPATH . WPINC . '/functions.php';
 		}
 
-		$message = __( 'The site is experiencing technical difficulties.' );
+		if ( is_protected_endpoint() ) {
+			$message = __( 'The site is experiencing technical difficulties. Please check your site admin email inbox for instructions.' );
+		} else {
+			$message = __( 'The site is experiencing technical difficulties.' );
+		}
 
 		$args = array(
 			'response' => 500,
