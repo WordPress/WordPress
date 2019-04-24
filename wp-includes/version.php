ac7877ce6fabd46b166485cfd3c89c0609c1bc0c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-04-24 18:37:52 +0000

    Twenty Nineteen: Fix padding discrepancies in responsive styles for Column block.
    Props kjellr, dianeco.
    Fixes #46643, #46999.
    Built from https://develop.svn.wordpress.org/trunk@45264
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45073 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-beta3-45263';
+$wp_version = '5.2-beta3-45264';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
