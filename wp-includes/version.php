655c21bbeaa72b4a05fcb1aef95803aecc1464d0
Andrew Ozz <admin@laptoptips.ca>
2019-04-24 21:39:53 +0000

    Fix parsing of inner blocks when auto-generating an excerpt. Helps to prevent cases where dynamic inner blocks may cause an infinite loop if trying to auto-generate an excerpt.
    Props desrosj, pento, gziolo, azaozz.
    Fixes #46133.
    Built from https://develop.svn.wordpress.org/trunk@45265
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45074 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-beta3-45264';
+$wp_version = '5.2-beta3-45265';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
