3360974ad2d2e53f7f1e7c412328db5f2d98cdca
desrosj <desrosj@git.wordpress.org>
2019-05-02 15:44:53 +0000

    Help/About: Polish the About page for 5.2.
    - The About page now uses a CSS grid to layout its design, making the page’s structure more obvious and hopefully easier for contributors in the future.
    - Removes some code that is no longer used (such as videos, mejs elements, etc.).
    - Update images.
    - Unnecessary HTML is removed from a string on the page. This was a regression.
    - Only users with the correct capability should be instructed to check their site status.
    - Fixes the placement of commas.
    
    Reviewed by melchoyce, afercia, jeremyfelt, and desrosj.
    
    Props ryelle, melchoyce, cathibosco1, man4toman, SergeyBiryukov, afercia, ramiy, kjellr, tellyworth, earnjam, andreamiddleton, marybaum.
    See #46901.
    Built from https://develop.svn.wordpress.org/trunk@45278
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45087 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -40,46 +40,54 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 			<a href="freedoms.php?privacy-notice" class="nav-tab"><?php _e( 'Privacy' ); ?></a>
 		</nav>
 
-		<h2 class="feature-section-header"><?php _e( 'Keeping Your Site Safe' ); ?></h2>
-
-		<div class="feature-section headline-feature one-col">
-			<div class="col">
-				<p><?php _e( 'WordPress 5.2 gives you even more robust tools for identifying and fixing configuration issues and fatal errors. Whether you are a developer helping clients or you manage your site solo, these tools can help get you the right information when you need it.' ); ?></p>
-				<div class="inline-svg">
-					<img src="https://make.wordpress.org/core/files/2019/04/site-safe.png" alt="">
-				</div>
+		<div class="headline-feature">
+			<h2><?php _e( 'Keeping Your Site Safe' ); ?></h2>
+			<p class="lead-description"><?php _e( 'WordPress 5.2 gives you even more robust tools for identifying and fixing configuration issues and fatal errors. Whether you are a developer helping clients or you manage your site solo, these tools can help get you the right information when you need it.' ); ?></p>
+			<div class="inline-svg aligncenter">
+				<img src="https://make.wordpress.org/core/files/2019/05/about_maintain-wordpress.png" alt="">
 			</div>
 		</div>
 
 		<hr />
 
-		<div class="feature-section one-col is-wide wp-clearfix">
-			<div class="col">
+		<div class="feature-section is-wide has-2-columns is-wider-left">
+			<div class="column is-vertically-aligned-center">
 				<h3><?php _e( 'Site Health Check' ); ?></h3>
-				<div class="inline-svg alignright">
-					<img src="https://make.wordpress.org/core/files/2019/04/health-check.png" alt="">
-				</div>
 				<p>
 					<?php
 					printf(
-						/* translators: 1: link to the WordPress 5.1 release post 2: link to /wp-admin/site-health.php 3: link to /wp-admin/site-health.php?tab=debug */
-						__( 'Building on <a href="%1$s">the Site Health features introduced in 5.1</a>, this release adds two new pages to help debug common configuration issues. It also adds space where developers can include debugging information for site maintainers. <a href="%2$s">Check your site status</a>, and <a href="%3$s">learn how to debug issues</a>.' ),
-						__( 'https://wordpress.org/news/2019/02/betty/' ),
-						admin_url( 'site-health.php' ),
-						admin_url( 'site-health.php?tab=debug' )
+						/* translators: 1: link to the WordPress 5.1 release post */
+						__( 'Building on <a href="%1$s">the Site Health features introduced in 5.1</a>, this release adds two new pages to help debug common configuration issues. It also adds space where developers can include debugging information for site maintainers.' ),
+						__( 'https://wordpress.org/news/2019/02/betty/' )
 					);
+
+					if ( current_user_can( 'install_plugins' ) ) {
+						printf(
+							/* translators: 1: link to /wp-admin/site-health.php 2: link to /wp-admin/site-health.php?tab=debug */
+							__( ' <a href="%1$s">Check your site status</a>, and <a href="%2$s">learn how to debug issues</a>.' ),
+							admin_url( 'site-health.php' ),
+							admin_url( 'site-health.php?tab=debug' )
+						);
+					}
 					?>
 				</p>
+			</div>
+			<div class="column">
+				<div class="inline-svg aligncenter">
+					<img src="https://s.w.org/images/core/5.2/about_site-health.svg" alt="">
+				</div>
 			</div>
 		</div>
 
 		<hr />
 
-		<div class="feature-section one-col is-wide wp-clearfix">
-			<div class="col">
-				<div class="inline-svg alignleft">
-					<img src="https://make.wordpress.org/core/files/2019/04/error-protection.png" alt="">
+		<div class="feature-section is-wide has-2-columns is-wider-right">
+			<div class="column">
+				<div class="inline-svg aligncenter">
+					<img src="https://s.w.org/images/core/5.2/about_error-protection.svg" alt="">
 				</div>
+			</div>
+			<div class="column is-vertically-aligned-center">
 				<h3><?php _e( 'PHP Error Protection' ); ?></h3>
 				<p><?php _e( 'This administrator-focused update will let you safely fix or manage fatal errors without requiring developer time. It features better handling of the so-called “white screen of death,” and a way to enter recovery mode,  which pauses error-causing plugins or themes.' ); ?></p>
 			</div>
@@ -87,14 +95,14 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 		<hr />
 
-		<h3 class="under-the-hood-header"><?php _e( 'Improvements for Everyone' ); ?></h3>
+		<h3 class="aligncenter"><?php _e( 'Improvements for Everyone' ); ?></h3>
 
-		<div class="under-the-hood feature-section two-col">
-			<div class="col cta">
+		<div class="has-2-columns">
+			<div class="column aligncenter">
 				<h4><?php _e( 'Accessibility Updates' ); ?></h4>
 				<p><?php _e( 'A number of changes work together to improve contextual awareness and keyboard navigation flow for those using screen readers and other assistive technologies.' ); ?></p>
 			</div>
-			<div class="col cta">
+			<div class="column aligncenter">
 				<h4><?php _e( 'New Dashboard Icons' ); ?></h4>
 				<p><?php _e( 'Thirteen new icons include Instagram, a suite of icons for BuddyPress, and rotated Earth icons for global inclusion. Find them in the Dashboard and have some fun!' ); ?></p>
 			</div>
@@ -102,33 +110,33 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 
 		<hr />
 
-		<h3 class="under-the-hood-header"><?php _e( 'Developer Happiness' ); ?></h3>
+		<h3 class="aligncenter"><?php _e( 'Developer Happiness' ); ?></h3>
 
-		<div class="under-the-hood feature-section two-col">
-			<div class="col">
+		<div class="has-2-columns is-fullwidth">
+			<div class="column">
 				<h4><a href="https://make.wordpress.org/core/2019/03/26/coding-standards-updates-for-php-5-6/"><?php _e( 'PHP Version Bump' ); ?></a></h4>
 				<p><?php _e( 'The minimum supported PHP version is now 5.6.20. As of WordPress 5.2, themes and plugins can safely take advantage of namespaces, anonymous functions, and more!' ); ?></p>
 			</div>
-			<div class="col">
+			<div class="column">
 				<h4><a href="https://make.wordpress.org/core/2019/04/24/developer-focused-privacy-updates-in-5-2/"><?php _e( 'Privacy Updates' ); ?></a></h4>
 				<p><?php _e( 'A new theme page template, a conditional function, and two CSS classes make designing and customizing the Privacy Policy page easier.' ); ?></p>
 			</div>
 		</div>
-		<div class="under-the-hood feature-section two-col">
-			<div class="col">
+		<div class="has-2-columns is-fullwidth">
+			<div class="column">
 				<h4><a href="https://make.wordpress.org/core/2019/04/24/miscellaneous-developer-updates-in-5-2/"><?php _e( 'New Body Tag Hook' ); ?></a></h4>
 				<p>
 					<?php
 					printf(
 						/* translators: 1: wp_body_open 2: <body> */
-						__( '5.2 introduces a <code>%1$s</code> hook, which lets themes support injecting code right at the beginning of the <code>%2$s</code> element.' ),
-						'wp_body_open',
-						'&lt;body&gt;'
+						__( '5.2 introduces a %1$s hook, which lets themes support injecting code right at the beginning of the %2$s element.' ),
+						'<code>wp_body_open</code>',
+						'<code>&lt;body&gt;</code>'
 					);
 					?>
 				</p>
 			</div>
-			<div class="col">
+			<div class="column">
 				<h4><a href="https://make.wordpress.org/core/2019/03/25/building-javascript/"><?php _e( 'Building JavaScript' ); ?></a></h4>
 				<p><?php _e( 'With the addition of webpack and Babel configurations in the @wordpress/scripts package, developers won&#8217;t have to worry about setting up complex build tools to write modern JavaScript.' ); ?></p>
 			</div>
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-RC1-45277';
+$wp_version = '5.2-RC1-45278';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
