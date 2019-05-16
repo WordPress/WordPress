9c1a666249919fdc1c49cd03772144a1173bb579
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-05-16 22:08:54 +0000

    Site Health: In HTTP requests test, when checking whether the `WP_HTTP_BLOCK_EXTERNAL` constant is defined, check its value as well.
    Props chesio.
    Fixes #47297.
    Built from https://develop.svn.wordpress.org/trunk@45328
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45139 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45326';
+$wp_version = '5.3-alpha-45328';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
