84256959b7d7a86c8017bc84b57426c399db7efd
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-05-17 00:56:54 +0000

    I18N: Avoid using placeholders for link start and end tags in alt text description strings added in [45158].
    Use an established pattern of moving additional link attributes out of the translatable string.
    
    Props dimadin.
    Merges [45334] to the 5.2 branch.
    Fixes #47159.
    Built from https://develop.svn.wordpress.org/branches/5.2@45335
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.2@45146 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/media-template.php
+++ wp-includes/media-template.php
@@ -163,15 +163,15 @@ function wp_print_media_templates() {
 	}
 
 	$alt_text_description = sprintf(
-		/* translators: 1: link start tag, 2: accessibility text, 3: link end tag */
-		__( '%1$sDescribe the purpose of the image%2$s%3$s. Leave empty if the image is purely decorative.' ),
-		'<a href="' . esc_url( 'https://www.w3.org/WAI/tutorials/images/decision-tree' ) . '" target="_blank" rel="noopener noreferrer">',
+		/* translators: 1: link to tutorial, 2: additional link attributes, 3: accessibility text */
+		__( '<a href="%1$s" %2$s>Describe the purpose of the image%3$s</a>. Leave empty if the image is purely decorative.' ),
+		esc_url( 'https://www.w3.org/WAI/tutorials/images/decision-tree' ),
+		'target="_blank" rel="noopener noreferrer"',
 		sprintf(
 			'<span class="screen-reader-text"> %s</span>',
 			/* translators: accessibility text */
 			__( '(opens in a new tab)' )
-		),
-		'</a>'
+		)
 	);
 	?>
 	<!--[if lte IE 8]>
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2.1-alpha-45333';
+$wp_version = '5.2.1-alpha-45335';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
