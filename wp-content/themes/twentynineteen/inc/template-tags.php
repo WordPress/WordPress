aaa3e181a0cc212d4943953cb1c5390483d342dc
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-05-23 01:31:54 +0000

    Twenty Nineteen: Correct function name in a `function_exists()` check for `twentynineteen_get_user_avatar_markup()`.
    Props mukesh27, diego-la-monica.
    Fixes #46410.
    Built from https://develop.svn.wordpress.org/trunk@45383
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45194 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentynineteen/inc/template-tags.php
+++ wp-content/themes/twentynineteen/inc/template-tags.php
@@ -166,7 +166,7 @@ if ( ! function_exists( 'twentynineteen_post_thumbnail' ) ) :
 	}
 endif;
 
-if ( ! function_exists( 'twentynineteen_comment_avatar' ) ) :
+if ( ! function_exists( 'twentynineteen_get_user_avatar_markup' ) ) :
 	/**
 	 * Returns the HTML markup to generate a user avatar.
 	 */
