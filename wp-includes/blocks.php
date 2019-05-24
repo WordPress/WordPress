68fd25ff1a548777753bc08700ece9ed38d984fc
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-05-24 01:05:52 +0000

    Docs: Remove unused `@global` reference in `do_blocks()` DocBlock.
    Props avinapatel.
    Fixes #47055.
    Built from https://develop.svn.wordpress.org/trunk@45393
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45204 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks.php
+++ wp-includes/blocks.php
@@ -301,9 +301,8 @@ function parse_blocks( $content ) {
  * Parses dynamic blocks out of `post_content` and re-renders them.
  *
  * @since 5.0.0
- * @global WP_Post $post The post to edit.
  *
- * @param  string $content Post content.
+ * @param string $content Post content.
  * @return string Updated post content.
  */
 function do_blocks( $content ) {
