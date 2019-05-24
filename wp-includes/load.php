27720e15a79548313a5f647f373494d13263379c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-05-24 01:47:51 +0000

    Bootstrap/Load: In `wp_get_server_protocol()`, check if `$_SERVER['SERVER_PROTOCOL']` is defined, to avoid a notice in CLI context.
    Props thakkarhardik, malthert.
    Fixes #47005.
    Built from https://develop.svn.wordpress.org/trunk@45400
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45211 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -13,7 +13,7 @@
  * @return string The HTTP protocol. Default: HTTP/1.0.
  */
 function wp_get_server_protocol() {
-	$protocol = $_SERVER['SERVER_PROTOCOL'];
+	$protocol = isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : '';
 	if ( ! in_array( $protocol, array( 'HTTP/1.1', 'HTTP/2', 'HTTP/2.0' ) ) ) {
 		$protocol = 'HTTP/1.0';
 	}
