9abcf7881f62459500dfdec40d8bbf0568a60e6d
John Blackbourn <johnbillion@git.wordpress.org>
2019-05-25 22:52:54 +0000

    Docs: Improve and correct documentation for hook-related variadic functions.
    See #37402
    
    Built from https://develop.svn.wordpress.org/trunk@45420
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45231 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45419';
+$wp_version = '5.3-alpha-45420';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
