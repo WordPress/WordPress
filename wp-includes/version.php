1899a99c35b5bc141e86902efe044f4fe29de7fc
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-05-30 11:53:53 +0000

    CSS Coding Standards: Use unitless values for `line-height` in `wp-admin/css/install.css`.
    Props ianbelanger, pbiron, afercia.
    Fixes #46513. See #44643.
    Built from https://develop.svn.wordpress.org/trunk@45465
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45276 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45464';
+$wp_version = '5.3-alpha-45465';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
