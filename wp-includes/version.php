c3bc45b95d4dd8a20a7a6588f6fb3dbe653911a8
Andrea Fercia <a.fercia@gmail.com>
2019-06-10 13:33:52 +0000

    Accessibility: Improve the Screen Options and Help buttons order.
    - makes the buttons visual order match the DOM order
    - also, restores the focus style on the "hero" primary button after [34948]
    
    Props vrimill, mukesh27 for reporting and testing.
    Merges [45503] to the 5.2 branch.
    Fixes #45094 for 5.2.2.
    
    Built from https://develop.svn.wordpress.org/branches/5.2@45509
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.2@45320 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2.2-alpha-45508';
+$wp_version = '5.2.2-alpha-45509';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
