3d1f02b53712874c4d2f2ea017ab562f5e63b229
Andrea Fercia <a.fercia@gmail.com>
2019-06-12 19:24:55 +0000

    Administration: Fix the responsive layout after changes to the Screen Options and Help buttons position.
    Merges [45520] to the 5.2 branch.
    Fixes #45094 for 5.2.2.
    
    Built from https://develop.svn.wordpress.org/branches/5.2@45521
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.2@45332 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2.2-RC1-45518';
+$wp_version = '5.2.2-RC1-45521';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
