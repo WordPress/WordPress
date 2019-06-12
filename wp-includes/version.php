63870541c761a431d9c1674e7e75e5e85bb5e3ac
Andrea Fercia <a.fercia@gmail.com>
2019-06-12 19:36:53 +0000

    Administration: Improve the horizontal centering of the Site Health tabs.
    Props dkarfa, davidbaumwald, Clorith, tinkerbelly, afercia.
    Merges [45522] to the 5.2 branch.
    Fixes #46881 for 5.2.2.
    
    Built from https://develop.svn.wordpress.org/branches/5.2@45523
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.2@45334 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2.2-RC1-45521';
+$wp_version = '5.2.2-RC1-45523';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
