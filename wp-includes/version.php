727838a11bc7e347daa2e4ee42fb17cc96543cb1
Andrew Ozz <admin@laptoptips.ca>
2019-06-13 16:00:52 +0000

    Add a Recovery Mode Exit button to the notice. This fixes it in responsive view.
    Props desrosj, karmatosed, chetan200891, afercia, afragen, garrett-eclipse.
    Fixes #47070.
    Built from https://develop.svn.wordpress.org/trunk@45529
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45340 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45527';
+$wp_version = '5.3-alpha-45529';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
