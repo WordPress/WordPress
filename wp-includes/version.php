d1cec4590f5fc51f1849ff304fb264623956d761
Andrew Ozz <admin@laptoptips.ca>
2019-06-13 17:08:52 +0000

    Bump version to 5.2.2-RC2 Built from https://develop.svn.wordpress.org/branches/5.2@45533
    git-svn-id: http://core.svn.wordpress.org/branches/5.2@45344 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2.2-RC1-45530';
+$wp_version = '5.2.2-RC2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
