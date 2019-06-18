e58fe38b123eb1cfdf2eecb03d247cd5d92aee51
desrosj <desrosj@git.wordpress.org>
2019-06-18 18:12:52 +0000

    Post WordPress 5.2.2 version bump. Built from https://develop.svn.wordpress.org/branches/5.2@45550
    git-svn-id: http://core.svn.wordpress.org/branches/5.2@45361 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2.2';
+$wp_version = '5.2.3-alpha-45550';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
