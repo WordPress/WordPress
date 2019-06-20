470177a906c51bcb7238ea76f877d266338acf45
desrosj <desrosj@git.wordpress.org>
2019-06-20 09:52:54 +0000

    Help/About: Improve wording of PHP Error Protection section.
    Props pedromendonca, garrett-eclipse.
    See #47475.
    Fixes #47538.
    Built from https://develop.svn.wordpress.org/trunk@45551
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45362 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45546';
+$wp_version = '5.3-alpha-45551';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
