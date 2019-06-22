fdb8ff498040f2d8bbab30357e119da9600b3a13
Peter Wilson <wilson@peterwilson.cc>
2019-06-22 11:12:51 +0000

    Code is Poetry: CS Fixes required in wp-cron.php.
    See #47396, #41057.
    
    
    Built from https://develop.svn.wordpress.org/trunk@45559
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45370 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-cron.php
+++ wp-cron.php
@@ -88,7 +88,8 @@ if ( empty( $doing_wp_cron ) ) {
 		if ( $doing_cron_transient && ( $doing_cron_transient + WP_CRON_LOCK_TIMEOUT > $gmt_time ) ) {
 			return;
 		}
-		$doing_cron_transient = $doing_wp_cron = sprintf( '%.22F', microtime( true ) );
+		$doing_wp_cron        = sprintf( '%.22F', microtime( true ) );
+		$doing_cron_transient = $doing_wp_cron;
 		set_transient( 'doing_cron', $doing_wp_cron );
 	} else {
 		$doing_wp_cron = $_GET['doing_wp_cron'];
@@ -114,7 +115,7 @@ foreach ( $crons as $timestamp => $cronhooks ) {
 
 			$schedule = $v['schedule'];
 
-			if ( $schedule != false ) {
+			if ( ! $schedule ) {
 				$new_args = array( $timestamp, $schedule, $hook, $v['args'] );
 				call_user_func_array( 'wp_reschedule_event', $new_args );
 			}
