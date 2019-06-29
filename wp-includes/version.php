93f3335f133bbf1296f42f493fc66cc786281146
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-06-29 00:51:58 +0000

    Twenty Seventeen: Correct the CSS selectors intended to fix hover colors for MediaElement controls.
    This is a follow-up to the previous fix in [45146], which didn't work as expected.
    
    Props ryokuhi.
    Fixes #47543. See #40843.
    Built from https://develop.svn.wordpress.org/trunk@45576
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45387 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45575';
+$wp_version = '5.3-alpha-45576';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
