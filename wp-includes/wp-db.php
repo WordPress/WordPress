b8be081e7ee0294dcb77e7a962c28d2170eb7840
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-07-04 10:58:56 +0000

    Text Changes: Use consistent question wording in database connection error messages.
    Props Presskopp.
    See #44878.
    Built from https://develop.svn.wordpress.org/trunk@45598
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45409 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -1669,8 +1669,8 @@ class wpdb {
 
 			$message .= "<ul>\n";
 			$message .= '<li>' . __( 'Are you sure you have the correct username and password?' ) . "</li>\n";
-			$message .= '<li>' . __( 'Are you sure that you have typed the correct hostname?' ) . "</li>\n";
-			$message .= '<li>' . __( 'Are you sure that the database server is running?' ) . "</li>\n";
+			$message .= '<li>' . __( 'Are you sure you have typed the correct hostname?' ) . "</li>\n";
+			$message .= '<li>' . __( 'Are you sure the database server is running?' ) . "</li>\n";
 			$message .= "</ul>\n";
 
 			$message .= '<p>' . sprintf(
@@ -1827,8 +1827,8 @@ class wpdb {
 		) . "</p>\n";
 
 		$message .= "<ul>\n";
-		$message .= '<li>' . __( 'Are you sure that the database server is running?' ) . "</li>\n";
-		$message .= '<li>' . __( 'Are you sure that the database server is not under particularly heavy load?' ) . "</li>\n";
+		$message .= '<li>' . __( 'Are you sure the database server is running?' ) . "</li>\n";
+		$message .= '<li>' . __( 'Are you sure the database server is not under particularly heavy load?' ) . "</li>\n";
 		$message .= "</ul>\n";
 
 		$message .= '<p>' . sprintf(
