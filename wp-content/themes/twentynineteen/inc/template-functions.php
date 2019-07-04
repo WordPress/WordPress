d1815a5c919dd53405e532a473bc82cedaac9e8c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-07-04 09:15:58 +0000

    Twenty Nineteen: Adjust markup in `twentynineteen_add_ellipses_to_nav()` for better readability.
    See #47067.
    Built from https://develop.svn.wordpress.org/trunk@45595
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45406 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentynineteen/inc/template-functions.php
+++ wp-content/themes/twentynineteen/inc/template-functions.php
@@ -202,24 +202,25 @@ function twentynineteen_add_ellipses_to_nav( $nav_menu, $args ) {
 
 	if ( 'menu-1' === $args->theme_location ) :
 
-		$nav_menu .= '<div class="main-menu-more">';
-		$nav_menu .= '<ul class="main-menu">';
-		$nav_menu .= '<li class="menu-item menu-item-has-children">';
-		$nav_menu .= '<button class="submenu-expand main-menu-more-toggle is-empty" tabindex="-1" aria-label="More" aria-haspopup="true" aria-expanded="false">';
-		$nav_menu .= '<span class="screen-reader-text">' . esc_html__( 'More', 'twentynineteen' ) . '</span>';
-		$nav_menu .= twentynineteen_get_icon_svg( 'arrow_drop_down_ellipsis' );
-		$nav_menu .= '</button>';
-		$nav_menu .= '<ul class="sub-menu hidden-links">';
-		$nav_menu .= '<li id="menu-item--1" class="mobile-parent-nav-menu-item menu-item--1">';
-		$nav_menu .= '<button class="menu-item-link-return">';
-		$nav_menu .= twentynineteen_get_icon_svg( 'chevron_left' );
-		$nav_menu .= esc_html__( 'Back', 'twentynineteen' );
-		$nav_menu .= '</button>';
-		$nav_menu .= '</li>';
-		$nav_menu .= '</ul>';
-		$nav_menu .= '</li>';
-		$nav_menu .= '</ul>';
-		$nav_menu .= '</div>';
+		$nav_menu .= '
+			<div class="main-menu-more">
+				<ul class="main-menu">
+					<li class="menu-item menu-item-has-children">
+						<button class="submenu-expand main-menu-more-toggle is-empty" tabindex="-1" aria-label="More" aria-haspopup="true" aria-expanded="false">
+							<span class="screen-reader-text">' . esc_html__( 'More', 'twentynineteen' ) . '</span>' .
+							twentynineteen_get_icon_svg( 'arrow_drop_down_ellipsis' ) . '
+						</button>
+						<ul class="sub-menu hidden-links">
+							<li id="menu-item--1" class="mobile-parent-nav-menu-item menu-item--1">
+								<button class="menu-item-link-return">' .
+									twentynineteen_get_icon_svg( 'chevron_left' ) .
+									esc_html__( 'Back', 'twentynineteen' ) . '
+								</button>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>';
 
 	endif;
 
