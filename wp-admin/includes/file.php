4be25f3d06756a550d8c426bcb760cf5609d97d8
Gary Pendergast <gary@pento.net>
2019-07-09 06:29:57 +0000

    Coding Standards: Fix instances of `WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase`.
    See #46732.
    
    Built from https://develop.svn.wordpress.org/trunk@45612
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45423 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/file.php
+++ wp-admin/includes/file.php
@@ -1209,10 +1209,12 @@ function verify_file_signature( $filename, $signatures, $filename_for_errors = f
 		// Allow for an old version of Sodium_Compat being loaded before the bundled WordPress one.
 		if ( method_exists( 'ParagonIE_Sodium_Compat', 'runtime_speed_test' ) ) {
 			// Run `ParagonIE_Sodium_Compat::runtime_speed_test()` in optimized integer mode, as that's what WordPress utilises during signing verifications.
+			// phpcs:disable WordPress.NamingConventions.ValidVariableName
 			$old_fastMult                      = ParagonIE_Sodium_Compat::$fastMult;
 			ParagonIE_Sodium_Compat::$fastMult = true;
 			$sodium_compat_is_fast             = ParagonIE_Sodium_Compat::runtime_speed_test( 100, 10 );
 			ParagonIE_Sodium_Compat::$fastMult = $old_fastMult;
+			// phpcs:enable
 		}
 
 		// This cannot be performed in a reasonable amount of time
