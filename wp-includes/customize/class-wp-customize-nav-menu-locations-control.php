ace077085dc5ee03bd057e415b90a1d0bd28263b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-07-25 22:56:55 +0000

    I18N: Update translator comment missed in [45676].
    See #47771.
    Built from https://develop.svn.wordpress.org/trunk@45677
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45488 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/customize/class-wp-customize-nav-menu-locations-control.php
+++ wp-includes/customize/class-wp-customize-nav-menu-locations-control.php
@@ -49,7 +49,7 @@ class WP_Customize_Nav_Menu_Locations_Control extends WP_Customize_Control {
 							<em class="new-menu-locations-widget-note">
 								<?php
 								printf(
-									/* translators: 1: Codex URL, 2: additional link attributes, 3: accessibility text */
+									/* translators: 1: Documentation URL, 2: additional link attributes, 3: accessibility text */
 									_x( '(If you plan to use a menu <a href="%1$s" %2$s>widget%3$s</a>, skip this step.)', 'menu locations' ),
 									__( 'https://wordpress.org/support/article/wordpress-widgets/' ),
 									' class="external-link" target="_blank"',
