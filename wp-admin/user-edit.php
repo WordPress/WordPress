98642b48214ab447cdc27b4980762b1ed5fe1679
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-07-28 17:42:55 +0000

    Users: Use case-insensitive email address comparison when checking whether "Grant this user super admin privileges" checkbox should be displayed on Edit User screen.
    Props trepmal.
    Fixes #47119.
    Built from https://develop.svn.wordpress.org/trunk@45698
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45509 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/user-edit.php
+++ wp-admin/user-edit.php
@@ -417,7 +417,7 @@ endif; //!IS_PROFILE_PAGE
 			?>
 <tr class="user-super-admin-wrap"><th><?php _e( 'Super Admin' ); ?></th>
 <td>
-			<?php if ( $profileuser->user_email != get_site_option( 'admin_email' ) || ! is_super_admin( $profileuser->ID ) ) : ?>
+			<?php if ( 0 !== strcasecmp( $profileuser->user_email, get_site_option( 'admin_email' ) ) || ! is_super_admin( $profileuser->ID ) ) : ?>
 <p><label><input type="checkbox" id="super_admin" name="super_admin"<?php checked( is_super_admin( $profileuser->ID ) ); ?> /> <?php _e( 'Grant this user super admin privileges for the Network.' ); ?></label></p>
 <?php else : ?>
 <p><?php _e( 'Super admin privileges cannot be removed because this user has the network admin email.' ); ?></p>
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45697';
+$wp_version = '5.3-alpha-45698';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
