441a3c97e0d6ca2710cf3b075296c1b20b91fc49
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-08-09 02:57:59 +0000

    Twenty Nineteen: Correct license URI.
    Props munyagu, shital-patel.
    Fixes #47850.
    Built from https://develop.svn.wordpress.org/trunk@45773
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45584 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45772';
+$wp_version = '5.3-alpha-45773';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
