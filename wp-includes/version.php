6e060079ae5c74e6f7748cd8c43a1fb6b10d40be
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-08-09 13:53:56 +0000

    Twenty Sixteen: Correct HelpHub URL in `readme.txt`.
    See #47771.
    Built from https://develop.svn.wordpress.org/trunk@45776
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45587 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45775';
+$wp_version = '5.3-alpha-45776';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
