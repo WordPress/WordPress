4cf729f5a978ac2ae95108d6513abcdf492ba47c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-08-13 23:35:56 +0000

    Site Health: Simplify the language in Status Health Info page introduction, add a link to Status page.
    Props birgire, mukesh27, subrataemfluence, leogermani.
    Fixes #46946.
    Built from https://develop.svn.wordpress.org/trunk@45791
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45602 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/site-health-info.php
+++ wp-admin/site-health-info.php
@@ -82,7 +82,10 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
 	</h2>
 
 	<p>
-		<?php _e( 'This page can show you every detail about the configuration of your WordPress website. If we see anything here that could be improved, we will let you know on the Site Health Status page.' ); ?>
+		<?php
+			/* translators: %s: URL to Site Health Status page */
+			printf( __( 'This page can show you every detail about the configuration of your WordPress website. For any improvements that could be made, see the <a href="%s">Site Health Status</a> page.' ), esc_url( admin_url( 'site-health.php' ) ) );
+		?>
 	</p>
 	<p>
 		<?php _e( 'If you want to export a handy list of all the information on this page, you can use the button below to copy it to the clipboard. You can then paste it in a text file and save it to your device, or paste it in an email exchange with a support engineer or theme/plugin developer for example.' ); ?>
