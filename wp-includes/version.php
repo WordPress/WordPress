cfce1bcf9746550925e9b736ffe1d9cbafb80b4d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-08-19 04:48:53 +0000

    Twenty Seventeen: Accessibility: Add missing focus state for native audio and video embeds.
    Props kjellr, karlgroves.
    Merges [45425] to the 5.2 branch.
    Fixes #47190.
    Built from https://develop.svn.wordpress.org/branches/5.2@45846
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.2@45657 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2.3-alpha-45845';
+$wp_version = '5.2.3-alpha-45846';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
