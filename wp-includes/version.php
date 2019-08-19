e81b064b0baa421e3ad80847dbde0a83f236131d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-08-19 04:02:52 +0000

    Customize: Fix text direction for color picker in RTL.
    Props vjik, SergeyBiryukov.
    Merges [45626] to the 5.2 branch.
    Fixes #47688. See #23740.
    Built from https://develop.svn.wordpress.org/branches/5.2@45830
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.2@45641 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2.3-alpha-45829';
+$wp_version = '5.2.3-alpha-45830';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
