f6a419e564d8cc7e789f2a62a7aecc618cd8bd97
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-08-20 19:15:52 +0000

    Accessibility: Fix the `alt` attribute of the Gallery images within the Classic Editor.
    Passes the images `alt` attribute value to the Gallery template used within the Classic Editor.
    
    Props yarnboy, wpboss.
    Merges [45725] to the 5.2 branch.
    Fixes #47687.
    Built from https://develop.svn.wordpress.org/branches/5.2@45865
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.2@45676 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2.3-alpha-45864';
+$wp_version = '5.2.3-alpha-45865';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
