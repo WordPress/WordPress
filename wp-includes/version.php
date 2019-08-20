40d4037fa69ca3d49dbaa3fddd67c0c458756441
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-08-20 19:12:50 +0000

    Twenty Seventeen: Fix tabs vs. spaces in [45764].
    See #47414.
    Built from https://develop.svn.wordpress.org/trunk@45863
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45674 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45862';
+$wp_version = '5.3-alpha-45863';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
