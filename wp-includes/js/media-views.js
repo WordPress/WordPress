6b9ab1c5c518b92905c0217cefe0a2b2468a3bcf
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-08-20 22:54:55 +0000

    Docs: Update `@since` tag for new JS functions and variables introduced in [45572].
    Props garrett-eclipse.
    See #47145.
    Built from https://develop.svn.wordpress.org/trunk@45867
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45678 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/media-views.js
+++ wp-includes/js/media-views.js
@@ -4598,7 +4598,7 @@ var FocusManager = wp.media.View.extend(/** @lends wp.media.view.FocusManager.pr
 	 * in Safari 11.1 and support is spotty in other browsers. In the future we
 	 * should consider to remove this helper function and only use `aria-modal="true"`.
 	 *
-	 * @since 5.3.0
+	 * @since 5.2.3
 	 *
 	 * @param {object} visibleElement The jQuery object representing the element that should not be hidden.
 	 *
@@ -4637,7 +4637,7 @@ var FocusManager = wp.media.View.extend(/** @lends wp.media.view.FocusManager.pr
 	 * Makes visible again to assistive technologies all body children
 	 * previously hidden and stored in this.ariaHiddenElements.
 	 *
-	 * @since 5.3.0
+	 * @since 5.2.3
 	 *
 	 * @returns {void}
 	 */
@@ -4653,7 +4653,7 @@ var FocusManager = wp.media.View.extend(/** @lends wp.media.view.FocusManager.pr
 	/**
 	 * Determines if the passed element should not be hidden from assistive technologies.
 	 *
-	 * @since 5.3.0
+	 * @since 5.2.3
 	 *
 	 * @param {object} element The DOM element that should be checked.
 	 *
@@ -4678,7 +4678,7 @@ var FocusManager = wp.media.View.extend(/** @lends wp.media.view.FocusManager.pr
 	/**
 	 * Whether the body children are hidden from assistive technologies.
 	 *
-	 * @since 5.3.0
+	 * @since 5.2.3
 	 */
 	isBodyAriaHidden: false,
 
@@ -4686,7 +4686,7 @@ var FocusManager = wp.media.View.extend(/** @lends wp.media.view.FocusManager.pr
 	 * Stores an array of DOM elements that should be hidden from assistive
 	 * technologies, for example when the media modal dialog opens.
 	 *
-	 * @since 5.3.0
+	 * @since 5.2.3
 	 */
 	ariaHiddenElements: []
 });
