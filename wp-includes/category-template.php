d1ac7170dc78f3fd7512fbedd71c7329fb58e253
Boone Gorges <boonebgorges@gmail.com>
2019-08-26 15:24:55 +0000

    Taxonomy: Standardize docs for `wp_list_categories()`.
    Many of the parameters historically documented on this function are merely
    passed through to `get_categories()` and its underlying functions. In order to
    avoid unnecessary duplication of function arguments, we remove documentation of
    these parameters, and replace it with reference to the wrapped functions.
    
    Parameters that receive special treatment or are otherwise changed before
    being passed through continue to be documented as part of `wp_list_categories()`.
    
    Props audrasjb, itowhid06, birgire.
    Fixes #47896.
    Built from https://develop.svn.wordpress.org/trunk@45894
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45705 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/category-template.php
+++ wp-includes/category-template.php
@@ -470,9 +470,9 @@ function wp_dropdown_categories( $args = '' ) {
  *              optionally accept an array of values.
  *
  * @param string|array $args {
- *     Array of optional arguments.
+ *     Array of optional arguments. See get_categories(), get_terms(), and WP_Tax_Query::construct()
+ *     for full lists of arguments that can be passed in `$args`.
  *
- *     @type int          $child_of              Term ID to retrieve child terms of. See get_terms(). Default 0.
  *     @type int|array    $current_category      ID of category, or array of IDs of categories, that should get the
  *                                               'current-cat' class. Default 0.
  *     @type int          $depth                 Category depth. Used for tab indentation. Default 0.
@@ -488,15 +488,8 @@ function wp_dropdown_categories( $args = '' ) {
  *     @type string       $feed_image            URL of an image to use for the feed link. Default empty string.
  *     @type string       $feed_type             Feed type. Used to build feed link. See get_term_feed_link().
  *                                               Default empty string (default feed).
- *     @type bool|int     $hide_empty            Whether to hide categories that don't have any posts attached to them.
- *                                               Default 1.
  *     @type bool         $hide_title_if_empty   Whether to hide the `$title_li` element if there are no terms in
  *                                               the list. Default false (title will always be shown).
- *     @type bool         $hierarchical          Whether to include terms that have non-empty descendants.
- *                                               See get_terms(). Default true.
- *     @type string       $order                 Which direction to order categories. Accepts 'ASC' or 'DESC'.
- *                                               Default 'ASC'.
- *     @type string       $orderby               The column to use for ordering categories. Default 'name'.
  *     @type string       $separator             Separator between links. Default '<br />'.
  *     @type bool|int     $show_count            Whether to show how many posts are in the category. Default 0.
  *     @type string       $show_option_all       Text to display for showing all categories. Default empty string.
@@ -505,7 +498,6 @@ function wp_dropdown_categories( $args = '' ) {
  *     @type string       $style                 The style used to display the categories list. If 'list', categories
  *                                               will be output as an unordered list. If left empty or another value,
  *                                               categories will be output separated by `<br>` tags. Default 'list'.
- *     @type string       $taxonomy              Taxonomy name. Default 'category'.
  *     @type string       $title_li              Text to use for the list title `<li>` element. Pass an empty string
  *                                               to disable. Default 'Categories'.
  *     @type bool|int     $use_desc_for_title    Whether to use the category description as the title attribute.
