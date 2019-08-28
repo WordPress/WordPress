e3d4e81ca039d973831b7b6c7cb80bd6c67e4eca
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-08-28 02:20:54 +0000

    Date/Time: Correct delta comparison in `test_should_return_wp_timestamp()`.
    See #25768.
    Built from https://develop.svn.wordpress.org/trunk@45902
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45713 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45901';
+$wp_version = '5.3-alpha-45902';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
