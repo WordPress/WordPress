c9d8c89ebda158462081ace06754d16024828d12
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-08-30 19:15:56 +0000

    Twenty Nineteen: Improve compatibility with custom colors in the separator block.
    Props kjellr.
    Fixes #47811.
    Built from https://develop.svn.wordpress.org/trunk@45920
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45731 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45919';
+$wp_version = '5.3-alpha-45920';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
