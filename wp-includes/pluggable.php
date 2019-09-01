7fd6077864b0344bc74ffef71c16031d741fc204
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-09-01 13:03:55 +0000

    Docs: Improve documentation for `auth_redirect()`.
    Props atachibana.
    Fixes #47956.
    Built from https://develop.svn.wordpress.org/trunk@45924
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45735 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -1005,6 +1005,11 @@ if ( ! function_exists( 'auth_redirect' ) ) :
 	/**
 	 * Checks if a user is logged in, if not it redirects them to the login page.
 	 *
+	 * When this code is called from a page, it checks to see if the user viewing the page is logged in.
+	 * If the user is not logged in, they are redirected to the login page. The user is redirected
+	 * in such a way that, upon logging in, they will be sent directly to the page they were originally
+	 * trying to access.
+	 *
 	 * @since 1.5.0
 	 */
 	function auth_redirect() {
