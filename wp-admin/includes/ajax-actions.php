087255d6fd08ba8ffd2624d2d02814ef00d3f9c4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-09-04 16:39:41 +0000

    Escape the output in `wp_ajax_upload_attachment()`.
    Merges [45936] to the 4.1 branch.
    Props whyisjake, sstoqnov.
    Built from https://develop.svn.wordpress.org/branches/4.1@45955
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.1@45766 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/ajax-actions.php
+++ wp-admin/includes/ajax-actions.php
@@ -1847,7 +1847,7 @@ function wp_ajax_upload_attachment() {
 			'success' => false,
 			'data'    => array(
 				'message'  => __( "You don't have permission to upload files." ),
-				'filename' => $_FILES['async-upload']['name'],
+				'filename' => esc_html( $_FILES['async-upload']['name'] ),
 			)
 		) );
 
@@ -1861,7 +1861,7 @@ function wp_ajax_upload_attachment() {
 				'success' => false,
 				'data'    => array(
 					'message'  => __( "You don't have permission to attach files to this post." ),
-					'filename' => $_FILES['async-upload']['name'],
+					'filename' => esc_html( $_FILES['async-upload']['name'] ),
 				)
 			) );
 
@@ -1885,7 +1885,7 @@ function wp_ajax_upload_attachment() {
 				'success' => false,
 				'data'    => array(
 					'message'  => __( 'The uploaded file is not a valid image. Please try again.' ),
-					'filename' => $_FILES['async-upload']['name'],
+					'filename' => esc_html( $_FILES['async-upload']['name'] ),
 				)
 			) );
 
@@ -1900,7 +1900,7 @@ function wp_ajax_upload_attachment() {
 			'success' => false,
 			'data'    => array(
 				'message'  => $attachment_id->get_error_message(),
-				'filename' => $_FILES['async-upload']['name'],
+				'filename' => esc_html( $_FILES['async-upload']['name'] ),
 			)
 		) );
 
