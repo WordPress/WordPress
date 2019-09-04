62b22b86a7d11704a495cca5a4f460e81ac044f1
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-09-04 17:37:55 +0000

    Improve handling the existing `rel` attribute in `wp_rel_nofollow_callback()`.
    Props xknown, sstoqnov.
    Built from https://develop.svn.wordpress.org/trunk@45990
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45801 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45979';
+$wp_version = '5.3-alpha-45990';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
