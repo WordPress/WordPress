a3c199b473691d694992834c7eb5d451bed3d59a
desrosj <desrosj@git.wordpress.org>
2019-09-04 17:57:52 +0000

    Fix for URL sanitization in `wp_kses_bad_protocol_once()`.
    Merges [45997] to the 5.2 branch.
    
    Props irsdl, sstoqnov, whyisjake.
    Built from https://develop.svn.wordpress.org/branches/5.2@46000
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.2@45811 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2.3-RC1-45991';
+$wp_version = '5.2.3-RC1-46000';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
