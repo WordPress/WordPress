699c6001fd6afabc8060acfd3b2a663093d33727
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-09-06 06:26:56 +0000

    Upgrade/Install: Escape the `%` in `width="100%"` in a `printf()` call in `Plugin_Upgrader_Skin::after()`.
    Props afragen.
    Fixes #47989.
    Built from https://develop.svn.wordpress.org/trunk@46072
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45884 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-plugin-upgrader-skin.php
+++ wp-admin/includes/class-plugin-upgrader-skin.php
@@ -47,7 +47,7 @@ class Plugin_Upgrader_Skin extends WP_Upgrader_Skin {
 		if ( ! empty( $this->plugin ) && ! is_wp_error( $this->result ) && $this->plugin_active ) {
 			// Currently used only when JS is off for a single plugin update?
 			printf(
-				'<iframe title="%s" style="border:0;overflow:hidden" width="100%" height="170" src="%s"></iframe>',
+				'<iframe title="%s" style="border:0;overflow:hidden" width="100%%" height="170" src="%s"></iframe>',
 				esc_attr__( 'Update progress' ),
 				wp_nonce_url( 'update.php?action=activate-plugin&networkwide=' . $this->plugin_network_active . '&plugin=' . urlencode( $this->plugin ), 'activate-plugin_' . $this->plugin )
 			);
