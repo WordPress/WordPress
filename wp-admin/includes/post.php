abed60bb5111eb46b49a92dc1879ef60a94247ea
desrosj <desrosj@git.wordpress.org>
2019-09-11 14:52:56 +0000

    Editor: Add missing block data fields for blocks registered server side.
    In [44122], the `styles` and `parent` block fields were omitted on accident. This change makes it possible to reuse them on the client.
    
    Props gziolo.
    Fixes #47017.
    Built from https://develop.svn.wordpress.org/trunk@46091
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45903 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/post.php
+++ wp-admin/includes/post.php
@@ -2201,7 +2201,7 @@ function get_block_categories( $post ) {
 function get_block_editor_server_block_settings() {
 	$block_registry = WP_Block_Type_Registry::get_instance();
 	$blocks         = array();
-	$keys_to_pick   = array( 'title', 'description', 'icon', 'category', 'keywords', 'supports', 'attributes' );
+	$keys_to_pick   = array( 'title', 'description', 'icon', 'category', 'keywords', 'parent', 'supports', 'attributes', 'styles' );
 
 	foreach ( $block_registry->get_all_registered() as $block_name => $block_type ) {
 		foreach ( $keys_to_pick as $key ) {
