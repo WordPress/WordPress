8988c1ce3804f7c4687926bc3b77ddbed48fe29a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-09-14 21:35:57 +0000

    Mail: Add a unit test to make sure `wp_mail()` does not duplicate the `MIME-Version` header added automatically by PHPMailer.
    See #43542.
    Built from https://develop.svn.wordpress.org/trunk@46118
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45930 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-46117';
+$wp_version = '5.3-alpha-46118';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
