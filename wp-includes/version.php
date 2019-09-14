232d97425c4cbbfe574b52e219d246c43febd725
Aaron Jorbin <aaron@jorb.in>
2019-09-14 19:28:55 +0000

    Build/Test Tools: add php info to travis output
    Fixes #48041.
    
    Built from https://develop.svn.wordpress.org/trunk@46114
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45926 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-46113';
+$wp_version = '5.3-alpha-46114';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
