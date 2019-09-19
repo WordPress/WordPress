c38e52afe6668156362f25bd0b6d754e29b3bfbd
desrosj <desrosj@git.wordpress.org>
2019-09-19 15:01:55 +0000

    Build/Test Tools: Improvements to Composer scripts.
    Prefixing a script command with `@php ` ensures that the script runs with the same version of PHP that Composer is installed with (and not the system default).
    
    This change also updates the `phpcs` and `phpcbf` commands to use the version of PHPCS installed by Composer.
    
    The `—standard` is no longer explicitly passed to the command. By default, PHPCS will look for `phpcs.xml.dist`, which is the name of the custom standards file currently in Core.
    
    Props dingo_d, jrf.
    Fixes #47853.
    Built from https://develop.svn.wordpress.org/trunk@46187
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@45999 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-46186';
+$wp_version = '5.3-alpha-46187';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
