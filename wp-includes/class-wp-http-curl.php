9a2ca4bcf5a09b8f8f0674e87ef1448d394b5a36
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-09-20 22:23:58 +0000

    Code Modernization: Remove a workaround for `CURLOPT_PROTOCOLS` in `WP_Http_Curl::request()`.
    The `CURLOPT_PROTOCOLS` constant was introduced in PHP 5.2.10, so no longer needs a workaround.
    
    Props jrf.
    See #48074.
    Built from https://develop.svn.wordpress.org/trunk@46218
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46030 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-http-curl.php
+++ wp-includes/class-wp-http-curl.php
@@ -143,9 +143,7 @@ class WP_Http_Curl {
 		 * a bug #17490 with redirected POST requests, so handle redirections outside Curl.
 		 */
 		curl_setopt( $handle, CURLOPT_FOLLOWLOCATION, false );
-		if ( defined( 'CURLOPT_PROTOCOLS' ) ) { // PHP 5.2.10 / cURL 7.19.4
-			curl_setopt( $handle, CURLOPT_PROTOCOLS, CURLPROTO_HTTP | CURLPROTO_HTTPS );
-		}
+		curl_setopt( $handle, CURLOPT_PROTOCOLS, CURLPROTO_HTTP | CURLPROTO_HTTPS );
 
 		switch ( $parsed_args['method'] ) {
 			case 'HEAD':
