f9dbf1dd84ce6ae0acf8cc4c1d80d55d7b67dd7b
Andrea Fercia <a.fercia@gmail.com>
2019-09-23 15:49:57 +0000

    Accessibility: Improve and modernize user interface controls for better contrast. Fourth part: Media Views form controls.
    Props anevins, audrasjb, nrqsnchzm, kjellr, karmatosed.
    Fixes #47150.
    
    Built from https://develop.svn.wordpress.org/trunk@46244
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46056 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-46243';
+$wp_version = '5.3-alpha-46244';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
