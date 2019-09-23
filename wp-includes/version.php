618883f6f4bc348b0e878a59f02d96a5b713cf34
Andrea Fercia <a.fercia@gmail.com>
2019-09-23 14:43:58 +0000

    Accessibility: Improve and modernize user interface controls for better contrast. First part: buttons and links.
    - Introduces new styles for the buttons, with better contrast for borders and better focus style.
    - Introduces a new focus style for links.
    - The new styles improve consistency with the ones used in the new Block Editor (Gutenberg).
    
    Props michaelarestad, truchot, mor10, kellychoffman, adamsoucie, paaljoachim, Joen, kjellr, melchoyce, karmatosed, audrasjb, afercia.
    Fixes #34904.
    
    Built from https://develop.svn.wordpress.org/trunk@46241
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46053 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-46240';
+$wp_version = '5.3-alpha-46241';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
