2b6efccc06da2b0d1e283a70c5bf9aeba85c8cdc
desrosj <desrosj@git.wordpress.org>
2019-09-23 18:17:58 +0000

    External Libraries: Fix PHP 7.4 compatibility issues in the SimplePie library.
    See: https://github.com/simplepie/simplepie/commit/38b504969ed08903cb12718e8270263a8c93080e
    
    Props jrf, stevenkword, jorbin, desrosj.
    See #47746.
    Built from https://develop.svn.wordpress.org/trunk@46260
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46072 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/SimplePie/Parse/Date.php
+++ wp-includes/SimplePie/Parse/Date.php
@@ -542,8 +542,8 @@ class SimplePie_Parse_Date
 	 */
 	public function __construct()
 	{
-		$this->day_pcre = '(' . implode(array_keys($this->day), '|') . ')';
-		$this->month_pcre = '(' . implode(array_keys($this->month), '|') . ')';
+		$this->day_pcre = '(' . implode('|', array_keys($this->day)) . ')';
+		$this->month_pcre = '(' . implode('|', array_keys($this->month)) . ')';
 
 		static $cache;
 		if (!isset($cache[get_class($this)]))
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-46259';
+$wp_version = '5.3-alpha-46260';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
