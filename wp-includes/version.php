3c6ce558f9ca34a239a964fd5ef1dd77a840eb6b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-09-23 21:18:58 +0000

    REST API: Use delta comparison for floating date tests added in [46252] to avoid race conditions.
    See #39953.
    Built from https://develop.svn.wordpress.org/trunk@46280
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46092 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-46279';
+$wp_version = '5.3-alpha-46280';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
