6ff7ff50cd57553e6654cd1c339521abc1fa8a24
desrosj <desrosj@git.wordpress.org>
2019-09-25 14:02:57 +0000

    Build/Test Tools: Move Composer script command line directives to the PHPCS ruleset.
    Also, move all arguments up to the top of the custom ruleset to make it easier to understand the conditions the ruleset is run under.
    
    Props desrosj, jrf.
    See #46152.
    Built from https://develop.svn.wordpress.org/trunk@46291
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46103 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta1-46290';
+$wp_version = '5.3-beta1-46291';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
