f8a25a89c93af49e79873710662743ca5c4f70e4
Andrea Fercia <a.fercia@gmail.com>
2019-09-25 16:40:00 +0000

    Accessibility: Implement the new focus styles on the login screen after [46247].
    Props @kjellr.
    See #47153.
    
    Built from https://develop.svn.wordpress.org/trunk@46293
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46105 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta1-46292';
+$wp_version = '5.3-beta1-46293';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
