1d334b273ea37171b72697f999b87220c40b8989
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-09-26 20:31:58 +0000

    Tests: Correct `::assertArrayHasKey()` capitalization in [46252].
    Props dkarfa, david.binda.
    Fixes #48155.
    Built from https://develop.svn.wordpress.org/trunk@46329
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46128 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta1-46328';
+$wp_version = '5.3-beta1-46329';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
