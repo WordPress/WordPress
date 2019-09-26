a3512439284c9d167485ec4d61a86426eb943d56
whyisjake <whyisjake@git.wordpress.org>
2019-09-26 00:37:58 +0000

    General: Further phpcs cleanup.
    In [46312] and [46313] we attempted to do some phpcs cleanup. This commit cleans up three issues that were introduced in [46309].
    
    Fixes #48142.
    
    Props: whyisjake, garrett-eclipse, desrosj
    
    Built from https://develop.svn.wordpress.org/trunk@46314
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46113 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta1-46313';
+$wp_version = '5.3-beta1-46314';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
