2a8854ca9d6c39ee7ec53b8145ca1c70d0258b55
Gary Pendergast <gary@pento.net>
2019-09-26 04:58:58 +0000

    Build Tools: Pass the current uid/gid to Docker containers.
    This prepares for an upcoming change to the Docker images, which improves file permission handling.
    
    See #47767.
    
    
    Built from https://develop.svn.wordpress.org/trunk@46320
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46119 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta1-46319';
+$wp_version = '5.3-beta1-46320';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
