dd44ab5a58dbee9718943bcf429e3599aad79548
Andrew Ozz <admin@laptoptips.ca>
2019-09-28 16:20:57 +0000

    Fix the context (right-click) menu when clicking the placeholder for the post title field on the classic Edit Post screen.
    Props masummdar, audrasjb, afercia.
    Fixes #47318.
    Built from https://develop.svn.wordpress.org/trunk@46338
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46137 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta1-46337';
+$wp_version = '5.3-beta1-46338';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
