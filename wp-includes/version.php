7d667995b44607ac41b1836772375907f2237204
desrosj <desrosj@git.wordpress.org>
2019-09-30 15:59:00 +0000

    Built/Test Tools: Ignore the `node_modules` folder in Twenty Twenty.
    Ignore the `node_modules` folder in Twenty Twenty in SVN, and when running `grunt build` or `grunt jshint:themes`.
    
    See #48110.
    Built from https://develop.svn.wordpress.org/trunk@46352
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46151 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta1-46351';
+$wp_version = '5.3-beta1-46352';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
