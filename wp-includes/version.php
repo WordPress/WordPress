4b939d5a9373016641a3f0a916394f9cf77e065f
Andrea Fercia <a.fercia@gmail.com>
2019-10-01 18:01:01 +0000

    Login and Registration: Hide the Edge browser "Reveal password" native button.
    Props Clorith.
    Fixes #42888.
    
    Built from https://develop.svn.wordpress.org/trunk@46372
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46171 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta2-46371';
+$wp_version = '5.3-beta2-46372';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
