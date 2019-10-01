44c3cefb24c12fe98c81f8eecf83d5f2a4e1f1c0
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-10-01 01:19:57 +0000

    Docs: Fix typo in `wp_get_missing_image_subsizes()` DocBlock.
    Props david.binda.
    Fixes #48179.
    Built from https://develop.svn.wordpress.org/trunk@46368
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46167 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/image.php
+++ wp-admin/includes/image.php
@@ -164,7 +164,7 @@ function wp_update_image_subsizes( $attachment_id ) {
  * @since 5.3.0
  * @access private
  *
- * @param array  $saved_data    The data retirned from WP_Image_Editor after successfully saving an image.
+ * @param array  $saved_data    The data returned from WP_Image_Editor after successfully saving an image.
  * @param string $original_file Path to the original file.
  * @param array  $image_meta    The image meta data.
  * @param int    $attachment_id The attachment post ID.
