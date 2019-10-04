538e0d4fb38f7883b2bc822eb6245f6030fdfa1d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-10-04 23:29:59 +0000

    Emoji: Stop emoji replacement from happening inside CodeMirror instances, and clean up some logic in the `MutationObserver`.
    Props pento, hirofumi2012, davidbaumwald.
    Fixes #46537.
    Built from https://develop.svn.wordpress.org/trunk@46393
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46192 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/wp-emoji.js
+++ wp-includes/js/wp-emoji.js
@@ -121,6 +121,11 @@
 						while ( ii-- ) {
 							node = addedNodes[ ii ];
 
+							// Don't replace emoji inside elements that don't support it.
+							if ( ! node || node.closest( '.wp-exclude-emoji' ) || node.closest( '.CodeMirror' ) ) {
+								continue;
+							}
+
 							// Node type 3 is a TEXT_NODE.
 							if ( node.nodeType === 3 ) {
 								if ( ! node.parentNode ) {
@@ -145,14 +150,8 @@
 								node = node.parentNode;
 							}
 
-							/*
-							 * If the class name of a non-element node contains 'wp-exclude-emoji' ignore it.
-							 *
-							 * Node type 1 is an ELEMENT_NODE.
-							 */
-							if ( ! node || node.nodeType !== 1 ||
-								( node.className && typeof node.className === 'string' && node.className.indexOf( 'wp-exclude-emoji' ) !== -1 ) ) {
-
+							// Only make replacements inside Element nodes.
+							if ( node.nodeType !== 1 ) {
 								continue;
 							}
 
