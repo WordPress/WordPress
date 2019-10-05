9bc996b193f7caf70c2866c68a4c5912f6ccf932
Aaron Jorbin <aaron@jorb.in>
2019-10-05 13:49:00 +0000

    Build/Test Tools: Fork and Update `grunt-replace`
    The version of grunt replace that is bundled in core is using an outdated version of lodash that is bringing 2 low, 3 high, and 1 critical issue. This package is currently abandoned. There is a community forked version, but that is also harboring some similar security issues.
    
    This switches to a fork by @whyisjake and causes no change to the build.
    
    See #48203.
    Fixes #48217.
    Props whyisjake, netweb for testing.
    
    
    Built from https://develop.svn.wordpress.org/trunk@46403
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46202 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta2-46402';
+$wp_version = '5.3-beta2-46403';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
