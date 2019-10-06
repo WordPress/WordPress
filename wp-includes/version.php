d54df2ffb309ca669f81499ba2d3f971e217cc2f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-10-06 15:14:03 +0000

    Tests: Use a consistent group for nav menu tests.
    Fixes #48230.
    Built from https://develop.svn.wordpress.org/trunk@46414
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46212 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta2-46413';
+$wp_version = '5.3-beta2-46414';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
