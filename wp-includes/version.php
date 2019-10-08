faaf3be0b12fb64d3c30a608d8464bb3e0f58a21
desrosj <desrosj@git.wordpress.org>
2019-10-08 19:21:02 +0000

    Bundled Themes: Change the `eol-style` property to `native` for Twenty Twenty files.
    Props aazaozz, desrosj.
    See #48110.
    Built from https://develop.svn.wordpress.org/trunk@46446
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46244 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta2-46445';
+$wp_version = '5.3-beta2-46446';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
