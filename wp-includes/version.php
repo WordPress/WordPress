843c39b9bcd75cfce7deb68fc48edeb8ed65b355
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-10-08 19:07:04 +0000

    Themes: Properly align Close/Previous/Next icons in theme preview overlay.
    Props chetan200891.
    Fixes #48248.
    Built from https://develop.svn.wordpress.org/trunk@46444
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46242 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta2-46443';
+$wp_version = '5.3-beta2-46444';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
