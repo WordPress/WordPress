634242343c99c347d174e45917a291ae9fbafab7
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-10-12 03:10:03 +0000

    Users: Make sure `wp_logout()` clears current user, so that a subsequent call to `is_user_logged_in()` works as expected.
    Props sebastian.pisula, donmhico, roytanck, juliobox.
    Fixes #35488.
    Built from https://develop.svn.wordpress.org/trunk@46467
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46265 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -571,6 +571,7 @@ if ( ! function_exists( 'wp_logout' ) ) :
 	function wp_logout() {
 		wp_destroy_current_session();
 		wp_clear_auth_cookie();
+		wp_set_current_user( 0 );
 
 		/**
 		 * Fires after a user is logged-out.
