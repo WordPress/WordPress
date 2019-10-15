1740246d2bb48c7207c09e8195023b00b54cdfe8
Aaron Jorbin <aaron@jorb.in>
2019-10-15 14:42:04 +0000

    Build/Test tools use Trunk revision 2170172 of importer
    Update Travis configuration to temporarily use trunk for the importer in order for PHP 7.4 tests to pass. When the new version is released, this will be updated to point to that tag.
    
    See: #48231
    
    
    Built from https://develop.svn.wordpress.org/trunk@46542
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46339 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta3-46507';
+$wp_version = '5.3-beta3-46542';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
