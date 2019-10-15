02810533c4dd8c38241e46e872ab449402ef39c1
Andrea Fercia <a.fercia@gmail.com>
2019-10-15 17:23:04 +0000

    Accessibility: Script Loader: Remove jQuery as dependency of `admin-bar` after [46440].
    A better approach needs to be explored, as there are valid concerns for potential conflicts between different jQuery versions added by themes or plugins.
    
    See #47069.
    
    Built from https://develop.svn.wordpress.org/trunk@46550
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46347 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-beta3-46549';
+$wp_version = '5.3-beta3-46550';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
