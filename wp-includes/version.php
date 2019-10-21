b37a2b5c3b2a3d193f0638f5daf186d9aac4338b
Andrew Ozz <admin@laptoptips.ca>
2019-10-21 23:23:04 +0000

    Media: After [46237] add the same fix to the `.button-group` on the "Insert form URL" screen in the media modal.
    Props afercia, sabernhardt.
    FIxes #48087.
    Built from https://develop.svn.wordpress.org/trunk@46567
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46364 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-RC1-46566';
+$wp_version = '5.3-RC1-46567';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
