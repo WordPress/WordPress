ac3ff7eb1a1d122cffbc0bf3d5623f855b3553c2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-10-26 00:58:04 +0000

    Docs: Improve documentation for `wptexturize()`.
    Props atachibana.
    Fixes #48397.
    Built from https://develop.svn.wordpress.org/trunk@46592
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46389 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -8,7 +8,10 @@
  */
 
 /**
- * Replaces common plain text characters into formatted entities
+ * Replaces common plain text characters with formatted entities.
+ *
+ * Returns given text with transformations of quotes into smart quotes, apostrophes,
+ * dashes, ellipses, the trademark symbol, and the multiplication symbol.
  *
  * As an example,
  *
@@ -18,13 +21,13 @@
  *
  *     &#8217;cause today&#8217;s effort makes it worth tomorrow&#8217;s &#8220;holiday&#8221; &#8230;
  *
- * Code within certain html blocks are skipped.
+ * Code within certain HTML blocks are skipped.
  *
  * Do not use this function before the {@see 'init'} action hook; everything will break.
  *
  * @since 0.71
  *
- * @global array $wp_cockneyreplace Array of formatted entities for certain common phrases
+ * @global array $wp_cockneyreplace Array of formatted entities for certain common phrases.
  * @global array $shortcode_tags
  * @staticvar array  $static_characters
  * @staticvar array  $static_replacements
@@ -44,9 +47,9 @@
  * @staticvar string $open_sq_flag
  * @staticvar string $apos_flag
  *
- * @param string $text The text to be formatted
+ * @param string $text  The text to be formatted.
  * @param bool   $reset Set to true for unit testing. Translated patterns will reset.
- * @return string The string replaced with html entities
+ * @return string The string replaced with HTML entities.
  */
 function wptexturize( $text, $reset = false ) {
 	global $wp_cockneyreplace, $shortcode_tags;
