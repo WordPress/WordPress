6b9ce116345c58d87a3d3ebae39515dd7fb112dd
desrosj <desrosj@git.wordpress.org>
2019-10-29 21:16:05 +0000

    Post WordPress 5.3 RC3 version bump. Built from https://develop.svn.wordpress.org/branches/5.3@46619
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46416 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-RC3';
+$wp_version = '5.3-RC3-46619';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
