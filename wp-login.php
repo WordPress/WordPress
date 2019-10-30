e678d5bf285e75083b054809b0e5244701d4dab5
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-10-30 02:59:03 +0000

    Docs: Document `$error`, `$interim_login`, and `$action` globals in `login_header()` and `login_footer()`.
    Props priyankabehera155, faisal03, mukesh27, SergeyBiryukov.
    Fixes #45777.
    Built from https://develop.svn.wordpress.org/trunk@46620
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46417 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-login.php
+++ wp-login.php
@@ -27,6 +27,12 @@ if ( force_ssl_admin() && ! is_ssl() ) {
  *
  * @since 2.1.0
  *
+ * @global string      $error         Login error message set by deprecated pluggable wp_login() function
+ *                                    or plugins replacing it.
+ * @global bool|string $interim_login Whether interim login modal is being displayed. String 'success'
+ *                                    upon successful login.
+ * @global string      $action        The action that brought the visitor to the login page.
+ *
  * @param string   $title    Optional. WordPress login Page title to display in the `<title>` element.
  *                           Default 'Log In'.
  * @param string   $message  Optional. Message to display in header. Default empty.
@@ -268,6 +274,9 @@ function login_header( $title = 'Log In', $message = '', $wp_error = null ) {
  * Outputs the footer for the login page.
  *
  * @since 3.1.0
+ *
+ * @global bool|string $interim_login Whether interim login modal is being displayed. String 'success'
+ *                                    upon successful login.
  *
  * @param string $input_id Which input to auto-focus.
  */
