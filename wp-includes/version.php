6cdb9d0a3730759d834e5c49baab00e59eed45b8
jorgefilipecosta <jorgefilipecosta@git.wordpress.org>
2019-11-05 19:36:07 +0000

    Block Editor: Bug fixes targeted for WordPress 5.3 RC4.
    The list of included fixes is:
    
    https://github.com/WordPress/gutenberg/pull/18183
    https://github.com/WordPress/gutenberg/pull/18194
    https://github.com/WordPress/gutenberg/pull/18230
    https://github.com/WordPress/gutenberg/pull/18275
    https://github.com/WordPress/gutenberg/pull/18287
    
    Updated packages:
     - @wordpress/block-editor@3.2.4
     - @wordpress/block-library@2.9.5
     - @wordpress/edit-post@3.8.5
     - @wordpress/editor@9.7.5
     - @wordpress/format-library@1.9.4
    
    Props @aduth, @mcsf, @youknowriad, @johnbillion.
    Fixes #48502.
    Built from https://develop.svn.wordpress.org/trunk@46656
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46456 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-46654';
+$wp_version = '5.4-alpha-46656';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
