46caa0bfe853863ff29f03f4314bb1821e699d9f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-11-05 23:18:02 +0000

    Coding Standards: Fix WPCS issues in [46670].
    See #48145.
    Built from https://develop.svn.wordpress.org/trunk@46671
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46471 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-46670';
+$wp_version = '5.4-alpha-46671';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
