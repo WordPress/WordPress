5e93894c98a6003f986d1501540d3e2c33e43f53
Andrew Ozz <admin@laptoptips.ca>
2019-11-08 22:58:03 +0000

    Upload: When an image was scaled because it is larger than the big image threshold, use the originally uploaded image's dimensions in `wp_get_missing_image_subsizes()`. Fixes an edge case/inconsistent behaviour when a registered image sub-size is also larger than the big image threshold.
    Props desrosj, azaozz.
    Merges [46677] to the 5.3 branch.
    Fixes #48518.
    Built from https://develop.svn.wordpress.org/branches/5.3@46680
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46480 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-RC4-46673';
+$wp_version = '5.3-RC4-46680';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
