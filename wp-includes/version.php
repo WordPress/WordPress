d804c8218d96be50d183fe311bdcafbbf977150d
whyisjake <whyisjake@git.wordpress.org>
2019-11-11 16:59:02 +0000

    Comments: Check if comment form element exists before adding a key handler to detect the cmd/ctrl-enter key press.
    Brings [46700] to the 5.3 branch.
    
    Follow-up to [45790].
    Props raamdev.
    Fixes #48543. See #41545.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.3@46706
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46506 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-RC4-46704';
+$wp_version = '5.3-RC4-46706';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
