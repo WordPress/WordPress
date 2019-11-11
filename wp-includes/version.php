f542e3f870c103c93dec327ff992bead614facf2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-11-11 20:32:05 +0000

    Tests: Fix a typo in an inline comment introduced in [46568].
    Reviewed by whyisjake, SergeyBiryukov.
    Props david.binda.
    Merges [46707] to the 5.3 branch.
    Fixes #48564. See #48312.
    Built from https://develop.svn.wordpress.org/branches/5.3@46708
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46508 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-RC4-46706';
+$wp_version = '5.3-RC4-46708';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
