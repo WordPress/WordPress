27e67093f766a1f94e6604cb533369920440b5a8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-11-24 11:03:02 +0000

    Embeds: Remove CollegeHumor oEmbed provider, as the service does not exist anymore.
    Props audrasjb, youknowriad.
    Merges [46761] to the 5.3 branch.
    Fixes #48696.
    Built from https://develop.svn.wordpress.org/branches/5.3@46768
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46568 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.1-alpha-46767';
+$wp_version = '5.3.1-alpha-46768';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
