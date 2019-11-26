6f3a28818e613e7566ebe4ab9384b5e256ee8334
desrosj <desrosj@git.wordpress.org>
2019-11-26 18:43:04 +0000

    Bundled Themes: Fixes JS TypeError in Twenty Twenty.
    On mobile devices using a webkit browser, the menu and search modals could not be opened due to a `TypeError: document.body is null`. This commit fixes that issue by adding a `touch-enabled` class to the body for browsers that do not support media queries.
    
    Props quicoto, poena, Boga86, acosmin, macmanx, ianbelanger.
    Fixes #48601.
    Built from https://develop.svn.wordpress.org/branches/5.3@46787
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46587 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.1-alpha-46783';
+$wp_version = '5.3.1-alpha-46787';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
