585af17b7cb50cc8f41b2fb1aeb307714ff552ef
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-11-29 22:17:01 +0000

    Tests: Merge [24251] from `_wp_filter_build_unique_id()` to `_test_filter_build_unique_id()`, for consistency.
    See #47407.
    Built from https://develop.svn.wordpress.org/trunk@46806
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46606 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-46805';
+$wp_version = '5.4-alpha-46806';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
