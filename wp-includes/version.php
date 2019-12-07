bd3bd0c6ff464c82b1baf7f1ef271651c07e9fa0
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-07 11:08:01 +0000

    Twenty Twenty: Replace Smooth Scroll JS implementation with `scroll-behavior` CSS property.
    The JS implementation had multiple issues and did not work as expected.
    
    This change includes an accessibility enhancement by using `prefers-reduced-motion: reduce` media query property for users that don't want motion effects. For further explanation on this media query, see MDN documentation: https://developer.mozilla.org/en-US/docs/Web/CSS/scroll-behavior#Accessibility_concerns
    
    Props audrasjb, melchoyce, joostdevalk, Anlino, mauteri, sergiomdgomes, littlebigthing, williampatton, netweb, andraganescu, joyously, acosmin, mukesh27, hareesh-pillai.
    Merges [46824] to the 5.3 branch.
    Fixes #48763, #48551, #48866.
    Built from https://develop.svn.wordpress.org/branches/5.3@46825
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46625 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.1-alpha-46818';
+$wp_version = '5.3.1-alpha-46825';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
