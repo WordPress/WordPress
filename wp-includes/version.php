860fc169544e7b180bea65f038d3769db0409688
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-08 18:02:07 +0000

    Tests: Speed up comment submission unit tests by creating less fixtures and reusing them where possible.
    See #30017, #48145.
    Built from https://develop.svn.wordpress.org/trunk@46829
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46629 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-46828';
+$wp_version = '5.4-alpha-46829';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
