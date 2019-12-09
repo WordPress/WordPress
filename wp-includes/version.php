e70c88213f5a2847417b8a4a2fbb21e442de26d7
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-09 10:41:05 +0000

    Tests: Pad incrementors used in factories with leading zeros to make sure MySQL sorting works as expected.
    Previously, tests could create sequential objects with names like `Term 99`, `Term 100`, which by default would be returned in reverse order when sorted by name alphabetically, causing seemingly random test failures that are extremely hard to debug.
    
    See #48145.
    Built from https://develop.svn.wordpress.org/trunk@46830
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46630 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-46829';
+$wp_version = '5.4-alpha-46830';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
