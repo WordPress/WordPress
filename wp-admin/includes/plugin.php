34876111290fcd78642b8853eb17c4a51bfb62c8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-09 21:06:03 +0000

    Menus: Avoid a PHP warning in `add_submenu_page()` when the same value is passed for both `$parent_slug` and `$menu_slug` parameters.
    Props welcher, ispreview, ayeshrajans, NextScripts, adamsilverstein, garrett-eclipse, 123host.
    Merges [46868] to the 5.3 branch.
    Fixes #48599.
    Built from https://develop.svn.wordpress.org/branches/5.3@46869
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46669 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/plugin.php
+++ wp-admin/includes/plugin.php
@@ -1388,8 +1388,9 @@ function add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, 
 
 		$submenu[ $parent_slug ][] = $new_sub_menu;
 	} else {
-		// If position is equal or higher than the number of items in the array, append the submenu.
-		if ( $position >= count( $submenu[ $parent_slug ] ) ) {
+		// Append the submenu if the parent item is not present in the submenu,
+		// or if position is equal or higher than the number of items in the array.
+		if ( ! isset( $submenu[ $parent_slug ] ) || $position >= count( $submenu[ $parent_slug ] ) ) {
 			$submenu[ $parent_slug ][] = $new_sub_menu;
 		} else {
 			// Test for a negative position.
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.1-alpha-46867';
+$wp_version = '5.3.1-alpha-46869';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
