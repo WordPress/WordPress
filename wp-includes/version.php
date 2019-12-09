7e5d62f1950df11ec4cf93ede18757ee1cc964a3
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-09 14:32:03 +0000

    Twenty Twenty: Make checkbox in comment form larger.
    This makes the checkbox in the comment form larger and more consistent with other checkboxes.
    
    Props kharisblank, audrasjb, ianbelanger.
    Merges [46844] to the 5.3 branch.
    Fixes #48652.
    Built from https://develop.svn.wordpress.org/branches/5.3@46847
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46647 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.1-alpha-46846';
+$wp_version = '5.3.1-alpha-46847';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
