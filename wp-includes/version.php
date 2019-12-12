58f8f500d356aed863f7b95f53adbdc919b3c0da
whyisjake <whyisjake@git.wordpress.org>
2019-12-12 18:13:03 +0000

    Update `wp_kses_bad_protocol()` to recognize `&colon;` on uri attributes,
    `wp_kses_bad_protocol()` makes sure to validate that uri attributes don’t contain invalid/or not allowed protocols. While this works fine in most cases, there’s a risk that by using the colon html5 named entity, one is able to bypass this function.
    
    Brings r46895 to the 5.3 branch.
    
    Props: xknown, nickdaugherty, peterwilsoncc.
    
    Built from https://develop.svn.wordpress.org/branches/5.3@46899
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46699 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.1-RC2-46898';
+$wp_version = '5.3.1-RC2-46899';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
