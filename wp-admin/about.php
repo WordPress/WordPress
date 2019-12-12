e4e88c57ce969bfb9caae556c641e24970db7326
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-12 20:35:22 +0000

    WordPress 3.8.32 Built from https://develop.svn.wordpress.org/branches/3.8@46935
    git-svn-id: http://core.svn.wordpress.org/branches/3.8@46735 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -44,6 +44,26 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 		<?php
 		printf(
 			/* translators: %s: WordPress version number */
+			__( '<strong>Version %s</strong> addressed one security issue.' ),
+			'3.8.32'
+		);
+		?>
+		<?php
+		printf(
+			/* translators: %s: HelpHub URL */
+			__( 'For more information, see <a href="%s">the release notes</a>.' ),
+			sprintf(
+				/* translators: %s: WordPress version */
+				esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+				sanitize_title( '3.8.32' )
+			)
+		);
+		?>
+	</p>
+	<p>
+		<?php
+		printf(
+			/* translators: %s: WordPress version number */
 			__( '<strong>Version %s</strong> addressed some security issues.' ),
 			'3.8.31'
 		);
