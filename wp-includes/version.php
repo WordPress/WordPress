8221d6d32063d2167da437eb6bba4512a84ae6bb
whyisjake <whyisjake@git.wordpress.org>
2019-12-12 18:11:01 +0000

    Prevent  stored XSS through wp_targeted_link_rel().
    Brings r46894 to the 5.3 branch.
    
    Props: vortfu, whyisjake, peterwilsoncc, xknown,  SergeyBiryukov, flaviozavan.
    
    Built from https://develop.svn.wordpress.org/branches/5.3@46898
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46698 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.1-RC2-46897';
+$wp_version = '5.3.1-RC2-46898';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
