5e48e39d477201808ccc9be4df3d59fd71db5d32
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-12 20:34:14 +0000

    WordPress 4.0.29 Built from https://develop.svn.wordpress.org/branches/4.0@46933
    git-svn-id: http://core.svn.wordpress.org/branches/4.0@46733 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -47,6 +47,26 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 		<?php
 		printf(
 			/* translators: %s: WordPress version number */
+			__( '<strong>Version %s</strong> addressed one security issue.' ),
+			'4.0.29'
+		);
+		?>
+		<?php
+		printf(
+			/* translators: %s: HelpHub URL */
+			__( 'For more information, see <a href="%s">the release notes</a>.' ),
+			sprintf(
+				/* translators: %s: WordPress version */
+				esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+				sanitize_title( '4.0.29' )
+			)
+		);
+		?>
+	</p>
+	<p>
+		<?php
+		printf(
+			/* translators: %s: WordPress version number */
 			__( '<strong>Version %s</strong> addressed some security issues.' ),
 			'4.0.28'
 		);
