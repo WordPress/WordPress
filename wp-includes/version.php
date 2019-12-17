18b0cd4f7de0fe273aea90d35a450f829b833e14
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-17 14:37:02 +0000

    Date/Time: Add a unit test for [46968].
    See #48145.
    Built from https://develop.svn.wordpress.org/trunk@46969
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46769 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-46968';
+$wp_version = '5.4-alpha-46969';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
