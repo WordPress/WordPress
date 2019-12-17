6f6975efdeaca8ce77772fe441dec55b133c9192
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-17 22:25:03 +0000

    Post WordPress 5.3.2 RC1 version bump Built from https://develop.svn.wordpress.org/branches/5.3@46984
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46784 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.2-RC1';
+$wp_version = '5.3.2-RC1-46984';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
