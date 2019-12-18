9525d1c9f75248a52f5eb8495725989bbb744d63
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-18 16:02:03 +0000

    Help/About: Update the About page for 5.3.2.
    Props audrasjb.
    Fixes #49019.
    Built from https://develop.svn.wordpress.org/branches/5.3@46992
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46792 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -56,6 +56,31 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 					printf(
 						/* translators: 1: WordPress version number, 2: plural number of bugs. More than one security issue. */
 						_n(
+							'<strong>Version %1$s</strong> addressed %2$s bug.',
+							'<strong>Version %1$s</strong> addressed %2$s bugs.',
+							5
+						),
+						'5.3.2',
+						number_format_i18n( 5 )
+					);
+					?>
+					<?php
+					printf(
+						/* translators: %s: HelpHub URL */
+						__( 'For more information, see <a href="%s">the release notes</a>.' ),
+						sprintf(
+							/* translators: %s: WordPress version */
+							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							sanitize_title( '5.3.2' )
+						)
+					);
+					?>
+				</p>
+				<p>
+					<?php
+					printf(
+						/* translators: 1: WordPress version number, 2: plural number of bugs. More than one security issue. */
+						_n(
 							'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bug.',
 							'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bugs.',
 							46
