6abeb1579136a1246dac33826ae487ac32db2b3a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-18 22:12:02 +0000

    WordPress 5.3.2 Built from https://develop.svn.wordpress.org/branches/5.3@46993
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46793 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.2-RC1-46992';
+$wp_version = '5.3.2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
