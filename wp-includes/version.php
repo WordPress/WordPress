5d5bf01d12f7707c3844e3142b18e0c479f019e1
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-19 15:45:05 +0000

    Tests: In `test_readme()`, if the HTTP request to `secure.php.net` or `dev.mysql.com` failed, include response code in the error message.
    See #40533, #44613.
    Built from https://develop.svn.wordpress.org/trunk@46996
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46796 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-46991';
+$wp_version = '5.4-alpha-46996';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
