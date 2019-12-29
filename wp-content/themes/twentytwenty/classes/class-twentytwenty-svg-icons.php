7590296984f0b77b37f59f45261c099c59f7f64f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2019-12-29 20:45:06 +0000

    Twenty Twenty: Remove extra space from `arrow-down-circled` icon name in `TwentyTwenty_SVG_Icons::$ui_icons`.
    Props kitchin.
    Fixes #49091.
    Built from https://develop.svn.wordpress.org/trunk@47019
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46819 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwenty/classes/class-twentytwenty-svg-icons.php
+++ wp-content/themes/twentytwenty/classes/class-twentytwenty-svg-icons.php
@@ -77,7 +77,7 @@ if ( ! class_exists( 'TwentyTwenty_SVG_Icons' ) ) {
 			'arrow-down'          => '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="24" viewBox="0 0 22 24">
 		<polygon fill="#FFF" points="721.105 856 721.105 874.315 728.083 867.313 730.204 869.41 719.59 880 709 869.41 711.074 867.313 718.076 874.315 718.076 856" transform="translate(-709 -856)"/>
 		</svg>',
-			'arrow-down-circled ' => '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+			'arrow-down-circled'  => '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
 		<path fill="#FFF" fill-rule="evenodd" d="M16,32 C7.163444,32 0,24.836556 0,16 C0,7.163444 7.163444,0 16,0 C24.836556,0 32,7.163444 32,16 C32,24.836556 24.836556,32 16,32 Z M16.7934656,8 L15.4886113,8 L15.4886113,21.5300971 L10.082786,16.1242718 L9.18181515,17.0407767 L16.1410384,24 L23.1157957,17.0407767 L22.1915239,16.1242718 L16.7934656,21.5300971 L16.7934656,8 Z"/>
 		</svg>',
 			'bookmark'            => '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="18" viewBox="0 0 15 18">
