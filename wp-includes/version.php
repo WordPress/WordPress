0f47f2b9e59e93d7a2510c531fec9e79e5678ae3
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-01-17 13:57:03 +0000

    Widgets: Remove extra padding from number inputs in Recent Posts and Recent Comments widgets.
    Props marcio-zebedeu, audrasjb, SergeyBiryukov.
    Fixes #49195.
    Built from https://develop.svn.wordpress.org/trunk@47082
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46882 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-47081';
+$wp_version = '5.4-alpha-47082';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
