d5be8f06351198771b8b81fa60f0e9ca83e11fa6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-01-20 14:08:05 +0000

    I18N: Correct translator comments in `WP_Users_List_Table::get_views()` to provide information about each placeholder.
    Props ramiy.
    Fixes #49242.
    Built from https://develop.svn.wordpress.org/trunk@47089
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46889 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-47088';
+$wp_version = '5.4-alpha-47089';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
