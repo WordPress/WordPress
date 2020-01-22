eb513c5ac8ea64063ee1e5f347056cc5f5cb0952
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-01-22 00:22:03 +0000

    Docs: Adjust `@return` value of `parent_dropdown()` for consistency with other dropdown functions.
    See #48303.
    Built from https://develop.svn.wordpress.org/trunk@47100
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46900 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/template.php
+++ wp-admin/includes/template.php
@@ -887,7 +887,8 @@ function page_template_dropdown( $default = '', $post_type = 'page' ) {
  * @param int         $level   Optional. Page depth level. Default 0.
  * @param int|WP_Post $post    Post ID or WP_Post object.
  *
- * @return null|false Boolean False if page has no children, otherwise print out html elements.
+ * @return void|false HTML dropdown list of page parents on success,
+ *                    false if the page has no children.
  */
 function parent_dropdown( $default = 0, $parent = 0, $level = 0, $post = null ) {
 	global $wpdb;
