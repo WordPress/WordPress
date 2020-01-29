b1e2b6174d27f698a685d81dd829f659fa9ea525
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-01-29 16:10:02 +0000

    Media: After [46375], enable JavaScript translations for the `media-views` script.
    Props ocean90, audrasjb.
    Merges [47040] to the 5.3 branch.
    Fixes #49134.
    Built from https://develop.svn.wordpress.org/branches/5.3@47126
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46926 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -1621,6 +1621,7 @@ function wp_default_scripts( &$scripts ) {
 	// To enqueue media-views or media-editor, call wp_enqueue_media().
 	// Both rely on numerous settings, styles, and templates to operate correctly.
 	$scripts->add( 'media-views', "/wp-includes/js/media-views$suffix.js", array( 'utils', 'media-models', 'wp-plupload', 'jquery-ui-sortable', 'wp-mediaelement', 'wp-api-request', 'wp-a11y', 'wp-i18n' ), false, 1 );
+	$scripts->set_translations( 'media-views' );
 	$scripts->add( 'media-editor', "/wp-includes/js/media-editor$suffix.js", array( 'shortcode', 'media-views' ), false, 1 );
 	$scripts->add( 'media-audiovideo', "/wp-includes/js/media-audiovideo$suffix.js", array( 'media-editor' ), false, 1 );
 	$scripts->add( 'mce-view', "/wp-includes/js/mce-view$suffix.js", array( 'shortcode', 'jquery', 'media-views', 'media-audiovideo' ), false, 1 );
