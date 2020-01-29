f5a8d325ee3fe89758c2448b6d04826a113610b2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-01-29 16:14:04 +0000

    File Editor: Remove extra padding on submit button for "Select plugin/theme to edit" dropdown on smaller screens.
    Props passoniate.
    Merges [47071] to the 5.3 branch.
    Fixes #49197.
    Built from https://develop.svn.wordpress.org/branches/5.3@47128
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46928 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.3-alpha-47127';
+$wp_version = '5.3.3-alpha-47128';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
