dd4a67807a5af154dc73958deb185913ef6aa001
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-01-29 15:22:05 +0000

    Build/Test Tools: Pass the `TRAVIS_BRANCH` and `TRAVIS_PULL_REQUEST` environment variables along to the Docker container.
    This ensures that `WP_UnitTestCase::skipOnAutomatedBranches()` has access to these variables.
    
    Correct the check for pull requests in `WP_UnitTestCase_Base::skipOnAutomatedBranches()`.
    
    Merges [46999], [47000], and [47001] to the 5.3 branch.
    Fixes #49050.
    Built from https://develop.svn.wordpress.org/branches/5.3@47125
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46925 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.3-alpha-46995';
+$wp_version = '5.3.3-alpha-47125';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
