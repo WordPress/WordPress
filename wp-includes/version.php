4e55b9a259ba7b08a3a6118fddff8628c30c79f2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-01-30 20:08:04 +0000

    Twenty Nineteen: Standardize the Required PHP and Tested Up To headers.
    * Remove `WordPress` from `Requires at least` headers.
    * Ensure the `Requires at least` and `Requires PHP` headers are present in the `style.css` file.
    
    Follow-up to [46676], which updated `style-rtl.css`, but not `style.scss` or `style.css`.
    
    Merges [47136] to the 5.3 branch.
    See #48517.
    Built from https://develop.svn.wordpress.org/branches/5.3@47137
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@46937 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.3-alpha-47132';
+$wp_version = '5.3.3-alpha-47137';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
