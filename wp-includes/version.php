60f5fa93675cfed900c1134b1911df6ee4b76934
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-01-31 00:16:04 +0000

    Tests: Rename `wpValidateBoolean.php` for consistency with other files.
    See #30238.
    Built from https://develop.svn.wordpress.org/trunk@47141
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@46941 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-47140';
+$wp_version = '5.4-alpha-47141';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
