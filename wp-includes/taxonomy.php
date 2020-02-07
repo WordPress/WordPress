71f099fdb01a10d408d4c94b31aeacc9f5571d20
John Blackbourn <johnbillion@git.wordpress.org>
2020-02-07 17:48:09 +0000

    Taxonomy: Clarify the docs for the return values of the `term_exists()` function.
    Props dotancohen, sathyapulse
    
    Fixes #43345
    Built from https://develop.svn.wordpress.org/trunk@47205
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47005 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/taxonomy.php
+++ wp-includes/taxonomy.php
@@ -1348,7 +1348,7 @@ function unregister_term_meta( $taxonomy, $meta_key ) {
 }
 
 /**
- * Determines whether a term exists.
+ * Determines whether a taxonomy term exists.
  *
  * Formerly is_term(), introduced in 2.3.0.
  *
@@ -1363,10 +1363,10 @@ function unregister_term_meta( $taxonomy, $meta_key ) {
  * @param int|string $term     The term to check. Accepts term ID, slug, or name.
  * @param string     $taxonomy Optional. The taxonomy name to use.
  * @param int        $parent   Optional. ID of parent term under which to confine the exists search.
- * @return mixed Returns null if the term does not exist. Returns the term ID
- *               if no taxonomy is specified and the term ID exists. Returns
- *               an array of the term ID and the term taxonomy ID if the taxonomy
- *               is specified and the pairing exists.
+ * @return mixed Returns null if the term does not exist.
+ *               Returns the term ID if no taxonomy is specified and the term ID exists.
+ *               Returns an array of the term ID and the term taxonomy ID if the taxonomy is specified and the pairing exists.
+ *               Returns 0 if term ID 0 is passed to the function.
  */
 function term_exists( $term, $taxonomy = '', $parent = null ) {
 	global $wpdb;
