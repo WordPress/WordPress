8fa658695878adf32dcbc9400461a80f05f9f76f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-02-08 05:31:07 +0000

    Tests: Add a basic test for `wp()` function.
    Props pbearne, donmhico.
    Fixes #48844.
    Built from https://develop.svn.wordpress.org/trunk@47212
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47012 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -1270,6 +1270,7 @@ function wp_remote_fopen( $uri ) {
  */
 function wp( $query_vars = '' ) {
 	global $wp, $wp_query, $wp_the_query;
+
 	$wp->main( $query_vars );
 
 	if ( ! isset( $wp_the_query ) ) {
