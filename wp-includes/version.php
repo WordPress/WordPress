7748f7082ffd28e8f7a3b439bc78ca604c866d5d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-02-08 12:49:07 +0000

    Menus: When adding a label for special pages in `wp_setup_nav_menu_item()`, check if `get_post_states()` is available, to avoid fatal errors on front end.
    Fixes #49374.
    Built from https://develop.svn.wordpress.org/trunk@47213
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47013 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-47212';
+$wp_version = '5.4-alpha-47213';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
