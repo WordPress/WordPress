961443404913ec674c2270c982091067eb8ec845
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-02-11 01:47:06 +0000

    Media: Improve the preview of transparent images in Image widget by using CSS3 to show a checkered background.
    Props bahia0019.
    Fixes #49237.
    Built from https://develop.svn.wordpress.org/trunk@47257
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47057 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-47256';
+$wp_version = '5.4-alpha-47257';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
