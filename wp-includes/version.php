40046869f39d9bba928b6527d2e6eb5beb818113
jorgefilipecosta <jorgefilipecosta@git.wordpress.org>
2020-02-11 19:18:09 +0000

    Upload: Fix Plugin and Theme upload form are misaligned.
    This commit makes sure that when uploading a plugin or theme file, the Install Now button is correctly aligned, and the button does not drops to the next line if filename is long.
    
    Props seedsca, SergeyBiryukov, audrasjb.
    Fixes #48859.
    Built from https://develop.svn.wordpress.org/trunk@47271
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47071 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-alpha-47270';
+$wp_version = '5.4-alpha-47271';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
