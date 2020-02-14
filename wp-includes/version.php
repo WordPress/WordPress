19fd7a0da93bcbab94396fb7bc2f26e56a4c1668
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-02-14 00:52:06 +0000

    Administration: Correct alignment of form controls inside custom meta boxes.
    Props audrasjb, dontdream, valentinbora.
    Fixes #49013.
    Built from https://develop.svn.wordpress.org/trunk@47289
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47089 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-beta1-47288';
+$wp_version = '5.4-beta1-47289';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
