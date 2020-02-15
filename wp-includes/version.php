f147a4003e81ceddb48c58e45b5a24c27c667677
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-02-15 02:50:08 +0000

    Widgets: Hide the "Align" legend in Image Widget display settings, since the associated controls are also hidden.
    Props audrasjb, fotisps, marcelo2605.
    Fixes #48931.
    Built from https://develop.svn.wordpress.org/trunk@47292
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47092 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-beta1-47291';
+$wp_version = '5.4-beta1-47292';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
