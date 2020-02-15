74b90a6b45b81f9cf711661b2381a124761a4617
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-02-15 05:08:05 +0000

    Themes: Correct the height of theme actions button container on various screen sizes.
    Props aftabmuni, alexischenal, ideaboxcreations, audrasjb, SergeyBiryukov.
    Fixes #49302.
    Built from https://develop.svn.wordpress.org/trunk@47293
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47093 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-beta1-47292';
+$wp_version = '5.4-beta1-47293';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
