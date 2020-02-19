c15cd5f5c43bb5e366d980d36139b5bf95d555f6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-02-19 13:45:07 +0000

    Docs: Add missing quote to `{@see 'comment_$old_status_to_$new_status'}` hook reference in `wp_transition_comment_status()` DocBlock.
    Props coffee2code.
    Fixes #49474.
    Built from https://develop.svn.wordpress.org/trunk@47324
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47118 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment.php
+++ wp-includes/comment.php
@@ -1675,12 +1675,12 @@ function wp_get_comment_status( $comment_id ) {
  *
  * Calls hooks for comment status transitions. If the new comment status is not the same
  * as the previous comment status, then two hooks will be ran, the first is
- * {@see 'transition_comment_status'} with new status, old status, and comment data. The
- * next action called is {@see comment_$old_status_to_$new_status'}. It has the
- * comment data.
+ * {@see 'transition_comment_status'} with new status, old status, and comment data.
+ * The next action called is {@see 'comment_$old_status_to_$new_status'}. It has
+ * the comment data.
  *
- * The final action will run whether or not the comment statuses are the same. The
- * action is named {@see 'comment_$new_status_$comment->comment_type'}.
+ * The final action will run whether or not the comment statuses are the same.
+ * The action is named {@see 'comment_$new_status_$comment->comment_type'}.
  *
  * @since 2.7.0
  *
