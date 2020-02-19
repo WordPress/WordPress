511a297fb4b89ad32e89ea0373347cf92367e1c3
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-02-19 04:40:05 +0000

    Tests: Correct assertions in `test_site_dates_are_gmt()`.
    `assertSame()` doesn't have the `$delta` parameter, only `assertEquals()` does.
    
    Follow-up to [47313].
    
    See #40364.
    Built from https://develop.svn.wordpress.org/trunk@47318
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47116 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-beta2-47317';
+$wp_version = '5.4-beta2-47318';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
