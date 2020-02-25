ab0cf0aa51add529b036b40b32254ebad1280800
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-02-25 21:01:06 +0000

    Privacy: Switch buttons in Next Steps column of privacy request tables to `break-word` for better legibility on various screen sizes.
    Props garrett-eclipse, afercia, nrqsnchz.
    Fixes #46304.
    Built from https://develop.svn.wordpress.org/trunk@47376
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47163 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-beta2-47375';
+$wp_version = '5.4-beta2-47376';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
