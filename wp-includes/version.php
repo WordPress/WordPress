c2bea683a297ec410d38b5fff63f6cc347d2076e
Andrew Ozz <admin@laptoptips.ca>
2020-02-26 05:15:09 +0000

    Build Tools: Do not delete the (now committed) `script-loader-packages.php` file when running `clean:js` and copy it to /build. Fixes cases when running `grunt watch` or `-dev` where that file may be deleted and not recreated.
    See #48154.
    Built from https://develop.svn.wordpress.org/trunk@47380
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47167 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-beta3-47379';
+$wp_version = '5.4-beta3-47380';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
