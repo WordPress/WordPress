7f26b262b20bc1aa991839b772857ccd52f241d6
ianbelanger <ianbelanger@git.wordpress.org>
2020-02-28 20:56:06 +0000

    Bundled Themes: Twenty Twenty searchform.php error.
    Fixes phpcs error in the searchform.php file. The variables in this file were not prefixed correctly.
    
    Props fahimmurshed.
    Fixes #49523.
    Built from https://develop.svn.wordpress.org/trunk@47393
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47180 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwenty/searchform.php
+++ wp-content/themes/twentytwenty/searchform.php
@@ -15,14 +15,14 @@
  * Generate a unique ID for each form and a string containing an aria-label
  * if one was passed to get_search_form() in the args array.
  */
-$unique_id = twentytwenty_unique_id( 'search-form-' );
+$twentytwenty_unique_id = twentytwenty_unique_id( 'search-form-' );
 
-$aria_label = ! empty( $args['label'] ) ? 'aria-label="' . esc_attr( $args['label'] ) . '"' : '';
+$twentytwenty_aria_label = ! empty( $args['label'] ) ? 'aria-label="' . esc_attr( $args['label'] ) . '"' : '';
 ?>
-<form role="search" <?php echo $aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?> method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="<?php echo esc_attr( $unique_id ); ?>">
+<form role="search" <?php echo $twentytwenty_aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?> method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label for="<?php echo esc_attr( $twentytwenty_unique_id ); ?>">
 		<span class="screen-reader-text"><?php _e( 'Search for:', 'twentytwenty' ); // phpcs:ignore: WordPress.Security.EscapeOutput.UnsafePrintingFunction -- core trusts translations ?></span>
-		<input type="search" id="<?php echo esc_attr( $unique_id ); ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentytwenty' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+		<input type="search" id="<?php echo esc_attr( $twentytwenty_unique_id ); ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentytwenty' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	</label>
 	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'twentytwenty' ); ?>" />
 </form>
