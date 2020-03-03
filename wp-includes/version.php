c30fd0b54db25559ba5893c6a154f0dc2ff38418
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-03-03 17:21:05 +0000

    Media: Improve the appearance of image editor on small and medium screens.
    This prevents the main area of Edit Media screen from being pushed down too far.
    
    Props sabernhardt, afercia, fierevere, sathyapulse, mikeschroder, johnbillion.
    Fixes #48780. See #47136.
    Built from https://develop.svn.wordpress.org/trunk@47418
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47205 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-beta3-47416';
+$wp_version = '5.4-beta3-47418';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
