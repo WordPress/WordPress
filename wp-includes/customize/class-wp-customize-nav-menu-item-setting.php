4a00ea11279faf4a814af87309592427b4143ca0
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-03-04 11:36:06 +0000

    Docs: Reorder `@throws` tags in `WP_Customize_Nav_Menu_Setting` and `WP_Customize_Nav_Menu_Item_Setting` for consistency with `WP_Customize_Custom_CSS_Setting`.
    See #49572.
    Built from https://develop.svn.wordpress.org/trunk@47428
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47215 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/customize/class-wp-customize-nav-menu-item-setting.php
+++ wp-includes/customize/class-wp-customize-nav-menu-item-setting.php
@@ -155,12 +155,12 @@ class WP_Customize_Nav_Menu_Item_Setting extends WP_Customize_Setting {
 	 *
 	 * @since 4.3.0
 	 *
+	 * @throws Exception If $id is not valid for this setting type.
+	 *
 	 * @param WP_Customize_Manager $manager Customizer bootstrap instance.
 	 * @param string               $id      A specific ID of the setting.
 	 *                                      Can be a theme mod or option name.
 	 * @param array                $args    Optional. Setting arguments.
-	 *
-	 * @throws Exception If $id is not valid for this setting type.
 	 */
 	public function __construct( WP_Customize_Manager $manager, $id, array $args = array() ) {
 		if ( empty( $manager->nav_menus ) ) {
