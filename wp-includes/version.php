9aa864f1a73860a72b83f3956c287d7e6102e284
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-03-10 20:53:06 +0000

    Bundled Themes: Update calendar widget styles for 5.4 markup to fix the alignment of a single month link.
    Follow-up to [47405].
    
    Props sabernhardt.
    Reviewed by ianbelanger, SergeyBiryukov.
    Fixes #49549.
    Built from https://develop.svn.wordpress.org/trunk@47444
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47231 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47443';
+$wp_version = '5.5-alpha-47444';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
