dacd7a55ea918c54f71d9691a29023b48c4ae779
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-03-14 16:01:08 +0000

    Toolbar: Move the logic for rendering the admin bar on `wp_footer` to `wp_admin_bar_render()`.
    Clarify in the function documentation that it is now called on `wp_body_open` action first, with `wp_footer` as a fallback.
    
    Follow-up to [47221].
    
    Fixes #47053.
    Built from https://develop.svn.wordpress.org/trunk@47455
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47242 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47454';
+$wp_version = '5.5-alpha-47455';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
