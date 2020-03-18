e94cd4e1a055c8d8827370406353367737d2eda5
Andrew Ozz <admin@laptoptips.ca>
2020-03-18 04:50:10 +0000

    Build Tools: Use the new `combinedOutputFile` setting for the updated DependencyExtractionWebpackPlugin and output `script-loader-packages.php` directly in `wp-includes/assets/`.
    Props gziolo, azaozz.
    See: #48154.
    Built from https://develop.svn.wordpress.org/trunk@47471
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47258 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47468';
+$wp_version = '5.5-alpha-47471';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
