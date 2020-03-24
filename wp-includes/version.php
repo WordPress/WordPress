e3b3b5c1b77b4934293c792a41228d49a4371fd9
Andrew Ozz <admin@laptoptips.ca>
2020-03-24 01:06:34 +0000

    Build/Test Tools: Introduce Install-changed. It keeps a hash of `package.json` and compares it when run. If it has any changes, it runs `npm install`.
    This keeps `node_modules` always updated and is useful not only when the packages have been updated, but also when the working copy has been "reverted" to an earlier revision.
    
    Fixes #49594.
    Built from https://develop.svn.wordpress.org/trunk@47497
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47272 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47496';
+$wp_version = '5.5-alpha-47497';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
