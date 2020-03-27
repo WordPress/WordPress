f4f1184debce6829fde01010c0df90c33f9163f8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-03-27 00:18:22 +0000

    Build/Test Tools: Update QUnit and Sinon to the latest versions.
    Update JS tests per the QUnit 2.x Upgrade Guide.
    
    Fixes #37117.
    Built from https://develop.svn.wordpress.org/trunk@47512
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47287 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47511';
+$wp_version = '5.5-alpha-47512';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
