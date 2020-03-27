285e5306870a5a427540ba40fb7f83329fc7dcf0
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-03-27 23:56:08 +0000

    Site Health: Remove `esc_url()` used on "Debugging in WordPress" article URL in `WP_Fatal_Error_Handler::display_default_error_template()`.
    The function may not be available in some contexts, for example if a fatal error happens in `advanced-cache.php` drop-in.
    
    Props rob006.
    Reviewed by jorgefilipecosta, SergeyBiryukov.
    Merges [47515] to the 5.4 branch.
    Fixes #49709.
    Built from https://develop.svn.wordpress.org/branches/5.4@47523
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.4@47298 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-RC4-47522';
+$wp_version = '5.4-RC4-47523';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
