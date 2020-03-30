2e7235f7ca4015f9fd9ed55b7d6aadfbebcc5953
ianbelanger <ianbelanger@git.wordpress.org>
2020-03-30 18:29:08 +0000

    Bundled Themes: Twenty Twenty Button block forces All Capital letters - revert [47531].
    Reverts [47531] to add `text-transform: uppercase;` back to button blocks.
    
    Props joyously.
    Fixes #49710.
    Built from https://develop.svn.wordpress.org/trunk@47534
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47309 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47532';
+$wp_version = '5.5-alpha-47534';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
