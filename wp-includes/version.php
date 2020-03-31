116d909f58b26f51a5a024f7968832973d758567
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-03-31 18:55:10 +0000

    Twenty Nineteen: Update `style-rtl.css` after [47339].
    Props ianbelanger.
    See #49410.
    Built from https://develop.svn.wordpress.org/trunk@47536
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47311 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47535';
+$wp_version = '5.5-alpha-47536';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
