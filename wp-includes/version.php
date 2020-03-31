f19d8ebaa71684a0ade772a294545598062ce301
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-03-31 19:00:08 +0000

    Twenty Nineteen: Update `style-editor.css` and `style-rtl.css` after running the build task.
    Follow-up to [47327], [47339].
    
    Props ianbelanger.
    Reviewed by desrosj, SergeyBiryukov.
    Merges [47536] and [47537] to the 5.4 branch.
    See #48526, #49410, #49743.
    Built from https://develop.svn.wordpress.org/branches/5.4@47538
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.4@47313 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4-RC5-47533';
+$wp_version = '5.4-RC5-47538';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
