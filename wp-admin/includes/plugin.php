441001f29fce73ab8ac2dd6e904251cf44d1fd19
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-04-03 17:23:11 +0000

    Docs: Add documentation for the `$parent` parameter of `get_admin_page_parent()`.
    Props subrataemfluence.
    Fixes #45849.
    Built from https://develop.svn.wordpress.org/trunk@47546
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47321 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/plugin.php
+++ wp-admin/includes/plugin.php
@@ -1812,15 +1812,17 @@ function menu_page_url( $menu_slug, $echo = true ) {
  * @since 1.5.0
  *
  * @global string $parent_file
- * @global array $menu
- * @global array $submenu
+ * @global array  $menu
+ * @global array  $submenu
  * @global string $pagenow
  * @global string $typenow
  * @global string $plugin_page
- * @global array $_wp_real_parent_file
- * @global array $_wp_menu_nopriv
- * @global array $_wp_submenu_nopriv
+ * @global array  $_wp_real_parent_file
+ * @global array  $_wp_menu_nopriv
+ * @global array  $_wp_submenu_nopriv
  *
+ * @param string $parent The slug name for the parent menu (or the file name of a standard
+ *                       WordPress admin page). Default empty string.
  * @return string The parent file of the current admin page.
  */
 function get_admin_page_parent( $parent = '' ) {
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47545';
+$wp_version = '5.5-alpha-47546';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
