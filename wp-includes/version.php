9f481cdf7a3efb26c88bcf66801c797c32cbd776
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-04-04 00:27:06 +0000

    Tests: Remove `test_post_thumbnails_types_true()`.
    The test incorrectly describes the behavior of `current_theme_supports( 'post-thumbnails' )` and only passes by accident.
    
    Follow-up to [30148].
    
    Fixes #49801.
    Built from https://develop.svn.wordpress.org/trunk@47548
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47323 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47547';
+$wp_version = '5.5-alpha-47548';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
