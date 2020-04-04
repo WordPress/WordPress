10e3e44219c8b92d607e6359fddc95c132417265
Andrew Ozz <admin@laptoptips.ca>
2020-04-04 22:54:09 +0000

    Media: Remove `display: none;` from the (visually hidden) `<input type="file">` button used in Plupload to select files for uploading. Fixes selecting files in Edge <= 44 and iOS Safari.
    Props treecutter, johnbillion, pbiron, isabel_brison, Ipstenu, azaozz.
    Fixes #49753 for trunk.
    Built from https://develop.svn.wordpress.org/trunk@47549
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47324 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47548';
+$wp_version = '5.5-alpha-47549';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
