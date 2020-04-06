78e62aa3e4eef1b898670f994b62aefbf728736d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-04-06 09:06:12 +0000

    Coding Standards: Whitelist `date_default_timezone_set()` usage in `wp-settings.php`.
    See #49542.
    Built from https://develop.svn.wordpress.org/trunk@47552
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47327 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-settings.php
+++ wp-settings.php
@@ -64,6 +64,7 @@ wp_initial_constants();
 wp_register_fatal_error_handler();
 
 // WordPress calculates offsets from UTC.
+// phpcs:ignore WordPress.DateTime.RestrictedFunctions.timezone_change_date_default_timezone_set
 date_default_timezone_set( 'UTC' );
 
 // Turn register_globals off.
