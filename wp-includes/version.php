c18e44d6b16c0789b4d52c64cf455161746cc9cb
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-04-08 18:33:08 +0000

    Privacy: Make the deprecated `wp_get_user_request_data()` function available on front end.
    Previously, it was accidentally made available only in admin context.
    
    Follow-up to [44606], [47245].
    
    Props garrett-eclipse, johnjamesjacoby, r-a-y, Dono12.
    Fixes #49802.
    Built from https://develop.svn.wordpress.org/trunk@47555
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47330 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47554';
+$wp_version = '5.5-alpha-47555';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
