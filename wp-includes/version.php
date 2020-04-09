2c5a618d63b19c45a8bf6d27927088711e623bdb
whyisjake <whyisjake@git.wordpress.org>
2020-04-09 22:46:07 +0000

    Privacy: Support additional elements (table, ol, ul) in privacy policy guide new styling
    The privacy policy guide supports a lot of HTML, include rules for lists, and ensure proper styling for the rest.
    
    Backports the trunk commit to the 5.4 branch.
    
    Props garrett-eclipse.
    Fixes #49772.
    
    Built from https://develop.svn.wordpress.org/branches/5.4@47561
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.4@47336 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4.1-alpha-47543';
+$wp_version = '5.4.1-alpha-47561';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
