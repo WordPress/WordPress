02310525f4e1f6d9fa16b5035a92da5024b85f2d
whyisjake <whyisjake@git.wordpress.org>
2020-04-10 03:59:06 +0000

    Site Health instantiation prevents use of some hooks by plugins.
    As the WP_Site_Health class is instantiated prior to plugins being required and the `plugins_loaded` hook being fired, it prevents plugins from using the following hooks in the functions called by `maybe_create_scheduled_event()`.
    
    Brings [47568] to the 5.4 branch.
    
    Fixes #49824.
    
    Props peterwilsoncc, whyisjake.
    
    Built from https://develop.svn.wordpress.org/branches/5.4@47569
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.4@47344 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-settings.php
+++ wp-settings.php
@@ -365,12 +365,6 @@ if ( ! is_multisite() ) {
 	wp_recovery_mode()->initialize();
 }
 
-// Create an instance of WP_Site_Health so that Cron events may fire.
-if ( ! class_exists( 'WP_Site_Health' ) ) {
-	require_once ABSPATH . 'wp-admin/includes/class-wp-site-health.php';
-}
-WP_Site_Health::get_instance();
-
 // Load active plugins.
 foreach ( wp_get_active_and_valid_plugins() as $plugin ) {
 	wp_register_plugin_realpath( $plugin );
@@ -522,6 +516,12 @@ unset( $theme );
  * @since 3.0.0
  */
 do_action( 'after_setup_theme' );
+
+// Create an instance of WP_Site_Health so that Cron events may fire.
+if ( ! class_exists( 'WP_Site_Health' ) ) {
+	require_once ABSPATH . 'wp-admin/includes/class-wp-site-health.php';
+}
+WP_Site_Health::get_instance();
 
 // Set up current user.
 $GLOBALS['wp']->init();
