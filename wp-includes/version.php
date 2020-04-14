ec3c0b493502397e69daa85b585c43f8dd73c9ec
whyisjake <whyisjake@git.wordpress.org>
2020-04-14 00:35:06 +0000

    Taxonomy: Un-depracate category_link and tag_link filters.
    Depracating these filter might have been an accident, so let's restore.
    
    Props SergeyBiryukov, audrasjb, peterwilsoncc.
    
    Fixes #49759 for the 5.4 branch.
    
    Built from https://develop.svn.wordpress.org/branches/5.4@47577
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.4@47352 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4.1-alpha-47575';
+$wp_version = '5.4.1-alpha-47577';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
