b2f072b97e8e7817d5f4b128400f4593dcc40602
ianbelanger <ianbelanger@git.wordpress.org>
2020-04-16 14:56:16 +0000

    Bundled Themes: Twenty Nineteen editor style interferes with navigation menu block.
    Update editor styles to prepare for changes coming to the editor in 5.5. Currently the issue is only present when using the latest version of the Gutenberg plugin.
    
    Props kjellr, Joen.
    Fixes #49613.
    Built from https://develop.svn.wordpress.org/trunk@47587
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47362 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47586';
+$wp_version = '5.5-alpha-47587';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
