589666abc3b21242c28dcea9f5e94b57032692e7
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-04-20 15:41:08 +0000

    External Libraries: Revert [47603].
    With the Docker image for PHP 7.4 updated to stable version, this should no longer be necessary.
    
    See https://github.com/WordPress/wpdev-docker-images/pull/26
    
    Props ocean90.
    Fixes #49945.
    Built from https://develop.svn.wordpress.org/trunk@47604
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47379 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47603';
+$wp_version = '5.5-alpha-47604';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
