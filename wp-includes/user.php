348c871b833e636e43b5fc51c495a03f45c9cceb
whyisjake <whyisjake@git.wordpress.org>
2020-04-29 16:56:12 +0000

    User: Invalidate `user_activation_key` on password update. Query: Ensure that only a single post can be returned on date/time based queries. Cache API: Ensure proper escaping around the stats method in the cache API. Formatting: Expand `sanitize_file_name` to have better support for utf8 characters.
    Brings the changes in [47634], [47635], [47637], and [47638] to the 3.9 branch.
    
    Props: batmoo, ehti, nickdaugherty, peterwilsoncc, sergeybiryukov, sstoqnov, westi, whyisjake, whyisjake, xknown.
    
    Built from https://develop.svn.wordpress.org/branches/3.9@47660
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.9@47437 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -1716,7 +1716,7 @@ function wp_insert_user( $userdata ) {
 	$data = wp_unslash( $data );
 
 	if ( $update ) {
-		if ( $user_email !== $old_user_data->user_email ) {
+		if ( $user_email !== $old_user_data->user_email || $user_pass !== $old_user_data->user_pass ) {
 			$data['user_activation_key'] = '';
 		}
 		$wpdb->update( $wpdb->users, $data, compact( 'ID' ) );
