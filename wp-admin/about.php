d99c518d40d22f253a2d923956531494d9b8d98f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-04-29 18:43:02 +0000

    Update the About page for WordPress 5.3.3 Built from https://develop.svn.wordpress.org/branches/5.3@47705
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@47482 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -54,6 +54,26 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 				<p>
 					<?php
 					printf(
+						/* translators: %s: WordPress version number */
+						__( '<strong>Version %s</strong> addressed some security issues.' ),
+						'5.3.3'
+					);
+					?>
+					<?php
+					printf(
+						/* translators: %s: HelpHub URL */
+						__( 'For more information, see <a href="%s">the release notes</a>.' ),
+						sprintf(
+							/* translators: %s: WordPress version */
+							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							sanitize_title( '5.3.3' )
+						)
+					);
+					?>
+				</p>
+				<p>
+					<?php
+					printf(
 						/* translators: 1: WordPress version number, 2: plural number of bugs. More than one security issue. */
 						_n(
 							'<strong>Version %1$s</strong> addressed %2$s bug.',
