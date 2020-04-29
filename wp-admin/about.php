b8368e4d724e06b8ecd56b33ccc7542b0e8af007
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-04-29 18:41:43 +0000

    Update the About page for WordPress 5.2.6 Built from https://develop.svn.wordpress.org/branches/5.2@47704
    git-svn-id: http://core.svn.wordpress.org/branches/5.2@47481 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -55,6 +55,26 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 				printf(
 					/* translators: %s: WordPress version number */
 					__( '<strong>Version %s</strong> addressed some security issues.' ),
+					'5.2.6'
+				);
+				?>
+				<?php
+				printf(
+					/* translators: %s: HelpHub URL */
+					__( 'For more information, see <a href="%s">the release notes</a>.' ),
+					sprintf(
+						/* translators: %s: WordPress version */
+						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+						sanitize_title( '5.2.6' )
+					)
+				);
+				?>
+			</p>
+			<p>
+				<?php
+				printf(
+					/* translators: %s: WordPress version number */
+					__( '<strong>Version %s</strong> addressed some security issues.' ),
 					'5.2.5'
 				);
 				?>
