96810af9f6391b0f9f2c54d1e15dfc9424dd829d
desrosj <desrosj@git.wordpress.org>
2020-04-29 18:04:29 +0000

    WordPress 3.7.33 Built from https://develop.svn.wordpress.org/branches/3.7@47683
    git-svn-id: http://core.svn.wordpress.org/branches/3.7@47460 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.7.32';
+$wp_version = '3.7.33';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
