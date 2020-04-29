572e7c11fe799750cd44f42763034583b2ded744
desrosj <desrosj@git.wordpress.org>
2020-04-29 20:03:10 +0000

    The 5.4 branch is now WordPress 5.4.2 alpha Built from https://develop.svn.wordpress.org/branches/5.4@47725
    git-svn-id: http://core.svn.wordpress.org/branches/5.4@47502 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4.1';
+$wp_version = '5.4.2-alpha-47725';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
