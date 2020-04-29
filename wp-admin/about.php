85df5a0e71689f79aa741589ab9818abd5cde76b
desrosj <desrosj@git.wordpress.org>
2020-04-29 18:21:17 +0000

    Updating the About page for WordPress 4.1.30 Built from https://develop.svn.wordpress.org/branches/4.1@47691
    git-svn-id: http://core.svn.wordpress.org/branches/4.1@47468 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -42,7 +42,27 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 </h2>
 
 <div class="changelog point-releases">
-	<h3><?php echo _n( 'Maintenance and Security Release', 'Maintenance and Security Releases', 28 ); ?></h3>
+	<h3><?php echo _n( 'Maintenance and Security Release', 'Maintenance and Security Releases', 30 ); ?></h3>
+	<p>
+		<?php
+		printf(
+			/* translators: %s: WordPress version number */
+			__( '<strong>Version %s</strong> addressed some security issues.' ),
+			'4.1.30'
+		);
+		?>
+		<?php
+		printf(
+			/* translators: %s: HelpHub URL */
+			__( 'For more information, see <a href="%s">the release notes</a>.' ),
+			sprintf(
+				/* translators: %s: WordPress version */
+				esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+				sanitize_title( '4.1.30' )
+			)
+		);
+		?>
+	</p>
 	<p>
 		<?php
 		printf(
