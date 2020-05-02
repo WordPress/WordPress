d29f5c14ac4457a4feb928f6d0852192ecd93a8c
John Blackbourn <johnbillion@git.wordpress.org>
2020-05-02 18:17:05 +0000

    Build/Test Tools: Exclude plugins and themes within `src`.
    Just like the `build` directory, `src` can include plugins and themes which shouldn't be linted by PHPCS.
    
    Fixes #49781
    Built from https://develop.svn.wordpress.org/trunk@47742
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47518 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47741';
+$wp_version = '5.5-alpha-47742';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
