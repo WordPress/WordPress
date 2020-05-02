7307415f6df237fb398b8b08ee458bd56dad2fa2
John Blackbourn <johnbillion@git.wordpress.org>
2020-05-02 19:16:07 +0000

    Build/Test Tools: Add more comprehensive instructions to the readme for the local development environment.
    See #50058
    Built from https://develop.svn.wordpress.org/trunk@47743
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47519 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47742';
+$wp_version = '5.5-alpha-47743';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
