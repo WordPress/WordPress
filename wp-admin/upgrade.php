9e343ccb3376946c41a63543e599b4309673e9ca
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-05-13 10:10:08 +0000

    Coding Standards: Use strict comparison in `wp-admin/upgrade.php`.
    Props mukesh27.
    Fixes #50155.
    Built from https://develop.svn.wordpress.org/trunk@47788
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47564 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/upgrade.php
+++ wp-admin/upgrade.php
@@ -67,7 +67,7 @@ header( 'Content-Type: ' . get_option( 'html_type' ) . '; charset=' . get_option
 <body class="wp-core-ui">
 <p id="logo"><a href="<?php echo esc_url( __( 'https://wordpress.org/' ) ); ?>"><?php _e( 'WordPress' ); ?></a></p>
 
-<?php if ( get_option( 'db_version' ) == $wp_db_version || ! is_blog_installed() ) : ?>
+<?php if ( (int) get_option( 'db_version' ) === $wp_db_version || ! is_blog_installed() ) : ?>
 
 <h1><?php _e( 'No Update Required' ); ?></h1>
 <p><?php _e( 'Your WordPress database is already up to date!' ); ?></p>
