618329a7504958c58b2bd9898bdd8c49d65d386e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-05-14 15:33:08 +0000

    Docs: Update Michael Heilemann's URL in a comment in `wp-includes/theme-compat/footer.php`.
    Props ritterml, mukesh27.
    Fixes #50171.
    Built from https://develop.svn.wordpress.org/trunk@47798
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47574 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/theme-compat/footer.php
+++ wp-includes/theme-compat/footer.php
@@ -32,7 +32,7 @@ _deprecated_file(
 </div>
 </div>
 
-<!-- Gorgeous design by Michael Heilemann - http://binarybonsai.com/kubrick/ -->
+<!-- Gorgeous design by Michael Heilemann - http://binarybonsai.com/ -->
 <?php /* "Just what do you think you're doing Dave?" */ ?>
 
 		<?php wp_footer(); ?>
