1e524e73228680bc4166428d7deb9039b6ac4750
TimothyBlynJacobs <TimothyBlynJacobs@git.wordpress.org>
2020-05-16 19:33:09 +0000

    REST API: Add @since entries for rest_validate_value_from_schema().
    See #49572, #48818, #44949, #50053, #48820, #49720, #42961, #44975, #43392, #38583.
    
    Built from https://develop.svn.wordpress.org/trunk@47811
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47587 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api.php
+++ wp-includes/rest-api.php
@@ -1227,6 +1227,13 @@ function rest_get_avatar_sizes() {
  * Validate a value based on a schema.
  *
  * @since 4.7.0
+ * @since 4.9.0 Support the "object" type.
+ * @since 5.2.0 Support validating "additionalProperties" against a schema.
+ * @since 5.3.0 Support multiple types.
+ * @since 5.4.0 Convert an empty string to an empty object.
+ * @since 5.5.0 Add the "uuid" and "hex-color" formats.
+ *              Support the "minLength", "maxLength" and "pattern" keywords for strings.
+ * 				Validate required properties.
  *
  * @param mixed  $value The value to validate.
  * @param array  $args  Schema array to use for validation.
