5850f5f6ef66398ee7c90cbaad72d024e17ae4cc
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-05-22 17:56:08 +0000

    Coding Standards: Fix WPCS issues in `wp-admin/includes/class-wp-site-health-auto-updates.php`.
    See #49542.
    Built from https://develop.svn.wordpress.org/trunk@47841
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47617 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-site-health-auto-updates.php
+++ wp-admin/includes/class-wp-site-health-auto-updates.php
@@ -65,7 +65,7 @@ class WP_Site_Health_Auto_Updates {
 	 * @return array The test results.
 	 */
 	public function test_constants( $constant, $value ) {
-		if ( defined( $constant ) && constant( $constant ) != $value ) {
+		if ( defined( $constant ) && constant( $constant ) !== $value ) {
 			return array(
 				'description' => sprintf(
 					/* translators: %s: Name of the constant used. */
@@ -245,7 +245,7 @@ class WP_Site_Health_Auto_Updates {
 				$check_dirs[] = $context_dir;
 
 				// Once we've hit '/' or 'C:\', we need to stop. dirname will keep returning the input here.
-				if ( dirname( $context_dir ) == $context_dir ) {
+				if ( dirname( $context_dir ) === $context_dir ) {
 					break;
 				}
 
