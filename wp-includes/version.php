e7150ae5c784bb9ddabd1e45230448b1cdf657de
whyisjake <whyisjake@git.wordpress.org>
2020-05-22 20:48:08 +0000

    REST API: Ensure proper namespacing when registering routes.
    The PR will corerce routes that have a leading slash and throwing a `_doing_it_wrong` notice while ensuring a proper namespace.
    
    Fixes #49749.
    Props TimothyBlynJacobs, skarabeq, afercia.
    
    
    Built from https://develop.svn.wordpress.org/trunk@47842
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47618 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47841';
+$wp_version = '5.5-alpha-47842';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
