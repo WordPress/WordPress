235bd98f3c7aa636c403de2bf59d1ea76c2e40cf
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-05-25 14:23:13 +0000

    Docs: Improve documentation for `add_existing_user_to_blog()`.
    Props stevenlinx.
    Fixes #50243.
    Built from https://develop.svn.wordpress.org/trunk@47854
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47630 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-functions.php
+++ wp-includes/ms-functions.php
@@ -2162,7 +2162,12 @@ function maybe_add_existing_user_to_blog() {
  *
  * @since MU (3.0.0)
  *
- * @param array $details User details.
+ * @param array $details {
+ *     User details. Must at least contain values for the keys listed below.
+ *
+ *     @type int    $user_id The ID of the user being added to the current blog.
+ *     @type string $role    The role to be assigned to the user.
+ * }
  * @return true|WP_Error|void True on success or a WP_Error object if the user doesn't exist
  *                            or could not be added. Void if $details array was not provided.
  */
