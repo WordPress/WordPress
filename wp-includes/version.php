9859bf620441e0df504beb69c29aa7da1996dbfe
whyisjake <whyisjake@git.wordpress.org>
2020-05-31 19:51:42 +0000

    Build/Test Tools: Revert some devDependency changes for 5.5.
    After bumping some major updates, tests on Travis are failing. 
    
    See: https://travis-ci.com/WordPress/wordpress-develop/builds/168839259 ##49768.
    Unprops: whyisjake.
    
    
    Built from https://develop.svn.wordpress.org/trunk@47869
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47644 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47867';
+$wp_version = '5.5-alpha-47869';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
