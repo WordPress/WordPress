e2790b4e630967548c9eb1f024f4e4f6734a0244
desrosj <desrosj@git.wordpress.org>
2020-06-03 17:03:12 +0000

    Build/Test Tools: Disable Travis CI build Slack notifications for non-official branches.
    When branches are unintentionally pushed to the GitHub `wordpress-develop` mirror by committers, a build is triggered in Travis and the result is reported in Slack if the criteria defined is met.
    
    Though this is not the desired workflow (any modifications made to the mirror are erased when the repository is synced from SVN), this can cause a lot of noise if several pushes are made and raise false alarms.
    
    This change limits builds only to the `master` branch, and branches meeting the `X.X` pattern to match each branched version.
    
    Props desrosj, jorbin, ocean90, TimothyBlynJacobs.
    Fixes #50307.
    Built from https://develop.svn.wordpress.org/trunk@47901
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47675 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47898';
+$wp_version = '5.5-alpha-47901';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
