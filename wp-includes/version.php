7e9267567f094fc9d210599f0dbdad93e26acabf
whyisjake <whyisjake@git.wordpress.org>
2020-06-10 17:21:20 +0000

    Editor: Prevent HTML decoding on by setting the proper editor context.
    Props: sstoqnov.
    
    Built from https://develop.svn.wordpress.org/trunk@47948
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47720 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47947';
+$wp_version = '5.5-alpha-47948';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
