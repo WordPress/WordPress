1bb15bafa8deb62d36ffa59a31fbdc0740da3b0f
whyisjake <whyisjake@git.wordpress.org>
2020-06-10 19:04:18 +0000

    General: Backport several commits for release.
    - Embeds: Ensure that the title attribute is set correctly on embeds.
    - Editor: Prevent HTML decoding on by setting the proper editor context.
    - Formatting: Ensure that wp_validate_redirect() sanitizes a wider variety of characters.
    - Themes: Ensure a broken theme name is returned properly.
    - Administration: Add a new filter to extend set-screen-option.
    
    Merges [47947-47951] to the 4.3 branch.
    
    Props xknown, sstoqnov, vortfu, SergeyBiryukov, whyisjake.
    
    Built from https://develop.svn.wordpress.org/branches/4.3@47982
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.3@47751 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -1231,7 +1231,7 @@ function wp_sanitize_redirect($location) {
 		){1,40}                              # ...one or more times
 		)/x';
 	$location = preg_replace_callback( $regex, '_wp_sanitize_utf8_in_redirect', $location );
-	$location = preg_replace('|[^a-z0-9-~+_.?#=&;,/:%!*\[\]()]|i', '', $location);
+	$location = preg_replace('|[^a-z0-9-~+_.?#=&;,/:%!*\[\]()@]|i', '', $location);
 	$location = wp_kses_no_null($location);
 
 	// remove %0d and %0a from location
@@ -1303,7 +1303,7 @@ if ( !function_exists('wp_validate_redirect') ) :
  * @return string redirect-sanitized URL
  **/
 function wp_validate_redirect($location, $default = '') {
-	$location = trim( $location, " \t\n\r\0\x08\x0B" );
+	$location = wp_sanitize_redirect( trim( $location, " \t\n\r\0\x08\x0B" ) );
 	// browsers will assume 'http' is your protocol, and will obey a redirect to a URL starting with '//'
 	if ( substr($location, 0, 2) == '//' )
 		$location = 'http:' . $location;
