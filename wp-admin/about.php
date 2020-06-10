916a395691b1dc63be87bc79b74ae0346b7f1206
desrosj <desrosj@git.wordpress.org>
2020-06-10 21:37:03 +0000

    WordPress 4.9.15. Built from https://develop.svn.wordpress.org/branches/4.9@47994
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@47762 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -36,17 +36,37 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 			<p>
 				<?php
 				printf(
-				/* translators: %s: WordPress version number */
+					/* translators: %s: WordPress version number */
+					__( '<strong>Version %s</strong> addressed some security issues.' ),
+					'4.9.15'
+				);
+				?>
+				<?php
+				printf(
+					/* translators: %s: HelpHub URL */
+					__( 'For more information, see <a href="%s">the release notes</a>.' ),
+					sprintf(
+						/* translators: %s: WordPress version */
+						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+						sanitize_title( '4.9.15' )
+					)
+				);
+				?>
+			</p>
+			<p>
+				<?php
+				printf(
+					/* translators: %s: WordPress version number */
 					__( '<strong>Version %s</strong> addressed some security issues.' ),
 					'4.9.14'
 				);
 				?>
 				<?php
 				printf(
-				/* translators: %s: HelpHub URL */
+					/* translators: %s: HelpHub URL */
 					__( 'For more information, see <a href="%s">the release notes</a>.' ),
 					sprintf(
-					/* translators: %s: WordPress version */
+						/* translators: %s: WordPress version */
 						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
 						sanitize_title( '4.9.14' )
 					)
@@ -56,17 +76,17 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 			<p>
 				<?php
 				printf(
-				/* translators: %s: WordPress version number */
+					/* translators: %s: WordPress version number */
 					__( '<strong>Version %s</strong> addressed some security issues.' ),
 					'4.9.13'
 				);
 				?>
 				<?php
 				printf(
-				/* translators: %s: HelpHub URL */
+					/* translators: %s: HelpHub URL */
 					__( 'For more information, see <a href="%s">the release notes</a>.' ),
 					sprintf(
-					/* translators: %s: WordPress version */
+						/* translators: %s: WordPress version */
 						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
 						sanitize_title( '4.9.13' )
 					)
@@ -76,17 +96,17 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 			<p>
 				<?php
 				printf(
-				/* translators: %s: WordPress version number */
+					/* translators: %s: WordPress version number */
 					__( '<strong>Version %s</strong> addressed some security issues.' ),
 					'4.9.12'
 				);
 				?>
 				<?php
 				printf(
-				/* translators: %s: HelpHub URL */
+					/* translators: %s: HelpHub URL */
 					__( 'For more information, see <a href="%s">the release notes</a>.' ),
 					sprintf(
-					/* translators: %s: WordPress version */
+						/* translators: %s: WordPress version */
 						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
 						sanitize_title( '4.9.12' )
 					)
@@ -96,17 +116,17 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 			<p>
 				<?php
 				printf(
-				/* translators: %s: WordPress version number */
+					/* translators: %s: WordPress version number */
 					__( '<strong>Version %s</strong> addressed some security issues.' ),
 					'4.9.11'
 				);
 				?>
 				<?php
 				printf(
-				/* translators: %s: HelpHub URL */
+					/* translators: %s: HelpHub URL */
 					__( 'For more information, see <a href="%s">the release notes</a>.' ),
 					sprintf(
-					/* translators: %s: WordPress version */
+						/* translators: %s: WordPress version */
 						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
 						sanitize_title( '4.9.11' )
 					)
