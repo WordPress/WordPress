8b8aef28342c3df3d068399817a429a61303a4be
desrosj <desrosj@git.wordpress.org>
2020-06-10 21:34:59 +0000

    WordPress 5.2.7. Built from https://develop.svn.wordpress.org/branches/5.2@47991
    git-svn-id: http://core.svn.wordpress.org/branches/5.2@47759 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -53,6 +53,31 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 			<p>
 				<?php
 				printf(
+					/* translators: 1: WordPress version number, 2: plural number of bugs. More than one security issue. */
+					_n(
+						'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bug.',
+						'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bugs.',
+						2
+					),
+					'5.2.7',
+					number_format_i18n( 2 )
+				);
+				?>
+				<?php
+				printf(
+					/* translators: %s: HelpHub URL */
+					__( 'For more information, see <a href="%s">the release notes</a>.' ),
+					sprintf(
+						/* translators: %s: WordPress version */
+						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+						sanitize_title( '5.2.7' )
+					)
+				);
+				?>
+			</p>
+			<p>
+				<?php
+				printf(
 					/* translators: %s: WordPress version number */
 					__( '<strong>Version %s</strong> addressed some security issues.' ),
 					'5.2.6'
