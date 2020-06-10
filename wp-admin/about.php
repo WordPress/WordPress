fb5af944fecf2b7841ccc0fdb568ec63e7c5a0ef
desrosj <desrosj@git.wordpress.org>
2020-06-10 21:38:34 +0000

    WordPress 4.5.22. Built from https://develop.svn.wordpress.org/branches/4.5@47998
    git-svn-id: http://core.svn.wordpress.org/branches/4.5@47766 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -45,6 +45,26 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 				printf(
 					/* translators: %s: WordPress version number */
 					__( '<strong>Version %s</strong> addressed some security issues.' ),
+					'4.5.22'
+				);
+				?>
+				<?php
+				printf(
+					/* translators: %s: HelpHub URL */
+					__( 'For more information, see <a href="%s">the release notes</a>.' ),
+					sprintf(
+						/* translators: %s: WordPress version */
+						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+						sanitize_title( '4.5.22' )
+					)
+				);
+				?>
+			</p>
+			<p>
+				<?php
+				printf(
+					/* translators: %s: WordPress version number */
+					__( '<strong>Version %s</strong> addressed some security issues.' ),
 					'4.5.21'
 				);
 				?>
@@ -110,10 +130,10 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 				?>
 				<?php
 				printf(
-				/* translators: %s: HelpHub URL */
+					/* translators: %s: HelpHub URL */
 					__( 'For more information, see <a href="%s">the release notes</a>.' ),
 					sprintf(
-					/* translators: %s: WordPress version */
+						/* translators: %s: WordPress version */
 						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
 						sanitize_title( '4.5.18' )
 					)
