10e2a50c523cf0b9785555a688d7d36a40fbeccf
whyisjake <whyisjake@git.wordpress.org>
2020-06-10 17:22:28 +0000

    Formatting: Ensure that `wp_validate_redirect()` sanitizes a wider variety of characters.
    Props: xknow, vortfu.
    
    
    Built from https://develop.svn.wordpress.org/trunk@47949
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47721 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-47948';
+$wp_version = '5.5-alpha-47949';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
