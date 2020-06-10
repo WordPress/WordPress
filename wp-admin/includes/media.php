db96ab55c2055c8429b74fdb81c1ab31a26b120c
whyisjake <whyisjake@git.wordpress.org>
2020-06-10 18:52:18 +0000

    General: Backport several commits for release. - Embeds: Ensure that the title attribute is set correctly on embeds. - Editor: Prevent HTML decoding on by setting the proper editor context. - Formatting: Ensure that wp_validate_redirect() sanitizes a wider variety of characters. - Themes: Ensure a broken theme name is returned properly. - Administration: Add a new filter to extend set-screen-option. Merges [47947-47951] to the 3.8  branch. Props xknown, sstoqnov, vortfu, SergeyBiryukov, whyisjake.
    Built from https://develop.svn.wordpress.org/branches/3.8@47965
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/3.8@47736 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/media.php
+++ wp-admin/includes/media.php
@@ -2407,7 +2407,7 @@ function edit_form_image_editor( $post ) {
 	?>
 
 	<label for="content"><strong><?php _e( 'Description' ); ?></strong></label>
-	<?php wp_editor( $post->post_content, 'attachment_content', $editor_args ); ?>
+	<?php wp_editor( format_to_edit( $post->post_content ), 'attachment_content', $editor_args ); ?>
 
 	</div>
 	<?php
