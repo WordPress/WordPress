232277586fe5f82c42947501e427741373ee0df5
ianbelanger <ianbelanger@git.wordpress.org>
2020-06-12 15:11:08 +0000

    Bundled Themes: Twenty Nineteen long submenu item titles do not wrap.
    Fixes the issue of long submenu item titles that do not wrap by adding a `max-width: 20rem;` and removing `white-space: nowrap;`.
    
    Props kjellr, mukesh27, mohsinrasool, samful.
    Fixes #45866.
    Built from https://develop.svn.wordpress.org/trunk@48032
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47799 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-48031';
+$wp_version = '5.5-alpha-48032';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
