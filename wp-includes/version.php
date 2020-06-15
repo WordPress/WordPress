b8de1549ff25d75f3c0ccaead3e5ce13a2f07b0a
desrosj <desrosj@git.wordpress.org>
2020-06-15 15:04:10 +0000

    Coding Standards: Remove the PHP Compatibility scanning job from the `allowed_failures` list.
    All pre-existing compatibility warnings and errors as flagged by the PHPCompatibilityWP ruleset have been addressed.
    
    Fixes #49922.
    Built from https://develop.svn.wordpress.org/trunk@48046
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47813 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-48045';
+$wp_version = '5.5-alpha-48046';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
