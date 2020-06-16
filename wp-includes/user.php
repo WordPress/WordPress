8fdb0728be6ec3b1f07200fbeff757179425c3ae
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-06-16 20:05:10 +0000

    Text Changes: Replace "webmaster" with "site admin" in `register_new_user()`.
    Follow-up to [48049], accidentally reverted in [48059].
    
    Props kebbet.
    See #50394.
    Built from https://develop.svn.wordpress.org/trunk@48066
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47833 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -2574,7 +2574,7 @@ function register_new_user( $user_login, $user_email ) {
 			'registerfail',
 			sprintf(
 				/* translators: %s: Admin email address. */
-				__( 'Couldn&#8217;t register you&hellip; please contact the <a href="mailto:%s">webmaster</a>!' ),
+				__( 'Couldn&#8217;t register you&hellip; please contact the <a href="mailto:%s">site admin</a>!' ),
 				get_option( 'admin_email' )
 			)
 		);
