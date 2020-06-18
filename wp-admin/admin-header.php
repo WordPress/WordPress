05e28b4d7cba7ddddbc8f9da44e83e0af8398bc8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-06-18 20:08:09 +0000

    Coding Standards: Use strict comparison for JS fragment in `wp-admin/admin-header.php`.
    Props subrataemfluence.
    Fixes #45810.
    Built from https://develop.svn.wordpress.org/trunk@48083
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47850 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/admin-header.php
+++ wp-admin/admin-header.php
@@ -81,7 +81,7 @@ wp_enqueue_script( 'svg-painter' );
 $admin_body_class = preg_replace( '/[^a-z0-9_-]+/i', '-', $hook_suffix );
 ?>
 <script type="text/javascript">
-addLoadEvent = function(func){if(typeof jQuery!="undefined")jQuery(document).ready(func);else if(typeof wpOnload!='function'){wpOnload=func;}else{var oldonload=wpOnload;wpOnload=function(){oldonload();func();}}};
+addLoadEvent = function(func){if(typeof jQuery!=='undefined')jQuery(document).ready(func);else if(typeof wpOnload!=='function'){wpOnload=func;}else{var oldonload=wpOnload;wpOnload=function(){oldonload();func();}}};
 var ajaxurl = '<?php echo admin_url( 'admin-ajax.php', 'relative' ); ?>',
 	pagenow = '<?php echo $current_screen->id; ?>',
 	typenow = '<?php echo $current_screen->post_type; ?>',
