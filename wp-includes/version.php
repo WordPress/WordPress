d61790f9dc229494ece2e502fbd0340c8765c31b
Andrew Ozz <admin@laptoptips.ca>
2020-06-18 00:46:09 +0000

    Fix empty line phpcs error.
    See #50280.
    Built from https://develop.svn.wordpress.org/trunk@48078
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47845 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-48077';
+$wp_version = '5.5-alpha-48078';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
