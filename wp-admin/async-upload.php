f51cb0a7c5532d5e255951f65654222aa5da3bbd
whyisjake <whyisjake@git.wordpress.org>
2020-06-22 23:53:09 +0000

    Media: Ensure that uploaded images get a success notification by users with `upload_files` capability.
    There was an early `wp_die` that was preventing the success notification from being sent in the upload process. 
    
    Fixes #44581.
    
    Props pbiron, mikeschroder, joemcgill, rebasaurus, whyisjake.
    
    
    Built from https://develop.svn.wordpress.org/trunk@48128
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47897 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/async-upload.php
+++ wp-admin/async-upload.php
@@ -45,9 +45,6 @@ if ( isset( $_REQUEST['attachment_id'] ) && intval( $_REQUEST['attachment_id'] )
 	if ( 'attachment' !== $post->post_type ) {
 		wp_die( __( 'Invalid post type.' ) );
 	}
-	if ( ! current_user_can( 'edit_post', $id ) ) {
-		wp_die( __( 'Sorry, you are not allowed to edit this item.' ) );
-	}
 
 	switch ( $_REQUEST['fetch'] ) {
 		case 3:
@@ -55,7 +52,11 @@ if ( isset( $_REQUEST['attachment_id'] ) && intval( $_REQUEST['attachment_id'] )
 			if ( $thumb_url ) {
 				echo '<img class="pinkynail" src="' . esc_url( $thumb_url[0] ) . '" alt="" />';
 			}
-			echo '<a class="edit-attachment" href="' . esc_url( get_edit_post_link( $id ) ) . '" target="_blank">' . _x( 'Edit', 'media item' ) . '</a>';
+			if ( current_user_can( 'edit_post', $id ) ) {
+				echo '<a class="edit-attachment" href="' . esc_url( get_edit_post_link( $id ) ) . '" target="_blank">' . _x( 'Edit', 'media item' ) . '</a>';
+			} else {
+				echo '<span class="edit-attachment">' . _x( 'Success', 'media item' ) . '</span>';
+			}
 
 			// Title shouldn't ever be empty, but use filename just in case.
 			$file  = get_attached_file( $post->ID );
