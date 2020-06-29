c6824c4330976f4f5a46ae24600344613f78986a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-06-29 23:09:05 +0000

    Tests: Remove unnecessary i18n from `_wp_update_comment_data_filter()`.
    Follow-up to [48154].
    
    See #39732.
    Built from https://develop.svn.wordpress.org/trunk@48217
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47986 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-48216';
+$wp_version = '5.5-alpha-48217';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
