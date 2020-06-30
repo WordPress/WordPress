438d16c69dbcb7017b65a442b250dfe152037de7
Andrea Fercia <a.fercia@gmail.com>
2020-06-30 14:22:03 +0000

    Fix Privacy tests after [48234]. See #50335.
    Built from https://develop.svn.wordpress.org/trunk@48236
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48005 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-48235';
+$wp_version = '5.5-alpha-48236';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
