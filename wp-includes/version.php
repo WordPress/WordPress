ca11e26edbad3b11559fc31f9d5bea21818e7627
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-06-30 00:44:04 +0000

    Docs: Remove `@return void` from some tests, per the documentation standards.
    See #49572.
    Built from https://develop.svn.wordpress.org/trunk@48221
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47990 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-48220';
+$wp_version = '5.5-alpha-48221';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
