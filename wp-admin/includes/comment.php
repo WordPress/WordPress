0a805dbee65a53b33f5af37edc279f39f9e1cdcf
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-06-30 01:04:03 +0000

    Docs: Add a `@since` note to `edit_comment()` about the new return value.
    Follow-up to [48154].
    
    See #39732.
    Built from https://develop.svn.wordpress.org/trunk@48223
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@47992 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/comment.php
+++ wp-admin/includes/comment.php
@@ -45,6 +45,10 @@ function comment_exists( $comment_author, $comment_date, $timezone = 'blog' ) {
  * Update a comment with values provided in $_POST.
  *
  * @since 2.0.0
+ * @since 5.5.0 A return value was added.
+ *
+ * @return int|WP_Error The value 1 if the comment was updated, 0 if not updated.
+ *                      A WP_Error object on failure.
  */
 function edit_comment() {
 	if ( ! current_user_can( 'edit_comment', (int) $_POST['comment_ID'] ) ) {
