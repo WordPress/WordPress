9a9352da00df986453e64a5f6198f6dc62df95cd
gziolo <gziolo@git.wordpress.org>
2020-07-01 13:10:02 +0000

    Editor: Support filtering arguments in block type registration
    Adds possibility to filter the settings of a block type during its registration.
    
    Props aduth, azaozz.
    Fixes #49615.
    
    
    Built from https://develop.svn.wordpress.org/trunk@48263
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48032 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-block-type.php
+++ wp-includes/class-wp-block-type.php
@@ -262,6 +262,17 @@ class WP_Block_Type {
 
 		$args['name'] = $this->name;
 
+		/**
+		 * Filters the arguments for registering a block type.
+		 *
+		 * @since 5.5.0
+		 *
+		 * @param array  $args       Array of arguments for registering a
+		 *                           block type.
+		 * @param string $block_type Block type name including namespace.
+		 */
+		$args = apply_filters( 'register_block_type_args', $args, $this->name );
+
 		foreach ( $args as $property_name => $property_value ) {
 			$this->$property_name = $property_value;
 		}
