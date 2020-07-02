707cb4a35b6fc63ee627002cff3ce4445d764ee2
gziolo <gziolo@git.wordpress.org>
2020-07-02 17:22:03 +0000

    Editor: Move core blocks registration to init hook
    After working on support for register_block_type_args filter in #49615, it became clear that we need to use init action for core blocks to make it possible to use this filter.
    
    Fixes #50263.
    
    
    Built from https://develop.svn.wordpress.org/trunk@48279
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48048 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks/index.php
+++ wp-includes/blocks/index.php
@@ -18,41 +18,50 @@ require ABSPATH . WPINC . '/blocks/shortcode.php';
 require ABSPATH . WPINC . '/blocks/social-link.php';
 require ABSPATH . WPINC . '/blocks/tag-cloud.php';
 
-$block_folders = array(
-	'audio',
-	'button',
-	'buttons',
-	'classic',
-	'code',
-	'column',
-	'columns',
-	'file',
-	'gallery',
-	'group',
-	'heading',
-	'html',
-	'image',
-	'list',
-	'media-text',
-	'missing',
-	'more',
-	'nextpage',
-	'paragraph',
-	'preformatted',
-	'pullquote',
-	'quote',
-	'separator',
-	'social-links',
-	'spacer',
-	'subhead',
-	'table',
-	'text-columns',
-	'verse',
-	'video',
-);
-
-foreach ( $block_folders as $block_folder ) {
-	register_block_type_from_metadata(
-		ABSPATH . WPINC . '/blocks/' . $block_folder
+/**
+ * Registers core block types using metadata files.
+ * Dynamic core blocks are registered separately.
+ *
+ * @since 5.5.0
+ */
+function register_core_block_types_from_metadata() {
+	$block_folders = array(
+		'audio',
+		'button',
+		'buttons',
+		'classic',
+		'code',
+		'column',
+		'columns',
+		'file',
+		'gallery',
+		'group',
+		'heading',
+		'html',
+		'image',
+		'list',
+		'media-text',
+		'missing',
+		'more',
+		'nextpage',
+		'paragraph',
+		'preformatted',
+		'pullquote',
+		'quote',
+		'separator',
+		'social-links',
+		'spacer',
+		'subhead',
+		'table',
+		'text-columns',
+		'verse',
+		'video',
 	);
+
+	foreach ( $block_folders as $block_folder ) {
+		register_block_type_from_metadata(
+			ABSPATH . WPINC . '/blocks/' . $block_folder
+		);
+	}
 }
+add_action( 'init', 'register_core_block_types_from_metadata' );
