25fb9a9d3a717dc7981f15f27fe35228390d7c77
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-07-02 13:10:08 +0000

    Script Loader: Restore correct versions for Masonry and imagesLoaded.
    These libraries were previously updated in [48038], but some changes were accidentally reverted in [48267].
    
    Props kebbet.
    See #50526.
    Built from https://develop.svn.wordpress.org/trunk@48276
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48045 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-48275';
+$wp_version = '5.5-alpha-48276';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
