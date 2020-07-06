ecd07ddada807d6d0975f910928afc6f334351fd
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-07-06 18:22:02 +0000

    Tests: Temporarily skip PDF tests if they fail due to ImageMagick permission errors.
    See #50573.
    Built from https://develop.svn.wordpress.org/trunk@48341
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48110 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-48340';
+$wp_version = '5.5-alpha-48341';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
