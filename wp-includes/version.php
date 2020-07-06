1fd50f7b2d4157d0bd5b8097413b2365c9cc22c4
youknowriad <youknowriad@git.wordpress.org>
2020-07-06 19:00:04 +0000

    Administration: Adapts the secondary buttons colors to the admin schemes.
    The admin schemes that don't meet the contrast guidelines are excluded from this change.
    
    Props mapk, michaelarestad, mcsf, ocean90.
    Closes #50537.
    
    Built from https://develop.svn.wordpress.org/trunk@48342
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48111 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-48341';
+$wp_version = '5.5-alpha-48342';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
