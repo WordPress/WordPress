156fcc21be243ddbd45eaaeab97531d59c08f2f4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-07-07 09:28:01 +0000

    Docs: Improve DocBlocks in `wp-includes/nav-menu-template.php` per the documentation standards.
    See #49572.
    Built from https://develop.svn.wordpress.org/trunk@48364
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48133 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/nav-menu-template.php
+++ wp-includes/nav-menu-template.php
@@ -20,14 +20,19 @@ require_once ABSPATH . WPINC . '/class-walker-nav-menu.php';
  * @param array $args {
  *     Optional. Array of nav menu arguments.
  *
- *     @type int|string|WP_Term $menu                 Desired menu. Accepts a menu ID, slug, name, or object. Default empty.
- *     @type string             $menu_class           CSS class to use for the ul element which forms the menu. Default 'menu'.
+ *     @type int|string|WP_Term $menu                 Desired menu. Accepts a menu ID, slug, name, or object.
+ *                                                    Default empty.
+ *     @type string             $menu_class           CSS class to use for the ul element which forms the menu.
+ *                                                    Default 'menu'.
  *     @type string             $menu_id              The ID that is applied to the ul element which forms the menu.
  *                                                    Default is the menu slug, incremented.
- *     @type string             $container            Whether to wrap the ul, and what to wrap it with. Default 'div'.
- *     @type string             $container_class      Class that is applied to the container. Default 'menu-{menu slug}-container'.
+ *     @type string             $container            Whether to wrap the ul, and what to wrap it with.
+ *                                                    Default 'div'.
+ *     @type string             $container_class      Class that is applied to the container.
+ *                                                    Default 'menu-{menu slug}-container'.
  *     @type string             $container_id         The ID that is applied to the container. Default empty.
- *     @type string             $container_aria_label The aria-label attribute that is applied to the container when it's a nav element. Default empty.
+ *     @type string             $container_aria_label The aria-label attribute that is applied to the container
+ *                                                    when it's a nav element. Default empty.
  *     @type callable|bool      $fallback_cb          If the menu doesn't exist, a callback function will fire.
  *                                                    Default is 'wp_page_menu'. Set to false for no fallback.
  *     @type string             $before               Text before the link markup. Default empty.
@@ -35,13 +40,16 @@ require_once ABSPATH . WPINC . '/class-walker-nav-menu.php';
  *     @type string             $link_before          Text before the link text. Default empty.
  *     @type string             $link_after           Text after the link text. Default empty.
  *     @type bool               $echo                 Whether to echo the menu or return it. Default true.
- *     @type int                $depth                How many levels of the hierarchy are to be included. 0 means all. Default 0.
+ *     @type int                $depth                How many levels of the hierarchy are to be included.
+ *                                                    0 means all. Default 0.
+ *                                                    Default 0.
  *     @type object             $walker               Instance of a custom walker class. Default empty.
- *     @type string             $theme_location       Theme location to be used. Must be registered with register_nav_menu()
- *                                                    in order to be selectable by the user.
- *     @type string             $items_wrap           How the list items should be wrapped. Default is a ul with an id and class.
- *                                                    Uses printf() format with numbered placeholders.
- *     @type string             $item_spacing         Whether to preserve whitespace within the menu's HTML. Accepts 'preserve' or 'discard'. Default 'preserve'.
+ *     @type string             $theme_location       Theme location to be used. Must be registered with
+ *                                                    register_nav_menu() in order to be selectable by the user.
+ *     @type string             $items_wrap           How the list items should be wrapped. Uses printf() format with
+ *                                                    numbered placeholders. Default is a ul with an id and class.
+ *     @type string             $item_spacing         Whether to preserve whitespace within the menu's HTML.
+ *                                                    Accepts 'preserve' or 'discard'. Default 'preserve'.
  * }
  * @return void|string|false Void if 'echo' argument is true, menu output if 'echo' is false.
  *                           False if there are no items or no menu was found.
