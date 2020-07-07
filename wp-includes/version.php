b7ed7cb184608e7b5d9b063c96c7efce83bfe3ea
Andrea Fercia <a.fercia@gmail.com>
2020-07-07 07:05:03 +0000

    Administration: Fix typo in `forms.css` after [46866].
    A stray `e` made browsers ignore a CSS ruleset preventing select elements from changing color on hover.
    
    Props ryokuhi.
    See #48420.
    Fixes #50580.
    
    Built from https://develop.svn.wordpress.org/trunk@48362
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48131 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-alpha-48361';
+$wp_version = '5.5-alpha-48362';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
