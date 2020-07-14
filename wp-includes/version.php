a386eaebf36d6ede75f11f45cf599bb19e81872e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-07-14 01:30:02 +0000

    Tests: Replace hardcoded `/tmp/` references with `get_temp_dir()`.
    This allows more tests to pass on Windows.
    
    Props danielhuesken, DJPaul, christophherr, joemcgill, netweb, davidbaumwald, SergeyBiryukov.
    Fixes #40856, #39975.
    Built from https://develop.svn.wordpress.org/trunk@48464
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48233 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-beta1-48463';
+$wp_version = '5.5-beta1-48464';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
