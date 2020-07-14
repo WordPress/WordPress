fdc8c8d784d4cf943457d84a5241a7a999965570
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-07-14 00:34:03 +0000

    Tests: Copy themes from `tests/phpunit/data` to `wp-content/themes`, instead of creating a symlink.
    This allows the theme file tests in `phpunit/tests/link/themeFile.php` to run on Windows without requiring administrative privileges.
    
    Follow-up to [42812], [42819].
    
    Props danielhuesken, christophherr, davidbaumwald, SergeyBiryukov.
    See #40856, #39975.
    Built from https://develop.svn.wordpress.org/trunk@48463
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48232 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-beta1-48462';
+$wp_version = '5.5-beta1-48463';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
