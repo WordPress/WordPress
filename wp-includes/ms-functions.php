4bfc4070b8553022ff9129d6f4fd6a11b6601855
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-07-14 11:06:08 +0000

    Docs: Improve description for `get_blog_post()`.
    Props stevenlinx.
    Fixes #50655.
    Built from https://develop.svn.wordpress.org/trunk@48467
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48236 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-functions.php
+++ wp-includes/ms-functions.php
@@ -132,13 +132,16 @@ function get_blog_count( $network_id = null ) {
 }
 
 /**
- * Get a blog post from any site on the network.
+ * Gets a blog post from any site on the network.
+ *
+ * This function is similar to get_post(), except that it can retrieve a post
+ * from any site on the network, not just the current site.
  *
  * @since MU (3.0.0)
  *
  * @param int $blog_id ID of the blog.
  * @param int $post_id ID of the post being looked for.
- * @return WP_Post|null WP_Post on success or null on failure
+ * @return WP_Post|null WP_Post object on success, null on failure
  */
 function get_blog_post( $blog_id, $post_id ) {
 	switch_to_blog( $blog_id );
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-beta1-48466';
+$wp_version = '5.5-beta1-48467';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
