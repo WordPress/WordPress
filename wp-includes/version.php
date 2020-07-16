322e38e28550554a577bc4ebd8e333400fe78ed2
whyisjake <whyisjake@git.wordpress.org>
2020-07-16 22:11:08 +0000

    Code Formatting: Fix some formatting around `WP_REST_Attachments_Controller` tests.
    See #50565.
    
    
    Built from https://develop.svn.wordpress.org/trunk@48499
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48261 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-beta2-48498';
+$wp_version = '5.5-beta2-48499';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
