c482c8e8adde72d65b0ee8e947edc3b9900c4668
desrosj <desrosj@git.wordpress.org>
2020-07-17 17:11:03 +0000

    Administration: Reverse the arrow direction for database import/export icons.
    In [48456], database import and export icons were introduced. However, the arrows were pointing in the wrong directions. This reverses the arrows to be pointing correctly based on the action described in the name.
    
    Props johnbillion, joen, desrosj, empireoflight.
    Fixes #49913.
    Built from https://develop.svn.wordpress.org/trunk@48506
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48268 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-beta2-48505';
+$wp_version = '5.5-beta2-48506';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
