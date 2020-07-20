637d9df95aeb1b62bd2cd1f43e2dd69c3329d292
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-07-20 11:54:05 +0000

    Docs: Improve description for `add_user_to_blog()`.
    Props stevenlinx.
    Fixes #50701.
    Built from https://develop.svn.wordpress.org/trunk@48511
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48273 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-functions.php
+++ wp-includes/ms-functions.php
@@ -152,7 +152,7 @@ function get_blog_post( $blog_id, $post_id ) {
 }
 
 /**
- * Adds a user to a blog.
+ * Adds a user to a blog, along with specifying the user's role.
  *
  * Use the {@see 'add_user_to_blog'} action to fire an event when users are added to a blog.
  *
@@ -160,7 +160,7 @@ function get_blog_post( $blog_id, $post_id ) {
  *
  * @param int    $blog_id ID of the blog the user is being added to.
  * @param int    $user_id ID of the user being added.
- * @param string $role    The role you want the user to have
+ * @param string $role    The role you want the user to have.
  * @return true|WP_Error True on success or a WP_Error object if the user doesn't exist
  *                       or could not be added.
  */
