3cd9474d68367d9addb03d74d1dc886ade07892f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-07-21 14:37:07 +0000

    Block Editor: Move the `add_action()` call for `_register_core_block_patterns_and_categories()` above `check_theme_switched()`.
    Follow-up to [48492], [48531].
    
    See #50669.
    Built from https://develop.svn.wordpress.org/trunk@48533
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48295 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-beta2-48532';
+$wp_version = '5.5-beta2-48533';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
