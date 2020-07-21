d1822e81ba374e5ee23f69b3755ba0b8006b2995
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-07-21 14:42:05 +0000

    Site Health: Use consistent error messages when switching plugin or theme auto-updates on or off.
    Props ramiy.
    Fixes #50721.
    Built from https://develop.svn.wordpress.org/trunk@48534
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48296 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/ajax-actions.php
+++ wp-admin/includes/ajax-actions.php
@@ -5324,7 +5324,7 @@ function wp_ajax_toggle_auto_updates() {
 	switch ( $type ) {
 		case 'plugin':
 			if ( ! current_user_can( 'update_plugins' ) ) {
-				$error_message = __( 'You do not have permission to modify plugins.' );
+				$error_message = __( 'Sorry, you are not allowed to modify plugins.' );
 				wp_send_json_error( array( 'error' => $error_message ) );
 			}
 
@@ -5334,7 +5334,7 @@ function wp_ajax_toggle_auto_updates() {
 			break;
 		case 'theme':
 			if ( ! current_user_can( 'update_themes' ) ) {
-				$error_message = __( 'You do not have permission to modify themes.' );
+				$error_message = __( 'Sorry, you are not allowed to modify themes.' );
 				wp_send_json_error( array( 'error' => $error_message ) );
 			}
 
