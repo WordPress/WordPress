9ce7bd633b90f99133ca200f97dd21a26d44e673
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-07-21 12:49:03 +0000

    Docs: Improve description for `edit_post()`.
    Props stevenlinx.
    Fixes #50713.
    Built from https://develop.svn.wordpress.org/trunk@48529
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48291 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/post.php
+++ wp-admin/includes/post.php
@@ -224,11 +224,16 @@ function _wp_get_allowed_postdata( $post_data = null ) {
 /**
  * Update an existing post with values provided in $_POST.
  *
+ * If post data is passed as an argument, it is treated as an array of data
+ * keyed appropriately for turning into a post object.
+ *
+ * If post data is not passed, the $_POST global variable is used instead.
+ *
  * @since 1.5.0
  *
  * @global wpdb $wpdb WordPress database abstraction object.
  *
- * @param array $post_data Optional.
+ * @param array $post_data Optional. Defaults to the $_POST global.
  * @return int Post ID.
  */
 function edit_post( $post_data = null ) {
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-beta2-48528';
+$wp_version = '5.5-beta2-48529';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
