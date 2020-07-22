413fae70e888652cc5581aaf25e7abfc9aa4f51c
whyisjake <whyisjake@git.wordpress.org>
2020-07-22 22:21:03 +0000

    i18n: Merge similar translation strings in wp-admin/options.php
    WARNING! is now Warning:
    
    You have been warned.
    
    Fixes #50715.
    Props: ramiy, audrasjb, ocean90.
    
    
    Built from https://develop.svn.wordpress.org/trunk@48570
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48332 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options.php
+++ wp-admin/options.php
@@ -348,7 +348,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php'; ?>
 	<h1><?php esc_html_e( 'All Settings' ); ?></h1>
 
 	<div class="notice notice-warning">
-		<p><strong><?php _e( 'WARNING!' ); ?></strong> <?php _e( 'This page allows direct access to your site settings. You can break things here. Please be cautious!' ); ?></p>
+		<p><strong><?php _e( 'Warning:' ); ?></strong> <?php _e( 'This page allows direct access to your site settings. You can break things here. Please be cautious!' ); ?></p>
 	</div>
 
 	<form name="form" action="options.php" method="post" id="all-options">
