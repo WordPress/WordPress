7913a1c7dbf31b62482b8152b079ba84b1f4257a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-07-24 01:11:02 +0000

    Docs: Improve documentation for `wp_list_comments()`.
    Note that passing `div` to the `style` parameter will result in no additional list markup.
    
    Props liuhaibin, jeremyfelt, functionalrhyme.
    Fixes #49181.
    Built from https://develop.svn.wordpress.org/trunk@48597
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48359 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment-template.php
+++ wp-includes/comment-template.php
@@ -2024,18 +2024,20 @@ function comment_form_title( $no_reply_text = false, $reply_text = false, $link_
  *
  *     @type object   $walker            Instance of a Walker class to list comments. Default null.
  *     @type int      $max_depth         The maximum comments depth. Default empty.
- *     @type string   $style             The style of list ordering. Default 'ul'. Accepts 'ul', 'ol'.
+ *     @type string   $style             The style of list ordering. Accepts 'ul', 'ol', or 'div'.
+ *                                       'div' will result in no additional list markup. Default 'ul'.
  *     @type callable $callback          Callback function to use. Default null.
  *     @type callable $end-callback      Callback function to use at the end. Default null.
- *     @type string   $type              Type of comments to list.
- *                                       Default 'all'. Accepts 'all', 'comment', 'pingback', 'trackback', 'pings'.
+ *     @type string   $type              Type of comments to list. Accepts 'all', 'comment',
+ *                                       'pingback', 'trackback', 'pings'. Default 'all'.
  *     @type int      $page              Page ID to list comments for. Default empty.
  *     @type int      $per_page          Number of comments to list per page. Default empty.
  *     @type int      $avatar_size       Height and width dimensions of the avatar size. Default 32.
- *     @type bool     $reverse_top_level Ordering of the listed comments. If true, will display newest comments first.
+ *     @type bool     $reverse_top_level Ordering of the listed comments. If true, will display
+ *                                       newest comments first. Default null.
  *     @type bool     $reverse_children  Whether to reverse child comments in the list. Default null.
- *     @type string   $format            How to format the comments list.
- *                                       Default 'html5' if the theme supports it. Accepts 'html5', 'xhtml'.
+ *     @type string   $format            How to format the comments list. Accepts 'html5', 'xhtml'.
+ *                                       Default 'html5' if the theme supports it.
  *     @type bool     $short_ping        Whether to output short pings. Default false.
  *     @type bool     $echo              Whether to echo the output or return it. Default true.
  * }
