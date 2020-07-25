d07fc084aff2ee55dcd0e76d84b917316ce5e423
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-07-25 12:31:05 +0000

    Docs: Improve description for `wp_unique_filename()`.
    Props stevenlinx.
    Fixes #50762.
    Built from https://develop.svn.wordpress.org/trunk@48606
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48368 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -2449,11 +2449,12 @@ function _wp_upload_dir( $time = null ) {
  * Get a filename that is sanitized and unique for the given directory.
  *
  * If the filename is not unique, then a number will be added to the filename
- * before the extension, and will continue adding numbers until the filename is
- * unique.
+ * before the extension, and will continue adding numbers until the filename
+ * is unique.
  *
- * The callback is passed three parameters, the first one is the directory, the
- * second is the filename, and the third is the extension.
+ * The callback function allows the caller to use their own method to create
+ * unique file names. If defined, the callback should take three arguments:
+ * - directory, base filename, and extension - and return a unique filename.
  *
  * @since 2.5.0
  *
