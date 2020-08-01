08a594aaae39a0592348ec31a403eca3bd813ba6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-01 14:13:03 +0000

    HTTP: Update the Root Certificate bundle.
    Keep 1024-bit legacy root certificates re-added in [35919], except for those already expired, for compatibility with older OpenSSL versions.
    
    Props barry, ayeshrajans, desrosj, whyisjake.
    Reviewed by whyisjake, SergeyBiryukov.
    Merges [48707] to the 5.5 branch.
    See #50828.
    Built from https://develop.svn.wordpress.org/branches/5.5@48708
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@48470 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-RC1-48706';
+$wp_version = '5.5-RC1-48708';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
