016df3b6c6a4ea2031768b39caae070435407cf2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-03 12:11:06 +0000

    Docs: Correct placement for `wp_editor_expand` filter DocBlock.
    See #50768.
    Built from https://develop.svn.wordpress.org/trunk@48714
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48476 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-form-advanced.php
+++ wp-admin/edit-form-advanced.php
@@ -53,9 +53,13 @@ $_content_editor_dfw = false;
  * @param bool   $expand    Whether to enable the 'expand' functionality. Default true.
  * @param string $post_type Post type.
  */
-if ( post_type_supports( $post_type, 'editor' ) && ! wp_is_mobile() &&
-	! ( $is_IE && preg_match( '/MSIE [5678]/', $_SERVER['HTTP_USER_AGENT'] ) ) &&
-	apply_filters( 'wp_editor_expand', true, $post_type ) ) {
+$_wp_editor_expand_enabled = apply_filters( 'wp_editor_expand', true, $post_type );
+
+if ( post_type_supports( $post_type, 'editor' )
+	&& ! wp_is_mobile()
+	&& ! ( $is_IE && preg_match( '/MSIE [5678]/', $_SERVER['HTTP_USER_AGENT'] ) )
+	&& $_wp_editor_expand_enabled
+) {
 
 	wp_enqueue_script( 'editor-expand' );
 	$_content_editor_dfw = true;
