80f9c9b8ab2e453acaa1a22e974bb4ea3731a310
whyisjake <whyisjake@git.wordpress.org>
2020-08-04 18:57:02 +0000

    WordPress 5.5 RC 2.
    Built from https://develop.svn.wordpress.org/branches/5.5@48738
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@48500 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-RC1-48737';
+$wp_version = '5.5-RC2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
