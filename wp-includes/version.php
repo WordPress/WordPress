12d6a09ff5c54239b2a618c30aff196f66c6802c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-04 17:04:05 +0000

    Editor: Make some hardcoded strings in block patterns translatable.
    Props ocean90.
    Reviewed by youknowriad, SergeyBiryukov.
    Merges [48726] to the 5.5 branch.
    Fixes #50842.
    Built from https://develop.svn.wordpress.org/branches/5.5@48727
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@48489 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-RC1-48723';
+$wp_version = '5.5-RC1-48727';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
