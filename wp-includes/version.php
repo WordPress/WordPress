d5b78890ed128db6bd2cb2c53636846f45fe1d06
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-09 02:02:02 +0000

    I18N: Fix typo in a string in `rest_validate_value_from_schema()`.
    Props pedromendonca.
    Reviewed by TimothyBlynJacobs, SergeyBiryukov.
    Merges [48767] to the 5.5 branch.
    Fixes #50879.
    Built from https://develop.svn.wordpress.org/branches/5.5@48768
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@48530 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5-RC2-48763';
+$wp_version = '5.5-RC2-48768';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
