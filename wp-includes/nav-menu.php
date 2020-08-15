756de63216d924fb57b34dfdaa4d2989672b8b97
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-15 13:12:06 +0000

    Docs: Document the parameters of `wp_get_associated_nav_menu_items()` as optional.
    See #50768.
    Built from https://develop.svn.wordpress.org/trunk@48793
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48555 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/nav-menu.php
+++ wp-includes/nav-menu.php
@@ -988,11 +988,12 @@ function wp_setup_nav_menu_item( $menu_item ) {
  *
  * @since 3.0.0
  *
- * @param int    $object_id   The ID of the original object.
- * @param string $object_type The type of object, such as 'taxonomy' or 'post_type'.
- * @param string $taxonomy    If $object_type is 'taxonomy', $taxonomy is the name of the tax
- *                            that $object_id belongs to.
- * @return int[] The array of menu item IDs; empty array if none;
+ * @param int    $object_id   Optional. The ID of the original object. Default 0.
+ * @param string $object_type Optional. The type of object, such as 'post_type' or 'taxonomy'.
+ *                            Default 'post_type'.
+ * @param string $taxonomy    Optional. If $object_type is 'taxonomy', $taxonomy is the name
+ *                            of the tax that $object_id belongs to. Default empty.
+ * @return int[] The array of menu item IDs; empty array if none.
  */
 function wp_get_associated_nav_menu_items( $object_id = 0, $object_type = 'post_type', $taxonomy = '' ) {
 	$object_id     = (int) $object_id;
