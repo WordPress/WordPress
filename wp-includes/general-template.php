26cf7ec46d64a73c9df1a0c719110092287cc081
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-18 12:05:09 +0000

    Docs: Add a `@since` note to `get_the_archive_title()` about the title part being wrapped in a `<span>` element.
    Follow-up to [48294].
    
    Props justinahinon, audrasjb.
    Fixes #51050.
    Built from https://develop.svn.wordpress.org/trunk@48814
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48576 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -1645,6 +1645,7 @@ function the_archive_title( $before = '', $after = '' ) {
  * Retrieve the archive title based on the queried object.
  *
  * @since 4.1.0
+ * @since 5.5.0 The title part is wrapped in a `<span>` element.
  *
  * @return string Archive title.
  */
