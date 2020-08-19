821c1c80952aef1a07b2d710d9ee54ff691f7d72
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-19 16:08:06 +0000

    Tests: Update unit tests to account for `comment_agent` and `comment_author_IP` values being passed to the `preprocess_comment` filter.
    See #51044.
    Built from https://develop.svn.wordpress.org/trunk@48830
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48592 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-48829';
+$wp_version = '5.6-alpha-48830';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
