2326c8196dd26034382fd04dd950d7bc72ac0054
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-19 12:31:08 +0000

    Docs: Add a `@since` note to `register_rest_route()` about the `_doing_it_wrong()` notice when the required `permission_callback` argument is not set.
    Follow-up to [48526].
    
    Props psykro.
    Fixes #51060.
    Built from https://develop.svn.wordpress.org/trunk@48827
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48589 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api.php
+++ wp-includes/rest-api.php
@@ -20,7 +20,8 @@ define( 'REST_API_VERSION', '2.0' );
  * Note: Do not use before the {@see 'rest_api_init'} hook.
  *
  * @since 4.4.0
- * @since 5.1.0 Added a _doing_it_wrong() notice when not called on or after the rest_api_init hook.
+ * @since 5.1.0 Added a `_doing_it_wrong()` notice when not called on or after the `rest_api_init` hook.
+ * @since 5.5.0 Added a `_doing_it_wrong()` notice when the required `permission_callback` argument is not set.
  *
  * @param string $namespace The first URL segment after core prefix. Should be unique to your package/plugin.
  * @param string $route     The base URL for route you are adding.
