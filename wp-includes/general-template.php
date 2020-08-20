aed2402ea900d6129bc4f3143c39a5d8fa0a1a20
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-20 10:43:05 +0000

    Docs: Update documentation for `get_custom_logo()` and `the_custom_logo()` to mention the `unlink-homepage-logo` theme feature.
    Follow-up to [48039], [48283], [48749], [48757], [48758].
    
    Props sabernhardt.
    Merges [48832] to the 5.5 branch.
    Fixes #51075.
    Built from https://develop.svn.wordpress.org/branches/5.5@48833
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@48595 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -996,10 +996,10 @@ function has_custom_logo( $blog_id = 0 ) {
 }
 
 /**
- * Returns a custom logo, linked to home when on another page.
+ * Returns a custom logo, linked to home unless the theme supports removing the link on the home page.
  *
  * @since 4.5.0
- * @since 5.5.0 Removed the link on the home page.
+ * @since 5.5.0 Added option to remove the link on the home page with `unlink-homepage-logo` theme support.
  *
  * @param int $blog_id Optional. ID of the blog in question. Default is the ID of the current blog.
  * @return string Custom logo markup.
@@ -1098,7 +1098,7 @@ function get_custom_logo( $blog_id = 0 ) {
 }
 
 /**
- * Displays a custom logo, linked to home when on another page.
+ * Displays a custom logo, linked to home unless the theme supports removing the link on the home page.
  *
  * @since 4.5.0
  *
