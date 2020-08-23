9606340d0f9ec0fadf5845db2469e049fa1f4b33
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-23 03:48:05 +0000

    Tests: Clear any previous term IDs from the metadata lazyloader queue in term meta lazy-loading test.
    This fixes random test failures when one of the term IDs from previous tests coincides with the orphan term ID from this test that should not be lazy-loaded.
    
    Follow-up to [34529], [36566].
    
    Fixes #51109.
    Built from https://develop.svn.wordpress.org/trunk@48844
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48606 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-48843';
+$wp_version = '5.6-alpha-48844';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
