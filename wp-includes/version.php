f53cc9e0c87ea95995db9c624bd8858aa85b54da
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-23 14:36:06 +0000

    General: Ensure `svn:eol-style` is consistently set for all recently added files.
    Fixes #51113.
    Built from https://develop.svn.wordpress.org/trunk@48845
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48607 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-48844';
+$wp_version = '5.6-alpha-48845';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
