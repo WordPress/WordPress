8169ae35a05305aeb79c0efd28c7aa5c657c8553
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-24 22:32:05 +0000

    Bootstrap/Load: Add `local` environment type to `wp_get_environment_type()`.
    This gives developers a better control over their existing development workflow and ensures that `local` is not the exact same as `development` if it does not need to be.
    
    Props claytoncollie, johnbillion, jeremyfelt, kreppar, dushakov, TimothyBlynJacobs, Ipstenu, khag7, knutsp, Clorith, markjaquith, joostdevalk, SergeyBiryukov.
    Fixes #51064.
    Built from https://develop.svn.wordpress.org/trunk@48856
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48618 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-48855';
+$wp_version = '5.6-alpha-48856';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
