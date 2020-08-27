5d3eb85af82cb7b93a16788627f9a6892b2b442e
desrosj <desrosj@git.wordpress.org>
2020-08-27 23:24:04 +0000

    Post WordPress 5.5.1 RC1 version bump. Built from https://develop.svn.wordpress.org/branches/5.5@48902
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@48664 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.1-RC1';
+$wp_version = '5.5.1-RC1-48902';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
