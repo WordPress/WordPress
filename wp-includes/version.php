a0096893c306630e9fc646dc31818114965d220c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-27 21:46:05 +0000

    Bootstrap/Load: Remove the ability to alter the list of environment types in `wp_get_environment_type()`.
    The intention of `wp_get_environment_type()` is to provide a consistent means of identifying the environment type, not of identifying a specific environment.
    
    Actual environments should fit within one of the existing types: `local`, `development`, `staging`, or `production`. That should cover the types that plugins and themes might be concerned about when toggling functionality. 
    
    Props johnbillion, joostdevalk, TimothyBlynJacobs, jeremyfelt, batmoo, claytoncollie, Clorith, markjaquith, garrett-eclipse, GaryJ, elrae.
    Merges [48894] and [48895] to the 5.5 branch.
    Fixes #50992.
    Built from https://develop.svn.wordpress.org/branches/5.5@48896
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@48658 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.1-alpha-48893';
+$wp_version = '5.5.1-alpha-48896';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
