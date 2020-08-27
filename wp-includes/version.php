c015f95eee29e462bc589e4c1913f6b470ee9f7b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-27 01:55:04 +0000

    Tests: Update unit tests to account for lazy-loading being disabled for custom logos by default.
    Follow-up to [48870].
    
    See #50933.
    Built from https://develop.svn.wordpress.org/trunk@48874
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48636 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-48872';
+$wp_version = '5.6-alpha-48874';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
