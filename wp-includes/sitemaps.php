b0038c85f12fe327f07f6b38878824de87745c67
Peter Wilson <wilson@peterwilson.cc>
2020-08-27 01:37:03 +0000

    Sitemaps: Prevent incorrect redirection of paged sitemap requests.
    Update `redirect_canonical()` to account for custom pagination and URL format used by sitemaps in order to follow standard practices.
    
    Introduce the function `get_sitemap_url()` to simplify getting the index and provider URLs as needed.
    
    Props jonathanstegall, pbiron, GamerZ, salvoaranzulla, peterwilsoncc.
    Merges [48872] to the 5.5 branch.
    Fixes #50910.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.5@48873
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@48635 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/sitemaps.php
+++ wp-includes/sitemaps.php
@@ -87,3 +87,39 @@ function wp_sitemaps_get_max_urls( $object_type ) {
 	 */
 	return apply_filters( 'wp_sitemaps_max_urls', 2000, $object_type );
 }
+
+/**
+ * Retrieves the full URL for a sitemap.
+ *
+ * @since 5.5.1
+ *
+ * @param string $name         The sitemap name.
+ * @param string $subtype_name The sitemap subtype name.  Default empty string.
+ * @param int    $page         The page of the sitemap.  Default 1.
+ * @return string|false The sitemap URL or false if the sitemap doesn't exist.
+ */
+function get_sitemap_url( $name, $subtype_name = '', $page = 1 ) {
+	$sitemaps = wp_sitemaps_get_server();
+	if ( ! $sitemaps ) {
+		return false;
+	}
+
+	if ( 'index' === $name ) {
+		return $sitemaps->index->get_index_url();
+	}
+
+	$provider = $sitemaps->registry->get_provider( $name );
+	if ( ! $provider ) {
+		return false;
+	}
+
+	if ( $subtype_name && ! in_array( $subtype_name, array_keys( $provider->get_object_subtypes() ), true ) ) {
+		return false;
+	}
+
+	$page = absint( $page );
+	if ( 0 >= $page ) {
+		$page = 1;
+	}
+	return $provider->get_sitemap_url( $subtype_name, $page );
+}
