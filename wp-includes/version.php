d4d3e23ad0b339332cf5ac065ec497094710d82b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-31 17:11:04 +0000

    Themes: Normalize the installed theme path in `Theme_Installer_Skin::do_overwrite()` before comparing with the uploaded theme.
    This ensures that the data for the currently installed theme is picked up properly when uploading a theme update on Windows.
    
    Follow-up to [48390].
    
    Props bobbingwide, wpamitkumar, mukesh27.
    Fixes #51182.
    Built from https://develop.svn.wordpress.org/trunk@48913
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48675 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-48912';
+$wp_version = '5.6-alpha-48913';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
