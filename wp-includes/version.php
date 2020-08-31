aec28121b3ad4ff21fbcac5d76ef6a8820127d7f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-31 15:58:04 +0000

    Date/Time: Make sure `get_the_date()` and related functions return correct time if the format was specified as `false`.
    Technically, the `$format` argument should always be a string, but passing `false` used to work before [47808], so this restores backward compatibility.
    
    The list of affected functions:
    * `get_the_date()`
    * `get_the_time()`
    * `get_comment_date()`
    * `get_comment_time()`
    
    Props wittich, Rarst, akabarikalpesh, SergeyBiryukov.
    Fixes #51184.
    Built from https://develop.svn.wordpress.org/trunk@48912
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48674 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-48911';
+$wp_version = '5.6-alpha-48912';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
