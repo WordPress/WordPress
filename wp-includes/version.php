e501d8ead8b77ab84b749a491f0e73c8531546fa
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-08-31 15:35:05 +0000

    Tests: Bring some consistency to Date/Time tests:
    * Move some tests from `post.php` to a more appropriate location in the `date` directory.
    * Rename `date/postTime.php` to `date/getPostTime.php` to match the function name.
    
    Props Rarst.
    See #51184.
    Built from https://develop.svn.wordpress.org/trunk@48911
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48673 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-48910';
+$wp_version = '5.6-alpha-48911';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
