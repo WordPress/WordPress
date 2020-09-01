bb254465ec152c4461bc4212f1ecaa6e1b345a9e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-09-01 17:44:03 +0000

    Help/About: Update the About page for 5.5.1.
    Props audrasjb, desrosj, SergeyBiryukov.
    Fixes #51202.
    Built from https://develop.svn.wordpress.org/branches/5.5@48933
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@48695 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -61,6 +61,39 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 
 		<hr />
 
+		<div class="about__section has-subtle-background-color">
+			<div class="column">
+				<h2><?php _e( 'Maintenance and Security Releases' ); ?></h2>
+				<p>
+					<?php
+					printf(
+						/* translators: 1: WordPress version number, 2: Plural number of bugs. */
+						_n(
+							'<strong>Version %1$s</strong> addressed %2$s bug.',
+							'<strong>Version %1$s</strong> addressed %2$s bugs.',
+							44
+						),
+						'5.5.1',
+						number_format_i18n( 44 )
+					);
+					?>
+					<?php
+					printf(
+						/* translators: %s: HelpHub URL. */
+						__( 'For more information, see <a href="%s">the release notes</a>.' ),
+						sprintf(
+							/* translators: %s: WordPress version. */
+							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							sanitize_title( '5.5.1' )
+						)
+					);
+					?>
+				</p>
+			</div>
+		</div>
+
+		<hr />
+
 		<div class="about__section has-1-column">
 			<div class="column">
 				<h2><?php _e( 'Speed' ); ?></h2>
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.1-RC2-48932';
+$wp_version = '5.5.1-RC2-48933';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
