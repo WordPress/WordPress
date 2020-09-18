0e0c620deeda1574b5d146aefdb0286abd18afd4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-09-18 13:55:04 +0000

    Docs: Update the code example in `WP_Ajax_UnitTestCase::dieHandler()` DocBlock to use `expectException()`.
    Follow-up to [48996], [48997].
    
    See #51344.
    Built from https://develop.svn.wordpress.org/trunk@48998
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48760 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-48997';
+$wp_version = '5.6-alpha-48998';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
