54fe0e06987299e6731e46775e7c861233b38049
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-09-19 10:36:03 +0000

    Tests: Speed up slashed data tests by reusing shared fixtures.
    Follow-up to [35249].
    
    See #51344.
    Built from https://develop.svn.wordpress.org/trunk@49003
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48765 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-49002';
+$wp_version = '5.6-alpha-49003';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
