9e3b5d2f12961c869ea72a4a91559c34f3829365
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-09-20 00:57:06 +0000

    Tests: Fix the failures in REST API `format` keyword validation tests on PHP 8.
    The tests ensure that `rest_sanitize_value_from_schema()` and `rest_validate_value_from_schema()` throw an "undefined offset" notice when the required `type` schema keyword is not passed.
    
    In PHP 8, that notice is now a warning, so the tests need to be adjusted accordingly.
    
    Follow-up to [48300], [48993].
    
    See #50913.
    Built from https://develop.svn.wordpress.org/trunk@49007
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48769 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-49006';
+$wp_version = '5.6-alpha-49007';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
