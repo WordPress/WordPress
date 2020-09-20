8791de899a7ae8566b430c3f71d279743589c60a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-09-20 01:18:04 +0000

    Coding Standards: Fix WPCS issue in [49007].
    See #50913.
    Built from https://develop.svn.wordpress.org/trunk@49008
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48770 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-49007';
+$wp_version = '5.6-alpha-49008';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
