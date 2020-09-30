36c3a480c895493cadd1a3947d54f23ded5977a0
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-09-30 13:14:04 +0000

    Build/Test Tools: Comment out the `xdebug` group test run for PHP 8 for now.
    Xdebug supports PHP 8 only from version 3.0, which is not released yet.
    
    Once Xdebug 3.0 is released and included in the Docker image, this should be uncommented again.
    
    Follow-up to [48957], [49037].
    
    See #50913, #50902.
    Built from https://develop.svn.wordpress.org/trunk@49074
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48836 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-49073';
+$wp_version = '5.6-alpha-49074';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
