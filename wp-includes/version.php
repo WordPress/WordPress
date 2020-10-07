a6ef541df06efc30ac4d13e1522f8a74cc3f5d44
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-07 10:38:08 +0000

    Coding Standards: Add missing space in `js/_enqueues/lib/comment-reply.js`.
    Props saqibameen, hareesh-pillai, imath, davidbaumwald.
    Fixes #43907.
    Built from https://develop.svn.wordpress.org/trunk@49097
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48859 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-49096';
+$wp_version = '5.6-alpha-49097';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
