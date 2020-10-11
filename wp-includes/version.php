8d945f4811e778d22472bd47d75db09ec0cd425c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-11 15:51:06 +0000

    Tests: Use `assertSame()` in `test_edit_user_blank_password()`, for consistency with other assertions.
    Follow-up to [49118].
    
    See #42766, #38266.
    Built from https://develop.svn.wordpress.org/trunk@49126
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48888 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-alpha-49125';
+$wp_version = '5.6-alpha-49126';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
