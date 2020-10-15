3b7a8a7eca27fb9d86b10ae9b471f649d3a71468
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-15 15:24:08 +0000

    Twenty Nineteen: Remove extra space from the comment link in `TwentyNineteen_Walker_Comment`.
    Fixes #51533.
    Built from https://develop.svn.wordpress.org/trunk@49159
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48921 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentynineteen/classes/class-twentynineteen-walker-comment.php
+++ wp-content/themes/twentynineteen/classes/class-twentynineteen-walker-comment.php
@@ -73,18 +73,20 @@ class TwentyNineteen_Walker_Comment extends Walker_Comment {
 					</div><!-- .comment-author -->
 
 					<div class="comment-metadata">
-						<a href="<?php echo esc_url( get_comment_link( $comment, $args ) ); ?>">
-							<?php
-								/* translators: 1: Comment date, 2: Comment time. */
-								$comment_timestamp = sprintf( __( '%1$s at %2$s', 'twentynineteen' ), get_comment_date( '', $comment ), get_comment_time() );
-							?>
-							<time datetime="<?php comment_time( 'c' ); ?>" title="<?php echo $comment_timestamp; ?>">
-								<?php echo $comment_timestamp; ?>
-							</time>
-						</a>
 						<?php
-							$edit_comment_icon = twentynineteen_get_icon_svg( 'edit', 16 );
-							edit_comment_link( __( 'Edit', 'twentynineteen' ), '<span class="edit-link-sep">&mdash;</span> <span class="edit-link">' . $edit_comment_icon, '</span>' );
+						/* translators: 1: Comment date, 2: Comment time. */
+						$comment_timestamp = sprintf( __( '%1$s at %2$s', 'twentynineteen' ), get_comment_date( '', $comment ), get_comment_time() );
+
+						printf(
+							'<a href="%s"><time datetime="%s" title="%s">%s</time></a>',
+							esc_url( get_comment_link( $comment, $args ) ),
+							get_comment_time( 'c' ),
+							esc_attr( $comment_timestamp ),
+							$comment_timestamp
+						);
+
+						$edit_comment_icon = twentynineteen_get_icon_svg( 'edit', 16 );
+						edit_comment_link( __( 'Edit', 'twentynineteen' ), ' <span class="edit-link-sep">&mdash;</span> <span class="edit-link">' . $edit_comment_icon, '</span>' );
 						?>
 					</div><!-- .comment-metadata -->
 
