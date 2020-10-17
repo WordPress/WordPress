e2d191f7105be2c9f06c80579e17be7e1a043d0c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-17 16:36:07 +0000

    Revisions: Explicitly declare the `Text_Diff::_getTempDir()` method as `static`.
    This fixes a "Non-static method cannot be called statically" fatal error on PHP 8.
    
    Props jrf.
    Fixes #51559.
    Built from https://develop.svn.wordpress.org/trunk@49185
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48947 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/Text/Diff.php
+++ wp-includes/Text/Diff.php
@@ -220,14 +220,12 @@ class Text_Diff {
     /**
      * Determines the location of the system temporary directory.
      *
-     * @static
-     *
      * @access protected
      *
      * @return string  A directory name which can be used for temp files.
      *                 Returns false if one could not be found.
      */
-    function _getTempDir()
+    static function _getTempDir()
     {
         $tmp_locations = array('/tmp', '/var/tmp', 'c:\WUTemp', 'c:\temp',
                                'c:\windows\temp', 'c:\winnt\temp');
