e209e0073443f7e42bb399695128cbceb4c1dda5
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-19 20:45:06 +0000

    Coding Standards: Fix WPCS issues in [49207].
    See #46866.
    Built from https://develop.svn.wordpress.org/trunk@49209
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48971 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/media.php
+++ wp-admin/includes/media.php
@@ -3295,29 +3295,34 @@ function attachment_submitbox_metadata() {
 	if ( $author->exists() ) {
 		$uploaded_by_name = $author->display_name ? $author->display_name : $author->nickname;
 		$uploaded_by_link = get_edit_user_link( $author->ID );
-	} ?>
+	}
+	?>
 	<div class="misc-pub-section misc-pub-uploadedby">
-		<?php if ( $uploaded_by_link ) : ?>
-			<?php _e( 'Uploaded by:' ); ?> <a href="<?php echo $uploaded_by_link ?>"><strong><?php echo $uploaded_by_name ?></strong></a>
-		<?php else: ?>
-			<?php _e( 'Uploaded by:' ); ?> <strong><?php echo $uploaded_by_name ?></strong>
-		<?php endif; ?>
+		<?php if ( $uploaded_by_link ) { ?>
+			<?php _e( 'Uploaded by:' ); ?> <a href="<?php echo $uploaded_by_link; ?>"><strong><?php echo $uploaded_by_name; ?></strong></a>
+		<?php } else { ?>
+			<?php _e( 'Uploaded by:' ); ?> <strong><?php echo $uploaded_by_name; ?></strong>
+		<?php } ?>
 	</div>
 
-	<?php if ( $post->post_parent ) :
+	<?php
+	if ( $post->post_parent ) {
 		$post_parent = get_post( $post->post_parent );
-		if ( $post_parent ) :
+		if ( $post_parent ) {
 			$uploaded_to_title = $post_parent->post_title ? $post_parent->post_title : __( '(no title)' );
-			$uploaded_to_link = get_edit_post_link( $post->post_parent, 'raw' ); ?>
+			$uploaded_to_link  = get_edit_post_link( $post->post_parent, 'raw' );
+			?>
 			<div class="misc-pub-section misc-pub-uploadedto">
-				<?php if ( $uploaded_to_link ) : ?>
-					<?php _e( 'Uploaded to:' ); ?> <a href="<?php echo $uploaded_to_link ?>"><strong><?php echo $uploaded_to_title; ?></strong></a>
-				<?php else: ?>
+				<?php if ( $uploaded_to_link ) { ?>
+					<?php _e( 'Uploaded to:' ); ?> <a href="<?php echo $uploaded_to_link; ?>"><strong><?php echo $uploaded_to_title; ?></strong></a>
+				<?php } else { ?>
 					<?php _e( 'Uploaded to:' ); ?> <strong><?php echo $uploaded_to_title; ?></strong>
-				<?php endif; ?>
+				<?php } ?>
 			</div>
-		<?php endif; ?>
-	<?php endif; ?>
+			<?php
+		}
+	}
+	?>
 
 	<div class="misc-pub-section misc-pub-attachment">
 		<label for="attachment_url"><?php _e( 'File URL:' ); ?></label>
