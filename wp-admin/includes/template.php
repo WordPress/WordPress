0287a4876428a624fffd06173cfc25dd409710f4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-20 14:41:05 +0000

    Docs: Update the `@since` note for `_media_states()`.
    Follow-up to [49223].
    
    Props mukesh27.
    Fixes #42063.
    Built from https://develop.svn.wordpress.org/trunk@49231
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@48993 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/template.php
+++ wp-admin/includes/template.php
@@ -2237,7 +2237,7 @@ function get_post_states( $post ) {
  * Outputs the attachment media states as HTML.
  *
  * @since 3.2.0
- * @since 5.6.0 Added the `$echo` parameter.
+ * @since 5.6.0 Added the `$echo` parameter and a return value.
  *
  * @param WP_Post $post The attachment post to retrieve states for.
  * @param bool    $echo Optional. Whether to echo the post states as an HTML string. Default true.
