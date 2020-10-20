a0f350b597607e3e32457365678e9e79155837b7
Helen Hou-Sandí <helen.y.hou@gmail.com>
2020-10-20 22:19:06 +0000

    Post-WordPress 5.6 Beta 1 version bump.
    Built from https://develop.svn.wordpress.org/trunk@49261
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49023 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta1';
+$wp_version = '5.6-beta1-r49261';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
