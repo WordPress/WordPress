bbfc20ee446f229b9b0579e96f6080a0f3ec43d6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-21 14:46:05 +0000

    Coding Standards: Rename a variable in `wp-login.php` for consistency with other instances.
    See #50767.
    Built from https://develop.svn.wordpress.org/trunk@49265
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49027 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-login.php
+++ wp-login.php
@@ -436,12 +436,12 @@ function retrieve_password() {
 	$message .= __( 'To reset your password, visit the following address:' ) . "\r\n\r\n";
 	$message .= network_site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user_login ), 'login' ) . "\r\n\r\n";
 
-	$requestor_ip = $_SERVER['REMOTE_ADDR'];
-	if ( $requestor_ip ) {
+	$requester_ip = $_SERVER['REMOTE_ADDR'];
+	if ( $requester_ip ) {
 		$message .= sprintf(
-			/* translators: %s: IP address of password reset requestor. */
+			/* translators: %s: IP address of password reset requester. */
 			__( 'This password reset request originated from the IP address %s.' ),
-			$requestor_ip
+			$requester_ip
 		) . "\r\n";
 	}
 
