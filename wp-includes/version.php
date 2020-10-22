f8e9df7a95c7beb9d1adc37217091d77fdc01a5b
Peter Wilson <wilson@peterwilson.cc>
2020-10-22 00:19:08 +0000

    Tests: Use shared fixtures in XML-RPC new comment unit tests.
    See #51344.
    
    
    Built from https://develop.svn.wordpress.org/trunk@49268
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49030 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta1-49267';
+$wp_version = '5.6-beta1-49268';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
