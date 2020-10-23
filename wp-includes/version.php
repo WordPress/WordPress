ce4b1cc8ba316a35fccb44ddf1561f1a63ad2e7b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-23 19:49:08 +0000

    Toolbar: Keep IE 11 scrollbar on the side instead of overlapping the toolbar.
    Props sabernhardt, shital-patel, y_kolev.
    Fixes #48545.
    Built from https://develop.svn.wordpress.org/trunk@49290
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49052 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta1-49289';
+$wp_version = '5.6-beta1-49290';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
