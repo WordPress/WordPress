d9838eed62e578cb76e9c9c4311ba87e2b602770
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-23 19:09:10 +0000

    Posts, Post Types: Set better default title for custom post types without title support.
    The default title is now set to "(no title supported)" instead of "Auto Draft".
    
    Props garrett-eclipse, rajanit2000, bridgetwillard, donmhico, nrqsnchz.
    Fixes #45516.
    Built from https://develop.svn.wordpress.org/trunk@49288
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49050 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta1-49287';
+$wp_version = '5.6-beta1-49288';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
