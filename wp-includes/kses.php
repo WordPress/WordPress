5770b676b04259af58c5c388a21d44ec19734ce7
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-25 00:14:05 +0000

    Docs: Correct an example in `wp_kses_normalize_entities()` description.
    Props ediamin.
    Fixes #51620.
    Built from https://develop.svn.wordpress.org/trunk@49304
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49066 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/kses.php
+++ wp-includes/kses.php
@@ -1777,7 +1777,7 @@ function wp_kses_bad_protocol_once2( $string, $allowed_protocols ) {
  * Converts and fixes HTML entities.
  *
  * This function normalizes HTML entities. It will convert `AT&T` to the correct
- * `AT&amp;T`, `&#00058;` to `&#58;`, `&#XYZZY;` to `&amp;#XYZZY;` and so on.
+ * `AT&amp;T`, `&#00058;` to `&#058;`, `&#XYZZY;` to `&amp;#XYZZY;` and so on.
  *
  * When `$context` is set to 'xml', HTML entities are converted to their code points.  For
  * example, `AT&T&hellip;&#XYZZY;` is converted to `AT&amp;T…&amp;#XYZZY;`.
