f062c1a2ccb48ffce0a509e557cc43f84719af82
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-26 22:58:06 +0000

    Build Tools: Update mergeinfo for [49313].
    See #51624.
    Built from https://develop.svn.wordpress.org/branches/5.5@49315
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@49077 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.2-alpha-49313';
+$wp_version = '5.5.2-alpha-49315';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
