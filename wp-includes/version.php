0a4f041f2a1dc4ccd1acfac70d8fcc224504f139
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-27 16:09:07 +0000

    Tests: Rename some files and classes in `phpunit/tests/post/` per the naming conventions.
    https://make.wordpress.org/core/handbook/testing/automated-testing/writing-phpunit-tests/#naming-and-organization
    
    Follow-up to [47780].
    
    See #51344.
    Built from https://develop.svn.wordpress.org/trunk@49327
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49088 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta1-49326';
+$wp_version = '5.6-beta1-49327';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
