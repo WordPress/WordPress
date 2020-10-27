21f3cc781e4588b54aed1ddf2f8a8af5054d9b89
Helen Hou-Sandí <helen.y.hou@gmail.com>
2020-10-27 21:42:07 +0000

    WordPress 5.6 Beta 2.
    Built from https://develop.svn.wordpress.org/trunk@49350
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49110 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta1-49349';
+$wp_version = '5.6-beta2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
