92b65c7449be851d0391f8c15e0063ad97c190ba
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-27 00:06:06 +0000

    Build Tools: Fix running installing Composer dependencies using Composer 2.0.
    This updates the `dealerdirect/phpcodesniffer-composer-installer` package to allow installing version `0.7.0` which supports Composer 2.0.
    
    Props itowhid06, jrf.
    Merges [49306] to the 5.4 branch.
    See #51624.
    Built from https://develop.svn.wordpress.org/branches/5.4@49318
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.4@49080 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4.3-alpha-48366';
+$wp_version = '5.4.3-alpha-49318';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
