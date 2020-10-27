a33d6305d1c21e2bbfe2507eaf1bc99f4919261f
Helen Hou-Sandí <helen.y.hou@gmail.com>
2020-10-27 22:14:05 +0000

    Twenty Twenty: Correct some textdomains in block patterns.
    Props justinahinon.
    Fixes #51648. See #51098.
    
    Built from https://develop.svn.wordpress.org/trunk@49352
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49112 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta2-49351';
+$wp_version = '5.6-beta2-49352';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
