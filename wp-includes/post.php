72cff29cb69a4198ffe943629e9ad9baedac2f54
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-27 16:42:05 +0000

    Posts, Post Types: Check if taxonomy is set for the `tax_input` parameter of `wp_insert_post()`.
    This avoids a PHP notice when creating a post with multiple taxonomies both having a default term.
    
    Props yakimun, szaqal21, hareesh-pillai, audrasjb.
    Fixes #51320.
    Built from https://develop.svn.wordpress.org/trunk@49328
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49089 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -4091,7 +4091,7 @@ function wp_insert_post( $postarr, $wp_error = false, $fire_after_hooks = true )
 			if ( ! empty( $tax_object->default_term ) ) {
 
 				// Filter out empty terms.
-				if ( isset( $postarr['tax_input'] ) && is_array( $postarr['tax_input'][ $taxonomy ] ) ) {
+				if ( isset( $postarr['tax_input'][ $taxonomy ] ) && is_array( $postarr['tax_input'][ $taxonomy ] ) ) {
 					$postarr['tax_input'][ $taxonomy ] = array_filter( $postarr['tax_input'][ $taxonomy ] );
 				}
 
