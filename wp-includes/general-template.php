e569358678dd1d3480740e451d546bf8b5ad8101
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-28 16:57:08 +0000

    General: Ensure that filtered arguments in `get_search_form()` contain all required default values.
    Props kellybleck, davidbaumwald, whyisjake, SergeyBiryukov.
    Fixes #51645.
    Built from https://develop.svn.wordpress.org/trunk@49355
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49115 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -281,6 +281,9 @@ function get_search_form( $args = array() ) {
 	 */
 	$args = apply_filters( 'search_form_args', $args );
 
+	// Ensure that the filtered arguments contain all required default values.
+	$args = array_merge( $defaults, $args );
+
 	$format = current_theme_supports( 'html5', 'search-form' ) ? 'html5' : 'xhtml';
 
 	/**
@@ -303,7 +306,7 @@ function get_search_form( $args = array() ) {
 		$form = ob_get_clean();
 	} else {
 		// Build a string containing an aria-label to use for the search form.
-		if ( isset( $args['aria_label'] ) && $args['aria_label'] ) {
+		if ( $args['aria_label'] ) {
 			$aria_label = 'aria-label="' . esc_attr( $args['aria_label'] ) . '" ';
 		} else {
 			/*
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta2-49354';
+$wp_version = '5.6-beta2-49355';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
