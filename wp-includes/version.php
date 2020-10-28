dbae445997c363f1f7d63156113ef3fc3ce26374
whyisjake <whyisjake@git.wordpress.org>
2020-10-28 19:11:06 +0000

    Embeds: Remove Facebook and Instagram as an oEmbed Source
    Facebook has depracated all non-authenticated endpoints for Facebook and Instagram. 
    
    See: https://developers.facebook.com/docs/plugins/oembed-legacy
    See also: https://make.wordpress.org/core/2020/09/22/facebook-and-instagram-embeds-to-be-deprecated-october-24th/
    
    With this change, endpoints are being removed. If a site is dependent on this feature, they need to pass either an app or client token. There are a few plugins that add this functionality.
    
    Fixes #50861.
    Props johnbillion, joyously, mkaz, dimadin, ayeshrajans, davisshaver, paaljoachim, Clorith, bridgetwillard, jb510, sippis, Clorith, TimothyBlynJacobs, desrosj, smub, audrasjb, whyisjake.
    
    Built from https://develop.svn.wordpress.org/trunk@49359
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49119 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta2-49358';
+$wp_version = '5.6-beta2-49359';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
