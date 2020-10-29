025f5a62a2fe8ff88a412926adee5122cc95dae8
desrosj <desrosj@git.wordpress.org>
2020-10-29 18:02:05 +0000

    Embeds: Disable embeds on deactivated Multisite sites.
    Props xknown, whyisjake, zieladam, peterwilsoncc.
    Merges [49374] to trunk.
    Built from https://develop.svn.wordpress.org/trunk@49383
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49142 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta2-49382';
+$wp_version = '5.6-beta2-49383';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
