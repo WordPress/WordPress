107e7856e67b598efb6b2dc89386fec9f2a4f0d1
whyisjake <whyisjake@git.wordpress.org>
2020-10-29 17:00:03 +0000

    XML-RPC: Return error message if attachment ID is incorrect.
    Throw an error for incorrect attachment IDs when requesting a media object to ensure return signature is correct and doesn't include incorrectly typed values within the object.
    
    Props zieladam, peterwilsoncc, whyisjake.
    See #49905.
    
    Built from https://develop.svn.wordpress.org/branches/5.5@49376
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@49135 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.2-alpha-49375';
+$wp_version = '5.5.2-alpha-49376';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
