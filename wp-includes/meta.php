84a7bde4147e47cb591becd66b53b418aec36c63
desrosj <desrosj@git.wordpress.org>
2020-10-29 18:08:06 +0000

    Meta: Sanitize meta key before checking protection status.
    Props zieladam, peterwilsoncc, xknown, whyisjake.
    Merges [49377,49381] to trunk.
    Built from https://develop.svn.wordpress.org/trunk@49387
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49146 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/meta.php
+++ wp-includes/meta.php
@@ -1159,7 +1159,8 @@ function _get_meta_table( $type ) {
  * @return bool Whether the meta key is considered protected.
  */
 function is_protected_meta( $meta_key, $meta_type = '' ) {
-	$protected = ( '_' === $meta_key[0] );
+	$sanitized_key = preg_replace( "/[^\x20-\x7E\p{L}]/", '', $meta_key );
+	$protected     = strlen( $sanitized_key ) > 0 && ( '_' === $sanitized_key[0] );
 
 	/**
 	 * Filters whether a meta key is considered protected.
