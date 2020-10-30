3448d022f8d8ee37fe5b44c5ad7d7fb1a7b6a848
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-30 19:32:05 +0000

    WordPress 5.5.3.
    Props audrasjb.
    Fixes #51684.
    Built from https://develop.svn.wordpress.org/branches/5.5@49458
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@49217 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.3-alpha-49453';
+$wp_version = '5.5.3';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
