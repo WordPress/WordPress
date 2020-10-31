7941c32ad3fc72642e3b9d30833a55942a458318
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-10-31 02:56:05 +0000

    Twenty Sixteen: Ensure the `<nav>` elements in widgets do not affect bottom margin.
    Follow-up to [49208].
    
    Props sabernhardt.
    Fixes #51445.
    Built from https://develop.svn.wordpress.org/trunk@49470
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49229 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta2-49469';
+$wp_version = '5.6-beta2-49470';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
