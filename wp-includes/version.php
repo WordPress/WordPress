409bd276598c41658eacd85290a8629b7babed39
Helen Hou-Sandí <helen.y.hou@gmail.com>
2020-11-02 19:09:09 +0000

    Remove accidentally duplicated code introduced in [49475].
    See #46536.
    
    Built from https://develop.svn.wordpress.org/trunk@49476
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49235 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta2-49475';
+$wp_version = '5.6-beta2-49476';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
