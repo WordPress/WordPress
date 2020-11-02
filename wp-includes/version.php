be95ec2c27c83415861b6b6ce7350c25ca8b796f
Helen Hou-Sandí <helen.y.hou@gmail.com>
2020-11-02 19:46:14 +0000

    About: Optimize freedoms sprite and add 2 column layout.
    Props garrett-eclipse, ryelle.
    Fixes #46363.
    
    Built from https://develop.svn.wordpress.org/trunk@49479
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49238 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta2-49478';
+$wp_version = '5.6-beta2-49479';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
