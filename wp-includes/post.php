618fb979c6fb408a715487e40fd3018cb15cc768
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-11-04 01:39:06 +0000

    Docs: Add a reference to `wp_insert_post()` for information on the arguments accepted by `wp_update_post()`.
    See #50768.
    Built from https://develop.svn.wordpress.org/trunk@49493
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49252 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -4327,7 +4327,8 @@ function wp_insert_post( $postarr, $wp_error = false, $fire_after_hooks = true )
  * @since 5.6.0 Added the `fire_after_hooks` parameter.
  *
  * @param array|object $postarr          Optional. Post data. Arrays are expected to be escaped,
- *                                       objects are not. Default array.
+ *                                       objects are not. See wp_insert_post() for accepted arguments.
+ *                                       Default array.
  * @param bool         $wp_error         Optional. Whether to return a WP_Error on failure. Default false.
  * @param bool         $fire_after_hooks Optional. Whether to fire the after insert hooks. Default true.
  * @return int|WP_Error The post ID on success. The value 0 or WP_Error on failure.
