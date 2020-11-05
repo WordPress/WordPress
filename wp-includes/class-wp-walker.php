42b6ece05920be90fc3bb155827f95e85fd6bb46
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-11-05 18:46:10 +0000

    Docs: Fix typo in a comment in `Walker::display_element()`.
    Props david.binda, sabernhardt.
    Fixes #51713.
    Built from https://develop.svn.wordpress.org/trunk@49509
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49264 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-walker.php
+++ wp-includes/class-wp-walker.php
@@ -143,7 +143,7 @@ class Walker {
 
 		$this->start_el( $output, $element, $depth, ...array_values( $args ) );
 
-		// Descend only when the depth is right and there are childrens for this element.
+		// Descend only when the depth is right and there are children for this element.
 		if ( ( 0 == $max_depth || $max_depth > $depth + 1 ) && isset( $children_elements[ $id ] ) ) {
 
 			foreach ( $children_elements[ $id ] as $child ) {
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta3-49508';
+$wp_version = '5.6-beta3-49509';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
