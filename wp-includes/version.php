a3faece50b69d6b43c1d1fa641b48d245d6d5a3b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-11-06 11:38:09 +0000

    Twenty Twenty: Correctly indent nested unordered lists in RTL editor styles.
    Props sabernhardt.
    Fixes #51574.
    Built from https://develop.svn.wordpress.org/trunk@49510
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49265 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta3-49509';
+$wp_version = '5.6-beta3-49510';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
