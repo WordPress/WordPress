964c3535046ddd30ce6cbbe3191b74a9525d86cc
desrosj <desrosj@git.wordpress.org>
2020-11-09 23:41:06 +0000

    Tests: Remove duplicate `fields` key in `WP_Query` test.
    Introduced in [46188].
    
    See #51344.
    Built from https://develop.svn.wordpress.org/trunk@49554
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49292 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta3-49553';
+$wp_version = '5.6-beta3-49554';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
