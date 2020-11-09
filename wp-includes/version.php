8562263fd66eae21e0a90e1337be0e33fa0b0160
desrosj <desrosj@git.wordpress.org>
2020-11-09 19:42:06 +0000

    Build/Test Tools: Remove PHP >= 5.3 check.
    WordPress no longer supports PHP < 5.6.20. This check is no longer necessary.
    
    Fixes #51737.
    Built from https://develop.svn.wordpress.org/trunk@49551
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49289 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta3-49550';
+$wp_version = '5.6-beta3-49551';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
