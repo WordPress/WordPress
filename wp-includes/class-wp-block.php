2c5607307f2c303f48481f8dda62e2ce4bdd63e0
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-11-09 11:01:05 +0000

    Coding Standards: Adjust formatting in `WP_Block::render()` for better readability.
    Follow-up to [49310], [49539].
    
    See #51606.
    Built from https://develop.svn.wordpress.org/trunk@49540
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49278 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-block.php
+++ wp-includes/class-wp-block.php
@@ -213,12 +213,16 @@ class WP_Block {
 		}
 
 		if ( $is_dynamic ) {
-			$global_post                        = $post;
-			$parent                             = WP_Block_Supports::$block_to_render;
+			$global_post = $post;
+			$parent      = WP_Block_Supports::$block_to_render;
+
 			WP_Block_Supports::$block_to_render = $this->parsed_block;
-			$block_content                      = (string) call_user_func( $this->block_type->render_callback, $this->attributes, $block_content, $this );
+
+			$block_content = (string) call_user_func( $this->block_type->render_callback, $this->attributes, $block_content, $this );
+
 			WP_Block_Supports::$block_to_render = $parent;
-			$post                               = $global_post;
+
+			$post = $global_post;
 		}
 
 		if ( ! empty( $this->block_type->script ) ) {
