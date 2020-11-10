1f984603a239cb79dfb68fbb38c56fa450ca3ba7
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-11-10 19:05:10 +0000

    Help/About: Add Site Health Status dashboard widget to the Help → Content tab.
    Reorder the items in the help tab to align with their default position on the Dashboard screen.
    
    Follow-up to [47063], [47300].
    
    Props nlpro.
    Fixes #51745.
    Built from https://develop.svn.wordpress.org/trunk@49558
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49296 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/index.php
+++ wp-admin/index.php
@@ -72,8 +72,16 @@ $screen->add_help_tab(
 
 $help = '<p>' . __( 'The boxes on your Dashboard screen are:' ) . '</p>';
 
+if ( current_user_can( 'edit_theme_options' ) ) {
+	$help .= '<p>' . __( '<strong>Welcome</strong> &mdash; Shows links for some of the most common tasks when setting up a new site.' ) . '</p>';
+}
+
+if ( current_user_can( 'view_site_health_checks' ) ) {
+	$help .= '<p>' . __( '<strong>Site Health Status</strong> &mdash; Informs you of any potential issues that should be addressed to improve the performance or security of your website.' ) . '</p>';
+}
+
 if ( current_user_can( 'edit_posts' ) ) {
-	$help .= '<p>' . __( '<strong>At A Glance</strong> &mdash; Displays a summary of the content on your site and identifies which theme and version of WordPress you are using.' ) . '</p>';
+	$help .= '<p>' . __( '<strong>At a Glance</strong> &mdash; Displays a summary of the content on your site and identifies which theme and version of WordPress you are using.' ) . '</p>';
 }
 
 $help .= '<p>' . __( '<strong>Activity</strong> &mdash; Shows the upcoming scheduled posts, recently published posts, and the most recent comments on your posts and allows you to moderate them.' ) . '</p>';
@@ -87,10 +95,6 @@ $help .= '<p>' . sprintf(
 	__( '<strong>WordPress Events and News</strong> &mdash; Upcoming events near you as well as the latest news from the official WordPress project and the <a href="%s">WordPress Planet</a>.' ),
 	__( 'https://planet.wordpress.org/' )
 ) . '</p>';
-
-if ( current_user_can( 'edit_theme_options' ) ) {
-	$help .= '<p>' . __( '<strong>Welcome</strong> &mdash; Shows links for some of the most common tasks when setting up a new site.' ) . '</p>';
-}
 
 $screen->add_help_tab(
 	array(
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-beta3-49557';
+$wp_version = '5.6-beta3-49558';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
