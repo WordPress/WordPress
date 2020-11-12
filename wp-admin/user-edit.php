5ee48d6915ccc9ad253357bbe91955dc23fade2f
TimothyBlynJacobs <TimothyBlynJacobs@git.wordpress.org>
2020-11-12 18:14:08 +0000

    App Passwords: MOAR accessibility improvements.
    Corrects the "Application Name" field's aria-describedby attribute and makes the description visible.
    
    Props alexstine, SergeyBiryukov.
    Fixes #51580.
    
    Built from https://develop.svn.wordpress.org/trunk@49573
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49311 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/user-edit.php
+++ wp-admin/user-edit.php
@@ -742,7 +742,8 @@ endif;
 		<div class="create-application-password form-wrap">
 			<div class="form-field">
 				<label for="new_application_password_name"><?php _e( 'New Application Password Name' ); ?></label>
-				<input type="text" size="30" id="new_application_password_name" name="new_application_password_name" placeholder="<?php esc_attr_e( 'WordPress App on My Phone' ); ?>" class="input" aria-required="true" aria-describedby="<?php esc_attr_e( 'Required to create an Application Password, but not to update the user.' ); ?>" />
+				<input type="text" size="30" id="new_application_password_name" name="new_application_password_name" placeholder="<?php esc_attr_e( 'WordPress App on My Phone' ); ?>" class="input" aria-required="true" aria-describedby="new_application_password_name_desc" />
+				<p class="description" id="new_application_password_name_desc"><?php _e( 'Required to create an Application Password, but not to update the user.' ); ?></p>
 			</div>
 
 			<?php
