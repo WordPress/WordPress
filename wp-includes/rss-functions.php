3c51950baf91ca2bedfbabe8987b90db18c41fbb
whyisjake <whyisjake@git.wordpress.org>
2020-11-16 22:52:05 +0000

    Feeds: Add an early exit when calling RSS functions directly.
    While this file is depracated, it shouldn't throw errors when called directly.
    
    Fixes #35835.
    
    Props thib3113, dd32, TheGP, flymike, zodiac1978, williampatton.
    
    Built from https://develop.svn.wordpress.org/trunk@49619
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49357 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rss-functions.php
+++ wp-includes/rss-functions.php
@@ -6,5 +6,9 @@
  * @deprecated 2.1.0
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit();
+}
+
 _deprecated_file( basename( __FILE__ ), '2.1.0', WPINC . '/rss.php' );
 require_once ABSPATH . WPINC . '/rss.php';
