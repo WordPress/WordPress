a434baf9e8d58cdb276a0f090522c9a8fdb719de
Helen Hou-Sandí <helen.y.hou@gmail.com>
2020-11-17 22:52:03 +0000

    Trunk is now 5.7-alpha.
    Built from https://develop.svn.wordpress.org/trunk@49644
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49382 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-RC1-49642';
+$wp_version = '5.7-alpha-49644';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
