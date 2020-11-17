5ac21f5fbc5acd2154c8b5123fb0d004893203a2
Helen Hou-Sandí <helen.y.hou@gmail.com>
2020-11-17 22:39:05 +0000

    Post WordPress 5.6 RC 1 version bump.
    Built from https://develop.svn.wordpress.org/trunk@49642
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49380 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-RC1';
+$wp_version = '5.6-RC1-49642';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
