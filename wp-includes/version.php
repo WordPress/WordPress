30cfb5eed3b62e1d2618e1dbecf320e8a718b78e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-11-17 23:07:05 +0000

    Security: Update the Security Policy for the 5.5 branch.
    See [49645], [49646].
    Props whyisjake.
    Built from https://develop.svn.wordpress.org/branches/5.5@49647
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@49385 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.4-alpha-49501';
+$wp_version = '5.5.4-alpha-49647';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
