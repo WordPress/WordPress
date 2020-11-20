d86f6bde55261d7a0ea24e1de8a75b944df3862b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-11-20 11:58:06 +0000

    Docs: Correct `$fire_after_hooks` parameter name in `@since` tags.
    Follow-up to [49172], [49173].
    
    See #45114.
    Built from https://develop.svn.wordpress.org/trunk@49673
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49396 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-49672';
+$wp_version = '5.7-alpha-49673';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
