f677bc838e27fa90204b46c174c33c689d87a250
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-11-22 16:06:06 +0000

    Upgrade/Install: Replace the conditionals that check the `AUTOMATIC_UPDATER_DISABLED` constant and the `automatic_updater_disabled` filter in update-core.php with a call to `WP_Automatic_Updater::is_disabled()`.
    This prevents a PHP warning, fixes the logic, and considers `wp_is_file_mod_allowed( 'automatic_updater' )` when determining the UI state.
    
    Props jamesros161, pbiron, audrasjb, azaozz.
    Merges [49677] and [49681] to the 5.6 branch.
    Fixes #51827.
    Built from https://develop.svn.wordpress.org/branches/5.6@49682
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@49405 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-RC1-49667';
+$wp_version = '5.6-RC1-49682';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
