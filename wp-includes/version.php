25d67c30b0873034f27d78fb41567d01c13ec8cc
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-11-27 12:52:06 +0000

    Tests: Test that `sanitize_title_with_dashes()` strips HTML tags.
    `sanitize_title()` already has the same test in its own file.
    
    Props pbearne.
    Fixes #51881.
    Built from https://develop.svn.wordpress.org/trunk@49701
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49424 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-49700';
+$wp_version = '5.7-alpha-49701';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
