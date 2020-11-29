6eec8bae02c72e84f79541347909bd9c7c8ad562
Andrew Ozz <admin@laptoptips.ca>
2020-11-29 09:56:09 +0000

    External Libraries: Fix hiding of the text in the jQuery UI dialog close button.
    Props marijnkoopman, SergeyBiryukov, Clorith, azaozz.
    Merges [49704] to the 5.6 branch.
    Fixes #51863.
    Built from https://develop.svn.wordpress.org/branches/5.6@49707
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@49430 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-RC1-49706';
+$wp_version = '5.6-RC1-49707';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
