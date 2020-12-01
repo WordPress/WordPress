effd0cc28e290ac22c1ff5829d394b410046458a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-12-01 17:06:05 +0000

    Editor: Don't unnecessarily split a translatable string in block templates.
    As a best practice, strings available for translation should contain entire sentences whenever possible.
    
    Splitting a sentence in two parts and putting them back together after translation should be avoided, as the word order in other languages can be different from English.
    
    Props tobifjellner, kebbet, audrasjb, mukesh27, hellofromTonya, azaozz, SergeyBiryukov.
    Reviewed by azaozz, SergeyBiryukov.
    Merges [49722] to the 5.6 branch.
    Fixes #51893.
    Built from https://develop.svn.wordpress.org/branches/5.6@49723
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@49446 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-RC1-49721';
+$wp_version = '5.6-RC1-49723';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
