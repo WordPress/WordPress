692c6498e948785e0364f0fb5ca2443c9c70a201
TimothyBlynJacobs <TimothyBlynJacobs@git.wordpress.org>
2020-12-03 21:06:08 +0000

    App Passwords: Ensure the Created At and Last Used dates are properly translated.
    The `date_i18n` function is now used when formatting the dates in PHP instead of `gmdate` which doesn't handle localization properly.
    
    Additionally, we now use a translation to get the date format to use instead of pulling from the `date_format` option which is only supposed to affect the front-end.
    
    Lastly, when passing the date format to the Backbone JS template, we now use `wp_json_encode()` to format the value for JavaScript. This ensures that backslashes are properly preserved which are used by some locales to escape date formatting control characters.
    
    Props pedromendonca, TimothyBlynJacobs, ocean90, hellofromtonya, SergeyBiryukov, antpb.
    Fixes #51918.
    See [35811].
    
    Built from https://develop.svn.wordpress.org/trunk@49746
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49469 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-49744';
+$wp_version = '5.7-alpha-49746';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
