e0b55f4a75a2da110a7fda1e7d4408f9be8dfbd6
Helen Hou-Sandí <helen.y.hou@gmail.com>
2020-12-08 21:52:05 +0000

    Update `$_old_files` for 5.6.
    See #39943.
    Merges [49774] to the 5.6 branch.
    
    Built from https://develop.svn.wordpress.org/branches/5.6@49775
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@49498 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6-RC5-49773';
+$wp_version = '5.6-RC5-49775';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
