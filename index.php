584f84b3430bb1d25bf95e5f3d37c12bdeab1aba
annatoma92 <64438641+annatoma92@users.noreply.github.com>
2020-12-08 18:26:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
+ * Front to the test WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
  *
  * @package WordPress
