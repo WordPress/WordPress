2e90ec87897d1a24a4cf759a093c41f0933b99e1
desrosj <desrosj@git.wordpress.org>
2020-12-09 13:35:05 +0000

    Build/Test Tools: Remove the Appveyor configuration file.
    Appveyor was added to Core in [44594] to ensure that NPM and the WordPress build tools continue to work correctly on Windows systems as changes are made. Using an additional service for this was required at the time, TravisCI did not support testing on Windows.
    
    [49162] introduced a GitHub Action workflow that performed the same testing. Since all automated testing is moving to GitHub Actions, using Appveyor is no longer necessary, and it’s preferable to have all automated testing in one location as much as possible.
    
    Props ayeshrajans.
    Fixes #51968.
    Built from https://develop.svn.wordpress.org/trunk@49779
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49502 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-49774';
+$wp_version = '5.7-alpha-49779';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
