da72be5227d3893e70d5986d6f923b7a20963e37
ryelle <ryelle@git.wordpress.org>
2020-12-14 16:49:03 +0000

    Twenty Twenty-One: Bring back PostCSS config.
    Twenty Twenty-One uses PostCSS to convert the custom properties for Internet Explorer. Without a config file, the postcss command does nothing, causing issues in IE. This brings back the config from GitHub, which replaces all custom properties with the default values.
    
    Props t-p, poena.
    Fixes #52040.
    
    
    Built from https://develop.svn.wordpress.org/trunk@49800
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49523 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-49799';
+$wp_version = '5.7-alpha-49800';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
