71bbd2b16608f56db65f5df7072194e0c627de36
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-12-14 15:22:09 +0000

    Twenty Twenty-One: Correct the DocBlock for `map-deep-set()` Sass function.
    Props mukesh27.
    Fixes #52064.
    Built from https://develop.svn.wordpress.org/trunk@49799
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49522 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-49798';
+$wp_version = '5.7-alpha-49799';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
