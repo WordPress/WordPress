21bd25a8e791dcc5885ca48509c0288c193e6a91
desrosj <desrosj@git.wordpress.org>
2020-12-17 12:58:05 +0000

    Twenty Twenty-One: Fix typos in some inline comments.
    Props tanvirul, mukesh27.
    Merges [49795] to the 5.6 branch.
    Fixes #52042.
    Built from https://develop.svn.wordpress.org/branches/5.6@49818
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@49537 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6.1-alpha-49817';
+$wp_version = '5.6.1-alpha-49818';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
