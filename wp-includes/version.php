9ef2645fd79d8680db9768933eed1e6317139b2f
desrosj <desrosj@git.wordpress.org>
2020-12-21 13:54:03 +0000

    Twenty Twenty-One: Use `esc_url()` for the WordPress.org link in `footer.php`.
    Props mukesh27, poena.
    Merges [49762] into the 5.6 branch.
    Fixes #51954.
    Built from https://develop.svn.wordpress.org/branches/5.6@49853
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@49572 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6.1-alpha-49852';
+$wp_version = '5.6.1-alpha-49853';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
