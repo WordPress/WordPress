f508fe5112781e56b1b297263581960b7a8cc028
John Blackbourn <johnbillion@git.wordpress.org>
2020-12-21 17:51:07 +0000

    Build/Test Tools: Don't mark the `Tests_Cache::test_flush()` test as risky when running the test suite with an object cache in place.
    See #51802
    
    Built from https://develop.svn.wordpress.org/trunk@49857
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49576 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-49856';
+$wp_version = '5.7-alpha-49857';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
