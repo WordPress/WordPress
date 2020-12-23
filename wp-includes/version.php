9f4be0f6987d72d7ccf6bc62078ebb25c43f4a93
Aaron Jorbin <aaron@jorb.in>
2020-12-23 18:19:05 +0000

    Build/Test: Send Code Coverage reports to Codecov.
    Help visualize and explore code coverage reports to avoid blunders.
    
    Props desrosj, swissspidy.
    Fixes #52141.
    
    
    Built from https://develop.svn.wordpress.org/trunk@49903
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49602 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-49902';
+$wp_version = '5.7-alpha-49903';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
