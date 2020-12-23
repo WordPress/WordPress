c50ff2ac10a3b4861d0ab5b4aa74f21a74db8c5e
Peter Wilson <wilson@peterwilson.cc>
2020-12-23 03:05:06 +0000

    Query: Add bad path tests with invalid `WP_Query` parameters.
    See #48556.
    
    Built from https://develop.svn.wordpress.org/trunk@49900
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49599 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-49899';
+$wp_version = '5.7-alpha-49900';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
