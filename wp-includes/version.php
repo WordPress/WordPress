fa88bbfc793989b9f4b967182f10f7512e1f123d
TimothyBlynJacobs <TimothyBlynJacobs@git.wordpress.org>
2021-01-02 21:35:04 +0000

    App Passwords: Only attempt auth if the username and password are set.
    Previously, only the username was checked which caused a PHP warning in some server setups, for instance Shibboleth SSO, where the server only populates the `PHP_AUTH_USER` field.
    
    Props MadtownLems, johnbillion, richard.tape, engahmeds3ed.
    Fixes #52003.
    
    Built from https://develop.svn.wordpress.org/trunk@49919
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49618 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-49918';
+$wp_version = '5.7-alpha-49919';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
