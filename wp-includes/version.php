0147765a7c492e1736402cc0b1622c32de04ef9c
desrosj <desrosj@git.wordpress.org>
2021-01-05 15:34:26 +0000

    Build/Test Tools: Update several NPM packages.
    This updates the following packages to the latest versions. 
    
    Updated packages:
    - `grunt-contrib-uglify` from `4.0.1` to `5.0.0`.
    - `grunt-jsdoc` from `2.4.0` to `2.4.1`.
    - `grunt-legacy-util` from `1.1.1` to `2.0.0`.
    - `grunt-webpack` from `3.1.3` to `4.0.2`.
    
    See #51801.
    Built from https://develop.svn.wordpress.org/trunk@49933
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49632 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-49932';
+$wp_version = '5.7-alpha-49933';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
