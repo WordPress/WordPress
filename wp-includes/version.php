60a2f144ac477723eaba4036824e16ab2dc34da3
desrosj <desrosj@git.wordpress.org>
2021-01-06 14:53:40 +0000

    Build/Test Tools: Update several NPM packages.
    This updates the following packages to the latest versions. 
    
    Updated packages:
    - `copy-webpack-plugin` from `5.1.1` to `5.1.2`.
    - `grunt` from `1.1.0` to `1.3.0`.
    - `grunt-contrib-jshint` from `2.1.0` to `3.0.0`.
    - `grunt-contrib-qunit` from `3.1.0` to `4.0.0`.
    - `grunt-rtlcss` from `2.0.1` to `2.0.2`.
    - `qunit` from `2.9.0` to `2.13.0`.
    - `sinon` from `9.0.0` to `9.2.2`.
    - `source-map-loader` from `0.2.4` to `1.1.3`.
    - `uuid` from `8.2.0` to `8.3.2`.
    - `webpack-dev-server` from `3.11.0` to `3.11.1`.
    
    See #51801.
    Built from https://develop.svn.wordpress.org/trunk@49939
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49638 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-49938';
+$wp_version = '5.7-alpha-49939';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
