1a32196979f3f2978763cec235d1c0e82cd24362
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2021-01-21 14:24:54 -0500

    Some changes after adjustments.

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.4-alpha-20210120.112313';
+$wp_version = '5.5.4-alpha-20210121.142252';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
