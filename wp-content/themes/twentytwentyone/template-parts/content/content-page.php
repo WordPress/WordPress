334c279d5b01fa5a83e530bc47c32fe78d75ba6c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-01-21 12:10:01 +0000

    Twenty Twenty-One: Add missing HTML comments for `</header>` tag.
    This brings some consistency to closing `</header>` tags in theme templates and ensures they all have an associated HTML comment.
    
    Props freewebmentor, mukesh27.
    Fixes #52328.
    Built from https://develop.svn.wordpress.org/trunk@49993
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49694 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwentyone/template-parts/content/content-page.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-page.php
@@ -17,11 +17,11 @@
 		<header class="entry-header alignwide">
 			<?php get_template_part( 'template-parts/header/entry-header' ); ?>
 			<?php twenty_twenty_one_post_thumbnail(); ?>
-		</header>
+		</header><!-- .entry-header -->
 	<?php elseif ( has_post_thumbnail() ) : ?>
 		<header class="entry-header alignwide">
 			<?php twenty_twenty_one_post_thumbnail(); ?>
-		</header>
+		</header><!-- .entry-header -->
 	<?php endif; ?>
 
 	<div class="entry-content">
--- wp-content/themes/twentytwentyone/template-parts/content/content-single.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-single.php
@@ -16,7 +16,7 @@
 	<header class="entry-header alignwide">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<?php twenty_twenty_one_post_thumbnail(); ?>
-	</header>
+	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
