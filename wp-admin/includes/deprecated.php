2259adf433d61fef8b490475bf7fd1b9c7b1ba7b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-01-24 13:59:58 +0000

    Docs: Document globals in `dropdown_categories()` and `dropdown_link_categories()`.
    Props umangvaghela123, pawki07.
    Fixes #41504.
    Built from https://develop.svn.wordpress.org/trunk@50008
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49709 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/deprecated.php
+++ wp-admin/includes/deprecated.php
@@ -75,6 +75,8 @@ function get_udims( $width, $height ) {
  * @deprecated 2.6.0 Use wp_category_checklist()
  * @see wp_category_checklist()
  *
+ * @global int $post_ID
+ *
  * @param int $default       Unused.
  * @param int $parent        Unused.
  * @param array $popular_ids Unused.
@@ -91,6 +93,8 @@ function dropdown_categories( $default = 0, $parent = 0, $popular_ids = array() 
  * @since 2.1.0
  * @deprecated 2.6.0 Use wp_link_category_checklist()
  * @see wp_link_category_checklist()
+ *
+ * @global int $link_id
  *
  * @param int $default Unused.
  */
