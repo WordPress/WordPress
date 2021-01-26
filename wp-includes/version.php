1c26077cf100be31e28aa37824eaf4a419221881
desrosj <desrosj@git.wordpress.org>
2021-01-26 15:18:33 +0000

    Build/Test Tools: Update several dependencies in the 5.6 branch.
    This backports several dependency updates to the 5.6 branch as a part of #52341 to allow all branches receiving security updates to run on NodeJS 14.x.
    
    Backports [49933,49937,49939,49940,49983,49989,49990,50016,50017] to the 5.6 branch.
    See #51801, #52341.
    Built from https://develop.svn.wordpress.org/branches/5.6@50023
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@49724 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6.1-alpha-50004';
+$wp_version = '5.6.1-alpha-50023';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
