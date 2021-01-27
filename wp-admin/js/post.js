c02d41fd63ac5e2d9754517ecd430e259da648b9
whyisjake <whyisjake@git.wordpress.org>
2021-01-27 19:52:00 +0000

    Editor: Fix improper triggering of the "Are you sure" prompt when navigating away from the old, "classic" Edit Post screen and there are no changes. Was triggered when there is an instance of TinyMCE in the Excerpt postbox.
    This brings the changes from [49807] to the 5.6 branch.
    
    Props rodrigosprimo, jonathanstegall, kevin940726, azaozz, metalandcoffee, ifnoob.
    
    Fixes #52038.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.6@50031
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@49732 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/post.js
+++ wp-admin/js/post.js
@@ -488,12 +488,20 @@ jQuery(document).ready( function($) {
 	 * When the user is trying to load another page, or reloads current page
 	 * show a confirmation dialog when there are unsaved changes.
 	 */
-	$(window).on( 'beforeunload.edit-post', function() {
-		var editor = typeof tinymce !== 'undefined' && tinymce.get('content');
+	$( window ).on( 'beforeunload.edit-post', function( event ) {
+		var editor  = window.tinymce && window.tinymce.get( 'content' );
+		var changed = false;
 
-		if ( ( editor && ! editor.isHidden() && editor.isDirty() ) ||
-			( wp.autosave && wp.autosave.server.postChanged() ) ) {
+		if ( wp.autosave ) {
+			changed = wp.autosave.server.postChanged();
+		} else if ( editor ) {
+			changed = ( ! editor.isHidden() && editor.isDirty() );
+		}
 
+		if ( changed ) {
+			event.preventDefault();
+			// The return string is needed for browser compat.
+			// See https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event.
 			return __( 'The changes you made will be lost if you navigate away from this page.' );
 		}
 	}).on( 'unload.edit-post', function( event ) {
