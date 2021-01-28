5ede1378347cef4616bf65208053bb6005f83e5c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-01-28 10:38:00 +0000

    Coding Standards: Simplify a long condition in `WP_Site_Health_Auto_Updates::test_wp_version_check_attached()` for better readability.
    Follow-up to [50035].
    
    Merges [50049] to the 5.6 branch.
    See #52135.
    Built from https://develop.svn.wordpress.org/branches/5.6@50050
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@49751 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6.1-alpha-50047';
+$wp_version = '5.6.1-alpha-50050';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
