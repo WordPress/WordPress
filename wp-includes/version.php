3563d7245536da002a182032dac46273c2dc47be
whyisjake <whyisjake@git.wordpress.org>
2021-01-28 17:24:59 +0000

    Site Health: Update the language around how PHP should be updated.
    Follow-up to [50042].
    
    This is a backport of [50058] to the 5.6 branch.
    
    Props SergeyBiryukov.
    
    See #52327.
    
    Built from https://develop.svn.wordpress.org/branches/5.6@50059
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@49760 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6.1-alpha-50056';
+$wp_version = '5.6.1-alpha-50059';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
