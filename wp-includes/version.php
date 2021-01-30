317f7331fcaa15c0f7b87495a055b09689abd222
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-01-30 10:38:58 +0000

    Tests: Disable update checks while running REST API plugin installation tests.
    This prevents external HTTP requests that are not required for the tests in question and may interfere with the results in case of a timeout.
    
    Follow-up to [48242], [49491].
    
    Merges [49913] to the 5.5 branch.
    See #51669.
    Built from https://develop.svn.wordpress.org/branches/5.5@50084
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@49784 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.4-alpha-49878';
+$wp_version = '5.5.4-alpha-50084';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
