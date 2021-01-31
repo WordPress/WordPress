3a901bbc2204e42f7b586fbe07736b0c02c30884
John Blackbourn <johnbillion@git.wordpress.org>
2021-01-31 18:17:57 +0000

    Comments: Fix a coding standards issue introduced in [50109].
    See #33717
    
    Built from https://develop.svn.wordpress.org/trunk@50113
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49792 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-50112';
+$wp_version = '5.7-alpha-50113';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
