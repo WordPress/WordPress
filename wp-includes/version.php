c1c5bfae42769c4bbc502bc9473f262d799a0c44
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2021-02-01 14:18:58 -0500

    Changes after converting to Dart Sass.

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.4-alpha-20210125.124306';
+$wp_version = '5.5.4-alpha-20210201.141511';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
