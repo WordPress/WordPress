2896790d57b31321e88457dc2be3b6acc25a0ac3
desrosj <desrosj@git.wordpress.org>
2021-02-02 19:40:04 +0000

    Twenty Twenty-One: Fix wrapping for long text within comments.
    This ensures that word wrapping occurs within comment content. 
    
    Props mayankmajeji, audrasjb.
    Fixes #52380.
    Built from https://develop.svn.wordpress.org/trunk@50158
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49837 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-50157';
+$wp_version = '5.7-alpha-50158';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
