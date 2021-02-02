ea3a1d782a233ea09edfd741832c22364fe04ca7
antpb <antpb@git.wordpress.org>
2021-02-02 18:29:05 +0000

    Twenty Twenty-One: Make transparent PNG logo visible on focus.
    This ensures a transparent logo remains visible while focused.
    
    Props bduclos, poena, paaljoachim, hellofromTonya.
    Fixes #52257.
    
    Built from https://develop.svn.wordpress.org/trunk@50154
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49833 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-50153';
+$wp_version = '5.7-alpha-50154';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
