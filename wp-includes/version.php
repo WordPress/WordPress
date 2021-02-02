286dd012126cb1abd566cc9fd5659f44fc153a9f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-02-02 22:23:03 +0000

    Post WordPress 5.7 Beta 1 version bump. Built from https://develop.svn.wordpress.org/trunk@50172
    git-svn-id: http://core.svn.wordpress.org/trunk@49851 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-beta1';
+$wp_version = '5.7-beta1-50172';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
