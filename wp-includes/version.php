56c9aa3cabced8ea80af7c9cec4f04c0d38b932d
Aaron Jorbin <aaron@jorb.in>
2021-02-02 20:28:08 +0000

    Administration: use shorthand css properties to improve readability
    Consolidating `border`, `padding`, and `margin` instances where the shorthand can be used to improve readability.
    
    Props ankitmaru, audrasjb, sabernhardt, mukesh27, hellofromTonya.
    Fixes #52148.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50162
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49841 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-alpha-50161';
+$wp_version = '5.7-alpha-50162';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
