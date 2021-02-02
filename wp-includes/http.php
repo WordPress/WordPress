54c18f74dac8011cd2d8189a2606f7da84ab3b02
Mikhail Kobzarev <mihdan@yandex.ru>
2021-02-02 14:25:58 +0300

    HTTP: Add `wp_remote_put()` method

--- wp-includes/http.php
+++ wp-includes/http.php
@@ -180,6 +180,23 @@ function wp_remote_post( $url, $args = array() ) {
 }
 
 /**
+ * Performs an HTTP request using the PUT method and returns its response.
+ *
+ * @since 5.7
+ *
+ * @see wp_remote_request() For more information on the response array format.
+ * @see WP_Http::request() For default arguments information.
+ *
+ * @param string $url  URL to retrieve.
+ * @param array  $args Optional. Request arguments. Default empty array.
+ * @return array|WP_Error The response or WP_Error on failure.
+ */
+function wp_remote_put( $url, $args = array() ) {
+	$http = _wp_http_get_object();
+	return $http->put( $url, $args );
+}
+
+/**
  * Performs an HTTP request using the HEAD method and returns its response.
  *
  * @since 2.7.0
