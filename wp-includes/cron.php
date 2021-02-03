a47580b24752f9e9cb85b3faf01ff81072f52af7
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-02-03 10:58:07 +0000

    Docs: Add a `@since` note to `wp_clear_scheduled_hook()` for the `$wp_error` parameter.
    Follow-up to [50143].
    
    See #49961.
    Built from https://develop.svn.wordpress.org/trunk@50174
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49853 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/cron.php
+++ wp-includes/cron.php
@@ -529,12 +529,13 @@ function wp_unschedule_event( $timestamp, $hook, $args = array(), $wp_error = fa
  * @since 2.1.0
  * @since 5.1.0 Return value modified to indicate success or failure,
  *              {@see 'pre_clear_scheduled_hook'} filter added to short-circuit the function.
+ * @since 5.7.0 The `$wp_error` parameter was added.
  *
- * @param string $hook Action hook, the execution of which will be unscheduled.
- * @param array  $args Optional. Array containing each separate argument to pass to the hook's callback function.
- *                     Although not passed to a callback, these arguments are used to uniquely identify the
- *                     event, so they should be the same as those used when originally scheduling the event.
- * @param bool   $wp_error  Optional. Whether to return a WP_Error on failure. Default false.
+ * @param string $hook     Action hook, the execution of which will be unscheduled.
+ * @param array  $args     Optional. Array containing each separate argument to pass to the hook's callback function.
+ *                         Although not passed to a callback, these arguments are used to uniquely identify the
+ *                         event, so they should be the same as those used when originally scheduling the event.
+ * @param bool   $wp_error Optional. Whether to return a WP_Error on failure. Default false.
  * @return int|false|WP_Error On success an integer indicating number of events unscheduled (0 indicates no
  *                            events were registered with the hook and arguments combination), false or WP_Error
  *                            if unscheduling one or more events fail.
