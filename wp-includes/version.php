2ca808a94c776d7073fbaa6f99cbedd57f32def4
desrosj <desrosj@git.wordpress.org>
2021-02-03 14:55:25 +0000

    Build/Test Tools: Update NPM dependencies
    This updates two dependencies to their latest versions:
    - `uglify-js` from `3.12.5` to `3.12.6`.
    - `sass` from `1.32.5` to `1.32.6`.
    
    See #51801.
    Built from https://develop.svn.wordpress.org/trunk@50176
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49855 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-beta1-50175';
+$wp_version = '5.7-beta1-50176';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
