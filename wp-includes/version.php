8cfd634243e816651dd2273bd991cfe1bc1c6fce
desrosj <desrosj@git.wordpress.org>
2021-02-05 02:23:18 +0000

    Build/Test Tools: Replace node-sass with Dart Sass
    Switch the Sass implementation used by Grunt to sass (the Dart version), since LibSass & node-sass have been deprecated.
    
    Additionally, this updates the `uglify-js` dependency.
    
    Merges [50126] and [50176] to the 5.6 branch.
    See #51763, #51801.
    Built from https://develop.svn.wordpress.org/branches/5.6@50184
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@49862 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6.2-alpha-50180';
+$wp_version = '5.6.2-alpha-50184';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
