2ffff78f512e46c60ab136e62899b58f838d0568
Peter Wilson <wilson@peterwilson.cc>
2021-02-09 03:44:06 +0000

    Twenty Twenty-One: Adapt vertical buttons to text length.
    Display vertically aligned buttons as inline elements.
    
    Props poena, paaljoachim, hellofromTonya, jeroenrotty, rolfsiebers.
    Fixes #52432.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50261
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49906 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-beta1-50260';
+$wp_version = '5.7-beta1-50261';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
