165413db2bc8c03550ca64420f8d75ed77b2874a
Peter Wilson <wilson@peterwilson.cc>
2021-02-09 03:24:05 +0000

    Twenty Twenty-One: Support font size option for code block.
    Props poena, paaljoachim, hellofromTonya.
    See #52431.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50260
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49905 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-beta1-50259';
+$wp_version = '5.7-beta1-50260';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
