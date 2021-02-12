fde749f4e4ebabd39384ce40cd827c9212eca36f
gziolo <gziolo@git.wordpress.org>
2021-02-12 16:41:03 +0000

    Tests: Fix failing unit test for block settings
    Follow-up to [50297].
    
    Fixes #52512.
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@50300
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49945 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-beta2-50299';
+$wp_version = '5.7-beta2-50300';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
