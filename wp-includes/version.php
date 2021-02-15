0bbe650e69abc616253a148226639e753d98cc81
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-02-15 14:02:07 +0000

    Tests: Add missing `@covers` tags for files in `phpunit/tests/http/`.
    Props patopaiar, jrf.
    See #39265.
    Built from https://develop.svn.wordpress.org/trunk@50344
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49955 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-beta2-50343';
+$wp_version = '5.7-beta2-50344';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
