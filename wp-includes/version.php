ec38e1434aba7ee9c14d0f5d040f9eed19ceaef7
ryelle <ryelle@git.wordpress.org>
2021-02-15 18:33:05 +0000

    Twenty Thirteen: Fix alignment of child blocks within the Cover block.
    Props kjellr, melchoyce.
    Fixes #51224.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50346
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49957 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-beta2-50345';
+$wp_version = '5.7-beta2-50346';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
