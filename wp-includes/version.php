455bc37c685cc2ac52e55c12e503e1d43cdfd48c
ryelle <ryelle@git.wordpress.org>
2021-02-16 20:12:02 +0000

    Twenty Twenty-One: Remove background color from Social Links the dark gray style.
    This block variation doesn't support custom colors, so allowing a background color leads to a mismatch between frontend and editor styles.
    
    Props poena, paaljoachim, audrasjb.
    Fixes #52499.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50364
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49975 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-beta2-50363';
+$wp_version = '5.7-beta2-50364';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
