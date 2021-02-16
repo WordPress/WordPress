b50e49f5c1dbe8655bb8fb45188f099464d30686
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-02-16 20:04:07 +0000

    Accessibility: Taxonomy: Correct keyboard navigation for the Tags meta box in classic editor.
    Props sabernhardt, almendron, jose64, alexstine, joedolson, ryokuhi.
    Fixes #52408.
    Built from https://develop.svn.wordpress.org/trunk@50363
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49974 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-beta2-50362';
+$wp_version = '5.7-beta2-50363';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
