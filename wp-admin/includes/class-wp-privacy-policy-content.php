dcbe3cb7b817e1949681a3d5294adafa5df6dea3
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-02-17 12:45:04 +0000

    Privacy: Suggest `wp_add_privacy_policy_content()` as an alternative for the deprecated `wp_get_default_privacy_policy_content` hook.
    Follow-up to [50161].
    
    See #49264.
    Built from https://develop.svn.wordpress.org/trunk@50374
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49985 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-privacy-policy-content.php
+++ wp-admin/includes/class-wp-privacy-policy-content.php
@@ -669,15 +669,19 @@ final class WP_Privacy_Policy_Content {
 		 *
 		 * @since 4.9.6
 		 * @since 5.0.0 Added the `$strings`, `$description`, and `$blocks` parameters.
-		 *
-		 * @deprecated 5.7.0 This filter has been deprecated.
+		 * @deprecated 5.7.0 Use wp_add_privacy_policy_content() instead.
 		 *
 		 * @param string   $content     The default policy content.
 		 * @param string[] $strings     An array of privacy policy content strings.
 		 * @param bool     $description Whether policy descriptions should be included.
 		 * @param bool     $blocks      Whether the content should be formatted for the block editor.
 		 */
-		return apply_filters_deprecated( 'wp_get_default_privacy_policy_content', array( $content, $strings, $description, $blocks ), '5.7.0', false );
+		return apply_filters_deprecated(
+			'wp_get_default_privacy_policy_content',
+			array( $content, $strings, $description, $blocks ),
+			'5.7.0',
+			'wp_add_privacy_policy_content()'
+		);
 	}
 
 	/**
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-beta3-50373';
+$wp_version = '5.7-beta3-50374';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
