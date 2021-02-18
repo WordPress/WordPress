9bc5ccb4a5761b9c0829f885a04b4f8ed9d3476c
Peter Wilson <wilson@peterwilson.cc>
2021-02-18 01:13:02 +0000

    Editor: Additional tests for reusable blocks.
    Ensure that subsequent renders of a reusable block will render correctly and that recursively inserting a reusable block into itself does not cause an internal server (500) error.
    
    Props bernhard-reiter, SergeyBiryukov.
    Fixes #52364.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50382
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@49993 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-beta3-50381';
+$wp_version = '5.7-beta3-50382';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
