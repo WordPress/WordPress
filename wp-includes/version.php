5ba4f4cee531c270f9f01c05f0fc3acd4a0537e7
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-02-23 21:38:04 +0000

    Security: Update the Security Policy to include WordPress 5.7.
    Fixes #52633.
    Built from https://develop.svn.wordpress.org/trunk@50428
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50039 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50427';
+$wp_version = '5.8-alpha-50428';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
