b231afc0b77d00833862a827898a5e3f25d09585
desrosj <desrosj@git.wordpress.org>
2021-02-24 15:51:06 +0000

    Help/About: Remove extra space in the 5.7 About page copy.
    Props tobifjellner, audrasjb.
    Fixes #52632.
    Built from https://develop.svn.wordpress.org/trunk@50430
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50041 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50429';
+$wp_version = '5.8-alpha-50430';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
