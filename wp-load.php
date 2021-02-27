1a7d246fccb5ebdfc1d58b3f6be43ccb9bc3a796
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-02-27 11:09:07 +0000

    Bootstrap/Load: Check if the `error_reporting()` function exists in `wp-load.php`.
    This avoids a fatal error on PHP 8 if `error_reporting()` is disabled in `php.ini`.
    
    On systems with this function disabled, it's best to add a dummy function to the `wp-config.php` file, as there are multiple other calls in core or plugins.
    
    However, as this call to the function is run prior to `wp-config.php` loading, it is now wrapped in a `function_exists()` check.
    
    Props peterwilsoncc, fijisunshine, ayeshrajans.
    Fixes #52226.
    Built from https://develop.svn.wordpress.org/trunk@50447
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50058 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-load.php
+++ wp-load.php
@@ -21,7 +21,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
 
-error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
+/*
+ * The error_reporting() function can be disabled in php.ini. On systems where that is the case,
+ * it's best to add a dummy function to the wp-config.php file, but as this call to the function
+ * is run prior to wp-config.php loading, it is wrapped in a function_exists() check.
+ */
+if ( function_exists( 'error_reporting' ) ) {
+	error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
+}
 
 /*
  * If wp-config.php exists in the WordPress root, or if it exists in the root and wp-settings.php
