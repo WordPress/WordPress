3cdbafe16408c54c8f6bad02cb94df9d55a04bd1
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-03-02 21:38:03 +0000

    Help/About: Clarify a string about lazy-loading iframes.
    Props davidbaumwald, audrasjb, hellofromTonya.
    See #52347.
    Built from https://develop.svn.wordpress.org/trunk@50483
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50094 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50479';
+$wp_version = '5.8-alpha-50483';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
