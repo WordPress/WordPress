4d2abda74a8bb70b65b94bd3d03bb5ac10d2fb3c
Peter Wilson <wilson@peterwilson.cc>
2021-03-04 00:14:04 +0000

    Roles/Caps: Return same result from `current_user_can` and `user_can()`.
    Ensure `current_user_can()` and `user_can()` return the same results for logged out users. For core capabilities this changes `user_can( 0, 'exist' )` to return `true` rather than `false` in line with `current_user_can( 'exist' )` for logged out users.
    
    Convert `current_user_can()` and `current_user_can_for_blog()` to wrapper functions ultimately calling `user_can()`.
    
    Add anonymous user to primitive capability checks as appropriate. Convert `Tests_User_Capabilities::test_other_caps_for_all_roles()` to use a data provider and add tests to check whether user exists in the database (`WP_User::exists()`) as that intentionally differs from the `exist` capability.
    
    Props jjj, johnbillion, peterwilsoncc, SergeyBiryukov, TimothyBlynJacobs.
    Fixes #52076.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50490
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50103 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-user.php
+++ wp-includes/class-wp-user.php
@@ -167,6 +167,9 @@ class WP_User {
 	 * @param int    $site_id Optional. The site ID to initialize for.
 	 */
 	public function init( $data, $site_id = '' ) {
+		if ( ! isset( $data->ID ) ) {
+			$data->ID = 0;
+		}
 		$this->data = $data;
 		$this->ID   = (int) $data->ID;
 
