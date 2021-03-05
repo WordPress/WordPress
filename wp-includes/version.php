5737a63b49cb474fdae335ee322a5516a0e418d8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-03-05 20:11:03 +0000

    WordPress 5.7 RC3. Built from https://develop.svn.wordpress.org/branches/5.7@50502
    git-svn-id: http://core.svn.wordpress.org/branches/5.7@50115 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7-RC2-50500';
+$wp_version = '5.7-RC3';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
