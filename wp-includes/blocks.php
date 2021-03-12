fb8c18e54198ad9364213d792ea39b2008d36725
davidbaumwald <davidbaumwald@git.wordpress.org>
2021-03-12 20:05:08 +0000

    Docs: Correct `unregister_block_style` @param for `$block_name`.
    In `unregister_block_style`, the `$block_name` parameter was documented as an array.  This change corrects the type to `string`, as that's what is expected in `WP_Block_Styles_Registry->unregister`. 
    
    Props kraftner.
    Fixes #52795.
    Built from https://develop.svn.wordpress.org/trunk@50528
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50141 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks.php
+++ wp-includes/blocks.php
@@ -902,7 +902,7 @@ function register_block_style( $block_name, $style_properties ) {
  * @since 5.3.0
  *
  * @param string $block_name       Block type name including namespace.
- * @param array  $block_style_name Block style name.
+ * @param string $block_style_name Block style name.
  * @return bool True if the block style was unregistered with success and false otherwise.
  */
 function unregister_block_style( $block_name, $block_style_name ) {
