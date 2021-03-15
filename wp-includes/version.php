5d983ec85b94c2fe33af8367059fd1e61f522bbe
ryelle <ryelle@git.wordpress.org>
2021-03-15 19:51:09 +0000

    Help/About: Run image comparison script after all assets have loaded.
    If the callback is called before the images are loaded, the comparison doesn't work. `wp.domReady` uses `DOMContentLoaded`, which does not guarantee that the images are loaded. This switches to use the `load` event, which waits for all resources to load.
    
    Props vladytimy, joyously, sergeybiryukov.
    Fixes #52758.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50535
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50148 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50534';
+$wp_version = '5.8-alpha-50535';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
