f9cc35ebad82753e9c86de322ea5c76a9001c7e2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-03-15 13:26:06 +0000

    Twenty Seventeen: Remove extra space around post editor.
    The editor uses the full height of its container, so the added margins cause the visual editor to break out of the container. This removes the top & bottom margins, and makes the left & right margins consistent with the meta box area.
    
    Follow-up to [50465].
    
    Props joseeyoast, audrasjb.
    Fixes #52816.
    Built from https://develop.svn.wordpress.org/trunk@50534
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50147 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50533';
+$wp_version = '5.8-alpha-50534';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
