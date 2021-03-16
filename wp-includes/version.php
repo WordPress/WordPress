4d514bb6ad0af5569113c5f679bee33e8e153d82
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-03-16 18:46:07 +0000

    Tests: Correct some newly introduced `@covers` tags.
    Follow-up to [50289], [50344].
    
    Props jrf.
    See #39265.
    Built from https://develop.svn.wordpress.org/trunk@50537
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50150 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50535';
+$wp_version = '5.8-alpha-50537';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
