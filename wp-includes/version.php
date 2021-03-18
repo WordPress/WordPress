3a92b15f936f8909b43d72cc30d205db522439f9
desrosj <desrosj@git.wordpress.org>
2021-03-18 15:50:18 +0000

    External Libraries: Update jQuery Color library to version `2.2.0`.
    This updates the `jquery-color` library from version `2.1.2` to `2.2.0`.
    
    This also switches back to installing the dependency from NPM instead of GitHub. The maintainers have resumed publishing to NPM with the 2.2.0 release.
    
    A full list of changes can be found on GitHub: https://github.com/jquery/jquery-color/compare/2.1.2...2.2.0.
    
    Props hareesh-pillai, desrosj.
    Fixes #51405.
    Built from https://develop.svn.wordpress.org/trunk@50543
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50156 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50542';
+$wp_version = '5.8-alpha-50543';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
