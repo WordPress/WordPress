37d01e03466262b0679e8cbbcb794d4a699a455b
Peter Wilson <wilson@peterwilson.cc>
2021-03-19 02:13:05 +0000

    Media: Pass the appropriate reference into `wp_getimagesize`.
    With changes that were introduced in #49889 the second parameter for getimagesize() function is expecting a a reference.
    
    Previously, most calls did not pass the 2nd param, and as a result, we are getting unexpected results.
    
    This was only a problem with applications that are using a custom stream wrapper, and the image contained EXIF data.
    
    For more see:
    
        https://github.com/humanmade/S3-Uploads/issues/496
        https://github.com/aws/aws-sdk-php/issues/1923 
    
    Fixes #52826.
    Merges [50552] to the 5.7 branch.
    Props terriann, SergeyBiryukov, Mista-Flo, hellofromTonya, rinatkhaziev, whyisjake.
    
    Built from https://develop.svn.wordpress.org/branches/5.7@50553
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.7@50166 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/image.php
+++ wp-admin/includes/image.php
@@ -711,6 +711,7 @@ function wp_read_image_metadata( $file ) {
 	);
 
 	$iptc = array();
+	$info = array();
 	/*
 	 * Read IPTC first, since it might contain data not available in exif such
 	 * as caption, description etc.
