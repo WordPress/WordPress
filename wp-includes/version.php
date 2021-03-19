b4c09beba832fedf4f333f4bb93c5a5d32671328
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-03-19 15:21:08 +0000

    I18N: Remove duplicate entries from the list of continents and cities.
    Follow-up to [37554].
    
    See #52861.
    Built from https://develop.svn.wordpress.org/trunk@50554
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50167 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50552';
+$wp_version = '5.8-alpha-50554';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
