0ee244739ac508ac5c6fcfe8150f00f35c1d221d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-03-22 22:42:04 +0000

    Code Modernization: Correct expected data type for `WP_User_Search::$page` property.
    This fixes erroneous parentheses placement and applies the type cast to the variable it was intended for.
    
    Follow-up to [3864].
    
    Props hellofromTonya, jrf, xknown.
    See #51423.
    Built from https://develop.svn.wordpress.org/trunk@50563
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50176 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50562';
+$wp_version = '5.8-alpha-50563';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
