7d19c6d338a4b460669ed2b9017a93f44cbd3e88
Peter Wilson <wilson@peterwilson.cc>
2021-03-25 00:07:04 +0000

    Privacy: Wrap text in buttons on privacy policy guide.
    On narrow screens allow the text to wrap in the copy buttons on the privacy policy guide screen to avoid horizontal overflow of the parent container.
    
    Props audrasjb, davidbaumwald, jaymanpandya, paaljoachim, palmiak, sabernhardt, SergeyBiryukov, sumitsingh.
    Merges [50568] to the 5.7 branch.
    Fixes #52751.
    
    Built from https://develop.svn.wordpress.org/branches/5.7@50574
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.7@50187 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7.1-alpha-50573';
+$wp_version = '5.7.1-alpha-50574';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
