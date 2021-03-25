2e34561536fe72f6c3a176b1bae4ce85cc7b2b62
desrosj <desrosj@git.wordpress.org>
2021-03-25 15:10:06 +0000

    Build/Test Tools: Disable `fail-fast` for the NPM testing workflow.
    This disables the `fail-fast` option in the GitHub Actions workflow for testing NPM. This allows all jobs in the test matrix to complete, even if one fails, painting a clear picture of where the issue lies.
    
    Follow up to [50435].
    
    See #52625.
    Built from https://develop.svn.wordpress.org/trunk@50579
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50192 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50577';
+$wp_version = '5.8-alpha-50579';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
