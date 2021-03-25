6bc61cc749b5a1e2b884f439d8100c26e3b1a122
ryelle <ryelle@git.wordpress.org>
2021-03-25 15:33:02 +0000

    Login, Registration: Prevent button misalignment on login screen.
    Replace the CSS removed in [50569], which is used to align the login & register buttons.
    
    Props mukesh27, audrasjb.
    Reviewed by SergeyBiryukov.
    Merges [50577] to the 5.7 branch.
    Fixes #52834.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.7@50580
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.7@50193 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7.1-alpha-50578';
+$wp_version = '5.7.1-alpha-50580';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
