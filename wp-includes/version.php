952acb240a6866db4fbe9a2535cccf976a5caa5f
desrosj <desrosj@git.wordpress.org>
2021-03-26 13:25:05 +0000

    Build/Test Tools: Fix code coverage reporting to generate report from `src`.
    Since [50441-50442] switched the test workflows to run from `src` instead of `build`, code coverage reporting has stopped working. This was caused by the code coverage configuration continuing to reference `build`. This corrects the configuration so coverage reporting can resume.
    
    This change also introduces the `workflow_dispatch` event to the workflow, which will allow committers to manually run the workflow when desired. For example, to confirm changes to the test suite do not break reporting.
    
    This also adds the `phpunit.xml.dist` and `tests/phpunit/multisite.xml` files to the `paths` list. Since these files are responsible for configuring the test suite and code coverage reporting, any changes to them should verify that no problems were introduced.
    
    Props jrf, johnbillion.
    Fixes #52786. See #51734.
    Built from https://develop.svn.wordpress.org/trunk@50592
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50205 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50590';
+$wp_version = '5.8-alpha-50592';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
