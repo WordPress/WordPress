755882578ee6014c77be23c9be0a8cc6bca22565
desrosj <desrosj@git.wordpress.org>
2021-03-26 15:28:05 +0000

    Twenty Twelve: Change theme version back to `3.3`.
    Because the changes in Twenty Twelve did not impact site look or functionality, version 3.4 of Twenty Twelve was not released in coordination with WordPress 5.7.
    
    This reverts the portion of [50508] relevant to Twenty Twelve, changing the version back to 3.3 so that version 3.4 is not skipped accidentally when there are changes that should be released.
    
    See #52704.
    Built from https://develop.svn.wordpress.org/trunk@50596
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50209 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50595';
+$wp_version = '5.8-alpha-50596';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
