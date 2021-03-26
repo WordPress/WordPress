2a76425048771a27b779250a477abe31013fcb8f
desrosj <desrosj@git.wordpress.org>
2021-03-26 15:11:04 +0000

    Build/Test Tools: Run code coverage workflow using parallel jobs.
    This splits the code coverage workflow into two separate jobs, allowing single site and multisite to run in parallel. This cuts the total workflow run time in half.
    
    Fixes #52923.
    Built from https://develop.svn.wordpress.org/trunk@50594
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50207 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50593';
+$wp_version = '5.8-alpha-50594';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
