59d32bfe24cee58554ac82573d8a69dd6b6749ab
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-03-29 18:07:25 +0000

    Build/Test Tools: Remove explicit puppeteer dependency.
    This was added in [48177] to fix an issue where Puppeteer was not being installed correctly as a dependency of `@wordpress/wp-scripts`. This has been fixed, so this explicit dependency can be removed.
    
    Props isabel_brison, desrosj, SergeyBiryukov.
    Fixes #52843.
    Built from https://develop.svn.wordpress.org/trunk@50612
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50225 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50611';
+$wp_version = '5.8-alpha-50612';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
