1a9eb60625ea4dfd7d469329d75336dd562dcd32
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-03-30 14:24:03 +0000

    Docs: Add documentation for the `::setup_export_contents_test` method used in personal data export tests.
    Follow-up to [50613].
    
    Props hellofromTonya.
    See #51423.
    Built from https://develop.svn.wordpress.org/trunk@50616
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50229 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50615';
+$wp_version = '5.8-alpha-50616';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
