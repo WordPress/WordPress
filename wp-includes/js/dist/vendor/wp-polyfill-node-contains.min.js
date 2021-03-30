7eee8521b0d66a6471c23c21f984de703af3cafd
desrosj <desrosj@git.wordpress.org>
2021-03-30 14:16:33 +0000

    External Libraries: Update several polyfill libraries.
    This updates the following polyfill libraries:
    
    - `formdata-polyfill` from `3.0.13` to `3.0.20`.
    - `objectFitPolyfill` from `2.3.4` to `2.3.5`.
    - `polyfill-library` from `3.42.0` to `3.104.0`.
    
    Props gziolo, desrosj.
    Fixes #52854.
    Built from https://develop.svn.wordpress.org/trunk@50615
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50228 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js
+++
@@ -1,1 +0,0 @@
-!function(){function t(t){if(!(0 in arguments))throw new TypeError("1 argument is required");do{if(this===t)return!0}while(t=t&&t.parentNode);return!1}if("HTMLElement"in this&&"contains"in HTMLElement.prototype)try{delete HTMLElement.prototype.contains}catch(t){}"Node"in this?Node.prototype.contains=t:document.contains=Element.prototype.contains=t}();
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50614';
+$wp_version = '5.8-alpha-50615';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
