3912ca99fd27901d0f996b4790a5de72b3405fa1
davidbaumwald <davidbaumwald@git.wordpress.org>
2021-04-02 19:42:21 +0000

    External Libraries: Update Underscore to version 1.12.1.
    Full set of changes at https://github.com/jashkenas/underscore/compare/1.8.3...1.12.1.  
    The new version includes a `sourceMappingURL` that causes a build failure, so this change 
    also introduces a task to remove this from the source during the build.
    
    Props mukesh27, hareesh-pillai, desrosj, SergeyBiryukov, TimoTijhof.
    Fixes #45785.
    Built from https://develop.svn.wordpress.org/trunk@50650
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50262 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50648';
+$wp_version = '5.8-alpha-50650';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
