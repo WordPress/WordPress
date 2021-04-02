cc0ce2c91d9b59c131012f1d8d648705486bc42b
davidbaumwald <davidbaumwald@git.wordpress.org>
2021-04-02 16:08:04 +0000

    Editor: Update the default writing prompt to match the block editor.
    Recently merged, https://github.com/WordPress/gutenberg/pull/29611, changed the default writing prompt text.  WordPress core offers the `write_your_story` filter with a default value using the same text.  This change updates the default text to match the recent change in the block editor.
    
    Props Joen, SergeyBiryukov.
    Fixes #52948.
    Built from https://develop.svn.wordpress.org/trunk@50647
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50259 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/edit-form-blocks.php
+++ wp-admin/edit-form-blocks.php
@@ -301,11 +301,12 @@ if ( $user_id ) {
  * Filters the body placeholder text.
  *
  * @since 5.0.0
+ * @since 5.8.0 Changed the default placeholder text.
  *
- * @param string  $text Placeholder text. Default 'Start writing or type / to choose a block'.
+ * @param string  $text Placeholder text. Default 'Type / to choose a block'.
  * @param WP_Post $post Post object.
  */
-$body_placeholder = apply_filters( 'write_your_story', __( 'Start writing or type / to choose a block' ), $post );
+$body_placeholder = apply_filters( 'write_your_story', __( 'Type / to choose a block' ), $post );
 
 $editor_settings = array(
 	'alignWide'                            => $align_wide,
