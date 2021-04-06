caa5abc6c189969fcdedb0809661e673eba92c92
desrosj <desrosj@git.wordpress.org>
2021-04-06 14:07:07 +0000

    Build/Test Tools: Update dependencies in default themes.
    This updates several dependencies in the Twenty Nineteen, Twenty Twenty, and Twenty Twenty-One default themes.
    
    See #52624.
    Built from https://develop.svn.wordpress.org/trunk@50666
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50278 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50665';
+$wp_version = '5.8-alpha-50666';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
