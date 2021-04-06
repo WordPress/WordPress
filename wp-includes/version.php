7442fe63a010d3a34b4ff94677fce8839d717348
Peter Wilson <wilson@peterwilson.cc>
2021-04-06 03:24:08 +0000

    Site Health: Reduce false reports of HTTPS failures.
    Reduce severity of failing HTTPS tests from critical to warning. Stop reporting failures if the site is being accessed over HTTPS but `wp_is_https_supported()` indicates a lack of support.
    
    Props annalamprou, AnotherDave, ayeshrajans, bobbingwide, Clorith, dragongate, eatsleepcode, gab81, geoffrey1963, Ipstenu, k3nsai, mmuyskens, nicegamer7, peterwilsoncc, pwallner, SergeyBiryukov, TimothyBlynJacobs, Toru.
    Fixes #52783.
    
    Built from https://develop.svn.wordpress.org/trunk@50659
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50271 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50658';
+$wp_version = '5.8-alpha-50659';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
