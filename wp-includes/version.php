2c8182fcbab409737f47ff61f24f14ff343fed26
Peter Wilson <wilson@peterwilson.cc>
2021-04-06 03:44:02 +0000

    Media: Conditionally pass 2nd parameter to `getimagesize()`.
    In the wrapper function `wp_getimagesize()` check if the second parameter was passed before sending it to the PHP function `getimagesize()`.
    
    The PHP function has a different execution path depending on the number of parameters passed, this ensures the wrapper function follows the appropriate path.
    
    Follow up to [50552].
    Props azaozz, hellofromtonya, Mista-Flo, peterwilsoncc, rinatkhaziev, RogerTheriault, SergeyBiryukov, terriann, whyisjake.
    Merges [50586] to the 5.7 branch.
    Fixes #52826.
    
    Built from https://develop.svn.wordpress.org/branches/5.7@50662
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.7@50274 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7.1-alpha-50661';
+$wp_version = '5.7.1-alpha-50662';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
