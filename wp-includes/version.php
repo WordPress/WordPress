2b73649b52bb86a63ece2d36ae7a9957e5db9ac9
desrosj <desrosj@git.wordpress.org>
2021-04-06 14:31:21 +0000

    Build/Test Tools: Update some dependencies.
    This updates `elliptic` to version `6.5.4` through `npm audit fix`.
    
    `uglify-js` is also updated to version `3.13.3`, and `grunt-contrib-uglify` to version `5.0.1`.
    
    See #52624.
    Built from https://develop.svn.wordpress.org/trunk@50668
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50280 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50667';
+$wp_version = '5.8-alpha-50668';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
