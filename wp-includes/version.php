c569c157f0400344786ce94744c49afce1566e77
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-04-11 13:44:13 +0000

    Coding Standards: Simplify the check for parent terms in `export_wp()`.
    This is more consistent with similar checks elsewhere in core.
    
    See #52627.
    Built from https://develop.svn.wordpress.org/trunk@50699
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50308 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50698';
+$wp_version = '5.8-alpha-50699';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
