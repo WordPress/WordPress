ac930e0e5f9134a008bc2c2a773a3b0d5c50fabb
desrosj <desrosj@git.wordpress.org>
2021-04-14 23:10:04 +0000

    External libraries: Include upstream GetID3 fix for PHP 8.
    Props jrf, xknown.
    Built from https://develop.svn.wordpress.org/trunk@50714
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50323 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50713';
+$wp_version = '5.8-alpha-50714';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
