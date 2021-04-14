ffc9a87780bacb732f761b9081435419243e879a
Peter Wilson <wilson@peterwilson.cc>
2021-04-14 23:40:18 +0000

    Editor: Update @wordpress npm packages for 5.7.1.
    Update @wordpress npm packages to the latest published versions for the 5.7 branch. This includes block editor bug fixes for WordPress 5.7.1.
    
    Props gziolo, noisysocks, isabel_brison, peterwilsoncc.
    Fixes #52912.
    
    Built from https://develop.svn.wordpress.org/branches/5.7@50719
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.7@50328 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7.1-RC1-50718';
+$wp_version = '5.7.1-RC1-50719';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
