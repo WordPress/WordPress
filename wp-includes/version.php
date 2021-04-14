e445660ee3c4aa204a152e0a6dbd250d1896abde
desrosj <desrosj@git.wordpress.org>
2021-04-14 23:24:04 +0000

    REST API: Allow authors to read their own password protected posts.
    Allow authenticated users to read the contents of password protected posts if they have the `edit_post` meta capability for the post.
    
    Props xknown, zieladam, peterwilsoncc, swissspidy, timothyblynjacobs.
    Built from https://develop.svn.wordpress.org/trunk@50717
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50326 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50714';
+$wp_version = '5.8-alpha-50717';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
