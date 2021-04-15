5ba41bd4f097c9bcd699736fc560686c0cad621e
Peter Wilson <wilson@peterwilson.cc>
2021-04-15 03:33:06 +0000

    Post 5.7.1 version bump.
    Built from https://develop.svn.wordpress.org/branches/5.7@50758
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.7@50367 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7.1';
+$wp_version = '5.7.2-alpha-50758';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
