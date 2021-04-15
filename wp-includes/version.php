c9370870aff9d322da03f3711e5911a40eb2f5c9
desrosj <desrosj@git.wordpress.org>
2021-04-15 01:08:03 +0000

    Grouped merges for 5.6.3.
    * REST API: Allow authors to read their own password protected posts.
    * About page update.
    
    Merges [50717] to the 5.6 branch.
    
    Built from https://develop.svn.wordpress.org/branches/5.6@50726
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@50335 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6.3-alpha-50721';
+$wp_version = '5.6.3-alpha-50726';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
