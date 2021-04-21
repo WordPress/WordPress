267f9d7479006b2eca7e63807347b341ade47ee7
gziolo <gziolo@git.wordpress.org>
2021-04-21 09:32:06 +0000

    Editor: Shape block editor filters to work better with the Gutenberg plugin
    This should allow to use new filters in the Gutenberg plugin and therefore it prevents deprecation warnings when in the debug mode.
    
    See #52920.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50777
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50386 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/block-editor.php
+++ wp-includes/block-editor.php
@@ -66,17 +66,18 @@ function get_default_block_categories() {
  */
 function get_block_categories( $editor_name_or_post ) {
 	// Assume the post editor when the WP_Post object passed.
-	$editor_name        = is_object( $editor_name_or_post ) ? 'post-editor' : $editor_name_or_post;
-	$default_categories = get_default_block_categories();
+	$editor_name      = is_object( $editor_name_or_post ) ? 'post-editor' : $editor_name_or_post;
+	$block_categories = get_default_block_categories();
 
 	/**
 	 * Filters the default array of categories for block types.
 	 *
 	 * @since 5.8.0
 	 *
-	 * @param array[] $default_categories Array of categories for block types.
+	 * @param array[] $block_categories Array of categories for block types.
+	 * @param string  $editor_name      The name of the editor, e.g. 'post-editor'.
 	 */
-	$block_categories = apply_filters( "block_categories_{$editor_name}", $default_categories );
+	$block_categories = apply_filters( 'block_categories_all', $block_categories, $editor_name );
 	if ( 'post-editor' === $editor_name ) {
 		$post = is_object( $editor_name_or_post ) ? $editor_name_or_post : get_post();
 
@@ -89,7 +90,7 @@ function get_block_categories( $editor_name_or_post ) {
 		 * @param array[] $block_categories Array of categories for block types.
 		 * @param WP_Post $post             Post being loaded.
 		 */
-		$block_categories = apply_filters_deprecated( 'block_categories', array( $block_categories, $post ), '5.8.0', "block_categories_{$editor_name}" );
+		$block_categories = apply_filters_deprecated( 'block_categories', array( $block_categories, $post ), '5.8.0', 'block_categories_all' );
 	}
 
 	return $block_categories;
@@ -108,15 +109,32 @@ function get_allowed_block_types( $editor_name ) {
 	$allowed_block_types = true;
 
 	/**
-	 * Filters the allowed block types for the given editor, defaulting to true (all
-	 * registered block types supported).
+	 * Filters the allowed block types for all editor types, defaulting to `true`
+	 * (all registered block types supported).
+	 *
 	 *
 	 * @since 5.8.0
 	 *
 	 * @param bool|array $allowed_block_types Array of block type slugs, or
 	 *                                        boolean to enable/disable all.
+	 * @param string     $editor_name         The name of the editor, e.g. 'post-editor'.
 	 */
-	$allowed_block_types = apply_filters( "allowed_block_types_{$editor_name}", $allowed_block_types );
+	$allowed_block_types = apply_filters( 'allowed_block_types_all', $allowed_block_types, $editor_name );
+
+	/**
+	 * Filters the allowed block types for the given editor, defaulting to `true`
+	 * (all registered block types supported).
+	 *
+	 * The dynamic portion of the hook name, `$editor_name`, refers to the name
+	 * of the editor type, e.g. 'post-editor', 'site-editor', etc.
+	 *
+	 * @since 5.8.0
+	 *
+	 * @param bool|array $allowed_block_types Array of block type slugs, or
+	 *                                        boolean to enable/disable all.
+	 * @param string     $editor_name         The name of the editor, e.g. 'post-editor'.
+	 */
+	$allowed_block_types = apply_filters( "allowed_block_types_{$editor_name}", $allowed_block_types, $editor_name );
 	if ( 'post-editor' === $editor_name ) {
 		$post = get_post();
 
@@ -131,7 +149,7 @@ function get_allowed_block_types( $editor_name ) {
 		 *                                        boolean to enable/disable all.
 		 * @param WP_Post    $post                The post resource data.
 		 */
-		$allowed_block_types = apply_filters_deprecated( 'allowed_block_types', array( $allowed_block_types, $post ), '5.8.0', "allowed_block_types_{$editor_name}" );
+		$allowed_block_types = apply_filters_deprecated( 'allowed_block_types', array( $allowed_block_types, $post ), '5.8.0', 'allowed_block_types_all' );
 	}
 
 	return $allowed_block_types;
@@ -241,13 +259,27 @@ function get_block_editor_settings( $editor_name, $custom_settings = array() ) {
 	);
 
 	/**
-	 * Filters the settings to pass to the block editor for a given editor type.
+	 * Filters the settings to pass to the block editor for all editor type.
 	 *
 	 * @since 5.8.0
 	 *
-	 * @param array $editor_settings Default editor settings.
+	 * @param array  $editor_settings Default editor settings.
+	 * @param string $editor_name     The name of the editor, e.g. 'post-editor'.
 	 */
-	$editor_settings = apply_filters( "block_editor_settings_{$editor_name}", $editor_settings );
+	$editor_settings = apply_filters( 'block_editor_settings_all', $editor_settings, $editor_name );
+
+	/**
+	 * Filters the settings to pass to the block editor for a given editor type.
+	 *
+	 * The dynamic portion of the hook name, `$editor_name`, refers to the name
+	 * of the editor type, e.g. 'post-editor', 'site-editor', etc.
+	 *
+	 * @since 5.8.0
+	 *
+	 * @param array  $editor_settings Default editor settings.
+	 * @param string $editor_name     The name of the editor, e.g. 'post-editor'.
+	 */
+	$editor_settings = apply_filters( "block_editor_settings_{$editor_name}", $editor_settings, $editor_name );
 	if ( 'post-editor' === $editor_name ) {
 		$post = get_post();
 
@@ -260,7 +292,7 @@ function get_block_editor_settings( $editor_name, $custom_settings = array() ) {
 		 * @param array   $editor_settings Default editor settings.
 		 * @param WP_Post $post            Post being edited.
 		 */
-		$editor_settings = apply_filters_deprecated( 'block_editor_settings', array( $editor_settings, $post ), '5.8.0', "block_editor_settings_{$editor_name}" );
+		$editor_settings = apply_filters_deprecated( 'block_editor_settings', array( $editor_settings, $post ), '5.8.0', 'block_editor_settings_all' );
 	}
 
 	return $editor_settings;
