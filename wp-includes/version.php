2d7eb07747097a249b44b2349db0047cc2df17cd
desrosj <desrosj@git.wordpress.org>
2021-04-22 00:56:05 +0000

    Build/Test Tools: Update the `caniuse-lite` database.
    This updates the `caniuse-lite` database to the most recent version (`1.0.30001214`) by running `npx browserslist@latest --update-db`.
    
    The resulting changes after running `grunt precommit:css` are also included in this commit.
    
    See #52624.
    Built from https://develop.svn.wordpress.org/trunk@50782
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50391 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50781';
+$wp_version = '5.8-alpha-50782';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
