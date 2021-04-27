8cd601c8ba6a6a16949ae0cb626921f3169ce7b8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-04-27 18:36:04 +0000

    REST API: Check the results of `get_metadata()` in `WP_REST_Meta_Fields` methods.
    This avoids PHP warnings in case the function returns boolean `false` instead of an array.
    
    Props david.binda.
    Fixes #53099.
    Built from https://develop.svn.wordpress.org/trunk@50793
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50402 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50792';
+$wp_version = '5.8-alpha-50793';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
