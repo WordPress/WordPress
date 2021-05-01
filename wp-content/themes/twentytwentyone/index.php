85d526c5d052014e9e164537ebf5569452b6d0a2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-05-01 18:14:05 +0000

    Twenty Twenty-One: Display page title as the H1 heading when a static page is selected as the "Posts page".
    This ensures that the heading represents the content of the page. Previously, the site title was displayed instead.
    
    Props sabernhardt, justinahinon, rianrietveld, mukesh27, francina, audrasjb, Boniu91.
    Fixes #52938.
    Built from https://develop.svn.wordpress.org/trunk@50802
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50411 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwentyone/index.php
+++ wp-content/themes/twentytwentyone/index.php
@@ -14,7 +14,13 @@
  * @since Twenty Twenty-One 1.0
  */
 
-get_header();
+get_header(); ?>
+
+<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) { ?>
+	<header class="page-header alignwide">
+		<h1 class="page-title"><?php single_post_title(); ?></h1>
+	</header><!-- .page-header -->
+<?php }
 
 if ( have_posts() ) {
 
