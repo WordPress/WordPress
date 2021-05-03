6f2fa9ef4222d5ff149a2c5f78795376da145753
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-05-03 14:35:02 +0000

    Twenty Twenty-One: Fix "Opening PHP tag must be on a line by itself" WPCS issue.
    Follow-up to [50802].
    
    See #52938.
    Built from https://develop.svn.wordpress.org/trunk@50806
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50415 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwentyone/index.php
+++ wp-content/themes/twentytwentyone/index.php
@@ -16,12 +16,13 @@
 
 get_header(); ?>
 
-<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) { ?>
+<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
 	<header class="page-header alignwide">
 		<h1 class="page-title"><?php single_post_title(); ?></h1>
 	</header><!-- .page-header -->
-<?php }
+<?php endif; ?>
 
+<?php
 if ( have_posts() ) {
 
 	// Load posts loop.
