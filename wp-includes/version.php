ef28363c7b44cfb36d5f7e36502f0cbd1c30c7fe
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-05-04 10:48:03 +0000

    Plugins: Standardize the terminology used for actions, filters, and callback functions.
    Use `$hook_name` when referring to a filter or action hook name, and `$callback` when referring to a callback function.
    
    This brings more consistency to parameter names in Plugin API functions.
    
    Includes minor code layout fixes for better readability and reordering some functions in a more logical order.
    
    Props johnbillion, jrf, SergeyBiryukov.
    Fixes #50531.
    Built from https://develop.svn.wordpress.org/trunk@50807
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50416 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50806';
+$wp_version = '5.8-alpha-50807';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
