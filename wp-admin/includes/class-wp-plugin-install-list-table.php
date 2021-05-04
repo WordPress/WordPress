5ced9a5afe6d99137427c9e4fa6f17b4a7dc4852
desrosj <desrosj@git.wordpress.org>
2021-05-04 13:38:58 +0000

    General: Use correct escaping function for the plugin icon URL.
    Props chintan1896, audrasjb.
    Fixes #53151.
    Built from https://develop.svn.wordpress.org/trunk@50808
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50417 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-plugin-install-list-table.php
+++ wp-admin/includes/class-wp-plugin-install-list-table.php
@@ -709,7 +709,7 @@ class WP_Plugin_Install_List_Table extends WP_List_Table {
 					<h3>
 						<a href="<?php echo esc_url( $details_link ); ?>" class="thickbox open-plugin-details-modal">
 						<?php echo $title; ?>
-						<img src="<?php echo esc_attr( $plugin_icon_url ); ?>" class="plugin-icon" alt="" />
+						<img src="<?php echo esc_url( $plugin_icon_url ); ?>" class="plugin-icon" alt="" />
 						</a>
 					</h3>
 				</div>
