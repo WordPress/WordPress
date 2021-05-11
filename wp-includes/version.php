f812323f1375c5e5a0f90536ab2591db6ff06ebd
desrosj <desrosj@git.wordpress.org>
2021-05-11 20:12:15 +0000

    Build/Test Tools: Update the several dependencies.
    This updates the following dependencies:
    
    - `grunt` from `1.3.0` to `1.4.0`.
    - `grunt-legacy-util` from `2.0.0` to `2.0.1`.
    - `sass` from `1.32.11` to `1.32.12`.
    - `sinon-test` from `3.0.0` to `3.1.0`.
    - `uglify-js` from `3.13.4` to `3.13.6`.
    
    See #52624.
    Built from https://develop.svn.wordpress.org/trunk@50843
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50452 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50842';
+$wp_version = '5.8-alpha-50843';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
