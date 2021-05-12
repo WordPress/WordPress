b6b214a557d5664f5f5746c5b1668c8c76e8d2aa
Peter Wilson <wilson@peterwilson.cc>
2021-05-12 23:11:59 +0000

    WordPress 5.7.2.
    Built from https://develop.svn.wordpress.org/branches/5.7@50869
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.7@50478 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7.2-alpha-50868';
+$wp_version = '5.7.2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
