bf11362dab334a12861ad16cf024fc966bf28137
Aaron Jorbin <aaron@jorb.in>
2021-05-14 19:35:00 +0000

    Boostrap/Load: Strengthen language in wp-config-sample.php
    Make it clearer where custom pieces belong in wp-config.php
    
    Props kraftbj, jorbin.
    Fixes #37199.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50915
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50524 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -79,7 +79,15 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', false );
 
-/* That's all, stop editing! Happy publishing. */
+/** Add any custom values after this line. */
+
+
+
+/**
+ * Custom Values must appear above this line.
+ *
+ * That's all, stop editing! Happy publishing.
+ */
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
