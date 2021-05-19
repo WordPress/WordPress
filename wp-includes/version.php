77c434e1302d2517595c6073fe4c8ee957b7d3ee
desrosj <desrosj@git.wordpress.org>
2021-05-19 17:37:59 +0000

    Build/Test Tools: Use the new `concurrency` setting for GitHub Actions.
    The new `concurrency` setting can be used to ensure only a single workflow run or job is in progress. When used in combination with the `cancel-in-progress` setting, incomplete workflow runs can be cancelled automatically to prevent running workflows unnecessarily.
    
    The workflows that take longer to run previously had this built into a step and utilized a 3rd-party action. Now that this is natively supported by GitHub Actions, using that is preferred.
    
    This option is currently in beta, but is stable enough to use in our workflows for the time being.
    
    Props ocean90.
    Fixes #53080.
    Built from https://develop.svn.wordpress.org/trunk@50930
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50539 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50929';
+$wp_version = '5.8-alpha-50930';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
