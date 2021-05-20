c7acbeda194b3ce25a7b4affb5c96a8822b361f2
gziolo <gziolo@git.wordpress.org>
2021-05-20 13:02:17 +0000

    Editor: Update `lodash` to the latest version 4.17.21
    It aligns with the version that Gutenberg uses.
    
    Props peterwilsoncc.
    See #52991.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50941
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50550 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/dist/components.js
+++ wp-includes/js/dist/components.js
@@ -41910,6 +41910,32 @@ function withStyles(styleFn) {
 
 /***/ }),
 
+/***/ "TO8r":
+/***/ (function(module, exports) {
+
+/** Used to match a single whitespace character. */
+var reWhitespace = /\s/;
+
+/**
+ * Used by `_.trim` and `_.trimEnd` to get the index of the last non-whitespace
+ * character of `string`.
+ *
+ * @private
+ * @param {string} string The string to inspect.
+ * @returns {number} Returns the index of the last non-whitespace character.
+ */
+function trimmedEndIndex(string) {
+  var index = string.length;
+
+  while (index-- && reWhitespace.test(string.charAt(index))) {}
+  return index;
+}
+
+module.exports = trimmedEndIndex;
+
+
+/***/ }),
+
 /***/ "TOwV":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -51895,6 +51921,32 @@ module.exports = _extends({
 
 /***/ }),
 
+/***/ "jXQH":
+/***/ (function(module, exports, __webpack_require__) {
+
+var trimmedEndIndex = __webpack_require__("TO8r");
+
+/** Used to match leading whitespace. */
+var reTrimStart = /^\s+/;
+
+/**
+ * The base implementation of `_.trim`.
+ *
+ * @private
+ * @param {string} string The string to trim.
+ * @returns {string} Returns the trimmed string.
+ */
+function baseTrim(string) {
+  return string
+    ? string.slice(0, trimmedEndIndex(string) + 1).replace(reTrimStart, '')
+    : string;
+}
+
+module.exports = baseTrim;
+
+
+/***/ }),
+
 /***/ "jenk":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -56426,14 +56478,12 @@ module.exports = keysShim;
 /***/ "tLB3":
 /***/ (function(module, exports, __webpack_require__) {
 
-var isObject = __webpack_require__("GoyQ"),
+var baseTrim = __webpack_require__("jXQH"),
+    isObject = __webpack_require__("GoyQ"),
     isSymbol = __webpack_require__("/9aa");
 
 /** Used as references for various `Number` constants. */
 var NAN = 0 / 0;
-
-/** Used to match leading and trailing whitespace. */
-var reTrim = /^\s+|\s+$/g;
 
 /** Used to detect bad signed hexadecimal string values. */
 var reIsBadHex = /^[-+]0x[0-9a-f]+$/i;
@@ -56484,7 +56534,7 @@ function toNumber(value) {
   if (typeof value != 'string') {
     return value === 0 ? value : +value;
   }
-  value = value.replace(reTrim, '');
+  value = baseTrim(value);
   var isBinary = reIsBinary.test(value);
   return (isBinary || reIsOctal.test(value))
     ? freeParseInt(value.slice(2), isBinary ? 2 : 8)
