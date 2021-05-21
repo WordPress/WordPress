5ddc1fb94644a9a2382d36c725b0320a83ca63b0
Adam Silverstein <adamsilverstein@earthboundhosting.com>
2021-05-21 15:29:55 +0000

    Revisions: add a new filter for revisions to keep by post type.
    Add a new `wp_{$post_type}_revisions_to_keep` filter that makes it convenient to filter the number of revisions created for a specific post type.
    
    Overrides both the value of WP_POST_REVISIONS and the `wp_revisions_to_keep` filter.
    
    Props dlh.
    Fixes #51550.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50949
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50558 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/revision.php
+++ wp-includes/revision.php
@@ -564,7 +564,24 @@ function wp_revisions_to_keep( $post ) {
 	 * @param int     $num  Number of revisions to store.
 	 * @param WP_Post $post Post object.
 	 */
-	return (int) apply_filters( 'wp_revisions_to_keep', $num, $post );
+	$num = apply_filters( 'wp_revisions_to_keep', $num, $post );
+
+	/**
+	 * Filters the number of revisions to save for the given post by its post type.
+	 *
+	 * Overrides both the value of WP_POST_REVISIONS and the {@see 'wp_revisions_to_keep'} filter.
+	 *
+	 * The dynamic portion of the hook name, `$post->post_type`, refers to
+	 * the post type slug.
+	 *
+	 * @since 5.8.0
+	 *
+	 * @param int     $num  Number of revisions to store.
+	 * @param WP_Post $post Post object.
+	 */
+	$num = apply_filters( "wp_{$post->post_type}_revisions_to_keep", $num, $post );
+
+	return (int) $num;
 }
 
 /**
