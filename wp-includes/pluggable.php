1e02e0f0493a1d6f013dcf5142efac489524941c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-05-22 13:09:56 +0000

    Docs: Use 3-digit, x.x.x-style semantic versioning for two `_doing_it_wrong()` calls.
    Follow-up to [37985], [38420], [39021].
    
    See #52628.
    Built from https://develop.svn.wordpress.org/trunk@50952
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50561 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -1221,7 +1221,7 @@ if ( ! function_exists( 'check_ajax_referer' ) ) :
 	 */
 	function check_ajax_referer( $action = -1, $query_arg = false, $die = true ) {
 		if ( -1 == $action ) {
-			_doing_it_wrong( __FUNCTION__, __( 'You should specify an action to be verified by using the first parameter.' ), '4.7' );
+			_doing_it_wrong( __FUNCTION__, __( 'You should specify an action to be verified by using the first parameter.' ), '4.7.0' );
 		}
 
 		$nonce = '';
