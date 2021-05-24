f8286bbd4de395fa715ef7bfe4f5e73469bce2a8
gziolo <gziolo@git.wordpress.org>
2021-05-24 06:26:58 +0000

    Editor: Add missing unit tests for `block_has_support`
    Follow-up for [50761].
    Props ntsekouras.
    Fixes #53257. See #52991.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50955
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50564 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50954';
+$wp_version = '5.8-alpha-50955';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
