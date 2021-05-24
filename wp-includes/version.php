5ce6da1a31e5b256553b848e1999319a473a8c40
Peter Wilson <wilson@peterwilson.cc>
2021-05-24 02:18:58 +0000

    XML-RPC: Set HTTP status code in accordance with the spec.
    When the XML-RPC endpoint is enabled, always return a HTTP `200 OK` status code in accordance with the XML-RPC specification. Continue to return an HTTP `405 Method Not Allowed` status code when the endpoint is disabled.
    
    Props ariskataoka, johnbillion.
    Fixes #52958.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50954
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50563 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50953';
+$wp_version = '5.8-alpha-50954';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
