3143af591013f36cb22b9d9cc598201397576055
gziolo <gziolo@git.wordpress.org>
2021-05-24 09:50:59 +0000

    Editor: Fix failing unit test for i18n support in `theme.json`
    As part of the change, I also did some code reorganization to match with how other test fixtures are handled.
    
    Follow-up [50959].
    
    See #52991.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50960
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50569 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50959';
+$wp_version = '5.8-alpha-50960';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
