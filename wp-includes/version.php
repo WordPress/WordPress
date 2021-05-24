5132f704d48f399b733991100b40d5642563b98e
desrosj <desrosj@git.wordpress.org>
2021-05-24 18:33:59 +0000

    Twenty Twenty-One: Re-add `px` unit to the admin bar height custom property.
    The `px` unit added in [50388] to prevent invalid values when using `calc` functions was inadvertently removed in [50972].
    
    This re-adds the unit to ensure the issue reported in #52564 remains fixed. The same stylelint rule adjustment made in [50388] to `.stylelintrc-css.json` has also been copied over to `stylelintrc.json` file,  which is the configuration file used when linting `.scss` files.
    
    Props ocean90.
    See #52624, #52564.
    Built from https://develop.svn.wordpress.org/trunk@50975
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50584 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50974';
+$wp_version = '5.8-alpha-50975';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
