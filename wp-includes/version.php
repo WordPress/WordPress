3989828e0bd393ceeea05735b59a06d9977bd33b
gziolo <gziolo@git.wordpress.org>
2021-05-24 07:38:58 +0000

    Editor: Add missing class WP_Block_Editor_Context
    Follow-up for [50956].
    See #52920.
    
    
    Built from https://develop.svn.wordpress.org/trunk@50957
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50566 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50956';
+$wp_version = '5.8-alpha-50957';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
