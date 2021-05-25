8775b69cd0637535c25cb2d3af3f3749ecf343c7
noisysocks <noisysocks@git.wordpress.org>
2021-05-25 08:52:58 +0000

    Adds the widgets block editor to widgets.php and customize.php
    Moves the widgets block editor from Gutenberg into WordPress Core.
    
    - Adds @wordpress/edit-widgets, @wordpress/customize-widgets and
      @wordpress/widgets.
    - Modifies wp-admin/widgets.php to branch between the old editor and new editor
      depending on wp_use_widgets_block_editor().
    - Modifies WP_Customize_Widgets to branch between the old editor control and new
      editor control depending on wp_use_widgets_block_editor().
    
    Follows [50996] which was missing these files.
    See #51506.
    Props isabel_brison, TimothyBlynJacobs, andraganescu, kevin940726, talldanwp.
    
    Built from https://develop.svn.wordpress.org/trunk@50997
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50606 1a063a9b-81f0-0310-95a4-ce76da25c4cd

---
+++ wp-admin/widgets-form-blocks.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The block-based widgets editor, for use in widgets.php.
+ *
+ * @package WordPress
+ * @subpackage Administration
+ */
+
+// Don't load directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+// Flag that we're loading the block editor.
+$current_screen = get_current_screen();
+$current_screen->is_block_editor( true );
+
+$block_editor_context = new WP_Block_Editor_Context();
+
+$preload_paths = array(
+	array( '/wp/v2/media', 'OPTIONS' ),
+	'/wp/v2/sidebars?context=edit&per_page=-1',
+	'/wp/v2/widgets?context=edit&per_page=-1&_embed=about',
+);
+block_editor_rest_api_preload( $preload_paths, $block_editor_context );
+
+$editor_settings = get_block_editor_settings( array(), $block_editor_context );
+
+wp_add_inline_script(
+	'wp-edit-widgets',
+	sprintf(
+		'wp.domReady( function() {
+			wp.editWidgets.initialize( "widgets-editor", %s );
+		} );',
+		wp_json_encode( $editor_settings )
+	)
+);
+
+// Preload server-registered block schemas.
+wp_add_inline_script(
+	'wp-blocks',
+	'wp.blocks.unstable__bootstrapServerSideBlockDefinitions(' . wp_json_encode( get_block_editor_server_block_settings() ) . ');'
+);
+
+wp_add_inline_script(
+	'wp-blocks',
+	sprintf( 'wp.blocks.setCategories( %s );', wp_json_encode( get_block_categories( 'widgets-editor' ) ) ),
+	'after'
+);
+
+wp_enqueue_script( 'wp-edit-widgets' );
+wp_enqueue_script( 'admin-widgets' );
+wp_enqueue_script( 'wp-format-library' );
+wp_enqueue_style( 'wp-edit-widgets' );
+wp_enqueue_style( 'wp-format-library' );
+
+/** This action is documented in edit-form-blocks.php */
+do_action( 'enqueue_block_editor_assets' );
+
+require_once ABSPATH . 'wp-admin/admin-header.php';
+?>
+
+<div id="widgets-editor" class="blocks-widgets-container"></div> 
+
+<?php
+require_once ABSPATH . 'wp-admin/admin-footer.php';
