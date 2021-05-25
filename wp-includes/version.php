66603b4bd75b30ead6afdf98c6dec43fb9c086fe
desrosj <desrosj@git.wordpress.org>
2021-05-25 23:28:15 +0000

    External Libraries: Update two polyfill libraries to their latest versions.
    This updates the following polyfill libraries bundled with WordPress to their latest versions:
    - `formdata-polyfill` from `3.0.20` to `4.0.0`.
    - `polyfill-library` from `3.104.0` to `3.105.0`.
    
    Fixes #52854.
    Built from https://develop.svn.wordpress.org/trunk@51017
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50626 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-51016';
+$wp_version = '5.8-alpha-51017';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
