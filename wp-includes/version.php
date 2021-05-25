670ec9cfa78b361131987d78b723f5c398048e14
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-05-25 12:26:58 +0000

    Comments: Include a "View Post" link on the Comments screen for a single post.
    This makes it easier to view the post on the front end of the site when moderating comments, without having to open the editor first.
    
    Props trejder, audrasjb, vladytimy, lukecarbis, ayeshrajans, chaion07, paaljoachim, hareesh-pillai, SergeyBiryukov.
    Fixes #52353.
    Built from https://develop.svn.wordpress.org/trunk@50999
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50608 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-50998';
+$wp_version = '5.8-alpha-50999';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
