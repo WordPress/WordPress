6e4b41c8481223049c36eb59bc367c0f868d58e9
ryelle <ryelle@git.wordpress.org>
2021-05-26 01:53:57 +0000

    Site Health, Privacy: Combine shared CSS for Site Health & Privacy Settings
    The styles for the Privacy settings page were based on the Site Health section. These were duplicated into `edit.css` in #49264. This change merges the selectors from Site Health into the Privacy section, to reduce that duplicate code.
    
    Props xkon, notlaura, clorith.
    See #52429.
    
    
    Built from https://develop.svn.wordpress.org/trunk@51025
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50634 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/dashboard.php
+++ wp-admin/includes/dashboard.php
@@ -1891,7 +1891,7 @@ function wp_dashboard_site_health() {
 
 	$issues_total = $issue_counts['recommended'] + $issue_counts['critical'];
 	?>
-	<div class="health-check-title-section site-health-progress-wrapper loading hide-if-no-js">
+	<div class="health-check-widget-title-section site-health-progress-wrapper loading hide-if-no-js">
 		<div class="site-health-progress">
 			<svg role="img" aria-hidden="true" focusable="false" width="100%" height="100%" viewBox="0 0 200 200" version="1.1" xmlns="http://www.w3.org/2000/svg">
 				<circle r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
