eae23abd88954e7919297d87b9dd429f03bf1497
Peter Wilson <wilson@peterwilson.cc>
2021-05-27 00:29:10 +0000

    Build tools: Use hashed module IDs for minified files.
    Further improves webpack configuration for editor files to use hashed module IDs in the compressed (`*.min.js`) production files.
    
    Follow up to [50940].
    
    Props @gziolo.
    Merges [51035] to the 5.7 branch.
    Fixes #53192.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.7@51036
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.7@50645 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7.3-alpha-50990';
+$wp_version = '5.7.3-alpha-51036';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
