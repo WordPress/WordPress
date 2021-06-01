44c185e0cf1a9b20243b89eaf0c8768f6d389eb1
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-06-01 11:30:57 +0000

    Coding Standards: Simplify a condition in `wp-admin/admin-footer.php`.
    Props jamil95, mukesh27.
    Fixes #53306.
    Built from https://develop.svn.wordpress.org/trunk@51053
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50662 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-51052';
+$wp_version = '5.8-alpha-51053';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
