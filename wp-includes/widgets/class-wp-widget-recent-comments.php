2e36162a2034467527c489f744a7c352a2b476ba
whyisjake <whyisjake@git.wordpress.org>
2021-06-08 23:40:00 +0000

    Widgets: Don't decode HTML entities ahead of the widget constructor.
    This reverts the changes from [51114]. 
    
    See #53138.
    
    Unprops whyisjake. 
    
    Built from https://develop.svn.wordpress.org/trunk@51127
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50736 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/widgets/class-wp-widget-recent-comments.php
+++ wp-includes/widgets/class-wp-widget-recent-comments.php
@@ -24,7 +24,7 @@ class WP_Widget_Recent_Comments extends WP_Widget {
 	public function __construct() {
 		$widget_ops = array(
 			'classname'                   => 'widget_recent_comments',
-			'description'                 => html_entity_decode( __( 'Your site&#8217;s most recent comments.' ) ),
+			'description'                 => __( 'Your site&#8217;s most recent comments.' ),
 			'customize_selective_refresh' => true,
 			'show_instance_in_rest'       => true,
 		);
