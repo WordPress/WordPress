bbc2d09a9fb8f8054330793826eae554fabf1313
noisysocks <noisysocks@git.wordpress.org>
2021-06-08 01:55:57 +0000

    Make new WordPress installations use blocks in widget areas instead of widgets
    Modifies `wp_install_defaults()` so that when you install a new WordPress site you
    have block in your widget areas, not widgets.
    
    Fixes #53324.
    Props isabel_brison, hellofromtonya, andraganescu.
    
    Built from https://develop.svn.wordpress.org/trunk@51088
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50697 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-alpha-51087';
+$wp_version = '5.8-alpha-51088';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
