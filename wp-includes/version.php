8df65cdbe91241b5154496c43fcad5d4b34fe5f4
Peter Wilson <wilson@peterwilson.cc>
2021-06-09 23:51:56 +0000

    Widgets: Improve unit tests for RSS Widget.
    Prevent unit tests from making HTTP requests to wordpress.org. Specify URLs as https rather than http.
    
    Follow up to [51107].
    
    Props hellofromTonya.
    Fixes #53278.
    
    Built from https://develop.svn.wordpress.org/trunk@51136
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50745 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-beta1-51135';
+$wp_version = '5.8-beta1-51136';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
