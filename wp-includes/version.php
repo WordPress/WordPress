adf23b3ed540842b20586b3ad28eeb05f7f92cf8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-06-09 19:42:58 +0000

    Tests: Ignore EOL differences in some tests using multiline string assertions.
    Unix vs. Windows EOL style mismatches can cause misleading failures in tests using the heredoc syntax (`<<<`) or multiline strings as the expected result.
    
    Follow-up to [48466], [50995], [51079].
    
    See #52625.
    Built from https://develop.svn.wordpress.org/trunk@51135
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50744 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-beta1-51134';
+$wp_version = '5.8-beta1-51135';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
