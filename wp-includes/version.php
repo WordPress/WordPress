ca5f9900786ca617a1553255753f6c705ab11755
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-06-21 04:29:56 +0000

    Docs: Correct `@since` version in the `wp-includes/version.php` file header.
    There was no public release of WordPress 1.1.
    
    Follow-up to [803], [43662].
    
    See #52628.
    Built from https://develop.svn.wordpress.org/trunk@51188
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50797 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -5,7 +5,7 @@
  * Contains version information for the current WordPress release.
  *
  * @package WordPress
- * @since 1.1.0
+ * @since 1.2.0
  */
 
 /**
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-beta2-51187';
+$wp_version = '5.8-beta2-51188';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
