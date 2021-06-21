d5c9c310e7550089f1dac1b657ad4a647597c81d
joedolson <joedolson@git.wordpress.org>
2021-06-21 20:46:02 +0000

    Media: Update total attachment count when media added or removed.
    Add handlers to increment the total attachments count for the media collection when an item is added or removed.
    
    props adamsilverstein.
    Fixes #53171.
    Built from https://develop.svn.wordpress.org/trunk@51191
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50800 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/media-models.js
+++ wp-includes/js/media-models.js
@@ -591,6 +591,8 @@ var Attachments = Backbone.Collection.extend(/** @lends wp.media.model.Attachmen
 		this.observers.push( attachments );
 
 		attachments.on( 'add change remove', this._validateHandler, this );
+		attachments.on( 'add', this._addToTotalAttachments, this );
+		attachments.on( 'remove', this._removeFromTotalAttachments, this );
 		attachments.on( 'reset', this._validateAllHandler, this );
 		this.validateAll( attachments );
 		return this;
@@ -614,6 +616,30 @@ var Attachments = Backbone.Collection.extend(/** @lends wp.media.model.Attachmen
 		}
 
 		return this;
+	},
+	/**
+	 * Update total attachment count when items are added to a collection.
+	 *
+	 * @access private
+	 *
+	 * @since 5.8.0
+	 */
+	_removeFromTotalAttachments: function() {
+		if ( this.mirroring ) {
+			this.mirroring.totalAttachments = this.mirroring.totalAttachments - 1;
+		}
+	},
+	/**
+	 * Update total attachment count when items are added to a collection.
+	 *
+	 * @access private
+	 *
+	 * @since 5.8.0
+	 */
+	_addToTotalAttachments: function() {
+		if ( this.mirroring ) {
+			this.mirroring.totalAttachments = this.mirroring.totalAttachments + 1;
+		}
 	},
 	/**
 	 * @access private
