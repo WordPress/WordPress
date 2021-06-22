0a2b52dfeb4e28e4ff3714899a2890a779c3748f
ryelle <ryelle@git.wordpress.org>
2021-06-22 20:14:59 +0000

    Bundled Themes: Improve display of blocks in widget areas.
    Fixes minor styling issues, mostly font size and spacing, in blocks used in widget areas. Changes made to Twenty Ten, Twenty Thirteen, Twenty Fourteen, Twenty Sixteen, Twenty Seventeen, and Twenty Twenty-One.
    
    Props noisysocks, sumaiyasiddika, danieldudzic, scruffian, jffng.
    Fixes #53422.
    
    
    Built from https://develop.svn.wordpress.org/trunk@51205
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50814 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-beta2-51204';
+$wp_version = '5.8-beta2-51205';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
