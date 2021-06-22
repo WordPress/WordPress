48e31b23f54b855a8f7edf24f6186542be372e3a
desrosj <desrosj@git.wordpress.org>
2021-06-22 23:05:58 +0000

    Docs: Shorten the copyright notice for the `WP_REST_Sidebars_Controller` class.
    The class has been heavily modified from the original source, so the copyright can be modified to simply reference the original author’s work. The carry forward of the copyright and original code is implied.
    
    Follow up to [50993-50995,51007,51020,51029].
    
    Props SergeyBiryukov, cbringmann, chanthaboune, desrosj.
    Fixes #41683.
    Built from https://develop.svn.wordpress.org/trunk@51210
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50819 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/endpoints/class-wp-rest-sidebars-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-sidebars-controller.php
@@ -2,29 +2,11 @@
 /**
  * REST API: WP_REST_Sidebars_Controller class
  *
+ * Original code from {@link https://github.com/martin-pettersson/wp-rest-api-sidebars Martin Pettersson (martin_pettersson@outlook.com)}.
+ *
  * @package WordPress
  * @subpackage REST_API
  * @since 5.8.0
- *
- * Copyright (C) 2015  Martin Pettersson
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @author    Martin Pettersson <martin_pettersson@outlook.com>
- * @copyright 2015 Martin Pettersson
- * @license   GPLv2
- * @link      https://github.com/martin-pettersson/wp-rest-api-sidebars
  */
 
 /**
