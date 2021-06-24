09fda454d08417f64c34ba410ff93a1d0e96e118
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-06-24 10:05:59 +0000

    Tests: Require PHPUnit >= 6 in tests using `::createPartialMock()`.
    This avoids a "Call to undefined method" fatal error when running the tests with PHPUnit 5.4.x, which WordPress still supports as the minimum version.
    
    Follow-up to [48945], [48947].
    
    See #52625.
    Built from https://develop.svn.wordpress.org/trunk@51226
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50835 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-beta3-51225';
+$wp_version = '5.8-beta3-51226';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
