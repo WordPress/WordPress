1c9ef38208a0e1b1d3f5f9aad5c940b2f6352ba3
iku1deep <try.ku1deep@gmail.com>
2021-06-26 15:17:23 +0530

    removed themes
    removed themes

--- wp-content/themes/twentytwenty/assets/js/editor-script-block.js
+++
@@ -1,9 +0,0 @@
-/**
- * Remove squared button style
- *
- * @since Twenty Twenty 1.0
- */
-/* global wp */
-wp.domReady( function() {
-	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
-} );
