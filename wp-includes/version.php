337cfb97c1d879b0ac6b43c7d8d31654df8a5b08
desrosj <desrosj@git.wordpress.org>
2021-06-30 18:47:01 +0000

    REST API: Allow multiple widgets to be deleted in a single batch request.
    This resets the `WP_Widget::$updated` flag when deleting a widget, to avoid blocking all future updates in a request.
    
    Props noisysocks, andraganescu.
    Merges [51277] to the 5.8 branch.
    Fixes #53557.
    Built from https://develop.svn.wordpress.org/branches/5.8@51290
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@50899 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-RC1-51275';
+$wp_version = '5.8-RC1-51290';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
