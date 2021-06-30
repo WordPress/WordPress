0420eac464e0a79bd078b46ce6c76589d8468dff
desrosj <desrosj@git.wordpress.org>
2021-06-30 18:48:58 +0000

    Script Loader: Revert [51267].
    This was missed in [51268] when reverting [51259-51260,51265].
    
    Props pbiron.
    Merges [51288] to the 5.8 branch.
    See #53397.
    Built from https://develop.svn.wordpress.org/branches/5.8@51291
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@50900 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-RC1-51290';
+$wp_version = '5.8-RC1-51291';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
