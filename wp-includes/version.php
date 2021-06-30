7884c3382198c8d49a72e32104585a5756944762
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-06-30 15:59:58 +0000

    Docs: Add `@ticket` references to some `WP_Theme_JSON` tests.
    Follow-up to [50973], [51054].
    
    See #52628, #53461.
    Built from https://develop.svn.wordpress.org/trunk@51281
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50890 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51280';
+$wp_version = '5.9-alpha-51281';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
