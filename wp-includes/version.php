4f26a883601e27dd104ad1c7519cfdc283dc1c74
desrosj <desrosj@git.wordpress.org>
2021-06-30 03:55:56 +0000

    Post branching 5.8 version bump.
    `trunk` is now `5.9-alpha`.
    Built from https://develop.svn.wordpress.org/trunk@51272
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50881 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-RC1-51270';
+$wp_version = '5.9-alpha-51272';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
