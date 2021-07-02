bc5908011bc030272fbb50193161a1375aa23d0d
desrosj <desrosj@git.wordpress.org>
2021-07-02 14:35:58 +0000

    Editor: Ensure the Query block pattern category is translatable.
    Because the `__()` translation function was used here, the Query block pattern category string is not translatable. `_x()` is the correct function here as that passes the second parameter as context and not text domain.
    
    Follow up to [50948].
    
    Props walbo.
    Fixes #53577.
    Built from https://develop.svn.wordpress.org/trunk@51307
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50916 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51306';
+$wp_version = '5.9-alpha-51307';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
