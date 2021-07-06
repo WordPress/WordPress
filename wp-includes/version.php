6f2b4548642d46bac1e6b6af7dbca9f05c20b77c
desrosj <desrosj@git.wordpress.org>
2021-07-06 13:13:59 +0000

    Build/Test Tools: Use caching built into `actions/setup-node`.
    This updates the `actions/setup-node` action to version 2.2.2 and takes advantage of the cache feature now built in. This removes the need for an additional step using `actions/cache` to cache NPM dependencies.
    
    The cache key used will be `${{ runner.os }}-npm-${{ hashFiles('') }}`, which matches the recommendation in the `actions/cache` documentation and the key currently in use in the WordPress workflows.
    
    Props gziolo.
    Fixes #53584.
    Built from https://develop.svn.wordpress.org/trunk@51341
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50950 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51337';
+$wp_version = '5.9-alpha-51341';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
