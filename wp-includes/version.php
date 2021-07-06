1940e5d03e08779d7f8156c722dcc0e3e605b859
John Blackbourn <johnbillion@git.wordpress.org>
2021-07-06 18:37:57 +0000

    Build/Test Tools: Add assertions to ensure version-controlled files are not modified during CI, and fix the `grunt clean` command.
    Some tests and some commands that run during the build steps modify files that are under version control. This adds assertions to ensure that these files don't remain in a modified state after the test runs on CI.
    
    This also fixes the `grunt clean` command which erroneously deletes `script-loader-packages.php`. This file is re-populated during the build, but deleting it during the clean is undesirable.
    
    Fixes #53606
    
    Built from https://develop.svn.wordpress.org/trunk@51355
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50964 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51353';
+$wp_version = '5.9-alpha-51355';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
