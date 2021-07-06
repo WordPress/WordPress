833382b2c900a5f71a2895f1afda25e8b5eef001
desrosj <desrosj@git.wordpress.org>
2021-07-06 14:02:57 +0000

    Bundled Themes: Correct `customzier` typo.
    This was found in Twenty Twenty-One and Twenty Nineteen.
    
    Props audrasjb, spytzo, sabernhardt.
    Fixes #53598.
    Built from https://develop.svn.wordpress.org/trunk@51343
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50952 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51342';
+$wp_version = '5.9-alpha-51343';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
