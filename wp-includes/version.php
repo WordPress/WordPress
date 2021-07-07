a1418c50bd0231ae3938d0daaacdbd03e28e17f1
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-07-07 10:51:58 +0000

    Coding Standards: Fix WPCS issues in [51367].
    This fixes an "Expected 1 space after comma in argument list; 2 found" error.
    
    See #53363.
    Built from https://develop.svn.wordpress.org/trunk@51368
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50979 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51367';
+$wp_version = '5.9-alpha-51368';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
