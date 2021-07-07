d6d75499ce4a0a88f0cca98fbb244a5070b88b0e
desrosj <desrosj@git.wordpress.org>
2021-07-07 00:07:57 +0000

    Themes: Remove inline comment that is not relevant in WordPress Core.
    This removes an inline comment block that was added when importing the `WP_Theme_JSON` class in [50959]. The comment is relevant to the code only when viewed within the context of the Gutenberg plugin.
    
    Props nosolosw, jorbin, SergeyBiryukov.
    Merges [51352] to the 5.8 branch.
    Fixes #53576.
    Built from https://develop.svn.wordpress.org/branches/5.8@51364
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@50973 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-theme-json.php
+++ wp-includes/class-wp-theme-json.php
@@ -1206,12 +1206,6 @@ class WP_Theme_JSON {
 			$theme_settings['settings']['typography']['fontSizes'] = $font_sizes;
 		}
 
-		/*
-		 * This allows to make the plugin work with WordPress 5.8 beta
-		 * as well as lower versions. The second check can be removed
-		 * as soon as the minimum WordPress version for the plugin
-		 * is bumped to 5.8.
-		 */
 		if ( isset( $settings['enableCustomSpacing'] ) ) {
 			if ( ! isset( $theme_settings['settings']['spacing'] ) ) {
 				$theme_settings['settings']['spacing'] = array();
