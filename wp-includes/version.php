dfd10943681cbdf2011bab4a310ae2fe36f90679
desrosj <desrosj@git.wordpress.org>
2021-07-07 00:37:57 +0000

    WordPress 5.8 RC2. Built from https://develop.svn.wordpress.org/branches/5.8@51365
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@50974 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-RC1-51364';
+$wp_version = '5.8-RC2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
