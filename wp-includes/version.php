4c165a015ed0ae3e77146e16e22bfa230cd2c8b1
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-07-07 10:34:00 +0000

    Tests: Use more appropriate assertions in various tests.
    This replaces instances of `assertSame( [number], count( ... ) )` with `assertCount()` to use native PHPUnit functionality.
    
    Follow-up to [51335], [51337].
    
    See #53363.
    Built from https://develop.svn.wordpress.org/trunk@51367
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@50978 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51362';
+$wp_version = '5.9-alpha-51367';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
