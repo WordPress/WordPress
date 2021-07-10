f45d515ff8434f2ba0616b9b83038354e19f48a2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-07-10 18:43:57 +0000

    Twenty Fifteen: Use strict comparison in `inc/custom-header.php`.
    Props kapilpaul.
    See #53359.
    Built from https://develop.svn.wordpress.org/trunk@51401
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51012 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyfifteen/inc/custom-header.php
+++ wp-content/themes/twentyfifteen/inc/custom-header.php
@@ -58,11 +58,11 @@ add_action( 'after_setup_theme', 'twentyfifteen_custom_header_setup' );
 function twentyfifteen_hex2rgb( $color ) {
 	$color = trim( $color, '#' );
 
-	if ( strlen( $color ) == 3 ) {
+	if ( strlen( $color ) === 3 ) {
 		$r = hexdec( substr( $color, 0, 1 ) . substr( $color, 0, 1 ) );
 		$g = hexdec( substr( $color, 1, 1 ) . substr( $color, 1, 1 ) );
 		$b = hexdec( substr( $color, 2, 1 ) . substr( $color, 2, 1 ) );
-	} elseif ( strlen( $color ) == 6 ) {
+	} elseif ( strlen( $color ) === 6 ) {
 		$r = hexdec( substr( $color, 0, 2 ) );
 		$g = hexdec( substr( $color, 2, 2 ) );
 		$b = hexdec( substr( $color, 4, 2 ) );
