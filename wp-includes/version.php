acd6e52e4f633b0a1c5a12f148d236b084be596c
desrosj <desrosj@git.wordpress.org>
2021-07-13 00:07:59 +0000

    Docs: Various documentation fixes following [51129].
    - Typo corrections in filter descriptions.
    - DocBlocks are now now wrapped to the next line after 80 characters, and not extending beyond 120 total characters wide.
    - Remove unnecessary repeated references to the suggested replacement hooks.
    - Adjustments to the indentation for consistency with other emails in Core, allowing the `phpcs:ignore` comment to be removed. 
    
    Props SergeyBiryukov.
    Merges [51410] to the 5.8 branch.
    Fixes #44314.
    Built from https://develop.svn.wordpress.org/branches/5.8@51411
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@51022 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-RC2-51407';
+$wp_version = '5.8-RC2-51411';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
