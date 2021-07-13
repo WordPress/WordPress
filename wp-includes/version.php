7523e1376ba28bb2035ea8941148bc60f2238701
desrosj <desrosj@git.wordpress.org>
2021-07-13 00:10:58 +0000

    Widgets: Use `wp_sidebar_description()` to retrieve a sidebar’s description.
    This switches `WP_REST_Sidebars_Controller` to use `wp_sidebar_description()` for retrieving the description of a given sidebar instead of referencing the value in the `$wp_registered_sidebars` global variable directly.
    
    `wp_sidebar_description()` uses `wp_kses()` to only allow the default list of `$allowed_tags` to be present in a sidebar’s description.
    
    Props timothyblynjacobs, desrosj, SergeyBiryukov.
    Merges [51408] to the 5.8 branch.
    Fixes #53646.
    Built from https://develop.svn.wordpress.org/branches/5.8@51412
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@51023 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8-RC2-51411';
+$wp_version = '5.8-RC2-51412';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
