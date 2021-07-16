7bcca73ed7b1a910a996bdc3b601d1570ecd48f3
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-07-16 19:15:00 +0000

    Tests: Use more appropriate assertions in various tests.
    This replaces instances of `assertSame( 0, strpos( ... ) )` with `assertStringStartsWith()` to use native PHPUnit functionality.
    
    Follow-up to [51335], [51337], [51367], [51397], [51403], [51404], [51436], [51438], [51448].
    
    See #53363.
    Built from https://develop.svn.wordpress.org/trunk@51449
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51060 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51448';
+$wp_version = '5.9-alpha-51449';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
