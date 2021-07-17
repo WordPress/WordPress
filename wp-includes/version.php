69a57fe80ed4028416cbf6cb6a69fd9dc1503251
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-07-17 10:24:59 +0000

    Tests: Correct the test for autosaving a post with Ajax.
    `wp_autosave()` only updates drafts and auto-drafts created by the current user if the post is not locked.
    
    As a result of previous Ajax test refactoring, setting the current user and creating a test post ended up in different methods, with the user being set after the post is already created.
    
    This resulted in the test post being created with the `post_author` field set to zero, and the current user check in `wp_autosave()` failed. Instead of updating the original post as the test intended, it created a new autosave.
    
    The test only passed accidentally due to `assertGreaterThanOrEqual()` not performing a strict type check.
    
    Follow-up to [26995], [35311].
    
    See #53363.
    Built from https://develop.svn.wordpress.org/trunk@51450
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51061 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51449';
+$wp_version = '5.9-alpha-51450';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
