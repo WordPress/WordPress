8f90cf9878b5a01e87eb21d962bf32da1de2f9b4
desrosj <desrosj@git.wordpress.org>
2021-07-27 13:57:57 +0000

    Upgrade/Install: Add files for 5.8 to the `$_old_files` list that were missed.
    Follow up to [51133], [51459].
    
    Props WFMattR, audrasjb, ocean90, johnbillion, pbiron.
    Fixes #53702.
    Built from https://develop.svn.wordpress.org/trunk@51497
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51108 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51496';
+$wp_version = '5.9-alpha-51497';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
