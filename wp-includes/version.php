94da59e312bd4ddddab7bf9d483671d440448e1f
desrosj <desrosj@git.wordpress.org>
2021-07-28 13:05:01 +0000

    Docs: Correct a comment about WebP constants in `wp-includes/compat.php`.
    Follow-up to [50810], [50814].
    
    Props GaryJ, rtm909, SergeyBiryukov.
    Merges [51474] to the 5.8 branch.
    Fixes #53680.
    Built from https://develop.svn.wordpress.org/branches/5.8@51508
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@51119 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8.1-alpha-51507';
+$wp_version = '5.8.1-alpha-51508';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
