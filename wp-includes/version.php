2c3326126b4fc01775760ca83e5eb975e09093b7
desrosj <desrosj@git.wordpress.org>
2021-07-30 12:22:00 +0000

    I18n: Fix broken loop in `WP_Theme_JSON_Resolver`
    Related issue in Gutenberg: https://github.com/WordPress/gutenberg/issues/33552.
    
    The loop in `WP_Theme_JSON_Resolver` to extract translatable paths was broken, as it contained an immediate and unconditional return. This caused the loop to immediately exit again after the first iteration, thus never actually looping.
    
    Follow-up to [50959].
    
    Props schlessera.
    Merges [51472] to the 5.8 branch.
    Fixes #53738.
    Built from https://develop.svn.wordpress.org/branches/5.8@51515
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@51126 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8.1-alpha-51509';
+$wp_version = '5.8.1-alpha-51515';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
