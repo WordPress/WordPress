3ad7f6e9b215ab29dae5dcce556750aa2aaad688
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-08-09 13:06:07 +0000

    Build/Test Tools: Remove Unicode character from PHPUnit version check message.
    Not all CLI tools can handle Unicode characters or non-system specific line endings well, so this type of CLI messaging should always be written with the optimal cross-platform, cross-CLI tool end-user experience in mind.
    
    Follow-up to [51581].
    
    Props jrf.
    See #53363.
    Built from https://develop.svn.wordpress.org/trunk@51584
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51195 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51583';
+$wp_version = '5.9-alpha-51584';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
