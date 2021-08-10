77cdaf8e9218c3409c2bad71bcce7cad44b0091d
desrosj <desrosj@git.wordpress.org>
2021-08-10 19:36:59 +0000

    Menus: Hide bulk-select on new menu page.
    Prevent the bulk-select option from displaying when adding a new menu. This also prevents the option from displaying when an administrator first visits the menu page and no menus are set.
    
    Props dlh, sabernhardt, peterwilsoncc.
    Merges [51539] to the 5.8 branch.
    Fixes #53654.
    Built from https://develop.svn.wordpress.org/branches/5.8@51596
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@51207 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8.1-alpha-51593';
+$wp_version = '5.8.1-alpha-51596';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
