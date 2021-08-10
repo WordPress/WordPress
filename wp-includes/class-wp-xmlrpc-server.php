16b7e3d44cf3600d714a075866a93d4b523bfbe9
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-08-10 12:54:56 +0000

    Docs: Add a `@see` reference to the `xmlrpc_enabled` filter in `wp_xmlrpc_server::set_is_enabled()`.
    Follow-up to [28065].
    
    See #53399.
    Built from https://develop.svn.wordpress.org/trunk@51590
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51201 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-xmlrpc-server.php
+++ wp-includes/class-wp-xmlrpc-server.php
@@ -186,7 +186,7 @@ class wp_xmlrpc_server extends IXR_Server {
 	private function set_is_enabled() {
 		/*
 		 * Respect old get_option() filters left for back-compat when the 'enable_xmlrpc'
-		 * option was deprecated in 3.5.0. Use the 'xmlrpc_enabled' hook instead.
+		 * option was deprecated in 3.5.0. Use the {@see 'xmlrpc_enabled'} hook instead.
 		 */
 		$is_enabled = apply_filters( 'pre_option_enable_xmlrpc', false );
 		if ( false === $is_enabled ) {
