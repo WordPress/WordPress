8f95d5bb9156cf33cb7ab4f4a1e2bf85ee37521a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-08-11 14:31:57 +0000

    Build/Test Tools: Hard deprecate `WP_UnitTestCase_Base::checkRequirements()`.
    The `WP_UnitTestCase_Base::checkRequirements()` method calls the `parent::checkRequirements()` method, but this method became `private` in PHPUnit 7.0 via commit  [https://github.com/sebastianbergmann/phpunit/commit/932238a6a3018cdfac6c2a7d8f1d5d49e65f5dc0 sebastianbergmann/phpunit@932238a].
    
    Aside from that, the `TestCase::getAnnotations()` method which is called next is now also removed in PHPUnit 9.5.
    
    WP core does not use the method anymore, and the method only remains to prevent potentially breaking external integration tests relying on the method. However, in effect, the method is not functional anymore in PHPUnit 7.0+.
    
    Follow-up to [893/tests], [894/tests], [896/tests], [918/tests], [30526], [40520], [40564], [43005], [44701], [51559-51577].
    
    Props jrf.
    See #46149.
    Built from https://develop.svn.wordpress.org/trunk@51602
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51213 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51601';
+$wp_version = '5.9-alpha-51602';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
