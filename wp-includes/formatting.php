6b144ab46af5f29c74360a0b0d6d3e8c9162baf4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-08-13 15:50:58 +0000

    Docs: Correct `@since` version for the `wp_parse_str` filter.
    The filter was introduced along with the `wp_parse_str()` function in WordPress 2.2.1.
    
    Follow-up to [5709], [8662], [26485].
    
    See #53399.
    Built from https://develop.svn.wordpress.org/trunk@51609
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51220 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -4969,7 +4969,7 @@ function wp_parse_str( $string, &$array ) {
 	/**
 	 * Filters the array of variables derived from a parsed string.
 	 *
-	 * @since 2.3.0
+	 * @since 2.2.1
 	 *
 	 * @param array $array The array populated with variables.
 	 */
