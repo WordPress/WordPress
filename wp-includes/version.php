864d9e9a355d002e18acce4cd14931452d86cd1f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-08-16 21:35:07 +0000

    Tests: Rename classes in `phpunit/tests/formatting/` per the naming conventions.
    https://make.wordpress.org/core/handbook/testing/automated-testing/writing-phpunit-tests/#naming-and-organization
    
    Follow-up to [47780], [48911], [49327], [50291], [50292], [50342], [50452], [50453], [50456], [50967], [50968], [50969], [51491], [51492], [51493].
    
    See #53363.
    Built from https://develop.svn.wordpress.org/trunk@51623
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51229 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51622';
+$wp_version = '5.9-alpha-51623';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
