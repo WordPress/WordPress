c9bf6d13ef01d81f6aa32e2f80360d4cfc56c547
Peter Wilson <wilson@peterwilson.cc>
2021-08-18 05:37:57 +0000

    Media: Fix layout of media library modal on narrow screens.
    Reduces the right margin of the media library modal on small and medium width screens to remove excess white-space. On very narrow screens this was preventing the media icons from displaying.
    
    Props andraganescu, desrosj, joedolson, moch11, mukesh27, sabernhardt, SergeyBiryukov, zieladam.
    Fixes #53679.
    
    Built from https://develop.svn.wordpress.org/trunk@51631
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51237 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51630';
+$wp_version = '5.9-alpha-51631';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
