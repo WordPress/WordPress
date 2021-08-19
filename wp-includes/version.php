9d2e8c096fec10f059b6a7a443f21ee570f51f37
ryelle <ryelle@git.wordpress.org>
2021-08-19 17:24:55 +0000

    Toolbar: Limit the icon transition style to color only.
    When transition is `all`, it also applies to the icon size, which can cause a bug in Safari where icons get stuck in the wrong size when resizing the browser window. The only expected animation is on the color property, so the transition can be limited to just `color`.
    
    Props johnjamesjacoby, sabernhardt, SergeyBiryukov, audrasjb.
    Fixes #43423.
    
    
    Built from https://develop.svn.wordpress.org/trunk@51644
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51250 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51641';
+$wp_version = '5.9-alpha-51644';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
