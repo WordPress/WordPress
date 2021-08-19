8718b2cae6d701300d2d8e5800e0e91f657cca1d
desrosj <desrosj@git.wordpress.org>
2021-08-19 15:01:57 +0000

    Media: Increase number of media items displayed per page.
    Increase the number of media items displayed per page from 40 to 80 to improve the experience for users navigating the library on sites with a large quantity of media in the library.
    
    Props AlGala, antpb, hellofromTonya, joedolson, SergeyBiryukov, wb1234, peterwilsoncc.
    Merges [51632] to the 5.8 branch.
    Fixes #53827.
    Built from https://develop.svn.wordpress.org/branches/5.8@51643
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@51249 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8.1-alpha-51642';
+$wp_version = '5.8.1-alpha-51643';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
