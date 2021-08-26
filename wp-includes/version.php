5cfd90b427b6b2aa5cac524eb73e3a6fca45c07c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-08-26 16:09:56 +0000

    Tests: Rename the test file and class for `wp_filter_object_list()` tests.
    This matches the name of the function being tested.
    
    Follow-up to [410/tests], [51663], [51664].
    
    See #53363, #53987.
    Built from https://develop.svn.wordpress.org/trunk@51665
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51271 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51664';
+$wp_version = '5.9-alpha-51665';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
