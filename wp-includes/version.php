1f7f8fb16a0ace53b07b128fa9f33fd6b7b04894
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-08-26 16:51:57 +0000

    Coding Standards: Add missing visibility keywords for `wp_filter_object_list()` and `wp_list_pluck()` tests.
    Follow-up to [51663-51667].
    
    Props pbearne.
    See #53363, #53987.
    Built from https://develop.svn.wordpress.org/trunk@51668
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51274 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51667';
+$wp_version = '5.9-alpha-51668';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
