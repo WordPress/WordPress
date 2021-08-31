46ba4f8b7089077807c5c6adadec389bd8d46bab
hellofromTonya <hellofromTonya@git.wordpress.org>
2021-08-31 17:23:55 +0000

    Tests: Add missing `@covers` tags for actions' tests.
    Props pbearne, hudson-atwell, jrf, hellofromTonya.
    See #39265.
    Built from https://develop.svn.wordpress.org/trunk@51702
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51308 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51701';
+$wp_version = '5.9-alpha-51702';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
