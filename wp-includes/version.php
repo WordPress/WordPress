33f4f396ef065abe7f34a6cafa02eaa350327d63
Peter Wilson <wilson@peterwilson.cc>
2021-09-01 04:00:59 +0000

    Build: Clean `css/dist` as part of the webpack build step.
    Move the cleaning of the `wp-includes/css/dist` folder from `clean:css` to `clean:webpack-assets` to avoid an order of operations issue in which the files were built shortly before been deleted later in the build process.
    
    Follow up to [51689].
    
    Props netweb, ramonopoly, peterwilsoncc.
    Fixes #53719.
    
    
    Built from https://develop.svn.wordpress.org/trunk@51713
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51319 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51709';
+$wp_version = '5.9-alpha-51713';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
