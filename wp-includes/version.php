af2f290084f0544f7bc467f0c1b543f674f007b5
desrosj <desrosj@git.wordpress.org>
2021-09-01 00:14:59 +0000

    Media: fix showing of the "Filter Media" filds when replacing an image from the media modal.
    Props mhuntdesign, sabernhardt, azaozz.
    Merges [51703] to the 5.8 branch.
    Fixes #53833.
    Built from https://develop.svn.wordpress.org/branches/5.8@51708
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@51314 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8.1-alpha-51707';
+$wp_version = '5.8.1-alpha-51708';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
