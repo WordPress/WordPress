c11fbdc7ae68dc74f002cbb04247d415747f1b9a
ryelle <ryelle@git.wordpress.org>
2021-09-02 22:18:00 +0000

    Coding Standards: Apply coding standards on CSS.
    Remove units when the value is zero. Combine redundant values in shorthand declarations.
    This was generated with `stylelint --fix` and a custom config (see #53866).
    
    Props ankitmaru, audrasjb, pbiron, ayeshrajans, hareesh-pillai, netweb, mukesh27.
    Fixes #53866.
    
    
    Built from https://develop.svn.wordpress.org/trunk@51727
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51333 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51726';
+$wp_version = '5.9-alpha-51727';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
