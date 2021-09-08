0fdb93612ffe4c8d33249c17e1b86878199cd907
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-09-08 17:21:02 +0000

    General: Only use `_jsonp_wp_die_handler()` for JSONP REST API requests.
    Props mdawaffe, peterwilsoncc.
    Merges [51740] to the 5.8 branch.
    Built from https://develop.svn.wordpress.org/branches/5.8@51741
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@51349 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8.1-RC1-51723';
+$wp_version = '5.8.1-RC1-51741';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
