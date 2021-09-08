e1e1d021ead453e6a7561468e6268fcb9edfbfbf
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-09-08 17:16:57 +0000

    General: Only use `_jsonp_wp_die_handler()` for JSONP REST API requests.
    Props mdawaffe, peterwilsoncc.
    Built from https://develop.svn.wordpress.org/trunk@51740
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51348 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -3557,9 +3557,9 @@ function wp_die( $message = '', $title = '', $args = array() ) {
 		 * @param callable $function Callback function name.
 		 */
 		$function = apply_filters( 'wp_die_json_handler', '_json_wp_die_handler' );
-	} elseif ( wp_is_jsonp_request() ) {
+	} elseif ( defined( 'REST_REQUEST' ) && REST_REQUEST && wp_is_jsonp_request() ) {
 		/**
-		 * Filters the callback for killing WordPress execution for JSONP requests.
+		 * Filters the callback for killing WordPress execution for JSONP REST requests.
 		 *
 		 * @since 5.2.0
 		 *
