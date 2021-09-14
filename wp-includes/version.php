d6cb1e431752fe53e38c9dc09b19b9146ac6ab29
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-09-14 17:31:59 +0000

    Docs: Update description for the `$wp_version` global.
    In addition to holding the version number, the WordPress version string is used to bust caches and to enable development mode for scripts when running from the `/src` directory.
    
    Follow-up to [803], [2585], [6554], [47230].
    
    Props muhammadfaizanhaidar, azaozz.
    Fixes #53413.
    Built from https://develop.svn.wordpress.org/trunk@51809
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51416 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -11,9 +11,12 @@
 /**
  * The WordPress version string.
  *
+ * Holds the current version number for WordPress core. Used to bust caches
+ * and to enable development mode for scripts when running from the /src directory.
+ *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51808';
+$wp_version = '5.9-alpha-51809';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
