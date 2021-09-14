130040c54e79ef3003481aa1b9a0387521606197
hellofromTonya <hellofromTonya@git.wordpress.org>
2021-09-14 18:33:57 +0000

    Build/Test Tools: Make `WP_TESTS_PHPUNIT_POLYFILLS_PATH` more flexible.
    The constant `WP_TESTS_PHPUNIT_POLYFILLS_PATH` is intended to contain the path to the root directory of the PHPUnit Polyfills library without trailing slash.
    
    The code already took into account that the value could potentially include a trailing slash.
    
    Now it will also take into account if it is accidentally set to point to the autoload file instead of the path.
    
    Follow-up to [51598].
    
    Props jrf, schlessera, hellofromTonya, jeherve, lucatume.
    See #46149.
    Built from https://develop.svn.wordpress.org/trunk@51810
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51417 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51809';
+$wp_version = '5.9-alpha-51810';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
