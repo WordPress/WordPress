8a5e9b79ed8b6955d020921780b7dfac7206c35a
Gary Pendergast <gary@pento.net>
2021-09-17 03:13:56 +0000

    Embeds: Add Pinterest as a trusted oEmbed provider.
    Props ayeshrajans, pento.
    
    Fixes #53448.
    
    Built from https://develop.svn.wordpress.org/trunk@51821
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51428 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-oembed.php
+++ wp-includes/class-wp-oembed.php
@@ -102,6 +102,7 @@ class WP_oEmbed {
 			'#https?://www\.someecards\.com/usercards/viewcard/.+#i' => array( 'https://www.someecards.com/v2/oembed/', true ),
 			'#https?://some\.ly\/.+#i'                     => array( 'https://www.someecards.com/v2/oembed/', true ),
 			'#https?://(www\.)?tiktok\.com/.*/video/.*#i'  => array( 'https://www.tiktok.com/oembed', true ),
+			'#https?://([a-z]{2}|www)\.pinterest\.com(\.(au|mx))?/.*#i' => array( 'https://www.pinterest.com/oembed.json', true ),
 		);
 
 		if ( ! empty( self::$early_providers['add'] ) ) {
@@ -179,6 +180,7 @@ class WP_oEmbed {
 		 * | Someecards   | some.ly                                   | 4.9.0   |
 		 * | Crowdsignal  | survey.fm                                 | 5.1.0   |
 		 * | TikTok       | tiktok.com                                | 5.4.0   |
+		 * | Pinterest    | pinterest.com                             | 5.9.0   |
 		 *
 		 * No longer supported providers:
 		 *
