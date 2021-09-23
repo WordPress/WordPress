7ce933c6951c0065faf9c0f3bcf3a30a6992c1ef
whyisjake <whyisjake@git.wordpress.org>
2021-09-23 20:34:56 +0000

    Coding Standards: Fix the alignment of the array.
    Unprops: whyisjake.
    See [51855].
    
    Built from https://develop.svn.wordpress.org/trunk@51856
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51455 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51855';
+$wp_version = '5.9-alpha-51856';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
