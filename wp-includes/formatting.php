a60032feecf2002187218bdbf9e09c408b6d9f3a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-10-18 16:53:57 +0000

    Coding Standards: Add a leading zero in the CSS declarations printed by the `print_emoji_styles()` function.
    Follow-up to [31733], [31786].
    
    Props audrasjb, sabernhardt.
    Fixes #54284.
    Built from https://develop.svn.wordpress.org/trunk@51918
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51511 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -5650,7 +5650,7 @@ img.emoji {
 	box-shadow: none !important;
 	height: 1em !important;
 	width: 1em !important;
-	margin: 0 .07em !important;
+	margin: 0 0.07em !important;
 	vertical-align: -0.1em !important;
 	background: none !important;
 	padding: 0 !important;
