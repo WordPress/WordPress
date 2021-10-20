e791d7f5db8252ba66c7777c456ed80343a450aa
desrosj <desrosj@git.wordpress.org>
2021-10-20 19:51:58 +0000

    Build/Test Tools: Fix syntax for passing secrets to a called workflow.
    Follow up to [51923].
    
    See #53363.
    Built from https://develop.svn.wordpress.org/trunk@51925
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51518 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51924';
+$wp_version = '5.9-alpha-51925';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
