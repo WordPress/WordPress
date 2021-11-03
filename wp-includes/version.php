3e81de0a3d446f2dc72536d65a3f6b32fec5839c
johnjamesjacoby <johnjamesjacoby@git.wordpress.org>
2021-11-03 17:01:57 +0000

    Themes/TwentySixteen: correct invalid CSS `font-style` value.
    This change swaps out `none` for `normal` in the `.wp-block-pullquote cite` styling of `blocks.css`.
    
    Props malae, mukesh27, sabernhardt.
    
    Fixes #46807.
    Built from https://develop.svn.wordpress.org/trunk@52004
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51593 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-52003';
+$wp_version = '5.9-alpha-52004';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
