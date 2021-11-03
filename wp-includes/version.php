a949417a2e82ac55b6bfb121c80fc1f21bab839b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-11-03 00:55:56 +0000

    Tests: Correct `@covers` tags in `WP_Comments_List_Table` tests.
    Follow-up to [51993], [51997].
    
    See #39265.
    Built from https://develop.svn.wordpress.org/trunk@51998
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51587 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-51997';
+$wp_version = '5.9-alpha-51998';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
