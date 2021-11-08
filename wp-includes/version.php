80b78b077dabaecc8c0d2c713f12b9c7b66a648d
desrosj <desrosj@git.wordpress.org>
2021-11-08 16:26:01 +0000

    Build/Test Tools: Update/Audit NPM dependencies for bundled themes.
    This updates several NPM dependencies to their latest (or latest allowed) versions for bundled themes that have a `package.json` file (Twenty Nineteen and newer).
    
    These updates did not result in any changes to the built files in Twenty Nineteen or Twenty Twenty. Some unnecessary parenthesis have been removed from `calc()` function calls within Twenty Twenty-One.
    
    This change also contains several updates to the `package-lock.json` files made by running `npm audit fix` within each of these themes.
    
    See #53361.
    Built from https://develop.svn.wordpress.org/trunk@52047
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51639 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-52046';
+$wp_version = '5.9-alpha-52047';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
