cba4afb663a8f282ead7829c13eae3758a7af0f7
jorgefilipecosta <jorgefilipecosta@git.wordpress.org>
2021-11-08 20:33:59 +0000

    Fix: Update block-theme-pl_PL.mo file.
    See #54336.
    Props oandregal.
    Built from https://develop.svn.wordpress.org/trunk@52050
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51642 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-52049';
+$wp_version = '5.9-alpha-52050';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
