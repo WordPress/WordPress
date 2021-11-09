80c868534afc1da5684dc3580b581d6f3034b0b9
hellofromTonya <hellofromTonya@git.wordpress.org>
2021-11-09 16:21:01 +0000

    Login and Registration: Fix failing test for "email already exists" registration error improvement.
    Follow-up to [52074].
    
    Fixes #53631.
    Built from https://develop.svn.wordpress.org/trunk@52075
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51667 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-52074';
+$wp_version = '5.9-alpha-52075';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
