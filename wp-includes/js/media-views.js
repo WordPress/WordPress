e47027ff1f32a61d370e18b1b362699966994c2a
Adam Silverstein <adamsilverstein@earthboundhosting.com>
2021-11-09 15:01:00 +0000

    Media: Add "webp" extension to wp.media.controller.Library `isImageAttachment`.
    Add "webp" extension to a media extension check, fixing an issue where the last used media
    link type was not used for WebP images.
    
    Props almendron, desrosj, kallookoo.
    Fixes #53917.
    
    
    Built from https://develop.svn.wordpress.org/trunk@52073
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51665 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/media-views.js
+++ wp-includes/js/media-views.js
@@ -3451,7 +3451,7 @@ Library = wp.media.controller.State.extend(/** @lends wp.media.controller.Librar
 	isImageAttachment: function( attachment ) {
 		// If uploading, we know the filename but not the mime type.
 		if ( attachment.get('uploading') ) {
-			return /\.(jpe?g|png|gif)$/i.test( attachment.get('filename') );
+			return /\.(jpe?g|png|gif|webp)$/i.test( attachment.get('filename') );
 		}
 
 		return attachment.get('type') === 'image';
