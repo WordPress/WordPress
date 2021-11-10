04a101e4d07fbe762a19fbfbf3d3f61d1c1da90c
desrosj <desrosj@git.wordpress.org>
2021-11-10 17:03:58 +0000

    WordPress 5.8.2. Built from https://develop.svn.wordpress.org/branches/5.8@52112
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@51704 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -52,6 +52,31 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 							'<strong>Version %1$s</strong> addressed a security issue and fixed %2$s bugs.',
 							60
 						),
+						'5.8.2',
+						number_format_i18n( 2 )
+					);
+					?>
+					<?php
+					printf(
+						/* translators: %s: HelpHub URL */
+						__( 'For more information, see <a href="%s">the release notes</a>.' ),
+						sprintf(
+							/* translators: %s: WordPress version */
+							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							sanitize_title( '5.8.2' )
+						)
+					);
+					?>
+				</p>
+				<p>
+					<?php
+					printf(
+						/* translators: 1: WordPress version number, 2: plural number of bugs. */
+						_n(
+							'<strong>Version %1$s</strong> addressed a security issue and fixed %2$s bug.',
+							'<strong>Version %1$s</strong> addressed a security issue and fixed %2$s bugs.',
+							60
+						),
 						'5.8.1',
 						number_format_i18n( 60 )
 					);
