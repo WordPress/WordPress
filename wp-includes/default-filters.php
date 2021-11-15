5b85c731945b5148fa9ad609f4f4581c99ab67b9
noisysocks <noisysocks@git.wordpress.org>
2021-11-15 11:01:00 +0000

    Editor: Load iframed assets in Site Editor
    Configure the Site Editor to load iframed block editor assets. This fixes some
    styling issues in the Site Editor.
    
    Follows [52069].
    See #54337.
    
    Built from https://develop.svn.wordpress.org/trunk@52160
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51752 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-filters.php
+++ wp-includes/default-filters.php
@@ -582,6 +582,7 @@ add_action( 'wp_footer', 'wp_maybe_inline_styles', 1 ); // Run for late-loaded s
 add_action( 'admin_footer-post.php', 'wp_add_iframed_editor_assets_html' );
 add_action( 'admin_footer-post-new.php', 'wp_add_iframed_editor_assets_html' );
 add_action( 'admin_footer-widgets.php', 'wp_add_iframed_editor_assets_html' );
+add_action( 'admin_footer-site-editor.php', 'wp_add_iframed_editor_assets_html' );
 
 add_action( 'use_block_editor_for_post_type', '_disable_block_editor_for_navigation_post_type', 10, 2 );
 add_action( 'edit_form_after_title', '_disable_content_editor_for_navigation_post_type' );
