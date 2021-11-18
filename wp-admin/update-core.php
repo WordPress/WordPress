b3b8d6959c76199d3c67b08951fbf0967ede3e27
audrasjb <audrasjb@git.wordpress.org>
2021-11-18 09:25:03 +0000

    Upgrade/Install: Add timezone info to last checked update time.
    The time displayed on the Updates screen matches the server time which may not be the timezone the person viewing the page is in. This change includes the timezone information with the time.
    
    Props mkaz.
    Fixes #53554.
    
    Built from https://develop.svn.wordpress.org/trunk@52203
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51795 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/update-core.php
+++ wp-admin/update-core.php
@@ -1046,7 +1046,7 @@ if ( 'upgrade-core' === $action ) {
 
 	echo '<p class="update-last-checked">';
 	/* translators: 1: Date, 2: Time. */
-	printf( __( 'Last checked on %1$s at %2$s.' ), date_i18n( __( 'F j, Y' ), $last_update_check ), date_i18n( __( 'g:i a' ), $last_update_check ) );
+	printf( __( 'Last checked on %1$s at %2$s.' ), date_i18n( __( 'F j, Y' ), $last_update_check ), date_i18n( __( 'g:i a T' ), $last_update_check ) );
 	echo ' <a href="' . esc_url( self_admin_url( 'update-core.php?force-check=1' ) ) . '">' . __( 'Check again.' ) . '</a>';
 	echo '</p>';
 
