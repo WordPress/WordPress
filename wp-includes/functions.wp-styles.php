fac37443878b915b25d6a4e1dd526796c3c86276
desrosj <desrosj@git.wordpress.org>
2021-11-19 15:14:00 +0000

    Script Loader: Document `path` as an accepted value for `$key` in `wp_style_add_data()`.
    Follow up to [50836].
    
    Props tmatsuur.
    Fixes #53792.
    Built from https://develop.svn.wordpress.org/trunk@52216
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51808 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.wp-styles.php
+++ wp-includes/functions.wp-styles.php
@@ -225,14 +225,18 @@ function wp_style_is( $handle, $list = 'enqueued' ) {
  * 'suffix'      string      Optional suffix, used in combination with RTL.
  * 'alt'         bool        For rel="alternate stylesheet".
  * 'title'       string      For preferred/alternate stylesheets.
+ * 'path'        string      The absolute path to a stylesheet. Stylesheet will
+ *                           load inline when 'path'' is set.
  *
  * @see WP_Dependencies::add_data()
  *
  * @since 3.6.0
+ * @since 5.8.0 Added 'path' as an official value for $key.
+ *              See {@see wp_maybe_inline_styles()}.
  *
  * @param string $handle Name of the stylesheet.
  * @param string $key    Name of data point for which we're storing a value.
- *                       Accepts 'conditional', 'rtl' and 'suffix', 'alt' and 'title'.
+ *                       Accepts 'conditional', 'rtl' and 'suffix', 'alt', 'title' and 'path'.
  * @param mixed  $value  String containing the CSS data to be added.
  * @return bool True on success, false on failure.
  */
