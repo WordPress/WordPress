c9ecd13aca2bb869d0aad7dbac8e951a286d3fd3
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-11-20 02:49:58 +0000

    Coding Standards: Wrap some long lines in `js/_enqueues/admin/post.js` per the JS coding standards for better readability.
    See #53359.
    Built from https://develop.svn.wordpress.org/trunk@52228
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51820 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-52227';
+$wp_version = '5.9-alpha-52228';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
