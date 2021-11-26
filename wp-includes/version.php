904c4a8b5cf4dfe660f6650a5b83d46d040ac330
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-11-26 01:29:04 +0000

    Tests: Remove unexpected output in `wp_dashboard_recent_drafts()` tests on PHP 8.1.
    This follows the approach used in other tests to let PHPUnit manage the output catching and effectively ignore the output until retrieving it later via `getActualOutput()`.
    
    Follow-up to [45505], [51968], [52173].
    
    See #53635, #53363.
    Built from https://develop.svn.wordpress.org/trunk@52253
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51845 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-52252';
+$wp_version = '5.9-alpha-52253';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
