5107bba1af13d75417f4fc7f6a74ad1998a78bcf
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-11-27 16:18:00 +0000

    Tests: Use a simpler approach to test the output in some tests.
    Instead of ignoring the output and catching it later with `getActualOutput()`, we can use `expectOutputRegex()` directly, which evaluates the output after the rest of the test code has run, if no unexpected errors were encountered.
    
    Follow-up to [52173], [52253].
    
    Props jrf.
    See #53635, #53363.
    Built from https://develop.svn.wordpress.org/trunk@52259
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51851 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-52258';
+$wp_version = '5.9-alpha-52259';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
