72905034809b3a4d92385cc83453499f0260ddd5
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-11-28 10:35:59 +0000

    Tests: Temporarily disable the check that the current recommended PHP version is actively supported.
    As PHP 7.4 moved from active support to security support until 28 Nov 2022, and no discussions or decisions have been made yet on bumping the recommended version to PHP 8.0, this commit disables the failing assertion for now.
    
    This should be revisited once WordPress achieves PHP 8.0 compatibility and the recommended PHP version in `readme.html` can be bumped, or another decision is made about this.
    
    Follow-up to [26166], [33937], [33944], [33946], [35172], [39582], [39583], [40241], [46682], [46996], [46998].
    
    Props costdev, audrasjb.
    Fixes #54528.
    Built from https://develop.svn.wordpress.org/trunk@52260
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51852 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-alpha-52259';
+$wp_version = '5.9-alpha-52260';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
