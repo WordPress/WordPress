600aa23dd6bf27d44aa8a0e1771e4cc1fff96060
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-12-07 21:20:59 +0000

    WordPress 5.9 Beta 2. Built from https://develop.svn.wordpress.org/trunk@52343
    git-svn-id: http://core.svn.wordpress.org/trunk@51935 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-beta1-52342';
+$wp_version = '5.9-beta2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
