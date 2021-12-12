cfd6406f8fe3a54eaaaf19720b19c1499db148a3
audrasjb <audrasjb@git.wordpress.org>
2021-12-12 19:01:02 +0000

    Docs: Capitalize "ID", when referring to a sidebar ID in a more consistent way.
    Follow-up to [52016].
    
    See #53399.
    
    
    Built from https://develop.svn.wordpress.org/trunk@52361
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51953 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/widgets.php
+++ wp-includes/widgets.php
@@ -1040,13 +1040,13 @@ function wp_get_sidebars_widgets( $deprecated = true ) {
 }
 
 /**
- * Retrieves the registered sidebar with the given id.
+ * Retrieves the registered sidebar with the given ID.
  *
  * @since 5.9.0
  *
  * @global array $wp_registered_sidebars The registered sidebars.
  *
- * @param string $id The sidebar id.
+ * @param string $id The sidebar ID.
  * @return array|null The discovered sidebar, or null if it is not registered.
  */
 function wp_get_sidebar( $id ) {
