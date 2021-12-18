28b48cecd6ee477faacb27fe395f3bbded5f7436
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-12-18 21:17:59 +0000

    Tests: Fix typo in a test method name.
    Follow-up to [52261].
    
    See #53363.
    Built from https://develop.svn.wordpress.org/trunk@52387
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51979 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-beta3-52386';
+$wp_version = '5.9-beta3-52387';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
