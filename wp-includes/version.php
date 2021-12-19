cc5119e5e140a239290eb05f04ac80daa874913f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-12-19 16:25:02 +0000

    Tests: Use shared fixtures in block theme tests.
    This removes duplicate test data and aims to avoid future confusion about which themes to use in which tests.
    
    Follow-up to [52049], [52246], [52247], [52279].
    
    See #53363.
    Built from https://develop.svn.wordpress.org/trunk@52391
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51983 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-beta3-52390';
+$wp_version = '5.9-beta3-52391';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
