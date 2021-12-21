770e631dfcf3d7628a9c46b05ee5c8c02cc8c389
hellofromTonya <hellofromTonya@git.wordpress.org>
2021-12-21 04:26:00 +0000

    Editor: Add 'edit_theme_options' capabilities to `wp_navigation` post type.
    Adds `'edit_theme_options'` capabilities top restrict Navigation permission.
    
    Partial backport from Gutenberg https://github.com/WordPress/gutenberg/pull/37454.
    
    Follow-up to [52069], [52145].
    
    Props spacedmonkey, get_dave, hellofromTonya.
    See #54487.
    Built from https://develop.svn.wordpress.org/trunk@52400
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@51992 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -509,6 +509,18 @@ function create_initial_post_types() {
 			'show_in_rest'          => true,
 			'rewrite'               => false,
 			'map_meta_cap'          => true,
+			'capabilities'          => array(
+				'edit_others_posts'      => 'edit_theme_options',
+				'delete_posts'           => 'edit_theme_options',
+				'publish_posts'          => 'edit_theme_options',
+				'create_posts'           => 'edit_theme_options',
+				'read_private_posts'     => 'edit_theme_options',
+				'delete_private_posts'   => 'edit_theme_options',
+				'delete_published_posts' => 'edit_theme_options',
+				'delete_others_posts'    => 'edit_theme_options',
+				'edit_private_posts'     => 'edit_theme_options',
+				'edit_published_posts'   => 'edit_theme_options',
+			),
 			'rest_base'             => 'navigation',
 			'rest_controller_class' => 'WP_REST_Posts_Controller',
 			'supports'              => array(
