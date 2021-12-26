2caff694aae3c5b5015d727d9cfb7739cb6da072
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-12-26 23:42:05 +0000

    Tests: Separate the tests for recommended PHP and MySQL versions in `readme.html`
    This aims to simplify the individual tests and reduce duplication using a helper function.
    
    Follow-up to [52260], [52319], [52358].
    
    See #41490.
    Built from https://develop.svn.wordpress.org/trunk@52418
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52010 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-beta4-52417';
+$wp_version = '5.9-beta4-52418';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
