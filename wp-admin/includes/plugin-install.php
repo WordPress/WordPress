05a3e199245397622bbdbcbc0fc430e315055d80
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2021-12-27 16:06:02 +0000

    Plugins: Escape the WordPress.org plugin page URL in the Plugin Installation modal.
    Follow-up to [8540], [38953].
    
    Props chintan1896, Presskopp, dimadin, henry.wright, aezazshekh, SergeyBiryukov.
    Fixes #54362.
    Built from https://develop.svn.wordpress.org/trunk@52419
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52011 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/plugin-install.php
+++ wp-admin/includes/plugin-install.php
@@ -697,7 +697,7 @@ function install_plugin_information() {
 				?>
 				</li>
 			<?php } if ( ! empty( $api->slug ) && empty( $api->external ) ) { ?>
-				<li><a target="_blank" href="<?php echo __( 'https://wordpress.org/plugins/' ) . $api->slug; ?>/"><?php _e( 'WordPress.org Plugin Page &#187;' ); ?></a></li>
+				<li><a target="_blank" href="<?php echo esc_url( __( 'https://wordpress.org/plugins/' ) . $api->slug ); ?>/"><?php _e( 'WordPress.org Plugin Page &#187;' ); ?></a></li>
 			<?php } if ( ! empty( $api->homepage ) ) { ?>
 				<li><a target="_blank" href="<?php echo esc_url( $api->homepage ); ?>"><?php _e( 'Plugin Homepage &#187;' ); ?></a></li>
 			<?php } if ( ! empty( $api->donate_link ) && empty( $api->contributors ) ) { ?>
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-beta4-52418';
+$wp_version = '5.9-beta4-52419';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
