e61c53f2e1959d757e039af5f106f298ca1bcd47
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-01-04 20:41:02 +0000

    Post WordPress 5.9 RC1 version bump. Built from https://develop.svn.wordpress.org/trunk@52446
    git-svn-id: http://core.svn.wordpress.org/trunk@52038 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-RC1';
+$wp_version = '5.9-RC1-52446';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
