5fc6f2a4889ed61149dfcb7adfd5312bb7edd8c1
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-01-04 20:54:00 +0000

    Trunk is now 6.0-alpha. Built from https://develop.svn.wordpress.org/trunk@52448
    git-svn-id: http://core.svn.wordpress.org/trunk@52040 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-RC1-52446';
+$wp_version = '6.0-alpha-52448';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
