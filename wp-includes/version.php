cfd95595231f49676829689fbd617be10eaa3757
ryelle <ryelle@git.wordpress.org>
2022-01-04 19:18:01 +0000

    Administration: Add missing texture image for Welcome panel & About page.
    See #54489, #54270.
    
    
    Built from https://develop.svn.wordpress.org/trunk@52443
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52035 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-beta4-52442';
+$wp_version = '5.9-beta4-52443';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
