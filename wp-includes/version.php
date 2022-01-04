9f9440a039272dd47c7ad22aedd8739033254974
hellofromTonya <hellofromTonya@git.wordpress.org>
2022-01-04 21:31:01 +0000

    Build/Test Tools: Add the 5.9 branch to the workflow for testing branches. Built from https://develop.svn.wordpress.org/trunk@52449
    git-svn-id: http://core.svn.wordpress.org/trunk@52041 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52448';
+$wp_version = '6.0-alpha-52449';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
