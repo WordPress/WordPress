2699b3032a710335e47a4a3a9d3fd5e44c35bac0
audrasjb <audrasjb@git.wordpress.org>
2022-01-05 22:14:59 +0000

    Help/About: Correction on the documentation link about adding new themes.
    In the Themes Screen Help Tab, the "Documentation on Adding New Themes" link previously targeted the "Using Themes" HelpHub post whereas it had to target the "Install Themes" section of "Appearance Themes Screen" HelpHub post, which is more relevant.
    
    Props jdy68.
    Fixes #54709.
    
    Built from https://develop.svn.wordpress.org/trunk@52452
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52044 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/theme-install.php
+++ wp-admin/theme-install.php
@@ -137,7 +137,7 @@ get_current_screen()->add_help_tab(
 
 get_current_screen()->set_help_sidebar(
 	'<p><strong>' . __( 'For more information:' ) . '</strong></p>' .
-	'<p>' . __( '<a href="https://wordpress.org/support/article/using-themes/#adding-new-themes">Documentation on Adding New Themes</a>' ) . '</p>' .
+	'<p>' . __( '<a href="https://wordpress.org/support/article/appearance-themes-screen/#install-themes">Documentation on Adding New Themes</a>' ) . '</p>' .
 	'<p>' . __( '<a href="https://wordpress.org/support/">Support</a>' ) . '</p>'
 );
 
