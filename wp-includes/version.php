77a972838c495dca96164e9ebfa24780be439e4e
desrosj <desrosj@git.wordpress.org>
2022-01-06 17:10:00 +0000

    Formatting: Correctly encode ASCII characters in post slugs.
    Props zieladam, whyisjake, xknown, peterwilsoncc, desrosj, iandunn.
    Built from https://develop.svn.wordpress.org/trunk@52457
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52049 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52456';
+$wp_version = '6.0-alpha-52457';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
