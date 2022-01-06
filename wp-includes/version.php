c9c4dabcc1c920fc43ee6f7331011abd029d4f9f
desrosj <desrosj@git.wordpress.org>
2022-01-06 17:05:02 +0000

    Query: Improve sanitization within `WP_Meta_Query`.
    Props vortfu, xknown, dd32.
    Built from https://develop.svn.wordpress.org/trunk@52455
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52047 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52454';
+$wp_version = '6.0-alpha-52455';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
