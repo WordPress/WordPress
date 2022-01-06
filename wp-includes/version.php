638ef7815ec29318434365ffde1d3cef7437b948
desrosj <desrosj@git.wordpress.org>
2022-01-06 17:07:03 +0000

    Upgrade/Install: Avoid using `unserialize()` unnecessarily.
    Props vortfu, xknown, dd32.
    Built from https://develop.svn.wordpress.org/trunk@52456
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52048 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52455';
+$wp_version = '6.0-alpha-52456';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
