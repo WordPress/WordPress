ff2e2ddcff8b3a166bfb9ab42be40c235de5fa67
desrosj <desrosj@git.wordpress.org>
2022-01-06 17:24:01 +0000

    Query: Improve sanitization within `WP_Meta_Query`.
    Merges [52455] to the 5.9 branch.
    Props vortfu, xknown, dd32.
    Built from https://develop.svn.wordpress.org/branches/5.9@52460
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52052 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-RC1-52458';
+$wp_version = '5.9-RC1-52460';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
