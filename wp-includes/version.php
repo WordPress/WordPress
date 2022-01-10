4e9316f722b25d04fb06a98843c9d5bc81e8d479
jorgefilipecosta <jorgefilipecosta@git.wordpress.org>
2022-01-10 17:59:02 +0000

    Fix: WordPress default presets aren't loaded for all themes.
    The default preset styles of WordPress (font sizes, colors, gradients) are now provided via the global stylesheet to avoid duplication of styles. This stylesheet can be found in the front-end with the name global-styles-inline-css.
    The default presets are important to support old content that may use them and also patterns, that can use cross-theme styles.
    
    Props oandregal.
    Merges [52547] to the 5.9 branch.
    Fixes #54781.
    Built from https://develop.svn.wordpress.org/branches/5.9@52548
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52138 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-RC1-52542';
+$wp_version = '5.9-RC1-52548';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
