f950eec32325e8cbd4e9cd87aac2b718c6879df0
davidbaumwald <davidbaumwald@git.wordpress.org>
2022-01-11 17:16:00 +0000

    Script Loader: Fix a variable typo in `wp_enqueue_block_style`.
    In `wp_enqueue_block_style` when `rtl` versions of CSS files are detected, `wp_style_add_data` is called to add the metadata to the registered sheet.  However, the stylesheet `handle` argument is misspelled as `hanle`.  This change corrects the argument name.
    
    Props omaeyusuke.
    Fixes #54786.
    Built from https://develop.svn.wordpress.org/trunk@52558
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52148 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52556';
+$wp_version = '6.0-alpha-52558';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
