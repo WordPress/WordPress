908a08696b58bfa4e1c7ea363b2ac053ac5f8907
ryelle <ryelle@git.wordpress.org>
2022-01-11 18:27:03 +0000

    Administration: Update design of the Dashboard welcome panel.
    Tone down the brightness of the dashboard panel by using a white background behind the main text, and use the highlight color from each color scheme for the main background. This also introduces a `$scheme-name` SCSS variable to allow overrides for specific color schemes.
    
    Props critterverse, sabernhardt, joedolson, audrasjb.
    See #54489.
    
    
    Built from https://develop.svn.wordpress.org/trunk@52562
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52152 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52558';
+$wp_version = '6.0-alpha-52562';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
