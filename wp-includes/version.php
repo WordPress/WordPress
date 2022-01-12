b00a83b9962d0dbe2e8d7aa3d707b847f4f1e964
audrasjb <audrasjb@git.wordpress.org>
2022-01-12 23:30:59 +0000

    Tests: Trac ticket number correction after changeset [52569].
    Follow-up to [52569].
    
    See #54798.
    
    Built from https://develop.svn.wordpress.org/trunk@52570
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52160 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52569';
+$wp_version = '6.0-alpha-52570';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
