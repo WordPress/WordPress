e158979d3d8e5d693969721341c711d012b7fcc7
hellofromTonya <hellofromTonya@git.wordpress.org>
2022-01-17 22:42:03 +0000

    Editor: Explicitly load remote block patterns in the block and site editor screens.
    Remote block patterns from wp.org were to be loaded through a callback hooked into the `current_screen` filter. Within 2 callbacks, i.e. `_load_remote_featured_patterns()` and `_load_remote_block_patterns()`, a guard clause bailed out early if the `$current_screen->is_block_editor` is `false`.
    
    However, the `current_screen` filter is unreliable to detect the block editor. Why? In the block and Site Editor screens, `$current_scren->is_block_editor` is not set until after the filter is executed. Whoopsie.
    
    This commit no longer uses the `current_screen` filter. Instead, it explicitly loads the remote block patterns by invoking both private functions (now not callbacks) directly in the screen files for the block and site editor screens.
    
    With this change, passing `WP_Screen` object into these functions is no longer needed. As the `_load_remote_block_patterns()` function was introduced in 5.8.0, its function parameter is now deprecated and the guard clause retained for backwards compatibility.
    
    Follow-up to [51021], [52377].
    
    Props poena, noisysocks, peterwilsoncc, hellofromTonya, audrasjb.
    Fixes #54806.
    Built from https://develop.svn.wordpress.org/trunk@52593
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52181 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/site-editor.php
+++ wp-admin/site-editor.php
@@ -31,6 +31,10 @@ $parent_file = 'themes.php';
 $current_screen = get_current_screen();
 $current_screen->is_block_editor( true );
 
+// Load block patterns from w.org.
+_load_remote_block_patterns();
+_load_remote_featured_patterns();
+
 // Default to is-fullscreen-mode to avoid jumps in the UI.
 add_filter(
 	'admin_body_class',
