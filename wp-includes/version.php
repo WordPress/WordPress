b9a42e2de2c2faa9d62a98d00c38c32a40042405
hellofromTonya <hellofromTonya@git.wordpress.org>
2022-01-17 20:40:02 +0000

    Administration: Revert [51946].
    Reverting changeset due to reported issue of menu being hidden after clicking on certain mobile devices.
    
    Props dhusakovic, audrasjb, SergeyBiryukov, costdev, joedolson.
    Reverts [51946] on the 5.9 branch.
    See #54837, #53587.
    Built from https://develop.svn.wordpress.org/branches/5.9@52592
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52180 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9-RC2-52590';
+$wp_version = '5.9-RC2-52592';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
