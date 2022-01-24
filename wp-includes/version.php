2900d9eccf46ede0b3bec4d56906c7a199b38eb7
desrosj <desrosj@git.wordpress.org>
2022-01-24 15:57:28 +0000

    Editor: Bump editor packages to include the latest fixes.
    This merges the latest package updates for the block editor to include the fix for https://github.com/WordPress/gutenberg/pull/38175.
    
    Props hellofromTonya, mamaduka, joen, talldanwp, cbravobernal, poena.
    Fixes #54487.
    Built from https://develop.svn.wordpress.org/trunk@52633
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52221 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52631';
+$wp_version = '6.0-alpha-52633';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
