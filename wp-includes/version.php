0e9a48e0cb43039cccb345c30715ae9787b3549d
audrasjb <audrasjb@git.wordpress.org>
2022-01-24 15:28:02 +0000

    Docs: Docblock corrections for `get_block_file_template()`.
    Follow-up to [52324].
    
    Props johnbillion, SergeyBiryukov, costdev.
    Fixes #54879.
    
    Built from https://develop.svn.wordpress.org/trunk@52631
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52219 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52628';
+$wp_version = '6.0-alpha-52631';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
