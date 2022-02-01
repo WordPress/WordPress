4894efc122bb1c40c05bbc1a1b38dc647c0c0608
audrasjb <audrasjb@git.wordpress.org>
2022-02-01 10:31:04 +0000

    Twenty Twenty-One: Replace GitHub with .org link in Theme URI.
    Theme URI is the URL of a public web page where users can find more information about the theme. For bundled themes, it should be a link to the theme’s page on the Theme Repository.
    
    Props kafleg.
    Fixes #55018.
    
    Built from https://develop.svn.wordpress.org/trunk@52658
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52247 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52657';
+$wp_version = '6.0-alpha-52658';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
