d4457b0debc925d418880c2adf9d2e81a84bc57d
Peter Wilson <wilson@peterwilson.cc>
2022-02-03 00:15:02 +0000

    Twenty Twenty-Two: Make 404 pattern search label and button translatable.
    This changes makes the Twenty Twenty-Two "hidden-404" pattern fully prepared for translation.
    
    Props kjellr, walbo, audrasjb.
    Merges [52664] to the 5.9 branch.
    Fixes #54928.
    
    Built from https://develop.svn.wordpress.org/branches/5.9@52666
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52255 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9.1-alpha-52665';
+$wp_version = '5.9.1-alpha-52666';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
