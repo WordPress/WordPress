1b818232449706f06f510f3120a13f666764908b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-02-04 14:35:00 +0000

    Tests: Use more appropriate assertions in `wp_get_global_stylesheet()` tests.
    This replaces instances of `assertTrue( str_contains( ... ) )` with `assertStringContainsString()` to use native PHPUnit functionality.
    
    Follow-up to [52675], [52676].
    
    See #54782.
    Built from https://develop.svn.wordpress.org/trunk@52677
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52266 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52676';
+$wp_version = '6.0-alpha-52677';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
