6b6f78705c61b77bfd2e8aa38940b9f2a26f6171
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-02-04 14:48:05 +0000

    Themes: Fix for Classic themes using default presets.
    This commit makes the presets provided by the theme via `add_theme_support()` always create CSS Custom Properties, whether or not the theme has a `theme.json` file. This way, if the theme overwrites a core preset, the core CSS variables are also overwritten and use the theme value.
    
    Props oandregal, hellofromTonya, desrosj, costdev, pbearne, johnstonphilip, webmandesign, jorgefilipecosta, SergeyBiryukov.
    Merges [52675-52677] to the 5.9 branch.
    Fixes #54782.
    Built from https://develop.svn.wordpress.org/branches/5.9@52678
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52267 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9.1-alpha-52667';
+$wp_version = '5.9.1-alpha-52678';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
