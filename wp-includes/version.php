ca5f3d0af6d96d9a283068ee408067c4b0c34ea9
audrasjb <audrasjb@git.wordpress.org>
2022-02-07 14:38:59 +0000

    Twenty Twenty-Two: Restore custom padding for group blocks with a background color.
    This change partially reverts [52679] which accidentally removed custom padding for group blocks with a background color.
    
    Follow-up to [52679].
    
    Props kjellr.
    Fixes #55103.
    See #54944.
    
    Built from https://develop.svn.wordpress.org/trunk@52686
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52275 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52685';
+$wp_version = '6.0-alpha-52686';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
