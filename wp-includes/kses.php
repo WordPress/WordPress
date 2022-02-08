22c9355e2dede8a53910f2ed075f6bda33698255
audrasjb <audrasjb@git.wordpress.org>
2022-02-08 16:25:01 +0000

    Docs: Fix an error in `wp_kses_attr()` docblock.
    Props kebbet.
    See #54729.
    
    Built from https://develop.svn.wordpress.org/trunk@52693
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52282 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/kses.php
+++ wp-includes/kses.php
@@ -1140,7 +1140,7 @@ function wp_kses_split2( $string, $allowed_html, $allowed_protocols ) {
  * Removes all attributes, if none are allowed for this element.
  *
  * If some are allowed it calls `wp_kses_hair()` to split them further, and then
- * it builds up new HTML code from the data that `kses_hair()` returns. It also
+ * it builds up new HTML code from the data that `wp_kses_hair()` returns. It also
  * removes `<` and `>` characters, if there are any left. One more thing it does
  * is to check if the tag has a closing XHTML slash, and if it does, it puts one
  * in the returned code as well.
