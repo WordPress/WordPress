47123dd3c8a904fd12e9d606eab4f7a56a364645
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-02-11 21:11:59 +0000

    Twenty Twenty-Two: Optimize the `assets/images/ducks.jpg` image.
    The new file should have a much better balance of quality and file size.
    
    Follow-up to [52081], [52107].
    
    Props kafleg, kjellr, jffng, kapilpaul.
    Merges [52651] to the 5.9 branch.
    Fixes #54911.
    Built from https://develop.svn.wordpress.org/branches/5.9@52710
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52299 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9.1-alpha-52709';
+$wp_version = '5.9.1-alpha-52710';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
