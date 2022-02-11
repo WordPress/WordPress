493585345fe884d4c013c764e36545d989c1f8a5
audrasjb <audrasjb@git.wordpress.org>
2022-02-11 21:38:02 +0000

    Editor: Remove standard post type UI for templates and template parts.
    In [52158] the standard post type UI was added back for templates and template parts, because these features had been temporarily removed from the site editor in the rush to get 5.9 ready for December. Since 5.9 these features were properly added back to the site editor, the `show_ui` values for these post types should be returned back to `false`.
    
    Follow-up to [52158].
    
    Props manfcarlo, audrasjb.
    Merges [52668] to the 5.9 branch.
    Fixes #54908.
    
    Built from https://develop.svn.wordpress.org/branches/5.9@52712
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52301 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9.1-alpha-52711';
+$wp_version = '5.9.1-alpha-52712';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
