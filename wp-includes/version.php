c3ff2a1cc663a466aa703615e4d8509ffe4c3af9
Clorith <Clorith@git.wordpress.org>
2022-02-15 17:49:00 +0000

    Filesystem API: Use a temp folder for `Content-Disposition` files.
    #38231 added support for files fetched remotely to have their filename defined by the host using the `Content-Disposition` header. This would then take priority over the existing temporary file name created with `wp_tempnam()` earlier in the process.
    
    The change unintentionally omitted the temporary directory path used during uploads, since the `wp_tempnam()` function would have added it previously, so that files with this header ended up being stored in the WordPress root folder, or wp-admin folder, when triggered by WP_Cron or user interactions respectively.
    
    This change makes sure the file path includes the temporary directory location when the header is used.
    
    Follow-up to [51939].
    
    Props antonynz, azouamauriac.
    Fixes #55109.
    Built from https://develop.svn.wordpress.org/trunk@52734
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52323 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52733';
+$wp_version = '6.0-alpha-52734';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
