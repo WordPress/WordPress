18945d341b5b5a31d7663703324755ba365c6654
hellofromTonya <hellofromTonya@git.wordpress.org>
2022-02-17 15:46:04 +0000

    Editor: Grant only admins access to the "Navigation Menus" UI for block and non-block themes.
    Restricts and grants access to only admin roles for the Navigation Menu UI screen, i.e. either directly through the URL wp-admin/edit.php?post_type=wp_navigation or via the Navigation block's "Manage menu" option (in the block's toolbar).
    
    It resolves 2 issues:
    
    - For non-block themes, fixes the issue where admins could not access the UI.
    - For block themes, restricts access to only admin roles, i.e. non-admins no longer have access to the UI.
    
    Non-admins will receive the "Sorry, you are not allowed to edit posts in this post type" error message.
    
    Follow-up [52069], [52145], [52330], [52400].
    
    Props ironprogrammer, costdev, noisysocks, talldanwp, hellofromTonya, manfcarlo, pyrobd.
    Merges [52755] to the 5.9 branch.
    Fixes #54889.
    Built from https://develop.svn.wordpress.org/branches/5.9@52756
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52345 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -503,7 +503,7 @@ function create_initial_post_types() {
 			'public'                => false,
 			'_builtin'              => true, /* internal use only. don't use this when registering your own post type. */
 			'has_archive'           => false,
-			'show_ui'               => wp_is_block_theme(),
+			'show_ui'               => true,
 			'show_in_menu'          => false,
 			'show_in_admin_bar'     => false,
 			'show_in_rest'          => true,
@@ -520,6 +520,7 @@ function create_initial_post_types() {
 				'delete_others_posts'    => 'edit_theme_options',
 				'edit_private_posts'     => 'edit_theme_options',
 				'edit_published_posts'   => 'edit_theme_options',
+				'edit_posts'             => 'edit_theme_options',
 			),
 			'rest_base'             => 'navigation',
 			'rest_controller_class' => 'WP_REST_Posts_Controller',
