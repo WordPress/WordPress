5040303d1a10d510a875d07d3722b615981d0a32
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-02-20 10:14:04 +0000

    Docs: Fix typo in `TracTickets::isTracTicketClosed()` description.
    Follow-up to [761/tests], [762/tests], [891/tests].
    
    See #54729.
    Built from https://develop.svn.wordpress.org/trunk@52779
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52368 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52778';
+$wp_version = '6.0-alpha-52779';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
