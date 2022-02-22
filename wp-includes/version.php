95c433c8cda199fc5cdc2f5020a7affb8b33c552
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-02-22 16:07:01 +0000

    Post WordPress 5.9.1 version bump.
    The 5.9 branch is now 5.9.2-alpha.
    Built from https://develop.svn.wordpress.org/branches/5.9@52788
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52377 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9.1';
+$wp_version = '5.9.2-alpha-52788';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
