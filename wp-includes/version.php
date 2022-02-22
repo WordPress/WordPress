ef7f72daec838f3928a904493fb2af6c143ac12c
audrasjb <audrasjb@git.wordpress.org>
2022-02-22 15:17:04 +0000

    WordPress 5.9.1.
    Built from https://develop.svn.wordpress.org/branches/5.9@52786
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52375 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9.1-RC1-52785';
+$wp_version = '5.9.1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
