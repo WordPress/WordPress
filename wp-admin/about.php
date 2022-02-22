d687a49c78b2e742ce557f53e682a5231c1c4353
audrasjb <audrasjb@git.wordpress.org>
2022-02-22 11:05:07 +0000

    Help/About: Update the About page for WordPress 5.9.1.
    Props hellofromTonya, audrasjb.
    Fixes #55173.
    
    Built from https://develop.svn.wordpress.org/branches/5.9@52785
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52374 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -39,6 +39,37 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 			</nav>
 		</div>
 
+		<div class="about__section changelog">
+			<div class="column">
+				<h2><?php _e( 'Maintenance Release' ); ?></h2>
+				<p>
+					<?php
+					printf(
+						/* translators: 1: WordPress version number, 2: plural number of bugs. */
+						_n(
+							'<strong>Version %1$s</strong> addressed %2$s bug.',
+							'<strong>Version %1$s</strong> addressed %2$s bugs.',
+							82
+						),
+						'5.9.1',
+						number_format_i18n( 82 )
+					);
+					?>
+					<?php
+					printf(
+						/* translators: %s: HelpHub URL. */
+						__( 'For more information, see <a href="%s">the release notes</a>.' ),
+						sprintf(
+							/* translators: %s: WordPress version. */
+							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							sanitize_title( '5.9.1' )
+						)
+					);
+					?>
+				</p>
+			</div>
+		</div>
+
 		<hr class="is-large" />
 
 		<div class="about__section">
