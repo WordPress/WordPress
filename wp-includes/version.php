f1d4f7f0f76ede7453e7f104523cce864c9ffc9a
audrasjb <audrasjb@git.wordpress.org>
2022-02-25 15:49:02 +0000

    Administration: Further enhancements on small text fields after [52649].
    This changeset replaces `#mailserver_port` to a more generic selector, fixes a coding standard issue, and adds a bigger margin to the field.
    
    Follow-up to [52649].
    
    Props SergeyBiryukov, kebbet, pyrobd.
    Fixes #50177.
    
    Built from https://develop.svn.wordpress.org/trunk@52801
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52390 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52800';
+$wp_version = '6.0-alpha-52801';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
