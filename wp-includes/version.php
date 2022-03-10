d1565514e442fa970467bf4d4dd1b9e49dcedf75
hellofromTonya <hellofromTonya@git.wordpress.org>
2022-03-10 16:02:01 +0000

    HTTP API: Fix typo in and improve readability of `wp_parse_url()` docblock.
    Removes the "starting" word from "starting containing" as this is a typo. Includes minor formatting adjustments to improve readability.
    
    Follow-up to [38726].
    
    Props mehedi890, SergeyBiryukov, mukesh27.
    Fixes #55355.
    Built from https://develop.svn.wordpress.org/trunk@52838
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52427 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52837';
+$wp_version = '6.0-alpha-52838';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
