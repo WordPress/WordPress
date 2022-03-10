2c9525edc38f16f5a7f4db1d85b5d010301e33ff
hellofromTonya <hellofromTonya@git.wordpress.org>
2022-03-10 16:28:03 +0000

    Build/Test Tools: Add trash and restore of a single post e2e tests.
    Adds an end-to-end (e2e) test case for:
    * Emptying a post from the trash.
    * Restoring a post from the trash.
    
    Props pavanpatil1, pooja1210, justinahinon, kevin940726.
    Fixes #54843.
    Built from https://develop.svn.wordpress.org/trunk@52839
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52428 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52838';
+$wp_version = '6.0-alpha-52839';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
