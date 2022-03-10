2108a32b6bc14438aa28c3a76647d5f5d6cf0d8f
davidbaumwald <davidbaumwald@git.wordpress.org>
2022-03-10 22:04:43 +0000

    WordPress 5.1.13. Built from https://develop.svn.wordpress.org/branches/5.1@52877
    git-svn-id: http://core.svn.wordpress.org/branches/5.1@52466 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1.12';
+$wp_version = '5.1.13';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
