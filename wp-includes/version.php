b972ba13bd3f09642bbac85d9fd313a2809a4fd7
audrasjb <audrasjb@git.wordpress.org>
2022-03-12 21:55:08 +0000

    Twenty Sixteen: Remove `.entry-content` selector from button styles.
    This change applies the default button styles to all button blocks in Twenty Sixteen, instead of only targeting those in `entry-content`. This means that any buttons added outside of `.entry-content`, such as in a widget area, will be styled the same as any other button blocks. This change also makes the theme custom colors work outside of `.entry-content`.
    
    Follow-up to [44306].
    
    Props mikachan, costdev, audrasjb.
    Fixes #55167.
    
    Built from https://develop.svn.wordpress.org/trunk@52926
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52515 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-52925';
+$wp_version = '6.0-alpha-52926';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
