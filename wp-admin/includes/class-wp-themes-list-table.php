775ae0b5f6eccff1cb8edfc7a95db7b45f233968
audrasjb <audrasjb@git.wordpress.org>
2022-03-18 12:44:04 +0000

    Themes: Add version to theme screenshot URL in `WP_Themes_List_Table`.
    Follow-up to [52947].
    
    See #53370.
    
    
    Built from https://develop.svn.wordpress.org/trunk@52948
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52537 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-themes-list-table.php
+++ wp-admin/includes/class-wp-themes-list-table.php
@@ -247,12 +247,12 @@ class WP_Themes_List_Table extends WP_List_Table {
 
 			<span class="screenshot hide-if-customize">
 				<?php if ( $screenshot ) : ?>
-					<img src="<?php echo esc_url( $screenshot ); ?>" alt="" />
+					<img src="<?php echo esc_url( $screenshot . '?ver=' . $theme->version ); ?>" alt="" />
 				<?php endif; ?>
 			</span>
 			<a href="<?php echo wp_customize_url( $stylesheet ); ?>" class="screenshot load-customize hide-if-no-customize">
 				<?php if ( $screenshot ) : ?>
-					<img src="<?php echo esc_url( $screenshot ); ?>" alt="" />
+					<img src="<?php echo esc_url( $screenshot . '?ver=' . $theme->version ); ?>" alt="" />
 				<?php endif; ?>
 			</a>
 
