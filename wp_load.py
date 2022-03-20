33c3df6910116626d0b0a62c42ab787868cfe584
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2022-03-20 16:21:43 +0530

    Update wp_load.py

--- wp_load.py
+++ wp_load.py
@@ -25,21 +25,24 @@ from pathlib import Path
 if not ABSPATH:
 	ABSPATH = Path(__file__).resolve().parent
 
+"""
 /*
  * The error_reporting() function can be disabled in php.ini. On systems where that is the case,
  * it's best to add a dummy function to the wp-config.php file, but as this call to the function
  * is run prior to wp-config.php loading, it is wrapped in a function_exists() check.
  */
-if ( function_exists( 'error_reporting' ) ) {
-	/*
-	 * Initialize error reporting to a known set of levels.
+"""
+
+if "error_reporting" in globals():
+	 """ * Initialize error reporting to a known set of levels.
 	 *
 	 * This will be adapted in wp_debug_mode() located in wp-includes/load.php based on WP_DEBUG.
 	 * @see http://php.net/manual/en/errorfunc.constants.php List of known error levels.
 	 */
-	error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
-}
+         """
+         error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR )
 
+"""
 /*
  * If wp-config.php exists in the WordPress root, or if it exists in the root and wp-settings.php
  * doesn't, load wp-config.php. The secondary check for wp-settings.php has the added benefit
@@ -48,6 +51,8 @@ if ( function_exists( 'error_reporting' ) ) {
  *
  * If neither set of conditions is true, initiate loading the setup process.
  */
+"""
+
 if ( file_exists( ABSPATH . 'wp-config.php' ) ) {
 
 	/** The config file resides in ABSPATH */
