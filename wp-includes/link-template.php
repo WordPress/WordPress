60ca4af9e1519ed7548c75a9f8742ac8ad1252d4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-03-20 15:05:05 +0000

    Docs: Correct variable references in `get_category_feed_link()` and `get_term_feed_link()` `@return` tags.
    The `$cat_id` and `$term_id` variables were replaced by `$cat` and `$term`, respectively.
    
    Follow-up to [52180].
    
    See #54729.
    Built from https://develop.svn.wordpress.org/trunk@52959
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52548 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/link-template.php
+++ wp-includes/link-template.php
@@ -904,10 +904,10 @@ function get_author_feed_link( $author_id, $feed = '' ) {
  *
  * @since 2.5.0
  *
- * @param int|WP_Term|object $cat  The ID or term object whose feed link will be retrieved.
+ * @param int|WP_Term|object $cat  The ID or category object whose feed link will be retrieved.
  * @param string             $feed Optional. Feed type. Possible values include 'rss2', 'atom'.
  *                                 Default is the value of get_default_feed().
- * @return string Link to the feed for the category specified by $cat_id.
+ * @return string Link to the feed for the category specified by `$cat`.
  */
 function get_category_feed_link( $cat, $feed = '' ) {
 	return get_term_feed_link( $cat, 'category', $feed );
@@ -925,7 +925,7 @@ function get_category_feed_link( $cat, $feed = '' ) {
  * @param string             $taxonomy Optional. Taxonomy of `$term_id`.
  * @param string             $feed     Optional. Feed type. Possible values include 'rss2', 'atom'.
  *                                     Default is the value of get_default_feed().
- * @return string|false Link to the feed for the term specified by $term_id and $taxonomy.
+ * @return string|false Link to the feed for the term specified by `$term` and `$taxonomy`.
  */
 function get_term_feed_link( $term, $taxonomy = '', $feed = '' ) {
 	if ( ! is_object( $term ) ) {
