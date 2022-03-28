1abfa45554e6605c40f2dee2654bdeeda4a03ad4
audrasjb <audrasjb@git.wordpress.org>
2022-03-28 09:45:06 +0000

    Docs: Use third-person singular verbs for function descriptions in `wp-includes/template.php`, per the documentation standards.
    See #54729.
    
    Built from https://develop.svn.wordpress.org/trunk@52999
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52588 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/template.php
+++ wp-admin/includes/template.php
@@ -19,7 +19,7 @@ require_once ABSPATH . 'wp-admin/includes/class-wp-internal-pointers.php';
 //
 
 /**
- * Output an unordered list of checkbox input elements labeled with category names.
+ * Outputs an unordered list of checkbox input elements labeled with category names.
  *
  * @since 2.5.1
  *
@@ -52,7 +52,7 @@ function wp_category_checklist( $post_id = 0, $descendants_and_self = 0, $select
 }
 
 /**
- * Output an unordered list of checkbox input elements labelled with term names.
+ * Outputs an unordered list of checkbox input elements labelled with term names.
  *
  * Taxonomy-independent version of wp_category_checklist().
  *
@@ -191,7 +191,7 @@ function wp_terms_checklist( $post_id = 0, $args = array() ) {
 }
 
 /**
- * Retrieve a list of the most popular terms from the specified taxonomy.
+ * Retrieves a list of the most popular terms from the specified taxonomy.
  *
  * If the $echo argument is true then the elements for a list of checkbox
  * `<input>` elements labelled with the names of the selected terms is output.
@@ -531,7 +531,7 @@ function wp_comment_reply( $position = 1, $checkbox = false, $mode = 'single', $
 }
 
 /**
- * Output 'undo move to Trash' text for comments
+ * Outputs 'undo move to Trash' text for comments.
  *
  * @since 2.9.0
  */
@@ -774,7 +774,7 @@ function meta_form( $post = null ) {
 }
 
 /**
- * Print out HTML form date elements for editing post or comment publish date.
+ * Prints out HTML form date elements for editing post or comment publish date.
  *
  * @since 0.71
  * @since 4.4.0 Converted to use get_comment() instead of the global `$comment`.
@@ -869,7 +869,7 @@ function touch_time( $edit = 1, $for_post = 1, $tab_index = 0, $multi = 0 ) {
 }
 
 /**
- * Print out option HTML elements for the page templates drop-down.
+ * Prints out option HTML elements for the page templates drop-down.
  *
  * @since 1.5.0
  * @since 4.7.0 Added the `$post_type` parameter.
@@ -889,7 +889,7 @@ function page_template_dropdown( $default = '', $post_type = 'page' ) {
 }
 
 /**
- * Print out option HTML elements for the page parents drop-down.
+ * Prints out option HTML elements for the page parents drop-down.
  *
  * @since 1.5.0
  * @since 4.4.0 `$post` argument was added.
@@ -927,7 +927,7 @@ function parent_dropdown( $default = 0, $parent = 0, $level = 0, $post = null ) 
 }
 
 /**
- * Print out option HTML elements for role selectors.
+ * Prints out option HTML elements for role selectors.
  *
  * @since 2.1.0
  *
@@ -1123,7 +1123,7 @@ function add_meta_box( $id, $title, $callback, $screen = null, $context = 'advan
 
 
 /**
- * Function that renders a "fake" meta box with an information message,
+ * Renders a "fake" meta box with an information message,
  * shown on the block editor, when an incompatible meta box is found.
  *
  * @since 5.0.0
@@ -1540,7 +1540,7 @@ function do_accordion_sections( $screen, $context, $object ) {
 }
 
 /**
- * Add a new section to a settings page.
+ * Adds a new section to a settings page.
  *
  * Part of the Settings API. Use this to define new settings sections for an admin page.
  * Show settings sections in your admin page callback function with do_settings_sections().
@@ -1598,7 +1598,7 @@ function add_settings_section( $id, $title, $callback, $page ) {
 }
 
 /**
- * Add a new field to a section of a settings page.
+ * Adds a new field to a section of a settings page.
  *
  * Part of the Settings API. Use this to define a settings field that will show
  * as part of a settings section inside a settings page. The fields are shown using
@@ -1708,7 +1708,7 @@ function do_settings_sections( $page ) {
 }
 
 /**
- * Print out the settings fields for a particular settings section.
+ * Prints out the settings fields for a particular settings section.
  *
  * Part of the Settings API. Use this in a settings page to output
  * a specific section. Should normally be called by do_settings_sections()
@@ -1751,7 +1751,7 @@ function do_settings_fields( $page, $section ) {
 }
 
 /**
- * Register a settings error to be displayed to the user.
+ * Registers a settings error to be displayed to the user.
  *
  * Part of the Settings API. Use this to show messages to users about settings validation
  * problems, missing settings or anything else.
@@ -1787,7 +1787,7 @@ function add_settings_error( $setting, $code, $message, $type = 'error' ) {
 }
 
 /**
- * Fetch settings errors registered by add_settings_error().
+ * Fetches settings errors registered by add_settings_error().
  *
  * Checks the $wp_settings_errors array for any errors declared during the current
  * pageload and returns them.
@@ -1858,7 +1858,7 @@ function get_settings_errors( $setting = '', $sanitize = false ) {
 }
 
 /**
- * Display settings errors registered by add_settings_error().
+ * Displays settings errors registered by add_settings_error().
  *
  * Part of the Settings API. Outputs a div for each error retrieved by
  * get_settings_errors().
@@ -1978,7 +1978,7 @@ function the_post_password() {
 }
 
 /**
- * Get the post title.
+ * Gets the post title.
  *
  * The post title is fetched and if it is blank then a default string is
  * returned.
@@ -2009,7 +2009,7 @@ function _admin_search_query() {
 }
 
 /**
- * Generic Iframe header for use with Thickbox
+ * Generic Iframe header for use with Thickbox.
  *
  * @since 2.7.0
  *
@@ -2097,7 +2097,7 @@ document.body.className = c;
 }
 
 /**
- * Generic Iframe footer for use with Thickbox
+ * Generic Iframe footer for use with Thickbox.
  *
  * @since 2.7.0
  */
@@ -2132,7 +2132,7 @@ function iframe_footer() {
 }
 
 /**
- * Function to echo or return the post states as HTML.
+ * Echoes or returns the post states as HTML.
  *
  * @since 2.7.0
  * @since 5.3.0 Added the `$echo` parameter and a return value.
@@ -2365,7 +2365,7 @@ function get_media_states( $post ) {
 }
 
 /**
- * Test support for compressing JavaScript from PHP
+ * Tests support for compressing JavaScript from PHP.
  *
  * Outputs JavaScript that tests if compression from PHP works as expected
  * and sets an option with the result. Has no effect when the current user
@@ -2455,7 +2455,7 @@ function submit_button( $text = null, $type = 'primary', $name = 'submit', $wrap
 }
 
 /**
- * Returns a submit button, with provided text and appropriate class
+ * Returns a submit button, with provided text and appropriate class.
  *
  * @since 3.1.0
  *
@@ -2558,7 +2558,7 @@ function _wp_admin_html_begin() {
 }
 
 /**
- * Convert a screen string to a screen object
+ * Converts a screen string to a screen object.
  *
  * @since 3.0.0
  *
@@ -2608,7 +2608,7 @@ function _local_storage_notice() {
 }
 
 /**
- * Output a HTML element with a star rating for a given rating.
+ * Outputs a HTML element with a star rating for a given rating.
  *
  * Outputs a HTML element with the star rating exposed on a 0..5 scale in
  * half star increments (ie. 1, 1.5, 2 stars). Optionally, if specified, the
