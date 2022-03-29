1de43a471cc2b367acd490f189c49c4046d2bd22
Helen Hou-Sandí <helen.y.hou@gmail.com>
2022-03-29 03:22:09 +0000

    Build/Test Tools: First pass at a .git-blame-ignore-revs file.
    This suppresses certain commits from Git blame views, notably in the GitHub UI as well as the command line. These commits are purely stylistic and excluding them from these views helps to reduce the noise when doing code archaeology.
    
    Still to come: More coding standards commits as they are reviewed.
    
    Props helen, costdev, peterwilsoncc.
    See #55422
    
    Built from https://develop.svn.wordpress.org/trunk@53007
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52596 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-53004';
+$wp_version = '6.0-alpha-53007';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
