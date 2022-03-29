22061e9c673698d723166b6024d6b59d86a9f581
audrasjb <audrasjb@git.wordpress.org>
2022-03-29 13:09:06 +0000

    Editor: Use `wp_unique_id()` instead of `uniqid()` to generate CSS class names.
    Backports changes from https://github.com/WordPress/gutenberg/pull/38891.
    See https://github.com/WordPress/gutenberg/issues/38889.
    
    Props westonruter, mamaduka.
    Merges [53012] to the 5.9 branch.
    See #55474.
    
    Built from https://develop.svn.wordpress.org/branches/5.9@53013
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52602 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/block-supports/layout.php
+++ wp-includes/block-supports/layout.php
@@ -159,18 +159,18 @@ function wp_render_layout_support_flag( $block_content, $block ) {
 		$used_layout = $default_layout;
 	}
 
-	$id        = uniqid();
-	$gap_value = _wp_array_get( $block, array( 'attrs', 'style', 'spacing', 'blockGap' ) );
+	$class_name = wp_unique_id( 'wp-container-' );
+	$gap_value  = _wp_array_get( $block, array( 'attrs', 'style', 'spacing', 'blockGap' ) );
 	// Skip if gap value contains unsupported characters.
 	// Regex for CSS value borrowed from `safecss_filter_attr`, and used here
 	// because we only want to match against the value, not the CSS attribute.
 	$gap_value = preg_match( '%[\\\(&=}]|/\*%', $gap_value ) ? null : $gap_value;
-	$style     = wp_get_layout_style( ".wp-container-$id", $used_layout, $has_block_gap_support, $gap_value );
+	$style     = wp_get_layout_style( ".$class_name", $used_layout, $has_block_gap_support, $gap_value );
 	// This assumes the hook only applies to blocks with a single wrapper.
 	// I think this is a reasonable limitation for that particular hook.
 	$content = preg_replace(
 		'/' . preg_quote( 'class="', '/' ) . '/',
-		'class="wp-container-' . $id . ' ',
+		'class="' . esc_attr( $class_name ) . ' ',
 		$block_content,
 		1
 	);
