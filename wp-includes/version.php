523c679e763b7590849e1d207a5be8ee58462161
desrosj <desrosj@git.wordpress.org>
2022-04-11 19:10:13 +0000

    Build/Test Tools: Update generated CSS files after [53141].
    This removes 2 instances of `-moz-tab-size`, which has not been required for Firefox to support the `tab-size` property since version 90 (current version is 99.
    
    See #55559.
    Built from https://develop.svn.wordpress.org/trunk@53143
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52732 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-53142';
+$wp_version = '6.0-alpha-53143';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
