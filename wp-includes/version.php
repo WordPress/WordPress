4dd4aec2d1b8c17027d38ddca7c6cfd3a961c561
joedolson <joedolson@git.wordpress.org>
2022-04-12 16:01:08 +0000

    Administration: Add unit test for term supplementary notice.
    Adds a new test assertion to handle the supplemental data array passed in `ajax-response.js`. 
    
    Follow up to [53123].
    
    Props SergeyBiryukov, peterwilsoncc.
    Fixes #55078.
    Built from https://develop.svn.wordpress.org/trunk@53159
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52748 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-53158';
+$wp_version = '6.0-alpha-53159';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
