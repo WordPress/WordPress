fd9d7a75473e92b8941fe8ecccc54f58c4919db2
joedolson <joedolson@git.wordpress.org>
2022-04-12 16:50:08 +0000

    Media: Fix editing controls covering image edit region.
    Move the editing control sidebar for mobile image editing below the image edit region so image is not obscured.
    
    Props mor10, HristoK, certainstrings, joedolson.
    Fixes #30154.
    Built from https://develop.svn.wordpress.org/trunk@53162
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52751 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-alpha-53161';
+$wp_version = '6.0-alpha-53162';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
