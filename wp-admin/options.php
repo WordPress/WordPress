57039311720709d55e96e1a074414ebadba64e00
Peter Wilson <wilson@peterwilson.cc>
2022-04-12 06:07:08 +0000

    Options: Show error when updating all options exceeds `max_input_vars`.
    On sites with a large number of options, the number of inputs when saving all options via the /wp-admin/options.php page may exceed PHP's `max_input_vars` setting, this will prevent saving.
    
    An error is now shown if this is the case.
    
    Props ishitaka, jorbin, maguijo, pento, sergeybiryukov. 
    Fixes #45438.
    
    Built from https://develop.svn.wordpress.org/trunk@53151
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52740 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options.php
+++ wp-admin/options.php
@@ -255,7 +255,7 @@ if ( 'update' === $action ) { // We are saving settings sent from a settings pag
 		if ( is_multisite() && ! current_user_can( 'manage_network_options' ) ) {
 			wp_die( __( 'Sorry, you are not allowed to modify unregistered settings for this site.' ) );
 		}
-		$options = explode( ',', wp_unslash( $_POST['page_options'] ) );
+		$options = isset( $_POST['page_options'] ) ? explode( ',', wp_unslash( $_POST['page_options'] ) ) : null;
 	} else {
 		$options = $allowed_options[ $option_page ];
 	}
@@ -332,6 +332,8 @@ if ( 'update' === $action ) { // We are saving settings sent from a settings pag
 		if ( $user_language_old !== $user_language_new ) {
 			load_default_textdomain( $user_language_new );
 		}
+	} else {
+		add_settings_error( 'general', 'settings_updated', __( 'Settings save failed.' ), 'error' );
 	}
 
 	/*
