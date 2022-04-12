bb723f99206bd1256a10134cd9a74984c33103b4
audrasjb <audrasjb@git.wordpress.org>
2022-04-12 15:40:11 +0000

    Widgets: Avoid 27 duplicate translations in Media Widgets constructor.
    This changeset rationalizes the declaration of translation strings in the Media Widgets constructor.
    
    Props Chouby, audrasjb, peterwilsoncc, costdev.
    Fixes #54561.
    
    Built from https://develop.svn.wordpress.org/trunk@53158
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52747 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-filters.php
+++ wp-includes/default-filters.php
@@ -630,6 +630,7 @@ add_filter( 'nav_menu_item_id', '_nav_menu_item_id_use_once', 10, 2 );
 // Widgets.
 add_action( 'after_setup_theme', 'wp_setup_widgets_block_editor', 1 );
 add_action( 'init', 'wp_widgets_init', 1 );
+add_action( 'change_locale', array( 'WP_Widget_Media', 'reset_default_labels' ) );
 
 // Admin Bar.
 // Don't remove. Wrong way to disable.
