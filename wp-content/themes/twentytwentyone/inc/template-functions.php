589215399205cf1efe963172992fc3134a2cc630
davidbaumwald <davidbaumwald@git.wordpress.org>
2022-04-19 15:23:11 +0000

    Twenty Twenty-One: Correct translator comment in `twenty_twenty_one_continue_reading_text`.
    This commit updates the translator comment in `twenty_twenty_one_continue_reading_text` to indicate that the text is visually hidden.  This change also corrects the comment above the `the_content_more_link` filter to reference the content, not the excerpt.
    
    Props sabernhardt, costdev.
    Fixes #55564.
    Built from https://develop.svn.wordpress.org/trunk@53221
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52810 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwentyone/inc/template-functions.php
+++ wp-content/themes/twentytwentyone/inc/template-functions.php
@@ -134,7 +134,7 @@ function twenty_twenty_one_get_avatar_size() {
  */
 function twenty_twenty_one_continue_reading_text() {
 	$continue_reading = sprintf(
-		/* translators: %s: Name of current post. */
+		/* translators: %s: Post title. Only visible to screen readers. */
 		esc_html__( 'Continue reading %s', 'twentytwentyone' ),
 		the_title( '<span class="screen-reader-text">', '</span>', false )
 	);
@@ -167,7 +167,7 @@ function twenty_twenty_one_continue_reading_link() {
 	}
 }
 
-// Filter the excerpt more link.
+// Filter the content more link.
 add_filter( 'the_content_more_link', 'twenty_twenty_one_continue_reading_link' );
 
 if ( ! function_exists( 'twenty_twenty_one_post_title' ) ) {
