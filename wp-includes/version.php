19d31e1b420198f2d3c29de8d7b3054e58a3a796
gziolo <gziolo@git.wordpress.org>
2022-04-25 16:57:10 +0000

    Tests: Update Gallery block unit tests to new gallery format
    The Gallery block unit tests still use the old format, so this updates it to the new format that uses nested Image blocks.
    
    Props glendaviesnz.
    Fixes #55571.
    
    
    Built from https://develop.svn.wordpress.org/trunk@53261
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52850 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-beta2-53260';
+$wp_version = '6.0-beta2-53261';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
