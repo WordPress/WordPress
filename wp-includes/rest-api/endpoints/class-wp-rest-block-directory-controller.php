98846baff8aa01fec9ffaae5ec02c2e21bf64215
John Blackbourn <johnbillion@git.wordpress.org>
2022-04-29 16:55:13 +0000

    REST API: Correct the schema for the `wp/v2/block-directory/search` endpoint.
    Props zieladam, TimothyBlynJacobs, spacedmonkey, johnbillion
    
    Fixes #53621
    
    Built from https://develop.svn.wordpress.org/trunk@53315
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52904 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/endpoints/class-wp-rest-block-directory-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-block-directory-controller.php
@@ -239,7 +239,7 @@ class WP_REST_Block_Directory_Controller extends WP_REST_Controller {
 				),
 				'rating'              => array(
 					'description' => __( 'The star rating of the block.' ),
-					'type'        => 'integer',
+					'type'        => 'number',
 					'context'     => array( 'view' ),
 				),
 				'rating_count'        => array(
@@ -249,12 +249,12 @@ class WP_REST_Block_Directory_Controller extends WP_REST_Controller {
 				),
 				'active_installs'     => array(
 					'description' => __( 'The number sites that have activated this block.' ),
-					'type'        => 'string',
+					'type'        => 'integer',
 					'context'     => array( 'view' ),
 				),
 				'author_block_rating' => array(
 					'description' => __( 'The average rating of blocks published by the same author.' ),
-					'type'        => 'integer',
+					'type'        => 'number',
 					'context'     => array( 'view' ),
 				),
 				'author_block_count'  => array(
