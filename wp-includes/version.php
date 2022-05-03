8cd8ef6c75f0c271c3a3d6937beb2933bc611bb5
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-05-03 21:43:04 +0000

    Security: Add 5.9 to the list of versions receiving security updates.
    Merges a part of [52448] to the 5.9 branch.
    Built from https://develop.svn.wordpress.org/branches/5.9@53348
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@52937 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9.4-alpha-53081';
+$wp_version = '5.9.4-alpha-53348';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
