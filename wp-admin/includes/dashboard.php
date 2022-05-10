e8c02840a7a211f5c3906fce0a80c59107f52464
ryelle <ryelle@git.wordpress.org>
2022-05-10 16:45:08 +0000

    Administration: Update design of the Dashboard welcome panel.
    This updates the panel to match the 6.0 About page styles, with the 6.0 graphic changing color based on the selected admin color scheme.
    
    Props fcoveram, critterverse, joedolson, SergeyBiryukov.
    Fixes #55532.
    
    
    Built from https://develop.svn.wordpress.org/trunk@53383
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52972 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/dashboard.php
+++ wp-admin/includes/dashboard.php
@@ -1996,6 +1996,9 @@ function wp_welcome_panel() {
 	?>
 	<div class="welcome-panel-content">
 	<div class="welcome-panel-header">
+		<div class="welcome-panel-header-image">
+			<?php echo file_get_contents( dirname( __DIR__ ) . '/images/about-header-about.svg' ); ?>
+		</div>
 		<h2><?php _e( 'Welcome to WordPress!' ); ?></h2>
 		<p>
 			<a href="<?php echo esc_url( admin_url( 'about.php' ) ); ?>">
