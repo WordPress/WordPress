d9b3f05bc0a08929c3244c691a091cd7a50a5723
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-05-10 17:30:09 +0000

    WordPress 6.0 RC2 Built from https://develop.svn.wordpress.org/branches/6.0@53386
    git-svn-id: http://core.svn.wordpress.org/branches/6.0@52975 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0-RC1-53385';
+$wp_version = '6.0-RC2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
