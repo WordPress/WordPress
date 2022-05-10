88a16c0ad70fa15d6ddc90a77452fd412fa4b18c
hellofromTonya <hellofromTonya@git.wordpress.org>
2022-05-10 21:30:11 +0000

    Editor: Use stricter assertions in comment template tests.
    Backport of the remaining Comment Template block tests from Gutenberg:
    
    * `assertEquals()` replaced with `assertSameSetsWithIndex()`.
    * assertion's argument order changed to ensure expected and then actual.
    
    Follow-up to [53353], [53298], [53172], [53138].
    
    Props bernhard-reiter.
    Fixes #55708.
    Built from https://develop.svn.wordpress.org/trunk@53388
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52977 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53383';
+$wp_version = '6.1-alpha-53388';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
