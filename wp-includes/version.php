aeb3b7530af57db885dfa8f377a7c6aa22e309bd
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-05-16 14:11:12 +0000

    Tests: Use a data provider in the test for conditional tags returning early if `$wp_query` is not set.
    Follow-up to [53395], [53396].
    
    Props peterwilsoncc.
    See #55104.
    Built from https://develop.svn.wordpress.org/trunk@53400
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@52989 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53399';
+$wp_version = '6.1-alpha-53400';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
