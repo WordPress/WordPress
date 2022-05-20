766a38f2fb420aafb9049ffe43e4a04f35a0c7c8
desrosj <desrosj@git.wordpress.org>
2022-05-20 17:42:09 +0000

    Build/Test Tools: Link to a specific run attempt in GitHub Action Slack notifications.
    This adjusts the workflow run URLs included in Slack notifications to link to the specific attempt being reported on making it easier for someone to see the proper context of a specific notification.
    
    Additionally, this eliminates the need for a contributor to share the reason for a failure in Slack before restarting the workflow, as the link will always be to that specificattempt. When a “fixed” notification occurs for a subsequent run attempt, the link will be to the new, successful run attempt.
    
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@53427
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53016 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53426';
+$wp_version = '6.1-alpha-53427';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
