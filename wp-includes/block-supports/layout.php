59c970101f55a1d3610efcb8cf9612bc90924a5b
gziolo <gziolo@git.wordpress.org>
2022-05-20 15:35:14 +0000

    Docs: Add missing documentation for fallback_gap_value param
    Follow-up for [53420]. 
    
    Props SergeyBiryukov.
    See #55567.
    
    
    Built from https://develop.svn.wordpress.org/trunk@53421
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53010 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/block-supports/layout.php
+++ wp-includes/block-supports/layout.php
@@ -41,6 +41,7 @@ function wp_register_layout_support( $block_type ) {
  * @param boolean $has_block_gap_support         Whether the theme has support for the block gap.
  * @param string  $gap_value                     The block gap value to apply.
  * @param boolean $should_skip_gap_serialization Whether to skip applying the user-defined value set in the editor.
+ * @param string  $fallback_gap_value            The custom fallback value for block gap.
  * @return string CSS style.
  */
 function wp_get_layout_style( $selector, $layout, $has_block_gap_support = false, $gap_value = null, $should_skip_gap_serialization = false, $fallback_gap_value = '0.5em' ) {
