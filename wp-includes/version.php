653a3af0d4571b9a943aa687cc5848e117e88431
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-05-23 17:02:16 +0000

    Upgrade/Install: Update `$_old_files` for 6.0.
    Props hellofromTonya, JeffPaul, SergeyBiryukov.
    Fixes #55794.
    Built from https://develop.svn.wordpress.org/trunk@53439
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53028 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53437';
+$wp_version = '6.1-alpha-53439';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
