6c79e4b29bee6ea3d2b90bea64b3d1f29aaa71f2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-06-05 14:06:11 +0000

    Tests: Update the URL to the documentation on GitHub Actions environment variables.
    Follow-up to [49264].
    
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@53467
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53056 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53466';
+$wp_version = '6.1-alpha-53467';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
