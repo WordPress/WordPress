4b286c4f77e7ada90954aae363618c003c2f252a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-06-07 14:45:12 +0000

    Tests: Move the tests for individual pluggable functions into their own directory.
    This aims to make the tests more discoverable and easier to expand.
    
    Follow-up to [50790], [53473], [53477].
    
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@53478
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53067 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53477';
+$wp_version = '6.1-alpha-53478';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
