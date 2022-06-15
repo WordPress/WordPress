cbca7c4fb966b79d159589ec413f0aa6c795e6c3
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-06-15 15:39:08 +0000

    Tests: Temporarily disable the failing REST API test for `update_post_parent_caches()`.
    This needs more investigation to address the test failure with persistent object cache.
    
    Follow-up to [53506].
    
    Props hellofromTonya.
    See #55593.
    Built from https://develop.svn.wordpress.org/trunk@53511
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53100 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53510';
+$wp_version = '6.1-alpha-53511';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
