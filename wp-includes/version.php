13b24017be13cf7a3cbe0b7ac01753e48db8a6e8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-06-19 16:53:08 +0000

    Tests: Refactor `Tests_Image_Functions::test_mime_overrides_filename()` to use a data provider.
    Using a data provider has a number of advantages:
    1. If the first test case fails, it won't prevent the other test cases from being tested.
    2. The output from PHPUnit will be more descriptive in case of failure when using a data provider.
    3. Using named test cases in the data provider will also make the `--testdox` output much more descriptive and informative.
    
    The actual cases being tested, or the test itself have not been changed.
    
    Includes:
    * Adding a `@covers` annotation.
    * Adding a failure message to each assertion.
    * Making the test method name more specific.
    
    Follow-up to [1061/tests], [53495], [53497], [53521], [53523], [53524], [53525], [53526], [53529].
    
    Props jrf.
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@53530
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53119 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53529';
+$wp_version = '6.1-alpha-53530';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
