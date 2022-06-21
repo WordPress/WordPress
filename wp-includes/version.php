ea5c6469c9967bf501ff093a5b7ff6b5a06951d1
audrasjb <audrasjb@git.wordpress.org>
2022-06-21 13:18:10 +0000

    Sitemaps: Remove duplicate sticky Posts from Sitemap Post Query.
    This changeset sets the `ignore_sticky_posts` parameter to `true` in the default arguments passed to `wp_sitemaps_posts_query_args`.
    
    Props RavanH, pbiron, swissspidy, audrasjb, SergeyBiryukov.
    Fixes #55633.
    
    Built from https://develop.svn.wordpress.org/trunk@53548
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53137 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53547';
+$wp_version = '6.1-alpha-53548';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
