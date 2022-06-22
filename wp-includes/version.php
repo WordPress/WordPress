5d2e8afebd5ed633ca66004dbb15e6c8427d4e8a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-06-22 14:55:11 +0000

    Code Modernization: Use the integer portion of an item position in `add_submenu_page()`.
    This fixes an `Implicit conversion from float to int loses precision` PHP 8.1 deprecation notice when adding a new admin menu item with a `float` value passed as the `$position` parameter.
    
    This change is covered by existing unit tests and addresses 8 errors when running the test suite on PHP 8.1.
    
    References:
    * [https://php.watch/versions/8.1/deprecate-implicit-conversion-incompatible-float-string PHP 8.1: Implicit incompatible float to int conversion is deprecated]
    * [https://wiki.php.net/rfc/implicit-float-int-deprecate PHP RFC: Deprecate implicit non-integer-compatible float to int conversions]
    
    Follow-up to [52569], [53104].
    
    Props jrf.
    See #55656, #54798.
    Built from https://develop.svn.wordpress.org/trunk@53555
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53144 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53554';
+$wp_version = '6.1-alpha-53555';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
