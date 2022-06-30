6437203f0997da70714a6c4ff9cdcd96cc6ddf8f
desrosj <desrosj@git.wordpress.org>
2022-06-30 16:28:05 +0000

    Build/Test Tools: Update 3rd party GitHub Actions.
    This updates the following GitHub Actions to the latest versions:
    
    - `actions/checkout`
    - `actions/cache`
    - `actions/github-script`
    - `actions/setup-node`
    - `codecov/codecov-action`
    - `ramsey/composer-install`
    - `shivammathur/setup-php`
    - `slackapi/slack-github-action`
    
    Various inline documentation updates are also included.
    
    Merges [53112], [53581], [53582], and [53592] to the 5.9 branch.
    See #55652.
    Built from https://develop.svn.wordpress.org/branches/5.9@53596
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@53184 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.9.4-alpha-53351';
+$wp_version = '5.9.4-alpha-53596';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
