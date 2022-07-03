5cfb35a5f2b269d7fb6e505b09aba99efbb2deb9
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-07-03 17:30:09 +0000

    Docs: Add `@since` tags for `_doing_it_wrong()` and deprecation notice handlers in the PHPUnit test suite.
    This affects methods in the `WP_UnitTestCase_Base` class:
    * `::expectDeprecated()`
    * `::expectedDeprecated()`
    * `::setExpectedException()`
    * `::deprecated_function_run()`
    * `::doing_it_wrong_run()`
    
    Follow-up to [25402], [25408], [25785], [37861], [40536], [40539], [40872], [51872], [53637].
    
    See #55652, #55646.
    Built from https://develop.svn.wordpress.org/trunk@53638
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53197 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53637';
+$wp_version = '6.1-alpha-53638';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
