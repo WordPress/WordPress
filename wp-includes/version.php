dcc970fc76814306d0deb78eb3c2a02cc41e56b1
Peter Wilson <wilson@peterwilson.cc>
2022-07-05 00:09:13 +0000

    Taxonomy: Fix caching issues in `WP_Term_Query` class.
    Introduced [52836] when passing `child_of` or `pad_counts` parameters to `get_terms` or `WP_Term_Query` class, the array 
    of terms received by the query, was not correctly cached. This change simplifies the logic in `WP_Term_Query` and 
    ensures terms are correctly cached. This change also, improves performance, by only caching an array of term ids where 
    possible.  
    
    Additionally, the database version bump included in this patch is also required for #55890 to initialize the user count 
    on single sites.
    
    Props denishua, spacedmonkey, oztaser, peterwilsoncc, SergeyBiryukov, georgestephanis, jnz31, knutsp, mukesh27, costdev, 
    tharsheblows.
    Merges [53496] to the 6.0 branch.
    Fixes #55837, #55890.
    
     --This line, and those below, will be ignored--
    
    _M   .
    M    src/wp-includes/class-wp-term-query.php
    M    src/wp-includes/version.php
    M    tests/phpunit/tests/term/getTerms.php
    
    Built from https://develop.svn.wordpress.org/branches/6.0@53650
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.0@53209 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,14 +16,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0.1-alpha-53649';
+$wp_version = '6.0.1-alpha-53650';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 51917;
+$wp_db_version = 53496;
 
 /**
  * Holds the TinyMCE version.
