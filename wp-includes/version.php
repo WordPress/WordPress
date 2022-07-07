590b9278ffabd583df94b8ac37c2fae44e1acdfe
Andrew Ozz <admin@laptoptips.ca>
2022-07-07 21:08:13 +0000

    Build/Test Tools: Add and fix @covers tags to the attachments and block-supports test groups.
    Props pbeane, hellofromTonya, ironprogrammer, antonvlasenko, SergeyBiryukov, costdev.
    See #39265.
    Built from https://develop.svn.wordpress.org/trunk@53680
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53239 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53679';
+$wp_version = '6.1-alpha-53680';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
