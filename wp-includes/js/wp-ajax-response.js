a65abecc15dc8f7d5896da0c81b3e709d776b16d
joedolson <joedolson@git.wordpress.org>
2022-07-16 01:08:10 +0000

    Administration: Audible messages on AJAX error cases.
    Issues appropriate audible messages via `wp.a11y.speak()` when `ajax-response.js` generates an error via `wp_die()`.
    
    Props afercia, alexstine.
    Fixes #54483.
    Built from https://develop.svn.wordpress.org/trunk@53709
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53268 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/wp-ajax-response.js
+++ wp-includes/js/wp-ajax-response.js
@@ -53,19 +53,27 @@ window.wpAjax = jQuery.extend( {
 				parsed.responses.push( response );
 			} );
 			if ( err.length ) {
-				re.html( '<div class="error">' + err + '</div>' );
+				re.html( '<div class="notice notice-error">' + err + '</div>' );
 				wp.a11y.speak( err );
 			} else if ( noticeMessage.length ) {
-				re.html( '<div class="updated notice is-dismissible"><p>' + noticeMessage + '</p></div>');
+				re.html( '<div class="notice notice-success is-dismissible"><p>' + noticeMessage + '</p></div>');
 				jQuery(document).trigger( 'wp-updates-notice-added' );
 				wp.a11y.speak( noticeMessage );
 			}
 			return parsed;
 		}
-		if ( isNaN(x) ) { return !re.html('<div class="error"><p>' + x + '</p></div>'); }
-		x = parseInt(x,10);
-		if ( -1 === x ) { return !re.html('<div class="error"><p>' + wpAjax.noPerm + '</p></div>'); }
-		else if ( 0 === x ) { return !re.html('<div class="error"><p>' + wpAjax.broken  + '</p></div>'); }
+		if ( isNaN( x ) ) {
+			wp.a11y.speak( x );
+			return ! re.html( '<div class="notice notice-error"><p>' + x + '</p></div>' );
+		}
+		x = parseInt( x, 10 );
+		if ( -1 === x ) {
+			wp.a11y.speak( wpAjax.noPerm );
+			return ! re.html( '<div class="notice notice-error"><p>' + wpAjax.noPerm + '</p></div>' );
+		} else if ( 0 === x ) {
+			wp.a11y.speak( wpAjax.broken );
+			return ! re.html( '<div class="notice notice-error"><p>' + wpAjax.broken  + '</p></div>' );
+		}
 		return true;
 	},
 	invalidateForm: function ( selector ) {
