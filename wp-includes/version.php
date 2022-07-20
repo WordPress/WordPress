cfe7d795f5f90042736735be9c99c85b994db83e
desrosj <desrosj@git.wordpress.org>
2022-07-20 17:43:13 +0000

    Build/Test Tools: Correctly detect the first workflow run for a branch or tag.
    When trying to determine the outcome of the previous run for a GitHub Action workflow, the current run is included in the list fetched from the GitHub API.
    
    This adjusts the logic checking for the previous run to account for that and fixes notifications for the first workflow runs of a new branch or tag.
    
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@53735
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53294 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53734';
+$wp_version = '6.1-alpha-53735';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
