63d113e62cdbaf3e8c4b555fda4f7367e3e22096
audrasjb <audrasjb@git.wordpress.org>
2022-07-20 20:37:07 +0000

    Docs: Use third-person singular verbs for function descriptions in `general-template.php`, as per docblocks standards.
    See #55646.
    
    Built from https://develop.svn.wordpress.org/trunk@53740
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53299 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -7,7 +7,7 @@
  */
 
 /**
- * Load header template.
+ * Loads header template.
  *
  * Includes the header template for a theme or if a name is specified then a
  * specialised header will be included.
@@ -51,7 +51,7 @@ function get_header( $name = null, $args = array() ) {
 }
 
 /**
- * Load footer template.
+ * Loads footer template.
  *
  * Includes the footer template for a theme or if a name is specified then a
  * specialised footer will be included.
@@ -95,7 +95,7 @@ function get_footer( $name = null, $args = array() ) {
 }
 
 /**
- * Load sidebar template.
+ * Loads sidebar template.
  *
  * Includes the sidebar template for a theme or if a name is specified then a
  * specialised sidebar will be included.
@@ -207,7 +207,7 @@ function get_template_part( $slug, $name = null, $args = array() ) {
 }
 
 /**
- * Display search form.
+ * Displays search form.
  *
  * Will first attempt to locate the searchform.php file in either the child or
  * the parent, then load it. If it doesn't exist, then the default search form
@@ -362,7 +362,7 @@ function get_search_form( $args = array() ) {
 }
 
 /**
- * Display the Log In/Out link.
+ * Displays the Log In/Out link.
  *
  * Displays a link, which allows users to navigate to the Log In page to log in
  * or log out depending on whether they are currently logged in.
@@ -629,7 +629,7 @@ function wp_login_form( $args = array() ) {
 }
 
 /**
- * Returns the URL that allows the user to retrieve the lost password
+ * Returns the URL that allows the user to retrieve the lost password.
  *
  * @since 2.8.0
  *
@@ -666,7 +666,7 @@ function wp_lostpassword_url( $redirect = '' ) {
 }
 
 /**
- * Display the Registration or Admin link.
+ * Displays the Registration or Admin link.
  *
  * Display a link which allows the user to navigate to the registration page if
  * not logged in and registration is enabled or to the dashboard if logged in.
@@ -991,7 +991,7 @@ function site_icon_url( $size = 512, $url = '', $blog_id = 0 ) {
 }
 
 /**
- * Whether the site has a Site Icon.
+ * Determines whether the site has a Site Icon.
  *
  * @since 4.3.0
  *
@@ -1293,7 +1293,7 @@ function _wp_render_title_tag() {
 }
 
 /**
- * Display or retrieve page title for all areas of blog.
+ * Displays or retrieves page title for all areas of blog.
  *
  * By default, the page title will display the separator before the page title,
  * so that the blog title will be before the page title. This is not good for
@@ -1447,7 +1447,7 @@ function wp_title( $sep = '&raquo;', $display = true, $seplocation = '' ) {
 }
 
 /**
- * Display or retrieve page title for post.
+ * Displays or retrieves page title for post.
  *
  * This is optimized for single.php template file for displaying the post title.
  *
@@ -1486,7 +1486,7 @@ function single_post_title( $prefix = '', $display = true ) {
 }
 
 /**
- * Display or retrieve title for a post type archive.
+ * Displays or retrieves title for a post type archive.
  *
  * This is optimized for archive.php and archive-{$post_type}.php template files
  * for displaying the title of the post type.
@@ -1527,7 +1527,7 @@ function post_type_archive_title( $prefix = '', $display = true ) {
 }
 
 /**
- * Display or retrieve page title for category archive.
+ * Displays or retrieves page title for category archive.
  *
  * Useful for category template files for displaying the category page title.
  * The prefix does not automatically place a space between the prefix, so if
@@ -1544,7 +1544,7 @@ function single_cat_title( $prefix = '', $display = true ) {
 }
 
 /**
- * Display or retrieve page title for tag post archive.
+ * Displays or retrieves page title for tag post archive.
  *
  * Useful for tag template files for displaying the tag page title. The prefix
  * does not automatically place a space between the prefix, so if there should
@@ -1561,7 +1561,7 @@ function single_tag_title( $prefix = '', $display = true ) {
 }
 
 /**
- * Display or retrieve page title for taxonomy term archive.
+ * Displays or retrieves page title for taxonomy term archive.
  *
  * Useful for taxonomy term template files for displaying the taxonomy term page title.
  * The prefix does not automatically place a space between the prefix, so if there should
@@ -1623,7 +1623,7 @@ function single_term_title( $prefix = '', $display = true ) {
 }
 
 /**
- * Display or retrieve page title for post archive based on date.
+ * Displays or retrieves page title for post archive based on date.
  *
  * Useful for when the template only needs to display the month and year,
  * if either are available. The prefix does not automatically place a space
@@ -1666,7 +1666,7 @@ function single_month_title( $prefix = '', $display = true ) {
 }
 
 /**
- * Display the archive title based on the queried object.
+ * Displays the archive title based on the queried object.
  *
  * @since 4.1.0
  *
@@ -1684,7 +1684,7 @@ function the_archive_title( $before = '', $after = '' ) {
 }
 
 /**
- * Retrieve the archive title based on the queried object.
+ * Retrieves the archive title based on the queried object.
  *
  * @since 4.1.0
  * @since 5.5.0 The title part is wrapped in a `<span>` element.
@@ -1782,7 +1782,7 @@ function get_the_archive_title() {
 }
 
 /**
- * Display category, tag, term, or author description.
+ * Displays category, tag, term, or author description.
  *
  * @since 4.1.0
  *
@@ -1863,7 +1863,7 @@ function get_the_post_type_description() {
 }
 
 /**
- * Retrieve archive link content based on predefined or custom code.
+ * Retrieves archive link content based on predefined or custom code.
  *
  * The format can be one of four styles. The 'link' for head element, 'option'
  * for use in the select element, 'html' for use in list (either ol or ul HTML
@@ -1932,7 +1932,7 @@ function get_archives_link( $url, $text, $format = 'html', $before = '', $after 
 }
 
 /**
- * Display archive links based on type and format.
+ * Displays archive links based on type and format.
  *
  * @since 1.2.0
  * @since 4.4.0 The `$post_type` argument was added.
@@ -2192,7 +2192,7 @@ function wp_get_archives( $args = '' ) {
 }
 
 /**
- * Get number of days since the start of the week.
+ * Gets number of days since the start of the week.
  *
  * @since 1.5.0
  *
@@ -2205,7 +2205,7 @@ function calendar_week_mod( $num ) {
 }
 
 /**
- * Display calendar with days that have posts as links.
+ * Displays calendar with days that have posts as links.
  *
  * The calendar is cached, which will be retrieved, if it exists. If there are
  * no posts for the month, then it will not be displayed.
@@ -2446,7 +2446,7 @@ function get_calendar( $initial = true, $echo = true ) {
 }
 
 /**
- * Purge the cached results of get_calendar.
+ * Purges the cached results of get_calendar.
  *
  * @see get_calendar()
  * @since 2.1.0
@@ -2456,7 +2456,7 @@ function delete_get_calendar_cache() {
 }
 
 /**
- * Display all of the allowed tags in HTML format with attributes.
+ * Displays all of the allowed tags in HTML format with attributes.
  *
  * This is useful for displaying in the comment area, which elements and
  * attributes are supported. As well as any plugins which want to display it.
@@ -2495,7 +2495,7 @@ function the_date_xml() {
 }
 
 /**
- * Display or Retrieve the date the current post was written (once per date)
+ * Displays or retrieves the date the current post was written (once per date)
  *
  * Will only output the date if the current post's date is different from the
  * previous one output.
@@ -2547,7 +2547,7 @@ function the_date( $format = '', $before = '', $after = '', $echo = true ) {
 }
 
 /**
- * Retrieve the date on which the post was written.
+ * Retrieves the date on which the post was written.
  *
  * Unlike the_date() this function will always return the date.
  * Modify output with the {@see 'get_the_date'} filter.
@@ -2582,7 +2582,7 @@ function get_the_date( $format = '', $post = null ) {
 }
 
 /**
- * Display the date on which the post was last modified.
+ * Displays the date on which the post was last modified.
  *
  * @since 2.1.0
  *
@@ -2616,7 +2616,7 @@ function the_modified_date( $format = '', $before = '', $after = '', $echo = tru
 }
 
 /**
- * Retrieve the date on which the post was last modified.
+ * Retrieves the date on which the post was last modified.
  *
  * @since 2.1.0
  * @since 4.6.0 Added the `$post` parameter.
@@ -2651,7 +2651,7 @@ function get_the_modified_date( $format = '', $post = null ) {
 }
 
 /**
- * Display the time at which the post was written.
+ * Displays the time at which the post was written.
  *
  * @since 0.71
  *
@@ -2673,7 +2673,7 @@ function the_time( $format = '' ) {
 }
 
 /**
- * Retrieve the time at which the post was written.
+ * Retrieves the time at which the post was written.
  *
  * @since 1.5.0
  *
@@ -2709,7 +2709,7 @@ function get_the_time( $format = '', $post = null ) {
 }
 
 /**
- * Retrieve the time at which the post was written.
+ * Retrieves the time at which the post was written.
  *
  * @since 2.0.0
  *
@@ -2766,7 +2766,7 @@ function get_post_time( $format = 'U', $gmt = false, $post = null, $translate = 
 }
 
 /**
- * Retrieve post published or modified time as a `DateTimeImmutable` object instance.
+ * Retrieves post published or modified time as a `DateTimeImmutable` object instance.
  *
  * The object will be set to the timezone from WordPress settings.
  *
@@ -2815,7 +2815,7 @@ function get_post_datetime( $post = null, $field = 'date', $source = 'local' ) {
 }
 
 /**
- * Retrieve post published or modified time as a Unix timestamp.
+ * Retrieves post published or modified time as a Unix timestamp.
  *
  * Note that this function returns a true Unix timestamp, not summed with timezone offset
  * like older WP functions.
@@ -2838,7 +2838,7 @@ function get_post_timestamp( $post = null, $field = 'date' ) {
 }
 
 /**
- * Display the time at which the post was last modified.
+ * Displays the time at which the post was last modified.
  *
  * @since 2.0.0
  *
@@ -2860,7 +2860,7 @@ function the_modified_time( $format = '' ) {
 }
 
 /**
- * Retrieve the time at which the post was last modified.
+ * Retrieves the time at which the post was last modified.
  *
  * @since 2.0.0
  * @since 4.6.0 Added the `$post` parameter.
@@ -2898,7 +2898,7 @@ function get_the_modified_time( $format = '', $post = null ) {
 }
 
 /**
- * Retrieve the time at which the post was last modified.
+ * Retrieves the time at which the post was last modified.
  *
  * @since 2.0.0
  *
@@ -2955,7 +2955,7 @@ function get_post_modified_time( $format = 'U', $gmt = false, $post = null, $tra
 }
 
 /**
- * Display the weekday on which the post was written.
+ * Displays the weekday on which the post was written.
  *
  * @since 0.71
  *
@@ -2983,7 +2983,7 @@ function the_weekday() {
 }
 
 /**
- * Display the weekday on which the post was written.
+ * Displays the weekday on which the post was written.
  *
  * Will only output the weekday if the current post's weekday is different from
  * the previous one output.
@@ -3028,7 +3028,7 @@ function the_weekday_date( $before = '', $after = '' ) {
 }
 
 /**
- * Fire the wp_head action.
+ * Fires the wp_head action.
  *
  * See {@see 'wp_head'}.
  *
@@ -3044,7 +3044,7 @@ function wp_head() {
 }
 
 /**
- * Fire the wp_footer action.
+ * Fires the wp_footer action.
  *
  * See {@see 'wp_footer'}.
  *
@@ -3060,7 +3060,7 @@ function wp_footer() {
 }
 
 /**
- * Fire the wp_body_open action.
+ * Fires the wp_body_open action.
  *
  * See {@see 'wp_body_open'}.
  *
@@ -3076,7 +3076,7 @@ function wp_body_open() {
 }
 
 /**
- * Display the links to the general feeds.
+ * Displays the links to the general feeds.
  *
  * @since 2.8.0
  *
@@ -3122,7 +3122,7 @@ function feed_links( $args = array() ) {
 }
 
 /**
- * Display the links to the extra feeds such as category feeds.
+ * Displays the links to the extra feeds such as category feeds.
  *
  * @since 2.8.0
  *
@@ -3212,7 +3212,7 @@ function feed_links_extra( $args = array() ) {
 }
 
 /**
- * Display the link to the Really Simple Discovery service endpoint.
+ * Displays the link to the Really Simple Discovery service endpoint.
  *
  * @link http://archipelago.phrasewise.com/rsd
  * @since 2.0.0
@@ -3222,7 +3222,7 @@ function rsd_link() {
 }
 
 /**
- * Display the link to the Windows Live Writer manifest file.
+ * Displays the link to the Windows Live Writer manifest file.
  *
  * @link https://msdn.microsoft.com/en-us/library/bb463265.aspx
  * @since 2.3.1
@@ -3250,7 +3250,7 @@ function wp_strict_cross_origin_referrer() {
 }
 
 /**
- * Display site icon meta tags.
+ * Displays site icon meta tags.
  *
  * @since 4.3.0
  *
@@ -3459,7 +3459,7 @@ function wp_dependencies_unique_hosts() {
 }
 
 /**
- * Whether the user can access the visual editor.
+ * Determines whether the user can access the visual editor.
  *
  * Checks if the user can access the visual editor and that it's supported by the user's browser.
  *
@@ -3503,7 +3503,7 @@ function user_can_richedit() {
 }
 
 /**
- * Find out which editor should be displayed by default.
+ * Finds out which editor should be displayed by default.
  *
  * Works out which of the two editors to display as the current editor for a
  * user. The 'html' setting is for the "Text" editor tab.
@@ -3575,7 +3575,7 @@ function wp_enqueue_editor() {
 }
 
 /**
- * Enqueue assets needed by the code editor for the given settings.
+ * Enqueues assets needed by the code editor for the given settings.
  *
  * @since 4.9.0
  *
@@ -3668,7 +3668,7 @@ function wp_enqueue_code_editor( $args ) {
 }
 
 /**
- * Generate and return code editor settings.
+ * Generates and returns code editor settings.
  *
  * @since 5.0.0
  *
@@ -4687,7 +4687,7 @@ function wp_generator() {
 }
 
 /**
- * Display the generator XML or Comment for RSS, ATOM, etc.
+ * Displays the generator XML or Comment for RSS, ATOM, etc.
  *
  * Returns the correct generator type for the requested output format. Allows
  * for a plugin to filter generators overall the {@see 'the_generator'} filter.
@@ -4908,9 +4908,9 @@ function __checked_selected_helper( $helper, $current, $echo, $type ) { // phpcs
 }
 
 /**
- * Default settings for heartbeat
+ * Default settings for heartbeat.
  *
- * Outputs the nonce used in the heartbeat XHR
+ * Outputs the nonce used in the heartbeat XHR.
  *
  * @since 3.6.0
  *
