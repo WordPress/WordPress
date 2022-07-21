23e60ed7652cc429a7ccfb4ad636bb502fed0219
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-07-21 16:09:12 +0000

    Coding Standards: Replace the old `wp-db.php` filename in `phpcompat.xml.dist`.
    This ensures the correct file is allowlisted for use of `mysql_` extension in PHP < 7.0.
    
    Follow-up to [53749].
    
    See #56268, #55647.
    Built from https://develop.svn.wordpress.org/trunk@53750
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53309 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53749';
+$wp_version = '6.1-alpha-53750';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
