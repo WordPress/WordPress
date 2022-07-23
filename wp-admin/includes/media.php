85e2d1df55f1b0d7e9e026eec70d3a3673f0c5d1
audrasjb <audrasjb@git.wordpress.org>
2022-07-23 09:00:11 +0000

    Docs: Add function description and `@since` mention for `get_upload_iframe_src()`.
    Follow-up to [14015].
    
    See #55646.
    
    Built from https://develop.svn.wordpress.org/trunk@53766
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53325 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/media.php
+++ wp-admin/includes/media.php
@@ -674,11 +674,16 @@ function media_buttons( $editor_id = 'content' ) {
 }
 
 /**
+ * Retrieves the upload iframe source URL.
+ *
+ * @since 3.0.0
+ *
  * @global int $post_ID
- * @param string $type
- * @param int    $post_id
- * @param string $tab
- * @return string
+ *
+ * @param string $type    Media type.
+ * @param int    $post_id Post ID.
+ * @param string $tab     Media upload tab.
+ * @return string Upload iframe source URL.
  */
 function get_upload_iframe_src( $type = null, $post_id = null, $tab = null ) {
 	global $post_ID;
