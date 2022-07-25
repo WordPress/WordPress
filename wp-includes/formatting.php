b6fad3a24146fcf65230cc9722932427289244c0
audrasjb <audrasjb@git.wordpress.org>
2022-07-25 17:28:09 +0000

    Docs: Remove an obsolete function description in `addslashes_gpc()`.
    - Usage of `get_magic_quotes_gpc()` was removed from `addslashes_gpc()` in [46105] / #47783.
    - A similar description fragment was removed from `wp_parse_str()` in that changeset, but this one was missed.
    - The `get_magic_quotes_gpc()` function itself was deprecated in PHP 7.4 and removed in PHP 8.0.
    
    Props mitweka, audrasjb, SergeyBiryukov.
    Fixes #56233.
    See #55646.
    
    Built from https://develop.svn.wordpress.org/trunk@53776
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53335 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -2785,9 +2785,6 @@ function untrailingslashit( $string ) {
 /**
  * Adds slashes to a string or recursively adds slashes to strings within an array.
  *
- * Slashes will first be removed if magic_quotes_gpc is set, see {@link
- * https://www.php.net/magic_quotes} for more details.
- *
  * @since 0.71
  *
  * @param string|array $gpc String or array of data to slash.
