a39042b23729a1817ca13d1f6bd3be0aeded113b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-07-25 13:33:12 +0000

    Tests: Add failure messages for site icon and custom logo tests.
    This makes the assertions more helpful, as per the [https://make.wordpress.org/core/handbook/testing/automated-testing/writing-phpunit-tests/#using-assertions Writing PHP Tests] guidelines:
    > All PHPUnit assertions, as well as all WordPress custom assertions, allow for a `$message` parameter to be passed. This message will be displayed when the assertion fails and can help immensely when debugging a test. This parameter should always be used if more than one assertion is used in a test method.
    
    Follow-up to [33181], [36905].
    
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@53773
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53332 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53772';
+$wp_version = '6.1-alpha-53773';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
