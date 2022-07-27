ac02adfaec5afe5ffc03e6dd49119fd0a366b4e4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-07-27 16:29:07 +0000

    Tests: Use named data providers in some `wp_insert_post()` tests.
    Follow-up to [42380], [49125], [53521], [53785], [53787], [53788].
    
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@53789
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53348 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53788';
+$wp_version = '6.1-alpha-53789';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
