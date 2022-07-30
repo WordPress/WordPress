35ab93306779cf1b04eba1b614f382365bea0b43
audrasjb <audrasjb@git.wordpress.org>
2022-07-30 14:09:10 +0000

    Docs: Fix `$feature` parameter type in `block_has_support()`.
    In `block_has_support()` docblock, the `$feature` parameter is an array, not a string.
    
    Props Soean, audrasjb.
    Fixes #56307.
    See #55646.
    
    Built from https://develop.svn.wordpress.org/trunk@53799
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53358 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks.php
+++ wp-includes/blocks.php
@@ -1017,7 +1017,7 @@ function unregister_block_style( $block_name, $block_style_name ) {
  * @since 5.8.0
  *
  * @param WP_Block_Type $block_type Block type to check for support.
- * @param string        $feature    Name of the feature to check support for.
+ * @param array         $feature    Path to a specific feature to check support for.
  * @param mixed         $default    Optional. Fallback value for feature support. Default false.
  * @return bool Whether the feature is supported.
  */
