6219eb95bfbefb646b5c4d13fb8dc2b22d6835d3
Peter Wilson <wilson@peterwilson.cc>
2022-08-05 05:49:11 +0000

    Sitemaps: Prevent invalid provider names throwing errors.
    Validate the requested sitemap is a string before attempting to use it in a provider. This prevents `WP_Sitemaps_Registry::get_provider()` from triggering a fatal error in more recent versions of PHP.
    
    The errors can be triggered by items outside the site owner or developers control (such as a user visiting `?sitemap[foo]=bar`) so the code fails silently to avoid filling error logs with unfixable errors.
    
    Props costdev, dd32.
    Fixes #56336.
    
    Built from https://develop.svn.wordpress.org/trunk@53838
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53397 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/sitemaps/class-wp-sitemaps-registry.php
+++ wp-includes/sitemaps/class-wp-sitemaps-registry.php
@@ -65,7 +65,7 @@ class WP_Sitemaps_Registry {
 	 * @return WP_Sitemaps_Provider|null Sitemap provider if it exists, null otherwise.
 	 */
 	public function get_provider( $name ) {
-		if ( ! isset( $this->providers[ $name ] ) ) {
+		if ( ! is_string( $name ) || ! isset( $this->providers[ $name ] ) ) {
 			return null;
 		}
 
