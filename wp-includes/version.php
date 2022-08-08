46dbe91667278572334109b582ffa223e514c2b7
Andrew Ozz <admin@laptoptips.ca>
2022-08-08 23:35:09 +0000

    Build/Test Tools: Add @covers tags to the options tests.
    Props pbearne, jrf, hellofromTonya, patopaiar, ironprogrammer, antonvlasenko, SergeyBiryukov, costdev.
    See #39265.
    Built from https://develop.svn.wordpress.org/trunk@53865
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53424 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53864';
+$wp_version = '6.1-alpha-53865';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
