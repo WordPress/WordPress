2f1cac50d34ea42286c89bed28c51ba0b81c17ca
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-08-12 13:24:10 +0000

    Tests: Use named data provider for `is_serialized_string()` tests.
    This makes the output when using the `--testdox` option more descriptive and is helpful when trying to debug which data set from a data provider failed the test.
    
    Follow-up to [37357], [44478].
    
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@53889
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53448 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53888';
+$wp_version = '6.1-alpha-53889';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
