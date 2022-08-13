fadf5c5fe968473d18af371b3e389ae07e2dd8d6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-08-13 12:11:11 +0000

    Tests: Bring some consistency to serialization tests.
    There were two sets of tests for `is_serialized()`:
    * One in the `functions.php` file, based on the same file name in core.
    * One in a separate class in the `functions` directory.
    
    To avoid confusion and make it easier to decide where new tests should go in the future, the existing tests are now combined in the latter location.
    
    Includes:
    * Moving `is_serialized()` and `maybe_serialize()` tests into their own classes.
    * Using named data providers to make test output more descriptive.
    * Combining test cases and removing duplicates.
    
    Follow-up to [278/tests], [279/tests], [328/tests], [32631], [45754], [47452], [49382], [53886], [53889].
    
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@53890
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53449 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53889';
+$wp_version = '6.1-alpha-53890';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
