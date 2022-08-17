431a8d54140d5514ff38b78c87d2beaea2672f2d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-08-17 21:05:12 +0000

    Build/Test Tools: Only define `WP_PLUGIN_DIR` when running core tests.
    This takes into account plugin or theme test suites that rely on `WP_PLUGIN_DIR` being set to a custom path.
    
    Follow-up to [49236], [49269], [49566], [53874].
    
    Props aaemnnosttv, flixos90.
    Fixes #39210.
    Built from https://develop.svn.wordpress.org/trunk@53902
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53461 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53901';
+$wp_version = '6.1-alpha-53902';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
