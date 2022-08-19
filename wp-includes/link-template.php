594f5ae8c13e551087438cad91bab12577b29f1d
desrosj <desrosj@git.wordpress.org>
2022-08-19 19:48:15 +0000

    Docs: Remove incorrect default value for `$path` argument of `admin_url()`.
    Props joyously, costdev.
    See #55646.
    Built from https://develop.svn.wordpress.org/trunk@53910
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53469 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/link-template.php
+++ wp-includes/link-template.php
@@ -3427,7 +3427,7 @@ function get_site_url( $blog_id = null, $path = '', $scheme = null ) {
  *
  * @since 2.6.0
  *
- * @param string $path   Optional. Path relative to the admin URL. Default 'admin'.
+ * @param string $path   Optional. Path relative to the admin URL. Default empty.
  * @param string $scheme The scheme to use. Default is 'admin', which obeys force_ssl_admin() and is_ssl().
  *                       'http' or 'https' can be passed to force those schemes.
  * @return string Admin URL link with optional path appended.
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53909';
+$wp_version = '6.1-alpha-53910';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
