9f48903eec202ea6ea68067d1a26d3702a695826
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-08-21 16:57:13 +0000

    Code Modernization: Remove dynamic properties in theme tests.
    Dynamic (non-explicitly declared) properties are deprecated as of PHP 8.2 and are expected to become a fatal error in PHP 9.0.
    
    In this particular group of test files, the test classes contain a `set_up()` method which sets a few dynamic (not explicitly declared) properties.
    
    For those properties which were set using a function call or variable access, the property has been explicitly declared on the class now.
    
    For those properties which were set using a constant scalar expression and for which the value is not changed by any of the tests, the property setting has been removed in favor of declaring a class constant.
    
    Includes removing one unused dynamic property declaration: `$this->queries` in `Test_Block_Supports_Layout`, which appears to be a copy/paste from `Tests_Theme_wpThemeJsonResolver`.
    
    Follow-up to [40/tests], [260/tests], [598/tests], [50960], [52675], [53085], [53557], [53558], [53850], [53851], [53852], [53853], [53854], [53856].
    
    Props jrf.
    See #56033.
    Built from https://develop.svn.wordpress.org/trunk@53916
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53475 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-53915';
+$wp_version = '6.1-alpha-53916';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
