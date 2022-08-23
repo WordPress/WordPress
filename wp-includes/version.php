774cac198a821441427462b42049d46d63507f3b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-08-23 13:32:12 +0000

    Editor: Fix `register_block_type` does not recognise `ancestor` block setting.
    When registering block through PHP, using `register_block_type` function, newly introduced `ancestor` block setting in `block.json` was not recognised. It worked though, when block is registered from JavaScript.
    
    Props lovor, annezazu, gziolo.
    Merges [53718] to the 6.0 branch.
    Fixes #56184.
    Built from https://develop.svn.wordpress.org/branches/6.0@53923
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.0@53482 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0.2-alpha-53903';
+$wp_version = '6.0.2-alpha-53923';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
