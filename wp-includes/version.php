4432149caf74c119abcc938392a10e5f36865602
desrosj <desrosj@git.wordpress.org>
2022-08-30 17:38:45 +0000

    WordPress 4.0.36. Built from https://develop.svn.wordpress.org/branches/4.0@54007
    git-svn-id: http://core.svn.wordpress.org/branches/4.0@53566 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.0.35';
+$wp_version = '4.0.36';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
