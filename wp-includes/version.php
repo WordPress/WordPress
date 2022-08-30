24ccd7f9a3b1dcb98e3806c31e00327dbb121087
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-08-30 15:20:12 +0000

    Posts, Post Types: Escape output within `the_meta()`.
    Convert markup to entities when displaying on the front end. Deprecates `the_meta()` in favor of `get_post_meta()`.
    
    Props tykoted, martinkrcho, xknown, dd32, peterwilsoncc, paulkevan, timothyblynjacobs.
    Merges [53958] to the 6.0 branch.
    Built from https://develop.svn.wordpress.org/branches/6.0@53961
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.0@53520 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0.2-RC1-53932';
+$wp_version = '6.0.2-RC1-53961';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
