b089b1d99c167a053b8308c3184b66f9bc9f29ef
audrasjb <audrasjb@git.wordpress.org>
2022-09-13 08:47:12 +0000

    Twenty Twenty: Improve support for Post Title block alignment.
    This changeset ensures alignement settings are correctly reflected for the Post Title block in the Editor.
    
    Props smit08, kajalgohel, devtanbir, sabernhardt, audrasjb.
    Fixes #56167.
    
    Built from https://develop.svn.wordpress.org/trunk@54141
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53700 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-54140';
+$wp_version = '6.1-alpha-54141';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
