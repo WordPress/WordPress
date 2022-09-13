7c01b0a340ff6072fad4f0a8248d77b60ddd33f3
audrasjb <audrasjb@git.wordpress.org>
2022-09-13 21:51:12 +0000

    Plugins: Visually align plugin cards in Plugin Install screen.
    This changeset adds a flexible CSS columns layout to plugin cards in the Plugin Install screen, to ensure plugin cards all have the same height.
    
    Props jamesckemp, audrasjb, joyously, sabernhardt, sharjeelkhanvmi, joyously.
    Fixes #55721.
    See #55272.
    
    Built from https://develop.svn.wordpress.org/trunk@54149
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53708 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-54148';
+$wp_version = '6.1-alpha-54149';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
