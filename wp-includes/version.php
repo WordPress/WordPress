1efbea514589bdd0ca308bbd81327411fc797961
audrasjb <audrasjb@git.wordpress.org>
2022-09-14 22:33:10 +0000

    Twenty Twenty-One: Remove WooCommerce related CSS selectors.
    The selectors for Twenty Twenty-One's default `max-width` have used the `.woocommerce` class since the theme's [https://github.com/WordPress/twentytwentyone/commit/956a3172a41545ca04a133c034b47ae870a4a9cd#diff-b757cb999144a1bc7e14604bc6d4bab7d87ea1a3bf5000c33937e490d75b9d92R3-R9 initial commit]. This changeset removes all WooCommerce CSS selector from the theme, as they would need to be supported forever. The change was tested with WooCommerce installed and no issues were raised. 
    
    Props sabernhardt, audrasjb, davidbaumwald, tahmidulkarim, mukesh27.
    Fixes #56366.
    
    Built from https://develop.svn.wordpress.org/trunk@54170
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53729 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-54169';
+$wp_version = '6.1-alpha-54170';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
