47d61a73efce94b4939273307123c121593c0863
hellofromTonya <hellofromTonya@git.wordpress.org>
2022-09-15 22:28:13 +0000

    Editor: Fix get_block_templates() to get templates for a post type.
    When a post type is passed to `get_block_templates()` in the query, return only the templates that match that post type.
    
    Fixes an issue where:
    * when a block template provided by a theme has been edited by the user
    * and that template has specific defined `postTypes`
    * but after editing, the template was available for all post types.
    
    Follow-up to [52062].
    
    Props aljullu, bernhard-reiter, hellofromTonya.
    Fixes #55881.
    Built from https://develop.svn.wordpress.org/trunk@54184
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53743 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/block-template-utils.php
+++ wp-includes/block-template-utils.php
@@ -552,8 +552,8 @@ function _build_block_template_result_from_post( $post ) {
 	}
 
 	$theme          = $terms[0]->name;
-	$has_theme_file = wp_get_theme()->get_stylesheet() === $theme &&
-		null !== _get_block_template_file( $post->post_type, $post->post_name );
+	$template_file = _get_block_template_file( $post->post_type, $post->post_name );
+	$has_theme_file = wp_get_theme()->get_stylesheet() === $theme && null !== $template_file;
 
 	$origin = get_post_meta( $post->ID, 'origin', true );
 
@@ -572,6 +572,10 @@ function _build_block_template_result_from_post( $post ) {
 	$template->has_theme_file = $has_theme_file;
 	$template->is_custom      = true;
 	$template->author         = $post->post_author;
+
+	if ( 'wp_template' === $post->post_type && $has_theme_file && isset( $template_file['postTypes'] ) ) {
+		$template->post_types = $template_file['postTypes'];
+	}
 
 	if ( 'wp_template' === $post->post_type && isset( $default_template_types[ $template->slug ] ) ) {
 		$template->is_custom = false;
@@ -672,6 +676,13 @@ function get_block_templates( $query = array(), $template_type = 'wp_template' )
 		}
 
 		if ( $post_type && ! $template->is_custom ) {
+			continue;
+		}
+
+		if ( $post_type &&
+			isset( $template->post_types ) &&
+			! in_array( $post_type, $template->post_types, true )
+		) {
 			continue;
 		}
 
