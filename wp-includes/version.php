af253aa544ac3a793eba2df321e934aba4f04969
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-09-19 23:24:11 +0000

    I18N: Update list of continents and cities for the timezone selection.
    Based on a two-way comparison between the available timezone city names in PHP 5.6.20 and PHP 8.2.0.
    
    Lists of available timezone names have been retrieved using the PHP `timezone_identifiers_list()` function.
    
    See: [https://3v4l.org/ro1vY/rfc#vgit.master timezone_identifiers_list() output and comparison].
    
    Note: Both spellings of `Kiev`/`Kyiv` need to be in the list to allow it to work PHP cross-version.
    * The “old” version — `Kiev` — will be used as the basis to find the localized name for the timezone dropdown lists on PHP 5.6 to 8.1.
    * The corrected spelling — `Kyiv` — will be used to find the localized name on PHP 8.2 and up.
    
    Follow-up to [50555], [54207], [54217].
    
    Props jrf, costdev.
    See #56468.
    Built from https://develop.svn.wordpress.org/trunk@54227
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53786 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-54226';
+$wp_version = '6.1-alpha-54227';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
