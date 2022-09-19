43e32ffa5c19fe6092b66777aea34b2176bc1ec2
joedolson <joedolson@git.wordpress.org>
2022-09-19 21:18:09 +0000

    Editor: Fix text zoom on link popup editor.
    Update CSS in the classic visual editor link popup to remove sizing in pixels that caused significant text overlaps when the base font size is scaled or set to a larger custom value in the browser or operating system.
    
    Props jonny-s, sabernhardt, joedolson.
    Fixes #53174.
    Built from https://develop.svn.wordpress.org/trunk@54216
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53775 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-alpha-54215';
+$wp_version = '6.1-alpha-54216';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
