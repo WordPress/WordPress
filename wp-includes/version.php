b85b4cb6342b7d61905c39f787980bf5eec61b41
desrosj <desrosj@git.wordpress.org>
2022-09-23 17:03:08 +0000

    Build/Test Tools: Increase the timeout value for MacOS jobs.
    The current `timeout-minutes` value of `20` is a bit too short for MacOS jobs in GitHub Actions, which on occasion take a bit longer.
    
    This bumps that limit to `30` to avoid unnecessarily flagging a job as stuck.
    
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@54293
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53852 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta1-54292';
+$wp_version = '6.1-beta1-54293';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
