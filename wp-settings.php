5e32e7de29bb18b56298f4c7c08e1937421d9069
jorgefilipecosta <jorgefilipecosta@git.wordpress.org>
2022-09-26 10:17:11 +0000

    Fix: Revert [54305].
    This commit caused an incompatibility with the latest released Gutenberg version.
    
    Props bernhard-reiter.
    Built from https://develop.svn.wordpress.org/trunk@54306
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53865 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-settings.php
+++ wp-settings.php
@@ -322,7 +322,6 @@ require ABSPATH . WPINC . '/block-editor.php';
 require ABSPATH . WPINC . '/block-patterns.php';
 require ABSPATH . WPINC . '/class-wp-block-supports.php';
 require ABSPATH . WPINC . '/block-supports/utils.php';
-require ABSPATH . WPINC . '/block-supports/settings.php';
 require ABSPATH . WPINC . '/block-supports/align.php';
 require ABSPATH . WPINC . '/block-supports/border.php';
 require ABSPATH . WPINC . '/block-supports/colors.php';
