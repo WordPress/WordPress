bed72ed20db773f02113bc48f7f8f8646f9d1c78
davidbaumwald <davidbaumwald@git.wordpress.org>
2022-09-27 16:33:10 +0000

    Editor: Revert dynamic template names and descriptions.
    After discussions with the Editor Tech leads for 6.1(@bernhard-reiter, @cbravobernal, and @ndiego) and additional guidance from @hellofromtonya, the decision has been made to exclude [https://github.com/WordPress/gutenberg/pull/43862 Gutenberg PR #43862] from the pre-Beta 2 Gutenberg sync PR and revert [54280]. Why? [54280] added the feature's PHP code, but the JS package updates were not included before feature freeze(Beta 1), meaning the feature was incomplete. As the PHP code does not work without the JS package update, the feature is incomplete and missed the feature freeze deadline.
    
    Leaving the PHP code was discussed. However, there is a risk of it needing to change which could complicate backwards compatibility in 6.2 when the feature is eventually introduced.
    
    Follow-up to [54280].
    
    Props hellofromTonya, bernhard-reiter, cbravobernal.
    See #56467.
    Built from https://develop.svn.wordpress.org/trunk@54333
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53892 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta1-54332';
+$wp_version = '6.1-beta1-54333';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
