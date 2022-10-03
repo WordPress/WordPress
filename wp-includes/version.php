f82413b29d4c036c9b4ea0ea57f0679c032216a7
davidbaumwald <davidbaumwald@git.wordpress.org>
2022-10-03 16:36:11 +0000

    Editor: Make template names and descriptions dynamic, again.
    In the lead up to 6.1 Beta 2, dynamic titles and descriptions for site editor templates was deemed "feature incomplete" and [54280] was reverted.  After further consideration, this code is being re-merged in preparation for Beta 3, reverting the revert in [54333].
    
    Follow-up to [54280] and [54333].
    
    Props bernie, ntsekouras, jorgefilipecosta, jameskoster, cbravobernal.
    See #56467.
    Built from https://develop.svn.wordpress.org/trunk@54370
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53929 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta2-54369';
+$wp_version = '6.1-beta2-54370';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
