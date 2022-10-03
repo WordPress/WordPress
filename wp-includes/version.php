91900d263956f6a5215bea5f93e220d626d758ce
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-03 16:03:13 +0000

    Build/Test Tools: Remove PHP 8.1 and 8.2 from allowed failures.
    With all known unit test failures now addressed, WordPress 6.1 aims to support PHP 8.1 and 8.2 as much as possible.
    
    While full compatibility with PHP 8.1 and 8.2 is still a work in progress, this commit aims to actively prevent new PHP issues from being introduced in WordPress core.
    
    All remaining known issues are deprecation notices. Please note, a deprecation notice is not an error, but rather an indicator of where additional work is needed for compatibility before PHP 9 (i.e. when the notices become fatal errors). With a deprecation notice, the PHP code will continue to work and nothing is broken.
    
    Follow-up to [49077], [49162], [50299], [51588], [51604], [53922], [54072].
    
    Props jrf, desrosj.
    See #55652, #55656, #56009, #56681.
    Built from https://develop.svn.wordpress.org/trunk@54369
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53928 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta2-54368';
+$wp_version = '6.1-beta2-54369';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
