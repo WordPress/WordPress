13b79b4a3b22a837e006fd7c9615b03946c9f6c9
audrasjb <audrasjb@git.wordpress.org>
2022-10-04 08:22:10 +0000

    Twenty Sixteen: Improve Pullquote Block text color consistency between editor and front-end.
    This changeset ensures the text color selected for the text of the Pullquote Block is reflected on both front-end and on the editor.
    
    Props nithins53, nidhidhandhukiya, audrasjb, sabernhardt, poena.
    Fixes #56008.
    
    Built from https://develop.svn.wordpress.org/trunk@54381
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53940 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta2-54380';
+$wp_version = '6.1-beta2-54381';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
