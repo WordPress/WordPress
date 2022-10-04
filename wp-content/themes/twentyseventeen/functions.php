9d727a185d9390a1e5c78cb2cdae2c8c9ec4a364
desrosj <desrosj@git.wordpress.org>
2022-10-04 01:43:12 +0000

    Twenty Seventeen: Update the `scrollTo` jQuery plugin.
    This updates the `scrollTo` jQuery plugin included in Twenty Seventeen to the latest version, `2.1.3`.
    
    For a full list of changes in this update, see GitHub: https://github.com/flesler/jquery.scrollTo/compare/2.1.2...v2.1.3.
    
    Props sarahricker, mukesh27, desrosj.
    Fixes #56702.
    Built from https://develop.svn.wordpress.org/trunk@54374
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53933 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyseventeen/functions.php
+++ wp-content/themes/twentyseventeen/functions.php
@@ -498,7 +498,7 @@ function twentyseventeen_scripts() {
 
 	wp_enqueue_script( 'twentyseventeen-global', get_theme_file_uri( '/assets/js/global.js' ), array( 'jquery' ), '20190121', true );
 
-	wp_enqueue_script( 'jquery-scrollto', get_theme_file_uri( '/assets/js/jquery.scrollTo.js' ), array( 'jquery' ), '2.1.2', true );
+	wp_enqueue_script( 'jquery-scrollto', get_theme_file_uri( '/assets/js/jquery.scrollTo.js' ), array( 'jquery' ), '2.1.3', true );
 
 	wp_localize_script( 'twentyseventeen-skip-link-focus-fix', 'twentyseventeenScreenReaderText', $twentyseventeen_l10n );
 
