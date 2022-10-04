9bda85099f5878f1e1a8f890170dd65ffad8e4e7
desrosj <desrosj@git.wordpress.org>
2022-10-04 00:45:14 +0000

    Build/Test Tools: Display PHPCS results in the GitHub Action logs.
    When running PHPCS scans (both for checking coding standards and PHP version compatibility), the results are currently only returned silently in a format that GitHub can consume for contextually annotating any code being flagged.
    
    This changes workflows using PHPCS to also display the results of each scan in the GitHub Action log, making it easier to find and understand what is causing failures.
    
    Props jrf.
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@54371
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53930 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta2-54370';
+$wp_version = '6.1-beta2-54371';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
