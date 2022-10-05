81cbe2c55b511b35b84e820941338b221f29a47a
audrasjb <audrasjb@git.wordpress.org>
2022-10-05 14:09:11 +0000

    General: Add a default `font-style` value for Adminbar links.
    If a theme includes a `theme.json` file and sets a specific `font-style` on links, the font-style also changes the links in the WordPress admin bar, when the admin bar is visible on front-end. For example, this happens with the `pitch` style variation of Twenty Twenty-Three.
    
    Using a default value for the `font-style` property prevents `theme.json` files from overriding it.
    
    Props audrasjb, poena.
    Fixes #56737.
    
    Built from https://develop.svn.wordpress.org/trunk@54395
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53954 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta3-54394';
+$wp_version = '6.1-beta3-54395';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
