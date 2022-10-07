22f52c83c2a66a150f7a5129969ca6a102eeb2b5
audrasjb <audrasjb@git.wordpress.org>
2022-10-07 22:19:11 +0000

    Build/Test Tools: Add tests for `wp_nonce_field()` and `wp_referer_field()`.
    This changeset adds missing unit tests for these two functions.
    
    Props pbearne, costdev, audrasjb.
    Fixes #55578.
    
    Built from https://develop.svn.wordpress.org/trunk@54420
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53979 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta3-54419';
+$wp_version = '6.1-beta3-54420';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
