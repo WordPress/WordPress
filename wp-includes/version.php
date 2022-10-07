a1518e3bc460c0c80c41190970dbf4b8ffece683
Peter Wilson <wilson@peterwilson.cc>
2022-10-07 02:51:10 +0000

    Build/Test tools: Add tests for `wp_nonce_url()`.
    Props pbearne, costdev.
    See #55652.
    Fixes #54870.
    
    Built from https://develop.svn.wordpress.org/trunk@54407
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53966 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta3-54406';
+$wp_version = '6.1-beta3-54407';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
