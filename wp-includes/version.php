646a284e7193122c9ef0553189edd8155d42e076
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-07 01:09:11 +0000

    Tests: Clean up test image for site icon in `Tests_REST_Server` on teardown.
    This makes sure there are no leftover images after the test class is run.
    
    Follow-up to [52080], [53463], [53920].
    
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@54403
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53962 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta3-54402';
+$wp_version = '6.1-beta3-54403';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
