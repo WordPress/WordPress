2e827f55b4506d988bf3b2ac112cf1970cafa673
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-09 16:39:28 +0000

    Coding Standards: Correct alignment in `test_import_theme_starter_content()`.
    This fixes an `Equals sign not aligned correctly` WPCS warning.
    
    Follow-up to [54425].
    
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@54426
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@53985 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta3-54425';
+$wp_version = '6.1-beta3-54426';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
