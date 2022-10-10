8ad3828d2091eed0749c0456197d31ac5f544015
audrasjb <audrasjb@git.wordpress.org>
2022-10-10 21:56:11 +0000

    Twenty Seventeen: Prevent scaling issues on featured images using Safari on iPadOS.
    This changeset ensures `background-attachment` is set to `scroll` for parallax images when the browser is iOS Safari and the screen is at least 48em (768px) wide. Narrower screens have the default `scroll` value regardless of the browser. It prevents from scaling issues on the images featured on the front page using Twenty Seventeen.
    
    Props JarretC, sabernhardt, mrfoxtalbot, dkotter.
    Fixes #48195.
    
    Built from https://develop.svn.wordpress.org/trunk@54450
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54009 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta3-54449';
+$wp_version = '6.1-beta3-54450';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
