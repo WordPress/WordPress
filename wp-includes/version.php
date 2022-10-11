d989d0b1f7bd873fe0a9078a66a508f35e903986
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-11 14:27:11 +0000

    Tests: Relocate the tests for recommended PHP, MySQL, and MariaDB versions.
    This commit moves the tests for recommended versions of dependencies referenced in `readme.html` to a new location for better context. Also updates test class name for clarity, and `@covers` annotations to `@coversNothing`.
    
    Follow-up to [26166], [33946], [35172], [52418], [52424].
    
    Props ironprogrammer, bbobnis, antonvlasenko, azaozz, chaion07, priyomukul.
    Fixes #45867.
    Built from https://develop.svn.wordpress.org/trunk@54479
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54038 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta3-54478';
+$wp_version = '6.1-beta3-54479';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
