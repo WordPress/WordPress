1d58ed2b6cf7616a9280e37596d1f1f90cb12eb1
audrasjb <audrasjb@git.wordpress.org>
2022-10-11 10:00:13 +0000

    Build/Test Tools: Add unit test for non-existing block-styles registration.
    Follow-up to [54155].
    
    Props aristath, mukesh27.
    Fixes #56664.
    
    Built from https://develop.svn.wordpress.org/trunk@54472
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54031 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-beta3-54471';
+$wp_version = '6.1-beta3-54472';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
