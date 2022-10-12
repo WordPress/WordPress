cd5494dc2f50a9e68cd346e1bab884d2521d0403
asifmuztaba1 <amuztaba18@gmail.com>
2022-10-12 12:47:12 +0600

    added null coelsing operator for rtrim function to avoid deprecation warning

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -2780,7 +2780,7 @@ function trailingslashit( $string ) {
  * @return string String without the trailing slashes.
  */
 function untrailingslashit( $string ) {
-	return rtrim( $string, '/\\' );
+	return rtrim( $string ?? '', '/\\' );
 }
 
 /**
