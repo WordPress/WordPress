4ecb2ec36b6533ad8e1913abd7da5b6cc2c1c9f6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-17 20:12:10 +0000

    WordPress 6.0.3.
    Built from https://develop.svn.wordpress.org/branches/6.0@54597
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.0@54151 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0.3-alpha-54577';
+$wp_version = '6.0.3';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
