90ea3369392bb89642f43f573dc334de3478eac2
audrasjb <audrasjb@git.wordpress.org>
2022-10-17 12:37:09 +0000

    Mail: Reset PHPMailer properties between use.
    Props xknown, martinkrcho, tykoted.
    Merges [54529] to the 6.0 branch.
    
    Built from https://develop.svn.wordpress.org/branches/6.0@54539
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.0@54094 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0.3-alpha-54538';
+$wp_version = '6.0.3-alpha-54539';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
