ce1e18ac33253388f1d419c460aece9834676a6e
desrosj <desrosj@git.wordpress.org>
2022-10-17 21:37:21 +0000

    WordPress 5.5.11. Built from https://develop.svn.wordpress.org/branches/5.5@54622
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@54174 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/dist/block-library.js
+++ wp-includes/js/dist/block-library.js
@@ -3959,7 +3959,6 @@ function (_super) {
     };
 
     _this.onTouchStart = function (e) {
-      e.preventDefault();
       document.addEventListener('touchmove', _this.onTouchMove, {
         passive: false
       }); // iOS 11 now defaults to passive: true
