558b045358bf271691aa2aab14efc67e25a57f12
audrasjb <audrasjb@git.wordpress.org>
2022-10-17 19:40:26 +0000

    Help/About: Update the About page for WordPress 6.0.3.
    Built from https://develop.svn.wordpress.org/branches/6.0@54577
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.0@54131 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0.3-alpha-54544';
+$wp_version = '6.0.3-alpha-54577';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
