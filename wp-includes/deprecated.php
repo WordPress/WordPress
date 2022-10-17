aac137786b82962a5bec38576d8a7bc2fe0e1049
audrasjb <audrasjb@git.wordpress.org>
2022-10-17 18:02:54 +0000

    Grouped backports to the 5.4 branch.
    - Editor: Bump @wordpress packages for the branch,
    - Media: Refactor search by filename within the admin,
    - REST API: Lockdown post parameter of the terms endpoint,
    - Customize: Escape blogname option in underscores templates,
    - Query: Validate relation in `WP_Date_Query`,
    - Posts, Post types: Apply KSES to post-by-email content,
    - General: Validate host on "Are you sure?" screen,
    - Posts, Post types: Remove emails from post-by-email logs,
    - Pings/trackbacks: Apply KSES to all trackbacks,
    - Mail: Reset PHPMailer properties between use,
    - Comments: Apply kses when editing comments,
    - Widgets: Escape RSS error messages for display.
    
    Merges [54521-54530] to the 5.4 branch.
    Props audrasjb, costdev, cu121, dd32, davidbaumwald, ehtis, johnbillion, johnjamesjacoby, martinkrcho, matveb, oztaser, paulkevan, peterwilsoncc, ravipatel, SergeyBiryukov, talldanwp, timothyblynjacobs, tykoted, voldemortensen, vortfu, xknown.
    
    Built from https://develop.svn.wordpress.org/branches/5.4@54559
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.4@54114 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/deprecated.php
+++ wp-includes/deprecated.php
@@ -3965,3 +3965,21 @@ function wp_get_user_request_data( $request_id ) {
 	_deprecated_function( __FUNCTION__, '5.4.0', 'wp_get_user_request()' );
 	return wp_get_user_request( $request_id );
 }
+
+
+/**
+ * Filter the SQL clauses of an attachment query to include filenames.
+ *
+ * @since 4.7.0
+ * @deprecated 6.0.3
+ * @access private
+ *
+ * @param array $clauses An array including WHERE, GROUP BY, JOIN, ORDER BY,
+ *                       DISTINCT, fields (SELECT), and LIMITS clauses.
+ * @return array The unmodified clauses.
+ */
+function _filter_query_attachment_filenames( $clauses ) {
+	_deprecated_function( __FUNCTION__, '6.0.3', 'add_filter( "wp_allow_query_attachment_by_filename", "__return_true" )');
+	remove_filter( 'posts_clauses', __FUNCTION__ );
+	return $clauses;
+}
