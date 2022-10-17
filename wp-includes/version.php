8b2ade73a2243a94219f5c31c021ea246142b687
audrasjb <audrasjb@git.wordpress.org>
2022-10-17 11:38:11 +0000

    REST API: Lockdown post parameter of the terms endpoint.
    Props johnbillion, tykoted, timothyblynjacobs, peterwilsoncc, martinkrcho, ehtis.
    
    Built from https://develop.svn.wordpress.org/trunk@54528
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54083 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-RC1-54527';
+$wp_version = '6.1-RC1-54528';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
