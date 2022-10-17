b683ac27c5084cdb33e0d211085c082537cf7a5f
audrasjb <audrasjb@git.wordpress.org>
2022-10-17 17:41:29 +0000

    Grouped backports to the 5.9 branch.
    - Editor: Bump @wordpress packages for the 5.9 branch,
    - Media: Refactor search by filename within the admin,
    - REST API: Lockdown post parameter of the terms endpoint,
    - Customize: Escape blogname option in underscores templates,
    - Query: Validate relation in `WP_Date_Query`,
    - Users: Revert use of shared objects for current user,
    - Posts, Post types: Apply KSES to post-by-email content,
    - General: Validate host on "Are you sure?" screen,
    - Posts, Post types: Remove emails from post-by-email logs,
    - Pings/trackbacks: Apply KSES to all trackbacks,
    - Mail: Reset PHPMailer properties between use,
    - Comments: Apply kses when editing comments,
    - Widgets: Escape RSS error messages for display.
    
    Merges [54521-54530] to the 5.9 branch.
    Props audrasjb, costdev, cu121, dd32, davidbaumwald, ehtis, johnbillion, johnjamesjacoby, martinkrcho, matveb, oztaser, paulkevan, peterwilsoncc, ravipatel, SergeyBiryukov, talldanwp, timothyblynjacobs, tykoted, voldemortensen, vortfu, xknown.
    
    Built from https://develop.svn.wordpress.org/branches/5.9@54545
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@54100 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/deprecated.php
+++ wp-includes/deprecated.php
@@ -4225,3 +4225,21 @@ function wp_render_duotone_filter_preset( $preset ) {
 	_deprecated_function( __FUNCTION__, '5.9.1', 'wp_get_duotone_filter_property()' );
 	return wp_get_duotone_filter_property( $preset );
 }
+
+/**
+ * Filter the SQL clauses of an attachment query to include filenames.
+ *
+ * @since 4.7.0
+ * @deprecated 6.0.3
+ * @access private
+ *
+ * @param array $clauses An array including WHERE, GROUP BY, JOIN, ORDER BY,
+ *                       DISTINCT, fields (SELECT), and LIMITS clauses.
+ * @return array The unmodified clauses.
+ */
+function _filter_query_attachment_filenames( $clauses ) {
+	_deprecated_function( __FUNCTION__, '6.0.3', 'add_filter( "wp_allow_query_attachment_by_filename", "__return_true" )');
+	remove_filter( 'posts_clauses', __FUNCTION__ );
+	return $clauses;
+}
+
