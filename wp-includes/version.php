03f76d4d77151a855478bead44e34b2bf367fa35
desrosj <desrosj@git.wordpress.org>
2022-10-17 20:16:50 +0000

    WordPress 5.3.14. Built from https://develop.svn.wordpress.org/branches/5.3@54600
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@54154 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.13';
+$wp_version = '5.3.14';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
