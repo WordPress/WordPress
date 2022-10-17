84c1c0c7da5afb8b9883b945682fa0ad8c9a523e
audrasjb <audrasjb@git.wordpress.org>
2022-10-17 12:11:09 +0000

    General: Validate host on "Are you sure?" screen.
    Props voldemortensen, xknown, peterwiloncc.
    
    Merges [54522] to the 6.0 branch.
    
    Built from https://develop.svn.wordpress.org/branches/6.0@54532
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.0@54087 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -3580,10 +3580,12 @@ function wp_nonce_ays( $action ) {
 	} else {
 		$html = __( 'The link you followed has expired.' );
 		if ( wp_get_referer() ) {
+			$wp_http_referer = remove_query_arg( 'updated', wp_get_referer() );
+			$wp_http_referer = wp_validate_redirect( esc_url_raw( $wp_http_referer ) );
 			$html .= '</p><p>';
 			$html .= sprintf(
 				'<a href="%s">%s</a>',
-				esc_url( remove_query_arg( 'updated', wp_get_referer() ) ),
+				esc_url( $wp_http_referer ),
 				__( 'Please try again.' )
 			);
 		}
