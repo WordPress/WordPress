7f6f4c36ed2d277a3f596c3dd05979c1191002ef
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-17 19:45:22 +0000

    WordPress 4.1.37.
    Built from https://develop.svn.wordpress.org/branches/4.1@54581
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.1@54135 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1.36';
+$wp_version = '4.1.37';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
