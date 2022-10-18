6743ca5fc4b87942537717b1e8cf97d41041def1
davidbaumwald <davidbaumwald@git.wordpress.org>
2022-10-18 14:47:15 +0000

    Editor: Fix legacy group inner block wrappers in constrained layouts.
    Reviewed by bernhard-reiter.
    
    Props isabel_brison, noisysocks, andrewserong, poena, bernhard-reiter.
    See #56467.
    Built from https://develop.svn.wordpress.org/trunk@54633
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54185 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/block-supports/layout.php
+++ wp-includes/block-supports/layout.php
@@ -466,7 +466,7 @@ function wp_restore_group_inner_container( $block_content, $block ) {
 	if (
 		WP_Theme_JSON_Resolver::theme_has_support() ||
 		1 === preg_match( $group_with_inner_container_regex, $block_content ) ||
-		( isset( $block['attrs']['layout']['type'] ) && 'default' !== $block['attrs']['layout']['type'] )
+		( isset( $block['attrs']['layout']['type'] ) && 'flex' === $block['attrs']['layout']['type'] )
 	) {
 		return $block_content;
 	}
