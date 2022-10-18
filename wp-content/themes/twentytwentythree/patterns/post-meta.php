7e598822d057598cdebd938f3ea70a014fdb9773
desrosj <desrosj@git.wordpress.org>
2022-10-18 00:37:16 +0000

    Twenty Twenty-Three: Sync the latest fixes for RC2.
    For a full list of changes included, see the `twentytwentythree` repository on GitHub: https://github.com/WordPress/twentytwentythree/compare/4ea5d832ec9b684dcbf9ac5e67ff4e9ee5ae1e16...0f273d787481b391d428de671659533642b3aa3c.
    
    Props mikachan, beafialho, kafleg, richtabor, poena, annezazu.
    Fixes #56383.
    Built from https://develop.svn.wordpress.org/trunk@54629
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54181 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwentythree/patterns/post-meta.php
+++ wp-content/themes/twentytwentythree/patterns/post-meta.php
@@ -11,14 +11,14 @@
 <div style="height:0" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--70);margin-bottom:var(--wp--preset--spacing--70)">
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--70)">
 	<!-- wp:separator {"opacity":"css","align":"wide","className":"is-style-wide"} -->
 	<hr class="wp-block-separator alignwide has-css-opacity is-style-wide"/>
 	<!-- /wp:separator -->
 
-	<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|70"}},"fontSize":"small"} -->
-	<div class="wp-block-columns alignwide has-small-font-size" style="margin-top:var(--wp--preset--spacing--40)">
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|30"}},"fontSize":"small"} -->
+	<div class="wp-block-columns alignwide has-small-font-size" style="margin-top:var(--wp--preset--spacing--30)">
 		<!-- wp:column {"style":{"spacing":{"blockGap":"0px"}}} -->
 		<div class="wp-block-column">
 			<!-- wp:group {"style":{"spacing":{"blockGap":"0.5ch"}},"layout":{"type":"flex"}} -->
