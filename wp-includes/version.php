0af9b6039cb6fe1a0169093c3ae0f96e14024542
Gary Pendergast <gary@pento.net>
2022-10-20 00:04:15 +0000

    Embeds: Broaden the Tumblr oEmbed matcher to include all Tumblr URL structures.
    Tumblr's oEmbed API correctly rejects invalid URLs, we can rely on that for the handful of cases that aren't embeddable URLs.
    
    Props cbravobernal, bernhard-reiter.
    Merges [54652] to the 6.1 branch.
    Fixes #56733.
    
    
    Built from https://develop.svn.wordpress.org/branches/6.1@54653
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.1@54205 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-RC2-54648';
+$wp_version = '6.1-RC2-54653';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
