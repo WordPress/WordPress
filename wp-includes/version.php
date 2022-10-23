7240236672446feea93508f4647a1a10f316193b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-23 12:38:15 +0000

    Tests: Make the message for skipping some tests with an external object cache more consistent.
    Follow-up to [33702], [34767], [49857], [53821].
    
    See #56793.
    Built from https://develop.svn.wordpress.org/trunk@54668
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54220 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54667';
+$wp_version = '6.2-alpha-54668';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
