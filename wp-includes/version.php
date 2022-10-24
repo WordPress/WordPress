f2dce6114bffa2ffe00cbf8609cab63eecf534ff
audrasjb <audrasjb@git.wordpress.org>
2022-10-24 21:43:14 +0000

    Editor: Fix modal height responsiveness on link popup editor.
    Fix the responsive breakpoint styles for short vertical viewports on the link popup modal. Follow-up to [54216].
    
    Props sabernhardt, joedolson.
    Fixes #53174.
    Merges [54660] to the 6.1 branch.
    
    Built from https://develop.svn.wordpress.org/branches/6.1@54684
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.1@54236 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-RC2-54683';
+$wp_version = '6.1-RC2-54684';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
