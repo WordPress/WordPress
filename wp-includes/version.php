60cf27f53c61bf7a5994625be234d91aad7e4459
desrosj <desrosj@git.wordpress.org>
2022-10-24 16:49:15 +0000

    Build/Test Tools: Ensure PHPCS related workflows are properly marked as failed.
    When a ruleset error is encountered during a PHPCodeSniffer scan, an XML report is not generated and `cs2pr` will exit with a `0`.
    
    In this situation, a workflow run will be marked as passing (even though a failure has occurred) due to the presence of `continue-on-error`.
    
    This adjusts the logic in the Coding Standards and PHP Compatibility workflows to remove the need for the `continue-on-error` option and ensures all failures are accurately reflected within the GitHub Actions UI.
    
    Follow up to [54371].
    
    Props jrf, TobiasBg.
    See #55652.
    Built from https://develop.svn.wordpress.org/trunk@54678
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54230 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54675';
+$wp_version = '6.2-alpha-54678';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
