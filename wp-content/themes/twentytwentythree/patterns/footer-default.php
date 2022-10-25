dd9429d05199c81e20fc0aa6f2929cc23d859a66
Dominik Schilling <dominikschilling+git@gmail.com>
2022-10-25 15:02:15 +0000

    Twenty Twenty-Three: Merge the latest changes from GitHub.
    For a full list of changes included, see the `twentytwentythree` repository on GitHub: https://github.com/WordPress/twentytwentythree/compare/0f273d787481b391d428de671659533642b3aa3c...e2005b0272b8480a8fa9155e5802e22659d84674.
    
    Props kafleg, poena, mikachan, tobifjellner, aristath.
    Merges [54686] to the 6.1 branch.
    Fixes #56383.
    Built from https://develop.svn.wordpress.org/branches/6.1@54691
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.1@54243 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwentythree/patterns/footer-default.php
+++ wp-content/themes/twentytwentythree/patterns/footer-default.php
@@ -6,9 +6,11 @@
  * Block Types: core/template-part/footer
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40)"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40)"><!-- wp:site-title {"level":0} /-->
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40)">
+		<!-- wp:site-title {"level":0} /-->
 		<!-- wp:paragraph {"align":"right"} -->
 		<p class="has-text-align-right">
 		<?php
