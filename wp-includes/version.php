72ae8d87ac7b8feb864bda854b7a645c915d530d
desrosj <desrosj@git.wordpress.org>
2022-10-26 15:08:13 +0000

    Build/Test Tools: Introduce a workflow for testing and building default themes.
    This introduces a GitHub Action workflow that performs the following steps for default themes:
    
    - Test installing npm dependencies and running the build script (for relevant themes).
    - Create ZIP files for the .org Theme directory and uploads them as workflow artifacts.
    
    The workflow will only run on limited occasions, one of those being when Twenty Nineteen, Twenty Twenty, or Twenty Twenty-One is edited. These are the themes with build scripts that need to be tested for issues.
    
    It can also be manually run through the GitHub Actions UI for occasions when the ZIP files are needed for a release.
    
    Props peterwilsoncc, desrosj
    Fixes #56898.
    Built from https://develop.svn.wordpress.org/trunk@54699
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54251 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54698';
+$wp_version = '6.2-alpha-54699';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
