0fdfa8f786576d08a4a1c31a88969dc73b155cdc
desrosj <desrosj@git.wordpress.org>
2022-10-27 13:47:15 +0000

    Docs: Add a `@since` note for `object-fit` support in `safecss_filter_attr()`.
    Follow-up to [54675].
    
    Props peterwilsoncc, SergeyBiryukov, davidbaumwald.
    Merges [54698] to the 6.1 branch.
    See #56855.
    Built from https://develop.svn.wordpress.org/branches/6.1@54700
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.1@54252 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-RC3-54697';
+$wp_version = '6.1-RC3-54700';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
