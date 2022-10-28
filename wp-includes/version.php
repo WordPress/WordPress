1d4fa825b8ed0259a8e65146a314740351fdfe35
antpb <antpb@git.wordpress.org>
2022-10-28 15:18:11 +0000

    Media: Reverts `get_attached_file()` changes for normalized Windows paths.
    Based on feedback from network storage configurations there was a noticed slowdown due to the usage of the `path_join()` function. This needs more time to find a workaround.
    
    Follow-up to [53934].
    Props mreishus, SergeyBiryukov, desrosj, mikeschroder.
    Reverts [53934].
    See #56924.
    
    Built from https://develop.svn.wordpress.org/trunk@54712
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54264 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54711';
+$wp_version = '6.2-alpha-54712';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
