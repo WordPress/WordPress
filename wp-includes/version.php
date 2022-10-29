c2a843c36a5b42800181c4870f6172d9a2ce710b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-29 15:59:11 +0000

    Tests: Remove unused `$user_ids` property in `Tests_Ajax_Autosave` class.
    This was previously used to delete the users created during `wpSetUpBeforeClass()`, however that now happens automatically in `WP_UnitTestCase_Base::tear_down_after_class()` via `_delete_all_data()`.
    
    Follow-up to [35311], [37404], [38398].
    
    See #56793.
    Built from https://develop.svn.wordpress.org/trunk@54720
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54272 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54719';
+$wp_version = '6.2-alpha-54720';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
