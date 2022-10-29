81a1638e9afdd7d3194e79df143445f7fe23bcc9
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-29 13:41:15 +0000

    Tests: Move the tests for pluggable function signatures to the `pluggable` directory.
    This aims to bring some consistency to the location of pluggable function tests.
    
    Includes:
    * Renaming the test class to better reflect the intention of the tests.
    * Adding a `@coversNothing` tag to the class DocBlock.
    
    Follow-up to [34126], [53478], [54702].
    
    Props pbearne, SergeyBiryukov.
    See #56793, #56782.
    Built from https://develop.svn.wordpress.org/trunk@54716
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54268 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54712';
+$wp_version = '6.2-alpha-54716';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
