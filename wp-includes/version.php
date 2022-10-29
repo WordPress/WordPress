fa79a5e83cbd79f2ed66125709a5e4fb058c7502
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-29 17:12:14 +0000

    Coding Standards: Correct alignment in `Tests_Ajax_Autosave::wpSetUpBeforeClass()`.
    This fixes an `Equals sign not aligned with surrounding assignments` WPCS warning.
    
    Follow-up to [54720].
    
    See #56793.
    Built from https://develop.svn.wordpress.org/trunk@54721
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54273 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54720';
+$wp_version = '6.2-alpha-54721';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
