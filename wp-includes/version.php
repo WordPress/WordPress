f4e379c341708e9866d501ed83ecfa258ae3d37f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-31 22:12:10 +0000

    Post WordPress 6.1 RC6 version bump. Built from https://develop.svn.wordpress.org/branches/6.1@54736
    git-svn-id: http://core.svn.wordpress.org/branches/6.1@54288 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-RC6';
+$wp_version = '6.1-RC6-54736';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
