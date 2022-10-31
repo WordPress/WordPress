e848e79e2feda8a911cb70bf83cea22c0462b05d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-10-31 20:28:12 +0000

    Tests: Temporarily skip WOFF file test on PHP 8.1.
    A recent change to how WOFF files are processed in PHP 8.2 RC3 has caused a new test failure.
    
    The tests was previously skipped on PHP 8.2, however, apparently after a `fileinfo` extension update, it started failing on PHP 8.1 too.
    
    This commit adjusts the skipping condition to include PHP 8.1.
    
    Follow-up to [54508], [54509].
    
    Reviewed by desrosj, SergeyBiryukov.
    Merges [54724] to the 6.1 branch.
    See #56817.
    Built from https://develop.svn.wordpress.org/branches/6.1@54732
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.1@54284 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1-RC5-54731';
+$wp_version = '6.1-RC5-54732';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
