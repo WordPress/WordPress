97468065ef727d6bd9c3bd1d36e0b9ba0be03867
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-11-02 19:52:18 +0000

    Docs: Add brackets to a function name in `get_page_template()` description.
    This ensures the `locate_block_template()` function name is correctly linked in the Developer Reference.
    
    Follow-up to [54179].
    
    See #56792.
    Built from https://develop.svn.wordpress.org/trunk@54747
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54299 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/template.php
+++ wp-includes/template.php
@@ -437,7 +437,7 @@ function get_privacy_policy_template() {
 /**
  * Retrieves path of page template in current or parent template.
  *
- * Note: For block themes, use locate_block_template function instead.
+ * Note: For block themes, use locate_block_template() function instead.
  *
  * The hierarchy for this template looks like:
  *
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54745';
+$wp_version = '6.2-alpha-54747';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
