854b4c8a6add88d5d5e4783ff9af96d5c759d822
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-11-06 16:21:11 +0000

    Tests: Move `update_blog_status()` tests to their own file.
    Reduce some of the clutter in `tests/multisite/site.php` and introduce `tests/multisite/updateBlogStatus.php`. Tests moved over are verbatim at this point.
    
    Follow-up to [1078/tests], [29916], [30784], [30785], [33253].
    
    See #56793.
    Built from https://develop.svn.wordpress.org/trunk@54757
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54309 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54756';
+$wp_version = '6.2-alpha-54757';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
