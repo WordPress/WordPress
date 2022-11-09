e988ef55b77c4fbe80083ba8f51382dd82dca689
desrosj <desrosj@git.wordpress.org>
2022-11-09 16:32:11 +0000

    Themes: Improve `WP_Query` call getting global styles.
    Change `orderby` clause used within `WP_Theme_JSON_Resolver::get_user_data_from_wp_global_styles` to `date` to match the `WP_Query` documentation for the parameter.
    
    Props miguelaxcar, johnbillion, JeffPaul, spacedmonkey, mxbclang, mukesh27.
    Merges [54770] to the 6.1 branch.
    Fixes #56900.
    Built from https://develop.svn.wordpress.org/branches/6.1@54779
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.1@54331 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1.1-alpha-54778';
+$wp_version = '6.1.1-alpha-54779';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
