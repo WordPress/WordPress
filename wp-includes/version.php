912ac10f4b880d5fb51b8dcc98af8db49fb3d9f0
Peter Wilson <wilson@peterwilson.cc>
2022-11-11 01:27:10 +0000

    I18N: Initialize textdomain registry in `wp_load_translations_early()`.
    Initialize `WP_Textdomain_Registry` in `wp_load_translations_early()`. This ensures the global `$wp_textdomain_registry` is set up prior to loading the translations.
    
    Props azurseisme, TimothyBlynJacobs, costdev, ocean90, flixos90, swissspidy.
    Fixes #57051.
    
    
    Built from https://develop.svn.wordpress.org/trunk@54803
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54355 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54802';
+$wp_version = '6.2-alpha-54803';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
