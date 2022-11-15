6d319069b0281b85471bb6217aff739369762223
desrosj <desrosj@git.wordpress.org>
2022-11-15 18:45:15 +0000

    Help/About: Update the About page for 6.1.1.
    Props jeffpaul.
    Fixes #57119.
    Built from https://develop.svn.wordpress.org/branches/6.1@54843
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.1@54395 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -42,6 +42,37 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 			</nav>
 		</div>
 
+		<div class="about__section changelog">
+			<div class="column">
+				<h2><?php _e( 'Maintenance and Security Releases' ); ?></h2>
+				<p>
+					<?php
+					printf(
+						/* translators: 1: WordPress version number, 2: Plural number of bugs. Singular security issue. */
+						_n(
+							'<strong>Version %1$s</strong> addressed %2$s bug.',
+							'<strong>Version %1$s</strong> addressed %2$s bugs.',
+							50
+						),
+						'6.1.1',
+						'50'
+					);
+					?>
+					<?php
+					printf(
+						/* translators: %s: HelpHub URL. */
+						__( 'For more information, see <a href="%s">the release notes</a>.' ),
+						sprintf(
+							/* translators: %s: WordPress version. */
+							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							sanitize_title( '6.1.1' )
+						)
+					);
+					?>
+				</p>
+			</div>
+		</div>
+
 		<div class="about__section">
 			<div class="column">
 				<h2 class="aligncenter">
