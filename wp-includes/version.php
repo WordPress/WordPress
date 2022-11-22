abe134c2090e84080adc46187884201a4badd649
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-11-22 21:27:16 +0000

    Tests: Correct references to `set_up()` and `tear_down()` in various DocBlocks.
    The `setUp()` and `tearDown()` methods were renamed to `set_up()` and `tear_down()`, respectively, as part of implementing the `void` return type solution for PHPUnit 8.0.
    
    Follow-up to [29120], [29251], [30277], [32173], [32806], [38829], [42379], [50450], [51276], [51568].
    
    See #56793.
    Built from https://develop.svn.wordpress.org/trunk@54865
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54417 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54864';
+$wp_version = '6.2-alpha-54865';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
