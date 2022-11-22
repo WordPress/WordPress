1a3a3ee2501786b00187994f8bf2cb35d6b67de3
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-11-22 15:11:14 +0000

    Tests: Add a `public` visibility to `wp_filesize()` tests.
    Follow-up to [52010], [52837], [52932], [54861].
    
    See #57171.
    Built from https://develop.svn.wordpress.org/trunk@54862
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54414 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54861';
+$wp_version = '6.2-alpha-54862';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
