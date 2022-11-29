6436dd1fd4edef457d09cf930167f37c6ae582e2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-11-29 22:12:12 +0000

    Twenty Seventeen: Fix comment indentation in `twentyseventeen_setup()`.
    Follow-up to [38833], [42343]
    
    Props jrf.
    See #56791.
    Built from https://develop.svn.wordpress.org/trunk@54902
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54454 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyseventeen/functions.php
+++ wp-content/themes/twentyseventeen/functions.php
@@ -122,7 +122,7 @@ function twentyseventeen_setup() {
 	/*
 	 * This theme styles the visual editor to resemble the theme style,
 	 * specifically font, colors, and column width.
-	  */
+	 */
 	add_editor_style( array( 'assets/css/editor-style.css', twentyseventeen_fonts_url() ) );
 
 	// Load regular editor styles into the new block-based editor.
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54897';
+$wp_version = '6.2-alpha-54902';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
