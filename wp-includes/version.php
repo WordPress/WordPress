ab923804f47bd00f2d51fb646c369a91277dd38b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-11-29 12:42:12 +0000

    Coding Standards: Add visibility to properties in `tests/phpunit/tests/`.
    Adds a `private` visibility to some test class properties where it was not explicitly specified. This continues the previous efforts to make sure visibility is declared on all properties. 
    
    Note: This will be enforced by WPCS 3.0.0.
    
    Follow-up to [49184], [51919], [52009], [52010], [54889].
    
    Props jrf.
    See #56791.
    Built from https://develop.svn.wordpress.org/trunk@54890
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54442 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54889';
+$wp_version = '6.2-alpha-54890';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
