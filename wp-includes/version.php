767bdad5162ef4a93c325351d2e23bd4799182a5
Peter Wilson <wilson@peterwilson.cc>
2022-11-30 22:54:12 +0000

    Security: Improve accessibility of security policy.
    Replace emoji indicators of support with "yes" and "no". This prevents "White Heavy Check Mark" indicating supported versions for screen-reader users.
    
    Props audrasjb.
    Fixes #57222.
    
    
    Built from https://develop.svn.wordpress.org/trunk@54919
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54471 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54918';
+$wp_version = '6.2-alpha-54919';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
