c0a3701246f787434271b9be73af862a435b9fa1
Peter Wilson <wilson@peterwilson.cc>
2022-11-30 22:33:14 +0000

    Security: Update supported WordPress versions in security policy.
    Removes WordPress versions 3.7.x-4.0.x from the list of supported versions in the security policy.
    
    Props audrasjb, peterwilsoncc.
    Fixes #57217.
    
    
    Built from https://develop.svn.wordpress.org/trunk@54918
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54470 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54917';
+$wp_version = '6.2-alpha-54918';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
