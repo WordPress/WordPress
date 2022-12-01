1e63fe0f2c24707b82f66912971566a3692d44d1
audrasjb <audrasjb@git.wordpress.org>
2022-12-01 22:08:12 +0000

    Twenty Twenty: Remove unwanted `title` attributes.
    This changeset removes `title` attributes from various links, as they add redundant information.
    
    Follow-up to [53844].
    
    Props sabernhardt, costdev, mukesh27.
    See #57199, #24766, #24203.
    
    Built from https://develop.svn.wordpress.org/trunk@54925
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54477 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54924';
+$wp_version = '6.2-alpha-54925';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
