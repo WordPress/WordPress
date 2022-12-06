aec68c417ec3c0cb31ac8657011027bb94bad2b9
spacedmonkey <spacedmonkey@git.wordpress.org>
2022-12-06 13:54:15 +0000

    Users: Clear the user_meta cache when `clean_user_cache` function is called.
    Unlike other clean cache functions like `clean_post_cache`, `clean_user_cache` did not also clear user meta caches. This is inconsistent and can result in some strange side effects. Update the `clean_user_cache` function to also clear user meta caches when called. 
    
    Props dd32, spacedmonkey, peterwilsoncc.
    Fixes #54316.
    Built from https://develop.svn.wordpress.org/trunk@54940
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54492 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -1873,6 +1873,7 @@ function update_user_caches( $user ) {
  *
  * @since 3.0.0
  * @since 4.4.0 'clean_user_cache' action was added.
+ * @since 6.2.0 User metadata caches are now cleared.
  *
  * @param WP_User|int $user User object or ID to be cleaned from the cache
  */
@@ -1892,6 +1893,8 @@ function clean_user_cache( $user ) {
 	if ( ! empty( $user->user_email ) ) {
 		wp_cache_delete( $user->user_email, 'useremail' );
 	}
+
+	wp_cache_delete( $user->ID, 'user_meta' );
 
 	/**
 	 * Fires immediately after the given user's cache is cleaned.
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54939';
+$wp_version = '6.2-alpha-54940';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
