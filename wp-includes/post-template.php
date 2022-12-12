3d29300173c79d629db105db798842e136a2a457
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-12-12 11:17:17 +0000

    Docs: Improve DocBlock formatting for `get_post_class()`.
    Follow-up to [9273], [27429], [31271], [37544].
    
    See #56792.
    Built from https://develop.svn.wordpress.org/trunk@54957
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54509 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post-template.php
+++ wp-includes/post-template.php
@@ -465,16 +465,18 @@ function post_class( $css_class = '', $post = null ) {
 /**
  * Retrieves an array of the class names for the post container element.
  *
- * The class names are many. If the post is a sticky, then the 'sticky'
- * class name. The class 'hentry' is always added to each post. If the post has a
- * post thumbnail, 'has-post-thumbnail' is added as a class. For each taxonomy that
- * the post belongs to, a class will be added of the format '{$taxonomy}-{$slug}' -
- * eg 'category-foo' or 'my_custom_taxonomy-bar'.
+ * The class names are many:
  *
- * The 'post_tag' taxonomy is a special
- * case; the class has the 'tag-' prefix instead of 'post_tag-'. All class names are
- * passed through the filter, {@see 'post_class'}, with the list of class names, followed by
- * $css_class parameter value, with the post ID as the last parameter.
+ *  - If the post has a post thumbnail, `has-post-thumbnail` is added as a class.
+ *  - If the post is sticky, then the `sticky` class name is added.
+ *  - The class `hentry` is always added to each post.
+ *  - For each taxonomy that the post belongs to, a class will be added of the format
+ *    `{$taxonomy}-{$slug}`, e.g. `category-foo` or `my_custom_taxonomy-bar`.
+ *    The `post_tag` taxonomy is a special case; the class has the `tag-` prefix
+ *    instead of `post_tag-`.
+ *
+ * All class names are passed through the filter, {@see 'post_class'}, followed by
+ * `$css_class` parameter value, with the post ID as the last parameter.
  *
  * @since 2.7.0
  * @since 4.2.0 Custom taxonomy class names were added.
