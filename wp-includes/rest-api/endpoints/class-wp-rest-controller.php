e623438f68cec0593a7514473d7fa5f00611ea1e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-12-14 13:41:13 +0000

    Code Modernization: Rename parameters that use reserved keywords in `wp-includes/rest-api/endpoints/class-wp-rest-controller.php`.
    While using reserved PHP keywords as parameter name labels is allowed, in the context of function calls using named parameters in PHP 8.0+, this will easily lead to confusion. To avoid that, it is recommended not to use reserved keywords as function parameter names.
    
    This commit:
    * Renames `$object` to `$data_object` in `WP_REST_Controller::update_additional_fields_for_object()`.
    * Includes a minor code layout fix for better readability.
    
    Follow-up to [52946], [52996], [52997], [52998], [53003], [53014], [53029], [53039], [53116], [53117], [53137], [53174], [53184], [53185], [53192], [53193], [53198], [53203], [53207], [53215], [53216], [53220], [53230], [53232], [53236], [53239], [53240], [53242], [53243], [53245], [53246], [53257], [53269], [53270], [53271], [53272], [53273], [53274], [53275], [53276], [53277], [53281], [53283], [53284], [53285], [53287], [53364], [53365], [54927], [54929], [54930], [54931], [54932], [54933], [54938], [54943], [54944], [54945], [54946], [54947], [54948], [54950], [54951], [54952], [54956], [54959], [54960], [54961], [54962], [54964], [54965].
    
    Props jrf, aristath, poena, justinahinon, SergeyBiryukov.
    See #56788.
    Built from https://develop.svn.wordpress.org/trunk@54969
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54521 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/endpoints/class-wp-rest-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-controller.php
@@ -431,7 +431,13 @@ abstract class WP_REST_Controller {
 				continue;
 			}
 
-			$prepared[ $field_name ] = call_user_func( $field_options['get_callback'], $prepared, $field_name, $request, $this->get_object_type() );
+			$prepared[ $field_name ] = call_user_func(
+				$field_options['get_callback'],
+				$prepared,
+				$field_name,
+				$request,
+				$this->get_object_type()
+			);
 		}
 
 		return $prepared;
@@ -442,11 +448,11 @@ abstract class WP_REST_Controller {
 	 *
 	 * @since 4.7.0
 	 *
-	 * @param object          $object  Data model like WP_Term or WP_Post.
-	 * @param WP_REST_Request $request Full details about the request.
+	 * @param object          $data_object Data model like WP_Term or WP_Post.
+	 * @param WP_REST_Request $request     Full details about the request.
 	 * @return true|WP_Error True on success, WP_Error object if a field cannot be updated.
 	 */
-	protected function update_additional_fields_for_object( $object, $request ) {
+	protected function update_additional_fields_for_object( $data_object, $request ) {
 		$additional_fields = $this->get_additional_fields();
 
 		foreach ( $additional_fields as $field_name => $field_options ) {
@@ -459,7 +465,14 @@ abstract class WP_REST_Controller {
 				continue;
 			}
 
-			$result = call_user_func( $field_options['update_callback'], $request[ $field_name ], $object, $field_name, $request, $this->get_object_type() );
+			$result = call_user_func(
+				$field_options['update_callback'],
+				$request[ $field_name ],
+				$data_object,
+				$field_name,
+				$request,
+				$this->get_object_type()
+			);
 
 			if ( is_wp_error( $result ) ) {
 				return $result;
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54968';
+$wp_version = '6.2-alpha-54969';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
