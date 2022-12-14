95c797463dc92748293246a7adce83026caf2dfe
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2022-12-14 14:31:11 +0000

    Code Modernization: Rename parameters that use reserved keywords in `wp-includes/rest-api/endpoints/class-wp-rest-widgets-controller.php`.
    While using reserved PHP keywords as parameter name labels is allowed, in the context of function calls using named parameters in PHP 8.0+, this will easily lead to confusion. To avoid that, it is recommended not to use reserved keywords as function parameter names.
    
    This commit renames the `$string` parameter to `$form_data` in a closure in `WP_REST_Widgets_Controller::get_item_schema()`.
    
    Follow-up to [52946], [52996], [52997], [52998], [53003], [53014], [53029], [53039], [53116], [53117], [53137], [53174], [53184], [53185], [53192], [53193], [53198], [53203], [53207], [53215], [53216], [53220], [53230], [53232], [53236], [53239], [53240], [53242], [53243], [53245], [53246], [53257], [53269], [53270], [53271], [53272], [53273], [53274], [53275], [53276], [53277], [53281], [53283], [53284], [53285], [53287], [53364], [53365], [54927], [54929], [54930], [54931], [54932], [54933], [54938], [54943], [54944], [54945], [54946], [54947], [54948], [54950], [54951], [54952], [54956], [54959], [54960], [54961], [54962], [54964], [54965], [54969], [54970], [54971].
    
    Props jrf, aristath, poena, justinahinon, SergeyBiryukov.
    See #56788.
    Built from https://develop.svn.wordpress.org/trunk@54972
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54524 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/endpoints/class-wp-rest-widgets-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-widgets-controller.php
@@ -861,9 +861,9 @@ class WP_REST_Widgets_Controller extends WP_REST_Controller {
 					'type'        => 'string',
 					'context'     => array(),
 					'arg_options' => array(
-						'sanitize_callback' => static function( $string ) {
+						'sanitize_callback' => static function( $form_data ) {
 							$array = array();
-							wp_parse_str( $string, $array );
+							wp_parse_str( $form_data, $array );
 							return $array;
 						},
 					),
