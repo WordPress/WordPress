8399a3a160c45ec6fc6d8f8d568308e453eee1cd
desrosj <desrosj@git.wordpress.org>
2022-12-14 00:24:18 +0000

    Build/Test Tools: Run Xdebug tests on PHP 8.2.
    Xdebug 3.2.0 (which adds support for PHP 8.2) has been released and is now included in the PHP 8.2 Docker container.
    
    The tests in the `xdebug` group can now be run against all PHP versions currently supported by WordPress.
    
    See https://github.com/WordPress/wpdev-docker-images/pull/92.
    
    See #56009.
    Built from https://develop.svn.wordpress.org/trunk@54967
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54519 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54965';
+$wp_version = '6.2-alpha-54967';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
