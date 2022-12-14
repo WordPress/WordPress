fdb5ee0291d1411b9925aa6fbd54eae1897b054b
Peter Wilson <wilson@peterwilson.cc>
2022-12-14 01:28:12 +0000

    Built/Test tools, HTTP API: Refactor test for multiple location headers.
    Remove wordpress.org as an external dependency testing `WP_HTTP::handle_redirects()`.
    
    This refactors and reenables an existing test to call the `WP_HTTP::handle_redirects()` method directly with a mocked array of HTTP headers containing multiple location headers.
    
    The test is moved from the external-http group to the http test group as it no longer makes an HTTP request.
    
    Follow up to [54955].
    
    Props SergeyBiryukov, dd32, peterwilsoncc.
    Fixes #57306.
    See #56793.
    
    
    Built from https://develop.svn.wordpress.org/trunk@54968
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54520 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54967';
+$wp_version = '6.2-alpha-54968';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
