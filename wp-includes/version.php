72948dee1eceeede921dff78e2295fd2fcd6d8af
spacedmonkey <spacedmonkey@git.wordpress.org>
2022-12-15 22:55:16 +0000

    Themes: Improve performance of `_add_block_template_part_area_info` and `_add_block_template_info` functions.
    Improve performance of `_add_block_template_part_area_info` and `_add_block_template_info` function by passing the `with_supports` parameter to the `WP_Theme_JSON_Resolver::get_theme_data` method. This results in hitting an existing cache and in many less calls to get_option. 
    
    Props spacedmonkey, flixos90, SergeyBiryukov, peterwilsoncc, desrosj.
    Fixes #57077.
    Built from https://develop.svn.wordpress.org/trunk@54998
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54531 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-54997';
+$wp_version = '6.2-alpha-54998';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
