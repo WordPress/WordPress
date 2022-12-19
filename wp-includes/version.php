eff9f72714475579d7e18f483a0d0fd4068aa0f2
hellofromTonya <hellofromTonya@git.wordpress.org>
2022-12-19 15:12:11 +0000

    HTTP API: Adds BC-layer `/library/Requests.php` file.
    When the Requests 2.0.0 [https://github.com/WordPress/Requests/blob/7ef0774f0bf32dddb7b6c1dd01e4d285c2ba3c3f/src/Autoload.php#L141-L143 autoloader detects the older (deprecated) `Requests` class], it attempts to load [https://github.com/WordPress/Requests/blob/v2.0.0/library/Requests.php its `/library/Requests.php` file]. Prior to this commit, this file did not exist in Core. Thus, a fatal error happened.
    
    Why not change Requests Autoloader?
    Requests is an external dependency that Core consumes. It is also used by other projects outside of Core. Thus, Core needs a fix to guard itself to prevent a fatal error.
    
    The fix:
    * Adds the missing `wp-includes/Requests/library/Requests.php` file, which then loads the `wp-includes/class-requests.php` (which will throw a deprecation notice to alert developers to upgrade).
    * Adds a test.
    
    Follow-up to [54997].
    
    Props bjorsch, costdev, jrf, mukesh27, peterwilsoncc, ironprogrammer, hellofromTonya.
    Fixes #57341.
    Built from https://develop.svn.wordpress.org/trunk@55007
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54540 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-55006';
+$wp_version = '6.2-alpha-55007';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
