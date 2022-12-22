f25373720c2dbd3d4adc17990228a055109aea43
Peter Wilson <wilson@peterwilson.cc>
2022-12-22 03:25:30 +0000

    External Libraries: Update jQuery to 3.6.3.
    Update the jQuery library from 3.6.1 to 3.6.3.
    
    * [https://blog.jquery.com/2022/12/13/jquery-3-6-2-released/ 3.6.2 release announcement]
    * [https://blog.jquery.com/2022/12/20/jquery-3-6-3-released-a-quick-selector-fix/ 3.6.3 release announcement]
    * [https://github.com/jquery/jquery/compare/3.6.1...3.6.3 Full changelog]
    
    Props TobiasBg, naeemhaque, mukesh27, mgol.
    Fixes #57324.
    
    Built from https://develop.svn.wordpress.org/trunk@55012
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54545 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -820,8 +820,8 @@ function wp_default_scripts( $scripts ) {
 
 	// jQuery.
 	// The unminified jquery.js and jquery-migrate.js are included to facilitate debugging.
-	$scripts->add( 'jquery', false, array( 'jquery-core', 'jquery-migrate' ), '3.6.1' );
-	$scripts->add( 'jquery-core', "/wp-includes/js/jquery/jquery$suffix.js", array(), '3.6.1' );
+	$scripts->add( 'jquery', false, array( 'jquery-core', 'jquery-migrate' ), '3.6.3' );
+	$scripts->add( 'jquery-core', "/wp-includes/js/jquery/jquery$suffix.js", array(), '3.6.3' );
 	$scripts->add( 'jquery-migrate', "/wp-includes/js/jquery/jquery-migrate$suffix.js", array(), '3.3.2' );
 
 	// Full jQuery UI.
