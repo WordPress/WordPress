2c0a6fefae248ec72fd46a1e4e13a73d9f02d53f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-01-12 00:19:12 +0000

    Upgrade/Install: Revert a temporary conditional for testing the Rollbacks feature project.
    The [https://make.wordpress.org/core/2021/02/19/feature-plugin-rollback-update-failure/ Rollback Update Failure feature project] has been split into two plugins for testing:
     * [https://github.com/afragen/faster-updates Faster Updates] speeds up plugin or theme updates by moving files rather than copying them, thus decreasing the memory usage and reducing the chance of timeouts or running out of disk space during updates.
     * [https://wordpress.org/plugins/rollback-update-failure/ Rollback Update Failure] creates a temporary backup of plugins and themes before updating. This aims to make the update process more reliable and ensure that if a plugin or theme update fails, the previous version can be safely restored.
    
    The current priority of the feature project is to test the new `move_dir()` function, which offers better performance than `copy_dir()`. Instead of copying a directory in a recursive manner file by file from one location to another, `move_dir()` uses the `rename()` PHP function to speed up the process, which is instrumental in updating large plugins without a delay. If the renaming failed, it falls back to the `copy_dir()` WP function.
    
    The `move_dir()` function is self-contained in the Faster Updates plugin and does not require any special hooks in core, so the conditional previously added to `WP_Upgrader::install_package()` to facilitate testing is no longer needed and can be removed.
    
    Follow-up to [53578], [54484], [54643].
    
    Props afragen, costdev, peterwilsoncc.
    See #56057, #57375, #57386.
    Built from https://develop.svn.wordpress.org/trunk@55055
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54588 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-55054';
+$wp_version = '6.2-alpha-55055';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
