db9b22b05185156d82009dc7df290e39e460f496
audrasjb <audrasjb@git.wordpress.org>
2023-01-18 14:52:18 +0000

    Twenty Twenty-One: Revert [55088].
    [55088] introduced some issues with the "Test Default Themes & Create ZIPs" workflow on GitHub Actions. This changeset reverts it to leave time to figure out what failed and why.
    
    See #53220.
    
    Built from https://develop.svn.wordpress.org/trunk@55089
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54622 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-55088';
+$wp_version = '6.2-alpha-55089';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
