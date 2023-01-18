69f0cb5e3faf22e3200c2e8b62b94d521f929275
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-01-18 13:00:15 +0000

    Tests: Add unique messages to assertions for attachment filenames in `wp_mail()`.
    This makes the assertions more helpful, as per the [https://make.wordpress.org/core/handbook/testing/automated-testing/writing-phpunit-tests/#using-assertions Writing PHP Tests] guidelines:
    > All PHPUnit assertions, as well as all WordPress custom assertions, allow for a `$message` parameter to be passed. This message will be displayed when the assertion fails and can help immensely when debugging a test. This parameter should always be used if more than one assertion is used in a test method.
    
    Follow-up to [55030], [55032].
    
    Props mukesh27, costdev.
    See #28407.
    Built from https://develop.svn.wordpress.org/trunk@55087
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54620 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-55086';
+$wp_version = '6.2-alpha-55087';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
