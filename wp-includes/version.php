a1b4d027f3cc3c06a91605405a402382fe11a71f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-01-19 21:48:17 +0000

    Tests: Correct `additional_field_get_callback()` parameters in some REST API tests.
    The second parameter passed to `get_callback` in `WP_REST_Controller::add_additional_fields_to_object()` is the field name, not the request details.
    
    Includes moving the `get_callback` and `update_callback` helper functions next to the tests they are used in.
    
    Follow-up to [38832], [43768].
    
    See #56793.
    Built from https://develop.svn.wordpress.org/trunk@55102
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54635 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-55101';
+$wp_version = '6.2-alpha-55102';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
