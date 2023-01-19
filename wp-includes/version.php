26d9268e3c674bfd40721428805b8df817045076
audrasjb <audrasjb@git.wordpress.org>
2023-01-19 22:57:14 +0000

    Permalinks: Remove floating on Permalinks settings screen.
    This changeset replaces floating elements with `flex` CSS positioning, while the markup stays the same to keep the elements' semantic value.
    
    Follow-up to [53706].
    
    Props sabernhardt, kebbet, audrasjb.
    Fixes #56673.
    See #55498.
    
    Built from https://develop.svn.wordpress.org/trunk@55103
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54636 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-55102';
+$wp_version = '6.2-alpha-55103';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
