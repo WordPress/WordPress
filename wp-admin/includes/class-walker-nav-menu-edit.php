a0b0f4560b5c142b5a1a14e26628a28d882bfefe
audrasjb <audrasjb@git.wordpress.org>
2023-01-24 21:32:14 +0000

    Menus: Add missing escaping function in Admin Menu walker.
    Props aniketpatel, mukesh27, costdev.
    Fixes #57541.
    
    Built from https://develop.svn.wordpress.org/trunk@55135
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54668 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-walker-nav-menu-edit.php
+++ wp-admin/includes/class-walker-nav-menu-edit.php
@@ -180,7 +180,7 @@ class Walker_Nav_Menu_Edit extends Walker_Nav_Menu {
 						printf(
 							'<a class="item-edit" id="edit-%s" href="%s" aria-label="%s"><span class="screen-reader-text">%s</span></a>',
 							$item_id,
-							$edit_url,
+							esc_url( $edit_url ),
 							esc_attr__( 'Edit menu item' ),
 							__( 'Edit' )
 						);
