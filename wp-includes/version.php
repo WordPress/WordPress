39aa80cb875067cac739fc694e59c5f8c80d5d92
Peter Wilson <wilson@peterwilson.cc>
2023-02-03 04:04:15 +0000

    Posts, Post Types: Add test coverage for `_truncate_post_slug()`.
    Props xknown, mukesh27, costdev, audrasjb.
    Fixes #56868.
    See #56793.
    
    Built from https://develop.svn.wordpress.org/trunk@55208
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54741 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-55207';
+$wp_version = '6.2-alpha-55208';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
