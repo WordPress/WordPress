e235883c4d99ad5f2bf194e1feb35208b053b472
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-02-03 14:41:26 +0000

    HTTP API: Restore one instance of the `X-Pingback` header capitalization.
    The revert in the previous commit appears to be accidental.
    
    Follow-up to [55210], [55211].
    
    See #54225.
    Built from https://develop.svn.wordpress.org/trunk@55212
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54745 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/upgrade.php
+++ wp-admin/includes/upgrade.php
@@ -506,13 +506,13 @@ function wp_install_maybe_enable_pretty_permalinks() {
 
 		/*
 		 * Send a request to the site, and check whether
-		 * the 'x-pingback' header is returned as expected.
+		 * the 'X-Pingback' header is returned as expected.
 		 *
 		 * Uses wp_remote_get() instead of wp_remote_head() because web servers
 		 * can block head requests.
 		 */
 		$response          = wp_remote_get( $test_url, array( 'timeout' => 5 ) );
-		$x_pingback_header = wp_remote_retrieve_header( $response, 'x-pingback' );
+		$x_pingback_header = wp_remote_retrieve_header( $response, 'X-Pingback' );
 		$pretty_permalinks = $x_pingback_header && get_bloginfo( 'pingback_url' ) === $x_pingback_header;
 
 		if ( $pretty_permalinks ) {
