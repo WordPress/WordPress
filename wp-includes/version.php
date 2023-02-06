b15e5308285fdd81687d8655d2128b3d8def65c3
audrasjb <audrasjb@git.wordpress.org>
2023-02-06 08:26:21 +0000

    Twenty Sixteen: Fix Gallery block images alignement.
    This changeset fixes an alignment issue that has been present since the gallery block markup was changed in WordPress 6.0.
    
    Props silicium23, zoonini, sabernhardt, bgoewert, ira2910, tahminar27.
    Fixes #56412.
    
    Built from https://develop.svn.wordpress.org/trunk@55227
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54760 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-55226';
+$wp_version = '6.2-alpha-55227';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
