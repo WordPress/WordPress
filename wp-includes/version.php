c1e29c4da65a7b6076bc000cb6c7688bac4dd88d
desrosj <desrosj@git.wordpress.org>
2023-02-06 18:44:30 +0000

    Build/Test Tools: Update `json2php` package.
    This updates the `json2php` package to the latest version, which is currently `0.0.7`.
    
    See #57535.
    Built from https://develop.svn.wordpress.org/trunk@55232
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54765 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-alpha-55231';
+$wp_version = '6.2-alpha-55232';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
