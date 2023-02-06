be3d8fdcc3e9009d7aee233e00e529e11ae6baed
Peter Wilson <wilson@peterwilson.cc>
2023-02-06 04:21:20 +0000

    Filesystem API: Prevent fatal error in `move_dir()`.
    Correctly instantiate `WP_Error()` within `move_dir()` to prevent a fatal error when unable to delete an existing directory that is intended to be replaced.
    
    Follow-up to [55204], [55219], [55220], [55223].
    
    Props swissspidy, costdev, afragen.
    Fixes #57375.
    
    
    Built from https://develop.svn.wordpress.org/trunk@55226
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54759 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/file.php
+++ wp-admin/includes/file.php
@@ -1979,7 +1979,7 @@ function move_dir( $from, $to, $overwrite = false ) {
 			return new WP_Error( 'destination_already_exists_move_dir', __( 'The destination folder already exists.' ), $to );
 		} elseif ( ! $wp_filesystem->delete( $to, true ) ) {
 			// Can't overwrite if the destination couldn't be deleted.
-			return WP_Error( 'destination_not_deleted_move_dir', __( 'The destination directory already exists and could not be removed.' ) );
+			return new WP_Error( 'destination_not_deleted_move_dir', __( 'The destination directory already exists and could not be removed.' ) );
 		}
 	}
 
