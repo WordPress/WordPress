42d571decb7dcac584a28b78682deae759cdfc93
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-02-11 12:43:22 +0000

    Mail: Check that `set_time_limit()` function is available before using it in `class-pop3.php`.
    This avoids a fatal error if the function is disabled on certain environments.
    
    Follow-up to [55258].
    
    Props mujuonly.
    Fixes #57659.
    Built from https://develop.svn.wordpress.org/trunk@55305
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54838 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-pop3.php
+++ wp-includes/class-pop3.php
@@ -59,7 +59,9 @@ class POP3 {
         if(!empty($timeout)) {
             settype($timeout,"integer");
             $this->TIMEOUT = $timeout;
-            set_time_limit($timeout);
+            if(function_exists("set_time_limit")){
+                set_time_limit($timeout);
+            }
         }
         return true;
     }
@@ -72,7 +74,9 @@ class POP3 {
 	}
 
     function update_timer () {
-        set_time_limit($this->TIMEOUT);
+        if(function_exists("set_time_limit")){
+            set_time_limit($this->TIMEOUT);
+        }
         return true;
     }
 
