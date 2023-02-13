a67d0acee70bb648a60e248c23965e8319ba79f7
audrasjb <audrasjb@git.wordpress.org>
2023-02-13 09:21:18 +0000

    Docs: Use third-person singular verbs for `WP_Meta_Query` related function descriptions, as per docblocks standards.
    See #56792.
    
    Built from https://develop.svn.wordpress.org/trunk@55311
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54844 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-meta-query.php
+++ wp-includes/class-wp-meta-query.php
@@ -180,7 +180,7 @@ class WP_Meta_Query {
 	}
 
 	/**
-	 * Ensure the 'meta_query' argument passed to the class constructor is well-formed.
+	 * Ensures the 'meta_query' argument passed to the class constructor is well-formed.
 	 *
 	 * Eliminates empty items and ensures that a 'relation' is set.
 	 *
@@ -247,7 +247,7 @@ class WP_Meta_Query {
 	}
 
 	/**
-	 * Determine whether a query clause is first-order.
+	 * Determines whether a query clause is first-order.
 	 *
 	 * A first-order meta query clause is one that has either a 'key' or
 	 * a 'value' array key.
@@ -266,7 +266,7 @@ class WP_Meta_Query {
 	 *
 	 * @since 3.2.0
 	 *
-	 * @param array $qv The query variables
+	 * @param array $qv The query variables.
 	 */
 	public function parse_query_vars( $qv ) {
 		$meta_query = array();
@@ -309,7 +309,7 @@ class WP_Meta_Query {
 	}
 
 	/**
-	 * Return the appropriate alias for the given meta type if applicable.
+	 * Returns the appropriate alias for the given meta type if applicable.
 	 *
 	 * @since 3.7.0
 	 *
@@ -395,7 +395,7 @@ class WP_Meta_Query {
 	}
 
 	/**
-	 * Generate SQL clauses to be appended to a main query.
+	 * Generates SQL clauses to be appended to a main query.
 	 *
 	 * Called by the public WP_Meta_Query::get_sql(), this method is abstracted
 	 * out to maintain parity with the other Query classes.
@@ -425,7 +425,7 @@ class WP_Meta_Query {
 	}
 
 	/**
-	 * Generate SQL clauses for a single query array.
+	 * Generates SQL clauses for a single query array.
 	 *
 	 * If nested subqueries are found, this method recurses the tree to
 	 * produce the properly nested SQL.
@@ -509,7 +509,7 @@ class WP_Meta_Query {
 	}
 
 	/**
-	 * Generate SQL JOIN and WHERE clauses for a first-order query clause.
+	 * Generates SQL JOIN and WHERE clauses for a first-order query clause.
 	 *
 	 * "First-order" means that it's an array with a 'key' or 'value'.
 	 *
@@ -792,7 +792,7 @@ class WP_Meta_Query {
 	}
 
 	/**
-	 * Get a flattened list of sanitized meta clauses.
+	 * Gets a flattened list of sanitized meta clauses.
 	 *
 	 * This array should be used for clause lookup, as when the table alias and CAST type must be determined for
 	 * a value of 'orderby' corresponding to a meta clause.
@@ -806,7 +806,7 @@ class WP_Meta_Query {
 	}
 
 	/**
-	 * Identify an existing table alias that is compatible with the current
+	 * Identifies an existing table alias that is compatible with the current
 	 * query clause.
 	 *
 	 * We avoid unnecessary table joins by allowing each clause to look for
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-beta1-55310';
+$wp_version = '6.2-beta1-55311';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
