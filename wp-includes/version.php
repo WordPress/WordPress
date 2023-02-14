fb6c9598848ba368d395960e4ce0560894624d8b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-02-14 16:12:17 +0000

    Tests: Merge file-level and class-level DocBlocks in various unit test files.
    Per the [https://developer.wordpress.org/coding-standards/inline-documentation-standards/php/#6-file-headers documentation standards], whenever possible, all WordPress files should contain a header DocBlock, regardless of the file’s contents – this includes files containing classes.
    
    However, this recommendation makes less sense for unit test classes if not applied consistently, and the duplicate tags cause some confusion.
    
    This commit aims to reduce confusion and avoid repeating information by combining the DocBlocks.
    
    Follow-up to [40607], [43183], [43291], [43292], [43499], [43568], [44502], [44535], [44628], [44786], [44824], [44906], [44909], [46175].
    
    Props wojtekn, antonvlasenko, arafatjamil01, hztyfoon, mukesh27, costdev, jamilbd07, robinwpdeveloper, SergeyBiryukov.
    Fixes #57099.
    Built from https://develop.svn.wordpress.org/trunk@55337
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54870 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-beta1-55336';
+$wp_version = '6.2-beta1-55337';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
