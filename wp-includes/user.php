1968eaaf10863e54be9c22c996f7dab6acfb8237
Atanas Penchev <apenchev@users.noreply.github.com>
2023-02-16 14:18:44 +0200

    9 chars instead of 12 in wp_get_password_hint()

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -2833,7 +2833,7 @@ function _wp_get_user_contactmethods( $user = null ) {
  * @return string The password hint text.
  */
 function wp_get_password_hint() {
-	$hint = __( 'Hint: The password should be at least twelve characters long. To make it stronger, use upper and lower case letters, numbers, and symbols like ! " ? $ % ^ &amp; ).' );
+	$hint = __( 'Hint: The password should be at least nine characters long. To make it stronger, use upper and lower case letters, numbers, and symbols like ! " ? $ % ^ &amp; ).' );
 
 	/**
 	 * Filters the text describing the site's password complexity policy.
