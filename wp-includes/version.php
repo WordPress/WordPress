c54e29fd68ffa423c4cf64da100ae47e9712d5ac
Helen Hou-Sandí <helen.y.hou@gmail.com>
2023-02-16 03:50:19 +0000

    Build/Test Tools: Improve devcontainer setup for non-Codespaces usage
    This now runs in local Docker as well. Sets the executable propset to explicitly identify `install-tool.sh` and `setup.sh` as executable. Unclear if this will sync via the Git mirror; ideally it would as otherwise the two files show as modified in a Git clone even in a fresh container.
    
    Props samruddhikhandale.
    See #57187.
    
    Built from https://develop.svn.wordpress.org/trunk@55353
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54886 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-beta2-55352';
+$wp_version = '6.2-beta2-55353';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
