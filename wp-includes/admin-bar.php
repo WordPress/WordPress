1356f6537220ffdc32b9dad2a6cdbe2d010b7a88
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-02-23 12:34:18 +0000

    Toolbar: Update the URL for Documentation link in the admin bar.
    This avoids an extra redirect from `https://wordpress.org/support/` to `https://wordpress.org/documentation/`.
    
    Follow-up to [55412], [55413], [55414], [55415].
    
    See #57726.
    Built from https://develop.svn.wordpress.org/trunk@55416
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54949 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/admin-bar.php
+++ wp-includes/admin-bar.php
@@ -175,7 +175,7 @@ function wp_admin_bar_wp_menu( $wp_admin_bar ) {
 			'parent' => 'wp-logo-external',
 			'id'     => 'documentation',
 			'title'  => __( 'Documentation' ),
-			'href'   => __( 'https://wordpress.org/support/' ),
+			'href'   => __( 'https://wordpress.org/documentation/' ),
 		)
 	);
 
