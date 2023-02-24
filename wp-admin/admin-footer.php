fc5a4227ad9b3426c5e4df9fef9c430d1ce4f207
Peter Kavanagh <permpkin@users.noreply.github.com>
2023-02-24 12:03:59 +1100

    remove admin footer

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -20,53 +20,6 @@ global $hook_suffix;
 <div class="clear"></div></div><!-- wpbody-content -->
 <div class="clear"></div></div><!-- wpbody -->
 <div class="clear"></div></div><!-- wpcontent -->
-
-<div id="wpfooter" role="contentinfo">
-	<?php
-	/**
-	 * Fires after the opening tag for the admin footer.
-	 *
-	 * @since 2.5.0
-	 */
-	do_action( 'in_admin_footer' );
-	?>
-	<p id="footer-left" class="alignleft">
-		<?php
-		$text = sprintf(
-			/* translators: %s: https://wordpress.org/ */
-			__( 'Thank you for creating with <a href="%s">WordPress</a>.' ),
-			__( 'https://wordpress.org/' )
-		);
-
-		/**
-		 * Filters the "Thank you" text displayed in the admin footer.
-		 *
-		 * @since 2.8.0
-		 *
-		 * @param string $text The content that will be printed.
-		 */
-		echo apply_filters( 'admin_footer_text', '<span id="footer-thankyou">' . $text . '</span>' );
-		?>
-	</p>
-	<p id="footer-upgrade" class="alignright">
-		<?php
-		/**
-		 * Filters the version/update text displayed in the admin footer.
-		 *
-		 * WordPress prints the current version and update information,
-		 * using core_update_footer() at priority 10.
-		 *
-		 * @since 2.3.0
-		 *
-		 * @see core_update_footer()
-		 *
-		 * @param string $content The content that will be printed.
-		 */
-		echo apply_filters( 'update_footer', '' );
-		?>
-	</p>
-	<div class="clear"></div>
-</div>
 <?php
 /**
  * Prints scripts or data before the default footer scripts.
