933bb3999b1a26db025dd439fd0baecfeed12adc
audrasjb <audrasjb@git.wordpress.org>
2023-02-28 12:52:18 +0000

    Help/About: Avoid extra redirections on HelpHub Links.
    This changeset replaces various HelpHub links that have changed to avoid extra 301 redirections.
    
    Follow-up to [55412], [55413], [55414], [55415], [55416], [55431].
    
    Props sabernhardt, audrasjb.
    Fixes #57726.
    
    Built from https://develop.svn.wordpress.org/trunk@55432
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54965 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/customize.php
+++ wp-admin/customize.php
@@ -250,7 +250,7 @@ do_action( 'customize_controls_head' );
 						</p>
 						<p>
 							<?php
-							_e( '<a href="https://wordpress.org/documentation/article/appearance-customize-screen/">Documentation on Customizer</a>' );
+							_e( '<a href="https://wordpress.org/documentation/article/customizer/">Documentation on Customizer</a>' );
 							?>
 						</p>
 					</div>
--- wp-admin/theme-editor.php
+++ wp-admin/theme-editor.php
@@ -51,7 +51,7 @@ get_current_screen()->set_help_sidebar(
 	'<p><strong>' . __( 'For more information:' ) . '</strong></p>' .
 	'<p>' . __( '<a href="https://developer.wordpress.org/themes/">Documentation on Theme Development</a>' ) . '</p>' .
 	'<p>' . __( '<a href="https://wordpress.org/documentation/article/appearance-editor-screen/">Documentation on Editing Themes</a>' ) . '</p>' .
-	'<p>' . __( '<a href="https://wordpress.org/documentation/article/editing-files/">Documentation on Editing Files</a>' ) . '</p>' .
+	'<p>' . __( '<a href="https://wordpress.org/documentation/article/appearance-theme-file-editor-screen/">Documentation on Editing Files</a>' ) . '</p>' .
 	'<p>' . __( '<a href="https://developer.wordpress.org/themes/basics/template-tags/">Documentation on Template Tags</a>' ) . '</p>' .
 	'<p>' . __( '<a href="https://wordpress.org/support/forums/">Support forums</a>' ) . '</p>'
 );
