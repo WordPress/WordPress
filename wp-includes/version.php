0c81b800a4ee346046fe543820b76ba605f580da
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-03-01 15:58:24 +0000

    Tests: Ignore EOL differences in a `wpautop()` test for `<math>` block elements.
    Unix vs. Windows EOL style mismatches can cause misleading failures in tests using the heredoc syntax (`<<<`) or multiline strings as the expected result.
    
    This commit resolves a failure when running the test suite on Windows:
    {{{
    1) Tests_Formatting_wpAutop::test_skip_block_math_elements
    Failed asserting that two strings are identical.
    ...
    #Warning: Strings contain different line endings!
    }}}
    
    Follow-up to [55272].
    
    Props davidbaumwald, ignatggeorgiev, sakibmd.
    Fixes #57718.
    Built from https://develop.svn.wordpress.org/trunk@55445
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54978 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-beta3-55444';
+$wp_version = '6.2-beta3-55445';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
