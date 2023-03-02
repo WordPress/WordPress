06305406dfad83d90f864202aa0dedb16e558438
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-03-02 13:57:23 +0000

    Docs: Fix typo in `wp_generate_password()` description.
    Follow-up to [42373].
    
    Props pyrobd, pravinparmar2404, mukesh27.
    Fixes #57847.
    Built from https://develop.svn.wordpress.org/trunk@55455
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54988 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-beta4-55454';
+$wp_version = '6.2-beta4-55455';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
