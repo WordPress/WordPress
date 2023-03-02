2d2f24f6f8959640baf18db2280b8359b0c2a16c
hellofromTonya <hellofromTonya@git.wordpress.org>
2023-03-02 21:05:22 +0000

    Build/Test Tools: Add wp_check_filetype() unit tests.
    Adds a test class and data set for unit testing `wp_check_filetype()`.
    
    Props pbearne, costdev.
    Fixes #57151.
    Built from https://develop.svn.wordpress.org/trunk@55456
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54989 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-beta4-55455';
+$wp_version = '6.2-beta4-55456';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
