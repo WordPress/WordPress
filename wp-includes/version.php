53fcd4bacbf5bbabd03a3a018209c98eaa884255
Joe McGill <joemcgill@gmail.com>
2023-03-03 20:39:20 +0000

    Build/Test Tools: Add a performance measurement workflow.
    This adds a new GitHub Action workflow that measures a set of performance metrics on every commit, so we can track changes in the performance of WordPress over time and more easily identify changes that are responsible for significant performance improvements or regressions during development cycles.
    
    The workflow measures the homepage of a classic theme (Twenty Twenty-One) and a block theme (Twenty Twenty-Three) set up with demo content from the Theme Test Data project. Using the e2e testing framework, it makes 20 requests and records the median value of the following Server Timing metrics, generated by an mu-plugin installed as part of this workflow:
    
    - Total server response time
    - Server time before templates are loaded
    - Server time during template rendering
    
    In addition to measuring the performance metrics of the current commit, it also records performance metrics of a consistent version of WordPress (6.1.1) to be used as a baseline measurement in order to remove variance caused by the GitHub workers themselves from our reporting.
    
    The measurements are collected and displayed at https://www.codevitals.run/project/wordpress.
    
    Props adamsilverstein, mukesh27, flixos90, youknowriad, oandregal, desrosj, costdev, swissspidy.
    Fixes #57687.
    
    Built from https://develop.svn.wordpress.org/trunk@55459
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54992 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-beta4-55458';
+$wp_version = '6.2-beta4-55459';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
