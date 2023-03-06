37d90408975e1519a0293c01f2b9eea6b8532c5e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-03-06 12:41:23 +0000

    Tests: Adjust the expected mime type for WOFF fonts on PHP 8.1.12+.
    As of PHP 8.1.12, which includes libmagic/file update to version 5.42, the expected mime type for WOFF files has changed to `font/woff`, so the type needs to be adjusted accordingly in `wp_check_filetype_and_ext()` tests.
    
    References:
    * [https://github.com/php/php-src/issues/8805 php-src: #8805: finfo returns wrong mime type for woff/woff2 files]
    * [https://www.php.net/ChangeLog-8.php#8.1.12 PHP 8.1.12 changelog]
    
    Follow-up to [40124], [54508], [54509], [54724].
    
    Props costdev, SergeyBiryukov.
    Fixes #56817.
    Built from https://develop.svn.wordpress.org/trunk@55462
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@54995 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-beta4-55461';
+$wp_version = '6.2-beta4-55462';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
