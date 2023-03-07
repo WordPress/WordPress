cd5b78739a869f17c29b70f7862fc71cda4cccb2
desrosj <desrosj@git.wordpress.org>
2023-03-07 19:38:13 +0000

    Build/Test Tools: Backport updates to GitHub Actions.
    This backports several changesets to GitHub Actions workflows. These changesets:
    - address the deprecated notices related to `save-output` and `set-output` to ensure the workflows continue to run after these are removed.
    - adds support for automatically retrying a failed workflow once.
    - backports some Docker environment related tooling updates for the sake of consistency across branches.
    
    Merges [53628], [53636], [53736], [53737], [53895], [53940], [53947], [54039], [54096], [54108], [54293], [54297], [54313], [54342], [54343], [54371], [54373], [54511], [54649], [54650], [54674], [54678], [54750], [54851], [54852], [54856], [54921], [55152] to the 6.0 branch.
    See 55702, #52708, #55652, #55652, #55700, #55652, #56407, #56407, #56528, #55652, #55652, #55652, #54695, #55652, #55652, #55652, #55652, #56820, #56820, #56816, #55652, #55652, #56820, #56793, #56793, #53841, #57148, #57572, #56682.
    Built from https://develop.svn.wordpress.org/branches/6.0@55482
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.0@55015 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0.4-alpha-55372';
+$wp_version = '6.0.4-alpha-55482';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
