4711b88811e31f6b41e033cf70444e6fdafbac4e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-03-08 15:02:18 +0000

    Tests: Add a unit test for `register_block_style_handle()` with an RTL locale.
    The test ensures that the function loads RTL stylesheets when a locale with the right-to-left text direction is set.
    
    Follow-up to [54330].
    
    Props costdev, thomasplevy, audrasjb, mukesh27.
    Fixes #56797.
    Built from https://develop.svn.wordpress.org/trunk@55486
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55019 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-beta5-55485';
+$wp_version = '6.2-beta5-55486';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
