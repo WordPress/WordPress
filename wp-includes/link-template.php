91787c363ce16093e49a3431a3143fd357169944
audrasjb <audrasjb@git.wordpress.org>
2023-03-08 13:36:22 +0000

    Docs: Add missing 6.2.0 @since mention in `get_the_privacy_policy_link()`.
    Follow-up to #55261.
    
    See #56792, #56345.
    
    Built from https://develop.svn.wordpress.org/trunk@55485
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55018 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/link-template.php
+++ wp-includes/link-template.php
@@ -4679,6 +4679,7 @@ function the_privacy_policy_link( $before = '', $after = '' ) {
  * Returns the privacy policy link with formatting, when applicable.
  *
  * @since 4.9.6
+ * @since 6.2.0 Added 'privacy-policy' rel attribute.
  *
  * @param string $before Optional. Display before privacy policy link. Default empty.
  * @param string $after  Optional. Display after privacy policy link. Default empty.
