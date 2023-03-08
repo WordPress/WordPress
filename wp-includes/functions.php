abac5af424f19704691a6c5b5abcaae614784c76
audrasjb <audrasjb@git.wordpress.org>
2023-03-08 22:19:23 +0000

    Docs: Add security warning in `remove_query_arg()` docblock to make it consistent with `add_query_arg()`.
    Props roytanck.
    Fixes #57885.
    See #56792.
    
    Built from https://develop.svn.wordpress.org/trunk@55492
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55025 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -1196,6 +1196,10 @@ function add_query_arg( ...$args ) {
 /**
  * Removes an item or items from a query string.
  *
+ * Important: The return value of remove_query_arg() is not escaped by default. Output should be
+ * late-escaped with esc_url() or similar to help prevent vulnerability to cross-site scripting
+ * (XSS) attacks.
+ *
  * @since 1.5.0
  *
  * @param string|string[] $key   Query key or keys to remove.
