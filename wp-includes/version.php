db39c052a508885467b5a215bca10de9c12a0562
desrosj <desrosj@git.wordpress.org>
2023-03-08 16:42:11 +0000

    Build/Test Tools: Update several GitHub Actions.
    This updates three GitHub Actions to their latest versions:
    - `shivammathur/setup-php`
    - `actions/cache`
    - `wow-actions/welcome`
    
    The welcome action now uses the `GITHUB_TOKEN` by default, so it no longer needs to be passed manually.
    
    Merges [55487] to the 6.1 branch.
    See #57572.
    Built from https://develop.svn.wordpress.org/branches/6.1@55489
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.1@55022 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.1.2-alpha-55488';
+$wp_version = '6.1.2-alpha-55489';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
