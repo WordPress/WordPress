00803d3e5f17173ee143b20353c75f0f99ecc99b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-03-14 16:26:18 +0000

    Coding Standards: Use single quotes for strings without variables in `register_block_style_handle()`.
    This resolves two WPCS errors:
    {{{
    String ".css" does not require double quotes; use single quotes instead
    String "-rtl.css" does not require double quotes; use single quotes instead
    }}}
    Follow-up to [55544].
    
    See #57903.
    Built from https://develop.svn.wordpress.org/trunk@55547
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55059 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks.php
+++ wp-includes/blocks.php
@@ -263,7 +263,7 @@ function register_block_style_handle( $metadata, $field_name, $index = 0 ) {
 		if ( $is_core_block ) {
 			$rtl_file = str_replace( "{$suffix}.css", "-rtl{$suffix}.css", $style_path_norm );
 		} else {
-			$rtl_file = str_replace( ".css", "-rtl.css", $style_path_norm );
+			$rtl_file = str_replace( '.css', '-rtl.css', $style_path_norm );
 		}
 
 		if ( is_rtl() && file_exists( $rtl_file ) ) {
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-alpha-55545';
+$wp_version = '6.3-alpha-55547';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
