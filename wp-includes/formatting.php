4630e311e7d5d515d1581308b7fc0ebfcb466f8b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-03-19 12:53:17 +0000

    Tests: Improve documentation and variable names in some formatting tests.
    Includes documenting data provider values using hash notation in the tests for:
    * `convert_smilies()`
    * `get_url_in_content()`
    * `links_add_target()`
    * `normalize_whitespace()`
    
    Follow-up to [26191], [26327], [26328], [26972], [55562].
    
    See #57841.
    Built from https://develop.svn.wordpress.org/trunk@55563
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55075 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -5337,19 +5337,19 @@ function _links_add_base( $m ) {
 }
 
 /**
- * Adds a Target attribute to all links in passed content.
+ * Adds a target attribute to all links in passed content.
  *
  * This function by default only applies to `<a>` tags, however this can be
- * modified by the 3rd param.
+ * modified by the `$tags` parameter.
  *
- * *NOTE:* Any current target attributed will be stripped and replaced.
+ * *NOTE:* Any current target attribute will be stripped and replaced.
  *
  * @since 2.7.0
  *
  * @global string $_links_add_target
  *
  * @param string   $content String to search for links in.
- * @param string   $target  The Target to add to the links.
+ * @param string   $target  The target to add to the links.
  * @param string[] $tags    An array of tags to apply to.
  * @return string The processed content.
  */
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-alpha-55562';
+$wp_version = '6.3-alpha-55563';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
