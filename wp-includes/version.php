b4252fa186a1cffeba5073f5e573bf891020cfd6
audrasjb <audrasjb@git.wordpress.org>
2023-03-21 17:16:21 +0000

    WordPress 6.2 RC3.
    Built from https://develop.svn.wordpress.org/branches/6.2@55578
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.2@55090 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-RC2-55576';
+$wp_version = '6.2-RC3';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
