50d75e5e07d6b7257694b2eb091146e329d593db
audrasjb <audrasjb@git.wordpress.org>
2023-03-23 17:32:23 +0000

    Post WordPress 6.2 RC4 version bump.
    Built from https://develop.svn.wordpress.org/branches/6.2@55587
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.2@55099 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-RC4';
+$wp_version = '6.2-RC4-55587';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
