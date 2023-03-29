3492ba32698935c1d93c2865a9699e11c8697647
davidbaumwald <davidbaumwald@git.wordpress.org>
2023-03-29 17:46:20 +0000

    WordPress 6.2. Built from https://develop.svn.wordpress.org/branches/6.2@55609
    git-svn-id: http://core.svn.wordpress.org/branches/6.2@55121 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2-RC5-55605';
+$wp_version = '6.2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
