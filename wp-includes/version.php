2193d75aa142e2a7e848e3564261ebfb48d99915
audrasjb <audrasjb@git.wordpress.org>
2023-04-08 09:31:18 +0000

    Networks and Sites: Fix incorrect color for Theme enabling admin notices.
    This changeset replaces a `notice-updated` class with `notice-success` to fix an issue where the notices were using a gray border color instead of green when enabling or disabling a theme for a network.
    
    Follow-up to [55418].
    
    Props ocean90, audrasjb, marineevain, SergeyBiryukov, dhrumilk, chiragrathod103.
    Fixes #58096.
    
    Built from https://develop.svn.wordpress.org/trunk@55637
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55149 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-alpha-55633';
+$wp_version = '6.3-alpha-55637';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
