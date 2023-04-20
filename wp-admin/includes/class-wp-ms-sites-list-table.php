19d36bfdd2e2033ee282ab17da60c5f45a2eb794
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-04-20 14:30:19 +0000

    Networks and Sites: Simplify the check for main site on Network Admin → Sites screen.
    This replaces a site ID comparison when displaying action links in `WP_MS_Sites_List_Table::handle_row_actions()` with a dedicated function call, `is_main_site()`, for clarity.
    
    Follow-up to [13918], [25125], [26120], [32644], [38814].
    
    Props ecorica, spacedmonkey, SergeyBiryukov.
    Fixes #58150.
    Built from https://develop.svn.wordpress.org/trunk@55666
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55178 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-ms-sites-list-table.php
+++ wp-admin/includes/class-wp-ms-sites-list-table.php
@@ -732,7 +732,7 @@ class WP_MS_Sites_List_Table extends WP_List_Table {
 			__( 'Dashboard' )
 		);
 
-		if ( get_network()->site_id !== (int) $blog['blog_id'] ) {
+		if ( ! is_main_site( $blog['blog_id'] ) ) {
 			if ( '1' === $blog['deleted'] ) {
 				$actions['activate'] = sprintf(
 					'<a href="%1$s">%2$s</a>',
