229077cc3f5a81be38de6402b59a9c6b64786473
Peter Wilson <wilson@peterwilson.cc>
2023-04-24 05:27:22 +0000

    Security: Update GitHub security policy to refer to H1.
    Update the security policy displayed on GitHub, `SECURITY.md`, to refer visitors to the [https://hackerone.com/wordpress HackerOne WordPress program] for the full policy.
    
    This allows the project to maintain a single source of truth and avoid the potential for conflicting information across the two sites.
    
    Props desrosj, hellofromTonya, costdev.
    Merges [55670] to the 6.2 branch.
    Fixes #57937.
    
    Built from https://develop.svn.wordpress.org/branches/6.2@55679
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.2@55191 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2.1-alpha-55668';
+$wp_version = '6.2.1-alpha-55679';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
