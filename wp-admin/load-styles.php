27c4548f19a5affbf21777d7f18fee11e363d91d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-05-03 15:46:22 +0000

    General: Restore `strpos()` check in `wp-admin/load-styles.php`.
    This resolves a fatal error on PHP < 8.0, as `wp-includes/compat.php` is not loaded in this file, so `str_starts_with()` may not be available.
    
    Follow-up to [55703].
    
    Props dd32, flixos90, DigTek.
    Fixes #58244. See #58012.
    Built from https://develop.svn.wordpress.org/trunk@55710
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55222 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/load-styles.php
+++ wp-admin/load-styles.php
@@ -73,7 +73,8 @@ foreach ( $load as $handle ) {
 
 	$content = get_file( $path ) . "\n";
 
-	if ( str_starts_with( $style->src, '/' . WPINC . '/css/' ) ) {
+	// str_starts_with() is not used here, as wp-includes/compat.php is not loaded in this file.
+	if ( 0 === strpos( $style->src, '/' . WPINC . '/css/' ) ) {
 		$content = str_replace( '../images/', '../' . WPINC . '/images/', $content );
 		$content = str_replace( '../js/tinymce/', '../' . WPINC . '/js/tinymce/', $content );
 		$content = str_replace( '../fonts/', '../' . WPINC . '/fonts/', $content );
