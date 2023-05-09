18bf5b98aa0a3f6d2c46ee251c0ed9a0a1e37d35
audrasjb <audrasjb@git.wordpress.org>
2023-05-09 12:17:19 +0000

    Script Loader: Optimize performance of `_wp_normalize_relative_css_links()` by more than 2x.
    - Replace `preg_match_all()` and its secondary `str_replace()` call with `preg_replace_callback()`.
    - Fix case where paths beginning with `http` and `https` (but not `http:` and `https:`) were erroneously not counted as relative.
    - Improve code style and readability by consolidating conditions and returning once.
    - Use `str_starts_with()` consistently instead of `strpos()`.
    
    Follow-up to [52036], [52695], and [52754].
    
    Props westonruter, adamsilverstein, azaozz.
    Merges [55658] and [55669] to the 6.2 branch.
    Fixes #58069.
    See #54243.
    
    Built from https://develop.svn.wordpress.org/branches/6.2@55736
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.2@55248 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2.1-alpha-55735';
+$wp_version = '6.2.1-alpha-55736';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
