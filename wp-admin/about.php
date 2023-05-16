a941d591a8956d1d574f3409d536ae4ee1acc25f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-05-16 14:59:21 +0000

    Help/About: Update the About page for 6.2.1.
    Props audrasjb.
    Fixes #58248.
    Built from https://develop.svn.wordpress.org/branches/6.2@55770
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.2@55282 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -42,6 +42,37 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 			<a href="privacy.php" class="nav-tab"><?php _e( 'Privacy' ); ?></a>
 		</nav>
 
+		<div class="about__section changelog">
+			<div class="column">
+				<h2><?php _e( 'Maintenance and Security Releases' ); ?></h2>
+				<p>
+					<?php
+					printf(
+						/* translators: 1: WordPress version number, 2: Plural number of bugs. More than one security issue. */
+						_n(
+							'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bug.',
+							'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bugs.',
+							30
+						),
+						'6.2.1',
+						'30'
+					);
+					?>
+					<?php
+					printf(
+						/* translators: %s: HelpHub URL. */
+						__( 'For more information, see <a href="%s">the release notes</a>.' ),
+						sprintf(
+							/* translators: %s: WordPress version. */
+							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							sanitize_title( '6.2.1' )
+						)
+					);
+					?>
+				</p>
+			</div>
+		</div>
+
 		<div class="about__section">
 			<div class="column">
 				<h2>
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.2.1-RC1-55769';
+$wp_version = '6.2.1-RC1-55770';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
