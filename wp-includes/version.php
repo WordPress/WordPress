f0cc0bb7176b87263e4292fab3c1c9b3e0e5284b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-05-16 15:26:21 +0000

    Grouped backports to the 4.3 branch.
    - Media: Prevent CSRF setting attachment thumbnails.
    
    Merges [55764] to the 4.3 branch.
    Props dd32, isabel_brison, martinkrcho, matveb, ocean90, paulkevan, peterwilsoncc, timothyblynjacobs, xknown, youknowriad.
    Built from https://develop.svn.wordpress.org/branches/4.3@55776
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.3@55288 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3.30';
+$wp_version = '4.3.31';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
