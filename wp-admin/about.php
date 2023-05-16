7a6bb60778ee97726c96ba8381b6497b21fab0df
audrasjb <audrasjb@git.wordpress.org>
2023-05-16 15:20:13 +0000

    Grouped backports to the 6.0 branch.
    - Media: Prevent CSRF setting attachment thumbnails.
    - Embeds: Add protocol validation for WordPress Embed code.
    - I18N: Introduce sanitization function for locale.
    - Editor: Ensure block comments are of a valid form.
    - Editor: Remove shortcode support from block templates.
    
    Merges [55760-55764] to the 6.0 branch.
    Props dd32, isabel_brison, martinkrcho, matveb, ocean90, paulkevan, peterwilsoncc, timothyblynjacobs, xknown, youknowriad.
    
    
    Built from https://develop.svn.wordpress.org/branches/6.0@55773
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.0@55285 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -45,6 +45,26 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 					printf(
 						/* translators: %s: WordPress version number. */
 						__( '<strong>Version %s</strong> addressed some security issues.' ),
+						'6.0.4'
+					);
+					?>
+					<?php
+					printf(
+						/* translators: %s: HelpHub URL. */
+						__( 'For more information, see <a href="%s">the release notes</a>.' ),
+						sprintf(
+							/* translators: %s: WordPress version. */
+							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							sanitize_title( '6.0.4' )
+						)
+					);
+					?>
+				</p>
+				<p>
+					<?php
+					printf(
+						/* translators: %s: WordPress version number. */
+						__( '<strong>Version %s</strong> addressed some security issues.' ),
 						'6.0.3'
 					);
 					?>
