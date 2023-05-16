1da3b10799eb6616afa8810c8fc21383a61f2cc0
audrasjb <audrasjb@git.wordpress.org>
2023-05-16 14:48:19 +0000

    Embeds: Add protocol validation for WordPress Embed code.
    Validate that links within auto-discovered embeds are using the `http` or `https` protocols before following links.
    
    Props xknown, dd32, peterwilsoncc.
    Merges [55763] to branch 6.2.
    
    
    Built from https://develop.svn.wordpress.org/branches/6.2@55768
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.2@55280 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/wp-embed.js
+++ wp-includes/js/wp-embed.js
@@ -49,6 +49,7 @@
 
 		var iframes = document.querySelectorAll( 'iframe[data-secret="' + data.secret + '"]' ),
 			blockquotes = document.querySelectorAll( 'blockquote[data-secret="' + data.secret + '"]' ),
+			allowedProtocols = new RegExp( '^https?:$', 'i' ),
 			i, source, height, sourceURL, targetURL;
 
 		for ( i = 0; i < blockquotes.length; i++ ) {
@@ -83,6 +84,11 @@
 
 				sourceURL.href = source.getAttribute( 'src' );
 				targetURL.href = data.value;
+
+				/* Only follow link if the protocol is in the allow list. */
+				if ( ! allowedProtocols.test( targetURL.protocol ) ) {
+					continue;
+				}
 
 				/* Only continue if link hostname matches iframe's hostname. */
 				if ( targetURL.host === sourceURL.host ) {
