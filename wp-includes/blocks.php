49a67fb33a27518adf2f393a5e958ac37837ecda
audrasjb <audrasjb@git.wordpress.org>
2023-05-16 15:23:17 +0000

    Grouped backports to the 5.9 branch.
    - Media: Prevent CSRF setting attachment thumbnails.
    - Embeds: Add protocol validation for WordPress Embed code.
    - I18N: Introduce sanitization function for locale.
    - Editor: Ensure block comments are of a valid form.
    - Editor: Remove shortcode support from block templates.
    
    Merges [55760-55764] to the 5.9 branch.
    Props dd32, isabel_brison, martinkrcho, matveb, ocean90, paulkevan, peterwilsoncc, timothyblynjacobs, xknown, youknowriad.
    
    
    Built from https://develop.svn.wordpress.org/branches/5.9@55774
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.9@55286 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks.php
+++ wp-includes/blocks.php
@@ -628,6 +628,10 @@ function serialize_blocks( $blocks ) {
 function filter_block_content( $text, $allowed_html = 'post', $allowed_protocols = array() ) {
 	$result = '';
 
+	if ( false !== strpos( $text, '<!--' ) && false !== strpos( $text, '--->' ) ) {
+		$text = preg_replace_callback( '%<!--(.*?)--->%', '_filter_block_content_callback', $text );
+	}
+
 	$blocks = parse_blocks( $text );
 	foreach ( $blocks as $block ) {
 		$block   = filter_block_kses( $block, $allowed_html, $allowed_protocols );
@@ -635,6 +639,19 @@ function filter_block_content( $text, $allowed_html = 'post', $allowed_protocols
 	}
 
 	return $result;
+}
+
+/**
+ * Callback used for regular expression replacement in filter_block_content().
+ *
+ * @private
+ * @since 6.2.1
+ *
+ * @param array $matches Array of preg_replace_callback matches.
+ * @return string Replacement string.
+ */
+function _filter_block_content_callback( $matches ) {
+	return '<!--' . rtrim( $matches[1], '-' ) . '-->';
 }
 
 /**
