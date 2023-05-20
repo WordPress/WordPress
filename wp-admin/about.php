ffed1a007167525490ac3c49a1d6f096ea299795
desrosj <desrosj@git.wordpress.org>
2023-05-20 04:13:22 +0000

    WordPress 6.2.2. Built from https://develop.svn.wordpress.org/branches/6.2@55833
    git-svn-id: http://core.svn.wordpress.org/branches/6.2@55345 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -50,6 +50,32 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 					printf(
 						/* translators: 1: WordPress version number, 2: Plural number of bugs. More than one security issue. */
 						_n(
+							'<strong>Version %1$s</strong> addressed a security issue and fixed %2$s bug.',
+							'<strong>Version %1$s</strong> addressed a security issue and fixed %2$s bugs.',
+							1
+						),
+						'6.2.2',
+						'1'
+					);
+					?>
+					<?php
+					printf(
+						/* translators: %s: HelpHub URL. */
+						__( 'For more information, see <a href="%s">the release notes</a>.' ),
+						sprintf(
+							/* translators: %s: WordPress version. */
+							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							sanitize_title( '6.2.2' )
+						)
+					);
+					?>
+				</p>
+
+				<p>
+					<?php
+					printf(
+						/* translators: 1: WordPress version number, 2: Plural number of bugs. More than one security issue. */
+						_n(
 							'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bug.',
 							'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bugs.',
 							30
