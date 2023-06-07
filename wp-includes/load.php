88461977b1512c790512ae03821455047cbf57af
danielbachhuber <danielbachhuber@git.wordpress.org>
2023-06-07 20:09:23 +0000

    Load: Avoid loading a theme's `functions.php` when `! wp_using_themes()`.
    Updates `wp_get_active_and_valid_themes()` to return early when `wp_using_themes()` returns `false`. This prevents a theme's `functions.php` from being loaded erroneously when the site isn't using themes.
    
    Also adds `define( 'WP_USE_THEMES', true );` to the test suite bootstrap. Some tests randomly break without it because they were dependent on the previous buggy behavior.
    
    Props bpayton, costdev, danielbachhuber, hellofromtonya, sergeybiryukov, spacedmonkey.
    Fixes #57928.
    
    Built from https://develop.svn.wordpress.org/trunk@55890
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55402 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -948,6 +948,10 @@ function wp_get_active_and_valid_themes() {
 		return $themes;
 	}
 
+	if ( ! wp_using_themes() ) {
+		return $themes;
+	}
+
 	if ( TEMPLATEPATH !== STYLESHEETPATH ) {
 		$themes[] = STYLESHEETPATH;
 	}
