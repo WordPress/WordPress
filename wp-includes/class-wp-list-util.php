e2f7452084de1789c75ec179fb3f0167c737d1c7
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-06-13 04:46:23 +0000

    Coding Standards: Use strict comparison in `wp-includes/class-wp-list-util.php`.
    Follow-up to [18606], [38928], [48413].
    
    Props aristath, poena, afercia, SergeyBiryukov.
    See #57839.
    Built from https://develop.svn.wordpress.org/trunk@55908
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55420 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-list-util.php
+++ wp-includes/class-wp-list-util.php
@@ -116,12 +116,12 @@ class WP_List_Util {
 			foreach ( $args as $m_key => $m_value ) {
 				if ( is_array( $obj ) ) {
 					// Treat object as an array.
-					if ( array_key_exists( $m_key, $obj ) && ( $m_value == $obj[ $m_key ] ) ) {
+					if ( array_key_exists( $m_key, $obj ) && ( $m_value === $obj[ $m_key ] ) ) {
 						$matched++;
 					}
 				} elseif ( is_object( $obj ) ) {
 					// Treat object as an object.
-					if ( isset( $obj->{$m_key} ) && ( $m_value == $obj->{$m_key} ) ) {
+					if ( isset( $obj->{$m_key} ) && ( $m_value === $obj->{$m_key} ) ) {
 						$matched++;
 					}
 				}
@@ -276,7 +276,7 @@ class WP_List_Util {
 				continue;
 			}
 
-			if ( $a[ $field ] == $b[ $field ] ) {
+			if ( $a[ $field ] === $b[ $field ] ) {
 				continue;
 			}
 
