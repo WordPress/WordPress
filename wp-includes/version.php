a3398d1cc4fa55ab460299f67949b16eaee7ba0d
joedolson <joedolson@git.wordpress.org>
2023-06-14 20:46:21 +0000

    Build/Test Tools: Revert accidental change to .env
    Follow-up to [55919] to restore .env state.
    
    Props joedolson.
    See #50523.
    Built from https://develop.svn.wordpress.org/trunk@55920
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55432 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-alpha-55919';
+$wp_version = '6.3-alpha-55920';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
