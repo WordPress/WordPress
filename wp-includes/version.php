df4c8c8497e894d74bc4293bd2009a02fbf6ef60
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-06-17 10:28:25 +0000

    Coding Standards: Use strict comparison in `wp-includes/pomo/mo.php`.
    Follow-up to [10584], [12174].
    
    Props aristath, poena, afercia, SergeyBiryukov.
    See #57839.
    Built from https://develop.svn.wordpress.org/trunk@55931
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55443 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-alpha-55930';
+$wp_version = '6.3-alpha-55931';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
