5433b41a88ded0784bfe8436882d94e5111e6a07
Pascal Birchler <pascal.birchler@gmail.com>
2023-06-21 08:14:20 +0000

    Embeds: Add Anghami as a trusted oEmbed provider.
    Props swissspidy, Boniu91, tb1909, monzuralam, azizantoun, audrasjb.
    Fixes #49850
    Built from https://develop.svn.wordpress.org/trunk@55961
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55473 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-oembed.php
+++ wp-includes/class-wp-oembed.php
@@ -109,6 +109,7 @@ class WP_oEmbed {
 			'#https?://([a-z]{2}|www)\.pinterest\.com(\.(au|mx))?/.*#i' => array( 'https://www.pinterest.com/oembed.json', true ),
 			'#https?://(www\.)?wolframcloud\.com/obj/.+#i' => array( 'https://www.wolframcloud.com/oembed', true ),
 			'#https?://pca\.st/.+#i'                       => array( 'https://pca.st/oembed.json', true ),
+			'#https?://((play|www)\.)?anghami\.com/.*#i'   => array( 'https://api.anghami.com/rest/v1/oembed.view', true ),
 		);
 
 		if ( ! empty( self::$early_providers['add'] ) ) {
@@ -188,6 +189,7 @@ class WP_oEmbed {
 		 * | WolframCloud | wolframcloud.com                          | 5.9.0   |
 		 * | Pocket Casts | pocketcasts.com                           | 6.1.0   |
 		 * | Crowdsignal  | crowdsignal.net                           | 6.2.0   |
+		 * | Anghami      | anghami.com                               | 6.3.0   |
 		 *
 		 * No longer supported providers:
 		 *
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-alpha-55960';
+$wp_version = '6.3-alpha-55961';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
