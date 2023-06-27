b7427b977cac2bbfa58683e8e2365128396ebff0
davidbaumwald <davidbaumwald@git.wordpress.org>
2023-06-27 19:58:27 +0000

    WordPress 6.3 Beta 1. Built from https://develop.svn.wordpress.org/trunk@56077
    git-svn-id: http://core.svn.wordpress.org/trunk@55589 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-alpha-56076';
+$wp_version = '6.3-beta1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
