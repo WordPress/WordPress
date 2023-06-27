9395b8dc162bd63ef07d9a13480cba656156dd27
audrasjb <audrasjb@git.wordpress.org>
2023-06-27 22:42:26 +0000

    Twenty Ten: Reflect Heading block text color on front-end.
    This changeset fixes an issue where the selected text color of the Heading block was not reflected on front-end.
    
    Props umesh84, sabernhardt, audrasjb, pooja1210, darshitrajyaguru97.
    Fixes #56603.
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@56080
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55592 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-alpha-56079';
+$wp_version = '6.3-alpha-56080';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
