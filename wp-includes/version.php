26a104cee8bce30ec150c4e73d6ffe6646f55d73
davidbaumwald <davidbaumwald@git.wordpress.org>
2023-06-27 20:34:25 +0000

    Reverting trunk back to alpha status while issues prevented a final 6.3 Beta 1 release. Built from https://develop.svn.wordpress.org/trunk@56078
    git-svn-id: http://core.svn.wordpress.org/trunk@55590 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-beta1';
+$wp_version = '6.3-alpha-56078';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
