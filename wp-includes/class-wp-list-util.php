84e1f03b1fc7d7562056c941463d2a3f24085a4c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-07-05 11:15:22 +0000

    General: Revert strict comparison in `WP_List_Util` for now.
    Casting the values to a string causes `Array to string conversion` PHP warnings, so using loose comparison restores the previous behavior for code relying on type juggling when using the `wp_list_filter()` function, e.g. comparing a numeric string to an integer.
    
    The unit test case added for this scenario in the previous commit remains. 
    
    Follow-up to [55908], [56137].
    
    See #57839.
    Built from https://develop.svn.wordpress.org/trunk@56138
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55650 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-list-util.php
+++ wp-includes/class-wp-list-util.php
@@ -116,16 +116,12 @@ class WP_List_Util {
 			foreach ( $args as $m_key => $m_value ) {
 				if ( is_array( $obj ) ) {
 					// Treat object as an array.
-					if ( array_key_exists( $m_key, $obj )
-						&& ( (string) $m_value === (string) $obj[ $m_key ] )
-					) {
+					if ( array_key_exists( $m_key, $obj ) && ( $m_value == $obj[ $m_key ] ) ) {
 						$matched++;
 					}
 				} elseif ( is_object( $obj ) ) {
 					// Treat object as an object.
-					if ( isset( $obj->{$m_key} )
-						&& ( (string) $m_value === (string) $obj->{$m_key} )
-					) {
+					if ( isset( $obj->{$m_key} ) && ( $m_value == $obj->{$m_key} ) ) {
 						$matched++;
 					}
 				}
@@ -280,7 +276,7 @@ class WP_List_Util {
 				continue;
 			}
 
-			if ( (string) $a[ $field ] === (string) $b[ $field ] ) {
+			if ( $a[ $field ] == $b[ $field ] ) {
 				continue;
 			}
 
