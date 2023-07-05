fec46200bba5541936e2b8d66b6c28b2dfd52619
davidbaumwald <davidbaumwald@git.wordpress.org>
2023-07-05 16:21:27 +0000

    Default Filters: Correct hook type for `wp_print_scripts` and `customize_controls_print_styles`.
    Props Cybr, bhi3315.
    Fixes #58665.
    Built from https://develop.svn.wordpress.org/trunk@56140
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55652 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/default-filters.php
+++ wp-includes/default-filters.php
@@ -589,9 +589,9 @@ add_action( 'enqueue_block_editor_assets', 'enqueue_editor_block_styles_assets' 
 add_action( 'enqueue_block_editor_assets', 'wp_enqueue_editor_block_directory_assets' );
 add_action( 'enqueue_block_editor_assets', 'wp_enqueue_editor_format_library_assets' );
 add_action( 'enqueue_block_editor_assets', 'wp_enqueue_global_styles_css_custom_properties' );
-add_filter( 'wp_print_scripts', 'wp_just_in_time_script_localization' );
+add_action( 'wp_print_scripts', 'wp_just_in_time_script_localization' );
 add_filter( 'print_scripts_array', 'wp_prototype_before_jquery' );
-add_filter( 'customize_controls_print_styles', 'wp_resource_hints', 1 );
+add_action( 'customize_controls_print_styles', 'wp_resource_hints', 1 );
 add_action( 'admin_head', 'wp_check_widget_editor_deps' );
 add_filter( 'block_editor_settings_all', 'wp_add_editor_classic_theme_styles' );
 
