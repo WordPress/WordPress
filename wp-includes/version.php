c7a4311ba00b3676d5d81e7beed5282614958415
Felix Arntz <felix-arntz@leaves-and-love.net>
2023-07-07 17:41:27 +0000

    Build/Test: Fix incorrect hook being used to separate Server-Timing metrics in performance tests.
    The `wp-before-template` and `wp-template` metric intend to separate WordPress core's own bootstrap process from any logic that is part of the eventual template loaded. The appropriate hook for that is the `template_include` filter as that occurs right before the template file is included.
    
    Props mukesh27, joemcgill.
    Fixes #58674.
    
    Built from https://develop.svn.wordpress.org/trunk@56162
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55674 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-beta3-56161';
+$wp_version = '6.3-beta3-56162';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
