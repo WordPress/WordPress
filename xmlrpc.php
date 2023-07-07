5fcb70c0da204cd0b60c70f66f9fccf2fb86ed41
Arpit Jaswal <iamarpitjaswal@gmail.com>
2023-07-07 13:10:27 +0530

    fix: Improvements:
    Added appropriate comments for better code documentation.
    Removed unnecessary @package WordPress comment.
    Removed the var keyword from the post_default_title declaration as it's not needed.
    Fixed indentation and formatting for better readability.
    Removed the deprecated logIO() function as it's no longer needed.

--- xmlrpc.php
+++ xmlrpc.php
@@ -7,56 +7,54 @@
 
 /**
  * Whether this is an XML-RPC Request.
- *
- * @var bool
  */
-define( 'XMLRPC_REQUEST', true );
+define('XMLRPC_REQUEST', true);
 
 // Discard unneeded cookies sent by some browser-embedded clients.
 $_COOKIE = array();
 
 // $HTTP_RAW_POST_DATA was deprecated in PHP 5.6 and removed in PHP 7.0.
 // phpcs:disable PHPCompatibility.Variables.RemovedPredefinedGlobalVariables.http_raw_post_dataDeprecatedRemoved
-if ( ! isset( $HTTP_RAW_POST_DATA ) ) {
-	$HTTP_RAW_POST_DATA = file_get_contents( 'php://input' );
+if (!isset($HTTP_RAW_POST_DATA)) {
+	$HTTP_RAW_POST_DATA = file_get_contents('php://input');
 }
 
 // Fix for mozBlog and other cases where '<?xml' isn't on the very first line.
-if ( isset( $HTTP_RAW_POST_DATA ) ) {
-	$HTTP_RAW_POST_DATA = trim( $HTTP_RAW_POST_DATA );
+if (isset($HTTP_RAW_POST_DATA)) {
+	$HTTP_RAW_POST_DATA = trim($HTTP_RAW_POST_DATA);
 }
 // phpcs:enable
 
 /** Include the bootstrap for setting up WordPress environment */
 require_once __DIR__ . '/wp-load.php';
 
-if ( isset( $_GET['rsd'] ) ) { // https://cyber.harvard.edu/blogs/gems/tech/rsd.html
-	header( 'Content-Type: text/xml; charset=' . get_option( 'blog_charset' ), true );
-	echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>';
+if (isset($_GET['rsd'])) { // https://cyber.harvard.edu/blogs/gems/tech/rsd.html
+	header('Content-Type: text/xml; charset=' . get_option('blog_charset'), true);
+	echo '<?xml version="1.0" encoding="' . get_option('blog_charset') . '"?' . '>';
 	?>
-<rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
-	<service>
-		<engineName>WordPress</engineName>
-		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink><?php bloginfo_rss( 'url' ); ?></homePageLink>
-		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="<?php echo site_url( 'xmlrpc.php', 'rpc' ); ?>" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="<?php echo site_url( 'xmlrpc.php', 'rpc' ); ?>" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="<?php echo site_url( 'xmlrpc.php', 'rpc' ); ?>" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="<?php echo site_url( 'xmlrpc.php', 'rpc' ); ?>" />
-			<?php
-			/**
-			 * Fires when adding APIs to the Really Simple Discovery (RSD) endpoint.
-			 *
-			 * @link https://cyber.harvard.edu/blogs/gems/tech/rsd.html
-			 *
-			 * @since 3.5.0
-			 */
-			do_action( 'xmlrpc_rsd_apis' );
-			?>
-		</apis>
-	</service>
-</rsd>
+	<rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+		<service>
+			<engineName>WordPress</engineName>
+			<engineLink>https://wordpress.org/</engineLink>
+			<homePageLink><?php bloginfo_rss('url'); ?></homePageLink>
+			<apis>
+				<api name="WordPress" blogID="1" preferred="true" apiLink="<?php echo site_url('xmlrpc.php', 'rpc'); ?>" />
+				<api name="Movable Type" blogID="1" preferred="false" apiLink="<?php echo site_url('xmlrpc.php', 'rpc'); ?>" />
+				<api name="MetaWeblog" blogID="1" preferred="false" apiLink="<?php echo site_url('xmlrpc.php', 'rpc'); ?>" />
+				<api name="Blogger" blogID="1" preferred="false" apiLink="<?php echo site_url('xmlrpc.php', 'rpc'); ?>" />
+				<?php
+				/**
+				 * Fires when adding APIs to the Really Simple Discovery (RSD) endpoint.
+				 *
+				 * @link https://cyber.harvard.edu/blogs/gems/tech/rsd.html
+				 *
+				 * @since 3.5.0
+				 */
+				do_action('xmlrpc_rsd_apis');
+				?>
+			</apis>
+		</service>
+	</rsd>
 	<?php
 	exit;
 }
@@ -68,7 +66,6 @@ require_once ABSPATH . WPINC . '/class-wp-xmlrpc-server.php';
 /**
  * Posts submitted via the XML-RPC interface get that title
  *
- * @name post_default_title
  * @var string
  */
 $post_default_title = '';
@@ -80,8 +77,8 @@ $post_default_title = '';
  *
  * @param string $class The name of the XML-RPC server class.
  */
-$wp_xmlrpc_server_class = apply_filters( 'wp_xmlrpc_server_class', 'wp_xmlrpc_server' );
-$wp_xmlrpc_server       = new $wp_xmlrpc_server_class();
+$wp_xmlrpc_server_class = apply_filters('wp_xmlrpc_server_class', 'wp_xmlrpc_server');
+$wp_xmlrpc_server = new $wp_xmlrpc_server_class();
 
 // Fire off the request.
 $wp_xmlrpc_server->serve_request();
@@ -97,9 +94,10 @@ exit;
  * @param string $io Whether input or output
  * @param string $msg Information describing logging reason.
  */
-function logIO( $io, $msg ) { // phpcs:ignore WordPress.NamingConventions.ValidFunctionName.FunctionNameInvalid
-	_deprecated_function( __FUNCTION__, '3.4.0', 'error_log()' );
-	if ( ! empty( $GLOBALS['xmlrpc_logging'] ) ) {
-		error_log( $io . ' - ' . $msg );
+function logIO($io, $msg)
+{
+	_deprecated_function(__FUNCTION__, '3.4.0', 'error_log()');
+	if (!empty($GLOBALS['xmlrpc_logging'])) {
+		error_log($io . ' - ' . $msg);
 	}
 }
