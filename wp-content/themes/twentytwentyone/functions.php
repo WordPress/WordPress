cf58af516705791f30a5dd0bba63b38425f68ba2
audrasjb <audrasjb@git.wordpress.org>
2023-07-09 14:31:24 +0000

    Twenty Twenty-One: Replace the experimental link color theme support.
    This replaces `add_theme_support( 'experimental-link-color' )` with `add_theme_support( 'link-color' )`, to provide the feature to Twenty Twenty-One users, but 
    also to avoid throwing a "doing it wrong" message when Gutenberg version 16.2 is active.
    
    Follow-up to [58597].
    
    Props poena.
    Fixes #58702.
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@56170
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55682 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwentyone/functions.php
+++ wp-content/themes/twentytwentyone/functions.php
@@ -322,8 +322,8 @@ if ( ! function_exists( 'twenty_twenty_one_setup' ) ) {
 		// Add support for custom line height controls.
 		add_theme_support( 'custom-line-height' );
 
-		// Add support for experimental link color control.
-		add_theme_support( 'experimental-link-color' );
+		// Add support for link color control.
+		add_theme_support( 'link-color' );
 
 		// Add support for experimental cover block spacing.
 		add_theme_support( 'custom-spacing' );
