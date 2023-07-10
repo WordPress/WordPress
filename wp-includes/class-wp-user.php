fe75c1b12f94fdb1deb37dd6ba1f29f639357a35
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-07-10 12:53:26 +0000

    Coding Standards: Use strict comparison in `wp-includes/class-wp-user.php`.
    Follow-up to [15919], [21866], [41624].
    
    Props aristath, poena, afercia, SergeyBiryukov.
    See #57839.
    Built from https://develop.svn.wordpress.org/trunk@56184
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55696 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-user.php
+++ wp-includes/class-wp-user.php
@@ -500,7 +500,7 @@ class WP_User {
 	 */
 	public function get_role_caps() {
 		$switch_site = false;
-		if ( is_multisite() && get_current_blog_id() != $this->site_id ) {
+		if ( is_multisite() && get_current_blog_id() !== $this->site_id ) {
 			$switch_site = true;
 
 			switch_to_blog( $this->site_id );
@@ -602,7 +602,7 @@ class WP_User {
 	 * @param string $role Role name.
 	 */
 	public function set_role( $role ) {
-		if ( 1 === count( $this->roles ) && current( $this->roles ) == $role ) {
+		if ( 1 === count( $this->roles ) && current( $this->roles ) === $role ) {
 			return;
 		}
 
