20004c5f3688f4868d4087c76d3686bbaaf142ce
Mike Schroder <mschrode@umich.edu>
2023-07-19 05:54:26 +0000

    Trunk is now 6.4 alpha. Built from https://develop.svn.wordpress.org/trunk@56267
    git-svn-id: http://core.svn.wordpress.org/trunk@55779 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-RC1-56265';
+$wp_version = '6.4-alpha-56267';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
