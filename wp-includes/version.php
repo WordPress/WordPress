b69a1e9a0cc2bfc9091eb8757de42661f293d1ba
audrasjb <audrasjb@git.wordpress.org>
2023-07-21 15:07:26 +0000

    Media: Fix height max value in image scaling.
    Set the `max` attribute in the height input for image scaling to reference the image height, instead of the width.
    
    Follow-up to [55919].
    
    Props costdev, joedolson, joemcgill.
    Reviewed by audrasjb.
    Merges [56277] to the 6.3 branch.
    Fixes #50523.
    
    
    
    Built from https://develop.svn.wordpress.org/branches/6.3@56278
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.3@55790 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-RC1-56275';
+$wp_version = '6.3-RC1-56278';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
