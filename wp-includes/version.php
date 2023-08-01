6baed7cbc93bbd3fc18e43f41130149145e21fd4
ryelle <ryelle@git.wordpress.org>
2023-08-01 15:11:20 +0000

    Help/About: Add images to About page, match header style to images.
    Props richtabor, markoserb, audrasjb, peterwilsoncc, mukesh27.
    Reviewed by davidbaumwald.
    Merges [56339] to the 6.3 branch.
    See #58067.
    
    
    Built from https://develop.svn.wordpress.org/branches/6.3@56340
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.3@55852 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3-RC2-56338';
+$wp_version = '6.3-RC2-56340';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
