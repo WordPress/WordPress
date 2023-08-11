c392de6215f9faa28a01432e1c00613bbaf10733
desrosj <desrosj@git.wordpress.org>
2023-08-11 13:05:17 +0000

    Build/Test Tools: Update 3rd-party GitHub Actions.
    This updates all 3rd-party GitHub Actions to their latest versions.
    
    See #58867.
    Built from https://develop.svn.wordpress.org/trunk@56388
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55900 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-alpha-56387';
+$wp_version = '6.4-alpha-56388';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
