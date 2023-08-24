6296d14857e055ba5170b62fa49e6c4863e023f4
Aaron Jorbin <aaron@jorb.in>
2023-08-24 14:35:16 +0000

    Build/Tests: Tests_Formatting_MakeClickable should use data providors
    Removes the foreach loops from the tests by moving the in-test data sets into data providers and combines the URL data sets into one data provider to test with one test method. By using a data providor, all the tests run rather than stopping at the first failure in this group.
    
    Fixes #57660.
    Props hellofromTonya.
    
    Built from https://develop.svn.wordpress.org/trunk@56444
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55956 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-alpha-56443';
+$wp_version = '6.4-alpha-56444';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
