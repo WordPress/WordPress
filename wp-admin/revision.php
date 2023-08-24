42c9882b4696c7d6b294ff32158dae13a3cdcc5e
audrasjb <audrasjb@git.wordpress.org>
2023-08-24 09:32:21 +0000

    Revisions: Add missing escaping function for `$post_edit_link` in `wp-admin/revision.php`.
    Props nidhidhandhukiya, Presskopp, mukesh27, rajinsharwar, costdev.
    Fixes #59141.
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@56437
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@55949 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/revision.php
+++ wp-admin/revision.php
@@ -104,10 +104,10 @@ switch ( $action ) {
 		}
 
 		$post_edit_link = get_edit_post_link();
-		$post_title     = '<a href="' . $post_edit_link . '">' . _draft_or_post_title() . '</a>';
+		$post_title     = '<a href="' . esc_url( $post_edit_link ) . '">' . _draft_or_post_title() . '</a>';
 		/* translators: %s: Post title. */
 		$h1             = sprintf( __( 'Compare Revisions of &#8220;%s&#8221;' ), $post_title );
-		$return_to_post = '<a href="' . $post_edit_link . '">' . __( '&larr; Go to editor' ) . '</a>';
+		$return_to_post = '<a href="' . esc_url( $post_edit_link ) . '">' . __( '&larr; Go to editor' ) . '</a>';
 		// Used in the HTML title tag.
 		$title = __( 'Revisions' );
 
