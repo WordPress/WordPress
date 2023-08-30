8429956b0947e40e3a7cd5f88a5a76dcd4874e15
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-08-30 10:51:21 +0000

    Tests: Correct uses of `ReflectionProperty::setValue()` for static properties.
    The single parameter signature, which was used for setting the value on a static property, is deprecated since PHP 8.3. A cross-version solution is to pass `null` as the first parameter.
    
    This commit updates all the instances that use the deprecated signature in WordPress core.
    
    Reference: [https://wiki.php.net/rfc/deprecate_functions_with_overloaded_signatures#reflectionpropertysetvalue PHP RFC: Deprecate functions with overloaded signatures: ReflectionProperty::setValue()].
    
    Follow-up to [53152], [54493], [54799].
    
    Props jrf, costdev, Tests: Correct uses of `ReflectionProperty::setValue()` for static properties.
    
    The single parameter signature, which was used for setting the value on a static property, is deprecated since PHP 8.3. A cross-version solution is to pass `null` as the first parameter.
    
    This commit updates all the instances that use the deprecated signature in WordPress core.
    
    Reference: [https://wiki.php.net/rfc/deprecate_functions_with_overloaded_signatures#reflectionpropertysetvalue PHP RFC: Deprecate functions with overloaded signatures: ReflectionProperty::setValue()].
    
    Follow-up to [53152], [54493], [54799].
    
    Props jrf, costdev, sc0ttkclark.
    See #59231.
    Built from https://develop.svn.wordpress.org/trunk@56492
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56004 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-alpha-56491';
+$wp_version = '6.4-alpha-56492';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
