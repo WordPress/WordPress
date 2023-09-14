7d3694a17c184fd10c91a144ce753a14d20027ed
Tammie Lister <tammie@automattic.com>
2023-09-14 09:47:19 +0000

    Twenty Nineteen: Fixes button line height.
    Line height was broken for button block and adding spacing.
    
    Props nidhidhandhukiya, shailu25.
    Fixes #58443.
    
    Built from https://develop.svn.wordpress.org/trunk@56580
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56092 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-alpha-56579';
+$wp_version = '6.4-alpha-56580';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
