cc1bb87546a7f869a201842bec5a125df6fdae2b
costdev <costdev@git.wordpress.org>
2023-09-18 10:57:16 +0000

    Docs: Document `aria-live` and `hidden` in `safecss_filter_attr()`.
    In [56603], support was added for `aria-live` and `hidden` attributes in `safecss_filter_attr()`.
    
    This adds a `@since` annotation to document this change.
    
    Follow-up to [56603].
    
    Props mukesh27.
    See #57791.
    Built from https://develop.svn.wordpress.org/trunk@56608
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56120 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/kses.php
+++ wp-includes/kses.php
@@ -2631,6 +2631,7 @@ function safecss_filter_attr( $css, $deprecated = '' ) {
  * @since 5.0.0 Added support for `data-*` wildcard attributes.
  * @since 6.0.0 Added `dir`, `lang`, and `xml:lang` to global attributes.
  * @since 6.3.0 Added `aria-controls`, `aria-current`, and `aria-expanded` attributes.
+ * @since 6.4.0 Added `aria-live` and `hidden` attributes.
  *
  * @access private
  * @ignore
