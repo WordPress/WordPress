64e87a4b3a39b49a9fb54648b496ab51d38a3736
costdev <costdev@git.wordpress.org>
2023-09-19 17:15:27 +0000

    Build/Test Tools: Add `sys_get_temp_dir()` to `open_basedir` tests.
    In PHPUnit 10.3.5, 9.6.13 and 8.5.34, the child processes used for process isolation now use temporary files to communicate their result to the parent process.
    
    This caused a failure in some tests that set the `open_basedir` PHP directive to a value that did not include `sys_get_temp_dir()`.
    
    This adds `sys_get_temp_dir()` to the `open_basedir` value set by the tests to ensure that permission is still granted for the temporary directory.
    
    PHPUnit uses `sys_get_temp_dir()`. To ensure the result is the same, Core's `get_temp_dir()` function is not used.
    
    References:
    - https://github.com/sebastianbergmann/phpunit/issues/5356
    
    Props desrosj, mukesh27, SergeyBiryukov, costdev.
    Merges [56622] to the 6.3 branch.
    See #59394.
    Built from https://develop.svn.wordpress.org/branches/6.3@56624
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.3@56136 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3.2-alpha-56582';
+$wp_version = '6.3.2-alpha-56624';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
