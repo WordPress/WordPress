4b17383d2c12886fecca49b95ef140a8b059357e
joedolson <joedolson@git.wordpress.org>
2023-09-22 18:14:16 +0000

    Code Modernization: Rename reserved keyword used as variable.
    Change the `$echo` parameter added to `wp_update_php_annnotation()` to `$display` to avoid using reserved PHP keywords as parameters. Follow up to [56570].
    
    Props kebbet, mukesh27.
    See #57791.
    Built from https://develop.svn.wordpress.org/trunk@56662
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56174 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -8318,19 +8318,19 @@ function wp_get_default_update_php_url() {
  *
  * @since 5.1.0
  * @since 5.2.0 Added the `$before` and `$after` parameters.
- * @since 6.4.0 Added the `$echo` parameter.
+ * @since 6.4.0 Added the `$display` parameter.
  *
- * @param string $before Markup to output before the annotation. Default `<p class="description">`.
- * @param string $after  Markup to output after the annotation. Default `</p>`.
- * @param bool   $echo   Markup should echo if true. Default `true`.
+ * @param string $before  Markup to output before the annotation. Default `<p class="description">`.
+ * @param string $after   Markup to output after the annotation. Default `</p>`.
+ * @param bool   $display Whether to echo or return the markup. Default `true` for echo.
  *
  * @return string|void
  */
-function wp_update_php_annotation( $before = '<p class="description">', $after = '</p>', $echo = true ) {
+function wp_update_php_annotation( $before = '<p class="description">', $after = '</p>', $display = true ) {
 	$annotation = wp_get_update_php_annotation();
 
 	if ( $annotation ) {
-		if ( $echo ) {
+		if ( $display ) {
 			echo $before . $annotation . $after;
 		} else {
 			return $before . $annotation . $after;
