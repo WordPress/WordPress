da9f594a5598bd33c16055d07487007bff063402
desrosj <desrosj@git.wordpress.org>
2023-09-22 17:43:21 +0000

    Build/Test Tools: Introduce install and upgrade testing workflows.
    This introduces GitHub Action workflows for testing the installation and upgrade process to ensure there are no unexpected issues. The goal of these workflows is to replicate some of the manual testing that happens during the release process. This does not replace that testing which is a fantastic, low barrier way for someone newer to contributing to get involved. Instead, it should serve as an additional sanity check to ensure no combinations are missed.
    
    The installation workflow tests installing the latest or specified version of WordPress on all PHP and MySQL version combinations for both single and multisite.
    
    The upgrade testing will test upgrading from each branch that still receives security updates to the latest or specified version on all PHP and MySQL version combinations for both single and multisite.
    
    Both workflows confirm that no errors are encountered when using WP-CLI to install or upgrade WordPress. No test assertions of any type are currently performed, but adding some will be explored.
    
    Props costdev, azaozz, ironprogrammer, desrosj.
    See #58977.
    Built from https://develop.svn.wordpress.org/trunk@56661
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56173 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-alpha-56660';
+$wp_version = '6.4-alpha-56661';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
