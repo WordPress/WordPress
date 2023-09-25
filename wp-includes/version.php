98a0a6b5e5a465ca69955f3d24fb71e0e35577e6
desrosj <desrosj@git.wordpress.org>
2023-09-25 20:49:18 +0000

    Docs: Revert [56679].
    `Memoizes` is actually correct in this context.
    
    Unprops desrosj.
    See #18298.
    Built from https://develop.svn.wordpress.org/trunk@56686
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56198 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-alpha-56684';
+$wp_version = '6.4-alpha-56686';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
