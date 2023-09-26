dd0700e3745371b8159f74b1cc6ab10474354035
desrosj <desrosj@git.wordpress.org>
2023-09-26 17:07:22 +0000

    General: Define the desired `mime-type` for WebP files.
    Fixes #59463.
    Built from https://develop.svn.wordpress.org/trunk@56725
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56237 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-alpha-56724';
+$wp_version = '6.4-alpha-56725';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
