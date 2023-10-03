04ed4159b4fa732c30bcec1d4ac85a192f85f404
gziolo <gziolo@git.wordpress.org>
2023-10-03 08:29:23 +0000

    Tests: Cover Block Hooks integration with a custom block theme
    Adds a simplified version of Twenty Twenty-Three theme that helps testing Block Hooks integration. The theme contains:
    
    - The required index.html template.
    - The optional single.html template used with tests.
    - 3 template parts where two of them reference patterns.
    - 3 patterns referenced in the templates and the template parts.
    
    New tests automatically register 4 custom blocks with the test theme where each of them hooks into another block using all four target relative positions: `before`, `after`, `firstChild`, `lastChild`.
    
    The tests verify that the block gets hooked into the correct positions when targeting:
    
    - template
    - template part
    - pattern
    
    Props ockham, costdev.
    See #59313, #59383.
    Follow-up [56610].
    
    Built from https://develop.svn.wordpress.org/trunk@56759
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56271 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-beta1-56757';
+$wp_version = '6.4-beta1-56759';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
