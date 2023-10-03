c9cbea7627f3f4299fb5e1e964b0f0086a956e40
gziolo <gziolo@git.wordpress.org>
2023-10-03 08:54:20 +0000

    Tests: Rename and improve the `blocks/context.php` file to follow handbook
    Renames `context.php` and `Tests_Blocks_Context` to `renderBlock.php` and `Tests_Blocks_RenderBlock`. See https://make.wordpress.org/core/handbook/testing/automated-testing/writing-phpunit-tests/#test-classes. 
    
    Simplifies also the tear_down method by using the same convention as in [56759].
    
    Props costdev, ockham.
    Follow-up [48224].
    See #49927.
    
    
    Built from https://develop.svn.wordpress.org/trunk@56761
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56273 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-beta1-56760';
+$wp_version = '6.4-beta1-56761';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
