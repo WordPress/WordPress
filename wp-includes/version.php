51ca8e0364e36577f11690403df5ea37159f7371
gziolo <gziolo@git.wordpress.org>
2023-10-03 08:36:20 +0000

    Chore: Remove empty test file that was missed after renaming the file
    Follow-up [56759].
    
    
    Built from https://develop.svn.wordpress.org/trunk@56760
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56272 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-beta1-56759';
+$wp_version = '6.4-beta1-56760';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
