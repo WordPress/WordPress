9a5c4b6919be80a70a96a23b86cbea793e392f38
audrasjb <audrasjb@git.wordpress.org>
2023-10-04 22:16:26 +0000

    Twenty Twenty: Fix style issues within iframed editor.
    This changeset:
    - Replaces `body` with `html` for the first CSS selector that makes text white against a dark background
    - Moves `twentytwenty_block_editor_styles()` from the `enqueue_block_editor_assets` action to `enqueue_block_assets` for WordPress 6.3 and later
    - Removes the obsolete `twentytwenty-block-editor-script` from the styles function to avoid an error in the iframe
    
    Props floydwilde, poena, huzaifaalmesbah, greenshady, sabernhardt, audrasjb, pooja1210, shailu25, joemcgill.
    Merges [56783] to the 6.3 branch.
    Fixes #59086.
    
    
    
    
    Built from https://develop.svn.wordpress.org/branches/6.3@56784
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.3@56296 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3.2-alpha-56782';
+$wp_version = '6.3.2-alpha-56784';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
