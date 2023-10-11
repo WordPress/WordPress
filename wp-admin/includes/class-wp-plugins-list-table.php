b8b4a9289530734bd4b120e6fef5aab6a0861e97
costdev <costdev@git.wordpress.org>
2023-10-11 04:33:32 +0000

    Plugins: Fix broken `sprintf()` call in plugins list table.
    In [56599], a `sprintf()` call was modified which resulted in an insufficient number of arguments.
    This caused a Fatal Error when an incompatible plugin notice was displayed.
    
    This fixes the `sprintf()` call.
    
    Follow-up to [56599].
    
    Props petitphp, TobiasBg, sabernhardt, mukesh27.
    Fixes #59590. See #57791.
    Built from https://develop.svn.wordpress.org/trunk@56824
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56336 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-plugins-list-table.php
+++ wp-admin/includes/class-wp-plugins-list-table.php
@@ -1280,8 +1280,7 @@ class WP_Plugins_List_Table extends WP_List_Table {
 
 		if ( ! $compatible_php || ! $compatible_wp ) {
 			printf(
-				'<tr class="plugin-update-tr">' .
-				'<td colspan="%s" class="plugin-update colspanchange">' .
+				'<tr class="plugin-update-tr"><td colspan="%s" class="plugin-update colspanchange">',
 				esc_attr( $this->get_column_count() )
 			);
 
