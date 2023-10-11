b918d61236ea8fbdecc2e1a47b776b3063dbed67
desrosj <desrosj@git.wordpress.org>
2023-10-11 18:09:24 +0000

    Build/Test Tools: Remove hardcoded PHPUnit config files.
    The configuration file passed to the callable workflow contains the correct one to use.
    
    See #58955.
    Built from https://develop.svn.wordpress.org/trunk@56831
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56343 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-beta3-56830';
+$wp_version = '6.4-beta3-56831';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
