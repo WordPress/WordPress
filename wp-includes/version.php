6a66e05d419c28c48ebd2ab5c3cca6eb41ce7bd0
desrosj <desrosj@git.wordpress.org>
2023-10-11 12:17:24 +0000

    Build/Test Tools: Don’t send Slack notification for workflow retries.
    [56780] changed Slack failure notifications to not send a failure notification during the first run of a workflow. Because workflows automatically restart once, a failure during the first run can be ignored.
    
    This adjusts the workflow to also not send a “fixed” notification when the second run of a workflow succeeds after a failure. Because the original failure is no longer reported, these notifications are unnecessary.
    
    See #58867.
    Built from https://develop.svn.wordpress.org/trunk@56827
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56339 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-beta3-56826';
+$wp_version = '6.4-beta3-56827';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
