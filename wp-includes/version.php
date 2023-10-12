d25076284abbc43126dfbf9166db4532df08861c
audrasjb <audrasjb@git.wordpress.org>
2023-10-12 18:25:54 +0000

    WordPress 5.1.17.
    Built from https://develop.svn.wordpress.org/branches/5.1@56879
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.1@56390 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1.16';
+$wp_version = '5.1.17';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
