bd5486577c07bd4af018bd18880cd90b1f0d7d7e
Aaron Jorbin <aaron@jorb.in>
2023-10-12 12:41:18 +0000

    Application Passwords: Prevent the use of some pseudo protocols in application passwords.
    Props tykoted, xknown, peterwilsoncc, jorbin, timothyblynjacobs, martinkrcho, paulkevan, dd32, ehtis.
    
    Built from https://develop.svn.wordpress.org/trunk@56837
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56349 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-beta3-56836';
+$wp_version = '6.4-beta3-56837';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
