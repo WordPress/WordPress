f1b2dc3e773993bfb3649c67ef02c895b3158cd8
audrasjb <audrasjb@git.wordpress.org>
2023-10-12 13:35:23 +0000

    Help/About: Update the About page for 6.3.2.
    Props beryldlg, audrasjb.
    
    
    
    
    Built from https://develop.svn.wordpress.org/branches/6.3@56847
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.3@56359 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -45,7 +45,32 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 
 		<div class="about__section changelog has-subtle-background-color">
 			<div class="column">
-				<h2><?php _e( 'Maintenance Release' ); ?></h2>
+				<h2><?php _e( 'Maintenance and Security Releases' ); ?></h2>
+				<p>
+					<?php
+					printf(
+						/* translators: 1: WordPress version number, 2: Plural number of bugs. More than one security issue. */
+						_n(
+							'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bug.',
+							'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bugs.',
+							41
+						),
+						'6.3.2',
+						'41'
+					);
+					?>
+					<?php
+					printf(
+						/* translators: %s: HelpHub URL. */
+						__( 'For more information, see <a href="%s">the release notes</a>.' ),
+						sprintf(
+							/* translators: %s: WordPress version. */
+							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							sanitize_title( '6.3.2' )
+						)
+					);
+					?>
+				</p>
 				<p>
 					<?php
 					printf(
