9fa577589cb1fde2017e598ab7722b347addb29c
Joe McGill <joemcgill@gmail.com>
2023-10-12 18:16:46 +0000

    Grouped backports to the 6.1 branch.
    - REST API: Limit `search_columns` for users without `list_users`.
    - Comments: Prevent users who can not see a post from seeing comments on it.
    - Application Passwords: Prevent the use of some pseudo protocols in application passwords.
    - Restrict media shortcode ajax to certain type
    - REST API: Ensure no-cache headers are sent when methods are overriden.
    - Prevent unintended behavior when certain objects are unserialized.
    
    Merges [56833], [56834], [56835], [56836], [56837], and [56838] to the 6.1 branch.
    Props xknown, jorbin, Vortfu, joehoyle, timothyblynjacobs, peterwilsoncc, ehtis, tykoted, martinkrcho, paulkevan, dd32, antpb, rmccue.
    
    Built from https://develop.svn.wordpress.org/branches/6.1@56867
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.1@56378 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/media.php
+++ wp-includes/media.php
@@ -2453,6 +2453,7 @@ function gallery_shortcode( $attr ) {
 			$attachments[ $val->ID ] = $_attachments[ $key ];
 		}
 	} elseif ( ! empty( $atts['exclude'] ) ) {
+		$post_parent_id = $id;
 		$attachments = get_children(
 			array(
 				'post_parent'    => $id,
@@ -2465,6 +2466,7 @@ function gallery_shortcode( $attr ) {
 			)
 		);
 	} else {
+		$post_parent_id = $id;
 		$attachments = get_children(
 			array(
 				'post_parent'    => $id,
@@ -2475,6 +2477,17 @@ function gallery_shortcode( $attr ) {
 				'orderby'        => $atts['orderby'],
 			)
 		);
+	}
+
+	if ( ! empty( $post_parent_id ) ) {
+		$post_parent = get_post( $post_parent_id );
+
+		// terminate the shortcode execution if user cannot read the post or password-protected
+		if (
+		( ! is_post_publicly_viewable( $post_parent->ID ) && ! current_user_can( 'read_post', $post_parent->ID ) )
+		|| post_password_required( $post_parent ) ) {
+			return '';
+		}
 	}
 
 	if ( empty( $attachments ) ) {
@@ -2801,6 +2814,15 @@ function wp_playlist_shortcode( $attr ) {
 	} else {
 		$args['post_parent'] = $id;
 		$attachments         = get_children( $args );
+	}
+
+	if ( ! empty( $args['post_parent'] ) ) {
+		$post_parent = get_post( $id );
+
+		// terminate the shortcode execution if user cannot read the post or password-protected
+		if ( ! current_user_can( 'read_post', $post_parent->ID ) || post_password_required( $post_parent ) ) {
+			return '';
+		}
 	}
 
 	if ( empty( $attachments ) ) {
