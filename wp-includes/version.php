0be437492120eb21becb1913613454bc6f9b73d7
audrasjb <audrasjb@git.wordpress.org>
2023-10-12 13:10:26 +0000

    REST API: Limit `search_columns` for users without `list_users`.
    Props Vortfu, jorbin, joehoyle, timothyblynjacobs, peterwilsoncc, ehtis.
    Reviewed by jorbin, audrasjb.
    
    
    
    
    Built from https://develop.svn.wordpress.org/branches/6.3@56840
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.3@56352 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.3.2-RC1-56795';
+$wp_version = '6.3.2-RC1-56840';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
