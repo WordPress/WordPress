0b35c4e2bfd88eff0aa2a7cc510c5b8cada383ad
Aaron Jorbin <aaron@jorb.in>
2023-10-12 12:34:33 +0000

    Prevent unintended behavior when certain objects are unserialized.
    Props ehtis, xknown.
    
    Built from https://develop.svn.wordpress.org/trunk@56835
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56347 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/Requests/src/Iri.php
+++ wp-includes/Requests/src/Iri.php
@@ -717,6 +717,20 @@ class Iri {
 		return true;
 	}
 
+	public function __wakeup() {
+		$class_props = get_class_vars( __CLASS__ );
+		$string_props = array( 'scheme', 'iuserinfo', 'ihost', 'port', 'ipath', 'iquery', 'ifragment' );
+		$array_props = array( 'normalization' );
+		foreach ( $class_props as $prop => $default_value ) {
+			if ( in_array( $prop, $string_props, true ) && ! is_string( $this->$prop ) ) {
+				throw new UnexpectedValueException();
+			} elseif ( in_array( $prop, $array_props, true ) && ! is_array( $this->$prop ) ) {
+				throw new UnexpectedValueException();
+			}
+			$this->$prop = null;
+		}
+	}
+
 	/**
 	 * Set the entire IRI. Returns true on success, false on failure (if there
 	 * are any invalid characters).
