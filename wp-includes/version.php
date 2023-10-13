ed4ba3bf5310bdc7b8817612d7679ea3295965b3
Pascal Birchler <pascal.birchler@gmail.com>
2023-10-13 08:15:21 +0000

    Build/Test Tools: Revert accidental change to `Gruntfile.js`
    This is a follow-up to r56926.
    
    See #59517.
    Built from https://develop.svn.wordpress.org/trunk@56927
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56438 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-beta4-56926';
+$wp_version = '6.4-beta4-56927';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
