d80a03fa4f65a9e49f8a213f954c37e780c265e0
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-10-15 08:09:27 +0000

    Tests: Reset the current user before performing assertions in some comment tests.
    This aims to avoid affecting other tests in case of failure.
    
    Follow-up to [54527].
    
    See #58955.
    Built from https://develop.svn.wordpress.org/trunk@56936
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56447 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-beta4-56935';
+$wp_version = '6.4-beta4-56936';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
