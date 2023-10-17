891a9ecdafa9cc3b2099ac9679e617d76dedb452
hellofromTonya <hellofromTonya@git.wordpress.org>
2023-10-17 18:41:24 +0000

    Trunk is now 6.5 alpha. Built from https://develop.svn.wordpress.org/trunk@56966
    git-svn-id: http://core.svn.wordpress.org/trunk@56477 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-RC1-56964';
+$wp_version = '6.5-alpha-56966';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
