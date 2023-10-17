9f39b4ea0f5d85641ab09619d2785da1b9655e4d
Pascal Birchler <pascal.birchler@gmail.com>
2023-10-17 11:29:27 +0000

    Build/Test Tools: Reinstate `PUPPETEER_SKIP_DOWNLOAD` for most CI workflows.
    The `PUPPETEER_SKIP_DOWNLOAD` environment variable is used to prevent Puppeteer from automatically downloading browser binaries.
    It was removed in [56926] due to the migration to Playwright. However, because of the QUnit tests, Puppeteer is actually still a dependency.
    
    Until those tests change, we have to keep this environment variable to prevent unnecessary downloads on CI.
    
    Props SergeyBiryukov.
    Fixes #59517.
    Built from https://develop.svn.wordpress.org/trunk@56954
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56465 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-beta4-56953';
+$wp_version = '6.4-beta4-56954';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
