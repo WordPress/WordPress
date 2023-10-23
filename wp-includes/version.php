a2b495afa9326c507ed55a56da442b3e187e9224
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-10-23 13:17:22 +0000

    Docs: Improve documentation for meta revision functions.
    Includes:
    
    * Correcting the position of `@since 6.4.0` in a few places.
    * Adding missing `@return` documentation.
    * Adjusting parameter spacing.
    
    Follow-up to [56714].
    
    Props jeremyfelt, mukesh27.
    Fixes #59666.
    Built from https://develop.svn.wordpress.org/trunk@56984
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56495 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-56983';
+$wp_version = '6.5-alpha-56984';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
