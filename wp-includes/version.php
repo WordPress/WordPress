8a2a1f4b415b0dc0fdbf47a1ed553acb4c1db2e5
hellofromTonya <hellofromTonya@git.wordpress.org>
2023-10-24 10:59:27 +0000

    Tests: Fix static property handling in r56991.
    Fixes static property handling for `WP_Duotone::$block_css_declarations` in the `Tests_Block_Supports_Duotone::test_css_declarations_are_generated_even_with_empty_block_content()`:
    
    * Fixes `ReflectionProperty::setValue()` to use an instance of `WP_Duotone`.
    * Adds an inline comment to explain why a static class (i.e. a class that is not intended to be an object by design as it only contains static properties and methods) needs an instance, i.e. needed for PHP 8.3 and higher.
    * Resets the static property's value to its original value, i.e. before the test started.
    
    Follow-up to [56991].
    
    Props costdev.
    See #59694.
    Built from https://develop.svn.wordpress.org/trunk@56996
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56507 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-56994';
+$wp_version = '6.5-alpha-56996';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
