a0d71729142207a098550ce15d86c40bf8bafe09
Aaron Jorbin <aaron@jorb.in>
2023-10-26 22:36:25 +0000

    REST API: Move `rest_pre_serve_request` filter to after no cache headers are sent.
    [56834] adjusted the order of activity inside the rest server responses. This lead to the `rest_pre_serve_request` filter potentially blocking the sending of the no cache headers. This moves that action back to being after the sending of no cache headers has finished to restore the pre 6.3.2 order of these two actions.
    
    Props perrelet, SergeyBiryukov, peterwilsoncc.
    Fixes #59722.
    
    Built from https://develop.svn.wordpress.org/trunk@57012
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56523 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57011';
+$wp_version = '6.5-alpha-57012';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
