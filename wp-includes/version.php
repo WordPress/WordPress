46a2d34de22d9f665474abed794af5e2b19428f3
Peter Wilson <wilson@peterwilson.cc>
2023-10-30 23:23:24 +0000

    Options, Meta APIs: Fast follow fixes for option cache priming functions.
    A collection of fixes for `wp_prime_option_caches()`:
    
    * cache arrays and objects in their serialized form for consistency with `get_option()` and `wp_load_alloptions()`
    * prevent repeat database queries for falsey and known non-existent options (notoptions)
    
    Additional tests for `wp_prime_option_caches()` to ensure:
    
    * additional database queries are not made repriming options (known, known-unknown and alloptions)
    * cache is primed consistently
    * `get_option()` returns a consistent value regardless of how it is primed
    * database queries do not contain earlier primed options
    * `get_option` does not prime the cache when testing the cache has been successfully primed
    
    Fixes a test for `wp_prime_option_caches_by_group()` to ensure `get_option` does not prime the cache when testing the cache has been successfully primed.
    
    Follow up to [56445],[56990],[57013].
    
    Reviewed by flixos90, hellofromTonya, joemcgill.
    Merges [57029] to the 6.4 branch.
    
    Props peterwilsoncc, costdev, flixos90, hellofromTonya, mikeschroder, joemcgill.
    Fixes #59738. See #58962.
    
    
    Built from https://develop.svn.wordpress.org/branches/6.4@57030
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.4@56541 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-RC2-57025';
+$wp_version = '6.4-RC2-57030';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
