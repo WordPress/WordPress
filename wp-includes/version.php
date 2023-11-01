6c91e1adfa16a0c93c6ebf69466b117a422145d0
desrosj <desrosj@git.wordpress.org>
2023-11-01 19:31:21 +0000

    Build/Test Tools: Increase the number of retries when restarting a workflow.
    This increases the number of times to retry restarting a workflow from 10 to 15, and the `timeout-minutes` value to `30`.
    
    For workflows with complex strategy matrix, the exponential backoff of 10 retries is still not enough to account for the GitHub Actions UI taking a long time to catch up.
    
    Follow up to [56829] and [56830].
    See #58867.
    Built from https://develop.svn.wordpress.org/trunk@57052
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56563 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57048';
+$wp_version = '6.5-alpha-57052';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
