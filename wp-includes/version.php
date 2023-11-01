e9e6d7d7532fd364febd634cf1a6ad3fc0d93ddd
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-11-01 11:20:25 +0000

    Docs: Update some reusable block references to synced patterns.
    In WordPress 6.3, [https://wordpress.org/documentation/article/reusable-blocks/ Reusable Blocks were renamed to Patterns]. A synced pattern will behave in exactly the same way as a reusable block.
    
    This commit updates some references in DocBlocks and inline comments to use the new name.
    
    Follow-up to [56030].
    
    Reviewed by hellofromTonya.
    Merges [57032] and [57033] to the 6.4 branch.
    
    Props benjaminknox, oglekler, hellofromTonya, marybaum, nicolefurlan.
    Fixes #59388.
    Built from https://develop.svn.wordpress.org/branches/6.4@57040
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.4@56551 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-RC2-57039';
+$wp_version = '6.4-RC2-57040';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
