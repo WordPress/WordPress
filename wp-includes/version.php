3a7d3823ffe3513e1cb641a0753da5e01dfb4b09
Aaron Jorbin <aaron@jorb.in>
2023-11-02 15:02:20 +0000

    Help/About: Update link to field guide for 6.4.
    Follow up to [56950].
    
    Merges [57054] to 6.4 branch.
    Reviewed by davidbaumwald, joedolson.
    
    Props afercia.
    Fixes #59289.
    
    Built from https://develop.svn.wordpress.org/branches/6.4@57055
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.4@56566 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-RC3-57051';
+$wp_version = '6.4-RC3-57055';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
