05673bcac87b2febb33e384d7b2a843836486aec
hellofromTonya <hellofromTonya@git.wordpress.org>
2023-11-06 16:22:19 +0000

    WordPress 6.4 RC4. Built from https://develop.svn.wordpress.org/branches/6.4@57073
    git-svn-id: http://core.svn.wordpress.org/branches/6.4@56584 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4-RC3-57072';
+$wp_version = '6.4-RC4';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
