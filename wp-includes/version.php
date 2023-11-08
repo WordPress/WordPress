7208116a8c2de1b85ae17af4224b18923c1759cb
desrosj <desrosj@git.wordpress.org>
2023-11-08 00:36:25 +0000

    Build/Test Tools: Add 6.3 to the Upgrade Testing workflow.
    This adds WordPress 6.3 to the Upgrade Testing GitHub Actions workflow.
    
    See #59805.
    Built from https://develop.svn.wordpress.org/trunk@57082
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56593 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57081';
+$wp_version = '6.5-alpha-57082';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
