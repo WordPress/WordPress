26cd5654fede4a40394c2690db3537cf47693a9b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-11-30 11:17:27 +0000

    Tests: Update `_wp_timezone_choice_usort_callback()` tests for consistency.
    * Use the same `@group` annotation as the other tests.
    * Use `assertSame()` to verify the type of the result.
    * Use `data_` prefix for the data provider.
    * Use named data set in the data provider. This makes the output when using the `--testdox` option more descriptive and is helpful when trying to debug which data set from a data provider failed the test.
    * Other minor corrections.
    
    Reference: [https://make.wordpress.org/core/handbook/testing/automated-testing/writing-phpunit-tests/#repetitive-tests Core Handbook: Writing PHP Tests: Repetitive Tests].
    
    Follow-up to [57145].
    
    See #59953, #59647.
    Built from https://develop.svn.wordpress.org/trunk@57146
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56657 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57145';
+$wp_version = '6.5-alpha-57146';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
