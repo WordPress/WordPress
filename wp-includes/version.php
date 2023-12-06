aca083594c55f292a778c1418a2b1b3cb79cb0de
Joe McGill <joemcgill@gmail.com>
2023-12-06 17:20:24 +0000

    Update package-lock.json after the 6.4.3-alpha version bump.
    This is a followup to [57167] to fix a version number in `package-lock.json` that was missed in the initial commit.
    
    Built from https://develop.svn.wordpress.org/branches/6.4@57168
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.4@56679 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4.3-alpha-57167';
+$wp_version = '6.4.3-alpha-57168';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
