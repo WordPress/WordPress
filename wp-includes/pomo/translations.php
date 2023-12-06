e0d74dfb10062957d327104c552f14806e538939
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-12-06 11:50:26 +0000

    I18N: Deprecate `Gettext_Translations::parenthesize_plural_exression()`.
    Aside from having a typo in the name, the method is unused by core as of WordPress 4.9.
    
    Follow-up to [10584], [12079], [41722].
    
    Props tohincoderex, jrf.
    Fixes #59347.
    Built from https://develop.svn.wordpress.org/trunk@57161
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56672 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pomo/translations.php
+++ wp-includes/pomo/translations.php
@@ -249,6 +249,9 @@ if ( ! class_exists( 'Translations', false ) ) :
 		 * Adds parentheses to the inner parts of ternary operators in
 		 * plural expressions, because PHP evaluates ternary oerators from left to right
 		 *
+		 * @deprecated 6.5.0 Use the Plural_Forms class instead.
+		 * @see Plural_Forms
+		 *
 		 * @param string $expression the expression without parentheses
 		 * @return string the expression with parentheses added
 		 */
