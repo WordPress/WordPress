cf50fd40f5e92a298fef43b0958f96cbe25eb1af
isabel_brison <isabel_brison@git.wordpress.org>
2023-12-08 06:32:24 +0000

    Themes: fix documentation link.
    Removes a period from an inline comment of `wp_block_theme_activate_nonce` that broke a link in the comment.
    
    Props stevenlinx, mukesh27.
    Fixes #60034.
    
    Built from https://develop.svn.wordpress.org/trunk@57171
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56682 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/theme-previews.php
+++ wp-includes/theme-previews.php
@@ -61,7 +61,7 @@ function wp_attach_theme_preview_middleware() {
  * Sets the JavaScript global WP_BLOCK_THEME_ACTIVATE_NONCE containing the nonce
  * required to activate a theme. For use within the site editor.
  *
- * @see https://github.com/WordPress/gutenberg/pull/41836.
+ * @see https://github.com/WordPress/gutenberg/pull/41836
  *
  * @since 6.3.0
  * @access private
