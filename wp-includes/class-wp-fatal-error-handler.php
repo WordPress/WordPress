fac28648413bfaf7ab353744ebb75ff743a55cb6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-12-16 09:38:25 +0000

    Docs: Document the `$wp_locale` global in `WP_Fatal_Error_Handler::handle()`.
    Follow-up to [45277].
    
    Props viralsampat.
    See #60021.
    Built from https://develop.svn.wordpress.org/trunk@57194
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56705 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-fatal-error-handler.php
+++ wp-includes/class-wp-fatal-error-handler.php
@@ -25,6 +25,8 @@ class WP_Fatal_Error_Handler {
 	 * This method is registered via `register_shutdown_function()`.
 	 *
 	 * @since 5.2.0
+	 *
+	 * @global WP_Locale $wp_locale WordPress date and time locale object.
 	 */
 	public function handle() {
 		if ( defined( 'WP_SANDBOX_SCRAPING' ) && WP_SANDBOX_SCRAPING ) {
