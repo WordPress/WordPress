f692428328fc67a0849fc11e2f43509e5ee07172
desrosj <desrosj@git.wordpress.org>
2023-12-22 01:00:20 +0000

    Build/Test Tools: Make the install testing workflow more flexible.
    In [56661], a new GitHub Actions workflow was introduced that focused on running some minimal installation tests for a version of WordPress for every PHP and MySQL combination.
    
    This workflow has tested well, but lacks flexibility and possesses one flaw: tests are only ever performed with currently supported versions, even if the version being tested had a different support policy.
    
    This updates the workflow to be more flexible, allowing all versions of WordPress currently receiving security fixes (back through 4.1) to be tested under the correct support policy.
    
    Additionally, the workflow can now run against the `nightly` build of WordPress. This replaces `latest` as the new default. This allows the tests to be run at any point during a release cycle without the need for an officially tagged version.
    
    Props jorbin, joemcgill, costdev.
    See #58977.
    Built from https://develop.svn.wordpress.org/trunk@57218
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56724 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57217';
+$wp_version = '6.5-alpha-57218';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
