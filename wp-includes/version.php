d8d6bee968e5b0d8664f690a9cfd2d262b38e2b9
desrosj <desrosj@git.wordpress.org>
2023-12-22 01:14:20 +0000

    Build/Test Tools: Add missing PHP versions for 6.5 and 6.4.
    This adds 6.4 and 6.5 to the PHP version support JSON files used to build the test matrix in the Installation Testing workflow.
    
    Also, WP 6.3 did not support PHP 8.3.
    
    Follow up to [57218].
    
    See #58977.
    Built from https://develop.svn.wordpress.org/trunk@57219
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56725 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57218';
+$wp_version = '6.5-alpha-57219';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
