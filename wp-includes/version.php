45c67f7f1162d53d34f267d39e71904523c79db2
desrosj <desrosj@git.wordpress.org>
2023-12-22 01:38:23 +0000

    Build/Test Tools: Add missing MySQL versions for WordPress 5.0.
    Follow up to [57218], [57219], [57220].
    
    See #58977.
    Built from https://develop.svn.wordpress.org/trunk@57221
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56727 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57220';
+$wp_version = '6.5-alpha-57221';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
