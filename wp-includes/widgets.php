f06012264cdc64b19760478fd533032119eb01a4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2023-12-31 13:34:20 +0000

    Docs: Document the `$wp_registered_sidebars` global in `wp_map_sidebars_widgets()`.
    Follow-up to [41555].
    
    Props shailu25.
    See #60021.
    Built from https://develop.svn.wordpress.org/trunk@57234
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56740 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/widgets.php
+++ wp-includes/widgets.php
@@ -1370,6 +1370,8 @@ function retrieve_widgets( $theme_changed = false ) {
  * @since 4.9.0
  * @since 4.9.2 Always tries to restore widget assignments from previous data, not just if sidebars needed mapping.
  *
+ * @global array $wp_registered_sidebars The registered sidebars.
+ *
  * @param array $existing_sidebars_widgets List of sidebars and their widget instance IDs.
  * @return array Mapped sidebars widgets.
  */
