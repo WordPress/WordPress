26099a407d60ff62962fa882ad7ee795a492c05e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-01-09 11:41:18 +0000

    Docs: Add a mention of `appearance-tools` as a possible value for `add_theme_support()`.
    Follow-up to [57255].
    
    See #60118.
    Built from https://develop.svn.wordpress.org/trunk@57256
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56762 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/theme.php
+++ wp-includes/theme.php
@@ -2617,12 +2617,15 @@ function get_theme_starter_content() {
  * @since 5.6.0 The `post-formats` feature warns if no array is passed as the second parameter.
  * @since 5.8.0 The `widgets-block-editor` feature enables the Widgets block editor.
  * @since 6.0.0 The `html5` feature warns if no array is passed as the second parameter.
+ * @since 6.5.0 The `appearance-tools` feature enables a few design tools for blocks,
+ *              see `WP_Theme_JSON::APPEARANCE_TOOLS_OPT_INS` for a complete list.
  *
  * @global array $_wp_theme_features
  *
  * @param string $feature The feature being added. Likely core values include:
  *                          - 'admin-bar'
  *                          - 'align-wide'
+ *                          - 'appearance-tools'
  *                          - 'automatic-feed-links'
  *                          - 'core-block-patterns'
  *                          - 'custom-background'
