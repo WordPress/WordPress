41fe45ced7aa4e6bb3e744fedda2815ad69a94b9
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-01-13 16:33:18 +0000

    Tests: Correct the `@group` annotation in some tests.
    Follow-up to [56971], [57146].
    
    See #59647.
    Built from https://develop.svn.wordpress.org/trunk@57278
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56784 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57277';
+$wp_version = '6.5-alpha-57278';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
