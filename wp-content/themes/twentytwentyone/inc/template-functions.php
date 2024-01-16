48f19a08aa43f15c51a7bafe4ddf5229cac09a11
desrosj <desrosj@git.wordpress.org>
2024-01-16 16:12:06 +0000

    Twenty Twenty-One: Revert usage of str_contains() in theme files.
    The theme supports WordPress 5.3 or later, while the polyfill for str_contains() only exists in WordPress 5.9 or later.
    
    Follow-up to [55988], [57275], [57276].
    
    Merges [57277] to the 6.4 branch.
    
    Props poena, SergeyBiryukov.
    Fixes #60241.
    Built from https://develop.svn.wordpress.org/branches/6.4@57293
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.4@56799 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwentyone/inc/template-functions.php
+++ wp-content/themes/twentytwentyone/inc/template-functions.php
@@ -445,7 +445,7 @@ function twenty_twenty_one_get_attachment_image_attributes( $attr, $attachment, 
 		return $attr;
 	}
 
-	if ( isset( $attr['class'] ) && str_contains( $attr['class'], 'custom-logo' ) ) {
+	if ( isset( $attr['class'] ) && false !== strpos( $attr['class'], 'custom-logo' ) ) {
 		return $attr;
 	}
 
