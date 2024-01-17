2ba732f9565449dc71ab838dfc7db63fbc386dfa
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-01-17 18:00:16 +0000

    Twenty Twenty-Four: Remove extra tab character inside the text domain.
    Follow-up to [57281].
    
    Props sabernhardt.
    Fixes #60245.
    Built from https://develop.svn.wordpress.org/trunk@57304
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56810 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57303';
+$wp_version = '6.5-alpha-57304';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
