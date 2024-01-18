5277ebbe4db824e9dfb6c68b95632295eb453c6b
Peter Wilson <wilson@peterwilson.cc>
2024-01-18 03:01:12 +0000

    Twenty Twenty-Four: Add missing textdomain in pattern category description.
    Follow-up to [56951].
    
    Reviewed by jorbin.
    Merges [57127] to the 6.4 branch.
    
    Props gavande1, acosmin, afercia, huzaifaalmesbah, ironprogrammer, SergeyBiryukov.
    Fixes #59840.
    
    
    
    Built from https://develop.svn.wordpress.org/branches/6.4@57307
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.4@56813 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4.3-alpha-57297';
+$wp_version = '6.4.3-alpha-57307';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
