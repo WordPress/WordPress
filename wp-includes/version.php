5056d865f721558ac20306bbce19964cf60ba7d2
Aaron Jorbin <aaron@jorb.in>
2024-01-26 00:54:15 +0000

    Build/Tests: Ensure set_error_handler is cleaned up.
    Follow up to: [57332].
    
    Fixes #60305.
    
    
    Built from https://develop.svn.wordpress.org/trunk@57361
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56867 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57357';
+$wp_version = '6.5-alpha-57361';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
