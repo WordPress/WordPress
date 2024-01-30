45fc776d33efb93045fee749daf48d2574154632
youknowriad <youknowriad@git.wordpress.org>
2024-01-30 11:41:17 +0000

    Editor: Update the minimum compatible version of Gutenberg.
    Previous Gutenberg versions are not compatible with recent trunk because of the
    WP_Navigation_Block_Renderer classname. It's present in both.
    
    Gutenberg has been updated to avoid the use of this class but we need to auto-disable 
    old plugins to avoid fatals.
    
    Props hellofromtonya.
    See #60315.
    Built from https://develop.svn.wordpress.org/trunk@57384
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@56890 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57383';
+$wp_version = '6.5-alpha-57384';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
