2c109cc2d402bcf9f9613dc3b30a0ffa0044e432
Joe McGill <joemcgill@gmail.com>
2024-01-30 18:22:59 +0000

    General: Backport polyfills for str_ends_with() and str_starts_with() .
    Merges [52040], [56016], and [56015] to 5.5 branch.
    
    Props ocean90, SergeyBiryukov, desrosj, joemcgill, jorbin, mukesh27.
    
    Built from https://develop.svn.wordpress.org/branches/5.5@57458
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@56959 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/compat.php
+++ wp-includes/compat.php
@@ -370,3 +370,49 @@ if ( ! function_exists( 'is_iterable' ) ) {
 		return ( is_array( $var ) || $var instanceof Traversable );
 	}
 }
+
+if ( ! function_exists( 'str_starts_with' ) ) {
+	/**
+	 * Polyfill for `str_starts_with()` function added in PHP 8.0.
+	 *
+	 * Performs a case-sensitive check indicating if
+	 * the haystack begins with needle.
+	 *
+	 * @since 5.9.0
+	 *
+	 * @param string $haystack The string to search in.
+	 * @param string $needle   The substring to search for in the `$haystack`.
+	 * @return bool True if `$haystack` starts with `$needle`, otherwise false.
+	 */
+	function str_starts_with( $haystack, $needle ) {
+		if ( '' === $needle ) {
+			return true;
+		}
+
+		return 0 === strpos( $haystack, $needle );
+	}
+}
+
+if ( ! function_exists( 'str_ends_with' ) ) {
+	/**
+	 * Polyfill for `str_ends_with()` function added in PHP 8.0.
+	 *
+	 * Performs a case-sensitive check indicating if
+	 * the haystack ends with needle.
+	 *
+	 * @since 5.9.0
+	 *
+	 * @param string $haystack The string to search in.
+	 * @param string $needle   The substring to search for in the `$haystack`.
+	 * @return bool True if `$haystack` ends with `$needle`, otherwise false.
+	 */
+	function str_ends_with( $haystack, $needle ) {
+		if ( '' === $haystack ) {
+			return '' === $needle;
+		}
+
+		$len = strlen( $needle );
+
+		return substr( $haystack, -$len, $len ) === $needle;
+	}
+}
