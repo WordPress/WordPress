6367bd749dcfa4b2c66542e8d0d1415660591400
Joe McGill <joemcgill@gmail.com>
2024-01-30 16:29:03 +0000

    WordPress 5.6.13.
    Built from https://develop.svn.wordpress.org/branches/5.6@57430
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.6@56936 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.6.12';
+$wp_version = '5.6.13';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
