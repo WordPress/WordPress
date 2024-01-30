0f1f3959518924ce1b7bb089f343985b05fcf0fd
Joe McGill <joemcgill@gmail.com>
2024-01-30 17:00:28 +0000

    WordPress 6.3.3.
    Built from https://develop.svn.wordpress.org/branches/6.3@57437
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.3@56943 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -49,6 +49,26 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 				<p>
 					<?php
 					printf(
+						/* translators: %s: WordPress version. */
+						__( '<strong>Version %s</strong> addressed some security issues.' ),
+						'6.3.3',
+					);
+					?>
+					<?php
+					printf(
+						/* translators: %s: HelpHub URL. */
+						__( 'For more information, see <a href="%s">the release notes</a>.' ),
+						sprintf(
+							/* translators: %s: WordPress version. */
+							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							sanitize_title( '6.3.3' )
+						)
+					);
+					?>
+				</p>
+				<p>
+					<?php
+					printf(
 						/* translators: 1: WordPress version number, 2: Plural number of bugs. More than one security issue. */
 						_n(
 							'<strong>Version %1$s</strong> addressed some security issues and fixed %2$s bug.',
@@ -98,7 +118,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 				</p>
 			</div>
 		</div>
-		
+
 		<div class="about__section aligncenter">
 			<div class="column">
 				<h2>
