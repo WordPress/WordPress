64a16b95a43e206cf9320ab4914d58e4a628e3c0
Joe McGill <joemcgill@gmail.com>
2024-01-30 16:25:56 +0000

    WordPress 5.4.15.
    Built from https://develop.svn.wordpress.org/branches/5.4@57428
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.4@56934 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4.14';
+$wp_version = '5.4.15';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
