ec68b4743ecc043168991b86e311b0d4b2a7d876
Pascal Birchler <pascal.birchler@gmail.com>
2024-01-31 21:51:14 +0000

    I18N: Revert [57386] pending further investigation.
    Reverts the change for fallback string lookup due to a performance regression in the bad case scenario.
    
    See #59656.
    Built from https://develop.svn.wordpress.org/trunk@57505
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57006 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/l10n/class-wp-translation-file.php
+++ wp-includes/l10n/class-wp-translation-file.php
@@ -203,24 +203,7 @@ abstract class WP_Translation_File {
 			$this->parse_file();
 		}
 
-		if ( isset( $this->entries[ $text ] ) ) {
-			return $this->entries[ $text ];
-		}
-
-		/*
-		 * Handle cases where a pluralized string is only used as a singular one.
-		 * For example, when both __( 'Product' ) and _n( 'Product', 'Products' )
-		 * are used, the entry key will have the format "ProductNULProducts".
-		 * Fall back to looking up just "Product" to support this edge case.
-		 */
-		foreach ( $this->entries as $key => $value ) {
-			if ( str_starts_with( $key, $text . "\0" ) ) {
-				$parts = explode( "\0", $value );
-				return $parts[0];
-			}
-		}
-
-		return false;
+		return $this->entries[ $text ] ?? false;
 	}
 
 	/**
