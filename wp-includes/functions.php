a8616f8e3330441a6196dd279ce1761d5f2677d4
Pascal Birchler <pascal.birchler@gmail.com>
2024-02-01 08:40:16 +0000

    Docs: Fix typo in `do_robots()` docblock.
    This was introduced in [45928].
    
    Props shailu25, mukesh27.
    Fixes #60405.
    Built from https://develop.svn.wordpress.org/trunk@57509
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57010 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -1692,7 +1692,7 @@ function do_feed_atom( $for_comments ) {
  * Displays the default robots.txt file content.
  *
  * @since 2.1.0
- * @since 5.3.0 Remove the "Disallow: /" output if search engine visiblity is
+ * @since 5.3.0 Remove the "Disallow: /" output if search engine visibility is
  *              discouraged in favor of robots meta HTML tag via wp_robots_no_robots()
  *              filter callback.
  */
