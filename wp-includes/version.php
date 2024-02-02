a9bb1bb5f7fb02912c92ef3875586b36466654ab
Andrea Fercia <a.fercia@gmail.com>
2024-02-02 09:55:14 +0000

    Administration: Accessibility: Use the default cursor style for labels and disabled form controls.
    The native cursor style for labels and form controls is `default`, which is the platform-dependent default cursor. Typically an arrow. Historically, WordPress always used the `pointer` style for all form controls and labels. While this isn't standard, there is some value in using the `pointer` style for form controls. However, labels should use the default style especially when the associated controls are disabled.
    Additionally, makes sure the disabled styling works for form controls with an `aria-disabled="true"` attribute.
    
    Props joedolson, afercia.
    Fixes #59733.
    
    Built from https://develop.svn.wordpress.org/trunk@57520
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57021 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57519';
+$wp_version = '6.5-alpha-57520';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
