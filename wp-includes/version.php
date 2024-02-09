336be7695cb065fbf349274e5baaaf8cbd2df41d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-02-09 17:57:13 +0000

    Twenty Sixteen: Add `border-radius` to avatar images in the editor.
    This ensures that avatars design in the Post Author or Avatar blocks in the editor matches the front end.
    
    Props pitamdey, sabernhardt, poena, shailu25.
    Fixes #59253.
    Built from https://develop.svn.wordpress.org/trunk@57577
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57078 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57576';
+$wp_version = '6.5-alpha-57577';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
