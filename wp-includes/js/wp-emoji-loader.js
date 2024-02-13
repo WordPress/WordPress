e05dc67dc65463919e869563f373ed899c455ed5
desrosj <desrosj@git.wordpress.org>
2024-02-13 14:36:07 +0000

    Emoji: Replace `twitter/twemoji` with `jdecked/twemoji`.
    After a chaotic change of ownership, the `twitter/twemoji` library is now considered abandoned.
    
    After waiting for this moment to arise, a fork was created by several former employees who had maintained the library which lives at `jdecked/twemoji` on GitHub.
    
    This switches out where the underlying source code comes from for the library, and applies the 15.0.3 update, which adheres to the Unicode 15 spec and adds support for all Emoji introduced in Emoji 15.0.
    
    This does not update the underlying `precommit:emoji` Grunt script responsible for updating `formatting.php`. After GitHub recently sunset support for SVN, the current process needs to be replaced with a new one. This will be handled in #60520.
    
    Let the masses rejoice for the 🐦‍⬛ singing in the dead of night, secure 🛜, aromatic 🫚, and some very silly 🪿🪿.
    
    RIP Twemoji. Long live Twemoji!
    
    Props peterwilsoncc, kraftbj, jeffpaul, azaozz, dd32, hareesh-pillai, jorbin.
    Fixes #57600.
    Built from https://develop.svn.wordpress.org/trunk@57626
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57127 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/wp-emoji-loader.js
+++ wp-includes/js/wp-emoji-loader.js
@@ -232,27 +232,24 @@
 				return ! isIdentical;
 			case 'emoji':
 				/*
-				 * Why can't we be friends? Everyone can now shake hands in emoji, regardless of skin tone!
+				 * Four and twenty blackbirds baked in a pie.
 				 *
-				 * To test for Emoji 14.0 support, try to render a new emoji: Handshake: Light Skin Tone, Dark Skin Tone.
+				 * To test for Emoji 15.0 support, try to render a new emoji: Blackbird.
 				 *
-				 * The Handshake: Light Skin Tone, Dark Skin Tone emoji is a ZWJ sequence combining 🫱 Rightwards Hand,
-				 * 🏻 Light Skin Tone, a Zero Width Joiner, 🫲 Leftwards Hand, and 🏿 Dark Skin Tone.
+				 * The Blackbird is a ZWJ sequence combining 🐦 Bird and ⬛ large black square.,
 				 *
-				 * 0x1FAF1 == Rightwards Hand
-				 * 0x1F3FB == Light Skin Tone
+				 * 0x1F426 (\uD83D\uDC26) == Bird
 				 * 0x200D == Zero-Width Joiner (ZWJ) that links the code points for the new emoji or
 				 * 0x200B == Zero-Width Space (ZWS) that is rendered for clients not supporting the new emoji.
-				 * 0x1FAF2 == Leftwards Hand
-				 * 0x1F3FF == Dark Skin Tone.
+				 * 0x2B1B == Large Black Square
 				 *
 				 * When updating this test for future Emoji releases, ensure that individual emoji that make up the
 				 * sequence come from older emoji standards.
 				 */
 				isIdentical = emojiSetsRenderIdentically(
 					context,
-					'\uD83E\uDEF1\uD83C\uDFFB\u200D\uD83E\uDEF2\uD83C\uDFFF', // as the zero-width joiner sequence
-					'\uD83E\uDEF1\uD83C\uDFFB\u200B\uD83E\uDEF2\uD83C\uDFFF' // separated by a zero-width space
+					'\uD83D\uDC26\u200D\u2B1B', // as the zero-width joiner sequence
+					'\uD83D\uDC26\u200B\u2B1B' // separated by a zero-width space
 				);
 
 				return ! isIdentical;
