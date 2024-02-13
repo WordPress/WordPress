2a78e6400c1ea384af7bd0a731a448de540021f1
Pascal Birchler <pascal.birchler@gmail.com>
2024-02-13 13:27:05 +0000

    Twenty Twenty-Four: Wrap header navigation block in row.
    Makes it easier to add siblings using the block hooks API without them being treated as flex items due to its parent being a row block.
    
    Props tomjcafferkey, poena.
    Fixes #60410.
    Built from https://develop.svn.wordpress.org/trunk@57621
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57122 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-alpha-57620';
+$wp_version = '6.5-alpha-57621';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
