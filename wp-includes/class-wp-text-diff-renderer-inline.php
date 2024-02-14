b37e80c59fea7c4482c452b301a5f7752b0ac87d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-02-14 19:27:09 +0000

    Coding Standards: Allow `$newlineEscape` parameter in `WP_Text_Diff_Renderer_inline::_splitOnWords()`.
    This resolves a WPCS warning:
    {{{
    Variable "$newlineEscape" is not in valid snake_case format, try "$newline_escape"
    }}}
    
    The `WP_Text_Diff_Renderer_inline` class extends the `Text_Diff_Renderer_inline` class from the `Text_Diff` package and should have the same parameters as the parent class method, per the Task 1 section of ticket #51553.
    
    Follow-up to [7747], [55163].
    
    See #59650.
    Built from https://develop.svn.wordpress.org/trunk@57633
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57134 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-text-diff-renderer-inline.php
+++ wp-includes/class-wp-text-diff-renderer-inline.php
@@ -24,10 +24,10 @@ class WP_Text_Diff_Renderer_inline extends Text_Diff_Renderer_inline {
 	 * @param string $newlineEscape
 	 * @return string
 	 */
-	public function _splitOnWords( $string, $newlineEscape = "\n" ) { // phpcs:ignore Universal.NamingConventions.NoReservedKeywordParameterNames.stringFound
+	public function _splitOnWords( $string, $newlineEscape = "\n" ) { // phpcs:ignore Universal.NamingConventions.NoReservedKeywordParameterNames.stringFound,WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
 		$string = str_replace( "\0", '', $string );
 		$words  = preg_split( '/([^\w])/u', $string, -1, PREG_SPLIT_DELIM_CAPTURE );
-		$words  = str_replace( "\n", $newlineEscape, $words );
+		$words  = str_replace( "\n", $newlineEscape, $words ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
 		return $words;
 	}
 }
