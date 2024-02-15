268ad5114fc093e913c8d285cfbb87f1c6aba723
desrosj <desrosj@git.wordpress.org>
2024-02-15 14:49:16 +0000

    Build/Test Tools: Remove unnecessary direct dependencies.
    This removes the `@emotion/is-prop-valid`, `@emotion/memoize`, and `framer-motion` packages as direct dependencies.
    
    These were introduced in [56065], but do not appear to have been integrated into the build process. Instead, these are now installed as peer dependencies to the `@wordpress/block-editor` and `@wordpress/components` packages.
    
    Props gziolo, huzaifaalmesbah.
    See #59658.
    Built from https://develop.svn.wordpress.org/trunk@57634
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57135 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-beta1-57633';
+$wp_version = '6.5-beta1-57634';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
