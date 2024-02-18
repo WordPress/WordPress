0f6fc935fe4501631f769540103c1b9e583521f9
Pascal Birchler <pascal.birchler@gmail.com>
2024-02-18 14:59:07 +0000

    Editor: Prevent PHP warning when parsing duotone hue values.
    Props jacobcassidy, rahmohn, mukesh27.
    Fixes #59496.
    Built from https://develop.svn.wordpress.org/trunk@57652
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57153 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-duotone.php
+++ wp-includes/class-wp-duotone.php
@@ -209,10 +209,7 @@ class WP_Duotone {
 			'rad'  => 360 / ( M_PI * 2 ),
 		);
 
-		$factor = $angle_units[ $unit ];
-		if ( ! $factor ) {
-			$factor = 1;
-		}
+		$factor = isset( $angle_units[ $unit ] ) ? $angle_units[ $unit ] : 1;
 
 		return (float) $value * $factor;
 	}
