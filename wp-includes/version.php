f99d9988f467c38b6c480d69d99a2ce6b8ba661a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-02-20 18:16:09 +0000

    Tests: Use `assertSame()` in `wp_read_image_metadata()` tests.
    This ensures that not only the return values match the expected results, but also that their type is the same.
    
    Going forward, stricter type checking by using `assertSame()` should generally be preferred to `assertEquals()` where appropriate, to make the tests more reliable.
    
    Follow-up to [49/tests], [198/tests], [34374], [48937], [52269].
    
    Props costdev.
    See #59655.
    Built from https://develop.svn.wordpress.org/trunk@57673
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57174 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-beta2-57671';
+$wp_version = '6.5-beta2-57673';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
