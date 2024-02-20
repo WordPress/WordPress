e12048f2d298c42e248a37af7b5e51ba0a4cc91f
audrasjb <audrasjb@git.wordpress.org>
2024-02-20 17:06:06 +0000

    WordPress 6.5 Beta 2.
    Built from https://develop.svn.wordpress.org/trunk@57670
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57171 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-beta1-57669';
+$wp_version = '6.5-beta2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
