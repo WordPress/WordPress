f95f3e8334c3f7057fd29917432dddd0d854e4e5
joedolson <joedolson@git.wordpress.org>
2024-02-21 20:57:12 +0000

    Media: Fix broken media modal following [57688]
    Fix undefined TypeError overlooked in [57688].
    
    Props antpb, swissspidy, joedolson.
    Fixes #58973.
    Built from https://develop.svn.wordpress.org/trunk@57691
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57192 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/media-views.js
+++ wp-includes/js/media-views.js
@@ -2282,12 +2282,16 @@ var State = Backbone.Model.extend(/** @lends wp.media.controller.State.prototype
 	_menu: function() {
 		var menu = this.frame.menu,
 			mode = this.get('menu'),
-			actionMenuItems = this.frame.menu.get('views'),
-			actionMenuLength = actionMenuItems ? actionMenuItems.views.get().length : 0,
+			actionMenuItems,
+			actionMenuLength,
 			view;
 
-		// Show action menu only if it is active and has more than one default element.
-		this.frame.$el.toggleClass( 'hide-menu', ! mode || actionMenuLength < 2 );
+		if ( this.frame.menu ) {
+			actionMenuItems = this.frame.menu.get('views'),
+			actionMenuLength = actionMenuItems ? actionMenuItems.views.get().length : 0,
+			// Show action menu only if it is active and has more than one default element.
+			this.frame.$el.toggleClass( 'hide-menu', ! mode || actionMenuLength < 2 );
+		}
 		if ( ! mode ) {
 			return;
 		}
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-beta2-57690';
+$wp_version = '6.5-beta2-57691';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
