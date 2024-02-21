83ba8ad091588c04cddb022090a60fe391b52ccd
joedolson <joedolson@git.wordpress.org>
2024-02-21 20:08:06 +0000

    Twenty Twenty-Three: Update XX-large font-sizes for accessibility.
    Update XX-large font-sizes so that the responsive typography meets the minimum ratio between the min and max font-sizes of 0.61 required for accessible responsive typography.
    
    Props mikachan, ryokuhi, poena, dhruvishah2203, joedolson.
    Fixes #58603.
    Built from https://develop.svn.wordpress.org/trunk@57690
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57191 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-beta2-57689';
+$wp_version = '6.5-beta2-57690';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
