5c49b1cc7bb92d5d97c4c438e674aa88ceb6c867
hellofromTonya <hellofromTonya@git.wordpress.org>
2024-02-21 20:06:12 +0000

    Build/Test Tools: Revert r57684.
    Changes to the `test_readme_mariadb_version()` test were made in an effort to stabilize the connection tests and CI failures. After the changeset, all multisite tests CI jobs connected and passed, but oddly all single site tests CI jobs repeatedly and consistently failed.
    
    Reverting to unblock the CI jobs while continued investigation happens.
    
    See #59647.
    Built from https://develop.svn.wordpress.org/trunk@57689
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57190 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-beta2-57688';
+$wp_version = '6.5-beta2-57689';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
