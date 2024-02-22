0006dd82d81ad05cf3da6bfe413a495d2128c844
Pascal Birchler <pascal.birchler@gmail.com>
2024-02-22 18:04:09 +0000

    Upgrade/Install: Improve plugin install button appearance.
    Fixes a misaligned spinner icon when opening the plugin details information in a new tab rather than the modal, and then installing the plugin in that window.
    
    Props huzaifaalmesbah, zunaid321, swissspidy.
    Fixes #60494.
    Built from https://develop.svn.wordpress.org/trunk@57697
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57198 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-beta2-57696';
+$wp_version = '6.5-beta2-57697';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
