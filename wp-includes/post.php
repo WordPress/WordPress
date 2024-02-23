096df041b623c91d0b0dc0401240613022cffc65
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-02-23 09:43:12 +0000

    Docs: Add a `@since` note for `$preferred_ext` parameter in `wp_mime_type_icon()`.
    Follow-up to [57687].
    
    See #31352.
    Built from https://develop.svn.wordpress.org/trunk@57701
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57202 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -6802,9 +6802,10 @@ function wp_attachment_is_image( $post = null ) {
  * Retrieves the icon for a MIME type or attachment.
  *
  * @since 2.1.0
+ * @since 6.5.0 Added the `$preferred_ext` parameter.
  *
  * @param string|int $mime          MIME type or attachment ID.
- * @param string     $preferred_ext File format to prefer in return. Default .png.
+ * @param string     $preferred_ext File format to prefer in return. Default '.png'.
  * @return string|false Icon, false otherwise.
  */
 function wp_mime_type_icon( $mime = 0, $preferred_ext = '.png' ) {
