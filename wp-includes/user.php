60b72e87d747a5e3d4b2bc49c1a8575404377c56
Pascal Birchler <pascal.birchler@gmail.com>
2024-02-26 10:43:06 +0000

    Login and Registration: Slash email address when updating an existing user.
    Addresses an issue with password reset keys when the email address contains special characters such as apostrophes.
    
    Props emirpprime, rajinsharwar, fnpen, hellofromTonya, oglekler, nicolefurlan.
    Fixes #52529.
    Built from https://develop.svn.wordpress.org/trunk@57711
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57212 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -2096,6 +2096,9 @@ function wp_insert_user( $userdata ) {
 			return new WP_Error( 'invalid_user_id', __( 'Invalid user ID.' ) );
 		}
 
+		// Slash current user email to compare it later with slashed new user email.
+		$old_user_data->user_email = wp_slash( $old_user_data->user_email );
+
 		// Hashed in wp_update_user(), plaintext if called directly.
 		$user_pass = ! empty( $userdata['user_pass'] ) ? $userdata['user_pass'] : $old_user_data->user_pass;
 	} else {
