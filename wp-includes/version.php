69a2411a72a3554d6f84d845b9cbd73e67288057
Pascal Birchler <pascal.birchler@gmail.com>
2024-02-27 17:17:09 +0000

    Build/Test Tools: Add unit tests for `wp_parse_slug_list()`.
    Props pbearne.
    Fixes #60217.
    Built from https://develop.svn.wordpress.org/trunk@57725
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57226 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-beta3-57724';
+$wp_version = '6.5-beta3-57725';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
