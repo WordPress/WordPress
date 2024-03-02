690fe018e1cda00b69f6c35145cadd6cabab646f
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-03-02 13:49:06 +0000

    Docs: Document the `$xmlrpc_logging` global in `logIO()`.
    Follow-up to [1348], [19935].
    
    Props viralsampat, upadalavipul, sabernhardt.
    See #60021.
    Built from https://develop.svn.wordpress.org/trunk@57752
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57253 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- xmlrpc.php
+++ xmlrpc.php
@@ -91,10 +91,13 @@ exit;
 /**
  * logIO() - Writes logging info to a file.
  *
+ * @since 1.2.0
  * @deprecated 3.4.0 Use error_log()
  * @see error_log()
  *
- * @param string $io Whether input or output
+ * @global int|bool $xmlrpc_logging Whether to enable XML-RPC logging.
+ *
+ * @param string $io  Whether input or output.
  * @param string $msg Information describing logging reason.
  */
 function logIO( $io, $msg ) {
