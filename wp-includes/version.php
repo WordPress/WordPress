175bea53b7c6ca4c1dc9c09527b4fd2a4862307c
Pascal Birchler <pascal.birchler@gmail.com>
2024-03-07 06:27:10 +0000

    Build/Test Tools: Fix typo in `variation-new-font-family.json` fixture file.
    Props huzaifaalmesbah, mukesh27.
    Fixes #60712.
    Built from https://develop.svn.wordpress.org/trunk@57788
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57289 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-57787';
+$wp_version = '6.6-alpha-57788';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
