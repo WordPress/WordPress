a5a961a3f75b96e53905387466e37758d18c37e4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-03-13 13:20:14 +0000

    Coding Standards: Update the config for `WordPress.PHP.NoSilencedErrors` in PHPCS ruleset.
    Includes alphabetizing the third party library exclusions section.
    
    Follow-up to [50810], [51658], [57524].
    
    See #60700.
    Built from https://develop.svn.wordpress.org/trunk@57829
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57330 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-57827';
+$wp_version = '6.6-alpha-57829';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
