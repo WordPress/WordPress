9eed18532d03558198137d22bdda2be234aef67e
Pascal Birchler <pascal.birchler@gmail.com>
2024-03-14 15:12:10 +0000

    Interactivity API: Do not propagate context from void tags to its siblings.
    Resolves an issue where context on a void tag element such as `<img>` was incorrectly passed to following elements.
    Adds tests.
    
    Reviewed by gziolo.
    Merges [57832] to the to the 6.5 branch.
    
    Props santosguillamot, luisherranz, cbravobernal, dmsnell, gziolo, swissspidy.
    Fixes #60768.
    Built from https://develop.svn.wordpress.org/branches/6.5@57834
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@57335 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-RC2-57830';
+$wp_version = '6.5-RC2-57834';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
