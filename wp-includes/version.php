1a0add316fb3704f35a808b6fb0ea6f3a198c7da
audrasjb <audrasjb@git.wordpress.org>
2024-03-19 14:46:15 +0000

    Docs: Update various HelpHub links to avoid unnecessary redirections.
    Follow-up to [57793], [57798], [57800], [57801].
    
    Props shailu25.
    See #60732, #60699.
    
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@57854
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57355 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-57853';
+$wp_version = '6.6-alpha-57854';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
