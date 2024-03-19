5cb687dbce0a91e057fb3010dbdbd163e7863eb1
Pascal Birchler <pascal.birchler@gmail.com>
2024-03-19 09:33:11 +0000

    Posts, Post Types: Introduce `delete_post_{$post->post_type}` and `deleted_post_{$post->post_type}` hooks.
    The hooks fire before the general `delete_post` / `deleted_post` hooks and have the same parameters.
    
    They complement the `save_post_{$post->post_type}` hook added in [25050] and the `edit_post_{$post->post_type}` hook added in [43617].
    
    Props benniledl, swissspidy, dargus.
    Fixes #60433.
    Built from https://develop.svn.wordpress.org/trunk@57853
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57354 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -3555,6 +3555,19 @@ function wp_delete_post( $postid = 0, $force_delete = false ) {
 	/**
 	 * Fires immediately before a post is deleted from the database.
 	 *
+	 * The dynamic portion of the hook name, `$post->post_type`, refers to
+	 * the post type slug.
+	 *
+	 * @since 6.6.0
+	 *
+	 * @param int     $postid Post ID.
+	 * @param WP_Post $post   Post object.
+	 */
+	do_action( "delete_post_{$post->post_type}", $postid, $post );
+
+	/**
+	 * Fires immediately before a post is deleted from the database.
+	 *
 	 * @since 1.2.0
 	 * @since 5.5.0 Added the `$post` parameter.
 	 *
@@ -3567,6 +3580,19 @@ function wp_delete_post( $postid = 0, $force_delete = false ) {
 	if ( ! $result ) {
 		return false;
 	}
+
+	/**
+	 * Fires immediately after a post is deleted from the database.
+	 *
+	 * The dynamic portion of the hook name, `$post->post_type`, refers to
+	 * the post type slug.
+	 *
+	 * @since 6.6.0
+	 *
+	 * @param int     $postid Post ID.
+	 * @param WP_Post $post   Post object.
+	 */
+	do_action( "deleted_post_{$post->post_type}", $postid, $post );
 
 	/**
 	 * Fires immediately after a post is deleted from the database.
