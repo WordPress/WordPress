abedb8facaa03cc6f9fad0008e0957e85966df9a
audrasjb <audrasjb@git.wordpress.org>
2024-03-22 15:26:11 +0000

    I18n: Ensure "Patterns" menu item is translatable.
    This changeset updates a translation string to ensure the "Patterns" menu item introduced in [57543] is translatable.
    This quickfix simply removes the erroneous `context` parameter so no new string is introduced during WP 6.5 string freeze period. A follow-up changeset will be needed to replace the current `__()` function with `_x()` and put back the `context` parameter.
    
    Follow-up to [57543].
    
    Reviewed by swissspidy.
    Merges [57864] to the to the 6.5 branch.
    
    Props jdy68, audrasjb, kebbet, swissspidy.
    Fixes #60825.
    
    
    
    Built from https://develop.svn.wordpress.org/branches/6.5@57866
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@57367 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5-RC3-57856';
+$wp_version = '6.5-RC3-57866';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
