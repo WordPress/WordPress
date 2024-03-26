d676def4a87551476ae94c4846f111e04a67fd8c
Pascal Birchler <pascal.birchler@gmail.com>
2024-03-26 09:39:11 +0000

    Media: Fix CSS issue preventing inserting images on smaller viewports.
    Addresses a regression introduced in [57605] where the “Select” button in the media modal was not clickable anymore due to an overlaid element.
    
    Props ramonopoly, swissspidy, freewebmentor.
    Fixes #33049.
    Built from https://develop.svn.wordpress.org/trunk@57881
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57382 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-57878';
+$wp_version = '6.6-alpha-57881';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
