af0a8e253d5ab1d10e9049dd9898e447df6ea867
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-03-27 12:30:12 +0000

    Coding Standards: Use strict comparison in `wp-includes/pomo/plural-forms.php`.
    Follow-up to [41722].
    
    Props aristath, poena, afercia, SergeyBiryukov.
    See #60700.
    Built from https://develop.svn.wordpress.org/trunk@57883
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57384 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-57882';
+$wp_version = '6.6-alpha-57883';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
