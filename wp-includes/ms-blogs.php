1dc3f878703ff788a5dc1b06efda6c665554dcb3
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-03-29 11:42:13 +0000

    Networks and Sites: Remove unused `$wpdb` global in `update_blog_details()`.
    Follow-up to [43548].
    
    Props viralsampat.
    See #60021.
    Built from https://develop.svn.wordpress.org/trunk@57898
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57399 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-blogs.php
+++ wp-includes/ms-blogs.php
@@ -293,15 +293,11 @@ function refresh_blog_details( $blog_id = 0 ) {
  *
  * @since MU (3.0.0)
  *
- * @global wpdb $wpdb WordPress database abstraction object.
- *
  * @param int   $blog_id Blog ID.
  * @param array $details Array of details keyed by blogs table field names.
  * @return bool True if update succeeds, false otherwise.
  */
 function update_blog_details( $blog_id, $details = array() ) {
-	global $wpdb;
-
 	if ( empty( $details ) ) {
 		return false;
 	}
