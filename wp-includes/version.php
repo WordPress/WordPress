657f36adfa683121a464560ef7fd3ee013793c18
desrosj <desrosj@git.wordpress.org>
2024-04-02 19:19:17 +0000

    Build/Test Tools: Include 6.4 and 6.5 in upgrade testing.
    This ensures that upgrading from 6.4 and 6.5 to the desired version is actually tested.
    
    See #60733.
    Built from https://develop.svn.wordpress.org/trunk@57915
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57416 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-57911';
+$wp_version = '6.6-alpha-57915';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
