db1e75b20a37a7eba598fa3de5e3acf193c22c94
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-04-02 19:24:16 +0000

    Docs: Update `@since` version for `wp_zip_file_is_valid()`.
    Follow-up to [57537].
    
    Props TobiasBg.
    See #60398.
    Built from https://develop.svn.wordpress.org/trunk@57916
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57417 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/file.php
+++ wp-admin/includes/file.php
@@ -1569,7 +1569,7 @@ function wp_trusted_keys() {
  * This function does not test to ensure that a file exists. Non-existent files
  * are not valid ZIPs, so those will also return false.
  *
- * @since 6.4.4
+ * @since 6.5.0
  *
  * @param string $file Full path to the ZIP file.
  * @return bool Whether the file is a valid ZIP file.
