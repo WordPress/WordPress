440a5f21cab0270df3667fb8be46e38410eb5def
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-04-08 19:21:16 +0000

    Twenty Twenty-Four: Remove pattern from home template to improve performance.
    Creating the Home Business pattern was needed to show it on the template replacement flows, but there's no need to use it in the template, and the minor code repetition is better than having to do a an extra pattern replacement.
    
    Follow-up to [https://github.com/WordPress/twentytwentyfour/pull/486 PR #486].
    
    Props onemaggie, youknowriad, poena, afercia.
    Fixes #60620.
    Built from https://develop.svn.wordpress.org/trunk@57945
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57442 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-57944';
+$wp_version = '6.6-alpha-57945';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
