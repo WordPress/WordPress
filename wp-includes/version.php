8fbd2fc6f40ea1f2ad746758b7111a66ab134e19
isabel_brison <isabel_brison@git.wordpress.org>
2024-04-09 07:20:15 +0000

    Editor: skip outputting base layout rules if content and wide size values don’t exist.
    Skip outputting layout rules that reference content and wide sizes CSS variables, if no layout sizes exist in the current `theme.json`.
    
    Props andrewserong.
    Fixes #60936.
    
    Built from https://develop.svn.wordpress.org/trunk@57948
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57445 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-57946';
+$wp_version = '6.6-alpha-57948';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
