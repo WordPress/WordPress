262144d2d47b8fee53f42b58aa7dccd25ed6ba70
John Blackbourn <johnbillion@git.wordpress.org>
2024-04-09 15:26:14 +0000

    WordPress 6.5.1.
    Built from https://develop.svn.wordpress.org/branches/6.5@57959
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@57453 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5.1-alpha-57950';
+$wp_version = '6.5.1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
