8689afae772abf5b262ad53adf77c3378f2a80b8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-04-14 21:57:13 +0000

    Twenty Twenty-One: Check for `WP_Error` before outputting `get_the_tag_list()`.
    This prevents a fatal error on PHP 8 and brings consistency with the other bundled themes.
    
    Follow-up to [47886].
    
    Props josephscott, sabernhardt, poena, nirav7707, devsahadat.
    Fixes #60800.
    Built from https://develop.svn.wordpress.org/trunk@57991
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57477 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-57990';
+$wp_version = '6.6-alpha-57991';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
