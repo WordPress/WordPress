44528d4fba2006278aa0c5cca5f73d4f51eeec28
Tammie Lister <tammie@automattic.com>
2024-04-15 16:26:15 +0000

    Twenty Twenty Two: Wraps Navigation block in row.
    Now that the Blocks Hooks API is released in 6.4, you might want to insert blocks into header of the theme after the Navigation block. This wraps to allow this.
    
    Props tomjcafferkey, poena, shailu25.
    Fixes #60724.
    
    Built from https://develop.svn.wordpress.org/trunk@58004
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57478 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-57991';
+$wp_version = '6.6-alpha-58004';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
