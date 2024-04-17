031702359a5a72a3b23d3a206453efeb9a7322e6
joedolson <joedolson@git.wordpress.org>
2024-04-17 19:15:14 +0000

    Help/About: Accessibility: Remove `target="_blank"` from data eraser links.
    Remove `target="_blank"` from two links to developer resources on adding the personal data eraser to plugins in the Help info for the privacy screens. Also rectifies differences between the export and erase screens for consistency and changes the order of paragraphs. 
    
    Props sabernhardt, joedolson.
    Fixes #60097.
    Built from https://develop.svn.wordpress.org/branches/6.5@58018
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@57484 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5.3-alpha-57980';
+$wp_version = '6.5.3-alpha-58018';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
