9c840416ccadb30f7614a100cc3c1169763bd97a
Bernhard Reiter <Bernhard Reiter@git.wordpress.org>
2024-04-26 10:31:18 +0000

    Tests: Skip Gutenberg plugin activation test on older WP versions.
    The purpose of `tests/e2e/specs/gutenberg-plugin.test.js` is to ensure that running the Gutenberg plugin (stable version) on a WordPress `trunk` install doesn't produce any fatals.
    
    The test was introduced in [54913], i.e. it has been around since WP 6.2. It makes sense to have it present on older branches, as the Gutenberg plugin not only supports `trunk`, but also the current stable version of WordPress (i.e. currently 6.5), and one version below (6.4). However, it is not expected to work on any earlier versions beyond that; in practice, it has produced errors on some of those.
    
    This changeset checks the REST API response from the plugin activation request. If it returns an error with error code `plugin_wp_incompatible`, it skips the test.
    
    Props jorbin, johnbillion, swissspidy.
    Fixes #60971.
    Built from https://develop.svn.wordpress.org/trunk@58046
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57512 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58045';
+$wp_version = '6.6-alpha-58046';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
