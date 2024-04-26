93116dbd5ca13bed8aeee27d8e529e9d373ccc11
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-04-26 15:51:15 +0000

    Tests: Add more specific messages for a custom assertion in `WP_Test_REST_TestCase`.
    All assertions in PHPUnit have a `$message` parameter. Setting this parameter allows to distinguish which assertion is failing when a test runs multiple assertions, making debugging of the tests easier.
    
    Follow-up to [34928], [51478], [58039].
    
    See #60426.
    Built from https://develop.svn.wordpress.org/trunk@58051
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57516 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58049';
+$wp_version = '6.6-alpha-58051';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
