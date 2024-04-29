5638ddb0ae2d013c485f9865ee7ea99ae4c24ec2
Tammie Lister <tammie@automattic.com>
2024-04-29 12:17:10 +0000

    Twenty Fourteen: Adds base font size to pullquote block.
    The pullquote block needed the base font size adding back in. This also adds in support for appearance settings for weight and style in addition to the original ticket. This fixes a previous commit which changed the default font size of pullquote blocks, quote blocks and blockquotes.
    
    Props sabernhardt.
    Fixes #61034.
    
    Built from https://develop.svn.wordpress.org/trunk@58053
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57518 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58052';
+$wp_version = '6.6-alpha-58053';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
