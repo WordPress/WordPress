07fe72c6b3c1f18cbcc3986c00a5d8ed1af354ca
isabel_brison <isabel_brison@git.wordpress.org>
2024-04-30 03:19:10 +0000

    Editor: limit layout rules on themes without theme.json.
    Removes output of base rules for flow and constrained layout types on themes without theme.json.
    
    Props evanltd, poena, isabel_brison, andrewserong, oandregal.
    Reviewed by jorbin.
    Merges [58028] to the 6.5 branch.
    See #60981.
    
    Built from https://develop.svn.wordpress.org/branches/6.5@58057
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@57522 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5.3-alpha-58056';
+$wp_version = '6.5.3-alpha-58057';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
