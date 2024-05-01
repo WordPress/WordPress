fe416fd2f657c094016d4d2162ca7c1a465bf6ad
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-05-01 18:56:09 +0000

    Tests: Use `assertSame()` in `wp_validate_redirect()` tests.
    This ensures that not only the return values match the expected results, but also that their type is the same.
    
    Going forward, stricter type checking by using `assertSame()` should generally be preferred to `assertEquals()` where appropriate, to make the tests more reliable.
    
    Follow-up to [36444].
    
    Props costdev.
    See #60706.
    Built from https://develop.svn.wordpress.org/trunk@58070
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57535 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58069';
+$wp_version = '6.6-alpha-58070';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
