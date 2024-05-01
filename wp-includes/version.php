f56cc47de970018b07d40d954882570251663894
dmsnell <dmsnell@git.wordpress.org>
2024-05-01 23:46:06 +0000

    HTML API: Fix context reset in html5lib test suite.
    The html5lib-tests suite parses tests from a number of files with a specific
    data format. It uses a dataProvider in a loop that yields test information.
    This relies on some variables being reset on each iteration. The context
    element has not properly reset on each iteration.
    
    The test specification describes the context element as follows:
    https://github.com/html5lib/html5lib-tests/blob/a9f44960a9fedf265093d22b2aa3c7ca123727b9/tree-construction/README.md
    
    > Then there *may* be a line that says "#document-fragment", which must be
    > followed by a newline (LF), followed by a string of characters that indicates
    > the context element, followed by a newline (LF). If the string of characters
    > starts with "svg ", the context element is in the SVG namespace and the
    > substring after "svg " is the local name. If the string of characters starts
    > with "math ", the context element is in the MathML namespace and the
    > substring after "math " is the local name. Otherwise, the context element is
    > in the HTML namespace and the string is the local name. If this line is
    > present the "#data" must be parsed using the HTML fragment parsing algorithm
    > with the context element as context.
    
    Without the proper reset of this value, a single context element would change
    subsequent tests, breaking the test suite.
    
    This patch adds the reset to ensure that the test suite works properly.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/6464
    Discussed in https://core.trac.wordpress.org/ticket/61102
    
    Fixes #61102.
    Props costdev, dmsnell, jonsurrell.
    
    Built from https://develop.svn.wordpress.org/trunk@58072
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57537 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58071';
+$wp_version = '6.6-alpha-58072';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
