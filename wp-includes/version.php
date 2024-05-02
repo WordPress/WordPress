e19f9d5d066539237b033c7a19f54fdab3ebf3de
Aaron Jorbin <aaron@jorb.in>
2024-05-02 16:29:14 +0000

    REST API: Return empty object when no fallback templates are found (wp/v2/templates/lookup)
    This prevents a number of php notices that are surfaced due to the endpoint being called on load of the post editor even when there are no templates.
    
    Reviewed by joemcgill.
    Merges [58079] to the 6.5 branch.
    
    Props grantmkin, CookiesForDevo, britner, wildworks, jorbin.
    Fixes #60909.
    
    Built from https://develop.svn.wordpress.org/branches/6.5@58080
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@57545 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5.3-alpha-58078';
+$wp_version = '6.5.3-alpha-58080';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
