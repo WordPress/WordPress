4546b73afbeffaea9dcd9f5b33a9044042089c4c
Aaron Jorbin <aaron@jorb.in>
2024-05-02 15:06:16 +0000

    Script Loader: Ensure wp_localize_script() works when called early.
    Before, wp_localize_script() did not work when the $wp_scripts global was not already set (for example because of a script registration happening elsewhere) and even emitted a warning in that case. Due to side effects such as block registration early in the load process, this usually never happened. However, the absence of these side effects in 6.5 caused the wp_localize_script() to no longer work in places such as the login_enqueue_scripts.
    
    By calling wp_scripts() in wp_localize_script(), the $wp_scripts global is automatically set if needed, restoring previous behavior. Adds both a PHP unit test and an e2e test to verify this use case. Hat tip: jorbin.
    
    Thanks for the birthday wishes, Pascal!
    
    Reviewed by Jorbin.
    Merges [58068] to the 6.5 branch.
    
    Props salcode, aslamdoctor, jorbin, swissspidy.
    Fixes #60862.
    
    Built from https://develop.svn.wordpress.org/branches/6.5@58078
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@57543 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5.3-alpha-58066';
+$wp_version = '6.5.3-alpha-58078';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
