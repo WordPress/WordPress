c2628e70d8caf85184b524fe7038a131023fcfde
desrosj <desrosj@git.wordpress.org>
2024-05-14 15:20:13 +0000

    Build/Test Tools: Don’t upload HTML reports to Codecov.
    When generating a human readable code coverage report in HTML format, the Codecov action used to fail silently. An error is now being returned, resulting in a workflow failure.
    
    This adds a skip condition to the appropriate steps so the coverage report is only uploaded when a `clover` format is generated.
    
    See #60733.
    Built from https://develop.svn.wordpress.org/trunk@58144
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57609 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58143';
+$wp_version = '6.6-alpha-58144';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
