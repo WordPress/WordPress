e2f60b202b9879875276f4ee247bbb6abd732240
Bernhard Reiter <Bernhard Reiter@git.wordpress.org>
2024-05-15 07:04:07 +0000

    Tests: Add comment to explain when e2e test for gutenberg is skipped
    Follow-up to [58046].
    
    Reviewed by jorbin.
    Merges [58141] to the 6.5 branch.
    
    See #60971.
    Built from https://develop.svn.wordpress.org/branches/6.5@58152
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@57617 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5.4-alpha-58151';
+$wp_version = '6.5.4-alpha-58152';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
