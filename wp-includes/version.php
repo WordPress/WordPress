fdbe1f11a5f5034d8bf89e792ba8017e12ce80fb
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-05-16 15:15:13 +0000

    Tests: Use `assertSame()` in `block_has_support()` tests.
    This ensures that not only the return values match the expected results, but also that their type is the same.
    
    Going forward, stricter type checking by using `assertSame()` should generally be preferred to `assertEquals()` where appropriate, to make the tests more reliable.
    
    Follow-up to [56382].
    
    See #60706.
    Built from https://develop.svn.wordpress.org/trunk@58160
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57623 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58159';
+$wp_version = '6.6-alpha-58160';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
