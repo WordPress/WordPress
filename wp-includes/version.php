4a4b9a64938685fc439da47f0ffbf8912b49d83a
Pascal Birchler <pascal.birchler@gmail.com>
2024-05-16 16:44:11 +0000

    Script Loader: Disable `injectPolyfill` flag in webpack config.
    This is a follow-up to [57981] to ensure that `wp-polyfill` is no longer automatically added as a dependency to package scripts. This avoids unnecessarily loading the polyfill script in places such as the block editor.
    
    See #60962.
    Built from https://develop.svn.wordpress.org/trunk@58162
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57625 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58161';
+$wp_version = '6.6-alpha-58162';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
