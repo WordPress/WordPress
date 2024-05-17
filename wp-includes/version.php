8c878056d50eedc007b6dab0824be5536e687478
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-05-17 13:13:16 +0000

    Tests: Remove trailing characters from `@ticket` annotations.
    Follow-up to [44693], [46209], [54230], [54733], [55151].
    
    Props jonsurrell.
    See #61238.
    Built from https://develop.svn.wordpress.org/trunk@58164
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57627 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58163';
+$wp_version = '6.6-alpha-58164';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
