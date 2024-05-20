73bb34454d07eaa164bb38a5df5cc0a5f9edd8fa
Tammie Lister <tammie@automattic.com>
2024-05-20 17:49:17 +0000

    Twenty Twenty-Three: Updates Screenshot.
    The screenshot wasn't showing the front view. This now resolves it to fit guidelines. 
    
    Props kafleg, poena, shivashankerbhatta, SergeyBiryukov.
    Fixes #56949.
    
    Built from https://develop.svn.wordpress.org/trunk@58179
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57642 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58178';
+$wp_version = '6.6-alpha-58179';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
