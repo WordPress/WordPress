fb4fcb43886f95bfbd88a5205a2687bba7cddb1b
audrasjb <audrasjb@git.wordpress.org>
2024-05-27 14:11:12 +0000

    Login and Registration: Ensure login header title is translatable.
    This changeset ensures the login header default title string is translatable in `login_header()`.
    
    Props juliobox, swissspidy, audrasjb.
    Fixes #60668.
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@58209
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57672 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-login.php
+++ wp-login.php
@@ -34,12 +34,16 @@ if ( force_ssl_admin() && ! is_ssl() ) {
  * @global string      $action        The action that brought the visitor to the login page.
  *
  * @param string   $title    Optional. WordPress login Page title to display in the `<title>` element.
- *                           Default 'Log In'.
+ *                           Default null, which displays 'Log In'.
  * @param string   $message  Optional. Message to display in header. Default empty.
  * @param WP_Error $wp_error Optional. The error to pass. Default is a WP_Error instance.
  */
-function login_header( $title = 'Log In', $message = '', $wp_error = null ) {
+function login_header( $title = null, $message = '', $wp_error = null ) {
 	global $error, $interim_login, $action;
+
+	if ( null === $title ) {
+		$title = __( 'Log In' );
+	}
 
 	// Don't index any of these forms.
 	add_filter( 'wp_robots', 'wp_robots_sensitive_page' );
