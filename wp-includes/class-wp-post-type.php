b2b6166091f5d431c8348a61dc3f23603cb3a651
Pascal Birchler <pascal.birchler@gmail.com>
2024-05-27 15:24:13 +0000

    Posts, Post Types: Remove now obsolete hardcoded `attachment` check for autosave support.
    Follow-up to [58201].
    
    Props swissspidy, spacedmonkey.
    See #41172.
    Built from https://develop.svn.wordpress.org/trunk@58211
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57674 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-post-type.php
+++ wp-includes/class-wp-post-type.php
@@ -937,10 +937,6 @@ final class WP_Post_Type {
 			return null;
 		}
 
-		if ( 'attachment' === $this->name ) {
-			return null;
-		}
-
 		$class = $this->autosave_rest_controller_class ? $this->autosave_rest_controller_class : WP_REST_Autosaves_Controller::class;
 
 		if ( ! class_exists( $class ) ) {
