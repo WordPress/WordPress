d66894e50dcc447cc331ac8cd959c2c7047ff5fb
Peter Wilson <wilson@peterwilson.cc>
2024-05-28 05:27:13 +0000

    Build/Test Tools: Assert files exist/do not exist directly.
    Replaces various boolean assertions wrapping `file_exists()` with either `assertFileExists()` or `assertFileDoesNotExist()` as appropriate for the test.
    
    Props peterwilsoncc, mukesh27.
    See #60705.
    
    Built from https://develop.svn.wordpress.org/trunk@58221
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57684 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58220';
+$wp_version = '6.6-alpha-58221';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
