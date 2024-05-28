61b45944d10eb68c8abb9ff067845a8e4efd710d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-05-28 11:13:12 +0000

    Docs: Clarify that the `$wp_error` parameter of `login_header()` is nullable.
    Follow-up to [7991], [25701], [28792], [43457], [58209], [58220].
    
    See #60668, #60699.
    Built from https://develop.svn.wordpress.org/trunk@58224
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57687 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58223';
+$wp_version = '6.6-alpha-58224';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
