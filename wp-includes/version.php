8f7d5cf1cadf222557138da4fc1c9a7aea95c365
joedolson <joedolson@git.wordpress.org>
2024-05-30 16:43:17 +0000

    Menus: Add `resize: vertical` to menu item selector panel.
    Make the panels for selecting menu items to add to a panel resizable using the CSS `resize` property. This minor change makes it easier to select items to add on desktop devices, and has nominal impact on other interfaces.
    
    Props namith.jawahar, NekoJonez, jorbin, joen, joedolson.
    Fixes #60763.
    Built from https://develop.svn.wordpress.org/trunk@58256
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57719 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58252';
+$wp_version = '6.6-alpha-58256';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
