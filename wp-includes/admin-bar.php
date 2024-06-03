6a5c55341c682fa830216a5ba611aea6754a5aee
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-06-03 15:44:10 +0000

    Docs: Correct description for `wp_admin_bar_edit_site_menu()`.
    Follow-up to [58035], [58296].
    
    See #60977.
    Built from https://develop.svn.wordpress.org/trunk@58297
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57757 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/admin-bar.php
+++ wp-includes/admin-bar.php
@@ -451,7 +451,7 @@ function wp_admin_bar_site_menu( $wp_admin_bar ) {
 }
 
 /**
- * Adds the "Site Editor" link to the Toolbar.
+ * Adds the "Edit site" link to the Toolbar.
  *
  * @since 5.9.0
  * @since 6.3.0 Added `$_wp_current_template_id` global for editing of current template directly from the admin bar.
