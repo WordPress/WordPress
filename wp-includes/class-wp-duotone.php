8810038062ea12eaf7fe23e4c4bbefb2b010c477
audrasjb <audrasjb@git.wordpress.org>
2024-06-04 05:52:10 +0000

    Docs: Fix docblock alignment in `class-wp-duotone.php`.
    Follow-up to [58313].
    
    Props mukesh27.
    See #61271, #60699.
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@58317
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57774 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-duotone.php
+++ wp-includes/class-wp-duotone.php
@@ -1160,7 +1160,7 @@ class WP_Duotone {
 	 *
 	 * @since 6.6.0
 	 *
-	 * @param  string $block_content Rendered block content.
+	 * @param string $block_content Rendered block content.
 	 * @return string Filtered block content.
 	 */
 	public static function restore_image_outer_container( $block_content ) {
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-alpha-58316';
+$wp_version = '6.6-alpha-58317';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
