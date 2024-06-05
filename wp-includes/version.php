c080d7f563f5966f1c8a8ee78035aab0e15944f7
hellofromTonya <hellofromTonya@git.wordpress.org>
2024-06-05 15:13:19 +0000

    Bump 6.5 branch to version 6.5.4. Built from https://develop.svn.wordpress.org/branches/6.5@58342
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@57798 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5.4-RC1-58260';
+$wp_version = '6.5.4';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
