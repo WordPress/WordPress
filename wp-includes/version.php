56538b5450523d78ee6480bde55ce6d388a7cc5f
Aaron Jorbin <aaron@jorb.in>
2024-06-05 16:22:16 +0000

    Post WordPress 6.5.4 version bump.
    Built from https://develop.svn.wordpress.org/branches/6.5@58344
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@57800 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5.4';
+$wp_version = '6.5.5-alpha-58344';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
