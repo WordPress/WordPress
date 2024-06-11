0ed09d9b4b8d60240dfc3955e06ea380b9bfa013
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-06-11 12:06:16 +0000

    Docs: Further improve the `wp_http_validate_url()` DocBlock.
    Follow-up to [58384].
    
    See #61092.
    Built from https://develop.svn.wordpress.org/trunk@58388
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57837 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/http.php
+++ wp-includes/http.php
@@ -539,15 +539,17 @@ function send_origin_headers() {
  *
  * Examples of URLs that are considered unsafe:
  *
- * - ftp://example.com/caniload.php (Invalid protocol - Only http and https are allowed)
- * - http:///example.com/caniload.php (Malformed URL)
- * - http://user:pass@example.com/caniload.php (Login information)
- * - http://exampleeeee.com/caniload.php (Invalid hostname, as the IP cannot be looked up in DNS)
+ * - ftp://example.com/caniload.php - Invalid protocol - only http and https are allowed.
+ * - http:///example.com/caniload.php - Malformed URL.
+ * - http://user:pass@example.com/caniload.php - Login information.
+ * - http://exampleeeee.com/caniload.php - Invalid hostname, as the IP cannot be looked up in DNS.
  *
- * Examples of URLS that are considered unsafe by default:
+ * Examples of URLs that are considered unsafe by default:
  *
- * - http://192.168.0.1/caniload.php (IPs from LAN networks. This can be changed with the Wordpress filter http_request_host_is_external)
- * - http://198.143.164.252:81/caniload.php (By default, only 80, 443 and 8080 are allowed. This can be changed with the Wordpress filter http_allowed_safe_ports)
+ * - http://192.168.0.1/caniload.php - IPs from LAN networks.
+ *   This can be changed with the {@see 'http_request_host_is_external'} filter.
+ * - http://198.143.164.252:81/caniload.php - By default, only 80, 443, and 8080 ports are allowed.
+ *   This can be changed with the {@see 'http_allowed_safe_ports'} filter.
  *
  * @since 3.5.2
  *
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-beta1-58387';
+$wp_version = '6.6-beta1-58388';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
