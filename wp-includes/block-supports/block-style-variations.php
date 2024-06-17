963175f22838a6f083c0b0bc0efd8b23c4044c41
oandregal <oandregal@git.wordpress.org>
2024-06-17 10:08:12 +0000

    Global Styles: Avoid unnecessary processing of theme.json variation partials.
    Props aaronrobertshaw, oandregal, mukesh27.
    See #61451.
    
    Built from https://develop.svn.wordpress.org/trunk@58423
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57872 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/block-supports/block-style-variations.php
+++ wp-includes/block-supports/block-style-variations.php
@@ -474,6 +474,23 @@ function wp_register_block_style_variations_from_theme_json_data( $variations ) 
  * @access private
  */
 function wp_register_block_style_variations_from_theme() {
+	/*
+	 * Skip any registration of styles if no theme.json or variation partials are present.
+	 *
+	 * Given the possibility of hybrid themes, this check can't rely on if the theme
+	 * is a block theme or not. Instead:
+	 *   - If there is a primary theme.json, continue.
+	 *   - If there is a partials directory, continue.
+	 *   - The only variations to be registered from the global styles user origin,
+	 *     are those that have been copied in from the selected theme style variation.
+	 *     For a theme style variation to be selected it would have to have a partial
+	 *     theme.json file covered by the previous check.
+	 */
+	$has_partials_directory = is_dir( get_stylesheet_directory() . '/styles' ) || is_dir( get_template_directory() . '/styles' );
+	if ( ! wp_theme_has_theme_json() && ! $has_partials_directory ) {
+		return;
+	}
+
 	// Partials from `/styles`.
 	$variations_partials = WP_Theme_JSON_Resolver::get_style_variations( 'block' );
 	wp_register_block_style_variations_from_theme_json_data( $variations_partials );
