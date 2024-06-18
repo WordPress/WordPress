c655ae0a221a26c04b1648904191e77e3a2fe42b
Pascal Birchler <pascal.birchler@gmail.com>
2024-06-18 10:51:13 +0000

    Build/Test Tools: install additional languages in end-to-end tests.
    As a follow-up to [58430], this makes another locale available during the e2e tests, allowing testing of scenarios that require loading translations in the future.
    
    See #61240.
    Built from https://develop.svn.wordpress.org/trunk@58431
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57880 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-beta2-58430';
+$wp_version = '6.6-beta2-58431';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
