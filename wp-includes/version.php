dd55b026a88f46c9b80434db4dc51370fd3406e6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-06-19 10:33:13 +0000

    Twenty Seventeen: Optimize theme screenshot.
    This further reduces image size by ~1.5 KB after running `npm run grunt precommit:image`.
    
    Follow-up to [38833], [57322].
    
    See #58996.
    Built from https://develop.svn.wordpress.org/trunk@58442
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57891 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-beta3-58441';
+$wp_version = '6.6-beta3-58442';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
