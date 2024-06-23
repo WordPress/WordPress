f46632a0f19405126a913458efb441e0e338a655
Tammie Lister <tammie@automattic.com>
2024-06-23 13:37:16 +0000

    Twenty Fourteen: Resolves post content links not being underlined.
    In the page list block the text decoration was not the same on the front as the editor. This adds underline to links when in the post editor classic or block. This does not resolve the font size issue that needs to be fixed in block editor reset styles.
    
    Props viralsampat, sabernhardt.
    Fixes #61441.
    
    Built from https://develop.svn.wordpress.org/trunk@58463
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57912 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-beta3-58462';
+$wp_version = '6.6-beta3-58463';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
