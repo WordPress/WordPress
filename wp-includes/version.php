31465cc5747c5323ab4fcddad19c2bc19859c18f
audrasjb <audrasjb@git.wordpress.org>
2024-06-24 14:42:09 +0000

    Editor: Sanitize Template Part HTML tag on save.
    Props xknown, peterwilsoncc, jorbin, bernhard-reiter, azaozz.
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@58471
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@57920 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-beta3-58470';
+$wp_version = '6.6-beta3-58471';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
