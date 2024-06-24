f2cbb565e4dbe07836f67648333d4dfeae220e39
Aaron Jorbin <aaron@jorb.in>
2024-06-24 18:38:18 +0000

    Update packages to include latest changes.
    Props desrosj.
    
    Built from https://develop.svn.wordpress.org/trunk@58553
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58001 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-beta3-58478';
+$wp_version = '6.6-beta3-58553';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
