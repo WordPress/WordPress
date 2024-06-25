e0a3dd8ee62156e711158d40de51cd297db0cc93
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-06-25 12:45:18 +0000

    Docs: Correct DocBlock formatting in `wp-includes/blocks.php`.
    Follow-up to [58471].
    
    Props david.binda, narenin, sabernhardt, shital-patel, SergeyBiryukov.
    Fixes #61493.
    Built from https://develop.svn.wordpress.org/trunk@58561
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58009 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks.php
+++ wp-includes/blocks.php
@@ -1774,17 +1774,16 @@ function filter_block_kses_value( $value, $allowed_html, $allowed_protocols = ar
 	return $value;
 }
 
-
 /**
  * Sanitizes the value of the Template Part block's `tagName` attribute.
  *
  * @since 6.5.5
  *
- * @param string          $attribute_value   The attribute value to filter.
- * @param string          $attribute_name    The attribute name.
- * @param array[]|string  $allowed_html      An array of allowed HTML elements and attributes,
- *                                           or a context name such as 'post'. See wp_kses_allowed_html()
- *                                           for the list of accepted context names.
+ * @param string         $attribute_value The attribute value to filter.
+ * @param string         $attribute_name  The attribute name.
+ * @param array[]|string $allowed_html    An array of allowed HTML elements and attributes,
+ *                                        or a context name such as 'post'. See wp_kses_allowed_html()
+ *                                        for the list of accepted context names.
  * @return string The sanitized attribute value.
  */
 function filter_block_core_template_part_attributes( $attribute_value, $attribute_name, $allowed_html ) {
