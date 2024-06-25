dfe2d45189ed730aa35df204082570dc30c424c6
audrasjb <audrasjb@git.wordpress.org>
2024-06-25 16:21:18 +0000

    Post WordPress 6.6 RC1 version bump.
    Built from https://develop.svn.wordpress.org/trunk@58573
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58021 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-RC1';
+$wp_version = '6.6-RC1-58573';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
