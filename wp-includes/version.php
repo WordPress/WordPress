13d010f2d682502846cfbacf00a1474d0e9cbb07
Tammie Lister <tammie@automattic.com>
2024-06-26 13:48:13 +0000

    Twenty Nineteen: Fixes avatar block size inconsistency.
    The avatar block size was different between front and editor. This was because the width and height were added to the avatar class. 
    
    Props poena, naeemhaque, sabernhardt, sakibmd.
    Fixes #60664.
    
    Built from https://develop.svn.wordpress.org/trunk@58580
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58027 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58579';
+$wp_version = '6.7-alpha-58580';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
