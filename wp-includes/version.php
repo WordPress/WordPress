c43d34cc8fa5359184fe840ddcb66bc3b7c9de76
desrosj <desrosj@git.wordpress.org>
2024-06-27 13:51:43 +0000

    Build/Test Tools: Revert `uglify-js` update.
    This partially reverts [58563], which applied an update of the `uglify-js` devDependency from `3.17.4` to `3.18.0`.
    
    The `3.18.0` update is causing some JavaScript errors in the `media-views.min.js` file, so needs to be investigated further.
    
    Reviewed by jorbin, hellofromTonya.
    Merges [58585] to the 6.6 branch.
    
    Props david.binda, mukesh27, alshakero, jorbin, hellofromTonya.
    Fixes #61519.
    Built from https://develop.svn.wordpress.org/branches/6.6@58586
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.6@58033 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-RC1-58573';
+$wp_version = '6.6-RC1-58586';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
