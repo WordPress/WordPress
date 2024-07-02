8d260fe6ed6119f5dc6ef614563861774ba1f9b2
Tammie Lister <tammie@automattic.com>
2024-07-02 20:01:16 +0000

    Twenty Nineteen: Adds center alignment to Archives and Categories List blocks.
    When selecting center alignment for Archives or Categories List blocks the alignment was not matching. It is worth noting this fixes for these blocks but another ticket could be made to fix for titles.
    
    Props pranitdugad, sabernhardt.
    Fixes #47044.
    
    Built from https://develop.svn.wordpress.org/trunk@58627
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58057 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58623';
+$wp_version = '6.7-alpha-58627';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
