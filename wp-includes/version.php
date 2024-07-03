98a9717a621df77577c3b80ff9372f6f61c4b082
desrosj <desrosj@git.wordpress.org>
2024-07-03 17:23:14 +0000

    Build/Test Tools: Change the default value of `LOCAL_PHP`.
    This changes the default values for `LOCAL_PHP` and `LOCAL_DB_VERSION` in the 6.6 branch from `latest` to `8.3-fpm` and `8.0`, respectively, to properly reflect the highest version of PHP this branch will support (with beta support).
    
    See #61533.
    Built from https://develop.svn.wordpress.org/branches/6.6@58657
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.6@58071 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-RC2-58646';
+$wp_version = '6.6-RC2-58657';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
