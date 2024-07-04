99c9e4267263f46cccabec754f0711663cf3fa61
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-07-04 15:11:20 +0000

    Tests: Use `assertSame()` in `WP_REST_Global_Styles_Revisions_Controller` tests.
    This ensures that not only the return values match the expected results, but also that their type is the same.
    
    Going forward, stricter type checking by using `assertSame()` should generally be preferred to `assertEquals()` where appropriate, to make the tests more reliable.
    
    Follow-up to [56082].
    
    See #61573.
    Built from https://develop.svn.wordpress.org/trunk@58673
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58075 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58672';
+$wp_version = '6.7-alpha-58673';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
