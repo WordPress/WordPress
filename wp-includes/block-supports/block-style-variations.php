c56593dfe14aabc5085564de9255ce8277c87506
ellatrix <ellatrix@git.wordpress.org>
2024-07-09 10:24:19 +0000

    Section Styles: Fix ref values within block style variations.
    Fixes #61589.
    Fixes https://github.com/WordPress/wordpress-develop/pull/6989.
    See https://github.com/WordPress/gutenberg/pull/63172.
    
    Props aaronrobertshaw, andrewserong, ramonopoly.
    
    
    Built from https://develop.svn.wordpress.org/trunk@58691
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58093 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/block-supports/block-style-variations.php
+++ wp-includes/block-supports/block-style-variations.php
@@ -41,6 +41,42 @@ function wp_get_block_style_variation_name_from_class( $class_string ) {
 }
 
 /**
+ * Recursively resolves any `ref` values within a block style variation's data.
+ *
+ * @since 6.6.0
+ * @access private
+ *
+ * @param array $variation_data Reference to the variation data being processed.
+ * @param array $theme_json     Theme.json data to retrieve referenced values from.
+ */
+function wp_resolve_block_style_variation_ref_values( &$variation_data, $theme_json ) {
+	foreach ( $variation_data as $key => &$value ) {
+		// Only need to potentially process arrays.
+		if ( is_array( $value ) ) {
+			// If ref value is set, attempt to find its matching value and update it.
+			if ( array_key_exists( 'ref', $value ) ) {
+				// Clean up any invalid ref value.
+				if ( empty( $value['ref'] ) || ! is_string( $value['ref'] ) ) {
+					unset( $variation_data[ $key ] );
+				}
+
+				$value_path = explode( '.', $value['ref'] ?? '' );
+				$ref_value  = _wp_array_get( $theme_json, $value_path );
+
+				// Only update the current value if the referenced path matched a value.
+				if ( null === $ref_value ) {
+					unset( $variation_data[ $key ] );
+				} else {
+					$value = $ref_value;
+				}
+			} else {
+				// Recursively look for ref instances.
+				wp_resolve_block_style_variation_ref_values( $value, $theme_json );
+			}
+		}
+	}
+}
+/**
  * Render the block style variation's styles.
  *
  * In the case of nested blocks with variations applied, we want the parent
@@ -81,6 +117,12 @@ function wp_render_block_style_variation_support_styles( $parsed_block ) {
 	if ( empty( $variation_data ) ) {
 		return $parsed_block;
 	}
+
+	/*
+	 * Recursively resolve any ref values with the appropriate value within the
+	 * theme_json data.
+	 */
+	wp_resolve_block_style_variation_ref_values( $variation_data, $theme_json );
 
 	$variation_instance = wp_create_block_style_variation_instance_name( $parsed_block, $variation );
 	$class_name         = "is-style-$variation_instance";
