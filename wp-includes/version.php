727cf8b0f471a3277f71d44b181478bc0bf3a7be
audrasjb <audrasjb@git.wordpress.org>
2024-07-15 14:59:16 +0000

    WordPress 6.6 RC4. Built from https://develop.svn.wordpress.org/branches/6.6@58722
    git-svn-id: http://core.svn.wordpress.org/branches/6.6@58124 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-RC3-58721';
+$wp_version = '6.6-RC4';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
