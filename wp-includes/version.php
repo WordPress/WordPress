83e01e562b69e0f1a15058eea513b032c86fa3ea
audrasjb <audrasjb@git.wordpress.org>
2024-07-15 14:24:19 +0000

    Editor: Update packages for 6.6 RC 4.
    This package update includes only one revert PR:
    https://github.com/WordPress/gutenberg/pull/63412
    
    You can confirm the changes in this package update here:
    https://github.com/WordPress/gutenberg/commits/wp/6.6/
    
    Reviewed by ellatrix, audrasjb.
    Merges [58719] to the 6.6 branch.
    
    Fixes #61654.
    Props santosguillamot.
    
    
    Built from https://develop.svn.wordpress.org/branches/6.6@58720
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.6@58122 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6-RC3-58711';
+$wp_version = '6.6-RC3-58720';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
