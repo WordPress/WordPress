04358f497dad059df9a35a57ef22868ec65f8c8d
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-07-17 11:28:16 +0000

    Docs: Fix typo in a comment in `wp.media.view.Attachment.Details.TwoColumn`.
    Follow-up to [41351].
    
    Props devansh2002, mukesh27.
    Fixes #61658.
    Built from https://develop.svn.wordpress.org/trunk@58743
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58145 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/media-grid.js
+++ wp-includes/js/media-grid.js
@@ -131,7 +131,7 @@ var Details = wp.media.view.Attachment.Details,
  * @augments wp.Backbone.View
  * @augments Backbone.View
  */
-TwoColumn = Details.extend(/** @lends wp.media.view.Attachment.Details.TowColumn.prototype */{
+TwoColumn = Details.extend(/** @lends wp.media.view.Attachment.Details.TwoColumn.prototype */{
 	template: wp.template( 'attachment-details-two-column' ),
 
 	initialize: function() {
