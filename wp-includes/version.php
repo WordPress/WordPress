05c7351e537084305b4236113534063b05c4affa
joedolson <joedolson@git.wordpress.org>
2024-07-18 00:52:17 +0000

    Toolbar: Move user and recovery menus to a higher priority.
    Following [58215], admin bar items in the `top-secondary` group have a changed visual order. Increase the priority of the user and recovery menu items so nodes added with higher priorities will still be shown visually before the user and recovery menu items, as they were prior to 58215.
    
    The items will appear in the reverse of the previous order, but the new order now matches their priority order, rather than being the opposite.
    
    Props sabernhardt, joemcgill, pbiron, joedolson.
    Fixes #61615.
    Built from https://develop.svn.wordpress.org/trunk@58748
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58150 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58747';
+$wp_version = '6.7-alpha-58748';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
