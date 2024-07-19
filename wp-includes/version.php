cdf6e7c128cab5d8c8bde550376c3b9e885ee367
desrosj <desrosj@git.wordpress.org>
2024-07-19 13:15:15 +0000

    Build/Test Tools: Ensure `TARGET_SHA` is set for older branches.
    The `workflow_dispatch` event payload does not contain the `before` property containing the previous commit’s SHA value, which causes scheduled runs of the performance testing workflow in older branches to fail.
    
    This adds a step specifically for this event type to use native Git commands to retrieve the required SHA value instead.
    
    Props joemcgill.
    Fixes #61699.
    Built from https://develop.svn.wordpress.org/trunk@58767
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58169 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58763';
+$wp_version = '6.7-alpha-58767';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
