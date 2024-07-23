afd60dd8a467eb8620b2ce32277b9ce8b6248c15
Tammie Lister <tammie@automattic.com>
2024-07-23 21:52:15 +0000

    Twenty Twenty: Fixes Quote block border not reflecting alignment.
    This resolves the border not also aligning with the quote block. When this theme was built the editor used style attributes for text alignment. This patch updates existing rules for center and right aligned quote blocks.
    
    Props viralsampat, poena, sabernhardt.
    Fixes #61132.
    
    Built from https://develop.svn.wordpress.org/trunk@58796
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58192 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58789';
+$wp_version = '6.7-alpha-58796';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
