0702cafa875a89a9ad93b975964c7f5cce5ab5f6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-07-24 15:10:13 +0000

    Tests: Use more specific assertions in `get_comment_author()` tests.
    Follow-up to [58335].
    
    See #61530.
    Built from https://develop.svn.wordpress.org/trunk@58803
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58199 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58801';
+$wp_version = '6.7-alpha-58803';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
