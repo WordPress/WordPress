8911189d5a3121c6c902b7ef33e35969f03f131c
Aaron Jorbin <aaron@jorb.in>
2024-07-25 16:35:13 +0000

    Filesystem: Normalize allowed_files so comparison is apples to apples.
    In [58470] a change was made to normalize the filename in validate_file, however this leads to instances where the list of files that are allowed aren't normalized such as in the theme editor. By normalizing the array, the comparison is apples to apples.
    
    Reviewed by hellofromTonya.
    Merges 58570 to the 6.5 branch.
    
    Fixes #61488.
    Props jorbin, hellofromtonya, swissspidy, misulicus, script2see, Presskopp, audrasjb, peterwilsoncc, siliconforks, littler.chicken, paulkevan,
    
    Built from https://develop.svn.wordpress.org/branches/6.5@58808
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@58204 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5.6-alpha-58658';
+$wp_version = '6.5.6-alpha-58808';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
