2fecc77a68598b5169989a34d6d4d66593d544c1
Tammie Lister <tammie@automattic.com>
2024-07-29 10:10:16 +0000

    Twenty Twenty: Fixes Table font size when custom showing on front.
    The Table block was not reflecting the custom font size on the front. This solution now brings custom font sizes in for front the same as back in the editor.
    
    Props umesh84, SergeyBiryukov, sabernhardt, shailu25.
    Fixes #56157.
    
    Built from https://develop.svn.wordpress.org/trunk@58824
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58220 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58823';
+$wp_version = '6.7-alpha-58824';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
