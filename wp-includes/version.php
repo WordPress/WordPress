77f5a025b2ea003c8ecb30fafeee17379706c78f
luisherranz <luisherranz@git.wordpress.org>
2024-07-29 11:10:23 +0000

    Interactivity API: Allow server derived state to appear in non-final position
    In some cases, derived state returns an associative array. Directives may wish to continue to access properties of the associative array, when using the syntax `state.arrayReturnedByClosure.property`. This patch continues evaluating the path after the associative array has been returned by the Closure.
    
    Props jonsurrell, luisherranz.
    
    Fixes #61741.
    Built from https://develop.svn.wordpress.org/trunk@58825
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58221 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58824';
+$wp_version = '6.7-alpha-58825';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
