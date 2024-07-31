2602d7802145ef0627d84b60ea0fdd2762a33864
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-07-31 14:05:16 +0000

    Bundled Themes: Update schema version in style variation files.
    This ensures that settings and styles are properly handled by code editors that support schema.
    
    Reference: [https://developer.wordpress.org/block-editor/reference-guides/theme-json-reference/theme-json-v2/#json-schema Theme.json Version 2 Reference: JSON Schema].
    
    Follow-up to [58403].
    
    Props poena, umeshsinghin.
    Fixes #61789.
    Built from https://develop.svn.wordpress.org/trunk@58835
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58231 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58834';
+$wp_version = '6.7-alpha-58835';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
