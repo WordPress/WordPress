7a86a5a42f8fc2ae8cc76f5914308d6d67d09dd0
dmsnell <dmsnell@git.wordpress.org>
2024-08-02 22:59:12 +0000

    HTML API: Indicate when WordPress rejects attribute updates.
    When setting an an attribute value in the HTML API, WordPress may reject
    an update based on rules in `kses`. In these cases, the return value from
    an escaping function will be an empty string, and the HTML API should
    reject the update. Unfortunately, it currently reports that it updates the
    attribute but sets an empty string value, which is misleading.
    
    In this patch, the HTML API will refuse the attribute update and return
    false to indicate as much when WordPress rejects the updates.
    
    Developed in https://github.com/wordpress/wordpress-develop/pull/7114
    Discussed in https://core.trac.wordpress.org/ticket/61719
    
    Follow-up to [58472].
    
    Props: amitraj2203, dmsnell, mukesh27.
    Fixes #61719.
    
    Built from https://develop.svn.wordpress.org/trunk@58844
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58240 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58843';
+$wp_version = '6.7-alpha-58844';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
