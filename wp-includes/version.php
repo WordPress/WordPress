76a44b13d4fc13982a2ca5479ac74767a2518efa
Tammie Lister <tammie@automattic.com>
2024-08-05 08:57:15 +0000

    Twenty Nineteen: Fixes Button block not changing appearance settings.
    The Button block isn't reflecting appearance settings. This fix keeps the default as font-weight bold but also adds support for other weights.
    
    Props pranitdugad, pitamdey, sabernhardt.
    Fixes #61437.
    
    Built from https://develop.svn.wordpress.org/trunk@58851
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58247 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58850';
+$wp_version = '6.7-alpha-58851';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
