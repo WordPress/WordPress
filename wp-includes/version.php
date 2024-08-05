2468ef9f8c339840da713bd18bec977465f99d25
talldanwp <talldanwp@git.wordpress.org>
2024-08-05 06:41:14 +0000

    Block Styles: Fix ordering of stylesheets when a style variation is applied.
    Adjusts the dependency order to ensure stylesheets are output in the correct order.
    
    Syncs the PHP changes from https://github.com/WordPress/gutenberg/pull/63918.
    
    Props talldanwp, aaronrobertshaw, andrewserong, aristath, mukesh27, ramonopoly, ytfeldrawkcab.
    Fixes #61748.
    
    
    Built from https://develop.svn.wordpress.org/trunk@58850
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58246 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58849';
+$wp_version = '6.7-alpha-58850';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
