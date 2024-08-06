c4cd3c1c4b141a19564e9c58e9e249f539bd9a21
dmsnell <dmsnell@git.wordpress.org>
2024-08-06 22:07:19 +0000

    HTML API: Use full parser in html5lib tests.
    Previously the `html5lib` tests have only run in the fragment parser mode,
    assuming IN BODY context. This limited the number of tests which could run
    and was a result of the HTML Processor only supporting the IN BODY fragment
    parser. In [58836], however, a full parser was added to the HTML Processor.
    
    In this patch the full parser is utilized in order to run more of the
    previously-skipped tests, asserting more behaviors in the HTML parsing.
    
    Developed in https://github.com/wordpress/wordpress-develop/pull/7117
    Discussed in https://core.trac.wordpress.org/ticket/61646
    
    Props: dmsnell, jonsurrell.
    See #61646.
    
    Built from https://develop.svn.wordpress.org/trunk@58859
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58255 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58858';
+$wp_version = '6.7-alpha-58859';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
