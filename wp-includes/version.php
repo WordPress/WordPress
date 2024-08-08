ed0e5cff79926ada451a355ed30d6a763d40c6bc
dmsnell <dmsnell@git.wordpress.org>
2024-08-08 07:33:19 +0000

    HTML API: Add support for SVG and MathML (Foreign content) (remove file)
    As part of work to add more spec support to the HTML API, this patch adds support for SVG and MathML elements, or more generally, "foreign content."
    
    The rules in foreign content are a mix of XML and HTML parsing rules and introduce additional complexity into the processor, but is important in order to avoid getting lost when inside these elements.
    
    This patch follows the first by deleting the empty files, which were mistakenly left in during the initial merge.
    
    Developed in https://github.com/wordpress/wordpress-develop/pull/6006
    Discussed in https://core.trac.wordpress.org/ticket/61576
    
    Follow-up to [58867].
    
    Props: dmsnell, jonsurrell, westonruter.
    See #61576.
    
    Built from https://develop.svn.wordpress.org/trunk@58868
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58264 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58867';
+$wp_version = '6.7-alpha-58868';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
