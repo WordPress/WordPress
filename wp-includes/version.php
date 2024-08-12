95f6808e033e7fd12e945556550d23d8c42f47c0
Tammie Lister <tammie@automattic.com>
2024-08-12 13:44:15 +0000

    Twenty Seventeen: Fixes Pullquote block text color not changing on front.
    The Pullquote block text color was not changing on the front. This overrides the inlined styles.
    
    Props iamjaydip, laurelfulford, sabernhardt.
    Fixes #46080.
    
    Built from https://develop.svn.wordpress.org/trunk@58886
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58282 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58885';
+$wp_version = '6.7-alpha-58886';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
