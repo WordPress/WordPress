af6ec4afee66f01972fc7e53f3624750750f53b4
Peter Wilson <wilson@peterwilson.cc>
2024-08-15 04:25:16 +0000

    Meta: Tidy up and update `.mailmap`.
    Updates the name mappings to sort display names ascii-alphabetically and to ascribe commits to updated usernames.
    
    Accounts without a seperate display name remain listed in the footer of the file.
    
    Props dmsnell, jorbin.
    See #61864.
    
    Built from https://develop.svn.wordpress.org/trunk@58900
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58296 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58899';
+$wp_version = '6.7-alpha-58900';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
