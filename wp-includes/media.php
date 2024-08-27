89519c939ce45a555f0051e0bfddd805cf7e2eea
Pascal Birchler <pascal.birchler@gmail.com>
2024-08-27 15:29:14 +0000

    Docs: Fix typo in `wp_show_heic_upload_error` docblock.
    Follow-up to [58849].
    See #53645.
    Built from https://develop.svn.wordpress.org/trunk@58942
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58338 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/media.php
+++ wp-includes/media.php
@@ -5487,7 +5487,7 @@ function _wp_add_additional_image_sizes() {
  * Callback to enable showing of the user error when uploading .heic images.
  *
  * @since 5.5.0
- * @since 6.7.0 The default behavior is to enable heic uplooads as long as the server
+ * @since 6.7.0 The default behavior is to enable heic uploads as long as the server
  *              supports the format. The uploads are converted to JPEG's by default.
  *
  * @param array[] $plupload_settings The settings for Plupload.js.
