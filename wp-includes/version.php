b428e1b5bef082562dac34371511e98c5493bede
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-09-02 22:18:17 +0000

    Tests: Add a unit test for `get_metadata()` with a non-existing object ID.
    Follow-up to [48658], [50641], [58962].
    
    Props rodrigosprimo, jrf.
    See #61530, #61608.
    Built from https://develop.svn.wordpress.org/trunk@58968
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58364 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-58967';
+$wp_version = '6.7-alpha-58968';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
