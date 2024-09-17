d5206419d75d9b1c1b958f076267f7eba9c41ce5
Helen Hou-Sandí <helen.y.hou@gmail.com>
2024-09-17 23:50:12 +0000

    Bootstrap/Load: Give more context and warning about editing compat.php.
    As indicated by name, this is a compatibility file which warrants more care to begin with, but it's still worth warning folks about how narrow function availability is in this file.
    
    Props jorbin, dmsnell, helen.
    See #61694.
    
    Built from https://develop.svn.wordpress.org/trunk@59043
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58439 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/compat.php
+++ wp-includes/compat.php
@@ -2,6 +2,11 @@
 /**
  * WordPress implementation for PHP functions either missing from older PHP versions or not included by default.
  *
+ * This file is loaded extremely early and the functions can be relied upon by drop-ins.
+ * Ergo, please ensure you do not rely on external functions when writing code for this file.
+ * Only use functions built into PHP or are defined in this file and have adequate testing
+ * and error suppression to ensure the file will run correctly and not break websites.
+ *
  * @package PHP
  * @access private
  */
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-59042';
+$wp_version = '6.7-alpha-59043';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
