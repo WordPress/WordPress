d598d459ac4825c2c35eacea1bc3fca8f9d9284c
TimothyBlynJacobs <TimothyBlynJacobs@git.wordpress.org>
2024-09-17 22:28:18 +0000

    Build Tools: Allow easier customization of the .env file.
    The .env file allows for configuring how the WordPress Local environment should be configured. However, because the file is version controlled, developers must be careful not to commit their modifications.
    
    This commit renames the .env file to be .env.example. During env start, the .env.example file is copied to .env if it does not exist. This allows for contributors to continue using the project without thinking about .env and to make changes when needed. This brings WordPress Core into the dotenv project guidelines.
    
    Props johnbillion, afragen, h71, desrosj.
    Fixes #52668.
    
    Built from https://develop.svn.wordpress.org/trunk@59038
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58434 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-59037';
+$wp_version = '6.7-alpha-59038';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
