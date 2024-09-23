682c23179a3dbd2a7da8442de9e47d6e6b00b9af
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-09-23 22:17:17 +0000

    Build/Test Tools: Prevent Composer lock file from being created.
    Composer 1.10.0 introduced a `lock` config option, which, when set to `false` will prevent a `composer.lock` file from being created and will ignore it when one exists.
    
    This is a useful option for packages like WordPress where the `lock` file has no meaning.
    
    It also makes life more straightforward for contributors as they don't have to remember that for this repo they should use `composer update` instead of `composer install`. Both will now work the same.
    
    Reference: [https://getcomposer.org/doc/06-config.md#lock Composer Documentation: Config: lock].
     
    Follow-up to [51543].
    
    Props jrf.
    See #61530.
    Built from https://develop.svn.wordpress.org/trunk@59082
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58478 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-59081';
+$wp_version = '6.7-alpha-59082';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
