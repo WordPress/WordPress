d8e532f388168c72d43d26803144a7df0b0b16e9
czapla <czapla@git.wordpress.org>
2024-09-25 15:57:15 +0000

    Built from https://develop.svn.wordpress.org/trunk@59087
    git-svn-id: http://core.svn.wordpress.org/trunk@58483 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-59086';
+$wp_version = '6.7-alpha-59087';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
