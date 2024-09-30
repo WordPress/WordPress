76afc20e4e53370874c58a9b2c9aa1cb64a87120
Peter Wilson <wilson@peterwilson.cc>
2024-09-30 05:29:17 +0000

    General: Delete `tests/phpunit/tests/formatting/wpTargetedLinkRel.php`.
    Really delete `tests/phpunit/tests/formatting/wpTargetedLinkRel.php` this time rather than leaving it hanging around as an empty file.
    
    Follow up to [59120].
    
    Props noisysocks.
    See #53843
    
    
    Built from https://develop.svn.wordpress.org/trunk@59121
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58517 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-alpha-59120';
+$wp_version = '6.7-alpha-59121';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
