a03401429b1db2cfc484aff20b9767fbd7cae401
Peter Wilson <wilson@peterwilson.cc>
2024-09-30 01:56:14 +0000

    Editor: Prevent direct access to `/wp-includes/blocks/index.php`.
    Adds a check for `ABSPATH` to the top of the `/wp-includes/blocks/index.php` file and prevents the file from loading if it is not defined.
    
    This prevents the file from throwing errors when accessed directly.
    
    Props khokansardar, mukesh27.
    Fixes #62108.
    See #60352.
    
    Built from https://develop.svn.wordpress.org/trunk@59117
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58513 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks/index.php
+++ wp-includes/blocks/index.php
@@ -5,6 +5,11 @@
  * @package WordPress
  */
 
+// Don't load directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
 define( 'BLOCKS_PATH', ABSPATH . WPINC . '/blocks/' );
 
 // Include files required for core blocks registration.
