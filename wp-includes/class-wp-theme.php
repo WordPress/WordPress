99474f16b6bce7178a5249a4e3e0e6059991cd5e
Peter Wilson <wilson@peterwilson.cc>
2024-10-07 22:28:15 +0000

    Docs/Bundled Themes: Add `@since` annotation for introduction of TT5.
    See #62180, #61608.
    
    
    Built from https://develop.svn.wordpress.org/trunk@59193
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58588 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-theme.php
+++ wp-includes/class-wp-theme.php
@@ -60,6 +60,7 @@ final class WP_Theme implements ArrayAccess {
 	 * @since 5.9.0 Added the Twenty Twenty-Two theme.
 	 * @since 6.1.0 Added the Twenty Twenty-Three theme.
 	 * @since 6.4.0 Added the Twenty Twenty-Four theme.
+	 * @since 6.7.0 Added the Twenty Twenty-Five theme.
 	 * @var string[]
 	 */
 	private static $default_themes = array(
