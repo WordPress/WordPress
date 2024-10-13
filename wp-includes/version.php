a1bd872c128af127445a1e03ea2e7e59c2ea1f15
Tammie Lister <tammie@automattic.com>
2024-10-13 15:43:18 +0000

    Twenty Nineteen: Fixes adjusting avatar dimensions for comment author.
    In Twenty Nineteen r58580 increased size of avatars. This caused issues with the position of author checkbox. This resolves by moving without impacting the avatar blocks.
    
    Props sabernhardt, nithins53, sudipatel007.
    Fixes #62096.
    
    Built from https://develop.svn.wordpress.org/trunk@59222
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58614 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-beta2-59220';
+$wp_version = '6.7-beta2-59222';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
