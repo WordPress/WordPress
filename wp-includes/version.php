1809b184049d7eacf26bc3ef68e0979a60ed7489
Peter Wilson <wilson@peterwilson.cc>
2024-10-23 22:39:17 +0000

    Editor: Update Google Fonts API endpoint for WordPress 6.7.
    Updates the Google Fonts API to use the URL `https://s.w.org/images/fonts/wp-6.7/collections/google-fonts-with-preview.json` with an updated collection of fonts available for install.
    
    Props dilip2615, mmaattiiaass, wildworks, dd32.
    Fixes #62258.
    
    
    Built from https://develop.svn.wordpress.org/trunk@59286
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58678 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59285';
+$wp_version = '6.8-alpha-59286';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
