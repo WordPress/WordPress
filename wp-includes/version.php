8df15c4374b5cea6627e3f149058904bfa793dcf
desrosj <desrosj@git.wordpress.org>
2024-10-23 02:32:18 +0000

    Build/Test Tools: Test against MySQL 9.0.
    Version 9.0 is the latest short-term innovation release of MySQL.
    
    See #62280.
    Built from https://develop.svn.wordpress.org/trunk@59282
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58674 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59281';
+$wp_version = '6.8-alpha-59282';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
