aea47f2bb57e3b7da880297772340df97780ec83
desrosj <desrosj@git.wordpress.org>
2024-10-28 18:13:16 +0000

    Build/Test Tools: Support Docker compose override files.
    This updates the logic introduced in [59279] to account for the presence of `docker-compose.override.yml` files.
    
    Reviewed by peterwilsoncc.
    Merges 59283] to the 6.7 branch.
    
    Props xknown, davidbaumwald.
    See #61218.
    Built from https://develop.svn.wordpress.org/branches/6.7@59311
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.7@58697 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-RC1-59310';
+$wp_version = '6.7-RC1-59311';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
