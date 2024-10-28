a17aba82df56eabeda842bdc39fba193a7b8594f
Peter Wilson <wilson@peterwilson.cc>
2024-10-28 00:31:21 +0000

    Revisions: Fix typo in navigation help text.
    Fixes a typo in the a11y helper text explaining how to navigate revisions. "Change revision by using the left and arrow keys" becomes "Change revision by using the left and right arrow keys".
    
    Follow up to [59225].
    
    Props pedromendonca, sabernhardt, fierevere, swissspidy.
    Fixes #62306.
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@59306
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58692 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59293';
+$wp_version = '6.8-alpha-59306';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
