f17b845a7849effd822b5dbbd5968e0de9976033
Peter Wilson <wilson@peterwilson.cc>
2024-10-30 23:29:19 +0000

    Tests/Build tools: Only fail importer tests if plugin is missing.
    Reverts an earlier change to the test suite in which the PHPUnit tests could not run if the importer plugin was not available.
    
    This update allows the test suite to run and will fail importer tests if the plugin is not available. 
    
    Follow up to r59085.
    
    Props peterwilsoncc, azaozz.
    See #62325.
    
    
    Built from https://develop.svn.wordpress.org/trunk@59326
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58712 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59325';
+$wp_version = '6.8-alpha-59326';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
