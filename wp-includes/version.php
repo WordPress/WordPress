527f8961c596918d93b6d87aed331b9a32802935
Aaron Jorbin <aaron@jorb.in>
2024-11-05 16:33:18 +0000

    WordPress 6.7 RC1.
    Built from https://develop.svn.wordpress.org/branches/6.7@59352
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.7@58738 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-RC2-59351';
+$wp_version = '6.7-RC3';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
