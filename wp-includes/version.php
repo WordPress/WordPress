a9b1b36403db4726b906efddf0e15cdb2745c8af
get_dave <get_dave@git.wordpress.org>
2024-11-07 15:16:24 +0000

    Editor: Update packages for 6.7 RC 4
    Syncs @wordpress/* packages to the wp-6.7 npm tag.
    
    Props get_dave, mikachan, gziolo, kevin940726, jonsurrell, jsnajdr
    Built from https://develop.svn.wordpress.org/trunk@59368
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58754 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59366';
+$wp_version = '6.8-alpha-59368';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
