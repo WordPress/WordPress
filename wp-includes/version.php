a867d33702bde4ad1b455634205aab62273a6e96
Aaron Jorbin <aaron@jorb.in>
2024-11-07 18:05:14 +0000

    Post WordPress 6.7 RC4 version bump. Built from https://develop.svn.wordpress.org/branches/6.7@59372
    git-svn-id: http://core.svn.wordpress.org/branches/6.7@58758 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-RC4';
+$wp_version = '6.7-RC4-59372';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
