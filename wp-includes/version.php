66cf685d1982df6aa6261caf90c4fbc066f30b74
desrosj <desrosj@git.wordpress.org>
2024-11-07 16:05:23 +0000

    Editor: Update packages for 6.7 RC 4.
    Syncs @wordpress/* packages to the wp-6.7 npm tag.
    
    Reviewed by get_dave, desrosj.
    Merges [59368] to the 6.7 branch.
    
    Props get_dave, mikachan, gziolo, kevin940726, jonsurrell, jsnajdr.
    Fixes #62358.
    Built from https://develop.svn.wordpress.org/branches/6.7@59369
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.7@58755 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7-RC3-59367';
+$wp_version = '6.7-RC3-59369';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
