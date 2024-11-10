c5bf14f31baff274b95cd7b2c1e67fc96f138592
Peter Wilson <wilson@peterwilson.cc>
2024-11-10 21:41:13 +0000

    Media: Remove dimension suffix from full size converted HEIC images.
    Removes the dimension suffix, eg `-1000x1000` from the file name of full size images automatically converted from HEIC to JPEGs by WordPress. Introduces unit tests for the default conversion of images and customized conversion settings via the `image_editor_output_format` filter.
    
    Follow up to [58849], [58942], [59317], [59346], [59366]
    
    Props mukesh27, peterwilsoncc, azaozz, apermo, flixos90, ironprogrammer.
    Fixes #62359.
    See #53645, #62305.
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@59379
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58765 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59378';
+$wp_version = '6.8-alpha-59379';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
