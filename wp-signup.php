f7fc7542205c6c82a14f2f458123473be2210078
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-11-10 21:36:19 +0000

    Docs: Add file header DocBlock for `wp-signup.php`.
    Follow-up to [https://mu.trac.wordpress.org/changeset/543 mu:543], [https://mu.trac.wordpress.org/changeset/557 mu:557], [12603].
    
    Props jdahir0789, apermo, aboelfotoh.
    Fixes #62254.
    Built from https://develop.svn.wordpress.org/trunk@59378
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58764 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-signup.php
+++ wp-signup.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * WordPress Signup Page
+ *
+ * Handles the user registration and site creation process for multisite installations.
+ *
+ * @package WordPress
+ */
 
 /** Sets up the WordPress Environment. */
 require __DIR__ . '/wp-load.php';
