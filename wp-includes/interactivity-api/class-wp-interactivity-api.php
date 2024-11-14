664c3049d1718a5282273b213201918b4dd660a8
cbravobernal <cbravobernal@git.wordpress.org>
2024-11-14 11:55:18 +0000

    Interactivity API: Allow missing state negation on server
    Aligns on the behavior of the negation operator with directives to missing paths in client and in server.
    
    With a directive like the following:
    {{{
    <div data-wp-bind--hidden="!state.missing.property">
    	This should be hidden by the <code>hidden</code> attribute.
    </div>
    }}}
    Both server and client will return with this fix:
    {{{
    <div data-wp-bind--hidden="!state.missing.property" hidden="">
    	This should be hidden by the <code>hidden</code> attribute.
    </div>
    }}}
    
    Reviewed by cbravobernal.
    Merges [59398] to the 6.7 branch.
    
    Props jonsurrell, luisherranz.
    Fixes #62374.
    
    Built from https://develop.svn.wordpress.org/branches/6.7@59404
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.7@58790 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/interactivity-api/class-wp-interactivity-api.php
+++ wp-includes/interactivity-api/class-wp-interactivity-api.php
@@ -584,7 +584,8 @@ final class WP_Interactivity_API {
 			} elseif ( is_object( $current ) && isset( $current->$path_segment ) ) {
 				$current = $current->$path_segment;
 			} else {
-				return null;
+				$current = null;
+				break;
 			}
 
 			if ( $current instanceof Closure ) {
