df05a5564fc630e00d7c1c1a754d0639510088cd
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-11-16 23:15:29 +0000

    Coding Standards: Escape attachment URL in `wp-admin/async-upload.php`.
    Follow-up to [58279].
    
    Props shyamkariya, pitamdey, nareshbheda, ketanniruke, desrosj.
    Fixes #62434.
    Built from https://develop.svn.wordpress.org/trunk@59407
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58793 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59406';
+$wp_version = '6.8-alpha-59407';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
