5a2559365d9e89652fb73bb1fc3b2ca00af0c719
joedolson <joedolson@git.wordpress.org>
2024-11-18 02:55:20 +0000

    Customizer: Fix layout issues in customizer accordions.
    Adjust some CSS characteristics in the customizer accordions to avoid a slight horizontal scroll, allow the chevron icon to be part of the clickable control surface, and resolve a pre-existing padding issue allowing overflow on accordion headings.
    
    Follow up to [59224].
    
    Props laurelfulford, wildworks, domainsupport, sabernhardt, rcreators, desrosj, sainathpoojary. 
    Fixes #62313, #62335.
    Built from https://develop.svn.wordpress.org/trunk@59409
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58795 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59408';
+$wp_version = '6.8-alpha-59409';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
