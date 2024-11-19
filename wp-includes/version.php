226f7f79641f60cb959520e178df9673ad99bfdb
desrosj <desrosj@git.wordpress.org>
2024-11-19 13:49:17 +0000

    Editor: Fix the JS to select, save, and update categories on the old Edit Post screen.
    Reviewed by desrosj, azaozz.
    Merges [59414] to the 6.7 branch.
    
    Props: charleslf, im3dabasia1, desrosj, dhruvang21, Zargarov, sainathpoojary, azaozz.
    Fixes: #62440.
    Built from https://develop.svn.wordpress.org/branches/6.7@59421
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.7@58807 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7.1-alpha-59420';
+$wp_version = '6.7.1-alpha-59421';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
