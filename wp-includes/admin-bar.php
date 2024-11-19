e8741c53c814cfd130462720b91d4f05665db742
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-11-19 22:18:21 +0000

    Toolbar: Allow the Learn WordPress link to be localized.
    The Learn WordPress website is supposed to automatically redirect to the correct locale according to the browser's language settings, however that may not work as expected in some cases.
    
    This commit brings consistency with the other WordPress.org links, which can be localized as appropriate.
    
    Follow-up to [56720].
    
    Props timse201, ruturajraval2305, yogeshbhutkar, ajayghaghretiya-multidots, swissspidy, sabernhardt, im3dabasia1, mukesh27.
    Fixes #62459.
    Built from https://develop.svn.wordpress.org/trunk@59425
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58811 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/admin-bar.php
+++ wp-includes/admin-bar.php
@@ -205,7 +205,7 @@ function wp_admin_bar_wp_menu( $wp_admin_bar ) {
 			'parent' => 'wp-logo-external',
 			'id'     => 'learn',
 			'title'  => __( 'Learn WordPress' ),
-			'href'   => 'https://learn.wordpress.org/',
+			'href'   => __( 'https://learn.wordpress.org/' ),
 		)
 	);
 
