d37c855db1525ce735cb6ab576d032f32c1cdeae
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2024-11-20 11:22:19 +0000

    I18N: Adjust translator comments in `nav-menu.js`.
    Includes:
    * Moving the comments directly above `wp.i18n._x()` so that they can be picked up properly.
    * Simplifying the context to avoid unnecessarily translating the string twice for the same use case.
    * Using the established translator comments format for consistency.
    
    Follow-up to [59428].
    
    See #62402.
    Built from https://develop.svn.wordpress.org/trunk@59431
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58817 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59430';
+$wp_version = '6.8-alpha-59431';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
