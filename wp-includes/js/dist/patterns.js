019b2bf7a4c1536243ab51d7e2379e4a5c360222
cbravobernal <cbravobernal@git.wordpress.org>
2024-11-20 16:00:32 +0000

    Editor: update npm packages in trunk for 6.7.1.
    Syncs Editor packages for WordPress 6.7.1 RC1. Includes the following PRs:
    - https://github.com/WordPress/gutenberg/pull/66945
    - https://github.com/WordPress/gutenberg/pull/66889
    - https://github.com/WordPress/gutenberg/pull/67139
    
    Props mmaattiiaass, ramonopoly, mamaduka, get_dave, poena, ntsekouras, mcsf, jsnajdr, 0mirka00, desrosj, joemcgill, cbravobernal, azaozz, room34, mayanktripathi32, im3dabasia1, jonsurrell.
    
    Fixes #62478, #62447. 
    
    
    Built from https://develop.svn.wordpress.org/branches/6.7@59437
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.7@58823 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/dist/patterns.js
+++ wp-includes/js/dist/patterns.js
@@ -683,7 +683,7 @@ function CreatePatternModalContents({
 function getTermLabels(pattern, categories) {
   // Theme patterns rely on core pattern categories.
   if (pattern.type !== PATTERN_TYPES.user) {
-    return categories.core?.filter(category => pattern.categories.includes(category.name)).map(category => category.label);
+    return categories.core?.filter(category => pattern.categories?.includes(category.name)).map(category => category.label);
   }
   return categories.user?.filter(category => pattern.wp_pattern_category.includes(category.id)).map(category => category.label);
 }
