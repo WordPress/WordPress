437ee0607cbaa381252a5f467ea4a7fc22869034
cbravobernal <cbravobernal@git.wordpress.org>
2024-11-21 15:11:19 +0000

    Post WordPress 6.7.1 version bump.
    The 6.7 branch is now 6.7.2-alpha.
    
    Built from https://develop.svn.wordpress.org/branches/6.7@59447
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.7@58833 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7.1';
+$wp_version = '6.7.2-alpha-59447';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
