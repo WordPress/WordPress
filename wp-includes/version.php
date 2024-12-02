8f8ffaab84214258fd0400c5ce11cd1c1f813a57
poena <poena@git.wordpress.org>
2024-12-02 05:44:19 +0000

    Twenty Thirteen & Twenty Sixteen: Correct the border of the latest comments block in the editor.
    This change hides the top border of the first comment in the latest comments block, so that the design
    in the editor and the front looks the same.
    
    Props viralsampat, sabernhardt, aishwarryapande, parthvataliya, imranhasanraaz.
    Fixes #62282.
    Built from https://develop.svn.wordpress.org/trunk@59476
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58862 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59475';
+$wp_version = '6.8-alpha-59476';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
