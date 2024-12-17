9aa68357b0580871b74933deabb3e6b82b6a3177
Pascal Birchler <pascal.birchler@gmail.com>
2024-12-17 13:28:17 +0000

    Build/Test Tools: Fix version number when downgrading WP in performance tests.
    The tagged version on GitHub requires the trailing `.0`, but `wp core update` doesn't accept a trailing zero.
    
    Follow-up to [59525].
    
    See #62148.
    Built from https://develop.svn.wordpress.org/trunk@59526
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58912 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59525';
+$wp_version = '6.8-alpha-59526';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
