dfd4b20ea2c79cce35b080a859a4e762e703cd58
John Blackbourn <johnbillion@git.wordpress.org>
2024-12-18 11:31:25 +0000

    Build/Test Tools: Remove an unnecessary call to svn in a debugging step.
    None of the steps in any of the workflows use svn, so this debugging step is unnecessary, and svn has been removed in the ubuntu-24.04 runner which will be rolling out to GitHub Actions imminently.
    
    See #62221
    
    Built from https://develop.svn.wordpress.org/trunk@59534
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58920 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59533';
+$wp_version = '6.8-alpha-59534';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
