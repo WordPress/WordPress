3ed22fda3a4c55faf263226709e248ae6cd4abd6
desrosj <desrosj@git.wordpress.org>
2025-01-16 20:21:23 +0000

    Build/Test Tools: Add input for disabling AppArmor.
    Old branches requiring outdated versions of Chromium to run JavaScript tests have recently started failing as a result of the `ubuntu-latest` container being updated to point to `ubuntu-24`.
    
    This introduces a new input to the reusable JavaScript testing workflow to allow a fix to be used without having to update Chromium or tests in these branches.
    
    Props swissspidy.
    See #62808.
    Built from https://develop.svn.wordpress.org/trunk@59635
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@58998 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59634';
+$wp_version = '6.8-alpha-59635';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
