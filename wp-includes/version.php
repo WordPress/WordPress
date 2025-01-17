570c050e53929af4b9a0e55b312d4334fd46f2c0
desrosj <desrosj@git.wordpress.org>
2025-01-17 14:03:22 +0000

    Build/Test Tools: Auto-close linked PRs on GitHub.
    There are currently ~2,000 open pull requests on GitHub for `wordpress-develop`. Many of these were for testing changes that have already been merged.
    
    To help prevent orphaned pull requests, this new workflow will search for any pull requests referencing the `Fixed` Trac tickets as noted in the commit message and close them out. For now, this only happens for `push` events.
    
    Props peterwilsoncc, swissspidy, johnbillion, davidbaumwald.
    Fixes #62817.
    Built from https://develop.svn.wordpress.org/trunk@59661
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59004 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59660';
+$wp_version = '6.8-alpha-59661';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
