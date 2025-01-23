de74a2bbad3936c7cc800d3386343e341acd3eb1
John Blackbourn <johnbillion@git.wordpress.org>
2025-01-23 17:14:22 +0000

    Build/Test Tools: Correct the usage of the conclusion of the previous run within the Slack notifications workflow.
    Prior to r59679 this value was echoed to GitHub output which meant that its surrounding double quotes lost their significance. Now this value is used directly in the job output it needs to be treated as a plain string.
    
    This concludes the conclusion confusion.
    
    See #82221
    
    Built from https://develop.svn.wordpress.org/trunk@59693
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59035 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59692';
+$wp_version = '6.8-alpha-59693';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
