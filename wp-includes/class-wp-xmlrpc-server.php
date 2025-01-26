906810c2db97e61235326eaaaafdd26afb531355
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-01-26 18:53:23 +0000

    Coding Standards: Use strict comparison in `wp_xmlrpc_server::_prepare_comment()`.
    Follow-up to [20856].
    
    Props aristath, poena, afercia, SergeyBiryukov.
    See #62279.
    Built from https://develop.svn.wordpress.org/trunk@59710
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59052 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-xmlrpc-server.php
+++ wp-includes/class-wp-xmlrpc-server.php
@@ -1179,11 +1179,11 @@ class wp_xmlrpc_server extends IXR_Server {
 		// Format page date.
 		$comment_date_gmt = $this->_convert_date_gmt( $comment->comment_date_gmt, $comment->comment_date );
 
-		if ( '0' == $comment->comment_approved ) {
+		if ( '0' === $comment->comment_approved ) {
 			$comment_status = 'hold';
 		} elseif ( 'spam' === $comment->comment_approved ) {
 			$comment_status = 'spam';
-		} elseif ( '1' == $comment->comment_approved ) {
+		} elseif ( '1' === $comment->comment_approved ) {
 			$comment_status = 'approve';
 		} else {
 			$comment_status = $comment->comment_approved;
