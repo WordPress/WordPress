484c43a0b8d821fb7feee1cf2d6d3a6a19272471
desrosj <desrosj@git.wordpress.org>
2025-01-28 14:02:27 +0000

    Build/Test Tools: Avoid using `*-latest` tags for runner images.
    While using the `ubuntu-latest`, `macos-latest`, and `windows-latest` runner image tags is convenient, it has proven to be problematic in a number of instances as the runners are slowly updated (see #62808 and #62843).
    
    This switches all workflows to using specific version tags representing the latest non-preview versions, which currently are as follows:
    - `ubuntu-24.04`
    - `windows-2022`
    - `macos-14`
    
    Props swissspidy, johnbillion.
    See #62221.
    Built from https://develop.svn.wordpress.org/trunk@59720
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59062 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59719';
+$wp_version = '6.8-alpha-59720';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
