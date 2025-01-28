c8f766843fcbef9126d3237d5e22891f028060a8
desrosj <desrosj@git.wordpress.org>
2025-01-28 16:20:23 +0000

    Build/Test Tools: Adjust the check for runner type when creating a ZIP file.
    Because the build process test workflow accepts an input for runner image, older workflows still use `ubuntu-latest`. This adjusts a conditional check to be more broad, allowing any `ubuntu-` image to match.
    
    Follow up to [59720].
    
    See #62221.
    Built from https://develop.svn.wordpress.org/trunk@59722
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59064 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59720';
+$wp_version = '6.8-alpha-59722';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
