b50b64bf3830a979fb4e7befb5a96e03e2679118
Felix Arntz <felix-arntz@leaves-and-love.net>
2025-01-30 22:32:17 +0000

    Editor: Fix block template registration failing for custom post types containing underscore characters.
    Custom post types may contain underscores, however block template registration has been using a regular expression that disallows underscores. Since the block template name for certain templates is directly associated with which post type it applies to, this regular expression was causing unexpected failures. This changeset adjusts the regular expression to allow block template names with underscore characters, effectively allowing block templates to be registered for any custom post type.
    
    Reviewed by jorbin.
    Merges [59742] to the 6.7 branch.
    
    Props alexandrebuffet, ankitkumarshah, gaambo, jorbin, karthickmurugan, oglekler, poena, sukhendu2002.
    Fixes #62523.
    
    Built from https://develop.svn.wordpress.org/branches/6.7@59743
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.7@59085 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7.2-alpha-59741';
+$wp_version = '6.7.2-alpha-59743';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
