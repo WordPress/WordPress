b39c7d7b4bc88ebd2c5bc1b53d38462876df44a8
John Blackbourn <johnbillion@git.wordpress.org>
2025-02-03 13:37:25 +0000

    Build/Test Tools: Fix the source code path handling when installing the local development environment.
    This ensures the correct code is used to run the installation depending on whether it should be running from the `src` or `build` directory.
    
    Props swissspidy, johnbillion
    
    See #62221
    Built from https://develop.svn.wordpress.org/trunk@59752
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59094 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59751';
+$wp_version = '6.8-alpha-59752';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
