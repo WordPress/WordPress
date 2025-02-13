7244fe456e5ade2bb57ef6df3b8d224284382966
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-02-13 20:31:28 +0000

    Editor: Add title and description to post format archive block templates.
    Follow-up to [52062], [52331], [55500].
    
    Props poena, rinkalpagdar, im3dabasia1, parthvataliya, ruchirj, praful2111.
    Fixes #62326.
    Built from https://develop.svn.wordpress.org/trunk@59819
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59161 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/block-template-utils.php
+++ wp-includes/block-template-utils.php
@@ -206,6 +206,23 @@ function get_default_block_template_types() {
 		),
 	);
 
+	// Add a title and description to post format templates.
+	$post_formats = get_post_format_strings();
+	foreach ( $post_formats as $post_format_slug => $post_format_name ) {
+		$default_template_types[ 'taxonomy-post_format-post-format-' . $post_format_slug ] = array(
+			'title'       => sprintf(
+				/* translators: %s: Post format name. */
+				_x( 'Post Format: %s', 'Template name' ),
+				$post_format_name
+			),
+			'description' => sprintf(
+				/* translators: %s: Post format name. */
+				__( 'Displays the %s post format archive.' ),
+				$post_format_name
+			),
+		);
+	}
+
 	/**
 	 * Filters the list of default template types.
 	 *
