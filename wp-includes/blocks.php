21ba106dfbec2c446c6f4b19206152a43d7c9427
johnjamesjacoby <johnjamesjacoby@git.wordpress.org>
2025-02-19 17:14:23 +0000

    Block Hooks: Remove `WP_Post` type hint.
    This change prevents a PHP deprecation notice in the new `apply_block_hooks_to_content_from_post_object()` function, introduced in r59838.
    
    Fixes #62716.
    Built from https://develop.svn.wordpress.org/trunk@59839
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59181 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks.php
+++ wp-includes/blocks.php
@@ -1160,7 +1160,7 @@ function apply_block_hooks_to_content( $content, $context = null, $callback = 'i
  *                               Default: 'insert_hooked_blocks'.
  * @return string The serialized markup.
  */
-function apply_block_hooks_to_content_from_post_object( $content, WP_Post $post = null, $callback = 'insert_hooked_blocks' ) {
+function apply_block_hooks_to_content_from_post_object( $content, $post = null, $callback = 'insert_hooked_blocks' ) {
 	// Default to the current post if no context is provided.
 	if ( null === $post ) {
 		$post = get_post();
