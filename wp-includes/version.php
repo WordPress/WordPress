bb88b6a7f2ebcb40341d5aa05673eaae45d729a7
Aaron Jorbin <aaron@jorb.in>
2025-02-20 20:02:31 +0000

    Build/Test: Fix JavaScript linting scripts
    `wp-prettier` was out of date and no longer compatible with wp-scripts. Additionally, many generated files and plugins were not being properly ignored which could cause linting to take so long that it was basically unusable.
    
    Props ankitkumarshah, jorbin.
    See #62935.
    
    
    Built from https://develop.svn.wordpress.org/trunk@59848
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59190 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59847';
+$wp_version = '6.8-alpha-59848';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
