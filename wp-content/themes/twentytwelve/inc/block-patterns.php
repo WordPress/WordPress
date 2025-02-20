a3d2826e61ecd470c4dcb3e27d534d600ccd606f
poena <poena@git.wordpress.org>
2025-02-20 05:40:24 +0000

    Twenty Twelve: Update the quote pattern to use the quote block.
    This change updates the Left-aligned Large Quote pattern to use the quote block instead of
    paragraph blocks.
    
    Props flairwebtechnologies, sabernhardt, karmatosed, wpeople, poena, nithins53, balub, sukhendu2002, dilip2615, itpathsolutions, shraddhagore, shailu25.
    Fixes #61765.
    Built from https://develop.svn.wordpress.org/trunk@59842
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59184 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/inc/block-patterns.php
+++ wp-content/themes/twentytwelve/inc/block-patterns.php
@@ -67,18 +67,17 @@ if ( function_exists( 'register_block_pattern' ) ) {
 		)
 	);
 
-	// Heading and paragraph arranged as a large quote.
+	// Quote with large paragraph text.
 	register_block_pattern(
 		'twentytwelve/large-quote',
 		array(
 			'title'         => esc_html__( 'Left-aligned Large Quote', 'twentytwelve' ),
 			'categories'    => array( 'twentytwelve' ),
 			'viewportWidth' => 700,
-			'content'       => '<!-- wp:paragraph {"style":{"typography":{"fontSize":"40px","lineHeight":1.5}}} -->
-				<p style="font-size:40px;line-height:1.5"><strong><em>' . esc_html__( '"Few people are capable of expressing with equanimity opinions which differ from the prejudices of their social environment. Most people are even incapable of forming such opinions."', 'twentytwelve' ) . '</em></strong></p>
-				<!-- /wp:paragraph --><!-- wp:paragraph -->
-				<p><em>' . esc_html__( '—  Albert Einstein', 'twentytwelve' ) . '</em></p>
-				<!-- /wp:paragraph -->',
+			'content'       => '<!-- wp:quote --> <blockquote class="wp-block-quote"><!-- wp:paragraph {"style":{"typography":{"fontSize":"40px","lineHeight":1.5}}} -->
+				<p style="font-size:40px;line-height:1.5"><strong>' . esc_html__( '"Few people are capable of expressing with equanimity opinions which differ from the prejudices of their social environment. Most people are even incapable of forming such opinions."', 'twentytwelve' ) . '</strong></p>
+				<!-- /wp:paragraph --><cite>' . esc_html__( '&mdash;  Albert Einstein', 'twentytwelve' ) . '</cite></blockquote>
+				<!-- /wp:quote -->',
 		)
 	);
 
