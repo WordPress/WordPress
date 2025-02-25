601df51eb813673a0faf702d656596845ad23ef8
audrasjb <audrasjb@git.wordpress.org>
2025-02-25 18:34:15 +0000

    Help/About: Replace "Support" with "Support Forums" in `import.php` help tab.
    This brings consistency with other admin screens.
    
    Props Guido07111975, audrasjb, sabernhardt.
    Fixes #63016.
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@59868
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59210 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/import.php
+++ wp-admin/import.php
@@ -30,7 +30,7 @@ get_current_screen()->add_help_tab(
 get_current_screen()->set_help_sidebar(
 	'<p><strong>' . __( 'For more information:' ) . '</strong></p>' .
 	'<p>' . __( '<a href="https://wordpress.org/documentation/article/tools-import-screen/">Documentation on Import</a>' ) . '</p>' .
-	'<p>' . __( '<a href="https://wordpress.org/support/forums">Support</a>' ) . '</p>'
+	'<p>' . __( '<a href="https://wordpress.org/support/forums/">Support forums</a>' ) . '</p>'
 );
 
 if ( current_user_can( 'install_plugins' ) ) {
