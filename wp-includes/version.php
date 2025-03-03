67fb549c43971acb5193a4fc4200b0f4ccf5574a
John Blackbourn <johnbillion@git.wordpress.org>
2025-03-03 22:09:24 +0000

    Build/Test Tools: Remove redundant escaping from the commit message that's passed to the Slack API.
    This escaping is no longer needed since all instances of inline expressions were removed from workflows in r59679. The commit message can now be treated as a plain text string in an environment variable throughout the workflow.
    
    Props johnbillion, swissspidy
    
    Unprops johnblackbourn
    
    See #62221
    
    Built from https://develop.svn.wordpress.org/trunk@59920
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59262 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59919';
+$wp_version = '6.8-alpha-59920';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
