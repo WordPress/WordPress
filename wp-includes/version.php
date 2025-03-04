9bb1251fcd454ecdf6fcb39d5e0324402f0fa0a6
joedolson <joedolson@git.wordpress.org>
2025-03-04 19:18:24 +0000

    Media: Tests: Remove extraneous @test annotation.
    The data provider for tests added in [59902] had an @test annotation, causing it to be run as if it were a test, throwing a risky test warning. Remove the @test annotation to prevent this undesired warning.
    
    Props johnbillion, joedolson.
    Fixes #54738.
    Built from https://develop.svn.wordpress.org/trunk@59934
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59276 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-beta1-59933';
+$wp_version = '6.8-beta1-59934';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
