0afbfec0fd0b123b5f6d8616379d84fcd7475906
Peter Wilson <wilson@peterwilson.cc>
2025-03-04 02:19:24 +0000

    Build/Test Tools: Remove redundant decoding from the commit message that's passed to the Slack API.
    This JSON decoding is no longer needed since the commit message was converted to a plain string in r59920. The commit message can now be treated as a plain text string in an environment variable throughout the workflow.
    
    Props johnbillion.
    See #62221
    
    Built from https://develop.svn.wordpress.org/trunk@59922
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59264 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-alpha-59921';
+$wp_version = '6.8-alpha-59922';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
