a422e1033a4c9e29ce319fcfa0633b78dd304d4d
audrasjb <audrasjb@git.wordpress.org>
2025-03-06 14:24:23 +0000

    Security: Remove Cafelog URL.
    This changeset removes Cafelog links from `licence.txt` and `readme.html` files as the domain was purchased by another entity, providing unsafe content.
    
    Props gencmedya, audrasjb, JeffPaul, peterwilsoncc, jorbin, johnbillion.
    Fixes #63000.
    
    
    Built from https://develop.svn.wordpress.org/trunk@59944
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59286 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-beta1-59943';
+$wp_version = '6.8-beta1-59944';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
