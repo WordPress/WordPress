41c50755ede588590b6df9020bdcb71a465c5251
audrasjb <audrasjb@git.wordpress.org>
2025-03-13 10:04:34 +0000

    Users: Remove unwanted `aria-describedby` attribute in User Edit.
    This changeset removes the `aria-describedby` attribute when not editing the current user, as no description paragraph is associated.
    
    Props kkmuffme, audrasjb, faisal03, shailu25, nandow, eddystile, marineevain, qhaensler, virginienacci.
    Fixes #63006.
    
    
    Built from https://develop.svn.wordpress.org/trunk@59978
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59320 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/user-edit.php
+++ wp-admin/user-edit.php
@@ -545,11 +545,13 @@ switch ( $action ) {
 					<tr class="user-email-wrap">
 						<th><label for="email"><?php _e( 'Email' ); ?> <span class="description"><?php _e( '(required)' ); ?></span></label></th>
 						<td>
-							<input type="email" name="email" id="email" aria-describedby="email-description" value="<?php echo esc_attr( $profile_user->user_email ); ?>" class="regular-text ltr" />
 							<?php if ( $profile_user->ID === $current_user->ID ) : ?>
+								<input type="email" name="email" id="email" aria-describedby="email-description" value="<?php echo esc_attr( $profile_user->user_email ); ?>" class="regular-text ltr" />
 								<p class="description" id="email-description">
 									<?php _e( 'If you change this, an email will be sent at your new address to confirm it. <strong>The new address will not become active until confirmed.</strong>' ); ?>
 								</p>
+							<?php else : ?>
+								<input type="email" name="email" id="email" value="<?php echo esc_attr( $profile_user->user_email ); ?>" class="regular-text ltr" />
 							<?php endif; ?>
 
 							<?php
