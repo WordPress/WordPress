5a57ebd850575516c013eb649e59b6f1f54357ba
desrosj <desrosj@git.wordpress.org>
2025-03-16 23:00:33 +0000

    Security: Update bundled Root Certificates.
    The Root Certificate bundle maintained by Mozilla ships in WordPress to allow SSL certificates to be verified on hosts with incomplete, outdated, or invalid local SSL configurations.
    
    This updates the `ca-bundle.crt` file to the latest version, which applies upstream changes from the bundle maintained by Mozilla and keeps all unexpired legacy 1024bit certificates which are kept for backward compatibility purposes (see [35919]).
    
    Partially merges [59740] and [59969] to the 6.5 branch.
    
    Props johnbillion, desrosj, whyisjake, ayeshrajans, SergeyBiryukov, swissspidy, skithund, barry.
    See #62811, #62711.
    Built from https://develop.svn.wordpress.org/branches/6.5@59995
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@59337 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5.6-alpha-59685';
+$wp_version = '6.5.6-alpha-59995';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
