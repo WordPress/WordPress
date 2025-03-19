3bf92ea78e99d94aa25d4b703e8d27f561a617b6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-03-19 22:10:29 +0000

    Build/Test Tools: Enable starting Xdebug with the PHP request in local environment.
    This aims to facilitate setting up Xdebug locally without editing the `tools/local-env/php-config.ini` file, which is not ignored by Git by default.
    
    Note that this does not enable Xdebug by default, since both `LOCAL_PHP_XDEBUG=true` and `LOCAL_PHP_XDEBUG_MODE=debug` are additionally required for Xdebug to work.
    
    Props SirLouen, jules-colle, johnbillion, pento.
    Fixes #49953.
    Built from https://develop.svn.wordpress.org/trunk@60056
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59392 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-beta3-60055';
+$wp_version = '6.8-beta3-60056';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
