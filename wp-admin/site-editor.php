e895a309d3ddd88b449e75e6e389aa5cba9d8fc6
Peter Wilson <wilson@peterwilson.cc>
2025-03-20 23:03:30 +0000

    Site Editor: Fix PHP warning pre-loading page REST requests.
    Fix a PHP warning when opening a page directly in the site editor, eg by navigating to the URL `/wp-admin/site-editor.php?p=%2Fpage&postId=200`, caused by an undefined variable name.
    
    Props abcd95, joemcgill, narenin, soyebsalar, websiteredev, wildworks.
    Fixes #63122.
    
    Built from https://develop.svn.wordpress.org/trunk@60061
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59397 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/site-editor.php
+++ wp-admin/site-editor.php
@@ -239,7 +239,7 @@ if ( $block_editor_context->post ) {
 			$preload_paths[] = add_query_arg(
 				'slug',
 				// @see https://github.com/WordPress/gutenberg/blob/e093fefd041eb6cc4a4e7f67b92ab54fd75c8858/packages/core-data/src/private-selectors.ts#L244-L254
-				empty( $block_editor_context->post->post_name ) ? 'page' : 'page-' . $post->post_name,
+				empty( $block_editor_context->post->post_name ) ? 'page' : 'page-' . $block_editor_context->post->post_name,
 				'/wp/v2/templates/lookup'
 			);
 		}
