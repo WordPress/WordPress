ff69524db8426072a3143b5f2e3b2b7c635d4289
desrosj <desrosj@git.wordpress.org>
2025-03-20 13:22:26 +0000

    Build/Test Tools: Use the wordpress-develop-pr-bot app for changes.
    This switches to using the `wordpress-develop-pr-bot` GitHub app when authoring commits instead of masquerading as Dependabot.
    
    Follow up to [59983], [60052].
    
    See #62221.
    Built from https://develop.svn.wordpress.org/trunk@60059
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59395 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-beta3-60058';
+$wp_version = '6.8-beta3-60059';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
