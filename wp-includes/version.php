c04ed74c304e722cc1eabfee9b957dbec15c276e
desrosj <desrosj@git.wordpress.org>
2025-03-25 16:43:25 +0000

    Post 6.8 branching version update.
    Trunk is now `6.9-alpha`
    
    Fixes #63164.
    Built from https://develop.svn.wordpress.org/trunk@60093
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59429 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-RC1-60090';
+$wp_version = '6.9-alpha-60093';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
