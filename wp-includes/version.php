61b115f4b86d5c86b8d183003963da31ae3bfe11
audrasjb <audrasjb@git.wordpress.org>
2025-03-30 23:39:20 +0000

    Docs: Remove an unwanted trailing space found after [60108].
    Follow-up to [60108].
    
    See #63041, #63166.
    
    
    Built from https://develop.svn.wordpress.org/trunk@60109
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59445 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60108';
+$wp_version = '6.9-alpha-60109';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
