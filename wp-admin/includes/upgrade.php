ec5bd33b1132bc2dc3d480208316ab04c8294b09
audrasjb <audrasjb@git.wordpress.org>
2025-03-31 12:46:24 +0000

    Editor: Update the blocks on the sample page to the latest HTML.
    This changeset updates the Quote Blocks HTML markup of the default sample page, to avoid unecessary migration (and the related console information) when loading its content in the editor.
    
    Props wildworks, abcd95, audrasjb, ankitkumarshah.
    Fixes #63176.
    
    
    Built from https://develop.svn.wordpress.org/trunk@60111
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59447 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/upgrade.php
+++ wp-admin/includes/upgrade.php
@@ -315,20 +315,20 @@ Commenter avatars come from <a href="%s">Gravatar</a>.'
 			$first_page .= __( "This is an example page. It's different from a blog post because it will stay in one place and will show up in your site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. It might say something like this:" );
 			$first_page .= "</p>\n<!-- /wp:paragraph -->\n\n";
 
-			$first_page .= "<!-- wp:quote -->\n<blockquote class=\"wp-block-quote\"><p>";
+			$first_page .= "<!-- wp:quote -->\n<blockquote class=\"wp-block-quote\">\n<!-- wp:paragraph -->\n<p>";
 			/* translators: First page content. */
 			$first_page .= __( "Hi there! I'm a bike messenger by day, aspiring actor by night, and this is my website. I live in Los Angeles, have a great dog named Jack, and I like pi&#241;a coladas. (And gettin' caught in the rain.)" );
-			$first_page .= "</p></blockquote>\n<!-- /wp:quote -->\n\n";
+			$first_page .= "</p>\n<!-- /wp:paragraph -->\n</blockquote>\n<!-- /wp:quote -->\n\n";
 
 			$first_page .= "<!-- wp:paragraph -->\n<p>";
 			/* translators: First page content. */
 			$first_page .= __( '...or something like this:' );
 			$first_page .= "</p>\n<!-- /wp:paragraph -->\n\n";
 
-			$first_page .= "<!-- wp:quote -->\n<blockquote class=\"wp-block-quote\"><p>";
+			$first_page .= "<!-- wp:quote -->\n<blockquote class=\"wp-block-quote\">\n<!-- wp:paragraph -->\n<p>";
 			/* translators: First page content. */
 			$first_page .= __( 'The XYZ Doohickey Company was founded in 1971, and has been providing quality doohickeys to the public ever since. Located in Gotham City, XYZ employs over 2,000 people and does all kinds of awesome things for the Gotham community.' );
-			$first_page .= "</p></blockquote>\n<!-- /wp:quote -->\n\n";
+			$first_page .= "</p>\n<!-- /wp:paragraph -->\n</blockquote>\n<!-- /wp:quote -->\n\n";
 
 			$first_page .= "<!-- wp:paragraph -->\n<p>";
 			$first_page .= sprintf(
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60110';
+$wp_version = '6.9-alpha-60111';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
