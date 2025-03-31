bccd9fa8760b77e46d67b112a2db0faf561dc616
Ruchita Sojitra <ruchita.classictechies@gmail.com>
2025-03-31 13:19:03 +0530

    Delete wp-content/themes/twentytwentytwo directory

--- wp-content/themes/twentytwentytwo/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// There is nothing output here because block themes do not use php templates.
-// There is a core ticket discussing removing this requirement for block themes:
-// https://core.trac.wordpress.org/ticket/54272.
