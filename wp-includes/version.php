26530a3abd2cdccbf63e45e1f4eb624d41788721
Joe McGill <joemcgill@gmail.com>
2025-04-01 13:32:26 +0000

    General: Improve unit tests for `wp_unique_id_from_values()`.
    This is a follow-up to [60038], which updates the PHPUnit tests to account for different systems producing potentially different hashes due to platform specific floating point precision settings.
    
    Reviewed by audrasjb, desrosj.
    Merges [60113] to the 6.8 branch.
    
    Props debarghyabanerjee, joemcgill, peterwilsoncc, siliconforks.
    Fixes #63175.
    
    Built from https://develop.svn.wordpress.org/branches/6.8@60116
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.8@59452 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-RC1-60115';
+$wp_version = '6.8-RC1-60116';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
