fde5146524087eef6978092499d903ae086d2b37
desrosj <desrosj@git.wordpress.org>
2025-04-03 14:48:26 +0000

    Security: Don't fail the Argon2-related tests when it's not available on the test infrastructure.
    These tests are not critical to the bcrypt functionality, they exist to reaffirm that the underlying use of `password_hash()` and `password_verify()` supports this algorithm. The Argon2 tests therefore shouldn't unnecessarily fail on hosts that don't support it.
    
    Reviewed by desrosj.
    Merges [60124] to the 6.8 branch.
    
    Props desrosj, johnbillion.
    Fixes #21022.
    Built from https://develop.svn.wordpress.org/branches/6.8@60126
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.8@59462 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8-RC2-60125';
+$wp_version = '6.8-RC2-60126';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
