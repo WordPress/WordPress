c980b941ec65b2f2ef488112463646a8fb2d5e9e
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-04-14 22:34:25 +0000

    Docs: Correct parameter types for `get_comments_number_text()`.
    This updates the `@param` types to correctly reflect the default `false` parameter.
    
    Follow-up to [6495], [25567], [28912], [49936].
    
    Props justlevine.
    See #63268.
    Built from https://develop.svn.wordpress.org/trunk@60166
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59502 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/comment-template.php
+++ wp-includes/comment-template.php
@@ -954,10 +954,10 @@ function comments_number( $zero = false, $one = false, $more = false, $post = 0 
  * @since 4.0.0
  * @since 5.4.0 Added the `$post` parameter to allow using the function outside of the loop.
  *
- * @param string      $zero Optional. Text for no comments. Default false.
- * @param string      $one  Optional. Text for one comment. Default false.
- * @param string      $more Optional. Text for more than one comment. Default false.
- * @param int|WP_Post $post Optional. Post ID or WP_Post object. Default is the global `$post`.
+ * @param string|false $zero Optional. Text for no comments. Default false.
+ * @param string|false $one  Optional. Text for one comment. Default false.
+ * @param string|false $more Optional. Text for more than one comment. Default false.
+ * @param int|WP_Post  $post Optional. Post ID or WP_Post object. Default is the global `$post`.
  * @return string Language string for the number of comments a post has.
  */
 function get_comments_number_text( $zero = false, $one = false, $more = false, $post = 0 ) {
