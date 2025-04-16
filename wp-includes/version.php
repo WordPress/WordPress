0d5205144d40387982b4a7b047ed099fc2407f1c
Peter Wilson <wilson@peterwilson.cc>
2025-04-16 22:23:29 +0000

    Media: Prevent Multisite fatal error in legacy file rewrites.
    Removes a call to the `is_super_admin()` function in `ms-files.php` as the function isn't available when using the `SHORTINIT` feature, causing fatal errors on systems using file rewrites.
    
    Reverts [59967].
    
    Reviewed by jorbin.
    Merges [60170] to the 6.8 branch.
    
    Props audrasjb, desrosj, jeremyfelt, johnjamesjacoby, jorbin, presskopp, verygoode, dd32, joemcgill.
    See #36803.
    Fixes #63285.
    
    Built from https://develop.svn.wordpress.org/branches/6.8@60171
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.8@59507 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8.1-alpha-60169';
+$wp_version = '6.8.1-alpha-60171';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
