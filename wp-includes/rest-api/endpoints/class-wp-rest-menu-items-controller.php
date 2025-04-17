f581a7f5b370bad6f5d8ba21f8d62edf591bfd49
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-04-17 12:10:36 +0000

    Docs: Correct parameter name for `rest_menu_read_access` filter.
    The filter's third parameter was incorrectly named `$this`, which is not a valid parameter name.
    
    Follow-up to [59718], [59734].
    
    Props justlevine.
    See #63268.
    Built from https://develop.svn.wordpress.org/trunk@60172
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59508 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/endpoints/class-wp-rest-menu-items-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-menu-items-controller.php
@@ -88,7 +88,7 @@ class WP_REST_Menu_Items_Controller extends WP_REST_Posts_Controller {
 		 * @param bool               $read_only_access Whether the current user has read access to menu items
 		 *                                             via the REST API.
 		 * @param WP_REST_Request    $request          Full details about the request.
-		 * @param WP_REST_Controller $this             The current instance of the controller.
+		 * @param WP_REST_Controller $controller       The current instance of the controller.
 		 */
 		$read_only_access = apply_filters( 'rest_menu_read_access', false, $request, $this );
 		if ( $read_only_access ) {
