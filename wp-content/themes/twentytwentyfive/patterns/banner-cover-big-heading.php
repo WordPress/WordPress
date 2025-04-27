b9c5b23417622cd778d27ff23520068ea6e9a2e8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-04-27 19:57:33 +0000

    Twenty Twenty-Five: Remove redundant `echo` from `esc_html_e()`.
    Follow-up to [59233].
    
    Props dilipbheda, sabernhardt.
    Fixes #63345.
    Built from https://develop.svn.wordpress.org/trunk@60192
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59528 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwentyfive/patterns/banner-cover-big-heading.php
+++ wp-content/themes/twentytwentyfive/patterns/banner-cover-big-heading.php
@@ -24,7 +24,7 @@
 		<!-- wp:group {"align":"full","layout":{"type":"default"}} -->
 		<div class="wp-block-group alignfull">
 			<!-- wp:heading {"align":"left","style":{"typography":{"fontSize":"clamp(1rem, 380px, 24vw)","letterSpacing":"-0.02em","lineHeight":"1","fontWeight":"700","fontStyle":"normal"}}} -->
-			<h2 class="wp-block-heading has-text-align-left" style="font-size:clamp(1rem, 380px, 24vw);font-style:normal;font-weight:700;letter-spacing:-0.02em;line-height:1"><?php echo esc_html_e( 'Stories', 'twentytwentyfive' ); ?></h2>
+			<h2 class="wp-block-heading has-text-align-left" style="font-size:clamp(1rem, 380px, 24vw);font-style:normal;font-weight:700;letter-spacing:-0.02em;line-height:1"><?php esc_html_e( 'Stories', 'twentytwentyfive' ); ?></h2>
 			<!-- /wp:heading -->
 
 		</div>
