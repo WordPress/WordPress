dc758b0b02509d66399ed17785f7ca9bc9247cb9
desrosj <desrosj@git.wordpress.org>
2025-05-02 13:01:21 +0000

    Build/Test Tools: Pin a Gutenberg version in the 6.6 branch.
    The minimum required version of WordPress for the Gutenberg plugin will be bumped to `6.7` in version `20.8.0`. This pins the final version with support for WordPress 6.6 to ensure E2E tests continue to pass.
    
    Props mamaduka.
    Fixes #63377.
    Built from https://develop.svn.wordpress.org/branches/6.6@60214
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.6@59550 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6.3-alpha-59994';
+$wp_version = '6.6.3-alpha-60214';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
