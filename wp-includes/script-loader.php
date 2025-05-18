779dc9c93d18c19c41897d5b993c77eec6787edc
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-05-18 21:17:27 +0000

    Script Loader: Add `common.js` as a dependency for `dashboard.js`.
    `dashboard.js` uses the `deprecateL10nObject()` function, which is defined in `common.js`.
    
    Follow-up to [48923], [49151].
    
    Props SirLouen.
    Fixes #63432.
    Built from https://develop.svn.wordpress.org/trunk@60241
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59577 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -1497,7 +1497,7 @@ function wp_default_scripts( $scripts ) {
 		$scripts->add( 'wp-color-picker', "/wp-admin/js/color-picker$suffix.js", array( 'iris' ), false, 1 );
 		$scripts->set_translations( 'wp-color-picker' );
 
-		$scripts->add( 'dashboard', "/wp-admin/js/dashboard$suffix.js", array( 'jquery', 'admin-comments', 'postbox', 'wp-util', 'wp-a11y', 'wp-date' ), false, 1 );
+		$scripts->add( 'dashboard', "/wp-admin/js/dashboard$suffix.js", array( 'common', 'jquery', 'admin-comments', 'postbox', 'wp-util', 'wp-a11y', 'wp-date' ), false, 1 );
 		$scripts->set_translations( 'dashboard' );
 
 		$scripts->add( 'list-revisions', "/wp-includes/js/wp-list-revisions$suffix.js" );
