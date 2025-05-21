70c96c141d37322ebef8385071f51e98908a4b82
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-05-21 13:01:31 +0000

    Twenty Eleven: Use correct escaping function for `alt` attributes in block patterns.
    Follow-up to [51103].
    
    Props viralsampat, sabernhardt.
    See #63264.
    Built from https://develop.svn.wordpress.org/trunk@60244
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59580 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60243';
+$wp_version = '6.9-alpha-60244';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
