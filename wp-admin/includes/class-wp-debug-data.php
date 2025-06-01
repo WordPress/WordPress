46acc4afff8fd4c9d0dc697a277f175b78313898
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-06-01 20:45:31 +0000

    Docs: Mark `WP_Debug_Data::get_sizes()` as deprecated in WordPress 5.6.
    Follow-up to [49154].
    
    See #63166.
    Built from https://develop.svn.wordpress.org/trunk@60272
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59608 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-debug-data.php
+++ wp-admin/includes/class-wp-debug-data.php
@@ -1860,6 +1860,8 @@ class WP_Debug_Data {
 	 * Intended to supplement the array returned by `WP_Debug_Data::debug_data()`.
 	 *
 	 * @since 5.2.0
+	 * @deprecated 5.6.0 Use WP_REST_Site_Health_Controller::get_directory_sizes()
+	 * @see WP_REST_Site_Health_Controller::get_directory_sizes()
 	 *
 	 * @return array The sizes of the directories, also the database size and total installation size.
 	 */
