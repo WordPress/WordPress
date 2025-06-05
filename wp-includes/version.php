410b4b0e2ebc2b002e3ffc1e7bd659d151ef6b82
Aaron Jorbin <aaron@jorb.in>
2025-06-05 20:04:30 +0000

    Build/Test Tools: Remove unused import.
    In [59279], this import was added but never used, likely since it was also imported as `local_env_uitls` on the next line.
    
    Props yogeshbhutkar, mukesh27.
    Fixes #63477. See #61218.
    
    Built from https://develop.svn.wordpress.org/trunk@60287
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59623 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60286';
+$wp_version = '6.9-alpha-60287';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
