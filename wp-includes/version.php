03720cec5c8a90e99a26f8e9ebf3b35d6ebda546
joedolson <joedolson@git.wordpress.org>
2025-06-05 14:03:49 +0000

    Administration: Hide skip to toolbar link on small viewports.
    Since r55326, the admin sidebar autocloses on small viewports when focus exits the sidebar. Because of this, there is no case where the Skip to Toolbar link will have any function on small viewports.
    
    Props sabernhardt, abcd95, alh0319, mleray, joedolson.
    Fixes #63286.
    Built from https://develop.svn.wordpress.org/trunk@60286
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59622 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60285';
+$wp_version = '6.9-alpha-60286';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
