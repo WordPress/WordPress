6fc918f8b02444782be1a2c69d941b873684f958
Weston Ruter <weston@xwp.co>
2025-06-13 23:03:30 +0000

    Build/Test Tools: Improve dev environment's CLI in speed, non-interactive usage, and argument handling.
    * Start `cli` container when running `env:start`. This greatly speeds up calls to WP-CLI since the container is already running rather than having to start up for each call. 
    * Facilitate calls to `env:cli` in non-interactive context (non-TTY) to allow piping content into commands or use in shell scripts.
    * Fix passing arguments to WP-CLI from `env:cli` so that arguments with spaces are passed as expected.
    * Fix JSHint issues.
    
    This aligns the wordpress-develop environment closer to wp-env. See https://github.com/WordPress/gutenberg/pull/50007.
    
    Props westonruter, jorbin, SirLouen, sandeepdahiya.
    Fixes #63564.
    
    Built from https://develop.svn.wordpress.org/trunk@60308
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59644 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60307';
+$wp_version = '6.9-alpha-60308';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
