47a60b9abefd2a1a2f5929b07baa7e2241b0dd55
John Blackbourn <johnbillion@git.wordpress.org>
2025-06-18 20:38:31 +0000

    Build/Test Tools: Bump the default baseline version used for performance comparison tests.
    See #63170
    Built from https://develop.svn.wordpress.org/trunk@60324
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59660 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60323';
+$wp_version = '6.9-alpha-60324';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
