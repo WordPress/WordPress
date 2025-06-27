142a0688156cc3acc5c0a889fc92f4c302572e66
John Blackbourn <johnbillion@git.wordpress.org>
2025-06-27 14:27:29 +0000

    Options, Meta APIs: Account for URL query parameters when checking the validity of requests to the `/wp/v2/settings` REST API route.
    Follow-up to [60301].
    
    Props sheldorofazeroth, Mamaduka, wildworks, johnbillion
    
    Fixes #41604
    Built from https://develop.svn.wordpress.org/trunk@60357
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59693 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/endpoints/class-wp-rest-settings-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-settings-controller.php
@@ -145,7 +145,7 @@ class WP_REST_Settings_Controller extends WP_REST_Controller {
 	public function update_item( $request ) {
 		$options = $this->get_registered_options();
 
-		$params = $request->get_params();
+		$params = array_diff_key( $request->get_params(), $request->get_query_params() );
 
 		if ( empty( $params ) || ! empty( array_diff_key( $params, $options ) ) ) {
 			$message = empty( $params )
