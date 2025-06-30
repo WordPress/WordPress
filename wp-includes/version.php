61b6f1deaca668379d177218658d98aa55f6cbc0
desrosj <desrosj@git.wordpress.org>
2025-06-30 17:13:22 +0000

    Security: Update composer/ca-bundle to version 1.5.7.
    Merges [60320] to the 6.7 branch.
    
    See #63165.
    Built from https://develop.svn.wordpress.org/branches/6.7@60370
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.7@59706 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7.3-alpha-59977';
+$wp_version = '6.7.3-alpha-60370';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
