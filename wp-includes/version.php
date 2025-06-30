489d3907bb1f1f8690ab1fa452f17238e77e84d7
Tammie Lister <tammie@automattic.com>
2025-06-30 13:00:30 +0000

    Twenty Twenty-Four: Fixes Latest Comment Link Color in Sidebar.
    This sets the link color to work for dark areas when 51205 set to white for any widget area. This was not good for Primary and Footer Widget Areas.
    
    Props sabernhardt, dhruvang21, ravigadhiyawp, sandeepdahiya.
    Fixes #63520.
    
    Built from https://develop.svn.wordpress.org/trunk@60368
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59704 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60367';
+$wp_version = '6.9-alpha-60368';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
