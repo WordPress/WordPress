5fab6cc55bb9d41183b62bd73d04a7abd14571b7
Tammie Lister <tammie@automattic.com>
2025-06-30 09:55:29 +0000

    Twenty Twenty-One: Revert 63549 which brings in margin fix.
    The tests were failing on the theme as adresult of bringing in this once committed. For now, reverting until explore.
    
    See #60366.
    
    Built from https://develop.svn.wordpress.org/trunk@60367
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59703 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60366';
+$wp_version = '6.9-alpha-60367';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
