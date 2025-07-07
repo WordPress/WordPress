7dea8cc0a2a425879e1d999f3bb725a5721103b7
John Blackbourn <johnbillion@git.wordpress.org>
2025-07-07 20:48:42 +0000

    Build/Test Tools: Remove the `e2e-test-utils` package which is no longer used.
    This package was superseded by `e2e-test-utils-playwright` in #59517.
    
    See #63171
    Built from https://develop.svn.wordpress.org/trunk@60429
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59765 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60424';
+$wp_version = '6.9-alpha-60429';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
