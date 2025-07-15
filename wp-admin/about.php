e5fec10f69978b243444f6d84a3f0512abbb5b52
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-07-15 13:13:24 +0000

    WordPress 4.5.33. Built from https://develop.svn.wordpress.org/branches/4.5@60462
    git-svn-id: http://core.svn.wordpress.org/branches/4.5@59798 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -45,6 +45,26 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 				printf(
 					/* translators: %s: WordPress version number */
 					__( '<strong>Version %s</strong> addressed one security issue.' ),
+					'4.5.33'
+				);
+				?>
+				<?php
+				printf(
+					/* translators: %s: HelpHub URL */
+					__( 'For more information, see <a href="%s">the release notes</a>.' ),
+					sprintf(
+						/* translators: %s: WordPress version */
+						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+						sanitize_title( '4.5.33' )
+					)
+				);
+				?>
+			</p>
+			<p>
+				<?php
+				printf(
+					/* translators: %s: WordPress version number */
+					__( '<strong>Version %s</strong> addressed one security issue.' ),
 					'4.5.32'
 				);
 				?>
