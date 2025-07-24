b3c5d26a8a0ce119d8e5bf5bf9a5f6795baa2cd2
Weston Ruter <weston@xwp.co>
2025-07-24 05:18:33 +0000

    Customize: Use `static` closures in tests to reduce memory usage.
    Follow-up to [60499].
    
    Props mukesh27.
    See #63632.
    
    Built from https://develop.svn.wordpress.org/trunk@60500
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59836 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60499';
+$wp_version = '6.9-alpha-60500';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
