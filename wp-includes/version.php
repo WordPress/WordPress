cb3859e3cb98405b5dea807fa3585733fd599b59
John Blackbourn <johnbillion@git.wordpress.org>
2025-07-24 11:40:38 +0000

    Build/Test Tools: Clarify the local development environment instructions by removing unnecesary references to Docker and mentioning some alternative container environments.
    Props ninos-ego, johnbillion
    
    Fixes #63641
    
    See #63628
    Built from https://develop.svn.wordpress.org/trunk@60504
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@59840 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60503';
+$wp_version = '6.9-alpha-60504';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
