418cb1569823839b40b97b91f6cbb943dedff9e7
Aaron Jorbin <aaron@jorb.in>
2025-08-05 20:26:14 +0000

    Post 6.5.6 release bump.
    The 6.5 branch is now 6.5.7-alpha.
    
    Follow-up to [60585]
    
    Built from https://develop.svn.wordpress.org/branches/6.5@60590
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.5@59926 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.5.7-alpha-60585';
+$wp_version = '6.5.7-alpha-60590';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
