c812814111df5fb5ae6be2c47782bdabd41af155
audrasjb <audrasjb@git.wordpress.org>
2025-08-05 21:03:14 +0000

    Post 5.8.11 release bump.
    The 5.8 branch is now 5.8.12-alpha.
    
    Built from https://develop.svn.wordpress.org/branches/5.8@60608
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@59944 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8.11';
+$wp_version = '5.8.12-alpha-60608';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
