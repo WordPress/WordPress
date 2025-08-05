4509d857b87a72f93fa45bba9ea42f9d6c3879d8
audrasjb <audrasjb@git.wordpress.org>
2025-08-05 20:59:09 +0000

    Post 5.7.13 release bump.
    The 5.7 branch is now 5.7.14-alpha.
    
    Built from https://develop.svn.wordpress.org/branches/5.7@60607
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.7@59943 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.7.13';
+$wp_version = '5.7.14-alpha-60607';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
