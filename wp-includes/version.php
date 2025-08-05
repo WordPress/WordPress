360673a56a98c1d485891a845040235598176bd3
audrasjb <audrasjb@git.wordpress.org>
2025-08-05 20:49:55 +0000

    Post 5.3.19 release bump.
    The 5.3 branch is now 5.3.20-alpha.
    
    Built from https://develop.svn.wordpress.org/branches/5.3@60603
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.3@59939 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3.19';
+$wp_version = '5.3.20-alpha-60603';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
