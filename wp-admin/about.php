dcb2b40cc9617d6fdfc8ff2e0c85d26b8614df04
John Blackbourn <johnbillion@git.wordpress.org>
2025-08-05 16:31:16 +0000

    WordPress 6.4.6.
    Built from https://develop.svn.wordpress.org/branches/6.4@60544
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.4@59880 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -49,6 +49,26 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 				<p>
 					<?php
 					printf(
+						/* translators: %s: WordPress version number. */
+						__( '<strong>Version %s</strong> addressed one security issue.' ),
+						'6.4.6'
+					);
+					?>
+					<?php
+					printf(
+						/* translators: %s: HelpHub URL. */
+						__( 'For more information, see <a href="%s">the release notes</a>.' ),
+						sprintf(
+							/* translators: %s: WordPress version. */
+							esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+							sanitize_title( '6.4.6' )
+						)
+					);
+					?>
+				</p>
+				<p>
+					<?php
+					printf(
 						__( '<strong>Version %s</strong> addressed some security issues.' ),
 						'6.4.5'
 					);
