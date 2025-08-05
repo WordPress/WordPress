f7d1795a8687e398a286134a8ee09c0cd7bf987e
davidbaumwald <davidbaumwald@git.wordpress.org>
2025-08-05 20:15:15 +0000

    Post 6.4.6 release bump.
    The 6.4 branch is now 6.4.7-alpha.
    Built from https://develop.svn.wordpress.org/branches/6.4@60586
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.4@59922 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.4.6';
+$wp_version = '6.4.7-alpha-60586';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
