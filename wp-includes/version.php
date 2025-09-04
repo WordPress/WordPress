725a319a88b460ee5cbd0468e4f1e2f307e705cf
jonsurrell <jonsurrell@git.wordpress.org>
2025-09-04 14:50:21 +0000

    Build/Test Tools: Ensure assertEqualHTML fallback render can be reached.
    `assertEqualHTML` catches `Exception`s and attempts to re-parse the HTML with the `Dom\HtmlDocument` class. Ensure that the fallback render is reachable by throwing `Exception` instead of `Error` in the tree builder function.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/9273.
    
    Follow-up to [60295].
    
    Props jonsurrell, bernhard-reiter, dmsnell.
    Fixes #63527.
    
    Built from https://develop.svn.wordpress.org/trunk@60707
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60043 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60706';
+$wp_version = '6.9-alpha-60707';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
