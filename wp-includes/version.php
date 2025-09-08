f7652f9cd8fc7c48376fe66275f97e63e7534eff
gziolo <gziolo@git.wordpress.org>
2025-09-08 11:29:22 +0000

    Tests: Update rendering test cases for Block Bindings
    Refactoring covered:
    
    - Use the newly introduced `block_bindings_supported_attributes_{$block_name}` filter to register a `test/block`'s attribute as supported by Block Bindings, rather than using an actual block (Paragraph) for most tests.
    - Merge three test cases that check if `get_value_callback` works correctly (accepts arguments; correctly includes symbols and numbers; return value is sanitized when rendered) into one, by using a `dataProvider`.
    - Merge two test cases that check if block context is correctly evaluated, and that access is only given to context included in a source's `uses_context` property.
    
    Follow-up to [60684].
    
    Props bernhard-reiter, gziolo.
    See #63840.
    
    Built from https://develop.svn.wordpress.org/trunk@60720
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60056 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60719';
+$wp_version = '6.9-alpha-60720';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
