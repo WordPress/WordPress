5db5ede87ea5bb11708fa2dfc740b6efa356b8c8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-09-15 12:14:31 +0000

    Twenty Thirteen: Document the `twentythirteen_credits` action hook.
    Follow-up to [23452], [60738], [60739].
    
    Props pmbaldha, sabernhardt.
    See #63648.
    Built from https://develop.svn.wordpress.org/trunk@60740
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60076 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentythirteen/footer.php
+++ wp-content/themes/twentythirteen/footer.php
@@ -15,7 +15,14 @@
 			<?php get_sidebar( 'main' ); ?>
 
 			<div class="site-info">
-				<?php do_action( 'twentythirteen_credits' ); ?>
+				<?php
+				/**
+				 * Fires before the Twenty Thirteen footer text for footer customization.
+				 *
+				 * @since Twenty Thirteen 1.0
+				 */
+				do_action( 'twentythirteen_credits' );
+				?>
 				<?php
 				if ( function_exists( 'the_privacy_policy_link' ) ) {
 					the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
