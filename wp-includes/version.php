fc03447026a056e5213db6b8291599da57810505
desrosj <desrosj@git.wordpress.org>
2025-09-15 20:21:09 +0000

    Build/Test Tools: Pin Importer plugin version for PHP < 7.2.
    The WordPress Importer plugin now requires PHP 7.2 or higher. The plugin’s unit tests were removed as of [59769] in favor of running only within the plugin’s repository on GitHub instead.
    
    [59769] was made during the 6.8 release cycle, so all WP <= 6.7 branches still checkout the latest revision for the plugin before running the PHPUnit test suite.
    
    This change modifies the `env:install` script to download version `0.9.0` of the plugin (which is the last version of the Importer plugin that supports PHP < 7.2) when a qualifying version is being tested.
    
    Merges [60748] to the 5.8 branch.
    
    Props @swissspidy.
    See #63983.
    Built from https://develop.svn.wordpress.org/branches/5.8@60756
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/5.8@60092 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.8.12-alpha-60608';
+$wp_version = '5.8.12-alpha-60756';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
