43018cae19516e9b47993c6fed30b2f650a72367
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-09-17 13:37:39 +0000

    Docs: Clarify the description for `get_temp_dir()`.
    Includes a note that `sys_get_temp_dir()` honors the `TMPDIR` environment variable.
    
    Follow-up to [17555], [22008], [28936].
    
    Props TimoTijhof, SergeyBiryukov.
    See #63711.
    Built from https://develop.svn.wordpress.org/trunk@60776
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60112 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -2209,12 +2209,14 @@ function wp_normalize_path( $path ) {
 /**
  * Determines a writable directory for temporary files.
  *
- * Function's preference is the return value of sys_get_temp_dir(),
- * followed by your PHP temporary upload directory, followed by WP_CONTENT_DIR,
- * before finally defaulting to /tmp/
+ * Function's preference is the return value of `sys_get_temp_dir()`,
+ * followed by the `upload_tmp_dir` value from `php.ini`, followed by `WP_CONTENT_DIR`,
+ * before finally defaulting to `/tmp/`.
+ *
+ * Note that `sys_get_temp_dir()` honors the `TMPDIR` environment variable.
  *
  * In the event that this function does not find a writable location,
- * It may be overridden by the WP_TEMP_DIR constant in your wp-config.php file.
+ * it may be overridden by the `WP_TEMP_DIR` constant in your `wp-config.php` file.
  *
  * @since 2.5.0
  *
