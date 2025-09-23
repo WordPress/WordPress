e972807636dfdf29877780eb2f452f7dc78f2f0b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-09-23 18:52:32 +0000

    Coding Standards: Add specific sniff exclusions for serialize functions in sodium_compat.
    These are necessary for a passing PHP Compatibility scan.
    
    Follow-up to [47735], [60787].
    
    See #64008.
    Built from https://develop.svn.wordpress.org/trunk@60795
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60131 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60794';
+$wp_version = '6.9-alpha-60795';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
