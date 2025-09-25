ad464e9a079375af2880d1ae12fd079a6f3d6fd5
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-09-25 14:53:30 +0000

    Tests: Clean up calls to the unused `wp_add_post_meta()` function.
    Follow-up to [1041/tests], [23416], [23554].
    
    Props SirLouen, mukesh27, SergeyBiryukov.
    See #63167.
    Built from https://develop.svn.wordpress.org/trunk@60799
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60135 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60798';
+$wp_version = '6.9-alpha-60799';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
