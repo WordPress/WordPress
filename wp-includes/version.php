07c593ec51a6086dc284999574e80e44bedbf740
desrosj <desrosj@git.wordpress.org>
2025-09-30 17:48:19 +0000

    WordPress 5.5.17. Built from https://develop.svn.wordpress.org/branches/5.5@60853
    git-svn-id: http://core.svn.wordpress.org/branches/5.5@60189 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.5.17-alpha-60831';
+$wp_version = '5.5.17';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
