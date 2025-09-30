cbfa9cc7b46bf60c57e49c72dc622566858a0fbe
desrosj <desrosj@git.wordpress.org>
2025-09-30 17:41:17 +0000

    WordPress 6.1.9. Built from https://develop.svn.wordpress.org/branches/6.1@60847
    git-svn-id: http://core.svn.wordpress.org/branches/6.1@60183 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -45,6 +45,26 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 		<div class="about__section changelog">
 			<div class="column">
 				<h2><?php _e( 'Maintenance and Security Releases' ); ?></h2>
+			<p>
+				<?php
+				printf(
+					/* translators: %s: WordPress version. */
+					__( '<strong>Version %s</strong> addressed some security issues.' ),
+					'6.1.9'
+				);
+				?>
+				<?php
+				printf(
+					/* translators: %s: HelpHub URL. */
+					__( 'For more information, see <a href="%s">the release notes</a>.' ),
+					sprintf(
+						/* translators: %s: WordPress version. */
+						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+						sanitize_title( '6.1.9' )
+					)
+				);
+				?>
+			</p>
 				<p>
 					<?php
 					printf(
