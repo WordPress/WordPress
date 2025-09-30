5a93e5bc64ac9750519623c5796d24983ebee4fe
desrosj <desrosj@git.wordpress.org>
2025-09-30 17:23:46 +0000

    Grouped backports for the 4.8 branch.
    - REST API: Increase the specificity of capability checks for collections when the `edit` context is in use.
    - Menus: Prevent HTML in menu item titles from being rendered unexpectedly.
    
    Merges [60814], [60815], [60816] to the 4.8 branch.
    
    Props andraganescu, desrosj, ehti, hurayraiit, iandunn, joehoyle, johnbillion, jorbin, mnelson4, noisysocks, peterwilsoncc, phillsav, rmccue, timothyblynjacobs, vortfu, westonruter , whyisjake, zieladam.
    Built from https://develop.svn.wordpress.org/branches/4.8@60838
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.8@60174 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.8.27-alpha-60598';
+$wp_version = '4.8.27-alpha-60838';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
