385e79b741e4694ff06697837090fd5e8a3264e3
desrosj <desrosj@git.wordpress.org>
2025-09-30 17:49:18 +0000

    WordPress 5.4.18. Built from https://develop.svn.wordpress.org/branches/5.4@60854
    git-svn-id: http://core.svn.wordpress.org/branches/5.4@60190 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -51,6 +51,26 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 		<div class="about__section changelog">
 			<div class="column">
 				<h2><?php _e( 'Maintenance and Security Releases' ); ?></h2>
+			<p>
+				<?php
+				printf(
+					/* translators: %s: WordPress version. */
+					__( '<strong>Version %s</strong> addressed some security issues.' ),
+					'5.4.18'
+				);
+				?>
+				<?php
+				printf(
+					/* translators: %s: HelpHub URL. */
+					__( 'For more information, see <a href="%s">the release notes</a>.' ),
+					sprintf(
+						/* translators: %s: WordPress version. */
+						esc_url( __( 'https://wordpress.org/support/wordpress-version/version-%s/' ) ),
+						sanitize_title( '5.4.18' )
+					)
+				);
+				?>
+			</p>
 				<p>
 					<?php
 					printf(
