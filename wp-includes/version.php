e8349eba19dd6f57791d3014d7ccdf41ca94a310
joedolson <joedolson@git.wordpress.org>
2025-10-10 17:30:53 +0000

    Bundled Themes: Prevent `pre` from overflowing container.
    Update CSS to set `overflow-x: auto` on `pre` elements in Twenty Twenty Two and Twenty Twenty Five. 
    
    Props pateljaymin, jaiminp, nikunj8866, sabernhardt, rollybueno, joedolson.
    Fixes #63875.
    Built from https://develop.svn.wordpress.org/trunk@60921
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60257 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60920';
+$wp_version = '6.9-alpha-60921';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
