8123c7a293a0e65e012bb6f968783859ab2aa1b9
joedolson <joedolson@git.wordpress.org>
2025-10-17 20:59:34 +0000

    Twenty Eleven: Remove code and checks for obsolete browsers.
    Remove IE specific code from Twenty Eleven. Remove custom IE stylesheet contents, the HTML5 shiv contents (also targeting Safari 4.x and Firefox 3.x), and IE-specific code. 
    
    Props desrosj, sabernhardt, karmatosed, neychok, metodiew, mukesh27, aristath, joedolson.
    See #58836.
    Built from https://develop.svn.wordpress.org/trunk@60960
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60296 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentyeleven/header.php
+++ wp-content/themes/twentyeleven/header.php
@@ -9,18 +9,7 @@
  * @since Twenty Eleven 1.0
  */
 ?><!DOCTYPE html>
-<!--[if IE 6]>
-<html id="ie6" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 7]>
-<html id="ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html id="ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 6) & !(IE 7) & !(IE 8)]><!-->
 <html <?php language_attributes(); ?>>
-<!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -51,9 +40,6 @@ if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
 <link rel="profile" href="https://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo esc_url( get_stylesheet_uri() ); ?>?ver=20250415" />
 <link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
-<!--[if lt IE 9]>
-<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js?ver=3.7.0" type="text/javascript"></script>
-<![endif]-->
 <?php
 	/*
 	 * We add some JavaScript to pages with the comment form
