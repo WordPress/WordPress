fd3520f7ecf0846be4172458f3b151d1a125edbb
dmsnell <dmsnell@git.wordpress.org>
2025-10-19 00:19:34 +0000

    HTML API: Rely on assertEqualHTML in oEmbed filtering tests.
    As part of ongoing work to improve the reliability of HTML parsing code in WordPress, this patch replaces the use of PCRE matches in oEmbed filtering tests with semantic assertions via the HTML API and `assertEqualHTML()`.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/9259
    Discussed in https://core.trac.wordpress.org/ticket/63694
    
    Props dmsnell, jonsurrell.
    See #63694
    
    Built from https://develop.svn.wordpress.org/trunk@60972
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60308 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60971';
+$wp_version = '6.9-alpha-60972';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
