e2c156312e5c8e09b5fd0a10ac508b453d4a0b12
joedolson <joedolson@git.wordpress.org>
2025-10-20 20:31:34 +0000

    Menus: Improve usability and accessibility of menu quick search.
    Change the admin menu quick search to limit to searching post titles. Add filter `wp_ajax_menu_quick_search_args` to support customizing post columns in query. Clear quick search results when search query is cleared. Send announcements about search status via `wp.a11y.speak()`.
    
    Props diebombe, afercia, audrasjb, nickjbedford, kevinlearynet, mukesh27, joedolson.
    Fixes #48655.
    Built from https://develop.svn.wordpress.org/trunk@60991
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60327 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/nav-menu.php
+++ wp-admin/includes/nav-menu.php
@@ -83,17 +83,34 @@ function _wp_ajax_menu_quick_search( $request = array() ) {
 	} elseif ( preg_match( '/quick-search-(posttype|taxonomy)-([a-zA-Z0-9_-]*\b)/', $type, $matches ) ) {
 		if ( 'posttype' === $matches[1] && get_post_type_object( $matches[2] ) ) {
 			$post_type_obj = _wp_nav_menu_meta_box_object( get_post_type_object( $matches[2] ) );
-			$args          = array_merge(
-				$args,
-				array(
-					'no_found_rows'          => true,
-					'update_post_meta_cache' => false,
-					'update_post_term_cache' => false,
-					'posts_per_page'         => 10,
-					'post_type'              => $matches[2],
-					's'                      => $query,
-				)
+			$query_args    = array(
+				'no_found_rows'          => true,
+				'update_post_meta_cache' => false,
+				'update_post_term_cache' => false,
+				'posts_per_page'         => 10,
+				'post_type'              => $matches[2],
+				's'                      => $query,
+				'search_columns'         => array( 'post_title' ),
 			);
+			/**
+			 * Filter the menu quick search arguments.
+			 *
+			 * @since 6.9.0
+			 *
+			 * @param array $args {
+			 *     Menu quick search arguments.
+			 *
+			 *     @type boolean      $no_found_rows          Whether to return found rows data. Default true.
+			 *     @type boolean      $update_post_meta_cache Whether to update post meta cache. Default false.
+			 *     @type boolean      $update_post_term_cache Whether to update post term cache. Default false.
+			 *     @type int          $posts_per_page         Number of posts to return. Default 10.
+			 *     @type string       $post_type              Type of post to return.
+			 *     @type string       $s                      Search query.
+			 *     @type array        $search_columns         Which post table columns to query.
+			 * }
+			*/
+			$query_args = apply_filters( 'wp_ajax_menu_quick_search_args', $query_args );
+			$args       = array_merge( $args, $query_args );
 
 			if ( isset( $post_type_obj->_default_query ) ) {
 				$args = array_merge( $args, (array) $post_type_obj->_default_query );
