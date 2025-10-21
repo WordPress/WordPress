6c21899c424641c7d06dcf9f7e6dc16ed45f5513
Weston Ruter <weston@xwp.co>
2025-10-21 19:04:34 +0000

    Script Loader: Remove hard-coded versions in test snapshots.
    Follow-up to [60999].
    
    Props ellatrix.
    See #63486.
    
    Built from https://develop.svn.wordpress.org/trunk@61041
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60377 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-beta1-61040';
+$wp_version = '6.9-beta1-61041';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
