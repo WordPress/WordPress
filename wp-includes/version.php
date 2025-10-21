fb078cc8482416adc72024dbe3bd3e38c9681beb
ellatrix <ellatrix@git.wordpress.org>
2025-10-21 17:33:33 +0000

    Post WordPress 6.9 Beta 1 version bump. Built from https://develop.svn.wordpress.org/trunk@61040
    git-svn-id: http://core.svn.wordpress.org/trunk@60376 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-beta1';
+$wp_version = '6.9-beta1-61040';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
