9e3ec7f07163bb73ac7bbe34a9daded9090cc71a
Aaron Jorbin <aaron@jorb.in>
2025-10-21 15:25:36 +0000

    Users: Revert Lazy load user capabilities in WP_User object.
    The change from public to protected broke tests for extenders and this also broke `WP_User::to_array()`.
    
    Reverts [60915].
    
    Props davidbaumwald, ellatrix, timothyblynjacobs, welcher, spacedmonkey, bradshawtm, peterwilsoncc, jorbin.
    See #58001.
    
    Built from https://develop.svn.wordpress.org/trunk@61037
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60373 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-61036';
+$wp_version = '6.9-alpha-61037';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
