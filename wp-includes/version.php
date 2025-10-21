d17bae0e9516c9f9ba1bfaccafa04717e9a990ab
ellatrix <ellatrix@git.wordpress.org>
2025-10-21 13:54:22 +0000

    Templates: add missing file after [61029].
    See #62755.
    Built from https://develop.svn.wordpress.org/trunk@61033
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60369 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-61032';
+$wp_version = '6.9-alpha-61033';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
