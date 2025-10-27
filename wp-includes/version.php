c3c4dade7ddcb7825498cd5865c20f8463dfaaa7
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-10-27 11:17:36 +0000

    Twenty Twenty-One: Document the `twenty_twenty_one_content_width` filter.
    Includes removing an unnecessary PHPCS ignore annotation. The linked WPCS issue is not open anymore, and [https://github.com/WordPress/WordPress-Coding-Standards/pull/1773 WordPress/WordPress-Coding-Standards#1773] indicates that the test now makes an exception for `$content_width`.
    
    Follow-up to [49216].
    
    Props viralsampat, pmbaldha, sabernhardt.
    Fixes #63664.
    Built from https://develop.svn.wordpress.org/trunk@61070
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60406 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-beta1-61069';
+$wp_version = '6.9-beta1-61070';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
