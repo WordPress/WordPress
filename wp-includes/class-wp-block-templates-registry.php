631c1adf1c6c37567f155b9161d814261f9ddff7
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-10-30 16:03:31 +0000

    Editor: Correct error message in `WP_Block_Templates_Registry::unregister()`.
    When attempting to unregister a non-existent block template, the error message will now include the template name.
    
    Follow-up to [59073].
    
    Props mukesh27, shailu25.
    Fixes #64072.
    Built from https://develop.svn.wordpress.org/trunk@61090
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60426 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-block-templates-registry.php
+++ wp-includes/class-wp-block-templates-registry.php
@@ -60,11 +60,8 @@ final class WP_Block_Templates_Registry {
 		}
 
 		if ( $error_message ) {
-			_doing_it_wrong(
-				__METHOD__,
-				$error_message,
-				'6.7.0'
-			);
+			_doing_it_wrong( __METHOD__, $error_message, '6.7.0' );
+
 			return new WP_Error( $error_code, $error_message );
 		}
 
@@ -221,14 +218,12 @@ final class WP_Block_Templates_Registry {
 	 */
 	public function unregister( $template_name ) {
 		if ( ! $this->is_registered( $template_name ) ) {
-			_doing_it_wrong(
-				__METHOD__,
-				/* translators: %s: Template name. */
-				sprintf( __( 'Template "%s" is not registered.' ), $template_name ),
-				'6.7.0'
-			);
 			/* translators: %s: Template name. */
-			return new WP_Error( 'template_not_registered', __( 'Template "%s" is not registered.' ) );
+			$error_message = sprintf( __( 'Template "%s" is not registered.' ), $template_name );
+
+			_doing_it_wrong( __METHOD__, $error_message, '6.7.0' );
+
+			return new WP_Error( 'template_not_registered', $error_message );
 		}
 
 		$unregistered_template = $this->registered_templates[ $template_name ];
