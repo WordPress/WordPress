bb4078cd0728510108f77fc6723c99cc8d1c1261
joedolson <joedolson@git.wordpress.org>
2025-10-30 18:26:56 +0000

    Twenty Twelve: Fix post navigation to respect sort order.
    Change the labels on post navigation links when the sort order is changed so the labels accurately reflect the target entries.
    
    Previously, if the sort order was reversed, 'Older' or 'Previous' links would navigate to newer entries and 'Newer' or 'Next' links would navigate to older entries.
    
    Props jikamens, dancameron, obenland, shrey0shrivastava, sirlouen, sabernhardt, shailu25, westonruter, joedolson.
    See #10219.
    Built from https://develop.svn.wordpress.org/trunk@61095
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60431 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwelve/functions.php
+++ wp-content/themes/twentytwelve/functions.php
@@ -413,14 +413,28 @@ if ( ! function_exists( 'twentytwelve_content_nav' ) ) :
 	function twentytwelve_content_nav( $html_id ) {
 		global $wp_query;
 
-		if ( $wp_query->max_num_pages > 1 ) : ?>
+		if ( $wp_query->max_num_pages > 1 ) :
+			$order   = get_query_var( 'order', 'DESC' );
+			$is_desc = ( 'DESC' === $order );
+
+			$new_posts_text = __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentytwelve' );
+			$old_posts_text = __( '<span class="meta-nav">&larr;</span> Older posts', 'twentytwelve' );
+
+			$prev_link = $is_desc ? get_next_posts_link( $old_posts_text ) : get_previous_posts_link( $old_posts_text );
+			$next_link = $is_desc ? get_previous_posts_link( $new_posts_text ) : get_next_posts_link( $new_posts_text );
+			?>
 			<nav id="<?php echo esc_attr( $html_id ); ?>" class="navigation">
 				<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-				<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentytwelve' ) ); ?></div>
-				<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?></div>
+				<?php if ( $prev_link ) : ?>
+					<div class="nav-previous"><?php echo $prev_link; ?></div>
+				<?php endif; ?>
+
+				<?php if ( $next_link ) : ?>
+					<div class="nav-next"><?php echo $next_link; ?></div>
+				<?php endif; ?>
 			</nav><!-- .navigation -->
 			<?php
-	endif;
+		endif;
 	}
 endif;
 
