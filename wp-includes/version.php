6d7766478e19b25ff17736cd9378ddb27ccbcfa8
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-11-01 22:52:34 +0000

    Docs: Correct `@return` type for `WP_REST_Response::remove_link()`.
    Follow-up to [34928].
    
    Props dilipbheda, mukesh27, kirasong, shailu25, khushdoms, welcher, audrasjb, SergeyBiryukov.
    See #63249.
    Built from https://develop.svn.wordpress.org/trunk@61112
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60448 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-beta2-61111';
+$wp_version = '6.9-beta2-61112';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
