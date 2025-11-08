847d9adf98275441726e31d06bea270613e26eda
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-11-08 13:48:38 +0000

    Tests: Add unit tests for a label fallback in `WP_Block_Styles_Registry::register()`.
    Follow-up to [59760].
    
    Props Rahmohn, mukesh27, SergeyBiryukov.
    See #63167.
    Built from https://develop.svn.wordpress.org/trunk@61186
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60522 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-beta4-61185';
+$wp_version = '6.9-beta4-61186';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
