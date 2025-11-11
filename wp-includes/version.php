4cdaa890aa29a3c18313f0e58341d64e63dc822f
desrosj <desrosj@git.wordpress.org>
2025-11-11 02:45:28 +0000

    General: Clarify that contributors should fork `wordpress-develop`.
    Because `WordPress/wordpress-develop` is a mirror, the only way to contribute to WordPress on GitHub is by forking the repository (even when a contributor has write access as a Core Committer). This updates the “Getting Started” section to make this more clear.
    
    The section about how to contribute using GitHub Codespaces is also being removed since this has been broken for some time (see #57896). This section can be reintroduced once the underlying tooling is fixed.
    
    Props pbearne, desrosj, westonruter, wildworks, peterwilsoncc.
    Fixes #64100. See #57896.
    Built from https://develop.svn.wordpress.org/trunk@61202
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60538 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-beta4-61201';
+$wp_version = '6.9-beta4-61202';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
