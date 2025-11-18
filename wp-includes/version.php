8335c4c775e96ff7dc2cab338fd4546fac9e32e6
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-11-18 23:37:32 +0000

    Twenty Twenty-Five: Update Fira Sans font for the theme from version 4.203 to 4.301.
    This aims to resolve rendering issues with the backtick character.
    
    Follow-up to [59146].
    
    Props lakshyajeet, jonsurrell.
    Fixes #63964.
    Built from https://develop.svn.wordpress.org/trunk@61267
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60579 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61262';
+$wp_version = '7.0-alpha-61267';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
