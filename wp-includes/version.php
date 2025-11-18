b1de5f4080b015908ac657e3f24eb9a3dd213233
jonsurrell <jonsurrell@git.wordpress.org>
2025-11-18 10:12:29 +0000

    Script Loader: Omit sourceURL from translation data when not printed.
    The data returned from `WP_Scripts::print_translations()` when `$display` is `false` may be used in unpredictable ways that are incompatible with `sourceURL` comments. Omit the `sourceURL` comment in this case.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/10505.
    
    Follow-up to [60719].
    
    Reviewed by westonruter.
    Merges [61223] to the 6.9 branch.
    
    Props jonsurrell, ralucastn, westonruter, peterwilsoncc.
    See #63887.
    
    Built from https://develop.svn.wordpress.org/branches/6.9@61260
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.9@60572 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-RC1-61259';
+$wp_version = '6.9-RC1-61260';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
