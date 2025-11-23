268d877d20283b5af7353550b3095f0af31182af
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-11-23 23:45:29 +0000

    Twenty Nineteen: Correct center or right alignment for Download link in the File block.
    Follow-up to [43808].
    
    Props lakshyajeet, truptikanzariya, sabernhardt, sourabhjain, SirLouen, poojapadamad, SergeyBiryukov.
    Fixes #59263.
    Built from https://develop.svn.wordpress.org/trunk@61284
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60596 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61283';
+$wp_version = '7.0-alpha-61284';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
