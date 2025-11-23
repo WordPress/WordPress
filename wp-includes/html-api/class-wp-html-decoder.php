78bb7f180ed791c90772739137eebf6d92d397d4
Weston Ruter <weston@xwp.co>
2025-11-23 06:00:26 +0000

    Docs: Fix return type for `WP_HTML_Decoder::read_character_reference()`.
    Developed in https://github.com/WordPress/wordpress-develop/pull/8945
    
    Follow-up to [58281].
    
    Props justlevine.
    See #64238, #61072.
    
    Built from https://develop.svn.wordpress.org/trunk@61283
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60595 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/html-api/class-wp-html-decoder.php
+++ wp-includes/html-api/class-wp-html-decoder.php
@@ -203,7 +203,7 @@ class WP_HTML_Decoder {
 	 * @param int    $at                 Optional. Byte offset into text where span begins, defaults to the beginning (0).
 	 * @param int    &$match_byte_length Optional. Set to byte-length of character reference if provided and if a match
 	 *                                   is found, otherwise not set. Default null.
-	 * @return string|false Decoded character reference in UTF-8 if found, otherwise `false`.
+	 * @return ?string Decoded character reference in UTF-8 if found, otherwise null.
 	 */
 	public static function read_character_reference( $context, $text, $at = 0, &$match_byte_length = null ) {
 		/**
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61282';
+$wp_version = '7.0-alpha-61283';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
