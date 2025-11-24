5973aca1824fe6718aa19028b81b1098a75702b4
joedolson <joedolson@git.wordpress.org>
2025-11-24 18:47:30 +0000

    Admin: Remove alt syntax from `dashicons.css`
    Safari only introduced support for alternative text in generated CSS content in version 17.4, released in 2024. This meets the latest two version declared for core browser support. But because `dashicons.css` is commonly required on the front-end for plugins and themes, this support impacts front-end users, and the browser support guidelines for core should not apply in this context.
    
    Follow up to [60885].
    
    Props codex-m, sabernhardt, wildworks, joedolson, desrosj, welcher.
    Fixes #64266.
    Built from https://develop.svn.wordpress.org/trunk@61290
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60602 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61286';
+$wp_version = '7.0-alpha-61290';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
