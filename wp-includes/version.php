f1acf741c89c61ef850fda031ae3688017e3ecfe
desrosj <desrosj@git.wordpress.org>
2025-11-24 17:57:35 +0000

    General: Final 6.9 post-branching tasks.
    This makes the following changes to the 6.9 branch:
    - Pin PHP 8.5 and MySQL 8.4 as the default valuses of `LOCAL_PHP` and `LOCAL_DB_VERSION`.
    - Removes unnecessary GitHub Actions workflow files.
    - Changes all local GitHub Actions references to remote ones targeting `trunk`.
    
    Reviewed by peterwilsoncc.
    
    Props peterwilsoncc, johnbillion, davidbaumwald.
    Fixes #64235.
    Built from https://develop.svn.wordpress.org/branches/6.9@61287
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.9@60599 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-RC2-61274';
+$wp_version = '6.9-RC2-61287';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
