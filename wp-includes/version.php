a05eec7fb1ff4f892ad5ac87034830666c116082
wildworks <wildworks@git.wordpress.org>
2025-11-24 08:07:33 +0000

    Editor: Rename block visibility support key to `visibility`.
    Renames the support key to `visibility` to avoid conflicting with the Block Visibility plugin's existing `blockVisibility` key.
    
    Follow-up to [61014].
    
    Props andrewserong, annezazu, cbravobernal, dlh, jorbin, joen, johnjamesjacoby, palak678, ramonopoly, talldanwp, tyxla, wildworks.
    Fixes #64272.
    Built from https://develop.svn.wordpress.org/trunk@61285
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60597 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61284';
+$wp_version = '7.0-alpha-61285';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
