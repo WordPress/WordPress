ecda2e5ffcb7a2a8fd1de35ca3702d36deab3368
ellatrix <ellatrix@git.wordpress.org>
2025-11-25 15:30:33 +0000

    Post WordPress 6.9 RC 3 version bump. Built from https://develop.svn.wordpress.org/branches/6.9@61308
    git-svn-id: http://core.svn.wordpress.org/branches/6.9@60620 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-RC3';
+$wp_version = '6.9-RC3-61308';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
