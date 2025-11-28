ac741b7d81f0664debe6fddc3f35c6719c2a5417
Weston Ruter <weston@xwp.co>
2025-11-28 21:24:33 +0000

    Twenty Twenty: Fix typo and improve param description for `twentytwenty_read_more_tag()`.
    Follow-up to [61302].
    
    Props sabernhardt.
    See #64277, #64224.
    
    Built from https://develop.svn.wordpress.org/trunk@61319
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60631 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwenty/functions.php
+++ wp-content/themes/twentytwenty/functions.php
@@ -635,7 +635,7 @@ add_action( 'after_setup_theme', 'twentytwenty_block_editor_settings' );
  * Overwrite default more tag with styling and screen reader markup.
  *
  * @param string $html The default output HTML for the more tag.
- * @return string The the read more link wrapped in a `div`.
+ * @return string Read More link element, wrapped in a `div`.
  */
 function twentytwenty_read_more_tag( $html ) {
 	return preg_replace( '/<a(.*)>(.*)<\/a>/iU', sprintf( '<div class="read-more-button-wrap"><a$1><span class="faux-button">$2</span> <span class="screen-reader-text">"%1$s"</span></a></div>', get_the_title( get_the_ID() ) ), $html );
