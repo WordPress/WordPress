05044b0c62c008893bec4e8328a2f48c13a7e031
wildworks <wildworks@git.wordpress.org>
2025-11-29 04:26:31 +0000

    Media: Remove `required` attribute from media uploader file input field.
    This commit removes the `required` attribute from the file input element in the media uploader to avoid browser form validation issues and ensure that the `form` element is submitted correctly.
    
    Props adamsilverstein, ellatrix, desrosj, huzaifaalmesbah, immeet94, krupajnanda, madhavishah01, parthvataliya, ravichudasama01, sabernhardt, wildworks, yagniksangani. 
    Fixes #64305.
    Built from https://develop.svn.wordpress.org/trunk@61320
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60632 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61319';
+$wp_version = '7.0-alpha-61320';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
