550376599471c7f7b0560766a06980ba3fe15acc
dmsnell <dmsnell@git.wordpress.org>
2025-12-01 23:44:35 +0000

    Block Processor: Remove use of NumberFormatter from test suite.
    The Block Processor tests call `NumberFormatter` for generating its
    error messages, but in environments lacking the `intl` extension this
    led to crashes while running the tests.
    
    This patch reworks the tests so that they avoid calling
    `NumberFormatter` so that the tests run on more diverse setups.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/10576
    Discussed in https://core.trac.wordpress.org/ticket/64329
    
    Follow-up to [60939].
    
    Props desrosj, ellatrix, peterwilsoncc, westonruter.
    
    See #64329.
    
    Built from https://develop.svn.wordpress.org/trunk@61342
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60654 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61338';
+$wp_version = '7.0-alpha-61342';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
