d1ce920f4132b02710fb92c2e2dc8dcbc143b16d
ellatrix <ellatrix@git.wordpress.org>
2025-12-01 17:09:33 +0000

    WordPress 6.9 RC 4. Built from https://develop.svn.wordpress.org/branches/6.9@61339
    git-svn-id: http://core.svn.wordpress.org/branches/6.9@60651 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-RC3-61337';
+$wp_version = '6.9-RC4';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
