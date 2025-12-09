2e1b6a079dbab30c26fac6dc756edcf8321fb87a
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-12-09 22:04:32 +0000

    Tests: Use `assertSame()` in some newly introduced tests.
    This ensures that not only the return values match the expected results, but also that their type is the same.
    
    Going forward, stricter type checking by using `assertSame()` should generally be preferred to `assertEquals()` where appropriate, to make the tests more reliable.
    
    Follow-up to [61032].
    
    See #64324.
    Built from https://develop.svn.wordpress.org/trunk@61364
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60676 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61363';
+$wp_version = '7.0-alpha-61364';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
