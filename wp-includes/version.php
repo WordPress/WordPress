1033c6c05ea96b48b995a0c2192121af2f55c372
wildworks <wildworks@git.wordpress.org>
2025-12-22 10:42:35 +0000

    Widgets: Insert widgets into the selected sidebar correctly in accessibility mode.
    Renames variables to resolve conflict with outer scope variables, fixing widget placement in selected sidebars.
    
    Props joedolson, sabernhardt, vybiral, wildworks.
    Fixes #64380.
    Built from https://develop.svn.wordpress.org/trunk@61399
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60711 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61398';
+$wp_version = '7.0-alpha-61399';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
