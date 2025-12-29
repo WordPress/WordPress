b9a793c0f178bca73ccc02dce18a1738073b2000
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2025-12-29 23:15:37 +0000

    Tests: Use `assertSame()` in `wp_count_posts()` tests.
    This ensures that not only the return values match the expected results, but also that their type is the same.
    
    Going forward, stricter type checking by using `assertSame()` should generally be preferred to `assertEquals()` where appropriate, to make the tests more reliable.
    
    Follow-up to [1323/tests], [25554], [27081], [60788].
    
    Props costdev, SergeyBiryukov.
    See #64324.
    Built from https://develop.svn.wordpress.org/trunk@61417
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60729 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61416';
+$wp_version = '7.0-alpha-61417';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
