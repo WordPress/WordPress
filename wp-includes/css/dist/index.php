a978fc5de1845bc929be3bdce175575f07b5689f
Peter Wilson <wilson@peterwilson.cc>
2026-01-13 22:09:49 +0000

    General: Use legacy `content` properties for CSS icons.
    In modern browsers the CSS `content` property provides alt text support for assistive technology in the form `display / alt text`. This introduces support for legacy browsers that do not support the new syntax.
    
    CSS content properties take advantage of the CSS cascade and browsers ignoring values they don't understand by adding duplicate `content` properties in the form:
    
    {{{#!css
    content: display;
    content: display / alt text;
    }}}
    
    Modern browsers will use the second property, legacy browsers will use the first.
    
    Follow-up to [60885] for #63603.
    
    Props acmoifr, joedolson, jorbin, mydesign78, ov3rfly, peterwilsoncc, presskopp, sabernhardt, siliconforks, swissspidy, threadi, wildworks, wolf45.
    Fixes #64350.
    
    Built from https://develop.svn.wordpress.org/trunk@61480
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60792 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/css/dist/index.php
+++ wp-includes/css/dist/index.php
@@ -38,14 +38,14 @@ return array(
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-components',
-		'path' => 'components/style',
-		'dependencies' => array(),
-	),
-	array(
 		'handle' => 'wp-widgets',
 		'path' => 'widgets/style',
 		'dependencies' => array('wp-block-editor', 'wp-components'),
+	),
+	array(
+		'handle' => 'wp-components',
+		'path' => 'components/style',
+		'dependencies' => array(),
 	),
 	array(
 		'handle' => 'wp-format-library',
@@ -58,24 +58,24 @@ return array(
 		'dependencies' => array('wp-block-editor', 'wp-components', 'wp-editor'),
 	),
 	array(
-		'handle' => 'wp-customize-widgets',
-		'path' => 'customize-widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
-	),
-	array(
 		'handle' => 'wp-media-utils',
 		'path' => 'media-utils/style',
 		'dependencies' => array('wp-components'),
 	),
 	array(
-		'handle' => 'wp-edit-widgets',
-		'path' => 'edit-widgets/style',
-		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
+		'handle' => 'wp-customize-widgets',
+		'path' => 'customize-widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
 		'handle' => 'wp-edit-post',
 		'path' => 'edit-post/style',
 		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-commands', 'wp-components', 'wp-editor', 'wp-preferences', 'wp-widgets'),
+	),
+	array(
+		'handle' => 'wp-edit-widgets',
+		'path' => 'edit-widgets/style',
+		'dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-components', 'wp-media-utils', 'wp-patterns', 'wp-preferences', 'wp-widgets'),
 	),
 	array(
 		'handle' => 'wp-block-library',
