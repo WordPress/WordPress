6ca4b5b04afeaf615349dd7d8b9e144cb23df962
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-01-22 23:16:34 +0000

    Docs: Add missing parameter descriptions for `get_cli_args()`.
    Follow-up to [14760].
    
    Props rejaulalomkhan, huzaifaalmesbah, westonruter, SergeyBiryukov.
    See #64224.
    Built from https://develop.svn.wordpress.org/trunk@61512
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60823 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-importer.php
+++ wp-admin/includes/class-wp-importer.php
@@ -289,9 +289,10 @@ class WP_Importer {
  * Returns value of command line params.
  * Exits when a required param is not set.
  *
- * @param string $param
- * @param bool   $required
- * @return mixed
+ * @param string $param    The parameter name to retrieve.
+ * @param bool   $required Optional. Whether the parameter is required. Default false.
+ * @return string|true|null|never The parameter value or true if found, null otherwise.
+ *                                The function exits when a required parameter is missing.
  */
 function get_cli_args( $param, $required = false ) {
 	$args = $_SERVER['argv'];
