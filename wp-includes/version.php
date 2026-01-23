6b2e194185ce53e98944cb29982139903708a29a
jonsurrell <jonsurrell@git.wordpress.org>
2026-01-23 18:05:34 +0000

    Build/Test Tools: Ensure `assertEqualHTML()` recognizes whitespace text.
    Ensure whitespace text nodes are correctly represented by `build_visual_html_tree()`.
    
    The `build_visual_html_tree()` function used by `assertEqualHTML()` would remove some leading whitespace from text nodes. Some whitespace-only text nodes were omitted from the tree.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/10765.
    
    Follow-up to [60295].
    
    Props jonsurrell, dmsnell, bernhard-reiter.
    Fixes #64531.
    
    Built from https://develop.svn.wordpress.org/trunk@61519
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60830 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61518';
+$wp_version = '7.0-alpha-61519';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
