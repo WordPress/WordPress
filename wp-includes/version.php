1873ce185af02c0f4d108a705adfd09edfb0696a
joedolson <joedolson@git.wordpress.org>
2026-01-23 02:54:35 +0000

    A11y: Use pointer cursor on webkit field cancellation buttons.
    Webkit-based browsers add a cancel button to search inputs. This input uses the default cursor standard to buttons and inputs, but the WordPress admin applies the `pointer` cursor to buttons and inputs. Apply the WordPress standard pointer to this pseudoelement for UI consistency.
    
    Props sumitsingh, manhphucofficial, joedolson, sabernhardt, dhruvang21.
    Fixes #64382.
    Built from https://develop.svn.wordpress.org/trunk@61514
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60825 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61513';
+$wp_version = '7.0-alpha-61514';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
