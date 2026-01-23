bf0ce6144574962a24607f0b82976574511de277
joedolson <joedolson@git.wordpress.org>
2026-01-23 03:00:33 +0000

    Bundled Themes: A11y: Remove auto-focus in Twenty Ten.
    Remove the autofocus script in Twenty Ten that forced a bypass of the navigation, main heading, and explanation on the 404 error page.
    
    Props sabernhardt, ravichudasama01, joedolson.
    Fixes #64064.
    Built from https://develop.svn.wordpress.org/trunk@61515
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60826 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61514';
+$wp_version = '7.0-alpha-61515';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
