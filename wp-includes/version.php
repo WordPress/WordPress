c764fba9091ab1075c08b75b1789ee56f0da6a3c
jonsurrell <jonsurrell@git.wordpress.org>
2026-01-26 15:17:35 +0000

    Customize: Allow arbitrary custom CSS.
    Update custom CSS validation to allow any CSS except `STYLE` close tags. Previously, some valid CSS would be rejected for containing HTML syntax characters, like this example:
    
    {{{
    @property --animate {
      syntax: "<custom-ident>"; /* <-- Validation error on `<` */
      inherits: true;
      initial-value: false;
    }
    }}}
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/10667.
    
    Follow-up to [61418], [61486].
    
    Props jonsurrell, westonruter, peterwilsoncc, johnbillion, xknown, sabernhardt, dmsnell, soyebsalar01, dlh.
    Fixes #64418.
    
    Built from https://develop.svn.wordpress.org/trunk@61527
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60838 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61526';
+$wp_version = '7.0-alpha-61527';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
