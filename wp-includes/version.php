8a3b20ddec0cde26b1ac5f9f4c8fd3bb8630a7d7
joedolson <joedolson@git.wordpress.org>
2026-01-27 00:09:34 +0000

    Twenty Twenty-Five: A11y: Fix color contrast in Written By pattern.
    Remove the text color setting on the Written by pattern. The setting was resulting in color contrast that was well below contrast guidelines. Allow the text color to be inherited by default.
    
    Props poena, rinkalpagdar, narenin, sabernhardt, noruzzaman, huzaifaalmesbah, joedolson.
    Fixes #62982.
    Built from https://develop.svn.wordpress.org/trunk@61534
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60845 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61532';
+$wp_version = '7.0-alpha-61534';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
