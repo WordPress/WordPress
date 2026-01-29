9600cdf6c7a91a68a2b6b00dfc5c0aa851e26bb9
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-01-29 22:03:44 +0000

    Docs: Replace some `@see` tags with `@link` for URLs.
    Follow-up to [58074], [61486].
    
    Props huzaifaalmesbah, shailu25.
    See #64224.
    Built from https://develop.svn.wordpress.org/trunk@61557
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60868 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api/endpoints/class-wp-rest-global-styles-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-global-styles-controller.php
@@ -713,7 +713,7 @@ class WP_REST_Global_Styles_Controller extends WP_REST_Posts_Controller {
 			 * that would have broken out of the containing STYLE element, thus
 			 * corrupting the page and potentially introducing security issues.
 			 *
-			 * @see https://html.spec.whatwg.org/multipage/parsing.html#rawtext-end-tag-name-state
+			 * @link https://html.spec.whatwg.org/multipage/parsing.html#rawtext-end-tag-name-state
 			 */
 			$possible_style_close_tag = 0 === substr_compare(
 				$css,
