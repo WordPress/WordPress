c7c552963635391185e5a39115baeda2f4d88d07
Weston Ruter <weston@xwp.co>
2026-01-29 18:15:42 +0000

    Script Loader: Preserve original CSS cascade for classic themes when hoisting late-printed styles.
    This refactors `wp_hoist_late_printed_styles()` to strictly preserve the classic theme CSS cascade when moving styles from `wp_footer` to the `HEAD`. Previously, hoisted styles were appended in an order closer to the CSS cascade for block themes, potentially causing specificity issues for CSS selectors written for the previous cascade. This is intended to eliminate the need for sites which upgraded to 6.9 to apply a hotfix involving `add_filter( 'should_load_separate_core_block_assets', '__return_false' )` to disable the optimization. 
    
    Key changes:
    * Identifies styles enqueued during `enqueue_block_assets`.
    * Separates core block styles from third-party block styles.
    * Inserts core block styles immediately after `wp-block-library`.
    * Inserts third-party block styles after `classic-theme-styles`.
    * Inserts `global-styles` after all `enqueue_block_assets` styles.
    
    This ensures the following order is maintained:
    1. Core block library.
    2. Core block styles.
    3. Classic theme styles.
    4. Third-party block styles.
    5. Global styles.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/10601
    
    Follow-up to [61174], [61122], [61076], [61008].
    
    Props westonruter, wildworks, jorbin, peterwilsoncc, sabernhardt, audrasjb, pmbs, threadi, madhavishah01, raftaar1191, noruzzaman, ozgursar.
    See #64099, #64150, #43258.
    Fixes #64354.
    
    Built from https://develop.svn.wordpress.org/trunk@61554
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60865 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61552';
+$wp_version = '7.0-alpha-61554';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
