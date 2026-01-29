0418804ae830c2367efa0f7a2ae3e091e1736e9c
Peter Wilson <wilson@peterwilson.cc>
2026-01-29 01:07:36 +0000

    Feeds: Fix backward compatibility of `fetch_feed()`.
    In [https://github.com/simplepie/simplepie/pull/795 simplepie/simplepie#795] handling of multiple feed requests was deprecated, triggering the message `Fetching multiple feeds with single SimplePie instance is deprecated since SimplePie 1.9.0, create one SimplePie instance per feed and use SimplePie::merge_items to get a single list of items.`
    
    This updates `fetch_feed()` to handle multiple requests using seperate SimplePie instances in order to retain backward compatibility.
    
    A PHP 8.5 deprecation was throwing notices in the event an empty URL was passed to `fetch_feed()`, `Using null as an array offset is deprecated, use an empty string instead`.
    
    This includes a workaround pending the release of a SimplePie version including [https://github.com/simplepie/simplepie/pull/949 simplepie/simplepie#949].
    
    Reviewed by jorbin.
    Merges [61551] to the 6.9 branch.
    
    Fixes #64136.
    Props audrasjb, jorbin, muryam, oglekler, ozgursar, presskopp, swissspidy, westonruter, wildworks, peterwilsoncc.
    
    
    Built from https://develop.svn.wordpress.org/branches/6.9@61553
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.9@60864 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9.1-alpha-61550';
+$wp_version = '6.9.1-alpha-61553';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
