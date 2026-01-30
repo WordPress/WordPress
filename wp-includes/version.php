226f5e7be4f55f0dccb3982427bec00a3846e82d
Peter Wilson <wilson@peterwilson.cc>
2026-01-30 01:29:31 +0000

    Site Health: Reduce thresholds for object cache threshold tests.
    Reduces the object counts required for Site Health to recommend a persistent object cache in the `Tests_Admin_wpSiteHealth::test_object_cache_thresholds()` data provider.
    
    This is to ensure that the number of objects (terms, posts, etc) are certainly below the defaults created by the test suite during set up. The new threasholds are:
    
    - terms count: 0
    - alloptions count: 1
    - alloptions bytes: 10
    
    This follows the reduction to the options count threshold to 1 in r61438.
    
    Merges [61562] to the 6.9 branch; merges the one line change from [61438] to the 6.9 branch.
    
    Props peterwilsoncc, swissspidy, wildworks.
    Fixes #60831.
    
    
    Built from https://develop.svn.wordpress.org/branches/6.9@61563
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.9@60874 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9.1-RC1-61561';
+$wp_version = '6.9.1-RC1-61563';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
