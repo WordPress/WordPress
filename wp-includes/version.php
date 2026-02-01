ef65bcde0dfc0137416bfe09bc53d96a2ff45c54
audrasjb <audrasjb@git.wordpress.org>
2026-02-01 10:53:34 +0000

    Editor: Fix an indentation issue after [61570].
    Follow-up to [61570].
    
    See #64530.
    
    Built from https://develop.svn.wordpress.org/trunk@61571
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60882 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61570';
+$wp_version = '7.0-alpha-61571';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
