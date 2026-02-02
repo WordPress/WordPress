1b9c5e167a41ba8a26b0d169a9277f1f9920aea5
Weston Ruter <weston@xwp.co>
2026-02-02 04:42:40 +0000

    Docs: Correct type of `WP_Theme::$cache_expiration` from `bool` to `int`.
    Follow-up to [20029].
    
    Props marian1, mukesh27.
    See #20103, #64224.
    Fixes #64583.
    
    Built from https://develop.svn.wordpress.org/trunk@61574
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60885 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61573';
+$wp_version = '7.0-alpha-61574';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
