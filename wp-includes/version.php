e8492584727865c61216778d8c9e1466548f04d7
Weston Ruter <weston@xwp.co>
2026-02-09 18:43:43 +0000

    Docs: Improve `@global` annotations in abstract-testcase.php
    Developed in https://github.com/WordPress/wordpress-develop/pull/10841
    
    Follow-up to [61589], [61584].
    
    Props noruzzaman, mukesh27, westonruter, shailu25, huzaifaalmesbah.
    See #64224.
    
    Built from https://develop.svn.wordpress.org/trunk@61604
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60915 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61603';
+$wp_version = '7.0-alpha-61604';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
