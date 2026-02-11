da4ead53bac77a60c9a44e05b5d4fadfffd56284
audrasjb <audrasjb@git.wordpress.org>
2026-02-11 20:27:38 +0000

    Build/Test Tools: Update the Playground PR comment in GitHub Actions.
    This changeset removes the "Plugin and Theme Directories cannot be accessed within Playground" bullet point from the Playground Pull Request Comment GitHub Action, as it is not the case anymore.
    
    Props audrasjb, westonruter.
    Fixes #64578.
    
    
    Built from https://develop.svn.wordpress.org/trunk@61613
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60924 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61612';
+$wp_version = '7.0-alpha-61613';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
