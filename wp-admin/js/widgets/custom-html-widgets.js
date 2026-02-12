5d6f43a16144e2d73ea961b7382b7ee47710f1c5
Weston Ruter <weston@xwp.co>
2026-02-12 19:42:39 +0000

    Docs: Update broken link in core merge comment.
    Follow-up to [41376], [41260].
    
    Props mayur8991, sabernhardt, westonruter.
    Fixes #64632.
    
    Built from https://develop.svn.wordpress.org/trunk@61627
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60938 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/widgets/custom-html-widgets.js
+++ wp-admin/js/widgets/custom-html-widgets.js
@@ -333,7 +333,7 @@ wp.customHtmlWidgets = ( function( $ ) {
 		 * This ensures that the textarea is visible and the editor can be initialized.
 		 */
 		renderWhenAnimationDone = function() {
-			if ( ! ( wp.customize ? widgetContainer.parent().hasClass( 'expanded' ) : widgetContainer.hasClass( 'open' ) ) ) { // Core merge: The wp.customize condition can be eliminated with this change being in core: https://github.com/xwp/wordpress-develop/pull/247/commits/5322387d
+			if ( ! ( wp.customize ? widgetContainer.parent().hasClass( 'expanded' ) : widgetContainer.hasClass( 'open' ) ) ) { // Core merge: The wp.customize condition can be eliminated with this change being in core: https://core.trac.wordpress.org/changeset/41260
 				setTimeout( renderWhenAnimationDone, animatedCheckDelay );
 			} else {
 				widgetControl.initializeEditor();
