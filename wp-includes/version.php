44ab6e2d12910e4dae4e4bd783aef0bd8b57bd6d
jonsurrell <jonsurrell@git.wordpress.org>
2026-02-12 15:58:40 +0000

    Build/Test Tools: Add theme keyboard navigation tests.
    Add the theme keyboard navigation test file that omitted from [60516].
    
    Follow-up to [60516].
    
    Props abcd95, jonsurrell, mukesh27.
    See #64225, #63126.
    
    Built from https://develop.svn.wordpress.org/trunk@61625
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60936 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61624';
+$wp_version = '7.0-alpha-61625';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
