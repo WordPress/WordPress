7776fd20083c0a54b771408f29ad649f487043ef
Weston Ruter <weston@xwp.co>
2026-02-12 21:08:42 +0000

    Coding Standards: Use tabs for indentation and remove trailing line whitespace in QUnit test.
    Follow-up to [61625], [60516].
    
    See #63126.
    
    Built from https://develop.svn.wordpress.org/trunk@61629
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60940 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61628';
+$wp_version = '7.0-alpha-61629';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
