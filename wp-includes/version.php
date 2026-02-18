f153b6bea09cc03063904885304f8ca9366d95b2
John Blackbourn <johnbillion@git.wordpress.org>
2026-02-18 00:21:45 +0000

    HTTP API: Remove 1024 bit certificates from bundled root certificates.
    These legacy certificates have historically been retained in the certificate bundle so some ancient versions of OpenSSL continue to work with the bundle despite path discovery bugs in those versions.
    
    The affected OpenSSL versions are all over a decade old now. To ensure the ongoing security of the HTTP API these 1024 bit certificates have been removed.
    
    Props kkmuffme, JavierCasares, desrosj
    
    Fixes #64063
    
    Built from https://develop.svn.wordpress.org/trunk@61669
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@60977 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61668';
+$wp_version = '7.0-alpha-61669';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
