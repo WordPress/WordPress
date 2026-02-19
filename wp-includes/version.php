10a7f4d8c75014ba089f3fc17af710a0950766ee
ellatrix <ellatrix@git.wordpress.org>
2026-02-19 15:26:56 +0000

    Real-time collaboration: fix unit tests.
    Fixes autosave controller tests, which began failing when RTC was enabled. The tests now need to cover two different code paths:
    
    https://github.com/WordPress/wordpress-develop/blob/15ffb4392eddb43fdd9f31e075ebc85e857664c1/src/wp-includes/rest-api/endpoints/class-wp-rest-autosaves-controller.php#L259
    
    Props czarate.
    See #64622.
    Built from https://develop.svn.wordpress.org/trunk@61697
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61005 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61696';
+$wp_version = '7.0-alpha-61697';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
