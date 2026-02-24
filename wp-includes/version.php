f45ab6c82b0c8a5ea81a3abebd17f51b2b9b6bbe
desrosj <desrosj@git.wordpress.org>
2026-02-24 18:33:15 +0000

    Build/Test Tools: Exclude `gutenberg` directory when running PHPCS.
    The relevant files will be scanned within `src` (provided the build script has been run).
    
    See #64394.
    Built from https://develop.svn.wordpress.org/trunk@61726
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61033 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta1-61724';
+$wp_version = '7.0-beta1-61726';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
