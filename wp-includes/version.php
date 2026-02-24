1802bd631a7459c44307e408ab51e1a5732e7ae4
joedolson <joedolson@git.wordpress.org>
2026-02-24 22:47:36 +0000

    Admin: Fix dismiss button alignment in Welcome panel.
    Fix the icon alignment in the welcome panel close button following the admin reskin.
    
    Props mohitbsf, opurockey, huzaifaalmesbah, joedolson.
    Fixes #64681.
    Built from https://develop.svn.wordpress.org/trunk@61728
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61035 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta1-61727';
+$wp_version = '7.0-beta1-61728';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
