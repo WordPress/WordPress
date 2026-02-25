bece250ff2a1f07bc9d1ebdb1c00c908829006ec
audrasjb <audrasjb@git.wordpress.org>
2026-02-25 07:03:37 +0000

    Administration: Fix inconsistent vertical spacing in “At a Glance” widget.
    This changeset standardizes the vertical spacing between the heading and the first content item in the “At a Glance” admin dashboard widget to match the spacing conventions used by other dashboard widgets.
    
    Props mohitbsf, opurockey, adnanhyder.
    Fixes #64682.
    
    
    Built from https://develop.svn.wordpress.org/trunk@61732
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61039 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta1-61731';
+$wp_version = '7.0-beta1-61732';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
