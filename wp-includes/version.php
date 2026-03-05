721db5ff147962badb4c383afc55119082393425
joedolson <joedolson@git.wordpress.org>
2026-03-05 20:04:44 +0000

    Quick/Bulk Edit: Update focus color on inline edit panel.
    Use admin theme variables to set the inline edit wrapper focus border.
    
    Props hmbashar, joedolson, huzaifaalmesbah, mukesh27, shailu25, audrasjb, rahultank, manhar, ozgursar.
    Fixes #64757.
    Built from https://develop.svn.wordpress.org/trunk@61851
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61138 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta3-61850';
+$wp_version = '7.0-beta3-61851';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
