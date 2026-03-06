3fd710b1fb7df2e893d045f78a853358a1d58ee1
joedolson <joedolson@git.wordpress.org>
2026-03-06 23:17:42 +0000

    Admin: Fix active filter tab link color.
    Change the border color on active filter tab links to use the admin theme color.
    
    Props hbhalodia, mukesh27, huzaifaalmesbah, noruzzaman, shailu25, joedolson.
    Fixes #64807.
    Built from https://develop.svn.wordpress.org/trunk@61858
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61145 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta3-61857';
+$wp_version = '7.0-beta3-61858';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
