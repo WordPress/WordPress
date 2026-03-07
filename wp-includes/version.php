735f4d33f40c79e4cbeb1e07b50df0d0e4f04376
desrosj <desrosj@git.wordpress.org>
2026-03-07 03:14:33 +0000

    Editor: Bump the database version to `61833`.
    The option to enable real-time collaboration was first added in [61689] as `enable_real_time_collaboration` with a value of `1` and the `$db_version` was bumped in [61696].
    
    The option then went through a series of changes. This included: the default value changing to `0` in [61702], being renamed to `wp_enable_real_time_collaboration` in [61722], removed entirely in [61828], and finally being re-added as `wp_enable_real_time_collaboration` in [61862].
    
    Because the `$db_version` was not bumped after these changes, it’s possible that the `wp_enable_real_time_collaboration` option is not present on any site that ran the nightly build generated between [61696] and [61702], or a nightly build/beta release published after [61828]. Since `populate_options()` runs when a new site is installed, this issue only affects pre-existing sites that had upgradd their database when `wp_enable_real_time_collaboration` was not specified as a default option within `$defaults`.
    
    This bumps the database version to `61833`, which is the most recent changeset to have modified the `$defaults` array in `populate_options()`.
    
    Props dlh, maxschmeling, smithjw1, kbat82.
    See #64824, #64622.
    Built from https://develop.svn.wordpress.org/trunk@61864
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61151 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,14 +16,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta3-61862';
+$wp_version = '7.0-beta3-61864';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 61696;
+$wp_db_version = 61833;
 
 /**
  * Holds the TinyMCE version.
