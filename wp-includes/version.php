f46ecd2f8c396aff2ff622746dc4089d00ab2011
John Blackbourn <johnbillion@git.wordpress.org>
2026-03-10 12:49:45 +0000

    Comments: Don't attempt to create a note if the user cannot edit the target post.
    Props johnbillion, peterwilsoncc, adamsilverstein.
    
    Built from https://develop.svn.wordpress.org/trunk@61888
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61170 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta3-61887';
+$wp_version = '7.0-beta3-61888';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
