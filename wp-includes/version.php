b2f104f406486bf0b49f8b9296126544fd7fcd5e
John Blackbourn <johnbillion@git.wordpress.org>
2026-03-10 12:37:40 +0000

    Media: Ensure the attachment parent is accessible to the user before showing a link to it in the media manager.
    Props johnbillion, peterwilsoncc.
    
    Built from https://develop.svn.wordpress.org/trunk@61884
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61166 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta3-61883';
+$wp_version = '7.0-beta3-61884';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
