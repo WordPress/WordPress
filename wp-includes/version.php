f6a2aea6c457260ed306f8adf2392e6945bd3a3f
John Blackbourn <johnbillion@git.wordpress.org>
2026-03-10 12:20:43 +0000

    HTML API: Prevent `WP_HTML_Tag_Processor` instances being unserialized and add some extra logic for validating pattern and template file paths.
    Props dmsnell, xknown, jonsurrell, peterwilsoncc, johnbillion.
    
    Built from https://develop.svn.wordpress.org/trunk@61880
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61162 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta3-61879';
+$wp_version = '7.0-beta3-61880';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
