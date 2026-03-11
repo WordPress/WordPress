1577c0efce6cb15084f6982dfd91c1ce114f7ffb
John Blackbourn <johnbillion@git.wordpress.org>
2026-03-11 17:20:32 +0000

    Post WordPress 6.9.4 release bump.
    The 6.9 branch is now 6.9.5-alpha.
    
    Built from https://develop.svn.wordpress.org/branches/6.9@61944
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.9@61226 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9.4';
+$wp_version = '6.9.5-alpha-61944';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
