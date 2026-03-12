7021fb0f00e90d930504b7272cef8aa1c6ebf638
ellatrix <ellatrix@git.wordpress.org>
2026-03-12 15:19:44 +0000

    WordPress 7.0 Beta 5. Built from https://develop.svn.wordpress.org/trunk@61990
    git-svn-id: http://core.svn.wordpress.org/trunk@61272 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta4-61989';
+$wp_version = '7.0-beta5';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
