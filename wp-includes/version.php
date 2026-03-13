1dd8565bb6861c7569f50ded444d607c809f9672
desrosj <desrosj@git.wordpress.org>
2026-03-13 22:16:41 +0000

    Build/Test Tools: Add additional innovation release `exclude`s.
    This excludes MySQL 9.5 jobs for the installation testing workflow. Only the most recent innovation release of MySQL should be tested.
    
    Follow up to [61875].
    
    See #64225, #64227.
    Built from https://develop.svn.wordpress.org/trunk@62023
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61305 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta5-62022';
+$wp_version = '7.0-beta5-62023';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
