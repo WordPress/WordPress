6a1a9c5d7c2cd5b80eec673f9c16fdbb4b1ed4d1
desrosj <desrosj@git.wordpress.org>
2026-03-13 22:12:49 +0000

    Build/Test Tools: Fix installation tests for PHP <= 7.3.
    In MySQL 8.0, the default authentication plugin changed from `mysql_native_password` to `caching_sha2_password`, which is not available in PHP 7.3 and earlier. The installation testing workflow currently accounts for this, but only for PHP 7.2 & 7.3.
    
    It also does not account for a change in MySQL 8.4 which changed the related flag for controlling this from `--default-authentication-plugin` to `--authentication-policy`.
    
    This adjusts the flags and surrounding conditions to ensure the correct ones are passed for the right combinations.
    
    Props johnbillion.
    See #64225, #64227.
    Built from https://develop.svn.wordpress.org/trunk@62022
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61304 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta5-62021';
+$wp_version = '7.0-beta5-62022';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
