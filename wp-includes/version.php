d6b79f6250c95e4191e265eee5e11bdbd191e804
Weston Ruter <weston@xwp.co>
2026-03-14 19:23:49 +0000

    Cache API: Address code quality issues with `wp_cache_switch_to_blog()`.
    This addresses 3 PHPStan errors at rule level 8:
    
    * `arguments.count`: Function `wp_cache_switch_to_blog_fallback()` invoked with 1 parameter, 0 required.
    * `method.nonObject`: Cannot call method `switch_to_blog()` on `class-string|object`.                                                             
    * `missingType.return`: Function `wp_cache_switch_to_blog()` has no return type specified.                                                                                                                                         
    
    Developed as subset of https://github.com/WordPress/wordpress-develop/pull/11151
    
    Follow-up to r61760.
    
    Props westonruter, johnjamesjacoby.
    See #23290, #64238.
    
    Built from https://develop.svn.wordpress.org/trunk@62028
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61310 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta5-62027';
+$wp_version = '7.0-beta5-62028';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
