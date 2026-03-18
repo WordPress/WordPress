277fffbce222b0c9e8a1cd93aac472f8de7e8ad7
audrasjb <audrasjb@git.wordpress.org>
2026-03-18 11:00:42 +0000

    Networks and Sites: Allow filtering signup link redirect when a blog is not found.
    This changeset applies the existing `network_site_url` filter to the signup link redirect used when the blog is not found.
    
    Props calin, realloc, audrasjb, peterwilsoncc.
    Fixes #48323.
    
    
    Built from https://develop.svn.wordpress.org/trunk@62044
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61326 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/ms-load.php
+++ wp-includes/ms-load.php
@@ -419,7 +419,10 @@ function ms_load_current_site_and_network( $domain, $path, $subdomain = false ) 
 
 		if ( $subdomain && ! defined( 'NOBLOGREDIRECT' ) ) {
 			// For a "subdomain" installation, redirect to the signup form specifically.
-			$destination .= 'wp-signup.php?new=' . str_replace( '.' . $current_site->domain, '', $domain );
+			$path = 'wp-signup.php?new=' . str_replace( '.' . $current_site->domain, '', $domain );
+
+			/** This filter is documented in wp-includes/link-template.php */
+			$destination = apply_filters( 'network_site_url', $destination . $path, $path, $scheme );
 		} elseif ( $subdomain ) {
 			/*
 			 * For a "subdomain" installation, the NOBLOGREDIRECT constant
