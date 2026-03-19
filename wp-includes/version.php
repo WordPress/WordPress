857d992a3160c15d716b23c935ef44c5546ed232
desrosj <desrosj@git.wordpress.org>
2026-03-19 10:32:55 +0000

    External Libraries: Update `underscore` to `1.13.8`.
    A full list of changes can be found on GitHub: https://github.com/jashkenas/underscore/compare/1.13.7...1.13.8.
    
    Props jonsurrell.
    Fixes #64891.
    Built from https://develop.svn.wordpress.org/trunk@62060
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61342 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta5-62059';
+$wp_version = '7.0-beta5-62060';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
