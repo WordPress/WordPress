beaa495197ba0c3f535e412fb002eb656d0ae9e8
joedolson <joedolson@git.wordpress.org>
2026-03-22 23:14:58 +0000

    Administration: Fix alignment in upload plugin/theme buttons.
    Following [61598], the upload button for plugins and themes could cover the file information in the file input for longer internationalized strings due to absolute positioning.
    
    Update layout to use remove absolute positioning while retaining the larger drop area.
    
    Props audrasjb, pratiknawkar94, huzaifaalmesbah, noruzzaman, presskopp, joedolson.
    Fixes #64832.
    Built from https://develop.svn.wordpress.org/trunk@62090
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61372 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta6-62089';
+$wp_version = '7.0-beta6-62090';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
