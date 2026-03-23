1d1a1ae9e247a8fc213919da320d280b2db5215b
wildworks <wildworks@git.wordpress.org>
2026-03-23 11:02:57 +0000

    Toolbar: Add search icon prefix to command palette item in admin bar.
    Displays a search icon before the shortcut text in desktop view to clarify the item's purpose.
    
    Follow-up to [61912], [61979].
    
    Props sabernhardt, siliconforks, presskopp, westonruter, wildworks.
    See #64672.
    Built from https://develop.svn.wordpress.org/trunk@62092
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61374 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta6-62091';
+$wp_version = '7.0-beta6-62092';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
