6a8166d4c80d5ab8d60c725d62f1bfe3d3dc6fb7
Weston Ruter <weston@xwp.co>
2026-03-24 17:39:51 +0000

    Code Modernization: Fix `rest_convert_error_to_response()` handling of non-array error data when obtaining `status`.
    While the error data is normally an array, this is not guaranteed. This issue would have been detected by PHPStan rule level 9, since `WP_Error::get_all_error_data()` returns `mixed[]`.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/11307
    
    Follow-up to r61429.
    
    Props nateallen, desrosj, jorbin, mukesh27, westonruter.
    See #63430.
    Fixes #64901.
    
    Built from https://develop.svn.wordpress.org/trunk@62107
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61389 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/rest-api.php
+++ wp-includes/rest-api.php
@@ -3427,8 +3427,15 @@ function rest_get_endpoint_args_for_schema( $schema, $method = WP_REST_Server::C
 function rest_convert_error_to_response( $error ) {
 	$status = array_reduce(
 		$error->get_all_error_data(),
-		static function ( $status, $error_data ) {
-			return $error_data['status'] ?? $status;
+		/**
+		 * @param int   $status     Status.
+		 * @param mixed $error_data Error data.
+		 */
+		static function ( int $status, $error_data ): int {
+			if ( is_array( $error_data ) && isset( $error_data['status'] ) && is_numeric( $error_data['status'] ) ) {
+				$status = (int) $error_data['status'];
+			}
+			return $status;
 		},
 		500
 	);
