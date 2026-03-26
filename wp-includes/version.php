4804e9275d5d9257afe7283b8fa55a813853325d
dmsnell <dmsnell@git.wordpress.org>
2026-03-26 16:28:38 +0000

    Build: Restore deleted files and reconnect version history (svn:ignore follow-up).
    Removes svn:ignore props that were excluding script package manifests [62143].
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/11064
    Discussed in: https://core.trac.wordpress.org/ticket/64393
    
    Follow-up to [62143].
    
    Props desrosj, dmsnell, ellatrix.
    See #64393.
    
    Built from https://develop.svn.wordpress.org/trunk@62148
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61430 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-RC1-62147';
+$wp_version = '7.0-RC1-62148';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
