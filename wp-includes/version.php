85646f6ee8a55648f4bfc4a53006ade441336113
desrosj <desrosj@git.wordpress.org>
2026-03-26 16:06:44 +0000

    Build/Test Tools: Commiting changes to built files after [62143].
    This commits changes to built files that were not included in the previous commit that restored version history for files that were previously subject to version control.
    
    This also reverts [62069], which was a temporary fix to ensure deleted files that are no longer desired were cleaned up on the build server prior to committing the changes.
    
    Finally, the `src/wp-includes/build` and all its contents to the exclusion list for PHPCS scans. There are small coding standards failures in these files that need to be resolved upstream before the scan will pass on this directory.
    
    Props dmsnell, ellatrix, desrosj.
    See #64393.
    Built from https://develop.svn.wordpress.org/trunk@62144
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61426 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-RC1-62143';
+$wp_version = '7.0-RC1-62144';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
