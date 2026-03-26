54af6b004727a2f66ca4a015559e42170139b0c8
wildworks <wildworks@git.wordpress.org>
2026-03-26 01:20:54 +0000

    Editor: Fix invalid navigation overlay patterns
    Fixes the following two patterns:
    
    - Navigation Overlay: Add explicit black text and link colors so the default overlay pattern remains readable even when the active theme uses white text.
    - Overlay with site info and CTA: Fix invalid block attributes to prevent the block from breaking.
    
    Follow-up to [61609].
    
    Props isabel_brison, mikachan, onemaggie, westonruter, wildworks.
    See #64589.
    Built from https://develop.svn.wordpress.org/trunk@62114
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61396 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/block-patterns/navigation-overlay.php
+++ wp-includes/block-patterns/navigation-overlay.php
@@ -9,8 +9,8 @@ return array(
 	'title'      => _x( 'Navigation Overlay', 'Block pattern title' ),
 	'blockTypes' => array( 'core/template-part/navigation-overlay' ),
 	'categories' => array( 'navigation' ),
-	'content'    => '<!-- wp:group {"metadata":{"name":"' . esc_attr( __( 'Navigation Overlay' ) ) . '"},"style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"dimensions":{"minHeight":"100vh"}},"backgroundColor":"white","layout":{"type":"default"}} -->
-<div class="wp-block-group has-white-background-color has-background" style="min-height:100vh;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+	'content'    => '<!-- wp:group {"metadata":{"name":"' . esc_attr( __( 'Navigation Overlay' ) ) . '"},"style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"dimensions":{"minHeight":"100vh"},"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"backgroundColor":"white","textColor":"black","layout":{"type":"default"}} -->
+<div class="wp-block-group has-black-color has-white-background-color has-text-color has-background has-link-color" style="min-height:100vh;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
 <div class="wp-block-group alignwide"><!-- wp:navigation-overlay-close /--></div>
 <!-- /wp:group -->
 
