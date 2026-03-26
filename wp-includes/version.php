a475ca610cc710a79beea92ad56d9e9eaef8d4a7
joedolson <joedolson@git.wordpress.org>
2026-03-26 19:04:45 +0000

    Admin: Add CSS fallback values in button and media views.
    Since button and media view CSS can sometimes be enqueued by extenders in contexts where either `wp-base-styles` are not present or the `body` wrapper class is omitted, these variables should use fallback values.
    
    Reviewed by audrasjb.
    Props huzaifaalmesbah, wildworks, mukesh27, noruzzaman, joedolson, sabernhardt, audrasjb, shailu25.
    Fixes #64840.
    Built from https://develop.svn.wordpress.org/trunk@62154
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61436 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-RC2-62153';
+$wp_version = '7.0-RC2-62154';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
