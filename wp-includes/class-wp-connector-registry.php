a9a354e21dbccde01d0a6ab8d04ca3da93a64832
jorgefilipecosta <jorgefilipecosta@git.wordpress.org>
2026-04-01 14:57:43 +0000

    Connectors: Replace `plugin.slug` with `plugin.file` in connector registration.
    Use the plugin's main file path relative to the plugins directory
    (e.g. `akismet/akismet.php` or `hello.php`) instead of the WordPress.org slug
    to identify a connector's associated plugin.
    This lets `_wp_connectors_get_connector_script_module_data()` check plugin
    status with `file_exists()` and `is_plugin_active()` directly, removing the
    `get_plugins()` slug-to-file mapping that was previously needed.
    
    Props jorgefilipecosta, mukesh27, gziolo.
    Fixes #65002.
    Built from https://develop.svn.wordpress.org/trunk@62192
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61472 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-connector-registry.php
+++ wp-includes/class-wp-connector-registry.php
@@ -40,7 +40,7 @@
  *         env_var_name?: non-empty-string
  *     },
  *     plugin?: array{
- *         slug: non-empty-string
+ *         file: non-empty-string
  *     }
  * }
  */
@@ -109,7 +109,8 @@ final class WP_Connector_Registry {
 	 *     @type array  $plugin         {
 	 *         Optional. Plugin data for install/activate UI.
 	 *
-	 *         @type string $slug The WordPress.org plugin slug.
+	 *         @type string $file The plugin's main file path relative to the plugins
+	 *                            directory (e.g. 'akismet/akismet.php' or 'hello.php').
 	 *     }
 	 * }
 	 * @return array|null The registered connector data on success, null on failure.
@@ -242,8 +243,8 @@ final class WP_Connector_Registry {
 			}
 		}
 
-		if ( ! empty( $args['plugin'] ) && is_array( $args['plugin'] ) ) {
-			$connector['plugin'] = $args['plugin'];
+		if ( ! empty( $args['plugin'] ) && is_array( $args['plugin'] ) && ! empty( $args['plugin']['file'] ) ) {
+			$connector['plugin'] = array( 'file' => $args['plugin']['file'] );
 		}
 
 		$this->registered_connectors[ $id ] = $connector;
