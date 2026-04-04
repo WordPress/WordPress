78f8920042697b8a739ea60a899f0386ebccd5ea
audrasjb <audrasjb@git.wordpress.org>
2026-04-04 06:38:44 +0000

    Administration: Improve dashboard widgets border styles.
    This changeset fixes a CSS glitch on dashboard widgets bottom border when they are collapsed.
    
    Follow-up to [61646].
    
    Props pratik-jain, audrasjb, ankitkumarshah.
    Fixes #65017.
    See #64549.
    
    
    Built from https://develop.svn.wordpress.org/trunk@62206
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61486 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62205';
+$wp_version = '7.1-alpha-62206';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
