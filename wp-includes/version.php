c2a2c47625276be8aedaa059630f9b5ef35189c1
audrasjb <audrasjb@git.wordpress.org>
2026-04-07 05:35:38 +0000

    I18N: Provide gettext context to disambiguate translation strings for "Bulk Edit".
    The "Bulk Edit" translation string is used for both verbs and nouns, and may have different translations in some Locales. This changeset helps disambuguating these different contexts.
    
    Follow-up to [61255].
    
    Reviewed by westonruter.
    Merges [62186] to the 7.0 branch.
    
    Props audrasjb, shailu25.
    Fixes #64994.
    
    
    Built from https://develop.svn.wordpress.org/branches/7.0@62215
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.0@61495 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-RC2-62214';
+$wp_version = '7.0-RC2-62215';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
