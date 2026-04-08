f896394e1ca70116d08df26ddb64276c347a8d63
jonsurrell <jonsurrell@git.wordpress.org>
2026-04-08 12:26:35 +0000

    Block Hooks: Set ignored blocks meta in REST API response.
    Set `_wp_ignored_hooked_blocks` post meta in the REST API response sent from post-like endpoints that support Block Hooks (see `rest_block_hooks_post_types` filter).
    
    Previously, it was enough to set that post meta on write (i.e. save to DB). However, due to the way real-time collaboration syncs posts and reconciles them with content received from the server side, this information is now vital on the client side to ensure hooked blocks aren't duplicated.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/11410.
    
    Props bernhard-reiter, czarate, ingeniumed.
    Fixes #65008.
    
    Built from https://develop.svn.wordpress.org/trunk@62219
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61499 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62218';
+$wp_version = '7.1-alpha-62219';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
