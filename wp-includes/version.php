3ff95922b802cb6ebe9d3139a0f66237fd2d3efc
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-04-08 18:43:36 +0000

    Tests: Add missing `@covers` tags for some multisite tests.
    Follow-up to [62213], [62218].
    
    See #64225.
    Built from https://develop.svn.wordpress.org/trunk@62222
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61502 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62221';
+$wp_version = '7.1-alpha-62222';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
