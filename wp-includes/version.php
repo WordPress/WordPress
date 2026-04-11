8c39ad063a943f1f8c3d25fa5ec7ccfa1dc37b62
dmsnell <dmsnell@git.wordpress.org>
2026-04-11 18:47:35 +0000

    Email: Add unit tests covering email validation and sanitization. (Take 2)
    When the original patch from PR#11552 was merged, it did not include the latest version of the PR code, which had removed a failing test.
    
    This patch removes the failing test to match what ran in the tests on the PR.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/11552
    Discussed in: https://core.trac.wordpress.org/ticket/31992
    
    Follow-up to: [62225].
    
    Props agulbra, akirk, benniledl, dmsnell.
    See #31992.
    
    Built from https://develop.svn.wordpress.org/trunk@62226
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61506 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62225';
+$wp_version = '7.1-alpha-62226';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
