c889ff00118f0025dd22679c0be42b1fe38c4a2b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-04-12 14:27:35 +0000

    Tests: Expand `@covers` tags for `WP_oEmbed` tests.
    Follow-up to [62223].
    
    See #64225.
    Built from https://develop.svn.wordpress.org/trunk@62228
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61508 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62227';
+$wp_version = '7.1-alpha-62228';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
