a7d2707212442acbf2840c54f60f1f572cf42ac5
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-04-13 07:30:43 +0000

    Tests: Expand `@covers` tags for `WP_Embed` tests.
    Includes removing the `external-http` group for a `WP_Embed::run_shortcode()` test which does not perform any HTTP requests.
    
    Follow-up to [50448], [62223], [62228].
    
    See #64225.
    Built from https://develop.svn.wordpress.org/trunk@62229
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61509 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62228';
+$wp_version = '7.1-alpha-62229';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
