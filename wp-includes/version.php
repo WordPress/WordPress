38018f08f39eca7473e859dfdfcd31c2f2473f57
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-04-14 22:31:41 +0000

    Tests: Rename some oEmbed test classes as per the naming conventions.
    Follow-up to [37708], [37892], [62224], [62227].
    
    See #64225.
    Built from https://develop.svn.wordpress.org/trunk@62234
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61514 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62233';
+$wp_version = '7.1-alpha-62234';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
