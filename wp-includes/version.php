6fe9d337d11bdb060300c2518f145327effb3291
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-04-20 23:13:46 +0000

    Tests: Use `assertSame()` in `get_adjacent_post()` tests.
    This ensures that not only the return values match the expected results, but also that their type is the same.
    
    Going forward, stricter type checking by using `assertSame()` should generally be preferred to `assertEquals()` where appropriate, to make the tests more reliable.
    
    Follow-up to [60733], [61066].
    
    Props sagardeshmukh.
    See #64324.
    Built from https://develop.svn.wordpress.org/trunk@62247
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61527 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62246';
+$wp_version = '7.1-alpha-62247';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
