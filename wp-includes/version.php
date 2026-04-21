17d92feaa2cc1c5d0287a757d42fbcd5c0c3b12c
John Blackbourn <johnbillion@git.wordpress.org>
2026-04-21 17:25:48 +0000

    Build/Test Tools: Remove unnecessary use of GitHub Actions expressions for values that resolve to "true" or "false" strings.
    See #64227
    
    Built from https://develop.svn.wordpress.org/trunk@62253
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61533 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62252';
+$wp_version = '7.1-alpha-62253';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
