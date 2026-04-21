dd690d23901a724f6fcc702df14866dcea480a25
dmsnell <dmsnell@git.wordpress.org>
2026-04-21 14:04:48 +0000

    Tests: Print invalid UTF-8 as ASCII to fix hosts test reporting failures.
    When serializing test output into XML, invalid UTF-8 bytes lead to a failure to load those test results when they are read. This patch adds code to remap those invalid bytes in an ASCII-readable form, whereas the invalid bytes are separated by parentheses and encoded in their hex form.
    
    This ensures that a proper XML file is generated from the testing results.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/11620
    Discussed in: https://core.trac.wordpress.org/ticket/31992
    Reported in: https://github.com/WordPress/phpunit-test-runner/pull/310
    
    Follow-up to: [62225].
    
    Props agulbra, amykamala, codexdemon, dmsnell, mywp459, rolle.
    See #31992.
    
    Built from https://develop.svn.wordpress.org/trunk@62249
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61529 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62248';
+$wp_version = '7.1-alpha-62249';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
