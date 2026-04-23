76b56b1a909fad6a1d043042102433f63886a649
Peter Wilson <wilson@peterwilson.cc>
2026-04-23 05:10:44 +0000

    Build/Test Tools: Consolidate vendor file copying to ensure `.min.js` files are minified.
    Relocates the copying of vendor JavaScript files back to the `grunt copy:vendor-js` subtask to ensure the files are in place prior to the `grunt uglify` step running to minify the files.
    
    Props desrosj.
    Fixes #65007.
    See #64393.
    
    Built from https://develop.svn.wordpress.org/trunk@62258
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61538 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62257';
+$wp_version = '7.1-alpha-62258';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
