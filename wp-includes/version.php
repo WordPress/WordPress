d46ae43eb730b565e82577bbe98d1493398e6a76
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-04-25 18:45:44 +0000

    Tests: Add missing `@covers` tag for `register_block_type_from_metadata()` tests.
    Follow-up to [59132].
    
    Props sagardeshmukh.
    See #64225.
    Built from https://develop.svn.wordpress.org/trunk@62261
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61541 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62260';
+$wp_version = '7.1-alpha-62261';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
