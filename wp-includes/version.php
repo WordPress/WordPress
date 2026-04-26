2ecb40b741da0da34ff3eceb061a512ef9b17807
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-04-26 23:50:44 +0000

    Tests: Move `@covers` tags to the class-level DocBlock in some block-related tests.
    See #64225.
    Built from https://develop.svn.wordpress.org/trunk@62264
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61544 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62263';
+$wp_version = '7.1-alpha-62264';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
