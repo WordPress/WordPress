6ed6234efd02ca9b4317ce841aca1286de8a405b
Peter Wilson <wilson@peterwilson.cc>
2026-04-27 02:11:46 +0000

    Administration: Widen screen options number of items field.
    Modifies the Screen Options > Number of items per page field to avoid cropping of three digit numbers when setting a list view to display a lot of items.
    
    Props apermo, audrasjb, darshitrajyaguru97, ekla, gaurangsondagar, jigarkahar, juanmaguitar, khushdoms, peterwilsoncc, sabernhardt, shailu25, shatrumyatra, yusufmudagal.
    Fixes #65104.
    
    
    Built from https://develop.svn.wordpress.org/trunk@62268
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61548 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62264';
+$wp_version = '7.1-alpha-62268';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
