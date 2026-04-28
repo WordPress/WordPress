86fa738da77eb40d290980b5a8851428d9d9941c
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-04-28 23:00:40 +0000

    Tests: Correct `@covers` tag for `resolve_pattern_blocks()` tests.
    Follow-up to [58303].
    
    See #64225.
    Built from https://develop.svn.wordpress.org/trunk@62281
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61561 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62279';
+$wp_version = '7.1-alpha-62281';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
