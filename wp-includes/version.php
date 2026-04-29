be69a1f6fdff36a6b7df5f7533021c65543357db
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-04-29 23:41:49 +0000

    Administration: Correct alignment of meta boxes on the Edit screen with classic editor.
    Follow-up to [61646], [61759].
    
    Props umeshnevase, sabernhardt, SergeyBiryukov.
    Fixes #65141.
    Built from https://develop.svn.wordpress.org/trunk@62284
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61564 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62282';
+$wp_version = '7.1-alpha-62284';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
