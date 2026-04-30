8cdc23797d894d413132277246bc326ba86fdab9
Peter Wilson <wilson@peterwilson.cc>
2026-04-30 05:01:40 +0000

    Editor: Correct alignment for “Remove tag” icon in classic editor.
    Follow-up to [38880], [61645].
    
    Reviewed by peterwilsoncc.
    Merges r62279 to the 7.0 branch.
    
    Props umesh.nevase, sabernhardt, SergeyBiryukov.
    Fixes #65133.
    
    Built from https://develop.svn.wordpress.org/branches/7.0@62287
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.0@61567 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-RC2-62286';
+$wp_version = '7.0-RC2-62287';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
