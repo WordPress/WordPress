fcd9ed0d993761d1c7769b5fc990b739ccaac502
Aaron Jorbin <aaron@jorb.in>
2026-05-06 19:39:43 +0000

    Quick/Bulk Edit: Restore compact size of inline edit select.
    Follow-up to [61827], [62171].
    
    Reviewed-by wildworks, jorbin.
    Merges [62296] to the 7.0 branch.
    
    Props wildworks, mukesh27, audrasjb, SergeyBiryukov.
    Fixes #64685.
    
    Built from https://develop.svn.wordpress.org/branches/7.0@62307
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.0@61587 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-RC2-62306';
+$wp_version = '7.0-RC2-62307';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
