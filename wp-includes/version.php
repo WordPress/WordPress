6cc58ee5fc62e5f7431c993726fe93fe54868ecc
jorgefilipecosta <jorgefilipecosta@git.wordpress.org>
2026-05-07 11:52:43 +0000

    Connectors: Register Akismet Anti-Spam as a connector.
    Akismet is bundled with WordPress core, but it wasn't appearing on the Connectors screen even when present on the file system.
    
    Partially reverts [62210], bringing back the main part of [62193].
    Follow-up to [62210], [62193].
    Developed in https://github.com/WordPress/wordpress-develop/pull/11688.
    
    Reviewed by gziolo.
    Merges [62310] to the 7.0 branch.
    
    Props audrasjb, bluefuton, davidbaumwald, desrosj, eclev91, gziolo, johnbillion, jorbin, jorgefilipecosta, lukecarbis, matt, matveb, peterwilsoncc, westonruter.
    Fixes #65012.
    Built from https://develop.svn.wordpress.org/branches/7.0@62319
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.0@61599 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-RC2-62318';
+$wp_version = '7.0-RC2-62319';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
