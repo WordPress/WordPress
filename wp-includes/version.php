c74d360cc92cf29918d2f61789423d486cfe6618
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-05-09 23:02:44 +0000

    Tests: Add unit tests for `wp_check_php_version()`.
    Follow-up to [44735].
    
    Props pbearne.
    Fixes #65203.
    Built from https://develop.svn.wordpress.org/trunk@62343
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61624 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62342';
+$wp_version = '7.1-alpha-62343';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
