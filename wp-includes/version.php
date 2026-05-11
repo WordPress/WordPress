ac10586750dabec0ef70c9fbb67f586d3b4a42a9
ramonopoly <ramonopoly@git.wordpress.org>
2026-05-11 03:17:40 +0000

    [REVERT] WP_Theme_JSON: Prevent implicit coercion in to_ruleset
    Reverts r62347. The fix the patch proposes is destined for 7.1. There is a general block for such patches until the 7.0 general release.
    
    Props ramonopoly, andrewserong, westonruter.
    
    See #64848.
    
    
    Built from https://develop.svn.wordpress.org/trunk@62348
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61629 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62347';
+$wp_version = '7.1-alpha-62348';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
