adddca92e389fb551b05edb757d7e0b48a77738b
ryelle <ryelle@git.wordpress.org>
2026-05-14 14:34:41 +0000

    Help/About: Update the About page for 7.0.
    Introducing the new content for the 7.0 About page.
    
    Fixes #64536.
    Props mukesh27, audrasjb, jorbin, ankit-k-gupta, parinpanjari, fcoveram, joen, markoserb, Benjamin_Zekavica, westonruter, peterwilsoncc, JeffPaul.
    
    
    Built from https://develop.svn.wordpress.org/trunk@62362
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61643 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62361';
+$wp_version = '7.1-alpha-62362';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
