0b5b981e55e5659ff0b9af8d7a0019a9ac7f319b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-05-15 23:48:41 +0000

    Tests: Add unit tests for `wp_page_reload_on_back_button_js()`.
    Follow-up to [37619].
    
    Props pbearne.
    Fixes #65193.
    Built from https://develop.svn.wordpress.org/trunk@62367
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61648 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62366';
+$wp_version = '7.1-alpha-62367';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
