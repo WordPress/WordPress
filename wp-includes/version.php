54ba28eb13c1fe4d6be123caffbac58b72fa25d9
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-05-19 21:51:43 +0000

    Tests: Add unit tests for `wp_make_theme_file_tree()`.
    Follow-up to [41851].
    
    Props pbearne.
    Fixes #65175.
    Built from https://develop.svn.wordpress.org/trunk@62388
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61669 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62384';
+$wp_version = '7.1-alpha-62388';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
