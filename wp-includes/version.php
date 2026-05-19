6f24501b34715624d60ad43e1a404e1cfc8a4438
ellatrix <ellatrix@git.wordpress.org>
2026-05-19 16:28:43 +0000

    Upgrade/Install: Update the `$_old_files` array for 7.0.
    Props desrosj, ellatrix.
    Fixes #65273.
    Built from https://develop.svn.wordpress.org/trunk@62383
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61664 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62380';
+$wp_version = '7.1-alpha-62383';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
