34361a2546f10eb1ea75c86bd804e40cbd7bde1e
gziolo <gziolo@git.wordpress.org>
2026-05-21 06:53:44 +0000

    Abilities: Return the site locale for the core/get-site-info language field
    The `core/get-site-info` ability resolved the `language` field via `get_bloginfo( 'language' )`, which runs through `determine_locale()` and can return the current user's locale in the admin context. As a site-level ability, the `language` field should consistently represent the site locale. Use `get_locale()` (with `_` normalized to `-`) instead so the value reflects the site locale regardless of the requesting user.
    
    Props iamadisingh, afercia, audrasjb, yusufmudagal, r1k0, gziolo.
    Fixes #64977.
    
    
    
    Built from https://develop.svn.wordpress.org/trunk@62396
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61677 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62393';
+$wp_version = '7.1-alpha-62396';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
