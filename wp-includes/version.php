a2117097228a22c7d9629ebaf2379fdea48f1fe5
desrosj <desrosj@git.wordpress.org>
2026-05-22 04:21:43 +0000

    Build/Test Tools: Add MySQL 9.7 to relevenat test matrices.
    This adds `9.7` (released on April 21, 2026) to the relevant testing strategies and makes it the default version in the local Docker environment.
    
    MySQL 9.7 is also an LTS release.
    
    Props chrisdavidmiles.
    See #64894.
    Built from https://develop.svn.wordpress.org/trunk@62407
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61688 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62406';
+$wp_version = '7.1-alpha-62407';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
