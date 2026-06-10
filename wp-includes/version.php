80cfce4c7349e8d7454fae5b2734774fed4761ae
joedolson <joedolson@git.wordpress.org>
2026-06-10 00:38:44 +0000

    Media: Fix filter toolbar spinner alignment.
    The spinner that appeared during filtering was positioned beneath the fields, causing a scrollbar in the filter wrapper container. While only visible during scrolling on MacOS, it was persistently visible on Windows.
    
    Fixes the alignment of the toolbar to appear with predictable alignment to the select fields without generating a scrollbar.
    
    Follow up to [61757].
    
    Props luismulinari, yogeshbhutkar, dhruvang21, r1k0, sabernhardt, wildworks, audrasjb, joedolson.
    Fixes #65275. See #23562.
    Built from https://develop.svn.wordpress.org/trunk@62480
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61761 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62479';
+$wp_version = '7.1-alpha-62480';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
