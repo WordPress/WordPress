13b985d982561bbe24612cc190a9dcf579c2d440
dmsnell <dmsnell@git.wordpress.org>
2026-06-10 23:37:43 +0000

    Charset: Fix broken test for utf8_decode() fallback.
    Detected while fuzz-testing the UTF-8 handling code, this defect meant that the tests were verifying the wrong behavior. Namely, they verified a stringification of ASCII digits, which always converted plainly, when they were meant to test handling of invalid UTF-8 sequences.
    
    This patch fixes the test by calling `chr()` on the byte values before concatenating into a big string.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/12147
    Discussed in: https://core.trac.wordpress.org/ticket/65372
    
    Props dmsnell, jonsurrell.
    Follow-up to: [60950].
    See #65372.
    
    Built from https://develop.svn.wordpress.org/trunk@62484
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61765 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62483';
+$wp_version = '7.1-alpha-62484';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
