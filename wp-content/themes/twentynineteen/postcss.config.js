a4bafe37793214f5f594af2b93eaf030886524e7
desrosj <desrosj@git.wordpress.org>
2026-06-16 18:21:45 +0000

    Twenty Nineteen: Ensure only one PostCSS module is configured.
    The `postcss.config.js` file in Twenty Ninteen currently has two `module.exports` asignments configuring two plugins: `autoprefixer` (for ensuring browser-specific CSS nuances are accounted for) and `postcss-focus-within` (ensures `:fucs-within` rules are duplicated as `[focus-within]` attribute selectors).
    
    The first assignment is being ignored entirely, which means `autoprefixer` never runs against generated CSS files.
    
    This updates the `postcss.config.js` file to contain only one `modules.export` statement, which restores the behavior of `autoprefixer`. The dependencies related to browser usage statistics have also been updated, and the resulting changes to built CSS files subject to version control are also included:
    - Browser-specific prefixes for the `hyphens` property have been removed.
    - The `::-moz-selection` is no longer required for the `::selection` psuedo-element.
    - Brower-specific prefixes are no longer required for `min-content` and `max-content` values.
    - Webkit browsers no longer require a `-webkit-` prefix for `user-select: none`.
    - The `-webkit-` prefix is no longer required for the `filter` property.
    - The `text-decoration` property no longer requires browser prefixes.
    
    Fixes #65452.
    Built from https://develop.svn.wordpress.org/trunk@62510
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61791 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentynineteen/postcss.config.js
+++ wp-content/themes/twentynineteen/postcss.config.js
@@ -1,15 +1,11 @@
 var postcssFocusWithin = require('postcss-focus-within');
-
-module.exports = {
-    plugins: {
-        autoprefixer: {}
-    }
-};
+var autoprefixer = require('autoprefixer');
 
 module.exports = {
     plugins: [
         postcssFocusWithin({
             disablePolyfillReadyClass: true
-        })
+        }),
+        autoprefixer()
     ]
 };
