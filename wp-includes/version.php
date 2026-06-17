73f8bbb1d8f3e72a7ac72e325ed35d63ebd7fb77
jonsurrell <jonsurrell@git.wordpress.org>
2026-06-17 14:56:40 +0000

    HTML API: Improve comment about HTML syntax characters.
    Developed in https://github.com/WordPress/wordpress-develop/pull/12207.
    
    Follow-up to [62507].
    
    Props dmsnell.
    See #64896.
    
    Built from https://develop.svn.wordpress.org/trunk@62519
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61800 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62518';
+$wp_version = '7.1-alpha-62519';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
