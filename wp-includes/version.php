45b2a2eb9e42a75e817c2a1bf56418ea8dc85303
cbravobernal <cbravobernal@git.wordpress.org>
2026-06-25 09:50:42 +0000

    KSES: Decode style attribute before sending to safecss_filter_attr().
    `safecss_filter_attr()` assumes that it receives already-unescaped HTML attribute values. For example, consider the raw HTML string:
    
    {{{
    style="background:url(&quot;bg.png&quot;)"
    }}}
    
    This should be decoded and passed into `safecss_filter_attr()` as:
    
    {{{
    background:url("bg.png")
    }}}
    
    Unfortunately his hasn’t been done in `wp_kses_attr_check()`, which takes the output from `wp_kses_hair()` and sends it directly to the filtering function.
    
    In this patch, `wp_kses_attr_check()` unescapes the `style` attribute, filters it, and then re-escapes it when updating the style value.
    
    Tests added by Codex
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/11868
    Discussed in: https://core.trac.wordpress.org/ticket/65270
    
    Reviewed by cbravobernal.
    Merges [62433] to the 7.0 branch.
    
    Props dmsnell, westonruter.
    Fixes #65270.
    
    Built from https://develop.svn.wordpress.org/branches/7.0@62558
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.0@61838 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0.1-alpha-62528';
+$wp_version = '7.0.1-alpha-62558';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
