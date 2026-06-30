627d4389392a413dd171006532c18fe166257bb3
joedolson <joedolson@git.wordpress.org>
2026-06-30 20:18:42 +0000

    Admin Reskin: Fix interactive control heights on mobile.
    Give interactive elements a consistent 40px height in the admin mobile viewport on the Add Plugins, Media Library grid, Settings > General, and Add Themes screens.
    
    Follow-up to [61645].
    Reviewed by joedolson.
    Merges [62516] to the 7.0 branch.
    
    Props abcd95, wildworks.
    Fixes #64999.
    Built from https://develop.svn.wordpress.org/branches/7.0@62603
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.0@61883 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0.1-alpha-62602';
+$wp_version = '7.0.1-alpha-62603';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
