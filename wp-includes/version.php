c2cf9dd6f1aee0283e87912baccaca6189bf0f20
desrosj <desrosj@git.wordpress.org>
2026-06-30 19:39:44 +0000

    Build/Test Tools: Support testing unmerged changes from Gutenberg.
    The `gutenberg.sha` property in the `package.json` file expects a full-length SHA value to be specified in order to download the built assets from the Gutenberg repository and include the files through the build script.
    
    After https://github.com/WordPress/gutenberg/pull/78211, assets are now published for `pull_request` events in addition to `push` so long as the `HEAD` branch of the pull request exists in the repository and not in a fork. This makes the built assets for pull requests available for testing within `wordpress-develop` before they are merged through a `pr-###` tag on the GHCR package.
    
    To test a given pull request, just set the `gutenberg.sha` value to the corresponding `pr-###` tag. In addition to pull request-specific tags, each `wp/X.Y`, `release/X.Y`, and `trunk` now have a tag that can be used to test the latest changes. Each time `build` or `build:dev` is called the script will attempt to confirm the latest version of the assets are present locally, pulling down the latest changes when an update is available.
    
    All of the tags described in this changeset are mutable, so they should never be committed. Only immutable SHA values should be used for `gutenberg.sha` in commits to this repository.
    
    This commit also adds the related JavaScript files to the TypeScript configuration file.
    
    Reviewed by jorbin.
    Merges [62422] to the 7.0 branch.
    
    Props westonruter, jorbin, manhar.
    Fixes #65224.
    Built from https://develop.svn.wordpress.org/branches/7.0@62599
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.0@61879 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0.1-alpha-62596';
+$wp_version = '7.0.1-alpha-62599';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
