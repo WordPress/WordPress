8aa04a9179f5c0f44a60d0d04f7d5bab77bd22ee
Weston Ruter <weston@xwp.co>
2026-07-06 16:51:48 +0000

    Site Health: Bump the recommended version of MariaDB to `10.11`.
    MariaDB 10.6 will reach end of life on 6 July 2026. MariaDB 10.11 is the next oldest LTS version still receiving support upstream and is now the recommended minimum version, updated both in `readme.html` and in `WP_Site_Health`.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/12409.
    Follow-up to r55665, r58432, r60319.
    
    Props mukesh27, peterwilsoncc.
    Fixes #65585.
    
    Built from https://develop.svn.wordpress.org/trunk@62646
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61931 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/class-wp-site-health.php
+++ wp-admin/includes/class-wp-site-health.php
@@ -18,7 +18,7 @@ class WP_Site_Health {
 	private $mysql_server_version        = '';
 	private $mysql_required_version      = '5.5';
 	private $mysql_recommended_version   = '8.0';
-	private $mariadb_recommended_version = '10.6';
+	private $mariadb_recommended_version = '10.11';
 
 	public $php_memory_limit;
 
