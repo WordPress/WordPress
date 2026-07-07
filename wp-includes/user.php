f1fd14eacece0703d07a254d594a8dec029187fb
Pascal Birchler <pascal.birchler@gmail.com>
2026-07-07 08:35:54 +0000

    Docs: Replace `@since` tag for `wp_get_user_request()` instead of amending it.
    Follow-up to r62653.
    
    See #65132.
    Built from https://develop.svn.wordpress.org/trunk@62654
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61939 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -5116,8 +5116,7 @@ function wp_validate_user_request_key(
 /**
  * Returns the user request object for the specified request ID.
  *
- * @since 4.9.6
- * @since 5.4.0 Renamed from wp_get_user_request_data() to wp_get_user_request().
+ * @since 5.4.0
  *
  * @param int $request_id The ID of the user request.
  * @return WP_User_Request|false
