ce4fbb72e4f389ed26a1fe04e36c21ec3834b4a0
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-07-07 22:32:54 +0000

    Tests: Correct test case name in `_truncate_post_slug()` tests.
    Follow-up to [55208].
    
    Props Soean, mukesh27.
    See #64894.
    Built from https://develop.svn.wordpress.org/trunk@62660
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61945 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62659';
+$wp_version = '7.1-alpha-62660';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
