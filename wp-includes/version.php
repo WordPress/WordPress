bfd90c1173ec57b5bc1e7c6c23070b0f1e5019c2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-07-09 14:35:50 +0000

    Tests: Remove duplicated array keys in test data.
    This removes redundant array entries that were silently overwritten by a later key with the same name.
    
    Follow-up to [943/tests], [53941], [59866].
    
    Props Soean.
    See #64894.
    Built from https://develop.svn.wordpress.org/trunk@62675
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61960 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62674';
+$wp_version = '7.1-alpha-62675';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
