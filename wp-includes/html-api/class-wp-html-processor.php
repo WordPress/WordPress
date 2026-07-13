79ffcec13ec21fa96de557f46ae78c82983489ed
jonsurrell <jonsurrell@git.wordpress.org>
2026-07-13 15:35:50 +0000

    HTML API: Handle processing instructions in head noscript.
    Processing instructions were omitted from a few insertion modes when they were introduced to the HTML standard. That oversight has since been corrected in the standard; update the HTML API to match.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/12501.
    
    Follow-up to [62687].
    
    Props jonsurrell.
    Fixes #65582.
    
    Built from https://develop.svn.wordpress.org/trunk@62714
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61998 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/html-api/class-wp-html-processor.php
+++ wp-includes/html-api/class-wp-html-processor.php
@@ -2090,13 +2090,14 @@ class WP_HTML_Processor extends WP_HTML_Tag_Processor {
 
 			/*
 			 * > A comment token
-			 * >
+			 * > A processing instruction token
 			 * > A start tag whose tag name is one of: "basefont", "bgsound",
 			 * > "link", "meta", "noframes", "style"
 			 */
 			case '#comment':
 			case '#funky-comment':
 			case '#presumptuous-tag':
+			case '#processing-instruction':
 			case '+BASEFONT':
 			case '+BGSOUND':
 			case '+LINK':
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62713';
+$wp_version = '7.1-alpha-62714';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
