83a8339f27dc96711fa5fd8d2c24b9e6dec3a2b0
wildworks <wildworks@git.wordpress.org>
2026-07-13 10:34:51 +0000

    KSES: Add `tabindex` to the list of allowed global attributes.
    Add `tabindex` to the list of global attributes allowed by `_wp_add_global_attributes()`, so it survives KSES filtering on any element that supports global attributes.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/12493
    
    Props ramonopoly, wildworks.
    Fixes #65619.
    
    Built from https://develop.svn.wordpress.org/trunk@62708
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61992 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/kses.php
+++ wp-includes/kses.php
@@ -2960,6 +2960,7 @@ function safecss_filter_attr( $css, $deprecated = '' ) {
  * @since 6.0.0 Added `dir`, `lang`, and `xml:lang` to global attributes.
  * @since 6.3.0 Added `aria-controls`, `aria-current`, and `aria-expanded` attributes.
  * @since 6.4.0 Added `aria-live` and `hidden` attributes.
+ * @since 7.1.0 Added `tabindex` attribute.
  *
  * @access private
  * @ignore
@@ -2985,6 +2986,7 @@ function _wp_add_global_attributes( $value ) {
 		'id'               => true,
 		'lang'             => true,
 		'style'            => true,
+		'tabindex'         => true,
 		'title'            => true,
 		'role'             => true,
 		'xml:lang'         => true,
