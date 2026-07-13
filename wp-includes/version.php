83f58ed3e7aa8ddec36426d5b5597a7bd1439c0f
ramonopoly <ramonopoly@git.wordpress.org>
2026-07-13 05:17:52 +0000

    Block Supports: guard test for element hover styles with a missing hover selector
    This commit adds a test to cover the following behaviour: elements (such as buttons) should only render `:hover` element styles when they define a hover selector.
    
    Props aaronrobertshaw, ramonopoly, westonruter, wildworks.
    
    Follow-up to [65435].
    
    Fixes #65538.
    
    
    Built from https://develop.svn.wordpress.org/trunk@62701
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@61985 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62700';
+$wp_version = '7.1-alpha-62701';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
