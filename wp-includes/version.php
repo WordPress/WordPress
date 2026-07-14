558828206710d533bed65ddfaccfac05bc3f402f
desrosj <desrosj@git.wordpress.org>
2026-07-14 18:24:52 +0000

    Build/Test Tools: Limit cancelled Slack notifications.
    When a worfklow run in GitHub Actions reaches the configured timeout value, the workflow is considered cancelled. Since there is an automatic re-run mechanism built into the project’s workflows, it’s not necessary to post a message in Slack that the first run has been cancelled.
    
    This adjusts the conditional checks controlling the cancelled notices so only the second run attempt results in a Slack notification.
    
    See #64893.
    Built from https://develop.svn.wordpress.org/trunk@62740
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62024 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62739';
+$wp_version = '7.1-alpha-62740';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
