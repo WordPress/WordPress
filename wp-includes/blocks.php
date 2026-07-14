5bf4f5471b2a2e9629c2ede3a6911ca331d5f613
wildworks <wildworks@git.wordpress.org>
2026-07-14 07:28:50 +0000

    Editor: Add a helper to apply content filters within blocks.
    Introduce the private `_wp_apply_block_content_filters()` function so that blocks rendering full post content, such as Latest Posts, process it through the standard content pipeline with optional recursion protection, instead of outputting raw block markup.
    
    Discussed in: https://github.com/WordPress/wordpress-develop/pull/12421
    
    Props get_dave, wildworks.
    See #65586.
    
    Built from https://develop.svn.wordpress.org/trunk@62726
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62010 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks.php
+++ wp-includes/blocks.php
@@ -2568,6 +2568,52 @@ function _restore_wpautop_hook( $content ) {
 }
 
 /**
+ * Applies standard content filters similar to the 'the_content' filter.
+ *
+ * This function runs the typical content processing filters that WordPress
+ * applies to post content, useful for blocks that render nested content.
+ *
+ * @since 7.1.0
+ * @access private
+ *
+ * @global WP_Embed $wp_embed WordPress Embed object.
+ *
+ * @param string      $content  The content to process.
+ * @param string      $context  Optional. Context identifier for wp_filter_content_tags().
+ *                              Default empty string.
+ * @param array|null  $seen_ids Optional. Reference to an array tracking seen IDs for
+ *                              recursion prevention. Default null.
+ * @param string|null $id       Optional. Unique identifier for this content, used with
+ *                              $seen_ids. Default null.
+ * @return string The processed content.
+ */
+function _wp_apply_block_content_filters( $content, $context = '', &$seen_ids = null, $id = null ) {
+	$content = shortcode_unautop( $content );
+	$content = do_shortcode( $content );
+
+	if ( null !== $seen_ids && null !== $id ) {
+		$seen_ids[ $id ] = true;
+	}
+
+	try {
+		$content = do_blocks( $content );
+	} finally {
+		if ( null !== $seen_ids && null !== $id ) {
+			unset( $seen_ids[ $id ] );
+		}
+	}
+
+	$content = wptexturize( $content );
+	$content = convert_smilies( $content );
+	$content = wp_filter_content_tags( $content, $context );
+
+	global $wp_embed;
+	$content = $wp_embed->autoembed( $content );
+
+	return $content;
+}
+
+/**
  * Returns the current version of the block format that the content string is using.
  *
  * If the string doesn't contain blocks, it returns 0.
