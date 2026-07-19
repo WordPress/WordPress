95c572cb4bddb002ba69cf9eca29c35ec1640b80
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-07-19 10:02:49 +0000

    Coding Standards: Upgrade WPCS to version 3.4.0.
    The impact on existing WordPress core code is expected to be minimal. The release updates some sniffs to account for changes in recent WordPress versions up to 7.0.
    
    References:
    * [https://github.com/WordPress/WordPress-Coding-Standards/releases/tag/3.4.0 WPCS 3.4.0 release notes]
    * [https://github.com/WordPress/WordPress-Coding-Standards/compare/3.3.0...3.4.0 Full list of changes in WPCS 3.4.0]
    
    Follow-up to [56695], [56799], [57378], [57986], [60523], [61306].
    
    Props jrf, dingo_d, garyj, rodrigosprimo, mattgaldino, pamprn, jasonkenison, jonmcpartland, rafaelfunchal, paulgibbs, tikifez, dd32, bhubbard, petitphp, paulopmt1, nicsevic, diegogarciarodrigues, Soean, SergeyBiryukov.
    Fixes #65659.
    Built from https://develop.svn.wordpress.org/trunk@62796
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62076 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-beta2-62795';
+$wp_version = '7.1-beta2-62796';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
