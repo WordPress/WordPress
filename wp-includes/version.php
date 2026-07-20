bdb275d72c60f2f49d3cf56bdfd0b3aa642aa0b6
Weston Ruter <weston@xwp.co>
2026-07-20 18:56:50 +0000

    Build/Test Tools: Fix typing and logic issues in `MockAction`.
    * Add type information.
    * Fix erroneous fallback case for `MockAction::current_filter()` in how it gets the last key of `$wp_actions`.
    * Fix `MockAction::get_call_count()` to handle getting counts of a provided filter in addition to a provided action.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/12618.
    
    See #64898, #64894.
    
    
    Built from https://develop.svn.wordpress.org/trunk@62808
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62088 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-beta2-62807';
+$wp_version = '7.1-beta2-62808';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
