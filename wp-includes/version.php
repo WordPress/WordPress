d66a7cc208870483f14703059b5ae736070af036
joedolson <joedolson@git.wordpress.org>
2026-07-24 22:32:50 +0000

    Privacy: Fix type inconsistencies in data erasure inline notices.
    Additional messages can be inserted by plugins using the `wp_privacy_personal_data_erasers` filter, and are generated as list items (`li`) inside the notice markup. However, `li` was not targeted with notice-specific styling, and inherited the list item styles from the containing table. 
    
    Add additional styles targeting list items inside notices in list tables to equalize font sizes and styling.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/12021
    
    Props kimannwall, masteradhoc, joedolson.
    Fixes #53611.
    Built from https://develop.svn.wordpress.org/trunk@62847
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62127 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-beta3-62846';
+$wp_version = '7.1-beta3-62847';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
