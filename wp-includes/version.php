872ab2eaa58c33ed2bf99ef3baf0899579b4667c
joedolson <joedolson@git.wordpress.org>
2026-07-28 17:53:50 +0000

    Administration: Fix focus/hover styles on collapse menu button.
    The new `:focus` and `:hover` styles for the admin menu were only applied to links, and not extended to the collapse menu trigger, which is a `button`. Additionally, the entire hover/focus state change was missing in some alternate admin color schemes.
    
    Add shape-based focus and hover states to collapse menu button to match the rest of the admin menu and add expected color changes across all admin schemes.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/12722
    
    Props afercia, iamchitti, dhruvang21, fushar, habiburdev, joedolson.
    Fixes #65726.
    Built from https://develop.svn.wordpress.org/trunk@62875
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62153 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-beta3-62874';
+$wp_version = '7.1-beta3-62875';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
