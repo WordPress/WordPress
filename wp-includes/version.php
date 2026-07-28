075a27658a9e6dcb2c9511e0318f7bd1507ffa5f
Lance Willett <nanobar@gmail.com>
2026-07-28 19:51:52 +0000

    Build/Test Tools: Record mergeinfo for [62859] and [62862] on the 7.0 branch. Reviewed by: jorbin. Built from https://develop.svn.wordpress.org/branches/7.0@62877
    git-svn-id: http://core.svn.wordpress.org/branches/7.0@62155 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0.3-alpha-62876';
+$wp_version = '7.0.3-alpha-62877';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
