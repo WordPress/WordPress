75bbe15e9a021ed1364b168a3a369856a9bfc257
joedolson <joedolson@git.wordpress.org>
2026-07-28 21:14:51 +0000

    Twenty Twenty One: Only filter untitled posts on the front end.
    Prevent the `the_title` filter used to alter untitled posts from running in the admin. Allows the excerpt feature added in [62685] to work on sites using Twenty Twenty One. No change on the front end.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/12688
    
    Props joedolson, khokansardar, shailu25, sabernhardt, luminuu, mirmpro.
    Fixes #65710.
    Built from https://develop.svn.wordpress.org/trunk@62889
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62157 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-content/themes/twentytwentyone/inc/template-functions.php
+++ wp-content/themes/twentytwentyone/inc/template-functions.php
@@ -182,12 +182,17 @@ if ( ! function_exists( 'twenty_twenty_one_post_title' ) ) {
 	 * Adds a title to posts and pages that are missing titles.
 	 *
 	 * @since Twenty Twenty-One 1.0
+	 * @since Twenty Twenty-One 2.9 Only applies the filter on the front end.
 	 *
 	 * @param string $title The title.
 	 * @return string
 	 */
 	function twenty_twenty_one_post_title( $title ) {
-		return '' === $title ? esc_html_x( 'Untitled', 'Added to posts and pages that are missing titles', 'twentytwentyone' ) : $title;
+		if ( is_admin() ) {
+			return $title;
+		}
+
+		return '' === $title ? esc_html_x( 'Untitled', 'Added on the front end to posts and pages that are missing titles', 'twentytwentyone' ) : $title;
 	}
 }
 add_filter( 'the_title', 'twenty_twenty_one_post_title' );
