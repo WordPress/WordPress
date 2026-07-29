424a32a8809f85f1eba617c0c0122faf7cb6fba6
joedolson <joedolson@git.wordpress.org>
2026-07-29 13:46:49 +0000

    Administration: Lower specificity of hover and focus states on disabled tooltips.
    Core tooltips can accept an existing button as an argument. When applying to an existing button, they should use the existing focus and hover states of that button. The styles for tooltips on disabled controls had higher specificity than the styles for active controls.
    
    Lower the specificity of the styles applied to disabled buttons with tooltips to avoid overriding existing styles. Follow up to [62816].
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/12712
    
    Props wildworks, khokansardar, mirmpro, joedolson.
    Fixes #65727.
    Built from https://develop.svn.wordpress.org/trunk@62897
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62164 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-beta3-62896';
+$wp_version = '7.1-beta3-62897';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
