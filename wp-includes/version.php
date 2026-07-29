41feb28d8d749477f1bfc28710564179892ee859
Lance Willett <nanobar@gmail.com>
2026-07-29 23:27:50 +0000

    Build/Test Tools: Wait for the database before running WP-CLI commands.
    Developed in: https://github.com/WordPress/wordpress-develop/pull/12734
    
    Props adrianmoldovanwp.
    Fixes #65742.
    
    Built from https://develop.svn.wordpress.org/trunk@62938
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62182 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-beta4-62937';
+$wp_version = '7.1-beta4-62938';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
