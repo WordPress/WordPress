6ef03095e3c035f98c216eced9a2aec06589bef7
Lance Willett <nanobar@gmail.com>
2026-07-30 18:22:50 +0000

    Build/Test Tools: Add a start period to the MySQL healthcheck.
    Merges [62937] to the 7.0 branch.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/12770
    
    Reviewed by: johnbillion.
    Props adrianmoldovanwp.
    See #65752.
    
    Built from https://develop.svn.wordpress.org/branches/7.0@62946
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.0@62189 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0.3-alpha-62900';
+$wp_version = '7.0.3-alpha-62946';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
