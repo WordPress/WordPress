685437ededae8aad1c3d699422f069162d758047
wildworks <wildworks@git.wordpress.org>
2026-08-01 12:30:52 +0000

    Media: Fix a jQuery Migrate warning for disabled buttons.
    Under jQuery 4.0, disabling a media button or the image cropper's action button triggered a jQuery Migrate warning about the boolean `disabled` attribute. These buttons now toggle the `disabled` property instead, which removes the warning and keeps the behavior unchanged.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/10661
    
    Props audrasjb, azaozz, hbhalodia, neo2k23, wildworks.
    See #64425.
    Built from https://develop.svn.wordpress.org/trunk@62963
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62204 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/js/media-views.js
+++ wp-includes/js/media-views.js
@@ -403,7 +403,7 @@ Cropper = wp.media.controller.State.extend(/** @lends wp.media.controller.Croppe
 						selection.set({cropDetails: controller.state().imgSelect.getSelection()});
 
 						this.$el.text(l10n.cropping);
-						this.$el.attr('disabled', true);
+						this.$el.prop( 'disabled', true );
 
 						controller.state().doCrop( selection ).done( function( croppedImage ) {
 							controller.trigger('cropped', croppedImage );
@@ -5267,7 +5267,7 @@ var Button = wp.media.View.extend(/** @lends wp.media.view.Button.prototype */{
 		classes = _.uniq( classes.concat( this.options.classes ) );
 		this.el.className = classes.join(' ');
 
-		this.$el.attr( 'disabled', model.disabled );
+		this.$el.prop( 'disabled', model.disabled );
 		this.$el.text( this.model.get('text') );
 
 		return this;
