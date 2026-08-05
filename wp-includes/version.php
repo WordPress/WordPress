3b7f9e0188596739cb27e5c4aaf42577ade4cd42
wildworks <wildworks@git.wordpress.org>
2026-08-05 15:11:52 +0000

    WordPress 7.1 RC 1. Built from https://develop.svn.wordpress.org/trunk@63033
    git-svn-id: http://core.svn.wordpress.org/trunk@62252 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-beta4-63032';
+$wp_version = '7.1-RC1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
