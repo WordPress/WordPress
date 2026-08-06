ce3c03189ab58ec3b17d4aef19e190f80738aee9
John Blackbourn <johnbillion@git.wordpress.org>
2026-08-06 15:30:52 +0000

    Emoji: Ensure that the emoji settings come from a script element.
    Props westonruter, jonsurrell, peterwilsoncc, davidbinda, lucasbustamante.
    
    Built from https://develop.svn.wordpress.org/trunk@63059
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62258 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-RC1-63037';
+$wp_version = '7.1-RC1-63059';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
