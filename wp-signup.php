18b4b0ba2c8aff7909d7c727595f89f12de005ff
John Blackbourn <johnbillion@git.wordpress.org>
2026-08-06 15:38:50 +0000

    Multisite: Enforce the active signup policy for existing users.
    Props johnbillion, jeremyfelt.
    
    Built from https://develop.svn.wordpress.org/trunk@63064
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62263 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-signup.php
+++ wp-signup.php
@@ -1002,7 +1002,11 @@ if ( 'none' === $active_signup ) {
 			}
 			break;
 		case 'gimmeanotherblog':
-			validate_another_blog_signup();
+			if ( 'all' === $active_signup || 'blog' === $active_signup ) {
+				validate_another_blog_signup();
+			} else {
+				_e( 'Site registration has been disabled.' );
+			}
 			break;
 		case 'default':
 		default:
