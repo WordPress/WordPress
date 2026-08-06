d1875441108b50e03fb9f3c0a32ce56a74e7b4b2
desrosj <desrosj@git.wordpress.org>
2026-08-06 19:36:38 +0000

    Security: Backport the WordPress 7.0.3 security fixes to the 6.4 branch.
    - Users: Ensure a proper email address is used before sending email confirmations.
    - Formatting: Prevent stack overflow in `safecss_filter_attr`.
    - Multisite: Enforce the active signup policy for existing users.
    - HTTP API: Improve compliance with IPv4 Special-Purpose Address Space.
    - Users: Prevent Usernames from mangling HTML
    - Canonical: Only redirect for publicly viewable post types.
    - Administration:  When `wp_is_large_user_count()`, ensure that the post author is always added to author dropdown.
    - Editor: Fix output of Post Date.
    
    Merges [63060],[63061],[63062],[63063],[63064],[63065],[63067] to the 6.4 branch.
    
    Props xknown, westonruter, jeremyfelt, peterwilsoncc, paulkevan, lucasbustamante, jorbin, desrosj, vortfu, dmsnell, johnbillion, ehtis, batmoo, lancewillett, jonsurrell, isabel_brison, bernhard-reiter, tyxla, aduth.
    Built from https://develop.svn.wordpress.org/branches/6.4@63096
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.4@62295 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/user.php
+++ wp-admin/includes/user.php
@@ -44,6 +44,8 @@ function edit_user( $user_id = 0 ) {
 		$user->user_login = sanitize_user( wp_unslash( $_POST['user_login'] ), true );
 	}
 
+	$errors = new WP_Error();
+
 	$pass1 = '';
 	$pass2 = '';
 	if ( isset( $_POST['pass1'] ) ) {
@@ -78,7 +80,12 @@ function edit_user( $user_id = 0 ) {
 	}
 
 	if ( isset( $_POST['email'] ) ) {
-		$user->user_email = sanitize_text_field( wp_unslash( $_POST['email'] ) );
+		$maybe_email = wp_unslash( $_POST['email'] );
+		if ( is_string( $maybe_email ) && is_email( $maybe_email ) ) {
+			$user->user_email = $maybe_email;
+		} else {
+			$errors->add( 'invalid_email', __( '<strong>Error:</strong> The email address is not correct.' ), array( 'form-field' => 'email' ) );
+		}
 	}
 	if ( isset( $_POST['url'] ) ) {
 		if ( empty( $_POST['url'] ) || 'http://' === $_POST['url'] ) {
@@ -144,8 +151,6 @@ function edit_user( $user_id = 0 ) {
 	if ( ! empty( $_POST['use_ssl'] ) ) {
 		$user->use_ssl = 1;
 	}
-
-	$errors = new WP_Error();
 
 	/* checking that username has been typed */
 	if ( '' === $user->user_login ) {
