c794a2e3c6fc62c1c903abdb6472c906c38dcc65
John Blackbourn <johnbillion@git.wordpress.org>
2026-08-06 15:33:47 +0000

    HTTP API: Improve compliance with IPv4 Special-Purpose Address Space.
    Props johnbillion, westonruter, paulkevan, ehtis, jeremyfelt.
    
    Built from https://develop.svn.wordpress.org/trunk@63062
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62261 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-RC1-63061';
+$wp_version = '7.1-RC1-63062';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
