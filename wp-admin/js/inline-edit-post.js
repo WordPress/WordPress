ed513016ef042488442d390f2649b3666ebf40c1
John Blackbourn <johnbillion@git.wordpress.org>
2026-08-06 16:02:34 +0000

    Administration:  When `wp_is_large_user_count()`, ensure that the post author is always added to author dropdown.
    Props lucasbustamante, jeremyfelt, jonsurrell.
    
    Merges [63061] into the 7.0 branch.
    
    Built from https://develop.svn.wordpress.org/branches/7.0@63070
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.0@62269 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/js/inline-edit-post.js
+++ wp-admin/js/inline-edit-post.js
@@ -359,7 +359,12 @@ window.wp = window.wp || {};
 		if ( !$(':input[name="post_author"] option[value="' + $('.post_author', rowData).text() + '"]', editRow).val() ) {
 
 			// The post author no longer has edit capabilities, so we need to add them to the list of authors.
-			$(':input[name="post_author"]', editRow).prepend('<option value="' + $('.post_author', rowData).text() + '">' + $('#post-' + id + ' .author').text() + '</option>');
+			$(':input[name="post_author"]', editRow).prepend(
+				new Option(
+					$('#post-' + id + ' .author').text(),
+					$('.post_author', rowData).text()
+				)
+			);
 		}
 		if ( $( ':input[name="post_author"] option', editRow ).length === 1 ) {
 			$('label.inline-edit-author', editRow).hide();
