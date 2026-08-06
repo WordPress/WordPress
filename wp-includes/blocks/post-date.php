bf9578787fa25d62c476a12f5e60ac8323483057
desrosj <desrosj@git.wordpress.org>
2026-08-06 17:39:34 +0000

    Security: Backport the WordPress 7.0.3 security fixes to the 6.6 branch.
    - Users: Ensure a proper email address is used before sending email confirmations.
    - Formatting: Prevent stack overflow in `safecss_filter_attr`.
    - Multisite: Enforce the active signup policy for existing users.
    - HTTP API: Improve compliance with IPv4 Special-Purpose Address Space.
    - Users: Prevent Usernames from mangling HTML
    - Canonical: Only redirect for publicly viewable post types.
    - Administration:  When `wp_is_large_user_count()`, ensure that the post author is always added to author dropdown.
    - Editor: Fix output of Post Date.
    
    Merges [63060],[63061],[63062],[63063],[63064],[63065],[63067] to the 6.6 branch.
    
    Props xknown, westonruter, jeremyfelt, peterwilsoncc, paulkevan, lucasbustamante, jorbin, desrosj, vortfu, dmsnell, johnbillion, ehtis, batmoo, lancewillett, jonsurrell, isabel_brison, bernhard-reiter, tyxla, aduth.
    Built from https://develop.svn.wordpress.org/branches/6.6@63080
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.6@62279 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/blocks/post-date.php
+++ wp-includes/blocks/post-date.php
@@ -49,7 +49,9 @@ function render_block_core_post_date( $attributes, $content, $block ) {
 	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => implode( ' ', $classes ) ) );
 
 	if ( isset( $attributes['isLink'] ) && $attributes['isLink'] ) {
-		$formatted_date = sprintf( '<a href="%1s">%2s</a>', get_the_permalink( $post_ID ), $formatted_date );
+		$formatted_date = sprintf( '<a href="%1$s">%2$s</a>', esc_url( get_the_permalink( $post_ID ) ), esc_html( $formatted_date ) );
+	} else {
+		$formatted_date = esc_html( $formatted_date );
 	}
 
 	return sprintf(
