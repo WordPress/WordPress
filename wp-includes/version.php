698f48a773eb29b310d59881762120da1ce9c6eb
Jeremy Felt <jeremy.felt@gmail.com>
2026-08-06 12:48:51 +0000

    Build/Test Tools: Ignore PHPCS Security Advisory CVE-2026-67434
    Add an ignore for GHSA-hmqg-cxww-wqhq (CVE-2026-67434), under both its GitHub and Packagist advisory IDs.
    
    WordPress tooling does not use the vulnerabile `Gitblame`, `Hgblame` or `Svnblame` report formats, so the pinned version poses no practical risk.
    
    Props johnbillion, joedolson, jonsurrell, sergeybiryukov.
    Fixes #65822.
    
    Built from https://develop.svn.wordpress.org/trunk@63037
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62256 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-RC1-63036';
+$wp_version = '7.1-RC1-63037';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
