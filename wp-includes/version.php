a64f1f32baaefec40e3c447466811f402bd182dd
desrosj <desrosj@git.wordpress.org>
2026-08-06 21:05:04 +0000

    WordPress 5.4.20. Built from https://develop.svn.wordpress.org/branches/5.4@63128
    git-svn-id: http://core.svn.wordpress.org/branches/5.4@62327 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.4.19';
+$wp_version = '5.4.20';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
