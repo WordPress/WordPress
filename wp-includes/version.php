714e87a6812080a9b88fb081a2cebabbc75dbfdd
John Blackbourn <johnbillion@git.wordpress.org>
2026-08-06 15:32:46 +0000

    Administration:  When `wp_is_large_user_count()`, ensure that the post author is always added to author dropdown.
    Props lucasbustamante, jeremyfelt, jonsurrell.
    
    Built from https://develop.svn.wordpress.org/trunk@63061
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62260 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-RC1-63060';
+$wp_version = '7.1-RC1-63061';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
