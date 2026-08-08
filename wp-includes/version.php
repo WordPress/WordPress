d3a8d859c86391fa57dac48ea404ed44b2a2490b
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-08-08 00:46:50 +0000

    Tests: Correct `@var` annotation in REST users controller tests.
    The `$query` variable comes from the `pre_user_query` filter args and is a `WP_User_Query` instance, the `assertInstanceOf( WP_User_Query::class, ... )` on the line above asserts exactly that. The docblock incorrectly declared it as `WP_User`.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/12834.
    
    Follow-up to r59899.
    
    Props Soean.
    See #64894.
    Built from https://develop.svn.wordpress.org/trunk@63160
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62359 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-RC2-63159';
+$wp_version = '7.1-RC2-63160';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
