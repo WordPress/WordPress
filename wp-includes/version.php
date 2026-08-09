c5233f1c9f0c0984297dcd3bb0c1aefd4d94b6d2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-08-09 18:50:50 +0000

    Tests: Use `assertNull()` instead of `assertSame()` with `null`.
    Using the dedicated `assertNull()` assertion instead of `assertSame( null, $actual )` clarifies intent and produces more descriptive failure messages.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/12818.
    
    Follow-up to r62969.
    
    Props Soean.
    See #64894.
    Built from https://develop.svn.wordpress.org/trunk@63161
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62360 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-RC2-63160';
+$wp_version = '7.1-RC2-63161';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
