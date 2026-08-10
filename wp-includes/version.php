7481e448692a21333147fd49c020a4ab955584c3
desrosj <desrosj@git.wordpress.org>
2026-08-10 17:22:50 +0000

    Build/Test Tools: Miscellaneous imrpovements after [62859].
    This applies several small changes to improve on the changes aimed at fetching the built Gutenberg asset zip file once per run:
    - Adds some missing step names.
    - Converts the `gutenberg-artifact` workflow input from a string to boolean.
    - Removes overly complex job description.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/12935.
    
    Follow up to [62859], [62862].
    
    Props desrosj, irozum.
    Fixes #65721.
    Built from https://develop.svn.wordpress.org/trunk@63162
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62361 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-RC2-63161';
+$wp_version = '7.1-RC2-63162';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
