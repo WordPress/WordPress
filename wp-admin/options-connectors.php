b4e6579af368cc18cce38ab6ed4ec50fac6de5a7
wildworks <wildworks@git.wordpress.org>
2026-08-11 06:05:54 +0000

    Administration: Remove no-JS notices from route-based screens.
    The Fonts and Connectors screens no longer render their own heading and JavaScript-required notice. The generated page template now provides both for every route-based screen, so the per-screen markup only produced a duplicate heading and notice when JavaScript was disabled.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/12946
    
    Follow-up to [62954].
    Props hbhalodia, wildworks.
    Fixes #65840.
    
    Built from https://develop.svn.wordpress.org/trunk@63174
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62371 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/options-connectors.php
+++ wp-admin/options-connectors.php
@@ -27,28 +27,12 @@ if ( ! class_exists( '\WordPress\AiClient\AiClient' ) || ! function_exists( 'wp_
 }
 
 // Set the page title.
-$title               = __( 'Connectors' );
-$js_required_message = __( 'The Connectors screen requires JavaScript. Please enable JavaScript in your browser settings to manage your connectors.' );
+$title = __( 'Connectors' );
 
 // Set parent file for menu highlighting.
 $parent_file = 'options-general.php';
 
 require_once ABSPATH . 'wp-admin/admin-header.php';
-
-?>
-<div class="wrap hide-if-js">
-	<h1 class="wp-heading-inline"><?php echo esc_html( $title ); ?></h1>
-	<?php
-		wp_admin_notice(
-			$js_required_message,
-			array(
-				'type'               => 'error',
-				'additional_classes' => array( 'hide-if-js' ),
-			)
-		);
-		?>
-</div>
-<?php
 
 // Render the Connectors page.
 wp_options_connectors_wp_admin_render_page();
