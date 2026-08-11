3fc2880a4f3b77c7b83746f14da0d7af2a4b42d3
wildworks <wildworks@git.wordpress.org>
2026-08-11 06:18:50 +0000

    Login and Registration: Clarify the "Remember Me" toggletip text.
    The help text on the login screen stated that the option reduces how often you are asked to log in. It actually extends how long you stay logged in before being asked again, so the wording now describes that behavior.
    
    Follow-up to [62741].
    
    Reviewed by johnbillion.
    Merges [63175] to the 7.1 branch.
    
    Props joedolson, johnbillion, sabernhardt, wildworks.
    Fixes #55343.
    Built from https://develop.svn.wordpress.org/branches/7.1@63176
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.1@62373 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-login.php
+++ wp-login.php
@@ -1548,7 +1548,7 @@ switch ( $action ) {
 			 */
 			$rememberme_help_text = apply_filters(
 				'login_remember_me_help_text',
-				__( 'Selecting "Remember Me" reduces the number of times you&#8217;ll be asked to log in using this device. To keep your account secure, use this option only on your personal devices.' )
+				__( 'Selecting "Remember Me" increases the length of time until you&#8217;re asked to log in again on this device. To keep your account secure, use this option only on your personal devices.' )
 			);
 			?>
 			<p class="forgetmenot">
