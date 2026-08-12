72a498bab6ebca9bcbdf8c1a21566cdaa37d9a71
wildworks <wildworks@git.wordpress.org>
2026-08-12 07:00:49 +0000

    Toolbar: Inconsistent text hover color for top-level nodes.
    Follow up to [63009]. Original commit removed a :not(.mobile) qualifier from the :hover rule, but not from the default state rule.
    
    Remove the default rule as well, so colors are handled consistently.
    
    Reviewed by wildworks.
    Merges [63188] to the 7.1 branch.
    
    Props fushar, khokansardar, madhavishah01, wildworks, afercia, joedolson.
    Fixes #65848.
    Built from https://develop.svn.wordpress.org/branches/7.1@63202
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.1@62395 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-RC2-63201';
+$wp_version = '7.1-RC2-63202';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
