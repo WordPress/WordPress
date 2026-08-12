7952ea083f088440945f2b30ca74c8e47d44abb6
John Blackbourn <johnbillion@git.wordpress.org>
2026-08-12 12:03:51 +0000

    Media: Prevent loading images into Imagick which might be PostScript.
    Props ehtis, dmsnell, jeremyfelt, westonruter, SergeyBiryukov, aaroncampbell, jorbin, batmoo, johnbillion, lancewillett, tyxla, vortfu, xknown.
    
    Built from https://develop.svn.wordpress.org/trunk@63210
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62403 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63207';
+$wp_version = '7.2-alpha-63210';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
