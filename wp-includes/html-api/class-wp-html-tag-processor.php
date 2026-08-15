9f47987558ec92ae42c1fd3dfe18da676249af34
wildworks <wildworks@git.wordpress.org>
2026-08-15 04:39:50 +0000

    HTML API: Make `class_list()` examples interactive.
    Converts the `WP_HTML_Processor::class_list()` and `WP_HTML_Tag_Processor::class_list()` examples to runnable WordPress Playground snippets. Corrects the encoded closing angle bracket so the expected output matches the parsed class name.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/13034.
    
    Reviewed by wildworks.
    Merges [63293] to the 7.1 branch.
    
    Props jonsurrell.
    See #64896.
    Built from https://develop.svn.wordpress.org/branches/7.1@63302
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.1@62495 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/html-api/class-wp-html-tag-processor.php
+++ wp-includes/html-api/class-wp-html-tag-processor.php
@@ -1178,14 +1178,20 @@ class WP_HTML_Tag_Processor {
 	 *
 	 * This generator function is designed to be used inside a "foreach" loop.
 	 *
-	 * Example:
-	 *
-	 *     $p = new WP_HTML_Tag_Processor( "<div class='free &lt;egg&lt;\tlang-en'>" );
-	 *     $p->next_tag();
-	 *     foreach ( $p->class_list() as $class_name ) {
-	 *         echo "{$class_name} ";
-	 *     }
-	 *     // Outputs: "free <egg> lang-en "
+	 * ```php interactive
+	 * <?php
+	 * require '/wordpress/wp-load.php';
+	 * $p = new WP_HTML_Tag_Processor( "<div class='free &lt;egg&gt;\tlang-en'>" );
+	 * $p->next_tag();
+	 * foreach ( $p->class_list() as $class_name ) {
+	 *   var_dump( $class_name );
+	 * }
+	 * ```
+	 * ```expected-output
+	 * string(4) "free"
+	 * string(5) "<egg>"
+	 * string(7) "lang-en"
+	 * ```
 	 *
 	 * @since 6.4.0
 	 *
