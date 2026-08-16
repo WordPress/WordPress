f37eb7ab386ba0785b153f64c9c4451a8df9d9e3
John Blackbourn <johnbillion@git.wordpress.org>
2026-08-16 23:08:51 +0000

    Build/Test Tools: Stop committing changes to tracked files in pull requests.
    This workflow has not proven to be as useful as originally hoped, so it's being removed to reduce maintenance burden. All workflows that run the build script already fail if they result in untracked changes.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/13087
    
    Props desrosj
    
    See #65845
    
    Built from https://develop.svn.wordpress.org/trunk@63315
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62508 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63314';
+$wp_version = '7.2-alpha-63315';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
