88b90add056521b8d71a0c64d2fe17b72ba0dbf4
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2026-08-18 11:44:50 +0000

    Docs: Add missing descriptions and declare types for `Translation_Entry` properties.
    Developed in https://github.com/WordPress/wordpress-develop/pull/13126.
    
    Follow-up to r10584, r32990, r57734.
    
    Props pedromendonca, mukesh27, dhruvang21.
    Fixes #65901.
    Built from https://develop.svn.wordpress.org/trunk@63323
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62516 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/pomo/entry.php
+++ wp-includes/pomo/entry.php
@@ -23,32 +23,85 @@ if ( ! class_exists( 'Translation_Entry', false ) ) :
 		 */
 		public $is_plural = false;
 
-		public $context             = null;
-		public $singular            = null;
-		public $plural              = null;
-		public $translations        = array();
-		public $translator_comments = '';
-		public $extracted_comments  = '';
-		public $references          = array();
-		public $flags               = array();
+		/**
+		 * The string to translate.
+		 *
+		 * @var string|null
+		 */
+		public $singular = null;
 
 		/**
-		 * @param array $args {
-		 *     Arguments array, supports the following keys:
+		 * The plural form of the string.
 		 *
-		 *     @type string $singular            The string to translate, if omitted an
-		 *                                       empty entry will be created.
-		 *     @type string $plural              The plural form of the string, setting
-		 *                                       this will set `$is_plural` to true.
-		 *     @type array  $translations        Translations of the string and possibly
-		 *                                       its plural forms.
-		 *     @type string $context             A string differentiating two equal strings
-		 *                                       used in different contexts.
-		 *     @type string $translator_comments Comments left by translators.
-		 *     @type string $extracted_comments  Comments left by developers.
-		 *     @type array  $references          Places in the code this string is used, in
-		 *                                       relative_to_root_path/file.php:linenum form.
-		 *     @type array  $flags               Flags like php-format.
+		 * @var string|null
+		 */
+		public $plural = null;
+
+		/**
+		 * Translations of the string and possibly its plural forms.
+		 *
+		 * Plural forms which have not been filled in are null.
+		 *
+		 * @var (string|null)[]
+		 */
+		public $translations = array();
+
+		/**
+		 * Context of the string, used to differentiate two equal strings used in different contexts.
+		 *
+		 * @var string|null
+		 */
+		public $context = null;
+
+		/**
+		 * Comments left by translators.
+		 *
+		 * @var string
+		 */
+		public $translator_comments = '';
+
+		/**
+		 * Comments left by developers.
+		 *
+		 * @var string
+		 */
+		public $extracted_comments = '';
+
+		/**
+		 * Places in the code this string is used, in relative_to_root_path/file.php:linenum form.
+		 *
+		 * @var string[]
+		 */
+		public $references = array();
+
+		/**
+		 * Flags like php-format.
+		 *
+		 * @var string[]
+		 */
+		public $flags = array();
+
+		/**
+		 * Constructor.
+		 *
+		 * @since 2.8.0
+		 *
+		 * @param array $args {
+		 *     Optional. Array of arguments. Default empty array.
+		 *
+		 *     @type string          $singular            The string to translate, if omitted an
+		 *                                                empty entry will be created.
+		 *     @type string          $plural              The plural form of the string, setting
+		 *                                                this will set `$is_plural` to true.
+		 *     @type (string|null)[] $translations        Translations of the string and possibly
+		 *                                                its plural forms.
+		 *     @type string          $context             A string differentiating two equal strings
+		 *                                                used in different contexts.
+		 *     @type string          $translator_comments Comments left by translators.
+		 *     @type string          $extracted_comments  Comments left by developers.
+		 *     @type string[]        $references          Places in the code this string is used, in
+		 *                                                relative_to_root_path/file.php:linenum form.
+		 *     @type string[]        $flags               Flags like php-format.
 		 * }
 		 */
 		public function __construct( $args = array() ) {
@@ -81,6 +134,8 @@ if ( ! class_exists( 'Translation_Entry', false ) ) :
 		 * @deprecated 5.4.0 Use __construct() instead.
 		 *
 		 * @see Translation_Entry::__construct()
+		 *
+		 * @param array $args Optional. Array of arguments. Default empty array.
 		 */
 		public function Translation_Entry( $args = array() ) {
 			_deprecated_constructor( self::class, '5.4.0', static::class );
