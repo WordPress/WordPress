88b0f61d9dea114152bf55add14a66f3f0c519cb
dmsnell <dmsnell@git.wordpress.org>
2026-08-24 19:21:48 +0000

    Code Quality: Resolve greater.invalid PHPStan errors in baseline.
    Resolves type issues in `upgrade_430_fix_comments()`, which checks for a string return from `$wpdb->get_col_length()`, which was removed from Core in r32364.
    
    This change was part of Contributor Day at WordCamp US 2026.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13080
    Discussed in: https://core.trac.wordpress.org/ticket/65817
    
    Follow-up to [32364].
    
    Props dpantazis, dmsnell.
    See #65817.
    
    Built from https://develop.svn.wordpress.org/trunk@63344
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62537 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/includes/upgrade.php
+++ wp-admin/includes/upgrade.php
@@ -2037,24 +2037,14 @@ function upgrade_430_fix_comments() {
 
 	$content_length = $wpdb->get_col_length( $wpdb->comments, 'comment_content' );
 
-	if ( is_wp_error( $content_length ) ) {
-		return;
-	}
-
 	if ( false === $content_length ) {
 		$content_length = array(
 			'type'   => 'byte',
 			'length' => 65535,
 		);
-	} elseif ( ! is_array( $content_length ) ) {
-		$length         = (int) $content_length > 0 ? (int) $content_length : 65535;
-		$content_length = array(
-			'type'   => 'byte',
-			'length' => $length,
-		);
 	}
 
-	if ( 'byte' !== $content_length['type'] || 0 === $content_length['length'] ) {
+	if ( ! is_array( $content_length ) || 'byte' !== $content_length['type'] || 0 === $content_length['length'] ) {
 		// Sites with malformed DB schemas are on their own.
 		return;
 	}
