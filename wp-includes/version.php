c6722b13ff286300487ab93ad2e4ac087441d3fe
Andrea Fercia <a.fercia@gmail.com>
2026-08-27 21:52:49 +0000

    Administration: Fix the CSS Coding Standards in site-icon.css.
    Lints the `src/wp-admin/css/site-icon.css` file.
    Part of the ongoing effort to lint all the admin CSS files.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/13148
    
    See #65898.
    
    Built from https://develop.svn.wordpress.org/trunk@63372
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62565 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63371';
+$wp_version = '7.2-alpha-63372';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
