05def567a7f41af8a56e960f4e7e9cbc253ab78a
wildworks <wildworks@git.wordpress.org>
2026-08-27 00:22:51 +0000

    View configuration API: Add a Reading settings field.
    Adds a Reading settings entry to the form configuration returned for templates. The editor renders it as a link to the Reading settings screen in the front page template's summary panel.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/13266
    
    Follow-up to [62547].
    
    Props ntsekouras, wildworks.
    Fixes #65959.
    Built from https://develop.svn.wordpress.org/trunk@63355
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62548 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63354';
+$wp_version = '7.2-alpha-63355';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
--- wp-includes/view-config.php
+++ wp-includes/view-config.php
@@ -725,6 +725,13 @@ function _wp_get_entity_view_config_posttype_wp_template( $data ) {
 				),
 			),
 			array(
+				'id'     => 'reading_settings',
+				'layout' => array(
+					'type'          => 'regular',
+					'labelPosition' => 'none',
+				),
+			),
+			array(
 				'id'     => 'last_edited_date',
 				'layout' => array(
 					'type'          => 'panel',
