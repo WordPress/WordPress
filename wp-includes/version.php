892b81d6c2aa80c1276613fbe9b37ecc6873b539
Andrea Fercia <a.fercia@gmail.com>
2026-08-27 21:58:48 +0000

    Administration: Fix the CSS Coding Standards in wp-auth-check.css.
    Lints the `src/wp-includes/css/wp-auth-check.css` file.
    Part of the ongoing effort to lint all the admin CSS files.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/13167
    
    See #65898.
    
    Built from https://develop.svn.wordpress.org/trunk@63374
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62567 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63373';
+$wp_version = '7.2-alpha-63374';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
