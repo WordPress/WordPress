4ae5edcdbe7dc9baed09154636e7add2c7d0a281
Lance Willett <nanobar@gmail.com>
2026-08-31 18:37:48 +0000

    Build/Test Tools: Expand path filtering for the old branch testing workflow.
    The test old branches workflow runs when the reusable PHPUnit test workflows change, to confirm those changes do not break workflows in old branches. Many other workflows also use reusable workflow files, so the path filtering is expanded to match.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13089
    
    Props desrosj.
    See #65845.
    
    Built from https://develop.svn.wordpress.org/trunk@63415
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62607 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63412';
+$wp_version = '7.2-alpha-63415';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
