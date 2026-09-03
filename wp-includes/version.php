3f1dc2148a89179d33deec8c66769c0ca87a320a
joedolson <joedolson@git.wordpress.org>
2026-09-03 17:51:50 +0000

    Login and Registration: Align tooltip icon with control.
    Update CSS for the login and registration screen for the tooltip icon on the Remember Me option so that the icon aligns correctly with the checkbox and input.
    
    Props utsavladani, timse201, nkpathan, khokansardar, dhruvang21, pbiron, r1k0, ocean90, saiful09, joedolson.
    Fixes #65918.
    Built from https://develop.svn.wordpress.org/trunk@63448
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62629 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63447';
+$wp_version = '7.2-alpha-63448';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
