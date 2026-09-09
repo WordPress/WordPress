328f8d90eba574206ed8158191de20ef307e93b0
afercia <afercia@git.wordpress.org>
2026-09-09 14:03:53 +0000

    Administration: Remove usage of the #787c82 gray color.
    This light gray color is used for controls that contain only icons. For example, the next / previous / close buttons in the media and themes browsers. Same for some icon-only controls in the Customizer. These icons must meet a 4.5:1 minimum color contrast ratio.
    
    The remaining usages are so limited that don't justify the introduction of a dedicated color so it's reasonable to remove it entirely.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/13320
    
    Fixes #65995.
    
    Built from https://develop.svn.wordpress.org/trunk@63559
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62735 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63558';
+$wp_version = '7.2-alpha-63559';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
