0ceb21c1c5a90f5dcbe81c9a15e8761b5d2bcdfd
adamsilverstein <adamsilverstein@git.wordpress.org>
2026-09-09 16:19:46 +0000

    Media: Prepare the attachment finalize response from the current post data.
    `finalize_item()` prepares its response from the `$post` fetched before the `wp_generate_attachment_metadata` filter runs, so anything a callback writes to the post row is missing from the response. The editor now stores that response as the attachment record instead of refetching, so re-read the post after the metadata is written and return the `WP_Error` if a callback deleted the attachment. See https://github.com/WordPress/gutenberg/issues/81844 and https://github.com/WordPress/gutenberg/pull/81947.
    
    Props gregbenz, andrewserong.
    Fixes #66056.
    
    
    Built from https://develop.svn.wordpress.org/trunk@63564
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62740 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63563';
+$wp_version = '7.2-alpha-63564';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
