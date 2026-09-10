8d08ebef12cbe83ab0c4d9f783d0985f46a29d9e
westonruter <westonruter@git.wordpress.org>
2026-09-10 01:01:57 +0000

    Sitemaps: Revert r63573 from the 7.0 branch.
    The sitemap 404 fix merged in r63573 was committed to the 7.0 branch in error; it was intended for the 7.1 branch. This restores the branch to its state as of r63449 and removes the recorded merge of r63570, leaving 7.0 unchanged from what shipped in 7.0.4.
    
    Reverts r63573.
    
    See #65945.
    
    Built from https://develop.svn.wordpress.org/branches/7.0@63574
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.0@62750 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp.php
+++ wp-includes/class-wp.php
@@ -746,9 +746,8 @@ class WP {
 
 		$set_404 = true;
 
-		// Never 404 here for the admin, robots, favicon, or sitemaps.
-		// Sitemap routes send their own status in WP_Sitemaps::render_sitemaps().
-		if ( is_admin() || is_robots() || is_favicon() || is_sitemap() || get_query_var( 'sitemap-stylesheet' ) ) {
+		// Never 404 for the admin, robots, or favicon.
+		if ( is_admin() || is_robots() || is_favicon() ) {
 			$set_404 = false;
 
 			// If posts were found, check for paged content.
