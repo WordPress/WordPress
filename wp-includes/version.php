3934fa5a6b2a15f1c7d934417169f544e714635a
desrosj <desrosj@git.wordpress.org>
2026-09-17 00:09:47 +0000

    Build/Test Tools: Increase the shorter timeout just a bit.
    In r63643 a timeout value of `3` was added to each `npm install` step to avoid runaway tasks after the Node.js 24.x update in r63539. This is causing a higher than usual number of runs to timeout because the change was made to reusable workflows due to older branches having more dependencies defined prior to r61438.
    
    This increases the `timeout-minutes` value to `5` to allow enough of a grace period for oolder branches to complete `npm install` while still catching any hanging commands within branches using `24.x`.
    
    See #66116, #65845, #65451.
    Built from https://develop.svn.wordpress.org/trunk@63647
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62822 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63646';
+$wp_version = '7.2-alpha-63647';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
