903c14c6802385263a2990ad2a108c1a78fcaf2f
desrosj <desrosj@git.wordpress.org>
2026-09-17 20:52:59 +0000

    Security: Backport the WordPress 7.1.1 security fixes to the 6.3 branch.
    - Posts, Post Types: Reject a supplied post ID on the create path in `_wp_translate_postdata()`.
    - Editor: Constrain block template file resolution to the template directory.
    - Comments: Enforce target post permissions when updating notes via REST.
    - XML-RPC: Reject writes to internal-only builtin post types.
    - Administration: Add authorization check to `wp_ajax_sample_permalink()`.
    - Customize: Improve `header_image_data` theme mod sanitization.
    - Media: Check `read_post` on the attachment's parent post.
    - Themes: Escape the theme installer preview route value.
    - Plugins: Require network plugin authority to Ajax-activate a network-only plugin.
    - Formatting: Prevent `wpautop()` moving a paragraph into an attribute of a blockquote.
    
    Merges r63657, r63658, r63659, r63660, r63663, r63665, r63669, r63670, r63672, r63675 to the 6.3 branch.
    
    Props xknown, westonruter, jorbin, vortfu, batmoo, davidbinda, jeremyfelt, ramonopoly, jonsurrell, adamsilverstein, villanovachile, johnbillion, peterwilsoncc, lancewillett, dmsnell, whyisjake, buffer1024, joehoyle, rafiem.
    Built from https://develop.svn.wordpress.org/branches/6.3@63698
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/6.3@62872 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/class-wp-xmlrpc-server.php
+++ wp-includes/class-wp-xmlrpc-server.php
@@ -1442,6 +1442,33 @@ class wp_xmlrpc_server extends IXR_Server {
 			return new IXR_Error( 403, __( 'Invalid post type.' ) );
 		}
 
+		// Reject writes to internal-only builtin post types (e.g. customize_changeset)
+		// whose intended write path is a dedicated helper, not a generic post API.
+		$is_internal_only = (
+			empty( $post_type->public )
+			&& empty( $post_type->show_in_rest )
+			&& ! empty( $post_type->_builtin )
+		);
+
+		/**
+		 * Filters whether a post type accepts writes via XML-RPC.
+		 *
+		 * Defaults to false for internal-only builtin post types (public=false,
+		 * show_in_rest=false, _builtin=true), such as customize_changeset, whose
+		 * writes are meant to flow through dedicated helpers. Return true to opt
+		 * a post type back in.
+		 *
+		 * @since 7.1.1
+		 *
+		 * @param bool         $allowed   Whether the post type accepts XML-RPC writes.
+		 * @param WP_Post_Type $post_type The post type object.
+		 */
+		$allowed = apply_filters( 'xmlrpc_allow_post_type_writes', ! $is_internal_only, $post_type );
+
+		if ( ! $allowed ) {
+			return new IXR_Error( 403, __( 'Sorry, this post type is not supported over XML-RPC.' ) );
+		}
+
 		$update = ! empty( $post_data['ID'] );
 
 		if ( $update ) {
