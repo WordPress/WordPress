c3e0b60765b0bf80e8d353353ff2164a02cbaaee
desrosj <desrosj@git.wordpress.org>
2026-09-17 17:32:46 +0000

    HTML API: Prevent `set_modifiable_text()` from abruptly closing comments.
    Merges r63656 to the 7.1 branch.
    
    Props jeremyfelt, jonsurrell.
    
    Built from https://develop.svn.wordpress.org/branches/7.1@63661
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.1@62835 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/html-api/class-wp-html-tag-processor.php
+++ wp-includes/html-api/class-wp-html-tag-processor.php
@@ -3988,7 +3988,7 @@ class WP_HTML_Tag_Processor {
 			self::COMMENT_AS_HTML_COMMENT === $this->comment_type
 		) {
 			// Check if the text could close the comment.
-			if ( 1 === preg_match( '/--!?>/', $plaintext_content ) ) {
+			if ( 1 === preg_match( '/^-?>|--!?>/', $plaintext_content ) ) {
 				_doing_it_wrong(
 					__METHOD__,
 					__( 'Comment text cannot contain a comment closer.' ),
