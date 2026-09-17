906a7ae31165abade0fc8c9f620d9a19392bd28d
desrosj <desrosj@git.wordpress.org>
2026-09-17 21:14:33 +0000

    Security: Backport the WordPress 7.1.1 security fixes to the 4.9 branch.
    - Posts, Post Types: Reject a supplied post ID on the create path in `_wp_translate_postdata()`.
    - XML-RPC: Reject writes to internal-only builtin post types.
    - Administration: Add authorization check to `wp_ajax_sample_permalink()`.
    - Customize: Improve `header_image_data` theme mod sanitization.
    - Themes: Escape the theme installer preview route value.
    - Plugins: Require network plugin authority to Ajax-activate a network-only plugin.
    - Formatting: Prevent `wpautop()` moving a paragraph into an attribute of a blockquote.
    
    Merges r63657, r63658, r63659, r63660, r63665, r63669, r63672 to the 4.9 branch.
    
    Props xknown, westonruter, jorbin, vortfu, batmoo, davidbinda, jeremyfelt, johnbillion, peterwilsoncc, lancewillett, jonsurrell, dmsnell, villanovachile, whyisjake, buffer1024, joehoyle, rafiem.
    Built from https://develop.svn.wordpress.org/branches/4.9@63713
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/4.9@62886 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-admin/custom-header.php
+++ wp-admin/custom-header.php
@@ -497,7 +497,7 @@ class Custom_Image_Header {
 		if ( $custom_header->height )
 			$header_image_style .= 'height:' . $custom_header->height . 'px;';
 	?>
-	<div id="headimg" style="<?php echo $header_image_style; ?>">
+	<div id="headimg" style="<?php echo esc_attr( $header_image_style ); ?>">
 		<?php
 		if ( display_header_text() )
 			$style = ' style="color:#' . get_header_textcolor() . ';"';
@@ -1020,8 +1020,8 @@ wp_nonce_field( 'custom-header-options', '_wpnonce-custom-header-options' ); ?>
 				'attachment_id' => $choice['attachment_id'],
 				'url'           => $choice['url'],
 				'thumbnail_url' => $choice['url'],
-				'height'        => $choice['height'],
-				'width'         => $choice['width'],
+				'height'        => absint( $choice['height'] ),
+				'width'         => absint( $choice['width'] ),
 			);
 
 			update_post_meta( $choice['attachment_id'], '_wp_attachment_is_custom_header', get_stylesheet() );
@@ -1048,7 +1048,13 @@ wp_nonce_field( 'custom-header-options', '_wpnonce-custom-header-options' ); ?>
 				return;
 		}
 
-		set_theme_mod( 'header_image', esc_url_raw( $header_image_data['url'] ) );
+		$header_image_data['url'] = esc_url_raw( $header_image_data['url'] );
+
+		if ( isset( $header_image_data['thumbnail_url'] ) ) {
+			$header_image_data['thumbnail_url'] = esc_url_raw( $header_image_data['thumbnail_url'] );
+		}
+
+		set_theme_mod( 'header_image', $header_image_data['url'] );
 		set_theme_mod( 'header_image_data', $header_image_data );
 	}
 
@@ -1407,9 +1413,8 @@ wp_nonce_field( 'custom-header-options', '_wpnonce-custom-header-options' ); ?>
 		$alt_text_key = '_wp_attachment_image_alt';
 
 		foreach ( $header_images as &$header_image ) {
-			$header_meta = get_post_meta( $header_image['attachment_id'] );
-			$header_image['timestamp'] = isset( $header_meta[ $timestamp_key ] ) ? $header_meta[ $timestamp_key ] : '';
-			$header_image['alt_text'] = isset( $header_meta[ $alt_text_key ] ) ? $header_meta[ $alt_text_key ] : '';
+			$header_image['timestamp'] = get_post_meta( $header_image['attachment_id'], $timestamp_key, true );
+			$header_image['alt_text'] = get_post_meta( $header_image['attachment_id'], $alt_text_key, true );
 		}
 
 		return $header_images;
