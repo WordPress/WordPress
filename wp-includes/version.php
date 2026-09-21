8180e733b4e42aea935c3a24864b4b32ea9f5156
afercia <afercia@git.wordpress.org>
2026-09-21 19:56:45 +0000

    Build/Test Tools: Exclude minified files from Grunt `jshint:plugins` more effectively.
    Follow-up to [63364]. Prevents `grunt jshint:plugins` from scanning minified JavaScript files that don't use a `.min` suffix in their filename. Assumes that JavaScript files that contain lines with a length that is longer than 500 characters are minified file and skips them.
    
    Developed in https://github.com/WordPress/wordpress-develop/pull/13573
    
    Props afercia, westonruter.
    Fixes #66123.
    
    Built from https://develop.svn.wordpress.org/trunk@63783
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62955 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63782';
+$wp_version = '7.2-alpha-63783';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
