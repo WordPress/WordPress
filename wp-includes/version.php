a58b5ce96af66beb1175966ff22180eb35842800
lancewillett <lancewillett@git.wordpress.org>
2026-09-21 17:25:49 +0000

    Tests: Use strict assertions in comment tests.
    Compare comment query IDs as strings and use strict assertions for pagination offsets and pingback results. Explain why comment object arrays retain value-based comparison.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13601
    
    Props r1k0, mukesh27.
    See #64895.
    
    Built from https://develop.svn.wordpress.org/trunk@63774
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@62946 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63773';
+$wp_version = '7.2-alpha-63774';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
