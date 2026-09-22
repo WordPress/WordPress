058e95a7181862ca90a009460f2255ed35450b2f
desrosj <desrosj@git.wordpress.org>
2026-09-22 18:23:59 +0000

    Include `-alpha-CHANGESET` in the 7.1 branch version.
    When `$wp_version` does not contain `alpha`, `beta`, or `rc`, the `wp-env` package will use the corresponding `ref` of the tag for the version explicilty listed instead of the branch’s tip.
    
    As a follow up to r63811, this adds `-alpha-63811` to the `7.1.3` version string in the `7.1` branch.
    
    Props johnbillion.
    Built from https://develop.svn.wordpress.org/branches/7.1@63869
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.1@63041 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1.3';
+$wp_version = '7.1.3-alpha-63869';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
