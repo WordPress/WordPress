0b741d24a60656d729bbc8809beafb6fa17c1478
lancewillett <lancewillett@git.wordpress.org>
2026-09-22 21:21:48 +0000

    Tests: Use a file path for cropped site icon attachments.
    Copy the parent image into the uploads directory and pass the resulting file path to WP_Site_Icon::insert_attachment(), matching the production call. Assert that the copy succeeds before inserting the attachment.
    
    This prevents attachment metadata generation from opening a URL over HTTP and waiting for socket timeouts when the test domain includes a port.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13661
    
    Props lucatume, adrianmoldovanwp.
    Fixes #66159.
    
    Built from https://develop.svn.wordpress.org/trunk@63874
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@63046 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63873';
+$wp_version = '7.2-alpha-63874';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
