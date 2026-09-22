72ca595c8400c8c89b2a97ce4b4538c868f727f6
lancewillett <lancewillett@git.wordpress.org>
2026-09-22 23:40:47 +0000

    Build/Test Tools: Format the copied Gutenberg style registry.
    Align array assignment operators and normalize dependency array spacing when copying the generated style registry into Core. This allows PHPCS scans after build:dev to pass without requiring PHP tooling in build-only jobs.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/12778
    
    Props benitoalba, desrosj, jorbin, wildworks.
    See #65278.
    
    Built from https://develop.svn.wordpress.org/trunk@63883
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@63055 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63882';
+$wp_version = '7.2-alpha-63883';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
