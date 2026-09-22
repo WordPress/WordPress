9b9d4a888069c5059ee38b1e02b91748f9805a4f
lancewillett <lancewillett@git.wordpress.org>
2026-09-22 23:12:49 +0000

    Build/Test Tools: Run develop upgrade tests weekly.
    Schedule the develop-version upgrade workflow for Mondays at 00:00 UTC. Make the fork upgrade job explicitly limited to pull requests, retaining its existing draft controls.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13609
    
    Props desrosj.
    See #65845.
    
    Built from https://develop.svn.wordpress.org/trunk@63881
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@63053 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63880';
+$wp_version = '7.2-alpha-63881';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
