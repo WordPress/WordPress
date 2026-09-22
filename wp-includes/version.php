aa133c6ea061027964c006f6aad32803ba9f1a35
lancewillett <lancewillett@git.wordpress.org>
2026-09-22 21:12:47 +0000

    Tests: Use strict assertions in post tests.
    Match expected category IDs to integers and menu item, revision author, post meta, and thumbnail IDs to strings. Use strict comparisons for cache invalidation and filtered post titles.
    
    Assert that a non-public post type is excluded from search with assertTrue(), checking both the expected boolean value and type.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13659
    
    Props haritpanchal.
    See #64895.
    
    Built from https://develop.svn.wordpress.org/trunk@63873
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@63045 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63872';
+$wp_version = '7.2-alpha-63873';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
