1fe057a918cf958fea065b2c0c95d877bce03794
lancewillett <lancewillett@git.wordpress.org>
2026-09-23 00:06:49 +0000

    Build/Test Tools: Correct RTL arrows with alternative text.
    Set aside CSS content alternative text before matching directional Dashicons, then restore it after swapping the glyph. This keeps modern declarations consistent with their legacy fallbacks in generated RTL stylesheets.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13361
    
    Props khokansardar, sabernhardt, joedolson.
    Fixes #66020.
    
    Built from https://develop.svn.wordpress.org/trunk@63885
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@63057 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63884';
+$wp_version = '7.2-alpha-63885';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
