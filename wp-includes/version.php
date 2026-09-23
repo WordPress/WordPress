4086ff2fb75774920de8e25e90afdd854625320d
lancewillett <lancewillett@git.wordpress.org>
2026-09-23 17:35:48 +0000

    Build/Test Tools: Set retention periods for build artifacts.
    Retain development packages and performance results for one day, and end-to-end failure artifacts and WordPress builds for 14 days. Limit storage while keeping short-term debugging output and cross-run build consumers available.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13527
    
    Props obenland, adrianmoldovanwp.
    See #65845.
    
    Built from https://develop.svn.wordpress.org/trunk@63898
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@63067 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63897';
+$wp_version = '7.2-alpha-63898';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
