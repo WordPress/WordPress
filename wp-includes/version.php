5cf6580a9899a2599e133aa2abeabff23262b4c4
whyisjake <whyisjake@git.wordpress.org>
2026-09-23 23:28:51 +0000

    Embeds: Update the ReverbNation oEmbed endpoint.
    ReverbNation has moved its site and oEmbed API to the `legacy.reverbnation.com` subdomain. The previous `www.reverbnation.com/oembed` endpoint returns a `404 Not Found` response and does not redirect, so embeds of artist and song URLs stopped working. This points the provider at the new endpoint and extends the URL pattern to match `legacy.reverbnation.com`, which `www.reverbnation.com` URLs now redirect to.
    
    Reviewed by whyisjake.
    Merges r63907 to the 7.1 branch.
    
    Props johnbillion, yogeshbhutkar, arkaprabhachowdhury, adamsilverstein.
    Fixes #66079.
    
    Built from https://develop.svn.wordpress.org/branches/7.1@63908
    
    
    git-svn-id: http://core.svn.wordpress.org/branches/7.1@63077 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1.3-alpha-63905';
+$wp_version = '7.1.3-alpha-63908';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
