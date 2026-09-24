4478405d6576880f0b3d95327c760fd1d50af9e5
lancewillett <lancewillett@git.wordpress.org>
2026-09-24 16:31:47 +0000

    Build/Test Tools: Revert copied style registry formatting.
    Revert [63883] so the generated style registry can be corrected upstream rather than reformatted during the copy into Core.
    
    See #65278.
    
    Built from https://develop.svn.wordpress.org/trunk@63919
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@63088 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63918';
+$wp_version = '7.2-alpha-63919';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
