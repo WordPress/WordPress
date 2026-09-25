8869c0a38c7539deb2b3b22aacedd3605f39de17
lancewillett <lancewillett@git.wordpress.org>
2026-09-25 14:47:42 +0000

    Tests: Cover REST icon context parameters and schema.
    Assert the default and allowed contexts for all three icon routes, and verify the four public item schema properties. Replace the empty tests and remove their doesNotPerformAssertions annotations.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13691
    
    Props shailu25.
    See #40538.
    
    Built from https://develop.svn.wordpress.org/trunk@63933
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@63099 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63932';
+$wp_version = '7.2-alpha-63933';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
