5a6eccf1758e81179efbf6dc346451916a0fa2ee
lancewillett <lancewillett@git.wordpress.org>
2026-09-25 15:01:52 +0000

    Tests: Use strict assertions across additional test directories.
    Replace loose comparisons with strict assertions where the expected types are known. Cast expected database IDs and serialized lengths to strings, and use float expectations for XPath counts.
    
    Correct the expected default idempotent ability annotation to null, matching the API's documented default. Preserve object-value comparisons where distinct instances are expected.
    
    Developed in: https://github.com/WordPress/wordpress-develop/pull/13756
    
    Props haritpanchal.
    See #64895.
    
    Built from https://develop.svn.wordpress.org/trunk@63934
    
    
    git-svn-id: http://core.svn.wordpress.org/trunk@63100 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.2-alpha-63933';
+$wp_version = '7.2-alpha-63934';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
